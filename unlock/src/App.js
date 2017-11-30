7bf4f47095a9c6cfea65db5abd49dbcaa6d01bd1
julien <julien.genestoux@gmail.com>
2017-11-30 18:11:41 -0500

    adding react app

---
+++ unlock/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Welcome to Unlcok</h2>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
