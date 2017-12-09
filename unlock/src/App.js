76830dd833a757fdd50a0dd1d98f3fe5526ac89c
julien <julien.genestoux@gmail.com>
2017-12-09 14:57:23 -0500

    first component for react app

--- unlock/src/App.js
+++ unlock/src/App.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import logo from './logo.svg'
+import './App.css'
+import Modal from './components/modal'
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Welcome to Unlcok</h2>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Modal/>
   }
 }
 
-export default App;
+export default App
