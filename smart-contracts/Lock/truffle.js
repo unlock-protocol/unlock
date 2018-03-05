6bb179fc8c5a343f6789531d1b6787e00751587b
julien <julien.genestoux@gmail.com>
2018-03-05 10:03:22 -0500

    adding the default dev config

--- smart-contracts/Lock/truffle.js
+++ smart-contracts/Lock/truffle.js
@@ -1,4 +1,12 @@
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
