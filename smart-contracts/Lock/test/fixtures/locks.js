49d6f064fceb3a1cbc1893a315225ce79953027c
julien <julien.genestoux@gmail.com>
2018-03-07 17:42:08 -0500

    refactoring for clarity

--- smart-contracts/Lock/test/fixtures/locks.js
+++ smart-contracts/Lock/test/fixtures/locks.js
@@ -1,9 +1,19 @@
 const Units = require('ethereumjs-units')
 
+// Note web3: the current version of web3 injected by Truffle is
+// old. web3.fromAscii will fail when this is updated
 module.exports = [
   {
-    lockId:
-      '0x7075626c69630000000000000000000000000000000000000000000000000000',
+    lockId: web3.fromAscii('FIRST LOCK'),
+    keyReleaseMechanism: 0, // KeyReleaseMechanisms.Public
+    expirationDuration: 60 * 60 * 24 * 30, // 30 days
+    expirationTimestamp: 0, // Not used
+    keyPriceCalculator: null, //
+    keyPrice: Units.convert(0.01, 'eth', 'wei'), // in wei
+    maxNumberOfKeys: 10
+  },
+  {
+    lockId: web3.fromAscii('PRIVATE LOCK'),
     keyReleaseMechanism: 0, // KeyReleaseMechanisms.Public
     expirationDuration: 60 * 60 * 24 * 30, // 30 days
     expirationTimestamp: 0, // Not used
