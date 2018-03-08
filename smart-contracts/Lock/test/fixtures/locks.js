7f394a5191707a0afe0a6596a9715d2f4921a6b2
julien <julien.genestoux@gmail.com>
2018-03-08 17:19:16 -0500

    better way to set locks

--- smart-contracts/Lock/test/fixtures/locks.js
+++ smart-contracts/Lock/test/fixtures/locks.js
@@ -1,24 +1,22 @@
 const Units = require('ethereumjs-units')
 
+let defaultLock = {
+  keyReleaseMechanism: 0, // KeyReleaseMechanisms.Public
+  expirationDuration: 60 * 60 * 24 * 30, // 30 days
+  expirationTimestamp: 0, // Not used
+  keyPriceCalculator: null, //
+  keyPrice: Units.convert(0.01, 'eth', 'wei'), // in wei
+  maxNumberOfKeys: 10
+}
+
 // Note web3: the current version of web3 injected by Truffle is
 // old. web3.fromAscii will fail when this is updated
 module.exports = [
-  {
-    lockId: web3.fromAscii('FIRST LOCK'),
-    keyReleaseMechanism: 0, // KeyReleaseMechanisms.Public
-    expirationDuration: 60 * 60 * 24 * 30, // 30 days
-    expirationTimestamp: 0, // Not used
-    keyPriceCalculator: null, //
-    keyPrice: Units.convert(0.01, 'eth', 'wei'), // in wei
-    maxNumberOfKeys: 10
-  },
-  {
+  Object.assign({}, defaultLock, {
+    lockId: web3.fromAscii('FIRST LOCK')
+  }),
+  Object.assign({}, defaultLock, {
     lockId: web3.fromAscii('PRIVATE LOCK'),
-    keyReleaseMechanism: 2, // KeyReleaseMechanisms.Private
-    expirationDuration: 60 * 60 * 24 * 30, // 30 days
-    expirationTimestamp: 0, // Not used
-    keyPriceCalculator: null, //
-    keyPrice: Units.convert(0.01, 'eth', 'wei'), // in wei
-    maxNumberOfKeys: 10
-  }
+    keyReleaseMechanism: 2 // KeyReleaseMechanisms.Private
+  })
 ]
