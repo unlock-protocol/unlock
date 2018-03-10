0f0b0e2cba656b90f552a55a00d1ed17a3c71d5f
julien <julien.genestoux@gmail.com>
2018-03-10 14:54:43 -0500

    using drizzle in order to interract with smart contracts

--- unlock-app/src/App.js
+++ unlock-app/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { DrizzleProvider } from 'drizzle-react'
+import logo from './logo.svg'
+import './App.css'
 
+// Import Unlock contract
+import Unlock from './artifacts/contracts/Unlock.json'
+
+const options = {
+  contracts: [Unlock],
+  events: [],
+  web3: {
+    block: false,
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:8545'
+    }
+  }
+}
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <DrizzleProvider options={options}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </DrizzleProvider>
+    )
   }
 }
 
-export default App;
+export default App
