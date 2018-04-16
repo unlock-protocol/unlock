4d7e403eebcba4e54afe42fe3a2e3adf5c035945
Julien Genestoux <julien.genestoux@gmail.com>
2018-04-16 11:42:34 -0400

    one file per component. split HasKey (#20)

--- unlock-app/src/components/consumer/Lock.js
+++ unlock-app/src/components/consumer/Lock.js
@@ -1,37 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Row, Col, Button } from 'reactstrap'
+import { Row, Col } from 'reactstrap'
+
+import HasKey from './HasKey'
 
 import { purchaseKey } from '../../actions/lock'
-
-const HasKey = (props) => {
-  const now = new Date().getTime() / 1000
-  if (props.currentKey.expiration === 0) {
-    return (<Col>
-      <Row>
-        <Col>
-          You need a key to access this content! Purchase one for {props.lock.keyPrice()}.
-        </Col>
-        <Col>
-          <Button color="primary" onClick={() => { props.purchaseKey(props.lock, props.account) }}>Purchase</Button>
-        </Col>
-      </Row>
-    </Col>)
-  } else if (props.currentKey.expiration < now) {
-    return (<Col>
-      <Row>
-        <Col>
-          Your key has expired! Purchase a new one for {props.lock.keyPrice()}.
-        </Col>
-        <Col>
-          <Button color="primary" onClick={() => { props.purchaseKey(props.lock, props.account) }}>Purchase</Button>
-        </Col>
-      </Row>
-    </Col>)
-  } else {
-    return (<p>Yes, your key expires at {props.currentKey.expiration}</p>)
-  }
-}
 
 const mapDispatchToProps = dispatch => ({
   purchaseKey: (lock, account) => dispatch(purchaseKey(lock, account))
