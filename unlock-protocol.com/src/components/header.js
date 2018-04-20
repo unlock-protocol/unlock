8884be1db82a693edca0622ee0ddafef13fabe97
Chris Van Patten <hello@chrisvanpatten.com>
2018-04-20 10:26:19 -0400

    [WIP] New Landing Page (#23)
    * add SVG assets and raster asset build script
    
    * add note about running svgexport directly
    
    * small typo
    
    * start setting up Gatsby
    
    * simplify the files and remove unneeded features

---
+++ unlock-protocol.com/src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = ({ siteTitle }) => (
+  <div>
+    {siteTitle}
+  </div>
+)
+
+export default Header
---
+++ unlock-protocol.com/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </div>
+)
+
+export default IndexPage
