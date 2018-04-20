55cdd9eff4ef8a05677f2c5284a6f5af26f7965e
Julien Genestoux <julien.genestoux@gmail.com>
2018-04-20 16:11:00 -0400

    Adding tests for redux actions (#28)
    * removed requestAccounts which was not used
    
    * adding tests for accounts
    
    * adding tests for lock actions
    
    * adding key actions

---
+++ unlock-app/src/__tests__/actions/lock.js
@@ -0,0 +1,30 @@
+import { createLock, setLock, resetLock, CREATE_LOCK, SET_LOCK, RESET_LOCK } from '../../actions/lock'
+
+describe('lock actions', () => {
+  it('should create an action to create a lock', () => {
+    const lock = {}
+    const expectedAction = {
+      type: CREATE_LOCK,
+      lock,
+    }
+    expect(createLock(lock)).toEqual(expectedAction)
+  })
+
+  it('should create an action to reset the lock', () => {
+    const lock = {}
+    const expectedAction = {
+      type: RESET_LOCK,
+      lock,
+    }
+    expect(resetLock(lock)).toEqual(expectedAction)
+  })
+
+  it('should create an action to set the lock', () => {
+    const lock = {}
+    const expectedAction = {
+      type: SET_LOCK,
+      lock,
+    }
+    expect(setLock(lock)).toEqual(expectedAction)
+  })
+})
--- unlock-app/src/actions/accounts.js
+++ unlock-app/src/actions/accounts.js
@@ -1,14 +1,9 @@
 export const ACCOUNTS_FETCHED = 'ACCOUNTS_FETCHED'
 export const SET_ACCOUNT = 'SET_ACCOUNT'
-export const REQUEST_ACCOUNTS = 'REQUEST_ACCOUNTS'
 
 export const setAccount = account => ({
   type: SET_ACCOUNT,
   account,
-})
-
-export const requestAccounts = account => ({
-  type: REQUEST_ACCOUNTS,
 })
 
 export const accountsFetched = (accounts) => ({
