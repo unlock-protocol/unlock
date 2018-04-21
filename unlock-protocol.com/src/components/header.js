91844c4a6641108bcbbece376623da8808d9dcab
Chris Van Patten <chris@vanpattenmedia.com>
2018-04-21 09:45:34 -0400

    add initial draft of website

--- unlock-protocol.com/src/components/header.js
+++ unlock-protocol.com/src/components/header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from './../assets/images/logo.svg'
 
 const Header = ({ siteTitle }) => (
-  <div>
-    {siteTitle}
-  </div>
+  <section className="page__left">
+    <img src={logo} alt={siteTitle} className="logo" />
+  </section>
 )
 
 export default Header
--- unlock-protocol.com/src/pages/index.js
+++ unlock-protocol.com/src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import OptinForm from '../components/OptinForm'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </div>
+  <section className="page__right">
+    <div className="page__description">
+      <p>Tempor incididunt, onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br /><br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div className="page__form">
+      <OptinForm />
+    </div>
+  </section>
 )
 
 export default IndexPage
