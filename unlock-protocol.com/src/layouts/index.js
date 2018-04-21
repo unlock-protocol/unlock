83fc1a7b83703e7769e4990131f3691543c2e01e
Chris Van Patten <hello@chrisvanpatten.com>
2018-04-21 11:16:11 -0400

    Unlock landing page (#31)
    * add SVG assets and raster asset build script
    
    * add note about running svgexport directly
    
    * small typo
    
    * start setting up Gatsby
    
    * simplify the files and remove unneeded features
    
    * add initial draft of website
    
    * add some text from the medium post as an example

--- unlock-protocol.com/src/layouts/index.js
+++ unlock-protocol.com/src/layouts/index.js
@@ -2,18 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
-import './../scss/index.scss'
+import Header from '../components/Header'
+import './../assets/scss/index.scss'
 
 const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+  <div className="page">
+    <Helmet>
+      <title>{data.site.siteMetadata.title}</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+    </Helmet>
+    <Header siteTitle={data.site.siteMetadata.title} />
     {children()}
   </div>
 )
--- unlock-protocol.com/src/pages/index.js
+++ unlock-protocol.com/src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import OptinForm from '../components/OptinForm'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </div>
+  <section className="page__right">
+    <div className="page__description">
+      <p>Unlock lets creators sell access to their creations. The price, scope and other terms are set by the creators themselves and the transactions happen on the blockchain, without middlemen.</p>
+    </div>
+    <div className="page__form">
+      <OptinForm />
+    </div>
+  </section>
 )
 
 export default IndexPage
