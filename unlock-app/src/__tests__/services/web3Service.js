91065e54591390c8357b95e71f94330da8eeeb4e
julien <julien.genestoux@gmail.com>
2018-04-23 17:38:55 -0400

    adding webb3Service tests

---
+++ unlock-app/src/__tests__/services/web3Service.js
@@ -0,0 +1,36 @@
+// Note, we use name import to import the non connected version of the component for testing
+import web3Service from '../../services/web3Service'
+
+describe('Web3Service', () => {
+
+  describe('initWeb3Service', () => {
+    it('should handle new blocks')
+  })
+
+  describe('once initialized', () => {
+    describe('createLock', () => {
+      it('should create a new lock with the params provided', () => {
+        web3Service.createLock()
+      })
+      it('should dispatch setLock once it has been successfuly created')
+      it('should handle failures if the lock could not be created')
+    })
+
+    describe('getLock', () => {
+      it('should dispatch setLock once the lock has been loaded')
+      it('should dispatch setLock with the right properties')
+      it('should dispatch resetLock once the properties have been reloaded')
+      it('should handle failures')
+    })
+
+    describe('purchaseKey', () => {
+
+    })
+
+    describe('getKey', () => {
+
+    })
+
+  })
+
+})
