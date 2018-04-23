ef772b8447ec1454420c8175079b0bdbe8d2fcc2
julien <julien.genestoux@gmail.com>
2018-04-23 10:34:17 -0400

    using the form from campaign monitor to actually collect emails

--- unlock-protocol.com/src/components/OptinForm.js
+++ unlock-protocol.com/src/components/OptinForm.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const OptinForm = () => (
-  <form action="/" className="subscribe">
-    <input type="email" className="subscribe__input" placeholder="Subscribe to be notified when we launch" />
-    <input type="submit" className="subscribe__button" value="" />
-  </form>
+  <div>
+    <form id="subForm" action="/" className="subscribe js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="A61C50BEC994754B1D79C5819EC1255C24DC9BD24F2D62D9FE1BE9C2DC3506D10407F5BF6B525B95AC41E626C05091706888A584548167E6B31CDBA36E66DB9D">
+      <input id="fieldEmail" name="cm-skyhlr-skyhlr" type="email" className="subscribe__input js-cm-email-input" placeholder="Subscribe to be notified when we launch" required />
+      <input type="submit" className="subscribe__button js-cm-submit-button" value="" />
+    </form>
+    <script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+  </div>
 )
 
 export default OptinForm
