fe79b9f0c0ab1b27b30d9485673b18722ed72279
Julien Genestoux <julien.genestoux@gmail.com>
2018-04-24 12:00:40 -0400

    Optimizing static site for speed (#35)
    * compressed images
    
    * using normalize from a npm module to not block rendernng

--- unlock-protocol.com/src/layouts/index.js
+++ unlock-protocol.com/src/layouts/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import 'normalize.css'
+
 import Header from '../components/Header'
 import './../assets/scss/index.scss'
 
@@ -9,7 +11,6 @@ const Layout = ({ children, data }) => (
   <div className="page">
     <Helmet>
       <title>{data.site.siteMetadata.title}</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
     </Helmet>
     <Header siteTitle={data.site.siteMetadata.title} />
     {children()}
