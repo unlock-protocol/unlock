05c315ff561b127006489a929b534e8ef247fbe1
Julien Genestoux <julien.genestoux@gmail.com>
2018-04-30 09:19:31 -0400

    linking to medium story (#43)
    * changed text on landing page
    
    * adding link to medium story about unlocl

--- unlock-protocol.com/src/pages/index.js
+++ unlock-protocol.com/src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
   <section className="page__right">
     <div className="page__description">
       <p>Unlock is an <i>access control protocol</i> built on a blockchain. It enables creators to monetize their content or software without relying on a middleman. It lets consumers manage all of their subscriptions in a consistent way, as well as earn discounts when they share the best content and applications they use.</p>
+      <p><a href="https://medium.com/@julien51/its-time-to-unlock-the-web-b98e9b94add1">Learn more...</a></p>
     </div>
     <div className="page__form">
       <OptinForm />
