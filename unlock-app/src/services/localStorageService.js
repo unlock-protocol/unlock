44b7ddf94fe622f1350cb93de55adaaaf41ef978
julien <julien.genestoux@gmail.com>
2018-05-02 16:22:52 -0400

    adding localStorage persistence

---
+++ unlock-app/src/services/localStorageService.js
@@ -0,0 +1,20 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined // TODO: warn user
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    return undefined // TODO: warn user
+  }
+}
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (err) {
+    // TODO log/warn user
+  }
+}
