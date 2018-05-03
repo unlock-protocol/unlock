fc0f4686b688140d647ae628e777dd1769459b68
Chris Van Patten <hello@chrisvanpatten.com>
2018-05-03 10:07:19 -0400

    Add new website components and refactor CSS (#49)
    * add new components and component styles, split up scss
    
    * add some missing classNames

--- unlock-protocol.com/src/pages/index.js
+++ unlock-protocol.com/src/pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+
+import Button from '../components/Button'
+import Column from '../components/Column'
+import Columns from '../components/Columns'
 import OptinForm from '../components/OptinForm'
 
 const IndexPage = () => (
   <section className="page__right">
     <div className="page__description">
       <p>Unlock is an <i>access control protocol</i> built on a blockchain. It enables creators to monetize their content or software without relying on a middleman. It lets consumers manage all of their subscriptions in a consistent way, as well as earn discounts when they share the best content and applications they use.</p>
-      <p><a href="https://medium.com/@julien51/its-time-to-unlock-the-web-b98e9b94add1">Learn more...</a></p>
+      <p>
+        <Button className="button--default" href="https://medium.com/@julien51/its-time-to-unlock-the-web-b98e9b94add1" text="Learn more..." />&nbsp;
+      </p>
     </div>
+
     <div className="page__form">
       <OptinForm />
     </div>
