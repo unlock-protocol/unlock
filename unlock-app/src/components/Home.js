0bfd0f639f9525b82b51e75aa05c5834d1dd743c
julien <julien.genestoux@gmail.com>
2018-05-07 15:05:24 -0400

    better ui

--- unlock-app/src/components/Home.js
+++ unlock-app/src/components/Home.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div className="container">
+    <div>
+      <header className="masthead mb-auto">
+        <div className="inner">
+          <h3 className="masthead-brand">&nbsp;</h3>
+          <nav className="nav nav-masthead justify-content-center">
+          </nav>
+        </div>
+      </header>
       <div className="jumbotron">
         <h1 className="display-4">Unlock</h1>
         <p className="lead">Unlock is an access control protocol built on a blockchain.
@@ -12,11 +19,12 @@ export const Home = () => {
         <p>It enables creators to monetize their content or software without relying on a middleman. It lets consumers manage all of their subscriptions in a consistent way, as well as earn discounts when they share the best content and applications they use.</p>
         <p className="lead">
           <Link className="btn btn-primary btn-lg" href="#" role="button" to={'/creator'}>
-          Create your lock
+            Create your lock
           </Link>
         </p>
       </div>
-    </div>)
+    </div>
+  )
 }
 
 export default Home
