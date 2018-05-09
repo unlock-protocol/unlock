6f09cd3aaf4a4319120fecea04903bc90365fba4
Julien Genestoux <julien.genestoux@gmail.com>
2018-05-09 18:28:30 -0400

    better ui (#59)

--- unlock-app/src/components/Unlock.js
+++ unlock-app/src/components/Unlock.js
@@ -7,7 +7,7 @@ import Home from './Home'
 
 export const Unlock = () => {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <Switch>
         <Route path="/creator" component={LockMaker} />
         <Route path="/lock/:lockAddress" component={Lock} />
