19ca9a926eeadd993ddf0f93ba032f3ed3d7532b
Julien Genestoux <julien.genestoux@gmail.com>
2018-05-08 22:09:00 -0400

    adding the withdraw function from locks (#57)

--- smart-contracts/test/Unlock.js
+++ smart-contracts/test/Unlock.js
@@ -221,5 +221,53 @@ contract('Unlock', (accounts) => {
         it('should succeed if the sending account was pre-approved')
       })
     })
+
+    describe('withdraw', () => {
+      let owner
+      let price = Units.convert('0.01', 'eth', 'wei')
+
+      before(() => {
+        const purchases = [accounts[1], accounts[2]].map((account) => {
+          return locks['OWNED'].purchase('', {
+            value: price,
+            from: account
+          })
+        })
+        return Promise.all(purchases)
+          .then(() => {
+            return locks['OWNED'].owner.call()
+          })
+          .then((_owner) => {
+            owner = _owner
+          })
+      })
+
+      it('should only allow the owner to withdraw', () => {
+        assert.notEqual(owner, accounts[1]) // Making sure
+        return locks['OWNED'].withdraw({
+          from: accounts[1]
+        }).catch(error => {
+          assert.equal(error.message, 'VM Exception while processing transaction: revert')
+        })
+      })
+
+      describe('when the owner withdraws funds', () => {
+        let ownerBalance, lockBalance
+        before(() => {
+          lockBalance = web3.eth.getBalance(locks['OWNED'].address)
+          ownerBalance = web3.eth.getBalance(owner)
+          return locks['OWNED'].withdraw({
+            from: owner
+          })
+        })
+        it('should increase the owner\'s balance with the funds from the lock', () => {
+          assert(web3.eth.getBalance(owner) > ownerBalance)
+        })
+
+        it('should set the lock\'s balance to 0', () => {
+          assert.equal(web3.eth.getBalance(locks['OWNED'].address), 0)
+        })
+      })
+    })
   })
 })
