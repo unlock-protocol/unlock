33ff8685ee29afda6913e0ccff5bf00a4e021c0d
julien <julien.genestoux@gmail.com>
2018-05-12 22:30:00 -0400

    adding the interface to change the network

--- unlock-app/src/components/Unlock.js
+++ unlock-app/src/components/Unlock.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router'
 import LockMaker from './creator/LockMaker'
 import Lock from './consumer/Lock'
 import Home from './Home'
+import Network from './Network'
 
 export const Unlock = () => {
   return (
     <div className="container">
       <Switch>
+        <Route path="/network" component={Network} />
         <Route path="/creator" component={LockMaker} />
         <Route path="/lock/:lockAddress" component={Lock} />
         <Route path="*" component={Home} />
