f83443f8594761d45df1133e806a1f12d522a0ff
Julien Genestoux <julien.genestoux@gmail.com>
2018-06-13 15:34:21 -0400

    refactored tests (#115)

--- smart-contracts/test/Unlock.js
+++ smart-contracts/test/Unlock.js
@@ -1,8 +1,5 @@
-const Units = require('ethereumjs-units')
-
 const deployLocks = require('./helpers/deployLocks')
 const Unlock = artifacts.require('./Unlock.sol')
-const zeroHex = '0x0000000000000000000000000000000000000000'
 
 contract('Unlock', (accounts) => {
   let unlock, locks
@@ -19,43 +16,11 @@ contract('Unlock', (accounts) => {
       })
   })
 
-  it('should have created locks', () => {
+  it('should have created locks with a link to Unlock', () => {
     const lock = locks['FIRST']
-    return Promise.all([
-      lock.owner.call(),
-      lock.keyReleaseMechanism.call(),
-      lock.expirationDuration.call(),
-      lock.expirationTimestamp.call(),
-      lock.keyPriceCalculator.call(),
-      lock.keyPrice.call(),
-      lock.maxNumberOfKeys.call(),
-      lock.outstandingKeys.call()
-    ]).then(
-      ([
-        owner,
-        keyReleaseMechanism,
-        expirationDuration,
-        expirationTimestamp,
-        keyPriceCalculator,
-        keyPrice,
-        maxNumberOfKeys,
-        outstandingKeys
-      ]) => {
-        assert.strictEqual(owner, accounts[0])
-        assert.strictEqual(keyReleaseMechanism.toNumber(), 0)
-        assert.strictEqual(
-          expirationDuration.toNumber(),
-          60 * 60 * 24 * 30
-        )
-        assert.strictEqual(expirationTimestamp.toNumber(), 0)
-        assert.strictEqual(keyPriceCalculator, zeroHex)
-        assert.strictEqual(
-          Units.convert(keyPrice.toNumber(), 'wei', 'eth'),
-          '0.01'
-        )
-        assert.strictEqual(maxNumberOfKeys.toNumber(), 10)
-        assert.strictEqual(outstandingKeys.toNumber(), 0)
-      }
-    )
+    return lock.unlockProtocol()
+      .then((unlockProtocol) => {
+        assert.equal(unlockProtocol, unlock.address)
+      })
   })
 })
