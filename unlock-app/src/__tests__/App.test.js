e602a8d01741d07afa37f6139b38a0f4d29b10ca
Julien Genestoux <julien.genestoux@gmail.com>
2018-06-28 11:33:56 -0400

    adding tests for Unlock component (#127)

--- unlock-app/src/__tests__/App.test.js
+++ unlock-app/src/__tests__/App.test.js
@@ -2,15 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
 
-// Mock the provider
-jest.mock('web3', () => {
-  return {
-    providers: {
-      WebsocketProvider: class WebsocketProvider {},
-    },
-  }
-})
-
 // Mock the service
 jest.mock('../services/web3Service', () => {
   return (function() {
