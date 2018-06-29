51928f90bb21b66026c929af7fa2f061c8025dfc
julien <julien.genestoux@gmail.com>
2018-06-29 16:11:33 -0400

    refactored a lot of solitidy code

--- smart-contracts/test/Lock/expireKeyFor.js
+++ smart-contracts/test/Lock/expireKeyFor.js
@@ -1,14 +1,9 @@
-
-const Units = require('ethereumjs-units')
-const Web3Utils = require('web3-utils')
-
 const deployLocks = require('../helpers/deployLocks')
 const Unlock = artifacts.require('../Unlock.sol')
 
 let unlock, locks
 
 contract('Lock', (accounts) => {
-  // Let's build the locks
   before(() => {
     return Unlock.deployed()
       .then(_unlock => {
