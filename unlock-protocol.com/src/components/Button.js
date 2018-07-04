0fd791598a227aaf0c6a526139bacb5fa1d73b14
Julien Genestoux <julien.genestoux@gmail.com>
2018-07-04 18:58:59 -0400

    adding icons to telegram and github (#132)

--- unlock-protocol.com/src/components/Button.js
+++ unlock-protocol.com/src/components/Button.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Button = (props) => (
-  <a className={props.className + ' button'} href={props.href}>{props.text}</a>
+  <a className={props.className + ' button'} href={props.href}>
+    {props.children &&
+      <i className="icon">
+        {props.children}
+      </i>
+    }
+    {props.text}
+  </a>
 )
 
 export default Button
