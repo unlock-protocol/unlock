4f5581d05c983653e4b6007831f453e7a662657a
julien <julien.genestoux@gmail.com>
2018-07-05 21:50:24 -0400

    running linting for solidity

--- tests.sh
+++ tests.sh
@@ -5,6 +5,7 @@ cd /home/unlock/smart-contracts
 # missing npm module from the truffle/ci image
 npm install
 truffle test --network test
+npm run lint
 
 # React tests && linting
 cd /home/unlock/unlock-app
