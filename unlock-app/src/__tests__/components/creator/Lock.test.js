8b88e7d38c43200167ac43888f9660379cc838af
Julien Genestoux <julien.genestoux@gmail.com>
2018-07-11 12:08:42 -0400

    adding missing tests for for LockOwner and KeyReleaseMechanism (#140)

--- unlock-app/src/__tests__/components/creator/Lock.test.js
+++ unlock-app/src/__tests__/components/creator/Lock.test.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 // Note, we use name import to import the non connected version of the component for testing
-import { WithdrawButton } from '../../../components/creator/Lock'
+import { WithdrawButton, KeyReleaseMechanism, LockOwner } from '../../../components/creator/Lock'
 
 describe('Lock', () => {
 
 })
 
 describe('LockOwner', () => {
+  const account = {
+    address: '0x123',
+  }
+  const owner = '0xabc'
 
+  it('should show the address of the lock owner', () => {
+    const wrapper = shallow(<LockOwner owner={owner} account={account} />)
+    expect(wrapper.text()).toEqual('0xabc')
+  })
+
+  it('should show "me" if the current user is the lock owner', () => {
+    const wrapper = shallow(<LockOwner owner={account.address} account={account} />)
+    expect(wrapper.text()).toEqual('Me 0x123')
+  })
 })
 
 describe('WithdrawButton', () => {
@@ -47,5 +60,18 @@ describe('WithdrawButton', () => {
 })
 
 describe('KeyReleaseMechanism', () => {
+  it('should display Public when the mechanism is 0', () => {
+    const wrapper = shallow(<KeyReleaseMechanism mechanism={'0'} />)
+    expect(wrapper.text()).toEqual('Public')
+  })
 
+  it('should display Permissioned when the mechanism is 1', () => {
+    const wrapper = shallow(<KeyReleaseMechanism mechanism={'1'} />)
+    expect(wrapper.text()).toEqual('Permissioned')
+  })
+
+  it('should display Private when the mechanism is 2', () => {
+    const wrapper = shallow(<KeyReleaseMechanism mechanism={'2'} />)
+    expect(wrapper.text()).toEqual('Private')
+  })
 })
