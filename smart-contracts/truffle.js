c98fd679c30b1b78d9063395ca54d222091a3243
Julien Genestoux <julien.genestoux@gmail.com>
2018-07-14 19:42:49 -0400

    deployed on rinkeby (#149)

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -24,6 +24,15 @@ module.exports = {
       host: 'ganache', // This is because we use docker-compose and that is the name of the service which runs ganache
       port: 8545, // We use ganache-cli and this is its default port
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      // used to run tests in docker (ci)
+      host: '127.0.0.1', // This will require us to deploy from a running geth node connected to rinkeby (see docs)
+      port: 8545, // We use ganache-cli and this is its default port
+      from: '0x3ca206264762caf81a8f0a843bbb850987b41e16', // Account which has a positive of eth
+      network_id: '*', // Network Id for Rinkeby
+      gas: 4712388,
+      gasPrice: 100000000000
     }
   },
   solc: {
