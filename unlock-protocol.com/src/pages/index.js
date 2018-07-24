74a17655bf034188254d47677873fde129ba3d19
julien <julien.genestoux@gmail.com>
2018-07-24 06:23:58 -0700

    linking to about page and to jobs page

--- unlock-protocol.com/src/pages/index.js
+++ unlock-protocol.com/src/pages/index.js
@@ -14,13 +14,8 @@ const IndexPage = () => (
         Unlock is an <strong>access control protocol</strong> built on a blockchain. It enables creators to monetize their content or software without relying on a middleman. It lets consumers manage all of their subscriptions in a consistent way, as well as earn discounts when they share the best content and applications they use.
       </p>
       <Buttons>
-        <Button className="button--default" href="https://medium.com/@julien51/its-time-to-unlock-the-web-b98e9b94add1" text="Learn more..." />
-        <Button className="button--default" href="https://github.com/unlock-protocol" text="Source Code">
-          <FontAwesome.FaGithub />
-        </Button>
-        <Button className="button--default" href="https://t.me/unlockprotocol" text="Telegram">
-          <FontAwesome.FaPaperPlane />
-        </Button>
+        <Button className="button--default" href="/about" text="Learn more..." />
+        <Button className="button--default" href="/jobs" text="Join us!" />
       </Buttons>
       <div className="page__form">
         <OptinForm />
