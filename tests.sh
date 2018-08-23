914fc5139e579712cffe0f380f8704f560867eff
Julien Genestoux <julien.genestoux@gmail.com>
2018-08-23 17:00:27 -0400

    Adding an explicit exit 1 to make sure that CI stops when tests are failing. Fixes #194 (#196)

--- tests.sh
+++ tests.sh
@@ -4,7 +4,9 @@
 cd /home/unlock/smart-contracts
 # missing npm module from the truffle/ci image
 npm install
-truffle test --network test
+
+# Note: explicit || exit 1 is important (but brittle) https://github.com/unlock-protocol/unlock/issues/194
+truffle test --network test || exit 1
 npm run lint
 
 # React tests && linting
