74bf33afcd16fdf28210aabea016a3befeef9e2e
Julien Genestoux <julien.genestoux@gmail.com>
2018-08-24 21:26:48 -0400

    fixing the Account component (#198)

--- unlock-app/src/__tests__/components/Account.test.js
+++ unlock-app/src/__tests__/components/Account.test.js
@@ -10,15 +10,10 @@ describe('Account Component', () => {
     balance: '1000',
   }
   const showAccountPicker = jest.fn()
-  const useMetamask = jest.fn()
-  const config = {
-    web3Available: true,
-  }
 
   const wrapper = shallow(<Account
     account={account}
-    showAccountPicker={showAccountPicker}
-    config={config} />)
+    showAccountPicker={showAccountPicker} />)
 
   it('shows the current account\'s public key', () => {
     expect(wrapper.find('span').first().text()).toEqual('0xdeadbeef')
@@ -30,65 +25,11 @@ describe('Account Component', () => {
     })
   })
 
-  describe('when metamask is available', () => {
-    describe('when metamask is being used', () => {
-      const config = { web3Available: true }
-      const metamaskUsedWrapper = shallow(<Account
-        config={config}
-        isMetamask={true}
-        account={account}
-        showAccountPicker={showAccountPicker} />)
-
-      it('should not show a switch account button', () => {
-        const button = metamaskUsedWrapper.find('button.js-accountSwitch')
-        expect(button.exists()).toEqual(false)
-      })
-    })
-
-    describe('when metamask is not being used', () => {
-      const config = { web3Available: true }
-      const wrapperMetamaskNotUsed = shallow(<Account
-        useMetamask={useMetamask}
-        config={config}
-        isMetamask={false}
-        account={account}
-        showAccountPicker={showAccountPicker} />)
-
-      it('shows a button to use metamask', () => {
-        const button = wrapperMetamaskNotUsed.find('a.js-accountUseMetamask')
-        expect(button).toExist()
-
-        expect(button.contains(<img src="/images/icons/icon-metamask.png" className="icon" />)).toBe(true)
-
-        button.simulate('click')
-        expect(useMetamask).toBeCalledWith()
-      })
-
-      it('shows a button to switch accounts', () => {
-        const button = wrapperMetamaskNotUsed.find('button.js-accountSwitch')
-        expect(button.text()).toEqual('Switch')
-        button.simulate('click')
-        expect(showAccountPicker).toBeCalledWith()
-      })
-    })
-  })
-
-  describe('when metamask is not available', () => {
-    const config = { web3Available: true }
-    const wrapperMetamaskNotAvailable = shallow(<Account
-      config={config}
-      account={account}
-      showAccountPicker={showAccountPicker} />)
-
-    it('shows no metamask button')
-
-    it('shows a button to switch accounts', () => {
-      const button = wrapperMetamaskNotAvailable.find('button.js-accountSwitch')
-      expect(button.text()).toEqual('Switch')
-      button.simulate('click')
-      expect(showAccountPicker).toBeCalledWith()
-    })
-
+  it('shows a button to switch accounts', () => {
+    const button = wrapper.find('button.js-accountSwitch')
+    expect(button.text()).toEqual('Switch')
+    button.simulate('click')
+    expect(showAccountPicker).toBeCalledWith()
   })
 
 })
--- unlock-app/src/components/Account.js
+++ unlock-app/src/components/Account.js
@@ -5,27 +5,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Balance from './helpers/Balance'
 
-import { setNetwork } from '../actions/network'
-
-import { withConfig } from '../utils/withConfig'
-
-export function Account({ account, isMetamask, showAccountPicker, useMetamask, config }) {
+export function Account({ account, showAccountPicker }) {
   return (
     <div className="container">
       <div className="row align-items-center">
         <span className="col-6 text-truncate">{account.address}</span>
         <span className="col text-muted text-right text-truncate"><Balance amount={account.balance} /></span>
-        {!isMetamask &&
         <span className="col-2">
           <button className="btn btn-small btn-outline-secondary js-accountSwitch" onClick={(event) => showAccountPicker()} type="button">Switch</button>
         </span>
-        }
-        {!isMetamask && config.web3Available &&
-          <span className="col-1 text-right">
-            <a className="js-accountUseMetamask" onClick={(event) => useMetamask()}><img src="/images/icons/icon-metamask.png" className="icon" alt="Use Metamask" /></a>
-          </span>
-
-        }
       </div>
     </div>
   )
@@ -34,20 +22,12 @@ export function Account({ account, isMetamask, showAccountPicker, useMetamask, c
 Account.propTypes = {
   account: UnlockPropTypes.account,
   showAccountPicker: PropTypes.func,
-  isMetamask: PropTypes.bool,
-  useMetamask: PropTypes.func,
-  config: UnlockPropTypes.configuration,
 }
 
 const mapStateToProps = state => {
   return {
-    isMetamask: state.network.name === 'metamask',
     account: state.network.account,
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  useMetamask: account => dispatch(setNetwork('metamask')),
-})
-
-export default withConfig(connect(mapStateToProps, mapDispatchToProps)(Account))
+export default connect(mapStateToProps)(Account)
