1b83ef5939a9d3405f3618f97ee50af949683111
Julien Genestoux <julien.genestoux@gmail.com>
2018-08-24 21:44:14 -0400

    adding explicit || exit 1 on all assertions in test.sh to make sure we exit when tests fail (#199)

--- tests.sh
+++ tests.sh
@@ -1,17 +1,18 @@
 # Runs all the tests!
 
+# Note: explicit || exit 1 is important (but brittle) https://github.com/unlock-protocol/unlock/issues/194
+
 # Solidity tests
 cd /home/unlock/smart-contracts
 # missing npm module from the truffle/ci image
 npm install
 
-# Note: explicit || exit 1 is important (but brittle) https://github.com/unlock-protocol/unlock/issues/194
 truffle test --network test || exit 1
-npm run lint
+npm run lint || exit 1
 
 # React tests && linting
 cd /home/unlock/unlock-app
 npm install
-CI=1 npm test
-npm run lint
+CI=1 npm test || exit 1
+npm run lint || exit 1
 
