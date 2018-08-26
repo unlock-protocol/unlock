b8e987dd2318235689fa38fbe2a3e31695c8dc1c
julien <julien.genestoux@gmail.com>
2018-08-25 21:00:34 -0400

    using null for the deafult provider value in the reducer

--- unlock-app/src/__tests__/reducers/providerReducer.test.js
+++ unlock-app/src/__tests__/reducers/providerReducer.test.js
@@ -5,8 +5,15 @@ describe('provider reducer', () => {
 
   const provider = 'WebSocket'
 
-  it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual('HTTP')
+  it('should return the initial state as null if there are no reducers', () => {
+    /**
+     * It is important that the default state is null and not undefined per this Redux error:
+     * Error: Reducer "provider" returned undefined during initialization.If the state passed
+     * to the reducer is undefined, you must explicitly return the initial state.The initial
+     * state may not be undefined.If you don't want to set a value for this reducer, you can
+     * use null instead of undefined.
+     */
+    expect(reducer(null, {})).toEqual(null)
   })
 
   it('should set the provider accordingly when receiving SET_PROVIDER', () => {
