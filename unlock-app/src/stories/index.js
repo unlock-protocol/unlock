87d5e6b9c2dfef16db94d6eac6ac18d0707322eb
Julien Genestoux <julien.genestoux@gmail.com>
2018-09-04 14:43:26 -0400

    getting started with storybook (#220)

---
+++ unlock-app/src/stories/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { NetworkBadge } from '../components/interface/NetworkBadge'
+
+storiesOf('NetworkBadge')
+  .add('with dev network', () => {
+    const network = {
+      name: '7331',
+    }
+    return (
+      <NetworkBadge network={network} />
+    )
+  })
+  .add('with rinkeby network', () => {
+    const network = {
+      name: '4',
+    }
+    return (
+      <NetworkBadge network={network} />
+    )
+  })
+  .add('with main network', () => {
+    const network = {
+      name: '1',
+    }
+    return (
+      <NetworkBadge network={network} />
+    )
+  })
