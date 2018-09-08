c1309e0d5ce8ccaf66adbb9d328a3683fcd30e05
Ben Werdmuller <ben@withknown.com>
2018-09-08 08:22:20 -0700

    Stories for Account and Authenticate (#232)

---
+++ unlock-app/src/stories/Authenticate.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Authenticate } from '../components/Authenticate'
+
+let dummyFunction = () => {
+  return false // We don't need this to do anything in the storybook context
+}
+
+storiesOf('Authenticate')
+  .add('Default', () => {
+    return (
+      <Authenticate hideAccountPicker={dummyFunction} loadAccount={dummyFunction} />
+    )
+  })
