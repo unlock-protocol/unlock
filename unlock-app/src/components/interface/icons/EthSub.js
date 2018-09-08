9cf1568cef42cd5ac069192c360abd71c6286a61
julien <julien.genestoux@gmail.com>
2018-09-08 18:29:05 +0200

    adding a script to generate react components from svg files

---
+++ unlock-app/src/components/interface/icons/EthSub.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EthSub = props => (
+  <svg viewBox="0 0 7 11" width="1em" height="1em" {...props}>
+    <path
+      d="M1 6V5h5v1H1zm-1 5v-1h7v1H0zM0 1V0h7v1H0z"
+      fill="#a6a6a6"
+      fillRule="evenodd"
+    />
+  </svg>
+)
+
+export default EthSub
