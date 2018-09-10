6e4aeb170a48eed073da88c8d1c9ddef69d56659
Julien Genestoux <julien.genestoux@gmail.com>
2018-09-10 18:46:33 +0200

    Adding Icons and their stories (#235)
    * Revert "adding a way to build svg sprite (#226)"
    
    This reverts commit 3e7c6cc1c2a1de72ecb30796e4d00c184f50afe8.
    
    * Adding Icons and their stories

---
+++ unlock-app/src/components/interface/icons/index.js
@@ -0,0 +1,33 @@
+import About from './About'
+import Code from './Code'
+import Copy from './Copy'
+import Download from './Download'
+import Edit from './Edit'
+import Eth from './Eth'
+import Etherscan from './Etherscan'
+import EthSub from './EthSub'
+import Export from './Export'
+import Github from './Github'
+import Infinity from './Infinity'
+import Jobs from './Jobs'
+import Unlock from './Unlock'
+import Upload from './Upload'
+import Withdraw from './Withdraw'
+
+export default {
+  About,
+  Code,
+  Copy,
+  Download,
+  Edit,
+  Eth,
+  Etherscan,
+  EthSub,
+  Export,
+  Github,
+  Infinity,
+  Jobs,
+  Unlock,
+  Upload,
+  Withdraw,
+}
