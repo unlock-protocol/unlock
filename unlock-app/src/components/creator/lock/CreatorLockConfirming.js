94997878f13ce73a8b163f5e5a7bc1a46335e3d6
Ben Werdmuller <ben@benwerd.com>
2018-09-13 15:21:55 -0700

    Splitting creator lock components into separate files

---
+++ unlock-app/src/components/creator/lock/CreatorLockConfirming.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import UnlockPropTypes from '../../../propTypes'
+import Icon from '../../lock/Icon'
+import Duration from '../../helpers/Duration'
+import Icons from '../../interface/icons'
+import Balance from '../../helpers/Balance'
+import {
+  LockIcon,
+  LockName,
+  LockAddress,
+  LockDuration,
+  LockKeys,
+  LockValue,
+  LockValueEth,
+  LockCurrency,
+  LockValueMain,
+  LockStatus,
+  LockStatusLabel } from './styles'
+
+export function CreatorLockConfirming({ lock }) {
+  // Some sanitization of strings to display
+  let name = lock.name || 'New Lock'
+  let outstandingKeys = lock.maxNumberOfKeys - lock.outstandingKeys || 0
+
+  return (
+    <LockConfirming>
+      <LockIcon><Icon lock={lock} address={lock.address} size={'3'} /></LockIcon>
+      <LockName>
+        {name}
+        <LockAddress>{lock.address}</LockAddress>
+      </LockName>
+      <LockDuration>
+        <Duration seconds={lock.expirationDuration} />
+      </LockDuration>
+      <LockKeys>{outstandingKeys} / {lock.maxNumberOfKeys}</LockKeys>
+      <LockValue>
+        <LockValueEth><LockCurrency><Icons.Eth /></LockCurrency> <Balance amount={lock.keyPrice} symbol={false} /></LockValueEth>
+      </LockValue>
+      <LockValue>
+        <LockValueMain><LockValueEth><LockCurrency><Icons.Eth /></LockCurrency> <Balance amount={lock.balance} symbol={false} /></LockValueEth></LockValueMain>
+      </LockValue>
+      <LockStatus>
+        <LockStatusLabel>
+          Pending
+        </LockStatusLabel>
+      </LockStatus>
+    </LockConfirming>)
+}
+
+CreatorLockConfirming.propTypes = {
+  lock: UnlockPropTypes.lock,
+}
+
+export default CreatorLockConfirming
+
+const LockConfirming = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr repeat(3, 1fr) 2fr 1fr;
+`
---
+++ unlock-app/src/components/creator/lock/CreatorLockSaved.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import styled from 'styled-components'
+import UnlockPropTypes from '../../../propTypes'
+import Icon from '../../lock/Icon'
+import Duration from '../../helpers/Duration'
+import Icons from '../../interface/icons'
+import Balance from '../../helpers/Balance'
+import {
+  LockIcon,
+  LockName,
+  LockAddress,
+  LockDuration,
+  LockKeys,
+  LockValue,
+  LockValueEth,
+  LockCurrency,
+  LockValueMain,
+  LockIconBar,
+  LockIconBarIcon } from './styles'
+
+export function CreatorLockSaved({ lock }) {
+  // Some sanitization of strings to display
+  let name = lock.name || 'New Lock'
+  let outstandingKeys = lock.maxNumberOfKeys - lock.outstandingKeys || 0
+
+  return (
+    <LockSaved>
+      <LockIcon><Icon lock={lock} address={lock.address} size={'3'} /></LockIcon>
+      <LockName>
+        {name}
+        <LockAddress>{lock.address}</LockAddress>
+      </LockName>
+      <LockDuration>
+        <Duration seconds={lock.expirationDuration} />
+      </LockDuration>
+      <LockKeys>{outstandingKeys} / {lock.maxNumberOfKeys}</LockKeys>
+      <LockValue>
+        <LockValueEth><LockCurrency><Icons.Eth /></LockCurrency> <Balance amount={lock.keyPrice} symbol={false} /></LockValueEth>
+      </LockValue>
+      <LockValue>
+        <LockValueMain><LockValueEth><LockCurrency><Icons.Eth /></LockCurrency> <Balance amount={lock.balance} symbol={false} /></LockValueEth></LockValueMain>
+      </LockValue>
+      <LockIconBar>
+        <LockIconBarIcon>
+          <Icons.Withdraw />
+        </LockIconBarIcon>
+        <LockIconBarIcon>
+          <Icons.Edit />
+        </LockIconBarIcon>
+        <LockIconBarIcon>
+          <Icons.Download />
+        </LockIconBarIcon>
+        <LockIconBarIcon>
+          <Icons.Code />
+        </LockIconBarIcon>
+      </LockIconBar>
+    </LockSaved>
+  )
+}
+
+CreatorLockSaved.propTypes = {
+  lock: UnlockPropTypes.lock,
+}
+
+export default CreatorLockSaved
+
+const LockSaved = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr repeat(4, 1fr) 2fr;
+`
