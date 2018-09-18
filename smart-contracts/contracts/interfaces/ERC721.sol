a2d837a8b21542633d78eebead913eb37dc386f0
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-09-18 11:30:57 -0700

    Move the ERC721 to interfaces.
    Also changed it from a 'contract' to an 'interface'.  The standard defines it as an interface, I'm not sure how/why it may have been changed to a contract.

--- smart-contracts/contracts/interfaces/ERC721.sol
+++ smart-contracts/contracts/interfaces/ERC721.sol
@@ -4,7 +4,7 @@ pragma solidity 0.4.24;
 * @title ERC721 interface (http://erc721.org/)
 */
 
-contract ERC721 {
+interface ERC721 {
   /// @dev This emits when ownership of any NFT changes by any mechanism.
   ///  This event emits when NFTs are created (`from` == 0) and destroyed
   ///  (`to` == 0). Exception: during contract creation, any number of NFTs
