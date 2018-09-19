af95a424980eb378e9df1e63e8716bc353e17bf7
Ben Werdmuller <ben@benwerd.com>
2018-09-19 11:48:50 -0700

    Adding new lock helper to provide accompanying transaction and confirmations

---
+++ unlock-app/src/__tests__/helpers/Locks.test.js
@@ -0,0 +1,104 @@
+import { getLockTransaction, getLockConfirmations } from '../../helpers/Locks'
+
+describe('lockHelpers', () => {
+  it ('should retrieve a transaction from a lock when a match exists', () => {
+    const Lock = {
+      address: '0x1234567890',
+    }
+    const transactions = {
+      all: {
+        notTheTransactionWereLookingFor: {
+          hash: 'notTheTransactionWereLookingFor',
+          confirmations: 24,
+          lock: {
+            address: '0x0987654321'
+          },
+        },
+        theTransactionWereLookingFor: {
+          hash: 'theTransactionWereLookingFor',
+          confirmations: 12,
+          lock: {
+            address: '0x1234567890',
+          },
+        },
+      },
+    }
+
+    expect(getlockTransaction(transactions, Lock.address).hash).toEqual('theTransactionWereLookingFor')
+  })
+  it ('should not retrieve a transaction from a lock when no match exists', () => {
+    const Lock = {
+      address: '0x9999999999',
+    }
+    const transactions = {
+      all: {
+        notTheTransactionWereLookingFor: {
+          hash: 'notTheTransactionWereLookingFor',
+          confirmations: 24,
+          lock: {
+            address: '0x0987654321'
+          },
+        },
+        theTransactionWereLookingFor: {
+          hash: 'theTransactionWereLookingFor',
+          confirmations: 12,
+          lock: {
+            address: '0x1234567890',
+          },
+        },
+      },
+    }
+
+    expect(getlockTransaction(transactions, Lock.address).hash).toEqual(null)
+  })
+  it ('should retrieve confirmations from a lock when a match exists', () => {
+    const Lock = {
+      address: '0x1234567890',
+    }
+    const transactions = {
+      all: {
+        notTheTransactionWereLookingFor: {
+          hash: 'notTheTransactionWereLookingFor',
+          confirmations: 24,
+          lock: {
+            address: '0x0987654321'
+          },
+        },
+        theTransactionWereLookingFor: {
+          hash: 'theTransactionWereLookingFor',
+          confirmations: 12,
+          lock: {
+            address: '0x1234567890',
+          },
+        },
+      },
+    }
+
+    expect(getLockConfirmations(transactions, Lock.address).hash).toEqual(12)
+  })
+  it ('should retrieve no confirmations from a lock when no match exists', () => {
+    const Lock = {
+      address: '0x99999999999',
+    }
+    const transactions = {
+      all: {
+        notTheTransactionWereLookingFor: {
+          hash: 'notTheTransactionWereLookingFor',
+          confirmations: 24,
+          lock: {
+            address: '0x0987654321'
+          },
+        },
+        theTransactionWereLookingFor: {
+          hash: 'theTransactionWereLookingFor',
+          confirmations: 12,
+          lock: {
+            address: '0x1234567890',
+          },
+        },
+      },
+    }
+
+    expect(getLockConfirmations(transactions, Lock.address).hash).toEqual(null)
+  })
+})
--- unlock-app/src/components/creator/Locks.js
+++ unlock-app/src/components/creator/Locks.js
@@ -3,12 +3,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Icon from '../lock/Icon'
+import {getLockConfirmations} from '../../helpers/Locks'
 
 const LockInList = (props) => {
   return (<li className="list-group-item">
     <Icon lock={props.lock} address={props.lock.address} />
     <Link to={`/creator/lock/${props.lock.address}`}>
-      {props.lock.address}
+      {props.lock.address} {getLockConfirmations(props.transactions, props.lock.address)}
     </Link>
   </li>)
 }
@@ -24,7 +25,7 @@ const Locks = (props) => {
       <ul className="list-group">
         {[...props.locks].map((lock, idx) => {
           return (
-            <LockInList lock={lock} key={idx} />
+            <LockInList lock={lock} key={idx} transactions={props.transactions} />
           )
         })}
       </ul>
@@ -34,12 +35,15 @@ const Locks = (props) => {
 
 Locks.propTypes = {
   locks: UnlockPropTypes.locks,
+  transactions: UnlockPropTypes.transactions,
 }
 
 const mapStateToProps = state => {
   const locks = state.locks || []
+  const transactions = state.transactions || {}
   return {
     locks,
+    transactions,
   }
 }
 
