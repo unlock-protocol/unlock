850b5b7847427116d4a9a4fe894665e69aeb16e1
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-09-19 10:47:12 -0700

    Undo ERC721 change
    We will explore switching to OpenZeppelin instead.

--- smart-contracts/contracts/interfaces/ERC721.sol
+++ smart-contracts/contracts/interfaces/ERC721.sol
@@ -4,7 +4,7 @@ pragma solidity 0.4.24;
 * @title ERC721 interface (http://erc721.org/)
 */
 
-interface ERC721 {
+contract ERC721 {
   /// @dev This emits when ownership of any NFT changes by any mechanism.
   ///  This event emits when NFTs are created (`from` == 0) and destroyed
   ///  (`to` == 0). Exception: during contract creation, any number of NFTs
