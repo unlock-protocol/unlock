c6d23441beb516c59c7c4109c93734f4bf40f7c1
julien <julien.genestoux@gmail.com>
2018-09-20 10:46:36 -0400

    Using non styled svg and styling them in CSS

--- unlock-app/src/components/interface/icons/About.js
+++ unlock-app/src/components/interface/icons/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 const About = props => (
   <svg {...props}>
     <path
-      fill="#A6A6A6"
-      d="M12 0C5.37 0 0 5.37 0 12s5.37 12 12 12 12-5.37 12-12S18.63 0 12 0zm1.2 18h-2.4v-7.2h2.4V18zm0-9.6h-2.4V6h2.4v2.4z"
+      d="M13.2 18h-2.4v-7.2h2.4V18zm0-9.6h-2.4V6h2.4v2.4z"
+      fillRule="evenodd"
     />
   </svg>
 )
