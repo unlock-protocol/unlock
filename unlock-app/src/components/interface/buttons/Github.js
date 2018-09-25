2a2baef1fb369a49676dfeb14b3d3e0ec7144800
Julien Genestoux <julien.genestoux@gmail.com>
2018-09-24 21:14:42 -0400

    adding hover state to buttons (#267)

--- unlock-app/src/components/interface/buttons/Github.js
+++ unlock-app/src/components/interface/buttons/Github.js
@@ -5,7 +5,7 @@ import { LayoutButton } from './Button'
 export default class Github extends PureComponent {
   render() {
     return (
-      <LayoutButton href="https://github.com/unlock-protocol/unlock">
+      <LayoutButton href="https://github.com/unlock-protocol/unlock" title="Source Code">
         <Icons.Github fill={'white'} />
       </LayoutButton>
     )
