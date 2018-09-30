cdc9a0ffff17716c153bebe37af637abb97f4876
Julien Genestoux <julien.genestoux@gmail.com>
2018-09-29 21:52:40 -0400

    adding Overlay files (#293)

--- unlock-app/src/components/interface/Logo.js
+++ unlock-app/src/components/interface/Logo.js
@@ -1,23 +1,34 @@
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Unlock from './svg/Unlock'
 
 import React from 'react'
 
-export const RoundedLogo = () => (
-  <Circle>
-    <Unlock />
+/**
+ * This wraps the Unlock SVG component into a pink circle.
+ * Note: we need to keep the viewBox to ensure that the SVG will resize.
+ * @param {string} size
+ */
+export const RoundedLogo = ({ size }) => (
+  <Circle size={size}>
+    <Unlock viewBox="0 0 56 56" />
   </Circle>
 )
+
+RoundedLogo.propTypes = {
+  size: PropTypes.string,
+}
 
 export default RoundedLogo
 
 const Circle = styled.div`
   background-color: var(--pink);
-  height: 56px;
-  width: 56px;
-  border-radius: 50px;
+  height: ${props => props.size || '56px'};
+  width: ${props => props.size || '56px'};
+  border-radius: 50%;
 
   > svg {
     fill: white;
-  }
+
+}
 `
