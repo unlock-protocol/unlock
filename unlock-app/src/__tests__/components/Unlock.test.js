e4370ee7aede79ac49041f3eff9eb43cab2fff48
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-01 19:00:54 -0400

    hooking up errors (#297)

--- unlock-app/src/__tests__/components/Unlock.test.js
+++ unlock-app/src/__tests__/components/Unlock.test.js
@@ -48,8 +48,8 @@ describe('Unlock Component', () => {
         config.isRequiredNetwork = () => false
         const component = (<Unlock network={network} config={config} path={'/'} />)
         const wrapper = shallow(component)
-        expect(wrapper.text()).toContain('Wrong network')
-        expect(wrapper.text()).toContain('This early version of Unlock requires you to use the dev network (you are currently connected to Rinkeby). Please switch your provider to use dev.')
+        expect(wrapper.props().currentNetwork).toContain('Rinkeby')
+        expect(wrapper.props().requiredNetwork).toContain('dev')
       })
     })
   })
--- unlock-app/src/stories/creator/FatalError.stories.js
+++ unlock-app/src/stories/creator/FatalError.stories.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { FatalError } from '../../components/creator/FatalError'
-
-import Network from '../../../public/images/illustrations/network.svg'
-import Wallet from '../../../public/images/illustrations/wallet.svg'
+import FatalError from '../../components/creator/FatalError'
 
 storiesOf('FatalError', FatalError)
   .add('Network mismatch', () => {
     return (
-      <FatalError title="Network mismatch" message="You’re currently on the Rinkeby Test Network but you need to be on the Main Ethereum Network. Please switch to Ethereum Main Net." illustration={Network} />
+      <FatalError.WrongNetwork currentNetwork='main' requiredNetwork='rinkeby' />
     )
   })
   .add('Wallet missing', () => {
-    const linkToMetamask = (<a href='https://metamask.io/'>Metamask</a>)
     return (
-      <FatalError title="Wallet missing" message={['It looks like you’re using an incompatible browser or are missig a crypto wallet. If you’re using Chrome or Firefox, you can install ', linkToMetamask, '.']} illustration={Wallet} />
+      <FatalError.MissingProvider />
     )
   })
