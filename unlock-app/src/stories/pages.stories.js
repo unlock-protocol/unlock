b3153b2873d7bd9603f592519ae7d700116a03a1
julien <julien.genestoux@gmail.com>
2018-10-01 15:51:43 -0400

    fixing home page story with storybook-react-router

--- unlock-app/src/stories/pages.stories.js
+++ unlock-app/src/stories/pages.stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Pages from '../components/pages'
+import StoryRouter from 'storybook-react-router'
 
 storiesOf('Content pages', Pages)
+  .addDecorator(StoryRouter())
   .add('the Home page', () => {
     return (
       <Pages.Home />
