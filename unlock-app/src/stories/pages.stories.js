d987ff80140442fe7652f070ad124a3c276b3399
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-08 12:57:04 -0400

    adding a link to home when clicking on router (#309)

--- unlock-app/src/stories/pages.stories.js
+++ unlock-app/src/stories/pages.stories.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Pages from '../components/pages'
-import StoryRouter from 'storybook-react-router'
 
 storiesOf('Content pages', Pages)
-  .addDecorator(StoryRouter())
   .add('the Home page', () => {
     return (
       <Pages.Home />
