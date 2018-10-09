7a3a54667f92b026213f07cb3e16c52b1d56ab2a
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-09 18:13:31 -0400

    locks are now stored as an object, indexed by their addresses (#315)

--- unlock-app/src/__tests__/reducers/locksReducer.test.js
+++ unlock-app/src/__tests__/reducers/locksReducer.test.js
@@ -9,30 +9,41 @@ describe('locks reducer', () => {
   }
 
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual([])
+    expect(reducer(undefined, {})).toEqual({})
   })
 
   it('should add the lock when receiving SET_LOCK and if it was not there yet', () => {
     expect(reducer(undefined, {
       type: SET_LOCK,
       lock,
-    })).toEqual([lock])
+    })).toEqual({
+      [lock.address]: lock,
+    })
   })
 
   it('should not add the lock twice even when receiving SET_LOCK again', () => {
-    expect(reducer([lock], {
+    expect(reducer({
+      [lock.address]: lock,
+    }, {
       type: SET_LOCK,
       lock,
-    })).toEqual([lock])
+    })).toEqual({
+      [lock.address]: lock,
+    })
 
   })
 
+  // It is kind of weird to test this.. but ok, I guess?
   it('should keep the locks when receiving SET_ACCOUNT with an account', () => {
     const account = {}
-    expect(reducer([lock], {
+    expect(reducer({
+      [lock.address]: lock,
+    }, {
       type: SET_ACCOUNT,
       account,
-    })).toEqual([lock])
+    })).toEqual({
+      [lock.address]: lock,
+    })
   })
 
 })
--- unlock-app/src/reducers/locksReducer.js
+++ unlock-app/src/reducers/locksReducer.js
@@ -1,18 +1,13 @@
 import { SET_LOCK } from '../actions/lock'
 
-const initialState = []
+const initialState = {}
 
 const locksReducer = (state = initialState, action) => {
 
   if (action.type === SET_LOCK) {
-    var newState = [...state]
-    let included = newState.reduce((included, l) => {
-      return included || (l.address === action.lock.address)
-    }, false)
-    if (!included) {
-      newState.push(action.lock)
+    return { ...state,
+      [action.lock.address]: action.lock,
     }
-    return newState
   }
 
   return state
