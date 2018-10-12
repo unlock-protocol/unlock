98f2f5bfa6f345778512301a7e1aa58a26b71dca
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-12 11:13:14 -0400

    removed eslint because it is part of react-scripts and tests will fail if there are 2 distinct versions running (#329)

--- unlock-app/.eslintrc.js
+++ unlock-app/.eslintrc.js
@@ -1,5 +1,17 @@
 module.exports = {
   "extends": ["standard", "eslint:recommended", "plugin:react/recommended"],
+  "settings": {
+    "react": {
+      "createClass": "createReactClass", // Regex for Component Factory to use,
+                                         // default to "createReactClass"
+      "pragma": "React",  // Pragma to use, default to "React"
+      "version": "16.4", // React version, default to the latest React stable release
+    },
+    "propWrapperFunctions": [ "forbidExtraProps" ] // The names of any functions used to wrap the
+                                                   // propTypes object, e.g. `forbidExtraProps`.
+                                                   // If this isn't set, any propTypes wrapped in
+                                                   // a function will be skipped.
+  },
   "env": {
     "es6": true,
     "node": true,
