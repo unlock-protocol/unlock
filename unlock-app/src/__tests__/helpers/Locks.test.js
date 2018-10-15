c2b69985fbc3474e4ab61b8ca9f2ac9b95bd0c5d
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-15 12:50:20 -0400

    locks are now indexed with a unique local id (#353)

--- unlock-app/src/__tests__/helpers/Locks.test.js
+++ unlock-app/src/__tests__/helpers/Locks.test.js
@@ -8,118 +8,102 @@ describe('lockHelpers', () => {
   describe('getLockTransaction', () => {
 
     it('should retrieve a transaction from a lock when a match exists', () => {
-      const Lock = {
-        address: '0x1234567890',
+      const lock = {
+        id: '1337',
       }
       const transactions = {
         all: {
           notTheTransactionWereLookingFor: {
             hash: 'notTheTransactionWereLookingFor',
             confirmations: 24,
-            lock: {
-              address: '0x0987654321',
-            },
+            lock: '987654321',
           },
           theTransactionWereLookingFor: {
             hash: 'theTransactionWereLookingFor',
             confirmations: 12,
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1337',
           },
         },
       }
 
-      expect(getLockTransaction(transactions, Lock.address).hash).toEqual('theTransactionWereLookingFor')
+      expect(getLockTransaction(transactions, lock.id).hash).toEqual('theTransactionWereLookingFor')
     })
 
     it('should not retrieve a transaction from a lock when no match exists', () => {
-      const Lock = {
-        address: '0x9999999999',
+      const lock = {
+        id: '404',
       }
       const transactions = {
         all: {
           notTheTransactionWereLookingFor: {
             hash: 'notTheTransactionWereLookingFor',
             confirmations: 24,
-            lock: {
-              address: '0x0987654321',
-            },
+            lock: '987654321',
           },
           theTransactionWereLookingFor: {
             hash: 'theTransactionWereLookingFor',
             confirmations: 12,
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockTransaction(transactions, Lock.address)).toEqual(null)
+      expect(getLockTransaction(transactions, lock.address)).toEqual(null)
     })
   })
 
   describe('getLockConfirmations', () => {
 
     it('should retrieve confirmations from a lock when a match exists', () => {
-      const Lock = {
-        address: '0x1234567890',
+      const lock = {
+        id: '1234567890',
       }
       const transactions = {
         all: {
           notTheTransactionWereLookingFor: {
             hash: 'notTheTransactionWereLookingFor',
             confirmations: 24,
-            lock: {
-              address: '0x0987654321',
-            },
+            lock: '987654321',
           },
           theTransactionWereLookingFor: {
             hash: 'theTransactionWereLookingFor',
             confirmations: 12,
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockConfirmations(transactions, Lock.address)).toEqual(12)
+      expect(getLockConfirmations(transactions, lock.id)).toEqual(12)
     })
 
     it('should retrieve no confirmations from a lock when no match exists', () => {
-      const Lock = {
-        address: '0x99999999999',
+      const lock = {
+        id: '99999999999',
       }
       const transactions = {
         all: {
           notTheTransactionWereLookingFor: {
             hash: 'notTheTransactionWereLookingFor',
             confirmations: 24,
-            lock: {
-              address: '0x0987654321',
-            },
+            lock: '0987654321',
           },
           theTransactionWereLookingFor: {
             hash: 'theTransactionWereLookingFor',
             confirmations: 12,
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockConfirmations(transactions, Lock.address)).toEqual(null)
+      expect(getLockConfirmations(transactions, lock.id)).toEqual(null)
     })
   })
 
   describe('getLockStatusString', () => {
 
     it('should return the status "deployed" for locks with mined transactions and at least the number of required confirmations', () => {
-      const Lock = {
-        address: '0x1234567890',
+      const lock = {
+        id: '1234567890',
       }
       const transactions = {
         all: {
@@ -127,38 +111,34 @@ describe('lockHelpers', () => {
             hash: 'theTransactionWereLookingFor',
             confirmations: config.requiredConfirmations,
             status: 'mined',
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockStatusString(transactions, Lock.address)).toEqual('deployed')
+      expect(getLockStatusString(transactions, lock.id)).toEqual('deployed')
     })
 
     it('should return the status "submitted" for locks whose transaction has been submitted but not been mined yet', () => {
-      const Lock = {
-        address: '0x1234567890',
+      const lock = {
+        id: '1234567890',
       }
       const transactions = {
         all: {
           theTransactionWereLookingFor: {
             hash: 'theTransactionWereLookingFor',
             status: 'submitted',
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockStatusString(transactions, Lock.address)).toEqual('submitted')
+      expect(getLockStatusString(transactions, lock.id)).toEqual('submitted')
     })
 
     it('should return the status "confirming" for locks with mined transactions and under the required number of confirmations', () => {
-      const Lock = {
-        address: '0x1234567890',
+      const lock = {
+        id: '1234567890',
       }
       const transactions = {
         all: {
@@ -166,14 +146,12 @@ describe('lockHelpers', () => {
             hash: 'theTransactionWereLookingFor',
             confirmations: config.requiredConfirmations - 1,
             status: 'mined',
-            lock: {
-              address: '0x1234567890',
-            },
+            lock: '1234567890',
           },
         },
       }
 
-      expect(getLockStatusString(transactions, Lock.address)).toEqual('confirming')
+      expect(getLockStatusString(transactions, lock.id)).toEqual('confirming')
     })
   })
 
