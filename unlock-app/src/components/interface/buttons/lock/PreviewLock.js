970ea0c4d6b9b282bc66d1619ef7e5215ac2d721
Ben Werdmuller <ben@withknown.com>
2018-10-15 13:56:07 -0700

    Adding preview button to lock icon bar (#360)

---
+++ unlock-app/src/components/interface/buttons/lock/PreviewLock.js
@@ -0,0 +1,11 @@
+import Svg from '../../svg'
+import React from 'react'
+import { LockButton } from '../Button'
+
+const PreviewLock = () => (
+  <LockButton>
+    <Svg.LockClosed />
+  </LockButton>
+)
+
+export default PreviewLock
