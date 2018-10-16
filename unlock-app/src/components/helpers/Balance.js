5661bd099d54098a84fda1015721b85b34f5dd9b
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-16 15:10:23 -0400

    not using the SVG icon in balance because it means styling is nearly impossible to remain consistent between the actual value and the unit (#368)

--- unlock-app/src/components/helpers/Balance.js
+++ unlock-app/src/components/helpers/Balance.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import React from 'react'
 import PropTypes from 'prop-types'
 import Web3Utils from 'web3-utils'
-import Svg from '../interface/svg'
 
 /**
  * Component which shows a balance in Eth
@@ -13,30 +12,18 @@ export function Balance({ amount, unit = 'wei' }) {
   let inWei = Web3Utils.toWei(amount || '0', unit)
   let inEth = Web3Utils.fromWei(inWei, 'ether')
   return (<BalanceWithUnit>
-    <Unit>
-      <Svg.Eth width="1em" height="1em" />
-    </Unit>
-    <Amount>{inEth}</Amount>
+    三 {inEth}
   </BalanceWithUnit>)
 }
 
 Balance.propTypes = {
   amount: PropTypes.string,
   unit: PropTypes.string,
-  symbol: PropTypes.bool,
 }
 
-export const BalanceWithUnit = styled.div`
-  display: grid;
-  align-content: stretch;
-  grid-auto-flow: column;
-`
-
-const Amount = styled.span``
-
-export const Unit = styled.span`
-  display: grid;
-  align-items: end;
+export const BalanceWithUnit = styled.span`
+  white-space: nowrap;
+  text-transform: uppercase;
 `
 
 export default Balance
