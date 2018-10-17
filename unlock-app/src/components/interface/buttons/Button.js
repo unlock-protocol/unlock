a6357fd3ca493b403fcf9d8f2cff566197bb8ea9
Ben Werdmuller <ben@withknown.com>
2018-10-17 19:36:36 -0400

    Add default param on href prop for Button Link

--- unlock-app/src/components/interface/buttons/Button.js
+++ unlock-app/src/components/interface/buttons/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 
-export const LayoutButton = ({href, title, children}) => (
+export const LayoutButton = ({href = '#', title, children}) => (
   <Link href={href}>
     <Button
       href={href}
