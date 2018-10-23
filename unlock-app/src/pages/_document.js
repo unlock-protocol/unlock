292493e0a23937334366ff98f1d72d397f07c365
julien <julien.genestoux@gmail.com>
2018-10-23 11:44:06 -0400

    making things responsive

--- unlock-app/src/pages/_document.js
+++ unlock-app/src/pages/_document.js
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name='viewport' content='width=device-width' />
           <link rel='shortcut icon' href='/static/favicon.ico' />
           {this.props.styleTags}
         </Head>
