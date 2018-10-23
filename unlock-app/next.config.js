d3320b32b892fd919f5fb65910eb200d98f8ebc7
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-23 12:54:09 -0400

    adding the ability to export staticly generated site (#387)

---
+++ unlock-app/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/jobs': { page: '/jobs' },
+      '/dashboard': { page: '/dashboard' },
+    }
+  },
+}
