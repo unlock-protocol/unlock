ec2c7507b48c396b058a3e6a7feb8d699012a8c5
Ben Werdmuller <ben@benwerd.com>
2018-10-23 14:41:01 -0700

    Adding props

--- unlock-app/src/components/interface/buttons/lock/Copy.js
+++ unlock-app/src/components/interface/buttons/lock/Copy.js
@@ -2,8 +2,8 @@ import Svg from '../../svg'
 import React from 'react'
 import { LockButton } from '../Button'
 
-const Copy = () => (
-  <LockButton>
+const Copy = (props) => (
+  <LockButton {...props}>
     <Svg.Copy />
   </LockButton>
 )
