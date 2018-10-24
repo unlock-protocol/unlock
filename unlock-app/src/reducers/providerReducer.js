239859bc37e3cf08a9f6517ea97254f87032b8d2
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-24 14:29:34 -0400

    Fixing providerReducer (#402)
    * changed order
    
    * fixing how we export default state from providerReducer

--- unlock-app/src/reducers/providerReducer.js
+++ unlock-app/src/reducers/providerReducer.js
@@ -3,12 +3,7 @@ import configure from '../config'
 
 const config = configure(global)
 
-export let initialState = null
-
-if (!config.isServer) {
-  // By default, we start with the first provider.
-  initialState = Object.keys(config.providers)[0]
-}
+export const initialState = Object.keys(config.providers)[0]
 
 const providerReducer = (state = initialState, action) => {
   if (action.type === SET_PROVIDER) {
