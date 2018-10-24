0a0247733d48151d47ea2469a16b83fce4b025d1
Ben Werdmuller <ben@withknown.com>
2018-10-23 17:11:08 -0700

    Hooking up embed code for locks in creator view (#384)
    * Updating shrinkwrap
    
    * Interim code to check out
    
    * Calling correct function
    
    * Finalized styles
    
    * Interim code to check out
    
    * Calling correct function
    
    * Finalized styles
    
    * Interim code to check out
    
    * Calling correct function
    
    * Finalized styles
    
    * Interim code to check out
    
    * Calling correct function
    
    * Finalized styles
    
    * Interim code to check out
    
    * Interim code to check out
    
    * Calling correct function
    
    * Calling correct function
    
    * Finalized styles
    
    * Finalized styles
    
    * Interim code to check out
    
    * Interim code to check out
    
    * Calling correct function
    
    * Calling correct function
    
    * Finalized styles
    
    * Finalized styles
    
    * Moving to EmbedCodeSnippet
    
    * Moving LockIconBar back into a function
    
    * Removing className
    
    * Adding props
    
    * Moving textarea dimensions to grid characteristics

--- unlock-app/src/components/creator/lock/LockIconBar.js
+++ unlock-app/src/components/creator/lock/LockIconBar.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
 import UnlockPropTypes from '../../../propTypes'
 import Buttons from '../../interface/buttons/lock'
 
-export function LockIconBar({ lock }) {
+export function LockIconBar({ lock, toggleCode }) {
   return (
     <IconBar>
       <Buttons.PreviewLock lock={lock} />
       <Buttons.Withdraw />
       <Buttons.Edit />
       { /* Reinstate when we're ready <Buttons.ExportLock /> */ }
-      <Buttons.Code />
+      <Buttons.Code onClick={toggleCode} />
     </IconBar>
   )
 }
 
 LockIconBar.propTypes = {
   lock: UnlockPropTypes.lock,
+  toggleCode: PropTypes.func,
 }
 
 export default LockIconBar
