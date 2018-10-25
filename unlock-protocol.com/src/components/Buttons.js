ea51c00d1cafa8944011b6e1127d9b5918a72ccb
Julien Genestoux <julien.genestoux@gmail.com>
2018-10-25 12:12:35 -0400

    Some cleanup for old site and branding (#408)
    * removed stale files
    
    * removed the old website built with gatsby
    
    * removed unlock branding since it was the old style

--- unlock-protocol.com/src/components/Buttons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Buttons = (props) => (
-  <div className="buttons">
-    {props.children}
-  </div>
-)
-
-export default Buttons
--- unlock-protocol.com/src/components/Columns.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Columns = (props) => (
-  <div className="columns">
-    {props.children}
-  </div>
-)
-
-export default Columns
