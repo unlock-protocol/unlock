5ecb91999ed1951fcfec6c9c61db6e54139884ef
julien <julien.genestoux@gmail.com>
2018-10-28 19:19:55 -0400

    fixing server url parsing

--- unlock-app/src/server.js
+++ unlock-app/src/server.js
@@ -1,3 +1,4 @@
+/* eslint no-console: 0 */
 const { createServer } = require('http')
 const { URL } = require('url')
 const next = require('next')
@@ -12,20 +13,26 @@ const route = pathMatch()
 app.prepare()
   .then(() => {
     createServer((req, res) => {
-      const parsedUrl = new URL(req.url, true)
-      const { pathname, query } = parsedUrl
+      try {
+        const parsedUrl = new URL(req.url, `http://${req.headers.host}/`)
+        const { pathname, query } = parsedUrl
 
-      // assigning `query` into the params means that we still
-      // get the query string passed to our application
-      const path = pathname.split('/')[1]
-      if (path === 'lock') {
-        const params = route('/lock/:lockaddress')(pathname)
-        app.render(req, res, '/lock', Object.assign(params, query))
-      } else if (path === 'demo') {
-        const params = route('/demo/:lockaddress')(pathname)
-        app.render(req, res, '/demo', Object.assign(params, query))
-      } else {
-        handle(req, res)
+        // assigning `query` into the params means that we still
+        // get the query string passed to our application
+        const path = pathname.split('/')[1]
+        if (path === 'lock') {
+          const params = route('/lock/:lockaddress')(pathname)
+          app.render(req, res, '/lock', Object.assign(params, query))
+        } else if (path === 'demo') {
+          const params = route('/demo/:lockaddress')(pathname)
+          app.render(req, res, '/demo', Object.assign(params, query))
+        } else {
+          handle(req, res)
+          return
+        }
+      } catch(error) {
+        console.error(error)
+        // TODO: handle error! Serve error?
         return
       }
     })
