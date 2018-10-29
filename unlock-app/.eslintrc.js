f9b138ffcbfb1666a1d3ca9cdf7595af3d3dda6a
julien <julien.genestoux@gmail.com>
2018-10-29 14:31:07 -0400

    disabled react/jsx-closing-tag-location

--- unlock-app/.eslintrc.js
+++ unlock-app/.eslintrc.js
@@ -58,6 +58,7 @@ module.exports = {
     "import/no-named-as-default-member": 0,
     "react/no-unused-prop-types": 0,
     "react/destructuring-assignment": 0,
-    "no-unused-vars": 0
+    "no-unused-vars": 0,
+    "react/jsx-closing-tag-location": 0,
   }
 };
