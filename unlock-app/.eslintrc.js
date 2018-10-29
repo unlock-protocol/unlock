f1a5dee63d2821bf9f5bc720010788a660d946f5
julien <julien.genestoux@gmail.com>
2018-10-29 14:26:40 -0400

    disabled react/require-default-props

--- unlock-app/.eslintrc.js
+++ unlock-app/.eslintrc.js
@@ -44,14 +44,16 @@ module.exports = {
     "eol-last": [
       "error"
     ],
-    "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }],
-    "import/no-named-as-default": 0,
     "jsx-a11y/anchor-is-valid": [
       "error", {
         "components": [ "Link" ],
         "specialLink": [ "hrefLeft", "hrefRight" ],
         "aspects": [ "invalidHref", "preferButton" ]
       }
-    ]
+    ],
+    // TODO All rules below should be enforced. They have been disabled to avoid a monster PR
+    "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }],
+    "import/no-named-as-default": 0,
+    "react/require-default-props": 0
   }
 };
