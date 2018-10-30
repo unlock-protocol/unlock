32f555111428425217c7b11fb7cf3da380dbdded
Nick Furfaro <nfurfaro33@gmail.com>
2018-10-30 11:08:39 -0600

    Refactor PublicLock.sol to be strictly Public (#446)
    Eventually there will be Private and Restricted locks. However, code
    pertaining to these other types doesn't belong here.
    As allowing a lock owner to change the keyReleaseMechanism of a lock is
    not likely to be a common use-case, the keyReleaseMechanism has been
    removed from Unlock.sol,PublicLock.sol and the lock's contructor.
    All relavent tests have had lines/tests involving the
    keyReleaseMechanism or Locks of any type other than Public commented out.

--- smart-contracts/test/Unlock/behaviors/createLock.js
+++ smart-contracts/test/Unlock/behaviors/createLock.js
@@ -6,7 +6,6 @@ exports.shouldCreateLock = function (accounts) {
     let transaction
     beforeEach(async function () {
       transaction = await this.unlock.createLock(
-        0, // keyReleaseMechanism: KeyReleaseMechanisms.Public
         60 * 60 * 24 * 30, // expirationDuration: 30 days
         Units.convert(1, 'eth', 'wei'), // keyPrice: in wei
         100 // maxNumberOfKeys
