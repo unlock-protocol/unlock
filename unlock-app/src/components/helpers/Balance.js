23f5c5db8aa7cd1093fa0a7dc5f00e7552b2ce47
Gregory Beaver <greg.beaver.cello@gmail.com>
2018-11-01 10:01:59 -0400

    refactor Balance to use formatEth and formatCurrency

--- unlock-app/src/components/helpers/Balance.js
+++ unlock-app/src/components/helpers/Balance.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import React from 'react'
 import PropTypes from 'prop-types'
 import Web3Utils from 'web3-utils'
+import { formatEth, formatCurrency } from '../../selectors/currency'
 
 /**
  * Component which shows a balance in Eth
@@ -18,12 +19,12 @@ export function Balance({ amount, unit = 'wei', conversion = { USD: undefined },
   } else {
     inEth = +amount
   }
-  const ethWithPresentation = BalancePresenter(inEth, 'ether')
+  const ethWithPresentation = formatEth(inEth)
   let convertedUSDValue
   if (!conversion.USD) {
     convertedUSDValue = '---'
   } else {
-    convertedUSDValue = BalancePresenter(inEth * conversion.USD, 'dollars')
+    convertedUSDValue = formatCurrency(inEth * conversion.USD)
   }
 
   return (
@@ -42,47 +43,6 @@ export function Balance({ amount, unit = 'wei', conversion = { USD: undefined },
       </Currency>
     </BalanceWithConversion>
   )
-}
-
-/**
- * Provide a representation of Eth Balances suitable for the Unlock frontend
- * application.
- * @param {string} eth: An amount of Eth
- * @param {string} currency: currency type, either ether or some real currency like dollars
- */
-function BalancePresenter(eth, currency = 'ether'){
-  const DECIMAL_PLACES = 2
-  const SIGNIFICANT_DIGITS = 2
-  const MINIMUM_THRESHOLD = 0.0001
-
-  let numericalEth = Number(eth)
-
-  if (currency !== 'ether') {
-    switch(true) {
-      case numericalEth < 1 && numericalEth > 0.01:
-        return parseFloat(numericalEth.toPrecision(SIGNIFICANT_DIGITS))
-      case numericalEth < 0.01:
-        return '0'
-      case numericalEth >= 1000 && numericalEth < 1e5 :
-        return Math.round(numericalEth).toLocaleString()
-      case numericalEth >= 1e5 && numericalEth < 1e6 :
-        return (+((numericalEth/1e3).toFixed(1))).toLocaleString() + 'k'
-      case numericalEth >= 1e6 && numericalEth < 1e9 :
-        return (+((numericalEth/1e6).toFixed(1))).toLocaleString() + 'm'
-      case numericalEth >= 1e9 :
-        return (+((numericalEth/1e9).toFixed(1))).toLocaleString() + 'b'
-      default:
-        return numericalEth.toFixed(DECIMAL_PLACES)
-    }
-  }
-  switch(true) {
-    case numericalEth < MINIMUM_THRESHOLD && numericalEth > 0 :
-      return '< 0.0001'
-    case numericalEth < 1:
-      return parseFloat(numericalEth.toPrecision(SIGNIFICANT_DIGITS))
-    default:
-      return numericalEth.toFixed(DECIMAL_PLACES)
-  }
 }
 
 Balance.propTypes = {
