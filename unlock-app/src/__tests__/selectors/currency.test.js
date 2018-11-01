811d1baef5db270206ee138127861b575bc10994
Gregory Beaver <greg.beaver.cello@gmail.com>
2018-11-01 08:53:30 -0400

    prepare to extract currency formatting selectors

---
+++ unlock-app/src/__tests__/selectors/currency.test.js
@@ -0,0 +1,18 @@
+import { formatEth } from '../../selectors/currency'
+
+describe('currency conversion and formatting selectors', () => {
+  describe('formatEth', () => {
+    it('small values')
+    it('decimal values')
+    it('larger values')
+  })
+  describe('formatCurrency', () => {
+    it('< 0.01')
+    it('0.01 to 1')
+    it('1 to 999')
+    it('1000 to 99,999')
+    it('100k to 1 million')
+    it('1 million to 1 billion')
+    it('1 billion+')
+  })
+})
