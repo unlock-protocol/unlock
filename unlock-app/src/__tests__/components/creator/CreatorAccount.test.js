a5644ebf4bf4709937a0d037555ed26d4fea29f1
Gregory Beaver <greg.beaver.cello@gmail.com>
2018-11-01 21:11:33 -0400

    fix tests using rtl (impossible with enzyme)
    no matter what I tried, enzyme.find() was unable to find `Balance` so I switched to rtl, and instantly it worked.

--- unlock-app/src/__tests__/components/creator/CreatorAccount.test.js
+++ unlock-app/src/__tests__/components/creator/CreatorAccount.test.js
@@ -1,23 +1,33 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import { Provider } from 'react-redux'
 // Note, we use name import to import the non connected version of the component for testing
 import { CreatorAccount } from '../../../components/creator/CreatorAccount'
+import createUnlockStore from '../../../createUnlockStore'
 
+afterEach(rtl.cleanup)
 describe('CreatorAccount', () => {
   it('should show the balance of the creator account', () => {
     const account = {
       address: '0x3ca206264762caf81a8f0a843bbb850987b41e16',
-      balance: '17.73',
+      balance: '200000000000000000',
     }
     const network = {
       name: 4,
     }
-    const conversion = {
+    const currency = {
       USD: 195.99,
     }
 
-    const wrapper = shallow(<CreatorAccount account={account} network={network} conversion={conversion} />)
-    expect(wrapper.find('Balance').first().props().amount).toEqual('17.73')
-    expect(wrapper.find('Balance').first().props().conversion).toEqual({ USD: 195.99 })
+    const store = createUnlockStore({ currency })
+
+    const wrapper = rtl.render(<Provider store={store}>
+      <CreatorAccount account={account} network={network} />
+    </Provider>)
+    // eth value
+    expect(wrapper.queryByText('0.2')).not.toBeNull()
+    // usd value
+    expect(wrapper.queryByText('39.20')).not.toBeNull()
   })
 })
--- unlock-app/src/__tests__/components/helpers/Balance.test.js
+++ unlock-app/src/__tests__/components/helpers/Balance.test.js
@@ -1,143 +1,115 @@
 import React from 'react'
-import { render } from 'enzyme'
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
 // Note, we use name import to import the non connected version of the component for testing
 import { Balance } from '../../../components/helpers/Balance'
 
+afterEach(rtl.cleanup)
 describe('Balance Component', () => {
   const unit = 'szabo'
   const conversion = { USD: 195.99 }
+
+  function renderIt(amount, c = conversion, u = unit) {
+    return rtl.render(<Balance
+      amount={amount}
+      unit={u}
+      conversion={c}
+    />)
+  }
 
   describe('when the balance is 0 Eth', () => {
     const amount = '0'
 
     it('no conversion data available', () => {
-      const wrapper = render(<Balance
-        amount={amount}
-        unit={unit}
-        conversion={{ USD: undefined }}
-      />)
+      const wrapper = renderIt(amount, { USD: undefined })
 
-      expect(wrapper.text()).toEqual('0---')
+      expect(wrapper.queryByText('0')).not.toBeNull()
+      expect(wrapper.queryByText('---')).not.toBeNull()
     })
     it('USD conversion data available', () => {
-      const wrapper = render(<Balance
-        amount={amount}
-        unit={unit}
-        conversion={conversion}
-      />)
-      expect(wrapper.text()).toEqual('00')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('0')).not.toBeNull()
+      expect(wrapper.queryByText('0')).not.toBeNull()
     })
   })
 
   describe('when the balance is < 0.0001 Eth', () => {
     const amount = '70'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the default minimum value of 三 < 0.0001', () => {
-      expect(wrapper.text()).toEqual('< 0.00010.014')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('< 0.0001')).not.toBeNull()
+      expect(wrapper.queryByText('0.014')).not.toBeNull()
     })
   })
 
   describe('when the balance is > 0.0001 Eth and less than 1 Eth', () => {
     const amount = '75800'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in Eth to two decimal places', () => {
-      expect(wrapper.text()).toEqual('0.07614.86')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('0.076')).not.toBeNull()
+      expect(wrapper.queryByText('14.86')).not.toBeNull()
     })
   })
 
   describe('when the balance is > 1 Eth ', () => {
     const amount = '2000000'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in Eth to two decimal places', () => {
-      expect(wrapper.text()).toEqual('2.00391.98')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('2.00')).not.toBeNull()
+      expect(wrapper.queryByText('391.98')).not.toBeNull()
     })
   })
 
   describe('when the balance converts to > $1000 ', () => {
     const amount = '20000000'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in dollars in locale format without decimal', () => {
-      expect(wrapper.text()).toEqual('20.003,920')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('20.00')).not.toBeNull()
+      expect(wrapper.queryByText('3,920')).not.toBeNull()
     })
   })
 
   describe('when the balance converts to > $100k ', () => {
     const amount = '2000000000'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in thousands of dollars postfixed with k', () => {
-      expect(wrapper.text()).toEqual('2000.00392k')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('2000.00')).not.toBeNull()
+      expect(wrapper.queryByText('392k')).not.toBeNull()
     })
   })
 
   describe('when the balance converts to > $1m ', () => {
     const amount = '20000000000'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in millions of dollars postfixed with m', () => {
-      expect(wrapper.text()).toEqual('20000.003.9m')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('20000.00')).not.toBeNull()
+      expect(wrapper.queryByText('3.9m')).not.toBeNull()
     })
   })
 
   describe('when the balance converts to > $1b ', () => {
     const amount = '20000000000000'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit={unit}
-      conversion={conversion}
-    />)
-
     it('shows the balance in billions of dollars postfixed with b', () => {
-      expect(wrapper.text()).toEqual('20000000.003.9b')
+      const wrapper = renderIt(amount)
+      expect(wrapper.queryByText('20000000.00')).not.toBeNull()
+      expect(wrapper.queryByText('3.9b')).not.toBeNull()
     })
   })
 
   describe('when the balance converts to > $1b, unit is eth (used in lock form)', () => {
     const amount = '9999999'
 
-    const wrapper = render(<Balance
-      amount={amount}
-      unit='eth'
-      conversion={conversion}
-    />)
-
     it('shows the balance in billions of dollars postfixed with b', () => {
-      expect(wrapper.text()).toEqual('9999999.002b')
+      const wrapper = renderIt(amount, undefined, 'eth')
+      expect(wrapper.queryByText('9999999.00')).not.toBeNull()
+      expect(wrapper.queryByText('2b')).not.toBeNull()
     })
   })
 })
