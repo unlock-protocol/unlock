45e97103c7fdf343ba93f89bdc1bed7a3a3868e4
Gregory Beaver <greg.beaver.cello@gmail.com>
2018-11-02 11:03:51 -0400

    fix pages story

--- unlock-app/src/stories/pages.stories.js
+++ unlock-app/src/stories/pages.stories.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { Provider } from 'react-redux'
+
 import Home from '../pages'
 import About from '../pages/about'
 import Jobs from '../pages/jobs'
+import createUnlockStore from '../createUnlockStore'
+
+const store = createUnlockStore({
+  currency: {
+    USD: 195.99,
+  },
+})
 
 storiesOf('Content pages', module)
+  .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
   .add('the Home page', () => {
     return (
       <Home />
