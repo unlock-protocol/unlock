4102c1ac930b70589ace67c265180eee2dd24048
julien <julien.genestoux@gmail.com>
2018-11-05 20:48:49 -0500

    showing log message when we cannot connect to web3

--- unlock-app/src/middlewares/lockMiddleware.js
+++ unlock-app/src/middlewares/lockMiddleware.js
@@ -16,7 +16,6 @@ export default function lockMiddleware ({ getState, dispatch }) {
   return function (next) {
     return function (action) {
       if (!web3Service.ready) {
-
         // We return to make sure other middleware actions are not processed
         return web3Service.connect({
           provider: getState().provider,
@@ -32,9 +31,11 @@ export default function lockMiddleware ({ getState, dispatch }) {
           Object.values(getState().transactions).forEach((transaction) => dispatch(refreshTransaction(transaction)))
           // We refresh keys
           Object.values(getState().keys).forEach((key) => web3Service.refreshKey(key))
-        }).catch(() => {
+        }).catch((error) => {
           // we could not connect
           // TODO: show error to user
+          console.error('Failed to connect to web3 service: ')
+          console.error(error)
         })
       }
 
