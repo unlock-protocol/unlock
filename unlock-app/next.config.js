fbd8e12d7c0de6be362c0bbcad23949bd1307c3e
Ben Werdmuller <ben@withknown.com>
2018-11-08 14:51:55 -0800

    Copy robots.txt and humans.txt on deploy in non-dev environments (#501)

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -1,5 +1,18 @@
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+
+const copyFile = promisify(fs.copyFile)
+
 module.exports = {
-  exportPathMap: function () {
+  exportPathMap: async (defaultPathMap, { dev, dir, outDir }) => {
+
+    // Export robots.txt and humans.txt in non-dev environments
+    if (!dev && outDir) {
+      await copyFile(join(dir, 'static', 'robots.txt'), join(outDir, 'robots.txt'))
+      await copyFile(join(dir, 'static', 'humans.txt'), join(outDir, 'humans.txt'))
+    }
+
     return {
       '/': { page: '/' },
       '/about': { page: '/about' },
