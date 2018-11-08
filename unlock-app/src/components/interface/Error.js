7d29c30a3051e5576ac4f211df8ae258afe1c9e7
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-08 18:41:07 -0500

    adding the error interface (#504)

---
+++ unlock-app/src/components/interface/Error.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+import Buttons from './buttons/layout'
+
+export const Error = ({ children }) => (
+  <Wrapper>
+    {children}
+    <Close size="16px">X</Close>
+  </Wrapper>
+)
+
+Error.propTypes = {
+  children: PropTypes.node,
+}
+
+export default Error
+
+const Wrapper = styled.section`
+  grid-template-columns: 1fr 20px;
+  display: grid;
+  border: 1px solid var(--lightgrey);
+  border-radius: 4px;
+  font-size: 16px;
+  justify-items: center;
+  justify-content: center;
+  align-items: center;
+  padding-right: 8px;
+  grid-gap: 8px;
+  a {
+    color: var(--red);
+  }
+`
+
+const Close = styled(Buttons.Close)`
+
+`
