e1d36f1764ab1e8e499cff9cc7e985d74ba0e7ad
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-08 18:24:06 -0500

    Resizing icons by making sure viewBox is set (#503)
    * story for large button
    
    * preserving viewBox to make sure SVG resize when scaled up

--- unlock-app/src/components/interface/svg/EthSub.js
+++ unlock-app/src/components/interface/svg/EthSub.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const EthSub = props => (
-  <svg viewBox="0 0 7 11" {...props}>
+const SvgEthSub = props => (
+  <svg viewBox="0 0 24 24" {...props}>
     <path
       d="M1 6V5h5v1H1zm-1 5v-1h7v1H0zM0 1V0h7v1H0z"
       fill="#a6a6a6"
@@ -10,4 +10,4 @@ const EthSub = props => (
   </svg>
 )
 
-export default EthSub
+export default SvgEthSub
