9416889cc3c7afa61879e54b5e30a4b656835696
Ben Werdmuller <ben@benwerd.com>
2018-11-12 16:31:39 -0800

    Fixing some properties

--- unlock-app/src/propTypes.js
+++ unlock-app/src/propTypes.js
@@ -38,7 +38,7 @@ export const mechanism = PropTypes.oneOf(['0', '1', '2', undefined])
 
 export const layout = PropTypes.instanceOf(Function) //PropTypes.instanceOf(React.Component)
 
-export const locks = PropTypes.arrayOf(PropTypes.object)
+export const locks = PropTypes.shape({})
 
 export const transactions = PropTypes.shape({})
 
