c55f05a8d10d95e4c8b9b75d626034cc9937ab46
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-17 18:12:56 -0500

    Refactored lockMiddleware and web3Service to use events (#563)

--- unlock-app/src/actions/accounts.js
+++ unlock-app/src/actions/accounts.js
@@ -1,24 +1,12 @@
 export const SET_ACCOUNT = 'SET_ACCOUNT'
-export const CREATE_ACCOUNT = 'CREATE_ACCOUNT'
 export const RESET_ACCOUNT_BALANCE = 'RESET_ACCOUNT_BALANCE'
-export const LOAD_ACCOUNT = 'LOAD_ACCOUNT'
 
 export const setAccount = account => ({
   type: SET_ACCOUNT,
   account,
 })
 
-export const createAccount = account => ({
-  type: CREATE_ACCOUNT,
-  account,
-})
-
 export const resetAccountBalance = (balance) => ({
   type: RESET_ACCOUNT_BALANCE,
   balance,
-})
-
-export const loadAccount = (privateKey) => ({
-  type: LOAD_ACCOUNT,
-  privateKey,
 })
