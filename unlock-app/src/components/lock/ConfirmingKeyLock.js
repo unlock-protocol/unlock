7d6c4e18843c06f1f506a427aeff8a53b64283d7
julien <julien.genestoux@gmail.com>
2018-11-18 16:23:30 -0500

    Implementing multiple lock statuses for overlay

---
+++ unlock-app/src/components/lock/ConfirmingKeyLock.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import UnlockPropTypes from '../../propTypes'
+import {
+  LockWrapper,
+  LockHeader,
+  LockBody,
+  LockDetails,
+  TransactionStatus,
+  LockDetail,
+} from './LockStyles'
+import BalanceProvider from '../helpers/BalanceProvider'
+import withConfig from '../../utils/withConfig'
+
+const ConfirmingKeyLock = ({ lock, transaction, config }) => (
+  <LockWrapper>
+    <Header>Payment Received</Header>
+    <LockBody>
+      <TransactionStatus>
+        Waiting for confirmation.
+        <br />
+        {transaction.confirmations}
+/
+        {config.requiredConfirmations}
+      </TransactionStatus>
+      <BalanceProvider
+        amount={lock.keyPrice}
+        render={(ethPrice, fiatPrice) => (
+          <LockDetails>
+            <LockDetail>{lock.name}</LockDetail>
+            <LockDetail bold>
+              {ethPrice}
+              {' '}
+ETH
+            </LockDetail>
+            <LockDetail>
+$
+              {fiatPrice}
+            </LockDetail>
+          </LockDetails>
+        )}
+      />
+    </LockBody>
+  </LockWrapper>
+)
+
+ConfirmingKeyLock.propTypes = {
+  lock: UnlockPropTypes.lock.isRequired,
+  transaction: UnlockPropTypes.transaction.isRequired,
+  config: UnlockPropTypes.configuration.isRequired,
+}
+
+export default withConfig(ConfirmingKeyLock)
+
+const Header = styled(LockHeader)`
+  background-color: var(--green);
+  color: var(--offwhite);
+`
---
+++ unlock-app/src/components/lock/PendingKeyLock.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import UnlockPropTypes from '../../propTypes'
+import {
+  LockWrapper,
+  LockHeader,
+  LockBody,
+  LockDetails,
+  TransactionStatus,
+  LockDetail,
+} from './LockStyles'
+import BalanceProvider from '../helpers/BalanceProvider'
+
+const PendingKeyLock = ({ lock }) => (
+  <LockWrapper>
+    <Header>Payment sent</Header>
+    <LockBody>
+      <TransactionStatus>Waiting for mining confirmation.</TransactionStatus>
+
+      <BalanceProvider
+        amount={lock.keyPrice}
+        render={(ethPrice, fiatPrice) => (
+          <LockDetails>
+            <LockDetail>{lock.name}</LockDetail>
+            <LockDetail bold>
+              {ethPrice}
+              {' '}
+ETH
+            </LockDetail>
+            <LockDetail>
+$
+              {fiatPrice}
+            </LockDetail>
+          </LockDetails>
+        )}
+      />
+    </LockBody>
+  </LockWrapper>
+)
+
+PendingKeyLock.propTypes = {
+  lock: UnlockPropTypes.lock.isRequired,
+}
+
+export default PendingKeyLock
+
+const Header = styled(LockHeader)`
+  background-color: var(--link);
+  color: var(--offwhite);
+`
