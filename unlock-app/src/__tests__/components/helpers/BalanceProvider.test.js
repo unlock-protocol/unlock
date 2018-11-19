324732f81c58d42ad83310373add4408a49100d7
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-19 13:47:18 -0500

    Refactored how <Balance /> works to use a provider and render props (#566)
    * removed the EthComponent which was not being used
    
    * adding a provider for balance so we can style balance in different ways

---
+++ unlock-app/src/__tests__/components/helpers/BalanceProvider.test.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import * as rtl from 'react-testing-library'
+// Note, we use name import to import the non connected version of the component for testing
+import { BalanceProvider } from '../../../components/helpers/BalanceProvider'
+
+describe('BalanceProvider Component', () => {
+  function renderIt({
+    amount,
+    unit = 'szabo',
+    conversion = { USD: 195.99 },
+    render,
+  }) {
+    return rtl.render(
+      <BalanceProvider
+        amount={amount}
+        unit={unit}
+        conversion={conversion}
+        render={render}
+      />
+    )
+  }
+
+  it('USD conversion data is not available', () => {
+    renderIt({
+      amount: '100',
+      unit: 'eth',
+      conversion: {},
+      render: (ethValue, fiatValue) => {
+        expect(ethValue).toEqual('100.00')
+        expect(fiatValue).toEqual('---')
+      },
+    })
+  })
+
+  it('USD conversion data is available', () => {
+    renderIt({
+      amount: '100',
+      unit: 'eth',
+      render: (ethValue, fiatValue) => {
+        expect(ethValue).toEqual('100.00')
+        expect(fiatValue).toEqual('19,599')
+      },
+    })
+  })
+
+  describe('when the balance is 0 Eth', () => {
+    it('should render 0 for both values', () => {
+      renderIt({
+        amount: '0',
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('0')
+          expect(fiatValue).toEqual('0')
+        },
+      })
+    })
+  })
+
+  describe('when the balance is < 0.0001 Eth', () => {
+    const amount = '70'
+
+    it('shows the default minimum value of 三 < 0.0001', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('< 0.0001')
+          expect(fiatValue).toEqual('0.014')
+        },
+      })
+    })
+  })
+
+  describe('when the balance is > 0.0001 Eth and less than 1 Eth', () => {
+    const amount = '75800'
+
+    it('shows the balance in Eth to two decimal places', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('0.076')
+          expect(fiatValue).toEqual('14.86')
+        },
+      })
+    })
+  })
+
+  describe('when the balance is > 1 Eth ', () => {
+    const amount = '2000000'
+
+    it('shows the balance in Eth to two decimal places', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('2.00')
+          expect(fiatValue).toEqual('391.98')
+        },
+      })
+    })
+  })
+
+  describe('when the balance converts to > $1000 ', () => {
+    const amount = '20000000'
+
+    it('shows the balance in dollars in locale format without decimal', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('20.00')
+          expect(fiatValue).toEqual('3,920')
+        },
+      })
+    })
+  })
+
+  describe('when the balance converts to > $100k ', () => {
+    const amount = '2000000000'
+
+    it('shows the balance in thousands of dollars postfixed with k', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('2000.00')
+          expect(fiatValue).toEqual('392k')
+        },
+      })
+    })
+  })
+
+  describe('when the balance converts to > $1m ', () => {
+    const amount = '20000000000'
+
+    it('shows the balance in millions of dollars postfixed with m', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('20000.00')
+          expect(fiatValue).toEqual('3.9m')
+        },
+      })
+    })
+  })
+
+  describe.skip('when the balance converts to > $1b ', () => {
+    const amount = '20000000000000'
+
+    it('shows the balance in billions of dollars postfixed with b', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('20000000.00')
+          expect(fiatValue).toEqual('3.9b')
+        },
+      })
+    })
+  })
+
+  describe.skip('when the balance converts to > $1b, unit is eth (used in lock form)', () => {
+    const amount = '9999999'
+
+    it('shows the balance in billions of dollars postfixed with b', () => {
+      renderIt({
+        amount,
+        render: (ethValue, fiatValue) => {
+          expect(ethValue).toEqual('9999999.00')
+          expect(fiatValue).toEqual('2b')
+        },
+      })
+    })
+  })
+})
