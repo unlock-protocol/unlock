46ad2d836b39f2652bdc9a4c2f56577de8784d8e
Nick Furfaro <nfurfaro33@gmail.com>
2018-11-19 14:02:47 -0700

    Upgrade openzepplin-solidity to v2.0.0
    Add zos to devDependencies.
    Introduced TestHelper from zos and refactored tests around it.
    Edited Unlock and PublicLock contracts to use fn transferOwnership

--- smart-contracts/test/Unlock/behaviors/shared.js
+++ smart-contracts/test/Unlock/behaviors/shared.js
@@ -4,11 +4,11 @@ const createLock = require('./createLock')
 const recordConsumedDiscount = require('./recordConsumedDiscount')
 const recordKeyPurchase = require('./recordKeyPurchase')
 
-exports.shouldBehaveLikeV1 = function (accounts, unlockOwner) {
+exports.shouldBehaveLikeV1 = function (accounts, unlockOwner, _logIndex) {
   describe('should behave like v1', function () {
     initialization.shouldHaveInitialized(unlockOwner)
     computeAvailableDiscountFor.shouldComputeAvailableDiscountFor()
-    createLock.shouldCreateLock(accounts)
+    createLock.shouldCreateLock(accounts, _logIndex)
     recordConsumedDiscount.shouldRecordConsumedDiscount()
     recordKeyPurchase.shouldRecordKeyPurchase()
   })
