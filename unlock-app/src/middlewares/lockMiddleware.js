6ae118864ede95d3c7f46b451a201ee5fc2bb05e
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-20 18:32:49 -0500

    loading account even when the network was changed (#582)

--- unlock-app/src/middlewares/lockMiddleware.js
+++ unlock-app/src/middlewares/lockMiddleware.js
@@ -96,6 +96,8 @@ export default function lockMiddleware({ getState, dispatch }) {
     if (getState().network.name !== networkId) {
       // Set the new network, which should also clean up all reducers
       dispatch(setNetwork(networkId))
+      // So we need a new account!
+      return web3Service.refreshOrGetAccount()
     } else {
       // The network is the same, so let's refresh
       // We refresh transactions
