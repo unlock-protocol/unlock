28f2bcc22567c6666c238bf2da1fa4a841f1a6b7
Ben Werdmuller <ben@withknown.com>
2018-11-21 10:23:28 -0800

    Withdrawal logic is now attached to withdraw button (#588)
    * Adding withdrawal logic
    
    * Converting withdraw button to component
    
    * Adding withdrawal logic
    
    * Converting withdraw button to component
    
    * Adding test
    
    * Reformat
    
    * Removing dash change
    
    * Revised withrawal test
    
    * Destructuring, stateless function

--- unlock-app/src/components/interface/buttons/lock/Withdraw.js
+++ unlock-app/src/components/interface/buttons/lock/Withdraw.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
 import Svg from '../../svg'
 import { LockButton } from '../Button'
+import UnlockPropTypes from '../../../../propTypes'
+import { withdrawFromLock } from '../../../../actions/lock'
 
-const Withdraw = props => (
-  <LockButton title="Withdraw balance" {...props}>
+export const Withdraw = ({ lock, withdraw, account, ...props }) => (
+  <LockButton
+    title="Withdraw balance"
+    action={() => {
+      if (lock.balance > 0) {
+        withdraw(lock, account)
+      }
+    }}
+    {...props}
+  >
     <Svg.Withdraw name="Withdraw" />
   </LockButton>
 )
 
-export default Withdraw
+Withdraw.propTypes = {
+  lock: UnlockPropTypes.lock.isRequired,
+  transaction: UnlockPropTypes.transaction,
+  withdraw: PropTypes.func,
+  account: UnlockPropTypes.account,
+}
+
+Withdraw.defaultProps = {
+  transaction: null,
+  account: null,
+  withdraw: () => {},
+}
+
+const mapStateToProps = ({ account, transactions }, { lock }) => {
+  const transaction = transactions[lock.transaction]
+  return {
+    account,
+    transaction,
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  withdraw: (lock, account) => dispatch(withdrawFromLock(lock, account)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Withdraw)
