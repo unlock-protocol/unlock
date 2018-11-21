566e0e21c0b9c66e0453baab5acf4eab06c925d3
Ben Werdmuller <ben@withknown.com>
2018-11-21 11:46:12 -0800

    Display label for withdrawal transactions (#590)
    * Display label for withdrawal transactions
    
    * Removing withdrawal confirmation margin
    
    * Oops. We do need *some* space ...

---
+++ unlock-app/src/__tests__/components/creator/lock/LockIconBar.test.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import { Provider } from 'react-redux'
+
+import { LockIconBar } from '../../../../components/creator/lock/LockIconBar'
+import configure from '../../../../config'
+import createUnlockStore from '../../../../createUnlockStore'
+
+const toggleCode = jest.fn()
+
+describe('LockIconBar', () => {
+  it('should display a submitted label when withdrawal has been submitted', () => {
+    const config = configure({})
+
+    const lock = {
+      id: 'lockwithdrawalsubmittedid',
+      keyPrice: '10000000000000000000',
+      expirationDuration: '172800',
+      maxNumberOfKeys: 240,
+      outstandingKeys: 3,
+      address: '0xbc7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+    }
+    const transaction = {
+      status: 'mined',
+      confirmations: 24,
+    }
+    const withdrawalTransaction = {
+      status: 'submitted',
+      confirmations: 0,
+      withdrawal: 'lockwithdrawalsubmittedid',
+    }
+
+    const store = createUnlockStore({})
+
+    let wrapper = rtl.render(
+      <Provider store={store}>
+        <LockIconBar
+          lock={lock}
+          transaction={transaction}
+          withdrawalTransaction={withdrawalTransaction}
+          toggleCode={toggleCode}
+          config={config}
+        />
+      </Provider>
+    )
+
+    expect(
+      wrapper.queryByText('Submitted to Network', { exact: false })
+    ).not.toBeNull()
+  })
+  it('should display a confirming label when withdrawal is confirming', () => {
+    const config = configure({})
+
+    const lock = {
+      id: 'lockwithdrawalconfirmingid',
+      keyPrice: '10000000000000000000',
+      expirationDuration: '172800',
+      maxNumberOfKeys: 240,
+      outstandingKeys: 3,
+      address: '0xba7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+    }
+    const transaction = {
+      status: 'mined',
+      confirmations: 24,
+    }
+    const withdrawalTransaction = {
+      status: 'mined',
+      confirmations: 2,
+      withdrawal: 'lockwithdrawalconfirmingid',
+    }
+
+    const store = createUnlockStore({})
+
+    let wrapper = rtl.render(
+      <Provider store={store}>
+        <LockIconBar
+          lock={lock}
+          transaction={transaction}
+          withdrawalTransaction={withdrawalTransaction}
+          toggleCode={toggleCode}
+          config={config}
+        />
+      </Provider>
+    )
+
+    expect(
+      wrapper.queryByText('Confirming Withdrawal', { exact: false })
+    ).not.toBeNull()
+    expect(wrapper.queryByText('2/12', { exact: false })).not.toBeNull()
+  })
+})
--- unlock-app/src/components/creator/lock/LockIconBar.js
+++ unlock-app/src/components/creator/lock/LockIconBar.js
@@ -8,7 +8,13 @@ import UnlockPropTypes from '../../../propTypes'
 import CreatorLockStatus from './CreatorLockStatus'
 import withConfig from '../../../utils/withConfig'
 
-export function LockIconBar({ lock, toggleCode, transaction, config }) {
+export function LockIconBar({
+  lock,
+  toggleCode,
+  transaction,
+  withdrawalTransaction,
+  config,
+}) {
   if (!transaction) {
     // We assume that the lock has been succeesfuly deployed?
     // TODO if the transaction is missing we should try to look it up from the lock address
@@ -28,14 +34,31 @@ export function LockIconBar({ lock, toggleCode, transaction, config }) {
   }
 
   return (
-    <IconBarContainer>
-      <IconBar>
-        <Buttons.Withdraw as="button" lock={lock} />
-        <Buttons.Edit as="button" />
-        {/* Reinstate when we're ready <Buttons.ExportLock /> */}
-        <Buttons.Code action={toggleCode} as="button" />
-      </IconBar>
-    </IconBarContainer>
+    <StatusBlock>
+      <IconBarContainer>
+        <IconBar>
+          <Buttons.Withdraw as="button" lock={lock} />
+          <Buttons.Edit as="button" />
+          {/* Reinstate when we're ready <Buttons.ExportLock /> */}
+          <Buttons.Code action={toggleCode} as="button" />
+        </IconBar>
+      </IconBarContainer>
+      <SubStatus>
+        {withdrawalTransaction &&
+          withdrawalTransaction.status === 'submitted' && (
+            <>Submitted to Network...</>
+        )}
+        {withdrawalTransaction &&
+          withdrawalTransaction.status === 'mined' &&
+          withdrawalTransaction.confirmations <
+            config.requiredConfirmations && (
+            <>
+              Confirming Withdrawal {withdrawalTransaction.confirmations}/
+              {config.requiredConfirmations}
+            </>
+        )}
+      </SubStatus>
+    </StatusBlock>
   )
 }
 
@@ -43,17 +66,25 @@ LockIconBar.propTypes = {
   lock: UnlockPropTypes.lock.isRequired,
   toggleCode: PropTypes.func.isRequired,
   transaction: UnlockPropTypes.transaction,
+  withdrawalTransaction: UnlockPropTypes.transaction,
   config: UnlockPropTypes.configuration.isRequired,
 }
 
 LockIconBar.defaultProps = {
   transaction: null,
+  withdrawalTransaction: null,
 }
 
-const mapStateToProps = (state, { lock }) => {
-  const transaction = state.transactions[lock.transaction]
+const mapStateToProps = ({ transactions }, { lock }) => {
+  const transaction = transactions[lock.transaction]
+  let withdrawalTransaction = null
+  Object.keys(transactions).forEach(el => {
+    if (transactions[el].withdrawal && transactions[el].withdrawal === lock.id)
+      withdrawalTransaction = transactions[el]
+  })
   return {
     transaction,
+    withdrawalTransaction,
   }
 }
 
@@ -69,4 +100,16 @@ const IconBar = styled.div`
   display: grid;
   grid-gap: 16px;
   grid-template-columns: repeat(3, 24px);
+`
+
+const StatusBlock = styled.div``
+
+const SubStatus = styled.div`
+  margin-top: 13px;
+  font-size: 10px;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: normal;
+  color: var(--green);
+  text-align: right;
+  padding-right: 24px;
 `
