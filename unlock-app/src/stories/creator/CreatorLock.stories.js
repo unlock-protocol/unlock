9c3155c86b670694379fa51972e999a8b0316ed6
Ben Werdmuller <ben@withknown.com>
2018-11-21 11:46:12 -0800

    Display label for withdrawal transactions (#590)
    * Display label for withdrawal transactions
    
    * Removing withdrawal confirmation margin
    
    * Oops. We do need *some* space ...

--- unlock-app/src/stories/creator/CreatorLock.stories.js
+++ unlock-app/src/stories/creator/CreatorLock.stories.js
@@ -6,21 +6,31 @@ import createUnlockStore from '../../createUnlockStore'
 
 const store = createUnlockStore({
   transactions: {
-    '0x123': {
+    deployedid: {
       status: 'mined',
       confirmations: 24,
     },
-    '0x456': {
+    confirmingid: {
       status: 'mined',
       confirmations: 4,
     },
-    '0x111': {
+    submittedid: {
       status: 'submitted',
       confirmations: 0,
     },
+    withdrawalconfirmingid: {
+      status: 'mined',
+      confirmations: 2,
+      withdrawal: 'lockwithdrawalconfirmingid',
+    },
+    withdrawalsubmittedid: {
+      status: 'submitted',
+      confirmations: 0,
+      withdrawal: 'lockwithdrawalsubmittedid',
+    },
   },
   keys: {
-    '0x678': {
+    keyid: {
       transaction: '0x23749328748932748932473298473289473298',
       lockAddress: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
       expiration: Math.floor(new Date().getTime() / 1000) + 86400 * 30, // 30 days from right now
@@ -41,7 +51,7 @@ storiesOf('CreatorLock', CreatorLock)
       maxNumberOfKeys: '240',
       outstandingKeys: '3',
       address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: '0x123',
+      transaction: 'deployedid',
     }
     return <CreatorLock lock={lock} />
   })
@@ -52,7 +62,7 @@ storiesOf('CreatorLock', CreatorLock)
       maxNumberOfKeys: '240',
       outstandingKeys: '3',
       address: '0x127c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: '0x111',
+      transaction: 'submittedid',
     }
     return <CreatorLock lock={lock} />
   })
@@ -63,7 +73,7 @@ storiesOf('CreatorLock', CreatorLock)
       maxNumberOfKeys: '240',
       outstandingKeys: '3',
       address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: '0x456',
+      transaction: 'confirmingid',
     }
     const transaction = {
       status: 'mined',
@@ -89,11 +99,35 @@ storiesOf('CreatorLock', CreatorLock)
       maxNumberOfKeys: '240',
       outstandingKeys: '3',
       address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: '0x123',
+      transaction: 'deployedid',
     }
     const transaction = {
       status: 'mined',
       confirmations: 14,
     }
     return <CreatorLock lock={lock} transaction={transaction} />
+  })
+  .add('Withdrawal submitted', () => {
+    const lock = {
+      id: 'lockwithdrawalsubmittedid',
+      keyPrice: '10000000000000000000',
+      expirationDuration: '172800',
+      maxNumberOfKeys: '240',
+      outstandingKeys: '3',
+      address: '0xbc7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+    }
+    return <CreatorLock lock={lock} />
+  })
+  .add('Withdrawing', () => {
+    const lock = {
+      id: 'lockwithdrawalconfirmingid',
+      keyPrice: '10000000000000000000',
+      expirationDuration: '172800',
+      maxNumberOfKeys: '240',
+      outstandingKeys: '3',
+      address: '0xba7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+    }
+    return <CreatorLock lock={lock} />
   })
