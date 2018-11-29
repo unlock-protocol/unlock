69233957a5a55a506168d2bb15a427ced1802393
Julien Genestoux <julien.genestoux@gmail.com>
2018-11-29 17:47:06 -0500

    Fixing Overlay to purchase keys (#631)

--- unlock-app/src/components/lock/ShowUnlessUserHasKeyToAnyLock.js
+++ unlock-app/src/components/lock/ShowUnlessUserHasKeyToAnyLock.js
@@ -1,33 +1,60 @@
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import UnlockPropTypes from '../../propTypes'
+import { showModal } from '../../actions/modal'
 
-/**
- * TODO: We should probably check that the transaction for that key has been confirmed...
- */
-export const ShowUnlessUserHasKeyToAnyLock = ({ locks, keys, children }) => {
-  const now = new Date().getTime() / 1000
-  for (let lock of Object.values(locks)) {
-    for (let key of Object.values(keys)) {
-      if (key.lockAddress === lock.address && key.expiration > now) {
-        return null
-      }
-    }
+export const ShowUnlessUserHasKeyToAnyLock = ({
+  keys,
+  modalShown,
+  showModal,
+  children,
+}) => {
+  // We have at least one valid key and the modal was not shown
+  if (keys.length > 0 && !modalShown) {
+    return null
   }
 
+  // There is a valid key, but we shown the modal previously
+  if (keys.length > 0 && !!modalShown) {
+    return children
+  }
+
+  // There is no valid key, but the modal has not been set so we set it
+  if (!modalShown) {
+    showModal()
+  }
   return children
 }
 
 ShowUnlessUserHasKeyToAnyLock.propTypes = {
+  modalShown: PropTypes.bool,
   locks: UnlockPropTypes.locks,
-  keys: UnlockPropTypes.keys,
+  keys: PropTypes.arrayOf(UnlockPropTypes.key),
   children: PropTypes.node,
 }
 
-const mapStateToProps = state => {
+export const mapDispatchToProps = (dispatch, { locks }) => ({
+  showModal: () => dispatch(showModal(Object.keys(locks).join('-'))),
+})
+
+export const mapStateToProps = ({ keys, modals }, { locks }) => {
+  let validKeys = []
+  const now = new Date().getTime() / 1000
+  for (let lock of Object.values(locks)) {
+    for (let k of Object.values(keys)) {
+      if (k.lockAddress === lock.address && k.expiration > now) {
+        validKeys.push(k)
+      }
+    }
+  }
+
   return {
-    keys: state.keys,
+    modalShown: !!modals[Object.keys(locks).join('-')],
+    keys: validKeys,
   }
 }
 
-export default connect(mapStateToProps)(ShowUnlessUserHasKeyToAnyLock)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShowUnlessUserHasKeyToAnyLock)
