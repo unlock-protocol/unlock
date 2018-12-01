e1813c2b076222355b6a23f2f7fe86da64203ac7
julien <julien.genestoux@gmail.com>
2018-12-01 14:04:23 -0500

    regenerated SVG components

--- unlock-app/src/components/interface/svg/Hamburger.js
+++ unlock-app/src/components/interface/svg/Hamburger.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const SvgHamburger = ({ title, ...props }) => (
-  <svg fill="none" {...props}>
+  <svg {...props}>
     <title>{title}</title>
     <path
+      fillRule="evenodd"
       clipRule="evenodd"
       d="M0 3a3 3 0 0 1 3-3h56a3 3 0 1 1 0 6H3a3 3 0 0 1-3-3zm0 18a3 3 0 0 1 3-3h56a3 3 0 1 1 0 6H3a3 3 0 0 1-3-3zm0 18a3 3 0 0 1 3-3h56a3 3 0 1 1 0 6H3a3 3 0 0 1-3-3z"
     />
