4eb066f87a710e731101c7adc99b5a4c801ed157
Julien Genestoux <julien.genestoux@gmail.com>
2018-12-05 22:40:15 -0500

    moved husky to root of repo (#673)

---
+++ .huskyrc.js
@@ -0,0 +1,27 @@
+const tasks = tasks => {
+  return tasks
+    .map(task => {
+      return task.command
+    })
+    .join(`&& cd ${__dirname} && `)
+}
+
+// If svg files are changed, generate the SVG Components
+const svg2Components = {
+  command:
+    'if [[ $(git diff --cached --name-only | grep -c "unlock-app/src/static/images/svg/.*.svg$") > 0 ]] ; then npm run svg-2-components && git add src/components/interface/svg/*.js; fi',
+  path: "unlock-app"
+}
+
+// Run lint on staged files
+const lintStaged = {
+  command: "cd unlock-app && npx lint-staged",
+  path: "unlock-app"
+}
+
+// tasks are given a path
+module.exports = {
+  hooks: {
+    "pre-commit": tasks([svg2Components, lintStaged])
+  }
+}
