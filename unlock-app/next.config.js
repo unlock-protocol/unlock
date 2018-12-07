b6ca1b9c9cd0e7eaecad7ae962fcb10557be184a
Julien Genestoux <julien.genestoux@gmail.com>
2018-12-07 09:22:19 -0500

    adding the demo page to static page generated as well as a way to redirect (#679)

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -6,11 +6,22 @@ const copyFile = promisify(fs.copyFile)
 
 module.exports = {
   exportPathMap: async (defaultPathMap, { dev, dir, outDir }) => {
-
     // Export robots.txt and humans.txt in non-dev environments
     if (!dev && outDir) {
-      await copyFile(join(dir, 'static', 'robots.txt'), join(outDir, 'robots.txt'))
-      await copyFile(join(dir, 'static', 'humans.txt'), join(outDir, 'humans.txt'))
+      await copyFile(
+        join(dir, 'static', 'robots.txt'),
+        join(outDir, 'robots.txt')
+      )
+      await copyFile(
+        join(dir, 'static', 'humans.txt'),
+        join(outDir, 'humans.txt')
+      )
+
+      // Export _redirects which is used by netlify for URL rewrites
+      await copyFile(
+        join(dir, 'static', '_redirects'),
+        join(outDir, '_redirects')
+      )
     }
 
     return {
@@ -18,6 +29,7 @@ module.exports = {
       '/about': { page: '/about' },
       '/jobs': { page: '/jobs' },
       '/dashboard': { page: '/dashboard' },
+      '/demo': { page: '/demo' },
     }
   },
 }
