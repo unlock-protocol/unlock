4afdf62f3830a36048bf357aa79f6c7d95036af7
Ben Werdmuller <ben@withknown.com>
2018-12-10 11:24:34 -0800

    Removing name from pending lock for now (#689)
    * Removing name from pending lock for now
    
    * Removing unused LockName reference

--- unlock-app/src/components/lock/PendingKeyLock.js
+++ unlock-app/src/components/lock/PendingKeyLock.js
@@ -8,7 +8,6 @@ import {
   LockDetails,
   TransactionStatus,
   LockDetail,
-  LockName,
 } from './LockStyles'
 import BalanceProvider from '../helpers/BalanceProvider'
 
@@ -22,7 +21,6 @@ export const PendingKeyLock = ({ lock }) => (
         amount={lock.keyPrice}
         render={(ethPrice, fiatPrice) => (
           <>
-            <LockName>{lock.name}</LockName>
             <LockDetails>
               <LockDetail bold>
                 {ethPrice}
