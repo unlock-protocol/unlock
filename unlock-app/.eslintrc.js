359ca1ae54e899414d3db226b52a916d12356696
Julien Genestoux <julien.genestoux@gmail.com>
2018-12-10 13:00:29 -0500

    forbidding exclusive tests in mocha (#686)

--- unlock-app/.eslintrc.js
+++ unlock-app/.eslintrc.js
@@ -1,58 +1,48 @@
 module.exports = {
-  "extends": [
-    "standard",
-    "airbnb",
-    "eslint:recommended",
-    "plugin:react/recommended",
+  extends: [
+    'standard',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
-  "env": {
-    "es6": true,
-    "node": true,
-    "browser": true,
-    "jest": true
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
   },
-  "parser": "babel-eslint",
-  "rules": {
-    "react/jsx-wrap-multilines": false,
-    "react/prefer-stateless-function": [2],
-    "indent": [
-      "error",
-      2
+  plugins: ['mocha'],
+  parser: 'babel-eslint',
+  rules: {
+    'react/jsx-wrap-multilines': false,
+    'react/prefer-stateless-function': [2],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 0,
+        maxBOF: 0,
+      },
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
+    'react/forbid-prop-types': 2,
+    'comma-dangle': [2, 'always-multiline'],
+    'eol-last': ['error'],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
     ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "no-multiple-empty-lines": [
-      "error", {
-        "max": 1,
-        "maxEOF": 0,
-        "maxBOF": 0
-      }
-    ],
-    "brace-style": [2, "1tbs", { "allowSingleLine": true }],
-    "react/forbid-prop-types": 2,
-    "comma-dangle": [2, "always-multiline"],
-    "eol-last": [
-      "error"
-    ],
-    "jsx-a11y/anchor-is-valid": [
-      "error", {
-        "components": [ "Link" ],
-        "specialLink": [ "hrefLeft", "hrefRight" ],
-        "aspects": [ "invalidHref", "preferButton" ]
-      }
-    ],
-    "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }], 
-    "import/no-named-as-default": 0,
-    "import/no-named-as-default-member": 0
-  }
-};
+    'mocha/no-exclusive-tests': 'error',
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+  },
+}
