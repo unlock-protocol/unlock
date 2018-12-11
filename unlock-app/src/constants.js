ac7c1491fd38d49f03f95223fe631665ad0c555b
wawhal <rishichandra.wawhal@gmail.com>
2018-12-12 00:18:58 +0530

    add constants

--- unlock-app/src/constants.js
+++ unlock-app/src/constants.js
@@ -45,3 +45,7 @@ export const MONTH_NAMES = [
   'Nov',
   'Dec',
 ]
+
+// constants related to pagination
+export const PGN_ITEMS_PER_PAGE = 10
+export const PGN_MAX_NUMBER_OF_PAGES_TO_SHOW_ALL = 10
