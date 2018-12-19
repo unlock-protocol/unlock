5969236737be0ac6136c7b79baacfb2899293bdd
Julien Genestoux <julien.genestoux@gmail.com>
2018-12-19 16:53:54 -0500

    not using the server side default props but the state of the router. This fixes #757 and #777 (#779)

--- unlock-app/src/pages/paywall.js
+++ unlock-app/src/pages/paywall.js
@@ -4,35 +4,28 @@ import NoSSR from 'react-no-ssr'
 import UnlockPropTypes from '../propTypes'
 import Overlay from '../components/lock/Overlay'
 import ShowUnlessUserHasKeyToAnyLock from '../components/lock/ShowUnlessUserHasKeyToAnyLock'
+import { LOCK_PATH_NAME_REGEXP } from '../constants'
 
-export class Paywall extends React.Component {
-  static async getInitialProps({ query: { lockAddress } }) {
-    return {
-      lockAddress: lockAddress,
-    }
-  }
-
-  render() {
-    const { lock } = this.props
-
-    return (
-      <NoSSR>
-        <ShowUnlessUserHasKeyToAnyLock locks={lock ? [lock] : []}>
-          <Overlay locks={lock ? [lock] : []} />
-        </ShowUnlessUserHasKeyToAnyLock>
-      </NoSSR>
-    )
-  }
+const Paywall = ({ lock }) => {
+  return (
+    <NoSSR>
+      <ShowUnlessUserHasKeyToAnyLock locks={lock ? [lock] : []}>
+        <Overlay locks={lock ? [lock] : []} />
+      </ShowUnlessUserHasKeyToAnyLock>
+    </NoSSR>
+  )
 }
 
 Paywall.propTypes = {
   lock: UnlockPropTypes.lock.isRequired,
 }
 
-export const mapStateToProps = ({ locks: stateLocks }, { lockAddress }) => {
-  const lock = Object.values(stateLocks).find(
-    lock => lock.address === lockAddress
-  )
+export const mapStateToProps = ({ locks, router }) => {
+  const match = router.location.pathname.match(LOCK_PATH_NAME_REGEXP)
+
+  const lock = match
+    ? Object.values(locks).find(lock => lock.address === match[1])
+    : null
 
   return {
     lock,
