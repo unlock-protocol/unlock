baeab6ce5c8ee900023d6b738ec6885c09082ec1
Gregory Beaver <greg@unlock-protocol.com>
2018-12-20 11:44:15 -0500

    implement and test the callback

--- unlock-app/src/__tests__/components/developer/DeveloperOverlay.test.js
+++ unlock-app/src/__tests__/components/developer/DeveloperOverlay.test.js
@@ -1,22 +1,82 @@
 import React from 'react'
 import * as rtl from 'react-testing-library'
+import createUnlockStore from '../../../createUnlockStore'
 
 import { DeveloperOverlay } from '../../../components/developer/DeveloperOverlay'
 
 describe('DeveloperOverlay', () => {
-  const providers = ['HTML', 'Metamask']
-  it('has a dropdown that can be used to choose between providers', () => {
-    const component = rtl.render(<DeveloperOverlay providers={providers} />)
+  let config
+  let callback
+  beforeEach(() => {
+    callback = jest.fn()
+    config = {
+      env: 'dev',
+      isRequiredNetwork: () => false,
+      isServer: false,
+      providers: {
+        HTTP: {},
+        Metamask: {},
+        Opera: {},
+      },
+    }
+  })
 
-    expect(component.queryByText('HTML')).not.toBeNull()
+  it('has a dropdown that can be used to choose between providers', () => {
+    const component = rtl.render(
+      <DeveloperOverlay config={config} selectProvider={callback} />
+    )
+
+    expect(component.queryByText('HTTP')).not.toBeNull()
     expect(component.queryByText('Metamask')).not.toBeNull()
   })
 
   it('sets selected provider from prop', () => {
     const component = rtl.render(
-      <DeveloperOverlay providers={providers} selected="Metamask" />
+      <DeveloperOverlay
+        config={config}
+        selected="Metamask"
+        selectProvider={callback}
+      />
     )
 
     expect(component.queryBySelectText('Metamask')).not.toBeNull()
+  })
+
+  it('selects provider', async () => {
+    const component = rtl.render(
+      <DeveloperOverlay
+        config={config}
+        selected="Metamask"
+        selectProvider={callback}
+      />
+    )
+    expect(component.queryBySelectText('Metamask')).not.toBeNull()
+
+    rtl.fireEvent.change(component.getBySelectText('Metamask'), {
+      target: { value: 'HTTP' },
+    })
+    expect(callback).toHaveBeenCalledWith('HTTP')
+  })
+
+  describe('connected overlay', () => {
+    let store
+
+    beforeEach(() => {
+      store = createUnlockStore({
+        provider: 'HTTP',
+      })
+      config = {
+        env: 'dev',
+        isRequiredNetwork: () => false,
+        isServer: false,
+        providers: {
+          HTTP: {},
+          Metamask: {},
+          Opera: {},
+        },
+      }
+    })
+
+    it('with actual config', () => {})
   })
 })
