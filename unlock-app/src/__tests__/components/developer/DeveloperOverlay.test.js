ff1d9861a0248a6a86beba9c4882e44f3990725a
Gregory Beaver <greg@unlock-protocol.com>
2018-12-20 11:01:45 -0500

    set value based on selected prop

--- unlock-app/src/__tests__/components/developer/DeveloperOverlay.test.js
+++ unlock-app/src/__tests__/components/developer/DeveloperOverlay.test.js
@@ -4,12 +4,19 @@ import * as rtl from 'react-testing-library'
 import { DeveloperOverlay } from '../../../components/developer/DeveloperOverlay'
 
 describe('DeveloperOverlay', () => {
+  const providers = ['HTML', 'Metamask']
   it('has a dropdown that can be used to choose between providers', () => {
+    const component = rtl.render(<DeveloperOverlay providers={providers} />)
+
+    expect(component.queryByText('HTML')).not.toBeNull()
+    expect(component.queryByText('Metamask')).not.toBeNull()
+  })
+
+  it('sets selected provider from prop', () => {
     const component = rtl.render(
-      <DeveloperOverlay providers={['HTML', 'Metamask']} />
+      <DeveloperOverlay providers={providers} selected="Metamask" />
     )
 
-    expect(component.getBySelectText('HTML'))
-    expect(component.getBySelectText('Metamask'))
+    expect(component.queryBySelectText('Metamask')).not.toBeNull()
   })
 })
