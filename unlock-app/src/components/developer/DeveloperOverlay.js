3587f9eda0e67642997a6f130d3ed0216dab406d
Gregory Beaver <greg@unlock-protocol.com>
2018-12-20 11:44:15 -0500

    implement and test the callback

--- unlock-app/src/components/developer/DeveloperOverlay.js
+++ unlock-app/src/components/developer/DeveloperOverlay.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import UnlockPropTypes from '../../propTypes'
 
-export const DeveloperOverlay = ({ providers, selected }) => (
-  <select value={selected}>
-    {providers.map(provider => (
-      <option value={provider} key={provider}>
-        {provider}
-      </option>
-    ))}
-  </select>
-)
+export function DeveloperOverlay({ config, selected, selectProvider }) {
+  const providers = Object.keys(config.providers)
+  return (
+    <select value={selected} onChange={e => selectProvider(e.target.value)}>
+      {providers.map(provider => (
+        <option value={provider} key={provider}>
+          {provider}
+        </option>
+      ))}
+    </select>
+  )
+}
 
 DeveloperOverlay.propTypes = {
-  providers: UnlockPropTypes.providerList.isRequired,
+  config: UnlockPropTypes.configuration.isRequired,
   selected: UnlockPropTypes.provider,
+  selectProvider: UnlockPropTypes.callback.isRequired,
 }
 
 DeveloperOverlay.defaultProps = {
