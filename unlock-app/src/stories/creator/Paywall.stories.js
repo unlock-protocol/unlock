0b7311f1418f4a994f2acc01e8a66eb072b7f350
Gregory Beaver <greg@unlock-protocol.com>
2018-12-21 12:53:30 -0500

    fix Paywall story

--- unlock-app/src/stories/creator/Paywall.stories.js
+++ unlock-app/src/stories/creator/Paywall.stories.js
@@ -8,14 +8,14 @@ const lock = {
   address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
   keyPrice: '10000000000000000000',
   expirationDuration: '86400',
-  maxNumberOfKeys: '800',
-  outstandingKeys: '32',
+  maxNumberOfKeys: 800,
+  outstandingKeys: 32,
 }
 
 const store = createUnlockStore({
-  locks: [
-    lock,
-    {
+  locks: {
+    '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e': lock,
+    '0xab7c74abc0c4d48d1bdad5dcb26153fc87ffffff': {
       address: '0xab7c74abc0c4d48d1bdad5dcb26153fc87ffffff',
       name: 'My Blog',
       keyPrice: '27000000000000000',
@@ -23,7 +23,7 @@ const store = createUnlockStore({
       maxNumberOfKeys: 240,
       outstandingKeys: 3,
     },
-  ],
+  },
   router: {
     location: {
       pathname: '/demo/' + lock.address,
@@ -37,5 +37,5 @@ const store = createUnlockStore({
 storiesOf('Paywall', Paywall)
   .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
   .add('the paywall overlay', () => {
-    return <Paywall lock={lock} />
+    return <Paywall />
   })
