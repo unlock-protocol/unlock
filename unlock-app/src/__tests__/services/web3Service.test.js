ef8500299e84e72da865fab100a24c196e0c6ffb
Julien Genestoux <julien.genestoux@gmail.com>
2018-12-21 13:51:13 -0500

    assigning types to transaction based on their data/input (#824)

--- unlock-app/src/__tests__/services/web3Service.test.js
+++ unlock-app/src/__tests__/services/web3Service.test.js
@@ -7,6 +7,7 @@ import Web3Service from '../../services/web3Service'
 import UnlockContract from '../../artifacts/contracts/Unlock.json'
 import LockContract from '../../artifacts/contracts/PublicLock.json'
 import configure from '../../config'
+import { TRANSACTION_TYPES } from '../../constants'
 
 const defaultState = {
   network: {
@@ -426,7 +427,7 @@ describe('Web3Service', () => {
 
     describe('getTransaction', () => {
       it('should update the number of confirmation based on number of blocks since the transaction', done => {
-        expect.assertions(1)
+        expect.assertions(2)
         ethBlockNumber(`0x${(29).toString('16')}`)
         ethGetTransactionByHash(transaction.hash, {
           hash:
@@ -449,8 +450,11 @@ describe('Web3Service', () => {
           status: '0x0',
         })
 
+        web3Service.getTransactionType = jest.fn(() => 'TYPE')
+
         web3Service.once('transaction.updated', (transaction, update) => {
           expect(update.confirmations).toEqual(15) //29-14
+          expect(update.type).toEqual('TYPE') //29-14
           done()
         })
 
@@ -496,7 +500,7 @@ describe('Web3Service', () => {
         })
 
         it('should mark the transaction as failed if the transaction receipt status is false', done => {
-          expect.assertions(2)
+          expect.assertions(3)
           ethGetTransactionReceipt(transaction.hash, {
             transactionIndex: '0x3',
             blockHash:
@@ -507,9 +511,11 @@ describe('Web3Service', () => {
             logs: [],
             status: '0x0',
           })
+          web3Service.getTransactionType = jest.fn(() => 'TYPE')
 
           web3Service.once('transaction.updated', (transaction, update) => {
             expect(update.confirmations).toEqual(15) //29-14
+            expect(update.type).toEqual('TYPE')
             web3Service.once('transaction.updated', (transaction, update) => {
               expect(update.status).toBe('failed')
               done()
@@ -520,7 +526,7 @@ describe('Web3Service', () => {
         })
 
         it('should parseTransactionLogsFromReceipt with the Unlock abi if the address is one of the Unlock contract', done => {
-          expect.assertions(4)
+          expect.assertions(5)
           const transactionReceipt = {
             transactionIndex: '0x3',
             blockHash:
@@ -534,6 +540,8 @@ describe('Web3Service', () => {
           ethGetTransactionReceipt(transaction.hash, transactionReceipt)
           const previousAddress = web3Service.unlockContractAddress
 
+          web3Service.getTransactionType = jest.fn(() => 'TYPE')
+
           web3Service.parseTransactionLogsFromReceipt = (
             transactionToUpdate,
             abi,
@@ -544,6 +552,10 @@ describe('Web3Service', () => {
             expect(receipt.blockNumber).toEqual(344)
             expect(receipt.logs).toEqual([])
             web3Service.unlockContractAddress = previousAddress
+            expect(web3Service.getTransactionType).toHaveBeenCalledWith(
+              UnlockContract.abi,
+              blockTransaction.input
+            )
             done()
           }
           web3Service.unlockContractAddress = blockTransaction.to
@@ -551,7 +563,7 @@ describe('Web3Service', () => {
         })
 
         it('should parseTransactionLogsFromReceipt with the Lock abi otherwise', done => {
-          expect.assertions(4)
+          expect.assertions(5)
           const transactionReceipt = {
             transactionIndex: '0x3',
             blockHash:
@@ -564,6 +576,8 @@ describe('Web3Service', () => {
           }
           ethGetTransactionReceipt(transaction.hash, transactionReceipt)
 
+          web3Service.getTransactionType = jest.fn(() => 'TYPE')
+
           web3Service.parseTransactionLogsFromReceipt = (
             transactionToUpdate,
             abi,
@@ -573,6 +587,10 @@ describe('Web3Service', () => {
             expect(abi).toEqual(LockContract.abi)
             expect(receipt.blockNumber).toEqual(344)
             expect(receipt.logs).toEqual([])
+            expect(web3Service.getTransactionType).toHaveBeenCalledWith(
+              LockContract.abi,
+              blockTransaction.input
+            )
             done()
           }
 
@@ -694,6 +712,39 @@ describe('Web3Service', () => {
       })
     })
 
+    describe('getTransactionType', () => {
+      it('should return the right transaction type on lock creation', () => {
+        expect.assertions(1)
+        const unlock = new web3Service.web3.eth.Contract(UnlockContract.abi, '')
+        const data = unlock.methods
+          .createLock('1000', '1000000000', '1')
+          .encodeABI()
+        expect(web3Service.getTransactionType(UnlockContract.abi, data)).toBe(
+          TRANSACTION_TYPES.LOCK_CREATION
+        )
+      })
+
+      it('should return the right transaction type on key purchase', () => {
+        expect.assertions(1)
+        const lock = new web3Service.web3.eth.Contract(LockContract.abi, '')
+        const data = lock.methods
+          .purchaseFor(nodeAccounts[0], Web3Utils.utf8ToHex(''))
+          .encodeABI()
+        expect(web3Service.getTransactionType(LockContract.abi, data)).toBe(
+          TRANSACTION_TYPES.KEY_PURCHASE
+        )
+      })
+
+      it('should return the right transaction type on withdrawals', () => {
+        expect.assertions(1)
+        const lock = new web3Service.web3.eth.Contract(LockContract.abi, '')
+        const data = lock.methods.withdraw().encodeABI()
+        expect(web3Service.getTransactionType(LockContract.abi, data)).toBe(
+          TRANSACTION_TYPES.WITHDRAW
+        )
+      })
+    })
+
     describe('handleTransaction', () => {
       it('should trigger transactionHash events and trigger a transaction.new event', done => {
         expect.assertions(2)
@@ -800,8 +851,8 @@ describe('Web3Service', () => {
     })
 
     describe('sendTransaction', () => {
-      it('should handle cases where the private key is not known and using an extrenal provider', () => {
-        expect.assertions(2)
+      it('should handle cases where the transaction is sent via a provider', () => {
+        expect.assertions(4)
 
         web3Service.handleTransaction = jest.fn()
 
@@ -809,6 +860,7 @@ describe('Web3Service', () => {
         const mockTransaction = {}
         mockSendTransaction.mockReturnValue(mockTransaction)
         web3Service.web3.eth.sendTransaction = mockSendTransaction
+        web3Service.getTransactionType = jest.fn(() => 'TYPE')
 
         const transaction = {}
         const to = ''
@@ -824,6 +876,11 @@ describe('Web3Service', () => {
           { to, from, data, value, gas, privateKey, contractAbi },
           callback
         )
+        expect(web3Service.getTransactionType).toHaveBeenCalledWith(
+          contractAbi,
+          data
+        )
+        expect(transaction.type).toBe('TYPE')
         expect(mockSendTransaction).toHaveBeenCalledWith({
           data,
           from,
