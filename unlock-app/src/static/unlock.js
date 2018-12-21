2b52d0377112de3721d3f452982ac0f1a1a9f964
Ben Werdmuller <ben@withknown.com>
2018-12-21 14:03:18 -0800

    Unlock.js autodetects domain it's hosted from (#785)
    * Unlock embed code now auto-detects domain it's hosted from
    
    * Allow window.unlock_url to supersede domain autodetection

--- unlock-app/src/static/unlock.js
+++ unlock-app/src/static/unlock.js
@@ -1,10 +1,22 @@
 window.onload = function() {
+  // Initialize source domain
+  var src = window.unlock_url || 'http://localhost:3000'
+
+  // Setting window.unlock_url hard sets the domain and URI scheme - if this is set, no need to auto-detect
+  if (!window.unlock_url) {
+    // Get the domain and URI scheme of host we're loading this script from
+    let scripts = document.getElementsByTagName('script')
+    for (let i = 0; i < scripts.length; i++) {
+      let pattern = /static\/unlock/i
+      if (pattern.test(scripts[i].getAttribute('src')))
+        src = scripts[i].getAttribute('src').replace('/static/unlock.js', '')
+    }
+  }
+
   const lockedNode = document.querySelector('meta[name=lock]')
 
   // If there is no lock, do nothing!
   if (lockedNode) {
-    var src = window.unlock_url || 'http://localhost:3000'
-
     var s = document.createElement('iframe')
     src += `/paywall/${lockedNode.getAttribute('content')}/`
 
