406019f77e7b439bc234bf56d970f95d2b314089
Gregory Beaver <greg@unlock-protocol.com>
2018-12-21 21:52:39 -0500

    merge in config changes from the issue 580 branch (#834)

---
+++ unlock-app/.jest/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true,
+      },
+    ],
+  },
+}
