8f43a261aa3083cd17708a58ae305328ebf49fc0
Gregory Beaver <greg@unlock-protocol.com>
2018-12-21 21:52:39 -0500

    merge in config changes from the issue 580 branch (#834)

--- unlock-app/jest.config.js
+++ unlock-app/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
+  setupFiles: ['<rootDir>/.jest/register-context.js'],
   setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
   },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
-  ],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
 }
