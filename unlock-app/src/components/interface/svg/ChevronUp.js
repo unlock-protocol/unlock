8020457dba8038430d34345e347c1d1997f46469
Sascha Mombartz <s@mombartz.com>
2018-12-26 09:31:48 +0100

    Replaced old with new chevron svg (#845)
    * Replaced old with new chevron svg
    
    * created components
    
    for new chevron
    
    * code snapshots

--- unlock-app/src/components/interface/svg/ChevronUp.js
+++ unlock-app/src/components/interface/svg/ChevronUp.js
@@ -3,7 +3,11 @@ import React from 'react'
 const SvgChevronUp = ({ title, ...props }) => (
   <svg {...props}>
     <title>{title}</title>
-    <path d="M3 29L29 5l26 24" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M26.965.796a3 3 0 0 1 4.07 0l26 24a3 3 0 1 1-4.07 4.408L29 7.083 5.035 29.204a3 3 0 0 1-4.07-4.408l26-24z"
+    />
   </svg>
 )
 
