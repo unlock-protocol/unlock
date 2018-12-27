1546be0616d6808547565b9fee38f480b265d928
julien <julien.genestoux@gmail.com>
2018-12-27 16:07:01 -0500

    Moving CI commands down to individual applications for better integration

--- tests.sh
+++ tests.sh
@@ -4,25 +4,10 @@
 
 # Solidity tests
 cd /home/unlock/smart-contracts
-# missing npm module from the truffle/ci image
-npm install
+npm ci
+CI=1 npm run ci
 
-truffle test --network test || exit 1
-npm run lint || exit 1
-
-# React tests && linting
+# React application
 cd /home/unlock/unlock-app
-npm install
-CI=1 npm test || exit 1
-npm run lint || exit 1
-# Check that the styling guide is respected
-npm run reformat
-cd .. # Back to root
-CHANGED=$(git diff-index --name-only HEAD -- | grep -v package-lock.json | grep -v npm-shrinkwrap.json)
-if [ -z "$CHANGED" ]; then
-  echo "Format discrepancies. Please run \`npm run reformat\` before your commit."
-  echo "Files with wrong format:"
-  git diff $CHANGED
-  exit 1
-fi
-
+npm ci
+CI=1 npm run ci
