deb4916746e5dc544c5127a17ab0eed6e417b9c6
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-05 18:08:13 -0500

    exploded the dockerfile to allow for better caching (#911)

---
+++ scripts/postinstall.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This script runs after npm install or npm ci
+# it will install/ci all the subdirectories dependencies
+# unless SKIP_SERVICES is set to true
+
+get_json_val() {
+  python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))";
+}
+
+get_npm_command() {
+  local temp=$(echo $npm_config_argv | get_json_val "['original'][0]")
+  echo "$temp" | tr -dc "[:alnum:]"
+}
+
+if [ "$SKIP_SERVICES" != "true" ]; then
+
+  # Will be either 'install' or 'ci'
+  CMD="$(get_npm_command)"
+
+  SERVICES=( smart-contracts unlock-app tests )
+
+  for i in "${SERVICES[@]}"
+  do
+    cd $i
+    npm $CMD
+    cd .. # back to root
+  done
+fi
