2fe1afd28554278ed1d54522c1aefbde9c0fa265
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-06 19:29:14 -0500

    Not trying to pull cache if env variables are missing (#935)

--- scripts/pull-image.sh
+++ scripts/pull-image.sh
@@ -9,7 +9,10 @@ pip install --user awscli;
 # put aws in the path
 export PATH=$PATH:$HOME/.local/bin;
 
-#needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-eval $(aws ecr get-login --no-include-email --region us-east-1);
-
-docker pull 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest;
+if [ -n "$AWS_ACCESS_KEY_ID" ] && [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+  #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+  eval $(aws ecr get-login --no-include-email --region us-east-1);
+  docker pull 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest;
+else
+  echo "Skipping cache pulling"
+fi
