40344777b5df5b09836d1e3786b70ba41836c29b
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-01-07 20:59:49 -0500

    Allows Locksmith to utilize env variable for DB config (#939)

---
+++ locksmith/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    logging: false,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+}
