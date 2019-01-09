14dd646418522c537f4c1054d5bcbe8ceab9a06d
Nick Furfaro <nfurfaro33@gmail.com>
2019-01-08 17:50:48 -0700

    Remove duplicated Unlock.js tests (#954)

--- smart-contracts/test/Unlock/UnlockProxy.js
+++ smart-contracts/test/Unlock/UnlockProxy.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test'
+
 const Unlock = artifacts.require('Unlock')
 const UnlockTestV2 = artifacts.require('UnlockTestV2')
 const UnlockTestV3 = artifacts.require('UnlockTestV3')
@@ -15,7 +17,11 @@ contract('Unlock', function (accounts) {
     beforeEach(async function () {
       // TestHelper retrieves project structure from the zos.json file and deploys everything to the current test network.
       this.project = await TestHelper({ from: proxyAdmin })
-      this.proxy = await this.project.createProxy(Unlock, { Unlock, initMethod: 'initialize', initArgs: [unlockOwner] })
+      this.proxy = await this.project.createProxy(Unlock, {
+        Unlock,
+        initMethod: 'initialize',
+        initArgs: [unlockOwner]
+      })
       this.unlock = await Unlock.at(this.proxy.address)
     })
 
@@ -26,7 +32,7 @@ contract('Unlock', function (accounts) {
     describe('should function as a proxy', function () {
       shared.shouldBehaveLikeV1(accounts, unlockOwner)
 
-      it('should fail if called from the proxy owner\'s account', async function () {
+      it("should fail if called from the proxy owner's account", async function () {
         try {
           await this.unlock.grossNetworkProduct({ from: proxyAdmin })
         } catch (e) {
@@ -40,7 +46,11 @@ contract('Unlock', function (accounts) {
     describe('should function after upgrade', function () {
       beforeEach(async function () {
         await shell.exec('zos add UnlockTestV2:Unlock --skip-compile')
-        await this.project.upgradeProxy(this.proxy.address, UnlockTestV2, { Unlock, initMethod: 'initializeV2', initArgs: [] })
+        await this.project.upgradeProxy(this.proxy.address, UnlockTestV2, {
+          Unlock,
+          initMethod: 'initializeV2',
+          initArgs: []
+        })
         this.unlock = await UnlockTestV2.at(this.proxy.address)
         await shell.exec('zos add Unlock:Unlock --skip-compile')
       })
@@ -62,14 +72,19 @@ contract('Unlock', function (accounts) {
       const transaction = await this.unlock.createLock(
         60 * 60 * 24 * 30, // expirationDuration: 30 days
         Units.convert(1, 'eth', 'wei'), // keyPrice: in wei
-        100 // maxNumberOfKeys
-        , {
+        100, // maxNumberOfKeys
+        {
           from: accounts[0]
-        })
+        }
+      )
       const newLockAddress = transaction.logs[0].args.newLockAddress
       const resultsBefore = await this.unlock.locks(newLockAddress)
       await shell.exec('zos add UnlockTestV2:Unlock --skip-compile')
-      await this.project.upgradeProxy(this.proxy.address, UnlockTestV2, { Unlock, initMethod: 'initializeV2', initArgs: [] })
+      await this.project.upgradeProxy(this.proxy.address, UnlockTestV2, {
+        Unlock,
+        initMethod: 'initializeV2',
+        initArgs: []
+      })
       this.unlock = await UnlockTestV2.at(this.proxy.address)
       await shell.exec('zos add Unlock:Unlock --skip-compile')
       const resultsAfter = await this.unlock.locks(newLockAddress)
@@ -79,7 +94,11 @@ contract('Unlock', function (accounts) {
     describe('should allow you to make significant changes to the contract', function () {
       beforeEach(async function () {
         await shell.exec('zos add UnlockTestV3:Unlock --skip-compile')
-        await this.project.upgradeProxy(this.proxy.address, UnlockTestV3, { Unlock, initMethod: 'initializeV3', initArgs: [] })
+        await this.project.upgradeProxy(this.proxy.address, UnlockTestV3, {
+          Unlock,
+          initMethod: 'initializeV3',
+          initArgs: []
+        })
         this.unlock = await UnlockTestV3.at(this.proxy.address)
         await shell.exec('zos add Unlock:Unlock --skip-compile')
       })
@@ -94,10 +113,11 @@ contract('Unlock', function (accounts) {
           await this.unlock.createLock(
             60 * 60 * 24 * 30,
             Units.convert(1, 'eth', 'wei'),
-            100
-            , {
+            100,
+            {
               from: accounts[0]
-            })
+            }
+          )
         } catch (e) {
           return
         }
