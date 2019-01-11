1306989d0c1611deac92c4f5e27f778e04f7fdfc
Gregory Beaver <greg@unlock-protocol.com>
2019-01-11 11:09:39 -0500

    new story for UnlockedFlag

---
+++ unlock-app/src/stories/lock/UnlockFlag.stories.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { UnlockedFlag } from '../../components/lock/UnlockedFlag'
+
+const key = {
+  address: '0xab7c74abc0c4d48d1bdad5dcb26153fc87eeeeee',
+  lock: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+  expiration: new Date('December 31, 3000 12:00:00').getTime() / 1000,
+}
+
+storiesOf('UnlockedFlag', module).add('the unlocked flag', () => {
+  return <UnlockedFlag keys={[key]} />
+})
