6c24fa8339691791f2de968d1275a7b5ef94c19d
Gregory Beaver <greg@unlock-protocol.com>
2019-01-11 11:23:03 -0500

    begin styling work

--- unlock-app/src/components/lock/UnlockedFlag.js
+++ unlock-app/src/components/lock/UnlockedFlag.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import UnlockPropTypes from '../../propTypes'
 import { RoundedLogo } from '../interface/Logo'
 import { Colophon } from './Overlay'
 import { mapStateToProps } from './ShowUnlessUserHasKeyToAnyLock'
 
-export const UnlockedFlag = ({ keys }) => {
+export const UnlockedFlag = () => {
   return (
     <Flag>
       <RoundedLogo size="28px" />
@@ -16,13 +14,12 @@ export const UnlockedFlag = ({ keys }) => {
   )
 }
 
-UnlockedFlag.propTypes = {
-  keys: PropTypes.arrayOf(UnlockPropTypes.key),
-}
-
 export default connect(mapStateToProps)(UnlockedFlag)
 
 const Flag = styled(Colophon)`
   box-shadow: 0px 0px 40px rgba(0, 0, 0, 0.08);
   background: var(--white);
+  position: fixed;
+  right: 0;
+  bottom: 105px;
 `
