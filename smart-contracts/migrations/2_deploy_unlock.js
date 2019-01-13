67feee29ba1a98411df91e933042c38b20446cc6
Nick Furfaro <nfurfaro33@gmail.com>
2019-01-12 22:36:40 -0700

    Add new migration script

--- smart-contracts/migrations/2_deploy_unlock.js
+++ smart-contracts/migrations/2_deploy_unlock.js
@@ -2,20 +2,16 @@ const Unlock = artifacts.require('./Unlock.sol')
 const shell = require('shelljs')
 
 module.exports = function deployProxy (deployer, network, accounts) {
-  // default account used by ganache
   const unlockOwner = accounts[0]
-  // zos proxy admin can be any account other than accounts[0]
-  // see "The transparent proxy pattern": https://blog.zeppelinos.org/the-transparent-proxy-pattern/
   const proxyAdmin = accounts[9]
 
   deployer.then(() => {
     if (network === 'test') {
-      console.log("Skipping proxy creation in 2_deploy_contracts.js while on network 'test'")
       return deployer.deploy(Unlock, unlockOwner)
     } else {
       if (
         shell.exec(
-          `zos create Unlock --init initialize --args ${unlockOwner} --network ${network} --from ${proxyAdmin}`
+          `zos push --reset --network development --from ${proxyAdmin}`
         ).code !== 0
       ) {
         throw new Error('Migration failed')
