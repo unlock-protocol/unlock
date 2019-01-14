a3f17b7eaa6bf25593190c9ad2bffa2e31e66043
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-14 09:43:31 -0500

    staging for better caching (#956)

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# First this script will deploy from an instance of unlock:latest
+REPO_ROOT=`dirname "$0"`/..
+DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
+COMMAND="cd smart-contracts && npm run deploy -- --network test"
+docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true unlock $COMMAND
+
+# Then it will run the integration tests
+COMMAND="npm run ci"
+docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true integration-tests $COMMAND
