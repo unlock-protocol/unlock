24a7f44138e8c6db13caa353d0060699a68d0657
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-01-14 16:07:35 -0500

    Updating to reflect feedback provided

--- smart-contracts/test/Lock/getOwnersByPage.js
+++ smart-contracts/test/Lock/getOwnersByPage.js
@@ -1,6 +1,5 @@
 
 const Units = require('ethereumjs-units')
-const Web3Utils = require('web3-utils')
 const deployLocks = require('../helpers/deployLocks')
 const Unlock = artifacts.require('../Unlock.sol')
 
@@ -29,61 +28,52 @@ contract('Lock', (accounts) => {
     })
 
     describe('when there are less owners than the page size', () => {
-      it('should return all of the key owners', () => {
-        return locks['FIRST'].purchaseFor(accounts[1], 'something', {
-          value: Units.convert('0.01', 'eth', 'wei')
-        }).then((f) => {
-          return locks['FIRST'].getOwnersByPage.call(0, 2, { from: accounts[0] })
-            .then((result) => {
-              assert.equal(result.length, 1)
-              assert.include(result, accounts[1])
-            })
-        })
+      it('should return all of the key owners', async () => {
+        await locks['FIRST'].purchaseFor(accounts[1], 'alpha', { value: Units.convert('0.01', 'eth', 'wei') })
+        let result = await locks['FIRST'].getOwnersByPage.call(0, 2, { from: accounts[0] })
+        assert.equal(result.length, 1)
+        assert.include(result, accounts[1])
       })
     })
 
     describe('when there are more owners than the page size', () => {
-      it('return page size number of key owners', () => {
-        return locks['FIRST'].purchaseFor(accounts[1], 'something', {
+      it('return page size number of key owners', async () => {
+        await locks['FIRST'].purchaseFor(accounts[1], 'alpha', {
           value: Units.convert('0.01', 'eth', 'wei')
-        }).then(() => {
-          return locks['FIRST'].purchaseFor(accounts[2], 'something_else', {
-            value: Units.convert('0.01', 'eth', 'wei')
-          }).then(() => {
-            return locks['FIRST'].purchaseFor(accounts[3], 'something_else_2', {
-              value: Units.convert('0.01', 'eth', 'wei')
-            }).then((f) => {
-              return locks['FIRST'].getOwnersByPage.call(0, 2, { from: accounts[0] })
-                .then((result) => {
-                  assert.equal(result.length, 2)
-                  assert.include(result, accounts[1])
-                  assert.include(result, accounts[2])
-                })
-            })
-          })
         })
+
+        await locks['FIRST'].purchaseFor(accounts[2], 'beta', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+
+        await locks['FIRST'].purchaseFor(accounts[3], 'gamma', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+
+        let result = await locks['FIRST'].getOwnersByPage.call(0, 2, { from: accounts[0] })
+        assert.equal(result.length, 2)
+        assert.include(result, accounts[1])
+        assert.include(result, accounts[2])
       })
     })
 
     describe('when requesting a secondary page', () => {
-      it('return page size number of key owners', () => {
-        return locks['FIRST'].purchaseFor(accounts[1], 'something', {
+      it('return page size number of key owners', async () => {
+        await locks['FIRST'].purchaseFor(accounts[1], 'alpha', {
           value: Units.convert('0.01', 'eth', 'wei')
-        }).then(() => {
-          return locks['FIRST'].purchaseFor(accounts[2], 'something_else', {
-            value: Units.convert('0.01', 'eth', 'wei')
-          }).then(() => {
-            return locks['FIRST'].purchaseFor(accounts[3], 'something_else_2', {
-              value: Units.convert('0.01', 'eth', 'wei')
-            }).then((f) => {
-              return locks['FIRST'].getOwnersByPage.call(1, 2, { from: accounts[0] })
-                .then((result) => {
-                  assert.equal(result.length, 1)
-                  assert.equal(accounts[3], result[0])
-                })
-            })
-          })
         })
+
+        await locks['FIRST'].purchaseFor(accounts[2], 'beta', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+
+        await locks['FIRST'].purchaseFor(accounts[3], 'gamma', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+
+        let result = await locks['FIRST'].getOwnersByPage.call(1, 2, { from: accounts[0] })
+        assert.equal(result.length, 1)
+        assert.equal(accounts[3], result[0])
       })
     })
   })
