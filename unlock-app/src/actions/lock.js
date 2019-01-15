d12c29f0c4acdd2f7b85a234c2eb626bd01ab7ef
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-14 21:30:36 -0500

    now showng a better error when declining to create lock and remove the pending lock (#837)

--- unlock-app/src/actions/lock.js
+++ unlock-app/src/actions/lock.js
@@ -1,9 +1,10 @@
-export const CREATE_LOCK = 'CREATE_LOCK'
 export const ADD_LOCK = 'ADD_LOCK'
-export const UPDATE_LOCK = 'UPDATE_LOCK'
-export const WITHDRAW_FROM_LOCK = 'WITHDRAW_FROM_LOCK'
+export const CREATE_LOCK = 'CREATE_LOCK'
+export const DELETE_LOCK = 'DELETE_LOCK'
 export const LOCK_DEPLOYED = 'LOCK_DEPLOYED'
+export const UPDATE_LOCK = 'UPDATE_LOCK'
 export const UPDATE_LOCK_KEY_PRICE = 'UPDATE_LOCK_KEY_PRICE'
+export const WITHDRAW_FROM_LOCK = 'WITHDRAW_FROM_LOCK'
 
 export const createLock = lock => ({
   type: CREATE_LOCK,
@@ -14,6 +15,11 @@ export const addLock = (address, lock) => ({
   type: ADD_LOCK,
   address,
   lock,
+})
+
+export const deleteLock = address => ({
+  type: DELETE_LOCK,
+  address,
 })
 
 export const updateLock = (address, update) => ({
