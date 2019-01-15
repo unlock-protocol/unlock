a75056b4a5f20e621f4c4d83476fcd92d93c51a5
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-01-15 09:56:06 -0500

    Implement multiple errors display in UI (#979)
    * implement multiple errors in redux side
    
    * simplify (+15 squashed commits)
    Squashed commits:
    [ceb6359] rebasing errors
    [df33520] push changes that didn't sync for some reason
    [12ca4af] revert unintentional deletion of line
    [2835d33] revert unintentional language change
    [cfaab14] remove resetError
    [8535140] update to work with new error message generation
    [cf51488] update styling, fix error with markup pre-populated
    [e5d4d10] add test for mapStateToProps
    [4db080b] implement multiple error display
    [eb5a1d3] use resetError instead of setError
    [b178afa] implement multiple errors in redux side
    [9aeb64a] use setError, not resetError
    [990dfeb] use initial state and verify it is actually it
    [5214522] use explicit initialState (less brittle)
    [0de3dce] remove resetError and company
    
    * each error has its own box and close button
    
    * add space between errors
    
    * update snaps
    
    * move Error to Errors
    
    - make close react to individual errors
    - remove SecondColumn, we don't need it any longer
    - add actions logger to show which error is being closed in storybook stories
    - test for correct close parameter called
    - update snaps
    
    * rename Error.test.js
    
    * rename Error.stories.js
    
    * update snaps
    
    * s/Error/Errors/
    
    * remove outdated code
    
    * update snapshots
    
    * force rebuild on travis

--- unlock-app/src/stories/interface/Error.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Error } from '../../components/interface/Error'
-
-const close = () => {}
-
-storiesOf('Error', module).add('Simple Error', () => {
-  return <Error close={close} error="We could not process that transaction." />
-})
