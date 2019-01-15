e4cad652bb942ca95a061fe9947f6b47c198f485
Gregory Beaver <greg@unlock-protocol.com>
2019-01-15 16:24:20 -0500

    add tests

--- unlock-app/src/utils/validators.js
+++ unlock-app/src/utils/validators.js
@@ -1,4 +1,4 @@
-export const missing = val => val === undefined || val === null || val === ''
+export const isNotEmpty = val => val || val === 0
 export const isPositiveInteger = val =>
   !isNaN(val) && +val === parseInt(val) && +val > 0
 export const isPositiveNumber = val => !isNaN(val) && +val > 0
