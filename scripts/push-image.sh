d28ab9b5cce530469719970ca0a7451598daedfc
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-15 21:34:40 -0500

    refactoring image building and pushing (#1050)

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -3,20 +3,14 @@
 # This script saves previously built images to docker hub on AWS.
 # This should only run on master merges
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+IMAGE_NAME=$1
+ARGS=""
 
-  # install aws cli w/o sudo
-  pip install --user awscli;
-
-  # put aws in the path
-  export PATH=$PATH:$HOME/.local/bin;
-
-  #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-  eval $(aws ecr get-login --no-include-email --region us-east-1);
-
-  docker tag unlock:latest 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest;
-
-  docker push 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest;
-else
-  echo "Skipping docker image push as this is not a master push."
+if [ "$DOCKER_REPOSITORY" != "" ]; then
+  if [ -n "$AWS_ENABLED" ]; then
+    IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
+    docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE;
+    docker push $IMAGE_CACHE;
+  fi
 fi
+
