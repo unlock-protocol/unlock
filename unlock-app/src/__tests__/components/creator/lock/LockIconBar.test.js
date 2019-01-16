e47cecbc198306068cf15d3f0e262a5c141aa96d
Gregory Beaver <greg@unlock-protocol.com>
2019-01-16 11:56:55 -0500

    re-organize the test

--- unlock-app/src/__tests__/components/creator/lock/LockIconBar.test.js
+++ unlock-app/src/__tests__/components/creator/lock/LockIconBar.test.js
@@ -6,13 +6,17 @@ import { LockIconBar } from '../../../../components/creator/lock/LockIconBar'
 import configure from '../../../../config'
 import createUnlockStore from '../../../../createUnlockStore'
 
-const toggleCode = jest.fn()
-
 describe('LockIconBar', () => {
-  it('should display a submitted label when withdrawal has been submitted', () => {
-    const config = configure()
+  let config
+  let lock
+  let transaction
+  let withdrawalTransaction
+  let store
+  let toggleCode
+  beforeEach(() => {
+    config = configure()
 
-    const lock = {
+    lock = {
       id: 'lockwithdrawalsubmittedid',
       keyPrice: '10000000000000000000',
       expirationDuration: '172800',
@@ -21,18 +25,21 @@ describe('LockIconBar', () => {
       address: '0xbc7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
       transaction: 'deployedid',
     }
-    const transaction = {
+    transaction = {
       status: 'mined',
       confirmations: 24,
     }
-    const withdrawalTransaction = {
+    withdrawalTransaction = {
       status: 'submitted',
       confirmations: 0,
       withdrawal: '0xbc7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
     }
 
-    const store = createUnlockStore({})
+    store = createUnlockStore({})
+    toggleCode = jest.fn()
+  })
 
+  it('should display a submitted label when withdrawal has been submitted', () => {
     let wrapper = rtl.render(
       <Provider store={store}>
         <LockIconBar
@@ -49,29 +56,16 @@ describe('LockIconBar', () => {
       wrapper.queryByText('Submitted to Network', { exact: false })
     ).not.toBeNull()
   })
-  it('should display a confirming label when withdrawal is confirming', () => {
-    const config = configure()
 
-    const lock = {
-      id: 'lockwithdrawalconfirmingid',
-      keyPrice: '10000000000000000000',
-      expirationDuration: '172800',
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xba7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: 'deployedid',
-    }
-    const transaction = {
-      status: 'mined',
-      confirmations: 24,
-    }
-    const withdrawalTransaction = {
+  it('should display a confirming label when withdrawal is confirming', () => {
+    config.requiredConfirmations = 12
+    withdrawalTransaction = {
       status: 'mined',
       confirmations: 2,
       withdrawal: 'lockwithdrawalconfirmingid',
     }
 
-    const store = createUnlockStore({})
+    store = createUnlockStore({})
 
     let wrapper = rtl.render(
       <Provider store={store}>
