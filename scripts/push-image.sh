70c76a7470b66a9acb857e337d627113e4045290
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-15 23:27:03 -0500

    Checking identity against aws (#1054)

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -6,11 +6,13 @@
 IMAGE_NAME=$1
 ARGS=""
 
-if [ "$DOCKER_REPOSITORY" != "" ]; then
-  if [ -n "$AWS_ENABLED" ]; then
-    IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
-    docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE;
-    docker push $IMAGE_CACHE;
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  if [ "$DOCKER_REPOSITORY" != "" ]; then
+    aws sts get-caller-identity
+    if [ $? -eq 0 ]; then
+      IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
+      docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE;
+      docker push $IMAGE_CACHE;
+    fi
   fi
 fi
-
