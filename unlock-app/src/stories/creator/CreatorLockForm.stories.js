e2e05b2d8d70246b1b70edbe5151efdffc0fb764
Gregory Beaver <greg@unlock-protocol.com>
2019-01-16 19:01:58 -0500

    fix story, update snapshots

--- unlock-app/src/stories/creator/CreatorLockForm.stories.js
+++ unlock-app/src/stories/creator/CreatorLockForm.stories.js
@@ -22,13 +22,13 @@ storiesOf('CreatorLockForm', module)
     // TODO: implement this
     const lock = {
       keyPrice: '10000000000000000000',
-      expirationDuration: '172800',
+      expirationDuration: 172800,
       maxNumberOfKeys: 240,
       outstandingKeys: 3,
       address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
       name: 'Existing Lock',
     }
     return (
-      <CreatorLockForm {...lock} hideAction={() => {}} createLock={() => {}} />
+      <CreatorLockForm hideAction={() => {}} createLock={() => {}} {...lock} />
     )
   })
