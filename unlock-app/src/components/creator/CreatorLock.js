566154c9c1104d32073bae2d3c9bc79426424c31
Gregory Beaver <greg@unlock-protocol.com>
2019-01-17 12:01:38 -0500

    extract shared styles for locks to a separate file (#1084)

--- unlock-app/src/components/creator/CreatorLock.js
+++ unlock-app/src/components/creator/CreatorLock.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 import UnlockPropTypes from '../../propTypes'
@@ -9,7 +8,19 @@ import EmbedCodeSnippet from './lock/EmbedCodeSnippet'
 import KeyList from './lock/KeyList'
 import Duration from '../helpers/Duration'
 import Balance from '../helpers/Balance'
-import Media, { NoPhone, Phone } from '../../theme/media'
+import { NoPhone, Phone } from '../../theme/media'
+
+import {
+  LockPanel,
+  LockAddress,
+  LockDivider,
+  LockDuration,
+  LockKeys,
+  LockName,
+  LockRow,
+  DoubleHeightCell,
+  BalanceContainer,
+} from './LockStyles'
 
 const LockKeysNumbers = ({ lock }) => (
   <LockKeys>
@@ -113,81 +124,3 @@ CreatorLock.defaultProps = {
 }
 
 export default CreatorLock
-
-export const LockRowGrid =
-  'grid-template-columns: 32px minmax(100px, 1fr) repeat(4, minmax(56px, 100px)) minmax(174px, 1fr);'
-
-export const PhoneLockRowGrid =
-  'grid-template-columns: 43px minmax(80px, 140px) repeat(2, minmax(56px, 80px)); grid-auto-flow: column;'
-
-export const LockRow = styled.div`
-  font-family: 'IBM Plex Mono', 'Courier New', Serif;
-  font-weight: 200;
-  min-height: 48px;
-  padding-left: 8px;
-  color: var(--slate);
-  font-size: 14px;
-  box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.08);
-  border-radius: 4px;
-  display: grid;
-  grid-row-gap: 0;
-  ${Media.nophone`
-    ${LockRowGrid} grid-template-rows: 84px;
-    grid-column-gap: 16px;
-  `} ${Media.phone`
-    grid-column-gap: 4px;
-    ${PhoneLockRowGrid}
-  `}
-  align-items: start;
-  cursor: pointer;
-
-  & > * {
-    padding-top: 16px;
-  }
-`
-
-export const LockLabel = styled.div`
-  color: var(--link);
-`
-
-export const DoubleHeightCell = styled.div`
-  grid-row: span 2;
-`
-
-export const BalanceContainer = styled.div``
-
-export const LockName = styled.div`
-  color: var(--link);
-  font-weight: 600;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  ${Media.phone`
-    grid-column: span 2;
-  `};
-`
-
-export const LockAddress = styled.div`
-  color: var(--grey);
-  font-weight: 200;
-  white-space: nowrap;
-  font-size: 10px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`
-
-export const LockDuration = styled.div``
-
-export const LockKeys = styled.div``
-
-const LockPanel = styled.div`
-  grid-column: 1 / span 7;
-  ${Media.phone`
-    display: none;
-  `};
-`
-
-const LockDivider = styled.div`
-  width: 99%;
-  height: 1px;
-  background-color: var(--lightgrey);
-`
