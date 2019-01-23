de1ef3074dc6850d931dcc9d48b248c4e72ab1df
Gregory Beaver <greg@unlock-protocol.com>
2019-01-22 21:52:40 -0500

    GlobalErrorConsumer component

---
+++ unlock-app/src/components/interface/GlobalErrorConsumer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { GlobalErrorContext } from '../../utils/GlobalErrorProvider'
+import { mapping } from '../creator/FatalError'
+import Layout from './Layout'
+
+const Consumer = GlobalErrorContext.Consumer
+
+export const displayError = (error, children) => {
+  if (error) return <Layout title="">{error}</Layout>
+  return <>{children}</>
+}
+
+export default function GlobalErrorConsumer({ displayError, children }) {
+  return (
+    <Consumer>
+      {({ error, errorMetadata }) => {
+        // if the error condition exists, set it to the mapped fatal error component
+        // or to the fallback
+        // if no error exists, set it to false
+        const Error = error ? mapping[error] || mapping['*'] : false
+
+        // call displayError with either false or the error element, and our child elements
+        return displayError(Error && <Error {...errorMetadata} />, children)
+      }}
+    </Consumer>
+  )
+}
+
+GlobalErrorConsumer.propTypes = {
+  children: PropTypes.node.isRequired,
+  displayError: PropTypes.func,
+}
+
+GlobalErrorConsumer.defaultProps = {
+  displayError,
+}
