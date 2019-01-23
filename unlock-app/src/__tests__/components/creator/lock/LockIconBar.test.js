07dcf0c9ec3df2ac6fd680e91d7d2b7c3c36a7c8
Gregory Beaver <greg@unlock-protocol.com>
2019-01-23 15:02:27 -0500

    update lock prop type to include missing fields, fix prop type errors it uncovered (#1173)
    * add missing fields to lock prop type
    
    * fix incorrect usages of lock

--- unlock-app/src/__tests__/components/creator/lock/LockIconBar.test.js
+++ unlock-app/src/__tests__/components/creator/lock/LockIconBar.test.js
@@ -19,7 +19,7 @@ describe('LockIconBar', () => {
     lock = {
       id: 'lockwithdrawalsubmittedid',
       keyPrice: '10000000000000000000',
-      expirationDuration: '172800',
+      expirationDuration: 172800,
       maxNumberOfKeys: 240,
       outstandingKeys: 3,
       address: '0xbc7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
