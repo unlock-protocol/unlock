ae2d0b3ec5541f6ee2afab9a8781be5c719fdd41
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-25 15:59:21 -0500

    passing all environment variables inside the docker image (#1233)
    * logging env variables for easier debugging
    
    * passing all environment variables inside the docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # This script invokes the deployment script for the target (as first arg) inside the default docker
-# image. Default env variables are passed: $TRAVIS_PULL_REQUEST and $TRAVIS_BRANCH
+# image by copying all the Travis env variables inside the docker image
 
 TARGET=$1
 NPM_SCRIPT="npm run deploy-$TARGET"
 SERVICE="unlock"
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
-ENV_VARS="-e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH=$TRAVIS_BRANCH"
+ENV_VARS=`env | grep TRAVIS_ | awk '{print "-e ",$1}' ORS=' '`
 
 if [ "$TARGET" = "netlify" ]; then
   ENV_VARS="$ENV_VARS -e NETLIFY_STAGING_SITE_ID=$NETLIFY_STAGING_SITE_ID -e NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN"
