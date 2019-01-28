cb7b24b9892c215f3e6186be276316a41e30b35c
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-01-28 13:33:39 -0800

    await on Promise (#1253)

--- smart-contracts/test/helpers/deployLocks.js
+++ smart-contracts/test/helpers/deployLocks.js
@@ -10,10 +10,10 @@ module.exports = function deployLocks (unlock) {
         Locks[name].expirationDuration,
         Locks[name].keyPrice,
         Locks[name].maxNumberOfKeys
-      ).then((tx) => {
+      ).then(async (tx) => {
         // THIS API IS LIKELY TO BREAK BECAUSE IT ASSUMES SO MUCH
         const evt = tx.logs[0]
-        locks[name] = PublicLock.at(evt.args.newLockAddress)
+        locks[name] = await PublicLock.at(evt.args.newLockAddress)
         locks[name].params = Locks[name]
       })
     })
