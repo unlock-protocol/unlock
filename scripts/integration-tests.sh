c5af8e15db06018317fe9aed1335d4f0f5681bb4
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-29 17:32:51 -0500

    Plugging the walletMiddleware and services to replace web3Service for transactions

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -4,7 +4,7 @@
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 COMMAND="cd smart-contracts && npm run deploy -- --network test"
-docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true unlock $COMMAND
+docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true unlock bash -c "$COMMAND"
 
 # Then it will run the integration tests
 COMMAND="npm run ci"
