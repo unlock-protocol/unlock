d56fa32a71b27bca824efd124241826cafbac508
Gregory Beaver <greg@unlock-protocol.com>
2019-01-29 21:47:01 -0500

    Remove outdated comments

--- unlock-app/src/components/lock/ShowWhenLocked.js
+++ unlock-app/src/components/lock/ShowWhenLocked.js
@@ -4,12 +4,10 @@ import React from 'react'
 import SuspendedRender from '../helpers/SuspendedRender'
 
 export default function ShowWhenLocked({ locked, children }) {
-  // We have at least one valid key and the modal was not shown
   if (!locked) {
     return null
   }
 
-  // There is no valid key or we shown the modal previously
   return <SuspendedRender>{children}</SuspendedRender>
 }
 
