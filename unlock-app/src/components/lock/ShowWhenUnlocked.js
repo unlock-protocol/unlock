bd13190df4ec53a4de4ab35d096a66ade9e63af6
Gregory Beaver <greg@unlock-protocol.com>
2019-01-29 21:45:23 -0500

    remove outdated comments

--- unlock-app/src/components/lock/ShowWhenUnlocked.js
+++ unlock-app/src/components/lock/ShowWhenUnlocked.js
@@ -2,12 +2,10 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 export default function ShowWhenUnlocked({ locked, children }) {
-  // We have at least one valid key and the modal was not shown
   if (locked) {
     return null
   }
 
-  // There is no valid key or we shown the modal previously
   return <>{children}</>
 }
 
