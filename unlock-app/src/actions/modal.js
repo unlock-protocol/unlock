ccd8e50c98c388fc4a4d21e3ffe25546f2d40986
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-01-31 18:28:49 -0500

    new action for opening paywall modal in a new window (#1330)

--- unlock-app/src/actions/modal.js
+++ unlock-app/src/actions/modal.js
@@ -1,5 +1,6 @@
 export const SHOW_MODAL = 'modal/SHOW_MODAL'
 export const HIDE_MODAL = 'modal/HIDE_MODAL'
+export const OPEN_MODAL_IN_NEW_WINDOW = 'modal/OPEN_MODAL_IN_NEW_WINDOW'
 
 export const showModal = modal => ({
   type: SHOW_MODAL,
@@ -9,4 +10,9 @@ export const showModal = modal => ({
 export const hideModal = modal => ({
   type: HIDE_MODAL,
   modal,
+})
+
+// TODO: this is to be used in a new middleware to open paywall modal in a new window
+export const openNewWindowModal = () => ({
+  type: OPEN_MODAL_IN_NEW_WINDOW,
 })
