2ae13ca6ddf533743a56bc2807d1ed9cf4897bec
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-01-31 08:27:38 -0800

    Use .call consistently #1307

--- smart-contracts/test/Lock/erc721/getTokenIdFor.js
+++ smart-contracts/test/Lock/erc721/getTokenIdFor.js
@@ -16,7 +16,7 @@ contract('Lock ERC721', (accounts) => {
 
   describe('getTokenIdFor', () => {
     it('should abort when the key has no owner', async () => {
-      await shouldFail(locks['FIRST'].getTokenIdFor(accounts[3]), 'No such key')
+      await shouldFail(locks['FIRST'].getTokenIdFor.call(accounts[3]), 'No such key')
     })
 
     it('should return the tokenId for the owner\'s key', async () => {
@@ -24,7 +24,7 @@ contract('Lock ERC721', (accounts) => {
         value: Units.convert('0.01', 'eth', 'wei'),
         from: accounts[1]
       })
-      let address = new BigNumber(await locks['FIRST'].getTokenIdFor(accounts[1]))
+      let address = new BigNumber(await locks['FIRST'].getTokenIdFor.call(accounts[1]))
       // Note that as we implement ERC721 support, the tokenId will no longer
       // be the same as the user's address
       assert.equal(Web3Utils.toChecksumAddress(Web3Utils.toHex(address)), Web3Utils.toChecksumAddress(accounts[1]))
