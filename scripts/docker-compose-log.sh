06215408d9d87c0d0f9f30ca5093df7772923220
Julien Genestoux <julien.genestoux@gmail.com>
2019-01-31 11:53:00 -0500

    adding a logging statement for docker compose to get a full log of all runnung docker containers. This should provide valuable debugging information (#1318)

---
+++ scripts/docker-compose-log.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# The script exposes the logs of all docker running instances when using compose
+# This should be invoked upon failure to provide details about the failure
+
+docker-compose -f docker/docker-compose.ci.yml logs --timestamps --tail="all"
