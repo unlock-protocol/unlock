864473cb0e591ac6e4e958e9d957728e147cbef5
Gregory Beaver <greg@unlock-protocol.com>
2019-01-31 15:56:31 -0500

    working blocker (#1305)

--- unlock-app/src/paywall-builder/build.js
+++ unlock-app/src/paywall-builder/build.js
@@ -1,7 +1,7 @@
 import { getIframe, add, show, hide } from './iframe'
 import { findPaywallUrl } from './script'
 
-export default function buildPaywall(window, document, lockAddress) {
+export default function buildPaywall(window, document, lockAddress, blocker) {
   // If there is no lock, do nothing!
   if (!lockAddress) {
     return
@@ -19,10 +19,12 @@ export default function buildPaywall(window, document, lockAddress) {
       if (event.data === 'locked' && !locked) {
         locked = true
         show(iframe, document)
+        blocker.remove()
       }
       if (event.data === 'unlocked' && locked) {
         locked = false
         hide(iframe, document)
+        blocker.remove()
       }
     },
     false
