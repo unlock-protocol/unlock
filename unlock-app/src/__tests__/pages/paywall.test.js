f2e2dbebe2d1c6d88171494535666ea415332fb1
Christopher Nascone <cnasc@users.noreply.github.com>
2019-02-01 08:47:05 -0500

    Refactor paywall to handle showing/hiding content based on lock status (#1319)
    * New props for paywall (and a test)
    
    * Moving the rafactor along, introducing iframe logic
    
    * Do the iframe things on component mount and update
    
    * Fix mysterious config-related test fail
    
    * Simplify isLocked logic
    
    * Simpler implementation
    
    * Removed unused bind
    
    * Testing the paywall

--- unlock-app/src/__tests__/pages/paywall.test.js
+++ unlock-app/src/__tests__/pages/paywall.test.js
@@ -1,19 +1,81 @@
-import { mapStateToProps } from '../../pages/paywall'
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import { Provider } from 'react-redux'
+import createUnlockStore from '../../createUnlockStore'
+import Paywall, { mapStateToProps } from '../../pages/paywall'
+import { lockPage, unlockPage } from '../../services/iframeService'
+import { ConfigContext } from '../../utils/withConfig'
 
+jest.mock('../../services/iframeService.js')
+
+const lock = { address: '0x4983D5ECDc5cc0E499c2D23BF4Ac32B982bAe53a' }
+const locks = {
+  [lock.address]: lock,
+}
+const router = {
+  location: {
+    pathname: `/paywall/${lock.address}`,
+    search: '',
+    hash: '',
+  },
+}
+
+let futureDate = new Date()
+futureDate.setYear(futureDate.getFullYear() + 1)
+futureDate = futureDate.getTime() / 1000
+
+const keys = {
+  aKey: {
+    lock: lock.address,
+    expiration: futureDate,
+  },
+}
+const modals = []
+
+const store = createUnlockStore({ locks, keys, modals, router })
+const noKeyStore = createUnlockStore({ locks, keys: {}, modals, router })
+
+afterEach(() => {
+  rtl.cleanup()
+  jest.clearAllMocks()
+})
 describe('Paywall', () => {
   describe('mapStateToProps', () => {
     it('should yield the lock which matches the address of the demo page', () => {
-      const lock = { address: '0x4983D5ECDc5cc0E499c2D23BF4Ac32B982bAe53a' }
-      const locks = {
-        [lock.address]: lock,
-      }
-      const router = {
-        location: {
-          pathname: `/paywall/${lock.address}`,
-        },
-      }
-      const props = mapStateToProps({ locks, router })
-      expect(props.lock).toBe(lock)
+      const props = mapStateToProps({ locks, keys, modals, router })
+      expect(props.locks[0]).toBe(lock)
+    })
+    it('should be locked when no keys are available', () => {
+      const props = mapStateToProps({ locks, keys: {}, modals, router })
+      expect(props.locked).toBe(true)
+    })
+    it('should not be locked when there is a matching key', () => {
+      const props = mapStateToProps({ locks, keys, modals, router })
+      expect(props.locked).toBe(false)
+    })
+  })
+  describe('handleIframe', () => {
+    it('should call lockPage when it is locked', () => {
+      expect.assertions(2)
+      rtl.render(
+        <Provider store={noKeyStore}>
+          <Paywall />
+        </Provider>
+      )
+      expect(lockPage).toHaveBeenCalled()
+      expect(unlockPage).not.toHaveBeenCalled()
+    })
+    it('should call unlockPage when it is not locked', () => {
+      expect.assertions(2)
+      rtl.render(
+        <ConfigContext.Provider value={{ providers: [] }}>
+          <Provider store={store}>
+            <Paywall />
+          </Provider>
+        </ConfigContext.Provider>
+      )
+      expect(lockPage).not.toHaveBeenCalled()
+      expect(unlockPage).toHaveBeenCalled()
     })
   })
 })
