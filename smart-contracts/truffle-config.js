285eb9b7f93cf5c185f5fc64c0bfac55c1ad6411
Nick Furfaro <nfurfaro33@gmail.com>
2019-01-31 18:58:09 -0700

    Remove test network from truffle config js (#1326)
    * Remove test network from truffle config js
    
    * Fix development network host

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     development: {
       // used for solidity-coverage
-      host: '127.0.0.1',
+      host: testHost,
       port: 8545,
       network_id: '*' // Match any network id
     },
@@ -47,12 +47,6 @@ module.exports = {
       // used for local dev but with the ganache gui
       host: '127.0.0.1',
       port: 8546, // We use ganache-gui and this is its default port
-      network_id: '*' // Match any network id
-    },
-    test: {
-      // used to run tests in docker (ci)
-      host: testHost,
-      port: 8545,
       network_id: '*' // Match any network id
     },
     rinkeby: {
