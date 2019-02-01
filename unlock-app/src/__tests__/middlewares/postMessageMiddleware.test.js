0948f850c60a9033842f795819e607225efbbcb4
Gregory Beaver <greg@unlock-protocol.com>
2019-02-01 11:41:00 -0500

    proper iframe detection, new tests

--- unlock-app/src/__tests__/middlewares/postMessageMiddleware.test.js
+++ unlock-app/src/__tests__/middlewares/postMessageMiddleware.test.js
@@ -1,45 +1,94 @@
-import postMessageMiddleware from '../../middlewares/postMessageMiddleware'
+import postMessageMiddleware, {
+  inIframe,
+} from '../../middlewares/postMessageMiddleware'
 import { openNewWindowModal } from '../../actions/modal'
 
 describe('postMessageMiddleware', () => {
-  it('does responds to OPEN_MODAL_IN_NEW_WINDOW if in an iframe', () => {
-    expect.assertions(2)
-    const next = jest.fn()
-
-    const action = openNewWindowModal()
-
-    const window = {
-      parent: {
-        contentWindow: {
-          postMessage: jest.fn(),
-          origin: 'origin',
-        },
-      },
-    }
-
-    const middleware = postMessageMiddleware(window)
-
-    middleware()(next)(action)
-
-    expect(next).toHaveBeenCalledWith(action)
-    expect(window.parent.contentWindow.postMessage).toHaveBeenCalledWith(
-      'redirect',
-      'origin'
-    )
+  describe('inIframe', () => {
+    it('should return false when self == top', () => {
+      const window = {}
+      window.self = window
+      window.top = window
+      expect(inIframe(window)).toBe(false)
+    })
+    it('should return true when self != top', () => {
+      const window = {
+        self: 'nope',
+        top: 'yes',
+      }
+      expect(inIframe(window)).toBe(true)
+    })
+    it('should return true when an exception is thrown', () => {
+      expect(inIframe()).toBe(true)
+    })
   })
-  it('passes actions to the next middleware', () => {
-    expect.assertions(1)
-    const next = jest.fn()
 
-    const action = {
-      type: 'boo',
-    }
+  describe('middleware functionality', () => {
+    it('does responds to OPEN_MODAL_IN_NEW_WINDOW if in an iframe', () => {
+      expect.assertions(2)
+      const next = jest.fn()
 
-    const window = {}
+      const action = openNewWindowModal()
 
-    const middleware = postMessageMiddleware(window)
+      const window = {
+        parent: {
+          contentWindow: {
+            postMessage: jest.fn(),
+            origin: 'origin',
+          },
+        },
+      }
+      window.self = window
+      window.top = 'not window'
 
-    middleware()(next)(action)
-    expect(next).toHaveBeenCalledWith(action)
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+
+      expect(next).toHaveBeenCalledWith(action)
+      expect(window.parent.contentWindow.postMessage).toHaveBeenCalledWith(
+        'redirect',
+        'origin'
+      )
+    })
+    it('does not respond to OPEN_MODAL_IN_NEW_WINDOW if not in an iframe', () => {
+      expect.assertions(2)
+      const next = jest.fn()
+
+      const action = openNewWindowModal()
+
+      const window = {
+        parent: {
+          contentWindow: {
+            postMessage: jest.fn(),
+            origin: 'origin',
+          },
+        },
+      }
+      window.self = window
+      window.top = window
+
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+
+      expect(next).toHaveBeenCalledWith(action)
+      expect(window.parent.contentWindow.postMessage).not.toHaveBeenCalled()
+    })
+    it('passes actions to the next middleware', () => {
+      expect.assertions(1)
+      const next = jest.fn()
+
+      const action = {
+        type: 'boo',
+      }
+
+      const window = {}
+
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+      expect(next).toHaveBeenCalledWith(action)
+    })
   })
 })
--- unlock-app/src/middlewares/postMessageMiddleware.js
+++ unlock-app/src/middlewares/postMessageMiddleware.js
@@ -1,11 +1,21 @@
 import { OPEN_MODAL_IN_NEW_WINDOW } from '../actions/modal'
 
+// cribbed from https://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
+export function inIframe(window) {
+  try {
+    return window.self !== window.top
+  } catch (e) {
+    return true
+  }
+}
+
 // store is unused in this middleware, it is only for listening for actions
 // and converting them into postMessage
 const postMessageMiddleware = window => () => {
-  let iframe = window.parent
+  const iframe = window.parent
+  const enabled = inIframe(window)
   return next => action => {
-    if (!iframe) return next(action)
+    if (!enabled) return next(action)
     if (action.type === OPEN_MODAL_IN_NEW_WINDOW) {
       iframe.contentWindow.postMessage('redirect', iframe.contentWindow.origin)
     }
