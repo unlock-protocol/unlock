7945ad75e7747ed1e07705dcb7943c0ba56175c8
HardlyDifficult <hardlydifficultllc@gmail.com>
2019-01-31 16:49:35 -0800

    Removing time test

--- smart-contracts/test/Lock/getHasValidKey.js
+++ smart-contracts/test/Lock/getHasValidKey.js
@@ -4,7 +4,6 @@ const BigNumber = require('bignumber.js')
 
 const deployLocks = require('../helpers/deployLocks')
 const shouldFail = require('../helpers/shouldFail')
-revertTime = require('../helpers/revertTime')
 const Unlock = artifacts.require('../Unlock.sol')
 
 let unlock, locks
@@ -36,14 +35,9 @@ contract('Lock', (accounts) => {
         const isValid = await lock.getHasValidKey.call(account)
         assert.equal(isValid, true)
       })
-
+  
       describe('after transfering a previously purchased key', () => {
-        before(async () => {
           await lock.transferFrom(account, accounts[5], await lock.getTokenIdFor.call(account), { from: account })
-        })
-
-        after(async () => {
-          await revertTime()
         })
 
         it('should be false', async () => {
