c05e8d74c97bef54d8027f6386faf8e91d9d0ebd
Gregory Beaver <greg@unlock-protocol.com>
2019-02-01 14:07:45 -0500

    inIframe function and new config variable isInIframe (#1350)

--- unlock-app/src/__tests__/config.test.js
+++ unlock-app/src/__tests__/config.test.js
@@ -1,6 +1,72 @@
-import configure from '../config'
+import configure, { inIframe } from '../config'
 
 describe('config', () => {
+  describe('inIframe', () => {
+    it('should return false when self == top', () => {
+      const window = {}
+      window.self = window
+      window.top = window
+      expect(inIframe(window)).toBe(false)
+    })
+    it('should return true when self != top', () => {
+      const window = {
+        self: 'nope',
+        top: 'yes',
+      }
+      expect(inIframe(window)).toBe(true)
+    })
+    it('should return true when an exception is thrown', () => {
+      expect(inIframe()).toBe(true)
+    })
+  })
+
+  describe('isInIframe', () => {
+    it('should return false when self == top', () => {
+      const window = {}
+      window.self = window
+      window.top = window
+
+      const config = configure(
+        global,
+        {
+          unlockEnv: 'dev',
+          httpProvider: '127.0.0.1',
+        },
+        window
+      )
+
+      expect(config.isInIframe).toBe(false)
+    })
+    it('should return true when self != top', () => {
+      const window = {
+        self: 'nope',
+        top: 'yes',
+      }
+
+      const config = configure(
+        global,
+        {
+          unlockEnv: 'dev',
+          httpProvider: '127.0.0.1',
+        },
+        window
+      )
+
+      expect(config.isInIframe).toBe(true)
+    })
+    it('should return true when an exception is thrown', () => {
+      const config = configure(
+        global,
+        {
+          unlockEnv: 'dev',
+          httpProvider: '127.0.0.1',
+        },
+        null
+      )
+
+      expect(config.isInIframe).toBe(true)
+    })
+  })
   describe('dev', () => {
     let config = configure(global, {
       unlockEnv: 'dev',
