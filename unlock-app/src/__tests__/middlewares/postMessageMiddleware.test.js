389a84bdd9470525b3ec9c0d57d50a96ea469ce7
Gregory Beaver <greg@unlock-protocol.com>
2019-02-01 14:38:44 -0500

    add postMessage middleware (#1343)
    * add postMessage middleware
    
    * proper iframe detection, new tests
    
    * move inIframe to config.js

---
+++ unlock-app/src/__tests__/middlewares/postMessageMiddleware.test.js
@@ -0,0 +1,73 @@
+import postMessageMiddleware from '../../middlewares/postMessageMiddleware'
+import { openNewWindowModal } from '../../actions/modal'
+
+describe('postMessageMiddleware', () => {
+  describe('middleware functionality', () => {
+    it('does responds to OPEN_MODAL_IN_NEW_WINDOW if in an iframe', () => {
+      expect.assertions(2)
+      const next = jest.fn()
+
+      const action = openNewWindowModal()
+
+      const window = {
+        parent: {
+          contentWindow: {
+            postMessage: jest.fn(),
+            origin: 'origin',
+          },
+        },
+      }
+      window.self = window
+      window.top = 'not window'
+
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+
+      expect(next).toHaveBeenCalledWith(action)
+      expect(window.parent.contentWindow.postMessage).toHaveBeenCalledWith(
+        'redirect',
+        'origin'
+      )
+    })
+    it('does not respond to OPEN_MODAL_IN_NEW_WINDOW if not in an iframe', () => {
+      expect.assertions(2)
+      const next = jest.fn()
+
+      const action = openNewWindowModal()
+
+      const window = {
+        parent: {
+          contentWindow: {
+            postMessage: jest.fn(),
+            origin: 'origin',
+          },
+        },
+      }
+      window.self = window
+      window.top = window
+
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+
+      expect(next).toHaveBeenCalledWith(action)
+      expect(window.parent.contentWindow.postMessage).not.toHaveBeenCalled()
+    })
+    it('passes actions to the next middleware', () => {
+      expect.assertions(1)
+      const next = jest.fn()
+
+      const action = {
+        type: 'boo',
+      }
+
+      const window = {}
+
+      const middleware = postMessageMiddleware(window)
+
+      middleware()(next)(action)
+      expect(next).toHaveBeenCalledWith(action)
+    })
+  })
+})
