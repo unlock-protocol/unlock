a7f35bda1d2c526bebcb489abd81016a10c0c77a
Gregory Beaver <greg@unlock-protocol.com>
2019-02-02 22:03:52 -0500

    use the parent window, not the iframe (#1374)

--- unlock-app/src/middlewares/postMessageMiddleware.js
+++ unlock-app/src/middlewares/postMessageMiddleware.js
@@ -4,12 +4,12 @@ import { inIframe } from '../config'
 // store is unused in this middleware, it is only for listening for actions
 // and converting them into postMessage
 const postMessageMiddleware = window => () => {
-  const iframe = window.parent
+  const parent = window.parent
   const enabled = inIframe(window)
   return next => action => {
     if (!enabled) return next(action)
     if (action.type === OPEN_MODAL_IN_NEW_WINDOW) {
-      iframe.contentWindow.postMessage('redirect', iframe.contentWindow.origin)
+      parent.postMessage('redirect', parent.origin)
     }
     return next(action)
   }
