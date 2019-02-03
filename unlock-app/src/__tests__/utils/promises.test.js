eadc2b0430cd4473a629025cb7142c7407d2071e
Gregory Beaver <greg@unlock-protocol.com>
2019-02-02 22:13:06 -0500

    better structure of test

--- unlock-app/src/__tests__/utils/promises.test.js
+++ unlock-app/src/__tests__/utils/promises.test.js
@@ -1,22 +1,24 @@
 import { delayPromise } from '../../utils/promises'
 
-describe('delay', () => {
-  it('should delay for the number of milliseconds specified', async () => {
-    expect.assertions(3)
+describe('promises', () => {
+  describe('delayPromise', () => {
+    it('should delay for the number of milliseconds specified', async () => {
+      expect.assertions(3)
 
-    jest.useFakeTimers()
+      jest.useFakeTimers()
 
-    let delayed = 0
-    delayPromise(100).then(time => {
-      delayed = time
+      let delayed = 0
+      delayPromise(100).then(time => {
+        delayed = time
+      })
+
+      expect(delayed).toBe(0)
+      jest.advanceTimersByTime(99)
+      expect(delayed).toBe(0)
+      jest.advanceTimersByTime(101)
+      await Promise.resolve()
+
+      expect(delayed).toBe(100)
     })
-
-    expect(delayed).toBe(0)
-    jest.advanceTimersByTime(99)
-    expect(delayed).toBe(0)
-    jest.advanceTimersByTime(101)
-    await Promise.resolve()
-
-    expect(delayed).toBe(100)
   })
 })
