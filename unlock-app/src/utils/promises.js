d68826062178b061be0e5a91df808ffc956bc098
Gregory Beaver <greg@unlock-protocol.com>
2019-02-03 22:52:48 -0500

    new delay promise (#1370)
    * new delay promise
    
    * add jsdoc
    
    * move from duration to new promises.js and rename delayPromise
    
    * better structure of test

---
+++ unlock-app/src/utils/promises.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * delay for ms milliseconds, then return the number of ms to resolve
+ */
+export function delayPromise(ms) {
+  return new Promise(resolve => setTimeout(resolve.bind(null, ms), ms))
+}
