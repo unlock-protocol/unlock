6acdca0a57b003800918ee885ad9c804336aa54e
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-02-06 11:57:33 -0500

    Adds Locksmith config enabling storage on staging

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -10,6 +10,7 @@ module.exports = withSourceMaps({
     unlockEnv: process.env.UNLOCK_ENV || 'dev',
     httpProvider: process.env.HTTP_PROVIDER || '127.0.0.1',
     readOnlyProvider: process.env.READ_ONLY_PROVIDER,
+    locksmithHost: process.env.LOCKSMITH_HOST,
   },
   webpack(config) {
     return config
