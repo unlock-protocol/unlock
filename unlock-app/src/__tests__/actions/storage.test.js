73f7b42e1ccb4ede1c340d1c8bc0c5ff3adb8c6f
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-06 17:06:49 -0500

    refactor storageMiddleware (#1448)

--- unlock-app/src/__tests__/actions/storage.test.js
+++ unlock-app/src/__tests__/actions/storage.test.js
@@ -1,27 +1,9 @@
 import {
-  setLockName,
   storageError,
-  SET_LOCK_NAME,
   STORAGE_ERROR,
   storeLockCreation,
-  storeLockUpdate,
   STORE_LOCK_CREATION,
-  STORE_LOCK_UPDATE,
 } from '../../actions/storage'
-
-describe('Storage action', () => {
-  it('should create an action to set a lock', () => {
-    const address = '0x2129127646'
-    const name = 'A Lock Name'
-
-    const expectation = {
-      type: SET_LOCK_NAME,
-      address: address,
-      name: name,
-    }
-    expect(setLockName(address, name)).toEqual(expectation)
-  })
-})
 
 describe('Storage error', () => {
   it('should create an action emitting a storage error', () => {
@@ -50,26 +32,5 @@ describe('Store Lock Creation', () => {
     }
 
     expect(storeLockCreation(owner, lock, token)).toEqual(expectation)
-  })
-})
-
-describe('Store Lock Update', () => {
-  it('should create an action indicating update of a lock', () => {
-    const owner = "An owner's address"
-    const currentLock = 'The current lock address'
-    const token = 'An authorization token'
-    const update = 'A new lock address'
-
-    const expectation = {
-      type: STORE_LOCK_UPDATE,
-      owner: owner,
-      lockAddress: currentLock,
-      token: token,
-      update: update,
-    }
-
-    expect(storeLockUpdate(owner, currentLock, token, update)).toEqual(
-      expectation
-    )
   })
 })
--- unlock-app/src/actions/storage.js
+++ unlock-app/src/actions/storage.js
@@ -1,15 +1,5 @@
-export const SET_LOCK_NAME = 'storage/SET_LOCK_NAME'
 export const STORAGE_ERROR = 'storage/STORAGE_ERROR'
 export const STORE_LOCK_CREATION = 'storage/STORE_LOCK_CREATION'
-export const STORE_LOCK_UPDATE = 'storage/STORE_LOCK_UPDATE'
-
-export function setLockName(address, name) {
-  return {
-    type: SET_LOCK_NAME,
-    address: address,
-    name: name,
-  }
-}
 
 export function storageError(error) {
   return {
@@ -24,15 +14,5 @@ export function storeLockCreation(owner, lock, token) {
     owner: owner,
     lock: lock,
     token: token,
-  }
-}
-
-export function storeLockUpdate(owner, currentLock, token, update) {
-  return {
-    type: STORE_LOCK_UPDATE,
-    owner: owner,
-    lockAddress: currentLock,
-    token: token,
-    update: update,
   }
 }
