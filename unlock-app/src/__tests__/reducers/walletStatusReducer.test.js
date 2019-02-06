4ea4f3013f4cb23725436880b582e3632d51ccbe
Christoph Nascone <chris@unlock-protocol.com>
2019-02-06 11:16:03 -0500

    Use initialState instead of notWaiting

--- unlock-app/src/__tests__/reducers/walletStatusReducer.test.js
+++ unlock-app/src/__tests__/reducers/walletStatusReducer.test.js
@@ -1,18 +1,17 @@
-import reducer from '../../reducers/walletStatusReducer'
+import reducer, { initialState } from '../../reducers/walletStatusReducer'
 import { WAIT_FOR_WALLET, GOT_WALLET } from '../../actions/walletStatus'
 
 describe('walletStatusReducer', () => {
-  const notWaiting = { waiting: false }
   const waiting = { waiting: true }
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(notWaiting)
+    expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should set waiting to true when receiving WAIT_FOR_WALLET', () => {
-    expect(reducer(notWaiting, { type: WAIT_FOR_WALLET })).toEqual(waiting)
+    expect(reducer(initialState, { type: WAIT_FOR_WALLET })).toEqual(waiting)
   })
 
   it('should set waiting to false when receiving GOT_WALLET', () => {
-    expect(reducer(waiting, { type: GOT_WALLET })).toEqual(notWaiting)
+    expect(reducer(waiting, { type: GOT_WALLET })).toEqual(initialState)
   })
 })
