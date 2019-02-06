5d7d8b6bf81d9a5d716b98cb41bfdf01153bcb17
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-06 09:48:15 -0500

    adds an action for new transaction (#1427)

--- unlock-app/src/actions/transaction.js
+++ unlock-app/src/actions/transaction.js
@@ -1,6 +1,12 @@
 export const ADD_TRANSACTION = 'transaction/ADD_TRANSACTION'
 export const UPDATE_TRANSACTION = 'transaction/UPDATE_TRANSACTION'
 export const DELETE_TRANSACTION = 'transaction/DELETE_TRANSACTION'
+export const NEW_TRANSACTION = 'transaction/NEW_TRANSACTION'
+
+export const newTransaction = transaction => ({
+  type: NEW_TRANSACTION,
+  transaction,
+})
 
 export const addTransaction = transaction => ({
   type: ADD_TRANSACTION,
