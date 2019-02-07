a440c88e181f520633d93abfd5b47e16d220f13d
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-07 10:32:38 -0500

    Putting and retrieving transactions from locksmith (#1455)

--- unlock-app/src/middlewares/storageMiddleware.js
+++ unlock-app/src/middlewares/storageMiddleware.js
@@ -3,6 +3,8 @@ import StorageService from '../services/storageService'
 import { STORE_LOCK_CREATION } from '../actions/storage'
 
 import configure from '../config'
+import { NEW_TRANSACTION, addTransaction } from '../actions/transaction'
+import { SET_ACCOUNT } from '../actions/accounts'
 
 const { services } = configure(global)
 
@@ -10,6 +12,30 @@ export default function storageMiddleware({ getState, dispatch }) {
   const storageService = new StorageService(services.storage.host)
   return next => {
     return async action => {
+      if (action.type === SET_ACCOUNT) {
+        // When we set the account, we want to retrieve the list of transactions
+        const transactionHashes = await storageService.getTransactionsHashesSentBy(
+          action.account.address
+        )
+        // Dispatch each lock. Greg probably wants to a batch action?
+        transactionHashes.forEach(hash => {
+          dispatch(
+            addTransaction({
+              hash,
+            })
+          )
+        })
+      }
+
+      if (action.type === NEW_TRANSACTION) {
+        // Storing a new transaction so that we can easoly point to it later on
+        await storageService.storeTransaction(
+          action.transaction.hash,
+          action.transaction.from,
+          action.transaction.to
+        )
+      }
+
       if (action.type === STORE_LOCK_CREATION) {
         // A new lock has been created
         await storageService.storeLockDetails(action.lock, action.token)
