6cc2f08a9fa763ea0ca84d301b547eb6157b0926
Christopher Nascone <cnasc@users.noreply.github.com>
2019-02-07 10:58:40 -0500

    Add walletStatus action to dismiss overlay (#1462)
    * New DISMISS_CHECK action
    
    * test dismissWalletCheck
    
    * Test reducer for new DISMISS_CHECK action
    
    * Move GOT_WALLET and DISMISS_CHECK into catch-all

--- unlock-app/src/reducers/walletStatusReducer.js
+++ unlock-app/src/reducers/walletStatusReducer.js
@@ -1,4 +1,8 @@
-import { WAIT_FOR_WALLET, GOT_WALLET } from '../actions/walletStatus'
+import {
+  WAIT_FOR_WALLET,
+  GOT_WALLET,
+  DISMISS_CHECK,
+} from '../actions/walletStatus'
 
 import { SET_PROVIDER } from '../actions/provider'
 import { SET_NETWORK } from '../actions/network'
@@ -7,16 +11,16 @@ import { SET_ACCOUNT } from '../actions/accounts'
 export const initialState = { waiting: false }
 
 const walletStatusReducer = (walletStatus = initialState, action) => {
-  if ([SET_PROVIDER, SET_NETWORK, SET_ACCOUNT].indexOf(action.type) > -1) {
+  if (
+    [SET_PROVIDER, SET_NETWORK, SET_ACCOUNT, GOT_WALLET, DISMISS_CHECK].indexOf(
+      action.type
+    ) > -1
+  ) {
     return initialState
   }
 
   if (action.type === WAIT_FOR_WALLET) {
     return { waiting: true }
-  }
-
-  if (action.type === GOT_WALLET) {
-    return initialState
   }
 
   return walletStatus
