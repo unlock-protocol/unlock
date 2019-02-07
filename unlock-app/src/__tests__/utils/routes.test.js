091063ba1165224b19a9d53ce6d50eb34ed01997
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-02-06 19:11:12 -0500

    add new account regex and also account hash in lock path regexp (#1451)
    * add new account regex and also account hash in lock path regexp
    
    * fix critical logic error
    
    We need the account to be parsed even if redirect is not present, for the paywall in the iframe to get it
    
    * add explanatory comment

--- unlock-app/src/__tests__/utils/routes.test.js
+++ unlock-app/src/__tests__/utils/routes.test.js
@@ -6,16 +6,19 @@ describe('lockRoute', () => {
       lockAddress: null,
       prefix: null,
       redirect: null,
+      account: null,
     })
     expect(lockRoute('/lock')).toEqual({
       lockAddress: null,
       prefix: null,
       redirect: null,
+      account: null,
     })
     expect(lockRoute('/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54')).toEqual({
       lockAddress: null,
       prefix: null,
       redirect: null,
+      account: null,
     })
   })
 
@@ -52,6 +55,43 @@ describe('lockRoute', () => {
       lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
       prefix: 'demo',
       redirect: 'http://hithere',
+    })
+  })
+  it('should return the correct account parameter when it matches', () => {
+    expect(
+      lockRoute(
+        '/demo/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54/http%3a%2f%2fhithere#0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54'
+      )
+    ).toEqual({
+      lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+      prefix: 'demo',
+      redirect: 'http://hithere',
+      account: '0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+    })
+  })
+  it('should ignore malformed account parameter', () => {
+    expect(
+      lockRoute(
+        // address is too short
+        '/demo/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54/http%3a%2f%2fhithere#0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb'
+      )
+    ).toEqual({
+      lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+      prefix: 'demo',
+      redirect: 'http://hithere',
+      account: undefined,
+    })
+  })
+  it('should return account parameter if redirect is not present', () => {
+    expect(
+      lockRoute(
+        '/demo/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54#0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54'
+      )
+    ).toEqual({
+      lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+      prefix: 'demo',
+      redirect: undefined,
+      account: '0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54',
     })
   })
 })
