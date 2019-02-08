9fdf6abf580f7db21ea48a28689eef91880f4d0e
Gregory Beaver <greg@unlock-protocol.com>
2019-02-08 12:02:27 -0500

    better regexp (+7 squashed commits) Squashed commits: [e6249bb8] fix test [95c42a2b] add test case [1ac6d11c] fix test [93e60f4f] working redirect and key retrieval [baa21d79] remove unused [46bbf2a3] working redirect, both by modal dismissal and return to paywall [1e4c6aa0] more work

--- unlock-app/src/middlewares/interWindowCommunicationMiddleware.js
+++ unlock-app/src/middlewares/interWindowCommunicationMiddleware.js
@@ -1,7 +1,7 @@
 /* eslint promise/prefer-await-to-then: 0 */
 
 import { OPEN_MODAL_IN_NEW_WINDOW, HIDE_MODAL } from '../actions/modal'
-import { ADD_KEY, UPDATE_KEY } from '../actions/key'
+import { UPDATE_KEY } from '../actions/key'
 import { inIframe } from '../config'
 import { lockRoute } from '../utils/routes'
 import { setAccount } from '../actions/accounts'
@@ -13,8 +13,7 @@ function redirectToContentFromPaywall(window, getState) {
     account: { address },
   } = getState()
 
-  const { prefix, redirect } = lockRoute(router.location.pathname)
-  if (prefix !== 'paywall') return
+  const { redirect } = lockRoute(router.location.pathname)
   if (redirect) {
     window.location.href = redirect + '#' + address
   }
@@ -26,68 +25,79 @@ const interWindowCommunicationMiddleware = window => ({
   getState,
   dispatch,
 }) => {
+  let accountChecked = false
   const parent = window.parent
   const isInIframe = inIframe(window)
   return next => {
-    if (isInIframe) {
-      // if we are in the paywall on an iframe, account is not defined
-      // for coinbase wallet or trust wallet, and probably others.
-      // this checks the hash for a valid account and if found,
-      // sets our account to this account. This triggers retrieval
-      // of keys for that account, allowing the paywall to function
-      const { router, account } = getState()
-      if (!account) {
-        const { account: address } = lockRoute(router.location.pathname)
-        if (address) {
-          dispatch(setAccount({ address }))
-          // for subsequent accesses to paywalls, we save the user account in localStorage
-          // this is ONLY a stopgap until we can implement our own paywall-specific provider
-          if (localStorageAvailable(window)) {
-            window.localStorage.setItem('__unlock__account__', address)
-          }
-        } else if (localStorageAvailable(window)) {
-          // retrieve the stored account saved when the user purchased a key in the past
-          const storedAddress = window.localStorage.getItem(
-            '__unlock__account__'
+    return action => {
+      const { router, account, modals } = getState()
+      if (isInIframe && !accountChecked) {
+        accountChecked = true
+        // if we are in the paywall on an iframe, account is not defined
+        // for coinbase wallet or trust wallet, and probably others.
+        // this checks the hash for a valid account and if found,
+        // sets our account to this account. This triggers retrieval
+        // of keys for that account, allowing the paywall to function
+        if (!account) {
+          const { account: address } = lockRoute(
+            // we need the hash in order to retrieve the account from the iframe URL
+            router.location.pathname + router.location.hash
           )
-          if (storedAddress) {
-            dispatch(setAccount({ address: storedAddress }))
+          if (address) {
+            dispatch(setAccount({ address }))
+            // for subsequent accesses to paywalls, we save the user account in localStorage
+            // this is ONLY a stopgap until we can implement our own paywall-specific provider
+            if (localStorageAvailable(window)) {
+              window.localStorage.setItem('__unlock__account__', address)
+            }
+          } else {
+            if (localStorageAvailable(window)) {
+              // retrieve the stored account saved when the user purchased a key in the past
+              const storedAddress = window.localStorage.getItem(
+                '__unlock__account__'
+              )
+              if (storedAddress) {
+                dispatch(
+                  // the fromLocalStorage flag is needed for the overlay to know
+                  // that it should open a new window, even though the account
+                  // is set.
+                  setAccount({ address: storedAddress, fromLocalStorage: true })
+                )
+              }
+            }
           }
         }
       }
-    }
-    return action => {
-      const { router, account } = getState()
-      const { lockAddress } = lockRoute(router.location.pathname)
-      if (isInIframe && action.type === OPEN_MODAL_IN_NEW_WINDOW) {
-        parent.postMessage('redirect', parent.origin)
-      } else if (!isInIframe && action.type === HIDE_MODAL) {
-        // if the user clicks the button to go to content,
-        // we redirect back to the content, appending as a hash
-        // the user account. This is not sent to the server.
-        // then, the paywall.min.js script detects the hash
-        // and forwards it to the paywall in the iframe.
-        // the code at the top of this file handles that case
-        redirectToContentFromPaywall(window, getState)
-      } else if (
-        (!isInIframe && action.type === ADD_KEY) ||
-        action.type === UPDATE_KEY
-      ) {
-        next(action)
-        const state = getState()
-        const key = state.keys[action.id]
-        const modalsShowing = Object.keys(state.modals).length
-        if (
-          account &&
-          key.lock === lockAddress &&
-          key.owner === account.address &&
-          key.expiration &&
-          !modalsShowing
-        ) {
-          console.log(JSON.stringify(key), JSON.stringify(account), lockAddress)
+      const { lockAddress, prefix } = lockRoute(router.location.pathname)
+      if (prefix !== 'paywall') return next(action)
+      if (isInIframe) {
+        if (action.type === OPEN_MODAL_IN_NEW_WINDOW) {
+          parent.postMessage('redirect', parent.origin)
+        }
+      } else {
+        if (action.type === HIDE_MODAL) {
+          // if the user clicks the button to go to content,
+          // we redirect back to the content, appending as a hash
+          // the user account. This is not sent to the server.
+          // then, the paywall.min.js script detects the hash
+          // and forwards it to the paywall in the iframe.
+          // the code at the top of this file handles that case
           redirectToContentFromPaywall(window, getState)
         }
-        return
+        if (action.type === UPDATE_KEY) {
+          const {
+            update: { lock, expiration, owner },
+          } = action
+          if (
+            account &&
+            !modals[lockAddress] &&
+            owner === account.address &&
+            lock === lockAddress &&
+            expiration > new Date().getTime() / 1000
+          ) {
+            redirectToContentFromPaywall(window, getState)
+          }
+        }
       }
       next(action)
     }
