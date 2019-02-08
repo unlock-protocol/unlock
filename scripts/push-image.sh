71d6386525b32404fa6c8440781b2225d2b38b26
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-08 11:42:23 -0500

    using docker.com instead of aws to store our docker image (#1481)

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -8,11 +8,9 @@ ARGS=""
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   if [ "$DOCKER_REPOSITORY" != "" ]; then
-    aws sts get-caller-identity
-    if [ $? -eq 0 ]; then
-      IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
-      docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE;
-      docker push $IMAGE_CACHE;
-    fi
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
+    docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE
+    docker push $IMAGE_CACHE
   fi
 fi
