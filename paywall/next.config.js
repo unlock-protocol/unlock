9945caa4602d5848d1d1fcd15d440405d3c29090
Gregory Beaver <greg@unlock-protocol.com>
2019-02-11 17:08:00 -0500

    basic configuration for paywall (#1502)
    * basic configuration for paywall
    
    * fix artifacts ignore
    
    * remove humans.txt
    
    * pull the static files out of unlock-app

---
+++ paywall/next.config.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+const withSourceMaps = require('@zeit/next-source-maps')
+
+const copyFile = promisify(fs.copyFile)
+
+module.exports = withSourceMaps({
+  publicRuntimeConfig: {
+    unlockEnv: process.env.UNLOCK_ENV || 'dev',
+    httpProvider: process.env.HTTP_PROVIDER || '127.0.0.1',
+    readOnlyProvider: process.env.READ_ONLY_PROVIDER,
+    locksmithHost: process.env.LOCKSMITH_HOST,
+  },
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty',
+    }
+
+    return config
+  },
+  exportPathMap: async (defaultPathMap, { dev, dir, outDir }) => {
+    // Export robots.txt and humans.txt in non-dev environments
+    if (!dev && outDir) {
+      await copyFile(
+        join(dir, 'static', 'robots.txt'),
+        join(outDir, 'robots.txt')
+      )
+
+      // Export _redirects which is used by netlify for URL rewrites
+      await copyFile(
+        join(dir, 'static', '_redirects'),
+        join(outDir, '_redirects')
+      )
+    }
+
+    return {
+      '/': { page: '/' },
+      '/paywall': { page: '/paywall' },
+    }
+  },
+})
