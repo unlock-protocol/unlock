6803b5281ab3d7f83d8c7f2976ff33a522e56451
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-02-11 17:08:00 -0500

    basic configuration for paywall (#1502)
    * basic configuration for paywall
    
    * fix artifacts ignore
    
    * remove humans.txt
    
    * pull the static files out of unlock-app

---
+++ paywall/.eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  extends: [
+    'standard',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  plugins: ['jest', 'mocha', 'promise'],
+  parser: 'babel-eslint',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prefer-stateless-function': [2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+    'brace-style': 0,
+    'react/forbid-prop-types': 2,
+    indent: 0, // this conflicts with prettier and is not needed
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'mocha/no-exclusive-tests': 'error',
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'standard/computed-property-even-spacing': 0,
+    'standard/object-curly-even-spacing': 0,
+    'standard/array-bracket-even-spacing': 0,
+    'promise/prefer-await-to-then': 'warn',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-identical-title': 'error',
+    'jest/no-focused-tests': 'error',
+  },
+}
