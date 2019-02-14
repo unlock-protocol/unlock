51bd721c0e2bbd827ac9837a23e2130e03e238ec
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-02-14 14:13:32 -0500

    Updates some low hanging fruit addressing some lint warning (#1529)

--- unlock-app/src/_server.js
+++ unlock-app/src/_server.js
@@ -5,38 +5,38 @@ const next = require('next')
 const pathMatch = require('path-match')
 
 function _server(port, dev) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const app = next({ dir: `${__dirname}/`, dev, quiet: true })
     const handle = app.getRequestHandler()
     const route = pathMatch()
 
-    app.prepare().then(() => {
-      let server = createServer((req, res) => {
-        console.info(`${req.method} ${req.url} > ${res.statusCode} `)
-        try {
-          const parsedUrl = new URL(req.url, `http://${req.headers.host}/`)
-          const { pathname, query } = parsedUrl
+    await app.prepare()
 
-          // assigning `query` into the params means that we still
-          // get the query string passed to our application
-          const path = pathname.split('/')[1]
-          if (path === 'paywall') {
-            const params = route('/paywall/:lockAddress/:redirect?')(pathname)
-            app.render(req, res, '/paywall', Object.assign(params, query))
-          } else if (path === 'demo') {
-            const params = route('/demo/:lockaddress')(pathname)
-            app.render(req, res, '/demo', Object.assign(params, query))
-          } else {
-            handle(req, res)
-            return
-          }
-        } catch (error) {
-          reject(error)
+    let server = createServer((req, res) => {
+      console.info(`${req.method} ${req.url} > ${res.statusCode} `)
+      try {
+        const parsedUrl = new URL(req.url, `http://${req.headers.host}/`)
+        const { pathname, query } = parsedUrl
+
+        // assigning `query` into the params means that we still
+        // get the query string passed to our application
+        const path = pathname.split('/')[1]
+        if (path === 'paywall') {
+          const params = route('/paywall/:lockAddress/:redirect?')(pathname)
+          app.render(req, res, '/paywall', Object.assign(params, query))
+        } else if (path === 'demo') {
+          const params = route('/demo/:lockaddress')(pathname)
+          app.render(req, res, '/demo', Object.assign(params, query))
+        } else {
+          handle(req, res)
+          return
         }
-      }).listen(port, err => {
-        if (err) throw reject(err)
-        resolve([server, app])
-      })
+      } catch (error) {
+        reject(error)
+      }
+    }).listen(port, err => {
+      if (err) throw reject(err)
+      resolve([server, app])
     })
   })
 }
