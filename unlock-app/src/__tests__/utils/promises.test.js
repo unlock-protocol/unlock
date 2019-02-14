4b39db036880d140429283114c371e0393505ecf
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-02-14 14:13:32 -0500

    Updates some low hanging fruit addressing some lint warning (#1529)

--- unlock-app/src/__tests__/utils/promises.test.js
+++ unlock-app/src/__tests__/utils/promises.test.js
@@ -8,15 +8,13 @@ describe('promises', () => {
       jest.useFakeTimers()
 
       let delayed = 0
-      delayPromise(100).then(time => {
-        delayed = time
-      })
+      let time = delayPromise(100)
 
       expect(delayed).toBe(0)
       jest.advanceTimersByTime(99)
       expect(delayed).toBe(0)
       jest.advanceTimersByTime(101)
-      await Promise.resolve()
+      delayed = await time
 
       expect(delayed).toBe(100)
     })
