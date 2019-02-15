ce5ef2a24e612f6958580dfbda7916ab6eb24982
Nick Furfaro <nfurfaro33@gmail.com>
2019-02-15 16:04:53 -0700

    Adds  disableLock and deleteLock functionality (#1516)
    * Add deprecate and destroy functions
    
    * Add tests
    
    * Add call to read only function calls

---
+++ smart-contracts/test/Lock/destroyLock.js
@@ -0,0 +1,91 @@
+const Units = require('ethereumjs-units')
+const Web3Utils = require('web3-utils')
+const BigNumber = require('bignumber.js')
+
+const deployLocks = require('../helpers/deployLocks')
+const shouldFail = require('../helpers/shouldFail')
+const Unlock = artifacts.require('../Unlock.sol')
+
+let unlock, locks
+
+contract('Lock', accounts => {
+  let lock
+
+  before(async () => {
+    unlock = await Unlock.deployed()
+    locks = await deployLocks(unlock)
+    lock = locks['FIRST']
+  })
+
+  describe('destroyLock', () => {
+    it('should fail if called by the wrong account', async () => {
+      await shouldFail(lock.destroyLock({ from: accounts[1] }), '')
+    })
+
+    describe('when called by the owner', () => {
+      let initialLockBalance, initialOwnerBalance, txObj, event
+
+      before(async () => {
+        await lock.purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+        assert.equal(await lock.getHasValidKey(accounts[1]), true) // pre-req
+
+        initialLockBalance = new BigNumber(
+          await web3.eth.getBalance(lock.address)
+        )
+        initialOwnerBalance = new BigNumber(
+          await web3.eth.getBalance(accounts[0])
+        )
+        await lock.disableLock() // We can't destroy a lock without first disabling it
+        txObj = await lock.destroyLock({ from: accounts[0] })
+        event = txObj.logs[0]
+      })
+
+      it('owner should have received funds from the contract', async () => {
+        const finalOwnerBalance = new BigNumber(
+          await web3.eth.getBalance(accounts[0])
+        )
+        assert(finalOwnerBalance.gt(initialOwnerBalance))
+      })
+
+      it('contract should no longer have any funds', async () => {
+        const finalLockBalance = new BigNumber(
+          await web3.eth.getBalance(lock.address)
+        )
+        assert.equal(finalLockBalance.toFixed(), 0)
+      })
+
+      it('should trigger the Destroy event', () => {
+        assert.equal(event.event, 'Destroy')
+        assert(event.args.balance.eq(initialLockBalance))
+        assert.equal(event.args.owner, accounts[0])
+      })
+
+      it('previously valid key is no longer valid', async () => {
+        assert.equal(await lock.getHasValidKey.call(accounts[1]), false)
+      })
+
+      // After selfdestruct, a user can't buy a key, but if they try they loose their money.
+      it('does not allow people to purchase new keys', async () => {
+        let initialLockBalance = new BigNumber(
+          await web3.eth.getBalance(lock.address)
+        ).toFixed()
+        assert.equal(initialLockBalance, 0)
+
+        // This line does not fail, but instead calls the fallback function and sends msg.value to the destroyed contract.
+        await lock.purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+
+        let finalLockBalance = new BigNumber(
+          await web3.eth.getBalance(lock.address)
+        ).toFixed()
+        assert.equal(finalLockBalance, 10000000000000000)
+
+        // The user did not purchase a key, but still sent their eth to the contract.
+        assert.equal(await lock.getHasValidKey.call(accounts[1]), false)
+      })
+    })
+  })
+})
---
+++ smart-contracts/test/Lock/disableLock.js
@@ -0,0 +1,103 @@
+const Units = require('ethereumjs-units')
+const Web3Utils = require('web3-utils')
+const BigNumber = require('bignumber.js')
+
+const deployLocks = require('../helpers/deployLocks')
+const shouldFail = require('../helpers/shouldFail')
+const Unlock = artifacts.require('../Unlock.sol')
+
+let unlock, locks, ID
+
+contract('Lock', accounts => {
+  let lock
+
+  before(async () => {
+    unlock = await Unlock.deployed()
+    locks = await deployLocks(unlock)
+    lock = locks['FIRST']
+    await lock.purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
+      value: Units.convert('0.01', 'eth', 'wei')
+    })
+    ID = new BigNumber(await lock.getTokenIdFor(accounts[1])).toFixed()
+  })
+
+  describe('disableLock', () => {
+    it('should fail if called by the wrong account', async () => {
+      await shouldFail(lock.disableLock({ from: accounts[1] }), '')
+    })
+
+    it('should fail if called before the lock is disabled', async () => {
+      await shouldFail(
+        lock.destroyLock(),
+        'Not allowed to delete an active lock'
+      )
+    })
+
+    describe('when the lock has been disabled', () => {
+      let txObj, event
+      before(async () => {
+        txObj = await lock.disableLock({ from: accounts[0] })
+        event = txObj.logs[0]
+      })
+
+      it('should trigger the Disable event', () => {
+        console.log(event)
+        assert.equal(event.event, 'Disable')
+      })
+
+      it('should fail if called while lock is disabled', async () => {
+        await shouldFail(
+          lock.disableLock(),
+          'No access after contract has been disabled'
+        )
+      })
+
+      it('should fail if a user tries to purchase a key', async () => {
+        await shouldFail(
+          lock.purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
+            value: Units.convert('0.01', 'eth', 'wei')
+          }),
+          'No access after contract has been disabled'
+        )
+      })
+
+      it('should fail if a user tries to purchase a key with a referral', async () => {
+        await shouldFail(
+          lock.purchaseForFrom(
+            accounts[1],
+            accounts[3],
+            Web3Utils.toHex('Julien'),
+            {
+              value: Units.convert('0.01', 'eth', 'wei')
+            }
+          ),
+          'No access after contract has been disabled'
+        )
+      })
+
+      it('should fail if a user tries to transfer a key', async () => {
+        await shouldFail(
+          lock.transferFrom(accounts[1], accounts[3], ID, {
+            from: accounts[1],
+            value: Units.convert('0.01', 'eth', 'wei')
+          }),
+          'No access after contract has been disabled'
+        )
+      })
+
+      it('should fail if a key owner tries to a approve an address', async () => {
+        await shouldFail(
+          lock.approve(accounts[3], ID, {
+            from: accounts[1]
+          }),
+          'No access after contract has been disabled'
+        )
+      })
+
+      it('should still allow access to non-payable contract functions', async () => {
+        let HasValidKey = await lock.getHasValidKey.call(accounts[1])
+        assert.equal(HasValidKey, true)
+      })
+    })
+  })
+})
