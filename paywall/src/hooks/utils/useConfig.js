6cb37edc70a8f334a1ad397140ffe9cf1a077644
Gregory Beaver <greg@unlock-protocol.com>
2019-02-14 22:16:15 -0500

    update to useConfig from the PR

--- paywall/src/hooks/utils/useConfig.js
+++ paywall/src/hooks/utils/useConfig.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react'
 
-export const ConfigContext = createContext(3)
+export const ConfigContext = createContext()
 
 export default function useConfig() {
   return useContext(ConfigContext)
