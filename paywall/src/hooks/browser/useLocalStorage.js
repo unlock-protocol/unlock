64741498ed0f57d37414dc0b68d7662b646206e4
Gregory Beaver <greg@unlock-protocol.com>
2019-02-14 21:28:03 -0500

    add comments

--- paywall/src/hooks/browser/useLocalStorage.js
+++ paywall/src/hooks/browser/useLocalStorage.js
@@ -8,13 +8,18 @@ export default function useLocalStorage(window, key) {
   if (available) {
     startVal = window.localStorage.getItem(key)
   }
+  // this hook uses state as a proxy for the actual values in localStorage
+  // in order to trigger re-renders on any change to the localStorage
   const [value, setValue] = useState(startVal)
   useEffect(
     () => {
-      if (!available) return
+      if (!available) return // do nothing if localStorage can't be used
       window.localStorage.setItem(key, value)
     },
+    // this effect only runs when the value changes
+    // and not on every update to the component containing the hook
     [value]
   )
+
   return [value, setValue]
 }
