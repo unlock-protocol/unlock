5818b250f8f8bbd1244a996dd445f90e90fe036c
Gregory Beaver <greg@unlock-protocol.com>
2019-02-15 09:22:52 -0500

    New hooks for managing postMessage calls in paywall (#1534)
    * new usePostMessage hook
    
    * Create useConfig.js
    
    * update to useConfig from the PR

---
+++ paywall/src/__tests__/hooks/browser/usePostMessage.test.js
@@ -0,0 +1,144 @@
+import * as rtl from 'react-testing-library'
+import React from 'react'
+
+import { ConfigContext } from '../../../hooks/utils/useConfig'
+import usePostMessage from '../../../hooks/browser/usePostMessage'
+
+describe('usePostMessage hook', () => {
+  const { Provider } = ConfigContext
+
+  let fakeWindow
+  let config
+
+  function wrapper(props) {
+    return <Provider value={config} {...props} />
+  }
+
+  beforeEach(() => {
+    fakeWindow = {
+      parent: {
+        postMessage: jest.fn(),
+        origin: 'origin',
+      },
+    }
+    config = { isServer: false, isInIframe: true }
+  })
+
+  it('posts a message to the window parent when postMessage is called', () => {
+    expect.assertions(2)
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    expect(typeof postMessage).toBe('function')
+    rtl.act(() => {
+      postMessage('hi')
+    })
+    expect(fakeWindow.parent.postMessage).toHaveBeenCalledWith('hi', 'origin')
+  })
+  it('ignores calls on the server', () => {
+    expect.assertions(1)
+
+    config.isServer = true
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    rtl.act(() => {
+      postMessage('hi')
+    })
+    expect(fakeWindow.parent.postMessage).not.toHaveBeenCalled()
+  })
+  it('ignores calls in the main window', () => {
+    expect.assertions(1)
+
+    config.isInIframe = false
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    rtl.act(() => {
+      postMessage('hi')
+    })
+    expect(fakeWindow.parent.postMessage).not.toHaveBeenCalled()
+  })
+  it('ignores calls with an empty message', () => {
+    expect.assertions(1)
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    rtl.act(() => {
+      postMessage()
+    })
+    expect(fakeWindow.parent.postMessage).not.toHaveBeenCalled()
+  })
+  it('does not call postMessage twice with the same message', () => {
+    expect.assertions(1)
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    rtl.act(() => {
+      postMessage('hi')
+    })
+    rtl.act(() => {
+      postMessage('hi')
+    })
+
+    expect(fakeWindow.parent.postMessage).toHaveBeenCalledTimes(1)
+  })
+  it('does call postMessage when the message changes', () => {
+    expect.assertions(2)
+
+    const {
+      result: {
+        current: { postMessage },
+      },
+    } = rtl.testHook(() => usePostMessage(fakeWindow), {
+      wrapper,
+    })
+
+    rtl.act(() => {
+      postMessage('hi')
+    })
+
+    rtl.act(() => {
+      postMessage('hi')
+    })
+
+    rtl.act(() => {
+      postMessage('bye')
+    })
+    expect(fakeWindow.parent.postMessage).toHaveBeenCalledTimes(2)
+    expect(fakeWindow.parent.postMessage).toHaveBeenLastCalledWith(
+      'bye',
+      'origin'
+    )
+  })
+})
