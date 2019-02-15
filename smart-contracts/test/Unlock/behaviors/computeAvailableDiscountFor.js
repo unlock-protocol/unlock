20e304f660a0fad9541d0b828d57d84a74c77ec7
hardlydifficult <hardlydifficultllc@gmail.com>
2019-02-15 11:52:36 -0800

    Fix lint errors

--- smart-contracts/test/Unlock/behaviors/computeAvailableDiscountFor.js
+++ smart-contracts/test/Unlock/behaviors/computeAvailableDiscountFor.js
@@ -2,10 +2,8 @@ const deployLocks = require('../../helpers/deployLocks')
 
 exports.shouldComputeAvailableDiscountFor = function () {
   describe('computeAvailableDiscountFor', function () {
-    let locks
-
     beforeEach(async function () {
-      locks = await deployLocks(this.unlock)
+      await deployLocks(this.unlock)
     })
 
     it('should return the eth amount of discount as well as the number of tokens used to claim that discount')
