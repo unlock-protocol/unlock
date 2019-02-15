61bf4eedf0c74c629dff222660008500071bc266
Gregory Beaver <greg@unlock-protocol.com>
2019-02-15 09:28:37 -0500

    new useLocalStorage hook (#1537)
    * new useLocalStorage hook
    
    * add comments

---
+++ paywall/src/__tests__/hooks/browser/useLocalStorage-no-localStorage.test.js
@@ -0,0 +1,37 @@
+import * as rtl from 'react-testing-library'
+import useLocalStorage from '../../../hooks/browser/useLocalStorage'
+
+jest.mock('../../../utils/localStorage', () => () => false)
+
+describe('no localStorage', () => {
+  let fakeWindow
+
+  beforeEach(() => {
+    jest.resetModules()
+    fakeWindow = {
+      storage: {},
+      localStorage: {
+        setItem: jest.fn((x, y) => (fakeWindow.storage[x] = y)),
+        getItem: jest.fn(x => fakeWindow.storage[x]),
+        removeItem: () => {},
+      },
+    }
+  })
+  it('does nothing', () => {
+    fakeWindow.localStorage.removeItem = () => {
+      throw new Error('nope')
+    }
+
+    const {
+      result: {
+        current: [, setValue],
+      },
+    } = rtl.testHook(() => useLocalStorage(fakeWindow, 'hi'))
+
+    expect(fakeWindow.localStorage.setItem).not.toHaveBeenCalled()
+
+    rtl.act(() => setValue('nope'))
+
+    expect(fakeWindow.localStorage.setItem).not.toHaveBeenCalled()
+  })
+})
