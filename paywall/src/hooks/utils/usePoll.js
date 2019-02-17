7a6c6e917727a6f879cdbb194d317de41f14cccb
Gregory Beaver <greg@unlock-protocol.com>
2019-02-17 17:17:13 -0500

    simplify and fix the polling effect based on real-world testing

--- paywall/src/hooks/utils/usePoll.js
+++ paywall/src/hooks/utils/usePoll.js
@@ -5,19 +5,23 @@ import { useEffect, useRef } from 'react'
  * that will automatically start the polling when the component mounts, and remove it when it
  * umounts
  */
-export default function usePoll(cb, delay) {
+export default function usePoll(cb, delay, noPoll = false) {
   // we use a ref to avoid re-render on change
   // refs act like class properties and are not to be confused with
   // the old React concept of ref
   const cancel = useRef()
   // this hook only runs on mount and cancels on umount
-  useEffect(() => {
-    let poll = f => {
-      cb()
-      cancel.current = setTimeout(f, delay)
-    }
-    poll = poll.bind(null, poll)
-    cancel.current = setTimeout(poll, delay)
-    return () => clearTimeout(cancel.current)
-  }, [])
+  useEffect(
+    () => {
+      if (noPoll) return
+      if (cancel.current) clearTimeout(cancel.current)
+      function poll() {
+        cb()
+        cancel.current = setTimeout(poll, delay)
+      }
+      poll()
+      return () => clearTimeout(cancel.current)
+    },
+    [cb]
+  )
 }
