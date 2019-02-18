d560f5b4d1178e55d613814c644fbd03cc918288
Christoph Nascone <chris@unlock-protocol.com>
2019-02-18 15:30:53 -0500

    Initial version of log page, can fetch transaction data with react hook

---
+++ unlock-app/src/pages/log.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+import UnlockPropTypes from '../propTypes'
+import Layout from '../components/interface/Layout'
+import CreatorAccount from '../components/creator/CreatorAccount'
+import CreatorLocks from '../components/creator/CreatorLocks'
+import DeveloperOverlay from '../components/developer/DeveloperOverlay'
+import BrowserOnly from '../components/helpers/BrowserOnly'
+import GlobalErrorConsumer from '../components/interface/GlobalErrorConsumer'
+import GlobalErrorProvider from '../utils/GlobalErrorProvider'
+import { pageTitle } from '../constants'
+import Web3Service from '../services/web3Service'
+
+export const Log = ({ account, network, lockAddresses }) => {
+  const [transactions, setTransactions] = useState({})
+  const w3s = new Web3Service()
+
+  const fetchTransactions = async () => {
+    const creations = await w3s.getPastLockCreationsTransactionsForUser(
+      account.address
+    )
+
+    let lockTxs = []
+    lockAddresses.forEach(async address => {
+      const txs = await w3s.getPastLockTransactions(address)
+      lockTxs = [...lockTxs, ...txs]
+    })
+
+    setTransactions([...creations, ...lockTxs])
+  }
+
+  useEffect(
+    () => {
+      fetchTransactions()
+    },
+    [account, lockAddresses]
+  )
+
+  console.log({ transactions, lockAddresses })
+  return (
+    <GlobalErrorProvider>
+      <GlobalErrorConsumer>
+        <Layout title="Creator Log">
+          <Head>
+            <title>{pageTitle('Log')}</title>
+          </Head>
+          <BrowserOnly>
+            <CreatorAccount network={network} account={account} />
+          </BrowserOnly>
+        </Layout>
+      </GlobalErrorConsumer>
+    </GlobalErrorProvider>
+  )
+}
+
+Log.displayName = 'Log'
+
+Log.propTypes = {
+  account: UnlockPropTypes.account.isRequired,
+  network: UnlockPropTypes.network.isRequired,
+  lockAddresses: PropTypes.arrayOf(UnlockPropTypes.address).isRequired,
+}
+
+export const mapStateToProps = ({ account, network, locks }) => {
+  const lockAddresses = Object.keys(locks)
+  return {
+    account,
+    network,
+    lockAddresses,
+  }
+}
+
+export default connect(mapStateToProps)(Log)
