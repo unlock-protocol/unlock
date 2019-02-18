123607f31781bfb1d074d98a6365f52f847f7618
Gregory Beaver <greg@unlock-protocol.com>
2019-02-18 00:24:03 -0500

    allow passing in custom check variables for useEffect

--- paywall/src/hooks/utils/usePoll.js
+++ paywall/src/hooks/utils/usePoll.js
@@ -5,23 +5,20 @@ import { useEffect, useRef } from 'react'
  * that will automatically start the polling when the component mounts, and remove it when it
  * umounts
  */
-export default function usePoll(cb, delay, noPoll = false) {
+export default function usePoll(cb, delay, vars = [cb], noPoll = false) {
   // we use a ref to avoid re-render on change
   // refs act like class properties and are not to be confused with
   // the old React concept of ref
   const cancel = useRef()
   // this hook only runs on mount and cancels on umount
-  useEffect(
-    () => {
-      if (noPoll) return
-      if (cancel.current) clearTimeout(cancel.current)
-      function poll() {
-        cb()
-        cancel.current = setTimeout(poll, delay)
-      }
-      poll()
-      return () => clearTimeout(cancel.current)
-    },
-    [cb]
-  )
+  useEffect(() => {
+    if (noPoll) return
+    if (cancel.current) clearTimeout(cancel.current)
+    function poll() {
+      cb()
+      cancel.current = setTimeout(poll, delay)
+    }
+    poll()
+    return () => clearTimeout(cancel.current)
+  }, vars)
 }
