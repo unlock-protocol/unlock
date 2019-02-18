cae6e17839122288269617023c6a736f0e35e2d2
Gregory Beaver <greg@unlock-protocol.com>
2019-02-18 00:18:10 -0500

    refactor - much cleaner, especially in the test

--- paywall/src/__tests__/hooks/asyncActions/accounts.test.js
+++ paywall/src/__tests__/hooks/asyncActions/accounts.test.js
@@ -1,240 +1,92 @@
-import {
-  makeGetAccount,
-  makePollForAccountChange,
-} from '../../../hooks/asyncActions/accounts'
+import { getAccounts, getBalance } from '../../../hooks/asyncActions/accounts'
 
 describe('useAccount async account actions', () => {
-  describe('makeGetAccount', () => {
-    let setAccount
-    let setBalance
-    let saveLocalStorageAccount
-    let localStorageAccount
-    let isInIframe
-    let web3
-    let fakeWindow = {
-      location: {
-        pathname:
-          '/demo/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54/http%3a%2f%2fhithere',
-        hash: '#0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54',
-      },
-    }
-
-    // this helper function allows us to easily create a mock getAccount
-    // to test the code works as expected
-    function testMockGetAccount() {
-      return makeGetAccount({
-        window: fakeWindow,
-        web3,
-        isInIframe,
-        localStorageAccount,
-        saveLocalStorageAccount,
-        setAccount,
-        setBalance,
-      })
-    }
-    beforeEach(() => {
-      setAccount = jest.fn()
-      setBalance = jest.fn()
-      saveLocalStorageAccount = jest.fn()
-      isInIframe = true
-      localStorageAccount = undefined
-      web3 = undefined
-    })
-    it('does nothing if web3 is not defined', async () => {
-      web3 = false
-      const getAccount = testMockGetAccount()
-
-      await getAccount()
-
-      expect(setAccount).not.toHaveBeenCalled()
-    })
-    describe('web3 is set', () => {
-      let accounts = ['account']
-      let balance = '1000000000000000'
-      beforeEach(() => {
-        web3 = {
-          eth: {
-            getAccounts() {
-              return Promise.resolve(accounts)
-            },
-            getBalance() {
-              return Promise.resolve(balance)
-            },
-          },
-        }
-      })
-      describe('paywall in iframe', () => {
-        describe('account does not exist (like in coinbase wallet)', () => {
-          beforeEach(() => {
-            accounts = []
-          })
-          it('sets the account from localStorage if possible', async () => {
-            localStorageAccount = 'local'
-
-            const getAccount = testMockGetAccount()
-
-            await getAccount()
-
-            expect(setAccount).toHaveBeenCalledWith('local')
-          })
-          it('sets the account from the URL if no localStorage account exists', async () => {
-            const getAccount = testMockGetAccount()
-
-            await getAccount()
-
-            expect(setAccount).toHaveBeenCalledWith(
-              '0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54'
-            )
-          })
-          it('saves the account in localStorage if provided in the URL', async () => {
-            const getAccount = testMockGetAccount()
-
-            await getAccount()
-
-            expect(saveLocalStorageAccount).toHaveBeenCalledWith(
-              '0xaaa8825a3e7Fb15263D0DD455B8aAfc08503bb54'
-            )
-          })
-        })
-      })
-      describe('normal account flow', () => {
-        let getAccount
-        beforeEach(() => {
-          getAccount = testMockGetAccount()
-          accounts = ['account']
-        })
-        it('sets the account', async () => {
-          await getAccount()
-
-          expect(setAccount).toHaveBeenCalledWith('account')
-        })
-        it('retrieves and sets the balance', async () => {
-          await getAccount()
-
-          expect(setBalance).toHaveBeenCalledWith('0.001')
-        })
-      })
-    })
-  })
-  describe('makePollForAccount', () => {
-    let web3
-    let isInIframe
-    let localStorageAccount
-    let account
-    let nextAccount
-    let balance
-    let setAccount
-    let setBalance
-
-    // easy mock pollForAccountChange creator for testing
-    function testMockPollForAccount() {
-      return makePollForAccountChange({
-        web3,
-        isInIframe,
-        localStorageAccount,
-        account,
-        setAccount,
-        setBalance,
-      })
-    }
-
-    beforeEach(() => {
-      account = 'account'
-      nextAccount = 'next'
-      balance = '10000000000000'
-      web3 = {
+  describe('getAccounts', () => {
+    it('succeeds', done => {
+      expect.assertions(1)
+      const web3 = {
         eth: {
-          getAccounts: jest.fn(() => {
-            return Promise.resolve([nextAccount])
-          }),
-          getBalance: jest.fn(() => {
-            return Promise.resolve(balance)
-          }),
+          getAccounts: () => Promise.resolve(['account']),
         },
       }
-      isInIframe = true
-      localStorageAccount = undefined
-      setAccount = jest.fn()
-      setBalance = jest.fn()
+      const handler = account => {
+        expect(account).toBe('account')
+        done()
+      }
+
+      getAccounts(handler, web3)
     })
-    describe('in iframe', () => {
-      it('does nothing if in the iframe and account was set via localStorage', async () => {
-        localStorageAccount = 'local'
+    it('fails on error', done => {
+      expect.assertions(1)
+      const web3 = {
+        eth: {
+          getAccounts: () => Promise.reject('account'),
+        },
+      }
+      const handler = account => {
+        expect(account).toBe(null)
+        done()
+      }
 
-        const pollForAccountChange = testMockPollForAccount()
-
-        await pollForAccountChange()
-
-        expect(web3.eth.getAccounts).not.toHaveBeenCalled()
-      })
-      it('retrieves account if account was not retrieved from localStorage', async () => {
-        const pollForAccountChange = testMockPollForAccount()
-
-        await pollForAccountChange()
-
-        expect(web3.eth.getAccounts).toHaveBeenCalled()
-      })
+      getAccounts(handler, web3)
     })
-    describe('normal polling', () => {
-      it('does nothing if the new account matches the old', async () => {
-        expect.assertions(2)
-        nextAccount = account
+    it('fails on no accounts', done => {
+      expect.assertions(1)
+      const web3 = {
+        eth: {
+          getAccounts: () => Promise.resolve([]),
+        },
+      }
+      const handler = account => {
+        expect(account).toBe(null)
+        done()
+      }
 
-        const pollForAccountChange = testMockPollForAccount()
+      getAccounts(handler, web3)
+    })
+  })
+  describe('getBalance', () => {
+    it('succeeds', done => {
+      expect.assertions(1)
+      const web3 = {
+        eth: {
+          getBalance: () => Promise.resolve('123'),
+        },
+      }
+      const handler = balance => {
+        expect(balance).toBe('123')
+        done()
+      }
 
-        await pollForAccountChange()
+      getBalance(handler, web3, 'account')
+    })
+    it('fails on error', done => {
+      expect.assertions(1)
+      const web3 = {
+        eth: {
+          getBalance: () => Promise.reject('account'),
+        },
+      }
+      const handler = balance => {
+        expect(balance).toBe('0')
+        done()
+      }
 
-        expect(web3.eth.getAccounts).toHaveBeenCalled()
-        expect(setAccount).not.toHaveBeenCalled()
-      })
-      it('does nothing if the user was logged out and is still logged out', async () => {
-        expect.assertions(2)
-        nextAccount = account = null
+      getBalance(handler, web3, 'account')
+    })
+    it('fails on no account', done => {
+      expect.assertions(1)
+      const web3 = {
+        eth: {
+          getBalance: () => Promise.resolve('123'),
+        },
+      }
+      const handler = balance => {
+        expect(balance).toBe('0')
+        done()
+      }
 
-        const pollForAccountChange = testMockPollForAccount()
-
-        await pollForAccountChange()
-
-        expect(web3.eth.getAccounts).toHaveBeenCalled()
-        expect(setAccount).not.toHaveBeenCalled()
-      })
-      it('swallows exceptions', async () => {
-        web3.eth.getAccounts = () => Promise.reject(new Error('nope'))
-
-        const pollForAccountChange = testMockPollForAccount()
-
-        await pollForAccountChange()
-
-        expect(setAccount).not.toHaveBeenCalled()
-      })
-      describe('account change', () => {
-        beforeEach(() => {
-          balance = '3000000000000000'
-        })
-        it('sets the account if it is different', async () => {
-          const pollForAccountChange = testMockPollForAccount()
-
-          await pollForAccountChange()
-
-          expect(setAccount).toHaveBeenCalledWith('next')
-        })
-        it('retrieves and sets the balance if the account exists', async () => {
-          const pollForAccountChange = testMockPollForAccount()
-
-          await pollForAccountChange()
-
-          expect(setBalance).toHaveBeenCalledWith('0.003')
-        })
-        it('resets balance to 0 if the account was logged out', async () => {
-          nextAccount = null
-
-          const pollForAccountChange = testMockPollForAccount()
-
-          await pollForAccountChange()
-
-          expect(setBalance).toHaveBeenCalledWith('0')
-        })
-      })
+      getBalance(handler, web3)
     })
   })
 })
--- paywall/src/hooks/asyncActions/accounts.js
+++ paywall/src/hooks/asyncActions/accounts.js
@@ -1,94 +1,24 @@
-import Web3Utils from 'web3-utils'
-
-import { lockRoute } from '../../utils/routes'
-
-export function makeGetAccount({
-  window,
-  web3,
-  isInIframe,
-  localStorageAccount,
-  saveLocalStorageAccount,
-  setAccount,
-  setBalance,
-}) {
-  const getAccount = async () => {
-    if (!web3) return
-    let thisAccount = null
-    let balance = '0'
-    try {
-      const accounts = await web3.eth.getAccounts()
-      thisAccount = accounts[0]
-      if (!thisAccount) thisAccount = null
-    } catch (e) {
-      // ignore
-      thisAccount = null
-    }
-    if (!thisAccount && isInIframe) {
-      // in coinbase wallet, trust wallet, and possibly others,
-      // we never have access to the account in an iframe.
-      // these fallback systems are used to propagate the
-      // account from the location of key purchase to the iframe.
-      if (localStorageAccount) {
-        // we have previously purchased a key, so we will use the account
-        // that was stored in localStorage
-        thisAccount = localStorageAccount
-      } else {
-        const { account: address } = lockRoute(
-          // we need the hash in order to retrieve the account from the iframe URL
-          window.location.pathname + window.location.hash
-        )
-        if (address) {
-          // we have just been redirected from the key purchase page to here
-          // and will save the account address for use the next time
-          // a user visits any paywall on the web
-          thisAccount = address
-          saveLocalStorageAccount(address)
-        }
-      }
-    }
-    setAccount(thisAccount)
-
-    if (thisAccount) {
-      // we only retrieve the balance if we have a real account
-      balance = await web3.eth.getBalance(thisAccount)
-    }
-    // see note above about this hack for testing
-    setBalance(Web3Utils.fromWei(balance, 'ether'))
+export const getAccounts = async (handle, wallet) => {
+  let account
+  try {
+    const accounts = await wallet.eth.getAccounts()
+    account = accounts[0] || null
+  } catch (e) {
+    account = null
   }
-  return getAccount
+  handle(account)
 }
 
-export function makePollForAccountChange({
-  web3,
-  isInIframe,
-  localStorageAccount,
-  account,
-  setAccount,
-  setBalance,
-}) {
-  const pollForAccountChange = async () => {
-    // if we are in something like coinbase wallet, and have purchased
-    // a key before, then the account is stored in localStorage. In this
-    // case, the account will never change, so we don't
-    // need to poll
-    if (isInIframe && localStorageAccount) return
-    try {
-      const nextAccounts = await web3.eth.getAccounts()
-      const next = nextAccounts && nextAccounts[0]
-      if (next !== account) {
-        setAccount(next)
-        if (next) {
-          const balance = await web3.eth.getBalance(next)
-          setBalance(Web3Utils.fromWei(balance, 'ether'))
-        } else {
-          // reset the balance to 0 for logged out user in
-          // case we had a balance with the old account
-          setBalance('0')
-        }
-      }
-    } catch (e) {
-      // swallow
-    }
+export const getBalance = async (handle, wallet, account) => {
+  if (!account) {
+    handle('0')
+    return
   }
-  return pollForAccountChange
+  let balance
+  try {
+    balance = await wallet.eth.getBalance(account)
+  } catch (e) {
+    balance = '0'
+  }
+  handle(balance)
 }
