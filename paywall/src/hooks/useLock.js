be3710250adaceaea04b2f9cfcb66ef8602607ef
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-02-18 08:57:58 -0500

    new useLock hook for the paywall (#1557)
    * new useLock hook for the paywall
    
    * bug: we depend on web3 as well, and it can be undefined
    
    * test for no re-render

---
+++ paywall/src/hooks/useLock.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+import LockContract from '../artifacts/contracts/PublicLock.json'
+import useWeb3 from './web3/useWeb3'
+import { makeGetLockAttributes } from './asyncActions/locks'
+
+export default function useLock(lockAddress) {
+  const [lock, setLock] = useState({ address: lockAddress })
+  const web3 = useWeb3()
+
+  const getLockAttributes = makeGetLockAttributes({
+    web3,
+    lockAddress,
+    setLock,
+  })
+  useEffect(
+    () => {
+      if (!web3) return
+      const contract = new web3.eth.Contract(LockContract.abi, lockAddress)
+      getLockAttributes(contract)
+    },
+    [web3, lockAddress]
+  )
+  return lock
+}
