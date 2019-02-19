65192757b24d4c4be17ba23e272ca0acb7a7c45a
Christoph Nascone <chris@unlock-protocol.com>
2019-02-19 10:45:59 -0500

    Nice looking table that auto-sizes

--- unlock-app/src/pages/log.js
+++ unlock-app/src/pages/log.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import UnlockPropTypes from '../propTypes'
 import Layout from '../components/interface/Layout'
 import CreatorAccount from '../components/creator/CreatorAccount'
@@ -14,7 +15,11 @@ import Web3Service from '../services/web3Service'
 export const Log = ({ account, network, lockAddresses }) => {
   const [transactions, setTransactions] = useState([])
   const w3s = new Web3Service()
-
+  // TODO: this can be smarter. We can probably cache a lot of this in
+  // localStorage and maintain a list of the last blockNumber we checked. With
+  // an update to _getPastTransactionsForContract to accept a fromBlock
+  // parameter we can then only query for data we don't already hold and merge
+  // it with what we've stored.
   const fetchTransactions = () => {
     let lockPromises = lockAddresses.map(address =>
       w3s.getPastLockTransactions(address)
@@ -31,13 +36,16 @@ export const Log = ({ account, network, lockAddresses }) => {
       allTransactions = allTransactions.sort(
         (a, b) => b.blockNumber - a.blockNumber
       )
+      console.log(allTransactions)
       setTransactions(allTransactions)
     })
   }
 
   useEffect(
     () => {
-      fetchTransactions()
+      if (account) {
+        fetchTransactions()
+      }
     },
     [account, lockAddresses]
   )
@@ -51,11 +59,25 @@ export const Log = ({ account, network, lockAddresses }) => {
           </Head>
           <BrowserOnly>
             <CreatorAccount network={network} account={account} />
-            <ol>
-              {transactions.map(tx => (
-                <li key={tx.id}>{tx.blockNumber + ' ' + tx.event}</li>
-              ))}
-            </ol>
+            <Body>
+              <LogHeader>Block Number</LogHeader>
+              <LogHeader>Lock Name/Address</LogHeader>
+              <LogHeader>Type</LogHeader>
+              {transactions.length > 0 &&
+                transactions.map(tx => (
+                  <>
+                    <LogElement key={tx.id + '__blockNumber'}>
+                      {tx.blockNumber}
+                    </LogElement>
+                    <LogElement key={tx.id + '__address'}>
+                      {tx.address}
+                    </LogElement>
+                    <Type key={tx.id + '__type'} type={tx.event}>
+                      {tx.event}
+                    </Type>
+                  </>
+                ))}
+            </Body>
           </BrowserOnly>
         </Layout>
       </GlobalErrorConsumer>
@@ -70,6 +92,34 @@ Log.propTypes = {
   network: UnlockPropTypes.network.isRequired,
   lockAddresses: PropTypes.arrayOf(UnlockPropTypes.address).isRequired,
 }
+
+const Body = styled.div`
+  display: grid;
+  grid-template-columns: min-content min-content min-content;
+  grid-template-rows: 20px;
+  grid-column-gap: 20px;
+`
+
+const LogHeader = styled.div`
+  font-size: 8px;
+  font-weight: normal;
+  text-transform: uppercase;
+  color: var(--grey);
+  white-space: nowrap;
+`
+
+const LogElement = styled.div`
+  font-size: 11px;
+  font-weight: 300;
+`
+
+const typeColors = {
+  NewLock: 'green',
+}
+
+const Type = styled(LogElement)`
+  color: var(-- ${props => typeColors[props.type] || 'darkgrey'});
+`
 
 export const mapStateToProps = ({ account, network, locks }) => {
   const lockAddresses = Object.keys(locks)
