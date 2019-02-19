d7331657c9153f428bb5f8459bcc44393f4c16d0
hardlydifficult <hardlydifficultllc@gmail.com>
2019-02-19 14:04:13 -0800

    Switch Lock from openzeppelin-eth to openzeppelin-solidity

--- smart-contracts/contracts/PublicLock.sol
+++ smart-contracts/contracts/PublicLock.sol
@@ -3,10 +3,9 @@ pragma solidity 0.4.25;
 import './interfaces/IUnlock.sol';
 import './interfaces/IERC721.sol';
 import './interfaces/ILockCore.sol';
-import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-eth/contracts/introspection/ERC165.sol';
-import 'openzeppelin-eth/contracts/math/SafeMath.sol';
-import 'openzeppelin-eth/contracts/token/ERC721/IERC721Receiver.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 
 /**
@@ -188,12 +187,11 @@ contract PublicLock is ILockCore, ERC165, IERC721, IERC721Receiver, Ownable {
     uint _keyPrice,
     uint _maxNumberOfKeys,
     uint _version
-  )
-  public {
+  ) public
+    Ownable(_owner)
+  {
     require(_expirationDuration <= 100 * 365 * 24 * 60 * 60, 'MAX_EXPIRATION_100_YEARS');
     unlockProtocol = msg.sender; // Make sure we link back to Unlock's smart contract.
-    Ownable.initialize(_owner);
-    ERC165.initialize();
     expirationDuration = _expirationDuration;
     keyPrice = _keyPrice;
     maxNumberOfKeys = _maxNumberOfKeys;
