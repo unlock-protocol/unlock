e77896e43742879f274b407ca2cb11d103108fcb
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-02-20 10:38:21 -0800

    Standard IERC721Receiver (#1605)
    Switching from our own interface to the openzeppelin implementation. This is a 1:1 match, should have 0 impact to the bytecode generated.

--- smart-contracts/contracts/interfaces/IERC721.sol
+++ smart-contracts/contracts/interfaces/IERC721.sol
@@ -100,25 +100,3 @@ interface IERC721 {
   /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
   // function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
-
-// interface ERC721TokenReceiver {
-//     /// @notice Handle the receipt of an NFT
-//     /// @dev The ERC721 smart contract calls this function on the
-//     /// recipient after a `transfer`. This function MAY throw to revert and reject the transfer. Return
-//     /// of other than the magic value MUST result in the transaction being reverted.
-//     /// @notice The contract address is always the message sender.
-//     /// @param _operator The address which called `safeTransferFrom` function
-//     /// @param _from The address which previously owned the token
-//     /// @param _tokenId The NFT identifier which is being transferred
-//     /// @param _data Additional data with no specified format
-//     /// @return `bytes4(keccak256("onERC721Received(address,address,uint,bytes)"))`
-//     /// unless throwing
-    // function onERC721Received(
-    //   address _operator,
-    //   address _from,
-    //   uint _tokenId,
-    //   bytes _data
-    // )
-    // external
-    // returns(bytes4);
-//  }
