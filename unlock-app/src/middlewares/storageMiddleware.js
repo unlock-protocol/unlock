f4e7ab0ff9878ee6ee7b17ce5a8773991302e639
Gregory Beaver <greg@unlock-protocol.com>
2019-02-20 17:25:09 -0500

    fix potential endless loop (temp fix until Akeem can clean it up) (#1619)

--- unlock-app/src/middlewares/storageMiddleware.js
+++ unlock-app/src/middlewares/storageMiddleware.js
@@ -63,6 +63,7 @@ export default function storageMiddleware({ getState, dispatch }) {
           storageService
             .lockLookUp(action.address)
             .then(name => {
+              if (!name) return
               dispatch(updateLock(action.address, { name }))
             })
             .catch(error => {
