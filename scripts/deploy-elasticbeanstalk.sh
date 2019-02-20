08800ac6b876963d23a4a4dfec54a12835097d94
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-02-20 08:53:40 -0500

    Updates deployment initiator

---
+++ scripts/deploy-elasticbeanstalk.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# This script deploys the current state of Locksmith
+# to the Elasticbeanstalk environment as detailed in the configuration
+# outlined at the top of the script.
+#
+# The heavy lifting is provided via the awscli
+
+readonly BUILD="locksmith-${TRAVIS_BUILD_ID}"
+readonly ARTIFACT_LOCATION="./builds/${BUILD}.zip"
+readonly S3_BUCKET="unlock-locksmith"
+readonly AWS_REGION="us-east-1"
+readonly APPLICATION="locksmith"
+readonly DEPLOYMENT_ENVIRONMENT="Locksmith-env-1"
+
+function package_application()
+{
+    local application=${1}
+    local artifact_location=${2}
+    
+    mkdir builds
+    pushd ./${application}
+    zip ../${artifact_location} -r * .[^.]* --exclude=*node_modules*
+    popd
+    return 0
+}
+
+function upload_to_s3()
+{
+    local artifact_location=${1}
+    local s3_bucket=${2}
+    
+    aws s3 cp ${artifact_location} s3://${s3_bucket}/
+}
+
+function elasticbeanstalk_create_application_version()
+{
+    local s3_bucket=${1}
+    local artifact=${2}
+    local description="$TRAVIS_COMMIT_MESSAGE"
+    
+    aws elasticbeanstalk create-application-version \
+    --application-name ${APPLICATION} \
+    --version-label ${BUILD} \
+    --description "${description}" \
+    --source-bundle S3Bucket="${s3_bucket}",S3Key="${artifact}" \
+    --auto-create-application \
+    --region ${AWS_REGION}
+}
+
+function update_environment()
+{
+    local environment=${1}
+    local version_label=${2}
+    
+    aws elasticbeanstalk update-environment \
+    --environment-name ${environment} \
+    --version-label ${version_label} \
+    --region ${AWS_REGION}
+}
+
+function check_preconditions()
+{
+    if [ -n "$AWS_ACCESS_KEY_ID" ] && \
+       [ -n "$AWS_SECRET_ACCESS_KEY" ] && \
+       [ "$TRAVIS_BRANCH" = "master" ] &&  \
+       [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        pip install --user awscli;
+        export PATH=$PATH:$HOME/.local/bin;
+        
+        return 0
+    else
+        echo "Unable to deploy due to missing configuration."
+        exit 1
+    fi
+}
+
+check_preconditions
+package_application ${APPLICATION} ${ARTIFACT_LOCATION}
+upload_to_s3 ${ARTIFACT_LOCATION} ${S3_BUCKET}
+elasticbeanstalk_create_application_version ${S3_BUCKET} ${BUILD}.zip
+update_environment Locksmith-env-1 ${BUILD}
+
