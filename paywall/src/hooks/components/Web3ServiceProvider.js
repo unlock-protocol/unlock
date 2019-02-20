a113ee833bd39c41f95e94855393d111c8bef143
Gregory Beaver <greg@unlock-protocol.com>
2019-02-19 23:16:21 -0500

    Web3Service -> Web3ServiceProvider

--- paywall/src/hooks/components/Web3ServiceProvider.js
+++ paywall/src/hooks/components/Web3ServiceProvider.js
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types'
 import React, { createContext } from 'react'
 
+import useConfig from '../utils/useConfig'
 import web3Service from '../../services/web3Service'
 
 export const Web3ServiceContext = createContext()
 
-export default function Web3Service({ children }) {
+export default function Web3ServiceProvider({ children }) {
   const { Provider } = Web3ServiceContext
-  const web3 = new web3Service()
+  const { unlockAddress } = useConfig()
+  const web3 = new web3Service(unlockAddress)
   return <Provider value={web3}>{children}</Provider>
 }
 
-Web3Service.propTypes = {
+Web3ServiceProvider.propTypes = {
   children: PropTypes.node.isRequired,
 }
