0ab5d46903b6419e2be4f394fab18ee5b22eeb08
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-02-21 12:55:23 -0800

    MixinDisableAndDestroy (#1631)

---
+++ smart-contracts/contracts/mixins/MixinDisableAndDestroy.sol
@@ -0,0 +1,67 @@
+pragma solidity 0.4.25;
+
+import '../interfaces/IERC721.sol';
+import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
+
+
+/**
+ * @title Mixin allowing the Lock owner to disable a Lock (preventing new purchases)
+ * and then destroy it.
+ * @author HardlyDifficult
+ * @dev `Mixins` are a design pattern seen in the 0x contracts.  It simply 
+ * separates logically groupings of code to ease readability. 
+ */
+contract MixinDisableAndDestroy is
+  IERC721,
+  Ownable
+{
+  // Used to disable payable functions when deprecating an old lock
+  bool public isAlive; 
+
+  event Destroy(
+    uint balance,
+    address indexed owner
+  );
+
+  event Disable();
+
+  constructor(
+  ) internal
+  {
+    isAlive = true;
+  }
+
+  // Only allow usage when contract is Alive
+  modifier onlyIfAlive() {
+    require(isAlive, 'LOCK_DEPRECATED');
+    _;
+  }
+
+  /**
+  * @dev Used to disable lock before migrating keys and/or destroying contract
+   */
+  function disableLock()
+    external
+    onlyOwner
+    onlyIfAlive
+  {
+    emit Disable();
+    isAlive = false;
+  }
+
+  /**
+  * @dev Used to clean up old lock contracts from the blockchain
+  * TODO: add a check to ensure all keys are INVALID!
+   */
+  function destroyLock()
+    external
+    onlyOwner
+  {
+    require(isAlive == false, 'DISABLE_FIRST');
+    emit Destroy(this.balance, msg.sender);
+    selfdestruct(msg.sender);
+    // Note we don't clean up the `locks` data in Unlock.sol as it should not be necessary
+    // and leaves some data behind ('Unlock.LockBalances') which may be helpful.
+  }
+
+}
