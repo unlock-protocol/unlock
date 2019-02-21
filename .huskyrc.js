a6c25c72f0a31654b49025ebef557d063375c338
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-21 20:03:26 +0100

    only running eslint on the files in the last commit (#1628)

--- .huskyrc.js
+++ .huskyrc.js
@@ -15,23 +15,31 @@ const svg2Components = {
 
 // lintStaged actually just runs formatting rules on staged files
 const lintStaged = path => {
-  return {command: 'lint-staged', path: path} 
+  return { command: "lint-staged", path: path }
 }
 
-// Run eslint on everything (used pre-push)
+// Run eslint on the files inside path for the last commit
+// It will try to amend the latest commit if possible to fix.
 const eslint = path => {
-  return { command: 'eslint --fix .', path: path }
+  return {
+    command: `git diff-tree --no-commit-id --name-only -r HEAD . | sed 's/${path}\\///' | xargs eslint --fix`,
+    path: path
+  }
 }
 
 // tasks are given a path
 module.exports = {
   hooks: {
-    "pre-push": tasks([eslint('unlock-app'),
-                       eslint('locksmith'),
-                       eslint('paywall')]),
-    "pre-commit": tasks([svg2Components, 
-                         lintStaged('unlock-app'),
-                         lintStaged('locksmith'),
-                         lintStaged('paywall')])
+    "pre-push": tasks([
+      eslint("unlock-app"),
+      eslint("locksmith"),
+      eslint("paywall")
+    ]),
+    "pre-commit": tasks([
+      svg2Components,
+      lintStaged("unlock-app"),
+      lintStaged("locksmith"),
+      lintStaged("paywall")
+    ])
   }
 }
