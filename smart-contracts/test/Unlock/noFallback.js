9cd215753aa4238898919589b2d2ebb534a8ae51
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-02-21 14:56:22 -0800

    No fallback function (#1604)

---
+++ smart-contracts/test/Unlock/noFallback.js
@@ -0,0 +1,29 @@
+const shouldFail = require('../helpers/shouldFail')
+const Unlock = artifacts.require('../Unlock.sol')
+const abi = new require('web3-eth-abi').AbiCoder()
+
+let unlock
+
+contract('Unlock', accounts => {
+  before(async () => {
+    unlock = await Unlock.deployed()
+  })
+
+  describe('noFallback', () => {
+    it('cannot call the fallback function directly', async () => {
+      await shouldFail(unlock.sendTransaction(), 'NO_FALLBACK')
+    })
+
+    it('does not accept ETH directly', async () => {
+      await shouldFail(unlock.send(1))
+    })
+
+    it('can call a function by name', async () => {
+      await unlock.sendTransaction({data: abi.encodeFunctionSignature('totalDiscountGranted()')})
+    })
+
+    it('cannot call a function which does not exist', async () => {
+      await shouldFail(unlock.sendTransaction({data: abi.encodeFunctionSignature('dne()')}), 'NO_FALLBACK')
+    })
+  })
+})
