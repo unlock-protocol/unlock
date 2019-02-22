fc155cb859e408a7ebe8ca000e31021a26b7f05f
Christopher Nascone <cnasc@users.noreply.github.com>
2019-02-22 10:58:57 -0500

    Add transaction log page (#1596)
    * Initial version of log page, can fetch transaction data with react hook
    
    * It works now
    
    * fetchTransactions now more consistent with itself
    
    * Clean up the fetch more
    
    * Nice looking table that auto-sizes
    
    * Remove console
    
    * Fix css after prettify messed it up
    
    * Consistent row height
    
    * Remove hook, start using Redux state for transactions
    
    * Fix lint errors
    
    * Type of transaction now shown in a nicely readable format
    
    * Get the transactions back sorted
    
    * Update to use chain explorer from config
    
    * Test log page logic
    
    * Adding story
    
    * Remove unused imports
    
    * Make requested changes
    
    * Make requested changes
    
    * Update story to use config

---
+++ unlock-app/src/__tests__/pages/log.test.js
@@ -0,0 +1,66 @@
+import { mapStateToProps, humanize } from '../../pages/log'
+import { TRANSACTION_TYPES } from '../../constants'
+import configure from '../../config'
+
+const transactions = {
+  '0x1234': {
+    hash: '0x12345678',
+    confirmations: 12,
+    status: 'mined',
+    lock: '0x12345678a',
+    blockNumber: 1,
+    type: TRANSACTION_TYPES.LOCK_CREATION,
+  },
+  '0x5678': {
+    hash: '0x56781234',
+    confirmations: 4,
+    status: 'mined',
+    lock: '0x56781234a',
+    blockNumber: 2,
+    type: TRANSACTION_TYPES.LOCK_CREATION,
+  },
+  '0x89ab': {
+    hash: '0x9abcdef0',
+    confirmations: 2,
+    status: 'mined',
+    lock: '0x9abcdef0a',
+    blockNumber: 3,
+    type: TRANSACTION_TYPES.LOCK_CREATION,
+  },
+}
+
+describe('Transaction Log', () => {
+  describe('humanize - make transaction types more readable', () => {
+    it('should correctly handle transaction types of various lengths', () => {
+      expect.assertions(3)
+      expect(humanize(TRANSACTION_TYPES.LOCK_CREATION)).toEqual('Lock Creation')
+      expect(humanize(TRANSACTION_TYPES.WITHDRAWAL)).toEqual('Withdrawal')
+      expect(humanize(TRANSACTION_TYPES.UPDATE_KEY_PRICE)).toEqual(
+        'Update Key Price'
+      )
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    const state = {
+      account: {},
+      network: {},
+      transactions,
+    }
+    const config = configure()
+    const { transactionFeed } = mapStateToProps(state, { config })
+    it('Should provide a feed of transactions sorted by blockNumber, descending', () => {
+      expect.assertions(4)
+      expect(transactionFeed).toHaveLength(3)
+      expect(transactionFeed[0].blockNumber).toEqual(3)
+      expect(transactionFeed[1].blockNumber).toEqual(2)
+      expect(transactionFeed[2].blockNumber).toEqual(1)
+    })
+    it('should include href to explorer and readable name in the feed', () => {
+      expect.assertions(2)
+      const tx = transactionFeed[0]
+      expect(tx).toHaveProperty('href')
+      expect(tx).toHaveProperty('readableName')
+    })
+  })
+})
---
+++ unlock-app/src/stories/creator/Log.stories.js
@@ -0,0 +1,74 @@
+import { Provider } from 'react-redux'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Log from '../../pages/log'
+import createUnlockStore from '../../createUnlockStore'
+import { TRANSACTION_TYPES } from '../../constants'
+import { ConfigContext } from '../../utils/withConfig'
+
+const account = {
+  address: '0x3ca206264762caf81a8f0a843bbb850987b41e16',
+}
+const network = {
+  name: 1984,
+}
+const transactions = {
+  '0x1234': {
+    hash: '0x12345678',
+    confirmations: 12,
+    status: 'mined',
+    lock: '0x8DE3f95E2efd3B9704ccb0d0925EC951bC78cb8B',
+    blockNumber: 1,
+    type: TRANSACTION_TYPES.LOCK_CREATION,
+  },
+  '0x5678': {
+    hash: '0x56781234',
+    confirmations: 4,
+    status: 'mined',
+    lock: '0x8DE3f95E2efd3B9704ccb0d0925EC951bC78cb8B',
+    blockNumber: 2,
+    type: TRANSACTION_TYPES.UPDATE_KEY_PRICE,
+  },
+  '0x89ab': {
+    hash: '0x9abcdef0',
+    confirmations: 2,
+    status: 'mined',
+    lock: '0xc43efE2C7116CB94d563b5A9D68F260CCc44256F',
+    blockNumber: 3,
+    type: TRANSACTION_TYPES.LOCK_CREATION,
+  },
+}
+
+const router = {
+  route: '/dashboard',
+}
+
+const currency = {
+  USD: 195.99,
+}
+
+const store = createUnlockStore({
+  account,
+  network,
+  router,
+  transactions,
+  currency,
+})
+
+const ConfigProvider = ConfigContext.Provider
+
+const config = {
+  chainExplorerUrlBuilders: {
+    etherScan: () => '',
+  },
+}
+
+storiesOf('Transaction Log', module)
+  .addDecorator(getStory => (
+    <ConfigProvider value={config}>{getStory()}</ConfigProvider>
+  ))
+  .add('the log', () => (
+    <Provider store={store}>
+      <Log />
+    </Provider>
+  ))
