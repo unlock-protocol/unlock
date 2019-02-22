b1f8b0df0c54b348dd557dd59a4604cbf86f6f71
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-22 18:31:49 +0100

    deploy.sh does not use env variables but arguments (#1664)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,26 +5,29 @@
 # The second argument indicates if the build is for staging or production
 
 TARGET=$1
+COMMIT=$2
+TAG=$3
+BRANCH=$4
+IS_PULL_REQUEST=$5
 ENV_TARGET="staging"
 NPM_SCRIPT="npm run deploy-$TARGET"
 SERVICE="unlock"
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
-TRAVIS_ENV_VARS=`env | grep TRAVIS_ | awk '{print "-e ",$1}' ORS=' '`
 
 # Identify the environment for the target (production or staging)
-if [ -n "$TRAVIS_TAG" ] &&
-   [ -n "$TRAVIS_COMMIT" ] &&
-   [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  # Check that the TRAVIS_COMMIT is actually on the master branch to avoid deploying tags which are not on master
+if [ -n "$TAG" ] &&
+   [ -n "$COMMIT" ] &&
+   [ "$IS_PULL_REQUEST" = "false" ]; then
+  # Check that the COMMIT is actually on the master branch to avoid deploying tags which are not on master
   git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
   git fetch >> /dev/null
-  git branch -r --contains $TRAVIS_COMMIT | grep 'master' >> /dev/null
+  git branch -r --contains $COMMIT | grep 'master' >> /dev/null
   if [ $? -eq 0 ]; then
     # This is a tag build on master. We deploy to the main site!
     ENV_TARGET="prod"
   else
-    echo "Skipping deployment because commit $TRAVIS_COMMIT for tag $TRAVIS_TAG is not on master"
+    echo "Skipping deployment because commit $COMMIT for tag $TAG is not on master"
     exit 0
   fi
 else
@@ -42,7 +45,7 @@ fi
 
 # Extracting environment variables
 DEPLOYMENT_ENV_VARS=`env | grep $ENV_PREFIX | awk '{print "-e ",$1}' ORS=' ' | sed -e "s/$ENV_PREFIX//g"`
-ENV_VARS="$TRAVIS_ENV_VARS $DEPLOYMENT_ENV_VARS"
+ENV_VARS="$DEPLOYMENT_ENV_VARS"
 
 ## Custom variables passed for netlify
 if [ "$TARGET" = "netlify" ]; then
@@ -51,11 +54,11 @@ fi
 
 # PUBLISH: whether to publish/promote the deployed version
 PUBLISH="false"
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$BRANCH" = "master" ] && [ "$IS_PULL_REQUEST" = "false" ]; then
   PUBLISH="true"
 fi
 
 # Deploy options
-OPTS="$ENV_TARGET $TRAVIS_COMMIT $PUBLISH"
+OPTS="$ENV_TARGET $COMMIT $PUBLISH"
 
 docker-compose -f $DOCKER_COMPOSE_FILE run $ENV_VARS $SERVICE $NPM_SCRIPT -- $OPTS
