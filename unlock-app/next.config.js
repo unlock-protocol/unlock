12a42fd32395c6b9d49230d311bb6978ee94481f
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-02-22 11:46:37 -0500

    add new paywallUrl configuration variable (#1661)

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -9,6 +9,7 @@ module.exports = withSourceMaps({
   publicRuntimeConfig: {
     unlockEnv: process.env.UNLOCK_ENV || 'dev',
     httpProvider: process.env.HTTP_PROVIDER || '127.0.0.1',
+    paywallUrl: process.env.PAYWALL_URL,
     readOnlyProvider: process.env.READ_ONLY_PROVIDER,
     locksmithHost: process.env.LOCKSMITH_URI,
   },
