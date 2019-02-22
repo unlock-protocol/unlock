341d201f19f108dd2f7beb946255f312c9241112
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-22 16:54:07 +0100

    updated the build script (#1657)

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,7 +2,7 @@
 
 # This script builds the corresponding images
 # The argument is the image name
-# When a DOCKER_REPOSITORY is available we will first pull it from AWS where the cached images are stored
+# When a DOCKER_REPOSITORY is available we will first pull it from there.
 
 IMAGE_NAME=$1
 REPO_ROOT=`dirname "$0"`/..
@@ -10,11 +10,11 @@ DOCKERFILE=$REPO_ROOT/docker/$IMAGE_NAME.dockerfile
 ARGS=""
 
 if [ "$DOCKER_REPOSITORY" != "" ]; then
-  IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
+  IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:master"
   echo "Pulling $IMAGE_CACHE to use as cache for $IMAGE_NAME"
   docker pull $IMAGE_CACHE;
 else
-  IMAGE_CACHE="$IMAGE_NAME:latest"
+  IMAGE_CACHE="$IMAGE_NAME:master"
 fi
 
 ARGS="$ARGS --cache-from $IMAGE_CACHE"
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -4,13 +4,10 @@
 # This should only run on master merges
 
 IMAGE_NAME=$1
-ARGS=""
+IMAGE_TAG=$2
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  if [ "$DOCKER_REPOSITORY" != "" ]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
-    docker tag "$IMAGE_NAME:latest" $IMAGE_CACHE
-    docker push $IMAGE_CACHE
-  fi
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+IMAGE_TO_PUSH="$DOCKER_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG"
+docker tag $IMAGE_NAME $IMAGE_TO_PUSH
+docker push $IMAGE_TO_PUSH
+
