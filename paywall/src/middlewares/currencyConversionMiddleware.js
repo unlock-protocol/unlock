e59c41c158b563b58cacae6a6340b247b713c66e
Gregory Beaver <greg@unlock-protocol.com>
2019-02-23 16:28:17 -0500

    don't poll on the server, stop polling when browser is offline

--- paywall/src/middlewares/currencyConversionMiddleware.js
+++ paywall/src/middlewares/currencyConversionMiddleware.js
@@ -4,6 +4,10 @@
 import axios from 'axios'
 
 import { setConversionRate } from '../actions/currencyConvert'
+import pollWithConditions from '../utils/polling'
+import configure from '../config'
+
+const config = configure()
 
 export default store => {
   axios
@@ -13,18 +17,28 @@ export default store => {
         setConversionRate(info.data.data.currency, info.data.data.amount)
       )
     )
-  setInterval(() => {
-    axios.get('https://api.coinbase.com/v2/prices/ETH-USD/buy').then(info => {
-      const {
-        data: {
-          data: { currency, amount },
-        },
-      } = info
-      const current = store.getState().currency[currency]
-      if (current === +amount) return
+  pollWithConditions(
+    () => {
+      axios.get('https://api.coinbase.com/v2/prices/ETH-USD/buy').then(info => {
+        const {
+          data: {
+            data: { currency, amount },
+          },
+        } = info
+        const current = store.getState().currency[currency]
+        if (current === +amount) return
 
-      store.dispatch(setConversionRate(currency, amount))
-    })
-  }, 10000)
+        store.dispatch(setConversionRate(currency, amount))
+      })
+    },
+    10000,
+    () => {
+      if (config.isServer) {
+        throw new Error('currency conversion polling is only on the client')
+      }
+      // temporarily stop polling when offline
+      return !window.navigator.isOnline
+    }
+  )
   return next => action => next(action)
 }
