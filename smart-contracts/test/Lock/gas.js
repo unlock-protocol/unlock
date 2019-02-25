314a52dbcd16c9a818e729748f13e048dbe410b6
hardlydifficult <hardlydifficultllc@gmail.com>
2019-02-25 09:57:07 -0800

    Adding basic gas tests

---
+++ smart-contracts/test/Lock/gas.js
@@ -0,0 +1,68 @@
+const Units = require('ethereumjs-units')
+const Web3Utils = require('web3-utils')
+const BigNumber = require('bignumber.js')
+
+const deployLocks = require('../helpers/deployLocks')
+const Unlock = artifacts.require('../Unlock.sol')
+
+let unlock, lock
+
+contract('Unlock', (accounts) => {
+  before(async () => {
+    unlock = await Unlock.deployed()
+    const locks = await deployLocks(unlock)
+    lock = locks['FIRST']
+  })
+
+  describe('gas usage', () => {
+    it('^ gas used to purchaseFor w/o key data', async () => {
+      let tx = await lock
+        .purchaseFor(accounts[0], '', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      console.log(gasUsed.toFormat())
+      // If this breaks, update unlock-app/src/services/walletService.js gasAmountConstants
+      assert(gasUsed.lte(1000000))
+    })
+
+    it('^ gas used to purchaseFor w/ key data', async () => {
+      let tx = await lock
+        .purchaseFor(accounts[0], Web3Utils.toHex('Julien'), {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      console.log(gasUsed.toFormat())
+      // If this breaks, update unlock-app/src/services/walletService.js gasAmountConstants
+      assert(gasUsed.lte(1000000))
+    })
+
+    it('^ gas used to transferFrom w/o key data', async () => {
+      await lock
+        .purchaseFor(accounts[0], '', {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+      let tx = await lock.transferFrom(accounts[0], accounts[1],
+        await lock.getTokenIdFor.call(accounts[0]), {
+          from: accounts[0]
+        })
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      console.log(gasUsed.toFormat())
+      assert(gasUsed.lte(1000000))
+    })
+
+    it('^ gas used to transferFrom w key data', async () => {
+      await lock
+        .purchaseFor(accounts[2], Web3Utils.toHex('Julien'), {
+          value: Units.convert('0.01', 'eth', 'wei')
+        })
+      let tx = await lock.transferFrom(accounts[2], accounts[1],
+        await lock.getTokenIdFor.call(accounts[2]), {
+          from: accounts[2]
+        })
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      console.log(gasUsed.toFormat())
+      assert(gasUsed.lte(1000000))
+    })
+  })
+})
---
+++ smart-contracts/test/Unlock/gas.js
@@ -0,0 +1,27 @@
+const Units = require('ethereumjs-units')
+const BigNumber = require('bignumber.js')
+const Unlock = artifacts.require('../Unlock.sol')
+
+let unlock
+
+contract('Unlock', (accounts) => {
+  before(async () => {
+    unlock = await Unlock.deployed()
+  })
+
+  describe('gas usage', () => {
+    it('^ gas used to createLock', async () => {
+      let tx = await unlock.createLock(
+        60 * 60 * 24 * 30, // expirationDuration: 30 days
+        Units.convert(1, 'eth', 'wei'), // keyPrice: in wei
+        100 // maxNumberOfKeys
+        , {
+          from: accounts[0]
+        })
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      console.log(gasUsed.toFormat())
+      // If this breaks, update unlock-app/src/services/walletService.js gasAmountConstants
+      assert(gasUsed.lte(2500000))
+    })
+  })
+})
