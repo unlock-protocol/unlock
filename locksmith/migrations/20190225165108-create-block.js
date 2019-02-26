b7b0f3589f12b368b914e6226041528c88f2fd9a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-02-26 10:40:42 -0500

    Migration associated with capturing Block mined times

---
+++ locksmith/migrations/20190225165108-create-block.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Blocks', {
+      timestamp: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      hash: {
+        type: Sequelize.STRING
+      },
+      number: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      chain: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Blocks');
+  }
+};
