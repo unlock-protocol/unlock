8546bdeb93da7c10568be81ca0edcb78944c1cee
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-27 01:22:25 +0100

    moving jest to the root of the repo (#1719)

--- unlock-app/jest.config.js
+++ unlock-app/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   setupFiles: ['<rootDir>/.jest/register-context.js'],
-  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
     '<rootDir>/src/__tests__/utils/fixtures/',
   ],
   transform: {
-    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
 }
