715fb47c12dee38f0b889111759a3f8417693a3b
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-02-27 15:45:02 -0800

    Adding a Lock name field (#1672)

--- smart-contracts/contracts/interfaces/IERC721.sol
+++ smart-contracts/contracts/interfaces/IERC721.sol
@@ -99,4 +99,7 @@ interface IERC721 {
   /// @param _operator The address that acts on behalf of the owner
   /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
   function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+
+  /// @notice A descriptive name for a collection of NFTs in this contract
+  function name() external view returns (string memory _name); 
 }
---
+++ smart-contracts/contracts/mixins/MixinLockMetadata.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.4.25;
+
+import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
+import '../interfaces/IERC721.sol';
+
+
+/**
+ * @title Mixin for metadata about the Lock.
+ * @author HardlyDifficult
+ * @dev `Mixins` are a design pattern seen in the 0x contracts.  It simply 
+ * separates logically groupings of code to ease readability. 
+ */
+contract MixinLockMetadata is
+  IERC721,
+  Ownable
+{
+  /// A descriptive name for a collection of NFTs in this contract
+  string private lockName;
+
+  /**
+   * Allows the Lock owner to assign a descriptive name for this Lock.
+   */
+  function updateLockName(
+    string _lockName
+  ) external
+    onlyOwner
+  {
+    lockName = _lockName;
+  }
+
+  /**
+    * @dev Gets the token name
+    * @return string representing the token name
+    */
+  function name(
+  ) external view 
+    returns (string memory)
+  {
+    return lockName;
+  }
+}
