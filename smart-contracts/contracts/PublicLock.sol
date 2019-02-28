4465cb25d8bf06012fa683c75dd228e37541ae54
hardlydifficult <hardlydifficultllc@gmail.com>
2019-02-28 12:59:00 -0800

    MixinPurchase

--- smart-contracts/contracts/PublicLock.sol
+++ smart-contracts/contracts/PublicLock.sol
@@ -1,11 +1,9 @@
 pragma solidity 0.4.25;
 
-import './interfaces/IUnlock.sol';
 import './interfaces/IERC721.sol';
 import './interfaces/ILockCore.sol';
 import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/introspection/ERC165.sol';
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol';
 import './mixins/MixinApproval.sol';
 import './mixins/MixinDisableAndDestroy.sol';
@@ -15,6 +13,7 @@ import './mixins/MixinRefunds.sol';
 import './mixins/MixinLockMetadata.sol';
 import './mixins/MixinNoFallback.sol';
 import './mixins/MixinTransfer.sol';
+import './mixins/MixinPurchase.sol';
 
 /**
  * @title The Lock contract
@@ -37,13 +36,9 @@ contract PublicLock is
   MixinLockCore,
   MixinLockMetadata,
   MixinRefunds,
+  MixinPurchase,
   MixinTransfer
 {
-  using SafeMath for uint;
-
-  // Events
-  // Fields
-  // Constructor
   constructor(
     address _owner,
     uint _expirationDuration,
@@ -57,119 +52,5 @@ contract PublicLock is
     // registering the interface for erc721 with ERC165.sol using
     // the ID specified in the standard: https://eips.ethereum.org/EIPS/eip-721
     _registerInterface(0x80ac58cd);
-  }
-
-  /**
-  * @dev Purchase function, public version, with no referrer.
-  * @param _recipient address of the recipient of the purchased key
-  * @param _data optional marker for the key
-  */
-  function purchaseFor(
-    address _recipient,
-    bytes _data
-  )
-    external
-    payable
-    onlyIfAlive
-  {
-    return _purchaseFor(_recipient, address(0), _data);
-  }
-
-  /**
-  * @dev Purchase function, public version, with referrer.
-  * @param _recipient address of the recipient of the purchased key
-  * @param _referrer address of the user making the referral
-  * @param _data optional marker for the key
-  */
-  function purchaseForFrom(
-    address _recipient,
-    address _referrer,
-    bytes _data
-  )
-    external
-    payable
-    onlyIfAlive
-    hasValidKey(_referrer)
-  {
-    return _purchaseFor(_recipient, _referrer, _data);
-  }
-
-  /**
-  * @dev Purchase function: this lets a user purchase a key from the lock for another user
-  * @param _recipient address of the recipient of the purchased key
-  * @param _data optional marker for the key
-  * This will fail if
-  *  - the keyReleaseMechanism is private
-  *  - the keyReleaseMechanism is Approved and the recipient has not been previously approved
-  *  - the amount value is smaller than the price
-  *  - the recipient already owns a key
-  * TODO: next version of solidity will allow for message to be added to require.
-  */
-  function _purchaseFor(
-    address _recipient,
-    address _referrer,
-    bytes memory _data
-  )
-    internal
-    notSoldOut()
-  { // solhint-disable-line function-max-lines
-    require(_recipient != address(0), 'INVALID_ADDRESS');
-
-    // Let's get the actual price for the key from the Unlock smart contract
-    IUnlock unlock = IUnlock(unlockProtocol);
-    uint discount;
-    uint tokens;
-    uint inMemoryKeyPrice = keyPrice;
-    (discount, tokens) = unlock.computeAvailableDiscountFor(_recipient, inMemoryKeyPrice);
-    uint netPrice = inMemoryKeyPrice;
-    if (discount > inMemoryKeyPrice) {
-      netPrice = 0;
-    } else {
-      // SafeSub not required as the if statement already confirmed `inMemoryKeyPrice - discount` cannot underflow
-      netPrice = inMemoryKeyPrice - discount;
-    }
-
-    // We explicitly allow for greater amounts to allow 'donations' or partial refunds after
-    // discounts (TODO implement partial refunds)
-    require(msg.value >= netPrice, 'NOT_ENOUGH_FUNDS');
-    // TODO: If there is more than the required price, then let's return whatever is extra
-    // extra (CAREFUL: re-entrancy!)
-
-    // Assign the key
-    Key storage toKey = _getKeyFor(_recipient);
-    uint previousExpiration = toKey.expirationTimestamp;
-    if (previousExpiration < block.timestamp) {
-      if (previousExpiration == 0) {
-        // This is a brand new owner, else an owner of an expired key buying an extension.
-        // We increment the tokenId counter
-        numberOfKeysSold++;
-        _addNewOwner(_recipient);
-        // We register the owner of the new tokenID
-        _setKeyOwner(numberOfKeysSold, _recipient);
-        // we assign the incremented `numberOfKeysSold` as the tokenId for the new key
-        toKey.tokenId = numberOfKeysSold;
-      }
-      // SafeAdd is not required here since expirationDuration is capped to a tiny value
-      // (relative to the size of a uint)
-      toKey.expirationTimestamp = block.timestamp + expirationDuration;
-    } else {
-      // This is an existing owner trying to extend their key
-      toKey.expirationTimestamp = previousExpiration.add(expirationDuration);
-    }
-    // Overwite data in all cases
-    toKey.data = _data;
-
-    if (discount > 0) {
-      unlock.recordConsumedDiscount(discount, tokens);
-    }
-
-    unlock.recordKeyPurchase(netPrice, _referrer);
-
-    // trigger event
-    emit Transfer(
-      address(0), // This is a creation.
-      _recipient,
-      numberOfKeysSold
-    );
   }
 }
