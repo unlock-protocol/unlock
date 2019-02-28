c8a0d3941e1205fc8c0414695de091138829304c
Gregory Beaver <greg@unlock-protocol.com>
2019-02-28 12:37:11 -0500

    enable storybook on the paywall (#1778)
    * enable storybook in paywall
    
    * Update addons.js
    
    * enable storyshot snapshots

---
+++ paywall/.storybook/config.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import StoryRouter from 'storybook-react-router'
+import GlobalStyle from '../src/theme/globalStyle'
+import Fonts from '../src/theme/fonts'
+
+import { setConfig } from 'next/config'
+
+setConfig({
+  publicRuntimeConfig: {
+    unlockEnv: 'test',
+  },
+})
+
+const req = require.context('../stories', true, /\.stories\.js$/)
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+const GlobalStyleDecorator = storyFn => (
+  <React.Fragment>
+    <Fonts />
+    <GlobalStyle />
+    {storyFn()}
+  </React.Fragment>
+)
+
+addDecorator(GlobalStyleDecorator)
+addDecorator(StoryRouter())
+configure(loadStories, module)
