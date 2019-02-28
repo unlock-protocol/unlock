716cdb8cccbaed26cd5e6e426e50ba21e59d113b
hardlydifficult <hardlydifficultllc@gmail.com>
2019-02-28 15:37:17 -0800

    Adding approve test cases and fixed a couple bugs

--- smart-contracts/test/Lock/erc721/approveForAll.js
+++ smart-contracts/test/Lock/erc721/approveForAll.js
@@ -71,27 +71,106 @@ contract('Lock ERC721', accounts => {
           assert.equal(event.args._approved, true)
         })
 
-        it('should allow the approved user to transferFrom', async () => {
-          await lock.transferFrom(owner, accounts[2], ID, {
+        it('an authorized operator may set the approved address for an NFT', async () => {
+          let newApprovedUser = accounts[8]
+
+          await lock.approve(newApprovedUser, ID, {
             from: approvedUser
+          })
+
+          assert.equal(await lock.getApproved.call(ID), newApprovedUser)
+        })
+
+        it('should allow the approved user to transferFrom', async () => {
+          await lock.transferFrom(owner, accounts[3], ID, {
+            from: approvedUser
+          })
+
+          // Transfer it back to the original owner for other tests
+          await lock.transferFrom(accounts[3], owner, ID, {
+            from: accounts[3]
+          })
+        })
+
+        it('isApprovedForAll is still true (not lost after transfer)', async () => {
+          assert.equal(
+            await lock.isApprovedForAll.call(owner, approvedUser),
+            true
+          )
+        })
+
+        describe('allows for multiple operators per owner', () => {
+          let newApprovedUser = accounts[8]
+
+          before(async () => {
+            await lock.setApprovalForAll(newApprovedUser, true, {
+              from: owner
+            })
+          })
+
+          it('new operator is approved', async () => {
+            assert.equal(
+              await lock.isApprovedForAll.call(owner, newApprovedUser),
+              true
+            )
+          })
+
+          it('original operator is still approved', async () => {
+            assert.equal(
+              await lock.isApprovedForAll.call(owner, approvedUser),
+              true
+            )
           })
         })
       })
 
       describe('can cancel an outstanding approval', () => {
+        let event
+
         before(async () => {
           await lock.setApprovalForAll(approvedUser, true, {
             from: owner
           })
-          await lock.setApprovalForAll(approvedUser, false, {
+          let result = await lock.setApprovalForAll(approvedUser, false, {
             from: owner
           })
+          event = result.logs[0]
         })
 
         it('isApprovedForAll is false again', async () => {
           assert.equal(
             await lock.isApprovedForAll.call(owner, approvedUser),
             false
+          )
+        })
+
+        it('This emits when an operator is (enabled or) disabled for an owner.', async () => {
+          assert.equal(event.event, 'ApprovalForAll')
+          assert.equal(event.args._owner, owner)
+          assert.equal(event.args._operator, approvedUser)
+          assert.equal(event.args._approved, false)
+        })
+      })
+    })
+
+    describe('when the owner does not have a key', () => {
+      let ownerWithoutAKey = accounts[7]
+
+      it('owner has no keys', async () => {
+        assert.equal(await lock.balanceOf(ownerWithoutAKey), 0)
+      })
+
+      describe('allows the owner to call approveForAll', () => {
+        before(async () => {
+          await lock.setApprovalForAll(approvedUser, true, {
+            from: ownerWithoutAKey
+          })
+        })
+
+        it('operator is approved', async () => {
+          assert.equal(
+            await lock.isApprovedForAll.call(ownerWithoutAKey, approvedUser),
+            true
           )
         })
       })
