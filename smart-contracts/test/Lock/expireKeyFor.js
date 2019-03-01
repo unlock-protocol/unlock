fd88912e154408c73ba333da76f0d87c333c57c4
Hardly Difficult <hardlydifficultllc@gmail.com>
2019-03-01 11:26:32 -0800

    Add ExpireKey event (#1798)

--- smart-contracts/test/Lock/expireKeyFor.js
+++ smart-contracts/test/Lock/expireKeyFor.js
@@ -48,20 +48,36 @@ contract('Lock', (accounts) => {
       }), 'KEY_NOT_VALID')
     })
 
-    it('should expire a valid key', async () => {
-      await locks['FIRST'].purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
-        value: locks['FIRST'].params.keyPrice.toFixed(),
-        from: accounts[0]
+    describe('should expire a valid key', () => {
+      let ID
+      let event
+
+      before(async () => {
+        await locks['FIRST'].purchaseFor(accounts[1], Web3Utils.toHex('Julien'), {
+          value: locks['FIRST'].params.keyPrice.toFixed(),
+          from: accounts[0]
+        })
+        ID = await locks['FIRST'].getTokenIdFor(accounts[1])
+        const expirationTimestamp = new BigNumber(await locks['FIRST'].keyExpirationTimestampFor.call(accounts[1]))
+        const now = Math.floor(new Date().getTime() / 1000)
+        // Pre-condition
+        assert(expirationTimestamp.gt(now))
+        const result = await locks['FIRST'].expireKeyFor(accounts[1], {
+          from: accounts[0]
+        })
+        event = result.logs[0]
       })
-      let expirationTimestamp = new BigNumber(await locks['FIRST'].keyExpirationTimestampFor.call(accounts[1]))
-      let now = Math.floor(new Date().getTime() / 1000)
-      assert(expirationTimestamp.gt(now))
-      await locks['FIRST'].expireKeyFor(accounts[1], {
-        from: accounts[0]
+
+      it('should expire a valid key', async () => {
+        const expirationTimestamp = new BigNumber(await locks['FIRST'].keyExpirationTimestampFor.call(accounts[1]))
+        const now = Math.floor(new Date().getTime() / 1000)
+        assert(expirationTimestamp.lte(now))
       })
-      expirationTimestamp = new BigNumber(await locks['FIRST'].keyExpirationTimestampFor.call(accounts[1]))
-      now = Math.floor(new Date().getTime() / 1000)
-      assert(expirationTimestamp.lte(now))
+
+      it('should emit an ExpireKey event', async () => {
+        assert.equal(event.event, 'ExpireKey')
+        assert.equal(event.args.tokenId.toString(), ID)
+      })
     })
   })
 })
