efd8d46176203bbd3e1e5865eca4cfc9180ec3d2
Gregory Beaver <greg@unlock-protocol.com>
2019-02-28 22:31:20 -0500

    Copy relevant stories over from unlock-app (#1780)
    * copy stories over from unlock-app that are relevant to detect UI changes in the paywall
    
    * add snapshots

---
+++ paywall/stories/lock/Lock.stories.js
@@ -0,0 +1,198 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Provider } from 'react-redux'
+import { Lock } from '../../src/components/lock/Lock'
+import createUnlockStore from '../../src/createUnlockStore'
+import { ConfigContext } from '../../src/utils/withConfig'
+import { UNLIMITED_KEYS_COUNT } from '../../src/constants'
+
+// lock, account, keys, purchaseKey
+const purchaseKey = () => {}
+const config = {
+  requiredConfirmations: 3,
+}
+
+const lock = {
+  address: '0x123',
+  name: 'Monthly',
+  keyPrice: '0.23',
+  fiatPrice: 240.38,
+  expirationDuration: 2592000,
+}
+
+const soldOutLock = Object.assign(
+  { maxNumberOfKeys: 1, outstandingKeys: 1 },
+  lock
+)
+
+const store = createUnlockStore({
+  currency: {
+    USD: 195.99,
+  },
+})
+
+const ConfigProvider = ConfigContext.Provider
+
+const storyConfig = {
+  requiredConfirmations: 12,
+}
+
+storiesOf('Lock', module)
+  .addDecorator(getStory => (
+    <ConfigProvider value={storyConfig}>{getStory()}</ConfigProvider>
+  ))
+  .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
+  .add('with no key (check hover state too)', () => {
+    return (
+      <Lock
+        lock={lock}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('disabled - another lock has a pending key', () => {
+    return (
+      <Lock
+        disabled
+        lock={lock}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('disabled - no keys left to sell', () => {
+    return (
+      <Lock
+        lock={soldOutLock}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('disabled - too expensive for current user', () => {
+    const account = {
+      balance: lock.keyPrice,
+    }
+    return (
+      <Lock
+        account={account}
+        lock={soldOutLock}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('with a pending key (not yet mined)', () => {
+    const k = {
+      lock: lock.address,
+    }
+    const t = {
+      status: 'submitted',
+    }
+    return (
+      <Lock
+        lock={lock}
+        transaction={t}
+        lockKey={k}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('with a mined key (which was not confirmed).', () => {
+    const k = {
+      lock: lock.address,
+    }
+    const t = {
+      status: 'mined',
+      confirmations: config.requiredConfirmations - 1,
+    }
+    return (
+      <Lock
+        lock={lock}
+        transaction={t}
+        lockKey={k}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('with a mined key.', () => {
+    const k = {
+      lock: lock.address,
+    }
+    const t = {
+      status: 'mined',
+      confirmations: config.requiredConfirmations + 1,
+    }
+    return (
+      <Lock
+        lock={lock}
+        transaction={t}
+        lockKey={k}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('with a balance', () => {
+    const lockWithBalance = {
+      address: '0x123',
+      name: 'Monthly',
+      keyPrice: '0.23',
+      fiatPrice: 240.38,
+      expirationDuration: 2592000,
+      balance: '5',
+    }
+    return (
+      <Lock
+        lock={lockWithBalance}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
+  .add('with an unlinited number of keys', () => {
+    const lockWithInfiniteNumberOfKeys = Object.assign(
+      { maxNumberOfKeys: UNLIMITED_KEYS_COUNT, outstandingKeys: 4 },
+      lock
+    )
+    return (
+      <Lock
+        lock={lockWithInfiniteNumberOfKeys}
+        transaction={null}
+        lockKey={null}
+        purchaseKey={purchaseKey}
+        config={config}
+        hideModal={() => {}}
+        openInNewWindow={false}
+      />
+    )
+  })
