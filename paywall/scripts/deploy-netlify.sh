f79cb795c56d144fe8146b74268d71774f7ba059
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-03 17:45:55 -0500

    the deploy.sh script can now be used for any service (#1800)

--- paywall/scripts/deploy-netlify.sh
+++ paywall/scripts/deploy-netlify.sh
@@ -4,12 +4,11 @@
 # It requires NETLIFY_AUTH_TOKEN to be set, as well as
 # is production in the netlify sense, so it could be on staging.unlock-protocol.com).
 
-CURRENT_DIR=`pwd`;
-UNLOCK_APP_PATH="unlock-app";
-BUILD_PATH="$UNLOCK_APP_PATH/src/out/";
-DEPLOY_ENV=$1
-COMMIT=$2
-PUBLISH=$3
+APP_PATH=$1
+DEPLOY_ENV=$2
+COMMIT=$3
+PUBLISH=$4
+BUILD_PATH="src/out/";
 
 if [ "$DEPLOY_ENV" = "staging" ]; then
   if [ "$PUBLISH" = "true" ]; then
@@ -29,12 +28,9 @@ if [ "$DEPLOY_ENV" = "prod" ]; then
 fi
 
 if [ -n "$NETLIFY_SITE_ID" ] && [ -n "$NETLIFY_AUTH_TOKEN" ]; then
-  cd $UNLOCK_APP_PATH;
   UNLOCK_ENV="$DEPLOY_ENV" npm run deploy;
-  cd $CURRENT_DIR
   echo $MESSAGE
   netlify deploy -s $NETLIFY_SITE_ID --dir=$BUILD_PATH $PROD --message='$MESSAGE'
 else
   echo "Skipping deployment to Netlify ($NETLIFY_SITE_ID)"
 fi
-
