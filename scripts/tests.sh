360e9d6bb0938843b39c551f9ade1eeabe7d685e
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-04 15:21:38 -0500

    passing env variables to the running docker container for tests (#1864)

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,4 +7,9 @@ REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 COMMAND="npm run ci"
 
-docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true $SERVICE $COMMAND
+# We pass only the relevent env vars, which are prefixed with the service name, uppercased
+# UNLOCK_APP_X will be passed to the container for tests in unlock_app as X.
+ENV_VARS_PREFIX="${SERVICE^^}_"
+ENV_VARS=`env | grep $ENV_VARS_PREFIX | awk '{print "-e ",$1}' ORS=' ' | sed -e "s/$ENV_VARS_PREFIX//g"`
+
+docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true $ENV_VARS $SERVICE $COMMAND
