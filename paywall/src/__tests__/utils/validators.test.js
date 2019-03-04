c1c4267ee69e9ca3a7b179d9f939c2b3422247e4
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-04 13:37:39 -0500

    merge validators from unlock-app to paywall (#1860)

---
+++ paywall/src/__tests__/utils/validators.test.js
@@ -0,0 +1,41 @@
+import * as validators from '../../utils/validators'
+
+describe('Form field validators', () => {
+  it('isMissing', () => {
+    expect.assertions(6)
+    expect(validators.isNotEmpty('hi')).toBeTruthy()
+    expect(validators.isNotEmpty('0')).toBeTruthy()
+    expect(validators.isNotEmpty(0)).toBeTruthy()
+
+    expect(validators.isNotEmpty('')).toBeFalsy()
+    expect(validators.isNotEmpty(null)).toBeFalsy()
+    expect(validators.isNotEmpty(false)).toBeFalsy()
+  })
+  it('isPositiveInteger', () => {
+    expect.assertions(8)
+    expect(validators.isPositiveInteger('1')).toBeTruthy()
+    expect(
+      validators.isPositiveInteger(
+        '178941236598123465918347651983476519387456918736459813476598123645891765894765'
+      )
+    ).toBeTruthy()
+
+    expect(validators.isPositiveInteger('0')).toBeTruthy()
+    expect(validators.isPositiveInteger('-1')).toBeFalsy()
+    expect(validators.isPositiveInteger('1.1')).toBeFalsy()
+    expect(validators.isPositiveInteger('av')).toBeFalsy()
+    expect(validators.isPositiveInteger(null)).toBeFalsy()
+    expect(validators.isPositiveInteger(false)).toBeFalsy()
+  })
+  it('isPositiveNumber', () => {
+    expect.assertions(7)
+    expect(validators.isPositiveNumber('1.3')).toBeTruthy()
+    expect(validators.isPositiveNumber('0.002')).toBeTruthy()
+
+    expect(validators.isPositiveNumber('0')).toBeTruthy()
+    expect(validators.isPositiveNumber('-1')).toBeFalsy()
+    expect(validators.isPositiveNumber('av')).toBeFalsy()
+    expect(validators.isPositiveNumber(null)).toBeFalsy()
+    expect(validators.isPositiveNumber(false)).toBeFalsy()
+  })
+})
