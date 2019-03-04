7502c2fea7aa31064d10f3b11ea39d9c0e37af3c
Gregory Beaver <greg@unlock-protocol.com>
2019-03-03 20:47:13 -0500

    Refactor useListenForPostMessage (#1821)
    * refactor useListenForPostMessage
    
    * remove unneeded files
    
    * remove blank line (+4 squashed commits)
    Squashed commits:
    [aeeb214e] camelCase
    [1fdcef25] refactor scrollLoop, in order to fix and verify that correct origin is passed to postMessage
    [d7247a8f] add propTypes
    [c72a14c9] add extensive docs

--- paywall/paywall-builder/build.js
+++ paywall/paywall-builder/build.js
@@ -9,13 +9,39 @@ import {
 
 // Currently, the constraint on the banner is that it starts out at
 // 30% of height, but at least 375px
-const baseBannerHeight = () => {
+const baseBannerHeight = window => {
   const viewportHeight = window.innerHeight
   const minHeight = 375
   const minHeightPct = 100 * (minHeight / viewportHeight)
   // So here we determine which of the 2 options is the larger, so we
   // can use it as the basis to scroll from
   return minHeightPct > 30 ? minHeightPct : 30
+}
+// iOS allows the page to scroll even when the paywall is up. Our
+// solution to this is to make the paywall grow to obscure the page
+// content as the user scrolls down the page. We register the
+// scroll-handling function here to preserve the context of the actual
+// page, not the iframe.
+export const scrollLoop = (window, document, iframe, origin) => {
+  const pageTop = window.pageYOffset
+  const viewportHeight = window.innerHeight
+  const pageHeight = document.documentElement.scrollHeight
+  const maximumScroll = pageHeight - viewportHeight
+  // Avoiding a divide-by-zero error when the page does not scroll!
+  if (maximumScroll === 0) {
+    return
+  }
+
+  const scrollPosition =
+    baseBannerHeight(window) + 100 * (pageTop / maximumScroll)
+  iframe.contentWindow.postMessage(
+    { type: POST_MESSAGE_SCROLL_POSITION, payload: scrollPosition },
+    origin
+  )
+
+  window.requestAnimationFrame(() =>
+    scrollLoop(window, document, iframe, origin)
+  )
 }
 
 export function redirect(window, paywallUrl) {
@@ -42,30 +68,9 @@ export default function buildPaywall(window, document, lockAddress, blocker) {
   const iframe = getIframe(document, paywallUrlWithOrigin)
   add(document, iframe)
 
-  // iOS allows the page to scroll even when the paywall is up. Our
-  // solution to this is to make the paywall grow to obscure the page
-  // content as the user scrolls down the page. We register the
-  // scroll-handling function here to preserve the context of the actual
-  // page, not the iframe.
-  const scrollLoop = () => {
-    const pageTop = window.pageYOffset
-    const viewportHeight = window.innerHeight
-    const pageHeight = document.documentElement.scrollHeight
-    const maximumScroll = pageHeight - viewportHeight
-    // Avoiding a divide-by-zero error when the page does not scroll!
-    if (maximumScroll === 0) {
-      return
-    }
+  const origin = new window.URL(paywallUrl).origin
 
-    const scrollPosition = baseBannerHeight() + 100 * (pageTop / maximumScroll)
-    iframe.contentWindow.postMessage(
-      { type: POST_MESSAGE_SCROLL_POSITION, scrollPosition },
-      '*'
-    )
-
-    window.requestAnimationFrame(scrollLoop)
-  }
-  scrollLoop()
+  scrollLoop(window, document, iframe, origin)
 
   let locked = false
   window.addEventListener(
