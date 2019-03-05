9509c634dd5e7689629beb1afb3508bf954774c6
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-05 08:29:22 -0500

    Refactor paywall to use hooks (#1869)
    * refactor to use hooks
    
    * update to newer constant names (+9 squashed commits)
    Squashed commits:
    [2295a214] merge from PR
    [5a35f50d] better usage of scroll position
    [b03bac48] update with refactored changes
    [54bede89] add validators from unlock-app
    [91150e3e] oops..
    [2d0b1393] fix test
    [74ce2030] working now
    [2c4e4042] remove unused variable
    [8cd750ed] move paywall component to hooks
    
    * refactor to use window context
    
    * revert unintentional whitespace change

--- paywall/src/__tests__/components/Paywall.test.js
+++ paywall/src/__tests__/components/Paywall.test.js
@@ -2,11 +2,9 @@ import React from 'react'
 import * as rtl from 'react-testing-library'
 import { Provider } from 'react-redux'
 import createUnlockStore from '../../createUnlockStore'
-import Paywall, { mapStateToProps } from '../../components/Paywall'
-import { lockPage, unlockPage } from '../../services/iframeService'
+import { Paywall, mapStateToProps } from '../../components/Paywall'
 import { ConfigContext } from '../../utils/withConfig'
-
-jest.mock('../../services/iframeService.js')
+import { WindowContext } from '../../hooks/browser/useWindow'
 
 const lock = { address: '0x4983D5ECDc5cc0E499c2D23BF4Ac32B982bAe53a' }
 const locks = {
@@ -15,7 +13,7 @@ const locks = {
 const router = {
   location: {
     pathname: `/paywall/${lock.address}/http%3a%2f%2fexample.com`,
-    search: '',
+    search: '?origin=http%3A%2F%2Fexample.com',
     hash: '',
   },
 }
@@ -23,11 +21,13 @@ const router = {
 const noRedirectRouter = {
   location: {
     pathname: `/paywall/${lock.address}`,
-    search: '',
+    search: '?origin=http%3A%2F%2Fexample.com',
     hash: '',
   },
 }
 
+let fakeWindow
+let config
 let futureDate = new Date()
 futureDate.setYear(futureDate.getFullYear() + 1)
 futureDate = futureDate.getTime() / 1000
@@ -41,13 +41,36 @@ const keys = {
 const modals = []
 
 const store = createUnlockStore({ locks, keys, modals, router })
-const noKeyStore = createUnlockStore({ locks, keys: {}, modals, router })
+
+function renderMockPaywall(props = {}) {
+  return rtl.render(
+    <ConfigContext.Provider value={config}>
+      <WindowContext.Provider value={fakeWindow}>
+        <Provider store={store}>
+          <Paywall locks={[]} locked redirect={false} {...props} />
+        </Provider>
+      </WindowContext.Provider>
+    </ConfigContext.Provider>
+  )
+}
 
 afterEach(() => {
   rtl.cleanup()
-  jest.clearAllMocks()
 })
 describe('Paywall', () => {
+  beforeEach(() => {
+    config = { providers: [], isInIframe: true }
+    fakeWindow = {
+      location: {
+        pathname: `/${lock.address}`,
+        search: '?origin=http%3A%2F%2Fexample.com',
+        hash: '',
+      },
+      parent: { postMessage: jest.fn() },
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+    }
+  })
   describe('mapStateToProps', () => {
     it('should yield the lock which matches the address of the demo page', () => {
       expect.assertions(1)
@@ -83,60 +106,6 @@ describe('Paywall', () => {
       })
       expect(props.redirect).toBeFalsy()
     })
-  })
-
-  describe('handleIframe', () => {
-    it('should call lockPage when it is locked', () => {
-      expect.assertions(2)
-      rtl.render(
-        <Provider store={noKeyStore}>
-          <Paywall />
-        </Provider>
-      )
-      expect(lockPage).toHaveBeenCalled()
-      expect(unlockPage).not.toHaveBeenCalled()
-    })
-
-    it('should call unlockPage when it is not locked', () => {
-      expect.assertions(2)
-      rtl.render(
-        <ConfigContext.Provider value={{ providers: [] }}>
-          <Provider store={store}>
-            <Paywall />
-          </Provider>
-        </ConfigContext.Provider>
-      )
-      expect(lockPage).not.toHaveBeenCalled()
-      expect(unlockPage).toHaveBeenCalled()
-    })
-  })
-
-  describe('the unlocked flag', () => {
-    it('should be present when the paywall is unlocked', () => {
-      expect.assertions(1)
-      const { queryByText } = rtl.render(
-        <ConfigContext.Provider value={{ providers: [] }}>
-          <Provider store={store}>
-            <Paywall />
-          </Provider>
-        </ConfigContext.Provider>
-      )
-      const flagText = queryByText('Subscribed with Unlock')
-      expect(flagText).not.toBeNull()
-    })
-
-    it('should not be present when the paywall is locked', () => {
-      expect.assertions(1)
-      const { queryByText } = rtl.render(
-        <ConfigContext.Provider value={{ providers: [] }}>
-          <Provider store={noKeyStore}>
-            <Paywall />
-          </Provider>
-        </ConfigContext.Provider>
-      )
-      const flagText = queryByText('Subscribed with Unlock')
-      expect(flagText).toBeNull()
-    })
 
     it('should pull the redirect parameter from the page', () => {
       expect.assertions(1)
@@ -151,6 +120,60 @@ describe('Paywall', () => {
       }
       const props = mapStateToProps({ locks, router, keys, modals })
       expect(props.redirect).toBe('http://example.com')
+    })
+  })
+
+  describe('handleIframe', () => {
+    it('should post "locked" when it is locked in iframe', () => {
+      expect.assertions(1)
+      config.isInIframe = true
+      rtl.act(() => {
+        renderMockPaywall()
+      })
+
+      expect(fakeWindow.parent.postMessage).toHaveBeenCalledWith(
+        'locked',
+        'http://example.com'
+      )
+    })
+    it('should not post any message when it is in the main window', () => {
+      expect.assertions(1)
+      config.isInIframe = false
+      rtl.act(() => {
+        renderMockPaywall()
+      })
+
+      expect(fakeWindow.parent.postMessage).not.toHaveBeenCalled()
+    })
+    it('should post "unlocked" when it is unlocked in iframe', () => {
+      expect.assertions(1)
+      config.isInIframe = true
+      rtl.act(() => {
+        renderMockPaywall({ locked: false })
+      })
+
+      expect(fakeWindow.parent.postMessage).toHaveBeenCalledWith(
+        'unlocked',
+        'http://example.com'
+      )
+    })
+  })
+
+  describe('the unlocked flag', () => {
+    it('should be present when the paywall is unlocked', () => {
+      expect.assertions(1)
+      const { queryByText } = renderMockPaywall({ locked: false })
+
+      const flagText = queryByText('Subscribed with Unlock')
+      expect(flagText).not.toBeNull()
+    })
+
+    it('should not be present when the paywall is locked', () => {
+      expect.assertions(1)
+      const { queryByText } = renderMockPaywall({ locked: true })
+
+      const flagText = queryByText('Subscribed with Unlock')
+      expect(flagText).toBeNull()
     })
   })
 })
