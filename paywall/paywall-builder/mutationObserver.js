2a1e29f893eaa201409e9106339bdcfa63714310
Gregory Beaver <greg@unlock-protocol.com>
2019-03-05 17:34:57 -0500

    fix build error

--- paywall/paywall-builder/mutationObserver.js
+++ paywall/paywall-builder/mutationObserver.js
@@ -3,8 +3,11 @@ import { findLocks } from './script'
 export function changeListener(callback, list) {
   list.forEach(mutation => {
     if (!mutation.addedNodes || !mutation.addedNodes.length) return
-    for (let info of mutation.addedNodes.entries()) {
-      const entry = info[1]
+    const entries = mutation.addedNodes.entries()
+    while (true) {
+      const info = entries.next()
+      if (info.done) break
+      const entry = info.value[1]
       if (entry.nodeName !== 'META' || entry.name !== 'lock') continue
       callback(entry.content)
     }
