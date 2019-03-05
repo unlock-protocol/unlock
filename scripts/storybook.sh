ced5df7f1cbd99463ea2212471d567c180d82004
Gregory Beaver <greg@unlock-protocol.com>
2019-03-04 22:54:10 -0500

    de-couple the storybook script from circle

--- scripts/storybook.sh
+++ scripts/storybook.sh
@@ -3,10 +3,17 @@
 # this script runs the chromatic storybook visual diffs in the service provided as first argument
 
 SERVICE=$1
-AUTO_ACCEPT_CHANGES=$2
+BRANCH=$2
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 COMMAND="npm run chromatic"
+
+# on master, we will auto-accept any changes, because they have been approved in the pull request stage
+if [ "$BRANCH" == "master" ]; then
+  AUTO_ACCEPT_CHANGES = "--auto-accept-changes"
+else
+  AUTO_ACCEPT_CHANGES = ""
+fi
 
 # We pass only the relevent env vars, which are prefixed with the service name, uppercased
 # UNLOCK_APP_X will be passed to the container for tests in unlock_app as X.
