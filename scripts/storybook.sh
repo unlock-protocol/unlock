f4ab73d03a3efb0efdb3b1d655f1a8b6cd7ea5c3
Gregory Beaver <greg@unlock-protocol.com>
2019-03-05 20:48:43 -0500

    add chromatic to config (#1857)
    * add chromatic to config
    
    * add chromaticqa to the ci build
    
    * fake change to see what the diffs looks like
    
    * whitespace
    
    * Revert "fake change to see what the diffs looks like"
    
    This reverts commit 22e911de14d384b1a887d9b1f2b8144ade4636e9.
    
    * run chromatic in its own process on circle
    
    * missing from workflow
    
    * never require review on master builds, don't fail on PR builds
    
    * Revert "Revert "fake change to see what the diffs looks like""
    
    This reverts commit 082e8a54271267697a66b59dc5f36b16175ec6b2.
    
    * Revert "Revert "Revert "fake change to see what the diffs looks like"""
    
    This reverts commit ef5e45a336c0ae6b6079577f9384f1f5ce8707d6.
    
    * de-couple the storybook script from circle

---
+++ scripts/storybook.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# this script runs the chromatic storybook visual diffs in the service provided as first argument
+
+SERVICE=$1
+BRANCH=$2
+REPO_ROOT=`dirname "$0"`/..
+DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
+COMMAND="npm run chromatic"
+
+# on master, we will auto-accept any changes, because they have been approved in the pull request stage
+if [ "$BRANCH" == "master" ]; then
+  AUTO_ACCEPT_CHANGES = "--auto-accept-changes"
+else
+  AUTO_ACCEPT_CHANGES = ""
+fi
+
+# We pass only the relevent env vars, which are prefixed with the service name, uppercased
+# UNLOCK_APP_X will be passed to the container for tests in unlock_app as X.
+UPCASE_SERVICE="${SERVICE^^}"
+ENV_VARS_PREFIX="${UPCASE_SERVICE//-/_}_"
+ENV_VARS=`env | grep $ENV_VARS_PREFIX | awk '{print "-e ",$1}' ORS=' ' | sed -e "s/$ENV_VARS_PREFIX//g"`
+
+docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true $ENV_VARS $SERVICE $COMMAND -- $AUTO_ACCEPT_CHANGES
