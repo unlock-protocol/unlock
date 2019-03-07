fb8da1ac9d92e1ff9f730ae2facce0a4c03a38d3
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-07 10:24:11 -0500

    clarify comments (#1939)

--- unlock-app/src/utils/validators.js
+++ unlock-app/src/utils/validators.js
@@ -1,13 +1,13 @@
 // tests whether a field's value was not entered by the user
 export const isNotEmpty = val => val || val === 0
 
-// tests whether a number is positive (0 included) and not a decimal number
+// tests whether a number is non-negative and not a decimal number
 export const isPositiveInteger = val => {
   const parsedInt = parseInt(val)
   return !isNaN(parsedInt) && val == parsedInt && +val >= 0
 }
 
-// tests whether a number is a positive (0 included) real number (decimals allowed)
+// tests whether a number is a non-negative real number (decimals allowed)
 export const isPositiveNumber = val => {
   const parsedFloat = parseFloat(val)
   return !isNaN(parsedFloat) && +parsedFloat >= 0
