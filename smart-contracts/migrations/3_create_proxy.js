44d4924900cfc877052226782f34e44194d5a568
Nick Furfaro <nfurfaro33@gmail.com>
2019-03-07 10:05:26 -0700

    Modify migrations scripts to remove shellJs (#1935)

--- smart-contracts/migrations/3_create_proxy.js
+++ smart-contracts/migrations/3_create_proxy.js
@@ -1,23 +1,44 @@
-const shell = require('shelljs')
+// Load zos scripts and truffle wrapper function
+const { scripts, ConfigVariablesInitializer } = require('zos')
+const { create } = scripts
 
-module.exports = function deployProxy (deployer, network, accounts) {
+async function deploy (options, accounts) {
   // default account used by ganache
   const unlockOwner = accounts[0]
-  // zos proxy admin can be any account other than accounts[0]
-  // see "The transparent proxy pattern": https://blog.zeppelinos.org/the-transparent-proxy-pattern/
+
+  // Create an instance of MyContract
+  await create(
+    Object.assign(
+      {
+        contractAlias: 'Unlock',
+        initMethod: 'initialize',
+        initArgs: [unlockOwner]
+      },
+      options
+    )
+  )
+}
+
+module.exports = function (deployer, networkName, accounts) {
   const proxyAdmin = accounts[9]
 
-  deployer.then(() => {
-    if (network === 'test' || network === 'development') {
-      console.log(`Skipping proxy creation in 3_create_proxy.js while on network '${network}'. Exiting...`)
+  deployer.then(async () => {
+    const {
+      network,
+      txParams
+    } = await ConfigVariablesInitializer.initNetworkConfiguration({
+      network: networkName,
+      from: proxyAdmin
+    })
+    txParams.gas = 4000000
+    const options = { network, txParams }
+
+    if (networkName === 'test' || networkName === 'development') {
+      console.log(
+        `Skipping proxy creation in 3_create_proxy.js while on network '${network}'. Exiting...`
+      )
     } else {
-      if (
-        shell.exec(
-          `zos create Unlock --init initialize --args ${unlockOwner} --network ${network} --from ${proxyAdmin}`
-        ).code !== 0
-      ) {
-        throw new Error('Migration failed')
-      }
+      await deploy(options, accounts)
     }
   })
 }
