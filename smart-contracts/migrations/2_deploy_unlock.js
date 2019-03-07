8c532918c3b2113ea665f7db6260d007ba245968
Nick Furfaro <nfurfaro33@gmail.com>
2019-03-07 10:05:26 -0700

    Modify migrations scripts to remove shellJs (#1935)

--- smart-contracts/migrations/2_deploy_unlock.js
+++ smart-contracts/migrations/2_deploy_unlock.js
@@ -1,21 +1,34 @@
-const Unlock = artifacts.require('./Unlock.sol')
-const shell = require('shelljs')
+const Unlock = artifacts.require('../Unlock.sol')
+// Load zos scripts and truffle wrapper function
+const { scripts, ConfigVariablesInitializer } = require('zos')
+const { add, push } = scripts
 
-module.exports = function deployProxy (deployer, network, accounts) {
+async function zosDeploy (options) {
+  // Register Unlock in the zos.json
+  add({ contractsData: [{ name: 'Unlock', alias: 'Unlock' }] })
+  // Push implementation contract to the network
+  await push(options)
+}
+
+module.exports = function deployUnlock (deployer, networkName, accounts) {
   const unlockOwner = accounts[0]
   const proxyAdmin = accounts[9]
 
-  deployer.then(() => {
-    if (network === 'test' || network === 'development') {
-      return deployer.deploy(Unlock, unlockOwner)
+  deployer.then(async () => {
+    if (networkName === 'test' || networkName === 'development') {
+      await deployer.deploy(Unlock, unlockOwner)
     } else {
-      if (
-        shell.exec(
-          `zos push --reset --network ${network} --from ${proxyAdmin}`
-        ).code !== 0
-      ) {
-        throw new Error('Migration failed')
-      }
+      const {
+        network,
+        txParams
+      } = await ConfigVariablesInitializer.initNetworkConfiguration({
+        network: networkName,
+        from: proxyAdmin
+      })
+      txParams.gas = 4000000
+      const options = { network, txParams }
+
+      await zosDeploy(options)
     }
   })
 }
