bfeabb08f128bcba95d0784203d83c177e3b8163
Gregory Beaver <greg@unlock-protocol.com>
2019-03-07 11:03:23 -0500

    Fix the demo/paywall interaction in unlock-app (#1927)
    * fix the demo page/paywall interaction
    
    * fix tests
    
    * remove console
    
    * add explanatory comment

--- unlock-app/src/paywall-builder/mutationObserver.js
+++ unlock-app/src/paywall-builder/mutationObserver.js
@@ -1,10 +1,17 @@
+/**
+ * This file is the core of the paywall script. It listens for changes to the <head></head> component
+ * and looks for a <meta name="lock" content="..." /> and pulls the lock address out of the content.
+ */
 import { findLocks } from './script'
 
 export function changeListener(callback, list) {
   list.forEach(mutation => {
     if (!mutation.addedNodes || !mutation.addedNodes.length) return
-    for (let info of mutation.addedNodes.entries()) {
-      const entry = info[1]
+    const entries = mutation.addedNodes.entries()
+    while (true) {
+      const info = entries.next()
+      if (info.done) break
+      const entry = info.value[1]
       if (entry.nodeName !== 'META' || entry.name !== 'lock') continue
       callback(entry.content)
     }
