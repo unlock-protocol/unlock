566762f12b11ff00212b288889502025e3e53645
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-11 17:48:53 -0400

    Deprecating the global error consumer by first using errors passed from the redux store to the consumer (#2004)

--- unlock-app/src/components/interface/GlobalErrorConsumer.js
+++ unlock-app/src/components/interface/GlobalErrorConsumer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { connect } from 'react-redux'
 import { GlobalErrorContext } from '../../utils/GlobalErrorProvider'
 import Layout from './Layout'
 import { mapErrorToComponent } from '../creator/FatalError'
 
+// DEPRECATED we should not use the Context to detect errors
 const Consumer = GlobalErrorContext.Consumer
 
+// TODO: move that logic back to <GlobalErrorConsumer> once the Context logic is gone
 export const displayError = (error, errorMetadata, children) => {
   if (error) {
     const Error = mapErrorToComponent(error, errorMetadata)
@@ -15,7 +17,14 @@ export const displayError = (error, errorMetadata, children) => {
   return <React.Fragment>{children}</React.Fragment>
 }
 
-export default function GlobalErrorConsumer({ displayError, children }) {
+/**
+ * The GlobalErrorConsumer should be renamed, but its job is simply to "intercept" any fatal error
+ * and prevent the UI from displaying anything else.
+ */
+export function GlobalErrorConsumer({ displayError, children, error }) {
+  if (error) {
+    return displayError(error, {}, children)
+  }
   return (
     <Consumer>
       {({ error, errorMetadata }) => {
@@ -28,8 +37,26 @@ export default function GlobalErrorConsumer({ displayError, children }) {
 GlobalErrorConsumer.propTypes = {
   children: PropTypes.node.isRequired,
   displayError: PropTypes.func,
+  error: PropTypes.string,
 }
 
 GlobalErrorConsumer.defaultProps = {
   displayError,
+  error: null,
 }
+
+/**
+ * Will pass the first fatal error as props.
+ * @param {*} state
+ */
+export const mapStateToProps = state => {
+  if (!state.errors) {
+    return {}
+  }
+  const error = state.errors.find(error => error.startsWith('FATAL_'))
+  return {
+    error: error,
+  }
+}
+
+export default connect(mapStateToProps)(GlobalErrorConsumer)
