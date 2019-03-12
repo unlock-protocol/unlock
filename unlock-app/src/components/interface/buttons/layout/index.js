e681c9646c31448d8239629d0a1e74c1457004de
Christoph Nascone <chris@unlock-protocol.com>
2019-03-12 08:46:25 -0400

    Organize nav buttons better

--- unlock-app/src/components/interface/buttons/layout/index.js
+++ unlock-app/src/components/interface/buttons/layout/index.js
@@ -7,8 +7,6 @@ import Newsletter from './Newsletter'
 import Telegram from './Telegram'
 import Bars from './Bars'
 import ChevronUp from './ChevronUp'
-import Dashboard from './Dashboard'
-import Log from './Log'
 
 export default {
   About,
@@ -20,6 +18,4 @@ export default {
   Telegram,
   Bars,
   ChevronUp,
-  Dashboard,
-  Log,
 }
---
+++ unlock-app/src/components/interface/buttons/navigation/index.js
@@ -0,0 +1,7 @@
+import Dashboard from './Dashboard'
+import Log from './Log'
+
+export default {
+  Dashboard,
+  Log,
+}
