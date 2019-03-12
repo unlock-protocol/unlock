0f7d3f324b5c0072debda84a1cbd7a1c04ac2fbc
Hardly Difficult <hardlydiff@gmail.com>
2019-03-12 12:40:30 -0700

    Adding a token implementation to use for testing. (#2028)

---
+++ smart-contracts/contracts/test-artifacts/TestErc20Token.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.5;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+
+
+/**
+ * This is an implementation of an ERC20 token to be used in tests.
+ * It's a standard ERC20 implementation + `mint` (for testing).
+ *
+ * This contract should not be used in production.
+ */
+contract TestErc20Token is
+  ERC20,
+  ERC20Mintable
+{}
