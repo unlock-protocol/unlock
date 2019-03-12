4da1f13f70455c7a28e55b891354b652a751a29a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-03-11 22:39:18 -0400

    Adds initial test coverage around the persistence of Lock names (#1973)
    The adjustment requires the environment to be set appropriately, the following
    environment variables are expected to be set:
    
    * CI
    * HTTP_PROVIDER
    * DB_USERNAME
    * DB_PASSWORD
    * DB_NAME
    * DB_HOSTNAME
    * LOCKSMITH_URI
    
    Wait for db & shared configuration
    
    With this update it was noticed that we were colliding with the travis configuration.
    On circle we were running into a race condition where locksmith was ready before the database ... hilarity soon followed.
    
    https://github.com/Eficode/wait-for

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+export DB_USERNAME='locksmith_test'
+export DB_PASSWORD='password'
+export DB_NAME='locksmith_test'
+export DB_HOSTNAME='db'
+export CI=true
+export HTTP_PROVIDER='ganache-integration'
+export LOCKSMITH_URI='http://locksmith:8080'
+
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 
 # Run the tests
 COMMAND="npm run ci"
+
 docker-compose -f $DOCKER_COMPOSE_FILE run integration-tests bash -c "$COMMAND"
