dc43085efe789b2f9592af9e3fa832069b52c8ff
Gregory Beaver <greg@unlock-protocol.com>
2019-03-12 10:44:38 -0400

    add docs

--- scripts/local-docker-integration-tests.sh
+++ scripts/local-docker-integration-tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-
+# This script is only used locally. It runs the integration tests in the same environment
+# that they are run in when running on the CI server.
+# The only dependency is on docker, everything else happens inside the containers
+# usage:
+#
+# scripts/local-docker-integration-tests.sh
 
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
