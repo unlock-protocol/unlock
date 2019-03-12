8f32a700e6e091f1bc35f0704312c03e65ba8cfa
Gregory Beaver <greg@unlock-protocol.com>
2019-03-12 15:26:33 -0400

    Revert "extract shared functionality and make the local script use integration-tests.sh"
    This reverts commit 5e6114b5df540d0e4dd8b21728ac23a4f33f6968.

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# set the environment variables needed for integration tests
-. $REPO_ROOT/scripts/integration-envs.sh
+export DB_USERNAME='locksmith_test'
+export DB_PASSWORD='password'
+export DB_NAME='locksmith_test'
+export DB_HOSTNAME='db'
+export CI=true
+export HTTP_PROVIDER='ganache-integration'
+export LOCKSMITH_URI='http://locksmith:8080'
+export PAYWALL_URL=http://unlock:3000/paywall
+export PAYWALL_SCRIPT_URL=http://unlock:3000/static/paywall.min.js
 
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
--- scripts/local-docker-integration-tests.sh
+++ scripts/local-docker-integration-tests.sh
@@ -10,9 +10,16 @@
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 
-# set the environment variables needed to build with cache
-# without these, the build will be incorrect and never hit cache
-. $REPO_ROOT/scripts/integration-envs.sh
+# environment variables passed in. Update as needed for testing
+export DB_USERNAME='username'
+export DB_PASSWORD='password'
+export DB_NAME='locksmith'
+export DB_HOSTNAME='db'
+export CI=true
+export HTTP_PROVIDER='ganache-integration'
+export LOCKSMITH_URI='http://locksmith:8080'
+export PAYWALL_URL=http://unlock:3000/paywall
+export PAYWALL_SCRIPT_URL=http://unlock:3000/static/paywall.min.js
 
 # if the integration test images are running, this ensures we remove any state
 # prior to attempting to run the tests. This line is critical!
@@ -22,4 +29,7 @@ docker-compose -f $DOCKER_COMPOSE_FILE down --volumes
 docker build -t unlock -f "$REPO_ROOT/docker/unlock.dockerfile" $REPO_ROOT
 docker build -t unlock-integration -f "$REPO_ROOT/docker/unlock-integration.dockerfile" $REPO_ROOT
 
-$REPO_ROOT/scripts/integration-tests.sh
+# Run the tests
+COMMAND="npm run ci"
+docker-compose -f $DOCKER_COMPOSE_FILE run integration-tests bash -c "$COMMAND" 
+
