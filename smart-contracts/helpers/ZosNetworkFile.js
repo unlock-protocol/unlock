a2b22512bdc75308179584f679998bb4c4ab30e0
Hardly Difficult <hardlydiff@gmail.com>
2019-03-12 14:52:39 -0700

    Reset ZOS when migrations is `--reset` (#2034)

---
+++ smart-contracts/helpers/ZosNetworkFile.js
@@ -0,0 +1,22 @@
+const Zos = require('zos')
+const { ZosPackageFile } = Zos.files
+const packageFile = new ZosPackageFile()
+
+module.exports = function getNetworkFile (web3) {
+  return web3.eth.net
+    .getId()
+    .then(_Id => {
+      let network
+      switch (_Id) {
+        case '1':
+          network = 'mainnet'
+          break
+        case '4':
+          network = 'rinkeby'
+          break
+        default:
+          network = `dev-${_Id}`
+      }
+      return packageFile.networkFile(network)
+    })
+}
