cad773130195e163b3f137ded6fcaa0d13951a1c
hardlydifficult <hardlydifficultllc@gmail.com>
2019-03-13 11:15:32 -0700

    Solidity 0.5.6

--- smart-contracts/contracts/mixins/MixinPurchase.sol
+++ smart-contracts/contracts/mixins/MixinPurchase.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.5;
+pragma solidity 0.5.6;
 
 import './MixinDisableAndDestroy.sol';
 import './MixinKeys.sol';
--- smart-contracts/contracts/test-artifacts/TestErc20Token.sol
+++ smart-contracts/contracts/test-artifacts/TestErc20Token.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.5;
+pragma solidity 0.5.6;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
