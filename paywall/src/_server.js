d48b19c164bc556bcbe83df38e40f719c8ba4d1f
Gregory Beaver <greg@unlock-protocol.com>
2019-03-13 10:19:58 -0400

    Fix serving, enhance logging

--- paywall/src/_server.js
+++ paywall/src/_server.js
@@ -6,20 +6,20 @@ const pathMatch = require('path-match')
 
 function _server(port, dev) {
   return new Promise((resolve, reject) => {
-    const app = next({ dir: `${__dirname}/../`, dev, quiet: true })
+    const app = next({ dir: `${__dirname}/`, dev, quiet: true })
     const handle = app.getRequestHandler()
     const route = pathMatch()
 
     app.prepare().then(() => {
       let server = createServer((req, res) => {
-        console.info(`${req.method} ${req.url} > ${res.statusCode} `)
+        console.info(`PAYWALL ${req.method} ${req.url} > ${res.statusCode} `)
         try {
           const parsedUrl = new URL(req.url, `http://${req.headers.host}/`)
           const { pathname, query } = parsedUrl
 
           // assigning `query` into the params means that we still
           // get the query string passed to our application
-          if (!pathname.match('/paywall')) {
+          if (pathname.match('/0x')) {
             const params = route('/:lockAddress/:redirect?')(pathname)
             app.render(req, res, '/', Object.assign(params, query))
           } else {
