9b609126b84e802886a5a37352a2d6c7aef51689
Gregory Beaver <greg@unlock-protocol.com>
2019-03-13 08:29:30 -0400

    allow args for debugging

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -13,8 +13,9 @@ export PAYWALL_SCRIPT_URL=http://unlock:3000/static/paywall.min.js
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
+EXTRA_ARGS=$*
 
 # Run the tests
 COMMAND="npm run ci"
 
-docker-compose -f $DOCKER_COMPOSE_FILE run integration-tests bash -c "$COMMAND"
+docker-compose -f $DOCKER_COMPOSE_FILE run $EXTRA_ARGS integration-tests bash -c "$COMMAND"
