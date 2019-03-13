ad5a9cd7d84af43a58e7d9643fc3c558d5817877
Gregory Beaver <greg@unlock-protocol.com>
2019-03-12 22:01:19 -0400

    move integration tests to use the split-off paywall

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -7,8 +7,8 @@ export DB_HOSTNAME='db'
 export CI=true
 export HTTP_PROVIDER='ganache-integration'
 export LOCKSMITH_URI='http://locksmith:8080'
-export PAYWALL_URL=http://unlock:3000/paywall
-export PAYWALL_SCRIPT_URL=http://unlock:3000/static/paywall.min.js
+export PAYWALL_URL=http://unlock:3001
+export PAYWALL_SCRIPT_URL=http://unlock:3001/static/paywall.min.js
 
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
--- scripts/local-docker-integration-tests.sh
+++ scripts/local-docker-integration-tests.sh
@@ -18,8 +18,8 @@ export DB_HOSTNAME='db'
 export CI=true
 export HTTP_PROVIDER='ganache-integration'
 export LOCKSMITH_URI='http://locksmith:8080'
-export PAYWALL_URL=http://unlock:3000/paywall
-export PAYWALL_SCRIPT_URL=http://unlock:3000/static/paywall.min.js
+export PAYWALL_URL=http://unlock:3001
+export PAYWALL_SCRIPT_URL=http://unlock:3001/static/paywall.min.js
 
 # if the integration test images are running, this ensures we remove any state
 # prior to attempting to run the tests. This line is critical!
