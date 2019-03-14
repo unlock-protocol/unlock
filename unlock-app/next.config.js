e0c44ff71642cac6a0b79aedeaacac27d6fa3002
Christopher Nascone <cnasc@users.noreply.github.com>
2019-03-14 13:39:54 -0400

    unlock-app now builds with the TypeScript compiler (#2071)
    * Add deps for TypeScript build
    
    * Add unlock-app deps for TypeScript build
    
    * Add tsc task to package.json
    
    * Initial tsconfig.json
    
    * Customize tsconfig
    
    * Include all files in the src directory
    
    * Set up next to use TypeScript
    
    * Hook up next-typescript and babel
    
    * Eslint will now resolve TS files
    
    * Jest can handle TS files now
    
    * Fix dep category
    
    * TypeScript things need to be regular deps so they can be used in CI

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const { join } = require('path')
 const { promisify } = require('util')
-const withSourceMaps = require('@zeit/next-source-maps')
+const withTypescript = require('@zeit/next-typescript')
 
 const copyFile = promisify(fs.copyFile)
 
@@ -33,7 +33,7 @@ Object.keys(requiredConfigVariables).forEach(configVariableName => {
   }
 })
 
-module.exports = withSourceMaps({
+module.exports = withTypescript({
   publicRuntimeConfig: requiredConfigVariables,
   webpack(config) {
     return config
