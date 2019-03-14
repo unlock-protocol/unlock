b80456b85c0e3f50e9b90f79899a306723366b4f
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-14 14:49:17 -0400

    fixing git command to checkout old version (#2073)

--- scripts/get-git-history.sh
+++ scripts/get-git-history.sh
@@ -3,4 +3,4 @@
 # This script gets the git history as by default CircleCI will only get the most recent commit.
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch >> /dev/null
-git checkout @{4.days.ago}
+git checkout `git rev-list -1 --before={4.days.ago} master`
