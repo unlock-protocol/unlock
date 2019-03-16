3f15aec9901270ae02f5826b4dc3ae66e096da11
Gregory Beaver <greg@unlock-protocol.com>
2019-03-16 13:40:29 -0400

    usePurchaseKey custom hook

---
+++ paywall/src/hooks/usePurchaseKey.js
@@ -0,0 +1,17 @@
+import { useCallback } from 'react'
+import usePostMessage from './browser/usePostMessage'
+import { POST_MESSAGE_REDIRECT } from '../paywall-builder/constants'
+
+export default function usePurchaseKey(purchaseKey, openInNewWindow) {
+  const { postMessage } = usePostMessage()
+  const purchase = useCallback(
+    key => {
+      if (openInNewWindow) {
+        return postMessage(POST_MESSAGE_REDIRECT)
+      }
+      purchaseKey(key)
+    },
+    [purchaseKey, postMessage, openInNewWindow]
+  )
+  return purchase
+}
