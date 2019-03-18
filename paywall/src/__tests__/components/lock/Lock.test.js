6b00fc7044c60a17968ea936c486be49a3b1cfa2
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-18 00:04:08 -0400

    fix key purchase on coinbase wallet (#2130)
    * fix key purchase on coinbase wallet
    
    * usePurchaseKey custom hook
    
    * refactor to use the custom hook
    
    * address PR feedback
    
    * one more space

--- paywall/src/__tests__/components/lock/Lock.test.js
+++ paywall/src/__tests__/components/lock/Lock.test.js
@@ -1,11 +1,18 @@
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import { Provider } from 'react-redux'
+
 import {
   mapStateToProps,
   mapDispatchToProps,
+  Lock,
 } from '../../../components/lock/Lock'
 import { purchaseKey } from '../../../actions/key'
 import { TRANSACTION_TYPES } from '../../../constants'
-import { openNewWindowModal } from '../../../actions/modal'
+import usePurchaseKey from '../../../hooks/usePurchaseKey'
+import createUnlockStore from '../../../createUnlockStore'
 
+jest.mock('../../../hooks/usePurchaseKey')
 describe('Lock', () => {
   describe('mapDispatchToProps', () => {
     it('should return a purchaseKey function which when invoked dispatches purchaseKey and invokes showModal', () => {
@@ -21,22 +28,6 @@ describe('Lock', () => {
       newProps.purchaseKey(key)
       expect(props.showModal).toHaveBeenCalledWith()
       expect(dispatch).toHaveBeenCalledWith(purchaseKey(key))
-    })
-
-    it('should dispatch openNewWindowModal if the openNewWindow prop is truthy', () => {
-      expect.assertions(2)
-      const dispatch = jest.fn()
-      const props = {
-        showModal: jest.fn(),
-        openInNewWindow: true,
-      }
-      const key = {}
-
-      const newProps = mapDispatchToProps(dispatch, props)
-
-      newProps.purchaseKey(key)
-      expect(props.showModal).not.toHaveBeenCalled()
-      expect(dispatch).toHaveBeenCalledWith(openNewWindowModal())
     })
   })
 
@@ -101,6 +92,61 @@ describe('Lock', () => {
       expect(newProps.lockKey.data).toEqual('hello')
       expect(newProps.lockKey.expiration).toEqual(1000)
       expect(newProps.transaction).toEqual(state.transactions['0x777'])
+    })
+  })
+
+  describe('usePurchaseKey is called for purchases', () => {
+    let purchase
+    const config = {
+      isInIframe: true,
+      isServer: false,
+      requiredConfirmations: 12,
+    }
+
+    const lock = {
+      address: '0xaaaaaaaaa0c4d48d1bdad5dcb26153fc8780f83e',
+      name: 'Monthly',
+      keyPrice: '0.23',
+      fiatPrice: 240.38,
+      expirationDuration: 2592000,
+    }
+
+    function renderMockLock(openInNewWindow) {
+      const state = {
+        network: {},
+        account: {
+          address: '0x123',
+        },
+      }
+      const store = createUnlockStore(state)
+
+      usePurchaseKey.mockImplementation(() => purchase)
+      return rtl.render(
+        <Provider store={store}>
+          <Lock
+            lock={lock}
+            transaction={null}
+            lockKey={null}
+            purchaseKey={purchaseKey}
+            config={config}
+            hideModal={() => {}}
+            showModal={() => {}}
+            openInNewWindow={openInNewWindow}
+          />
+        </Provider>
+      )
+    }
+
+    it('should call useKeyPurchase purchase', () => {
+      expect.assertions(1)
+      purchase = jest.fn()
+      const component = renderMockLock(true)
+
+      rtl.act(() => {
+        rtl.fireEvent.click(component.getByText('Monthly'))
+      })
+
+      expect(usePurchaseKey).toHaveBeenCalled()
     })
   })
 })
