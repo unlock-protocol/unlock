6a6c905fde6071f57e9f12845c559bd710ee29e0
Christoph Nascone <chris@unlock-protocol.com>
2019-03-19 15:35:52 -0400

    More thorough test

--- unlock-app/src/__tests__/components/creator/CreatorLog.test.tsx
+++ unlock-app/src/__tests__/components/creator/CreatorLog.test.tsx
@@ -17,28 +17,40 @@ const transactionFeed: UnlockTypes.Transaction[] = [
 
 const transactionMetadata: UnlockTypes.TransactionMetadata = {
   '0x123': {
-    href: 'THIS_IS_MY_HREF',
+    href: 'http://www.this_is_my_href.com/',
     readableName: 'Lock Creation',
   },
 }
 
+let wrapper: rtl.RenderResult<typeof rtl.queries>
 
+afterEach(rtl.cleanup)
 describe('CreatorLog', () => {
+  beforeEach(() => {
+    wrapper = rtl.render(
+      <CreatorLog
+        transactionFeed={transactionFeed}
+        transactionMetadata={transactionMetadata} />
+    )
+  })
   it('should show the blockNumber of the transaction', () => {
-    const wrapper = rtl.render(React.createElement(CreatorLog, { transactionFeed, transactionMetadata }))
     expect.assertions(1)
     expect(wrapper.queryByText('1205')).not.toBeNull()
   })
 
-  it('should show the lock address', () => {
-    const wrapper = rtl.render(React.createElement(CreatorLog, { transactionFeed, transactionMetadata }))
-    expect.assertions(1)
-    expect(wrapper.queryByText('0x456')).not.toBeNull()
+  it('should show the lock address, which is a link containing the href', () => {
+    const { queryByText, container } = wrapper
+    expect.assertions(2)
+    expect(queryByText('0x456')).not.toBeNull()
+    const link = container.querySelector('a')
+    if (link) {
+      expect(link.href).toBe('http://www.this_is_my_href.com/')
+    }
   })
 
   it('should show the transaction type in readable form', () => {
-    const wrapper = rtl.render(React.createElement(CreatorLog, { transactionFeed, transactionMetadata }))
-    expect.assertions(1)
+    expect.assertions(2)
     expect(wrapper.queryByText(UnlockTypes.TransactionType.LOCK_CREATION)).toBeNull()
+    expect(wrapper.queryByText('Lock Creation')).not.toBeNull()
   })
 })
