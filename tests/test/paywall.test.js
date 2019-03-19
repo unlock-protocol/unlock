ede5ac93663610188cab2fad55e75dd0275d9370
Gregory Beaver <greg@unlock-protocol.com>
2019-03-19 11:57:55 -0400

    integration tests for the paywall (#2138)
    * integration tests for the paywall
    
    * add a test for the blocker, increase default timeout, fix test title
    
    * clean up the old stuff from this test

---
+++ tests/test/paywall.test.js
@@ -0,0 +1,55 @@
+const url = require('../helpers/url')
+
+jest.setTimeout(30000)
+
+describe('The Unlock Paywall', () => {
+  const testLockAddress = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
+
+  function lockSelector(name) {
+    return `#${name}_${testLockAddress}`
+  }
+
+  beforeAll(async () => {
+    await Promise.all([
+      page.goto(url(`/demo/${testLockAddress}`)),
+      page.waitForNavigation(),
+    ])
+  }, 10000)
+
+  it('should remove the blocker', async () => {
+    await page.waitForFunction(() => !document.querySelector('#_unlock_blocker'))
+  })
+
+  it('should display the lock after the blocker is gone', async () => {
+    await page.waitForFunction(() => window.frames.length)
+    const paywallIframe = page.mainFrame().childFrames()[0]
+    await paywallIframe.waitForSelector(lockSelector('Lock'))
+    await paywallIframe.waitForFunction((ethPriceSelector) => {
+      const eth = document.querySelector(ethPriceSelector)
+      if (!eth) return false
+      return eth.innerText === '0.33 ETH'
+    }, {}, lockSelector('EthPrice'))
+  })
+
+  it('clicking the lock purchases a key', async () => {
+    const paywallIframe = page.mainFrame().childFrames()[0]
+    const paywallBody = await paywallIframe.$('body')
+    await expect(paywallBody).toClick(lockSelector('PurchaseKey'))
+    await paywallIframe.waitForFunction((footerSelector) => {
+      const footer = document.querySelector(footerSelector)
+      if (!footer) return false
+      return footer.innerText === 'Payment Pending'
+    }, {}, 'footer')
+  })
+
+  it('after key purchase, unlocked flag appears', async () => {
+    await page.reload()
+    await page.waitForFunction(() => window.frames.length)
+    const paywallIframe = page.mainFrame().childFrames()[0]
+    await paywallIframe.waitForFunction(() => {
+      const unlockFlag = document.querySelector('#UnlockFlag')
+      if (!unlockFlag) return false
+      return true
+    })
+  })
+})
