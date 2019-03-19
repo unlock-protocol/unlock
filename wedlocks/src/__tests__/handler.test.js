f5f15059f0bb7c012c37d3e64ab1adf31cc7f9b6
julien <julien.genestoux@gmail.com>
2019-03-19 11:47:43 -0400

    handling HTTP logic andn passing requests to the routing function

--- wedlocks/src/__tests__/handler.test.js
+++ wedlocks/src/__tests__/handler.test.js
@@ -1,12 +1,161 @@
 import { handler } from '../handler'
+import { route } from '../route'
+
+jest.mock('../route')
 
 describe('handler', () => {
-  it('should yield hello world', done => {
-    handler({}, {}, (error, response) => {
-      expect.assertions(2)
-      expect(response.statusCode).toBe(200)
-      expect(response.body).toBe('Hello, World')
-      done()
+  it('should render 405 if the method is not a POST', done => {
+    expect.assertions(2)
+    handler(
+      {
+        httpMethod: 'GET'
+      },
+      {},
+      (error, response) => {
+        expect(response.statusCode).toBe(405)
+        expect(response.body).toBe('Unsupported Method')
+        done()
+      }
+    )
+  })
+
+  it('should render 415 if there are no headers', done => {
+    expect.assertions(2)
+    handler(
+      {
+        httpMethod: 'POST'
+      },
+      {},
+      (error, response) => {
+        expect(response.statusCode).toBe(415)
+        expect(response.body).toBe('Unsupported Media Type')
+        done()
+      }
+    )
+  })
+
+  it('should render 415 if the content type is not json', done => {
+    expect.assertions(2)
+    handler(
+      {
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'text/html'
+        }
+      },
+      {},
+      (error, response) => {
+        expect(response.statusCode).toBe(415)
+        expect(response.body).toBe('Unsupported Media Type')
+        done()
+      }
+    )
+  })
+
+  it('should render 422 if the body is malformed JSON', done => {
+    expect.assertions(2)
+    handler(
+      {
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: 'hello world'
+      },
+      {},
+      (error, response) => {
+        expect(response.statusCode).toBe(422)
+        expect(response.body).toBe('Malformed Body')
+        done()
+      }
+    )
+  })
+
+  it('should route the request and yields its response', done => {
+    expect.assertions(3)
+    const body = {
+      hello: 'world'
+    }
+    const responseBody = {
+      lorem: 'ipsum'
+    }
+    route.mockImplementationOnce((_body, _callback) => {
+      expect(_body).toEqual(body)
+      return _callback(null, responseBody)
     })
+
+    handler(
+      {
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      },
+      {},
+      (error, response) => {
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toBe(JSON.stringify(responseBody))
+        done()
+      }
+    )
+  })
+
+  it('should route the request and yields its response when it is an error', done => {
+    expect.assertions(3)
+    const body = {
+      hello: 'world'
+    }
+    const error = 'Could not send email'
+    route.mockImplementationOnce((_body, _callback) => {
+      expect(_body).toEqual(body)
+      return _callback('Could not send email')
+    })
+
+    handler(
+      {
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      },
+      {},
+      (_error, response) => {
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toBe(error)
+        done()
+      }
+    )
+  })
+
+  it('should route the request and catch errors in response processing', done => {
+    expect.assertions(3)
+    const body = {
+      hello: 'world'
+    }
+
+    const error = new Error('Processing error')
+
+    route.mockImplementationOnce(_body => {
+      expect(_body).toEqual(body)
+      throw error
+    })
+
+    handler(
+      {
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      },
+      {},
+      (_error, response) => {
+        expect(response.statusCode).toBe(500)
+        expect(response.body).toBe(error.message)
+        done()
+      }
+    )
   })
 })
