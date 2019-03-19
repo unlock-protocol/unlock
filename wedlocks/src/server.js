8b907b6867292c85fd8e0ed8b91af77da53be507
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-19 16:22:52 -0400

    Fixing the dev server for wedlocks (#2201)

--- wedlocks/src/server.js
+++ wedlocks/src/server.js
@@ -1,13 +1,31 @@
 /* eslint no-console: 0 */
 
 import http from 'http'
-import handler from './handler'
+import { handler } from './handler'
 
 http
   .createServer((req, res) => {
-    handler({}, {}, response => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end(response)
+    let body = ''
+    req.on('data', chunk => {
+      body += chunk.toString() // convert Buffer to string
+    })
+    req.on('end', () => {
+      handler(
+        {
+          httpMethod: req.method,
+          headers: req.headers,
+          body
+        },
+        {},
+        (error, response) => {
+          if (error) {
+            res.writeHead(500, {})
+            return res.end(error.message)
+          }
+          res.writeHead(response.statusCode, response.headers)
+          res.end(response.body)
+        }
+      )
     })
   })
   .listen(1337, '127.0.0.1')
