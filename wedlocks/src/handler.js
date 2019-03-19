d05842c347b334a0d1925b98d8ce80f1d9c93bf3
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-19 13:27:55 -0400

    handling HTTP logic andn passing requests to the routing function (#2190)

--- wedlocks/src/handler.js
+++ wedlocks/src/handler.js
@@ -1,10 +1,50 @@
 /* eslint no-console: 0 */
 /* eslint import/prefer-default-export: 0 */
-// AWS needs named exports
+import { route } from './route'
+
 export const handler = (event, context, callback) => {
-  // Do stuff with async and callback
-  return callback(null, {
-    statusCode: 200,
-    body: 'Hello, World'
-  })
+  if (event.httpMethod != 'POST') {
+    return callback(null, {
+      statusCode: 405,
+      body: 'Unsupported Method'
+    })
+  }
+
+  if (!event.headers || event.headers['content-type'] !== 'application/json') {
+    return callback(null, {
+      statusCode: 415,
+      body: 'Unsupported Media Type'
+    })
+  }
+
+  let body
+  try {
+    body = JSON.parse(event.body)
+  } catch (e) {
+    return callback(null, {
+      statusCode: 422,
+      body: 'Malformed Body'
+    })
+  }
+
+  try {
+    route(body, (error, response) => {
+      if (error) {
+        return callback(null, {
+          statusCode: 400,
+          body: error
+        })
+      }
+      return callback(null, {
+        statusCode: 200,
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(response)
+      })
+    })
+  } catch (e) {
+    return callback(null, {
+      statusCode: 500,
+      body: e.message
+    })
+  }
 }
---
+++ wedlocks/src/route.js
@@ -0,0 +1,14 @@
+// This function dispatches the perfom the actual email sending
+// TODO: implement me
+// params: {
+//  template: templateName string
+//  recipient: email aderess string
+//  params: params for the template (as a hash)
+// }
+export const route = (params, callback) => {
+  return callback(null, { message: 'hello world!' })
+}
+
+export default {
+  route
+}
