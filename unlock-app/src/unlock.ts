ee96cf331656e469a27f3e16e91e41c37b07a8a6
Christoph Nascone <chris@unlock-protocol.com>
2019-03-20 11:35:19 -0400

    Define new interfaces for upcoming work

--- unlock-app/src/unlock.ts
+++ unlock-app/src/unlock.ts
@@ -9,11 +9,44 @@ export enum TransactionType {
 }
 
 export interface Transaction {
-  status: string,
-  confirmations: number,
-  createdAt: number,
-  hash: string,
-  lock: string,
-  name: string,
-  type: TransactionType,
+  status: string
+  confirmations: number
+  hash: string
+  lock: string
+  name: string
+  type: TransactionType
+  blockNumber: number
+  
+  key?: string // TODO: tighten up our types, hopefully we won't have too many
+               // optional properties.
+}
+
+// Analogous to the transactions in Redux
+export interface Transactions {
+  [hash: string]: Transaction
+}
+
+export interface Account {
+  address: string
+  balance: string
+}
+
+export interface Network {
+  name: number
+}
+
+// TransactionMetadata and TransactionMetadataMap are used in the log page to
+// pass additional information about transactions that do not belong in the
+// state.
+export interface TransactionMetadata {
+  href: string
+  readableName: string
+}
+
+export interface TransactionMetadataMap {
+  [hash: string]: TransactionMetadata
+}
+
+export interface ChainExplorerURLBuilders {
+  [site: string]: (address: string) => string
 }
