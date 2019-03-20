49a2e7926db02d5aeb9a62de0dcf76b399842cd1
Gregory Beaver <greg@unlock-protocol.com>
2019-03-20 18:39:40 -0400

    add screenshotOnFail and use .png for screenshots

--- tests/helpers/debugging.js
+++ tests/helpers/debugging.js
@@ -44,9 +44,34 @@ module.exports = {
   },
   async screenshot(page, file = 'test') {
     await page.screenshot({
-      path: `/screenshots/${file}-${counter}.jpg`,
+      path: `/screenshots/${file}-${counter}.png`,
       fullPage: true,
     })
     counter += 1
+  },
+  screenshotOnFail: page => (testDescription, code) => {
+    const runTest = async () => {
+      try {
+        // await will do nothing for non-async code, and make async tests work
+        module.exports.debugPage(page, true, true)
+        await code()
+        // if the code does not throw, we did not fail
+      } catch (e) {
+        // locally, we use the volume mounted at /screenshots
+        // on CI, we use the /home/unlock/screenshots directory used to retrieve artifacts
+        const screenshotPath = `/screenshots/${testDescription.replace(
+          /[ ,/"'^$\\]+/g,
+          '-'
+        )}.png`
+        console.error(`writing screenshot to ${screenshotPath}`)
+        try {
+          await page.screenshot({ path: screenshotPath, fullPage: true })
+        } catch (screenshotError) {
+          console.error(screenshotError)
+        }
+        throw e
+      }
+    }
+    it(testDescription, runTest)
   },
 }
