1724d3bf6d49bcb34b81a10f8fec68b6f0d9c191
Christopher Nascone <cnasc@users.noreply.github.com>
2019-03-20 14:01:10 -0400

    Define new interfaces for upcoming work (#2214)
    * Define new interfaces for upcoming work
    
    * Add some TODOs

--- unlock-app/src/unlock.ts
+++ unlock-app/src/unlock.ts
@@ -9,11 +9,45 @@ export enum TransactionType {
 }
 
 export interface Transaction {
-  status: string,
-  confirmations: number,
-  createdAt: number,
-  hash: string,
-  lock: string,
-  name: string,
-  type: TransactionType,
+  status: string
+  confirmations: number
+  hash: string
+  lock: string
+  name: string
+  type: TransactionType
+  blockNumber: number
+  
+  key?: string // TODO: tighten up our types, hopefully we won't have too many
+               // optional properties.
+}
+
+// Analogous to the transactions in Redux
+export interface Transactions {
+  [hash: string]: Transaction
+}
+
+export interface Account {
+  address: string
+  balance: string // TODO: Stricter balance type (enforce currency, precision)
+}
+
+export interface Network {
+  name: number // TODO: This is very misleading, change property name to id
+}
+
+// TransactionMetadata and TransactionMetadataMap are used in the log page to
+// pass additional information about transactions that do not belong in the
+// state. TODO: There is certainly a better way to handle this, find out what
+// that is and do it.
+export interface TransactionMetadata {
+  href: string
+  readableName: string
+}
+
+export interface TransactionMetadataMap {
+  [hash: string]: TransactionMetadata
+}
+
+export interface ChainExplorerURLBuilders {
+  [site: string]: (address: string) => string
 }
