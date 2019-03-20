ec308b9c788decf1cbdfd7dc6c544b69194b232e
Gregory Beaver <greg@unlock-protocol.com>
2019-03-20 17:07:36 -0400

    add missing http://

--- tests/helpers/url.js
+++ tests/helpers/url.js
@@ -1,6 +1,6 @@
 const host = process.env.UNLOCK_HOST || '127.0.0.1'
 const port = process.env.UNLOCK_PORT || 3000
-const paywall = process.env.PAYWALL_URL || '127.0.0.1:3001'
+const paywall = process.env.PAYWALL_URL || 'http://127.0.0.1:3001'
 
 module.exports = {
   main: (path = '/') => `http://${host}:${port}${path}`,
