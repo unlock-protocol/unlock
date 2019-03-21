9054dfa70131610770fd6fd6ace47e262229014a
Gregory Beaver <greg@unlock-protocol.com>
2019-03-21 11:57:13 -0400

    tests

--- tests/test/url.test.js
+++ tests/test/url.test.js
@@ -1,30 +1,11 @@
+process.env.UNLOCK_HOST = 'host'
+process.env.UNLOCK_PORT = 999
+process.env.PAYWALL_URL = 'http://whatever'
+const url = require('../helpers/url')
 
-describe('url test helper', () => {
-  describe('main', () => {
-    it('from env', () => {
-      expect.assertions(2)
-      process.env.UNLOCK_HOST = 'host'
-      process.env.UNLOCK_PORT = 90
-
-      // eslint-disable-next-line
-      const url = require('../helpers/url')
-
-      expect(url.main()).toBe('http://host:90/')
-      expect(url.main('/test')).toBe('http://host:90/test')
-    })
-    it('from defaults', () => {
-      expect.assertions(2)
-      process.env.UNLOCK_HOST = false
-      process.env.UNLOCK_PORT = false
-
-      // eslint-disable-next-line
-      const url = require('../helpers/url')
-
-      expect(url.main()).toBe('http://127.0.0.1:3000/')
-      expect(url.main('/test')).toBe('http://127.0.0.1:3000/test')
-    })
-  })
-  describe('paywall', () => {
-
+describe('url test helper, from environment', () => {
+  it('main', () => {
+    expect(url.main()).toBe('http://host:999/')
+    expect(url.main('/path')).toBe('http://host:999/path')
   })
 })
