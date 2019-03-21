ff5fddcf500dc2ccb4cd1d11a9839f614865181c
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-03-21 17:01:50 -0400

    Adds user migrations and models to the project (#2267)

---
+++ locksmith/migrations/20190318152733-create-user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      publicKey: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+      },
+      recoveryPhrase: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      passwordEncryptedPrivateKey: {
+        type: Sequelize.JSON,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
