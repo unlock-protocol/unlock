c288ffcf87e6b25961c5c57f4b7071bc58539f55
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-20 20:36:46 -0400

    adding transactionOperations (#2193)

--- locksmith/src/routes/transaction.js
+++ locksmith/src/routes/transaction.js
@@ -4,6 +4,6 @@ var router = express.Router()
 var transaction_controller = require('../controllers/transactionController')
 
 router.post('/transaction', transaction_controller.transactionCreate)
-router.get('/transactions', transaction_controller.transactionGet)
+router.get('/transactions', transaction_controller.transactionsGet)
 
 module.exports = router
