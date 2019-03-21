936bb25e1d79d8360084f030d8e8893e74edbb11
Ben Werdmuller <ben@withknown.com>
2019-03-21 13:12:15 -0700

    Ben improved email confirmation template (#2259)
    * New email confirmation template text
    
    * Updating test

--- wedlocks/src/__tests__/templates/confirmEmail.test.js
+++ wedlocks/src/__tests__/templates/confirmEmail.test.js
@@ -11,7 +11,20 @@ describe('confirmEmail', () => {
         confirmLink: 'https://unlock-protocol.com/confirm...'
       })
     ).toBe(
-      'Please confirm your email address by clicking on the following link https://unlock-protocol.com/confirm....'
+    `Welcome to Unlock!
+
+To get started, please confirm your email address by clicking on the following link:
+
+  https://unlock-protocol.com/confirm...
+
+Once your email address is confirmed, you'll be able to use your Unlock account to pay for content and services.
+
+If you have any questions, you can always email us at hello@unlock-protocol.com.
+
+And again, welcome!
+
+- The Unlock team
+`
     )
   })
 })
