d0597928fdc4d51b876ec65410a1dec3beb873cc
Christoph Nascone <chris@unlock-protocol.com>
2019-03-21 10:40:11 -0400

    Not a working test

--- unlock-app/src/__tests__/services/web3Service.test.js
+++ unlock-app/src/__tests__/services/web3Service.test.js
@@ -1073,4 +1073,24 @@ describe('Web3Service', () => {
       expect(web3Service.getTransaction).toHaveBeenCalledWith(transactionHash)
     })
   })
+
+  describe('inputsHandlers', () => {
+    describe('createLock', () => {
+      it('should emit lock.updated with correctly typed values', async done => {
+        expect.assertions(2)
+        const params = {
+          _expirationDuration: '7',
+          _maxNumberOfKeys: '5',
+          _keyPrice: '0.05',
+        }
+        web3Service.on('lock.updated', (newLockAddress, update) => {
+          expect(update.expirationDuration).toBe(7)
+          expect(update.maxNumberOfKeys).toBe(5)
+          done()
+        })
+
+        await web3Service.inputsHandlers.createLock('0x123', '0x456', params)
+      })
+    })
+  })
 })
