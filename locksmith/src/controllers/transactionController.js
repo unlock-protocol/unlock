ef1347834653c3d0ab437aebb4a2b77e11e75b2e
Christopher Nascone <cnasc@users.noreply.github.com>
2019-03-21 09:57:22 -0400

    Update locksmith/src/controllers/transactionController.js
    Co-Authored-By: julien51 <julien.genestoux@gmail.com>

--- locksmith/src/controllers/transactionController.js
+++ locksmith/src/controllers/transactionController.js
@@ -33,7 +33,7 @@ const transactionsGet = async (req, res) => {
 /**
  * This will retun a JSON object to indicate the likelines that a transaction has to succeed.
  * 0: the transaction will surely fail
- * 1: the transaction will surecly succeed
+ * 1: the transaction will surely succeed
  * TODO: implement logic
  * @param {*} req
  * @param {*} res
