ad9f2b9dde69ab390c8a79b10fae6039735a7d20
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-03-22 11:41:13 -0400

    Adds a user controller to Locksmith (#2281)
    * Adds a user controller to Locksmith
    
    * Adding missed files
    
    * Updating to reflect code review feedback

---
+++ locksmith/src/controllers/userController.ts
@@ -0,0 +1,40 @@
+import UserOperations = require('../operations/userOperations')
+import { Request, Response } from 'express-serve-static-core'
+
+namespace UserController {
+  export async function createUser(req: Request, res: Response): Promise<any> {
+    let user = req.body.user
+
+    try {
+      if (user) {
+        let creationStatus: Boolean = await UserOperations.createUser(
+          { emailAddress: user.emailAddress,
+          publicKey: user.publicKey,
+          passwordEncryptedPrivateKey: user.passwordEncryptedPrivateKey,
+          recoveryPhrase: user.recoveryPhrase}
+        )
+
+        let status = creationStatus ? 200 : 400
+        return res.sendStatus(status)
+      }
+    } catch (e) {
+      return res.sendStatus(400)
+    }
+  }
+
+  export async function retrieveEncryptedPrivatekey(
+    req: Request,
+    res: Response
+  ): Promise<any> {
+    let emailAddress = req.params.emailAddress
+    let result = await UserOperations.getUserPrivateKeyByEmailAddress(emailAddress)
+
+    if (result) {
+      return res.json({ passwordEncryptedPrivateKey: result })
+    } else {
+      return res.sendStatus(400)
+    }
+  }
+}
+
+export = UserController
