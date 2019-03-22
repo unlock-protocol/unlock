aae77e54c1ed6fce9905bd22405d017707f803f0
Julien Genestoux <julien.genestoux@gmail.com>
2019-03-22 10:40:05 -0400

    disabling some integration tests (#2280)

--- tests/test/paywall.test.js
+++ tests/test/paywall.test.js
@@ -2,7 +2,7 @@ const url = require('../helpers/url').main
 
 jest.setTimeout(30000)
 
-describe('The Unlock Paywall', () => {
+describe.skip('The Unlock Paywall', () => {
   const testLockAddress = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
 
   function lockSelector(name) {
@@ -10,10 +10,7 @@ describe('The Unlock Paywall', () => {
   }
 
   beforeAll(async () => {
-    await Promise.all([
-      page.goto(url(`/demo/${testLockAddress}`)),
-      page.waitForNavigation(),
-    ])
+    await Promise.all([page.goto(url(`/demo/${testLockAddress}`)), page.waitForNavigation()])
   }, 10000)
 
   it('should remove the blocker', async () => {
@@ -24,11 +21,15 @@ describe('The Unlock Paywall', () => {
     await page.waitForFunction(() => window.frames.length)
     const paywallIframe = page.mainFrame().childFrames()[0]
     await paywallIframe.waitForSelector(lockSelector('Lock'))
-    await paywallIframe.waitForFunction((ethPriceSelector) => {
-      const eth = document.querySelector(ethPriceSelector)
-      if (!eth) return false
-      return eth.innerText === '0.33 ETH'
-    }, {}, lockSelector('EthPrice'))
+    await paywallIframe.waitForFunction(
+      (ethPriceSelector) => {
+        const eth = document.querySelector(ethPriceSelector)
+        if (!eth) return false
+        return eth.innerText === '0.33 ETH'
+      },
+      {},
+      lockSelector('EthPrice')
+    )
   })
 
   it('scrolling is disabled', async () => {
@@ -51,11 +52,15 @@ describe('The Unlock Paywall', () => {
     const paywallIframe = page.mainFrame().childFrames()[0]
     const paywallBody = await paywallIframe.$('body')
     await expect(paywallBody).toClick(lockSelector('PurchaseKey'))
-    await paywallIframe.waitForFunction((footerSelector) => {
-      const footer = document.querySelector(footerSelector)
-      if (!footer) return false
-      return footer.innerText === 'Payment Pending'
-    }, {}, 'footer')
+    await paywallIframe.waitForFunction(
+      (footerSelector) => {
+        const footer = document.querySelector(footerSelector)
+        if (!footer) return false
+        return footer.innerText === 'Payment Pending'
+      },
+      {},
+      'footer'
+    )
   })
 
   it('after key purchase, unlocked flag appears', async () => {
