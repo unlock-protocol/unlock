66dd12f325c9f1153835714201aa3d1f293ba13b
Ben Werdmuller <ben@withknown.com>
2019-03-22 14:15:18 -0700

    Email templates for ejected, ejection warning, and recovery key emails (#2269)
    * Email templates for ejected, ejection warning, and recovery key emails
    
    * Minor wording change
    
    * Defined ejectedEmail

--- wedlocks/src/templates/index.js
+++ wedlocks/src/templates/index.js
@@ -1,5 +1,11 @@
 import confirmEmail from './confirmEmail'
+import ejectedEmail from './ejectedEmail'
+import ejectionWarningEmail from './ejectionWarningEmail'
+import recoveryKeyEmail from './recoveryKeyEmail'
 
 export default {
-  confirmEmail
+  confirmEmail,
+  ejectedEmail,
+  ejectionWarningEmail,
+  recoveryKeyEmail,
 }
