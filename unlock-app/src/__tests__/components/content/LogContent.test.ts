c4b46d44cc560ac663bba86abcfd6470c3314223
Christoph Nascone <chris@unlock-protocol.com>
2019-03-22 09:22:34 -0400

    Final testing and cleanup

--- unlock-app/src/__tests__/components/content/LogContent.test.ts
+++ unlock-app/src/__tests__/components/content/LogContent.test.ts
@@ -1,43 +1,54 @@
 import { mapStateToProps } from '../../../components/content/LogContent'
-import { TransactionType } from '../../../unlock'
-import configure from '../../../config'
+import * as UnlockTypes from '../../../unlock'
 
 const transactions = {
-  '0x1234': {
+  '0x12345678': {
     hash: '0x12345678',
     confirmations: 12,
     status: 'mined',
     lock: '0x12345678a',
     blockNumber: 1,
-    type: TransactionType.LOCK_CREATION,
+    type: UnlockTypes.TransactionType.LOCK_CREATION,
+    name: 'Lock of Seagulls',
   },
-  '0x5678': {
+  '0x56781234': {
     hash: '0x56781234',
     confirmations: 4,
     status: 'mined',
     lock: '0x56781234a',
     blockNumber: 2,
-    type: TransactionType.LOCK_CREATION,
+    type: UnlockTypes.TransactionType.LOCK_CREATION,
+    name: 'Lock the Casbah',
   },
-  '0x89ab': {
+  '0x9abcdef0': {
     hash: '0x9abcdef0',
     confirmations: 2,
     status: 'mined',
     lock: '0x9abcdef0a',
     blockNumber: 3,
-    type: TransactionType.LOCK_CREATION,
+    type: UnlockTypes.TransactionType.LOCK_CREATION,
+    name: 'Lock Robster',
   },
 }
 
 describe('Transaction Log', () => {
   describe('mapStateToProps', () => {
     const state = {
-      account: {},
-      network: {},
+      account: {
+        address: '0x123456',
+        balance: '5',
+      },
+      network: {
+        name: 1984,
+      },
       transactions,
     }
-    const config = configure()
-    const { transactionFeed } = mapStateToProps(state, { config })
+    const config = {
+      chainExplorerUrlBuilders: {
+        etherScan: (address: string) => `https://blockchain.party/address/${address}/`
+      },
+    }
+    const { transactionFeed, explorerLinks } = mapStateToProps(state, { config })
     it('Should provide a feed of transactions sorted by blockNumber, descending', () => {
       expect.assertions(4)
       expect(transactionFeed).toHaveLength(3)
@@ -45,10 +56,10 @@ describe('Transaction Log', () => {
       expect(transactionFeed[1].blockNumber).toEqual(2)
       expect(transactionFeed[2].blockNumber).toEqual(1)
     })
-    it('should include href to explorer in the feed', () => {
-      expect.assertions(1)
-      const tx = transactionFeed[0]
-      expect(tx).toHaveProperty('href')
+    it('should include a separate feed of URLs to chain explorer for each transaction', () => {
+      expect.assertions(2)
+      expect(Object.keys(explorerLinks)).toHaveLength(3)
+      expect(explorerLinks['0x9abcdef0']).toBe('https://blockchain.party/address/0x9abcdef0a/')
     })
   })
 })
