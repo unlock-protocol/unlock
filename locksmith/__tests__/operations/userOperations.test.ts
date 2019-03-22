e9a4ed946e03eb10585c787589192aec6107c244
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-03-22 10:46:24 -0400

    Introduces user operations into the codebase for Locksmith (#2272)
    * Introduces user operations into the codebase for Locksmith
    
    * Updated to reflect code review feedback
    
    * Updating test to reflect boolean returned from creation method

---
+++ locksmith/__tests__/operations/userOperations.test.ts
@@ -0,0 +1,94 @@
+import UserOperations from '../../src/operations/userOperations'
+
+const models = require('../../src/models')
+let User: any = models.User
+let UserReference: any = models.UserReference
+
+describe('User creation', () => {
+  /* details are crafted to ensure normalization downstream*/
+  let userCreationDetails = {
+    emailAddress: 'USER@EXAMPLE.COM',
+    publicKey: '0x21cc9c438d9751a3225496f6fd1f1215c7bd5d83',
+    passwordEncryptedPrivateKey: '{"data" : "encryptedPassword"}',
+    recoveryPhrase: 'recoveryPhrase',
+  }
+
+  describe('data normalization', () => {
+    it('should normalize the public key/address & email address', async () => {
+      expect.assertions(1)
+      UserReference.create = jest.fn(() => {})
+
+      await UserOperations.createUser(userCreationDetails)
+      expect(UserReference.create).toHaveBeenCalledWith(
+        {
+          User: {
+            passwordEncryptedPrivateKey: '{"data" : "encryptedPassword"}',
+            publicKey: '0x21cC9C438D9751A3225496F6FD1F1215C7bd5D83',
+            recoveryPhrase: 'recoveryPhrase',
+          },
+          emailAddress: 'user@example.com',
+        },
+        { include: User }
+      )
+    })
+  })
+
+  describe('when able to create the user and associated data', () => {
+    it('returns true', async () => {
+      expect.assertions(1)
+      UserReference.create = jest.fn(() => {
+        return {}
+      })
+
+      let result = await UserOperations.createUser(userCreationDetails)
+      expect(result).toBe(true)
+    })
+  })
+
+  describe('when unable to create the user and associated data', () => {
+    it('returns false', async () => {
+      expect.assertions(1)
+      UserReference.create = jest.fn(() => {
+        return null
+      })
+
+      let result = await UserOperations.createUser(userCreationDetails)
+      expect(result).toBe(false)
+    })
+  })
+})
+
+describe('Private Key Lookup', () => {
+  UserReference.findOne = jest
+    .fn()
+    .mockImplementationOnce(() => {
+      return {
+        User: {
+          passwordEncryptedPrivateKey: 'the value',
+        },
+      }
+    })
+    .mockImplementationOnce(() => {
+      null
+    })
+
+  describe('when the email address exists in persistence', () => {
+    it('returns the appropriate encrypted private key', async () => {
+      expect.assertions(1)
+      let result = await UserOperations.getUserPrivateKeyByEmailAddress(
+        'existing@example.com'
+      )
+      expect(result).toEqual('the value')
+    })
+  })
+
+  describe('when the email address does not exist in persistence', () => {
+    it('returns null', async () => {
+      expect.assertions(1)
+      let result = await UserOperations.getUserPrivateKeyByEmailAddress(
+        'non-existant@example.com'
+      )
+      expect(result).toEqual(null)
+    })
+  })
+})
---
+++ locksmith/src/operations/userOperations.ts
@@ -0,0 +1,45 @@
+const ethJsUtil = require('ethereumjs-util')
+const models = require('../models')
+const { User, UserReference } = models
+import { UserCreationInput } from '../types'
+
+namespace UserOperations {
+  export async function createUser(input: UserCreationInput): Promise<Boolean> {
+    let userReference = await UserReference.create(
+      {
+        emailAddress: input.emailAddress.toLowerCase(),
+        User: {
+          publicKey: ethJsUtil.toChecksumAddress(input.publicKey),
+          recoveryPhrase: input.recoveryPhrase,
+          passwordEncryptedPrivateKey: input.passwordEncryptedPrivateKey,
+        },
+      },
+      {
+        include: User,
+      }
+    )
+
+    if (userReference) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  export async function getUserPrivateKeyByEmailAddress(
+    emailAddress: String
+  ): Promise<String | null> {
+    let user = await UserReference.findOne({
+      where: { emailAddress: emailAddress.toLowerCase() },
+      include: [{ model: User, attributes: ['passwordEncryptedPrivateKey'] }],
+    })
+
+    if (user) {
+      return user.User.passwordEncryptedPrivateKey
+    } else {
+      return null
+    }
+  }
+}
+
+export = UserOperations
