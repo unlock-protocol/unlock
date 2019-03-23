a34e94b2883675f474d7dc05128f88b567ad425b
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-23 17:35:18 -0400

    document test helpers (#2310)

--- tests/helpers/debugging.js
+++ tests/helpers/debugging.js
@@ -1,5 +1,31 @@
 /* eslint no-console: 0 */
 
+/**
+ * debugPage
+ ***********
+ *
+ * debugPage is used to turn on the display of console.log calls within the browser context.
+ * This includes calls within page.evaluate and page.waitForFunction.
+ *
+ * Usage:
+ *
+ * debugPage(page, true)
+ *
+ * screenshot
+ ************
+ *
+ * screenshot is used to take a screenshot. It will work both locally when run with the
+ * scripts/local-docker-integration-tests.sh script or on CI. Screenshots locally are
+ * placed in the current directory. In CI, they are found in the artifacts on CircleCI.
+ *
+ * Usage:
+ *
+ * await screenshot(page) // creates test-1.jpg
+ * await screenshot(page) // creates test-2.jpg
+ * await screenshot(page, 'another') // creates another-3.jpg
+ *
+ * The numeric prefix is always unique and represents the order of the screenshot.
+ */
 let counter = 1
 module.exports = {
   debugPage(page, debug = false) {
