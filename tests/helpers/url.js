2bd095c8cef7a01221f09eb4d15ff00bf6b72252
Gregory Beaver <greg@unlock-protocol.com>
2019-03-23 17:35:18 -0400

    document test helpers (#2310)

--- tests/helpers/url.js
+++ tests/helpers/url.js
@@ -2,6 +2,16 @@ const host = process.env.UNLOCK_HOST || '127.0.0.1'
 const port = process.env.UNLOCK_PORT || 3000
 const paywall = process.env.PAYWALL_URL || 'http://127.0.0.1:3001'
 
+/**
+ * Use these helpers to get relative paths inside the main unlock-app and the paywall.
+ * So, for instance, to access the paywall in the main window, use:
+ *
+ * paywall('/0x.....')
+ *
+ * To access the dashboard, use:
+ *
+ * main('/dashboard')
+ */
 module.exports = {
   main: (path = '/') => `http://${host}:${port}${path}`,
   paywall: (path = '/') => `${paywall}${path}`,
