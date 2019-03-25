c3f5f1bb8a583d2212e9a9de52ff5c240d282384
julien <julien.genestoux@gmail.com>
2019-03-25 14:04:57 -0400

    trimmed dashboard features

--- unlock-protocol.com/src/actions/provider.js
+++
@@ -1,6 +0,0 @@
-export const SET_PROVIDER = 'provider/SET_PROVIDER'
-
-export const setProvider = provider => ({
-  type: SET_PROVIDER,
-  provider,
-})
--- unlock-protocol.com/src/pages/paywall.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import PaywallContent from '../components/content/PaywallContent'
-
-// TODO: remove since we are using the paywall app separately
-const Paywall = () => <PaywallContent />
-
-export default Paywall
