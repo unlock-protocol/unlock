6529159e0e3fab5a625d695810f60f8f26965046
Gregory Beaver <greg@unlock-protocol.com>
2019-03-25 16:32:00 -0400

    (paywall) demo component and story (#2332)
    * demo component and story
    
    * missing snapshots

---
+++ paywall/src/stories/interface/StaticDemo.stories.js
@@ -0,0 +1,56 @@
+import { Provider } from 'react-redux'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import StaticDemo from '../../components/interface/StaticDemo'
+import createUnlockStore from '../../createUnlockStore'
+import { ConfigContext } from '../../utils/withConfig'
+
+const myLock = {
+  address: '0xaaaaaaaaa0c4d48d1bdad5dcb26153fc8780f83e',
+  name: 'My Blog',
+  keyPrice: '0.027',
+  expirationDuration: 172800,
+  maxNumberOfKeys: 240,
+  outstandingKeys: 3,
+}
+const store = createUnlockStore({
+  account: {
+    address: '0x123',
+    balance: '5.4',
+  },
+  locks: {
+    '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e': {
+      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      keyPrice: '10000000000000000000',
+      expirationDuration: 86400,
+      maxNumberOfKeys: 800,
+      outstandingKeys: 32,
+    },
+    '0xaaaaaaaaa0c4d48d1bdad5dcb26153fc8780f83e': myLock,
+  },
+  router: {
+    location: {
+      pathname: '/demo/' + myLock.address,
+      search: '',
+      hash: '',
+    },
+  },
+  currency: {
+    USD: 195.99,
+  },
+})
+
+const config = {
+  env: 'dev',
+  providers: { HTTP: {}, Metamask: {} },
+}
+
+storiesOf('StaticDemo', module)
+  .addDecorator(getStory => (
+    <ConfigContext.Provider value={config}>
+      <Provider store={store}>{getStory()}</Provider>
+    </ConfigContext.Provider>
+  ))
+  .add('the demo', () => {
+    return <StaticDemo />
+  })
