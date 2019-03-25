7f954f01a4b38ad721898fd0da3c5fb1897a2bed
Hardly Difficult <hardlydiff@gmail.com>
2019-03-25 09:43:00 -0700

    Adding ERC20 test artifacts (#2223)

---
+++ smart-contracts/contracts/test-artifacts/TestNoop.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.6;
+
+
+/**
+ * This is an empty contract to be used in tests to see what happens
+ * when an external contract does not have an API we expect.
+ *
+ * This contract should not be used in production.
+ */
+contract TestNoop
+{}
