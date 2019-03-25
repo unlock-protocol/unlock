86f6676fed9af7a07a43a32ba92c56b8de38c648
Christopher Nascone <cnasc@users.noreply.github.com>
2019-03-25 08:38:12 -0400

    Add lock creation form visibility to state (#2301)
    * Add and test actions
    
    * Add and test reducer
    
    * add reducer to store
    
    * Handle SET_ACCOUNT as well

---
+++ unlock-app/src/__tests__/reducers/lockFormVisibilityReducer.test.ts
@@ -0,0 +1,45 @@
+import reducer, { initialState } from '../../reducers/lockFormVisibilityReducer'
+import { SHOW_FORM, HIDE_FORM } from '../../actions/lockFormVisibility'
+import { SET_PROVIDER } from '../../actions/provider'
+import { SET_NETWORK } from '../../actions/network'
+import { SET_ACCOUNT } from '../../actions/accounts'
+
+describe('lock form visibility reducer', () => {
+  it('should return the initial state', () => {
+    expect.assertions(1)
+    expect(reducer(undefined, { type: 'aaa' })).toEqual(initialState)
+  })
+
+  it('should return the initial state when receiving SET_PROVIDER', () => {
+    expect.assertions(1)
+    expect(reducer({ visible: true }, { type: SET_PROVIDER }))
+      .toEqual(initialState)
+  })
+
+  it('should return the initial state when receiving SET_NETWORK', () => {
+    expect.assertions(1)
+    expect(reducer({ visible: true }, { type: SET_NETWORK }))
+      .toEqual(initialState)
+  })
+
+  it('should return the initial state when receiving SET_ACCOUNT', () => {
+    expect.assertions(1)
+    expect(reducer({ visible: true }, { type: SET_ACCOUNT }))
+      .toEqual(initialState)
+  })
+
+  it('should show the form', () => {
+    expect.assertions(1)
+    expect(reducer(initialState, { type: SHOW_FORM })).toEqual({
+      visible: true,
+    })
+  })
+
+  it('should hide the form', () => {
+    expect.assertions(1)
+    expect(reducer({ visible: true },
+                   { type: HIDE_FORM })).toEqual({
+      visible: false,
+    })
+  })
+})
