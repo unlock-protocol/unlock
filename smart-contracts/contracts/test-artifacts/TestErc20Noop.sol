a753171f8a84871725ce2fedaadd3d0243d3d83d
Hardly Difficult <hardlydiff@gmail.com>
2019-03-25 09:43:00 -0700

    Adding ERC20 test artifacts (#2223)

---
+++ smart-contracts/contracts/test-artifacts/TestErc20Noop.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.5.6;
+
+
+/**
+ * This is an implementation of the ERC20 interface but without any data
+ * backing the token to be used in tests.
+ *
+ * This contract should not be used in production.
+ */
+contract TestErc20Noop 
+{
+  function transfer(
+    address to,
+    uint value
+  ) external 
+    returns (bool)
+  {
+    return true;
+  }
+
+  function approve(
+    address spender,
+    uint value
+  ) external
+    returns (bool)
+  {
+    return true;
+  }
+
+  function mint(
+    address to,
+    uint value
+  ) public 
+    returns (bool)
+  {
+    return true;
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint value
+  ) external
+    returns (bool)
+  {
+    return true;
+  }
+
+  function balanceOf(
+    address who
+  ) external view
+    returns (uint)
+  {
+    return uint(-1);
+  }
+}
