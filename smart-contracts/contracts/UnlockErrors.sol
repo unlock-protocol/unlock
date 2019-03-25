b8ac98ac8aba7990de6c0dd30b205193ec669398
Hardly Difficult <hardlydiff@gmail.com>
2019-03-25 10:56:08 -0700

    Define transferFee and allow it to be changed (#2078)

--- smart-contracts/contracts/UnlockErrors.sol
+++ smart-contracts/contracts/UnlockErrors.sol
@@ -19,6 +19,10 @@ contract UnlockErrors {
   // The specified address is not valid (i.e. must not be 0).
   string public constant INVALID_ADDRESS = 'INVALID_ADDRESS';
 
+  // The specified rate is not valid, the denominator must be greater than 0.
+  // To set the rate to 0, set the numerator to 0 and the denominator to 1.
+  string public constant INVALID_RATE = 'INVALID_RATE';
+
   // The user does not have a valid key, they may or may not have purchased one previously.
   string public constant KEY_NOT_VALID = 'KEY_NOT_VALID';
 
