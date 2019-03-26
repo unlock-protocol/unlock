873d9753b9748bc2e5fd320e2a36c202de0a2abe
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-26 16:24:47 -0400

    Optimistic Unlocking flags (#2357)
    * optimistic unlocking flags
    
    * mis-named
    
    * new snapshots
    
    * boundary conditions
    
    * new test, fix fail
    
    * use FlagStyles.js instead of LockStyles.js for shared styles

---
+++ paywall/src/components/lock/ConfirmedFlag.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { RoundedLogo } from '../interface/Logo'
+import ConfirmedKey from '../interface/buttons/overlay/ConfirmedKey'
+import { OptimisticFlag, OptimisticLogo, FlagContent } from './FlagStyles'
+
+export default function ConfirmedFlag({ dismiss }) {
+  return (
+    <OptimisticFlag>
+      <OptimisticLogo>
+        <RoundedLogo size="28px" />
+      </OptimisticLogo>
+      <FlagContent>
+        <p>Purchase Confirmed</p>
+        <ConfirmedKey height="24px" width="24px" hideModal={dismiss} />
+      </FlagContent>
+    </OptimisticFlag>
+  )
+}
+
+ConfirmedFlag.propTypes = {
+  dismiss: PropTypes.func.isRequired,
+}
