ed6c3c5d8b4201b8e255889690a66d75fc909076
Ben Werdmuller <ben@withknown.com>
2019-03-27 12:18:53 -0700

    Removing blog utils and pages from unlock-app (#2378)
    * Removing blog utils from unlock-app
    
    * Returning pages
    
    * More removals: tests, stories, and components
    
    * Storyshots
    
    * First pass the post

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const { join } = require('path')
 const { promisify } = require('util')
 const withTypescript = require('@zeit/next-typescript')
-const { addBlogPagesToPageObject } = require('./src/utils/blog')
 
 const copyFile = promisify(fs.copyFile)
 
@@ -63,7 +62,7 @@ module.exports = withTypescript({
     }
 
     // Our statically-defined pages to export
-    let pages = {
+    return {
       '/': { page: '/home' },
       '/about': { page: '/about' },
       '/jobs': { page: '/jobs' },
@@ -72,10 +71,6 @@ module.exports = withTypescript({
       '/terms': { page: '/terms' },
       '/privacy': { page: '/privacy' },
       '/log': { page: '/log' },
-      '/post': { page: '/post' },
-      '/blog': { page: '/blog' },
     }
-
-    return addBlogPagesToPageObject(dir, pages)
   },
 })
