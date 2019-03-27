871ddb0799904e5d04c67a25a5ea22b282e09af6
Ben Werdmuller <ben@withknown.com>
2019-03-27 12:18:53 -0700

    Removing blog utils and pages from unlock-app (#2378)
    * Removing blog utils from unlock-app
    
    * Returning pages
    
    * More removals: tests, stories, and components
    
    * Storyshots
    
    * First pass the post

--- unlock-app/src/__tests__/pages/pages.test.js
+++ unlock-app/src/__tests__/pages/pages.test.js
@@ -12,8 +12,6 @@ import Terms from '../../pages/terms'
 
 import { pageTitle, ETHEREUM_NETWORKS_NAMES } from '../../constants'
 import createUnlockStore from '../../createUnlockStore'
-import Blog from '../../pages/blog'
-import Post from '../../pages/post'
 import { ConfigContext } from '../../utils/withConfig'
 import configure from '../../config'
 
@@ -150,65 +148,6 @@ describe('Pages', () => {
         </ConfigProvider>
       )
       expect(pageTitle).toBeCalledWith('Terms of Service')
-    })
-  })
-
-  describe('Blog', () => {
-    it('should render title correctly', () => {
-      expect.assertions(1)
-      rtl.render(
-        <Provider store={store}>
-          <Blog posts={[]} />
-        </Provider>
-      )
-
-      expect(pageTitle).toBeCalledWith('Blog')
-    })
-    it('should render post preview correctly', () => {
-      expect.assertions(3)
-
-      const posts = [
-        {
-          slug: 'test1',
-          title: 'Test post',
-          publishDate: 'November 23, 1963',
-          authorName: 'Susan Foreman',
-          description: 'One day, I will come back.',
-        },
-      ]
-
-      const page = rtl.render(
-        <Provider store={store}>
-          <Blog posts={posts} />
-        </Provider>
-      )
-
-      expect(page.queryByText(posts[0].title)).not.toBeNull()
-      expect(page.queryByText(posts[0].authorName)).not.toBeNull()
-      expect(page.queryByText(posts[0].description)).not.toBeNull()
-    })
-  })
-
-  describe('Post', () => {
-    it('should render title correctly', () => {
-      expect.assertions(3)
-      const post = {
-        title: 'Test post',
-        slug: 'test1',
-        description: 'Test description',
-        publishDate: 'November 23, 1963',
-        __content: 'Now is the limiter of this content',
-      }
-      const slug = 'test1'
-      const page = rtl.render(
-        <Provider store={store}>
-          <Post post={post} slug={slug} />
-        </Provider>
-      )
-
-      expect(pageTitle).toBeCalledWith(post.title)
-      expect(page.queryByText(post.publishDate)).not.toBeNull()
-      expect(page.queryByText(post.__content)).not.toBeNull()
     })
   })
 })
