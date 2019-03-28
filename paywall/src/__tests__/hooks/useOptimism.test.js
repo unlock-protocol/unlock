50d8c596aa5219444d06969514a9a59949884e67
Gregory Beaver <greg@unlock-protocol.com>
2019-03-28 15:22:55 -0400

    fully test, and 15 second timeout

--- paywall/src/__tests__/hooks/useOptimism.test.js
+++ paywall/src/__tests__/hooks/useOptimism.test.js
@@ -5,9 +5,10 @@ import useOptimism from '../../hooks/useOptimism'
 import useLocksmith from '../../hooks/useLocksmith'
 
 jest.mock('../../hooks/useLocksmith')
+jest.useFakeTimers()
 
 describe('useOptimism hook', () => {
-  function MockOptimismn() {
+  function MockOptimism() {
     const result = useOptimism('0x123')
 
     return <div>{JSON.stringify(result)}</div>
@@ -15,25 +16,41 @@ describe('useOptimism hook', () => {
 
   beforeEach(() => {
     useLocksmith.mockImplementation(() => ({
-      willSucceed: 0,
+      willSucceed: 1,
     }))
   })
 
   it('calls useLocksmith properly', () => {
     expect.assertions(1)
 
-    rtl.render(<MockOptimismn />)
+    rtl.render(<MockOptimism />)
 
     expect(useLocksmith).toHaveBeenCalledWith('/transaction/0x123/odds', {
-      willSucceed: 1,
+      willSucceed: 0,
     })
   })
 
   it('returns the result of useLocksmith', () => {
     expect.assertions(1)
 
-    const wrapper = rtl.render(<MockOptimismn />)
+    const wrapper = rtl.render(<MockOptimism />)
 
-    expect(wrapper.getByText('0')).not.toBeNull()
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 1, past: 0 }))
+    ).not.toBeNull()
+  })
+
+  it('freaks out after 15 seconds', () => {
+    expect.assertions(1)
+
+    let wrapper
+    rtl.act(() => {
+      wrapper = rtl.render(<MockOptimism />)
+      jest.runAllTimers()
+    })
+
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 0, past: 1 }))
+    ).not.toBeNull()
   })
 })
