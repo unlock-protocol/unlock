a168885d2b669115bd2af598e530f577c4d24a27
Gregory Beaver <greg@unlock-protocol.com>
2019-03-28 16:11:54 -0400

    If no <meta> lock tags exist, don't block the page (#2412)
    * If no <meta> lock tags exist, don't block the page
    
    * explanatory comments

--- paywall/src/paywall-builder/mutationObserver.js
+++ paywall/src/paywall-builder/mutationObserver.js
@@ -4,25 +4,12 @@
  */
 import { findLocks } from './script'
 
-export function changeListener(callback, list) {
-  list.forEach(mutation => {
-    if (!mutation.addedNodes || !mutation.addedNodes.length) return
-    const entries = mutation.addedNodes.entries()
-    while (true) {
-      const info = entries.next()
-      if (info.done) break
-      const entry = info.value[1]
-      if (entry.nodeName !== 'META' || entry.name !== 'lock') continue
-      callback(entry.content)
-    }
-  })
-}
-
-export function listenForNewLocks(callback, head) {
+export default function listenForNewLocks(callback, fail, head) {
   const existingLock = findLocks(head)
   if (existingLock) {
     return callback(existingLock)
   }
-  const observer = new MutationObserver(changeListener.bind(null, callback))
-  observer.observe(head, { childList: true, attributes: true })
+
+  // if we reach here, there were no locks found on the page, so we call the fail callback
+  fail()
 }
