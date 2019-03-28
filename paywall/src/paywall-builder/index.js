8102d50e2b1dc046d99a7d016e697a3d1b4190a7
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-03-28 16:11:54 -0400

    If no <meta> lock tags exist, don't block the page (#2412)
    * If no <meta> lock tags exist, don't block the page
    
    * explanatory comments

--- paywall/src/paywall-builder/index.js
+++ paywall/src/paywall-builder/index.js
@@ -1,5 +1,5 @@
-import { listenForNewLocks } from './mutationObserver'
-import { getBlocker, addBlocker } from './blocker'
+import listenForNewLocks from './mutationObserver'
+import { getBlocker, addBlocker, removeBlocker } from './blocker'
 import buildPaywall from './build'
 
 window.onload = () => {
@@ -7,6 +7,8 @@ window.onload = () => {
   addBlocker(document, blocker)
   listenForNewLocks(
     lock => buildPaywall(window, document, lock, blocker),
+    // our fail callback removes the blocker, since the paywall is useless without a lock to display
+    () => removeBlocker(blocker),
     document.head
   )
 }
