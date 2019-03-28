08afc7090ba1f13f36c81a614e211b1f1d188b28
Christoph Nascone <chris@unlock-protocol.com>
2019-03-28 10:52:12 -0400

    Make tests pass

--- locksmith/src/types.ts
+++ locksmith/src/types.ts
@@ -5,9 +5,8 @@ export interface UserCreationInput {
   recoveryPhrase: String
 }
 
-// eslint chokes on this file when there is only one thing exported, because it
-// thinks default exports are necessary for files that only export one
-// thing. You can't do a default export of an interface, so here we are.
-export interface Dummy {
+export interface Lock {
+  address: string
   name: string
+  owner: string
 }
