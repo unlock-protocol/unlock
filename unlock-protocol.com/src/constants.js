10535216fa2a75e98a85373ce7b8e3e28a18cb18
julien <julien.genestoux@gmail.com>
2019-03-28 14:57:42 -0400

    final config for the unlock-protocol.com static site

--- unlock-protocol.com/src/constants.js
+++ unlock-protocol.com/src/constants.js
@@ -18,8 +18,6 @@ export const PAGE_DEFAULT_IMAGE =
   'https://unlock-protocol.com/static/images/pages/png/simple.png'
 
 export const CANONICAL_BASE_URL = 'https://unlock-protocol.com' // Leave trailing slash off
-export const CANONICAL_BASE_DEV_URL = 'http://localhost:3000'
-export const CANONICAL_BASE_STAGING_URL = 'https://staging.unlock-protocol.com'
 
 export const MAX_DEVICE_WIDTHS = {
   PHONE: 736,
