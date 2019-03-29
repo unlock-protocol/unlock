9581a0d5cc1786e2d02946e1d2d643e16276fdf5
Ben Werdmuller <ben@withknown.com>
2019-03-29 15:03:01 -0700

    Adding a confirmation email for account recovery (#2428)
    * Recovery key confirmation email
    
    * Lint fix

--- wedlocks/src/templates/index.js
+++ wedlocks/src/templates/index.js
@@ -2,10 +2,12 @@ import confirmEmail from './confirmEmail'
 import ejectedEmail from './ejectedEmail'
 import ejectionWarningEmail from './ejectionWarningEmail'
 import recoveryKeyEmail from './recoveryKeyEmail'
+import recoveryKeyConfirmEmail from './recoveryKeyConfirmEmail'
 
 export default {
   confirmEmail,
   ejectedEmail,
   ejectionWarningEmail,
   recoveryKeyEmail,
+  recoveryKeyConfirmEmail,
 }
