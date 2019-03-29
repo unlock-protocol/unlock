01dc594ea6772282ea7340360dbbadec8c04f56e
Gregory Beaver <greg@unlock-protocol.com>
2019-03-29 16:26:23 -0400

    retrieve the most current optimism value for key purchase success from locksmith (#2415)
    * new useLocksmith hook
    
    * useOptimism hook for querying the optimistic unlocking API on locksmith
    
    * begin work on client-side optimism
    
    * fully test, and 15 second timeout
    
    * reverse crap needed for rebase
    
    * fully working optimism polling
    
    * address PR concerns

---
+++ paywall/src/__tests__/hooks/useOptimism.test.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import * as rtl from 'react-testing-library'
+
+import useOptimism from '../../hooks/useOptimism'
+import { WindowContext } from '../../hooks/browser/useWindow'
+import { ConfigContext } from '../../utils/withConfig'
+import configure from '../../config'
+
+const WindowProvider = WindowContext.Provider
+const ConfigProvider = ConfigContext.Provider
+const config = configure()
+
+jest.useFakeTimers()
+
+describe('useOptimism hook', () => {
+  let transaction
+  let fakeWindow
+  let finishFetch
+  let fakeResponse
+  const fetchResponse = {
+    json: () => fakeResponse,
+  }
+
+  function MakeOptimism() {
+    const result = useOptimism(transaction)
+
+    return <div>{JSON.stringify(result)}</div>
+  }
+
+  function MockOptimism() {
+    return (
+      <WindowProvider value={fakeWindow}>
+        <ConfigProvider value={config}>
+          <MakeOptimism />
+        </ConfigProvider>
+      </WindowProvider>
+    )
+  }
+
+  beforeEach(() => {
+    fakeResponse = {
+      willSucceed: 1,
+    }
+    fakeWindow = {
+      fetch: jest.fn(() => {
+        return {
+          then: cb => {
+            finishFetch = cb
+            return {
+              catch: () => {},
+            }
+          },
+        }
+      }),
+    }
+    transaction = {
+      hash: '0x123',
+      status: 'pending',
+    }
+  })
+
+  it('returns the result of useLocksmith', () => {
+    expect.assertions(1)
+
+    let wrapper
+    rtl.act(() => {
+      wrapper = rtl.render(<MockOptimism />)
+      finishFetch(fetchResponse)
+    })
+
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 1, past: 0 }))
+    ).not.toBeNull()
+  })
+
+  it('polls again after 15 seconds', () => {
+    expect.assertions(4)
+
+    let wrapper
+    rtl.act(() => {
+      wrapper = rtl.render(<MockOptimism />)
+      finishFetch(fetchResponse)
+      // ensure we are calling for the new fetcher
+      finishFetch = () => {}
+    })
+
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 1, past: 0 }))
+    ).not.toBeNull()
+    expect(fakeWindow.fetch).toHaveBeenCalledTimes(1)
+
+    fakeResponse = {
+      willSucceed: 0,
+    }
+
+    rtl.act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+    rtl.act(() => {
+      // initially this was combined with the pending timer run above, but
+      // finishFetch is not set until after leaving the rtl.act,
+      // so it must be in its own block to pass
+      finishFetch(fetchResponse)
+    })
+    expect(fakeWindow.fetch).toHaveBeenCalledTimes(2)
+
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 0, past: 1 }))
+    ).not.toBeNull()
+  })
+
+  it('does not fetch if transaction status is not pending', () => {
+    expect.assertions(1)
+
+    transaction.status = 'mined'
+
+    rtl.act(() => {
+      rtl.render(<MockOptimism />)
+    })
+
+    expect(fakeWindow.fetch).not.toHaveBeenCalled()
+  })
+
+  it('stops polling if transaction.status is no longer pending', () => {
+    expect.assertions(4)
+
+    let wrapper
+    rtl.act(() => {
+      wrapper = rtl.render(<MockOptimism />)
+      finishFetch(fetchResponse)
+      // ensure we are calling for the new fetcher
+      finishFetch = () => {}
+    })
+
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 1, past: 0 }))
+    ).not.toBeNull()
+    expect(fakeWindow.fetch).toHaveBeenCalledTimes(1)
+
+    fakeResponse = {
+      willSucceed: 0,
+    }
+
+    transaction.status = 'mined'
+    rtl.act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+    expect(fakeWindow.fetch).toHaveBeenCalledTimes(1)
+    expect(
+      wrapper.getByText(JSON.stringify({ current: 1, past: 0 }))
+    ).not.toBeNull()
+  })
+
+  it('does nothing if transaction is not set', () => {
+    expect.assertions(1)
+
+    transaction = null
+
+    rtl.act(() => {
+      rtl.render(<MockOptimism />)
+    })
+
+    expect(fakeWindow.fetch).not.toHaveBeenCalled()
+  })
+})
---
+++ paywall/src/hooks/useOptimism.js
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useReducer } from 'react'
+
+import useLocksmith from './useLocksmith'
+import { OPTIMISM_POLLING_INTERVAL } from '../constants'
+
+export default function useOptimism(transaction) {
+  const apiEndPoint = transaction ? `/transaction/${transaction.hash}/odds` : ''
+  const hash = transaction ? transaction.hash : false
+  const status = transaction ? transaction.status : 'inactive'
+  const [locksmithOptimism, reSendQuery] = useLocksmith(
+    apiEndPoint,
+    {
+      willSucceed: 0,
+    },
+    transaction && transaction.status === 'pending'
+  )
+  const [optimism, setOptimism] = useReducer(
+    (state, newState) => {
+      if (state.current !== newState) {
+        return {
+          current: newState,
+          past: state.current,
+        }
+      }
+      return state
+    },
+    { current: locksmithOptimism.willSucceed, past: 0 }
+  )
+  const timeout = useRef()
+  const reQuery = () => {
+    reSendQuery()
+    setTimeout(reQuery, OPTIMISM_POLLING_INTERVAL)
+  }
+  const endPolling = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current)
+      timeout.current = undefined
+    }
+  }
+  useEffect(() => {
+    if (!hash) {
+      endPolling()
+    } else {
+      // if we reach here, there is a chance that we have
+      // gotten a new optimism value from locksmith, so
+      // pass it to the reducer
+      setOptimism(locksmithOptimism.willSucceed)
+
+      // set up polling from locksmith, which will happen until the
+      // transaction is mined
+      if (status !== 'pending') {
+        endPolling()
+        return
+      }
+      setTimeout(() => {
+        reQuery()
+      }, OPTIMISM_POLLING_INTERVAL)
+    }
+  }, [hash, status, locksmithOptimism])
+
+  return optimism
+}
