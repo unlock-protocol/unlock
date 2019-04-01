c96d0bc1c58da642d71fab5d479fb1feb94d8b6a
Gregory Beaver <greg@unlock-protocol.com>
2019-04-01 19:13:10 -0400

    make optimistic locking fully operational (frontend-side)

--- paywall/src/__tests__/components/Paywall.test.js
+++ paywall/src/__tests__/components/Paywall.test.js
@@ -6,8 +6,13 @@ import { Paywall, mapStateToProps } from '../../components/Paywall'
 import { ConfigContext } from '../../utils/withConfig'
 import { WindowContext } from '../../hooks/browser/useWindow'
 import { POST_MESSAGE_SCROLL_POSITION } from '../../paywall-builder/constants'
+import { TRANSACTION_TYPES } from '../../constants'
+import useOptimism from '../../hooks/useOptimism'
 
 jest.useFakeTimers()
+jest.mock('../../hooks/useOptimism', () => {
+  return jest.fn(() => ({ current: 1, past: 0 }))
+})
 
 const lock = { address: '0x4983D5ECDc5cc0E499c2D23BF4Ac32B982bAe53a' }
 const locks = {
@@ -42,8 +47,15 @@ const keys = {
   },
 }
 const modals = []
+const transactions = {}
 
-const store = createUnlockStore({ locks, keys, modals, router })
+const store = createUnlockStore({
+  locks,
+  keys,
+  modals,
+  router,
+  transactions,
+})
 
 function renderMockPaywall(props = {}) {
   return rtl.render(
@@ -86,25 +98,49 @@ describe('Paywall', () => {
   describe('mapStateToProps', () => {
     it('should yield the lock which matches the address of the demo page', () => {
       expect.assertions(1)
-      const props = mapStateToProps({ locks, keys, modals, router })
+      const props = mapStateToProps({
+        locks,
+        keys,
+        modals,
+        router,
+        transactions,
+      })
       expect(props.locks[0]).toBe(lock)
     })
 
     it('should be locked when no keys are available', () => {
       expect.assertions(1)
-      const props = mapStateToProps({ locks, keys: {}, modals, router })
+      const props = mapStateToProps({
+        locks,
+        keys: {},
+        modals,
+        router,
+        transactions,
+      })
       expect(props.locked).toBe(true)
     })
 
     it('should not be locked when there is a matching key', () => {
       expect.assertions(1)
-      const props = mapStateToProps({ locks, keys, modals, router })
+      const props = mapStateToProps({
+        locks,
+        keys,
+        modals,
+        router,
+        transactions,
+      })
       expect(props.locked).toBe(false)
     })
 
     it('should pass redirect if present in the URI', () => {
       expect.assertions(1)
-      const props = mapStateToProps({ locks, keys, modals, router })
+      const props = mapStateToProps({
+        locks,
+        keys,
+        modals,
+        router,
+        transactions,
+      })
       expect(props.redirect).toBe('http://example.com')
     })
 
@@ -115,6 +151,7 @@ describe('Paywall', () => {
         keys,
         modals,
         router: noRedirectRouter,
+        transactions,
       })
       expect(props.redirect).toBeFalsy()
     })
@@ -130,8 +167,74 @@ describe('Paywall', () => {
           pathname: `/paywall/${lock.address}/http%3A%2F%2Fexample.com`,
         },
       }
-      const props = mapStateToProps({ locks, router, keys, modals })
+      const props = mapStateToProps({
+        locks,
+        router,
+        keys,
+        modals,
+        transactions,
+      })
       expect(props.redirect).toBe('http://example.com')
+    })
+
+    it('should return the transaction if applicable', () => {
+      expect.assertions(1)
+
+      const transaction = {
+        type: TRANSACTION_TYPES.KEY_PURCHASE,
+        status: 'pending',
+        hash: '0x777',
+        key: '0x123-0x456',
+      }
+      const newProps = mapStateToProps({
+        account: {
+          address: '0x123',
+        },
+        locks,
+        keys: {
+          '0x123-0x456': {
+            id: '0x123-0x456',
+            lock: lock.address,
+            owner: '0x123',
+            expiration: 0,
+            data: 'hello',
+          },
+        },
+        modals,
+        router,
+        transactions: {
+          '0x777': transaction,
+        },
+      })
+      expect(newProps.transaction).toEqual(transaction)
+    })
+  })
+
+  describe('uses optimism', () => {
+    beforeEach(() => {
+      config = { providers: [], isInIframe: true }
+      fakeWindow = {
+        location: {
+          pathname: `/${lock.address}`,
+          search: '?origin=http%3A%2F%2Fexample.com',
+          hash: '',
+        },
+        parent: { postMessage: jest.fn() },
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        document: {
+          body: {
+            style: {},
+          },
+        },
+      }
+    })
+
+    it('calls the useOptimism hook', () => {
+      expect.assertions(1)
+      renderMockPaywall({ locks: [lock] })
+
+      expect(useOptimism).toHaveBeenCalled()
     })
   })
 
