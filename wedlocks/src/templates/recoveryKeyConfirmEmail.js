45ac8338fb127c1373d9dea66e208ce12af60253
julien <julien.genestoux@gmail.com>
2019-04-01 16:28:07 -0400

    linting wedlocks

--- wedlocks/src/templates/recoveryKeyConfirmEmail.js
+++ wedlocks/src/templates/recoveryKeyConfirmEmail.js
@@ -2,9 +2,8 @@ export default {
   subject: () => 'Please confirm your account recovery',
   text: params =>
     `We received a request to recover your account using your recovery key.
-    
-If you did not make this request, please disregard this email. Otherwise, please click on the following link to confirm
-your account recovery request:
+
+If you did not make this request, please disregard this email. Otherwise, please click on the following link to confirm your account recovery request:
 
   ${params.recoveryKeyConfirmLink}
 
