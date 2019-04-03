f3656eb9df1b61ebdb20f666141279fc47d7e03c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-04-03 14:06:40 -0400

    Add providerReady action (#2488)
    * Add providerReady action
    
    * Should have a test

--- unlock-app/src/actions/provider.js
+++ unlock-app/src/actions/provider.js
@@ -1,6 +1,11 @@
 export const SET_PROVIDER = 'provider/SET_PROVIDER'
+export const PROVIDER_READY = 'provider/PROVIDER_READY'
 
 export const setProvider = provider => ({
   type: SET_PROVIDER,
   provider,
+})
+
+export const providerReady = () => ({
+  type: PROVIDER_READY,
 })
