c448ff0d470319c57fc13f409d98a84d669a4491
Christopher Nascone <cnasc@users.noreply.github.com>
2019-04-03 11:39:09 -0400

    Provider Middleware (#2479)
    * Initial work on provider middleware
    
    * Simplify initializeProvider
    
    * Handle case where enable fails
    
    * next cannot be inside the if

---
+++ unlock-app/src/__tests__/middlewares/providerMiddleware.test.ts
@@ -0,0 +1,102 @@
+import providerMiddleware from '../../middlewares/providerMiddleware'
+import { SET_PROVIDER } from '../../actions/provider'
+import { setError } from '../../actions/error'
+import {
+  FATAL_MISSING_PROVIDER,
+  FATAL_NOT_ENABLED_IN_PROVIDER,
+} from '../../errors'
+
+const config = {
+  providers: {
+    UNLOCK: {
+      enable: jest.fn(),
+      isUnlock: true,
+    },
+    NUNLOCK: {
+      enable: jest.fn(),
+    },
+  },
+}
+
+const getState = () => ({
+  provider: 'NUNLOCK',
+})
+
+const unlockAction = {
+  type: SET_PROVIDER,
+  provider: 'UNLOCK',
+}
+
+const erroneousAction = {
+  type: SET_PROVIDER,
+  provider: 'HONLOCK',
+}
+
+const sameAction = {
+  type: SET_PROVIDER,
+  provider: 'NUNLOCK',
+}
+
+let dispatch: () => any
+
+describe('provider middleware', () => {
+  beforeEach(() => {
+    config.providers['UNLOCK'].enable = jest.fn()
+    config.providers['NUNLOCK'].enable = jest.fn()
+    dispatch = jest.fn()
+  })
+  describe('SET_PROVIDER', () => {
+    it('should initialize the provider when provider is different from one in state', done => {
+      expect.assertions(2)
+      const next = () => {
+        expect(config.providers['UNLOCK'].enable).toHaveBeenCalled()
+        expect(config.providers['NUNLOCK'].enable).not.toHaveBeenCalled()
+        done()
+      }
+
+      providerMiddleware(config)({ getState, dispatch })(next)(unlockAction)
+    })
+
+    it('should set an error and return if there is no matching provider', done => {
+      expect.assertions(3)
+      const next = () => {
+        expect(config.providers['UNLOCK'].enable).not.toHaveBeenCalled()
+        expect(config.providers['NUNLOCK'].enable).not.toHaveBeenCalled()
+        expect(dispatch).toHaveBeenCalledWith(setError(FATAL_MISSING_PROVIDER))
+        done()
+      }
+
+      providerMiddleware(config)({ getState, dispatch })(next)(erroneousAction)
+    })
+
+    it('should set an error and return if the call to enable fails', () => {
+      expect.assertions(3)
+
+      config.providers['UNLOCK'].enable = jest.fn(() => {
+        throw new Error('The front fell off.')
+      })
+
+      const next = () => {
+        expect(config.providers['UNLOCK'].enable).toHaveBeenCalled()
+        expect(config.providers['NUNLOCK'].enable).not.toHaveBeenCalled()
+        expect(dispatch).toHaveBeenCalledWith(
+          setError(FATAL_NOT_ENABLED_IN_PROVIDER)
+        )
+      }
+
+      providerMiddleware(config)({ getState, dispatch })(next)(unlockAction)
+    })
+
+    it('should do nothing if provider is the same as in state', done => {
+      expect.assertions(3)
+      const next = () => {
+        expect(config.providers['UNLOCK'].enable).not.toHaveBeenCalled()
+        expect(config.providers['NUNLOCK'].enable).not.toHaveBeenCalled()
+        expect(dispatch).not.toHaveBeenCalled()
+        done()
+      }
+
+      providerMiddleware(config)({ getState, dispatch })(next)(sameAction)
+    })
+  })
+})
