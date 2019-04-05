cb9f596c09ffe2e2e0f812113180ab4b91c0a176
Gregory Beaver <greg@unlock-protocol.com>
2019-04-05 17:26:38 -0400

    implement unlocked flag, confirmed flag, confirming flag

--- paywall/src/components/lock/FlagStyles.js
+++ paywall/src/components/lock/FlagStyles.js
@@ -1,38 +1,19 @@
 import styled from 'styled-components'
 import Media from '../../theme/media'
 
-export const OptimisticFlag = styled.div`
-  height: 80px;
-  width: 120px;
-  float: right;
-  box-shadow: 14px 0px 40px rgba(0, 0, 0, 0.08);
-  background: var(--white);
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  position: relative;
-
-  ${Media.phone`
-    justify-self: center;
-    box-shadow: 14px 0px 40px rgba(0, 0, 0, 0.14);
-  `}
-`
-
 export const OptimisticLogo = styled.div`
-  position: absolute;
-  top: 26px;
-  left: -14px;
-  height: 28px;
-  width: 28px;
+  grid-row 1 / span 2;
+  margin-left: -14px;
+  align-self: center;
 `
 
 export const ProgressBar = styled.div`
+  align-self: start;
   width: 74px;
   height: 8px;
   background: var(--lightgrey);
   position: relative;
+  grid-column: 2;
 `
 
 export const Progress = styled(ProgressBar)`
@@ -52,14 +33,98 @@ export const Progress = styled(ProgressBar)`
   }};
 `
 
-export const FlagContent = styled.div`
-  display: flex;
-  width: 74px;
-  flex-direction: column;
-  margin-left: 14px;
-  font-size: 12px;
+export const OptimisticFlag = styled.div`
+  height: 80px;
+  width: 120px;
+  float: right;
+  box-shadow: 14px 0px 40px rgba(0, 0, 0, 0.08);
+  background: var(--white);
+  margin: auto;
+  display: grid;
+  grid-template-columns: 25px 1fr;
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+
   & > p {
+    margin-bottom: 5px;
+    font-size: 12px;
     text-align: center;
-    margin-bottom: 3px;
   }
+
+  & > ${Progress}, & > ${ProgressBar} {
+    margin-left: 10px;
+  }
+
+  ${Media.phone`
+    font-size: 10px;
+    display: grid;
+    height: 43px;
+    grid-template-columns: 1.6fr 1fr 1fr;
+    grid-template-rows: 43px;
+    width: 100%;
+    align-items: center;
+
+    & > ${OptimisticLogo} {
+      display: none;
+    }
+
+    & > ${Progress}, & > ${ProgressBar} {
+      align-self: center;
+    }
+
+    & > p {
+      margin-bottom: inherit;
+    }
+  `}
+`
+
+export const PoweredByUnlock = styled.div`
+  display: none;
+  ${Media.phone`
+    font-size: 10px;
+    width: 170px;
+    display: grid;
+    grid-template-columns: 1fr 18px 1fr;
+    grid-template-rows: 1fr;
+
+    font-size: 12px;
+
+
+    & > div {
+      grid-row: 1;
+      grid-column: 2;
+      margin-left: 0;
+      align-self: center;
+    }
+
+    & > p {
+      width: 100%;
+      color: var(--slate);
+      grid-row: 1;
+      grid-column: 1;
+      text-align: right;
+      margin: 0;
+      align-self: center;
+      padding-right: 2px;
+    }
+
+    & > a {
+      grid-row: 1;
+      grid-column: 3;
+      justify-self: start;
+      align-self: center;
+      color: var(--red);
+    }
+`}
+`
+
+export const ConfirmedKeyWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-self: start;
+  ${Media.phone`
+    align-self: initial;
+  `}
 `
