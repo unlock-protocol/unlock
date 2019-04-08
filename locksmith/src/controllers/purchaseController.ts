5490eb18c6ed0f75881a9f5682275894ac59c964
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-04-08 17:21:21 -0400

    Rejecting signatures accompanying purchase requests that have expired (#2559)
    Signatures with out an expiry check are subject to replay. The associated risk is attached to the use case of the signature.
    
    In our case, we would like to avoid signature replay here.
    
    It may be worth moving this to middleware but lets see if we have a further use case first.

--- locksmith/src/controllers/purchaseController.ts
+++ locksmith/src/controllers/purchaseController.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express-serve-static-core' // eslint-disable-
 
 namespace PurchaseController {
   //eslint-disable-next-line import/prefer-default-export
-  export const purchase = async (
-    _req: Request,
-    res: Response
-  ): Promise<any> => {
+  export const purchase = async (req: Request, res: Response): Promise<any> => {
+    let currentTime = Math.floor(Date.now() / 1000)
+    const expiry = req.body.message.purchaseRequest.expiry
+
+    if (expiry < currentTime) {
+      return res.sendStatus(412)
+    }
+
     return res.sendStatus(202)
   }
 }
