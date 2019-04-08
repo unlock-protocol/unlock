2442c5c20cf94e1aab3b84abdfb1b8322bde433b
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-04-08 17:44:09 -0400

    Shifting base64.js to ts (#2560)
    * Shifting base64.js to ts

--- locksmith/src/utils/base64.ts
+++ locksmith/src/utils/base64.ts
@@ -1,9 +1,9 @@
-exports.decode = data => {
+export function decode(data: string) {
   const buff = Buffer.from(data, 'base64')
   return buff.toString('utf-8')
 }
 
-exports.encode = data => {
+export function encode(data: string) {
   const buff = Buffer.from(data, 'utf-8')
   return buff.toString('base64')
 }
