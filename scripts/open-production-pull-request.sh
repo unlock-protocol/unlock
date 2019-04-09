d8c18314cf7bcbd1e75ee716472f33f87ccabc4d
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-09 18:39:08 -0400

    fixing production deploys to avoid conflicts (#2582)

--- scripts/open-production-pull-request.sh
+++ scripts/open-production-pull-request.sh
@@ -4,12 +4,21 @@
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch >> /dev/null
 
-echo "Checking out 4 days ago"
-git checkout `git rev-list -1 --before={4.days.ago} master`
+echo "Checking master out 4 days ago"
+STABLE_MASTER=`git rev-list -1 --before={4.days.ago} master`
+git checkout $STABLE_MASTER
 
 echo "Creating new production branch"
 BRANCH="production-$(date +%Y%m%d-%H%M%S)"
 git checkout -b $BRANCH
+
+echo "Diffing versus latest production"
+LATEST_PRODUCTION=`git rev-parse origin/production`
+git reset --soft $LATEST_PRODUCTION
+
+echo "Committing diff"
+COMMIT_MESSAGE="Automated deploy between $LATEST_PRODUCTION and $STABLE_MASTER"
+git commit -m $COMMIT_MESSAGE -a --no-verify
 
 echo "Push new production branch"
 git push origin $BRANCH
