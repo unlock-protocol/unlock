d6032fd3a68cd88f20c53d9c174d5a5d117b75df
Michael J. Cohen <mcohen@criterion360.com>
2019-04-10 09:58:11 -0400

    Add message to log page to indicate there are no transactions (#2538)
    * implement default error for no transactions on creator log
    
    * link log component to loading state
    
    * add test coverage for no transactions in log
    
    * add loading component to log and refactor out for reuse
    
    * remove unused/commented code
    
    * implement suggested edits

--- unlock-app/src/components/creator/CreatorLog.tsx
+++ unlock-app/src/components/creator/CreatorLog.tsx
@@ -1,34 +1,68 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 import * as UnlockTypes from '../../unlockTypes'
+import { DefaultError } from './FatalError'
+import Loading from '../interface/Loading'
 
 interface Props {
   transactionFeed: UnlockTypes.Transaction[]
   explorerLinks: { [key: string]: string }
+  loading: boolean
 }
 
-const CreatorLog = ({ transactionFeed, explorerLinks }: Props) => {
+export const CreatorLog = ({
+  transactionFeed,
+  explorerLinks,
+  loading,
+}: Props) => {
   return (
-    <Grid>
-      <HeaderItem>Block Number</HeaderItem>
-      <HeaderItem>Lock Name/Address</HeaderItem>
-      <HeaderItem>Type</HeaderItem>
-      {transactionFeed.map(tx => {
-        return (
-          <React.Fragment key={tx.hash}>
-            <BlockNumber>{tx.blockNumber}</BlockNumber>
-            <Address href={explorerLinks[tx.hash]} target="_blank">
-              {tx.lock}
-            </Address>
-            <Type type={tx.type}>{tx.type}</Type>
-          </React.Fragment>
-        )
-      })}
-    </Grid>
+    <React.Fragment>
+      <Grid>
+        <HeaderItem>Block Number</HeaderItem>
+        <HeaderItem>Lock Name/Address</HeaderItem>
+        <HeaderItem>Type</HeaderItem>
+        {transactionFeed.length > 0 &&
+          transactionFeed.map(tx => {
+            return (
+              <React.Fragment key={tx.hash}>
+                <BlockNumber>{tx.blockNumber}</BlockNumber>
+                <Address href={explorerLinks[tx.hash]} target="_blank">
+                  {tx.lock}
+                </Address>
+                <Type type={tx.type}>{tx.type}</Type>
+              </React.Fragment>
+            )
+          })}
+      </Grid>
+      {transactionFeed.length === 0 && !loading && (
+        <DefaultError
+          title="Your log is empty: No transactions yet."
+          illustration="/static/images/illustrations/mug.svg"
+          critical={false}
+        >
+          Check back after you&#8216;ve created a lock.
+        </DefaultError>
+      )}
+      {loading && <Loading />}
+    </React.Fragment>
   )
 }
 
-export default CreatorLog
+interface State {
+  loading: UnlockTypes.Loading
+}
+
+export const mapStateToProps = ({ loading }: State) => {
+  return {
+    loading: !!loading,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(CreatorLog)
 
 const Grid = styled.div`
   display: grid;
