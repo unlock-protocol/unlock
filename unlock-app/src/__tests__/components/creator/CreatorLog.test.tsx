d52cf72b21acd67d8f4537835a4d961a885ab425
Michael J. Cohen <mcohen@criterion360.com>
2019-04-10 09:58:11 -0400

    Add message to log page to indicate there are no transactions (#2538)
    * implement default error for no transactions on creator log
    
    * link log component to loading state
    
    * add test coverage for no transactions in log
    
    * add loading component to log and refactor out for reuse
    
    * remove unused/commented code
    
    * implement suggested edits

--- unlock-app/src/__tests__/components/creator/CreatorLog.test.tsx
+++ unlock-app/src/__tests__/components/creator/CreatorLog.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as rtl from 'react-testing-library'
-import CreatorLog from '../../../components/creator/CreatorLog'
+
+import { CreatorLog } from '../../../components/creator/CreatorLog'
 import * as UnlockTypes from '../../../unlockTypes'
 
 const transactionFeed: UnlockTypes.Transaction[] = [
@@ -15,35 +16,69 @@ const transactionFeed: UnlockTypes.Transaction[] = [
   },
 ]
 
+const emptyTransactionFeed: UnlockTypes.Transaction[] = []
+
 const explorerLinks: { [key: string]: string } = {
   '0x123': 'http://www.this_is_my_href.com/',
 }
 
-let wrapper: rtl.RenderResult<typeof rtl.queries>
-
-afterEach(rtl.cleanup)
 describe('CreatorLog', () => {
-  beforeEach(() => {
-    wrapper = rtl.render(
+  it('should show a message indicating that no transactions have been logged when none have been executed', () => {
+    expect.assertions(1)
+    const wrapper = rtl.render(
       <CreatorLog
-        transactionFeed={transactionFeed}
+        transactionFeed={emptyTransactionFeed}
         explorerLinks={explorerLinks}
+        loading={false}
       />
     )
+
+    expect(
+      wrapper.getByText('Your log is empty: No transactions yet.')
+    ).not.toBeNull()
   })
 
   it('should show the blockNumber of the transaction', () => {
     expect.assertions(1)
+    const wrapper = rtl.render(
+      <CreatorLog
+        transactionFeed={transactionFeed}
+        explorerLinks={explorerLinks}
+        loading={false}
+      />
+    )
+
     expect(wrapper.queryByText('1205')).not.toBeNull()
   })
 
   it('should show the lock address, which is a link containing the href', () => {
     expect.assertions(2)
+    const wrapper = rtl.render(
+      <CreatorLog
+        transactionFeed={transactionFeed}
+        explorerLinks={explorerLinks}
+        loading={false}
+      />
+    )
+
     const { queryByText, container } = wrapper
     expect(queryByText('0x456')).not.toBeNull()
     const link = container.querySelector('a')
     if (link) {
       expect(link.href).toBe('http://www.this_is_my_href.com/')
     }
+  })
+
+  it('should show the loading icon when transactions are being loaded', () => {
+    expect.assertions(1)
+    const wrapper = rtl.render(
+      <CreatorLog
+        transactionFeed={emptyTransactionFeed}
+        explorerLinks={explorerLinks}
+        loading
+      />
+    )
+
+    expect(wrapper.getByText('loading')).not.toBeNull()
   })
 })
