108b05749d7dc0d41bf36f65f88fb3e6ef87fc6c
Ben Werdmuller <ben@withknown.com>
2019-04-11 09:37:22 -0700

    Making more of next.config.js testable (#2601)

---
+++ unlock-app/src/__tests__/utils/exportStatic.test.js
@@ -0,0 +1,53 @@
+describe('exportStatic', () => {
+  beforeEach(() => {
+    jest.resetModules()
+
+    jest.mock('fs', () => ({
+      copyFileSync: jest.fn(() => {}),
+    }))
+  })
+
+  it('should copy files when not in dev mode', async () => {
+    expect.assertions(7)
+
+    const { copyFileSync } = require('fs')
+    const { moveStaticFiles } = require('../../utils/exportStatic')
+    moveStaticFiles(false, '/foo/bar', '/alice/bob')
+
+    expect(copyFileSync.mock.calls.length).toBe(3)
+    expect(copyFileSync.mock.calls[0][0]).toBe('/foo/bar/static/robots.txt')
+    expect(copyFileSync.mock.calls[0][1]).toBe('/alice/bob/robots.txt')
+    expect(copyFileSync.mock.calls[1][0]).toBe('/foo/bar/static/humans.txt')
+    expect(copyFileSync.mock.calls[1][1]).toBe('/alice/bob/humans.txt')
+    expect(copyFileSync.mock.calls[2][0]).toBe('/foo/bar/static/_redirects')
+    expect(copyFileSync.mock.calls[2][1]).toBe('/alice/bob/_redirects')
+  })
+
+  it('should not copy files when in dev mode', () => {
+    expect.assertions(1)
+
+    const { copyFileSync } = require('fs')
+    const { moveStaticFiles } = require('../../utils/exportStatic')
+    moveStaticFiles(true, '/foo/bar', '/alice/bob')
+
+    expect(copyFileSync.mock.calls.length).toBe(0)
+  })
+
+  it('should generate path map', () => {
+    expect.assertions(1)
+    const { exportPaths } = require('../../utils/exportStatic')
+
+    const paths = exportPaths({}, { dev: true, dir: '/foo', outDir: '/bar' })
+
+    expect(paths).toEqual({
+      '/': { page: '/home' },
+      '/about': { page: '/about' },
+      '/jobs': { page: '/jobs' },
+      '/dashboard': { page: '/dashboard' },
+      '/keychain': { page: '/keyChain' },
+      '/terms': { page: '/terms' },
+      '/privacy': { page: '/privacy' },
+      '/log': { page: '/log' },
+    })
+  })
+})
