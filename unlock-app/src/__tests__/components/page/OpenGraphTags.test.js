486dcac6bf30fb149660f68e159ab05b7650fcfd
Ben Werdmuller <ben@withknown.com>
2019-04-11 13:06:43 -0700

    Improving code coverage stats for social tag components (#2603)
    * Improving code coverage stats for social tag components
    
    * Adding a dumb space
    
    * Bringing tags in line

--- unlock-app/src/__tests__/components/page/OpenGraphTags.test.js
+++ unlock-app/src/__tests__/components/page/OpenGraphTags.test.js
@@ -10,7 +10,14 @@ import OpenGraphTags from '../../../components/page/OpenGraphTags'
 describe('OpenGraphTags', () => {
   it('should render open graph tags based on default values', () => {
     expect.assertions(5)
-    const tags = rtl.render(<OpenGraphTags />)
+    const tags = rtl.render(
+      <OpenGraphTags
+        title={null}
+        description={null}
+        image={null}
+        canonicalPath={null}
+      />
+    )
     expect(
       tags.container.querySelector("meta[property='og:title']").content
     ).toBe(pageTitle())
