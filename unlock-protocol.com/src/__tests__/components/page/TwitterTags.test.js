1781b5e1cc03e77d68c5c7801099125a9c40daa8
Ben Werdmuller <ben@benwerd.com>
2019-04-10 17:29:15 -0700

    Improving code coverage stats for social tag components

--- unlock-protocol.com/src/__tests__/components/page/TwitterTags.test.js
+++ unlock-protocol.com/src/__tests__/components/page/TwitterTags.test.js
@@ -10,7 +10,9 @@ import TwitterTags from '../../../components/page/TwitterTags'
 describe('TwitterTags', () => {
   it('should render twitter tags based on default values', () => {
     expect.assertions(3)
-    const tags = rtl.render(<TwitterTags />)
+    const tags = rtl.render(
+      <TwitterTags title={null} description={null} image={null}/>
+    )
     expect(
       tags.container.querySelector("meta[name='twitter:title']").content
     ).toBe(pageTitle())
@@ -28,7 +30,7 @@ describe('TwitterTags', () => {
     let description = 'I am the very model of a model view controller'
     let image = '/some/image.png'
     const tags = rtl.render(
-      <TwitterTags title={title} description={description} image={image} />
+      <TwitterTags title={title} description={description} image={image}/>
     )
     expect(
       tags.container.querySelector("meta[name='twitter:title']").content
