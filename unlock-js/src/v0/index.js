a05d2c5bd219411db6eb78cd0274bbfd5517fb45
julien <julien.genestoux@gmail.com>
2019-04-14 19:45:18 -0400

    isolating version dependent logic

---
+++ unlock-js/src/v0/index.js
@@ -0,0 +1,23 @@
+import createLock from './createLock'
+import updateKeyPrice from './updateKeyPrice'
+import purchaseKey from './purchaseKey'
+import partialWithdrawFromLock from './partialWithdrawFromLock'
+import withdrawFromLock from './withdrawFromLock'
+import getPastLockTransactions from './getPastLockTransactions'
+import getKeysForLockOnPage from './getKeysForLockOnPage'
+import getPastLockCreationsTransactionsForUser from './getPastLockCreationsTransactionsForUser'
+import getLock from './getLock'
+import getKeyByLockForOwner from './getKeyByLockForOwner'
+
+export default {
+  createLock,
+  updateKeyPrice,
+  purchaseKey,
+  partialWithdrawFromLock,
+  withdrawFromLock,
+  getPastLockTransactions,
+  getKeysForLockOnPage,
+  getPastLockCreationsTransactionsForUser,
+  getLock,
+  getKeyByLockForOwner,
+}
