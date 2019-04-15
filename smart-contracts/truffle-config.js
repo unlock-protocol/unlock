b9427479506f8087a11f2a43b881e340271efd23
hardlydifficult <hardlydiff@gmail.com>
2019-04-15 10:07:00 -0700

    Switch truffle provider

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -42,9 +42,6 @@ if (mainnetProviderUrl) {
   mainnetMnemonic = require('./mnemonic.mainnet') // eslint-disable-line import/no-unresolved
 }
 
-// When running CI, we connect to the 'ganache' container
-const testHost = process.env.CI ? 'ganache' : '127.0.0.1'
-
 const mainnetProvider = function() {
   return new HDWalletProvider(
     mainnetMnemonic.seed,
@@ -73,8 +70,7 @@ module.exports = {
   networks: {
     development: {
       // used for local dev
-      host: testHost,
-      port: 8545,
+      provider: ganacheProvider,
       network_id: '*', // Match any network id
     },
     rinkeby: {
