c429bf99d359668408bccd8f4a20c6bc72d9dc30
Christopher Nascone <cnasc@users.noreply.github.com>
2019-04-15 11:16:41 -0400

    Add basic wedlocks service (#2643)
    * Add Wedlocks Service
    
    * Initial WedlocksService class
    
    * Use proper headers
    
    * Testing the service
    
    * rename these files

---
+++ unlock-app/src/__tests__/services/wedlockService.test.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import WedlockService, { emailTemplate } from '../../services/wedlockService'
+
+jest.mock('axios')
+
+let w
+describe('Wedlocks Service', () => {
+  beforeEach(() => {
+    w = new WedlockService('http://notareal.host')
+  })
+
+  it('should should request an email confirmation, with the right headers', async () => {
+    expect.assertions(1)
+    const recipient = 'thomas.elphinstone@hambled.on'
+    const expectedPayload = {
+      template: emailTemplate.signupConfirmation,
+      recipient,
+      params: {},
+    }
+    axios.post.mockReturnValue()
+    await w.confirmEmail(recipient)
+
+    expect(axios.post).toHaveBeenCalledWith(
+      'http://notareal.host',
+      expectedPayload,
+      {
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    )
+  })
+})
