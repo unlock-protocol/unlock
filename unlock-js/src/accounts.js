e72b2ca7a00f1ebaef1610c5f03e98b2cd72a479
Gregory Beaver <greg@unlock-protocol.com>
2019-04-16 11:15:05 -0400

    test accounts

--- unlock-js/src/accounts.js
+++ unlock-js/src/accounts.js
@@ -1,4 +1,7 @@
+const Web3 = require('web3')
+
 export function createAccountAndPasswordEncryptKey(password) {
+  const web3 = new Web3()
   const { address, privateKey } = web3.eth.accounts.create()
 
   const passwordEncryptedPrivateKey = web3.eth.accounts.encrypt(
