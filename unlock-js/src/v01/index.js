b127207d1deca57781453239eaa23b9514a92eb4
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-16 15:31:52 -0400

    adding missing imports/exports and tests (#2675)

--- unlock-js/src/v01/index.js
+++ unlock-js/src/v01/index.js
@@ -1,23 +1,35 @@
+import _getKeyByLockForOwner from './_getKeyByLockForOwner'
+import _getPendingTransaction from './_getPendingTransaction'
+import _getSubmittedTransaction from './_getSubmittedTransaction'
 import createLock from './createLock'
-import updateKeyPrice from './updateKeyPrice'
-import purchaseKey from './purchaseKey'
-import partialWithdrawFromLock from './partialWithdrawFromLock'
-import withdrawFromLock from './withdrawFromLock'
-import getPastLockTransactions from './getPastLockTransactions'
-import getKeysForLockOnPage from './getKeysForLockOnPage'
-import getPastLockCreationsTransactionsForUser from './getPastLockCreationsTransactionsForUser'
-import getLock from './getLock'
 import getKeyByLockForOwner from './getKeyByLockForOwner'
+import getKeysForLockOnPage from './getKeysForLockOnPage'
+import getLock from './getLock'
+import getPastLockCreationsTransactionsForUser from './getPastLockCreationsTransactionsForUser'
+import getPastLockTransactions from './getPastLockTransactions'
+import getTransaction from './getTransaction'
+import getTransactionType from './getTransactionType'
+import parseTransactionFromInput from './parseTransactionFromInput'
+import partialWithdrawFromLock from './partialWithdrawFromLock'
+import purchaseKey from './purchaseKey'
+import updateKeyPrice from './updateKeyPrice'
+import withdrawFromLock from './withdrawFromLock'
 
 export default {
+  _getKeyByLockForOwner,
+  _getPendingTransaction,
+  _getSubmittedTransaction,
   createLock,
-  updateKeyPrice,
-  purchaseKey,
-  partialWithdrawFromLock,
-  withdrawFromLock,
-  getPastLockTransactions,
-  getKeysForLockOnPage,
-  getPastLockCreationsTransactionsForUser,
-  getLock,
   getKeyByLockForOwner,
+  getKeysForLockOnPage,
+  getLock,
+  getPastLockCreationsTransactionsForUser,
+  getPastLockTransactions,
+  getTransaction,
+  getTransactionType,
+  parseTransactionFromInput,
+  partialWithdrawFromLock,
+  purchaseKey,
+  updateKeyPrice,
+  withdrawFromLock,
 }
