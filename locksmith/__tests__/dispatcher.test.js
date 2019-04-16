236b274d1c6e30b37c85173d0b96bc631cf43294
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-04-16 13:54:48 -0400

    Adds the first pass at the dispatcher (#2657)
    This is still very rough around the edges, but the idea is that
    we have a consolidated piece of code making transactions on the behalf
    of others.
    
    The assumed next steps will be to connect this to the existing flow of logic
    and begin to sturdy this against edgecases.

---
+++ locksmith/__tests__/dispatcher.test.js
@@ -0,0 +1,110 @@
+import Dispatcher from '../src/dispatcher'
+
+let mockWeb3Service, mockWalletService, dispatcher
+let lockAddress = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
+let recipient = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
+let unlockAddress = '0x885EF47c3439ADE0CB9b33a4D3c534C99964Db93'
+let purchasingAddress = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
+let credential =
+  '0xfd8abdd241b9e7679e3ef88f05b31545816d6fbcaf11e86ebd5a57ba281ce229'
+let host = 'http://localhost:8545'
+
+let standardLock = {
+  asOf: 227,
+  balance: '0.01',
+  expirationDuration: 2592000,
+  keyPrice: '0.01',
+  maxNumberOfKeys: 10,
+  outstandingKeys: 1,
+  owner: '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2',
+}
+
+mockWeb3Service = {
+  getLock: jest
+    .fn()
+    .mockResolvedValue(standardLock)
+    .mockResolvedValueOnce(standardLock),
+}
+
+mockWalletService = {
+  connect: jest.fn(),
+  purchaseKey: jest.fn(),
+}
+
+jest.mock('@unlock-protocol/unlock-js', () => ({
+  Web3Service: function() {
+    return mockWeb3Service
+  },
+  WalletService: function() {
+    return mockWalletService
+  },
+}))
+
+describe('Dispatcher', () => {
+  beforeAll(() => {
+    dispatcher = new Dispatcher(
+      unlockAddress,
+      purchasingAddress,
+      credential,
+      host
+    )
+  })
+
+  describe('retrieveLock', () => {
+    describe('when the lock is retrievable', () => {
+      it('returns the Lock details', async () => {
+        expect.assertions(1)
+
+        let lockInformation = await dispatcher.retrieveLock(lockAddress)
+        expect(lockInformation).toEqual(expect.objectContaining(standardLock))
+      })
+    })
+
+    describe('when the lock is not retrievable', () => {
+      it('raises and error', async () => {
+        expect.assertions(1)
+        mockWeb3Service.getLock = jest.fn(() => {
+          throw new Error()
+        })
+        expect(() => {
+          dispatcher.retrieveLock('0x222')
+        }).toThrow('Unable to retrieve Lock information')
+      })
+    })
+  })
+
+  describe('purchase', () => {
+    describe('when the key is purchasable', () => {
+      it("purchases a key on the recipient's behalf", async () => {
+        expect.assertions(1)
+        dispatcher.retrieveLock = jest.fn().mockResolvedValue(standardLock)
+
+        await dispatcher.purchase(lockAddress, recipient)
+        expect(mockWalletService.purchaseKey).toHaveBeenCalledWith(
+          lockAddress,
+          recipient,
+          '0.01',
+          '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
+        )
+      })
+    })
+
+    describe('when the key is not purchasable', () => {
+      it("will not purchase the key on the recipient's behalf", async () => {
+        expect.assertions(1)
+        dispatcher.retrieveLock = jest.fn().mockResolvedValueOnce({
+          asOf: 227,
+          balance: '0.01',
+          expirationDuration: 2592000,
+          keyPrice: '0.01',
+          maxNumberOfKeys: 10,
+          outstandingKeys: 10,
+          owner: '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2',
+        })
+        expect(dispatcher.purchase(lockAddress, recipient)).rejects.toThrow(
+          'No Available Keys.'
+        )
+      })
+    })
+  })
+})
