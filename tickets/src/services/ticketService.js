1e25e472bab2f2b1fc3e5abed6cd0388503a26a8
Ben Werdmuller <ben@withknown.com>
2019-04-16 11:37:17 -0700

    Adding ticket service (#2660)
    * Adding ticket service to speak to locksmith
    
    * Revising tests

---
+++ tickets/src/services/ticketService.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+export default class TicketService {
+  constructor(host) {
+    this.host = host
+  }
+
+  /**
+   * Saves a new event to locksmith
+   * @param {*} event
+   * @returns {Promise<*>}
+   */
+  async createEvent({
+    lockAddress,
+    name,
+    description,
+    location,
+    date,
+    owner,
+    logo,
+  }) {
+    const payload = {
+      lockAddress,
+      name,
+      description,
+      location,
+      date,
+      owner,
+      logo,
+    }
+    try {
+      return await axios.post(`${this.host}/events/`, payload)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+
+  /**
+   * Retrieves new event details from locksmith by its lock address
+   * @param {*} lockAddress
+   * @returns {Promise<*>}
+   */
+  async getEvent(lockAddress) {
+    try {
+      return await axios.get(`${this.host}/events/${lockAddress}`)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+}
