73da240c54540cb9a66d91d9db059a675d72ad8c
Ben Werdmuller <ben@benwerd.com>
2019-04-15 17:05:16 -0700

    Adding story

--- tickets/src/stories/content/CreateContent.stories.js
+++ tickets/src/stories/content/CreateContent.stories.js
@@ -5,7 +5,12 @@ import CreateContent from '../../components/content/CreateContent'
 
 import createUnlockStore from '../../createUnlockStore'
 
-const store = createUnlockStore({})
+const store = createUnlockStore({
+  locks: {
+    abc123: { address: 'abc123' },
+    def459: { address: 'def456' },
+  },
+})
 
 storiesOf('Create event landing page', module)
   .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
