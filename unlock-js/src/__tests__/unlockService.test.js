7c4dece3b56611ecbf1561b2c97e710f4f68f330
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-16 17:49:02 -0400

    adding version detection mechanism for locks, independently of the deployed unlock version (#2681)

--- unlock-js/src/__tests__/unlockService.test.js
+++ unlock-js/src/__tests__/unlockService.test.js
@@ -1,8 +1,8 @@
 /* eslint no-console: 0 */
 
-import nock from 'nock'
 import * as UnlockV0 from 'unlock-abi-0'
 import * as UnlockV01 from 'unlock-abi-0-1'
+
 import UnlockService, { Errors } from '../unlockService'
 import v0 from '../v0'
 import v01 from '../v01'
@@ -14,7 +14,6 @@ let unlockService
 
 describe('UnlockService', () => {
   beforeEach(() => {
-    nock.cleanAll()
     unlockService = new UnlockService({
       unlockAddress,
     })
@@ -87,6 +86,81 @@ describe('UnlockService', () => {
       unlockService.opCodeForAddress[unlockAddress] =
         UnlockV0.Unlock.deployedBytecode
       const version = await unlockService.unlockContractAbiVersion()
+      expect(unlockService.web3.eth.getCode).not.toHaveBeenCalled()
+      expect(version).toEqual(v0)
+    })
+  })
+
+  describe('lockContractAbiVersion', () => {
+    it('should return UnlockV0 when the opCode matches', async () => {
+      expect.assertions(2)
+      unlockService.web3.eth = {
+        getCode: jest.fn(() => {
+          return Promise.resolve(UnlockV0.PublicLock.deployedBytecode)
+        }),
+      }
+      const address = '0xabc'
+      const version = await unlockService.lockContractAbiVersion(address)
+
+      expect(unlockService.web3.eth.getCode).toHaveBeenCalledWith(address)
+      expect(version).toEqual(v0)
+    })
+
+    it('should return UnlockV01 when the opCode matches', async () => {
+      expect.assertions(2)
+      unlockService.web3.eth = {
+        getCode: jest.fn(() => {
+          return Promise.resolve(UnlockV01.PublicLock.deployedBytecode)
+        }),
+      }
+      const address = '0xabc'
+      const version = await unlockService.lockContractAbiVersion(address)
+
+      expect(unlockService.web3.eth.getCode).toHaveBeenCalledWith(address)
+      expect(version).toEqual(v01)
+    })
+
+    it('should throw NON_DEPLOYED_CONTRACT if the opCode is 0x', async () => {
+      expect.assertions(2)
+      unlockService.web3.eth = {
+        getCode: jest.fn(() => {
+          return Promise.resolve('0x')
+        }),
+      }
+      const address = '0xabc'
+      try {
+        await unlockService.lockContractAbiVersion(address)
+      } catch (error) {
+        expect(error.message).toEqual(Errors.NON_DEPLOYED_CONTRACT)
+      }
+      expect(unlockService.web3.eth.getCode).toHaveBeenCalledWith(address)
+    })
+
+    it('should throw UNKNOWN_CONTRACT if the opCode does not match any version', async () => {
+      expect.assertions(2)
+      unlockService.web3.eth = {
+        getCode: jest.fn(() => {
+          return Promise.resolve('0xabc')
+        }),
+      }
+      const address = '0xabc'
+      try {
+        await unlockService.lockContractAbiVersion(address)
+      } catch (error) {
+        expect(error.message).toEqual(Errors.UNKNOWN_CONTRACT)
+      }
+      expect(unlockService.web3.eth.getCode).toHaveBeenCalledWith(address)
+    })
+
+    it('should memoize the result', async () => {
+      expect.assertions(2)
+      unlockService.web3.eth = {
+        getCode: jest.fn(),
+      }
+      const address = '0xabc'
+      unlockService.opCodeForAddress[address] =
+        UnlockV0.PublicLock.deployedBytecode
+      const version = await unlockService.lockContractAbiVersion(address)
       expect(unlockService.web3.eth.getCode).not.toHaveBeenCalled()
       expect(version).toEqual(v0)
     })
