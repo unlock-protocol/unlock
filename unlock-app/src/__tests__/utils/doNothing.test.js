e6fc14fd3df3e449fcf535e6b4ec224a6542758c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-04-18 15:20:29 -0400

    Update doNothing.test.js

--- unlock-app/src/__tests__/utils/doNothing.test.js
+++ unlock-app/src/__tests__/utils/doNothing.test.js
@@ -1,6 +1,6 @@
 import doNothing from '../../utils/doNothing'
 
-describe('the silliest test ever written', () => {
+describe('Do nothing (test coverage story helper)', () => {
   it('should not do anything', () => {
     expect.assertions(1)
     const result = doNothing()
