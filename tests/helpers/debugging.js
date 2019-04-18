21a4c7f141bb355146c222acfbfa8e0f04147387
Gregory Beaver <greg@unlock-protocol.com>
2019-04-18 14:19:30 -0400

    add screenshotOnFail and use .png for screenshots (#2235)
    * add screenshotOnFail and use .png for screenshots
    
    * add documentation
    
    * don't auto-log console statements, and add a link to await docs

--- tests/helpers/debugging.js
+++ tests/helpers/debugging.js
@@ -1,30 +1,51 @@
 /* eslint no-console: 0 */
 
 /**
- * debugPage
- ***********
+ * debugPage [local dev only]
  *
- * debugPage is used to turn on the display of console.log calls within the browser context.
- * This includes calls within page.evaluate and page.waitForFunction.
- *
- * Usage:
+ * Use debugPage() to display the output of console.log from within the page context.
+ * It should be called at the point you would like logging to begin with:
  *
  * debugPage(page, true)
  *
- * screenshot
- ************
+ * ****************************
+ * screenshot [local or CI dev]
  *
- * screenshot is used to take a screenshot. It will work both locally when run with the
- * scripts/local-docker-integration-tests.sh script or on CI. Screenshots locally are
- * placed in the current directory. In CI, they are found in the artifacts on CircleCI.
+ * screenshot() will immediately take a screenshot and save it. It accepts
+ * an optional filename prefix, which defaults to 'test'. With each call to
+ * screenshot, the file generated has a unique name based on a counter. The
+ * counter increments every time screenshot is called regardless of the filename
+ * prefix. These calls:
  *
- * Usage:
+ * screenshot(page)
+ * screenshot(page, 'different')
+ * screenshot(page)
  *
- * await screenshot(page) // creates test-1.jpg
- * await screenshot(page) // creates test-2.jpg
- * await screenshot(page, 'another') // creates another-3.jpg
+ * will generate files '/screenshots/test-1.png', '/screenshots/different-2.png', and
+ * '/screenshots/test-3.png'
  *
- * The numeric prefix is always unique and represents the order of the screenshot.
+ * screenshot can be used either in development or production. Be aware that on Mac
+ * OS X screenshots take a minimum of 1/6 second, and can distort timing results, so
+ * should only be used for development of integration tests.
+ *
+ * screenshots are saved into /tmp/screenshots
+ *
+ * ****************************
+ * screenshotOnFail [local and CI]
+ *
+ * screenshotOnFail() is designed to be a replacement for jest's "it"
+ * that is used to take a screenshot on any test failure. It is a factory
+ * that returns the replacement for "it." Usage:
+ *
+ * const it = screenshotOnFail(page)
+ *
+ * Note that there is no requirement that it replace "it", and one could just as easily use:
+ *
+ * const itWithScreenshot = screenshotOnFail(page)
+ *
+ * screenshots are saved into /tmp/screenshots for local dev, and will appear on CircleCI
+ * in the Artifacts section of integration tests for viewing when the tests conplete.
+ * Screenshot names are based on the test description.
  */
 let counter = 1
 module.exports = {
@@ -44,9 +65,34 @@ module.exports = {
   },
   async screenshot(page, file = 'test') {
     await page.screenshot({
-      path: `/screenshots/${file}-${counter}.jpg`,
+      path: `/screenshots/${file}-${counter}.png`,
       fullPage: true,
     })
     counter += 1
+  },
+  screenshotOnFail: page => (testDescription, code) => {
+    const runTest = async () => {
+      try {
+        // await will do nothing for non-async code, and make async tests work
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+        await code()
+        // if the code does not throw, we did not fail
+      } catch (e) {
+        // locally, we use the volume mounted at /screenshots
+        // on CI, we use the /home/unlock/screenshots directory used to retrieve artifacts
+        const screenshotPath = `/screenshots/${testDescription.replace(
+          /[ ,/"'^$\\]+/g,
+          '-'
+        )}.png`
+        console.error(`writing screenshot to ${screenshotPath}`)
+        try {
+          await page.screenshot({ path: screenshotPath, fullPage: true })
+        } catch (screenshotError) {
+          console.error(screenshotError)
+        }
+        throw e
+      }
+    }
+    it(testDescription, runTest)
   },
 }
