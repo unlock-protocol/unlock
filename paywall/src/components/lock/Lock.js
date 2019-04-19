1c38bc3dcfa50617385dad2709369f4ac7dfe4b1
Gregory Beaver <greg@unlock-protocol.com>
2019-04-19 16:13:01 -0400

    refactor Lock to remove redundant mapStateToProps (#2772)
    * prepare Paywall for passing down the values needed and using the new key status
    
    * new test that verifies we can still process a new key purchase for an expired key
    
    * save stuff
    
    * tweaks to make it work
    
    * remove the expired message - that is for a future PR
    
    * merge from master
    
    * revert change to snapshot
    
    * comment is on wrong line

--- paywall/src/components/lock/Lock.js
+++ paywall/src/components/lock/Lock.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 import UnlockPropTypes from '../../propTypes'
-import withConfig from '../../utils/withConfig'
 
 import { purchaseKey } from '../../actions/key'
 
@@ -10,7 +9,7 @@ import PendingKeyLock from './PendingKeyLock'
 import ConfirmingKeyLock from './ConfirmingKeyLock'
 import ConfirmedKeyLock from './ConfirmedKeyLock'
 import NoKeyLock from './NoKeyLock'
-import { UNLIMITED_KEYS_COUNT, TRANSACTION_TYPES } from '../../constants'
+import { UNLIMITED_KEYS_COUNT } from '../../constants'
 import usePurchaseKey from '../../hooks/usePurchaseKey'
 
 export const Lock = ({
@@ -25,6 +24,9 @@ export const Lock = ({
   keyStatus,
 }) => {
   const purchase = usePurchaseKey(purchaseKey, openInNewWindow)
+  // When the lock is not disabled for other reasons (pending key on
+  // other lock...), we need to ensure that the lock is disabled
+  // when the lock is sold out or too expensive for the current account
   const soldOut =
     lock.outstandingKeys >= lock.maxNumberOfKeys &&
     lock.maxNumberOfKeys !== UNLIMITED_KEYS_COUNT
@@ -81,42 +83,7 @@ export const mapDispatchToProps = (dispatch, { showModal }) => ({
   },
 })
 
-export const mapStateToProps = (state, { lock }) => {
-  const account = state.account
-
-  // If there is no account (probably not loaded yet), we do not want to create a key
-  if (!account) {
-    return {}
-  }
-
-  let lockKey = Object.values(state.keys).find(
-    key => key.lock === lock.address && key.owner === account.address
-  )
-  let transaction = null
-
-  if (!lockKey) {
-    lockKey = {
-      lock: lock.address,
-      owner: account.address,
-    }
-  }
-
-  // Let's select the transaction corresponding to this key purchase, if it exists
-  // This transaction is of type KEY_PURCHASE
-  transaction = Object.values(state.transactions).find(
-    transaction =>
-      transaction.type === TRANSACTION_TYPES.KEY_PURCHASE &&
-      transaction.key === lockKey.id
-  )
-
-  return {
-    account,
-    lockKey,
-    transaction,
-  }
-}
-
 export default connect(
-  mapStateToProps,
+  undefined, // no mapStateToProps needed
   mapDispatchToProps
-)(withConfig(Lock))
+)(Lock)
