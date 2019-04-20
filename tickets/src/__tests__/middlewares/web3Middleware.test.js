235d52560ad47f72382f368cb37375b776e99d75
Gregory Beaver <greg@unlock-protocol.com>
2019-04-19 23:50:44 -0400

    use the unlock-js accounts, deployer, and provider functionality (#2756)
    * remove all usages of web3 outside of unlock-js (this doesn't work yet)
    
    * fix tests

--- tickets/src/__tests__/middlewares/web3Middleware.test.js
+++ tickets/src/__tests__/middlewares/web3Middleware.test.js
@@ -59,11 +59,15 @@ class MockWebService extends EventEmitter {
 
 let mockWeb3Service = new MockWebService()
 
-jest.mock('@unlock-protocol/unlock-js', () => ({
-  Web3Service: function() {
-    return mockWeb3Service
-  },
-}))
+jest.mock('@unlock-protocol/unlock-js', () => {
+  const mockUnlock = require.requireActual('@unlock-protocol/unlock-js') // Original module
+  return {
+    ...mockUnlock,
+    Web3Service: function() {
+      return mockWeb3Service
+    },
+  }
+})
 
 UnlockJs.mockImplementation = MockWebService
 
