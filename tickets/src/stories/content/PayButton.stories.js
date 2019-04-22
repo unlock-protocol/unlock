897e99bbfe39b37e5a61c69c76bef10fb224462c
Christoph Nascone <chris@unlock-protocol.com>
2019-04-22 11:31:16 -0400

    Add a PayButton story

---
+++ tickets/src/stories/content/PayButton.stories.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { PayButton } from '../../components/content/purchase/PayButton'
+
+const config = {
+  requiredConfirmations: 12,
+}
+const purchaseKey = () => {}
+
+const tx = (status, confirmations) => ({
+  status,
+  confirmations,
+})
+const submittedTx = tx('submitted', 0)
+const minedTx = tx('mined', 6)
+const confirmedTx = tx('mined', 12)
+
+storiesOf('PayButton', module)
+  .add('Default', () => (
+    <PayButton transaction={{}} purchaseKey={purchaseKey} config={config} />
+  ))
+  .add('Payment Sent', () => (
+    <PayButton
+      transaction={submittedTx}
+      purchaseKey={purchaseKey}
+      config={config}
+    />
+  ))
+  .add('Confirming', () => (
+    <PayButton
+      transaction={minedTx}
+      purchaseKey={purchaseKey}
+      config={config}
+    />
+  ))
+  .add('Confirmed', () => (
+    <PayButton
+      transaction={confirmedTx}
+      purchaseKey={purchaseKey}
+      config={config}
+    />
+  ))
