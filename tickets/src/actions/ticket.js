e7ce0e205e7428f3c0cf56483491711177bc64f2
Christopher Nascone <cnasc@users.noreply.github.com>
2019-04-22 10:59:37 -0400

    Add and test tickets actions (#2795)

--- tickets/src/actions/ticket.js
+++ tickets/src/actions/ticket.js
@@ -1,10 +1,17 @@
 export const ADD_EVENT = 'ticket/ADD_EVENT'
+export const UPDATE_EVENT = 'ticket/UPDATE_EVENT'
 export const TICKET_ERROR = 'ticket/TICKET_ERROR'
 export const SIGN_ADDRESS = 'ticket/SIGN_ADDRESS'
 export const GOT_SIGNED_ADDRESS = 'ticket/GOT_SIGNED_ADDRESS'
 
-export const addEvent = event => ({
+export const addEvent = (event, token) => ({
   type: ADD_EVENT,
+  event,
+  token,
+})
+
+export const updateEvent = event => ({
+  type: UPDATE_EVENT,
   event,
 })
 
