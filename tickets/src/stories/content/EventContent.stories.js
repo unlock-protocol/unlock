38d363bffd7ae922eee44f76396416adcdfaf3e0
Ben Werdmuller <ben@withknown.com>
2019-04-22 12:31:06 -0700

    Ben ticketed event content (#2805)
    * Introducing live event content page component
    
    * Minor design fix
    
    * Now using middleware
    
    * Adding loadEvent to mapDispatchToProps
    
    * Updating tests

--- tickets/src/stories/content/EventContent.stories.js
+++ tickets/src/stories/content/EventContent.stories.js
@@ -4,34 +4,104 @@ import { Provider } from 'react-redux'
 import { EventContent } from '../../components/content/EventContent'
 
 import createUnlockStore from '../../createUnlockStore'
+import configure from '../../config'
+import { ConfigContext } from '../../utils/withConfig'
 
 const store = createUnlockStore({})
 
+const ConfigProvider = ConfigContext.Provider
+
+const lock = {
+  keyPrice: '0.01',
+  expirationDuration: 172800,
+  maxNumberOfKeys: 240,
+  outstandingKeys: 3,
+  address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+  transaction: 'deployedid',
+}
+const event = {
+  date: new Date(2063, 10, 23),
+  name: 'My Doctor Who party',
+  description: `Unbelievably, it's been 100 years since it first came to our screens.
+    
+Join us for an hour or two of fine entertainment.`,
+  location: 'Totters Lane, London',
+}
+const config = configure({})
+const purchaseKey = () => {}
+const loadEvent = () => {}
+
 storiesOf('Event RSVP page', module)
   .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
-  .add('Event RSVP page', () => {
-    const date = new Date(2063, 10, 23)
-    const name = 'My Doctor Who party'
-    const description = `Unbelievably, it's been 100 years since it first came to our screens.
-    
-Join us for an hour or two of fine entertainment.`
-    const location = 'Totters Lane, London'
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: 'deployedid',
+  .add('Event RSVP page with unpurchased key', () => {
+    const transaction = null
+
+    return (
+      <ConfigProvider value={config}>
+        <EventContent
+          event={event}
+          lock={lock}
+          config={config}
+          purchaseKey={purchaseKey}
+          loadEvent={loadEvent}
+          transaction={transaction}
+        />
+      </ConfigProvider>
+    )
+  })
+  .add('Event RSVP page with submitted key', () => {
+    const transaction = {
+      status: 'submitted',
     }
 
     return (
-      <EventContent
-        date={date}
-        name={name}
-        description={description}
-        location={location}
-        lock={lock}
-      />
+      <ConfigProvider value={config}>
+        <EventContent
+          event={event}
+          lock={lock}
+          config={config}
+          purchaseKey={purchaseKey}
+          loadEvent={loadEvent}
+          transaction={transaction}
+        />
+      </ConfigProvider>
+    )
+  })
+  .add('Event RSVP page with confirming key', () => {
+    const transaction = {
+      status: 'mined',
+      confirmations: 3,
+    }
+
+    return (
+      <ConfigProvider value={config}>
+        <EventContent
+          event={event}
+          lock={lock}
+          config={config}
+          purchaseKey={purchaseKey}
+          loadEvent={loadEvent}
+          transaction={transaction}
+        />
+      </ConfigProvider>
+    )
+  })
+  .add('Event RSVP page with confirmed key', () => {
+    const transaction = {
+      status: 'mined',
+      confirmations: 14,
+    }
+
+    return (
+      <ConfigProvider value={config}>
+        <EventContent
+          event={event}
+          lock={lock}
+          config={config}
+          purchaseKey={purchaseKey}
+          loadEvent={loadEvent}
+          transaction={transaction}
+        />
+      </ConfigProvider>
     )
   })
