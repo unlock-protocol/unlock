c17aadc2d48b633aec5a3f40ad4234dfdc27a5d9
Ben Werdmuller <ben@withknown.com>
2019-04-24 10:12:16 -0700

    Introducing the TicketCode component (#2847)
    * Introducing the TicketCode component
    
    * Now using a ticket url from configuration

---
+++ tickets/src/__tests__/components/content/TicketCode.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import * as rtl from 'react-testing-library'
+import { TicketCode } from '../../../components/content/purchase/TicketCode'
+
+const config = {
+  unlockTicketsUrl: 'https://tickets.unlock-protocol.com',
+}
+
+describe('TicketCode', () => {
+  it('should display a QR code when a signed address and public key are supplied', () => {
+    expect.assertions(1)
+
+    const wrapper = rtl.render(
+      <TicketCode
+        signedAddress="this-is-a-signed-address"
+        publicKey="12345"
+        config={config}
+      />
+    )
+
+    expect(wrapper.container.querySelector('svg')).not.toBeNull()
+  })
+
+  it('should not display a QR code when a signed address and public key are not supplied', () => {
+    expect.assertions(1)
+
+    const wrapper = rtl.render(<TicketCode config={config} />)
+
+    expect(wrapper.container.querySelector('svg')).toBeNull()
+  })
+})
