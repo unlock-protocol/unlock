54a5a1387706539e5a0272ae10087a542a84f7d9
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-23 21:17:25 -0400

    not asserting status of lock since the next test actually does it (#2834)

--- tests/helpers/wait.js
+++ tests/helpers/wait.js
@@ -16,6 +16,23 @@ const untilIsFalse = async (statement, ...variables) =>
   })
 
 /**
+ * Helper function which waits for something to be false
+ */
+const untilIsTrue = async (statement, ...variables) =>
+  new Promise(resolve => {
+    const waitIfTrue = async () => {
+      const isTrue = await page.evaluate(statement, variables)
+      if (isTrue) {
+        return resolve()
+      }
+      return setTimeout(async () => {
+        return waitIfTrue()
+      }, 10)
+    }
+    waitIfTrue()
+  })
+
+/**
  * Helper function to ensure that a DOM element is gone.
  */
 const untilIsGone = async selector =>
@@ -36,6 +53,7 @@ const forIframe = async () => {
 
 module.exports = {
   untilIsFalse,
+  untilIsTrue,
   untilIsGone,
   forLoadingDone,
   forIframe,
