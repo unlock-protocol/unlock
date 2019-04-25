848999643f8c373c1b4b757ecc041b7021d7225b
Christoph Nascone <chris@unlock-protocol.com>
2019-04-25 10:53:34 -0400

    Constant for 100 years in days

--- unlock-app/src/constants.ts
+++ unlock-app/src/constants.ts
@@ -91,6 +91,9 @@ export const MAX_DEVICE_WIDTHS = {
 export const INFINITY = '∞'
 export const UNLIMITED_KEYS_COUNT = -1
 
+// oneHundredYearsInDays -- based on the calculation for max duration in smart contract.
+export const oneHundredYearsInDays = 100 * 365
+
 export const SHOW_FLAG_FOR = 2000 // milliseconds
 
 export const MAX_UINT =
