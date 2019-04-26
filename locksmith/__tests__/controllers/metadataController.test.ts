af12b4e2d6541ceb7f291475fd5a276d1ae91ade
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-04-26 13:03:40 -0400

    A stub to support further development of key metadata feature (#2881)
    * A stub to support further development of key metadata feature
    
    * Import to keep ts happy
    
    * more typescript fun
    
    * Adjusting response for typescript

---
+++ locksmith/__tests__/controllers/metadataController.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+
+const app = require('../../src/app')
+
+describe('Metadata Controller', () => {
+  describe('the data stub', () => {
+    it('returns wellformed stub data', async () => {
+      expect.assertions(2)
+
+      let response = await request(app)
+        .get('/api/key/anykey_for_now')
+        .set('Accept', 'json')
+
+      expect(response.status).toBe(200)
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          description: expect.any(String),
+          image: expect.any(String),
+          name: expect.any(String),
+        })
+      )
+    })
+  })
+})
