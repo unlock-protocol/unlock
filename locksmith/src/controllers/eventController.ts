df887e2324ac94e5f631979f75fc63cf4010a2ca
Ben Werdmuller <ben@benwerd.com>
2019-04-26 09:06:12 -0700

    Locksmith endpoint for saving events (#2879)
    * event saving route
    
    * Updating tests
    
    * Properly normalizing tests
    
    * Updating address comparison and test case

--- locksmith/src/controllers/eventController.ts
+++ locksmith/src/controllers/eventController.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars, import/no-unresolved
 import { Request, Response } from 'express-serve-static-core'
+import Normalizer from '../utils/normalizer'
 
 const eventOperation = require('../operations/eventOperations')
 
@@ -16,10 +17,29 @@ namespace EventController {
     }
   }
 
+  export const save = async (req: Request, res: Response): Promise<any> => {
+    let event = req.body.message.event
+    let lockAddress = req.params.lockAddress
+    try {
+      const databaseEvent = await eventOperation.find(lockAddress)
+      if (
+        databaseEvent.owner !== Normalizer.ethereumAddress(event.owner) ||
+        databaseEvent.lockAddress != Normalizer.ethereumAddress(lockAddress)
+      ) {
+        return res.sendStatus(401)
+      }
+
+      await eventOperation.update(event)
+      return res.sendStatus(202)
+    } catch (e) {
+      return res.sendStatus(409)
+    }
+  }
+
   export const find = async (req: Request, res: Response): Promise<any> => {
     let lockAddress = req.params.lockAddress
-    let event = await eventOperation.find(lockAddress)
 
+    let event = await eventOperation.find(lockAddress)
     if (event) {
       return res.json(event.toJSON())
     } else {
