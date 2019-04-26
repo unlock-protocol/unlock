5ee9e6ec8074a184ac4e6ee166e5285dd86c33de
Nick Furfaro <nfurfaro33@gmail.com>
2019-04-26 15:21:12 -0600

    Fix UnlockUtils

--- smart-contracts/contracts/UnlockUtils.sol
+++ smart-contracts/contracts/UnlockUtils.sol
@@ -12,14 +12,18 @@ contract UnlockUtils {
   ) internal
     pure
     returns (string memory)
-  {
+ {
     bytes memory _ba = bytes(_a);
     bytes memory _bb = bytes(_b);
     string memory ab = new string(_ba.length + _bb.length);
     bytes memory bab = bytes(ab);
     uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
-    for (uint i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+    for (uint i = 0; i < _ba.length; i++) {
+      bab[k++] = _ba[i];
+    }
+    for (uint i = 0; i < _bb.length; i++) {
+      bab[k++] = _bb[i];
+    }
     return string(bab);
   }
 
@@ -39,16 +43,18 @@ contract UnlockUtils {
       pure
       returns (string memory)
     {
-      if (i == 0) return "0";
+      if (i == 0) {
+        return "0";
+      }
       uint j = i;
       uint len;
-      while (j != 0){
+      while (j != 0) {
         len++;
         j /= 10;
       }
       bytes memory bstr = new bytes(len);
       uint k = len - 1;
-      while (i != 0){
+      while (i != 0) {
         bstr[k--] = byte(48 + i % 10);
         i /= 10;
       }
