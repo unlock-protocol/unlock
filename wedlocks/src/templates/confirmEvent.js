cba04f354c7531a7b0ba8d003371b562f68ca3e7
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-26 16:33:18 -0400

    adding the confirmEvent template and the ability to send attachements (#2878)

---
+++ wedlocks/src/templates/confirmEvent.js
@@ -0,0 +1,20 @@
+export default {
+  subject: () => 'You are attending an event!',
+  text: params =>
+    `Hello,
+
+This is just a reminder that you are attending the event ${
+      params.eventName
+    } on ${params.eventDate}!
+
+When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
+
+${params.ticketLink}.
+
+You can also show the QR code attached to this email.
+
+Enjoy!
+
+- The Unlock team
+`,
+}
