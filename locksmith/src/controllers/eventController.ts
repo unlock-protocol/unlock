42fa272502048cc68b5d92716c2bef2f5ae4d569
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-04-29 10:22:23 -0400

    Provides ability to add/update links for an event (#2882)
    * Provides ability to add/update links for an event

--- locksmith/src/controllers/eventController.ts
+++ locksmith/src/controllers/eventController.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars, import/no-unresolved
 import { Request, Response } from 'express-serve-static-core'
+import { SignedRequest } from '../types' // eslint-disable-line no-unused-vars, import/no-unresolved
 import Normalizer from '../utils/normalizer'
 
 const eventOperation = require('../operations/eventOperations')
@@ -44,6 +45,27 @@ namespace EventController {
       return res.json(event.toJSON())
     } else {
       return res.sendStatus(404)
+    }
+  }
+
+  export const addLinks = async (
+    req: SignedRequest,
+    res: Response
+  ): Promise<any> => {
+    let lockAddress = req.params.lockAddress
+    let eventLinks = req.body.message.eventLinks
+
+    const databaseEvent = await eventOperation.find(lockAddress)
+
+    if (databaseEvent.owner != Normalizer.ethereumAddress(req.owner)) {
+      return res.sendStatus(401)
+    }
+
+    try {
+      await eventOperation.addLinks(lockAddress, eventLinks)
+      return res.sendStatus(202)
+    } catch (e) {
+      return res.sendStatus(409)
     }
   }
 }
