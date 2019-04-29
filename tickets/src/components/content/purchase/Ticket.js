9ea159d4979494ac0f3fb9dcb381597d579a99a8
Julien Genestoux <julien.genestoux@gmail.com>
2019-04-29 19:38:48 -0400

    Adding the ability to send events by email

--- tickets/src/components/content/purchase/Ticket.js
+++ tickets/src/components/content/purchase/Ticket.js
@@ -4,24 +4,31 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import UnlockPropTypes from '../../../propTypes'
 import { signAddress } from '../../../actions/ticket'
+import { sendConfirmation } from '../../../actions/email'
 import Media from '../../../theme/media'
 
 import { KeyStatus } from '../../../selectors/keys'
 import TicketCode from './TicketCode'
 import withConfig from '../../../utils/withConfig'
 
+const getDataUriFromCanvas = () => {
+  const canvas = document.querySelector('canvas')
+  if (canvas) {
+    return canvas.toDataURL()
+  }
+}
 export class Ticket extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
       signed: false,
+      email: '',
+      sent: false,
     }
-
-    this.setAsSigned.bind(this)
   }
 
-  componentDidUpdate() {
+  componentDidUpdate = () => {
     const { lock, signAddress, signedLockAddress, keyStatus } = this.props
     if (keyStatus === KeyStatus.VALID) {
       const { signed } = this.state
@@ -34,15 +41,54 @@ export class Ticket extends Component {
     }
   }
 
-  setAsSigned() {
+  setAsSigned = () => {
     this.setState({ signed: true })
+  }
+
+  handleChange = event => {
+    const email = event.target.value
+    this.setState(state => {
+      return {
+        ...state,
+        email,
+      }
+    })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault()
+    const { sendConfirmation, event, getDataUriFromCanvas } = this.props
+    const { email, sent } = this.state
+
+    if (!sent && email) {
+      // We could do email validation but since we use type="email" the browser will actually not even let us submit the form.
+      const dataUri = getDataUriFromCanvas()
+      if (dataUri) {
+        sendConfirmation(
+          email,
+          dataUri,
+          event.name,
+          event.date.toDateString(),
+          window.location.toString()
+        )
+        // remember as sent!
+        this.setState(state => {
+          return {
+            ...state,
+            sent: true,
+          }
+        })
+      }
+    }
   }
 
   render() {
     const { account, lock, signedLockAddress, keyStatus } = this.props
+    const { email, sent } = this.state
     if (keyStatus !== KeyStatus.VALID) {
       return null
     }
+
     return (
       <TicketInfo>
         <TicketCode
@@ -50,6 +96,20 @@ export class Ticket extends Component {
           signedAddress={signedLockAddress}
           lockAddress={lock.address}
         />
+        <Form onSubmit={this.handleSubmit}>
+          <Input
+            disabled={sent}
+            placeholder="Enter your email address"
+            type="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <SendButton
+            type="submit"
+            disabled={sent || !email}
+            value={sent ? 'Sent!' : 'Send'}
+          />
+        </Form>
       </TicketInfo>
     )
   }
@@ -59,9 +119,14 @@ export const mapDispatchToProps = dispatch => ({
   signAddress: address => {
     dispatch(signAddress(address))
   },
+  sendConfirmation: (recipient, ticket, eventName, eventDate, confirmLink) => {
+    dispatch(
+      sendConfirmation(recipient, ticket, eventName, eventDate, confirmLink)
+    )
+  },
 })
 
-export const mapStateToProps = ({ tickets }, props) => {
+export const mapStateToProps = ({ tickets, event }, props) => {
   const { lock, account } = props
   let signedLockAddress
   if (tickets[lock.address]) {
@@ -72,6 +137,7 @@ export const mapStateToProps = ({ tickets }, props) => {
     signedLockAddress,
     account,
     lock,
+    event,
   }
 }
 
@@ -79,13 +145,18 @@ Ticket.propTypes = {
   account: UnlockPropTypes.account.isRequired,
   lock: UnlockPropTypes.lock.isRequired,
   signAddress: PropTypes.func.isRequired,
+  sendConfirmation: PropTypes.func.isRequired,
   signedLockAddress: PropTypes.string,
   keyStatus: PropTypes.string,
+  event: UnlockPropTypes.ticketedEvent,
+  getDataUriFromCanvas: PropTypes.func,
 }
 
 Ticket.defaultProps = {
   signedLockAddress: null,
   keyStatus: null,
+  event: null,
+  getDataUriFromCanvas,
 }
 
 export default withConfig(
@@ -95,9 +166,48 @@ export default withConfig(
   )(Ticket)
 )
 
+const Form = styled.form`
+  display: grid;
+  grid-gap: 10px;
+`
+
+const SendButton = styled.input`
+  background-color: ${props =>
+    props.disabled ? 'var(--grey)' : 'var(--green)'};
+  border: none;
+  font-size: 16px;
+  color: var(--white);
+  font-family: 'IBM Plex Sans', sans-serif;
+  border-radius: 4px;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  transition: background-color 200ms ease;
+  & :hover {
+    background-color: ${props =>
+      props.disabled ? 'var(--grey)' : 'var(--activegreen)'};
+  }
+  height: 60px;
+  ${Media.phone`
+    width: 100%;
+  `};
+`
+
 const TicketInfo = styled.div`
   display: grid;
+  grid-gap: 20px;
+
   ${Media.phone`
     justify-content: center;
   `}
+`
+
+const Input = styled.input`
+  height: 60px;
+  border: none;
+  background-color: var(--offwhite);
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 16px;
+  color: var(--darkgrey);
 `
