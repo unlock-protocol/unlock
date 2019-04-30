2f6f4510a37aec4ee47699a40f5fe0ec7b6bcc6a
Gregory Beaver <greg@unlock-protocol.com>
2019-04-30 16:23:59 -0400

    implement ethers-ready versions of unlockService

---
+++ unlock-js/src/__tests__/unlockService.ethers.test..js
@@ -0,0 +1,114 @@
+/* eslint no-console: 0 */
+
+import { providers as ethersProviders } from 'ethers'
+
+import UnlockService from '../unlockService'
+import NockHelper from './helpers/nockHelper'
+import v0 from '../v0'
+import v02 from '../v02'
+
+const endpoint = 'http://127.0.0.1:8545'
+const nock = new NockHelper(endpoint, false /** debug */)
+
+// This unlock address smart contract is fake
+let unlockAddress = '0x885ef47c3439ade0cb9b33a4d3c534c99964db93'
+let unlockService
+
+describe('UnlockService', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+    unlockService = new UnlockService({
+      unlockAddress,
+    })
+    unlockService.provider = new ethersProviders.JsonRpcProvider(endpoint)
+  })
+
+  describe('ethers_unlockContractAbiVersion', () => {
+    it('should return the v2 implementation when the opCode matches', async () => {
+      expect.assertions(3)
+      unlockService.ethers_getVersionFromContract = jest.fn(() => {
+        return Promise.resolve(2)
+      })
+      const version = await unlockService.ethers_unlockContractAbiVersion()
+
+      expect(unlockService.ethers_getVersionFromContract).toHaveBeenCalledWith(
+        unlockAddress
+      )
+      expect(version).toEqual(v02)
+      expect(unlockService.ethers_versionForAddress[unlockAddress]).toEqual(2)
+    })
+
+    it('should return v0 by default', async () => {
+      expect.assertions(3)
+      unlockService.ethers_getVersionFromContract = jest.fn(() => {
+        return Promise.resolve(0)
+      })
+
+      const version = await unlockService.ethers_unlockContractAbiVersion()
+
+      expect(unlockService.ethers_getVersionFromContract).toHaveBeenCalledWith(
+        unlockAddress
+      )
+      expect(version).toEqual(v0)
+      expect(unlockService.ethers_versionForAddress[unlockAddress]).toEqual(0)
+    })
+
+    it('should used the memoized the result', async () => {
+      expect.assertions(2)
+      unlockService.ethers_getVersionFromContract = jest.fn(() => {})
+      unlockService.ethers_versionForAddress[unlockAddress] = 2
+      const version = await unlockService.ethers_unlockContractAbiVersion()
+      expect(unlockService.ethers_getVersionFromContract).not.toHaveBeenCalled()
+      expect(version).toEqual(v02)
+    })
+  })
+
+  describe('lockContractAbiVersion', () => {
+    it('should return UnlockV0 when the version matches', async () => {
+      expect.assertions(3)
+      unlockService.ethers_getPublicLockVersionFromContract = jest.fn(() => {
+        return Promise.resolve(0)
+      })
+
+      const address = '0xabc'
+      const version = await unlockService.ethers_lockContractAbiVersion(address)
+
+      expect(
+        unlockService.ethers_getPublicLockVersionFromContract
+      ).toHaveBeenCalledWith(address)
+      expect(version).toEqual(v0)
+      expect(unlockService.ethers_versionForAddress[address]).toEqual(0)
+    })
+
+    it('should return UnlockV01 when the version matches', async () => {
+      expect.assertions(3)
+      unlockService.ethers_getPublicLockVersionFromContract = jest.fn(() => {
+        return Promise.resolve(1) // See code for explaination
+      })
+
+      const address = '0xabc'
+      const version = await unlockService.ethers_lockContractAbiVersion(address)
+
+      expect(
+        unlockService.ethers_getPublicLockVersionFromContract
+      ).toHaveBeenCalledWith(address)
+      expect(version).toEqual(v02)
+      expect(unlockService.ethers_versionForAddress[address]).toEqual(1)
+    })
+
+    it('should memoize the result', async () => {
+      expect.assertions(3)
+      unlockService.ethers_getPublicLockVersionFromContract = jest.fn(() => {})
+
+      const address = '0xabc'
+      unlockService.ethers_versionForAddress[address] = 1
+      const version = await unlockService.ethers_lockContractAbiVersion(address)
+
+      expect(
+        unlockService.ethers_getPublicLockVersionFromContract
+      ).not.toHaveBeenCalled()
+      expect(version).toEqual(v02)
+      expect(unlockService.ethers_versionForAddress[address]).toEqual(1)
+    })
+  })
+})
--- unlock-js/src/unlockService.js
+++ unlock-js/src/unlockService.js
@@ -1,6 +1,8 @@
 import EventEmitter from 'events'
+import { Contract } from 'ethers'
 
 import v0 from './v0'
+import v01 from './v01'
 import v02 from './v02'
 
 export const Errors = {
@@ -14,13 +16,19 @@ export const Errors = {
  * It is not meant to be instantiated (only subclasses should)
  */
 export default class UnlockService extends EventEmitter {
-  constructor({ unlockAddress }) {
+  constructor({ unlockAddress, writable = false }) {
     super()
+    this.writable = writable
     this.unlockContractAddress = unlockAddress
     this.web3 = null
+    this.provider = null
     /* Memoization for opCode per address */
     // Used to cache
     this.versionForAddress = {}
+    this.ethers_versionForAddress = {}
+    this.unlockContract = null
+    // this will populate on-demand as locks are accessed
+    this.lockContracts = {}
   }
 
   /**
@@ -33,10 +41,14 @@ export default class UnlockService extends EventEmitter {
     }
 
     let version = this.versionForAddress[this.unlockContractAddress]
-    if (!version) {
-      // This was no memo-ized
+    if (version === undefined) {
+      // This was not memo-ized
       version = await this._getVersionFromContract(this.unlockContractAddress)
       this.versionForAddress[this.unlockContractAddress] = version
+    }
+
+    if (1 === version) {
+      return v01
     }
 
     if (2 === version) {
@@ -56,11 +68,11 @@ export default class UnlockService extends EventEmitter {
       throw new Error(Errors.MISSING_WEB3)
     }
 
-    let version = this.versionForAddress[address]
-    if (!version) {
+    let version = this.versionForAddress[address.toLowerCase()]
+    if (version === undefined) {
       // This was not memo-ized
       version = await this._getPublicLockVersionFromContract(address)
-      this.versionForAddress[address] = version
+      this.versionForAddress[address.toLowerCase()] = version
     }
 
     // NOTE: we (julien) F'ed up the deploy on the PublicLock and v02 still uses 1 for its version.
@@ -135,5 +147,133 @@ export default class UnlockService extends EventEmitter {
       // This is an older version of Unlock which did not support unlockVersion
     }
     return version
+  }
+
+  /**
+   * Returns the implementation based on the deployed version
+   * @param {*} address
+   */
+  async ethers_unlockContractAbiVersion() {
+    if (!this.provider) {
+      throw new Error(Errors.MISSING_WEB3)
+    }
+
+    let version = this.ethers_versionForAddress[this.unlockContractAddress]
+    if (version === undefined) {
+      // This was not memo-ized
+      version = await this.ethers_getVersionFromContract(
+        this.unlockContractAddress
+      )
+      this.ethers_versionForAddress[this.unlockContractAddress] = version
+    }
+
+    if (1 === version) {
+      return v01
+    }
+
+    if (2 === version) {
+      return v02
+    }
+
+    // Defaults to v0
+    return v0
+  }
+
+  /**
+   * Returns the ABI for the Lock contract deployed at the provided address
+   * @param {*} address
+   */
+  async ethers_lockContractAbiVersion(address) {
+    if (!this.provider) {
+      throw new Error(Errors.MISSING_WEB3)
+    }
+
+    let version = this.ethers_versionForAddress[address.toLowerCase()]
+    if (version === undefined) {
+      // This was not memo-ized
+      version = await this.ethers_getPublicLockVersionFromContract(address)
+      this.ethers_versionForAddress[address.toLowerCase()] = version
+    }
+
+    // NOTE: we (julien) F'ed up the deploy on the PublicLock and v02 still uses 1 for its version.
+    // The good news (luck) is that no contract was ever deployed as v01
+    if (1 === version) {
+      return v02
+    }
+
+    // Defaults to v0
+    return v0
+  }
+
+  async ethers_getPublicLockVersionFromContract(address) {
+    const contract = new Contract(
+      address,
+      ['function publicLockVersion() view returns (uint8)'],
+      this.provider
+    )
+    let version = 0
+    try {
+      const contractVersion = await contract.publicLockVersion()
+      version = parseInt(contractVersion, 10) || 0
+    } catch (error) {
+      // This is an older version of Unlock which did not support publicLockVersion
+    }
+    return version
+  }
+
+  /**
+   * Private method, which given an address will query the contract and return the corresponding method
+   * @param {*} address
+   */
+  async ethers_getVersionFromContract(address) {
+    const contract = new Contract(
+      address,
+      ['function unlockVersion() view returns (uint8)'],
+      this.provider
+    )
+    let version = 0
+    try {
+      const contractVersion = await contract.unlockVersion()
+      version = parseInt(contractVersion, 10) || 0
+    } catch (error) {
+      // This is an older version of Unlock which did not support unlockVersion
+    }
+    return version
+  }
+
+  getContract(address, contract) {
+    if (this.writable) return this.getWritableContract(address, contract)
+    return new Contract(address, contract.abi, this.provider)
+  }
+
+  async getWritableContract(address, contract) {
+    const signer = this.provider.getSigner()
+    return new Contract(address, contract.abi, signer)
+  }
+
+  async getLockContract(lockAddress) {
+    if (this.lockContracts[lockAddress]) {
+      return this.lockContracts[lockAddress]
+    }
+    const version = await this.lockContractAbiVersion(lockAddress)
+    this.lockContracts[lockAddress] = this.getContract(
+      lockAddress,
+      version.PublicLock,
+      this.provider
+    )
+    return this.lockContracts[lockAddress]
+  }
+
+  async getUnlockContract() {
+    if (this.unlockContract) {
+      return this.unlockContract
+    }
+    const version = await this.ethers_unlockContractAbiVersion()
+    this.unlockContract = this.getContract(
+      this.unlockContractAddress,
+      version.Unlock,
+      this.provider
+    )
+    return this.unlockContract
   }
 }
