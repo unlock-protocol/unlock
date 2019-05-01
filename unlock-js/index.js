2cd1523b0639c2376b2dbcfb5987b17b535ccd1c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-05-01 14:42:54 -0400

    Fix malformed export in unlock-js (#2935)
    * Should probably use the actual function name
    
    * Bump package version

--- unlock-js/index.js
+++ unlock-js/index.js
@@ -2,7 +2,7 @@ const Web3Service = require('./lib/web3Service')
 const WalletService = require('./lib/walletService')
 const {
   createAccountAndPasswordEncryptKey,
-  getAddressFromPrivateKey,
+  getAccountFromPrivateKey,
 } = require('./lib/accounts')
 const { getCurrentProvider, getWeb3Provider } = require('./lib/providers')
 const deploy = require('./lib/deploy').default
@@ -11,7 +11,7 @@ module.exports = {
   Web3Service: Web3Service.default,
   WalletService: WalletService.default,
   createAccountAndPasswordEncryptKey,
-  getAddressFromPrivateKey,
+  getAccountFromPrivateKey,
   getCurrentProvider,
   getWeb3Provider,
   deploy,
