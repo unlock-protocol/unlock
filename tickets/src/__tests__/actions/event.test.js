aa192e951796a479981934a4945a6aac8b15cd31
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-01 11:18:53 -0400

    more refactoring by splitting the logic between the event and the ticket into 2 different tracks (actions, reducers, middlewares...)

---
+++ tickets/src/__tests__/actions/event.test.js
@@ -0,0 +1,56 @@
+import {
+  ADD_EVENT,
+  addEvent,
+  LOAD_EVENT,
+  loadEvent,
+  UPDATE_EVENT,
+  updateEvent,
+  EVENT_ERROR,
+  eventError,
+} from '../../actions/event'
+
+describe('event actions', () => {
+  it('should create an event to add an event', () => {
+    expect.assertions(1)
+    const event = {}
+    const token = {}
+    const expectedAction = {
+      type: ADD_EVENT,
+      event,
+      token,
+    }
+    expect(addEvent(event, token)).toEqual(expectedAction)
+  })
+
+  it('should create an event to load an event', () => {
+    expect.assertions(1)
+    const address = '0x123'
+    const expectedAction = {
+      type: LOAD_EVENT,
+      address,
+    }
+    expect(loadEvent(address)).toEqual(expectedAction)
+  })
+
+  it('should create an action to update an event', () => {
+    expect.assertions(1)
+    const event = {}
+    const expectedAction = {
+      type: UPDATE_EVENT,
+      event,
+    }
+    expect(updateEvent(event)).toEqual(expectedAction)
+  })
+
+  it('should create an action emitting a event error', () => {
+    expect.assertions(1)
+    const error = 'a comic error'
+
+    const expectedError = {
+      type: EVENT_ERROR,
+      error: error,
+    }
+
+    expect(eventError(error)).toEqual(expectedError)
+  })
+})
--- tickets/src/actions/ticket.js
+++ tickets/src/actions/ticket.js
@@ -1,32 +1,5 @@
-export const ADD_EVENT = 'ticket/ADD_EVENT'
-export const LOAD_EVENT = 'ticket/LOAD_EVENT'
-export const UPDATE_EVENT = 'ticket/UPDATE_EVENT'
-export const TICKET_ERROR = 'ticket/TICKET_ERROR'
 export const SIGN_ADDRESS = 'ticket/SIGN_ADDRESS'
 export const GOT_SIGNED_ADDRESS = 'ticket/GOT_SIGNED_ADDRESS'
-
-export const addEvent = (event, token) => ({
-  type: ADD_EVENT,
-  event,
-  token,
-})
-
-export const loadEvent = address => ({
-  type: LOAD_EVENT,
-  address,
-})
-
-export const updateEvent = event => ({
-  type: UPDATE_EVENT,
-  event,
-})
-
-export function ticketError(error) {
-  return {
-    type: TICKET_ERROR,
-    error: error,
-  }
-}
 
 export const signAddress = address => ({
   type: SIGN_ADDRESS,
