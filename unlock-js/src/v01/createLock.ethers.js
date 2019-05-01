93ce6997f8b989482489b47920077b3168819eb7
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-05-01 16:51:37 -0400

    migrate createLock to ethers (#2936)
    * migrate createLock to ethers
    
    * move ZERO to constants

---
+++ unlock-js/src/v01/createLock.ethers.js
@@ -0,0 +1,45 @@
+import ethersUtils from '../utils.ethers'
+import { GAS_AMOUNTS, ZERO } from '../constants'
+import TransactionTypes from '../transactionTypes'
+
+/**
+ * Creates a lock on behalf of the user, using version v0
+ * @param {PropTypes.lock} lock
+ * @param {PropTypes.address} owner
+ */
+export default async function(lock, owner) {
+  const unlockContract = await this.getUnlockContract()
+  let transactionPromise
+  try {
+    transactionPromise = unlockContract.functions[
+      'createLock(uint256,address,uint256,uint256)'
+    ](
+      lock.expirationDuration,
+      ZERO, // ERC20 address, 0 is for eth
+      ethersUtils.toWei(lock.keyPrice, 'ether'),
+      lock.maxNumberOfKeys,
+      {
+        gasLimit: GAS_AMOUNTS.createLock, // overrides default value for transaction gas price
+      }
+    )
+    const hash = await this._handleMethodCall(
+      transactionPromise,
+      TransactionTypes.LOCK_CREATION
+    )
+
+    // Let's update the lock to reflect that it is linked to this
+    // This is an exception because, until we are able to determine the lock address
+    // before the transaction is mined, we need to link the lock and transaction.
+    this.emit('lock.updated', lock.address, {
+      expirationDuration: lock.expirationDuration,
+      keyPrice: lock.keyPrice, // Must be expressed in Eth!
+      maxNumberOfKeys: lock.maxNumberOfKeys,
+      owner: owner,
+      outstandingKeys: 0,
+      balance: '0',
+      transaction: hash,
+    })
+  } catch (error) {
+    this.emit('error', new Error(TransactionTypes.FAILED_TO_CREATE_LOCK))
+  }
+}
