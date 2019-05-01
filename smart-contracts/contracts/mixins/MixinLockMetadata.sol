55f8fa6f44e455c86b16d34909b0641c091a9305
Nick Furfaro <nfurfaro33@gmail.com>
2019-05-01 15:09:03 -0600

    Adding metadata functions

--- smart-contracts/contracts/mixins/MixinLockMetadata.sol
+++ smart-contracts/contracts/mixins/MixinLockMetadata.sol
@@ -16,8 +16,14 @@ contract MixinLockMetadata is
   Ownable,
   UnlockUtils
 {
-  /// A descriptive name for a collection of NFTs in this contract
+  /// A descriptive name for a collection of NFTs in this contract.Defaults to "Unlock-Protocol" but is settable by lock owner
   string private lockName;
+
+  /// An abbreviated name for NFTs in this contract. Defaults to "KEY" but is settable by lock owner
+  string private lockSymbol;
+
+  // the base Token URI for this Lock. If not set by lock owner, the global URI stored in Unlock is used.
+  string private baseTokenURI;
 
   constructor(
     string memory _lockName
@@ -45,6 +51,76 @@ contract MixinLockMetadata is
   ) external view
     returns (string memory)
   {
-    return lockName;
+    string memory name;
+    if(lockName == '') {
+      name = unlockProtocol.getGlobalLockName;
+    } else {
+      name = lockName;
+    }
+    return name;
+  }
+
+  /**
+   * Allows the Lock owner to assign a Symbol for this Lock.
+   */
+  function updateLockSymbol(
+    string calldata _lockSymbol
+  ) external
+    onlyOwner
+  {
+    lockSymbol = _lockSymbol;
+  }
+
+  /**
+    * @dev Gets the token symbol
+    * @return string representing the token name
+    */
+  function symbol()
+    external view
+    returns(string memory)
+  {
+    string memory symbol;
+    if(lockSymbol == '') {
+      symbol = unlockProtocol.getGlobalLockSymbol;
+    } else {
+      symbol = lockSymbol;
+    }
+    return symbol;
+  }
+
+  /**
+   * Allows the Lock owner to update the baseTokenURI for this Lock.
+   */
+  function setBaseTokenURI(
+    string calldata _baseTokenURI
+  ) external
+    onlyOwner
+  {
+    baseTokenURI = _baseTokenURI;
+  }
+
+  /**  @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+   * @dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
+   *  3986. The URI may point to a JSON file that conforms to the "ERC721
+   *  Metadata JSON Schema".
+   * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+   */
+  function tokenURI(
+    uint256 _tokenId
+  ) external
+    view
+    isKey(_tokenId)
+    returns(string memory)
+  {
+    string memory URI;
+    if(baseTokenURI == '') {
+      URI = unlockProtocol.getGlobalBaseURI;
+    } else {
+      URI = baseTokenURI;
+    }
+    return UnlockUtils.strConcat(URI, UnlockUtils.strConcat(
+      UnlockUtils.address2Str(address(this)),
+      UnlockUtils.uint2str(_tokenId))
+    );
   }
 }
