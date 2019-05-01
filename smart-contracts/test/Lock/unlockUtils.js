117c5dad767d86e61bdd274460b3c103c8b99361
Nick Furfaro <nfurfaro33@gmail.com>
2019-05-01 10:43:41 -0600

    Add UnlockUtils to MixinMetadata

---
+++ smart-contracts/test/Lock/unlockUtils.js
@@ -0,0 +1,51 @@
+const deployLocks = require('../helpers/deployLocks')
+// const shouldFail = require('../helpers/shouldFail')
+
+const unlockContract = artifacts.require('../Unlock.sol')
+const getUnlockProxy = require('../helpers/proxy')
+
+let unlock, lock
+
+contract('Lock / erc721 / unlockUtils', accounts => {
+  before(async () => {
+    unlock = await getUnlockProxy(unlockContract)
+    const locks = await deployLocks(unlock, accounts[0])
+    lock = locks['FIRST']
+  })
+
+  describe('function uint2str', () => {
+    let str1, str2
+    it('should convert a uint to a string', async () => {
+      str1 = await lock.uint2str.call(0)
+      assert.equal(str1, '0')
+      str2 = await lock.uint2str.call(42)
+      assert.equal(str2, '42')
+    })
+  })
+
+  describe('function strConcat', () => {
+    let resultingStr1, resultingStr2, resultingStr3, moreThan2Str
+    it('should concatenate 2 strings', async () => {
+      resultingStr1 = await lock.strConcat.call('hello', ' unlock')
+      resultingStr2 = await lock.strConcat.call('4', '2')
+      resultingStr3 = await lock.strConcat.call(
+        'https://locksmith.unlock-protocol.com/api/key/',
+        '11'
+      )
+      assert.equal(resultingStr1, 'hello unlock')
+      assert.equal(resultingStr2, '42')
+      assert.equal(
+        resultingStr3,
+        'https://locksmith.unlock-protocol.com/api/key/11'
+      )
+    })
+
+    it('should concatenate more than 2 strings', async () => {
+      moreThan2Str = await lock.strConcat.call(
+        await lock.strConcat.call('1', '2'),
+        '3'
+      )
+      assert.equal(moreThan2Str, '123')
+    })
+  })
+})
