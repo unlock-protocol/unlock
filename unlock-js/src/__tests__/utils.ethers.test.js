f387c62b2456cc8104fa01d6303fa649c06c74dc
Gregory Beaver <greg@unlock-protocol.com>
2019-05-02 11:22:50 -0400

    test the new utils

--- unlock-js/src/__tests__/utils.ethers.test.js
+++ unlock-js/src/__tests__/utils.ethers.test.js
@@ -36,4 +36,43 @@ describe('ethers utils', () => {
       '1000000000000'
     )
   })
+
+  it('isInfiniteKeys', () => {
+    expect.assertions(2)
+
+    expect(ethersUtils.isInfiniteKeys(50000000000)).toBe(false)
+    expect(
+      ethersUtils.isInfiniteKeys(
+        '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+      )
+    ).toBe(true)
+  })
+
+  it('toNumber', () => {
+    expect.assertions(3)
+
+    expect(ethersUtils.toNumber('0x12355')).toBe(74581)
+    expect(ethersUtils.toNumber(74581)).toBe(74581)
+    expect(ethersUtils.toNumber(utils.bigNumberify('0x12355'))).toBe(74581)
+  })
+
+  it('rpcResultNumber', () => {
+    expect.assertions(4)
+
+    expect(ethersUtils.toRpcResultNumber(0)).toBe(
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+    )
+
+    expect(ethersUtils.toRpcResultNumber(100000)).toBe(
+      '0x00000000000000000000000000000000000000000000000000000000000186a0'
+    )
+
+    expect(ethersUtils.toRpcResultNumber('0x12345')).toBe(
+      '0x0000000000000000000000000000000000000000000000000000000000012345'
+    )
+
+    expect(ethersUtils.toRpcResultNumber(utils.bigNumberify('0x12345'))).toBe(
+      '0x0000000000000000000000000000000000000000000000000000000000012345'
+    )
+  })
 })
