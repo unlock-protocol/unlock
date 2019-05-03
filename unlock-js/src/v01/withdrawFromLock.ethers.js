b7f71cedf8eb984da66b28ce77e8583922eb2984
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-05-03 11:54:47 -0400

    migrate withdrawFromLock to ethers (#2978)
    * migrate withdrawFromLock to ethers
    
    * oops! use withdraw gas price instead of updateKeyPrice
    
    * remove obsolete code
    
    * restore the web3 double agents
    
    * fix tests

---
+++ unlock-js/src/v01/withdrawFromLock.ethers.js
@@ -0,0 +1,26 @@
+import { GAS_AMOUNTS } from '../constants'
+import TransactionTypes from '../transactionTypes'
+import Errors from '../errors'
+
+/**
+ * Triggers a transaction to withdraw funds from the lock and assign them to the owner.
+ * @param {PropTypes.address} lock
+ * @param {PropTypes.address} account (unused in ethers)
+ * @param {Function} callback TODO: implement...
+ */
+export default async function(lockAddress) {
+  const lockContract = await this.getLockContract(lockAddress)
+  let transactionPromise
+  try {
+    transactionPromise = lockContract['withdraw()']({
+      gasLimit: GAS_AMOUNTS.withdraw, // overrides default value for transaction gas price
+    })
+    const ret = await this._handleMethodCall(
+      transactionPromise,
+      TransactionTypes.WITHDRAWAL
+    )
+    return ret
+  } catch (error) {
+    this.emit('error', new Error(Errors.FAILED_TO_WITHDRAW_FROM_LOCK))
+  }
+}
