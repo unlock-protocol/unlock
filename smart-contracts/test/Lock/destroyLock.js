4d8bd289f2086380d63b92fd2e68e525659568c1
hardlydifficult <hardlydiff@gmail.com>
2019-05-03 09:57:06 -0700

    Fix test scenarios

--- smart-contracts/test/Lock/destroyLock.js
+++ smart-contracts/test/Lock/destroyLock.js
@@ -17,8 +17,8 @@ let unlock, locks
 contract('Lock / destroyLock', accounts => {
   let lock
   let lockApi
-  let tokenAddresses
   let testToken
+  const scenarios = [false, true]
 
   before(async () => {
     testToken = await TestErc20Token.new()
@@ -28,151 +28,144 @@ contract('Lock / destroyLock', accounts => {
         from: accounts[0],
       })
     }
-    tokenAddresses = [Web3Utils.padLeft(0, 40), testToken.address]
   })
 
-  it('Test each token type', () => {
-    tokenAddresses.forEach(tokenAddress => {
-      describe(`Test ${tokenAddress}`, () => {
+  scenarios.forEach(isErc20 => {
+    describe(`Test ${isErc20 ? 'ERC20' : 'ETH'}`, () => {
+      let tokenAddress
+      before(async () => {
+        tokenAddress = isErc20 ? testToken.address : Web3Utils.padLeft(0, 40)
+        unlock = await getUnlockProxy(unlockContract)
+        locks = await deployLocks(unlock, accounts[0], tokenAddress)
+        lock = locks['FIRST']
+        lockApi = new LockApi(lock)
+
+        for (let i = 0; i < accounts.length; i++) {
+          await testToken.approve(lock.address, -1, { from: accounts[i] })
+        }
+
+        // Add ETH to the lock, even if it's priced in ERC20
+        // TODO: should we block this from happening instead?
+        await lockApi.purchaseFor(
+          accounts[9],
+          accounts[9],
+          Units.convert('0.01', 'eth', 'wei')
+        )
+      })
+
+      it('should fail if called by the wrong account', async () => {
+        await shouldFail(lock.destroyLock({ from: accounts[1] }), '')
+      })
+
+      describe('when called by the owner', () => {
+        let initialLockBalance, initialOwnerBalance, txObj, event
+
         before(async () => {
-          unlock = await getUnlockProxy(unlockContract)
-          locks = await deployLocks(unlock, accounts[0], tokenAddress)
-          lock = locks['FIRST']
-          lockApi = new LockApi(lock)
+          let value =
+            tokenAddress === Web3Utils.padLeft(0, 40)
+              ? Units.convert('0.01', 'eth', 'wei')
+              : 0
 
-          for (let i = 0; i < accounts.length; i++) {
-            await testToken.approve(lock.address, -1, { from: accounts[i] })
-          }
+          await lockApi.purchaseFor(accounts[1], accounts[1], value)
+          assert.equal(await lock.getHasValidKey.call(accounts[1]), true) // pre-req
 
-          // Add ETH to the lock, even if it's priced in ERC20
-          // TODO: should we block this from happening instead?
-          await lockApi.purchaseFor(
-            accounts[9],
-            accounts[9],
-            Units.convert('0.01', 'eth', 'wei')
+          initialLockBalance = await getTokenBalance(lock.address, tokenAddress)
+          initialOwnerBalance = await getTokenBalance(accounts[0], tokenAddress)
+          await lock.disableLock() // We can't destroy a lock without first disabling it
+          txObj = await lock.destroyLock({ from: accounts[0] })
+          event = txObj.logs[0]
+        })
+
+        it('owner should have received funds from the contract', async () => {
+          const finalOwnerBalance = await getTokenBalance(
+            accounts[0],
+            tokenAddress
           )
+          assert(finalOwnerBalance.gt(initialOwnerBalance))
         })
 
-        it('should fail if called by the wrong account', async () => {
-          await shouldFail(lock.destroyLock({ from: accounts[1] }), '')
+        it('contract should no longer have any funds', async () => {
+          const finalLockBalance = await getTokenBalance(
+            lock.address,
+            tokenAddress
+          )
+          assert.equal(finalLockBalance.toFixed(), 0)
         })
 
-        describe('when called by the owner', () => {
-          let initialLockBalance, initialOwnerBalance, txObj, event
+        it("contract should no longer have any ETH even if it's priced in ERC20", async () => {
+          const finalLockBalance = new BigNumber(
+            await web3.eth.getBalance(lock.address)
+          )
+          assert.equal(finalLockBalance.toFixed(), 0)
+        })
 
-          before(async () => {
+        it('should trigger the Destroy event', () => {
+          assert.equal(event.event, 'Destroy')
+          assert.equal(
+            new BigNumber(event.args.balance).toFixed(),
+            initialLockBalance.toFixed()
+          )
+          assert.equal(event.args.owner, accounts[0])
+        })
+
+        it('previously valid key is no longer valid', async () => {
+          if (!process.env.TEST_COVERAGE) {
+            try {
+              await lock.getHasValidKey.call(accounts[1])
+            } catch (e) {
+              assert.equal(
+                e,
+                "Error: Returned values aren't valid, did it run Out of Gas?"
+              )
+            }
+          } else {
+            try {
+              await lock.getHasValidKey.call(accounts[1])
+            } catch (e) {
+              assert(e.message.endsWith('is not a contract address'))
+              return
+            }
+            throw new Error('Expected error')
+          }
+        })
+
+        // After selfdestruct, a user can't buy a key, but if they try they loose their money.
+        it('does not allow people to purchase new keys', async () => {
+          if (!process.env.TEST_COVERAGE) {
+            let initialLockBalance = await getTokenBalance(
+              lock.address,
+              tokenAddress
+            )
+            assert.equal(initialLockBalance.toFixed(), 0)
+
             let value =
               tokenAddress === Web3Utils.padLeft(0, 40)
                 ? Units.convert('0.01', 'eth', 'wei')
                 : 0
 
+            // This line does not fail, but instead calls the fallback function and sends msg.value to the destroyed contract.
             await lockApi.purchaseFor(accounts[1], accounts[1], value)
-            assert.equal(await lock.getHasValidKey.call(accounts[1]), true) // pre-req
 
-            initialLockBalance = await getTokenBalance(
+            let finalLockBalance = await getTokenBalance(
               lock.address,
               tokenAddress
             )
-            initialOwnerBalance = await getTokenBalance(
-              accounts[0],
-              tokenAddress
-            )
-            await lock.disableLock() // We can't destroy a lock without first disabling it
-            txObj = await lock.destroyLock({ from: accounts[0] })
-            event = txObj.logs[0]
-          })
+            assert.equal(finalLockBalance.toFixed(), 10000000000000000)
 
-          it('owner should have received funds from the contract', async () => {
-            const finalOwnerBalance = await getTokenBalance(
-              accounts[0],
-              tokenAddress
-            )
-            assert(finalOwnerBalance.gt(initialOwnerBalance))
-          })
-
-          it('contract should no longer have any funds', async () => {
-            const finalLockBalance = await getTokenBalance(
-              lock.address,
-              tokenAddress
-            )
-            assert.equal(finalLockBalance.toFixed(), 0)
-          })
-
-          it("contract should no longer have any ETH even if it's priced in ERC20", async () => {
-            const finalLockBalance = new BigNumber(
-              await web3.eth.getBalance(lock.address)
-            )
-            assert.equal(finalLockBalance.toFixed(), 0)
-          })
-
-          it('should trigger the Destroy event', () => {
-            assert.equal(event.event, 'Destroy')
-            assert.equal(
-              new BigNumber(event.args.balance).toFixed(),
-              initialLockBalance.toFixed()
-            )
-            assert.equal(event.args.owner, accounts[0])
-          })
-
-          it('previously valid key is no longer valid', async () => {
-            if (!process.env.TEST_COVERAGE) {
-              try {
-                await lock.getHasValidKey.call(accounts[1])
-              } catch (e) {
-                assert.equal(
-                  e,
-                  "Error: Returned values aren't valid, did it run Out of Gas?"
-                )
-              }
-            } else {
-              try {
-                await lock.getHasValidKey.call(accounts[1])
-              } catch (e) {
-                assert(e.message.endsWith('is not a contract address'))
-                return
-              }
-              throw new Error('Expected error')
+            // The user did not purchase a key, but still sent their eth to the contract.
+            // Calling getHasValidKey will fail
+            // assert.equal(await lock.getHasValidKey.call(accounts[1]), false)
+          } else {
+            try {
+              await lock.purchaseFor(accounts[1], {
+                value: Units.convert('0.01', 'eth', 'wei'),
+              })
+            } catch (e) {
+              assert(e.message.endsWith('is not a contract address'))
+              return
             }
-          })
-
-          // After selfdestruct, a user can't buy a key, but if they try they loose their money.
-          it('does not allow people to purchase new keys', async () => {
-            if (!process.env.TEST_COVERAGE) {
-              let initialLockBalance = await getTokenBalance(
-                lock.address,
-                tokenAddress
-              )
-              assert.equal(initialLockBalance.toFixed(), 0)
-
-              let value =
-                tokenAddress === Web3Utils.padLeft(0, 40)
-                  ? Units.convert('0.01', 'eth', 'wei')
-                  : 0
-
-              // This line does not fail, but instead calls the fallback function and sends msg.value to the destroyed contract.
-              await lockApi.purchaseFor(accounts[1], accounts[1], value)
-
-              let finalLockBalance = await getTokenBalance(
-                lock.address,
-                tokenAddress
-              )
-              assert.equal(finalLockBalance.toFixed(), 10000000000000000)
-
-              // The user did not purchase a key, but still sent their eth to the contract.
-              // Calling getHasValidKey will fail
-              // assert.equal(await lock.getHasValidKey.call(accounts[1]), false)
-            } else {
-              try {
-                await lock.purchaseFor(accounts[1], {
-                  value: Units.convert('0.01', 'eth', 'wei'),
-                })
-              } catch (e) {
-                assert(e.message.endsWith('is not a contract address'))
-                return
-              }
-              throw new Error('Expected error')
-            }
-          })
+            throw new Error('Expected error')
+          }
         })
       })
     })
