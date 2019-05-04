3ccb7bec783648c1233e60d0b0c9606316d67d29
Gregory Beaver <greg@unlock-protocol.com>
2019-05-04 10:20:40 -0400

    remove leftovers

--- unlock-js/index.js
+++ unlock-js/index.js
@@ -6,12 +6,10 @@ const {
 } = require('./lib/accounts')
 const { getCurrentProvider, getWeb3Provider } = require('./lib/providers')
 const deploy = require('./lib/deploy').default
-const { ETHERS_MAX_UINT } = require('./lib/constants')
 
 module.exports = {
   Web3Service: Web3Service.default,
   WalletService: WalletService.default,
-  INFINITE_KEYS: ETHERS_MAX_UINT,
   createAccountAndPasswordEncryptKey,
   getAccountFromPrivateKey,
   getCurrentProvider,
