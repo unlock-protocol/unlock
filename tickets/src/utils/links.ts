ceac8ca248e992c7e3ab85ba1ecd85b766a5889f
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-06 17:09:19 -0400

    harcoding duration for the NFT event and updating the calendar link accordingly (#3043)

--- tickets/src/utils/links.ts
+++ tickets/src/utils/links.ts
@@ -1,7 +1,13 @@
-const formatDateForCalendar = (date: Date) => {
+const formatDateForCalendar = (date: Date, allDay: boolean) => {
   const year = date.getFullYear()
   const month = ('0' + (date.getMonth() + 1)).slice(-2)
   const day = ('0' + date.getDate()).slice(-2)
+  if (!allDay) {
+    const hours = ('0' + date.getUTCHours()).slice(-2)
+    const minutes = ('0' + date.getMinutes()).slice(-2)
+    const time = `T${hours}${minutes}00Z`
+    return [year, month, day, time].join('')
+  }
   return [year, month, day].join('')
 }
 
@@ -9,15 +15,26 @@ export function googleCalendarLinkBuilder(
   name: string,
   details: string,
   date: Date,
+  duration: number,
   location: string
 ) {
-  const start = formatDateForCalendar(date)
-  const end = new Date(date)
-  end.setDate(date.getDate() + 1)
+  const start = formatDateForCalendar(date, !duration)
+  let end
+  if (duration) {
+    // Duration is in seconds
+    end = formatDateForCalendar(
+      new Date(new Date(date).getTime() + duration * 1000),
+      false
+    )
+  } else {
+    const endDate = new Date(date)
+    endDate.setDate(date.getDate() + 1)
+    end = formatDateForCalendar(endDate, true)
+  }
 
   let googleCalendarLink = `https://calendar.google.com/calendar/r/eventedit?`
   googleCalendarLink += `&text=${name}`
-  googleCalendarLink += `&dates=${start}/${formatDateForCalendar(end)}`
+  googleCalendarLink += `&dates=${start}/${end}`
   googleCalendarLink += `&details=${details}`
   googleCalendarLink += `&location=${location}`
   googleCalendarLink += `&sf=true&output=xml`
