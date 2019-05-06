0bec45391d496812d6cf66f18778d02b11b7ca6b
Christoph Nascone <chris@unlock-protocol.com>
2019-05-06 13:02:53 -0400

    Starting to update CreatorLock stories

--- unlock-app/src/stories/creator/CreatorLock.stories.js
+++ unlock-app/src/stories/creator/CreatorLock.stories.js
@@ -6,6 +6,25 @@ import CreatorLock from '../../components/creator/CreatorLock'
 import createUnlockStore from '../../createUnlockStore'
 import { ConfigContext } from '../../utils/withConfig'
 
+const lockWith = params => {
+  const standardLock = {
+    address: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
+    asOf: 31337,
+    balance: '0.045',
+    expirationDuration: '2592000',
+    keyPrice: '0.01',
+    maxNumberOfKeys: 42,
+    name: 'Lock Robster',
+    outstandingKeys: 5,
+    owner: '0xaFAEfc6dd3C9feF66f92BA838b132644451F0716',
+    totalSupply: 5,
+    transaction: 'defaultTransaction',
+    unlimitedKeys: false,
+  }
+
+  return Object.assign(standardLock, params)
+}
+
 const withdrawalConfirmingAddress = '0xAAAAAAAAAAAAAAAAAAAAAAAAAA73289473298'
 const withdrawalSubmittedAddress = '0xbbbbbbbbbbbbbbbbbbbbbbbbbb73289473298'
 const store = createUnlockStore({
@@ -33,10 +52,16 @@ const store = createUnlockStore({
       withdrawal: withdrawalSubmittedAddress,
     },
   },
+  keysForLockByPage: {
+    '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B': {
+      keys: {},
+      page: '',
+    },
+  },
   keys: {
     keyid: {
-      transaction: '0x23749328748932748932473298473289473298',
-      lock: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+      lock: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
       expiration: Math.floor(new Date().getTime() / 1000) + 86400 * 30, // 30 days from right now
       data: 'ben@unlock-protocol.com',
     },
@@ -57,48 +82,25 @@ storiesOf('CreatorLock', module)
   .addDecorator(getStory => (
     <ConfigProvider value={config}>{getStory()}</ConfigProvider>
   ))
-  .add('Deployed', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: 'deployedid',
-    }
-    return <CreatorLock lock={lock} edit={action('edit')} />
-  })
   .add('Submitted', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0x127c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+    const lock = lockWith({
       transaction: 'submittedid',
-    }
+      balance: '0',
+      outstandingKeys: 0,
+    })
     return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Confirming', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+    const lock = lockWith({
       transaction: 'confirmingid',
-    }
-    const transaction = {
-      status: 'mined',
-      confirmations: 2,
-    }
-    return (
-      <CreatorLock
-        lock={lock}
-        transaction={transaction}
-        edit={action('edit')}
-      />
-    )
+      balance: '0',
+      outstandingKeys: 0,
+    })
+    return <CreatorLock lock={lock} edit={action('edit')} />
+  })
+  .add('Deployed', () => {
+    const lock = lockWith({ transaction: 'deployedid' })
+    return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Not found', () => {
     const lock = {
