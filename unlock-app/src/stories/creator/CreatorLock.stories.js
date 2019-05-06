53fedc2d61fee045831a349bd85f79dd726de0ed
Christopher Nascone <cnasc@users.noreply.github.com>
2019-05-06 15:22:28 -0400

    Fix withdrawal text (#3037)
    * Starting to update CreatorLock stories
    
    * Submitted now shows as submitted
    
    * Submitted withdrawal now shows as submitted
    
    * Confirming withdrawal story works now
    
    * Fix mysterious appearing zero
    
    * Update stories

--- unlock-app/src/stories/creator/CreatorLock.stories.js
+++ unlock-app/src/stories/creator/CreatorLock.stories.js
@@ -5,6 +5,26 @@ import { action } from '@storybook/addon-actions'
 import CreatorLock from '../../components/creator/CreatorLock'
 import createUnlockStore from '../../createUnlockStore'
 import { ConfigContext } from '../../utils/withConfig'
+import { TransactionType } from '../../unlockTypes'
+
+const lockWith = params => {
+  const standardLock = {
+    address: '0xabcdef123',
+    asOf: 31337,
+    balance: '0.045',
+    expirationDuration: 2592000,
+    keyPrice: '0.01',
+    maxNumberOfKeys: 42,
+    name: 'Lock Robster',
+    outstandingKeys: 5,
+    owner: '0xaFAEfc6dd3C9feF66f92BA838b132644451F0716',
+    totalSupply: 5,
+    transaction: 'defaultTransaction',
+    unlimitedKeys: false,
+  }
+
+  return Object.assign(standardLock, params)
+}
 
 const withdrawalConfirmingAddress = '0xAAAAAAAAAAAAAAAAAAAAAAAAAA73289473298'
 const withdrawalSubmittedAddress = '0xbbbbbbbbbbbbbbbbbbbbbbbbbb73289473298'
@@ -13,30 +33,40 @@ const store = createUnlockStore({
     deployedid: {
       status: 'mined',
       confirmations: 24,
+      lock: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
     },
     confirmingid: {
       status: 'mined',
       confirmations: 4,
+      type: TransactionType.LOCK_CREATION,
+      lock: '0x123456abc',
     },
     submittedid: {
       status: 'submitted',
       confirmations: 0,
+      type: TransactionType.LOCK_CREATION,
+      lock: '0xabcdef123',
     },
     withdrawalconfirmingid: {
       status: 'mined',
       confirmations: 2,
       withdrawal: withdrawalConfirmingAddress,
+      lock: '0xfeedbabe',
+      type: TransactionType.WITHDRAWAL,
     },
     withdrawalsubmittedid: {
       status: 'submitted',
       confirmations: 0,
       withdrawal: withdrawalSubmittedAddress,
+      lock: '0xdefcab445',
+      type: TransactionType.WITHDRAWAL,
     },
   },
+  keysForLockByPage: {},
   keys: {
     keyid: {
-      transaction: '0x23749328748932748932473298473289473298',
-      lock: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      transaction: 'deployedid',
+      lock: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
       expiration: Math.floor(new Date().getTime() / 1000) + 86400 * 30, // 30 days from right now
       data: 'ben@unlock-protocol.com',
     },
@@ -57,50 +87,32 @@ storiesOf('CreatorLock', module)
   .addDecorator(getStory => (
     <ConfigProvider value={config}>{getStory()}</ConfigProvider>
   ))
-  .add('Deployed', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
-      transaction: 'deployedid',
-    }
-    return <CreatorLock lock={lock} edit={action('edit')} />
-  })
   .add('Submitted', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0x127c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+    const lock = lockWith({
       transaction: 'submittedid',
-    }
+      balance: '0',
+      outstandingKeys: 0,
+    })
     return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Confirming', () => {
-    const lock = {
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+    const lock = lockWith({
+      address: '0x123456abc',
       transaction: 'confirmingid',
-    }
-    const transaction = {
-      status: 'mined',
-      confirmations: 2,
-    }
-    return (
-      <CreatorLock
-        lock={lock}
-        transaction={transaction}
-        edit={action('edit')}
-      />
-    )
+      balance: '0',
+      outstandingKeys: 0,
+    })
+    return <CreatorLock lock={lock} edit={action('edit')} />
+  })
+  .add('Deployed', () => {
+    const lock = lockWith({
+      address: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
+      transaction: 'deployedid',
+    })
+    return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Not found', () => {
+    // TODO: what is this actually supposed to test?
     const lock = {
       keyPrice: '0.01',
       expirationDuration: 172800,
@@ -112,47 +124,24 @@ storiesOf('CreatorLock', module)
     return <CreatorLock lock={lock} transaction={null} edit={action('edit')} />
   })
   .add('With key', () => {
-    const lock = {
+    // TODO: not sure how to force this to have a key...
+    const lock = lockWith({
       keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
       outstandingKeys: 3,
-      address: '0xab7c74abc0c4d48d1bdad5dcb26153fc8780f83e',
+      address: '0xBF6C4DC63B4a2cD73884552DF6FeB7cD2d48278B',
       transaction: 'deployedid',
-    }
-    const transaction = {
-      status: 'mined',
-      confirmations: 14,
-    }
-    return (
-      <CreatorLock
-        lock={lock}
-        transaction={transaction}
-        edit={action('edit')}
-      />
-    )
+    })
+    return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Withdrawal submitted', () => {
-    const lock = {
-      id: 'lockwithdrawalsubmittedid',
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: withdrawalSubmittedAddress,
-      transaction: 'deployedid',
-    }
+    const lock = lockWith({
+      address: '0xdefcab445',
+    })
     return <CreatorLock lock={lock} edit={action('edit')} />
   })
   .add('Withdrawing', () => {
-    const lock = {
-      id: 'lockwithdrawalconfirmingid',
-      keyPrice: '0.01',
-      expirationDuration: 172800,
-      maxNumberOfKeys: 240,
-      outstandingKeys: 3,
-      address: withdrawalConfirmingAddress,
-      transaction: 'deployedid',
-    }
+    const lock = lockWith({
+      address: '0xfeedbabe',
+    })
     return <CreatorLock lock={lock} edit={action('edit')} />
   })
