1ef36b32a7820b310e9b772b274b1ff1333c2658
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-06 13:39:13 -0400

    fixing date handling (#3026)

--- tickets/src/utils/links.ts
+++ tickets/src/utils/links.ts
@@ -1,7 +1,7 @@
 const formatDateForCalendar = (date: Date) => {
   const year = date.getFullYear()
   const month = ('0' + (date.getMonth() + 1)).slice(-2)
-  const day = ('0' + (date.getDate() + 1)).slice(-2)
+  const day = ('0' + date.getDate()).slice(-2)
   return [year, month, day].join('')
 }
 
