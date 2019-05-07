0610cbd94ae588b95425b9b2d1e8550717fc3f12
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-06 22:19:18 -0400

    manual deploy of tickets (#3051)

--- tickets/src/utils/links.ts
+++ tickets/src/utils/links.ts
@@ -1,7 +1,7 @@
 const formatDateForCalendar = (date: Date, allDay: boolean) => {
-  const year = date.getFullYear()
-  const month = ('0' + (date.getMonth() + 1)).slice(-2)
-  const day = ('0' + date.getDate()).slice(-2)
+  const year = date.getUTCFullYear()
+  const month = ('0' + (date.getUTCMonth() + 1)).slice(-2)
+  const day = ('0' + date.getUTCDate()).slice(-2)
   if (!allDay) {
     const hours = ('0' + date.getUTCHours()).slice(-2)
     const minutes = ('0' + date.getMinutes()).slice(-2)
