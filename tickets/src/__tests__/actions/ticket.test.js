9791b4ddb16b5a21e1fd2a55787842bc0b4c2a24
Ben Werdmuller <ben@benwerd.com>
2019-05-08 09:08:47 -0700

    Ticket verification actions (#3072)

--- tickets/src/__tests__/actions/ticket.test.js
+++ tickets/src/__tests__/actions/ticket.test.js
@@ -3,6 +3,12 @@ import {
   signAddress,
   GOT_SIGNED_ADDRESS,
   gotSignedAddress,
+  VERIFY_SIGNED_ADDRESS,
+  verifySignedAddress,
+  SIGNED_ADDRESS_VERIFIED,
+  signedAddressVerified,
+  SIGNED_ADDRESS_MISMATCH,
+  signedAddressMismatch,
 } from '../../actions/ticket'
 
 describe('ticket actions', () => {
@@ -28,5 +34,48 @@ describe('ticket actions', () => {
     }
 
     expect(gotSignedAddress(address, signedAddress)).toEqual(expectedAction)
+  })
+
+  it('should create an action emitting a request to verify that the address and the signed address match', () => {
+    expect.assertions(1)
+    const address = '0x12345678'
+    const signedAddress = 'ENCRYPTED'
+    const expectedAction = {
+      type: VERIFY_SIGNED_ADDRESS,
+      address,
+      signedAddress,
+    }
+
+    expect(verifySignedAddress(address, signedAddress)).toEqual(expectedAction)
+  })
+
+  it('should create an action emitting a notification that the address and the signed address match', () => {
+    expect.assertions(1)
+    const address = '0x12345678'
+    const signedAddress = 'ENCRYPTED'
+    const expectedAction = {
+      type: SIGNED_ADDRESS_VERIFIED,
+      address,
+      signedAddress,
+    }
+
+    expect(signedAddressVerified(address, signedAddress)).toEqual(
+      expectedAction
+    )
+  })
+
+  it('should create an action emitting a notification that the address and the signed address do not match', () => {
+    expect.assertions(1)
+    const address = '0x12345678'
+    const signedAddress = 'ENCRYPTED'
+    const expectedAction = {
+      type: SIGNED_ADDRESS_MISMATCH,
+      address,
+      signedAddress,
+    }
+
+    expect(signedAddressMismatch(address, signedAddress)).toEqual(
+      expectedAction
+    )
   })
 })
