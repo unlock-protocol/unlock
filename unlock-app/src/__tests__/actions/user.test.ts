1a170af9d450b0b8bf3d6bcbf088b871f3aef080
Christoph Nascone <chris@unlock-protocol.com>
2019-05-09 09:50:00 -0400

    Wrap these actions with a master description

--- unlock-app/src/__tests__/actions/user.test.ts
+++ unlock-app/src/__tests__/actions/user.test.ts
@@ -17,96 +17,100 @@ import {
   signupSucceeded,
 } from '../../actions/user'
 
-describe('login actions', () => {
-  it('should create an action with credentials in tow', () => {
-    expect.assertions(1)
-    const emailAddress = 'vitalik@bagholde.rs'
-    const password = 'guest'
-    const expectedAction = {
-      type: LOGIN_CREDENTIALS,
-      emailAddress,
-      password,
-    }
+describe('user account actions', () => {
+  describe('login actions', () => {
+    it('should create an action with credentials in tow', () => {
+      expect.assertions(1)
+      const emailAddress = 'vitalik@bagholde.rs'
+      const password = 'guest'
+      const expectedAction = {
+        type: LOGIN_CREDENTIALS,
+        emailAddress,
+        password,
+      }
 
-    expect(loginCredentials({ emailAddress, password })).toEqual(expectedAction)
+      expect(loginCredentials({ emailAddress, password })).toEqual(
+        expectedAction
+      )
+    })
+
+    it('should create an action to indicate successful login', () => {
+      expect.assertions(1)
+      const expectedAction = {
+        type: LOGIN_SUCCEEDED,
+      }
+
+      expect(loginSucceeded()).toEqual(expectedAction)
+    })
+
+    it('should create an action to indicate failed login', () => {
+      expect.assertions(1)
+      const reason = 'Incorrect secret Swiss account #'
+      const expectedAction = {
+        type: LOGIN_FAILED,
+        reason,
+      }
+
+      expect(loginFailed(reason)).toEqual(expectedAction)
+    })
   })
 
-  it('should create an action to indicate successful login', () => {
-    expect.assertions(1)
-    const expectedAction = {
-      type: LOGIN_SUCCEEDED,
-    }
+  describe('signup actions', () => {
+    it('should create an action indicating an email has been submitted', () => {
+      expect.assertions(1)
+      const emailAddress = 'ray@smuckles.escalade'
+      const expectedAction = {
+        type: SIGNUP_EMAIL,
+        emailAddress,
+      }
 
-    expect(loginSucceeded()).toEqual(expectedAction)
-  })
+      expect(signupEmail(emailAddress)).toEqual(expectedAction)
+    })
 
-  it('should create an action to indicate failed login', () => {
-    expect.assertions(1)
-    const reason = 'Incorrect secret Swiss account #'
-    const expectedAction = {
-      type: LOGIN_FAILED,
-      reason,
-    }
+    it('should create an action indicating a password has been submitted', () => {
+      expect.assertions(1)
+      const password = 'entropy9'
+      const expectedAction = {
+        type: SIGNUP_PASSWORD,
+        password,
+      }
 
-    expect(loginFailed(reason)).toEqual(expectedAction)
-  })
-})
+      expect(signupPassword(password)).toEqual(expectedAction)
+    })
 
-describe('signup actions', () => {
-  it('should create an action indicating an email has been submitted', () => {
-    expect.assertions(1)
-    const emailAddress = 'ray@smuckles.escalade'
-    const expectedAction = {
-      type: SIGNUP_EMAIL,
-      emailAddress,
-    }
+    it('should create an action indicating a bundle of credentials has been submitted', () => {
+      expect.assertions(1)
+      const password = 'entropy9'
+      const emailAddress = 'ray@smuckles.escalade'
+      const expectedAction = {
+        type: SIGNUP_CREDENTIALS,
+        password,
+        emailAddress,
+      }
 
-    expect(signupEmail(emailAddress)).toEqual(expectedAction)
-  })
+      expect(signupCredentials({ password, emailAddress })).toEqual(
+        expectedAction
+      )
+    })
 
-  it('should create an action indicating a password has been submitted', () => {
-    expect.assertions(1)
-    const password = 'entropy9'
-    const expectedAction = {
-      type: SIGNUP_PASSWORD,
-      password,
-    }
+    it('should create an action indicating a signup has failed', () => {
+      expect.assertions(1)
+      const reason = 'The email is already in use.'
+      const expectedAction = {
+        type: SIGNUP_FAILED,
+        reason,
+      }
 
-    expect(signupPassword(password)).toEqual(expectedAction)
-  })
+      expect(signupFailed(reason)).toEqual(expectedAction)
+    })
 
-  it('should create an action indicating a bundle of credentials has been submitted', () => {
-    expect.assertions(1)
-    const password = 'entropy9'
-    const emailAddress = 'ray@smuckles.escalade'
-    const expectedAction = {
-      type: SIGNUP_CREDENTIALS,
-      password,
-      emailAddress,
-    }
+    it('should create an action indicating a signup has succeeded', () => {
+      expect.assertions(1)
+      const expectedAction = {
+        type: SIGNUP_SUCCEEDED,
+      }
 
-    expect(signupCredentials({ password, emailAddress })).toEqual(
-      expectedAction
-    )
-  })
-
-  it('should create an action indicating a signup has failed', () => {
-    expect.assertions(1)
-    const reason = 'The email is already in use.'
-    const expectedAction = {
-      type: SIGNUP_FAILED,
-      reason,
-    }
-
-    expect(signupFailed(reason)).toEqual(expectedAction)
-  })
-
-  it('should create an action indicating a signup has succeeded', () => {
-    expect.assertions(1)
-    const expectedAction = {
-      type: SIGNUP_SUCCEEDED,
-    }
-
-    expect(signupSucceeded()).toEqual(expectedAction)
+      expect(signupSucceeded()).toEqual(expectedAction)
+    })
   })
 })
