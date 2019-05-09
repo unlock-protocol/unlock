be1cc75ddd132f370a99b4a5523bcaedc3dc45f6
Christopher Nascone <cnasc@users.noreply.github.com>
2019-05-09 09:30:59 -0400

    Convert StorageService to be an EventEmitter (#3088)
    * Migrate storeTransaction to eventEmitter
    
    * Migrate getTransactionHashesSentBy to eventEmitter
    
    * Migrate lockLookUp to eventEmitter
    
    * migrate storeLockDetails
    
    * Migrate updateLockDetails
    
    * Migrate createUser to eventEmitter
    
    * Migrate updateUser
    
    * Migrate getUserPrivateKey
    
    * Migrate getUserRecoveryPhrase
    
    * Param consistency
    
    * Consistent testing format
    
    * Updating export/import
    
    * remove unused action
    
    * It's happening
    
    * Almost done migrating to eventEmitter
    
    * Final cleanup

--- unlock-app/src/__tests__/actions/storage.test.js
+++ unlock-app/src/__tests__/actions/storage.test.js
@@ -1,9 +1,4 @@
-import {
-  storageError,
-  STORAGE_ERROR,
-  storeLockName,
-  STORE_LOCK_NAME,
-} from '../../actions/storage'
+import { storageError, STORAGE_ERROR } from '../../actions/storage'
 
 describe('Storage error', () => {
   it('should create an action emitting a storage error', () => {
@@ -16,23 +11,5 @@ describe('Storage error', () => {
     }
 
     expect(storageError(error)).toEqual(expectation)
-  })
-})
-
-describe('Store Lock Creation', () => {
-  it('should create an action indicating storage of a newly created lock', () => {
-    expect.assertions(1)
-    const owner = "An owner's address"
-    const lock = {}
-    const token = 'An authorization token'
-
-    const expectation = {
-      type: STORE_LOCK_NAME,
-      owner: owner,
-      lock: lock,
-      token: token,
-    }
-
-    expect(storeLockName(owner, lock, token)).toEqual(expectation)
   })
 })
--- unlock-app/src/middlewares/storageMiddleware.js
+++ unlock-app/src/middlewares/storageMiddleware.js
@@ -8,8 +8,8 @@ import { UPDATE_LOCK, updateLock, UPDATE_LOCK_NAME } from '../actions/lock'
 
 import { startLoading, doneLoading } from '../actions/loading'
 
-import StorageService from '../services/storageService'
-import { STORE_LOCK_NAME, storageError } from '../actions/storage'
+import { StorageService, success, failure } from '../services/storageService'
+import { storageError } from '../actions/storage'
 
 import { NEW_TRANSACTION, addTransaction } from '../actions/transaction'
 import { SET_ACCOUNT, setAccount } from '../actions/accounts'
@@ -22,49 +22,82 @@ import {
   loginSucceeded,
 } from '../actions/login'
 import UnlockUser from '../structured_data/unlockUser'
-import { setError } from '../actions/error'
-import { FAILED_TO_CREATE_USER } from '../errors'
 
 const storageMiddleware = config => {
   const { services } = config
   return ({ getState, dispatch }) => {
     const storageService = new StorageService(services.storage.host)
 
+    // NEW_TRANSACTION
+    storageService.on(failure.storeTransaction, error => {
+      dispatch(storageError(error))
+    })
+
+    // SET_ACCOUNT
+    storageService.on(success.getTransactionHashesSentBy, ({ hashes }) => {
+      // Dispatch each lock. Greg probably wants to do a batch action?
+      hashes.forEach(hash => {
+        if (hash.network === getState().network.name) {
+          dispatch(addTransaction(hash))
+        }
+      })
+      dispatch(doneLoading())
+    })
+    storageService.on(failure.getTransactionHashesSentBy, error => {
+      dispatch(storageError(error))
+      dispatch(doneLoading())
+    })
+
+    // UPDATE_LOCK
+    storageService.on(success.lockLookUp, ({ address, name }) => {
+      dispatch(updateLock(address, { name }))
+    })
+    storageService.on(failure.lockLookUp, error => {
+      dispatch(storageError(error))
+    })
+
+    // SIGNED_DATA
+    storageService.on(failure.storeLockDetails, ({ error }) => {
+      dispatch(storageError(error))
+    })
+
+    // SIGNUP_CREDENTIALS
+    storageService.on(success.createUser, publicKey => {
+      dispatch(setAccount({ address: publicKey }))
+    })
+    storageService.on(failure.createUser, error => {
+      dispatch(storageError(error))
+    })
+
+    // LOGIN_CREDENTIALS
+    storageService.on(failure.getUserPrivateKey, ({ error }) => {
+      dispatch(storageError(error))
+    })
+
     return next => {
       return action => {
-        // TODO: never async/await middlewares
+        if (action.type === NEW_TRANSACTION) {
+          // Storing a new transaction so that we can easily point to it later on
+          storageService.storeTransaction(
+            action.transaction.hash,
+            action.transaction.from,
+            action.transaction.to,
+            getState().network.name
+          )
+        }
+
         if (action.type === SET_ACCOUNT) {
           dispatch(startLoading())
           // When we set the account, we want to retrieve the list of transactions
-          storageService
-            .getTransactionsHashesSentBy(action.account.address)
-            .then(transactions => {
-              dispatch(doneLoading())
-              // Dispatch each lock. Greg probably wants to a batch action?
-              transactions.forEach(transaction => {
-                if (transaction.network === getState().network.name) {
-                  dispatch(addTransaction(transaction))
-                }
-              })
-            })
-            .catch(error => {
-              dispatch(doneLoading())
-              dispatch(storageError(error))
-            })
+          storageService.getTransactionsHashesSentBy(action.account.address)
         }
 
-        if (action.type === NEW_TRANSACTION) {
-          // Storing a new transaction so that we can easoly point to it later on
-          storageService
-            .storeTransaction(
-              action.transaction.hash,
-              action.transaction.from,
-              action.transaction.to,
-              getState().network.name
-            )
-            .catch(error => {
-              dispatch(storageError(error))
-            })
+        if (action.type === UPDATE_LOCK) {
+          // Only look up the name for a lock for which the name is empty/not-set
+          const lock = getState().locks[action.address]
+          if (lock && !lock.name) {
+            storageService.lockLookUp(action.address)
+          }
         }
 
         if (
@@ -73,11 +106,7 @@ const storageMiddleware = config => {
           action.data.message.lock
         ) {
           // Once signed, let's save it!
-          storageService
-            .storeLockDetails(action.data, action.signature)
-            .catch(error => {
-              dispatch(storageError(error))
-            })
+          storageService.storeLockDetails(action.data, action.signature)
         }
 
         if (action.type === UPDATE_LOCK_NAME) {
@@ -90,31 +119,6 @@ const storageMiddleware = config => {
           })
           // Ask someone to sign it!
           dispatch(signData(data))
-        }
-
-        // TODO : remove me because it is not needed anymore
-        if (action.type === STORE_LOCK_NAME) {
-          // A new lock has been created
-          storageService
-            .storeLockDetails(action.lock, action.token)
-            .catch(error => {
-              dispatch(storageError(error))
-            })
-        }
-
-        if (action.type === UPDATE_LOCK) {
-          // Only look up the name for a lock for which the name is empty/not-set
-          const lock = getState().locks[action.address]
-          if (lock && !lock.name) {
-            storageService
-              .lockLookUp(action.address)
-              .then(name => {
-                dispatch(updateLock(action.address, { name }))
-              })
-              .catch(error => {
-                dispatch(storageError(error))
-              })
-          }
         }
 
         if (action.type === SIGNUP_CREDENTIALS) {
@@ -131,31 +135,23 @@ const storageMiddleware = config => {
             passwordEncryptedPrivateKey,
           })
 
-          storageService
-            .createUser(user) // TODO: Now what?
-            .then(() => dispatch(setAccount({ address })))
-            .catch(() => {
-              dispatch(setError(FAILED_TO_CREATE_USER))
-            })
+          storageService.createUser(user)
         }
 
         if (action.type === LOGIN_CREDENTIALS) {
           const { emailAddress, password } = action
-          storageService
-            .getUserPrivateKey(emailAddress)
-            .then(key => {
-              try {
-                // TODO: store more than just the account address (encrypted key, etc.)
-                const account = getAccountFromPrivateKey(key, password)
-                if (account && account.address) {
-                  dispatch(loginSucceeded())
-                  dispatch(setAccount(account))
-                }
-              } catch (err) {
-                dispatch(loginFailed(err))
+          storageService.getUserPrivateKey(emailAddress).then(key => {
+            try {
+              // TODO: store more than just the account address (encrypted key, etc.)
+              const account = getAccountFromPrivateKey(key, password)
+              if (account && account.address) {
+                dispatch(loginSucceeded())
+                dispatch(setAccount(account))
               }
-            })
-            .catch(err => dispatch(loginFailed(err)))
+            } catch (err) {
+              dispatch(loginFailed(err))
+            }
+          })
         }
 
         next(action)
