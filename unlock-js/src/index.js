ded48901396eda951fdc94c2416560ee4baa385d
Gregory Beaver <greg@unlock-protocol.com>
2019-05-10 09:02:57 -0400

    esm-ify unlockService and utils, add esm index.js to unlock-js (#3134)
    * prepare the source for es modules
    
    * revert unintentional whitespace change

---
+++ unlock-js/src/index.js
@@ -0,0 +1,8 @@
+export { default as Web3Service } from './web3Service'
+export { default as WalletService } from './walletService'
+export {
+  createAccountAndPasswordEncryptKey,
+  getAccountFromPrivateKey,
+} from './accounts'
+export { getCurrentProvider, getWeb3Provider } from './providers'
+export { default as deploy } from './deploy'
