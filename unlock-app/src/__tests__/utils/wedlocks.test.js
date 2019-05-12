23aa33bf19b1492cb672bf1b43e73bf0d8a68154
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-12 17:47:56 -0400

    Adding a util to check the validity of signed values. (#3150)

---
+++ unlock-app/src/__tests__/utils/wedlocks.test.js
@@ -0,0 +1,53 @@
+import forge from 'node-forge'
+import { Base64 } from 'js-base64'
+
+import { verifyEmailSignature } from '../../utils/wedlocks'
+
+const emailAddressToSign = 'julien@unlock-protocol.com'
+let signature, base64PublicKey
+
+describe('verifyEmailSignature', () => {
+  beforeAll(done => {
+    forge.rsa.generateKeyPair({ bits: 2048, workers: 2 }, function(
+      err,
+      keypair
+    ) {
+      const privateKeyPem = forge.pki.privateKeyToPem(keypair.privateKey)
+      const publicKey = forge.pki.publicKeyToPem(keypair.publicKey)
+      base64PublicKey = Base64.encode(publicKey)
+      // First, let's create a signature!
+      const md = forge.md.sha1.create()
+      md.update(emailAddressToSign, 'utf8')
+      const privateKey = forge.pki.privateKeyFromPem(privateKeyPem)
+      const signed = privateKey.sign(md)
+      signature = Buffer.from(signed).toString('base64')
+      done()
+    })
+  })
+
+  it('should return true if the signature matches the wedlocks public key', () => {
+    expect.assertions(1)
+    expect(
+      verifyEmailSignature(emailAddressToSign, signature, base64PublicKey)
+    ).toBe(true)
+  })
+
+  it('should return false if the signature does not match the wedlocks public key', () => {
+    expect.assertions(1)
+    const badSignature = Buffer.from('not a signature').toString('base64')
+    expect(
+      verifyEmailSignature(emailAddressToSign, badSignature, base64PublicKey)
+    ).toBe(false)
+  })
+
+  it('should return false if the signature is for another email address', () => {
+    expect.assertions(1)
+    expect(
+      verifyEmailSignature(
+        'another@unlock-protocol.com',
+        signature,
+        base64PublicKey
+      )
+    ).toBe(false)
+  })
+})
---
+++ unlock-app/src/utils/wedlocks.js
@@ -0,0 +1,29 @@
+import forge from 'node-forge'
+import { Base64 } from 'js-base64'
+import configure from '../config'
+
+const config = configure()
+
+/**
+ * Verifies that an email was indeed signed by wedlocks
+ */
+export const verifyEmailSignature = (
+  email,
+  signed,
+  base64WedlocksPublicKey = config.base64WedlocksPublicKey
+) => {
+  try {
+    const signature = Base64.decode(signed)
+    const md = forge.md.sha1.create()
+    md.update(email, 'utf8')
+    const publicKeyPem = Base64.decode(base64WedlocksPublicKey)
+    const publicKey = forge.pki.publicKeyFromPem(publicKeyPem)
+    return publicKey.verify(md.digest().bytes(), signature)
+  } catch (e) {
+    return false
+  }
+}
+
+export default {
+  verifyEmailSignature,
+}
