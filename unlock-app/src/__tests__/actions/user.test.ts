ec790769ade8ac6b78e6cfc41fbcf5258b8450dd
Christopher Nascone <cnasc@users.noreply.github.com>
2019-05-12 08:10:23 -0400

    Store encrypted private key in Redux (#3143)
    * Add actions for setting encrypted key in state
    
    * Create reducer for encrypted private key
    
    * Create the private key reducer
    
    * State can hold encrypted private key now

--- unlock-app/src/__tests__/actions/user.test.ts
+++ unlock-app/src/__tests__/actions/user.test.ts
@@ -17,6 +17,8 @@ import {
   changePassword,
   GOT_PASSWORD,
   gotPassword,
+  SET_ENCRYPTED_PRIVATE_KEY,
+  setEncryptedPrivateKey,
 } from '../../actions/user'
 
 describe('user account actions', () => {
@@ -54,6 +56,39 @@ describe('user account actions', () => {
       }
 
       expect(loginFailed(reason)).toEqual(expectedAction)
+    })
+
+    it("should create an action to set the user's encrypted key in the state", () => {
+      expect.assertions(1)
+      const key = {
+        version: 3,
+        id: '04e9bcbb-96fa-497b-94d1-14df4cd20af6',
+        address: '2c7536e3605d9c16a7a3d7b1898e529396a65c23',
+        crypto: {
+          ciphertext:
+            'a1c25da3ecde4e6a24f3697251dd15d6208520efc84ad97397e906e6df24d251',
+          cipherparams: { iv: '2885df2b63f7ef247d753c82fa20038a' },
+          cipher: 'aes-128-ctr',
+          kdf: 'scrypt',
+          kdfparams: {
+            dklen: 32,
+            salt:
+              '4531b3c174cc3ff32a6a7a85d6761b410db674807b2d216d022318ceee50be10',
+            n: 262144,
+            r: 8,
+            p: 1,
+          },
+          mac:
+            'b8b010fff37f9ae5559a352a185e86f9b9c1d7f7a9f1bd4e82a5dd35468fc7f6',
+        },
+      }
+
+      const expectedAction = {
+        type: SET_ENCRYPTED_PRIVATE_KEY,
+        key,
+      }
+
+      expect(setEncryptedPrivateKey(key)).toEqual(expectedAction)
     })
   })
 
