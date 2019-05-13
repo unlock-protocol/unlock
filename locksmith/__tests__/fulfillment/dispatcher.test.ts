1b68bdfad6f646dc2f4926deee70bfee202e6eff
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-05-13 17:03:09 -0400

    Locksmith updated with delegated purchasing (#3171)
    * Locksmith updated with delegated purchasing
    
    The code has been updated to support purchasing on a users
    behalf. At the moment this is written with Ethereum based locks
    in mind but shall be updated for others as well.

--- locksmith/__tests__/fulfillment/dispatcher.test.ts
+++ locksmith/__tests__/fulfillment/dispatcher.test.ts
@@ -6,10 +6,10 @@ let mockWeb3Service: { getLock: any },
 let lockAddress = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
 let recipient = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
 let unlockAddress = '0x885EF47c3439ADE0CB9b33a4D3c534C99964Db93'
-let purchasingAddress = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
 let credential =
   '0xfd8abdd241b9e7679e3ef88f05b31545816d6fbcaf11e86ebd5a57ba281ce229'
 let host = 'http://localhost:8545'
+let buyer = '0xpurchasingEthereumAddress'
 
 let standardLock = {
   asOf: 227,
@@ -46,9 +46,10 @@ describe('Dispatcher', () => {
   beforeAll(() => {
     dispatcher = new Dispatcher(
       unlockAddress,
-      purchasingAddress,
+      recipient,
       credential,
-      host
+      host,
+      buyer
     )
   })
 
@@ -65,12 +66,15 @@ describe('Dispatcher', () => {
     describe('when the lock is not retrievable', () => {
       it('raises and error', async () => {
         expect.assertions(1)
-        mockWeb3Service.getLock = jest.fn(() => {
-          throw new Error()
-        })
-        expect(() => {
-          dispatcher.retrieveLock('0x222')
-        }).toThrow('Unable to retrieve Lock information')
+        mockWeb3Service.getLock = jest.fn().mockRejectedValue('foo')
+
+        try {
+          await dispatcher.retrieveLock('0x222')
+        } catch (error) {
+          expect(error).toEqual(
+            new Error('Unable to retrieve Lock information')
+          )
+        }
       })
     })
   })
@@ -86,7 +90,7 @@ describe('Dispatcher', () => {
           lockAddress,
           recipient,
           '0.01',
-          '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
+          buyer
         )
       })
     })
