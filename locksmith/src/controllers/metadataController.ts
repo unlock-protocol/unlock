ccd3263ae37c1a2d72f82f027e86ccb4f5a3c12a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-05-14 09:48:24 -0400

    Updated metadata output to better reflect requested output (#3176)

--- locksmith/src/controllers/metadataController.ts
+++ locksmith/src/controllers/metadataController.ts
@@ -3,13 +3,14 @@ import { Request, Response } from 'express-serve-static-core' // eslint-disable-
 namespace MetadataController {
   // eslint-disable-next-line import/prefer-default-export
   export const data = async (_req: Request, res: Response): Promise<any> => {
-    let stub = {
-      description: 'Test metadata for Unlock protocol',
+    let defaultResponse = {
+      description:
+        'A key to a Lock contract built on the unlock-protocol, a general-purpose blockchain-based access-control protocol. https://unlock-protocol.com/',
       image:
-        'https://raw.githubusercontent.com/unlock-protocol/unlock/master/unlock-app/src/static/images/unlock-word-mark.png',
-      name: 'Lock Metadata Test',
+        'https://camo.githubusercontent.com/d57c0f29d87bbd92ff398a5735a113af2b997427/68747470733a2f2f696d616765732e7a656e68756275736572636f6e74656e742e636f6d2f3563373830656364303430303465336261343862393664342f61626666376561322d393138652d343039612d396435652d613432383664303061333834',
+      name: 'Unlock-Protocol Key',
     }
-    return res.json(stub)
+    return res.json(defaultResponse)
   }
 }
 
