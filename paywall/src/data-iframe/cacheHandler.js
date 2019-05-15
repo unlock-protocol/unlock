800ab124977965bb7108cfeee34f64ea7bf123a1
Gregory Beaver <greg@unlock-protocol.com>
2019-05-14 22:28:22 -0400

    simplify the source

--- paywall/src/data-iframe/cacheHandler.js
+++ paywall/src/data-iframe/cacheHandler.js
@@ -8,38 +8,38 @@ export function setup(networkId, account) {
   currentAccount = account
 }
 
+async function _get(window, key) {
+  return await cache.get(window, currentNetwork, currentAccount, key)
+}
+
 export async function getKeys(window) {
-  return await cache.get(window, currentNetwork, currentAccount, 'keys')
+  return await _get(window, 'keys')
 }
 
 export async function getLocks(window) {
-  return await cache.get(window, currentNetwork, currentAccount, 'locks')
+  return await _get(window, 'locks')
 }
 
 export async function getTransactions(window) {
-  return await cache.get(window, currentNetwork, currentAccount, 'transactions')
+  return await _get(window, 'transactions')
 }
 
 export async function addKey(window, key) {
-  const keys =
-    (await cache.get(window, currentNetwork, currentAccount, 'keys')) || {}
+  const keys = (await getKeys(window)) || {}
 
   keys[key.id] = key
   await cache.put(window, currentNetwork, currentAccount, 'keys', keys)
 }
 
 export async function addLock(window, lock) {
-  const locks =
-    (await cache.get(window, currentNetwork, currentAccount, 'locks')) || {}
+  const locks = (await getLocks(window)) || {}
 
   locks[lock.address] = lock
   await cache.put(window, currentNetwork, currentAccount, 'locks', locks)
 }
 
 export async function addTransaction(window, transaction) {
-  const transactions =
-    (await cache.get(window, currentNetwork, currentAccount, 'transactions')) ||
-    {}
+  const transactions = (await getTransactions(window)) || {}
 
   transactions[transaction.hash] = transaction
   await cache.put(
