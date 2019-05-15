afac959bf7fb3ad29c7ea57079170fb3d1fbdfe8
Ben Werdmuller <ben@benwerd.com>
2019-05-14 21:17:08 -0400

    Introducing rsvpRoute (#3203)

--- tickets/src/__tests__/utils/routes.test.js
+++ tickets/src/__tests__/utils/routes.test.js
@@ -1,4 +1,4 @@
-import { lockRoute } from '../../utils/routes'
+import { lockRoute, rsvpRoute } from '../../utils/routes'
 
 describe('route utilities', () => {
   const baseRoute = {
@@ -20,6 +20,29 @@ describe('route utilities', () => {
         ...baseRoute,
         lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
         prefix: 'lock',
+      })
+    })
+  })
+
+  describe('rsvpRoute', () => {
+    it('should return null value when it does not match', () => {
+      expect.assertions(2)
+      expect(rsvpRoute('/dashboard')).toEqual(baseRoute)
+      expect(rsvpRoute('/lock')).toEqual(baseRoute)
+    })
+
+    it('should return the right prefix and lockAddress value when it matches', () => {
+      expect.assertions(1)
+      expect(
+        rsvpRoute(
+          '/checkin/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54/0x80b8825a3e7Fb15263D0DD455B8aAfc08503bb54/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54d23493439'
+        )
+      ).toEqual({
+        ...baseRoute,
+        lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+        publicKey: '0x80b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
+        signature: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54d23493439',
+        prefix: 'checkin',
       })
     })
   })
