43c68220baa2f8696c48d3d0bb71346734110318
Gregory Beaver <greg@unlock-protocol.com>
2019-05-18 20:34:15 -0400

    getLocks helper for the ad remover paywall (#3256)
    * getLocks helper for the ad remover paywall
    
    * ensure the wallet is ready first!
    
    * refactor - don't require wallet to retrieve locks

---
+++ paywall/src/__tests__/data-iframe/blockchainHandler/getLocks.test.js
@@ -0,0 +1,86 @@
+import getLocksAndKeys from '../../../data-iframe/blockchainHandler/getLocks'
+import { setAccount } from '../../../data-iframe/blockchainHandler/account'
+
+describe('getLocks', () => {
+  let fakeWeb3Service
+  const fakeWalletService = {}
+  beforeEach(() => {
+    fakeWeb3Service = {
+      getLock: jest.fn(address => Promise.resolve({ address })),
+    }
+  })
+
+  it('calls web3Service.getLock for all the locks', async () => {
+    expect.assertions(4)
+
+    await getLocksAndKeys({
+      walletService: fakeWalletService,
+      locksToRetrieve: [1, 2, 3],
+      existingKeys: {},
+      web3Service: fakeWeb3Service,
+    })
+
+    expect(fakeWeb3Service.getLock).toHaveBeenCalledTimes(3)
+    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(1, 1)
+    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(2, 2)
+    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(3, 3)
+  })
+
+  it("creates default keys for locks that don't have one", async () => {
+    expect.assertions(1)
+
+    setAccount('account')
+    const result = await getLocksAndKeys({
+      walletService: fakeWalletService,
+      locksToRetrieve: [1, 2, 3],
+      existingKeys: { '2-account': 'hi' },
+      web3Service: fakeWeb3Service,
+    })
+
+    expect(result).toEqual({
+      locks: {
+        1: { address: 1 },
+        2: { address: 2 },
+        3: { address: 3 },
+      },
+      keys: {
+        '1-account': {
+          lock: 1,
+          owner: 'account',
+          expiration: 0,
+          transactions: {},
+        },
+        '2-account': 'hi',
+        '3-account': {
+          lock: 3,
+          owner: 'account',
+          expiration: 0,
+          transactions: {},
+        },
+      },
+    })
+  })
+
+  it('does not make keys if there is no account', async () => {
+    expect.assertions(1)
+
+    setAccount(null)
+    const result = await getLocksAndKeys({
+      walletService: fakeWalletService,
+      locksToRetrieve: [1, 2, 3],
+      existingKeys: { '2-account': 'hi' },
+      web3Service: fakeWeb3Service,
+    })
+
+    expect(result).toEqual({
+      locks: {
+        1: { address: 1 },
+        2: { address: 2 },
+        3: { address: 3 },
+      },
+      keys: {
+        '2-account': 'hi',
+      },
+    })
+  })
+})
---
+++ paywall/src/data-iframe/blockchainHandler/getLocks.js
@@ -0,0 +1,42 @@
+import { getAccount } from './account'
+
+/**
+ * Retrieve lock information and construct default keys for the locks
+ *
+ * @param {array} locksToRetrieve an array of lock addresses to retrieve
+ * @param {object} existingKeys a map indexing keys by key id
+ * @param {web3Service} web3Service the web3Service, needed for getLock
+ * @returns {{ locks, keys }}
+ */
+export default async function getLocksAndKeys({
+  locksToRetrieve,
+  existingKeys,
+  web3Service,
+}) {
+  const newLocks = await Promise.all(
+    locksToRetrieve.map(lockAddress => web3Service.getLock(lockAddress))
+  )
+  const account = getAccount()
+  const keys = { ...existingKeys }
+  // convert into a map indexed by lock address
+  const locks = newLocks.reduce(
+    (allLocks, lock) => ({ ...allLocks, [lock.address]: lock }),
+    {}
+  )
+  newLocks.forEach(lock => {
+    const lockAddress = lock.address
+    if (account) {
+      const keyId = `${lockAddress}-${account}`
+      if (!keys[keyId]) {
+        // create a default key for every lock that doesn't already have one
+        keys[keyId] = {
+          lock: lockAddress,
+          owner: account,
+          expiration: 0,
+          transactions: {},
+        }
+      }
+    }
+  })
+  return { locks, keys }
+}
