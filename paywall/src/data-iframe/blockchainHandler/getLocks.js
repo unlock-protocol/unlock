dc09d78d22cf9819da422cda7cff78949d37ad63
Gregory Beaver <greg@unlock-protocol.com>
2019-05-21 11:04:24 -0400

    forgot to add confirmations

--- paywall/src/data-iframe/blockchainHandler/getLocks.js
+++ paywall/src/data-iframe/blockchainHandler/getLocks.js
@@ -34,6 +34,7 @@ export default async function getLocksAndKeys({
           owner: account,
           expiration: 0,
           status: 'none',
+          confirmations: 0,
           transactions: [],
         }
       }
