70e507bc9133aeed7f7e76a5586fb95f7660fc4a
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-21 11:25:01 -0400

    attempt at fixing the wedlocks logger based on netlify support responses (#3288)

--- wedlocks/logger.js
+++ wedlocks/logger.js
@@ -1,15 +1,36 @@
+/* eslint no-console: 0 */
+
 import winston from 'winston'
 import config from './config'
 
+// TODO: remove me. this is suggestion by the netlify support to debug logging...
+console.log('Logger imported')
 /**
  * The logger. Logging to stdout
  */
 
+// log function overwritten based on comments at https://github.com/winstonjs/winston/issues/1594#issuecomment-492420971
+const LEVEL = Symbol.for('level')
+const MESSAGE = Symbol.for('message')
+
 const consoleTransport = new winston.transports.Console({
   silent: config.unlockEnv === 'test',
+  log: function(info, callback) {
+    setImmediate(() => this.emit('logged', info))
+
+    if (this.stderrLevels[info[LEVEL]]) {
+      console.error(info[MESSAGE])
+      if (callback) callback()
+      return
+    }
+
+    console.log(info[MESSAGE])
+    if (callback) callback()
+  },
 })
 
 export default winston.createLogger({
+  format: winston.format.json(),
   transports: [consoleTransport],
   level: config.unlockEnv === 'dev' ? 'debug' : 'info',
 })
