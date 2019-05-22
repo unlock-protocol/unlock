48601e5bd36d6025a3821031bbf6dda517eef413
Gregory Beaver <greg@unlock-protocol.com>
2019-05-22 10:49:49 -0400

    refactor getLocks to separate lock retrieval from linking to keys (#3318)

--- paywall/src/__tests__/data-iframe/blockchainHandler/getLocks.test.js
+++ paywall/src/__tests__/data-iframe/blockchainHandler/getLocks.test.js
@@ -1,90 +1,150 @@
-import getLocksAndKeys from '../../../data-iframe/blockchainHandler/getLocks'
+import {
+  getLocks,
+  linkKeysToLocks,
+} from '../../../data-iframe/blockchainHandler/getLocks'
 import { setAccount } from '../../../data-iframe/blockchainHandler/account'
 
-describe('getLocks', () => {
-  let fakeWeb3Service
-  const fakeWalletService = {}
-  beforeEach(() => {
-    fakeWeb3Service = {
-      getLock: jest.fn(address => Promise.resolve({ address })),
-    }
-  })
+jest.mock('../../../data-iframe/blockchainHandler/ensureWalletReady')
 
-  it('calls web3Service.getLock for all the locks', async () => {
-    expect.assertions(4)
-
-    await getLocksAndKeys({
-      walletService: fakeWalletService,
-      locksToRetrieve: [1, 2, 3],
-      existingKeys: {},
-      web3Service: fakeWeb3Service,
+describe('Locks retrieval', () => {
+  describe('getLocks', () => {
+    let fakeWeb3Service
+    beforeEach(() => {
+      fakeWeb3Service = {
+        getLock: jest.fn(address => Promise.resolve({ address })),
+      }
     })
 
-    expect(fakeWeb3Service.getLock).toHaveBeenCalledTimes(3)
-    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(1, 1)
-    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(2, 2)
-    expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(3, 3)
-  })
+    it('calls web3Service.getLock for all the locks', async () => {
+      expect.assertions(4)
 
-  it("creates default keys for locks that don't have one", async () => {
-    expect.assertions(1)
+      await getLocks({
+        locksToRetrieve: [1, 2, 3],
+        web3Service: fakeWeb3Service,
+      })
 
-    setAccount('account')
-    const result = await getLocksAndKeys({
-      walletService: fakeWalletService,
-      locksToRetrieve: [1, 2, 3],
-      existingKeys: { '2-account': 'hi' },
-      web3Service: fakeWeb3Service,
+      expect(fakeWeb3Service.getLock).toHaveBeenCalledTimes(3)
+      expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(1, 1)
+      expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(2, 2)
+      expect(fakeWeb3Service.getLock).toHaveBeenNthCalledWith(3, 3)
     })
 
-    expect(result).toEqual({
-      locks: {
+    it('returns the locks indexed by address', async () => {
+      expect.assertions(1)
+
+      setAccount('account')
+      const result = await getLocks({
+        locksToRetrieve: [1, 2, 3],
+        web3Service: fakeWeb3Service,
+      })
+
+      expect(result).toEqual({
         1: { address: 1 },
         2: { address: 2 },
         3: { address: 3 },
-      },
-      keys: {
-        '1-account': {
-          lock: 1,
-          owner: 'account',
-          expiration: 0,
-          confirmations: 0,
-          status: 'none',
-          transactions: [],
-        },
-        '2-account': 'hi',
-        '3-account': {
-          lock: 3,
-          owner: 'account',
-          expiration: 0,
-          confirmations: 0,
-          status: 'none',
-          transactions: [],
-        },
-      },
+      })
     })
   })
 
-  it('does not make keys if there is no account', async () => {
-    expect.assertions(1)
+  describe('linkKeysToLocks', () => {
+    let fakeWeb3Service
+    let fakeWalletService
 
-    setAccount(null)
-    const result = await getLocksAndKeys({
-      walletService: fakeWalletService,
-      locksToRetrieve: [1, 2, 3],
-      existingKeys: { '2-account': 'hi' },
-      web3Service: fakeWeb3Service,
+    beforeEach(() => {
+      fakeWalletService = {}
+      fakeWeb3Service = {
+        keyExpiry: {},
+        getKeyByLockForOwner(lock) {
+          return {
+            id: 'whatever' + lock,
+            lock,
+            owner: 'account',
+            expiration: fakeWeb3Service.keyExpiry[lock] || 0,
+          }
+        },
+      }
     })
 
-    expect(result).toEqual({
-      locks: {
-        1: { address: 1 },
-        2: { address: 2 },
-        3: { address: 3 },
-      },
-      keys: {
-        '2-account': 'hi',
-      },
+    it('links keys to the locks they unlock', async () => {
+      expect.assertions(1)
+
+      const locks = {
+        '0x123': {
+          address: '0x123',
+          keyPrice: '5',
+          expirationDuration: '6',
+          maxNumberOfKeys: 4,
+        },
+        '0x456': {
+          address: '0x456',
+          keyPrice: '55',
+          expirationDuration: '66',
+          maxNumberOfKeys: 44,
+        },
+      }
+
+      fakeWeb3Service.keyExpiry = {
+        '0x123': new Date().getTime() / 1000 + 123,
+        // no expiry for '0x456' means 0
+      }
+
+      const transactions = {
+        hash: {
+          hash: 'hash',
+          from: 'account',
+          to: '0x123',
+          key: '0x123-account',
+          lock: '0x123',
+          status: 'mined',
+          confirmations: 2,
+          blockNumber: 5,
+        },
+        old: {
+          hash: 'old',
+          from: 'account',
+          to: '0x123',
+          key: '0x123-account',
+          lock: '0x123',
+          status: 'mined',
+          confirmations: 223,
+          blockNumber: 4,
+        },
+      }
+
+      const newLocks = await linkKeysToLocks({
+        locks,
+        walletService: fakeWalletService,
+        transactions,
+        web3Service: fakeWeb3Service,
+        requiredConfirmations: 3,
+      })
+
+      expect(newLocks).toEqual({
+        '0x123': {
+          ...locks['0x123'],
+          key: {
+            confirmations: 2,
+            expiration: fakeWeb3Service.keyExpiry['0x123'],
+            id: 'whatever0x123',
+            lock: '0x123',
+            owner: 'account',
+            status: 'confirming',
+            transactions: [transactions.hash, transactions.old],
+          },
+        },
+        '0x456': {
+          ...locks['0x456'],
+          key: {
+            confirmations: 0,
+            expiration: 0,
+            id: 'whatever0x456',
+            lock: '0x456',
+            owner: 'account',
+            status: 'none',
+            transactions: [],
+          },
+        },
+      })
     })
   })
 })
