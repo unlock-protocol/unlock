d102c6d8e8f65f4313557a5a3659f73b27d8f673
Ben Werdmuller <ben@benwerd.com>
2019-05-22 12:40:55 -0700

    Events URL now uses base domain from configuration

--- tickets/src/__tests__/components/helpers/EventUrl.test.js
+++ tickets/src/__tests__/components/helpers/EventUrl.test.js
@@ -2,10 +2,14 @@ import React from 'react'
 import * as rtl from 'react-testing-library'
 import { EventUrl } from '../../../components/helpers/EventUrl'
 
+const config = {
+  unlockTicketsUrl: 'https://tickets.unlock-protocol.com',
+}
+
 describe('EventUrl helper component', () => {
   it('shows an event URL when an address is supplied', () => {
     expect.assertions(1)
-    const wrapper = rtl.render(<EventUrl address="0x123" />)
+    const wrapper = rtl.render(<EventUrl address="0x123" config={config} />)
     expect(
       wrapper.queryByText('https://tickets.unlock-protocol.com/event/0x123')
     ).not.toBeNull()
