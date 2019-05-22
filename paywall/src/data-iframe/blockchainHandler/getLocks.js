777bbad9d4501963da3d600206dcd821d2d6df1c
Gregory Beaver <greg@unlock-protocol.com>
2019-05-22 10:49:49 -0400

    refactor getLocks to separate lock retrieval from linking to keys (#3318)

--- paywall/src/data-iframe/blockchainHandler/getLocks.js
+++ paywall/src/data-iframe/blockchainHandler/getLocks.js
@@ -1,4 +1,5 @@
-import { getAccount } from './account'
+import getKeys from './getKeys'
+import { linkTransactionsToKey } from './keyStatus'
 
 /**
  * Retrieve lock information and construct default keys for the locks
@@ -8,37 +9,50 @@ import { getAccount } from './account'
  * @param {web3Service} web3Service the web3Service, needed for getLock
  * @returns {{ locks, keys }}
  */
-export default async function getLocksAndKeys({
-  locksToRetrieve,
-  existingKeys,
-  web3Service,
-}) {
+export async function getLocks({ locksToRetrieve, web3Service }) {
   const newLocks = await Promise.all(
     locksToRetrieve.map(lockAddress => web3Service.getLock(lockAddress))
   )
-  const account = getAccount()
-  const keys = { ...existingKeys }
   // convert into a map indexed by lock address
-  const locks = newLocks.reduce(
-    (allLocks, lock) => ({ ...allLocks, [lock.address]: lock }),
+  return newLocks.reduce(
+    (allLocks, lock) => ({
+      ...allLocks,
+      [lock.address]: lock,
+    }),
     {}
   )
-  newLocks.forEach(lock => {
-    const lockAddress = lock.address
-    if (account) {
-      const keyId = `${lockAddress}-${account}`
-      if (!keys[keyId]) {
-        // create a default key for every lock that doesn't already have one
-        keys[keyId] = {
-          lock: lockAddress,
-          owner: account,
-          expiration: 0,
-          status: 'none',
-          confirmations: 0,
-          transactions: [],
-        }
-      }
-    }
+}
+
+export async function linkKeysToLocks({
+  locks,
+  walletService,
+  transactions,
+  web3Service,
+  requiredConfirmations,
+}) {
+  const lockArray = Object.values(locks)
+  const keys = await getKeys({
+    locks: lockArray.map(lock => lock.address),
+    walletService,
+    transactions,
+    web3Service,
+    requiredConfirmations,
   })
-  return { locks, keys }
+
+  // convert into a map indexed by lock address
+  // and link each key to its lock
+  return lockArray.reduce(
+    (allLocks, lock) => ({
+      ...allLocks,
+      [lock.address]: {
+        ...lock,
+        key: linkTransactionsToKey({
+          key: keys[lock.address],
+          transactions,
+          requiredConfirmations,
+        }),
+      },
+    }),
+    {}
+  )
 }
