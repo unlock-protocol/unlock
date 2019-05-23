c00f66ee3d843ba9d0ac75145c0a75b90d354215
Unlock Deployer <ops@unlock-protocol.com>
2019-05-23 20:30:35 +0000

    Automated deploy as of commit:24b0cb4d0221780a929e21cd723ec79927c7dd11

--- unlock-app/src/_server.js
+++ unlock-app/src/_server.js
@@ -2,13 +2,11 @@
 const { createServer } = require('http')
 const { URL } = require('url')
 const next = require('next')
-const pathMatch = require('path-match')
 
 function _server(port, dev) {
   return new Promise(async (resolve, reject) => {
     const app = next({ dir: `${__dirname}/`, dev, quiet: true })
     const handle = app.getRequestHandler()
-    const route = pathMatch()
 
     await app.prepare()
 
@@ -16,7 +14,7 @@ function _server(port, dev) {
       console.info(`${req.method} ${req.url} > ${res.statusCode} `)
       try {
         const parsedUrl = new URL(req.url, `http://${req.headers.host}/`)
-        const { pathname, query } = parsedUrl
+        const { pathname } = parsedUrl
 
         // assigning `query` into the params means that we still
         // get the query string passed to our application
@@ -25,14 +23,6 @@ function _server(port, dev) {
           app.render(req, res, '/home', {})
         } else if (path === 'keychain') {
           app.render(req, res, '/keyChain', {})
-        } else if (path === 'paywall') {
-          // TODO: cleanup pages which is for paywall
-          const params = route('/paywall/:lockAddress/:redirect?')(pathname)
-          app.render(req, res, '/paywall', Object.assign(params, query))
-        } else if (path === 'demo') {
-          // TODO: cleanup pages below which is for paywall
-          const params = route('/demo/:lockaddress')(pathname)
-          app.render(req, res, '/demo', Object.assign(params, query))
         } else {
           handle(req, res)
           return
