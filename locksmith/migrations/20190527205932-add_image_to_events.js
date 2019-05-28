3d8ad04d4b361921de16af08bd7d3994517d8750
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-05-28 10:45:58 -0400

    Adds image field to Event schema (#3395)

---
+++ locksmith/migrations/20190527205932-add_image_to_events.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const table = 'Events'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(table, 'image', {
+      type: Sequelize.STRING,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(table, 'image')
+  }
+};
