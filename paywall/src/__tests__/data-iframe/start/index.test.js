d898455a9d7dd21d4a36cc8b0f522211102fc8a4
Gregory Beaver <greg@unlock-protocol.com>
2019-05-28 17:32:49 -0400

    finish the data iframe! (#3420)

---
+++ paywall/src/__tests__/data-iframe/start/index.test.js
@@ -0,0 +1,64 @@
+import start from '../../../data-iframe/start'
+import postOffice from '../../../data-iframe/postOffice'
+import { addListener } from '../../../data-iframe/cacheHandler'
+import setupPostOfficeListener from '../../../data-iframe/postOfficeListener'
+import makeSetConfig from '../../../data-iframe/start/makeSetConfig'
+import { purchaseKey } from '../../../data-iframe/start/purchaseKeySetup'
+
+jest.mock('../../../data-iframe/postOffice')
+jest.mock('../../../data-iframe/cacheHandler')
+jest.mock('../../../data-iframe/postOfficeListener')
+jest.mock('../../../data-iframe/start/makeSetConfig')
+
+describe('data iframe startup index', () => {
+  const constants = {
+    readOnlyProvider: 'hi',
+    unlockAddress: 'address',
+    blockTime: 8000,
+    requiredConfirmations: 2,
+    locksmitHost: 'locksmith',
+  }
+
+  beforeEach(() => {
+    postOffice.mockReset()
+  })
+
+  it('should set up the post office', async () => {
+    expect.assertions(1)
+
+    await start(window, constants)
+
+    expect(postOffice).toHaveBeenCalledWith(
+      window,
+      constants.requiredConfirmations
+    )
+  })
+
+  it('should add a listener to the cache with the post office updater', async () => {
+    expect.assertions(1)
+
+    const updater = jest.fn()
+    postOffice.mockImplementationOnce(() => updater)
+    await start(window, constants)
+
+    expect(addListener).toHaveBeenCalledWith(updater)
+  })
+
+  it('should setup the post office listener', async () => {
+    expect.assertions(1)
+
+    const updater = jest.fn()
+    const setConfig = jest.fn()
+    postOffice.mockImplementationOnce(() => updater)
+    makeSetConfig.mockImplementationOnce(() => setConfig)
+
+    await start(window, constants)
+
+    expect(setupPostOfficeListener).toHaveBeenCalledWith(
+      window,
+      updater,
+      setConfig,
+      purchaseKey
+    )
+  })
+})
---
+++ paywall/src/data-iframe/index.js
@@ -0,0 +1,27 @@
+import start from './start'
+
+const constants = {
+  dev: {
+    unlockAddress: '0x885EF47c3439ADE0CB9b33a4D3c534C99964Db93',
+    blockTime: 3000,
+    requiredConfirmations: 6,
+    locksmithHost: process.env.LOCKSMITH_URI || 'http://localhost:8080',
+    readOnlyProvider: process.env.READ_ONLY_PROVIDER || 'http://localhost:8545',
+  },
+  staging: {
+    unlockAddress: '0xD8C88BE5e8EB88E38E6ff5cE186d764676012B0b',
+    blockTime: 8000,
+    requiredConfirmations: 12,
+    locksmithHost: process.env.LOCKSMITH_URI,
+    readOnlyProvider: process.env.READ_ONLY_PROVIDER,
+  },
+  prod: {
+    unlockAddress: '0x3d5409CcE1d45233dE1D4eBDEe74b8E004abDD13',
+    blockTime: 8000,
+    requiredConfirmations: 12,
+    locksmithHost: process.env.LOCKSMITH_URI,
+    readOnlyProvider: process.env.READ_ONLY_PROVIDER,
+  },
+}
+
+start(window, constants[process.env.UNLOCK_ENV])
