16d59e7c0884433fbd2c38827e5f393eda3de88e
Christopher Nascone <cnasc@users.noreply.github.com>
2019-05-28 14:25:59 -0400

    Improve error constructors and add type guards (#3404)

--- unlock-app/src/__tests__/utils/Error.test.ts
+++ unlock-app/src/__tests__/utils/Error.test.ts
@@ -1,27 +1,88 @@
-import {
-  ApplicationError,
-  FormValidationError,
-  LogInError,
-  SignUpError,
-  SignatureError,
-  StorageError,
-  TransactionError,
+import Error, {
+  isFatalError,
+  isWarningError,
+  isDiagnosticError,
 } from '../../utils/Error'
 
 describe('Error constructors', () => {
-  it.each([
-    ApplicationError,
-    FormValidationError,
-    LogInError,
-    SignUpError,
-    SignatureError,
-    StorageError,
-    TransactionError,
-  ])('each of these carries the message passed to it', constructors => {
-    expect.assertions(2)
-    const fatalMessage = 'Not enough teapots, dumping core'
-    const warning = 'Passwords must contain at least 43 different characters'
-    expect(constructors.Fatal(fatalMessage).message).toBe(fatalMessage)
-    expect(constructors.Warn(warning).message).toBe(warning)
+  for (let name in Error) {
+    describe(name, () => {
+      it('should carry the message param on all methods', () => {
+        expect.assertions(3)
+        const message = 'This is a message'
+
+        expect(Error[name].Fatal(message)).toEqual(
+          expect.objectContaining({
+            message,
+          })
+        )
+        expect(Error[name].Warning(message)).toEqual(
+          expect.objectContaining({
+            message,
+          })
+        )
+        expect(Error[name].Diagnostic(message)).toEqual(
+          expect.objectContaining({
+            message,
+          })
+        )
+      })
+
+      it('should construct the right kind of errors', () => {
+        expect.assertions(3)
+        const message = 'This is a message'
+
+        expect(Error[name].Fatal(message)).toEqual(
+          expect.objectContaining({
+            kind: name,
+          })
+        )
+        expect(Error[name].Warning(message)).toEqual(
+          expect.objectContaining({
+            kind: name,
+          })
+        )
+        expect(Error[name].Diagnostic(message)).toEqual(
+          expect.objectContaining({
+            kind: name,
+          })
+        )
+      })
+    })
+  }
+})
+
+describe('type guards', () => {
+  const errors = [
+    Error.Storage.Fatal('Not enough teapots.'),
+    Error.Signature.Warning('Pen running low on ink, refill and try again'),
+    Error.FormValidation.Diagnostic(
+      'Just letting you know that I validated the form'
+    ),
+  ]
+  describe('isFatalError', () => {
+    it('should pick out only FatalErrors', () => {
+      expect.assertions(2)
+      const filteredErrors = errors.filter(isFatalError)
+      expect(filteredErrors.length).toBe(1)
+      // This last check is kind of redundant with the type system
+      expect(filteredErrors[0].level).toEqual('Fatal')
+    })
+  })
+  describe('isWarningError', () => {
+    it('should pick out only WarningErrors', () => {
+      expect.assertions(2)
+      const filteredErrors = errors.filter(isWarningError)
+      expect(filteredErrors.length).toBe(1)
+      expect(filteredErrors[0].level).toEqual('Warning')
+    })
+  })
+  describe('isDiagnosticError', () => {
+    it('should pick out only DiagnosticErrors', () => {
+      expect.assertions(2)
+      const filteredErrors = errors.filter(isDiagnosticError)
+      expect(filteredErrors.length).toBe(1)
+      expect(filteredErrors[0].level).toEqual('Diagnostic')
+    })
   })
 })
