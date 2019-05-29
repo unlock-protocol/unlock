373ea5841b8ddffbe593000daa9cb5ad22c85cf5
Gregory Beaver <greg@unlock-protocol.com>
2019-05-29 17:54:57 -0400

    fix the environment variables for build scripts, add CSS to unlock.min.js (#3449)

--- paywall/unlock.1.0.js.webpack.config.js
+++ paywall/unlock.1.0.js.webpack.config.js
@@ -1,7 +1,33 @@
+/* eslint no-console: 0 */
+const dotenv = require('dotenv')
 var path = require('path')
 const webpack = require('webpack')
 
-module.exports = env => {
+const unlockEnv = process.env.UNLOCK_ENV || 'dev'
+
+dotenv.config({
+  path: path.resolve(__dirname, '..', `.env.${unlockEnv}.local`),
+})
+
+const requiredConfigVariables = {
+  unlockEnv,
+  paywallUrl: process.env.PAYWALL_URL,
+}
+
+Object.keys(requiredConfigVariables).forEach(configVariableName => {
+  if (!requiredConfigVariables[configVariableName]) {
+    if (['dev', 'test'].indexOf(requiredConfigVariables.unlockEnv) > -1) {
+      return console.error(
+        `The configuration variable ${configVariableName} is falsy.`
+      )
+    }
+    throw new Error(
+      `The configuration variable ${configVariableName} is falsy.`
+    )
+  }
+})
+
+module.exports = () => {
   return {
     mode: 'production',
     devtool: 'source-map',
@@ -11,11 +37,19 @@ module.exports = env => {
       filename: 'unlock.1.0.min.js',
       globalObject: 'self',
     },
-
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
     plugins: [
       new webpack.DefinePlugin({
-        'process.env.UNLOCK_ENV': "'" + env.UNLOCK_ENV + "'",
-        'process.env.PAYWALL_URL': "'" + env.PAYWALL_URL + "'",
+        'process.env.UNLOCK_ENV': "'" + unlockEnv + "'",
+        'process.env.PAYWALL_URL':
+          "'" + requiredConfigVariables.paywallUrl + "'",
       }),
     ],
   }
