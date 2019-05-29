3a773dee78b6bfe62c747892892f2e1d414b960f
Julien Genestoux <julien.genestoux@gmail.com>
2019-05-29 15:19:53 -0400

    updating the default image and description (#3436)

--- locksmith/src/controllers/metadataController.ts
+++ locksmith/src/controllers/metadataController.ts
@@ -5,10 +5,9 @@ namespace MetadataController {
   export const data = async (_req: Request, res: Response): Promise<any> => {
     let defaultResponse = {
       description:
-        'A key to a Lock contract built on the unlock-protocol, a general-purpose blockchain-based access-control protocol. https://unlock-protocol.com/',
-      image:
-        'https://camo.githubusercontent.com/d57c0f29d87bbd92ff398a5735a113af2b997427/68747470733a2f2f696d616765732e7a656e68756275736572636f6e74656e742e636f6d2f3563373830656364303430303465336261343862393664342f61626666376561322d393138652d343039612d396435652d613432383664303061333834',
-      name: 'Unlock-Protocol Key',
+        'A Key to an Unlock lock. Unlock Protocol is a blockchain-based access-control protocol. https://unlock-protocol.com/',
+      image: 'https://assets.unlock-protocol.com/unlock-default-key-image.png',
+      name: 'Unlock Key',
     }
     return res.json(defaultResponse)
   }
