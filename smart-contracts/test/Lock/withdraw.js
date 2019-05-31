1c7f39f0d54c28db593d7f74b3604553fbc18702
hardlydifficult <hardlydiff@gmail.com>
2019-05-31 12:53:24 -0700

    Fix existing tests

--- smart-contracts/test/Lock/withdraw.js
+++ smart-contracts/test/Lock/withdraw.js
@@ -37,7 +37,7 @@ contract('Lock / withdraw', accounts => {
   it('should only allow the owner to withdraw', async () => {
     assert.notEqual(owner, accounts[1]) // Making sure
     await shouldFail(
-      locks['OWNED'].withdraw({
+      locks['OWNED'].withdraw(0, {
         from: accounts[1],
       }),
       ''
@@ -45,16 +45,47 @@ contract('Lock / withdraw', accounts => {
   })
 
   describe('when the owner withdraws funds', () => {
+    let tx
     let ownerBalance
+    let contractBalance
     before(async () => {
       ownerBalance = new BigNumber(await web3.eth.getBalance(owner))
-      return locks['OWNED'].withdraw({
+      contractBalance = new BigNumber(
+        await web3.eth.getBalance(locks['OWNED'].address)
+      )
+      tx = await locks['OWNED'].withdraw(0, {
         from: owner,
       })
     })
+
+    it("should set the lock's balance to 0", async () => {
+      assert.equal(await web3.eth.getBalance(locks['OWNED'].address), 0)
+    })
+
     it("should increase the owner's balance with the funds from the lock", async () => {
       const balance = new BigNumber(await web3.eth.getBalance(owner))
-      assert(balance.gt(ownerBalance))
+      const txHash = await web3.eth.getTransaction(tx.tx)
+      const gasUsed = new BigNumber(tx.receipt.gasUsed)
+      const gasPrice = new BigNumber(txHash.gasPrice)
+      const txFee = gasPrice.times(gasUsed)
+      assert.equal(
+        balance.toString(),
+        ownerBalance
+          .plus(contractBalance)
+          .minus(txFee)
+          .toString()
+      )
+    })
+
+    it('should fail if there is nothing left to withdraw', async () => {
+      await shouldFail(
+        locks['OWNED'].withdraw(0, {
+          from: owner,
+        }),
+        'NOT_ENOUGH_FUNDS'
+      )
+    })
+  })
     })
 
     it("should set the lock's balance to 0", async () => {
