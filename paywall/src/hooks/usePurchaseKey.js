ae07488ec9687d1e0ea48260edb302a702210044
Gregory Beaver <greg@unlock-protocol.com>
2019-06-03 10:55:28 -0400

    add more explanatory names for local/remote in postMessage debugging

--- paywall/src/hooks/usePurchaseKey.js
+++ paywall/src/hooks/usePurchaseKey.js
@@ -3,7 +3,7 @@ import usePostMessage from './browser/usePostMessage'
 import { POST_MESSAGE_REDIRECT } from '../paywall-builder/constants'
 
 export default function usePurchaseKey(purchaseKey, openInNewWindow) {
-  const { postMessage } = usePostMessage()
+  const { postMessage } = usePostMessage('usePurchaseKey')
   return useCallback(
     key => {
       if (openInNewWindow) {
