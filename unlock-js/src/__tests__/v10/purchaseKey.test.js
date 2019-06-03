fcd907617275066db15af99facc9e0a05eab87d3
Julien Genestoux <julien.genestoux@gmail.com>
2019-06-03 17:10:18 -0400

    Fixing the nock recorder (#3524)

--- unlock-js/src/__tests__/v10/purchaseKey.test.js
+++ unlock-js/src/__tests__/v10/purchaseKey.test.js
@@ -6,7 +6,7 @@ import { prepWalletService, prepContract } from '../helpers/walletServiceHelper'
 
 const { FAILED_TO_PURCHASE_KEY } = Errors
 const endpoint = 'http://127.0.0.1:8545'
-const nock = new NockHelper(endpoint, false /** debug */, true /** ethers */)
+const nock = new NockHelper(endpoint, false /** debug */)
 
 let walletService
 let transaction
