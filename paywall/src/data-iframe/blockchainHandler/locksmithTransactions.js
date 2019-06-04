617ed5a45f002fe6ffd0829ca877051dbee859b7
Ben Werdmuller <ben@benwerd.com>
2019-06-04 16:02:42 -0700

    refactor locksmithTransactions to remove any transaction retrieval or return (#3573)

--- paywall/src/data-iframe/blockchainHandler/locksmithTransactions.js
+++ paywall/src/data-iframe/blockchainHandler/locksmithTransactions.js
@@ -63,37 +63,30 @@ export default async function locksmithTransactions({
   const account = getAccount()
   const network = getNetwork()
 
-  const url = `${locksmithHost}/transactions?sender=${account}`
+  const url = `${locksmithHost}/transactions?sender=${account.toLowerCase()}`
 
   const response = await window.fetch(url)
   const result = await response.json()
-  if (result.data && result.data.transactions) {
-    const newTransactions = result.data.transactions
-      .map(t => ({
-        hash: t.transactionHash,
-        network: t.chain,
-        to: t.recipient,
-        input: t.data,
-        from: t.sender,
-      }))
-      .filter(transaction => transaction.network === network)
-      .map(transaction => {
-        // we pass the transaction as defaults if it has input set, so that we can
-        // parse out the transaction type and other details. If input is not set,
-        // we can't safely pass the transaction default
-        return web3Service.getTransaction(
-          transaction.hash,
-          transaction.input ? transaction : undefined
-        )
-      })
-    const updatedTransactions = await Promise.all(newTransactions)
-    return updatedTransactions.reduce(
-      (collect, transaction) => ({
-        ...collect,
-        [transaction.hash]: transaction,
-      }),
-      {}
+  if (result.transactions) {
+    await Promise.all(
+      result.transactions
+        .map(t => ({
+          hash: t.transactionHash,
+          network: t.chain,
+          to: t.recipient,
+          input: t.data,
+          from: t.sender,
+        }))
+        .filter(transaction => transaction.network === network)
+        .map(transaction => {
+          // we pass the transaction as defaults if it has input set, so that we can
+          // parse out the transaction type and other details. If input is not set,
+          // we can't safely pass the transaction default
+          web3Service.getTransaction(
+            transaction.hash,
+            transaction.input ? transaction : undefined
+          )
+        })
     )
   }
-  return {}
 }
