f45fcf357064161740b9916500019c7f454bf32c
Ben Werdmuller <ben@benwerd.com>
2019-06-03 18:23:24 -0700

    Fixing timezone issue

--- tickets/src/utils/links.ts
+++ tickets/src/utils/links.ts
@@ -1,11 +1,11 @@
 const formatDateForCalendar = (date: Date, allDay: boolean) => {
-  const year = date.getUTCFullYear()
-  const month = ('0' + (date.getUTCMonth() + 1)).slice(-2)
-  const day = ('0' + date.getUTCDate()).slice(-2)
+  const year = date.getFullYear()
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const day = ('0' + date.getDate()).slice(-2)
   if (!allDay) {
-    const hours = ('0' + date.getUTCHours()).slice(-2)
+    const hours = ('0' + date.getHours()).slice(-2)
     const minutes = ('0' + date.getMinutes()).slice(-2)
-    const time = `T${hours}${minutes}00Z`
+    const time = `T${hours}${minutes}00`
     return [year, month, day, time].join('')
   }
   return [year, month, day].join('')
