82d071cf7ee5e05d1fab701b9300727a2ba7c9e8
Gregory Beaver <greg@unlock-protocol.com>
2019-06-04 14:19:55 -0400

    new storeTransaction function for ad remover paywall (#3560)

--- paywall/src/data-iframe/blockchainHandler/locksmithTransactions.js
+++ paywall/src/data-iframe/blockchainHandler/locksmithTransactions.js
@@ -3,6 +3,51 @@ import { getAccount } from './account'
 import { getNetwork } from './network'
 
 /**
+ * sync a new transaction to locksmith
+ *
+ * @param {window} window the current global context (window, self, global)
+ * @param {object} transaction the transaction to sync
+ * @param {string} locksmithHost the base endpoint for locksmith
+ * @param {walletService} walletService this is used to ensure we have an account/network
+ */
+export async function storeTransaction({
+  window,
+  transaction,
+  locksmithHost,
+  walletService,
+}) {
+  await ensureWalletReady(walletService)
+  const account = getAccount()
+  const network = getNetwork()
+
+  const url = `${locksmithHost}/transaction`
+
+  const payload = {
+    transactionHash: transaction.hash,
+    sender: account.toLowerCase(),
+    recipient: transaction.to.toLowerCase(),
+    data: transaction.input,
+    chain: network,
+  }
+  try {
+    await window.fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+  } catch (e) {
+    // we don't pass this error along because it is a non-essential feature
+    // eslint-disable-next-line no-console
+    console.log('unable to save key purchase transaction')
+    // eslint-disable-next-line no-console
+    console.error(e)
+  }
+}
+
+/**
  * @param {window} window the global window context, needed for both `fetch`
  * @param {string} locksmithHost the full URI for locksmith
  * @param {web3Service} web3Service the web3Service instance
