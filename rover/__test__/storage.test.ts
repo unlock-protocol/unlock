cc3990147f3c54e0548d8d0875417aca8a4e9e76
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-06-05 13:25:02 -0400

    Adds storage wrapper and basic test (#3589)

---
+++ rover/__test__/storage.test.ts
@@ -0,0 +1,56 @@
+import { Storage } from "../src/storage";
+
+describe("Storage", () => {
+  let connectionManager;
+  let storage;
+  beforeAll(() => {
+    connectionManager = {
+      save: jest.fn()
+    };
+    storage = new Storage(connectionManager);
+  });
+
+  describe("storeBlock", () => {
+    it("persists the passed block", () => {
+      storage.storeBlock({ miner: "minerid" });
+      expect(connectionManager.save).toHaveBeenCalledWith({
+        difficulty: undefined,
+        extraData: undefined,
+        gasLimit: "undefined",
+        gasUsed: "undefined",
+        hash: undefined,
+        miner: "minerid",
+        nonce: undefined,
+        number: undefined,
+        parentHash: undefined,
+        timestamp: undefined
+      });
+    });
+  });
+
+  describe("storeTransaction", () => {
+    it("perists the passed transaction", () => {
+      storage.storeTransaction({ r: 37 });
+      expect(connectionManager.save).toHaveBeenCalledWith({
+        blockHash: undefined,
+        blockNumber: undefined,
+        confirmations: undefined,
+        creates: undefined,
+        data: undefined,
+        from: undefined,
+        gasLimit: undefined,
+        gasPrice: undefined,
+        hash: undefined,
+        networkId: undefined,
+        nonce: undefined,
+        r: 37,
+        raw: undefined,
+        s: undefined,
+        to: undefined,
+        transactionIndex: undefined,
+        v: undefined,
+        value: undefined
+      });
+    });
+  });
+});
