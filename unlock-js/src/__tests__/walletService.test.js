528e9779f49718253474341feddb81786b5376f7
Julien Genestoux <julien.genestoux@gmail.com>
2019-06-05 14:17:33 -0400

    adding support for v11

--- unlock-js/src/__tests__/walletService.test.js
+++ unlock-js/src/__tests__/walletService.test.js
@@ -6,6 +6,7 @@ import v0 from '../v0'
 import v01 from '../v01'
 import v02 from '../v02'
 import v10 from '../v10'
+import v11 from '../v11'
 
 import utils from '../utils'
 import WalletService from '../walletService'
