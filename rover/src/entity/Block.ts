ef5d66fcf7088f64f9b50577c72482a5cc38edfa
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-06-05 09:33:27 -0400

    Adds data entities to project (#3582)

---
+++ rover/src/entity/Block.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryColumn, Column } from "typeorm";
+
+@Entity()
+export class Block {
+  @PrimaryColumn({ unique: true })
+  hash: string;
+
+  @Column()
+  parentHash: string;
+
+  @Column()
+  number: number;
+
+  @Column()
+  timestamp: number;
+
+  @Column()
+  nonce: string;
+
+  @Column("bigint")
+  difficulty: number;
+
+  @Column()
+  gasLimit: string;
+
+  @Column()
+  gasUsed: string;
+
+  @Column()
+  miner: string;
+
+  @Column()
+  extraData: string;
+}
