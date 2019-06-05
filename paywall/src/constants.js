87a9628f4845ff5a30540a61c94db992ad83ef9e
Gregory Beaver <greg@unlock-protocol.com>
2019-06-05 13:38:11 -0400

    migrate media.js to typescript (#3584)
    * migrate media.js to typescript
    
    * update snaps

--- paywall/src/constants.js
+++ paywall/src/constants.js
@@ -98,7 +98,12 @@ export const PGN_MAX_NUMBER_OF_PAGES_TO_SHOW_ALL = 10
 export const MAX_DEVICE_WIDTHS = {
   PHONE: 736,
   TABLET: 1000,
-  DESKTOP: false,
+}
+export const MIN_DEVICE_WIDTHS = {
+  // this needs to be 257 because the paywall width in the iframe is 256px
+  // on desktop, so we need to make sure that we don't match that window width
+  // if you change this, please change the value in src/components/Paywall.css
+  PHONE: 257,
 }
 
 export const INFINITY = '∞'
