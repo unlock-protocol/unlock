51d3b355aea4740a6befb23b67db9186a2f2eda2
Christoph Nascone <chris@unlock-protocol.com>
2019-06-08 09:38:40 -0400

    Add explanatory comment

--- unlock-app/src/reducers/privateKeyReducer.ts
+++ unlock-app/src/reducers/privateKeyReducer.ts
@@ -8,6 +8,10 @@ const privateKeyReducer = (
   state: State = initialState,
   action: Action
 ): State => {
+  // NOTE: this reducer is unlike the others in that it ignores SET_ACCOUNT,
+  // SET_NETWORK, and SET_PROVIDER. This is by design, the user details should
+  // be retained even when those actions are dispatched.
+
   if (action.type === SET_ENCRYPTED_PRIVATE_KEY) {
     return {
       key: action.key,
