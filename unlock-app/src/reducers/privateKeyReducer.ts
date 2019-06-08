e1cf0eeb1865024e53203856d00a09439e2eb3a6
Christoph Nascone <chris@unlock-protocol.com>
2019-06-08 09:34:15 -0400

    Hang on to account info when receiving {SET_ACCOUNT, SET_PROVIDER, SET_NETWORK}

--- unlock-app/src/reducers/privateKeyReducer.ts
+++ unlock-app/src/reducers/privateKeyReducer.ts
@@ -1,7 +1,4 @@
 import { SET_ENCRYPTED_PRIVATE_KEY } from '../actions/user'
-import { SET_PROVIDER } from '../actions/provider'
-import { SET_NETWORK } from '../actions/network'
-import { SET_ACCOUNT } from '../actions/accounts'
 import { Action, EncryptedPrivateKey } from '../unlockTypes' // eslint-disable-line no-unused-vars
 
 type State = { key: EncryptedPrivateKey; email: string } | null
@@ -11,10 +8,6 @@ const privateKeyReducer = (
   state: State = initialState,
   action: Action
 ): State => {
-  if ([SET_PROVIDER, SET_NETWORK, SET_ACCOUNT].indexOf(action.type) > -1) {
-    return initialState
-  }
-
   if (action.type === SET_ENCRYPTED_PRIVATE_KEY) {
     return {
       key: action.key,
