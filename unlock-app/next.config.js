df2dbdca522fa9be6cd74510d7589104e55e4011
Julien Genestoux <julien.genestoux@gmail.com>
2019-06-10 10:58:45 -0400

    adding config to support ERC20 locks creation and display on the unlock-app dashboard (#3650)

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -20,6 +20,8 @@ let requiredConfigVariables = {
   wedlocksUri: process.env.WEDLOCKS_URI,
   unlockStaticUrl: process.env.UNLOCK_STATIC_URL,
   base64WedlocksPublicKey: process.env.BASE64_WEDLOCKS_PUBLIC_KEY,
+  erc20ContractSymbol: process.env.ERC20_CONTRACT_SYMBOL,
+  erc20ContractAddress: process.env.ERC20_CONTRACT_ADDRESS,
 }
 
 let optionalConfigVariables = {
