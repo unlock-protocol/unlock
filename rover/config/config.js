61a9c9134493f000a0d0d8e1b1db897e8921521a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-06-10 17:07:25 -0400

    Adds initial configuration & first entry point (#3677)

---
+++ rover/config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOSTNAME,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+  provider_uri: process.env.WEB3_PROVIDER_HOST,
+}
