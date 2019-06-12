1337fbb4eb45ea5d254572da6e501f2098c2156e
Gregory Beaver <greg@unlock-protocol.com>
2019-06-12 15:09:37 -0400

    add missing tests config, and catch for errors in startup (#3720)

--- paywall/src/data-iframe/index.js
+++ paywall/src/data-iframe/index.js
@@ -8,6 +8,14 @@ const constants = {
     locksmithHost: process.env.LOCKSMITH_URI || 'http://localhost:8080',
     readOnlyProvider: process.env.READ_ONLY_PROVIDER || 'http://localhost:8545',
   },
+  test: {
+    unlockAddress: '0x885EF47c3439ADE0CB9b33a4D3c534C99964Db93',
+    blockTime: 3000,
+    requiredConfirmations: 6,
+    locksmithHost: process.env.LOCKSMITH_URI || 'http://locksmith:8080',
+    readOnlyProvider:
+      process.env.READ_ONLY_PROVIDER || 'http://ganache-integration:8545',
+  },
   staging: {
     unlockAddress: '0xD8C88BE5e8EB88E38E6ff5cE186d764676012B0b',
     blockTime: 8000,
@@ -24,4 +32,6 @@ const constants = {
   },
 }
 
-start(window, constants[process.env.UNLOCK_ENV])
+start(window, constants[process.env.UNLOCK_ENV]).catch(e => {
+  console.error('startup error', e) // eslint-disable-line
+})
