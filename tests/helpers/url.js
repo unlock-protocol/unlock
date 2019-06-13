0b21be5b8ce5b6edb12951462d71478604c540c9
Gregory Beaver <greg@unlock-protocol.com>
2019-06-13 14:09:28 -0400

    update configuration for integration testing the ad remover paywall (#3719)

--- tests/helpers/url.js
+++ tests/helpers/url.js
@@ -1,6 +1,7 @@
 const host = process.env.UNLOCK_HOST || '127.0.0.1'
 const port = process.env.UNLOCK_PORT || 3000
 const paywall = process.env.PAYWALL_URL || 'http://127.0.0.1:3001'
+const provider = process.env.READ_ONLY_PROVIDER || 'http://localhost:8545'
 
 /**
  * Use these helpers to get relative paths inside the main unlock-app and the paywall.
@@ -15,4 +16,5 @@ const paywall = process.env.PAYWALL_URL || 'http://127.0.0.1:3001'
 module.exports = {
   main: (path = '/') => `http://${host}:${port}${path}`,
   paywall: (path = '/') => `${paywall}${path}`,
+  provider,
 }
