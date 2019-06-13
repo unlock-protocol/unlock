ec118bee6fdf2fb7d9eb7386e92755f926ca784f
Gregory Beaver <greg@unlock-protocol.com>
2019-06-13 16:39:16 -0400

    remove blockchain connect timeout from ensureWalletReady (#3787)

--- paywall/src/data-iframe/blockchainHandler/ensureWalletReady.js
+++ paywall/src/data-iframe/blockchainHandler/ensureWalletReady.js
@@ -1,5 +1,4 @@
 export default async function ensureWalletReady(walletService) {
-  let waiting = true
   return new Promise((resolve, reject) => {
     if (!walletService) {
       return reject(
@@ -12,12 +11,7 @@ export default async function ensureWalletReady(walletService) {
       return resolve()
     }
     walletService.once('ready', () => {
-      if (!waiting) return // timed out
       resolve()
     })
-    setTimeout(() => {
-      waiting = false
-      reject(new Error('connecting to blockchain timed out'))
-    }, 10000)
   })
 }
