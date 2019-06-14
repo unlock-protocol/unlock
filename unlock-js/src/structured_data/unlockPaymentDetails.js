ee2503b18107cf5c67b26608c4228c32237dd374
Christopher Nascone <cnasc@users.noreply.github.com>
2019-06-14 13:14:35 -0400

    unlockProvider gains a method to sign payment details (#3849)
    * unlockProvider gains a method to sign payment details
    
    * More descriptive name for the stripe token
    
    * Also bump the version

---
+++ unlock-js/src/structured_data/unlockPaymentDetails.js
@@ -0,0 +1,40 @@
+export default class UnlockPaymentDetails {
+  static build(input) {
+    let domain = [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+      { name: 'salt', type: 'bytes32' },
+    ]
+
+    let user = [
+      { name: 'emailAddress', type: 'string' },
+      { name: 'publicKey', type: 'address' },
+      { name: 'stripeTokenId', type: 'string' },
+    ]
+
+    let domainData = {
+      name: 'Unlock',
+      version: '1',
+    }
+
+    let message = {
+      user: {
+        emailAddress: input.emailAddress,
+        publicKey: input.publicKey,
+        stripeTokenId: input.stripeTokenId,
+      },
+    }
+
+    return {
+      types: {
+        EIP712Domain: domain,
+        User: user,
+      },
+      domain: domainData,
+      primaryType: 'User',
+      message: message,
+    }
+  }
+}
