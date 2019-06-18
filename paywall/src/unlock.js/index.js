6c48e8c42baf6ffc71cb232609b25cf8c880e55a
Gregory Beaver <greg@unlock-protocol.com>
2019-06-18 10:23:01 -0400

    fix multiple load (#3894)

--- paywall/src/unlock.js/index.js
+++ paywall/src/unlock.js/index.js
@@ -1,12 +1,18 @@
 import startup from './startup'
 import '../paywall-builder/iframe.css'
 
+let started = false
 if (document.readyState !== 'loading') {
   // in most cases, we will start up after the document is interactive
   // so listening for the DOMContentLoaded or load events is superfluous
   startup(window)
+  started = true
 } else {
+  const begin = () => {
+    if (!started) startup(window)
+    started = true
+  }
   // if we reach here, the page is sitll loading
-  window.addEventListener('DOMContentLoaded', () => startup(window))
-  window.addEventListener('load', () => startup(window))
+  window.addEventListener('DOMContentLoaded', begin)
+  window.addEventListener('load', begin)
 }
