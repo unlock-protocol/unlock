62abad1c216519ae75e8c75ae747132f784cbeac
Ben Werdmuller <ben@benwerd.com>
2019-06-19 13:53:32 -0700

    Suspending react/display-name for ticket tests (#3927)

--- tickets/src/__tests__/components/content/CreateContent.test.js
+++ tickets/src/__tests__/components/content/CreateContent.test.js
@@ -1,4 +1,4 @@
-/* eslint react/prop-types: 0 */
+/* eslint react/prop-types: 0, react/display-name: 0 */
 import React from 'react'
 import { Provider } from 'react-redux'
 import * as rtl from 'react-testing-library'
