4ebbd4d358fd8ec096156a92c40215ec78346180
Christopher Nascone <cnasc@users.noreply.github.com>
2019-06-20 16:16:09 -0400

    Initial bare-bones payment details form (#3943)
    * Initial payment details component (will break, did a --no-verify)
    
    * PaymentDetails in settings, mostly works
    
    * Bare bones payment details component
    
    * Add explanatory comment

---
+++ unlock-app/src/components/interface/user-account/PaymentDetails.tsx
@@ -0,0 +1,80 @@
+/* eslint react/display-name: 0 */
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+  ReactStripeElements,
+  StripeProvider,
+  Elements,
+  CardElement,
+  injectStripe,
+} from 'react-stripe-elements'
+import { SectionHeader, Column, Grid, SubmitButton } from './styles'
+import { signPaymentData } from '../../../actions/user'
+
+interface PaymentDetailsProps {
+  stripe: any
+  signPaymentData: (stripeTokenId: string) => any
+}
+
+// Memoized because it would constantly rerender (which cleared the Stripe form)
+// because it couldn't tell the props were the same
+export const PaymentDetails = React.memo(
+  ({ stripe, signPaymentData }: PaymentDetailsProps) => {
+    const Form = injectStripe(PaymentForm)
+    return (
+      <StripeProvider stripe={stripe}>
+        <Elements>
+          <Form signPaymentData={signPaymentData} />
+        </Elements>
+      </StripeProvider>
+    )
+  }
+)
+
+interface PaymentFormProps {
+  signPaymentData: (stripeTokenId: string) => any
+}
+
+export class PaymentForm extends React.Component<
+  PaymentFormProps & ReactStripeElements.InjectedStripeProps
+> {
+  handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const { stripe, signPaymentData } = this.props
+    if (stripe) {
+      const result = await stripe.createToken()
+
+      // TODO: handle result.error case here
+      if (result.token) {
+        signPaymentData(result.token.id)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Grid>
+        <SectionHeader>Card Details</SectionHeader>
+        <Column size="full">
+          <CardElement />
+        </Column>
+        <Column size="half">
+          <SubmitButton onClick={this.handleSubmit}>
+            Add Payment Method
+          </SubmitButton>
+        </Column>
+      </Grid>
+    )
+  }
+}
+
+export const mapDispatchToProps = (dispatch: any) => ({
+  signPaymentData: (stripeTokenId: string) =>
+    dispatch(signPaymentData(stripeTokenId)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PaymentDetails)
