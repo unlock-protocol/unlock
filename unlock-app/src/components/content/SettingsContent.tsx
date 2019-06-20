db95b658d9a84c598065d5716b2666b4b2894a0a
Christopher Nascone <cnasc@users.noreply.github.com>
2019-06-20 16:16:09 -0400

    Initial bare-bones payment details form (#3943)
    * Initial payment details component (will break, did a --no-verify)
    
    * PaymentDetails in settings, mostly works
    
    * Bare bones payment details component
    
    * Add explanatory comment

--- unlock-app/src/components/content/SettingsContent.tsx
+++ unlock-app/src/components/content/SettingsContent.tsx
@@ -1,23 +1,77 @@
 import React from 'react'
 import Head from 'next/head'
+import withConfig from '../../utils/withConfig'
 import Layout from '../interface/Layout'
 import { pageTitle } from '../../constants'
 import Errors from '../interface/Errors'
 import AccountInfo from '../interface/user-account/AccountInfo'
 import ChangePassword from '../interface/user-account/ChangePassword'
+import PaymentDetails from '../interface/user-account/PaymentDetails'
 
-export const SettingsContent = () => {
-  return (
-    <Layout title="Account Settings">
-      <Head>
-        <title>{pageTitle('Account Settings')}</title>
-        <script src="https://js.stripe.com/v3/"></script>
-      </Head>
-      <Errors />
-      <AccountInfo />
-      <ChangePassword />
-    </Layout>
-  )
+// TODO: tighten up this type
+declare global {
+  interface Window {
+    Stripe?: any
+  }
 }
 
-export default SettingsContent
+interface SettingsContentProps {
+  config: {
+    stripeApiKey: string
+  }
+}
+interface SettingsContentState {
+  stripe: any
+}
+
+export class SettingsContent extends React.Component<
+  SettingsContentProps,
+  SettingsContentState
+> {
+  interval: number | null
+  constructor(props: SettingsContentProps) {
+    super(props)
+    this.state = {
+      stripe: null,
+    }
+    this.interval = null
+  }
+
+  componentDidMount() {
+    // componentDidMount only runs in the browser; this is necessary to listen
+    // for the stripe-js load event which *cannot* be rendered server side
+    this.interval = setInterval(this.getStripe, 500)
+  }
+
+  getStripe = () => {
+    const {
+      config: { stripeApiKey },
+    } = this.props
+    if (window.Stripe) {
+      this.setState({
+        stripe: window.Stripe(stripeApiKey),
+      })
+      if (this.interval) {
+        clearInterval(this.interval)
+      }
+    }
+  }
+
+  render() {
+    const { stripe } = this.state
+    return (
+      <Layout title="Account Settings">
+        <Head>
+          <title>{pageTitle('Account Settings')}</title>
+          <script id="stripe-js" src="https://js.stripe.com/v3/" async></script>
+        </Head>
+        <Errors />
+        <AccountInfo />
+        <ChangePassword />
+        <PaymentDetails stripe={stripe} />
+      </Layout>
+    )
+  }
+}
+
+export default withConfig(SettingsContent)
