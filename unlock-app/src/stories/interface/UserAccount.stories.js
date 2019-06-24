0e7b238d6466f82d28bd9f86055fbd0eb8f98c9d
Christopher Nascone <cnasc@users.noreply.github.com>
2019-06-24 15:56:38 -0400

    Add PaymentMethods component (#3972)

--- unlock-app/src/stories/interface/UserAccount.stories.js
+++ unlock-app/src/stories/interface/UserAccount.stories.js
@@ -3,14 +3,32 @@ import { storiesOf } from '@storybook/react'
 import { AccountInfo } from '../../components/interface/user-account/AccountInfo'
 import { ChangePassword } from '../../components/interface/user-account/ChangePassword'
 import { KeyPurchaseConfirmation } from '../../components/interface/user-account/KeyPurchaseConfirmation'
+import { PaymentMethods } from '../../components/interface/user-account/PaymentMethods'
 import { changePassword, confirmKeyPurchase } from '../../actions/user'
 import { Grid } from '../../components/interface/user-account/styles'
 
-storiesOf('User Account/Components/AccountInfo', module)
-  .add('no info', () => {
+const cards = [
+  {
+    id: 'card_1Eox8QIsiZS2oQBMkU2KqFnq',
+    brand: 'Visa',
+    exp_month: 8,
+    exp_year: 2020,
+    last4: '4242',
+  },
+  {
+    id: 'card_1EoxVMIsiZS2oQBMFzQ3ToR5',
+    brand: 'American Express',
+    exp_month: 12,
+    exp_year: 2020,
+    last4: '0005',
+  },
+]
+
+storiesOf('User Account/Components', module)
+  .add('AccountInfo, no info', () => {
     return <AccountInfo email="" address="" />
   })
-  .add('info provided', () => {
+  .add('AccountInfo, info provided', () => {
     return (
       <AccountInfo
         emailAddress="gordon@lonsdale.me"
@@ -18,25 +36,20 @@ storiesOf('User Account/Components/AccountInfo', module)
       />
     )
   })
-
-storiesOf('User Account/Components/ChangePassword', module).add(
-  'default',
-  () => {
+  .add('ChangePassword', () => {
     return <ChangePassword changePassword={changePassword} />
-  }
-)
-
-storiesOf('User Account/Components/KeyPurchaseConfirmation', module).add(
-  'default',
-  () => {
+  })
+  .add('KeyPurchaseConfirmation', () => {
     return (
       <KeyPurchaseConfirmation
         emailAddress="jenny@googlemail.com"
         confirmKeyPurchase={confirmKeyPurchase}
       />
     )
-  }
-)
+  })
+  .add('PaymentMethods', () => {
+    return <PaymentMethods cards={cards} />
+  })
 
 storiesOf('User Account/Settings', module).add('Mock settings page', () => {
   return (
@@ -46,6 +59,7 @@ storiesOf('User Account/Settings', module).add('Mock settings page', () => {
         address="0x09438E46Ea66647EA65E4b104C125c82076FDcE5"
       />
       <ChangePassword changePassword={changePassword} />
+      <PaymentMethods cards={cards} />
     </Grid>
   )
 })
