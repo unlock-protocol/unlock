868574485dceb065630c09f838de7c549f2c29e8
Christoph Nascone <chris@unlock-protocol.com>
2019-06-25 14:18:13 -0400

    Using real components in the AccountContent page

--- unlock-app/src/components/content/AccountContent.tsx
+++ unlock-app/src/components/content/AccountContent.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Head from 'next/head'
+import withConfig from '../../utils/withConfig'
 import Layout from '../interface/Layout'
 import { pageTitle } from '../../constants'
 import Errors from '../interface/Errors'
 import PaymentDetails from '../interface/user-account/PaymentDetails'
+import LogInSignUp from '../interface/LogInSignUp'
+import KeyPurchaseConfirmation from '../interface/user-account/KeyPurchaseConfirmation'
 
 declare global {
   interface Window {
@@ -12,6 +16,11 @@ declare global {
 }
 
 interface AccountContentProps {
+  emailAddress?: string
+  cards?: stripe.Card[]
+}
+
+interface FullAccountContentProps extends AccountContentProps {
   config: {
     stripeApiKey: string
   }
@@ -20,12 +29,14 @@ interface AccountContentState {
   stripe: stripe.Stripe | null
 }
 
+type PageMode = 'LogIn' | 'CollectPaymentDetails' | 'ConfirmPurchase'
+
 export class AccountContent extends React.Component<
-  AccountContentProps,
+  FullAccountContentProps,
   AccountContentState
 > {
   interval: number | null
-  constructor(props: AccountContentProps) {
+  constructor(props: FullAccountContentProps) {
     super(props)
     this.state = {
       stripe: null,
@@ -37,6 +48,28 @@ export class AccountContent extends React.Component<
     // componentDidMount only runs in the browser; this is necessary to check
     // for the stripe-js library which *cannot* be rendered server side
     this.interval = setInterval(this.getStripe, 500)
+  }
+
+  getComponent = (mode: PageMode) => {
+    const { stripe } = this.state
+    const components: Record<PageMode, JSX.Element> = {
+      LogIn: <LogInSignUp login />,
+      CollectPaymentDetails: <PaymentDetails stripe={stripe} />,
+      ConfirmPurchase: <KeyPurchaseConfirmation />,
+    }
+
+    return components[mode]
+  }
+
+  currentPageMode = (): PageMode => {
+    const { emailAddress, cards } = this.props
+    if (!emailAddress) {
+      return 'LogIn'
+    } else if (!cards) {
+      return 'CollectPaymentDetails'
+    } else {
+      return 'ConfirmPurchase'
+    }
   }
 
   getStripe = () => {
@@ -54,7 +87,7 @@ export class AccountContent extends React.Component<
   }
 
   render() {
-    const { stripe } = this.state
+    const mode = this.currentPageMode()
     return (
       <Layout title="Account">
         <Head>
@@ -62,10 +95,32 @@ export class AccountContent extends React.Component<
           <script src="https://js.stripe.com/v3/" async />
         </Head>
         <Errors />
-        <PaymentDetails stripe={stripe} />
+        {this.getComponent(mode)}
       </Layout>
     )
   }
 }
 
-export default AccountContent
+interface ReduxState {
+  account?: {
+    emailAddress?: string
+    cards?: stripe.Card[]
+  }
+}
+export const mapStateToProps = (state: ReduxState) => {
+  let props: AccountContentProps = {}
+
+  if (state.account) {
+    const { emailAddress, cards } = state.account
+    if (emailAddress) {
+      props.emailAddress = emailAddress
+    }
+    if (state.account.cards) {
+      props.cards = cards
+    }
+  }
+
+  return props
+}
+
+export default withConfig(connect(mapStateToProps)(AccountContent))
