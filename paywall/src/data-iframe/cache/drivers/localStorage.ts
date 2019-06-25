8066d17f8d32973a6d77ffe9798ed0df35f811b3
Gregory Beaver <greg@unlock-protocol.com>
2019-06-25 14:52:44 -0400

    new cache drivers for abstracting low-level cache access (#3991)

---
+++ paywall/src/data-iframe/cache/drivers/localStorage.ts
@@ -0,0 +1,61 @@
+import localStorageAvailable from '../../../utils/localStorage'
+import CacheDriver from './driverInterface'
+import { LocalStorageWindow } from '../../../windowTypes'
+
+export function storageId(networkId: number, accountAddress: string) {
+  return `unlock-protocol/${networkId}/${accountAddress}`
+}
+
+export default class LocalStorageDriver implements CacheDriver {
+  private window: LocalStorageWindow
+  constructor(window: LocalStorageWindow) {
+    this.window = window
+  }
+
+  async getKeyedItem(networkId: number, accountAddress: string) {
+    const item = this.window.localStorage.getItem(
+      storageId(networkId, accountAddress)
+    )
+    // not present in localStorage
+    if (!item) return null
+    try {
+      const container = JSON.parse(item)
+      return container
+    } catch (_) {
+      return null
+    }
+  }
+
+  available() {
+    return localStorageAvailable(this.window)
+  }
+
+  async getUnkeyedItem(key: 'account' | 'network') {
+    const item = this.window.localStorage.getItem(`__unlockProtocol.${key}`)
+    if (!item) return null
+    try {
+      const container = JSON.parse(item)
+      return container
+    } catch (_) {
+      return null
+    }
+  }
+
+  async saveKeyedItem(networkId: number, accountAddress: string, value: any) {
+    this.window.localStorage.setItem(
+      storageId(networkId, accountAddress),
+      JSON.stringify(value)
+    )
+  }
+
+  async saveUnkeyedItem(key: 'account' | 'network', value: any) {
+    this.window.localStorage.setItem(
+      `__unlockProtocol.${key}`,
+      JSON.stringify(value)
+    )
+  }
+
+  async clearKeyedCache(networkId: number, accountAddress: string) {
+    this.window.localStorage.removeItem(storageId(networkId, accountAddress))
+  }
+}
---
+++ paywall/src/data-iframe/cache/drivers/memory.ts
@@ -0,0 +1,50 @@
+import CacheDriver from './driverInterface'
+
+interface InMemoryCache {
+  account?: string
+  network?: number
+  [network: number]: {
+    [account: string]: {
+      [key: string]: any
+    }
+  }
+}
+
+export default class InMemoryDriver implements CacheDriver {
+  private cache: InMemoryCache = {}
+
+  async getKeyedItem(networkId: number, accountAddress: string) {
+    const container =
+      this.cache[networkId] && this.cache[networkId][accountAddress]
+    if (!container) return null
+    return container
+  }
+
+  available() {
+    return true
+  }
+
+  async getUnkeyedItem(key: 'account' | 'network') {
+    const item = this.cache[key]
+    if (!item) return null
+    return item
+  }
+
+  async saveKeyedItem(networkId: number, accountAddress: string, value: any) {
+    this.cache[networkId] = this.cache[networkId] || {}
+    this.cache[networkId][accountAddress] = value
+  }
+
+  async saveUnkeyedItem(key: 'account' | 'network', value: any) {
+    this.cache[key] = value
+  }
+
+  async clearKeyedCache(networkId: number, accountAddress: string) {
+    if (!this.cache[networkId]) return
+    delete this.cache[networkId][accountAddress]
+  }
+
+  __clear() {
+    this.cache = {}
+  }
+}
