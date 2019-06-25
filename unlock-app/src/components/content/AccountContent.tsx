5fa474d597977d69cddb8fa007962be5e2f15637
Christoph Nascone <chris@unlock-protocol.com>
2019-06-25 09:27:13 -0400

    Starting to fill in the AccountContent page

--- unlock-app/src/components/content/AccountContent.tsx
+++ unlock-app/src/components/content/AccountContent.tsx
@@ -3,17 +3,69 @@ import Head from 'next/head'
 import Layout from '../interface/Layout'
 import { pageTitle } from '../../constants'
 import Errors from '../interface/Errors'
+import PaymentDetails from '../interface/user-account/PaymentDetails'
 
-export const AccountContent = () => {
-  return (
-    <Layout title="Account">
-      <Head>
-        <title>{pageTitle('Account')}</title>
-        <script src="https://js.stripe.com/v3/" />
-      </Head>
-      <Errors />
-    </Layout>
-  )
+declare global {
+  interface Window {
+    Stripe?: stripe.StripeStatic
+  }
+}
+
+interface AccountContentProps {
+  config: {
+    stripeApiKey: string
+  }
+}
+interface AccountContentState {
+  stripe: stripe.Stripe | null
+}
+
+export class AccountContent extends React.Component<
+  AccountContentProps,
+  AccountContentState
+> {
+  interval: number | null
+  constructor(props: AccountContentProps) {
+    super(props)
+    this.state = {
+      stripe: null,
+    }
+    this.interval = null
+  }
+
+  componentDidMount() {
+    // componentDidMount only runs in the browser; this is necessary to check
+    // for the stripe-js library which *cannot* be rendered server side
+    this.interval = setInterval(this.getStripe, 500)
+  }
+
+  getStripe = () => {
+    const {
+      config: { stripeApiKey },
+    } = this.props
+    if (window.Stripe) {
+      this.setState({
+        stripe: window.Stripe(stripeApiKey),
+      })
+      if (this.interval) {
+        clearInterval(this.interval)
+      }
+    }
+  }
+
+  render() {
+    const { stripe } = this.state
+    return (
+      <Layout title="Account">
+        <Head>
+          <title>{pageTitle('Account')}</title>
+          <script src="https://js.stripe.com/v3/" async />
+        </Head>
+        <Errors />
+        <PaymentDetails stripe={stripe} />
+      </Layout>
+    )
+  }
 }
 
 export default AccountContent
