61dbc466f4dd3abcc2622c3c09d91b883c2933ed
Christoph Nascone <chris@unlock-protocol.com>
2019-06-28 16:51:35 -0400

    Test KeyPurchaseConfirmation

--- unlock-app/src/components/interface/user-account/KeyPurchaseConfirmation.tsx
+++ unlock-app/src/components/interface/user-account/KeyPurchaseConfirmation.tsx
@@ -36,7 +36,7 @@ export const KeyPurchaseConfirmation = ({
         <ItemValue>Visa ending in 5869</ItemValue>
       </Item>
       <LockInfo price="$17.19" timeRemaining="30 Days" />
-      <SubmitButton onClick={signPurchaseData(data)} roundBottomOnly>
+      <SubmitButton onClick={() => signPurchaseData(data)} roundBottomOnly>
         Confirm Purchase
       </SubmitButton>
     </Grid>
