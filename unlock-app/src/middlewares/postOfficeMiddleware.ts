2183596a5d1b7abdea9461d5579ce2923f471a67
Christopher Nascone <cnasc@users.noreply.github.com>
2019-07-02 14:23:07 -0400

    Fleshing out postOfficeMiddleware (#4091)
    * Set up to allow testing
    
    * Add PostOffice error category
    
    * Handling the error event
    
    * Starting to test KeyPurchase event
    
    * PostOfficeMiddleware MVP

--- unlock-app/src/middlewares/postOfficeMiddleware.ts
+++ unlock-app/src/middlewares/postOfficeMiddleware.ts
@@ -1,11 +1,31 @@
-import { iframePostOffice, IframePostOfficeWindow } from '../utils/postOffice'
+import { IframePostOfficeWindow } from '../utils/postOffice'
+import {
+  PostOfficeEvents,
+  PostOfficeService,
+} from '../services/postOfficeService'
+import { setError } from '../actions/error'
+import { KEY_PURCHASE_INITIATED } from '../actions/user'
+import { PostOffice } from '../utils/Error'
+import { addToCart } from '../actions/keyPurchase'
 
-const postOfficeMiddleware = (window: IframePostOfficeWindow) => {
-  const postOffice = iframePostOffice(window, 'Account UI')
-  return (_: any) => {
-    //return ({ getState, dispatch }) => {
-    postOffice.addHandler('boilerplate, not used yet', () => {})
+const postOfficeMiddleware = (window: IframePostOfficeWindow, config: any) => {
+  const postOfficeService = new PostOfficeService(
+    window,
+    config.requiredNetworkId
+  )
+  return ({ dispatch }: any) => {
+    postOfficeService.on(PostOfficeEvents.Error, message => {
+      dispatch(setError(PostOffice.Diagnostic(message)))
+    })
+    postOfficeService.on(PostOfficeEvents.KeyPurchase, (lock, tip) => {
+      postOfficeService.showAccountModal()
+      dispatch(addToCart({ lock, tip }))
+    })
     return (next: (action: any) => void) => (action: any) => {
+      if (action.type === KEY_PURCHASE_INITIATED) {
+        postOfficeService.transactionInitiated()
+        postOfficeService.hideAccountModal()
+      }
       next(action)
     }
   }
