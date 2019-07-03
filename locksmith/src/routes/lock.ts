f4d39249646d46893b15974f818ce747cd0ad80a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-07-03 15:44:00 -0400

    Relaxes lock creation requirement (#4097)
    * Removes signature requirement around Lock creation
    * Removes the ability to update Lock info through API

--- locksmith/src/routes/lock.ts
+++ locksmith/src/routes/lock.ts
@@ -1,19 +1,7 @@
 import express from 'express'
-import signatureValidationMiddleware from '../middlewares/signatureValidationMiddleware'
 
 var router = express.Router()
 var lockController = require('../controllers/lockController')
-
-let lockSignatureConfiguration = {
-  name: 'lock',
-  required: ['name', 'owner', 'address'],
-  signee: 'owner',
-}
-
-router.post(
-  /^\/lock\/?$/i,
-  signatureValidationMiddleware.generateProcessor(lockSignatureConfiguration)
-)
 
 router.post('/lock', lockController.lockSave)
 router.get('/lock/:lockAddress', lockController.lockGet)
