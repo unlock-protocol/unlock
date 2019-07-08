0ca1bfd9bcc6377cac8b59d3e122c28f006899fb
Julien Genestoux <julien.genestoux@gmail.com>
2019-07-08 14:44:20 -0400

    adding custom metadata for Week in Ethereum News

--- locksmith/__tests__/controllers/metadataController.test.ts
+++ locksmith/__tests__/controllers/metadataController.test.ts
@@ -21,5 +21,25 @@ describe('Metadata Controller', () => {
         })
       )
     })
+
+    it('returns wellformed data for Week in Ethereum News', async () => {
+      expect.assertions(2)
+
+      let response = await request(app)
+        .get('/api/key/0x95de5F777A3e283bFf0c47374998E10D8A2183C7/1')
+        .set('Accept', 'json')
+
+      expect(response.status).toBe(200)
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          description:
+            "A Key to the 'Week in Ethereum News' lock. Unlock protocol for memberships. https://unlock-protocol.com/",
+          image:
+            'https://assets.unlock-protocol.com/unlock-default-key-image.png',
+          name: 'Unlock Key to Week in Ethereum News',
+        })
+      )
+    })
   })
 })
