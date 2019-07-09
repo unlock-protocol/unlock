0785248997cb03c68aa6e5accc860bd3026698c1
metjm <metjm.mail@gmail.com>
2019-07-09 14:42:21 +0200

    simple transfer test

--- smart-contracts/test/UnlockDiscountToken/UnlockDiscountToken.js
+++ smart-contracts/test/UnlockDiscountToken/UnlockDiscountToken.js
@@ -43,4 +43,35 @@ contract('UnlockDiscountToken', accounts => {
       });
     });
   });
+
+  describe('Transfer', () => {
+    const mintAmount = 1000000;
+
+    before(async () => {
+      for (let i = 0; i < 3; i++)
+        await unlockDiscountToken.mint(accounts[i], mintAmount, {from: accounts[0]})
+    });
+
+    describe('transfer', async () => {
+      const transferAmount = new BigNumber(123);
+      let balanceBefore0, balanceBefore1;
+
+      before(async () => {
+        balanceBefore0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]));
+        balanceBefore1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]));
+      });
+
+      it('normal transfer', async () => {
+        await unlockDiscountToken.transfer(
+          accounts[1],
+          transferAmount,
+          {from: accounts[0]}
+        );
+        const balanceAfter0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]));
+        const balanceAfter1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]));
+        assert(balanceBefore0.minus(transferAmount).eq(balanceAfter0), 'Sender balance must have gone down by amount sent');
+        assert(balanceBefore1.plus(transferAmount).eq(balanceAfter1), 'Recipient balance must have gone down by amount sent');
+      })
+    })
+  })
 });
