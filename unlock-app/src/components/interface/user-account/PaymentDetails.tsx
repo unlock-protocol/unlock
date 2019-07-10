b41e41a6f45036d6ebe921096a2171cd11ab159b
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-07-10 10:51:34 -0400

    Adds additional fields to CC form (#4139)
    * Adds additional fields to CC form
    
    * Address typescript complaint
    
    * Updating to reflect the current state with the form added

--- unlock-app/src/components/interface/user-account/PaymentDetails.tsx
+++ unlock-app/src/components/interface/user-account/PaymentDetails.tsx
@@ -8,12 +8,25 @@ import {
   CardElement,
   injectStripe,
 } from 'react-stripe-elements'
-import { SectionHeader, Column, Grid, SubmitButton } from './styles'
+import { Input, SectionHeader, Column, Grid, SubmitButton } from './styles'
 import { signPaymentData } from '../../../actions/user'
 
 interface PaymentDetailsProps {
   stripe: stripe.Stripe | null
   signPaymentData: (stripeTokenId: string) => any
+}
+
+interface PaymentFormProps {
+  signPaymentData: (stripeTokenId: string) => any
+}
+
+interface PaymentFormState {
+  cardHolderName: string
+  address1: string
+  address2: string
+  addressCity: string
+  addressState: string
+  addressCountry: string
 }
 
 // Memoized because it would constantly rerender (which cleared the Stripe form)
@@ -31,19 +44,56 @@ export const PaymentDetails = React.memo(
   }
 )
 
-interface PaymentFormProps {
-  signPaymentData: (stripeTokenId: string) => any
-}
-
 export class PaymentForm extends React.Component<
-  PaymentFormProps & ReactStripeElements.InjectedStripeProps
+  PaymentFormProps & ReactStripeElements.InjectedStripeProps,
+  PaymentFormState
 > {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      cardHolderName: '',
+      address1: '',
+      address2: '',
+      addressCity: '',
+      addressState: '',
+      addressCountry: '',
+    }
+  }
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target
+
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        [name]: value,
+      }
+
+      return newState
+    })
+  }
+
   handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
 
     const { stripe, signPaymentData } = this.props
+    const {
+      address1,
+      address2,
+      addressCity,
+      addressState,
+      addressCountry,
+      cardHolderName,
+    } = this.state
     if (stripe) {
-      const result = await stripe.createToken()
+      const result = await stripe.createToken({
+        address_line1: address1,
+        address_line2: address2,
+        address_city: addressCity,
+        address_state: addressState,
+        address_country: addressCountry,
+        name: cardHolderName,
+      })
 
       // TODO: handle result.error case here
       if (result.token) {
@@ -56,6 +106,48 @@ export class PaymentForm extends React.Component<
     return (
       <Grid>
         <SectionHeader>Card Details</SectionHeader>
+        <Input
+          name="cardHolderName"
+          id="cardHolderName"
+          type="text"
+          placeholder="Card Holder Name"
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name="address1"
+          id="address1"
+          type="text"
+          placeholder="Address Line 1"
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name="address2"
+          id="address2"
+          type="text"
+          placeholder="Address Line 2"
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name="addressCity"
+          id="addressCity"
+          type="text"
+          placeholder="City"
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name="addressState"
+          id="addressState"
+          type="text"
+          placeholder="State"
+          onChange={this.handleInputChange}
+        />
+        <Input
+          name="addressCountry"
+          id="addressCountry"
+          type="country"
+          placeholder="Country"
+          onChange={this.handleInputChange}
+        />
         <Column size="full">
           <CardElement />
         </Column>
