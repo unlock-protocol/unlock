2ba71673a608eac1e7a2873cdd104783d75851fa
Gregory Beaver <greg@unlock-protocol.com>
2019-07-10 22:37:10 -0400

    use the dev ganache for integration tests, to add erc20 support

--- docker/development/ganache.dockerfile
+++ docker/development/ganache.dockerfile
@@ -1,14 +1,17 @@
 FROM trufflesuite/ganache-cli:v6.4.3
 LABEL Unlock <ops@unlock-protocol.com>
 
+ARG blocktime=3
+ENV BLOCKTIME=${blocktime}
+
 RUN apk add --no-cache git openssh bash
 
 # Adding python which is needed by some dependencies (keccak...)
 RUN apk add --no-cache \
-    python \
-    python-dev \
-    py-pip \
-    build-base \
+  python \
+  python-dev \
+  py-pip \
+  build-base \
   && pip install virtualenv
 
 RUN npm install -g npm@6.4.1
@@ -31,4 +34,4 @@ COPY ./standup.sh .
 ENV DOCKER true
 EXPOSE 8545
 
-ENTRYPOINT ["/bin/bash", "/app/standup.sh"]
+ENTRYPOINT /app/standup.sh ${BLOCKTIME}
---
+++ tests/helpers/erc20IsUp.js
@@ -0,0 +1,85 @@
+// This file is used to probe the ganache server. It attempts to call
+// balanceOf(0xaaadeed4...) to get the ERC20 token balance.
+// when the standup script has finished, it will return 500 tokens,
+// which is assumed if our call to balanceOf returns a non-zero value.
+const axios = require('axios')
+const url = require('./url')
+
+const erc20ContractAddress = '0x591AD9066603f5499d12fF4bC207e2f577448c46'
+const testingAddress = '0xaaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2'
+
+// 0x70a08231 = encoded "balanceOf" signature
+// aaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2 = the encoded testingAddress
+// this is used in the params for eth_call
+const balanceOfQuery =
+  '0x70a08231000000000000000000000000aaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2'
+let id = 1
+
+const erc20IsUp = ({ delay, maxAttempts }) => {
+  let attempts = 1
+  let lastResponse = undefined
+  return new Promise((resolve, reject) => {
+    function retrieveBalance() {
+      axios
+        .post(
+          url.provider,
+          {
+            jsonrpc: '2.0',
+            id: id++,
+            method: 'eth_call',
+            params: [
+              {
+                to: erc20ContractAddress,
+                from: testingAddress,
+                data: balanceOfQuery,
+              },
+            ],
+          },
+          {
+            headers: {
+              'content-type': 'application/json',
+            },
+          }
+        )
+        .then(response => {
+          if (
+            response.data.error ||
+            response.data.result === '0x' || // contract not deployed
+            response.data.result === // no erc20 coins minted yet
+              '0x0000000000000000000000000000000000000000000000000000000000000000'
+          ) {
+            if (response.data.result) {
+              if (lastResponse !== response.data.result) {
+                lastResponse = response.data.result
+                if (response.data.result === '0x') {
+                  console.log('...ERC20 contract not deployed') // eslint-disable-line
+                } else if (
+                  response.data.result ===
+                  '0x0000000000000000000000000000000000000000000000000000000000000000'
+                ) {
+                  console.log('...ERC20 contract deployed') // eslint-disable-line
+                } else {
+                  console.log(`...${lastResponse} coins minted`) // eslint-disable-line
+                }
+              }
+            }
+            if (attempts < maxAttempts) {
+              setTimeout(retrieveBalance, delay)
+              return
+            }
+          }
+          resolve()
+        })
+        .catch(error => {
+          if (attempts < maxAttempts) {
+            setTimeout(retrieveBalance, delay)
+            return
+          }
+          return reject(error)
+        })
+    }
+    retrieveBalance()
+  })
+}
+
+module.exports = erc20IsUp
