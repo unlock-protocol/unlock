f99f4397ffdb8f380722e8b6e7a434443fa9f3e6
metjm <metjm.mail@gmail.com>
2019-07-12 13:38:42 +0200

    remove extra semicolons

--- smart-contracts/test/UnlockDiscountToken/UnlockDiscountToken.js
+++ smart-contracts/test/UnlockDiscountToken/UnlockDiscountToken.js
@@ -1,77 +1,78 @@
-const BigNumber = require('bignumber.js');
-const UnlockDiscountToken = artifacts.require('../UnlockDiscountToken.sol');
+const BigNumber = require('bignumber.js')
 
-let unlockDiscountToken;
+const UnlockDiscountToken = artifacts.require('../UnlockDiscountToken.sol')
+
+let unlockDiscountToken
 
 contract('UnlockDiscountToken', accounts => {
   before(async () => {
-    unlockDiscountToken = await UnlockDiscountToken.deployed();
-  });
+    unlockDiscountToken = await UnlockDiscountToken.deployed()
+  })
 
   describe('Supply', () => {
     it('Starting supply is 0', async () => {
-      const totalSupply = new BigNumber(await unlockDiscountToken.totalSupply());
-      assert(totalSupply.eq(0), 'starting supply must be 0');
-    });
+      const totalSupply = new BigNumber(await unlockDiscountToken.totalSupply())
+      assert(totalSupply.eq(0), 'starting supply must be 0')
+    })
 
     it('Minting tokens', async () => {
-      const mintAmount = 1000;
-      const recipient = accounts[1];
-      const balanceBefore = new BigNumber(await unlockDiscountToken.balanceOf(recipient));
-      const totalSupplyBefore = await unlockDiscountToken.totalSupply();
+      const mintAmount = 1000
+      const recipient = accounts[1]
+      const balanceBefore = new BigNumber(await unlockDiscountToken.balanceOf(recipient))
+      const totalSupplyBefore = await unlockDiscountToken.totalSupply()
 
       before(async () => {
         await unlockDiscountToken.mint(
           recipient,
           mintAmount,
           {from: accounts[0]}
-        );
-      });
+        )
+      })
 
       it('Balance went up', async () => {
-        const balanceAfter = await unlockDiscountToken.balanceOf(recipient);
+        const balanceAfter = await unlockDiscountToken.balanceOf(recipient)
         assert(balanceAfter.eq(
           balanceBefore.plus(mintAmount)
-        ), 'Balance must increase by amount minted');
-      });
+        ), 'Balance must increase by amount minted')
+      })
 
       it('Total supply went up', async () => {
-        const totalSupplyAfter = await unlockDiscountToken.totalSupply();
+        const totalSupplyAfter = await unlockDiscountToken.totalSupply()
         assert(totalSupplyAfter.eq(
           totalSupplyBefore.plus(mintAmount)
-        ), 'Total supply must increase by amount minted');
-      });
-    });
-  });
+        ), 'Total supply must increase by amount minted')
+      })
+    })
+  })
 
   describe('Transfer', () => {
-    const mintAmount = 1000000;
+    const mintAmount = 1000000
 
     before(async () => {
       for (let i = 0; i < 3; i++)
         await unlockDiscountToken.mint(accounts[i], mintAmount, {from: accounts[0]})
-    });
+    })
 
     describe('transfer', async () => {
-      const transferAmount = new BigNumber(123);
-      let balanceBefore0, balanceBefore1;
+      const transferAmount = new BigNumber(123)
+      let balanceBefore0, balanceBefore1
 
       before(async () => {
-        balanceBefore0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]));
-        balanceBefore1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]));
-      });
+        balanceBefore0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]))
+        balanceBefore1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]))
+      })
 
       it('normal transfer', async () => {
         await unlockDiscountToken.transfer(
           accounts[1],
           transferAmount,
           {from: accounts[0]}
-        );
-        const balanceAfter0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]));
-        const balanceAfter1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]));
-        assert(balanceBefore0.minus(transferAmount).eq(balanceAfter0), 'Sender balance must have gone down by amount sent');
-        assert(balanceBefore1.plus(transferAmount).eq(balanceAfter1), 'Recipient balance must have gone down by amount sent');
+        )
+        const balanceAfter0 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[0]))
+        const balanceAfter1 = new BigNumber(await unlockDiscountToken.balanceOf(accounts[1]))
+        assert(balanceBefore0.minus(transferAmount).eq(balanceAfter0), 'Sender balance must have gone down by amount sent')
+        assert(balanceBefore1.plus(transferAmount).eq(balanceAfter1), 'Recipient balance must have gone down by amount sent')
       })
     })
   })
-});
+})
