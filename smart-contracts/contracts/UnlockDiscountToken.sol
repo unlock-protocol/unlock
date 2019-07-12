fd3a0705cf54ed02baa8396bafbd30b3842d8c00
metjm <metjm.mail@gmail.com>
2019-07-12 20:10:28 +0200

    Add UnlockDiscountToken.sol (#4160)
    * Add basic UnlockDiscountToken with migrations file and a simple test
    
    * clean up UnlockDiscountToken test file
    
    * total supply test, change structure of test
    
    * simple transfer test
    
    * fix comment in UnlockDiscountToken.sol
    
    * change indentation to 2 spaces
    
    * remove extra semicolons
    
    * reformat with prettier

---
+++ smart-contracts/contracts/UnlockDiscountToken.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.5.9;
+
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Mintable.sol';
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Detailed.sol';
+
+
+/**
+* @title The Unlock Discount Token
+* This smart contract implements the Unlock Discount Token
+*/
+contract UnlockDiscountToken is
+ERC20Mintable,
+ERC20Detailed
+{
+  function initialize() public initializer {
+    ERC20Mintable.initialize(msg.sender);
+    ERC20Detailed.initialize('Unlock Discount Token', 'UDT', 18);
+  }
+}
