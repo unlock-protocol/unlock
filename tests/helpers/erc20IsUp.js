648bc7223b1d39a1a87aac46500b216ae5b000e4
Gregory Beaver <greg@unlock-protocol.com>
2019-07-14 21:05:10 -0400

    remove axios dependency

--- tests/helpers/erc20IsUp.js
+++ tests/helpers/erc20IsUp.js
@@ -2,7 +2,7 @@
 // balanceOf(0xaaadeed4...) to get the ERC20 token balance.
 // when the standup script has finished, it will return 500 tokens,
 // which is assumed if our call to balanceOf returns a non-zero value.
-const axios = require('axios')
+const http = require('http')
 const url = require('./url')
 
 const erc20ContractAddress = process.env.ERC20_CONTRACT_ADDRESS
@@ -15,32 +15,76 @@ const balanceOfQuery =
   '0x70a08231000000000000000000000000aaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2'
 let id = 1
 
+function post(host, payload) {
+  return new Promise((resolve, reject) => {
+    const body = JSON.stringify(payload)
+
+    const options = {
+      hostname: host,
+      port: 8545,
+      path: '/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(body),
+      },
+      body: body,
+    }
+
+    const request = new http.ClientRequest(options)
+
+    request.on('response', res => {
+      if (res.statusCode !== 200) {
+        reject(new Error(`failed ${res.statusCode}`))
+      }
+      res.setEncoding('utf8')
+      let result = ''
+      res.on('data', chunk => {
+        result += chunk
+      })
+      res.on('end', () => {
+        try {
+          const response = {
+            data: JSON.parse(result),
+          }
+          resolve(response)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+
+    request.on('error', e => {
+      reject(new Error(`problem with request: ${e.message}`))
+    })
+
+    request.end(body)
+  })
+}
+
 const erc20IsUp = ({ delay, maxAttempts }) => {
   let attempts = 1
   let lastResponse = undefined
   return new Promise((resolve, reject) => {
     function retrieveBalance() {
-      axios
-        .post(
-          url.provider,
-          {
-            jsonrpc: '2.0',
-            id: id++,
-            method: 'eth_call',
-            params: [
-              {
-                to: erc20ContractAddress,
-                from: testingAddress,
-                data: balanceOfQuery,
-              },
-            ],
-          },
-          {
-            headers: {
-              'content-type': 'application/json',
+      post(
+        url.provider,
+        {
+          jsonrpc: '2.0',
+          id: id++,
+          method: 'eth_call',
+          params: [
+            {
+              to: erc20ContractAddress,
+              from: testingAddress,
+              data: balanceOfQuery,
             },
-          }
-        )
+          ],
+        },
+        {
+          'content-type': 'application/json',
+        }
+      )
         .then(response => {
           if (
             response.data.error ||
