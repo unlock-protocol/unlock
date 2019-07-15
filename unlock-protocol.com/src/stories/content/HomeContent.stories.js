01966d3f8d2578a07a569b50a6aa4be194fcaa84
Ben Werdmuller <ben@benwerd.com>
2019-07-15 16:57:26 -0700

    Adding mailing list back to homepage (#4201)
    * Adding export default
    
    * Privacy message

---
+++ unlock-protocol.com/src/stories/content/HomeContent.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import HomeContent from '../../components/content/HomeContent'
+
+storiesOf('HomeContent', module).add('Homepage content', () => <HomeContent />)
