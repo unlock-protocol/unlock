55e525f725c7080837b157b07eccddf865dbfcdb
NickCuso <hardlydiff@gmail.com>
2019-07-16 14:23:45 -0700

    Deploy UDT throw a proxy

--- smart-contracts/migrations/4_deploy_udt.js
+++ smart-contracts/migrations/4_deploy_udt.js
@@ -1,7 +1,28 @@
-const UnlockDiscountToken = artifacts.require("UnlockDiscountToken");
+const UnlockDiscountToken = artifacts.require('UnlockDiscountToken')
 
-module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(UnlockDiscountToken);
-  const unlockDiscountToken = await UnlockDiscountToken.deployed();
-  await unlockDiscountToken.initialize();
-};
+const { scripts } = require('zos')
+
+const { add, create, push } = scripts
+
+module.exports = async function(deployer, networkName, accounts) {
+  const proxyAdmin = accounts[9]
+
+  // Register Unlock in the zos.json
+  add({
+    contractsData: [
+      { name: 'UnlockDiscountToken', alias: 'UnlockDiscountToken' },
+    ],
+  })
+  // Push implementation contract to the network
+  await push({
+    network: networkName,
+    from: proxyAdmin,
+  })
+  await create({
+    contractAlias: 'UnlockDiscountToken',
+    methodName: 'initialize',
+    methodArgs: [],
+    network: networkName,
+    from: proxyAdmin,
+  })
+}
