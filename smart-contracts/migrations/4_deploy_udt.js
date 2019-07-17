585ee641738d5d5dc8f44579e1bfbef11e6ef3b4
NickCuso <hardlydiff@gmail.com>
2019-07-17 12:26:06 -0700

    Update tests to use proxy

--- smart-contracts/migrations/4_deploy_udt.js
+++ smart-contracts/migrations/4_deploy_udt.js
@@ -1,28 +1,39 @@
-const UnlockDiscountToken = artifacts.require('UnlockDiscountToken')
-
-const { scripts } = require('zos')
+const { scripts, ConfigManager } = require('zos')
 
 const { add, create, push } = scripts
 
 module.exports = async function(deployer, networkName, accounts) {
   const proxyAdmin = accounts[9]
 
+  const { network, txParams } = await ConfigManager.initNetworkConfiguration({
+    network: networkName,
+    from: proxyAdmin,
+  })
+
   // Register Unlock in the zos.json
-  add({
+  await add({
     contractsData: [
-      { name: 'UnlockDiscountToken', alias: 'UnlockDiscountToken' },
+      {
+        name: 'UnlockDiscountToken',
+        alias: 'UnlockDiscountToken',
+        network,
+        txParams,
+      },
     ],
   })
   // Push implementation contract to the network
   await push({
     network: networkName,
     from: proxyAdmin,
+    network,
+    txParams,
   })
   await create({
     contractAlias: 'UnlockDiscountToken',
     methodName: 'initialize',
     methodArgs: [],
-    network: networkName,
     from: proxyAdmin,
+    network,
+    txParams,
   })
 }
