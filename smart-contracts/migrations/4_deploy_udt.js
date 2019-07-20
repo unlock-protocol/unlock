fd1e282a891899486fc090485ad4bbb18adc157b
Hardly Difficult <hardlydiff@gmail.com>
2019-07-19 22:55:25 -0700

    Deploy UnlockDiscountToken via an upgradable proxy (#4232)
    * Deploy UDT throw a proxy
    
    * Update tests to use proxy

--- smart-contracts/migrations/4_deploy_udt.js
+++ smart-contracts/migrations/4_deploy_udt.js
@@ -1,7 +1,39 @@
-const UnlockDiscountToken = artifacts.require("UnlockDiscountToken");
+const { scripts, ConfigManager } = require('zos')
 
-module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(UnlockDiscountToken);
-  const unlockDiscountToken = await UnlockDiscountToken.deployed();
-  await unlockDiscountToken.initialize();
-};
+const { add, create, push } = scripts
+
+module.exports = async function(deployer, networkName, accounts) {
+  const proxyAdmin = accounts[9]
+
+  const { network, txParams } = await ConfigManager.initNetworkConfiguration({
+    network: networkName,
+    from: proxyAdmin,
+  })
+
+  // Register Unlock in the zos.json
+  await add({
+    contractsData: [
+      {
+        name: 'UnlockDiscountToken',
+        alias: 'UnlockDiscountToken',
+        network,
+        txParams,
+      },
+    ],
+  })
+  // Push implementation contract to the network
+  await push({
+    network: networkName,
+    from: proxyAdmin,
+    network,
+    txParams,
+  })
+  await create({
+    contractAlias: 'UnlockDiscountToken',
+    methodName: 'initialize',
+    methodArgs: [],
+    from: proxyAdmin,
+    network,
+    txParams,
+  })
+}
