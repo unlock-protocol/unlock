ddb7e25b0df71be19f1c9ebcc2270f4e9364e93e
Christopher Nascone <cnasc@users.noreply.github.com>
2019-07-23 11:53:05 -0400

    Basic access control by account type (#4272)
    * Add buttons to header for V1-related pages
    
    * Header now filters app navigation based on account type

--- unlock-app/src/components/interface/buttons/navigation/index.js
+++ unlock-app/src/components/interface/buttons/navigation/index.js
@@ -1,7 +1,11 @@
 import Dashboard from './Dashboard'
 import Log from './Log'
+import Settings from './Settings'
+import KeyChain from './KeyChain'
 
 export default {
   Dashboard,
+  KeyChain,
   Log,
+  Settings,
 }
