418d2e82680a7b72a79bef36f77c870697b8fe75
Hardly Difficult <hardlydiff@gmail.com>
2019-07-24 07:20:11 -0700

    Switch gas report to USD (#4277)
    * Switch gas report to USD
    
    * Calc gas assuming 5 gwei

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -111,6 +111,11 @@ module.exports = {
   mocha: {
     reporter: 'eth-gas-reporter',
     useColors: true,
+    reporterOptions: {
+      currency: 'USD',
+      excludeContracts: ['Migrations', 'TestErc20Token'],
+      gasPrice: 5,
+    },
   },
   plugins: ['truffle-security'],
 }
