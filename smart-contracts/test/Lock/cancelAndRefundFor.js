00e40c839d5c0d3a5ac722119568e68ba1740735
Hardly Difficult <hardlydiff@gmail.com>
2019-07-24 20:23:34 -0700

    Update cancelAndRefundFor.js

--- smart-contracts/test/Lock/cancelAndRefundFor.js
+++ smart-contracts/test/Lock/cancelAndRefundFor.js
@@ -127,7 +127,7 @@ contract('Lock / cancelAndRefundFor', accounts => {
         await lock.getCancelAndRefundApprovalHash(keyOwners[1], txSender),
         keyOwners[1]
       )
-      await lock.invalidateCancelSignature({ from: keyOwners[1] })
+      await lock.invalidateApprovalToCancelKey({ from: keyOwners[1] })
       await shouldFail(
         lock.cancelAndRefundFor(keyOwners[1], signature, {
           from: txSender,
