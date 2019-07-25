fe1e549c022c90467b6108071d3737d8794b6108
Hardly Difficult <hardlydiff@gmail.com>
2019-07-24 20:22:45 -0700

    Update MixinRefunds.sol

--- smart-contracts/contracts/mixins/MixinRefunds.sol
+++ smart-contracts/contracts/mixins/MixinRefunds.sol
@@ -74,7 +74,7 @@ contract MixinRefunds is
    * @dev Increments the current nonce for the msg.sender.
    * This can be used to invalidate a previously signed message.
    */
-  function invalidateCancelSignature(
+  function invalidateApprovalToCancelKey(
   ) external
   {
     keyOwnerToNonce[msg.sender]++;
