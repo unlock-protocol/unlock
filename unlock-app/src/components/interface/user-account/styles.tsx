25b4bf8e4110e9bb1cf5c6536b8001f5bc7edc89
Ben Werdmuller <ben@benwerd.com>
2019-07-29 13:56:12 -0700

    Refactored CC form padding

--- unlock-app/src/components/interface/user-account/styles.tsx
+++ unlock-app/src/components/interface/user-account/styles.tsx
@@ -11,6 +11,10 @@ export const Grid = styled.div`
   }
 `
 
+export const GridPadding = styled.div`
+  padding: 25px;
+`
+
 export const SectionHeader = styled.span`
   font-family: IBM Plex Sans;
   font-style: normal;
