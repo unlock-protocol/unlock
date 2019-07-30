6e44f152b65aa1a28af3130a3db26f651000c2d5
Julien Genestoux <julien.genestoux@gmail.com>
2019-07-30 15:16:53 +0200

    moved metadata config in its own file (#4316)

---
+++ locksmith/config/metadata.js
@@ -0,0 +1,45 @@
+/**
+ * This file includes the metadata for locks
+ * This is temporary until we have a proper storage for this which can be edited from the web app.
+ */
+
+module.exports = [
+  {
+    address: '0x95de5F777A3e283bFf0c47374998E10D8A2183C7',
+    name: 'Unlock Key to Week in Ethereum News',
+    description: "A Key to the 'Week in Ethereum News' lock.",
+    image: 'https://assets.unlock-protocol.com/week-in-ethereum.png',
+  },
+  {
+    address: '0x75fA3Aa7E999B9899010C5f05E52cD0543dAb465',
+    name: 'Forbes, ad-free, 1 day',
+    description: 'A key to the Forbes ad-free experience, valid 1 day.',
+    image: 'https://assets.unlock-protocol.com/forbes/forbes-1-day.png',
+  },
+  {
+    address: '0xb2B879764C649C7769f7c90845b0cb2A86add821',
+    name: 'Forbes, ad-free, 1 week',
+    description: 'A key to the Forbes ad-free experience, valid 1 week.',
+    image: 'https://assets.unlock-protocol.com/forbes/forbes-1-week.png',
+  },
+  {
+    address: '0x98c0cbf0e9525f1a6975a51c9d5e8e063c034d6d',
+    name: 'Ticket for Berlin Open Source Salon 2019.',
+    description:
+      'One ticket for the Berlin Open Source Salon 2019. https://berlin.opensourcesalon.com/',
+    image:
+      'https://ipfs.bmann.ca/ipfs/QmNoDcxMV8CzUVkzQiLupYgZvpLQ9o1Q2xMEaLVTkk7WzQ',
+  },
+  {
+    address: '0x3a65A52598B4d9A3C92514368A4152B182546A6D',
+    name: 'Key to EthHub Community',
+    description: 'A key to the EthHub Community Lock, valid 30 days',
+    image: 'https://assets.unlock-protocol.com/ethub-lock.png',
+  },
+  // {
+  //   address: '',
+  //   name: '',
+  //   description: '',
+  //   image: '',
+  // },
+]
