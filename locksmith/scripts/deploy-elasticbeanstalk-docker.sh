a8f73c7ab58dc0e8732578deb923bad9307e85df
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-07-29 22:27:32 -0400

    Support double digit params in the deployment script (#4321)
    https://stackoverflow.com/questions/18318716/why-do-bash-command-line-arguments-after-9-require-curly-brackets

--- locksmith/scripts/deploy-elasticbeanstalk-docker.sh
+++ locksmith/scripts/deploy-elasticbeanstalk-docker.sh
@@ -8,8 +8,8 @@ node_env=$7
 is_forked_pr=$8
 build_id=$9
 message="${10}"
-stripe_secret=$11
-purchaser_credentials=$12
+stripe_secret=${11}
+purchaser_credentials=${12}
 
 function check_is_forked_pr()
 {
