1bbdfdd375dcc1119f5409d998fab24350d98549
Gregory Beaver <greg@unlock-protocol.com>
2019-07-30 12:51:21 -0400

    enable new ConfirmingCheckoutModal (#4303)
    * enable new ConfirmingCheckoutModal
    
    * fix typo (H/T @julien51)
    
    Co-Authored-By: Julien Genestoux <julien.genestoux@gmail.com>

--- paywall/src/stories/content/CheckoutContent.stories.js
+++ paywall/src/stories/content/CheckoutContent.stories.js
@@ -91,6 +91,61 @@ const locks = {
   },
 }
 
+const nextYear =
+  Math.round(new Date().getTime() / 1000) + 60 * 60 * 24 * 365 + 5000
+
+const confirmingLocks = {
+  ...locks,
+  [lockAddress1]: {
+    name: 'Weekly',
+    address: lockAddress1,
+    keyPrice: '0.1',
+    expirationDuration: 60 * 60 * 24 * 7,
+    key: {
+      status: 'pending',
+      transactions: [
+        {
+          hash: 'hash',
+          confirmations: 432,
+          for: lockAddress1,
+          lock: lockAddress1,
+          key: `${lockAddress1}-${lockAddress1}`,
+        },
+      ],
+      expiration: nextYear,
+      confirmations: 0,
+      owner: lockAddress1,
+      lock: lockAddress1,
+    },
+  },
+}
+
+const unlockedLocks = {
+  ...locks,
+  [lockAddress1]: {
+    name: 'Weekly',
+    address: lockAddress1,
+    keyPrice: '0.1',
+    expirationDuration: 60 * 60 * 24 * 7,
+    key: {
+      status: 'valid',
+      transactions: [
+        {
+          hash: 'hash',
+          confirmations: 432,
+          for: lockAddress1,
+          lock: lockAddress1,
+          key: `${lockAddress1}-${lockAddress1}`,
+        },
+      ],
+      expiration: nextYear,
+      confirmations: 432,
+      owner: lockAddress1,
+      lock: lockAddress1,
+    },
+  },
+}
+
 const config = {
   ...configure(),
   requiredNetworkId: 1,
@@ -339,6 +394,70 @@ storiesOf('Checkout page', module)
     })
     return <CheckoutContent />
   })
+  .add('Checkout page, paywall checkout confirming', () => {
+    // set the data needed to display the checkout
+    useEffect(() => {
+      const messageTemplate = {
+        type: 'message',
+        source: fakeWindow.parent,
+        origin: 'origin',
+      }
+      fakeWindow.handlers.message.forEach(postedMessage => {
+        postedMessage({
+          ...messageTemplate,
+          data: {
+            type: POST_MESSAGE_CONFIG,
+            payload: paywallTypePaywallConfig,
+          },
+        })
+        setTimeout(() => {
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_UPDATE_ACCOUNT,
+              payload: lockAddress1,
+            },
+          })
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_UPDATE_ACCOUNT_BALANCE,
+              payload: '889',
+            },
+          })
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_UPDATE_LOCKS,
+              payload: confirmingLocks,
+            },
+          })
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_UPDATE_NETWORK,
+              payload: 1,
+            },
+          })
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_LOCKED,
+              payload: undefined,
+            },
+          })
+          postedMessage({
+            ...messageTemplate,
+            data: {
+              type: POST_MESSAGE_UNLOCKED,
+              payload: [lockAddress1],
+            },
+          })
+        })
+      })
+    })
+    return <CheckoutContent />
+  })
   .add('Checkout page, paywall checkout unlocked', () => {
     // set the data needed to display the checkout
     useEffect(() => {
@@ -374,7 +493,7 @@ storiesOf('Checkout page', module)
             ...messageTemplate,
             data: {
               type: POST_MESSAGE_UPDATE_LOCKS,
-              payload: locks,
+              payload: unlockedLocks,
             },
           })
           postedMessage({
