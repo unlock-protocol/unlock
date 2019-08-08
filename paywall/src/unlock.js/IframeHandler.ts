8f0b56dbbe71c8c55f16dbd2f4d94a2a378bc7f6
Gregory Beaver <greg@unlock-protocol.com>
2019-08-07 22:35:10 -0400

    add comments

--- paywall/src/unlock.js/IframeHandler.ts
+++ paywall/src/unlock.js/IframeHandler.ts
@@ -9,6 +9,13 @@ import {
 import { PaywallConfig } from '../unlockTypes'
 import { PostMessages } from '../messageTypes'
 
+/**
+ * This class creates the 3 iframes and provides a simple way to access all 3
+ *
+ * In addition, it contains code for passing information to the accounts iframe
+ * because putting anywhere else introduces a circular dependency and eslint
+ * bails out
+ */
 export default class IframeHandler {
   data: DataIframeMessageEmitter
   checkout: CheckoutIframeMessageEmitter
@@ -22,15 +29,19 @@ export default class IframeHandler {
   ) {
     this.data = new DataIframeMessageEmitter(window, dataIframeUrl)
     this.checkout = new CheckoutIframeMessageEmitter(window, checkoutIframeUrl)
+    // note that until "iframes.accounts.createIframe()" is called in Wallet.setupWallet(), this is a fake iframe
     this.accounts = new AccountsIframeMessageEmitter(window, userIframeUrl)
   }
 
   init() {
     this.data.setupListeners()
     this.checkout.setupListeners()
-    // account listener setup will be on-demand
+    // account listener setup will be on-demand, done by the Wallet in setupWallet()
   }
 
+  /**
+   * This is called in Wallet.init() if user accounts are enabled
+   */
   setupAccountUIHandler(config: PaywallConfig) {
     // listen for updates to state from the data iframe, and forward them to the checkout UI
     this.data.on(PostMessages.UPDATE_LOCKS, locks =>
