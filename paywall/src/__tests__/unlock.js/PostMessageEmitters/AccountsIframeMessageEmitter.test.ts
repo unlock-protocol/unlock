71403e01598ff7bd3fc43e963c77822caf141bf0
Gregory Beaver <greg@unlock-protocol.com>
2019-08-09 21:08:54 -0400

    new account iframe emitter (#4403)
    * new account iframe handler
    
    * correct variable name

---
+++ paywall/src/__tests__/unlock.js/PostMessageEmitters/AccountsIframeMessageEmitter.test.ts
@@ -0,0 +1,231 @@
+import FakeWindow from '../../test-helpers/fakeWindowHelpers'
+import AccountsIframeMessageEmitter from '../../../unlock.js/PostMessageEmitters/AccountsIframeMessageEmitter'
+import { PostMessages, ExtractPayload } from '../../../messageTypes'
+
+describe('AccountsIframeMessageEmitter', () => {
+  let fakeWindow: FakeWindow
+  const accountsOrigin = 'http://fun.times'
+
+  function makeEmitter(fakeWindow: FakeWindow) {
+    const emitter = new AccountsIframeMessageEmitter(
+      fakeWindow,
+      'http://fun.times/fakey'
+    )
+    return emitter
+  }
+
+  function makeEmitterWithIframe(fakeWindow: FakeWindow) {
+    const emitter = new AccountsIframeMessageEmitter(
+      fakeWindow,
+      'http://fun.times/fakey'
+    )
+    emitter.createIframe()
+    return emitter
+  }
+
+  describe('emitter construction', () => {
+    beforeEach(() => {
+      fakeWindow = new FakeWindow()
+    })
+
+    it('should create a dummy user accounts iframe', () => {
+      expect.assertions(2)
+
+      const emitter = makeEmitter(fakeWindow)
+
+      expect(emitter.iframe.src).toBe('http://fun.times/fakey')
+      expect(emitter.iframe.name).toBe('unlock accounts')
+    })
+
+    it('should not add the dummy iframe to the document', () => {
+      expect.assertions(1)
+
+      makeEmitter(fakeWindow)
+
+      expect(
+        fakeWindow.document.body.insertAdjacentElement
+      ).not.toHaveBeenCalled()
+    })
+
+    it('should set up postMessage after the iframe is created', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitterWithIframe(fakeWindow)
+
+      emitter.postMessage(PostMessages.SCROLL_POSITION, 5)
+
+      fakeWindow.expectPostMessageSentToIframe(
+        PostMessages.SCROLL_POSITION,
+        5,
+        emitter.iframe,
+        accountsOrigin // iframe origin
+      )
+    })
+
+    it('should set up addHandler after the iframe is created', () => {
+      expect.assertions(1)
+
+      const fakeReady = jest.fn()
+      const emitter = makeEmitterWithIframe(fakeWindow)
+
+      emitter.addHandler(PostMessages.READY, fakeReady)
+
+      fakeWindow.receivePostMessageFromIframe(
+        PostMessages.READY,
+        undefined,
+        emitter.iframe,
+        accountsOrigin
+      )
+
+      expect(fakeReady).toHaveBeenCalled()
+    })
+
+    it('should set up a dummy postMessage if the iframe is not created yet', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitter(fakeWindow)
+      emitter.iframe.contentWindow.postMessage = jest.fn()
+
+      emitter.postMessage(PostMessages.SCROLL_POSITION, 5)
+
+      expect(emitter.iframe.contentWindow.postMessage).not.toHaveBeenCalled()
+    })
+
+    it('should set up a dummy addHandler if the iframe is not created yet', () => {
+      expect.assertions(1)
+
+      const fakeReady = jest.fn()
+      const emitter = makeEmitter(fakeWindow)
+
+      emitter.addHandler(PostMessages.READY, fakeReady)
+
+      fakeWindow.receivePostMessageFromIframe(
+        PostMessages.READY,
+        undefined,
+        emitter.iframe,
+        accountsOrigin
+      )
+
+      expect(fakeReady).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('iframe visibility', () => {
+    beforeEach(() => {
+      fakeWindow = new FakeWindow()
+    })
+
+    it('should show the iframe when showIframe() is called', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitter(fakeWindow)
+
+      emitter.showIframe()
+
+      expect(emitter.iframe.className).toBe('unlock start show')
+    })
+
+    it('should disable scrolling on showing the iframe', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitter(fakeWindow)
+
+      emitter.showIframe()
+
+      expect(fakeWindow.document.body.style.overflow).toBe('hidden')
+    })
+
+    it('should hide the iframe when hideIframe() is called', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitter(fakeWindow)
+
+      emitter.showIframe()
+      emitter.hideIframe()
+
+      expect(emitter.iframe.className).toBe('unlock start')
+    })
+
+    it('should enable scrolling on hiding the iframe', () => {
+      expect.assertions(1)
+
+      const emitter = makeEmitter(fakeWindow)
+
+      emitter.showIframe()
+      emitter.hideIframe()
+
+      expect(fakeWindow.document.body.style.overflow).toBe('')
+    })
+  })
+
+  describe('setupListeners', () => {
+    let ready: () => void
+
+    beforeEach(() => {
+      fakeWindow = new FakeWindow()
+      ready = jest.fn()
+    })
+
+    type messageTypes =
+      | PostMessages.READY
+      | PostMessages.INITIATED_TRANSACTION
+      | PostMessages.SHOW_ACCOUNTS_MODAL
+      | PostMessages.HIDE_ACCOUNTS_MODAL
+    type VoidMessages = [string, messageTypes][]
+    it.each(<VoidMessages>[
+      ['READY', PostMessages.READY],
+      ['INITIATED_TRANSACTION', PostMessages.INITIATED_TRANSACTION],
+      ['SHOW_ACCOUNTS_MODAL', PostMessages.SHOW_ACCOUNTS_MODAL],
+      ['HIDE_ACCOUNTS_MODAL', PostMessages.HIDE_ACCOUNTS_MODAL],
+    ])('should emit PostMessages.%s upon receiving it', (_, message) => {
+      expect.assertions(1)
+
+      const emitter = makeEmitterWithIframe(fakeWindow)
+
+      emitter.on(message, ready)
+
+      fakeWindow.receivePostMessageFromIframe(
+        message,
+        undefined,
+        emitter.iframe,
+        accountsOrigin
+      )
+
+      expect(ready).toHaveBeenCalled()
+    })
+
+    interface PayloadMessageTypes {
+      [PostMessages.UPDATE_ACCOUNT]: ExtractPayload<PostMessages.UPDATE_ACCOUNT>
+      [PostMessages.UPDATE_NETWORK]: ExtractPayload<PostMessages.UPDATE_NETWORK>
+    }
+    type Messages = keyof PayloadMessageTypes
+    type PayloadMessages<T extends Messages = Messages> = [
+      string,
+      T,
+      PayloadMessageTypes[T]
+    ][]
+    it.each(<PayloadMessages>[
+      ['UPDATE_ACCOUNT', PostMessages.UPDATE_ACCOUNT, 'account'],
+      ['UPDATE_NETWORK', PostMessages.UPDATE_NETWORK, 4],
+    ])(
+      'should emit PostMessages.%s upon receiving it',
+      (_, message, payload) => {
+        expect.assertions(1)
+
+        const emitter = makeEmitterWithIframe(fakeWindow)
+        const receive: (p: typeof payload) => void = jest.fn()
+
+        emitter.on(message, receive)
+
+        fakeWindow.receivePostMessageFromIframe(
+          message,
+          payload,
+          emitter.iframe,
+          accountsOrigin
+        )
+
+        expect(receive).toHaveBeenCalledWith(payload)
+      }
+    )
+  })
+})
---
+++ paywall/src/unlock.js/PostMessageEmitters/AccountsIframeMessageEmitter.ts
@@ -0,0 +1,147 @@
+import { EventEmitter } from 'events'
+import { PostMessages, MessageTypes, ExtractPayload } from '../../messageTypes'
+import {
+  PostMessageResponder,
+  mainWindowPostOffice,
+  PostMessageListener,
+} from '../../utils/postOffice'
+import {
+  IframeType,
+  IframeManagingWindow,
+  PostOfficeWindow,
+  OriginWindow,
+} from '../../windowTypes'
+import {
+  makeIframe,
+  addIframeToDocument,
+  hideIframe,
+  showIframe,
+} from '../iframeManager'
+import {
+  UserAccountsIframeEventEmitter,
+  UserAccountsIframeEvents,
+} from './EventEmitterTypes'
+
+// eslint is too stupid to parse this if the extends is in the class declaration below, so we extract it
+class FancyEmitter extends (EventEmitter as {
+  new (): UserAccountsIframeEventEmitter
+}) {}
+
+/**
+ * This class loads user accounts on-demand
+ *
+ * As such, it is slightly different from the other emitters.
+ * This one does not create the iframe in the constructor, but instead
+ * makes a fake one so that we can interact with it as if it were there,
+ * but nothing happens unless we actually have an explicit need for user
+ * accounts.
+ *
+ * The Wallet class handles initialization of this, and the
+ * IframeHandler class handles setting up cross-iframe communication
+ * in the IframeHandler.setupAccountUIHandler() method, which is also
+ * called by the Wallet class in Wallet.setupWallet()
+ */
+export default class AccountsIframeMessageEmitter extends FancyEmitter {
+  private _addHandler?: (
+    type: keyof UserAccountsIframeEvents,
+    listener: PostMessageListener
+  ) => void
+  private window: IframeManagingWindow & PostOfficeWindow & OriginWindow
+
+  private _postMessage?: PostMessageResponder<PostMessages>
+  public iframe: IframeType
+
+  constructor(
+    window: IframeManagingWindow & PostOfficeWindow & OriginWindow,
+    userIframeUrl: string
+  ) {
+    super()
+
+    this.window = window
+    this.iframe = {
+      contentWindow: {
+        postMessage: () => {},
+      },
+      className: '',
+      name: 'unlock accounts',
+      src: userIframeUrl,
+      setAttribute: () => {},
+    }
+  }
+
+  /**
+   * This is called by the Wallet.setupWallet() method if user accounts
+   * are needed
+   */
+  createIframe() {
+    const url = new URL(this.iframe.src)
+    this.iframe = makeIframe(this.window, this.iframe.src, 'unlock accounts')
+    addIframeToDocument(this.window, this.iframe)
+
+    const { postMessage, addHandler } = mainWindowPostOffice(
+      this.window,
+      this.iframe,
+      url.origin,
+      'main window',
+      'User Accounts iframe'
+    )
+    this._postMessage = postMessage
+    this._addHandler = addHandler
+    this.setupListeners()
+  }
+
+  /**
+   * This method is used by the MainWindowHandler to show the account iframe when needed.
+   * It also hides the checkout iframe if it is visible
+   */
+  showIframe() {
+    // note: if we are using a dummy iframe this will not display anything
+    showIframe(this.window, this.iframe)
+  }
+
+  hideIframe() {
+    // note: if we are using a dummy iframe this will not hide anything
+    hideIframe(this.window, this.iframe)
+  }
+
+  private setupListeners() {
+    this.addHandler(PostMessages.READY, () => this.emit(PostMessages.READY))
+    this.addHandler(PostMessages.UPDATE_ACCOUNT, account =>
+      this.emit(PostMessages.UPDATE_ACCOUNT, account)
+    )
+    this.addHandler(PostMessages.UPDATE_NETWORK, network =>
+      this.emit(PostMessages.UPDATE_NETWORK, network)
+    )
+    this.addHandler(PostMessages.INITIATED_TRANSACTION, () =>
+      this.emit(PostMessages.INITIATED_TRANSACTION)
+    )
+    this.addHandler(PostMessages.SHOW_ACCOUNTS_MODAL, () =>
+      this.emit(PostMessages.SHOW_ACCOUNTS_MODAL)
+    )
+    this.addHandler(PostMessages.HIDE_ACCOUNTS_MODAL, () =>
+      this.emit(PostMessages.HIDE_ACCOUNTS_MODAL)
+    )
+  }
+
+  /**
+   * This is a proxy that ignores requests if the account iframe is not active yet
+   */
+  async addHandler(
+    type: keyof UserAccountsIframeEvents,
+    listener: PostMessageListener
+  ) {
+    if (!this._addHandler) return
+    this._addHandler(type, listener)
+  }
+
+  /**
+   * This is a proxy that ignores requests if the account iframe is not active yet
+   */
+  async postMessage<T extends MessageTypes = MessageTypes>(
+    type: T,
+    payload: ExtractPayload<T>
+  ) {
+    if (!this._postMessage) return
+    this._postMessage(type, payload)
+  }
+}
