529092edd6576ecba1ccc822a2fffaa742173af1
Gregory Beaver <greg@unlock-protocol.com>
2019-08-13 16:20:47 -0400

    make the integration test pass now that user accounts must be explicit

--- tests/test/unlock-provider-locked-page-visit.test.js
+++ tests/test/unlock-provider-locked-page-visit.test.js
@@ -5,7 +5,10 @@ describe('Unlock Account Creation', () => {
   describe('when visiting a locked page without a wallet', () => {
     it('displays a login modal', async () => {
       expect.assertions(4)
-      await page.goto(url('0x8276A24C03B7ff9307c5bb9c0f31aa60d284375f'))
+      await page.goto(
+        url('0x8276A24C03B7ff9307c5bb9c0f31aa60d284375f') +
+          '&unlockUserAccounts=true'
+      )
       await wait.forIframe()
       const loginIframe = page.mainFrame().childFrames()[1]
       let content = await loginIframe.content()
