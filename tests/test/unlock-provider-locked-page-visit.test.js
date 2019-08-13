907d9dfbabe2554d6b316e0e35f9127304c24cb1
Gregory Beaver <greg@unlock-protocol.com>
2019-08-13 16:50:00 -0400

    don't use newdemo for testing user accounts

--- tests/test/unlock-provider-locked-page-visit.test.js
+++ tests/test/unlock-provider-locked-page-visit.test.js
@@ -5,11 +5,24 @@ describe('Unlock Account Creation', () => {
   describe('when visiting a locked page without a wallet', () => {
     it('displays a login modal', async () => {
       expect.assertions(4)
+      const locks = [
+        {
+          address: '0x8276A24C03B7ff9307c5bb9c0f31aa60d284375f',
+        },
+      ]
+      // this is the only page on the paywall app that does not have an http provider wallet enabled
+      // and so it is best for testing user accounts
       await page.goto(
-        url('0x8276A24C03B7ff9307c5bb9c0f31aa60d284375f') +
-          '&unlockUserAccounts=true'
+        url.paywall(
+          `/static/adremover/integrationtesting-loggedout.html?locks=${encodeURIComponent(
+            JSON.stringify(locks)
+          )}&paywall=${encodeURIComponent(
+            url.paywall('')
+          )}&unlockUserAccounts=1`
+        ),
+        { waitUntil: 'networkidle2' }
       )
-      await wait.forIframe()
+      await wait.forIframe(2) // wait for 2 iframes to be loaded, the data and checkout iframes
       const loginIframe = page.mainFrame().childFrames()[1]
       let content = await loginIframe.content()
 
