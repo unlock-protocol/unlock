92ecd8acf5415943e36facd4d11020088205a653
Julien Genestoux <julien.genestoux@gmail.com>
2019-08-14 17:17:31 -0400

    now dispatching actions to tell the user to check their wallet when enabling (#4441)

--- tickets/src/middlewares/providerMiddleware.ts
+++ tickets/src/middlewares/providerMiddleware.ts
@@ -1,5 +1,6 @@
 import { SET_PROVIDER, providerReady } from '../actions/provider'
 import { setError } from '../actions/error'
+import { waitForWallet, dismissWalletCheck } from '../actions/walletStatus'
 import { FATAL_NOT_ENABLED_IN_PROVIDER } from '../errors'
 
 interface Action {
@@ -15,10 +16,17 @@ function initializeProvider(provider: { enable?: () => any }, dispatch: any) {
   // provider.enable exists for metamask and other modern dapp wallets and must be called, see:
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   if (provider.enable) {
+    dispatch(waitForWallet())
     provider
       .enable()
-      .then(() => dispatch(providerReady()))
-      .catch(() => dispatch(setError(FATAL_NOT_ENABLED_IN_PROVIDER)))
+      .then(() => {
+        dispatch(dismissWalletCheck())
+        dispatch(providerReady())
+      })
+      .catch(() => {
+        dispatch(dismissWalletCheck())
+        dispatch(setError(FATAL_NOT_ENABLED_IN_PROVIDER))
+      })
   } else {
     // Default case, provider doesn't have an enable method, so it must already be ready
     dispatch(providerReady())
--- unlock-app/src/middlewares/providerMiddleware.ts
+++ unlock-app/src/middlewares/providerMiddleware.ts
@@ -1,5 +1,6 @@
 import { SET_PROVIDER, providerReady } from '../actions/provider'
 import { setError } from '../actions/error'
+import { waitForWallet, dismissWalletCheck } from '../actions/fullScreenModals'
 import {
   FATAL_MISSING_PROVIDER,
   FATAL_NOT_ENABLED_IN_PROVIDER,
@@ -31,12 +32,17 @@ export function initializeProvider(provider: Provider, dispatch: any) {
   // provider.enable exists for metamask and other modern dapp wallets and must be called, see:
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   if (provider.enable) {
+    dispatch(waitForWallet())
     provider
       .enable()
-      .then(() => dispatch(providerReady()))
-      .catch(() =>
+      .then(() => {
+        dispatch(dismissWalletCheck())
+        dispatch(providerReady())
+      })
+      .catch(() => {
+        dispatch(dismissWalletCheck())
         dispatch(setError(Application.Fatal(FATAL_NOT_ENABLED_IN_PROVIDER)))
-      )
+      })
   } else if (provider.isUnlock) {
     // This initialization is handled on receipt of GOT_ENCRYPTED_PRIVATE_KEY_PAYLOAD
     return
