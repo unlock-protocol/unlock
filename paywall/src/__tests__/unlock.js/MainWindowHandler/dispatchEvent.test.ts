b322fef77823f0ce91b370f05b8bb4f78f9627f0
Julien Genestoux <julien.genestoux@gmail.com>
2019-08-20 17:19:43 -0400

    removed the type config element as there is only one single type

--- paywall/src/__tests__/unlock.js/MainWindowHandler/dispatchEvent.test.ts
+++ paywall/src/__tests__/unlock.js/MainWindowHandler/dispatchEvent.test.ts
@@ -1,19 +1,9 @@
 import FakeWindow from '../../test-helpers/fakeWindowHelpers'
-import { PaywallConfig } from '../../../unlockTypes'
 import IframeHandler from '../../../unlock.js/IframeHandler'
 import MainWindowHandler from '../../../unlock.js/MainWindowHandler'
 
 describe('MainWindowHandler - dispatchEvent', () => {
   let fakeWindow: FakeWindow
-  const config: PaywallConfig = {
-    locks: {},
-    callToAction: {
-      default: '',
-      pending: '',
-      expired: '',
-      confirmed: '',
-    },
-  }
 
   function getMainWindowHandler() {
     const iframes = new IframeHandler(
@@ -22,7 +12,7 @@ describe('MainWindowHandler - dispatchEvent', () => {
       'http://u',
       'http://v'
     )
-    return new MainWindowHandler(fakeWindow, iframes, config)
+    return new MainWindowHandler(fakeWindow, iframes)
   }
 
   beforeEach(() => {
--- paywall/src/__tests__/unlock.js/MainWindowHandler/hideAccountIframe.test.ts
+++ paywall/src/__tests__/unlock.js/MainWindowHandler/hideAccountIframe.test.ts
@@ -1,25 +1,15 @@
 import FakeWindow from '../../test-helpers/fakeWindowHelpers'
-import { PaywallConfig } from '../../../unlockTypes'
 import IframeHandler from '../../../unlock.js/IframeHandler'
 import MainWindowHandler from '../../../unlock.js/MainWindowHandler'
 
 describe('MainWindowHandler - hideAccountIframe', () => {
   let fakeWindow: FakeWindow
   let iframes: IframeHandler
-  const config: PaywallConfig = {
-    locks: {},
-    callToAction: {
-      default: '',
-      pending: '',
-      expired: '',
-      confirmed: '',
-    },
-  }
 
   function getMainWindowHandler() {
     // iframe URLs are unused in this test
     iframes = new IframeHandler(fakeWindow, 'http://t', 'http://u', 'http://v')
-    return new MainWindowHandler(fakeWindow, iframes, config)
+    return new MainWindowHandler(fakeWindow, iframes)
   }
 
   beforeEach(() => {
