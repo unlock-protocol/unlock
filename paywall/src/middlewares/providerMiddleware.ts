f80aefb4fa85b1099cec54ff0f36f39bbc7789d9
Christoph Nascone <chris@unlock-protocol.com>
2019-08-20 14:52:46 -0400

    Paywall shows the wallet check overlay when we enable provider

--- paywall/src/middlewares/providerMiddleware.ts
+++ paywall/src/middlewares/providerMiddleware.ts
@@ -4,6 +4,7 @@ import {
   FATAL_MISSING_PROVIDER,
   FATAL_NOT_ENABLED_IN_PROVIDER,
 } from '../errors'
+import { waitForWallet, dismissWalletCheck } from '../actions/walletStatus'
 
 interface Action {
   type: string
@@ -12,7 +13,29 @@ interface Action {
 
 type dispatcher = (action: Action) => void
 
-function initializeProvider(
+export function delayedDispatch(
+  dispatch: dispatcher,
+  action: () => { type: string },
+  time: number
+) {
+  return setTimeout(() => dispatch(action()), time)
+}
+
+// Returns true if provider was successfully enabled, false otherwise
+export async function enableProvider(provider: { enable?: () => any }) {
+  if (provider.enable) {
+    try {
+      await provider.enable()
+      return true
+    } catch (_) {
+      return false
+    }
+  }
+  // provider.enable doesn't exist, so provider is de facto enabled
+  return true
+}
+
+export async function initializeProvider(
   provider: { enable?: () => any },
   dispatch: dispatcher
 ) {
@@ -23,15 +46,17 @@ function initializeProvider(
 
   // provider.enable exists for metamask and other modern dapp wallets and must be called, see:
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
-  if (provider.enable) {
-    provider
-      .enable()
-      .then(() => dispatch(providerReady()))
-      .catch(() => dispatch(setError(FATAL_NOT_ENABLED_IN_PROVIDER)))
-  } else {
-    // Default case, provider doesn't have an enable method, so it must already be ready
+  const timeout = delayedDispatch(dispatch, waitForWallet, 750)
+  const couldEnable = await enableProvider(provider)
+
+  if (couldEnable) {
     dispatch(providerReady())
+  } else {
+    dispatch(setError(FATAL_NOT_ENABLED_IN_PROVIDER))
   }
+
+  clearTimeout(timeout)
+  dispatch(dismissWalletCheck())
 }
 
 const providerMiddleware = (config: any) => {
