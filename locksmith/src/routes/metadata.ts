b73c48f0151c45f91706b241873e275d22cd1dc4
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-08-21 11:31:56 -0400

    Adds the ability to persist Lock metadata (#4472)
    * Adds the ability to persist Lock metadata
    
    In effort to support Lock creators providing their own default Lock token metadata

--- locksmith/src/routes/metadata.ts
+++ locksmith/src/routes/metadata.ts
@@ -1,8 +1,22 @@
 import express from 'express'
+import signatureValidationMiddleware from '../middlewares/signatureValidationMiddleware'
 
 var router = express.Router()
+
+let metaDataConfiguration = {
+  name: 'LockMetaData',
+  required: ['address', 'name', 'description', 'owner', 'image'],
+  signee: 'owner',
+}
+
+router.put(
+  '/:lockAddress',
+  signatureValidationMiddleware.generateProcessor(metaDataConfiguration)
+)
+
 var metaDataController = require('../controllers/metadataController')
 
 router.get('/:lockAddress/:keyId', metaDataController.data)
+router.put('/:lockAddress', metaDataController.updateDefaults)
 
 module.exports = router
