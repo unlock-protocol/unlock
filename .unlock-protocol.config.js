da7c1ca19a843e7ceb1b4a9925a79a48823891ce
Julien Genestoux <julien.genestoux@gmail.com>
2019-08-23 10:46:59 -0400

    adding .unlock-protocol.config.js (#4496)

---
+++ .unlock-protocol.config.js
@@ -0,0 +1,1 @@
+{"icon":"https://donate.unlock-protocol.com/unlock-word-mark-red.svg","callToAction":{"default":"Support our work by becoming a member of the Unlock community. It’s free, you’ll get your own Non Fungible Token and you can then leave comments on our blog!"},"locks":{"0xB0114bbDCe17e0AF91b2Be32916a1e236cf6034F":{"name":"Unlock Blog Members"}}}
