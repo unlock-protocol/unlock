4c2ba8c7922b8d75dce2321fe1a948a6f5409733
Unlock Deployer <ops@unlock-protocol.com>
2019-08-29 20:30:23 +0000

    Automated deploy as of commit:2ce9bd28f7c9b3c957e37982f7bd5c3a1910ae11

---
+++ locksmith/src/utils/keyData.ts
@@ -0,0 +1,40 @@
+import { ethers } from 'ethers'
+
+export default class KeyData {
+  provider: ethers.providers.JsonRpcProvider
+
+  constructor(provider: string) {
+    this.provider = new ethers.providers.JsonRpcProvider(provider)
+  }
+
+  async get(lockAddress: string, tokenId: string) {
+    const contract = this.genContract(lockAddress)
+
+    try {
+      let owner = await contract.ownerOf(parseInt(tokenId))
+
+      if (owner) {
+        let expiration = await contract.keyExpirationTimestampFor(owner)
+        return {
+          owner: owner,
+          expiration: expiration.toNumber(),
+        }
+      } else {
+        return {}
+      }
+    } catch (e) {
+      return {}
+    }
+  }
+
+  genContract(lockAddress: string) {
+    return new ethers.Contract(
+      lockAddress,
+      [
+        'function ownerOf(uint256 _tokenId) constant view returns (address)',
+        'function keyExpirationTimestampFor(address _owner) constant view returns (uint256 timestamp)',
+      ],
+      this.provider
+    )
+  }
+}
