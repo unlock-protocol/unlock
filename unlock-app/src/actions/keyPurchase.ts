aeb96b36a1daf8fbe7902c4cb7055fe60d2f3ebc
Christoph Nascone <chris@unlock-protocol.com>
2019-08-29 15:29:15 -0400

    Bucket of fees goes into state instead of computed price

--- unlock-app/src/actions/keyPurchase.ts
+++ unlock-app/src/actions/keyPurchase.ts
@@ -2,15 +2,19 @@ export const ADD_TO_CART = 'keyPurchase/ADD_TO_CART'
 export const UPDATE_PRICE = 'keyPurchase/UPDATE_PRICE'
 export const DISMISS_PURCHASE_MODAL = 'keyPurchase/DISMISS_MODAL'
 
+interface Fees {
+  [name: string]: number
+}
+
 export const addToCart = ({ lock, tip }: any) => ({
   type: ADD_TO_CART,
   lock,
   tip,
 })
 
-export const updatePrice = (price: number) => ({
+export const updatePrice = (fees: Fees) => ({
   type: UPDATE_PRICE,
-  price,
+  fees,
 })
 
 export const dismissPurchaseModal = () => ({
