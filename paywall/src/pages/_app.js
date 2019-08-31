a0bab4b19835d462432524c2075b0b58c2db2b4d
Julien Genestoux <julien.genestoux@gmail.com>
2019-08-30 20:51:25 -0400

    removed providerMiddleware (#4567)

--- paywall/src/pages/_app.js
+++ paywall/src/pages/_app.js
@@ -15,7 +15,6 @@ import walletMiddleware from '../middlewares/walletMiddleware'
 import interWindowCommunicationMiddleware from '../middlewares/interWindowCommunicationMiddleware'
 import { WindowContext } from '../hooks/browser/useWindow'
 import storageMiddleware from '../middlewares/storageMiddleware'
-import providerMiddleware from '../middlewares/providerMiddleware'
 
 const config = configure()
 
@@ -24,7 +23,6 @@ const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__'
 function getOrCreateStore(initialState, history) {
   const middlewares = [
     interWindowCommunicationMiddleware(global),
-    providerMiddleware(config),
     web3Middleware(config),
     storageMiddleware(config),
     walletMiddleware(config),
