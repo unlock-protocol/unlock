50fa9d87f2d1992a4c69898d52453cbc1476ec62
Julien Genestoux <julien.genestoux@gmail.com>
2019-08-31 20:11:45 -0400

    removed interWindowCommunicationMiddleware (#4572)

--- paywall/src/pages/_app.js
+++ paywall/src/pages/_app.js
@@ -12,7 +12,6 @@ import { ConfigContext } from '../utils/withConfig'
 // Middlewares
 import web3Middleware from '../middlewares/web3Middleware'
 import walletMiddleware from '../middlewares/walletMiddleware'
-import interWindowCommunicationMiddleware from '../middlewares/interWindowCommunicationMiddleware'
 import { WindowContext } from '../hooks/browser/useWindow'
 
 const config = configure()
@@ -21,7 +20,6 @@ const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__'
 
 function getOrCreateStore(initialState, history) {
   const middlewares = [
-    interWindowCommunicationMiddleware(global),
     web3Middleware(config),
     walletMiddleware(config),
   ]
