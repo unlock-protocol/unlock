8b82494629f148f1971d57378cb70d5910a3d2ca
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-16 00:53:19 +0200

    removed the scroll events and related functions since we do not limit scrolling anymore (#4731)

--- paywall/src/paywall-builder/constants.js
+++ paywall/src/paywall-builder/constants.js
@@ -1,6 +1,5 @@
 export const POST_MESSAGE_LOCKED = 'locked'
 export const POST_MESSAGE_UNLOCKED = 'unlocked'
-export const POST_MESSAGE_SCROLL_POSITION = 'scrollPosition'
 export const POST_MESSAGE_REDIRECT = 'redirect'
 export const POST_MESSAGE_GET_OPTIMISTIC = 'optimistic'
 export const POST_MESSAGE_GET_PESSIMISTIC = 'pessimistic'
