98fa432310b8f4d4e58d629ca3a112f0f1fddc98
Hardly Difficult <hardlydiff@gmail.com>
2019-09-22 10:05:43 -0700

    Upgrade to Solidity 0.5.11 (#4803)

--- smart-contracts/contracts/Unlock.sol
+++ smart-contracts/contracts/Unlock.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.10;
+pragma solidity 0.5.11;
 
 /**
  * @title The Unlock contract
--- smart-contracts/contracts/mixins/MixinRefunds.sol
+++ smart-contracts/contracts/mixins/MixinRefunds.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.10;
+pragma solidity 0.5.11;
 
 import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
 import 'openzeppelin-eth/contracts/math/SafeMath.sol';
