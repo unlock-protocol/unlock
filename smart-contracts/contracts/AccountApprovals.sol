c7166827c7a7700d13bc289292f6df7a68c3ca53
NickCuso <hardlydiff@gmail.com>
2019-09-22 13:03:39 -0700

    Initial AccountApprovals implementation

---
+++ smart-contracts/contracts/AccountApprovals.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.5.11;
+
+import 'openzeppelin-eth/contracts/access/roles/WhitelistedRole.sol';
+
+
+contract AccountApprovals is
+  WhitelistedRole
+{
+  mapping(address => bytes) accountData;
+
+  event Approve(address operator, address account, bytes data);
+
+  function approve(
+    address _account,
+    bytes calldata _data
+  ) external
+    onlyWhitelisted
+  {
+    accountData[_account] = _data;
+    emit Approve(msg.sender, _account, _data);
+  }
+}
