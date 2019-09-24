696b2f35cc49fcf5d7d083a5106ffdddc96d389c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-09-24 17:29:31 -0400

    Use lazy styles in unlock-web (#4846)
    * Use lazy styles in unlock-web module
    
    * Update CHANGELOG

--- paywall/src/unlock.js/module.ts
+++ paywall/src/unlock.js/module.ts
@@ -1,8 +1,14 @@
 import startupWhenReady from './startup'
-import '../static/iframe.css'
 import constants from './constants'
 
+// Using require here so that we can use CSS modules without creating types for each declaration
+const styles = require('../static/iframe.css')
+
 export default function startup() {
+  // Styles will be deferred until here, where they will load. This
+  // prevents issues with `document` being undefined in SSR
+  styles.use()
+
   const env = process.env.UNLOCK_ENV || 'dev'
   const startupConstants = constants[env]
 
