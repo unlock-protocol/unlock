c26042d2af1e4faefa07ea1a8728a1eb49c00df8
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-25 15:55:52 -0400

    fixing the dashboard to display the actual ERC20 symbol

---
+++ unlock-app/src/__tests__/utils/lock.js
@@ -0,0 +1,32 @@
+import { currencySymbol } from '../../utils/lock'
+
+const defaultERC20 = {
+  address: '0xerc20',
+  name: 'TOKEN',
+}
+
+describe('currencySymbol', () => {
+  it('should return the locks currencySymbol if it is set', async () => {
+    expect.assertions(1)
+    const lock = {
+      currencySymbol: 'cDAI',
+    }
+    expect(currencySymbol(lock, defaultERC20)).toBe('cDAI')
+  })
+
+  it('should return the defaultERC20 symbol if the lock if for that currency', async () => {
+    expect.assertions(1)
+    const lock = {
+      currencyContractAddress: defaultERC20.address,
+    }
+    expect(currencySymbol(lock, defaultERC20)).toBe('TOKEN')
+  })
+
+  it('should return ERC20 if the lock is for another currency', () => {
+    expect.assertions(1)
+    const lock = {
+      currencyContractAddress: '0x456',
+    }
+    expect(currencySymbol(lock, defaultERC20)).toBe('ERC20')
+  })
+})
