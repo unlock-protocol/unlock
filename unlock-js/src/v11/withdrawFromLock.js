5af426cabf681afb3176b9770b139cd3adb9cf2b
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-25 02:45:47 +0200

    more cleanups for TODOs inside of unlock-js (#4848)

--- unlock-js/src/v11/withdrawFromLock.js
+++ unlock-js/src/v11/withdrawFromLock.js
@@ -6,16 +6,17 @@ import Errors from '../errors'
 /**
  * Triggers a transaction to withdraw funds from the lock and assign them to the owner.
  * @param {PropTypes.address} lock
- * @param {PropTypes.address} amount
- * @param {Function} callback TODO: implement...
+ * @param {PropTypes.string} amount
+ * @param {PropTypes.string} decimals
  */
-export default async function(lockAddress, amount = '0') {
+export default async function(lockAddress, amount = '0', decimals = 18) {
   const lockContract = await this.getLockContract(lockAddress)
   let transactionPromise
-  const weiAmount = utils.toWei(amount) // TODO: fix for ERC20 locks for which the amount to withdraw may be of a different unit!
+
+  const actualAmount = utils.toDecimal(amount, decimals)
 
   try {
-    transactionPromise = lockContract['withdraw(uint256)'](weiAmount, {
+    transactionPromise = lockContract['withdraw(uint256)'](actualAmount, {
       gasLimit: GAS_AMOUNTS.withdraw, // overrides default value for transaction gas price
     })
     const ret = await this._handleMethodCall(
