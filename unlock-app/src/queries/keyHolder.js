1810e191717ef4dd17d6992e627d62a13d3ee34e
Christoph Nascone <chris@unlock-protocol.com>
2019-09-25 12:04:04 -0400

    address_not -> address

--- unlock-app/src/queries/keyHolder.js
+++ unlock-app/src/queries/keyHolder.js
@@ -3,7 +3,7 @@ import { gql } from 'apollo-boost'
 export default function keyHolderQuery() {
   return gql`
     query KeyHolder($address: String!) {
-      keyHolders(where: { address_not: $address }) {
+      keyHolders(where: { address: $address }) {
         id
         address
         keys {
