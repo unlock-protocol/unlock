41af47a679148bccc5e688a6f475ca76117f4576
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-26 16:48:07 -0400

    Manual deploy as of commit:5a352335526379750b3957de2125de557fc9cb9d

--- unlock-app/src/components/interface/modal-templates/index.ts
+++ unlock-app/src/components/interface/modal-templates/index.ts
@@ -1,6 +1,4 @@
 import WalletCheck from './WalletCheck'
-import PasswordPrompt from './PasswordPrompt'
-import ResetPasswordPrompt from './ResetPasswordPrompt'
 import * as styles from './styles'
 
-export { WalletCheck, PasswordPrompt, ResetPasswordPrompt, styles }
+export { WalletCheck, styles }
