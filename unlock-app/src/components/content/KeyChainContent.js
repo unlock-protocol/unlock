9280880d7c487d7b3077722032ccca76632d865a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-09-27 14:10:14 -0400

    Update styling

--- unlock-app/src/components/content/KeyChainContent.js
+++ unlock-app/src/components/content/KeyChainContent.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import Head from 'next/head'
 import 'cross-fetch/polyfill'
 import { useQuery } from '@apollo/react-hooks'
+import styled from 'styled-components'
 import BrowserOnly from '../helpers/BrowserOnly'
 import UnlockPropTypes from '../../propTypes'
 import DeveloperOverlay from '../developer/DeveloperOverlay'
@@ -11,11 +12,14 @@ import Account from '../interface/Account'
 import { pageTitle } from '../../constants'
 import LogInSignUp from '../interface/LogInSignUp'
 import keyHolderQuery from '../../queries/keyHolder'
+import {
+  expirationAsDate,
+  durationsAsTextFromSeconds,
+} from '../../utils/durations'
 
 export const KeyChainContent = ({ account, network, router }) => {
   const { hash } = router.location
   const emailAddress = hash.slice(1) // trim off leading '#'
-
   return (
     <Layout title="Key Chain">
       <Head>
@@ -55,28 +59,129 @@ export const mapStateToProps = ({ account, network, router }) => {
   }
 }
 
+const formatPrice = price => {
+  return parseInt(price) / Math.pow(10, 18)
+}
+
 const keyDetails = address => {
   const { loading, error, data } = useQuery(keyHolderQuery(), {
     variables: { address },
   })
 
-  
-
   if (loading) return <p>Loading...</p>
-  if (error) return( <p>Error :c</p>)
-  console.log(data.keyHolders)
+  if (error) return <p>Error :c</p>
 
-  return data.keyHolders[0].keys
-    .map(ownedKeys => ownedKeys.lock)
-    .map(({ id, name, expirationDuration, price }) => {
-      return (
-        <div key={id}>
-          <p>{name}</p>
-          <p>{expirationDuration}</p>
-          <p>{price}</p>
-        </div>
-      )
-    })
+  if (data.keyHolders.length == 0) {
+    return <div></div>
+  }
+
+  return (
+    <Container>
+      {data.keyHolders[0].keys.map(ownedKeys => {
+        return (
+          <Box key={ownedKeys.lock.id}>
+            <LockName>{ownedKeys.lock.name}</LockName>
+            <LockExpirationDuration>
+              {durationsAsTextFromSeconds(ownedKeys.lock.expirationDuration)}
+            </LockExpirationDuration>
+
+            <ValidUntil>Valid Until</ValidUntil>
+            <KeyExpiration>
+              {expirationAsDate(ownedKeys.expiration)}
+            </KeyExpiration>
+
+            <KeyPrice>Ξ {formatPrice(ownedKeys.lock.price)}</KeyPrice>
+          </Box>
+        )
+      })}
+    </Container>
+  )
 }
 
 export default connect(mapStateToProps)(KeyChainContent)
+
+const Box = styled.div`
+  border: thin #dddddd solid;
+  width: 212px;
+  float: left;
+  margin-right: 32px;
+  &:hover {
+    border: thin #aaaaaa solid;
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const LockName = styled.div`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 19px;
+  /* or 127% */
+
+  display: flex;
+  align-items: center;
+  color: #4d8be8;
+  padding: 16px;
+`
+
+const LockExpirationDuration = styled.div`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 19px;
+  /* identical to box height, or 127% */
+
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 18px;
+
+  /* Grey 4 */
+
+  color: #333333;
+`
+
+const ValidUntil = styled.div`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 8px;
+  line-height: 10px;
+  /* identical to box height */
+
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #a6a6a6;
+  padding-left: 16px;
+`
+
+const KeyExpiration = styled.div`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #333333;
+  padding-left: 16px;
+  padding-bottom: 10px;
+`
+
+const KeyPrice = styled.div`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  /* identical to box height, or 125% */
+
+  color: #333333;
+  padding-left: 16px;
+  padding-bottom: 40px;
+`
--- unlock-app/src/stories/keyChain/keyChain.stories.js
+++ unlock-app/src/stories/keyChain/keyChain.stories.js
@@ -1,8 +1,8 @@
 import { Provider } from 'react-redux'
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import KeyChainContent from '../../components/content/KeyChainContent'
 import { MockedProvider } from '@apollo/react-testing'
+import KeyChainContent from '../../components/content/KeyChainContent'
 import createUnlockStore from '../../createUnlockStore'
 import { ConfigContext } from '../../utils/withConfig'
 import configure from '../../config'
@@ -73,21 +73,22 @@ const mocks = [
       data: {
         keyHolders: [
           {
-            __typename: "KeyHolder",
-            address: "0xaaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2",
+            __typename: 'KeyHolder',
+            address: '0xaaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2',
             id: '0xaaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2',
             keys: [
               {
                 __typename: 'Key',
                 id: '0x80bc6d2870bb72cb3e37b648c160da20733386f7-1',
                 keyId: '1',
+                expiration: '132546546',
                 lock: {
-                  __typename: "Lock",
-                  address: "0x80bc6d2870bb72cb3e37b648c160da20733386f7",
-                  expirationDuration: "300",
-                  name: "ERC20 paywall lock",
-                  price: "1000000000000000000",
-                  tokenAddress: "0x591ad9066603f5499d12ff4bc207e2f577448c46",
+                  __typename: 'Lock',
+                  address: '0x80bc6d2870bb72cb3e37b648c160da20733386f7',
+                  expirationDuration: '300',
+                  name: 'ERC20 paywall lock',
+                  price: '1000000000000000000',
+                  tokenAddress: '0x591ad9066603f5499d12ff4bc207e2f577448c46',
                 },
               },
             ],
@@ -107,18 +108,16 @@ const config = configure({
 storiesOf('KeyChainContent', module)
   .addDecorator(getStory => (
     <ConfigProvider value={config}>
-      <MockedProvider mocks={mocks}>
-        {getStory()}
-      </MockedProvider>
+      <MockedProvider mocks={mocks}>{getStory()}</MockedProvider>
     </ConfigProvider>
   ))
-  // .add('the key chain, with keys', () => {
-  //   return (
-  //     <Provider store={keyHolderStore}>
-  //       <KeyChainContent />
-  //     </Provider>
-  //   )
-  // })
+  .add('the key chain, with keys', () => {
+    return (
+      <Provider store={keyHolderStore}>
+        <KeyChainContent />
+      </Provider>
+    )
+  })
   .add('the key chain, no keys', () => {
     return (
       <Provider store={store}>
