9432e0cc7edf7a52938f8154e5768f5b0d9605ee
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-01 16:41:46 -0400

    Update the event email template (#4916)
    * Include event name in subject line
    
    * Test that subject is set properly
    
    * Wording to match client request
    
    * Fix lint issues
    
    * Add hardcoded message for ETHWaterloo, test render of optional text
    
    * More lint fixes
    
    * Update wording to be more general (for user accounts and crypto)
    
    * We don't have html right now
    
    * Final wording update

--- wedlocks/src/__tests__/templates/confirmEvent.test.js
+++ wedlocks/src/__tests__/templates/confirmEvent.test.js
@@ -3,7 +3,11 @@ import confirmEvent from '../../templates/confirmEvent'
 describe('confirmEvent', () => {
   it('should have the right subject', () => {
     expect.assertions(1)
-    expect(confirmEvent.subject()).toBe('You are attending an event!')
+    expect(
+      confirmEvent.subject({
+        eventName: 'The Tupperware Party',
+      })
+    ).toBe("You've got your ticket for The Tupperware Party!")
   })
 
   it('should have the right text', () => {
@@ -20,11 +24,36 @@ describe('confirmEvent', () => {
 
 This is just a reminder that you are attending the event Unlock's launch party! on June 6th 2019!
 
-When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
+When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click the following link and open it using your crypto-enabled web browser or Unlock user account.
 
-https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4.
+https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4
 
-You can also show the QR code attached to this email.
+Enjoy!
+
+The Unlock team
+`
+    )
+  })
+
+  it('should have the right text for ETHWaterloo', () => {
+    expect.assertions(1)
+    expect(
+      confirmEvent.text({
+        eventName: 'ETHWaterloo',
+        eventDate: 'June 6th 2019',
+        ticketLink:
+          'https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4',
+      })
+    ).toBe(
+      `Hello,
+
+This is just a reminder that you are attending the event ETHWaterloo on June 6th 2019!
+
+After you check in to ETHWaterloo we'll refund your stake in DAI by the end of the weekend. If you are an Unlock account user, you'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
+
+When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click the following link and open it using your crypto-enabled web browser or Unlock user account.
+
+https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4
 
 Enjoy!
 
