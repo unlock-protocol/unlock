d2c98c69d08b26fdb32b4c41aee73a522a775b0a
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-01 19:03:55 -0400

    Update wording for EthWaterloo and dispatch correctly (#4919)

--- wedlocks/src/__tests__/templates/confirmEvent.test.js
+++ wedlocks/src/__tests__/templates/confirmEvent.test.js
@@ -49,7 +49,7 @@ The Unlock team
 
 This is just a reminder that you are attending the event ETHWaterloo on June 6th 2019!
 
-After you check in to ETHWaterloo we'll refund your stake in DAI by the end of the weekend. If you are an Unlock account user, you'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
+After you check in to ETHWaterloo we'll refund your stake in DAI. If you are an Unlock account user, you'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
 
 When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click the following link and open it using your crypto-enabled web browser or Unlock user account.
 
