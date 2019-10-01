89603070389c4ce5672c4bf26aa2e9c99596c5e7
Christoph Nascone <chris@unlock-protocol.com>
2019-10-01 15:08:27 -0400

    Add hardcoded message for ETHWaterloo, test render of optional text

--- wedlocks/src/__tests__/templates/confirmEvent.test.js
+++ wedlocks/src/__tests__/templates/confirmEvent.test.js
@@ -36,4 +36,33 @@ The Unlock team
 `
     )
   })
+
+  it('should have the right text for ETHWaterloo', () => {
+    expect.assertions(1)
+    expect(
+      confirmEvent.text({
+        eventName: "ETHWaterloo",
+        eventDate: 'June 6th 2019',
+        ticketLink:
+          'https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4',
+      })
+    ).toBe(
+      `Hello,
+
+This is just a reminder that you are attending the event ETHWaterloo on June 6th 2019!
+
+After you check in to ETHWaterloo, we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
+
+When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
+
+https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4.
+
+You can also show the QR code attached to this email.
+
+Enjoy!
+
+The Unlock team
+`
+    )
+  })
 })
--- wedlocks/src/templates/confirmEvent.js
+++ wedlocks/src/templates/confirmEvent.js
@@ -1,9 +1,13 @@
+const ethWaterlooMessage = `
+
+After you check in to ETHWaterloo, we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.`
+
 export default {
   subject: params => `You've got your ticket for ${params.eventName}!`,
   text: params =>
     `Hello,
 
-This is just a reminder that you are attending the event ${params.eventName} on ${params.eventDate}!
+This is just a reminder that you are attending the event ${params.eventName} on ${params.eventDate}!${params.eventName === 'ETHWaterloo' ? ethWaterlooMessage : ''}
 
 When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
 
