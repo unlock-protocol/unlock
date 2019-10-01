46ca10664c080cf1a073ef562f30fd64dc46572c
Christoph Nascone <chris@unlock-protocol.com>
2019-10-01 15:57:55 -0400

    Update wording to be more general (for user accounts and crypto)

--- wedlocks/src/__tests__/templates/confirmEvent.test.js
+++ wedlocks/src/__tests__/templates/confirmEvent.test.js
@@ -24,11 +24,7 @@ describe('confirmEvent', () => {
 
 This is just a reminder that you are attending the event Unlock's launch party! on June 6th 2019!
 
-When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
-
-https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4.
-
-You can also show the QR code attached to this email.
+When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click <a href="https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4">here</a> and open it using your crypto-enabled web browser or Unlock user account.
 
 Enjoy!
 
@@ -51,13 +47,9 @@ The Unlock team
 
 This is just a reminder that you are attending the event ETHWaterloo on June 6th 2019!
 
-After you check in to ETHWaterloo, we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
+If you purchased a ticket using an Unlock user account, after you check in to ETHWaterloo we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.
 
-When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
-
-https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4.
-
-You can also show the QR code attached to this email.
+When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click <a href="https://tickets.unlock-protocol.com/event/0x5865Ff2CBd045Ef1cfE19739df19E83B32b783b4">here</a> and open it using your crypto-enabled web browser or Unlock user account.
 
 Enjoy!
 
--- wedlocks/src/templates/confirmEvent.js
+++ wedlocks/src/templates/confirmEvent.js
@@ -1,6 +1,6 @@
 const ethWaterlooMessage = `
 
-After you check in to ETHWaterloo, we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.`
+If you purchased a ticket using an Unlock user account, after you check in to ETHWaterloo we'll refund your stake in DAI by the end of the weekend. You'll be able to access those funds once you eject your Unlock account into a crypto wallet. We'll provide more details on that process soon! Note that this event ticket is non-transferable.`
 
 export default {
   subject: params => `You've got your ticket for ${params.eventName}!`,
@@ -13,11 +13,9 @@ This is just a reminder that you are attending the event ${
       params.eventName === 'ETHWaterloo' ? ethWaterlooMessage : ''
     }
 
-When you're asked for your ticket at the door, just click on the following link and open it using your crypto enabled web browser:
-
-${params.ticketLink}.
-
-You can also show the QR code attached to this email.
+When you're asked for your ticket at the door, simply show the QR code attached to this email. Alternately, you can click <a href="${
+      params.ticketLink
+    }">here</a> and open it using your crypto-enabled web browser or Unlock user account.
 
 Enjoy!
 
