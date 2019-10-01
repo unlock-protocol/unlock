931d68565f9670d50d93bb64163d9af801cfb34b
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-10-01 15:08:00 -0400

    Adds metadata operation allowing users to persist their own data associated witha given lock

---
+++ locksmith/__tests__/operations/userMetadataOperations.test.ts
@@ -0,0 +1,60 @@
+// import * as Sequelize from 'sequelize'
+import addMetadata from '../../src/operations/userMetadataOperations'
+
+const models = require('../../src/models')
+
+let UserMetadata: any = models.UserMetadata
+
+describe('userMetadataOperations', () => {
+  afterAll(async () => {
+    await UserMetadata.truncate()
+  })
+
+  describe('addMetadata', () => {
+    describe('adding new metadata', () => {
+      it('should return true', async () => {
+        expect.assertions(1)
+        let metadataUpdate = addMetadata({
+          tokenAddress: '0x720b9F6D572C3CA4689E93CF029B40569c6b40e8',
+          userAddress: '0xcFd35259E3A468E7bDF84a95bCddAc0B614A9212',
+          data: {
+            name: 'Bruce wayne',
+          },
+        })
+
+        let result = await metadataUpdate
+
+        expect(result[0]).toEqual(
+          expect.objectContaining({
+            data: { userMetadata: { name: 'Bruce wayne' } },
+            tokenAddress: '0x720b9F6D572C3CA4689E93CF029B40569c6b40e8',
+            userAddress: '0xcFd35259E3A468E7bDF84a95bCddAc0B614A9212',
+          })
+        )
+      })
+    })
+
+    describe('updating existing metadata', () => {
+      it('should return true', async () => {
+        expect.assertions(1)
+        let metadataUpdate = addMetadata({
+          tokenAddress: '0x720b9F6D572C3CA4689E93CF029B40569c6b40e8',
+          userAddress: '0xcFd35259E3A468E7bDF84a95bCddAc0B614A9212',
+          data: {
+            name: 'Clark Kent',
+          },
+        })
+
+        let result = await metadataUpdate
+
+        expect(result[0]).toEqual(
+          expect.objectContaining({
+            data: { userMetadata: { name: 'Clark Kent' } },
+            tokenAddress: '0x720b9F6D572C3CA4689E93CF029B40569c6b40e8',
+            userAddress: '0xcFd35259E3A468E7bDF84a95bCddAc0B614A9212',
+          })
+        )
+      })
+    })
+  })
+})
