ba6fbd10474365b0f3058b03d2d16aba8b687a82
Julien Genestoux <julien.genestoux@gmail.com>
2019-10-03 09:55:50 -0400

    getting erc20Address and decimals from the contract when purchasing a key (#4930)

--- unlock-js/src/v10/purchaseKey.js
+++ unlock-js/src/v10/purchaseKey.js
@@ -1,8 +1,8 @@
 import utils from '../utils'
-import { GAS_AMOUNTS } from '../constants'
+import { GAS_AMOUNTS, ZERO } from '../constants'
 import TransactionTypes from '../transactionTypes'
 import Errors from '../errors'
-import { approveTransfer } from '../erc20'
+import { approveTransfer, getErc20Decimals } from '../erc20'
 
 /**
  * Purchase key function. This implementation requires the following
@@ -12,26 +12,38 @@ import { approveTransfer } from '../erc20'
  * - {string} keyPrice
  * - {PropTypes.address} erc20Address
  * - {number} decimals
+ * @return {string} hash of the transaction
  */
 export default async function({
   lockAddress,
   owner,
   keyPrice,
   erc20Address,
-  decimals = 18,
+  decimals,
 }) {
   const lockContract = await this.getLockContract(lockAddress)
 
-  // TODO: if the erc20Address is missing, retrieve it from chain
-  // TODO: if the decimals is missing, retrieve it from chain
+  if (!erc20Address || erc20Address !== ZERO) {
+    erc20Address = await lockContract.tokenAddress()
+  }
+
+  // decimals could be 0!
+  if (decimals == null) {
+    // get the decimals from the ERC20 contract or default to 18
+    if (erc20Address && erc20Address !== ZERO) {
+      decimals = await getErc20Decimals(erc20Address, this.provider)
+    } else {
+      decimals = 18
+    }
+  }
+
   const actualAmount = utils.toDecimal(keyPrice, decimals)
 
   const purchaseForOptions = {
     gasLimit: GAS_AMOUNTS.purchaseFor,
   }
 
-  // first, we need to apprive the transfer
-  if (erc20Address) {
+  if (erc20Address && erc20Address !== ZERO) {
     await approveTransfer(
       erc20Address,
       lockAddress,
@@ -48,11 +60,10 @@ export default async function({
       owner,
       purchaseForOptions
     )
-    const ret = await this._handleMethodCall(
+    return await this._handleMethodCall(
       transactionPromise,
       TransactionTypes.KEY_PURCHASE
     )
-    return ret
   } catch (error) {
     this.emit('error', new Error(Errors.FAILED_TO_PURCHASE_KEY))
   }
