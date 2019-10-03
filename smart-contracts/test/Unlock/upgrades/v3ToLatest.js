4721a7076ead0e1cc88baae28971689d55a2446d
Nick Furfaro <nfurfaro33@gmail.com>
2019-10-03 10:27:26 -0600

    Add V3 upgrade test

---
+++ smart-contracts/test/Unlock/upgrades/v3ToLatest.js
@@ -0,0 +1,204 @@
+const Units = require('ethereumjs-units')
+const Web3Utils = require('web3-utils')
+const { TestHelper } = require('@openzeppelin/cli')
+const BigNumber = require('bignumber.js')
+const { ZWeb3, Contracts } = require('@openzeppelin/upgrades')
+
+ZWeb3.initialize(web3.currentProvider)
+const UnlockV3 = Contracts.getFromNodeModules('unlock-abi-0-1', '../../Unlock')
+const PublicLockV3 = require('../../../published-npm-modules/V1.0/PublicLock.json')
+
+const UnlockLatest = Contracts.getFromLocal('Unlock')
+const PublicLockLatest = Contracts.getFromLocal('PublicLock')
+const LatestVersionNumber = require('./latestVersion.js')
+
+let project, proxy, unlock
+
+contract('Unlock / upgrades', accounts => {
+  const unlockOwner = accounts[9]
+  const lockOwner = accounts[1]
+  const keyOwner = accounts[2]
+  const keyPrice = Units.convert('0.01', 'eth', 'wei')
+  let lockV3
+  let v0LockData
+
+  before(async () => {
+    project = await TestHelper({ from: unlockOwner })
+
+    // Deploy
+    UnlockV3.schema.contractName = 'UnlockV3'
+    proxy = await project.createProxy(UnlockV3, {
+      UnlockV3,
+      methodName: 'initialize',
+      methodArgs: [unlockOwner],
+    })
+
+    unlock = await UnlockV3.at(proxy.address)
+
+    // Create Lock
+    const lockTx = await unlock.methods
+      .createLock(
+        60 * 60 * 24, // expirationDuration 1 day
+        Web3Utils.padLeft(0, 40), // token address
+        keyPrice,
+        5 // maxNumberOfKeys
+        // 'upgradeTestLock' //lock name
+      )
+      .send({ from: lockOwner, gas: 6000000 })
+    // THIS API IS LIKELY TO BREAK BECAUSE IT ASSUMES SO MUCH
+    const evt = lockTx.events.NewLock
+    lockV3 = new web3.eth.Contract(
+      PublicLockV3.abi,
+      evt.returnValues.newLockAddress
+    )
+
+    // Buy Key
+    await lockV3.methods.purchaseFor(keyOwner).send({
+      value: keyPrice,
+      from: keyOwner,
+      gas: 4000000,
+    })
+
+    // Record sample lock data
+    v0LockData = await unlock.methods.locks(lockV3._address).call()
+  })
+
+  it('V3 Key is owned', async () => {
+    const id = await lockV3.methods.getTokenIdFor(keyOwner).call()
+    const bool = await lockV3.methods.isKeyOwner(id, keyOwner).call()
+    assert.equal(bool, true)
+  })
+
+  it('the versions V3 and latest version have different bytecode', async () => {
+    assert.notEqual(UnlockLatest.schema.bytecode, UnlockV3.schema.bytecode)
+  })
+
+  describe('latest', () => {
+    before(async () => {
+      project.upgradeProxy(proxy.address, UnlockLatest)
+      unlock = UnlockLatest.at(proxy.address)
+    })
+
+    describe('Lock created with UnlockV3 is still available', () => {
+      it('V3 Key is still owned', async () => {
+        const id = await lockV3.methods.getTokenIdFor(keyOwner).call()
+        const bool = await lockV3.methods.isKeyOwner(id, keyOwner).call()
+        assert.equal(bool, true)
+      })
+
+      it('New keys may still be purchased', async () => {
+        const tx = await lockV3.methods.purchaseFor(accounts[6]).send({
+          value: keyPrice,
+          from: accounts[6],
+          gas: 4000000,
+        })
+        assert.equal(tx.events.Transfer.event, 'Transfer')
+      })
+
+      it('Keys may still be transfered', async () => {
+        await lockV3.methods.purchaseFor(accounts[7]).send({
+          value: keyPrice,
+          from: accounts[7],
+          gas: 4000000,
+        })
+        const tx = await lockV3.methods
+          .transferFrom(
+            accounts[7],
+            accounts[8],
+            await lockV3.methods.getTokenIdFor(accounts[7]).call()
+          )
+          .send({
+            from: accounts[7],
+            gas: 4000000,
+          })
+        assert.equal(tx.events.Transfer.event, 'Transfer')
+      })
+
+      it('grossNetworkProduct remains', async () => {
+        const grossNetworkProduct = new BigNumber(
+          await unlock.methods.grossNetworkProduct().call()
+        )
+        assert.equal(
+          grossNetworkProduct.toFixed(),
+          new BigNumber(keyPrice).times(3).toFixed()
+        )
+      })
+
+      it('lock data should persist state between upgrades', async function() {
+        const resultsAfter = await unlock.methods.locks(lockV3._address).call()
+        assert.equal(resultsAfter.deployed, v0LockData.deployed)
+        assert.equal(
+          resultsAfter.yieldedDiscountTokens,
+          v0LockData.yieldedDiscountTokens
+        )
+      })
+    })
+
+    describe('Using latest version after an upgrade', () => {
+      let lockLatest
+
+      before(async () => {
+        // Create a new Lock
+        const lockTx = await unlock.methods
+          .createLock(
+            60 * 60 * 24, // expirationDuration 1 day
+            Web3Utils.padLeft(0, 40),
+            keyPrice,
+            5, // maxNumberOfKeys
+            'After-Upgrade Lock'
+          )
+          .send({
+            from: lockOwner,
+            gas: 6000000,
+          })
+        // THIS API IS LIKELY TO BREAK BECAUSE IT ASSUMES SO MUCH
+        const evt = lockTx.events.NewLock
+        lockLatest = await PublicLockLatest.at(evt.returnValues.newLockAddress)
+
+        // Buy Key
+        await lockLatest.methods
+          .purchase(keyOwner, web3.utils.padLeft(0, 40), [])
+          .send({
+            value: keyPrice,
+            from: keyOwner,
+            gas: 4000000,
+          })
+      })
+
+      it('grossNetworkProduct sums previous version purchases with new version purchases', async () => {
+        const grossNetworkProduct = new BigNumber(
+          await unlock.methods.grossNetworkProduct().call()
+        )
+        assert.equal(
+          grossNetworkProduct.toFixed(),
+          new BigNumber(keyPrice).times(4).toFixed()
+        )
+      })
+
+      it('Latest Key is owned', async () => {
+        const id = new BigNumber(
+          await lockLatest.methods.getTokenIdFor(keyOwner).call()
+        )
+        assert.equal(id.toFixed(), 1)
+      })
+
+      it('V3 Key is still owned', async () => {
+        const id = await lockV3.methods.getTokenIdFor(keyOwner).call()
+        const bool = await lockV3.methods.isKeyOwner(id, keyOwner).call()
+        assert.equal(bool, true)
+      })
+
+      it('Latest Unlock version is correct', async () => {
+        const unlockVersion = await unlock.methods.unlockVersion().call()
+        assert.equal(unlockVersion, LatestVersionNumber)
+      })
+
+      it('Latest publicLock version is correct', async () => {
+        const publicLockVersion = await lockLatest.methods
+          .publicLockVersion()
+          .call()
+        assert.equal(publicLockVersion, LatestVersionNumber)
+      })
+    })
+  })
+})
