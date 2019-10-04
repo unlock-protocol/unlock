8b9daae9941e4870beb41cc311aabcdfc1b4acd8
Christoph Nascone <chris@unlock-protocol.com>
2019-10-04 16:34:43 -0400

    Add an action to put a web3 method call on the bus

--- unlock-app/src/actions/web3call.ts
+++ unlock-app/src/actions/web3call.ts
@@ -1,14 +1,8 @@
-import { web3MethodCall, web3MethodResult } from '../windowTypes'
+import { web3MethodCall } from '../windowTypes'
 
 export const WEB3_CALL = 'web3.call'
-export const WEB3_RESULT = 'web3.result'
 
 export const web3Call = (payload: web3MethodCall) => ({
   type: WEB3_CALL,
-  payload,
-})
-
-export const web3Result = (payload: web3MethodResult) => ({
-  type: WEB3_RESULT,
   payload,
 })
