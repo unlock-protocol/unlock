28af00fb808ec48ad18251e45daee879f1c3cf37
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-05 20:02:18 -0400

    Fix determination of expensiveness (#4951)
    * Fix determination of expensiveness
    
    * Stories relied on incorrect behavior in absence of account
    
    * Get token balances when we reset on account change
    
    * Use helper instead of duplicating
    
    * Update comment to be more correct

--- paywall/src/__tests__/utils/locks.test.js
+++ paywall/src/__tests__/utils/locks.test.js
@@ -1,8 +1,48 @@
-import { currencySymbolForLock, isTooExpensiveForUser } from '../../utils/locks'
+import {
+  currencySymbolForLock,
+  isTooExpensiveForUser,
+  isTooExpensiveForUserByCurrency,
+  isEthLock,
+  isERC20Lock,
+} from '../../utils/locks'
 
 import configure from '../../config'
 
 const config = configure()
+
+const ethLock = {
+  keyPrice: '12',
+}
+const ethLockExpensive = {
+  keyPrice: '1200',
+}
+const ERC20Lock = {
+  currencyContractAddress: '0x123abc',
+  keyPrice: '2',
+}
+const ERC20LockExpensive = {
+  ...ERC20Lock,
+  keyPrice: '9001',
+}
+
+const accountWithoutBalance = {}
+const accountWithInvalidBalance = {
+  balance: {
+    eth: 'lmbo this string is not a float',
+  },
+}
+const accountWithValidBalance = {
+  balance: {
+    eth: '42',
+    '0x123abc': '9000',
+  },
+}
+const accountWithSameBalanceAsPrice = {
+  balance: {
+    eth: '12',
+    '0x123abc': '2',
+  },
+}
 
 describe('locks utilities', () => {
   describe('currencySymbolForLock', () => {
@@ -36,6 +76,142 @@ describe('locks utilities', () => {
         currencyContractAddress: '0xMyERC20',
       }
       expect(currencySymbolForLock(lock, config)).toBe('ERC20')
+    })
+  })
+
+  describe('isEthLock', () => {
+    it('should return true for locks that *do not* have a currencyContractAddress', () => {
+      expect.assertions(1)
+      expect(isEthLock(ethLock)).toBeTruthy()
+    })
+
+    it('should return false for locks that *do* have a currencyContractAddress', () => {
+      expect.assertions(1)
+      expect(isEthLock(ERC20Lock)).toBeFalsy()
+    })
+  })
+
+  describe('isERC20Lock', () => {
+    it('should return true for locks that *do* have a currencyContractAddress', () => {
+      expect.assertions(1)
+      expect(isERC20Lock(ERC20Lock)).toBeTruthy()
+    })
+
+    it('should return false for locks that *do not* have a currencyContractAddress', () => {
+      expect.assertions(1)
+      expect(isERC20Lock(ethLock)).toBeFalsy()
+    })
+  })
+
+  describe('isTooExpensiveForUserByCurrency', () => {
+    it('should be too expensive when there is no account', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(ethLock, undefined, 'eth')
+      ).toBeTruthy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20Lock,
+          undefined,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeTruthy()
+    })
+
+    it('should be too expensive when there is an account but no balance', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(ethLock, accountWithoutBalance, 'eth')
+      ).toBeTruthy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20Lock,
+          accountWithoutBalance,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeTruthy()
+    })
+
+    it('should be too expensive when there is a balance value, but it cannot be parsed', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ethLock,
+          accountWithInvalidBalance,
+          'eth'
+        )
+      ).toBeTruthy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20Lock,
+          accountWithInvalidBalance,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeTruthy()
+    })
+
+    it('should be too expensive when there is no keyPrice on a lock', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency({}, accountWithValidBalance, 'eth')
+      ).toBeTruthy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          {},
+          accountWithValidBalance,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeTruthy()
+    })
+
+    it('should be too expensive if keyPrice is bigger than balance', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ethLockExpensive,
+          accountWithValidBalance,
+          'eth'
+        )
+      ).toBeTruthy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20LockExpensive,
+          accountWithValidBalance,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeTruthy()
+    })
+
+    it('should be affordable if balance is the same as keyPrice', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ethLock,
+          accountWithSameBalanceAsPrice,
+          'eth'
+        )
+      ).toBeFalsy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20Lock,
+          accountWithSameBalanceAsPrice,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeFalsy()
+    })
+
+    it('should be affordable if balance is bigger than keyPrice', () => {
+      expect.assertions(2)
+      expect(
+        isTooExpensiveForUserByCurrency(ethLock, accountWithValidBalance, 'eth')
+      ).toBeFalsy()
+      expect(
+        isTooExpensiveForUserByCurrency(
+          ERC20Lock,
+          accountWithValidBalance,
+          ERC20Lock.currencyContractAddress
+        )
+      ).toBeFalsy()
     })
   })
 
