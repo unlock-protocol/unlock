6a1aba84c0a8a75d380fef8d8c1d1cef4f16aa23
NickCuso <hardlydiff@gmail.com>
2019-10-07 12:12:07 -0700

    Bump solc

--- smart-contracts/contracts/mixins/CloneFactory.sol
+++ smart-contracts/contracts/mixins/CloneFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.10;
+pragma solidity 0.5.11;
 
 // From https://github.com/optionality/clone-factory/blob/master/contracts/CloneFactory.sol
 // Updated to support Solidity 5
@@ -6,6 +6,7 @@ pragma solidity 0.5.10;
 contract CloneFactory {
   function createClone(address target) internal returns (address result) {
     bytes20 targetBytes = bytes20(target);
+    // solium-disable-next-line
     assembly {
       let clone := mload(0x40)
       mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
