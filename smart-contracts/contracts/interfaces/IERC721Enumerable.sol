e30feeaf36c7fdce52843c67795b0cb317cbdbbb
Hardly Difficult <hardlydiff@gmail.com>
2019-10-08 11:54:36 -0700

    Solidity 0.5.12 (#4971)

--- smart-contracts/contracts/interfaces/IERC721Enumerable.sol
+++ smart-contracts/contracts/interfaces/IERC721Enumerable.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.11;
+pragma solidity 0.5.12;
 
 /**
  * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
--- smart-contracts/contracts/mixins/MixinApproval.sol
+++ smart-contracts/contracts/mixins/MixinApproval.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.11;
+pragma solidity 0.5.12;
 
 import '../interfaces/IERC721.sol';
 import './MixinDisableAndDestroy.sol';
