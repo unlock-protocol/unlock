0f184a3f72ede83a9c0d19f6e762bca6c646c28a
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-10-09 17:29:52 -0400

    More shifting to TS (#4978)

--- paywall/src/components/helpers/Duration.tsx
+++ paywall/src/components/helpers/Duration.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {
   durationsAsTextFromSeconds,
   durationsAsArrayFromSeconds,
 } from '../../utils/durations'
 
+interface DurationProps {
+  seconds?: number
+  round?: boolean
+}
 /**
  * Component which shows a duration, rounded to next day if `round` is `true`
  * @param {*} seconds: time in seconds
  */
-export function Duration({ seconds, round }) {
-  if (seconds === null) {
+export function Duration({ seconds, round }: DurationProps) {
+  if (seconds === null || typeof seconds === 'undefined') {
     return <span> - </span>
   }
   if (round) {
     return <span>{durationsAsArrayFromSeconds(seconds)[0]}</span>
   }
   return <span>{durationsAsTextFromSeconds(seconds)}</span>
-}
-
-Duration.propTypes = {
-  seconds: PropTypes.number,
-  round: PropTypes.bool,
-}
-
-Duration.defaultProps = {
-  seconds: null,
-  round: false,
 }
 
 export default Duration
