e9b29f23415717ca78bacb129fe4524ee1d6966d
Christoph Nascone <chris@unlock-protocol.com>
2019-10-09 10:28:46 -0400

    Undelete this test

--- unlock-app/src/__tests__/actions/web3call.test.ts
+++ unlock-app/src/__tests__/actions/web3call.test.ts
@@ -1,5 +1,10 @@
 import { web3MethodCall } from '../../windowTypes'
-import { WEB3_CALL, web3Call } from '../../actions/web3call'
+import {
+  WEB3_CALL,
+  web3Call,
+  web3Result,
+  WEB3_RESULT,
+} from '../../actions/web3call'
 
 describe('web3Call action', () => {
   it('should create an action representing a JSON-RPC call', () => {
@@ -14,6 +19,42 @@ describe('web3Call action', () => {
 
     expect(web3Call(payload)).toEqual({
       type: WEB3_CALL,
+      payload,
+    })
+  })
+})
+
+describe('web3Result action', () => {
+  it('should create an action representing a successful result for a JSON-RPC call', () => {
+    expect.assertions(1)
+
+    const payload: web3MethodResult = {
+      id: 1337,
+      jsonrpc: '2.0',
+      result: {
+        id: 1337,
+        jsonrpc: '2.0',
+        result: 'the number twelve',
+      },
+    }
+
+    expect(web3Result(payload)).toEqual({
+      type: WEB3_RESULT,
+      payload,
+    })
+  })
+
+  it('should create an action representing an error result for a JSON-RPC call', () => {
+    expect.assertions(1)
+
+    const payload: web3MethodResult = {
+      id: 1337,
+      jsonrpc: '2.0',
+      error: 'bees in the datacenter',
+    }
+
+    expect(web3Result(payload)).toEqual({
+      type: WEB3_RESULT,
       payload,
     })
   })
