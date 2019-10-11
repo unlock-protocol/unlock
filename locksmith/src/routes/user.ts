92f80c785cebb8452e348aea8af2ff29acdb4255
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-10-11 10:19:04 -0400

    Removing key endpoint and associated code (#4997)
    With our leveraging of the unlock subgraph we can now leverage this for key related functionality, invaliding this initial approach

--- locksmith/src/routes/user.ts
+++ locksmith/src/routes/user.ts
@@ -39,7 +39,6 @@ router.get(
 )
 
 router.get('/:emailAddress/cards', userController.cards)
-router.get('/:ethereumAddress/keys', userController.keys)
 router.put('/:emailAddress', userController.updateUser)
 router.put('/:emailAddress/paymentdetails', userController.updatePaymentDetails)
 router.put(
