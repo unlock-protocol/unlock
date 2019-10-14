87b89c712ce1ce34033ff3db0690b1079f736f89
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-14 19:01:53 -0400

    Make sure to zero out eth balance for logged out users (#5014)
    * Make sure to zero out eth balance for logged out users
    
    * Keep track of addresses we allow managed purchases from
    
    * Inject balance _only_ for the approved token address
    
    * Conform to type
    
    * Would be nice to pass the correct number of params

--- paywall/src/unlock.js/constants.ts
+++ paywall/src/unlock.js/constants.ts
@@ -17,24 +17,32 @@ const constants: { [key: string]: StartupConstants } = {
     debug: process.env.DEBUG,
     paywallUrl: process.env.PAYWALL_URL || 'http://localhost:3001',
     accountsUrl: process.env.USER_IFRAME_URL || 'http://localhost:3000/account',
+    managedPurchaseStablecoinAddress:
+      '0x591AD9066603f5499d12fF4bC207e2f577448c46',
   },
   test: {
     network: 1984,
     debug: process.env.DEBUG,
     paywallUrl: process.env.PAYWALL_URL || 'http://localhost:3001',
     accountsUrl: process.env.USER_IFRAME_URL || 'http://localhost:3000/account',
+    managedPurchaseStablecoinAddress:
+      '0x591AD9066603f5499d12fF4bC207e2f577448c46',
   },
   staging: {
     network: 4,
     debug: process.env.DEBUG,
     paywallUrl: process.env.PAYWALL_URL,
     accountsUrl: process.env.USER_IFRAME_URL,
+    managedPurchaseStablecoinAddress:
+      '0x8f2e097E79B1c51Be9cBA42658862f0192C3E487',
   },
   prod: {
     network: 1,
     debug: process.env.DEBUG,
     paywallUrl: process.env.PAYWALL_URL,
     accountsUrl: process.env.USER_IFRAME_URL,
+    managedPurchaseStablecoinAddress:
+      '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
   },
 }
 
