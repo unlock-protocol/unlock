6b627281a522a49b7cfd074f5e5fa34edabea1c1
Julien Genestoux <julien.genestoux@gmail.com>
2019-10-15 16:52:25 -0400

    fixing fs.writeFile

--- unlock-protocol.com/scripts/build-blog.js
+++ unlock-protocol.com/scripts/build-blog.js
@@ -5,6 +5,11 @@ const { generateBlogIndexFile, generateBlogFeed } = require('../src/utils/blog')
 
 const dir = resolve(__dirname, '..')
 
-generateBlogIndexFile(dir, generateBlogFeed(dir))
-
-console.log('Blog index file generated')
+generateBlogIndexFile(dir, generateBlogFeed(dir), error => {
+  if (error) {
+    console.error('Failed to generate blog file')
+    console.error(error)
+    process.exit(1)
+  }
+  console.log('Blog index file generated')
+})
