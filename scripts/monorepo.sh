8532f2c2cc7ba22b5778e9b536d439193531047e
Julien Genestoux <julien.genestoux@gmail.com>
2019-10-15 12:07:15 -0400

    rather than using the commit, use the diff to master (#5022)

--- scripts/monorepo.sh
+++ scripts/monorepo.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 SERVICE=$1
-COMMIT=$2
-BRANCH=$3
+BRANCH=$2
 
 # This script is highly specific to circleci and will "succeed" early when the
 # current git changes are not relevant to the folder being tested (and not on master)
@@ -10,7 +9,7 @@ BRANCH=$3
 SHARED_CONFIG_FILES="package.json .eslintrc.js .prettierrc"
 
 if [ ! "$BRANCH" = "master" ]; then
-  HAS_CHANGES=$(git show --name-only $COMMIT -- $SHARED_CONFIG_FILES $SERVICE)
+  HAS_CHANGES=$(git diff `git merge-base HEAD origin/master` --name-only -- $SHARED_CONFIG_FILES $SERVICE)
   if [ ! -n "$HAS_CHANGES" ]; then
     echo "No change in $SERVICE, skipping job."
     circleci step halt
