5a558de6cc9babba26b9a3598c68b4827680034d
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-18 14:46:52 -0400

    personal_sign just returns the signature for consistency (#5048)
    * unlock-provider gains personal_sign method (#4943)
    
    * unlock-provider can personal_sign
    
    * Update CHANGELOG and README
    
    * walletService dispatches personal_sign for unlock-provider (#5019)
    
    * unlock-provider gains personal_sign method (#4943)
    
    * unlock-provider can personal_sign
    
    * Update CHANGELOG and README
    
    * Dispatch `personal_sign` for unlock-provider
    
    * update CHANGELOG and package.json
    
    * Just return the signature from personal_sign
    
    * Update changelog and version number
    
    * Test to make sure walletservice doesn't error

--- unlock-js/src/__tests__/unlockProvider.test.js
+++ unlock-js/src/__tests__/unlockProvider.test.js
@@ -1,6 +1,7 @@
 import nock from 'nock'
 import sigUtil from 'eth-sig-util'
 import UnlockProvider from '../unlockProvider'
+import WalletService from '../walletService'
 import utils from '../utils'
 
 // TODO: move this to the integration tests directory
@@ -138,11 +139,25 @@ describe('Unlock Provider', () => {
         const messageHash = utils.utf8ToHex(someData)
 
         // second param is unused, but in keeping with what we receive from WalletService
-        const output = provider.personal_sign([messageHash, ''])
+        const sig = provider.personal_sign([messageHash, ''])
 
-        expect(sigUtil.recoverPersonalSignature(output)).toEqual(
-          publicKey.toLowerCase()
-        )
+        expect(
+          sigUtil.recoverPersonalSignature({
+            data: messageHash,
+            sig,
+          })
+        ).toEqual(publicKey.toLowerCase())
+      })
+
+      it('should be compatible with walletService', async done => {
+        expect.assertions(1)
+
+        const ws = new WalletService({ unlockAddress: 'does not matter here' })
+        await ws.connect(provider)
+        ws.signDataPersonal('', 'this is my data', error => {
+          expect(error).toBeNull()
+          done()
+        })
       })
     })
   })
