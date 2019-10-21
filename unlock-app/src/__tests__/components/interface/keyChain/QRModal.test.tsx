c51cf69acc9c05b6c941cdc6c8a9f953a3170dbf
Christoph Nascone <chris@unlock-protocol.com>
2019-10-21 16:38:30 -0400

    Adding the email form

--- unlock-app/src/__tests__/components/interface/keyChain/QRModal.test.tsx
+++ unlock-app/src/__tests__/components/interface/keyChain/QRModal.test.tsx
@@ -6,8 +6,14 @@ describe('QRModal', () => {
   it('should render a QR code', () => {
     expect.assertions(1)
     const dismiss = jest.fn()
+    const sendEmail = jest.fn()
     const { container } = rtl.render(
-      <QRModal active dismiss={dismiss} value="just some test data" />
+      <QRModal
+        active
+        dismiss={dismiss}
+        sendEmail={sendEmail}
+        value="just some test data"
+      />
     )
 
     expect(container.querySelector('canvas')).not.toBeNull()
--- unlock-app/src/components/interface/keyChain/QRModal.tsx
+++ unlock-app/src/components/interface/keyChain/QRModal.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import QRCode from 'qrcode.react'
 import InlineModal from '../InlineModal'
+import { Input, SubmitButton, DisabledButton } from '../user-account/styles'
 
 interface Props {
   active: boolean
   dismiss: () => void
   value: string
+  sendEmail: (recipient: string) => void
 }
 
-export const QRModal = ({ active, dismiss, value }: Props) => {
+export const QRModal = ({ active, dismiss, value, sendEmail }: Props) => {
+  const [recipient, setRecipient] = useState('')
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipient(evt.currentTarget.value)
+  }
+
   return (
     <InlineModal active={active} dismiss={dismiss}>
       <QRCode value={value} size={256} includeMargin />
+      <Input
+        type="text"
+        value={recipient}
+        onChange={handleChange}
+        placeholder="Email address"
+      />
+      <Submit
+        active={recipient.length > 0}
+        submit={() => sendEmail(recipient)}
+      />
     </InlineModal>
   )
+}
+
+interface SubmitProps {
+  active: boolean
+  submit: () => void
+}
+const Submit = ({ active, submit }: SubmitProps) => {
+  if (active) {
+    return <SubmitButton onClick={submit}>Send Email</SubmitButton>
+  }
+  return <DisabledButton>Send Email</DisabledButton>
 }
 
 export default QRModal
