9835c74cd9c9138e88aced6eb351790711f27494
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-22 15:13:32 -0400

    Using the new QR modal on the keychain (#5085)
    * Reorganization for easier modification
    
    * Keychain uses inline QR modal
    
    * Passing down qrEmail
    
    * Better test coverage
    
    * Actually send the email

--- unlock-app/src/__tests__/components/interface/keyChain/KeyDetails.test.tsx
+++ unlock-app/src/__tests__/components/interface/keyChain/KeyDetails.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import * as rtl from 'react-testing-library'
-import { MockedProvider } from '@apollo/react-testing'
-import {
-  Key,
-  OwnedKey,
-  KeyDetails,
-} from '../../../../components/interface/keyChain/KeyDetails'
+import { MockedProvider, MockedResponse } from '@apollo/react-testing'
+import { KeyDetails } from '../../../../components/interface/keyChain/KeyDetails'
+import { OwnedKey } from '../../../../components/interface/keyChain/KeychainTypes'
 import keyHolderQuery from '../../../../queries/keyHolder'
 
 const accountAddress = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
@@ -21,82 +18,33 @@ const aKey: OwnedKey = {
     price: '50',
   },
 }
-/* const aSignature = {
- *   data: 'nice',
- *   signature: 'really nice',
- * } */
 
-describe('keyChain -- Key', () => {
-  it('should render the lock name', () => {
-    expect.assertions(0)
-    const signData = jest.fn()
-    const { getByText } = rtl.render(
-      <Key
+const render = (...mocks: MockedResponse[]) => {
+  const signData = jest.fn()
+  const qrEmail = jest.fn()
+  return rtl.render(
+    <MockedProvider mocks={mocks || []} addTypename={false}>
+      <KeyDetails
+        address={accountAddress}
         signData={signData}
-        signature={null}
-        displayQR={jest.fn()}
-        accountAddress={accountAddress}
-        ownedKey={aKey}
+        qrEmail={qrEmail}
+        signatures={{}}
       />
-    )
-
-    getByText(aKey.lock.name)
-  })
-
-  it('should dispatch a payload to be signed', () => {
-    expect.assertions(1)
-    const signData = jest.fn()
-    const realDateNow = Date.now.bind(global.Date)
-    const dateNowStub = jest.fn(() => 1530518207007)
-    global.Date.now = dateNowStub
-    const { getByText } = rtl.render(
-      <Key
-        signData={signData}
-        signature={null}
-        displayQR={jest.fn()}
-        accountAddress={accountAddress}
-        ownedKey={aKey}
-      />
-    )
-
-    const button = getByText('Assert Ownership')
-    rtl.fireEvent.click(button)
-
-    expect(signData).toHaveBeenCalledWith(
-      JSON.stringify({
-        accountAddress,
-        lockAddress: aKey.lock.address,
-        timestamp: dateNowStub(),
-      }),
-      aKey.lock.address
-    )
-
-    global.Date.now = realDateNow
-  })
-})
+    </MockedProvider>
+  )
+}
 
 describe('keyChain -- KeyDetails', () => {
   it('should render loading state', () => {
     expect.assertions(0)
-    const signData = jest.fn()
 
-    const { getByTitle } = rtl.render(
-      <MockedProvider mocks={[]}>
-        <KeyDetails
-          address={accountAddress}
-          signData={signData}
-          displayQR={jest.fn}
-          signatures={{}}
-        />
-      </MockedProvider>
-    )
+    const { getByTitle } = render()
 
     getByTitle('loading')
   })
 
   it('should render error state', async () => {
     expect.assertions(2)
-    const signData = jest.fn()
 
     const errorMock = {
       request: {
@@ -108,16 +56,7 @@ describe('keyChain -- KeyDetails', () => {
       error: new Error('welp'),
     }
 
-    const { getByText } = rtl.render(
-      <MockedProvider mocks={[errorMock]} addTypename={false}>
-        <KeyDetails
-          address={accountAddress}
-          signData={signData}
-          displayQR={jest.fn}
-          signatures={{}}
-        />
-      </MockedProvider>
-    )
+    const { getByText } = render(errorMock)
 
     await rtl.wait(() => {
       expect(getByText('Could not retrieve keys')).toBeInTheDocument()
@@ -128,7 +67,6 @@ describe('keyChain -- KeyDetails', () => {
 
   it('should show a default when no keyholder is found', async () => {
     expect.assertions(1)
-    const signData = jest.fn()
 
     const noResultsMock = {
       request: {
@@ -144,16 +82,7 @@ describe('keyChain -- KeyDetails', () => {
       },
     }
 
-    const { getByText } = rtl.render(
-      <MockedProvider mocks={[noResultsMock]} addTypename={false}>
-        <KeyDetails
-          address={accountAddress}
-          signData={signData}
-          displayQR={jest.fn}
-          signatures={{}}
-        />
-      </MockedProvider>
-    )
+    const { getByText } = render(noResultsMock)
 
     await rtl.wait(() => {
       expect(getByText("You don't have any keys yet")).toBeInTheDocument()
@@ -162,7 +91,6 @@ describe('keyChain -- KeyDetails', () => {
 
   it('should render the key details when keys are returned', async () => {
     expect.assertions(1)
-    const signData = jest.fn()
 
     const resultsMock = {
       request: {
@@ -184,16 +112,7 @@ describe('keyChain -- KeyDetails', () => {
       },
     }
 
-    const { getByText } = rtl.render(
-      <MockedProvider mocks={[resultsMock]} addTypename={false}>
-        <KeyDetails
-          address={accountAddress}
-          signData={signData}
-          displayQR={jest.fn}
-          signatures={{}}
-        />
-      </MockedProvider>
-    )
+    const { getByText } = render(resultsMock)
 
     await rtl.wait(() => {
       expect(getByText('ERC20 paywall lock')).toBeInTheDocument()
