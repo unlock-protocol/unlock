dc2fda7ddfb20d0dc8ded0fc5be1c02266d60c5c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-22 10:43:13 -0400

    Add QR modal based on InlineModal (#5075)
    * Component in most basic state
    
    * Adding the email form
    
    * updating storyshots
    
    * Full test coverage
    
    * Also send a QR data url when submitting
    
    * Update storyshots
    
    * Slightly more sophisticated input validation

---
+++ unlock-app/src/components/interface/keyChain/QRModal.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import QRCode from 'qrcode.react'
+import InlineModal from '../InlineModal'
+import { Input, SubmitButton, DisabledButton } from '../user-account/styles'
+
+interface Props {
+  active: boolean
+  dismiss: () => void
+  value: string
+  sendEmail: (recipient: string, qrImage: string) => void
+}
+
+export const QRModal = ({ active, dismiss, value, sendEmail }: Props) => {
+  const [recipient, setRecipient] = useState('')
+  const [isValid, setIsValid] = useState(false)
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipient(evt.currentTarget.value)
+    setIsValid(evt.currentTarget.validity.valid)
+  }
+
+  return (
+    <InlineModal active={active} dismiss={dismiss}>
+      <QRCode value={value} size={256} includeMargin />
+      <Input
+        type="email"
+        required
+        value={recipient}
+        onChange={handleChange}
+        placeholder="Email address"
+      />
+      <Submit
+        active={isValid}
+        submit={() => {
+          const canvas = document.querySelector('canvas')
+          if (canvas) {
+            sendEmail(recipient, canvas.toDataURL())
+          }
+        }}
+      />
+    </InlineModal>
+  )
+}
+
+export default QRModal
+
+interface SubmitProps {
+  active: boolean
+  submit: () => void
+}
+const Submit = ({ active, submit }: SubmitProps) => {
+  const [submitted, setSubmitted] = useState(false)
+  if (submitted) {
+    return <DisabledButton>Sent!</DisabledButton>
+  } else if (active) {
+    return (
+      <SubmitButton
+        onClick={() => {
+          setSubmitted(true)
+          submit()
+        }}
+      >
+        Send Email
+      </SubmitButton>
+    )
+  }
+  return <DisabledButton>Send Email</DisabledButton>
+}
--- unlock-app/src/stories/interface/InlineModal.stories.js
+++ unlock-app/src/stories/interface/InlineModal.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import InlineModal from '../../components/interface/InlineModal'
+import QRModal from '../../components/interface/keyChain/QRModal'
 
 storiesOf('InlineModal', module)
   .add('When active', () => {
@@ -15,5 +16,15 @@ storiesOf('InlineModal', module)
       <InlineModal active={false} dismiss={() => {}}>
         <p>Just some text to show the modal with.</p>
       </InlineModal>
+    )
+  })
+  .add('In QR Code form', () => {
+    return (
+      <QRModal
+        active
+        dismiss={() => {}}
+        value="Just some test data"
+        sendEmail={() => {}}
+      />
     )
   })
