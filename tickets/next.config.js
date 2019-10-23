aed3068db7c697b20dac6428d3aa88550da9359a
Christoph Nascone <chris@unlock-protocol.com>
2019-10-23 09:00:05 -0400

    Use environment variables in tickets for app and static

--- tickets/next.config.js
+++ tickets/next.config.js
@@ -21,6 +21,8 @@ const requiredConfigVariables = {
   locksmithUri: process.env.LOCKSMITH_URI,
   wedlocksUri: process.env.WEDLOCKS_URI,
   unlockTicketsUrl: process.env.UNLOCK_TICKETS_URL,
+  unlockAppUrl: process.env.UNLOCK_APP_URL,
+  unlockStaticUrl: process.env.UNLOCK_STATIC_URL,
   erc20ContractSymbol: process.env.ERC20_CONTRACT_SYMBOL,
   erc20ContractAddress: process.env.ERC20_CONTRACT_ADDRESS,
 }
