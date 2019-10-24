b1e08ce292e57aa9fee4b75dbcf485b3e63ca3bf
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-24 16:10:48 -0400

    Get refund contract address from environment variable (#5115)
    * Get refund contract address from environment variable
    
    * Add comments around new variable

--- unlock-app/src/config.js
+++ unlock-app/src/config.js
@@ -54,6 +54,10 @@ export default function configure(
     'http://localhost:3001/static/paywall.min.js'
   let unlockStaticUrl = runtimeConfig.unlockStaticUrl || 'http://localhost:3002'
   let httpProvider = runtimeConfig.httpProvider || '127.0.0.1'
+  // TODO: remove after EthWaterloo, this is temporary
+  let externalRefundContractAddress =
+    runtimeConfig.externalRefundContractAddress ||
+    '0xb75f06e743f9CfFf2efa711427098304f5ccfFa4'
   let blockTime = 8000 // in mseconds.
   let chainExplorerUrlBuilders = {
     etherScan: () => '',
@@ -200,5 +204,6 @@ export default function configure(
     chainExplorerUrlBuilders,
     stripeApiKey,
     subgraphURI,
+    externalRefundContractAddress,
   }
 }
