8f51c7e4207962c723c37e8e5b0fe63c200a1671
Christopher Nascone <cnasc@users.noreply.github.com>
2019-10-28 13:32:29 -0400

    Standing up the external refund contract better (#5136)

--- docker/development/deploy-external-refund.js
+++ docker/development/deploy-external-refund.js
@@ -12,6 +12,7 @@ async function deployExternalRefund(
   provider
 ) {
   let wallet = await provider.getSigner(0)
+  const walletAddress = await wallet.getAddress()
   let factory = new ethers.ContractFactory(
     externalRefund.abi,
     externalRefund.bytecode,
@@ -23,7 +24,20 @@ async function deployExternalRefund(
   })
 
   let deployment = await contract.deployed()
-  console.log('The External Refund was deployed: ', deployment.address)
+
+  // the "first" account (typically
+  // 0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2) is admin of the
+  // contract, but is not whitelisted by default. So after we deploy
+  // the contract, we enable that address to perform refunds.
+  let abi = [
+    'function addWhitelisted(address account)',
+  ]
+  let deployedContract = new ethers.Contract(deployment.address, abi, wallet)
+  await deployedContract.addWhitelisted(walletAddress)
+  
+  console.log('The External Refund was deployed:   ', deployment.address)
+  console.log('The External Refund contract admin: ', walletAddress)
+  console.log(`The External Refund lock address:   ${lockAddress}`)
 }
 
 module.exports = {
