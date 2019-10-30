2c72b33440dbed32d4e4e9d871b8240ca10312bd
Christoph Nascone <chris@unlock-protocol.com>
2019-10-30 14:30:55 -0400

    Point to correct file, remove unneeded import

--- unlock-app/jest.setup.ts
+++ unlock-app/jest.setup.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import '@testing-library/jest-dom/extend-expect'
-import '@testing-library/react/cleanup-after-each'
 import 'jest-styled-components'
 
 import { setConfig } from 'next/config'
