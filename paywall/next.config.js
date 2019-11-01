789747b8fc9d5055e2b601ff5af7d64e557c08da
Christoph Nascone <chris@unlock-protocol.com>
2019-11-01 10:39:04 -0400

    Paywall can resolve typescript files

--- paywall/next.config.js
+++ paywall/next.config.js
@@ -7,23 +7,6 @@ const configVariables = require('./environment')
 
 const copyFile = promisify(fs.copyFile)
 
-// TODO: remove this when next gets their act together
-// see https://github.com/zeit/next-plugins/issues/392#issuecomment-475845330
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  console.warn(
-    'HACK: Removing `minimize` option from `css-loader` entries in Webpack config'
-  )
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize
-        }
-      })
-    }
-  })
-}
-
 module.exports = withCSS({
   publicRuntimeConfig: {
     ...configVariables,
@@ -33,7 +16,10 @@ module.exports = withCSS({
     config.node = {
       fs: 'empty',
     }
-    HACK_removeMinimizeOptionFromCssLoaders(config)
+
+    // Even though next is allegedly configured to use TS by default, you still
+    // have to add the typescript file extensions to webpack
+    config.resolve.extensions = [...config.resolve.extensions, '.ts', '.tsx']
 
     return config
   },
