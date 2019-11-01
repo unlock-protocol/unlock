6e4caff116cdf3b0bb43aa036cab9aef89179175
Christoph Nascone <chris@unlock-protocol.com>
2019-11-01 10:27:20 -0400

    Make this work with next 9

--- unlock-app/src/pages/_error.js
+++ unlock-app/src/pages/_error.js
@@ -1,34 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Router from 'next/router'
 
 import UnlockPropTypes from '../propTypes'
 import withConfig from '../utils/withConfig'
 
 class Error extends React.Component {
-  static getInitialProps({ res, err, req }) {
+  static getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null
 
-    // redirect to home now if not found
+    // why did we previously automatically redirect to home?
+    // at any rate, we cannot use Router here because Router doesn't exist server-side
     if (statusCode) {
-      const configure = require('../config').default
-      const config = configure()
       console.error(`Server-side error! ${statusCode}`, err) // eslint-disable-line
-      if (
-        !(
-          config.env !== 'production' &&
-          req.connection.remoteAddress.match(/^127.0.0.1|^localhost/)
-        )
-      ) {
-        res.writeHead(307, {
-          Location: '/',
-        })
-        res.end()
-      }
-    } else {
-      Router.push('/')
     }
-
     return { statusCode }
   }
 
