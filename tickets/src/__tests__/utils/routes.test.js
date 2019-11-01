0716c95d8bc28e386ee29aa0386289ab4e1ee84a
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-01 14:42:44 -0400

    removed the rsvp route (#5189)

--- tickets/src/__tests__/utils/routes.test.js
+++ tickets/src/__tests__/utils/routes.test.js
@@ -1,16 +1,11 @@
-import { lockRoute, rsvpRoute } from '../../utils/routes'
+import { lockRoute } from '../../utils/routes'
 
 describe('route utilities', () => {
   const baseRoute = {
     lockAddress: null,
     prefix: null,
   }
-  const rsvpBaseRoute = {
-    signature: null,
-    publicKey: null,
-    lockAddress: null,
-    prefix: null,
-  }
+
   describe('lockRoute', () => {
     it('should return null value when it does not match', () => {
       expect.assertions(2)
@@ -26,39 +21,6 @@ describe('route utilities', () => {
         ...baseRoute,
         lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
         prefix: 'lock',
-      })
-    })
-  })
-  describe('rsvpRoute', () => {
-    it('should return null value when it does not match', () => {
-      expect.assertions(2)
-      expect(rsvpRoute('/dashboard')).toEqual(rsvpBaseRoute)
-      expect(rsvpRoute('/lock')).toEqual(rsvpBaseRoute)
-    })
-
-    it('should return the right prefix and lockAddress value when it matches the full URL', () => {
-      expect.assertions(1)
-      expect(
-        rsvpRoute(
-          '/checkin/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54/0x80b8825a3e7Fb15263D0DD455B8aAfc08503bb54/MMdskljfsdIOJWERO79b8825a3e7Fb15263D0DD455B8aAfc08503bb54d23493439'
-        )
-      ).toEqual({
-        lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
-        publicKey: '0x80b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
-        signature:
-          'MMdskljfsdIOJWERO79b8825a3e7Fb15263D0DD455B8aAfc08503bb54d23493439',
-        prefix: 'checkin',
-      })
-    })
-
-    it('should return the right prefix and lockAddress value when it matches the partial URL', () => {
-      expect.assertions(1)
-      expect(
-        rsvpRoute('/checkin/0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54')
-      ).toEqual({
-        ...rsvpBaseRoute,
-        lockAddress: '0x79b8825a3e7Fb15263D0DD455B8aAfc08503bb54',
-        prefix: 'checkin',
       })
     })
   })
--- tickets/src/utils/routes.js
+++ tickets/src/utils/routes.js
@@ -1,8 +1,4 @@
-import {
-  LOCK_PATH_NAME_REGEXP,
-  EVENT_PATH_NAME_REGEXP,
-  UNSIGNED_TICKET_PATH_NAME_REGEXP,
-} from '../constants'
+import { LOCK_PATH_NAME_REGEXP } from '../constants'
 
 if (!global.URL) {
   // polyfill for server
@@ -39,36 +35,6 @@ export const lockRoute = path => {
   }
 }
 
-export const rsvpRoute = path => {
-  const url = new URL(path, 'http://tickets.unlock-protocol.com')
-  const match = url.pathname.match(EVENT_PATH_NAME_REGEXP)
-  const unsignedMatch = url.pathname.match(UNSIGNED_TICKET_PATH_NAME_REGEXP)
-
-  if (!match && !unsignedMatch) {
-    return {
-      signature: null,
-      publicKey: null,
-      lockAddress: null,
-      prefix: null,
-    }
-  } else if (match) {
-    return {
-      signature: match[4] || null,
-      publicKey: match[3] || null,
-      lockAddress: match[2] || null,
-      prefix: match[1] || null,
-    }
-  } else if (unsignedMatch) {
-    return {
-      signature: null,
-      publicKey: null,
-      lockAddress: unsignedMatch[2] || null,
-      prefix: unsignedMatch[1] || null,
-    }
-  }
-}
-
 export default {
   lockRoute,
-  rsvpRoute,
 }
