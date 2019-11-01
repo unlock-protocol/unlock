9219b512ec092c7ca47cca2e6a01972878193c9b
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-01 15:10:42 -0400

    removed unused lock actions (#5194)

--- tickets/src/reducers/locksReducer.js
+++ tickets/src/reducers/locksReducer.js
@@ -1,10 +1,4 @@
-import {
-  ADD_LOCK,
-  DELETE_LOCK,
-  UPDATE_LOCK,
-  UPDATE_LOCK_KEY_PRICE,
-} from '../actions/lock'
-import { DELETE_TRANSACTION } from '../actions/transaction'
+import { ADD_LOCK, UPDATE_LOCK } from '../actions/lock'
 import { SET_PROVIDER } from '../actions/provider'
 import { SET_NETWORK } from '../actions/network'
 
@@ -52,33 +46,6 @@ const locksReducer = (state = initialState, action) => {
       ...state,
       [action.address]: { ...state[action.address], ...action.update },
     }
-  }
-
-  if (action.type === UPDATE_LOCK_KEY_PRICE) {
-    return {
-      ...state,
-      [action.address]: {
-        ...state[action.address],
-        keyPrice: action.price,
-      },
-    }
-  }
-
-  if (action.type === DELETE_LOCK) {
-    delete state[action.address]
-    return {
-      ...state,
-    }
-  }
-
-  // If a transaction is deleted, we need to delete the corresponding lock, if any!
-  if (
-    action.type === DELETE_TRANSACTION &&
-    action.transaction &&
-    action.transaction.lock
-  ) {
-    const { [action.transaction.lock]: lockToRemove, ...otherLocks } = state
-    return otherLocks
   }
 
   return state
