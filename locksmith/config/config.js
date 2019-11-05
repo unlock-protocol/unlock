ce674fc25abfa022bade40c054af148f8888e117
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-05 09:01:06 -0500

    using a utils

--- locksmith/config/config.js
+++ locksmith/config/config.js
@@ -1,11 +1,4 @@
-const path = require('path')
-const dotenv = require('dotenv')
-
-const unlockEnv = process.env.UNLOCK_ENV || 'dev'
-
-dotenv.config({
-  path: path.resolve(__dirname, '..', `.env.${unlockEnv}.local`),
-})
+require('../src/utils/envLoader')
 
 module.exports = {
   development: {
