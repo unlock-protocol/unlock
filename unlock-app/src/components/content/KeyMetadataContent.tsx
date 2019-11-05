0a4b77a86f0b14b9ff41d7b4039d567c00fea59c
Christoph Nascone <chris@unlock-protocol.com>
2019-11-05 17:41:03 -0500

    First pass at getting the metadata table up

--- unlock-app/src/components/content/KeyMetadataContent.tsx
+++ unlock-app/src/components/content/KeyMetadataContent.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import 'cross-fetch/polyfill'
 import { connect } from 'react-redux'
 import Head from 'next/head'
+import queryString from 'query-string'
 import BrowserOnly from '../helpers/BrowserOnly'
 import Layout from '../interface/Layout'
 import Account from '../interface/Account'
 import { pageTitle } from '../../constants'
-import { Account as AccountType, Network } from '../../unlockTypes'
+import { Account as AccountType, Network, Router } from '../../unlockTypes'
+import { MetadataTable } from '../interface/MetadataTable'
+import keyHolderQuery from '../../queries/keyholdersByLock'
 
 interface Props {
   account: AccountType
   network: Network
+  lockAddresses: string[]
 }
 
-export const KeyMetadataContent = ({ account, network }: Props) => {
+export const KeyMetadataContent = ({
+  account,
+  network,
+  lockAddresses,
+}: Props) => {
   return (
     <Layout title="Key Chain">
       <Head>
@@ -21,21 +31,47 @@ export const KeyMetadataContent = ({ account, network }: Props) => {
       {account && (
         <BrowserOnly>
           <Account network={network} account={account} />
+          <GraphWrapper lockAddresses={lockAddresses} />
         </BrowserOnly>
       )}
     </Layout>
   )
 }
 
+const GraphWrapper = ({ lockAddresses }: { lockAddresses: string[] }) => {
+  const { loading, error, data } = useQuery(keyHolderQuery(), {
+    variables: { addresses: lockAddresses },
+  })
+
+  if (loading) {
+    return <span>Loading...</span>
+  }
+
+  if (error) {
+    return <span>An error occurred.</span>
+  }
+
+  const columns = ['lockName', 'keyholderAddress', 'expiration']
+
+  return <MetadataTable columns={columns} metadata={data} />
+}
+
 interface ReduxState {
   account: AccountType
   network: Network
+  router: Router
 }
 
-export const mapStateToProps = ({ account, network }: ReduxState) => {
+export const mapStateToProps = ({ account, network, router }: ReduxState) => {
+  const search = queryString.parse(router.location.search)
+  let lockAddresses: string[] = []
+  if (search.locks) {
+    lockAddresses = search.locks
+  }
   return {
     account,
     network,
+    lockAddresses,
   }
 }
 
