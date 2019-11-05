0197dfdc173fb5eed365cd7d287c6bb85f7e04d8
Nick Furfaro <nfurfaro33@gmail.com>
2019-11-05 12:46:03 -0700

    Add timeMachine mock

---
+++ smart-contracts/contracts/mocks/TimeMachineMock.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+import "../PublicLock.sol";
+
+contract TimeMachineMock is
+  PublicLock
+{
+  /**
+  * @notice Modify the expirationTimestamp of a key
+  * by a given amount.
+  * @param _owner The owner of the key to modify
+  * @param _deltaT The amount of time in seconds by which
+  * to modify the keys expirationTimestamp
+  * @param _addTime Choose whether to increase or decrease
+  * expirationTimestamp (false == decrease, true == increase)
+  * @dev Throws if owner does not have a valid key.
+  */
+  function timeMachine(
+    address _owner,
+    uint256 _deltaT,
+    bool _addTime
+  ) public
+  {
+    _timeMachine(_owner, _deltaT, _addTime);
+  }
+}
