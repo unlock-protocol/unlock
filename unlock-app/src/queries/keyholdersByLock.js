c2cdd64b8357282101922cfc3f62c4969042123f
Christopher Nascone <cnasc@users.noreply.github.com>
2019-11-05 15:38:19 -0500

    Add query to get all keys for a given lock (#5212)
    * Add query to get all keys for a given lock
    
    * Adjust query to get multiple locks

---
+++ unlock-app/src/queries/keyholdersByLock.js
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-boost'
+
+export default function keyholdersByLockQuery() {
+  return gql`
+    query Lock($addresses: [String!]) {
+      locks(where: { address_in: $addresses }) {
+        keys {
+          owner {
+            address
+          }
+          keyId
+          expiration
+        }
+        name
+      }
+    }
+  `
+}
