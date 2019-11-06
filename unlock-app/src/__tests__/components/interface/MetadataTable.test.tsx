3b1e1a3abb50a7b0fe13d7a9c492481ddc577c86
Christopher Nascone <cnasc@users.noreply.github.com>
2019-11-06 11:18:27 -0500

    Add a page where lock creators can view keyholder metadata (#5219)
    * We can render a table for these things now
    
    * Be able to go to the metadata page
    
    * First pass at getting the metadata table up
    
    * Update array format
    
    * Page is functional, snapshots work again
    
    * Fix requests from review
    
    * Rename
    
    * Make header less readable
    
    * Snapshots needed to be updated twice for some reason

---
+++ unlock-app/src/__tests__/components/interface/MetadataTable.test.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import {
+  MetadataTable,
+  camelCaseToTitle,
+} from '../../../components/interface/MetadataTable'
+
+const metadata = [
+  {
+    lockName: 'Giant Martian Insect Party',
+    keyholderAddress: '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2',
+    expiration: '123456789',
+    emailAddress: 'support@tether.to',
+  },
+  {
+    lockName: 'Giant Martian Insect Party',
+    keyholderAddress: '0x84BCb1DFF32Ee9e7Bc7c6868954C3E6F346046b4',
+    expiration: '123456789',
+    emailAddress: 'rex.smythe@higgi.ns',
+  },
+  {
+    lockName: 'Giant Martian Insect Party',
+    keyholderAddress: '0xD6858301c9F434cCcDbFaB8E984bea08BbDBFDCE',
+    expiration: '123456789',
+    emailAddress: 'ssgt_jones@area51.gov',
+  },
+  {
+    lockName: 'Giant Martian Insect Party',
+    keyholderAddress: '0xaFAEfc6dd3C9feF66f92BA838b132644451F0715',
+    expiration: '123456789',
+    emailAddress: "we don't validate email inputs",
+  },
+]
+
+describe('MetadataTable', () => {
+  describe('camelCaseToTitle helper', () => {
+    it('transforms itself', () => {
+      expect.assertions(1)
+      expect(camelCaseToTitle('camelCaseToTitle')).toEqual(
+        'Camel Case To Title'
+      )
+    })
+  })
+
+  describe('MetadataTable component', () => {
+    it('renders the headings correctly', () => {
+      expect.assertions(4)
+
+      const { container } = rtl.render(
+        <MetadataTable
+          columns={[
+            'lockName',
+            'keyholderAddress',
+            'expiration',
+            'emailAddress',
+          ]}
+          metadata={metadata}
+        />
+      )
+
+      const headings = container.querySelectorAll('th')
+      const expectedHeadings = [
+        'Lock Name',
+        'Keyholder Address',
+        'Expiration',
+        'Email Address',
+      ]
+
+      expectedHeadings.forEach((heading, index) => {
+        expect(headings[index].textContent).toEqual(heading)
+      })
+    })
+
+    it('Aligns matching values and headers', () => {
+      expect.assertions(4)
+
+      const { container } = rtl.render(
+        <MetadataTable
+          columns={[
+            'lockName',
+            'keyholderAddress',
+            'expiration',
+            'emailAddress',
+          ]}
+          metadata={metadata}
+        />
+      )
+
+      const body = container.getElementsByTagName('tbody')[0]
+      const firstRow = body.getElementsByTagName('tr')[0]
+      const values = firstRow.getElementsByTagName('td')
+      ;[
+        'Giant Martian Insect Party',
+        '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2',
+        '123456789',
+        'support@tether.to',
+      ].forEach((expectedValue, index) => {
+        expect(values[index].textContent).toEqual(expectedValue)
+      })
+    })
+  })
+})
---
+++ unlock-app/src/components/content/MembersContent.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import 'cross-fetch/polyfill'
+import { connect } from 'react-redux'
+import Head from 'next/head'
+import queryString from 'query-string'
+import BrowserOnly from '../helpers/BrowserOnly'
+import Layout from '../interface/Layout'
+import Account from '../interface/Account'
+import { pageTitle } from '../../constants'
+import { Account as AccountType, Network, Router } from '../../unlockTypes'
+import { MetadataTable } from '../interface/MetadataTable'
+import keyHolderQuery from '../../queries/keyholdersByLock'
+import { expirationAsDate } from '../../utils/durations'
+
+interface KeyMetadata {
+  // These 3 properties are always present -- they come down from the graph as
+  // strings
+  lockName: string
+  expiration: string
+  keyholderAddress: string
+  // Can have any other arbitrary properies, as long as the values are strings.
+  [key: string]: string
+}
+
+interface Props {
+  account: AccountType
+  network: Network
+  lockAddresses: string[]
+}
+
+export const MembersContent = ({ account, network, lockAddresses }: Props) => {
+  return (
+    <Layout title="Members">
+      <Head>
+        <title>{pageTitle('Members')}</title>
+      </Head>
+      {account && (
+        <BrowserOnly>
+          <Account network={network} account={account} />
+          <GraphWrapper lockAddresses={lockAddresses} />
+        </BrowserOnly>
+      )}
+    </Layout>
+  )
+}
+
+const GraphWrapper = ({ lockAddresses }: { lockAddresses: string[] }) => {
+  const { loading, error, data } = useQuery(keyHolderQuery(), {
+    variables: { addresses: lockAddresses },
+  })
+
+  if (loading) {
+    return <span>Loading...</span>
+  }
+
+  if (error) {
+    return <span>An error occurred.</span>
+  }
+
+  const columns = ['lockName', 'keyholderAddress', 'expiration']
+  let metadata: KeyMetadata[] = []
+  // TODO: types for all graph data -- grouped by query?
+  data.locks.forEach((lock: any) => {
+    lock.keys.forEach((key: any) => {
+      metadata.push({
+        lockName: lock.name,
+        expiration: expirationAsDate(parseInt(key.expiration)),
+        keyholderAddress: key.owner.address,
+      })
+    })
+  })
+
+  // TODO: get additional metadata from locksmith if it exists, for each key.
+  // We'll have to delay rendering until we retrieve all of it so the table
+  // doesn't jump.
+
+  return <MetadataTable columns={columns} metadata={metadata} />
+}
+
+interface ReduxState {
+  account: AccountType
+  network: Network
+  router: Router
+}
+
+export const mapStateToProps = ({ account, network, router }: ReduxState) => {
+  // URL formatted like: ?locks=address1,address2,address3
+  const search = queryString.parse(router.location.search, {
+    arrayFormat: 'comma',
+  })
+  let lockAddresses: string[] = []
+  if (search.locks) {
+    // search.locks will be either a string or an array.
+    // when there is only one value, it's a string. For any more, it's an array.
+    if (typeof search.locks === 'string') {
+      lockAddresses.push(search.locks)
+    } else {
+      lockAddresses = search.locks as any
+    }
+  }
+  return {
+    account,
+    network,
+    lockAddresses,
+  }
+}
+
+export default connect(mapStateToProps)(MembersContent)
