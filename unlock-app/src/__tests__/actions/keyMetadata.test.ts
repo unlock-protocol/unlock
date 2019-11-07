8a977028c1208aafd26cf42369d3637da7cd286c
Christopher Nascone <cnasc@users.noreply.github.com>
2019-11-07 11:04:39 -0500

    Improve metadata actions (#5244)
    * Add an action to store metadata in redux
    
    * Metadata actions carry necessary info, normalize taxonomy
    
    * WalletService correctly handles new actions

--- unlock-app/src/__tests__/actions/keyMetadata.test.ts
+++ unlock-app/src/__tests__/actions/keyMetadata.test.ts
@@ -1,23 +1,28 @@
 import {
   SIGN_METADATA_REQUEST,
   SIGN_METADATA_RESPONSE,
+  GOT_METADATA,
   signMetadataRequest,
   signMetadataResponse,
+  gotMetadata,
 } from '../../actions/keyMetadata'
+
+const keyIds = ['1', '13']
+const lockAddress = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
 
 describe('Key metadata signature actions', () => {
   describe('signMetadataRequest', () => {
     it('should create an action to request a signature', () => {
       expect.assertions(1)
 
-      const address = '0x5Cd3FC283c42B4d5083dbA4a6bE5ac58fC0f0267'
       const owner = '0xe29ec42f0b620b1c9a716f79a02e9dc5a5f5f98a'
-      const result = signMetadataRequest(address, owner)
+      const result = signMetadataRequest(lockAddress, owner, keyIds)
 
       expect(result).toEqual({
         type: SIGN_METADATA_REQUEST,
-        address,
+        lockAddress,
         owner,
+        keyIds,
         timestamp: expect.any(Number),
       })
     })
@@ -27,12 +32,34 @@ describe('Key metadata signature actions', () => {
     it('should create an action to return a signature', () => {
       expect.assertions(1)
 
-      const result = signMetadataResponse('some data', 'a signature')
+      const result = signMetadataResponse(
+        'some data',
+        'a signature',
+        keyIds,
+        lockAddress
+      )
 
       expect(result).toEqual({
         type: SIGN_METADATA_RESPONSE,
         data: 'some data',
         signature: 'a signature',
+        keyIds,
+        lockAddress,
+      })
+    })
+  })
+
+  describe('gotMetadata', () => {
+    it('should create an action to put the metadata in redux', () => {
+      expect.assertions(1)
+
+      const result = gotMetadata('a lock address', '1', 'some data')
+
+      expect(result).toEqual({
+        type: GOT_METADATA,
+        lockAddress: 'a lock address',
+        keyId: '1',
+        data: 'some data',
       })
     })
   })
--- unlock-app/src/actions/keyMetadata.ts
+++ unlock-app/src/actions/keyMetadata.ts
@@ -1,19 +1,41 @@
 export const SIGN_METADATA_REQUEST = 'keyMetadata/SIGN_METADATA_REQUEST'
 export const SIGN_METADATA_RESPONSE = 'keyMetadata/SIGN_METADATA_RESPONSE'
+export const GOT_METADATA = 'keyMetadata/GOT_METADATA'
 
-export function signMetadataRequest(address: string, owner: string) {
+export function signMetadataRequest(
+  lockAddress: string,
+  owner: string,
+  keyIds: string[]
+) {
   return {
     type: SIGN_METADATA_REQUEST,
-    address,
+    lockAddress,
     owner,
+    keyIds,
     timestamp: Date.now(),
   }
 }
 
-export function signMetadataResponse(data: any, signature: any) {
+export function signMetadataResponse(
+  data: any,
+  signature: any,
+  keyIds: string[],
+  lockAddress: string
+) {
   return {
     type: SIGN_METADATA_RESPONSE,
     data,
     signature,
+    keyIds,
+    lockAddress,
+  }
+}
+
+export function gotMetadata(lockAddress: string, keyId: string, data: any) {
+  return {
+    type: GOT_METADATA,
+    lockAddress,
+    keyId,
+    data,
   }
 }
