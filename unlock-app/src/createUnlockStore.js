5fdc6ecf3c4b94807994285c880da662aaad44b2
Christopher Nascone <cnasc@users.noreply.github.com>
2019-11-07 11:27:56 -0500

    Store key metadata in Redux (#5245)
    * Add an action to store metadata in redux
    
    * Metadata actions carry necessary info, normalize taxonomy
    
    * Add a reducer to store key metadata
    
    * Hook up the reducer
    
    * Fix bad merge

--- unlock-app/src/createUnlockStore.js
+++ unlock-app/src/createUnlockStore.js
@@ -58,6 +58,9 @@ import pageStatusReducer, {
 import signatureReducer, {
   initialState as defaultSignatureState,
 } from './reducers/signatureReducer'
+import metadataReducer, {
+  initialState as defaultMetadataState,
+} from './reducers/metadataReducer'
 
 const config = configure()
 
@@ -84,6 +87,7 @@ export const createUnlockStore = (
     recoveryPhrase: recoveryReducer,
     pageIsLocked: pageStatusReducer,
     signature: signatureReducer,
+    metadata: metadataReducer,
   }
 
   // Cleanup the defaultState to remove all null values so that we do not overwrite existing
@@ -113,6 +117,7 @@ export const createUnlockStore = (
       cart: defaultCartState,
       pageIsLocked: defaultPageStatus,
       signature: defaultSignatureState,
+      metadata: defaultMetadataState,
     },
     {
       provider: Object.keys(config.providers)[0],
