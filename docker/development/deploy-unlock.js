4c54be8892928ab70d88f996c41ff410aafafd5e
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-14 09:07:44 -0500

    removed the refund contract for ethwaterloo (#5278)

--- docker/development/deploy-unlock.js
+++ docker/development/deploy-unlock.js
@@ -7,7 +7,6 @@ const {
   Web3Service,
 } = require('@unlock-protocol/unlock-js')
 const TokenDeployer = require('./deploy-locks')
-const ExternalRefundDeployer = require('./deploy-external-refund')
 
 /*
  * This script is meant to be used in dev environment to deploy a version of the Unlock smart
@@ -72,20 +71,9 @@ serverIsUp(1000 /* every second */, 120 /* up to 2 minutes */)
           programmaticPurchaser,
           userAddress
         )
-
         // Change the base URL for token metadata
         const baseUri = 'http://0.0.0.0:8080/api/key/'
         unlockContract.setGlobalBaseTokenURI(baseUri)
-
-        // TODO REMOVE AS THIS IS NOT USED ANYMORE
-        let lockAddress = '0x0AAF2059Cb2cE8Eeb1a0C60f4e0f2789214350a5'
-        let tokenAddress = '0x591AD9066603f5499d12fF4bC207e2f577448c46'
-        await ExternalRefundDeployer.deployExternalRefund(
-          lockAddress,
-          '21500000000000000000',
-          tokenAddress,
-          provider
-        )
       })
 
       wallet.on('network.changed', () => {
