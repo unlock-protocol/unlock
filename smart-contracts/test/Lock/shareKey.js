8fa8ef13994070df871c1c14c6c7d9e60c7eb784
Nick Furfaro <nfurfaro33@gmail.com>
2019-11-14 08:59:27 -0700

    Got tests working

--- smart-contracts/test/Lock/shareKey.js
+++ smart-contracts/test/Lock/shareKey.js
@@ -10,15 +10,18 @@ const getProxy = require('../helpers/proxy')
 let unlock, locks
 
 contract('Lock / shareKey', accounts => {
-  let lock
-  const keyOwners = [accounts[1], accounts[2], accounts[3]]
-  const keyRecipient = accounts[4]
-  const keyPrice = new BigNumber(Units.convert(0.01, 'eth', 'wei'))
-
   before(async () => {
     unlock = await getProxy(unlockContract)
     locks = await deployLocks(unlock, accounts[0])
-    lock = locks['First']
+  })
+
+  let lock
+  const keyOwners = [accounts[1], accounts[2], accounts[3]]
+  const accountWithNoKey = accounts[4]
+  const keyPrice = new BigNumber(Units.convert(0.01, 'eth', 'wei'))
+
+  before(async () => {
+    lock = locks['FIRST']
     const purchases = keyOwners.map(account => {
       return lock.purchase(0, account, web3.utils.padLeft(0, 40), [], {
         value: keyPrice.toFixed(),
@@ -28,53 +31,54 @@ contract('Lock / shareKey', accounts => {
     await Promise.all(purchases)
   })
 
-  describe('should fail when', () => {
-    it('sender does not have a key', async () => {
-      await shouldFail(
-        lock.shareKey(
-          accounts[6],
-          accounts[7],
-          11,
-          1000,
-          {
-            from: accounts(6),
-          },
-          'KEY_NOT_VALID'
+  describe('failing to share a key', () => {
+    describe('not meeting pre-requisites', () => {
+      it('sender does not have a key', async () => {
+        await shouldFail(
+          lock.shareKey(
+            accountWithNoKey,
+            accounts[7],
+            11,
+            1000,
+            {
+              from: accountWithNoKey,
+            },
+            'KEY_NOT_VALID'
+          )
         )
-      )
-    })
+      })
+      it('called by other than keyOwner or approved ', async () => {
+        await shouldFail(
+          lock.shareKey(
+            keyOwners[0],
+            accounts[3],
+            await lock.getTokenIdFor.call(keyOwners[0]),
+            1000,
+            {
+              from: accounts[6],
+            },
+            'KEY_NOT_VALID'
+          )
+        )
+      })
 
-    it('called by other than keyOwner or approved ', async () => {
-      await shouldFail(
-        lock.shareKey(
-          keyOwners[0],
-          accounts[3],
-          await lock.getTokenIdFor.call(keyOwners[0]),
-          1000,
-          {
-            from: accounts(6),
-          },
-          'KEY_NOT_VALID'
+      it('should abort if the recipient is 0x', async () => {
+        await shouldFail(
+          lock.shareKey(
+            keyOwners[0],
+            Web3Utils.padLeft(0, 40),
+            await lock.getTokenIdFor.call(keyOwners[0]),
+            1000,
+            {
+              from: accounts[6],
+            },
+            'INVALID_ADDRESS'
+          )
         )
-      )
+      })
     })
-
-    it('should abort if the recipient is 0x', async () => {
-      await shouldFail(
-        lock.shareKey(
-          keyOwners[0],
-          Web3Utils.padLeft(0, 40),
-          await lock.getTokenIdFor.call(keyOwners[0]),
-          1000,
-          {
-            from: accounts(6),
-          },
-          'INVALID_ADDRESS'
-        )
-      )
-    })
-    describe('when keyOwner does not have enough time remaining', () => {
-      it('should share all available time if amount is too much', async () => {
+    describe('fallback behaviors', () => {
+      it('transfers all remaining time if amount to share >= remaining time', async () => {
         let timestamp = new BigNumber(
           await lock.keyExpirationTimestampFor.call(keyOwners[0])
         )
@@ -82,7 +86,7 @@ contract('Lock / shareKey', accounts => {
         let timeRemaining = timestamp.minus(now)
         await lock.shareKey(
           keyOwners[0],
-          keyRecipient,
+          accountWithNoKey,
           await lock.getTokenIdFor.call(keyOwners[0]),
           timeRemaining + 1000, // trying to share too much
           {
@@ -90,11 +94,12 @@ contract('Lock / shareKey', accounts => {
           }
         )
       })
+      it('does not expire the origin key', async () => {})
     })
-    describe('when keyOwner has enough time remaining', () => {
-      it('should subtract the time shared + fee from keyOwner', async () => {})
-      it('should create a new key and add the time shared to it', async () => {})
-      it('should extend the key of an existing owner', async () => {})
-    })
+  })
+  describe('successful key sharing', () => {
+    it('should subtract the time shared + fee from keyOwner', async () => {})
+    it('should create a new key and add the time shared to it', async () => {})
+    it('should extend the key of an existing owner', async () => {})
   })
 })
