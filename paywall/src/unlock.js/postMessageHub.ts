8f701e8f8996543f8bcfb349d7c2d4b98f0b5dd9
Christoph Nascone <chris@unlock-protocol.com>
2019-11-15 16:09:52 -0500

    Extract postmessage listening into one hub

---
+++ paywall/src/unlock.js/postMessageHub.ts
@@ -0,0 +1,89 @@
+/* eslint-disable import/prefer-default-export */
+import { PostMessages } from '../messageTypes'
+import DataIframeMessageEmitter from './PostMessageEmitters/DataIframeMessageEmitter'
+import CheckoutIframeMessageEmitter from './PostMessageEmitters/CheckoutIframeMessageEmitter'
+import { Balance } from '../unlockTypes'
+
+export interface checkoutHandlerInitArgs {
+  usingManagedAccount: boolean
+  // TODO: types?
+  dataIframe: DataIframeMessageEmitter
+  checkoutIframe: CheckoutIframeMessageEmitter
+  injectDefaultBalance: (
+    oldBalance: Balance,
+    erc20ContractAddress: string
+  ) => Balance
+  config: any
+  constants: any
+}
+
+export function checkoutHandlerInit({
+  usingManagedAccount,
+  dataIframe,
+  checkoutIframe,
+  config,
+  constants,
+  injectDefaultBalance,
+}: checkoutHandlerInitArgs) {
+  // listen for updates to state from the data iframe, and forward them to the checkout UI
+  dataIframe.on(PostMessages.UPDATE_ACCOUNT, account =>
+    checkoutIframe.postMessage(PostMessages.UPDATE_ACCOUNT, account)
+  )
+  dataIframe.on(PostMessages.UPDATE_ACCOUNT_BALANCE, balance => {
+    let balanceUpdate = balance
+    if (usingManagedAccount) {
+      const { erc20ContractAddress } = constants
+      balanceUpdate = injectDefaultBalance(balance, erc20ContractAddress)
+    }
+    checkoutIframe.postMessage(
+      PostMessages.UPDATE_ACCOUNT_BALANCE,
+      balanceUpdate
+    )
+  })
+  dataIframe.on(PostMessages.UPDATE_LOCKS, locks =>
+    checkoutIframe.postMessage(PostMessages.UPDATE_LOCKS, locks)
+  )
+  dataIframe.on(PostMessages.UPDATE_KEYS, keys =>
+    checkoutIframe.postMessage(PostMessages.UPDATE_KEYS, keys)
+  )
+  dataIframe.on(PostMessages.UPDATE_TRANSACTIONS, transactions => {
+    return checkoutIframe.postMessage(
+      PostMessages.UPDATE_TRANSACTIONS,
+      transactions
+    )
+  })
+  dataIframe.on(PostMessages.UPDATE_NETWORK, network =>
+    checkoutIframe.postMessage(PostMessages.UPDATE_NETWORK, network)
+  )
+
+  // pass on the configuration and request the latest data
+  checkoutIframe.on(PostMessages.READY, () => {
+    checkoutIframe.postMessage(PostMessages.CONFIG, config)
+
+    const updateKinds = [
+      'locks',
+      'account',
+      'balance',
+      'network',
+      'keys',
+      'transactions',
+    ]
+
+    updateKinds.forEach(kind =>
+      dataIframe.postMessage(PostMessages.SEND_UPDATES, kind)
+    )
+  })
+
+  // pass on any errors
+  dataIframe.on(PostMessages.ERROR, error =>
+    checkoutIframe.postMessage(PostMessages.ERROR, error)
+  )
+
+  // pass on locked status
+  dataIframe.on(PostMessages.LOCKED, () =>
+    checkoutIframe.postMessage(PostMessages.LOCKED, undefined)
+  )
+  dataIframe.on(PostMessages.UNLOCKED, lockAddresses =>
+    checkoutIframe.postMessage(PostMessages.UNLOCKED, lockAddresses)
+  )
+}
