93bc0438c44d817d9718f86b50c3155c9c2238bd
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-19 11:37:21 -0500

    adding method to return svg icon

--- locksmith/src/routes/lock.ts
+++ locksmith/src/routes/lock.ts
@@ -6,6 +6,7 @@ var lockController = require('../controllers/lockController')
 router.post('/lock', lockController.lockSave)
 router.get('/lock/:lockAddress', lockController.lockGet)
 router.get('/lock/:lockAddress/cycle', lockController.lockOwnershipCheck)
+router.get('/lock/:lockAddress/icon', lockController.lockIcon)
 router.get('/:owner/locks', lockController.lockOwnerGet)
 
 module.exports = router
