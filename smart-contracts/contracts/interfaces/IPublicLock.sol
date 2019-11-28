84594c9d74601d3f7a8961a240c8614746f4987c
Nick Furfaro <nfurfaro33@gmail.com>
2019-11-28 13:49:14 -0700

    Fix quotes

--- smart-contracts/contracts/interfaces/IPublicLock.sol
+++ smart-contracts/contracts/interfaces/IPublicLock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "./IERC721.sol";
-import "./IERC721Enumerable.sol";
+import './IERC721.sol';
+import './IERC721Enumerable.sol';
 
 /**
 * @title The PublicLock Interface
