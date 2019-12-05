5801db45b49fa4fca7c054cf97fc5baf9b322afb
Christoph Nascone <chris@unlock-protocol.com>
2019-12-05 15:37:36 -0500

    Remove unneeded typedef

--- paywall/src/unlockTypes.ts
+++ paywall/src/unlockTypes.ts
@@ -148,10 +148,6 @@ export interface Key {
   lock: string
 }
 
-export interface Keys {
-  [lockAddress: string]: Keys
-}
-
 export interface PurchaseKeyRequest {
   lock: string // lock address
   extraTip: string // extra value to add in addition to key price
