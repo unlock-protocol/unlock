c61c03f5cb1cbe89885c7a9835075274709287a9
Julien Genestoux <julien.genestoux@gmail.com>
2019-12-06 09:00:27 -0500

    adding CSV export to members page (#5415)

---
+++ unlock-app/src/utils/csv.ts
@@ -0,0 +1,16 @@
+import { camelCaseToTitle } from './strings'
+
+export const buildCSV = (columns: string[], objects: any[]) => {
+  const csv = objects
+    .map((datum: any) => {
+      return columns
+        .map((col: string) => {
+          return `"${datum[col] ? datum[col] : ''}"`
+        })
+        .join(',')
+    })
+    .filter(row => row)
+
+  csv.unshift(columns.map((col: string) => camelCaseToTitle(col)).join(','))
+  return csv.join('\n')
+}
