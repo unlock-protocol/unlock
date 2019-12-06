5719e2434d2afc5ed753be29f9f6eb0b6c1ec0ef
Hardly Difficult <hardlydiff@gmail.com>
2019-12-05 20:44:39 -0800

    Delete TestUnlock.sol (#5420)

--- smart-contracts/test/Unlock/TestUnlock.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.5.13;
-
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../../contracts/Unlock.sol";
-
-contract TestUnlock {
-  Unlock unlock = Unlock(DeployedAddresses.Unlock());
-}
