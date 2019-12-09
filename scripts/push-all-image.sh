160b40b42b9caec5dc70260d8896ff2d04643da7
Akeem Adeniji <akeem.adeniji@gmail.com>
2019-12-09 11:09:51 -0500

    Updates image push process to include latest and timestamped tags (#5434)
    As part of making the setup a bit more standardized and consistent we will now include tags that allow people to develop and run against known builds

--- scripts/push-all-image.sh
+++ scripts/push-all-image.sh
@@ -5,17 +5,22 @@
 # This should only run on master merges
 
 IMAGE_NAME=$1
-IMAGE_TAG=master
+TIMESTAMP=`date +%s`
+
+IMAGE_TAG_LIST=( master latest $TIMESTAMP) 
+IMAGES=( unlock-core paywall smart-contracts unlock-app locksmith integration-tests wedlocks unlock-protocol-com tickets unlock-js )
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-IMAGES=( unlock-core paywall smart-contracts unlock-app locksmith integration-tests wedlocks unlock-protocol-com tickets unlock-js )
-
-for IMAGE_NAME in "${IMAGES[@]}"
+for IMAGE_TAG in "${IMAGE_TAG_LIST[@]}"
 do
-  IMAGE_TO_PUSH="$DOCKER_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG"
-  docker tag $IMAGE_NAME $IMAGE_TO_PUSH
-  docker push $IMAGE_TO_PUSH &
-done
 
+  for IMAGE_NAME in "${IMAGES[@]}"
+  do
+    IMAGE_TO_PUSH="$DOCKER_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG"
+    docker tag $IMAGE_NAME $IMAGE_TO_PUSH
+    docker push $IMAGE_TO_PUSH &
+  done
+
+done
 wait
