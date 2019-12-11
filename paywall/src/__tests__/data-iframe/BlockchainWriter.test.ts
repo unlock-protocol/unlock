44572853476011fe8b93a583710619ddb0f7c09a
Christoph Nascone <chris@unlock-protocol.com>
2019-12-11 15:02:53 -0500

    Simpler implementation

--- paywall/src/__tests__/data-iframe/BlockchainWriter.test.ts
+++ paywall/src/__tests__/data-iframe/BlockchainWriter.test.ts
@@ -1,57 +1,61 @@
-import { BlockchainWriter, Events } from '../../data-iframe/BlockchainWriter'
+import { BlockchainWriter } from '../../data-iframe/BlockchainWriter'
 import { getWalletService } from '../test-helpers/setupBlockchainHelpers'
 
+const mock = () => {
+  const walletService = getWalletService({})
+  const setAccount = jest.fn()
+  const setNetwork = jest.fn()
+  const addTransaction = jest.fn()
+  const alertError = jest.fn()
+  const writer = new BlockchainWriter(
+    walletService,
+    setAccount,
+    setNetwork,
+    addTransaction,
+    alertError
+  )
+
+  return {
+    walletService,
+    setAccount,
+    setNetwork,
+    addTransaction,
+    alertError,
+    writer,
+  }
+}
+
 describe('BlockchainWriter', () => {
-  let writer: BlockchainWriter
-  beforeEach(() => {
-    const walletService = getWalletService({})
-    writer = new BlockchainWriter(walletService)
-  })
-
-  describe('newAccount', () => {
-    it('emits an event with the new account address', done => {
+  describe('setAccount', () => {
+    it('emits an event with the new account address', () => {
       expect.assertions(1)
-
-      writer.on(Events.newAccount, accountAddress => {
-        expect(accountAddress).toEqual(
-          '0xaFAEfc6dd3C9feF66f92BA838b132644451F0715'
-        )
-        done()
-      })
-      writer.newAccount('0xaFAEfc6dd3C9feF66f92BA838b132644451F0715')
+      const { walletService, setAccount } = mock()
+      walletService.emit(
+        'account.changed',
+        '0xaFAEfc6dd3C9feF66f92BA838b132644451F0715'
+      )
+      expect(setAccount).toHaveBeenCalledWith(
+        '0xaFAEfc6dd3C9feF66f92BA838b132644451F0715'
+      )
     })
   })
 
   describe('setNetwork', () => {
-    it('sets the network id to the new network id', done => {
+    it('sets the network id to the new network id', () => {
       expect.assertions(1)
-      writer.on(Events.newNetwork, networkId => {
-        expect(networkId).toEqual(4)
-        done()
-      })
-
-      writer.newNetwork(4)
+      const { walletService, setNetwork } = mock()
+      walletService.emit('network.changed', 4)
+      expect(setNetwork).toHaveBeenCalledWith(4)
     })
   })
 
-  describe('newTransaction', () => {
-    it('emits a formatted transaction', done => {
+  describe('addTransaction', () => {
+    it('emits a formatted transaction', () => {
       expect.assertions(1)
-      writer.on(Events.newTransaction, tx => {
-        expect(tx).toEqual({
-          blockNumber: 9007199254740991,
-          for: '0xafaefc6dd3c9fef66f92ba838b132644451f0715',
-          from: '0xafaefc6dd3c9fef66f92ba838b132644451f0715',
-          hash: 'aHash',
-          input: '',
-          status: 'pending',
-          to: '0x84bcb1dff32ee9e7bc7c6868954c3e6f346046b4',
-          type: 'KEY_PURCHASE',
-        })
-        done()
-      })
+      const { walletService, addTransaction } = mock()
 
-      writer.newTransaction(
+      walletService.emit(
+        'transaction.new',
         'aHash',
         '0xaFAEfc6dd3C9feF66f92BA838b132644451F0715',
         '0x84BCb1DFF32Ee9e7Bc7c6868954C3E6F346046b4',
@@ -59,25 +63,35 @@ describe('BlockchainWriter', () => {
         'KEY_PURCHASE',
         'pending'
       )
+
+      expect(addTransaction).toHaveBeenCalledWith({
+        blockNumber: 9007199254740991,
+        for: '0xafaefc6dd3c9fef66f92ba838b132644451f0715',
+        from: '0xafaefc6dd3c9fef66f92ba838b132644451f0715',
+        hash: 'aHash',
+        input: '',
+        status: 'pending',
+        to: '0x84bcb1dff32ee9e7bc7c6868954c3e6f346046b4',
+        type: 'KEY_PURCHASE',
+      })
     })
   })
 
-  describe('handleError', () => {
-    it('emits the error for a key purchase failure', done => {
+  describe('alertError', () => {
+    it('emits the error for a key purchase failure', () => {
       expect.assertions(1)
-      writer.on(Events.error, (e: Error) => {
-        expect(e.message).toEqual('purchase failed')
-        done()
-      })
+      const { walletService, alertError } = mock()
 
-      writer.handleError(new Error('FAILED_TO_PURCHASE_KEY'))
+      walletService.emit('error', new Error('FAILED_TO_PURCHASE_KEY'))
+      expect(alertError).toHaveBeenCalledWith(new Error('purchase failed'))
     })
 
     it('does not emit for any other error', () => {
       expect.assertions(1)
-      writer.emit = jest.fn()
-      writer.handleError(new Error('KILLER_BEES_RELEASED'))
-      expect(writer.emit).not.toHaveBeenCalled()
+      const { walletService, alertError } = mock()
+
+      walletService.emit('error', new Error('KILLER_BEES_RELEASED'))
+      expect(alertError).not.toHaveBeenCalled()
     })
   })
 })
--- paywall/src/data-iframe/BlockchainWriter.ts
+++ paywall/src/data-iframe/BlockchainWriter.ts
@@ -1,64 +1,60 @@
-import { EventEmitter } from 'events'
 import { normalizeLockAddress } from '../utils/normalizeAddresses'
+import { TransactionDefaults } from './blockchainHandler/blockChainTypes'
 
-export enum Events {
-  newAccount = 'newAccount',
-  newNetwork = 'newNetwork',
-  newTransaction = 'newTransaction',
-  error = 'error',
+export const formatTransaction = (
+  hash: string,
+  from: string,
+  to: string,
+  input: string,
+  type: string,
+  status: string
+): TransactionDefaults => {
+  const normalizedTo = normalizeLockAddress(to)
+  const normalizedFrom = normalizeLockAddress(from)
+  return {
+    hash,
+    from: normalizedFrom,
+    for: normalizedFrom,
+    to: normalizedTo,
+    input,
+    type,
+    status,
+    blockNumber: Number.MAX_SAFE_INTEGER,
+  }
 }
+
+// hideous
+type NewTransactionArgs = [string, string, string, string, string, string]
 
 /**
  * This class is a thin wrapper around WalletService, primarily here
  * for the purpose of transforming data to expected formats.
  */
-export class BlockchainWriter extends EventEmitter {
+export class BlockchainWriter {
   // TODO: types from unlock-js
   walletService: any
 
-  constructor(walletService: any) {
-    super()
+  constructor(
+    walletService: any,
+    setAccount: (accountAddress: string) => void,
+    setNetwork: (networkId: string) => void,
+    addTransaction: (tx: TransactionDefaults) => void,
+    alertError: (error: Error) => void
+  ) {
     this.walletService = walletService
-  }
 
-  newAccount = (accountAddress: string) => {
-    this.emit(Events.newAccount, accountAddress)
-  }
-
-  newNetwork = (networkId: number) => {
-    this.emit(Events.newNetwork, networkId)
-  }
-
-  newTransaction = (
-    hash: string,
-    from: string,
-    to: string,
-    input: string,
-    type: string,
-    status: string
-  ) => {
-    const normalizedTo = normalizeLockAddress(to)
-    const normalizedFrom = normalizeLockAddress(from)
-    const newTransaction = {
-      hash,
-      from: normalizedFrom,
-      for: normalizedFrom,
-      to: normalizedTo,
-      input,
-      type,
-      status,
-      blockNumber: Number.MAX_SAFE_INTEGER,
-    }
-
-    this.emit(Events.newTransaction, newTransaction)
-  }
-
-  handleError = (error: any) => {
-    // TODO: why do we wrap this instead of just emitting the error
-    // from walletservice?
-    if (error.message === 'FAILED_TO_PURCHASE_KEY') {
-      this.emit(Events.error, new Error('purchase failed'))
-    }
+    this.walletService.on('account.changed', setAccount)
+    this.walletService.on('network.changed', setNetwork)
+    this.walletService.on('transaction.new', (...args: NewTransactionArgs) => {
+      addTransaction(formatTransaction(...args))
+    })
+    this.walletService.on('error', (error: Error) => {
+      // TODO: why do we wrap this instead of just emitting the error
+      // from walletservice?
+      if (error.message === 'FAILED_TO_PURCHASE_KEY') {
+        alertError(new Error('purchase failed'))
+      }
+    })
   }
 }
 
