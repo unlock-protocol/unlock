0c0d8bd9fe3287a7d112489f0bed3a0741a9a211
Julien Genestoux <julien.genestoux@gmail.com>
2019-12-14 18:47:06 -0500

    adding a method to retrieve the token information

--- newsletter/utils/token.js
+++ newsletter/utils/token.js
@@ -80,6 +80,85 @@ export const saveEmail = async (web3Provider, locks, email) => {
   return results.reduce((acc, value) => acc && value, true)
 }
 
+/**
+ * Function which yields a user's email address if it was saved for a lock
+ * @param {*} web3Provider
+ * @param {*} lockAddress
+ */
+export const getEmail = async (web3Provider, lockAddress) => {
+  const wallet = web3Provider.getSigner()
+  const userAddress = await wallet.getAddress()
+  const now = Date.now()
+
+  const tokenMetadata = JSON.stringify({
+    types: {
+      EIP712Domain: [
+        {
+          name: 'name',
+          type: 'string',
+        },
+        {
+          name: 'version',
+          type: 'string',
+        },
+        {
+          name: 'chainId',
+          type: 'uint256',
+        },
+        {
+          name: 'verifyingContract',
+          type: 'address',
+        },
+        {
+          name: 'salt',
+          type: 'bytes32',
+        },
+      ],
+      KeyMetadata: [],
+    },
+    domain: {
+      name: 'Unlock',
+      version: '1',
+    },
+    primaryType: 'KeyMetadata',
+    message: {
+      UserMetaData: {
+        owner: userAddress,
+        timestamp: now,
+      },
+    },
+  })
+
+  const signature = await web3Provider.send('personal_sign', [
+    ethers.utils.hexlify(ethers.utils.toUtf8Bytes(tokenMetadata)),
+    userAddress.toLowerCase(),
+  ])
+
+  const tokenEndpoint = `${config.locksmithUri}/api/key/${lockAddress}/user/${userAddress}?`
+
+  try {
+    const response = await axios.get(tokenEndpoint, {
+      params: {
+        data: tokenMetadata,
+      },
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer-Simple ${btoa(signature)}`,
+      },
+    })
+    if (
+      response.data &&
+      response.data.userMetadata &&
+      response.data.userMetadata.protected
+    ) {
+      return response.data.userMetadata.protected.email
+    }
+    return null
+  } catch (error) {
+    return null
+  }
+}
+
 export default {
   saveEmail,
 }
