0c0b87b98dd2c3d6b1ef7d0d082e411f4bb61843
Christopher Nascone <cnasc@users.noreply.github.com>
2019-12-16 20:49:52 -0500

    Wrap WalletService for data-iframe (#5488)
    * Function for wallet wrapper
    
    * Add wallet wrapper to index
    
    * Fix settimeout bug
    
    * Return walletService so we can connect a wallet to it when ready
    
    * Add ProxyWallet
    
    * Hook up proxy wallet

--- paywall/src/data-iframe/index.2.0.ts
+++ paywall/src/data-iframe/index.2.0.ts
@@ -1,8 +1,11 @@
 import Postmate from 'postmate'
 import { Web3Service } from '@unlock-protocol/unlock-js'
+import { walletWrapper } from './walletWrapper'
 import { BlockchainReader } from './BlockchainReader'
 import { BlockchainDataStorable } from './BlockchainDataStorable'
 import config from './constants'
+import { WalletServiceType } from './blockchainHandler/blockChainTypes'
+import { ProxyWallet } from './ProxyWallet'
 
 const {
   readOnlyProvider,
@@ -10,12 +13,17 @@ const {
   blockTime,
   requiredConfirmations,
 } = config
+
 const web3Service = new Web3Service({
   readOnlyProvider,
   unlockAddress,
   blockTime,
   requiredConfirmations,
 })
+
+let parent: Postmate.ChildAPI
+let walletService: WalletServiceType
+let proxyWallet: ProxyWallet
 
 // Start with a null object, it will be replaced when the authenticate
 // method in the model is called.
@@ -37,13 +45,21 @@ const initializeReader = ({ lockAddresses, accountAddress }: ReaderArgs) => {
   )
 }
 
+const connectProxyWallet = (walletInfo: any) => {
+  proxyWallet = new ProxyWallet(walletInfo, parent.emit)
+  walletService.connect(proxyWallet)
+}
+
 const handshake = new Postmate.Model({
   locks: () => blockchainReader.locks,
   keys: () => blockchainReader.keys,
   transactions: () => blockchainReader.transactions,
   initializeReader,
+  connectProxyWallet,
 })
 
 handshake.then(parent => {
-  parent.emit('ready')
+  walletService = walletWrapper(unlockAddress, (name, data) => {
+    parent.emit(name, data)
+  })
 })
