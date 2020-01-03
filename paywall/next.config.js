b5aa950a882df5ee3b80613dd94cbb62d3ce1438
Christoph Nascone <chris@unlock-protocol.com>
2020-01-03 16:45:10 -0500

    Sprucing up next config

--- paywall/next.config.js
+++ paywall/next.config.js
@@ -7,33 +7,16 @@ const configVariables = require('./environment')
 
 const copyFile = promisify(fs.copyFile)
 
-// TODO: remove this when next gets their act together
-// see https://github.com/zeit/next-plugins/issues/392#issuecomment-475845330
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  console.warn(
-    'HACK: Removing `minimize` option from `css-loader` entries in Webpack config'
-  )
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize
-        }
-      })
-    }
-  })
-}
-
 module.exports = withCSS({
   publicRuntimeConfig: {
     ...configVariables,
   },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
+    // TODO: is this still needed? How can we be sure?
     config.node = {
       fs: 'empty',
     }
-    HACK_removeMinimizeOptionFromCssLoaders(config)
 
     return config
   },
