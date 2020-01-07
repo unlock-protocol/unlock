65cfdcddf4737c808cd9deb2b2d9f1edae12930b
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-07 09:39:39 -0500

    Eliminates use of integration test environment variable setup script (#5616)
    As part of adjusting our docker setup, and eliminating some of the moving pieces around
    the state of the environment, we are removing the script and shifting towards a more standarized
    approach of using env files for our test setup.
    
    This will be smoothed out further in subsequent paths.
    
    Leveraging an integration tests specific env file, we should now be able to eliminate this script

--- scripts/local-docker-integration-tests.sh
+++ scripts/local-docker-integration-tests.sh
@@ -11,9 +11,6 @@ REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 EXTRA_ARGS=$*
 
-# set the environment variables needed for integration testing
-eval "$($REPO_ROOT/scripts/set-integration-tests-env-variables.sh)"
-
 # if the integration test images are running, this ensures we remove any state
 # prior to attempting to run the tests. This line is critical!
 docker-compose -f $DOCKER_COMPOSE_FILE down
