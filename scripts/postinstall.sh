3b5d7f5791913bb0dc69e3eb01463e6644f59a13
Christoph Nascone <chris@unlock-protocol.com>
2020-01-07 10:32:05 -0500

    docker/development uses yarn

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,30 +6,10 @@
 
 if [ "$SKIP_SERVICES" != "true" ]; then
 
-  SERVICES=( docker/development )
   ROOT_DIR=$(pwd)
-  for i in "${SERVICES[@]}"
-  do
-      cd $i
-      npm ci # We run npm ci by default. &
-      cd $ROOT_DIR
-  done
-
-  wait
-
-  # Copy the parent binaries into the sub projects
-  npm run link-parent-bin
-  # remove node_modules from subfolders who do not need it
-  # TODO: fix link-parent-bin to only run in the folders where we need it!
-  rm -rf .circleci/node_modules
-  rm -rf .git/node_modules
-  rm -rf .github/node_modules
-  rm -rf docker/node_modules
-  rm -rf scripts/node_modules
-  rm -rf versions/node_modules
-  rm -rf wiki/node_modules
-
-  NEW_SERVICES=(
+  
+  SERVICES=(
+      docker/development
       locksmith
       newsletter
       paywall
@@ -41,11 +21,13 @@ if [ "$SKIP_SERVICES" != "true" ]; then
       unlock-protocol.com
       wedlocks
   )
-  for i in "${NEW_SERVICES[@]}"
+  
+  for i in "${SERVICES[@]}"
   do
       cd $i
       rm -rf node_modules/
       yarn
       cd $ROOT_DIR
   done
+  
 fi
