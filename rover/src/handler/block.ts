89d8e1da2832a3e7ee63224bc8ea07041051c4ef
Julien Genestoux <julien.genestoux@gmail.com>
2020-01-09 10:48:17 -0500

    removed rover

--- rover/src/handler/block.ts
+++
@@ -1,8 +0,0 @@
-export async function blockHandler(storage, blockNumber, emitter, provider) {
-  let blockDetails = await provider.getBlock(blockNumber)
-  storage.storeBlock(blockDetails)
-
-  blockDetails.transactions.forEach(transactionHash => {
-    emitter.emit('transaction', transactionHash)
-  })
-}
