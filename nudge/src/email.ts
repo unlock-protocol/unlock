c220f35578bbf52a3ab557624737bbf80bca7ffb
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-17 16:18:42 -0500

    Adding email wrapping lib (#5746)
    * Adding email wrapping lib

---
+++ nudge/src/email.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv'
+import request from 'request-promise-native'
+
+dotenv.config()
+const config = require('../config/config')
+
+export class Email {
+  static async dispatch(key: Key): Promise<boolean> {
+    let body = {
+      template: 'keyMined',
+      recipient: key.emailAddress,
+      params: {
+        keyId: key.keyId,
+        lockName: 'The Ethereal Party',
+        keychainUrl: 'https://app.unlock-protocol.com/keychain/',
+      },
+    }
+
+    let options = {
+      body,
+      json: true,
+      method: 'POST',
+      uri: config.wedlocksURI,
+      resolveWithFullResponse: true,
+    }
+
+    let dispatchResponse = await request(options)
+    return dispatchResponse.statusCode === 204
+  }
+}
