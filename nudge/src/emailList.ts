729f390af18d46e33a9ca5f3c08f9405d9a4a2c8
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-17 18:12:13 -0500

    Add library to enrich token information with email address when appropriate

---
+++ nudge/src/emailList.ts
@@ -0,0 +1,21 @@
+import { generateKeyMetadata } from './metadataOperations'
+
+export async function extractEmails(results: any[]): Promise<Key[]> {
+  let keysData = results.map(async (result: any) => {
+    let keyMetadata: any
+
+    try {
+      keyMetadata = await generateKeyMetadata(result.lockAddress, result.keyId)
+    } catch (_) {
+      keyMetadata = {}
+    }
+
+    return {
+      lockAddress: result.lockAddress,
+      keyId: result.keyId,
+      emailAddress: keyMetadata.userMetadata?.protected?.emailAddress,
+    } as Key
+  })
+
+  return Promise.all(keysData)
+}
