44b95e5bb879416d4f9d2a3f2f91a04c82749f12
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-17 11:17:11 -0500

    Adds utils & associated tests
    There is a bit of associated config coming a long for the ride as the tests can not run without it

---
+++ nudge/tests/utils/keyData.test.ts
@@ -0,0 +1,29 @@
+import KeyData from '../../src/utils/keyData'
+
+describe('KeyData', () => {
+  const keyData = new KeyData('http://someprovider')
+
+  describe('openSeaPresentation', () => {
+    describe('when the data has an expiration', () => {
+      it('returns the data structured for OpenSea', () => {
+        expect.assertions(1)
+        expect(keyData.openSeaPresentation({ expiration: 1234 })).toEqual({
+          attributes: [
+            {
+              display_type: 'date',
+              trait_type: 'Expiration',
+              value: 1234,
+            },
+          ],
+        })
+      })
+    })
+
+    describe('when the data is missing an expiration', () => {
+      it('passes the data through', () => {
+        expect.assertions(1)
+        expect(keyData.openSeaPresentation({})).toEqual({})
+      })
+    })
+  })
+})
