79816bc44d25e934fd3e678b4b214bcf44a48862
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-17 11:38:00 -0500

    Adds utils & associated tests (#5736)
    There is a bit of associated config coming a long for the ride as the tests can not run without it

---
+++ nudge/tests/utils/normalizer.test.ts
@@ -0,0 +1,21 @@
+import * as Normalizer from '../../src/utils/normalizer'
+
+describe('Normalizer', () => {
+  describe('emailAddress', () => {
+    it('returns a normalized email address', () => {
+      expect.assertions(1)
+      expect(Normalizer.emailAddress('TEST@EXAMPLE.COM')).toEqual(
+        'test@example.com'
+      )
+    })
+  })
+
+  describe('ethereumAddress', () => {
+    it('returns a normalized ethereum address', () => {
+      expect.assertions(1)
+      expect(
+        Normalizer.ethereumAddress('0xaaadeed4c0b861cb36f4ce006a9c90ba2e43fdc2')
+      ).toEqual('0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2')
+    })
+  })
+})
