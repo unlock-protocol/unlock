2c219a1c2d79b04e4ca1c84de13e370a7acf6fc1
Julien Genestoux <julien.genestoux@gmail.com>
2020-01-20 17:56:55 -0500

    updated tests for v0/getLock (#5775)

--- unlock-js/src/__tests__/helpers/contracts.js
+++ unlock-js/src/__tests__/helpers/contracts.js
@@ -1,12 +1,8 @@
 import { ethers } from 'ethers'
 
 // A helper which yields ether contract objects
-export const getTestUnlockContract = ({
-  unlockAddress = '0x559247Ec8A8771E8C97cDd39b96b9255651E39C5',
-  abi,
-  provider,
-}) => {
-  const contract = new ethers.Contract(unlockAddress, abi, provider)
+export const getTestContract = ({ address, abi, provider }) => {
+  const contract = new ethers.Contract(address, abi, provider)
 
   // WARNING: this is really strange
   // The ethers contract object defines read only properties
@@ -16,4 +12,18 @@ export const getTestUnlockContract = ({
   return Object.create({
     ...contract,
   })
+}
+
+// A helper which yields an unlockContact
+export const getTestUnlockContract = ({
+  unlockAddress = '0x559247Ec8A8771E8C97cDd39b96b9255651E39C5',
+  abi,
+  provider,
+}) => {
+  return getTestContract({ address: unlockAddress, abi, provider })
+}
+
+// A helper which yields a lock contract
+export const getTestLockContract = ({ lockAddress, abi, provider }) => {
+  return getTestContract({ address: lockAddress, abi, provider })
 }
