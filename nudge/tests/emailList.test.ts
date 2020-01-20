5c9872ba5cb5547574804983e5a85a1965ca0a4d
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-20 10:18:48 -0500

    Add library to enrich token information with email address when appropriate (#5751)
    * Add library to enrich token information with email address when appropriate

---
+++ nudge/tests/emailList.test.ts
@@ -0,0 +1,88 @@
+import { extractEmails } from '../src/emailList'
+
+jest.mock('../src/generateKeyMetadata', () => {
+  return {
+    generateKeyMetadata: jest
+      .fn()
+      .mockResolvedValueOnce({
+        userMetadata: {
+          protected: {
+            emailAddress: 'email@example.com',
+          },
+        },
+      })
+      .mockResolvedValueOnce({
+        userMetadata: {},
+      })
+      .mockRejectedValueOnce({}),
+  }
+})
+
+describe('extractEmails', () => {
+  describe('when passing an empty collection', () => {
+    it('returns an empty collection', async () => {
+      expect.assertions(1)
+      expect(await extractEmails([])).toEqual([])
+    })
+  })
+
+  describe('when a non-empty collection with available email data', () => {
+    it('return a collection of items and associated email addresses', async () => {
+      expect.assertions(1)
+      expect(
+        await extractEmails([
+          {
+            lockAddress: 'valid lock address',
+            keyId: '1',
+          },
+        ])
+      ).toEqual([
+        {
+          lockAddress: 'valid lock address',
+          keyId: '1',
+          emailAddress: 'email@example.com',
+        },
+      ])
+    })
+  })
+
+  describe('when a non-empty collection without available email data', () => {
+    it('return a collection of items without associated email addresses', async () => {
+      expect.assertions(1)
+      expect(
+        await extractEmails([
+          {
+            lockAddress: 'valid lock address',
+            keyId: '2',
+          },
+        ])
+      ).toEqual([
+        {
+          lockAddress: 'valid lock address',
+          keyId: '2',
+          emailAddress: undefined,
+        },
+      ])
+    })
+  })
+
+  describe('when something fails attempting to generate metadata', () => {
+    it('return a collection of items without associated email addresses', async () => {
+      expect.assertions(1)
+      expect(
+        await extractEmails([
+          {
+            lockAddress: 'valid lock address',
+            keyId: '3',
+          },
+        ])
+      ).toEqual([
+        {
+          lockAddress: 'valid lock address',
+          keyId: '3',
+          emailAddress: undefined,
+        },
+      ])
+    })
+  })
+})
