a0ba12b82924cc58df1cfd0d7c2ed04dc4c573e6
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-22 16:55:56 -0500

    Setups project to run in our development environment
    * Dockerfile for local dev
    * Update cron config to run every 5 minutes
    * Adds migration task to eliminate manual running

--- nudge/src/index.ts
+++ nudge/src/index.ts
@@ -3,8 +3,10 @@ import { Processor } from './processor'
 
 const config = require('../config/config')
 const approvedLocks: string[] = []
+const runInterval = '*/5 * * * *'
 
-new CronJob(
-  '5 * * * *',
-  new Processor(config.graphQLEndpoint, approvedLocks).processKeys
-).start()
+new CronJob(runInterval, () => {
+  console.log(config)
+
+  new Processor(config.graphQLEndpoint, approvedLocks).processKeys()
+}).start()
