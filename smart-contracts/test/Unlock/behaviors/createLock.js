0cfd46a1bab92a853dec166f0d66718b9626930d
NickCuso <hardlydiff@gmail.com>
2020-01-21 17:11:54 -0800

    Add a test 'behavior' pattern for testing different Lock configs
    Test fix
    
    
    Style update
    
    
    Update Unlock behaviors to match

--- smart-contracts/test/Unlock/behaviors/createLock.js
+++ smart-contracts/test/Unlock/behaviors/createLock.js
@@ -4,12 +4,19 @@ const shouldFail = require('../../helpers/shouldFail')
 
 const PublicLock = artifacts.require('../../PublicLock.sol')
 
-exports.shouldCreateLock = function(accounts) {
-  describe('Unlock / behaviors / createLock', function() {
-    describe('lock created successfully', function() {
+exports.shouldCreateLock = options => {
+  describe('Unlock / behaviors / createLock', () => {
+    let unlock
+    let accounts
+
+    beforeEach(async () => {
+      ;({ unlock, accounts } = options)
+    })
+
+    describe('lock created successfully', () => {
       let transaction
-      beforeEach(async function() {
-        transaction = await this.unlock.methods
+      beforeEach(async () => {
+        transaction = await unlock.methods
           .createLock(
             60 * 60 * 24 * 30, // expirationDuration: 30 days
             Web3Utils.padLeft(0, 40),
@@ -24,18 +31,18 @@ exports.shouldCreateLock = function(accounts) {
           })
       })
 
-      it('should have kept track of the Lock inside Unlock with the right balances', async function() {
+      it('should have kept track of the Lock inside Unlock with the right balances', async () => {
         let publicLock = await PublicLock.at(
           transaction.events.NewLock.returnValues.newLockAddress
         )
         // This is a bit of a dumb test because when the lock is missing, the value are 0 anyway...
-        let results = await this.unlock.methods.locks(publicLock.address).call()
+        let results = await unlock.methods.locks(publicLock.address).call()
         assert.equal(results.deployed, true)
         assert.equal(results.totalSales, 0)
         assert.equal(results.yieldedDiscountTokens, 0)
       })
 
-      it('should trigger the NewLock event', function() {
+      it('should trigger the NewLock event', () => {
         const event = transaction.events.NewLock
         assert(event)
         assert.equal(
@@ -45,22 +52,22 @@ exports.shouldCreateLock = function(accounts) {
         assert(event.returnValues.newLockAddress)
       })
 
-      it('should have created the lock with the right address for unlock', async function() {
+      it('should have created the lock with the right address for unlock', async () => {
         let publicLock = await PublicLock.at(
           transaction.events.NewLock.returnValues.newLockAddress
         )
         let unlockProtocol = await publicLock.unlockProtocol.call()
         assert.equal(
           Web3Utils.toChecksumAddress(unlockProtocol),
-          Web3Utils.toChecksumAddress(this.unlock.address)
+          Web3Utils.toChecksumAddress(unlock.address)
         )
       })
     })
 
-    describe('lock creation fails', function() {
-      it('should fail if expirationDuration is too large', async function() {
+    describe('lock creation fails', () => {
+      it('should fail if expirationDuration is too large', async () => {
         await shouldFail(
-          this.unlock.methods
+          unlock.methods
             .createLock(
               60 * 60 * 24 * 365 * 101, // expirationDuration: 101 years
               Web3Utils.padLeft(0, 40),
