db195b7c7d33459c088d55de8db1084b6e71e8c5
Julien Genestoux <julien.genestoux@gmail.com>
2020-01-24 21:57:47 -0500

    removed useless params on demo link

--- paywall/src/constants.js
+++ paywall/src/constants.js
@@ -129,4 +129,4 @@ export const STABLECOINS_ADDRESSES = [
   '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
   '0xaFF4481D10270F50f203E0763e2597776068CBc5', // WEENUS
-]
+].map(address => address.toLowerCase())
