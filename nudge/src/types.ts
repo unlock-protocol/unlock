d123fcb0280b045a89ca768a3b902991eff6ba7f
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-01-28 10:21:18 -0500

    Nudge add lockname email (#5841)
    * Updates Nudge to pass Lock names to email dispatch

--- nudge/src/types.ts
+++ nudge/src/types.ts
@@ -1,11 +1,12 @@
 interface Key {
-  lockAddress: string
   emailAddress: string | undefined
   keyId: string
+  lockAddress: string
+  lockName: string| null
 }
 
 interface UserTokenMetadataInput {
+  data: any
   tokenAddress: string
   userAddress: string
-  data: any
 }
