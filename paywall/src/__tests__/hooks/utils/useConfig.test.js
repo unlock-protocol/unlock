c4a0830c171fc18435c10e0e8aaf8bbb1149ffe2
Christopher Nascone <cnasc@users.noreply.github.com>
2020-01-29 14:43:27 -0500

    Update to a stable version of @testing-library/react-hooks (#5854)
    * Update to a stable version of @testing-library/react-hooks
    
    * Update import

--- paywall/src/__tests__/hooks/utils/useConfig.test.js
+++ paywall/src/__tests__/hooks/utils/useConfig.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { renderHook } from 'react-hooks-testing-library'
+import { renderHook } from '@testing-library/react-hooks'
 
 import configuration from '../../../config'
 import useConfig from '../../../hooks/utils/useConfig'
