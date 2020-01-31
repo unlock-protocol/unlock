6e08040d9977a527cae8bbdca605d4d4a232a36d
Nick Furfaro <nfurfaro33@gmail.com>
2020-01-30 18:15:39 -0700

    Moving the _timeMachine function to MixinKeys (#5859)
    * Move timeMachine to mixinKeys
    
    * Add comment for new event
    
    * Remove old Event

--- smart-contracts/contracts/mixins/MixinTransfer.sol
+++ smart-contracts/contracts/mixins/MixinTransfer.sol
@@ -30,12 +30,6 @@ contract MixinTransfer is
     uint transferFeeBasisPoints
   );
 
-  event ExpirationChanged(
-    uint indexed _tokenId,
-    uint _amount,
-    bool _timeAdded
-  );
-
   // 0x150b7a02 == bytes4(keccak256('onERC721Received(address,address,uint256,bytes)'))
   bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
 
@@ -252,39 +246,6 @@ contract MixinTransfer is
     }
     fee = timeToTransfer.mul(transferFeeBasisPoints) / BASIS_POINTS_DEN;
     return fee;
-  }
-
-  /**
-  * @notice Modify the expirationTimestamp of a key
-  * by a given amount.
-  * @param _tokenId The ID of the key to modify.
-  * @param _deltaT The amount of time in seconds by which
-  * to modify the keys expirationTimestamp
-  * @param _addTime Choose whether to increase or decrease
-  * expirationTimestamp (false == decrease, true == increase)
-  * @dev Throws if owner does not have a valid key.
-  */
-  function _timeMachine(
-    uint _tokenId,
-    uint256 _deltaT,
-    bool _addTime
-  ) internal
-  {
-    address tokenOwner = _ownerOf[_tokenId];
-    require(tokenOwner != address(0), 'NON_EXISTENT_KEY');
-    Key storage key = keyByOwner[tokenOwner];
-    uint formerTimestamp = key.expirationTimestamp;
-    bool validKey = getHasValidKey(tokenOwner);
-    if(_addTime) {
-      if(validKey) {
-        key.expirationTimestamp = formerTimestamp.add(_deltaT);
-      } else {
-        key.expirationTimestamp = block.timestamp.add(_deltaT);
-      }
-    } else {
-      key.expirationTimestamp = formerTimestamp.sub(_deltaT);
-    }
-    emit ExpirationChanged(_tokenId, _deltaT, _addTime);
   }
 
   /**
