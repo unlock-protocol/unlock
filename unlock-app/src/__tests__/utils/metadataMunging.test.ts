5d3bbe1fbd6e41e0424e3facc21636f4edc33235
Julien Genestoux <julien.genestoux@gmail.com>
2020-02-04 18:10:18 -0500

    removed metadata signing handling via redux (#5910)

--- unlock-app/src/__tests__/utils/metadataMunging.test.ts
+++ unlock-app/src/__tests__/utils/metadataMunging.test.ts
@@ -1,8 +1,4 @@
-import {
-  generateColumns,
-  mergeSingleDatum,
-  mergeKeyholderMetadata,
-} from '../../utils/metadataMunging'
+import { generateColumns } from '../../utils/metadataMunging'
 
 const baseKeyMetadata = {
   lockName: 'a lock',
@@ -63,104 +59,6 @@ describe('metadata munging functions', () => {
         'expiration',
         'email',
         'xylophone',
-      ])
-    })
-  })
-
-  describe('mergeSingleDatum', () => {
-    it('merges a base metadatum with additional fields from stored metadata', () => {
-      expect.assertions(1)
-      const lock = {
-        address: '0x123',
-        name: 'Lock',
-      }
-      const key = {
-        keyId: '1',
-        expiration: '123456',
-        owner: {
-          address: '0x123',
-        },
-      }
-      const storedMetadata = {
-        [lock.address]: {
-          [key.owner.address]: {
-            protected: {
-              zebra: 'true',
-            },
-            public: {
-              elephant: 'trunk',
-            },
-          },
-        },
-      }
-      const metadatum = mergeSingleDatum(lock, key, storedMetadata)
-      expect(metadatum).toEqual({
-        lockName: 'Lock',
-        expiration: 'Expired',
-        keyholderAddress: '0x123',
-        elephant: 'trunk',
-        zebra: 'true',
-      })
-    })
-  })
-
-  describe('mergeKeyholderMetadata', () => {
-    it('returns empty array when there are no locks', () => {
-      expect.assertions(1)
-      expect(
-        mergeKeyholderMetadata(
-          {
-            locks: [],
-          },
-          {}
-        )
-      ).toEqual([])
-    })
-
-    it('adds metadata for each key in a lock', () => {
-      expect.assertions(1)
-      const data = {
-        locks: [
-          {
-            address: '0x123',
-            name: 'Lock',
-            keys: [
-              {
-                keyId: '1',
-                expiration: '123456',
-                owner: {
-                  address: '0x126',
-                },
-              },
-            ],
-          },
-          {
-            address: '0x124',
-            name: 'Dock',
-            keys: [
-              {
-                keyId: '1',
-                expiration: '123556',
-                owner: {
-                  address: '0x127',
-                },
-              },
-            ],
-          },
-        ],
-      }
-
-      expect(mergeKeyholderMetadata(data, {})).toEqual([
-        {
-          expiration: 'Expired',
-          keyholderAddress: '0x126',
-          lockName: 'Lock',
-        },
-        {
-          expiration: 'Expired',
-          keyholderAddress: '0x127',
-          lockName: 'Dock',
-        },
       ])
     })
   })
