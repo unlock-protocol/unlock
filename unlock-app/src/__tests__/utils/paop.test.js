9649842038c2ef9010e11641183a0523f804222c
Julien Genestoux <julien.genestoux@gmail.com>
2020-02-07 09:02:38 -0500

    adding integration to POAP (#5918)

---
+++ unlock-app/src/__tests__/utils/paop.test.js
@@ -0,0 +1,51 @@
+import pingPoap from '../../utils/poap'
+
+describe('pingPoap', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
+  it('should ping the poap server with the right data', () => {
+    expect.assertions(3)
+    fetch.mockResponseOnce(JSON.stringify({ data: '' }))
+    const owner = '0xowner'
+    const key = {
+      lock: {
+        address: '0xlock',
+      },
+    }
+    const signature = 'signature'
+    const timestamp = 123
+    pingPoap(key, owner, signature, timestamp)
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][0]).toEqual('https://api.poap.xyz/tasks/')
+    expect(fetch.mock.calls[0][1]).toEqual({
+      body:
+        '{"accountAddress":"0xowner","lockAddress":"0xlock","timestamp":123,"signature":"signature"}',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'a75495d2-c9c5-494c-9ba4-80976b371bae',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+  })
+
+  it('should not fail if fetch failed', () => {
+    expect.assertions(2)
+    const owner = '0xowner'
+    const key = {
+      lock: {
+        address: '0xlock',
+      },
+    }
+    const signature = 'signature'
+    const timestamp = 123
+    const consoleSpy = jest.spyOn(global.console, 'error')
+    fetch.mockReject(() => {
+      throw new Error('fake error message')
+    })
+    expect(() => pingPoap(key, owner, signature, timestamp)).not.toThrow()
+    expect(consoleSpy).toHaveBeenCalled()
+  })
+})
---
+++ unlock-app/src/utils/poap.js
@@ -0,0 +1,33 @@
+/**
+ * Helper function to send a request to POAP when a
+ * user is checked-in.
+ * @param {*} key
+ * @param {*} signature
+ * @param {*} timestamp
+ */
+export const pingPoap = (key, owner, signature, timestamp) => {
+  try {
+    // Only on the web!
+    if (typeof fetch !== 'undefined') {
+      fetch('https://api.poap.xyz/tasks/', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'a75495d2-c9c5-494c-9ba4-80976b371bae',
+        },
+        body: JSON.stringify({
+          accountAddress: owner,
+          lockAddress: key.lock.address,
+          timestamp,
+          signature,
+        }),
+      })
+    }
+  } catch (e) {
+    // Fail silently. Our POAP integration is best effort.
+    console.error(e)
+  }
+}
+
+export default pingPoap
