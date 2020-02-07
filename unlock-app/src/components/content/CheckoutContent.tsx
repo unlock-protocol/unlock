d40d7477fd31550bd0a4411172381d33b81c9329
Christoph Nascone <chris@unlock-protocol.com>
2020-02-07 14:03:26 -0500

    CheckoutContent can retrieve paywall config from URL param

--- unlock-app/src/components/content/CheckoutContent.tsx
+++ unlock-app/src/components/content/CheckoutContent.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Head from 'next/head'
+import queryString from 'query-string'
 import BrowserOnly from '../helpers/BrowserOnly'
 import Layout from '../interface/Layout'
 import Account from '../interface/Account'
 import { pageTitle } from '../../constants'
 import LogInSignUp from '../interface/LogInSignUp'
-import { Account as AccountType, Network } from '../../unlockTypes'
+import {
+  Account as AccountType,
+  Network,
+  Router,
+  PaywallConfig,
+} from '../../unlockTypes'
+import getConfigFromSearch from '../../utils/getConfigFromSearch'
 
 interface CheckoutContentProps {
   account: AccountType
   network: Network
+  config?: PaywallConfig
 }
 
 export const CheckoutContent = ({ account, network }: CheckoutContentProps) => {
@@ -35,12 +43,18 @@ export const CheckoutContent = ({ account, network }: CheckoutContentProps) => {
 interface ReduxState {
   account: AccountType
   network: Network
+  router: Router
 }
 
-export const mapStateToProps = ({ account, network }: ReduxState) => {
+export const mapStateToProps = ({ account, network, router }: ReduxState) => {
+  const search = queryString.parse(router.location.search)
+
+  const config = getConfigFromSearch(search)
+
   return {
     account,
     network,
+    config,
   }
 }
 
