0393bc84f4f4e4fbc6c59909f98e722cc7628a67
Nick Furfaro <nfurfaro33@gmail.com>
2020-02-14 11:04:49 -0700

    Add 2 test cases

--- smart-contracts/test/Lock/permissions/keyManager.js
+++ smart-contracts/test/Lock/permissions/keyManager.js
@@ -4,12 +4,24 @@ const getProxy = require('../../helpers/proxy')
 
 const unlockContract = artifacts.require('../Unlock.sol')
 
+let lockCreator
+const LockManager = lockCreator
+const KeyGranter = lockCreator
+
 contract('Permissions / KeyManager', accounts => {
+  lockCreator = accounts[0]
+
+  before(async () => {
+    unlock = await getProxy(unlockContract)
+    locks = await deployLocks(unlock, lockCreator)
+    lock = locks.FIRST
+  })
+
   describe('Key Purchases', () => {
     // KM == KeyManager
     it('should set KM == keyOwner for new purchases', async () => {})
-    it('should not change KM when re-newing expired keys', async () => {})
     it('should not change KM when topping-up valid keys', async () => {})
+    it('should not change KM when re-newing expired keys', async () => {})
   })
   describe('Key Transfers', () => {
     it('should set KM == keyOwner for new recipients', async () => {})
@@ -22,5 +34,10 @@ contract('Permissions / KeyManager', accounts => {
   describe('Key Granting', () => {
     it('should let KeyGranter set an arbitrary KM for granted key', async () => {})
     it('should let KeyGranter set no KM for granted key', async () => {})
+  })
+
+  describe('updating the key manager', () => {
+    it('should allow the current keyManager to set a new KM', async () => {})
+    it('should allow a LockManager to set a new KM', async () => {})
   })
 })
