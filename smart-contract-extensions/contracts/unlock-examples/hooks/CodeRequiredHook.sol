a6a536a1b5e1394f59afcadc2625e3141d4c509c
Hardly Difficult <hardlydiff@gmail.com>
2020-02-16 18:03:15 -0800

    Add CodeRequiredHook (#5984)
    * Add CodeRequiredHook
    
    * Remove `fixSignature`
    
    * Require non-zero answer on creation
    
    * Update comment
    
    * Bump to hd-contracts 9
    
    * Rename to lockCreator
    
    * Lint
    
    * Lint
    
    * Remove 1820

---
+++ smart-contract-extensions/contracts/unlock-examples/hooks/CodeRequiredHook.sol
@@ -0,0 +1,66 @@
+pragma solidity 0.5.16;
+
+import '@openzeppelin/contracts/cryptography/ECDSA.sol';
+import '@openzeppelin/contracts/access/roles/WhitelistAdminRole.sol';
+import '@openzeppelin/contracts/introspection/IERC1820Registry.sol';
+import 'hardlydifficult-ethereum-contracts/contracts/proxies/CallContract.sol';
+
+/**
+ * @notice Used with a Lock in order to require the user knows
+ * a code in order to buy.
+ * @dev Set this contract as the beneficiary of the lock.
+ */
+contract CodeRequiredHook is WhitelistAdminRole
+{
+  using CallContract for address;
+
+  IERC1820Registry public constant erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+
+  // `keccak256("IUnlockEventHooks_keySold")`
+  bytes32 public constant keySoldInterfaceId = 0x4d99da10ff5120f726d35edd8dbd417bbe55d90453b8432acd284e650ee2c6f0;
+
+  /**
+   * @notice The answer expressed as an address where the private key is
+   * keccak256(abi.encode(answer, lock.address))
+   */
+  address public answerAddress;
+
+  constructor(
+    address _answerAddress
+  ) public
+  {
+    require(_answerAddress != address(0), 'INVALID_ANSWER');
+    answerAddress = _answerAddress;
+    erc1820.setInterfaceImplementer(address(this), keySoldInterfaceId, address(this));
+  }
+
+  /**
+   * @notice This is called by the Lock when a purchase is attempted.
+   * When this fails so does the purchase itself.
+   * @param _data contains the signature from the `answerAddress`.
+   */
+  function keySold(
+    address /* _from */,
+    address _to,
+    address /* _referrer */,
+    uint /* _pricePaid */,
+    bytes calldata _data
+  ) external view
+  {
+    // Confirm `_to` (the new keyOwner)
+    bytes32 secretHash = ECDSA.toEthSignedMessageHash(keccak256(abi.encode(_to)));
+    require(ECDSA.recover(secretHash, _data) == answerAddress, 'INCORRECT_CODE');
+  }
+
+  /**
+   * @notice This call allows the owner to make any call, such as calling updateBeneficiary on the lock
+   * which would remove the code requirement.
+   */
+  function proxyCall(
+    address _contract,
+    bytes memory _callData
+  ) public onlyWhitelistAdmin()
+  {
+    _contract._call(_callData, 0);
+  }
+}
---
+++ smart-contract-extensions/test/unlock-examples/hooks/codeRequiredHook.js
@@ -0,0 +1,108 @@
+const { constants, protocols } = require('hardlydifficult-ethereum-contracts')
+const { reverts } = require('truffle-assertions')
+
+const CodeRequiredHook = artifacts.require('CodeRequiredHook.sol')
+
+contract('CodeRequiredHook', accounts => {
+  const lockCreator = accounts[1]
+  const keyBuyer = accounts[5]
+  let lock
+  let hookContract
+  // Never expose the answer or its PK directly in JS for any integrations
+  // Only the answer account's address can safely be exposed
+  let answerAccount
+
+  beforeEach(async () => {
+    // Create a free lock
+    lock = await protocols.unlock.createTestLock(web3, {
+      keyPrice: '0',
+      from: lockCreator,
+    })
+
+    // Calculate the answer account
+    const answer = 'super secret hard to guess answer goes here'
+    const answerPK = web3.eth.abi.encodeParameters(
+      ['address', 'bytes32'],
+      // By including the lock address in the answerPK, we can have
+      // answers reused for multiple locks without that being visible on-chain
+      [lock.address, web3.utils.keccak256(answer)]
+    )
+    answerAccount = web3.eth.accounts.privateKeyToAccount(answerPK)
+
+    // Create the hook contract with that answer
+    hookContract = await CodeRequiredHook.new(answerAccount.address, {
+      from: lockCreator,
+    })
+
+    // Change the beneficiary to the hook to enable it
+    await lock.updateBeneficiary(hookContract.address, { from: lockCreator })
+  })
+
+  it('can buy if you know the answer', async () => {
+    const messageToSign = web3.utils.keccak256(
+      web3.eth.abi.encodeParameters(['address'], [keyBuyer])
+    )
+    const signature = (await answerAccount.sign(messageToSign)).signature
+    await lock.purchase('0', keyBuyer, constants.ZERO_ADDRESS, signature, {
+      from: keyBuyer,
+    })
+  })
+
+  it('can not buy with no answer provided', async () => {
+    await reverts(
+      lock.purchase('0', keyBuyer, constants.ZERO_ADDRESS, [], {
+        from: keyBuyer,
+      }),
+      'INCORRECT_CODE'
+    )
+  })
+
+  it('can not buy with an incorrect answer provided', async () => {
+    // Calculate an incorrect answer
+    const answer = 'WRONG'
+    const answerPK = web3.eth.abi.encodeParameters(
+      ['address', 'bytes32'],
+      // By including the lock address in the answerPK, we can have
+      // answers reused for multiple locks without that being visible on-chain
+      [lock.address, web3.utils.keccak256(answer)]
+    )
+    const wrongAnswerAccount = web3.eth.accounts.privateKeyToAccount(answerPK)
+    const messageToSign = web3.utils.keccak256(
+      web3.eth.abi.encodeParameters(['address'], [keyBuyer])
+    )
+    const signature = (await wrongAnswerAccount.sign(messageToSign)).signature
+    await reverts(
+      lock.purchase('0', keyBuyer, constants.ZERO_ADDRESS, signature, {
+        from: keyBuyer,
+      }),
+      'INCORRECT_CODE'
+    )
+  })
+
+  describe('uninstall hook', () => {
+    beforeEach(async () => {
+      const callData = lock.contract.methods
+        .updateBeneficiary(lockCreator)
+        .encodeABI()
+      await hookContract.proxyCall(lock.address, callData, {
+        from: lockCreator,
+      })
+    })
+
+    it('can now buy without the answer', async () => {
+      await lock.purchase('0', keyBuyer, constants.ZERO_ADDRESS, [], {
+        from: keyBuyer,
+      })
+    })
+  })
+
+  it('non-admins cannot call proxyCall', async () => {
+    const callData = lock.contract.methods
+      .updateBeneficiary(lockCreator)
+      .encodeABI()
+    await reverts(
+      hookContract.proxyCall(lock.address, callData, { from: accounts[0] }),
+      'WhitelistAdminRole: caller does not have the WhitelistAdmin role'
+    )
+  })
+})
