961bb1b99fa0ea8daa8a2a2f0273d87a8b10689f
Christopher Nascone <cnasc@users.noreply.github.com>
2020-02-18 16:27:26 -0500

    Copy some checkout components into unlock-app (#6014)
    * Copy some checkout components into unlock-app
    
    * Add tests to boost coverage

---
+++ unlock-app/src/__tests__/components/interface/checkout/CheckoutWrapper.test.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import CheckoutWrapper from '../../../../components/interface/checkout/CheckoutWrapper'
+
+describe('CheckoutWrapper', () => {
+  it('renders the footer', () => {
+    expect.assertions(1)
+
+    const hideCheckout = jest.fn()
+    const allowClose = false
+
+    const { container } = rtl.render(
+      <CheckoutWrapper hideCheckout={hideCheckout} allowClose={allowClose} />
+    )
+
+    // the close button is also an a tag, but it doesn't render when allowClose is false
+    expect(container.getElementsByTagName('a')).toHaveLength(1)
+  })
+
+  it('does not have a close button when allowClose is false', () => {
+    expect.assertions(1)
+
+    const hideCheckout = jest.fn()
+    const allowClose = false
+
+    const { queryByTitle } = rtl.render(
+      <CheckoutWrapper hideCheckout={hideCheckout} allowClose={allowClose} />
+    )
+
+    expect(queryByTitle('Close')).toBeNull()
+  })
+
+  it('calls hideCheckout on click when allowClose is true', () => {
+    expect.assertions(2)
+
+    const hideCheckout = jest.fn()
+    const allowClose = true
+
+    const { getByTitle } = rtl.render(
+      <CheckoutWrapper hideCheckout={hideCheckout} allowClose={allowClose} />
+    )
+
+    const button = getByTitle('Close')
+
+    expect(hideCheckout).not.toHaveBeenCalled()
+
+    rtl.fireEvent.click(button)
+
+    expect(hideCheckout).toHaveBeenCalled()
+  })
+})
