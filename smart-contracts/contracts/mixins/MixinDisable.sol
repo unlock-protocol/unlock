950b918215d144c3968b9fda0dc01cfb0ebc56b0
Unlock Deployer <ops@unlock-protocol.com>
2020-02-19 20:30:38 +0000

    Automated deploy as of commit:a6a536a1b5e1394f59afcadc2625e3141d4c509c

--- smart-contracts/contracts/mixins/MixinDisable.sol
+++ smart-contracts/contracts/mixins/MixinDisable.sol
@@ -10,21 +10,16 @@ import './MixinFunds.sol';
  * @dev `Mixins` are a design pattern seen in the 0x contracts.  It simply
  * separates logically groupings of code to ease readability.
  */
-contract MixinDisableAndDestroy is
+contract MixinDisable is
   Ownable,
   MixinFunds
 {
   // Used to disable payable functions when deprecating an old lock
   bool public isAlive;
 
-  event Destroy(
-    uint balance,
-    address indexed owner
-  );
-
   event Disable();
 
-  function _initializeMixinDisableAndDestroy(
+  function _initializeMixinDisable(
   ) internal
   {
     isAlive = true;
@@ -47,25 +42,4 @@ contract MixinDisableAndDestroy is
     emit Disable();
     isAlive = false;
   }
-
-  /**
-  * @dev Used to clean up old lock contracts from the blockchain
-  * TODO: add a check to ensure all keys are INVALID!
-   */
-  function destroyLock()
-    external
-    onlyOwner
-  {
-    require(isAlive == false, 'DISABLE_FIRST');
-
-    emit Destroy(address(this).balance, msg.sender);
-
-    // this will send any ETH or ERC20 held by the lock to the owner
-    _transfer(tokenAddress, msg.sender, getBalance(tokenAddress, address(this)));
-    selfdestruct(msg.sender);
-
-    // Note we don't clean up the `locks` data in Unlock.sol as it should not be necessary
-    // and leaves some data behind ('Unlock.LockBalances') which may be helpful.
-  }
-
 }
