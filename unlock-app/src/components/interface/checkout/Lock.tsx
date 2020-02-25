b89daed4a8cb19b4d66702fb8c61024ba9f523c5
Christopher Nascone <cnasc@users.noreply.github.com>
2020-02-25 16:54:18 -0500

    Checkout keeps track of purchases in progress (#6054)

--- unlock-app/src/components/interface/checkout/Lock.tsx
+++ unlock-app/src/components/interface/checkout/Lock.tsx
@@ -166,27 +166,54 @@ export const LoadingLock = () => {
 
 interface LockProps {
   lock: RawLock
+  purchasingLockAddress: string | null
+  setPurchasingLockAddress: (lockAddress: string) => void
 }
 
-const lockKeysAvailable = (lock: RawLock) => {
-  if ((lock as any).unlimitedKeys) {
+interface LockKeysAvailableLock {
+  unlimitedKeys?: boolean
+  maxNumberOfKeys?: number
+  outstandingKeys?: number
+}
+
+export const lockKeysAvailable = ({
+  unlimitedKeys,
+  maxNumberOfKeys,
+  outstandingKeys,
+}: LockKeysAvailableLock) => {
+  if (unlimitedKeys) {
     return 'Unlimited'
   }
 
   // maxNumberOfKeys and outstandingKeys are assumed to be defined
   // if they are ever not, a runtime error can occur
-  return (lock.maxNumberOfKeys! - lock.outstandingKeys!).toString()
+  return (maxNumberOfKeys! - outstandingKeys!).toString()
 }
 
-export const Lock = ({ lock }: LockProps) => {
+export const Lock = ({
+  lock,
+  purchasingLockAddress,
+  setPurchasingLockAddress,
+}: LockProps) => {
   const { purchaseKey } = usePurchaseKey(lock)
+
+  const onClick = () => {
+    if (purchasingLockAddress) {
+      // There is already a key purchase in progress (or completed) -- do not start another one
+      return
+    }
+
+    setPurchasingLockAddress(lock.address)
+    purchaseKey()
+  }
+
   return (
     <LockContainer>
       <InfoWrapper>
         <LockName>{lock.name}</LockName>
         <KeysAvailable>{lockKeysAvailable(lock)} Available</KeysAvailable>
       </InfoWrapper>
-      <LockBody onClick={purchaseKey}>
+      <LockBody onClick={onClick}>
         <LockPrice>{lock.keyPrice}</LockPrice>
         <TickerSymbol>{(lock as any).currencySymbol || 'ETH'}</TickerSymbol>
         <ValidityDuration>
