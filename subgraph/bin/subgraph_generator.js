5069090007090fa06288001c94f9679cc935f237
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-02-25 15:25:30 -0500

    Updating the template to support network names

--- subgraph/bin/subgraph_generator.js
+++ subgraph/bin/subgraph_generator.js
@@ -8,19 +8,23 @@ let templateValues = network => {
   switch (network.toLowerCase()) {
     case "local-dev":
       address = "0x559247Ec8A8771E8C97cDd39b96b9255651E39C5";
+      networkName = "mainnet"
       break;
     case "kovan":
       address = "0x0B9fe963b789151E53b8bd601590Ea32F9f2453D";
+      networkName = "kovan"
       break;
     case "rinkeby":
       address = "0xD8C88BE5e8EB88E38E6ff5cE186d764676012B0b";
+      networkName = "rinkeby"
       break;
     case "mainnet":
       address = "0x3d5409CcE1d45233dE1D4eBDEe74b8E004abDD13";
+      networkName = "mainnet"
       break;
   }
 
-  return { unlockContractAddress: address };
+  return { network: networkName , unlockContractAddress: address };
 };
 
 let generate = async () => {
@@ -33,7 +37,7 @@ let generate = async () => {
   const source = await fs.readFile(subgraphTemplateFilePath, "utf-8");
   const template = Handlebars.compile(source);
   const result = template(templateValues("local-dev"));
-  await fs.writeFile(path.join(__dirname, "..", "subgraph.yaml"), result);  
+  await fs.writeFile(path.join(__dirname, "..", "subgraph.yaml"), result);
 };
 
 generate();
