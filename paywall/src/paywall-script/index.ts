6227ebab3fff8a7929cd74b51e764e6e0bbb0f1a
Christoph Nascone <chris@unlock-protocol.com>
2020-03-04 13:13:08 -0500

    Bring in the unlock_app_url variable

--- paywall/src/paywall-script/index.ts
+++ paywall/src/paywall-script/index.ts
@@ -1,11 +1,13 @@
 import Postmate from 'postmate'
 
-const baseUrl = 'http://localhost:3000/checkout'
+declare let __ENVIRONMENT_VARIABLES__: { unlockAppUrl: string }
+
+const { unlockAppUrl } = __ENVIRONMENT_VARIABLES__
 const rawConfig = (window as any).unlockProtocolConfig
 const encodedConfig = encodeURI(JSON.stringify(rawConfig))
 
 const handshake = new Postmate({
-  url: `${baseUrl}?paywallConfig=${encodedConfig}`,
+  url: `${unlockAppUrl}/checkout?paywallConfig=${encodedConfig}`,
   classListArray: ['unlock-protocol-checkout'],
 })
 
