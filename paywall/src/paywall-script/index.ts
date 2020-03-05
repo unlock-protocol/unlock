8017269b618d79c437d8d15bef5142618238302d
Christoph Nascone <chris@unlock-protocol.com>
2020-03-05 15:37:53 -0500

    New paywall script and checkout can communicate

--- paywall/src/paywall-script/index.ts
+++ paywall/src/paywall-script/index.ts
@@ -1,14 +1,31 @@
 import Postmate from 'postmate'
+import {
+  CheckoutEvents,
+  UserInfo,
+  TransactionInfo,
+} from '../../../unlock-app/src/hooks/useCheckoutCommunication'
 
 declare let __ENVIRONMENT_VARIABLES__: { unlockAppUrl: string }
 
 const { unlockAppUrl } = __ENVIRONMENT_VARIABLES__
 const rawConfig = (window as any).unlockProtocolConfig
-const encodedConfig = encodeURI(JSON.stringify(rawConfig))
+const encodedConfig = encodeURIComponent(JSON.stringify(rawConfig))
 
 const handshake = new Postmate({
   url: `${unlockAppUrl}/checkout?paywallConfig=${encodedConfig}`,
   classListArray: ['unlock-protocol-checkout'],
 })
 
-handshake.then(child => console.log(child))
+handshake.then(child => {
+  child.on(CheckoutEvents.closeModal, () => {
+    console.log('Close the modal when we receive this event.')
+  })
+
+  child.on(CheckoutEvents.userInfo, (info: UserInfo) => {
+    console.log(`got user address: ${info.address}`)
+  })
+
+  child.on(CheckoutEvents.transactionInfo, (info: TransactionInfo) => {
+    console.log(`got transaction hash: ${info.hash}`)
+  })
+})
