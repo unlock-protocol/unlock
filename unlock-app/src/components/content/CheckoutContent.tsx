8d2d60eb56401e70e852d0f29b1ff96ce45568b6
Christoph Nascone <chris@unlock-protocol.com>
2020-03-10 15:27:39 -0400

    config from postmate takes precedence over config in URL

--- unlock-app/src/components/content/CheckoutContent.tsx
+++ unlock-app/src/components/content/CheckoutContent.tsx
@@ -19,19 +19,19 @@ import { useCheckoutCommunication } from '../../hooks/useCheckoutCommunication'
 
 interface CheckoutContentProps {
   account: AccountType
-  config?: PaywallConfig
+  configFromSearch?: PaywallConfig
 }
 
 const defaultLockAddresses: string[] = []
 
-export const CheckoutContent = ({ account, config }: CheckoutContentProps) => {
-  const lockAddresses = config
-    ? Object.keys(config.locks)
-    : defaultLockAddresses
-
+export const CheckoutContent = ({
+  account,
+  configFromSearch,
+}: CheckoutContentProps) => {
   const [showingLogin, setShowingLogin] = useState(false)
-  const [aConfig, setConfig] = useState<PaywallConfig | undefined>(undefined)
-  console.log({ aConfig })
+  const [configFromPostmate, setConfig] = useState<PaywallConfig | undefined>(
+    undefined
+  )
 
   const {
     emitTransactionInfo,
@@ -46,6 +46,13 @@ export const CheckoutContent = ({ account, config }: CheckoutContentProps) => {
       })
     }
   }, [account])
+
+  // Config value from postmate always takes precedence over the one in the URL if it is present.
+  const config = configFromPostmate || configFromSearch
+
+  const lockAddresses = config
+    ? Object.keys(config.locks)
+    : defaultLockAddresses
 
   return (
     <CheckoutContainer close={emitCloseModal}>
@@ -90,11 +97,11 @@ interface ReduxState {
 export const mapStateToProps = ({ account, router }: ReduxState) => {
   const search = queryString.parse(router.location.search)
 
-  const config = getConfigFromSearch(search)
+  const configFromSearch = getConfigFromSearch(search)
 
   return {
     account,
-    config,
+    configFromSearch,
   }
 }
 
