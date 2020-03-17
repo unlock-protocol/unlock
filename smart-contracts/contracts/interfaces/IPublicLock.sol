12caa952b99856f6d117cf8bb52f1df43780ad8f
Hardly Difficult <hardlydiff@gmail.com>
2020-03-17 12:43:58 -0700

    Fix interface dependency (#6246)
    * Fix interface dependency
    
    * Fix comment

--- smart-contracts/contracts/interfaces/IPublicLock.sol
+++ smart-contracts/contracts/interfaces/IPublicLock.sol
@@ -1,13 +1,13 @@
 pragma solidity 0.5.16;
 
-import '@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/IERC721Enumerable.sol';
 /**
 * @title The PublicLock Interface
 * @author Nick Furfaro (unlock-protocol.com)
  */
 
 
-contract IPublicLock is IERC721Enumerable {
+contract IPublicLock
+{
 
 // See indentationissue description here:
 // https://github.com/duaraghav8/Ethlint/issues/268
@@ -439,4 +439,49 @@ contract IPublicLock is IERC721Enumerable {
   function supportsInterface(bytes4 interfaceId) external view returns (bool );
   ///===================================================================
 
+  /// From ERC-721
+  /**
+     * @dev Returns the number of NFTs in `owner`'s account.
+     */
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the NFT specified by `tokenId`.
+     */
+    function ownerOf(uint256 tokenId) public view returns (address _owner);
+
+    /**
+     * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
+     * another (`to`).
+     *
+     *
+     *
+     * Requirements:
+     * - `from`, `to` cannot be zero.
+     * - `tokenId` must be owned by `from`.
+     * - If the caller is not `from`, it must be have been allowed to move this
+     * NFT by either {approve} or {setApprovalForAll}.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) public;
+    /**
+     * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
+     * another (`to`).
+     *
+     * Requirements:
+     * - If the caller is not `from`, it must be approved to move this NFT by
+     * either {approve} or {setApprovalForAll}.
+     */
+    function transferFrom(address from, address to, uint256 tokenId) public;
+    function approve(address to, uint256 tokenId) public;
+    function getApproved(uint256 tokenId) public view returns (address operator);
+
+    function setApprovalForAll(address operator, bool _approved) public;
+    function isApprovedForAll(address _owner, address operator) public view returns (bool);
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public;
+
+    function totalSupply() public view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 index) public view returns (uint256 tokenId);
+
+    function tokenByIndex(uint256 index) public view returns (uint256);
 }
