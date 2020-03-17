2d70599ce30fefa1f9b6ff38216b3de13140ac4d
Christoph Nascone <chris@unlock-protocol.com>
2020-03-17 12:14:55 -0400

    Add and test new actions

--- unlock-app/src/__tests__/hooks/useCheckoutStore.test.ts
+++ unlock-app/src/__tests__/hooks/useCheckoutStore.test.ts
@@ -3,6 +3,9 @@ import {
   setConfig,
   setShowingLogin,
   setPurchasingLockAddress,
+  setTransactionHash,
+  setShowingMetadataForm,
+  setDelayedPurchase,
 } from '../../utils/checkoutActions'
 
 const newConfig = {
@@ -18,6 +21,11 @@ const newConfig = {
     confirmed: '',
     noWallet: '',
   },
+}
+
+const newDelayedPurchase = {
+  lockAddress: '0xalock',
+  purchaseKey: jest.fn(),
 }
 
 describe('useCheckoutStore -- reducer', () => {
@@ -49,6 +57,38 @@ describe('useCheckoutStore -- reducer', () => {
     expect(reducer(undefined, setPurchasingLockAddress(newAddress))).toEqual(
       expect.objectContaining({
         purchasingLockAddress: newAddress,
+      })
+    )
+  })
+
+  it('returns the state updated with a new transaction hash on setTransactionHash', () => {
+    expect.assertions(1)
+
+    const transactionHash = '0xthehash'
+
+    expect(reducer(undefined, setTransactionHash(transactionHash))).toEqual(
+      expect.objectContaining({
+        transactionHash,
+      })
+    )
+  })
+
+  it('returns the state updated with the new metadata form state on setShowingMetadataForm', () => {
+    expect.assertions(1)
+
+    expect(reducer(undefined, setShowingMetadataForm(true))).toEqual(
+      expect.objectContaining({
+        showingMetadataForm: true,
+      })
+    )
+  })
+
+  it('returns the state updated with a new delayed purchase on setDelayedPurchase', () => {
+    expect.assertions(1)
+
+    expect(reducer(undefined, setDelayedPurchase(newDelayedPurchase))).toEqual(
+      expect.objectContaining({
+        delayedPurchase: newDelayedPurchase,
       })
     )
   })
