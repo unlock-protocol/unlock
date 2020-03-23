6e3eeece03d850f25cf5b498190b94745595f025
Christoph Nascone <chris@unlock-protocol.com>
2020-03-23 13:36:36 -0400

    Mocks set corrrectly in useProvider test

--- unlock-app/src/__tests__/hooks/useProvider.test.ts
+++ unlock-app/src/__tests__/hooks/useProvider.test.ts
@@ -1,9 +1,23 @@
 import { renderHook } from '@testing-library/react-hooks'
 import * as Redux from 'react-redux'
+import React from 'react'
 import { useProvider } from '../../hooks/useProvider'
+import { ConfigContext } from '../../utils/withConfig'
 
-jest.spyOn(Redux, 'useSelector').mockImplementation(() => {
-  return {}
+jest.spyOn(Redux, 'useSelector').mockImplementation(selector => {
+  return selector({
+    provider: 'Unlock',
+  })
+})
+
+jest.spyOn(React, 'useContext').mockImplementation(context => {
+  if (context === ConfigContext) {
+    return {
+      providers: {
+        Unlock: {},
+      },
+    }
+  }
 })
 
 describe('useProvider', () => {
