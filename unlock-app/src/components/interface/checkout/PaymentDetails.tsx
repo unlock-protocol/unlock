5e99db1ff1e112860c527f2b6f3a11ccfc6c0028
Christoph Nascone <chris@unlock-protocol.com>
2020-03-24 13:23:01 -0400

    Add new payment details form

---
+++ unlock-app/src/components/interface/checkout/PaymentDetails.tsx
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { loadStripe } from '@stripe/stripe-js'
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js'
+import configure from '../../../config'
+import { countries } from '../../../utils/countries'
+import { useProvider } from '../../../hooks/useProvider'
+import { StorageServiceContext } from '../../../utils/withStorageService'
+import { StorageService } from '../../../services/storageService'
+
+const { stripeApiKey } = configure()
+const stripePromise = loadStripe(stripeApiKey)
+
+export const PaymentDetails = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <Form />
+    </Elements>
+  )
+}
+
+export const Form = () => {
+  const { register, handleSubmit } = useForm()
+  const stripe = useStripe()
+  const elements = useElements()
+  const { provider } = useProvider()
+  const storageService: StorageService = useContext(StorageServiceContext)
+
+  const onSubmit = async (data: Record<string, any>) => {
+    const cardElement = elements!.getElement(CardElement)
+    const result = await stripe!.createToken(cardElement!, {
+      address_country: data.address_country,
+      name: data.name,
+    })
+
+    if (result.token) {
+      const { data, sig } = provider!.signPaymentData(result.token.id)
+      await storageService.addPaymentMethod(
+        data.message.user.emailAddress,
+        data,
+        sig
+      )
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="name" ref={register({ required: true })} />
+      <CardElement />
+      <select name="address_country">
+        {countries.map(country => (
+          <option
+            key={country}
+            value={country}
+            selected={country === 'United States'}
+          >
+            {country}
+          </option>
+        ))}
+      </select>
+      <button type="submit" disabled={!stripe}>
+        Submit
+      </button>
+    </form>
+  )
+}
