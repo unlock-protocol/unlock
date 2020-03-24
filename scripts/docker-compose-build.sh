289f0033db6530037c4e714ef07cbc4a9ebbc0f1
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-03-24 13:06:53 -0400

    Attempting to  setup the compose environment
    We are taking the approach of not building the images ahead of time as an approach to reserve space on the container

--- scripts/docker-compose-build.sh
+++ scripts/docker-compose-build.sh
@@ -10,16 +10,16 @@ docker pull "$DOCKER_REPOSITORY/unlock-core:$BRANCH"
 ARGS="--cache-from $DOCKER_REPOSITORY/unlock-core:master"
 docker build -t unlock-core -f $REPO_ROOT/docker/unlock-core.dockerfile $ARGS $REPO_ROOT
 
-# First build all of the images
-. $REPO_ROOT/scripts/build-image.sh unlock-app true &
-. $REPO_ROOT/scripts/build-image.sh wedlocks true &
-. $REPO_ROOT/scripts/build-image.sh smart-contracts true &
-. $REPO_ROOT/scripts/build-image.sh paywall true &
-. $REPO_ROOT/scripts/build-image.sh locksmith true &
-. $REPO_ROOT/scripts/build-image.sh nudge true &
-. $REPO_ROOT/scripts/build-image.sh unlock-protocol-com true &
-. $REPO_ROOT/scripts/build-image.sh integration-tests true &
-wait
+# # First build all of the images
+# . $REPO_ROOT/scripts/build-image.sh unlock-app true &
+# . $REPO_ROOT/scripts/build-image.sh wedlocks true &
+# . $REPO_ROOT/scripts/build-image.sh smart-contracts true &
+# . $REPO_ROOT/scripts/build-image.sh paywall true &
+# . $REPO_ROOT/scripts/build-image.sh locksmith true &
+# . $REPO_ROOT/scripts/build-image.sh nudge true &
+# . $REPO_ROOT/scripts/build-image.sh unlock-protocol-com true &
+# . $REPO_ROOT/scripts/build-image.sh integration-tests true &
+# wait
 
 # And then finish if anything else is needed
 docker-compose -f $REPO_ROOT/docker/docker-compose.yml -f $REPO_ROOT/docker/docker-compose.ci.yml build
