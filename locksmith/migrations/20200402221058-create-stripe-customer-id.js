8d8f8234433a87592d2782b7cf0be6d0ba391961
Julien Genestoux <julien.genestoux@gmail.com>
2020-04-02 18:47:20 -0400

    adding stripeCustomer (#6403)

---
+++ locksmith/migrations/20200402221058-create-stripe-customer-id.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('StripeCustomers', {
+      publicKey: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      StripeCustomerId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('StripeCustomers');
+  }
+};
