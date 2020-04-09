b772340fb405e128cb32834605f0b631fe1d4583
Akeem Adeniji <akeem.adeniji@gmail.com>
2020-04-09 14:00:07 -0400

    Adds a small library to perform threshold checks (#6467)
    * Adds a small library to perform threshold checks

---
+++ locksmith/__tests__/utils/priceRange.test.ts
@@ -0,0 +1,18 @@
+import * as PriceRange from '../../src/utils/priceRange'
+
+describe('PriceRange', () => {
+  describe('within', () => {
+    describe('when the requested price is within the acceptable range', () => {
+      it('returns true', () => {
+        expect.assertions(1)
+        expect(PriceRange.within(5, 5)).toBe(true)
+      })
+    })
+    describe('when the requested price is outside of the acceptable range', () => {
+      it('returns false', () => {
+        expect.assertions(1)
+        expect(PriceRange.within(5, 100)).toBe(false)
+      })
+    })
+  })
+})
---
+++ locksmith/src/utils/priceRange.ts
@@ -0,0 +1,10 @@
+const THRESHOLD = 0.03
+
+export function within(requestedPrice: number, current: number): boolean {
+  const range = generateRange(current, THRESHOLD)
+  return requestedPrice >= range.lower && requestedPrice <= range.upper
+}
+
+function generateRange(value: number, percentage: number): any {
+  return { upper: value * (1 + percentage), lower: value * (1 - percentage) }
+}
