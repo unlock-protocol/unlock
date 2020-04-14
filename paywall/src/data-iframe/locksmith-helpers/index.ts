4a87e76903adcca5faa101d69f5720412d9fee6c
Christopher Nascone <cnasc@users.noreply.github.com>
2020-04-14 15:53:55 -0400

    Remove dead code from Paywall (#6503)
    * Remove source for legacy paywall scripts
    
    * Shuffling around to fix type errors
    
    * Remove test helpers from legacy paywall

--- paywall/src/data-iframe/locksmith-helpers/index.ts
+++
@@ -1,2 +0,0 @@
-export { getTransactionsFor } from './getTransactionsFor'
-export { storeTransactionFor } from './storeTransactionFor'
--- paywall/src/unlock.js/index.ts
+++
@@ -1,6 +0,0 @@
-import startupWhenReady from './startup'
-import '../static/iframe.css'
-
-declare let __ENVIRONMENT_VARIABLES__: any
-
-startupWhenReady(window, __ENVIRONMENT_VARIABLES__)
