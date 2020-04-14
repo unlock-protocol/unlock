5d348edf94d11c99f8aaaea3b11bd9ec61ec1f9f
Christoph Nascone <chris@unlock-protocol.com>
2020-04-14 12:25:58 -0400

    Remove tests dependent on deprecated paywall

--- tests/test/unlock-provider.test.js
+++
@@ -1,9 +0,0 @@
-const url = require('../helpers/url')
-
-describe('The Login Page', () => {
-  it('it loads the login module', async () => {
-    expect.assertions(1)
-    await page.goto(url.unlockProviderApp('/account'))
-    await expect(page).toMatch('Log In')
-  })
-})
