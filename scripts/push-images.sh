03f145a80e919a98552a767c8a5774a13d20529e
Julien Genestoux <julien.genestoux@gmail.com>
2020-10-19 18:22:56 -0400

    updated the push images script (#6891)

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -4,21 +4,24 @@
 # This script saves previously built images to docker hub or AWS.
 # This should only run on master merges
 
-IMAGES=( $1 )
+IMAGES=( $@ )
 TIMESTAMP=`date +%s`
 
 IMAGE_TAG_LIST=( master latest $TIMESTAMP)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for IMAGE_TAG in "${IMAGE_TAG_LIST[@]}"
+
+for IMAGE_NAME in "${IMAGES[@]}"
 do
 
-  for IMAGE_NAME in "${IMAGES[@]}"
+
+  for IMAGE_TAG in "${IMAGE_TAG_LIST[@]}"
   do
     IMAGE_TO_PUSH="$DOCKER_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG"
+    echo "Pushing $IMAGE_TO_PUSH"
     docker tag $IMAGE_NAME $IMAGE_TO_PUSH
-    docker push $IMAGE_TO_PUSH &
+    docker push $IMAGE_TO_PUSH
   done
 
 done
