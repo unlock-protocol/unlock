5917db4480d9575febda9c59a2adce6e79484f78
Julien Genestoux <julien.genestoux@gmail.com>
2020-12-12 22:25:40 -0500

    fixing push image script

--- scripts/promote-image.sh
+++ scripts/promote-image.sh
@@ -12,8 +12,11 @@ COMMAND="yarn run ci"
 # Setting the right env var
 export UNLOCK_ENV=test
 
-# Build image
-UNLOCK_ENV=test docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.ci.yml build $SERVICE
+# Build the core
+docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t unlock-core -f docker/unlock-core.dockerfile --cache-from unlockprotocol/unlock-core:master .
 
-# Push image
+# Build the image
+docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t $SERVICE -f docker/$SERVICE.dockerfile --cache-from unlockprotocol/$SERVICE:master .
+
+# Push images
 scripts/push-images.sh unlock-core $SERVICE
