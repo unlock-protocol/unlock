63520ee25adab8fac45762f17cb99001fd6a376b
Julien Genestoux <julien.genestoux@gmail.com>
2021-03-08 13:31:05 -0500

    setting base url

--- paywall/src/paywall-script/index.ts
+++ paywall/src/paywall-script/index.ts
@@ -5,6 +5,7 @@ import { networkConfigs } from './networkConfigs'
 declare var PAYWALL_URL: string
 
 const rawConfig = (window as any).unlockProtocolConfig
+const baseUrl = PAYWALL_URL || 'localhost' // Set at build time
 if (!rawConfig) {
   console.error(
     'Missing window.unlockProtocolConfig. See docs on how to configure your locks: https://docs.unlock-protocol.com/'
@@ -12,12 +13,12 @@ if (!rawConfig) {
 } else {
   // set network based on hostname if missing in rawConfig
   if (!rawConfig.network) {
-    if (PAYWALL_URL.match('staging-paywall.unlock-protocol.com')) {
+    if (baseUrl.match('staging-paywall.unlock-protocol.com')) {
       rawConfig.network = '4'
       console.error(
         'Missing network in Unlock config. Assigned default to mainnet: 1. See https://docs.unlock-protocol.com/'
       )
-    } else if (PAYWALL_URL.match('paywall.unlock-protocol.com')) {
+    } else if (baseUrl.match('paywall.unlock-protocol.com')) {
       rawConfig.network = '1'
       console.error(
         'Missing network in Unlock config. Assigned default to rinkeby 4. See https://docs.unlock-protocol.com/'
