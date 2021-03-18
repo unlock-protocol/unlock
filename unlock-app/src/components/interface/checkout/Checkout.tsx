165a26fe5195ceb7dd0f68f67ed2b3a3573138ce
Julien Genestoux <julien.genestoux@gmail.com>
2021-03-17 21:55:02 -0400

    Logo size (#7016)
    * Verification fixed (#7012)
    
    * fixing paywall cconfig
    
    * fixed useMembes
    
    * adding back verification
    
    * caping logo size

--- unlock-app/src/components/interface/checkout/Checkout.tsx
+++ unlock-app/src/components/interface/checkout/Checkout.tsx
@@ -172,9 +172,12 @@ export const Checkout = ({
 }
 
 const PaywallLogoWrapper = styled.div`
-  max-height: 100px;
-  max-width: 200px;
   margin-bottom: 20px;
+
+  > img {
+    max-height: 100px;
+    max-width: 200px;
+  }
 `
 
 const Spacer = styled.div`
