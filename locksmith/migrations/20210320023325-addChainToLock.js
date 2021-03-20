419325df425f175b77ca497aa5a1213063a0706d
Julien Genestoux <julien.genestoux@gmail.com>
2021-03-19 22:41:46 -0400

    adding migration to block

---
+++ locksmith/migrations/20210320023325-addChainToLock.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(table, 'chain', {
+      type: Sequelize.INTEGER,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(table, 'chain'
+  }
+};
