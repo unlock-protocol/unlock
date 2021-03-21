9b8e21cb71360fc8fb0f9037947c12046740ed16
Julien Genestoux <julien.genestoux@gmail.com>
2021-03-20 20:39:01 -0400

    adding missing migrations

---
+++ locksmith/migrations/20210321003458-addChainToKeyMetadata.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const config = require('../config/config')
+
+const table = 'KeyMetadata'
+
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    if (!config.defaultNetwork) {
+      console.error('CANNOT MIGRATE WITHOUT A DEFAULT NETWORK')
+      throw new Error('Missing default network')
+    }
+
+    await queryInterface.addColumn(table, 'chain', {
+      type: Sequelize.INTEGER,
+    })
+    // And now let's set the right value!
+    await queryInterface.bulkUpdate(table, {
+      chain: parseInt(config.defaultNetwork),
+    }, {
+      chain: null,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(table, 'chain')
+  }
+};
