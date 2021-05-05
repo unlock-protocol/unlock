72eb4582677d34a9e67141dd01a33c465ee7845b
Julien Genestoux <julien.genestoux@gmail.com>
2021-05-05 16:27:07 -0400

    on side chains, we do not mint new tokens but distribute from the existing balance

--- smart-contracts/contracts/interfaces/IMintableERC20.sol
+++ smart-contracts/contracts/interfaces/IMintableERC20.sol
@@ -3,5 +3,6 @@ pragma solidity 0.5.17;
 interface IMintableERC20
 {
   function mint(address account, uint256 amount) external returns (bool);
+  function transfer(address recipient, uint256 amount) external returns (bool);
   function totalSupply() external returns (uint);
 }
