763b98dd282efe9ac551009121ea94e16639f0e3
Julien Genestoux <julien.genestoux@gmail.com>
2021-05-11 21:00:40 -0400

    adding multilocks support to paywall (#7095)
    * adding multilocks support to paywall
    
    * fixing network config

--- paywall/src/utils/isUnlocked.ts
+++ paywall/src/utils/isUnlocked.ts
@@ -1,54 +1,46 @@
 import { keyExpirationTimestampFor } from './keyExpirationTimestampFor'
 import { optimisticUnlocking } from './optimisticUnlocking'
-import { PaywallConfig } from '../unlockTypes'
-
-interface ModuleConfig {
-  readOnlyProvider: string
-  locksmithUri: string
-}
+import { PaywallConfig, NetworkConfigs } from '../unlockTypes'
 
 /**
- * A function which, given a user account, a paywall config will return a boolean
- * to indicate whether the user has Unlocked any of the locks.
+ * A function which, given a user account, a paywall config will return the list of unlocked locks.
  * It will first check for the existence of keys, and if no valid one has been found
  * it will check for pending transactions which might be optimistic.
  */
 export const isUnlocked = async (
   userAccountAddress: string,
   paywallConfig: PaywallConfig,
-  { readOnlyProvider, locksmithUri }: ModuleConfig
-): Promise<boolean> => {
-  const lockAddresses = Object.keys(paywallConfig.locks)
-  const timeStamps = await Promise.all(
-    lockAddresses.map((lockAddress) => {
-      return keyExpirationTimestampFor(
+  networks: NetworkConfigs
+): Promise<string[]> => {
+  const unlockedLocks: string[] = []
+  await Promise.all(
+    // For each lock
+    Object.entries(paywallConfig.locks).map(async ([lockAddress, props]) => {
+      let network = props.network || paywallConfig.network
+      const { readOnlyProvider, locksmithUri } = networks[network]
+      const timestamp = await keyExpirationTimestampFor(
         readOnlyProvider,
         lockAddress,
         userAccountAddress!
       )
+      if (timestamp > new Date().getTime() / 1000) {
+        // This lock is unlocked!
+        unlockedLocks.push(lockAddress)
+      } else if (!paywallConfig.pessimistic) {
+        const optimistic = await optimisticUnlocking(
+          readOnlyProvider,
+          locksmithUri,
+          [lockAddress],
+          userAccountAddress!
+        )
+        if (optimistic) {
+          unlockedLocks.push(lockAddress)
+        }
+      }
     })
   )
 
-  // If any key is valid, we unlock the page
-  if (timeStamps.some((val) => val > new Date().getTime() / 1000)) {
-    return true
-  }
-
-  if (!paywallConfig.pessimistic) {
-    // If not key exists on chain, let's see if we can be optimistic before locking the page.
-    const optimistic = await optimisticUnlocking(
-      readOnlyProvider,
-      locksmithUri,
-      Object.keys(paywallConfig.locks),
-      userAccountAddress!
-    )
-    if (optimistic) {
-      return true
-    }
-  }
-
-  // If no key exists, or no transaction exists which could be optimistic, we lock
-  return false
+  return unlockedLocks
 }
 export default {
   isUnlocked,
