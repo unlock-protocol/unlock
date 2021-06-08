598a2c6c11f8c46f63c237d629de51ad5eb8601e
Julien Genestoux <julien.genestoux@gmail.com>
2021-06-08 16:45:50 -0400

    adding v9 npm package (#7197)

---
+++ smart-contracts/published-npm-modules/V9/index.js
@@ -0,0 +1,7 @@
+const PublicLock = require('./PublicLock.json')
+const Unlock = require('./Unlock.json')
+const UnlockDiscountToken = require('./UnlockDiscountToken.json')
+
+exports.Unlock = Unlock
+exports.PublicLock = PublicLock
+exports.UnlockDiscountToken = UnlockDiscountToken
