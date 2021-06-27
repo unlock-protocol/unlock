f569a1133eb7304c4e639a67af499e3a368ef6d3
Julien Genestoux <julien.genestoux@gmail.com>
2021-06-27 19:53:26 -0400

    enabling events when using injected wallet (#7242)

--- unlock-app/src/hooks/useCheckoutCommunication.ts
+++ unlock-app/src/hooks/useCheckoutCommunication.ts
@@ -16,9 +16,10 @@ export enum CheckoutEvents {
   closeModal = 'checkout.closeModal',
   transactionInfo = 'checkout.transactionInfo',
   methodCall = 'checkout.methodCall',
+  onEvent = 'checkout.onEvent',
 }
 
-type Payload = UserInfo | TransactionInfo | MethodCall
+type Payload = UserInfo | TransactionInfo | MethodCall | string
 
 interface BufferedEvent {
   kind: CheckoutEvents
@@ -47,6 +48,7 @@ export type AsyncSendable = {
     callback: (error: any, response: any) => void
   ) => void
   send?: (request: any, callback: (error: any, response: any) => void) => void
+  on?: (name: string, callback: () => void) => void
 }
 
 // Callbacks from method calls that have been sent to the parent
@@ -54,6 +56,11 @@ export type AsyncSendable = {
 // it will trigger the callback and remove it from the table.
 export const waitingMethodCalls: {
   [id: number]: (error: any, response: any) => void
+} = {}
+
+// TODO: see if we can support multiple handlers for same event name
+export const eventHandlers: {
+  [name: string]: () => void
 } = {}
 
 export const resolveMethodCall = (result: MethodCallResult) => {
@@ -68,6 +75,13 @@ export const resolveMethodCall = (result: MethodCallResult) => {
   }
   delete waitingMethodCalls[result.id]
   callback(result.error, result.response)
+}
+
+export const resolveOnEvent = (name: string) => {
+  const callback = eventHandlers[name]
+  if (callback) {
+    callback()
+  }
 }
 
 // This is just a convenience hook that wraps the `emit` function
@@ -87,6 +101,7 @@ export const useCheckoutCommunication = () => {
       setConfig(config)
     },
     resolveMethodCall,
+    resolveOnEvent,
   })
 
   const pushOrEmit = (kind: CheckoutEvents, payload?: Payload) => {
@@ -123,6 +138,9 @@ export const useCheckoutCommunication = () => {
     pushOrEmit(CheckoutEvents.methodCall, call)
   }
 
+  const emitOnEvent = (eventName: string) => {
+    pushOrEmit(CheckoutEvents.onEvent, eventName)
+  }
   // If the page is not inside an iframe, window and window.top will be identical
   const insideIframe = window.top !== window
 
@@ -133,6 +151,10 @@ export const useCheckoutCommunication = () => {
           callback(error, response)
         }
         emitMethodCall(request)
+      },
+      on: (event: string, callback: any) => {
+        eventHandlers[event] = callback
+        emitOnEvent(event)
       },
     })
   }
