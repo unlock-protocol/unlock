d9c2e2647f3669745dfd924bbb69228ef576c45b
Clement Renaud <clement@clementrenaud.com>
2021-07-06 11:25:08 +0200

    init testing for UDT upgrade

--- smart-contracts/test/UnlockDiscountToken/upgrades.js
+++ smart-contracts/test/UnlockDiscountToken/upgrades.js
@@ -1,21 +1,64 @@
 const { ethers, upgrades } = require('hardhat');
+const { reverts } = require('truffle-assertions')
+const BigNumber = require('bignumber.js')
 
-it('test upgrade', async () => {
-  const UnlockDiscountToken = await ethers.getContractFactory(
-    'UnlockDiscountToken'
-  )
+// helper function
+const upgradeContract = async (contractAddress) => {
   const UnlockDiscountTokenV2 = await ethers.getContractFactory(
     'UnlockDiscountTokenV2'
   )
+  const updated = await upgrades.upgradeProxy(contractAddress, UnlockDiscountTokenV2, {})
+  return updated
+}
 
-  const [minter] = await ethers.getSigners()
+contract('UnlockDiscountToken upgrade', async () => {
+  
+  let unlockDiscountToken
+  const mintAmount = 1000
+  
+  beforeEach(async () => {
 
-  const instance = await upgrades
-    .deployProxy(UnlockDiscountToken, [minter.address], {
-      kind: 'transparent',
-      initializer: 'initialize(address)',
+    const UnlockDiscountToken = await ethers.getContractFactory(
+      'UnlockDiscountToken'
+    )
+    
+    const [minter] = await ethers.getSigners()
+
+    unlockDiscountToken = await upgrades
+      .deployProxy(UnlockDiscountToken, [minter.address], {
+        kind: 'transparent',
+        initializer: 'initialize(address)',
+      })
+      .then((f) => f.deployed())
+        
+  })
+
+  describe('Supply', () => {
+    
+    it('Starting supply is 0', async () => {
+      const totalSupply = await unlockDiscountToken.totalSupply()
+      assert.equal( totalSupply.toNumber(), 0, 'starting supply must be 0')
     })
-    .then((f) => f.deployed())
 
-  await upgrades.upgradeProxy(instance.address, UnlockDiscountTokenV2, {})
+    it('Supply is preserved after upgrade', async () => {
+      const [minter, recipient] = await ethers.getSigners()
+
+      // mint some tokens
+      await unlockDiscountToken.mint(recipient.address, mintAmount, {
+        from: minter.address,
+      })
+      const totalSupply = await unlockDiscountToken.totalSupply()
+      assert.equal(totalSupply.toNumber(), mintAmount)
+
+      // upgrade
+      const updated = await upgradeContract(unlockDiscountToken.address)
+      console.log(updated)
+
+      const totalSupplyAfterUpdate = await updated.totalSupply()
+      assert.equal(totalSupplyAfterUpdate.toNumber(), mintAmount)
+
+    })
+
+  })
+
 })
