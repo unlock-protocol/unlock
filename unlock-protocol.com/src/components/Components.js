f193702a116672e3b0b78446d6377a2a236b74b7
Julien Genestoux <julien.genestoux@gmail.com>
2021-07-16 11:36:48 +0200

    fixed jobs page #7315

--- unlock-protocol.com/src/components/Components.js
+++ unlock-protocol.com/src/components/Components.js
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 export const Section = styled.section.attrs((props) => ({
   id: props.anchor,
 }))`
-  margin-top: 50px;
   margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 16px;
   &:before {
     display: block;
     content: '';
