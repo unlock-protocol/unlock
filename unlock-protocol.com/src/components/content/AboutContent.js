bbce33e128b2e9beda1bb5f69ae0a918fbc0be91
Julien Genestoux <julien.genestoux@gmail.com>
2021-07-16 11:45:33 +0200

    fixing about page. Fixes #7314

--- unlock-protocol.com/src/components/content/AboutContent.js
+++ unlock-protocol.com/src/components/content/AboutContent.js
@@ -3,13 +3,22 @@ import styled from 'styled-components'
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../interface/Layout'
+import Svg from '../interface/svg'
+import {
+  H2,
+  ActionButtons,
+  ActionButton,
+  Box,
+  Column,
+  Icon,
+} from '../interface/LandingPageComponents'
 
 import {
   Section,
   Title,
   Headline,
   // SubTitle,
-  Column,
+  // Column,
   TwoColumns,
 } from '../Components'
 import { pageTitle } from '../../constants'
@@ -105,19 +114,20 @@ export const AboutContent = ({ posts }) => (
     <Section>
       <Title>About</Title>
       <Headline>
-        We’re a small, smart and nimble team of coders and designers with a
-        vision for a better and fairer way to monetize content.
+        Unlock is an open source, Ethereum-based protocol designed to streamline
+        membership benefits for online communities.
       </Headline>
       <TwoColumns>
         <Column>
-          At Unlock, we believe the web needs a new business model. We believe
-          the decentralization promise of the web cannot be achieved if economic
-          incentives are not aligned between consumers and creators.
+          Unlock is meant to help creators find ways to monetize without relying
+          on a middleman. It’s a protocol — and not a centralized platform that
+          controls everything that happens on it.
           <br />
           <br />
-          For this, we&#39;re building a protocol which lets anyone sell
-          memberships to their work and for consumers to earn points when they
-          discover and promote the best creations.
+          Unlock’s mission is about taking back subscription and access from the
+          domain of middlemen — from a million tiny silos and a handful of
+          gigantic ones — and transforming it into a fundamental business model
+          for the web.
         </Column>
         <Column>
           The Unlock Protocol can be applied to publishing (paywalls),
@@ -127,21 +137,66 @@ export const AboutContent = ({ posts }) => (
         </Column>
       </TwoColumns>
     </Section>
-    {/* <Section>
-      <Title>Team</Title>
-      <Headline>
-        Our work matters. We are building a healthier web ecosystem. If this is
-        something that you would like to help us with, please,{' '}
-        <Link href="/jobs">
-          <a>join us!</a>
-        </Link>
-      </Headline>
-      <Columns>
-        {People.map((person) => {
-          return <Person {...person} key={person.name} />
-        })}
-      </Columns>
-    </Section> */}
+
+    <Box
+      color="var(--white)"
+      style={{ textAlign: 'center', marginBottom: '48px' }}
+    >
+      <H2 style={{ marginBottom: '32px' }}>
+        We are commmunity built and governed
+      </H2>
+      <p style={{ width: '75%', margin: 'auto' }}>
+        Join the 500+ community of developers, creators and governants building
+        the future of Unlock!
+      </p>
+
+      <ActionButtons style={{ justifyContent: 'center', marginTop: '32px' }}>
+        <ActionButton
+          target="_blank"
+          href="https://discord.gg/Ah6ZEJyTDp"
+          color="#5865F2"
+          contrastColor="var(--white)"
+        >
+          <Icon size="36">
+            <Svg.Discord />
+          </Icon>
+          Discord
+        </ActionButton>
+        <ActionButton
+          target="_blank"
+          href="https://unlock.community/"
+          color="#4A4A4A"
+          contrastColor="var(--white)"
+        >
+          <Icon size="36">
+            <Svg.Discourse />
+          </Icon>
+          Forum
+        </ActionButton>
+        <ActionButton
+          target="_blank"
+          href="https://twitter.com/unlockprotocol"
+          color="#1DA1F2"
+          contrastColor="var(--white)"
+        >
+          <Icon size="36">
+            <Svg.Twitter />
+          </Icon>
+          Twitter
+        </ActionButton>
+        <ActionButton
+          target="_blank"
+          href="https://vote.unlock-protocol.com/#/unlock-protocol.eth"
+          color="var(--brand)"
+          contrastColor="var(--white)"
+        >
+          <Icon size="36">
+            <Svg.Key />
+          </Icon>
+          Governance
+        </ActionButton>
+      </ActionButtons>
+    </Box>
     <Section>
       <Title>News</Title>
       <News>
