1eeb48e856af1116ee23bda8e20bf311bb053b9c
Clément Renaud <clement@clementrenaud.com>
2021-07-30 12:40:58 +0200

    UDT Erc20 upgrade (#7296)
    * init token openzeppelin 4.0 compatible + deps cleanup
    
    * upgrade hardhat
    
    * rename erc20 upgrade files
    
    * ignore erc20 upgrade build artifacts
    
    * add basic README in smart-contracts/scripts
    
    * more solidity verions + cleanup
    
    * init testing for UDT upgrade
    
    * explanations in readme
    
    * upgradable contracts bump version
    
    * contracts: better upgrade patch+workflow
    
    * check in UDT v2 contract
    
    * add patched erc20
    
    * UDT v2 contract lint
    
    * delete truffle config
    
    * lint fix udt upgrades test
    
    * init test on a mainnet fork
    
    * replicate udt mint tests for upgrades
    
    * udt upgrades tests
    
    * upgrades tests ok
    
    * upgrade .openzeppelin from cli
    
    * mainnet tests running properly
    
    * udt upgrade test details
    
    * update readme w mainnet fork process
    
    * udt v2 deployment script
    
    * mainnet tests details better handling
    
    * check in UDT v0 flattened version
    
    * add erc20votes w tests
    
    * udt add burn token for comp votes
    
    * udt v2 contract indent + gov tests lintfix
    
    * add migrate-oz-cli-project  to README
    
    * hardhat storage layout task
    
    * simplify mainnet fork tests
    
    * udt upgrade fix oz CLI deprec types
    
    * better mainnet tests
    
    * uprgade test on mainnet fork: add multisig
    
    * udt upgrade script returns address
    
    * udt v2 rename flatten script
    
    * udt test add multisig
    
    * mainnet fork test upgrade ok
    
    * test minting tokens add back miniting indication
    
    * patch testnet network files
    
    * proper network name parsing on udt upgrade
    
    * udt upgrade deployment instructions
    
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,20 +1,24 @@
 // hardhat.config.js
 
+require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-truffle5')
+
+// full stack trace if needed
+require('hardhat-tracer')
 
 // erc1820 deployment
 require('hardhat-erc1820')
 
 // for upgrades
-require('@nomiclabs/hardhat-ethers')
 require('@openzeppelin/hardhat-upgrades')
+
+// debug storage
+require('hardhat-storage-layout')
 
 // gas reporting for tests
 require('hardhat-gas-reporter')
 
 const { task } = require('hardhat/config')
-
-// const { deploy } = require('./scripts/deploy')
 
 const { getHardhatNetwork } = require('./helpers/network')
 
@@ -23,11 +27,15 @@ const settings = {
     enabled: true,
     runs: 200,
   },
+  outputSelection: {
+    '*': {
+      '*': ['storageLayout'],
+    },
+  },
 }
 
 // When running CI, we connect to the 'ganache' container
 const testHost = process.env.CI === 'true' ? 'ganache' : '127.0.0.1'
-
 const defaultNetworks = {
   ganache: {
     url: `http://${testHost}:8545`,
@@ -39,6 +47,24 @@ const defaultNetworks = {
 }
 
 const networks = getHardhatNetwork(defaultNetworks)
+
+// add mainnet fork -- if API key is present
+if (process.env.RUN_MAINNET_FORK) {
+  // eslint-disable-next-line no-console
+  console.log('Running a mainnet fork...')
+  const alchemyAPIKey = process.env.ALCHEMY_API_KEY
+  if (!alchemyAPIKey) {
+    throw new Error('Missing Alchemy API Key, couldnt run a mainnet fork')
+  }
+  const alchemyURL = `https://eth-mainnet.alchemyapi.io/v2/${alchemyAPIKey}`
+  networks.hardhat = {
+    forking: {
+      url: alchemyURL,
+      blockNumber: 12811244, // June 12th 2021
+      // gasPrice: 150000000000, // not working, see https://github.com/nomiclabs/hardhat/issues/1216
+    },
+  }
+}
 
 task('accounts', 'Prints the list of accounts', async () => {
   // eslint-disable-next-line no-undef
@@ -73,12 +99,14 @@ module.exports = {
     compilers: [
       { version: '0.4.24', settings },
       { version: '0.4.25', settings },
+      { version: '0.5.0', settings },
+      { version: '0.5.17', settings },
+      { version: '0.5.14', settings },
       { version: '0.5.7', settings },
       { version: '0.5.9', settings },
-      { version: '0.5.14', settings },
-      { version: '0.5.17', settings },
       { version: '0.6.12', settings },
       { version: '0.7.6', settings },
+      { version: '0.8.0', settings },
       { version: '0.8.4', settings },
     ],
   },
