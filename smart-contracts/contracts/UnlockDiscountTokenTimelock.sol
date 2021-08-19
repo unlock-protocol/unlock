3cec6b798f6b6d43105033649a66768a4269ad45
Clement Renaud <clement@clementrenaud.com>
2021-08-19 11:50:14 +0200

    sol files linting

--- smart-contracts/contracts/UnlockDiscountTokenTimelock.sol
+++ smart-contracts/contracts/UnlockDiscountTokenTimelock.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+import '@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol';
 
 contract UnlockDiscountTokenTimelock is TimelockControllerUpgradeable {
-    function initialize (
-        uint256 minDelay,
-        address[] memory proposers,
-        address[] memory executors
-    ) public initializer {
-        __TimelockController_init(minDelay, proposers, executors);
-    }
+  function initialize (
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
+  ) public initializer {
+    __TimelockController_init(minDelay, proposers, executors);
+  }
 }
