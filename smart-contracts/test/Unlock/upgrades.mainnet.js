9acfba84cb5767ba6c0028a6583468695929a26d
Julien Genestoux <julien.genestoux@gmail.com>
2021-08-20 18:32:47 +0200

    improved upgrade tests

--- smart-contracts/test/Unlock/upgrades.mainnet.js
+++ smart-contracts/test/Unlock/upgrades.mainnet.js
@@ -12,6 +12,8 @@ const proxyAdminAddress = UDTProxyInfo.admin // '0x79918A4389A437906538E0bbf3991
 
 const deployerAddress = '0x33ab07dF7f09e793dDD1E9A25b079989a557119A'
 const multisigAddress = '0xa39b44c4AFfbb56b76a1BF1d19Eb93a5DfC2EBA9'
+const recipient = '0xDD8e2548da5A992A63aE5520C6bC92c37a2Bcc44'
+const referrer = '0xDD8e2548da5A992A63aE5520C6bC92c37a2Bcc44'
 
 // helper function
 const upgradeContract = async () => {
@@ -77,6 +79,8 @@ contract('Unlock (on mainnet)', async () => {
   let deployer
 
   before(async function setupMainnetForkTestEnv() {
+    this.timeout(2000000)
+
     if (!process.env.RUN_MAINNET_FORK) {
       // all suite will be skipped
       this.skip()
@@ -122,12 +126,67 @@ contract('Unlock (on mainnet)', async () => {
     })
   })
 
-  describe('Unlocck contract', () => {
-    it('has locks', async () => {
+  describe('Unlock contract', () => {
+    it('has persisted data', async () => {
+      const globalTokenSymbolBefore = await unlock.globalTokenSymbol()
+      const globalBaseTokenURIBefore = await unlock.globalBaseTokenURI()
+      const grossNetworkProductBefore = await unlock.grossNetworkProduct()
+      const udtBefore = await unlock.udt()
+      const wethBefore = await unlock.weth()
       const publicLockAddressBefore = await unlock.publicLockAddress()
+      const estimatedGasForPurchaseBefore =
+        await unlock.estimatedGasForPurchase()
+
       const updated = await upgradeContract()
+      const globalTokenSymbolAfter = await updated.globalTokenSymbol()
+      const globalBaseTokenURIAfter = await updated.globalBaseTokenURI()
+      const grossNetworkProductAfter = await updated.grossNetworkProduct()
+      const udtAfter = await updated.udt()
+      const wethAfter = await updated.weth()
       const publicLockAddressAfter = await updated.publicLockAddress()
+      const estimatedGasForPurchaseAfter =
+        await updated.estimatedGasForPurchase()
+      assert.equal(globalTokenSymbolBefore, globalTokenSymbolAfter)
+      assert.equal(globalBaseTokenURIBefore, globalBaseTokenURIAfter)
+      assert.equal(grossNetworkProductBefore.eq(grossNetworkProductAfter), true)
+      assert.equal(udtBefore, udtAfter)
+      assert.equal(wethBefore, wethAfter)
       assert.equal(publicLockAddressBefore, publicLockAddressAfter)
+      assert.equal(
+        estimatedGasForPurchaseBefore.eq(estimatedGasForPurchaseAfter),
+        true
+      )
+    })
+
+    it('deploys a lock and purchases a key!', async () => {
+      const updated = await upgradeContract()
+      let tx = await updated.createLock(
+        60, // expirationDuration: 1 minute!
+        web3.utils.padLeft(0, 40),
+        0, // keyPrice: in wei
+        100, // maxNumberOfKeys
+        'Upgrade Test Lock',
+        '0x000000000000000000000000'
+      )
+      const { events } = await tx.wait()
+      const evt = events.find(({ event }) => event === 'NewLock')
+
+      const PublicLock = await ethers.getContractFactory('PublicLock', deployer)
+      let publicLock = await PublicLock.attach(evt.args.newLockAddress)
+
+      const expirationBefore = await publicLock.keyExpirationTimestampFor(
+        recipient
+      )
+      assert(expirationBefore.eq(0))
+
+      let purchaseTx = await publicLock.purchase(0, recipient, referrer, [])
+      await purchaseTx.wait()
+
+      const expirationAfter = await publicLock.keyExpirationTimestampFor(
+        recipient
+      )
+
+      assert(expirationAfter.gt(0))
     })
   })
 })
