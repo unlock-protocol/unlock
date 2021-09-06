9486b5ffa42b36eea4fa0b24ebfe9ce657b46fc8
Clement Renaud <clement@clementrenaud.com>
2021-09-06 17:06:37 +0200

    storyshot jest-styled-components fix

--- paywall/src/__tests__/storybook/storyshots.test.js
+++ paywall/src/__tests__/storybook/storyshots.test.js
@@ -1,18 +1,32 @@
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
 import initStoryshots, {
-  snapshotWithOptions,
+  Stories2SnapsConverter,
 } from '@storybook/addon-storyshots'
-import { render as renderer } from '@testing-library/react'
+import * as renderer from 'react-test-renderer'
+
+import 'jest-styled-components'
+import { styleSheetSerializer } from 'jest-styled-components/serializer'
+import { addSerializer } from 'jest-specific-snapshot'
+
+addSerializer(styleSheetSerializer)
+
+registerRequireContextHook()
 
 initStoryshots({
   renderer,
-  test: (info) => {
-    /* eslint-disable no-console */
+  test: ({ story, context }) => {
+    const converter = new Stories2SnapsConverter()
+    const snapshotFilename = converter.getSnapshotFileName(context)
 
+    /* eslint-disable no-console */
     const { error } = console
 
     try {
       console.error = jest.fn(console.error)
-      snapshotWithOptions({ renderer })(info)
+      const storyElement = story.render(context)
+      const tree = renderer.create(storyElement).toJSON()
+      expect(tree).toMatchSpecificSnapshot(snapshotFilename)
+      // snapshotWithOptions({ renderer })(info)
       expect(console.error).not.toHaveBeenCalled()
     } finally {
       console.error = error
