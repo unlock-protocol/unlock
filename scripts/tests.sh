578c6d4732e435495bf5ed3fb3d2d220d5cf816d
Clement Renaud <clement@clementrenaud.com>
2021-09-06 14:03:46 +0200

    scripts build step uses compose

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -18,10 +18,8 @@ UPCASE_SERVICE="${SERVICE^^}"
 ENV_VARS_PREFIX="${UPCASE_SERVICE//-/_}_"
 ENV_VARS=`env | grep "^$ENV_VARS_PREFIX" | awk '{print "-e ",$1}' ORS=' ' | sed -e "s/$ENV_VARS_PREFIX//g"`
 
-# We cannot rely on docker compose to build the images since we have a base :(
 # First we need to build the base
-docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t unlock-core -f docker/unlock-core.dockerfile --cache-from unlockprotocol/unlock-core:master .
-docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t $SERVICE -f docker/$SERVICE.dockerfile --cache-from unlockprotocol/$SERVICE:master .
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE build $SERVICE
 
 # Run tests
 docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE run -e CI=true $ENV_VARS $SERVICE $COMMAND
