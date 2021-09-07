478c8037a7f12e42e281427a5dcc7669ddde6382
Clement Renaud <clement@clementrenaud.com>
2021-09-07 12:42:18 +0200

    setimmediate logger patch

--- locksmith/src/app.ts
+++ locksmith/src/app.ts
@@ -1,8 +1,8 @@
+import 'setimmediate'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import 'setimmediate'
 import expressWinston from 'express-winston' // TODO: use a single logger!
 import winston from 'winston'
 import * as Sentry from '@sentry/node'
