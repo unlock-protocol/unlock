3bd99363458f2421ca444b9bea3ef6cdc22d2350
Julien Genestoux <julien.genestoux@gmail.com>
2021-09-09 13:56:47 -0400

    removed nudge (#7524)

--- nudge/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/build/'],
-  preset: 'ts-jest/presets/default',
-}
--- nudge/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { CronJob } from 'cron'
-import { Processor } from './processor'
-
-const config = require('../config/config')
-const runInterval = '*/5 * * * *'
-
-new CronJob(runInterval, () => {
-  new Processor(config.graphQLEndpoint).processKeys()
-}).start()
