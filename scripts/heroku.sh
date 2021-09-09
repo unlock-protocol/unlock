caee1f875cd482dc500d93f10d98c45797e6fe2d
Clément Renaud <clement@clementrenaud.com>
2021-09-09 17:59:33 +0200

    Fix aws deployment (#7523)
    * add prod stage to docker file
    
    * locksmith move packages to devDeps
    
    * docker yarn prod cmd
    
    * move sequelize ts out of dev deps
    
    * bring back missing test dep
    
    * reflect-metadata is needed in prod
    
    * basic heroku deployment script
    
    * add heroku deployment to CI
    
    * ci config params
    
    * ci factor ubuntu executor
    
    * better home dir setup
    
    * docker soft dir creation
    
    * refactor ci config test workflow
    
    * ci config refactor netlify
    
    * docker build info
    
    * bypass ci tests to debug heroku
    
    * add missing build to ci heroku docker
    
    * docker cmd typo
    
    * ci fix service name
    
    * better heroku naming on ci
    
    * move heroku workflow from ci to scripts
    
    * typo ci
    
    * add staging connection to db
    
    * fix heroku cli + add db migrate task
    
    * heroku script remove exit
    
    * fix  heroku db ssl
    
    * renamed heroku app
    
    * pass down heroku run exit code
    
    * invert args heroku run
    
    * remove --exit-code flag bcz of heroku bug
    
    * heroku requires integrqtion tests

---
+++ scripts/heroku.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -eu
+
+# two args
+SERVICE=$1
+HEROKU_APP_NAME=$2
+
+echo "Deploying $SERVICE to Heroku $HEROKU_APP_NAME ..."
+
+# install heroku client
+if ! command -v heroku &> /dev/null
+then
+    echo "installing heroku"
+    curl https://cli-assets.heroku.com/install.sh | sh
+fi
+
+# build image
+docker build --rm=false -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$SERVICE .
+
+# push image to Heroku registry
+docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+
+#make sure we are logged in
+heroku container:login
+
+# release on heroku 
+heroku container:release -a $HEROKU_APP_NAME web
+
+# migrate the database
+heroku run --app $HEROKU_APP_NAME yarn db:migrate
