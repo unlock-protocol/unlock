b6c64369589a1e3d82c9004ce8c4a242782fe0f9
Clement Renaud <clement@clementrenaud.com>
2021-09-15 17:56:06 +0200

    init deploymeny script

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -4,68 +4,96 @@ const { ethers, upgrades } = require('hardhat')
 const { getNetworkName } = require('../helpers/network')
 const { addDeployment } = require('../helpers/deployments')
 
+const { AddressZero } = ethers.constants
+
+// TODO: params
+const premintAmount = 20000
+const estimatedGasForPurchase = 0
+const wethAddress = AddressZero
+const locksmithHost = process.env.LOCKSMITH_HOST || '127.0.0.1'
+const locksmithPort = process.env.LOCKSMITH_PORT || 3000
+let udtAddress = null
+
+// helpers
+const log = (...message) => {
+  // eslint-disable-next-line no-console
+  console.log('UNLOCK SETUP >', ...message)
+}
+
+const saveDeploymentInfo = async (contractName, data) => {
+  await addDeployment(contractName, data, true)
+}
+
 async function main() {
-  const [unlockOwner, minter] = await ethers.getSigners()
+  const [unlockOwner, minter, holder] = await ethers.getSigners()
 
   // fetch chain info
   const chainId = await unlockOwner.getChainId()
   const networkName = getNetworkName(chainId)
-
-  // eslint-disable-next-line no-console
-  console.log(
+  const isLocalNet = networkName === 'localhost'
+  log(
     `Deploying contracts on ${networkName} with the account: ${unlockOwner.address}`
   )
+  log(`isLocalNet : ${isLocalNet}`)
+  log(`Deployment info saved to ./deployments/${networkName}.`)
 
-  // 1. deploying Unlock with a proxy
+  // 1. deploying Unlock with a transparent / upgradable proxy
   const Unlock = await ethers.getContractFactory('Unlock')
-
   const unlock = await upgrades.deployProxy(Unlock, [unlockOwner.address], {
     initializer: 'initialize(address)',
   })
   await unlock.deployed()
-
-  // eslint-disable-next-line no-console
-  console.log('Unlock proxy deployed to:', unlock.address)
-
-  // save deployment info
-  const unlockDeployment = await addDeployment('Unlock', unlock, true)
-  // eslint-disable-next-line no-console
-  console.log(`Deployment info for ${unlockDeployment.contractName} saved.`)
+  await saveDeploymentInfo('Unlock', unlock)
+  log('Unlock proxy deployed to:', unlock.address)
 
   // 2. deploying PublicLock
   const PublicLock = await ethers.getContractFactory('PublicLock')
   const publicLock = await PublicLock.deploy()
-
-  // eslint-disable-next-line no-console
-  console.log('PublicLock deployed at', publicLock.address)
-
-  // save deployment info
-  const lockDeployment = await addDeployment('PublicLock', publicLock)
-
-  // eslint-disable-next-line no-console
-  console.log(`Deployment info for ${lockDeployment.contractName} saved.`)
+  await saveDeploymentInfo('PublicLock', publicLock)
+  log('PublicLock deployed at', publicLock.address)
 
   // 3. setting lock template
   unlock.setLockTemplate(publicLock.address, {
     from: unlockOwner.address,
     gasLimit: constants.MAX_GAS,
   })
-  // eslint-disable-next-line no-console
-  console.log('Template set for newly deployed lock')
+  log('Template set for newly deployed lock')
 
-  // 4. deploy UDT
-  const UDT = await ethers.getContractFactory('UnlockDiscountToken')
-  const token = await upgrades.deployProxy(UDT, [minter.address], {
-    initializer: 'initialize(address)',
-  })
-  await token.deployed()
-  // eslint-disable-next-line no-console
-  console.log('UDT deployed to:', token.address)
+  // 4. setup UDT if needed
+  if (!udtAddress) {
+    // deploy UDT v2 (upgradable)
+    const UDT = await ethers.getContractFactory('UnlockDiscountTokenV2')
+    let udt = await upgrades.deployProxy(UDT, [minter.address], {
+      initializer: 'initialize(address)',
+    })
+    await udt.deployed()
+    await saveDeploymentInfo('UnlockDiscountToken', udt)
+    log('UDT (v2) deployed to:', udt.address)
 
-  // save deployment info
-  const UDTdeployment = await addDeployment('UnlockDiscountToken', token, true)
-  // eslint-disable-next-line no-console
-  console.log(`Deployment info for ${UDTdeployment.contractName} saved.`)
+    // pre-mint some UDTs, then delegate mint caps to contract
+    udt = udt.connect(minter)
+    await udt.mint(holder.address, premintAmount)
+    log(`Pre-minted ${premintAmount} UDT`)
+
+    await udt.addMinter(unlock.address)
+    log('grant minting permissions to the Unlock Contract')
+
+    await udt.renounceMinter()
+    log('minter renounced minter role')
+
+    udtAddress = udt.address
+  }
+
+  // 5. Config unlock
+  unlock.configUnlock(
+    udtAddress,
+    wethAddress,
+    estimatedGasForPurchase,
+    'UDT',
+    `http://${locksmithHost}:${locksmithPort}/api/key/`,
+    chainId
+  )
+  log('unlock configured properly')
 }
 
 main()
