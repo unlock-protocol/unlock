e86225ffefbc014c407ee571e0c0d6d696ade130
Clement Renaud <clement@clementrenaud.com>
2021-09-21 11:38:36 +0200

    add networks to paywall + bump 0.0.2

--- paywall/src/paywall-script/networkConfigs.ts
+++ paywall/src/paywall-script/networkConfigs.ts
@@ -1,8 +1,9 @@
+import networks from '@unlock-protocol/networks'
 import { NetworkConfigs } from '../unlockTypes'
 
 declare var PAYWALL_URL: string
 
-let unlockAppUrl
+let unlockAppUrl: string
 const baseUrl = PAYWALL_URL || 'localhost' // Set at build time
 
 if (baseUrl.match('staging-paywall.unlock-protocol.com')) {
@@ -15,33 +16,22 @@ if (baseUrl.match('staging-paywall.unlock-protocol.com')) {
 
 // TODO: allow customization of these values when running the script
 // This means probably adding to the unlockProtocolConfig object to include the provider, loksmith Uri and unlockAppUrl
-export const networkConfigs: NetworkConfigs = {
-  1: {
-    readOnlyProvider:
-      'https://eth-mainnet.alchemyapi.io/v2/b7Mxclz5hGyHqoeodGLQ17F5Qi97S7xJ',
-    locksmithUri: 'https://locksmith.unlock-protocol.com',
-    unlockAppUrl,
-  },
-  4: {
-    readOnlyProvider:
-      'https://eth-rinkeby.alchemyapi.io/v2/n0NXRSZ9olpkJUPDLBC00Es75jaqysyT',
-    locksmithUri: 'https://rinkeby.locksmith.unlock-protocol.com',
-    unlockAppUrl,
-  },
-  100: {
-    readOnlyProvider: 'https://rpc.xdaichain.com/',
-    locksmithUri: 'https://locksmith.unlock-protocol.com',
-    unlockAppUrl,
-  },
-  137: {
-    readOnlyProvider:
-      'https://snowy-weathered-waterfall.matic.quiknode.pro/5b11a0413a62a295070c0dfb25637d5f8c591aba/',
-    locksmithUri: 'https://locksmith.unlock-protocol.com',
-    unlockAppUrl,
-  },
-  1337: {
-    readOnlyProvider: 'http://127.0.0.1:8545',
-    locksmithUri: 'http://127.0.0.1:8080',
-    unlockAppUrl,
-  },
-}
+export const networkConfigs: NetworkConfigs = {}
+
+const chainIds = [
+  1, 4, 100, 137, 1337
+]
+
+chainIds.map((chainId) => {
+  const {
+    readOnlyProvider,
+    provider,
+    locksmithUri
+  } = networks[chainId]
+
+  networkConfigs[chainId] = {
+    readOnlyProvider : readOnlyProvider || provider,
+    locksmithUri,
+    unlockAppUrl
+   }
+})
