3518cd3b9f4236deaedc54f0690c85613baa2177
Clement Renaud <clement@clementrenaud.com>
2021-09-22 16:49:40 +0200

    fix local packages linkage in docker prod build

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -15,7 +15,7 @@ then
 fi
 
 # build image
-docker build --rm=false -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$SERVICE .
+docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$SERVICE .
 
 # push image to Heroku registry
 docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
