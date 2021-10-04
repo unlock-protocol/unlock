4a59c93e89f95cffeff86f44df103e4b9272ea0a
Clement Renaud <clement@clementrenaud.com>
2021-10-04 12:45:41 +0200

    lintfix eslintrc - so meta

--- newsletter/.eslintrc.js
+++ newsletter/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: ['@unlock-protocol/eslint-config'],
-  "rules": {
+  rules: {
     // note you must disable the base rule as it can report incorrect errors
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "no-nested-ternary": "off"
-  }
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-nested-ternary': 'off',
+  },
 }
