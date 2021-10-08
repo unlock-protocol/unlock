57f7e13bca8bd4b8ef291b7c800a54f58bde982e
Clement Renaud <clement@clementrenaud.com>
2021-10-08 14:49:46 +0200

    add missing ERC20-like approval events

--- smart-contracts/contracts/mixins/MixinLockCore.sol
+++ smart-contracts/contracts/mixins/MixinLockCore.sol
@@ -39,6 +39,21 @@ contract MixinLockCore is
     address tokenAddress
   );
 
+   /**
+    * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+    */
+  event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+  /**
+    * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+    */
+  event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+  /**
+    * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+    */
+  event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
   // Unlock Protocol address
   // TODO: should we make that private/internal?
   IUnlock public unlockProtocol;
