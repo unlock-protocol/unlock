299aeb224455e0ee3ce5a6c4edd76d9fec6083af
Julien Genestoux <julien.genestoux@gmail.com>
2021-10-12 11:47:22 -0400

    not failing when locks could not be loaded (#7642)

--- unlock-app/src/services/graphService.ts
+++ unlock-app/src/services/graphService.ts
@@ -14,24 +14,32 @@ export class GraphService {
 
   locksByManager = async (owner: string) => {
     const query = locksByManager()
-    const result = await this.client.query({
-      query,
-      variables: {
-        owner,
-      },
-    })
+    try {
+      const result = await this.client.query({
+        query,
+        variables: {
+          owner,
+        },
+      })
 
-    // TODO: map fields so that we get the same output values than unlock-js (keyPrice should use decimals... etc)
+      // TODO: map fields so that we get the same output values than unlock-js (keyPrice should use decimals... etc)
 
-    // the Graph returns lower cased addresses.
-    // To make sure we stay consistent with the rest of the app
-    // We use checksumed addresses.
-    return result.data.lockManagers.map((manager: any) => {
-      return {
-        ...manager.lock,
-        address: utils.getAddress(manager.lock.address),
-      }
-    })
+      // the Graph returns lower cased addresses.
+      // To make sure we stay consistent with the rest of the app
+      // We use checksumed addresses.
+      return result.data.lockManagers.map((manager: any) => {
+        return {
+          ...manager.lock,
+          address: utils.getAddress(manager.lock.address),
+        }
+      })
+    } catch (error) {
+      console.error(error)
+      alert(
+        'We could not load your locks. Please retry and let us know if that keeps failing'
+      )
+      return []
+    }
   }
 
   keysByLocks = async (
