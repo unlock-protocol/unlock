28011656042c244a419c138c52fbf718e2e178d4
Clement Renaud <clement@clementrenaud.com>
2021-10-20 16:04:36 +0200

    passing tests

--- smart-contracts/test/mainnet/udt.js
+++ smart-contracts/test/mainnet/udt.js
@@ -1,5 +1,8 @@
 const { ethers } = require('hardhat')
 const { reverts } = require('truffle-assertions')
+const { getProxyAddress } = require('../../helpers/proxy')
+
+const { resetState } = require('../helpers/mainnet')
 
 const { errorMessages } = require('../helpers/constants')
 
@@ -7,21 +10,30 @@ const { VM_ERROR_REVERT_WITH_REASON } = errorMessages
 
 contract('UnlockDiscountToken on mainnet', async () => {
   let udt
+  const chainId = 1 // mainnet
 
-  before(() => {
+  beforeEach(async function setupMainnetForkTestEnv() {
     if (!process.env.RUN_MAINNET_FORK) {
       // all suite will be skipped
       this.skip()
     }
-  })
 
-  beforeEach(async () => {
+    // reset fork
+    await resetState()
+
+    // prepare proxy info
+    const proxyAddress = getProxyAddress(chainId, 'UnlockDiscountTokenV2')
+    // const proxyAdmin = getProxyAdminAddress({ network })
+    // await impersonate(proxyAdmin)
+
+    // mocha settings
+    this.timeout(200000)
+
     const UnlockDiscountToken = await ethers.getContractFactory(
       'UnlockDiscountTokenV2'
     )
-
     const [, minter] = await ethers.getSigners()
-    udt = await UnlockDiscountToken.connect(minter)
+    udt = await UnlockDiscountToken.attach(proxyAddress).connect(minter)
   })
 
   describe('ERC20 Details', () => {
@@ -55,17 +67,19 @@ contract('UnlockDiscountToken on mainnet', async () => {
 
   describe('Burn', () => {
     it('function does not exist', async () => {
-      // reverts
-      await udt.burn()
+      assert.equal(udt.burn, null)
+      assert.equal(Object.keys(udt).includes('burn'), false)
+      assert.equal(Object.keys(udt.functions).includes('burn'), false)
+      assert.equal(Object.keys(udt.functions).includes('burn()'), false)
     })
   })
 
   describe('Supply', () => {
     it('is not 0', async () => {
       const totalSupply = await udt.totalSupply()
-      assert.notEqual(
-        totalSupply.toNumber(),
-        0,
+      assert.equal(
+        totalSupply.gt(0),
+        true,
         'starting supply must be different from 0'
       )
     })
