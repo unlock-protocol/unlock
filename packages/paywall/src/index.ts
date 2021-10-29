b6c4a82a0f0f674217ba813def9548f860b7d6ee
Julien Genestoux <julien.genestoux@gmail.com>
2021-10-29 17:45:25 -0400

    refactored network on paywall

--- packages/paywall/src/index.ts
+++ packages/paywall/src/index.ts
@@ -3,8 +3,6 @@ import { setupUnlockProtocolVariable } from './utils'
 import { networkConfigs } from './networkConfigs'
 
 const rawConfig = (window as any).unlockProtocolConfig
-const baseUrl =
-  document?.currentScript?.getAttribute('src') || 'paywall.unlock-protocol.com' // assume prod
 
 if (!rawConfig) {
   console.error(
@@ -13,23 +11,12 @@ if (!rawConfig) {
 } else {
   // set network based on hostname if missing in rawConfig
   if (!rawConfig.network) {
-    if (baseUrl.match('staging-paywall.unlock-protocol.com')) {
-      rawConfig.network = 4
-      console.error(
-        'Missing network in Unlock config. Assigned default to mainnet: 1. See https://docs.unlock-protocol.com/'
-      )
-    } else if (baseUrl.match('paywall.unlock-protocol.com')) {
-      rawConfig.network = 1
-      console.error(
-        'Missing network in Unlock config. Assigned default to rinkeby 4. See https://docs.unlock-protocol.com/'
-      )
-    } else {
-      console.error(
-        'Missing network in Unlock config. Please set one. See https://docs.unlock-protocol.com/'
-      )
-    }
+    rawConfig.network = 1
+    console.info(
+      'For backward compatibility setting default network to 1. See https://docs.unlock-protocol.com/'
+    )
   } else {
-    rawConfig.network = parseInt(rawConfig.network)
+    rawConfig.network = parseInt(rawConfig.network, 10)
   }
 
   const paywall = new Paywall(rawConfig, networkConfigs)
