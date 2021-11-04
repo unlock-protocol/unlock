3f7aa524a94901999f709707d2a67a4259208dd3
Clement Renaud <clement@clementrenaud.com>
2021-11-04 13:38:14 +0100

    helper to add UDT to any address

--- smart-contracts/test/helpers/mainnet.js
+++ smart-contracts/test/helpers/mainnet.js
@@ -1,4 +1,5 @@
 const { ethers, network, config } = require('hardhat')
+const { getDeployment } = require('../../helpers/deployments')
 
 const resetNodeState = async () => {
   // reset fork
@@ -29,8 +30,39 @@ const impersonate = async (address) => {
   await addSomeETH(address) // give some ETH just in case
 }
 
+const toBytes32 = (bn) => {
+  return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32))
+}
+
+const addUDT = async (recipientAddress, amount = 1000) => {
+  const { chainId } = await ethers.provider.getNetwork()
+
+  // UDT contract
+  const { address: udtAddress } = getDeployment(
+    chainId,
+    'UnlockDiscountTokenV2'
+  )
+  const udtAmount = ethers.utils.parseEther(`${amount}`)
+
+  // NB: slot has been found by using slot20 - see https://kndrck.co/posts/local_erc20_bal_mani_w_hh/
+  // Get storage slot index
+  const index = ethers.utils.solidityKeccak256(
+    ['uint256', 'uint256'],
+    [recipientAddress, 51] // key, slot
+  )
+
+  // Manipulate local balance (needs to be bytes32 string)
+  await network.provider.request({
+    method: 'hardhat_setStorageAt',
+    params: [udtAddress, index.toString(), toBytes32(udtAmount).toString()],
+  })
+  // Just mines to the next block
+  await ethers.provider.send('evm_mine', [])
+}
+
 module.exports = {
   resetNodeState,
   impersonate,
+  addUDT,
   addSomeETH,
 }
