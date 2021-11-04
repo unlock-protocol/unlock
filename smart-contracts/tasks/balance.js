383ae0e80b56c1511f03d39aaaa5c525fb03aa8e
Clement Renaud <clement@clementrenaud.com>
2021-11-04 17:42:45 +0100

    helpers for udt balance

--- smart-contracts/tasks/balance.js
+++ smart-contracts/tasks/balance.js
@@ -1,10 +1,19 @@
 const { task } = require('hardhat/config')
+const { getDeployment } = require('../helpers/deployments')
 
-task('balance', "Prints an account's balance")
+task('balance', "Prints an account's ETH balance")
   .addParam('account', "The account's address")
   .setAction(async (taskArgs) => {
     const account = web3.utils.toChecksumAddress(taskArgs.account)
     const balance = await web3.eth.getBalance(account)
     // eslint-disable-next-line no-console
     console.log(web3.utils.fromWei(balance, 'ether'), 'ETH')
+  })
+
+task('balance:udt', "Prints an account's UDT balance")
+  .addParam('account', "The account's address")
+  .setAction(async ({ account }) => {
+    // eslint-disable-next-line global-require
+    const udtBalance = require('../scripts/getters/udt-balance')
+    await udtBalance({ account })
   })
