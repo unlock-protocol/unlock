81c7fe1e2d6039d37636cbb48d75611ff1cac9f7
Clement Renaud <clement@clementrenaud.com>
2021-11-05 12:35:53 +0100

    move authoritaran logic to main script

--- smart-contracts/scripts/gov/vote.js
+++ smart-contracts/scripts/gov/vote.js
@@ -2,58 +2,36 @@ const { ethers } = require('hardhat')
 const { time } = require('@openzeppelin/test-helpers')
 const { getDeployment } = require('../../helpers/deployments')
 const { getProposalState } = require('../../helpers/gov')
-const { impersonate, addUDT } = require('../../test/helpers/mainnet')
+const { impersonate } = require('../../test/helpers/mainnet')
 
-async function main({ voter, proposalId, authority }) {
+async function main({ voter, proposalId }) {
   // env settings
   const { chainId } = await ethers.provider.getNetwork()
   const isDev = chainId === 31337
-  // eslint-disable-next-line no-console
-  if (isDev) console.log('Dev mode ON')
 
-  if (!voter) {
-    // eslint-disable-next-line no-console
-    throw new Error('GOV VOTE > Missing voter address.')
-  }
   if (!proposalId) {
     // eslint-disable-next-line no-console
     throw new Error('GOV VOTE > Missing proposal ID.')
   }
-  if (authority && !isDev) {
+  if (!voter && !isDev) {
     // eslint-disable-next-line no-console
     throw new Error(
-      'GOV VOTE > The authority flag can only be used on dev network.'
+      'GOV VOTE > Cast vote w/o voter (authoritarian mode) can only be used on dev network.'
     )
   }
 
   if (isDev) {
+    if (!voter) {
+      // No voter equals to authoritarian mode: a single voter win!
+      const [, , dictator] = await ethers.getSigners()
+      voter = dictator.address
+      // eslint-disable-next-line no-console
+      console.log('GOV VOTE (dev) > Authoritarian mode ON: bypass quorum')
+    }
     await impersonate(voter)
     // make sure proposal state is active
     const state = await getProposalState(proposalId)
     if (state === 'Pending') {
-      await time.advanceBlock()
-    }
-
-    if (authority) {
-      // let's reach concensus
-      const quorum = 15000 // 15k UDT quorum hardcoded in contract
-      const [, holder] = await ethers.getSigners()
-
-      // give twice the quorum in UDT
-      await addUDT(holder.address, quorum * 2)
-      const { address: udtAddress, abi: udtAbi } = getDeployment(
-        chainId,
-        'UnlockDiscountTokenV2'
-      )
-      const udt = await new ethers.Contract(udtAddress, udtAbi, holder)
-
-      const tx = await udt.delegate(voter)
-      const { events } = await tx.wait()
-      const evt = events.find((v) => v.event === 'DelegateVotesChanged')
-      if (evt) {
-        // eslint-disable-next-line no-console
-        console.log('GOV VOTE (dev) > delegated quorum to voter')
-      }
       await time.advanceBlock()
     }
   }
@@ -62,22 +40,35 @@ async function main({ voter, proposalId, authority }) {
   const { address, abi } = getDeployment(chainId, 'UnlockProtocolGovernor')
   const gov = await ethers.getContractAt(abi, address)
 
+  const currentBlock = await ethers.provider.getBlockNumber()
+  // eslint-disable-next-line no-console
+  console.log(
+    'GOV VOTE >',
+    `voter ${voter}: power ${ethers.utils.formatUnits(
+      await gov.getVotes(voter, currentBlock - 1),
+      18
+    )} `,
+    `(quorum ${ethers.utils.formatUnits(
+      await gov.quorum(currentBlock - 1),
+      18
+    )})`
+  )
   const state = await gov.state(proposalId)
   if (state === 1) {
     const tx = await gov.connect(voterWallet).castVote(proposalId, '1')
     const { events, transactionHash } = await tx.wait()
-
-    // console.log(await gov.state(proposalId))
     const evt = events.find((v) => v.event === 'VoteCast')
 
+    // double check vote happened
+    const hasVoted = await gov.hasVoted(proposalId, voter)
+
     // check for failure
-    if (!evt) {
+    if (!evt || !hasVoted) {
       throw new Error('GOV VOTE > Vote not casted.')
     }
-
     // eslint-disable-next-line no-console
     console.log(
-      `vote casted: ${voter} approves (votes 1) (txid: ${transactionHash})`
+      `GOV VOTE > vote casted: ${voter} approves (weigth: ${evt.args.weight.toString()}) (txid: ${transactionHash})`
     )
   } else {
     throw new Error(`GOV VOTE > Proposal state (${state}) does not allow vote.`)
