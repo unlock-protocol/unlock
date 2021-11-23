c47fe07afc7f541bf3fa46dd16726ec3402ec321
Clement Renaud <clement@clementrenaud.com>
2021-11-23 10:24:12 +0100

    add comment on contract

--- smart-contracts/contracts/UnlockDiscountTokenV3.sol
+++ smart-contracts/contracts/UnlockDiscountTokenV3.sol
@@ -8,9 +8,15 @@ import './UnlockDiscountTokenV3.sol';
 * This smart contract implements the Unlock Discount Token
 */
 contract UnlockDiscountTokenV3 is UnlockDiscountTokenV3 {
+
   function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+    /**
+      * This two lines were added on Nov 21st 2021 after an attack on UDT deployer wallet
+      * They prevent token transfers from Polygon and xDai bridges
+      */
     require(from != 0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf, "Transfer from Polygon disabled");
     require(from != 0x88ad09518695c6c3712AC10a214bE5109a655671, "Transfer from xDAI disabled");
+
     return super._beforeTokenTransfer(from, to, amount);
   }
 }
