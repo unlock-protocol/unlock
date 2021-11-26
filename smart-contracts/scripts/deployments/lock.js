fe40248345eda6ae504efff87d663251f19ebb0f
Clement Renaud <clement@clementrenaud.com>
2021-11-26 16:58:30 +0100

    script to deploy locks

---
+++ smart-contracts/scripts/deployments/lock.js
@@ -0,0 +1,76 @@
+const { ethers } = require('hardhat')
+const contracts = require('@unlock-protocol/contracts')
+
+async function main({ unlockAddress, unlockVersion, serializedLock, salt }) {
+  // get the right version of Unlock
+  let Unlock
+  if (!unlockVersion) {
+    Unlock = await ethers.getContractFactory('Unlock')
+  } else {
+    const { abi, bytecode } = contracts[`UnlockV${unlockVersion}`]
+    Unlock = await ethers.getContractFactory(abi, bytecode)
+  }
+  const unlock = Unlock.attach(unlockAddress)
+
+  const { expirationDuration, tokenAddress, keyPrice, maxNumberOfKeys, name } =
+    serializedLock
+
+  const tx = unlock.createLock(
+    // _lock.beneficiary,
+    expirationDuration,
+    tokenAddress,
+    keyPrice,
+    maxNumberOfKeys,
+    name,
+    salt
+  )
+
+  const w = await tx.wait()
+  const { args } = w.events.find(({ event }) => event === 'NewLock')
+  const { newLockAddress } = args
+
+  // eslint-disable-next-line no-console
+  console.log(
+    `CLONE LOCK > deployed to : ${newLockAddress} (tx: ${w.deployTransaction.hash})`
+  )
+
+  // set lock
+  const publicLockVersion = await unlock.publicLockVersion()
+  const { abi: publicLockABI, bytecode: publicLockBytecode } =
+    contracts[`PublicLockV${publicLockVersion}`]
+  const PublicLock = await ethers.getContractFactory(
+    publicLockABI,
+    publicLockBytecode
+  )
+  const lock = PublicLock.attach(newLockAddress)
+
+  // check for default var
+  if (
+    serializedLock.freeTrialLength != 1000 ||
+    serializedLock.refundPenaltyBasisPoints != 1000
+  ) {
+    lock.updateRefundPenalty(
+      serializedLock.freeTrialLength,
+      serializedLock.refundPenaltyBasisPoints
+    )
+  }
+
+  if (serializedLock.transferFeeBasisPoints != 1000) {
+    lock.updateTransferFee(serializedLock.transferFeeBasisPoints)
+  }
+
+  return newLockAddress
+}
+
+// execute as standalone
+if (require.main === module) {
+  /* eslint-disable promise/prefer-await-to-then, no-console */
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+module.exports = main
---
+++ smart-contracts/test/scripts/deploy/lock.js
@@ -0,0 +1,112 @@
+const { ethers } = require('hardhat')
+const { UnlockV9 } = require('@unlock-protocol/contracts')
+const { PublicLockV8 } = require('@unlock-protocol/contracts')
+
+const Locks = require('../../fixtures/locks')
+const deployLock = require('../../../scripts/deployments/lock')
+const compareValues = require('../../LockSerializer/_compareValues')
+
+contract('LockSerializer', () => {
+  let serializer
+  let unlockAddress
+  let PublicLock
+  const locks = {}
+  // addresses
+  let unlockOwner
+  let beneficiary
+  let manager
+
+  beforeEach(async () => {
+    ;[unlockOwner, beneficiary, manager] = await ethers.getSigners()
+
+    // deploy unlock
+    const Unlock = await ethers.getContractFactory(
+      UnlockV9.abi,
+      UnlockV9.bytecode
+    )
+    const unlock = await Unlock.deploy()
+    await unlock.deployed()
+
+    // deploy template
+    const PublicLock = await ethers.getContractFactory(
+      PublicLockV8.abi,
+      PublicLockV8.bytecode
+    )
+    const publicLock = await PublicLock.deploy()
+    await publicLock.deployed()
+
+    // set unlock
+    await unlock.initialize(unlockOwner.address)
+    await unlock.connect(unlockOwner).setLockTemplate(publicLock.address)
+
+    // deploy serializer
+    const LockSerializer = await ethers.getContractFactory('LockSerializer')
+    serializer = await LockSerializer.deploy()
+    await serializer.deployed()
+
+    // deploy locks
+    await Promise.all(
+      Object.keys(Locks).map(async (name) => {
+        const tx = await unlock.createLock(
+          Locks[name].expirationDuration.toFixed(),
+          web3.utils.padLeft(0, 40),
+          Locks[name].keyPrice.toFixed(),
+          Locks[name].maxNumberOfKeys.toFixed(),
+          Locks[name].lockName,
+          web3.utils.randomHex(12)
+        )
+        const { events } = await tx.wait()
+        const { args } = events.find((v) => v.event === 'NewLock')
+        locks[name] = await PublicLock.attach(args.newLockAddress)
+        locks[name].params = Locks[name]
+      })
+    )
+
+    unlockAddress = unlock.address
+  })
+
+  it('identical init args', async () => {
+    // check for all locks
+    Object.keys(locks).forEach(async (id) => {
+      const lock = locks[id]
+      const serialized = await serializer.serialize(lock.address)
+
+      // redeploy our lock
+      const tx = await deployLock({
+        unlockAddress,
+        unlockVersion: 9,
+        serializedLock: serialized,
+        salt: web3.utils.randomHex(12),
+      })
+      const { events } = await tx.wait()
+      const { args } = events.find(({ event }) => event === 'LockCLoned')
+      const { newLockAddress } = args
+
+      // make sure values are identical
+      const newLock = PublicLock.attach(newLockAddress)
+      await compareValues(serialized, lock)
+      await compareValues(serialized, newLock)
+    })
+  })
+  it('identical custom fees', async () => {
+    const lock = locks.FIRST
+
+    // set custom values
+    await lock.addLockManager(manager.address)
+    await lock.connect(manager).updateRefundPenalty(2222, 2222)
+    await lock.connect(manager).updateTransferFee(2222)
+
+    const serialized = await serializer.serialize(lock.address)
+
+    // redeploy our lock
+    const tx = await deployLock(unlockAddress, serialized)
+    const { events } = await tx.wait()
+    const { args } = events.find(({ event }) => event === 'LockCLoned')
+    const { newLockAddress } = args
+
+    // make sure values are identical
+    const newLock = PublicLock.attach(newLockAddress)
+    await compareValues(serialized, lock)
+    await compareValues(serialized, newLock)
+  })
+})
