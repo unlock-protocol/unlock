51a79bf28d8ba2b2ebd54a50bfb3f3940f0f2699
Clement Renaud <clement@clementrenaud.com>
2021-11-30 13:44:52 +0100

    clone lock managers

--- smart-contracts/scripts/lock/clone.js
+++ smart-contracts/scripts/lock/clone.js
@@ -1,5 +1,6 @@
 const { ethers } = require('hardhat')
-const createLock = require('../deployments/lock.js')
+const createLock = require('../deployments/lock')
+const listManagers = require('./listManagers')
 
 async function main({
   lockAddress,
@@ -25,7 +26,25 @@ async function main({
     salt: web3.utils.randomHex(12),
   })
 
-  console.log(newLockAddress)
+  // eslint-disable-next-line no-console
+  console.log('CLONE LOCK > fetching managers...')
+
+  // fetch managers from graph
+  const managers = await listManagers({ lockAddress: newLockAddress })
+
+  // add back managers to the lock
+  if (managers.length) {
+    // eslint-disable-next-line no-restricted-syntax
+    const txs = Promise.all(
+      managers.map((manager) => lock.addLockManager(manager))
+    )
+    const { events } = await txs.wait()
+    const added = events.map(({ event }) => event.name === 'LockManagerAdded')
+    added.forEach(({ args }) => {
+      // eslint-disable-next-line no-console
+      console.log(`LOCK CLONE > ${args[0]} added as lock manager.`)
+    })
+  }
 }
 
 // execute as standalone
