469ed80873ecae1bb19a617c423acb7bf74b8bb1
Clement Renaud <clement@clementrenaud.com>
2021-12-01 17:48:36 +0100

    fix lock manager clone logic

--- smart-contracts/scripts/lock/clone.js
+++ smart-contracts/scripts/lock/clone.js
@@ -49,19 +49,19 @@ async function main({
   console.log('CLONE LOCK > fetching managers...')
 
   // fetch managers from graph
-  const managers = await listManagers({ lockAddress: newLockAddress })
+  const managers = await listManagers({ lockAddress })
 
   // add back managers to the lock
   if (managers.length) {
     // eslint-disable-next-line no-restricted-syntax
-    const txs = Promise.all(
+    const txs = await Promise.all(
       managers.map((manager) => newLock.addLockManager(manager))
     )
-    const { events } = await txs.wait()
-    const added = events.map(({ event }) => event.name === 'LockManagerAdded')
-    added.forEach(({ args }) => {
+    const waits = await Promise.all(txs.map((tx) => tx.wait()))
+    waits.forEach(({ events }) => {
+      const evt = events.find(({ event }) => event === 'LockManagerAdded')
       // eslint-disable-next-line no-console
-      console.log(`LOCK CLONE > ${args[0]} added as lock manager.`)
+      console.log(`LOCK CLONE > ${evt.args.account} added as lock manager.`)
     })
   }
 }
