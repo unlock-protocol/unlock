3f8f72bfea221ff21f8147e4095f18e7d8dff5ba
Clement Renaud <clement@clementrenaud.com>
2021-12-01 17:07:01 +0100

    proper subgraph parsing

--- smart-contracts/scripts/lock/listManagers.js
+++ smart-contracts/scripts/lock/listManagers.js
@@ -1,47 +1,53 @@
 const { ethers } = require('hardhat')
 const fetch = require('cross-fetch')
-const { getNetworkName } = require('../../helpers/network')
+const { networks } = require('@unlock-protocol/networks')
 
 async function main({ lockAddress }) {
   const PublicLock = await ethers.getContractFactory('PublicLock')
   const lock = PublicLock.attach(lockAddress)
 
   const { chainId } = await ethers.provider.getNetwork()
-  const networkName = chainId === 1 ? 'unlock' : getNetworkName(chainId)
+  const { subgraphURI } = networks[chainId]
 
-  if (networkName === 'localhost') {
+  if (!subgraphURI) {
     // eslint-disable-next-line no-console
-    console.log('Can not fetch from The Graph on local network')
+    console.log(
+      'Missing subGraphURI for this network. Can not fetch from The Graph'
+    )
     return []
   }
 
-  const query = `query {
-    lockManagers {
-        address
-        lock {
-          id
+  const query = `
+    {
+      locks(where:{
+        address: "${lockAddress}"
+      }) {
+        LockManagers {
+          address
         }
       }
-    }`
-
-  const q = await fetch(
-    `https://api.thegraph.com/subgraphs/name/unlock-protocol/${networkName}`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        query,
-      }),
     }
-  )
+  `
+
+  const q = await fetch(subgraphURI, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query,
+    }),
+  })
+
+  const { data, errors } = await q.json()
+  if (errors) {
+    // eslint-disable-next-line no-console
+    console.log('LOCK > Error while fetching the graph', errors)
+    return []
+  }
 
   const {
-    data: { lockManagers },
-  } = await q.json()
-
-  const managers = lockManagers
-    .filter((m) => m.lock.id === lockAddress)
-    .map((m) => m.address)
+    locks: [{ LockManagers }],
+  } = data
+  const managers = LockManagers.map((m) => m.address)
 
   // eslint-disable-next-line no-console
   console.log(`LOCK > managers for the lock '${await lock.name()}':`)
