a61f75d9fd259654cd6a62a56e7544b4b8064bd9
Clement Renaud <clement@clementrenaud.com>
2021-12-02 13:34:19 +0100

    helper to fetch lock managers

---
+++ locksmith/src/utils/lockManagers.ts
@@ -0,0 +1,45 @@
+import fetch from 'cross-fetch'
+
+interface listManagersProps {
+  lockAddress: string
+  subgraphURI: string
+}
+
+// fetch all managers from the GRAPH
+export default async function ({
+  lockAddress,
+  subgraphURI,
+}: listManagersProps) {
+  const query = `
+    {
+      locks(where:{
+        address: "${lockAddress}"
+      }) {
+        LockManagers {
+          address
+        }
+      }
+    }
+  `
+
+  const q = await fetch(subgraphURI, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query,
+    }),
+  })
+
+  const { data, errors } = await q.json()
+  if (errors) {
+    // eslint-disable-next-line no-console
+    console.log('LOCK > Error while fetching the graph', errors)
+    return []
+  }
+
+  const {
+    locks: [{ LockManagers }],
+  } = data
+  const managers = LockManagers.map((m: any) => m.address)
+  return managers
+}
