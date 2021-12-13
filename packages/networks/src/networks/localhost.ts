6ddd5b7f9debe9389482daf809d1718f6e664c4f
Clément Renaud <clement@clementrenaud.com>
2021-12-13 17:45:30 +0100

    Locksmith route to migrate lock (#7882)

--- packages/networks/src/networks/localhost.ts
+++ packages/networks/src/networks/localhost.ts
@@ -8,4 +8,6 @@ export const localhost : NetworkConfig = {
     locksmithUri: 'http://127.0.0.1:8080',
     unlockAppUrl: 'http://0.0.0.0:3000',
     subgraphURI: 'http://localhost:8000/subgraphs/name/unlock-protocol/unlock',
+    serializerAddress: '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1',
+    unlockAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
 }
