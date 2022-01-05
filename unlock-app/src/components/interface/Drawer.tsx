f762724fd596bb2202987e150f6181d8da27cb1e
Julien Genestoux <julien.genestoux@gmail.com>
2022-01-05 14:34:48 +0100

    linting

--- unlock-app/src/components/interface/Drawer.tsx
+++ unlock-app/src/components/interface/Drawer.tsx
@@ -14,65 +14,73 @@ interface DrawerInterface {
  * @param param0
  * @returns
  */
-export const Drawer = ({isOpen, setIsOpen, children, title}: DrawerInterface) => (
+export const Drawer = ({
+  isOpen,
+  setIsOpen,
+  children,
+  title,
+}: DrawerInterface) => (
   <Transition.Root show={isOpen} as={React.Fragment}>
-      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setIsOpen}>
-        <div className="absolute inset-0 overflow-hidden">
+    <Dialog
+      as="div"
+      className="fixed inset-0 overflow-hidden"
+      onClose={setIsOpen}
+    >
+      <div className="absolute inset-0 overflow-hidden">
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-in-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+        <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
           <Transition.Child
             as={React.Fragment}
-            enter="ease-in-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in-out duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter="transform transition ease-in-out duration-300 sm:duration-500"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transform transition ease-in-out duration-300 sm:duration-500"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
           >
-            <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
-          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
-            <Transition.Child
-              as={React.Fragment}
-              enter="transform transition ease-in-out duration-300 sm:duration-500"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-300 sm:duration-500"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <div className="relative w-screen max-w-md">
-                <Transition.Child
-                  as={React.Fragment}
-                  enter="ease-in-out duration-500"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-500"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
+            <div className="relative w-screen max-w-md">
+              <Transition.Child
+                as={React.Fragment}
+                enter="ease-in-out duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
               >
-                  <div className="absolute top-0 right-0 h-12 w-12 p-4">
-                    <Close size="16px" onClick={() => setIsOpen(false)} />
-                  </div>
-                </Transition.Child>
+                <div className="absolute top-0 right-0 h-12 w-12 p-4">
+                  <Close size="16px" onClick={() => setIsOpen(false)} />
+                </div>
+              </Transition.Child>
               <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-
-
-
                 <div className="px-4 sm:px-6">
-                  {title && <Dialog.Title className="text-lg font-medium text-gray-900">{title}</Dialog.Title>}
+                  {title && (
+                    <Dialog.Title className="text-lg font-medium text-gray-900">
+                      {title}
+                    </Dialog.Title>
+                  )}
                 </div>
 
-
-
-                  <div className="mt-6 relative flex-1 px-4 sm:px-6">
-                    {children}
-                  </div>
+                <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                  {children}
                 </div>
               </div>
-            </Transition.Child>
-          </div>
+            </div>
+          </Transition.Child>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </div>
+    </Dialog>
+  </Transition.Root>
 )
 
 export default Drawer
