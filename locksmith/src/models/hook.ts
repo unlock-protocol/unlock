1b63bb6f7eb5795a7b3cb4d8d64a8d550fb0d9e0
searchableguy <me@searchableguy.com>
2022-01-10 21:49:54 +0530

    feat(websub): Add hook, hookEvent, etc models (#8011)

---
+++ locksmith/src/models/hook.ts
@@ -0,0 +1,36 @@
+import { Table, Column, Model, AllowNull, Validate } from 'sequelize-typescript'
+
+@Table({ tableName: 'Hooks', timestamps: true })
+export class Hook extends Model<Hook> {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id!: number
+
+  @Column
+  network!: string
+
+  @Column
+  topic!: string
+
+  @AllowNull(true)
+  @Column
+  lock?: string
+
+  @Column
+  expiration!: Date
+
+  @Validate({
+    isIn: [['subscribe', 'unsubscribe']],
+  })
+  @Column
+  mode!: 'subscribe' | 'unsubscribe'
+
+  @Validate({
+    isUrl: true,
+  })
+  @Column
+  callback!: string
+
+  @AllowNull(true)
+  @Column
+  secret?: string
+}
