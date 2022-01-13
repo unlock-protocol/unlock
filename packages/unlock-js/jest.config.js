2243c6d2c78e503a9ed6def6bd52b53fa10b0080
Clement Renaud <clement@clementrenaud.com>
2022-01-13 11:16:29 +0100

    uncomment jest transform

--- packages/unlock-js/jest.config.js
+++ packages/unlock-js/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '<rootDir>/src/__tests__/helpers/',
     '<rootDir>/src/__tests__/setup/',
   ],
-  // transform: {
-  //   '^.+\\.js?$': require.resolve('./jest.transform.js'),
-  // },
+  transform: {
+    '^.+\\.js?$': require.resolve('./jest.transform.js'),
+  },
 }
