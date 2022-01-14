52b70b326501d90d853238991a8895f0094d25ed
searchableguy <me@searchableguy.com>
2022-01-14 12:22:44 +0530

    feat(ui): Add Alert, IconButton, etc components (#8028)
    * feat(ui): Add Alert, IconButton, etc components
    
    * commit the package.json and lock changes
    
    * bring lock file from master
    
    * resolved conflict file

---
+++ packages/ui/lib/components/IconButton/IconButton.tsx
@@ -0,0 +1,41 @@
+import type { ReactNode, ButtonHTMLAttributes, ForwardedRef } from 'react'
+import type { SizeStyleProp, Size } from '../../types'
+import { twMerge } from 'tailwind-merge'
+import { forwardRef } from 'react'
+
+// Once we have a design system or language, we should omit className so user doesn't end up deviating too much.
+interface Props
+  extends Omit<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    'size' | 'ref' | 'children'
+  > {
+  size?: Size
+  icon: ReactNode
+  label: string
+}
+
+const SIZE_STYLES: SizeStyleProp = {
+  small: 'p-1',
+  medium: 'p-2',
+  large: 'p-3',
+}
+
+export const IconButton = forwardRef(
+  (props: Props, ref: ForwardedRef<HTMLButtonElement>) => {
+    const buttonClass = twMerge(
+      'border rounded hover:bg-gray-50',
+      props.size ? SIZE_STYLES[props.size] : SIZE_STYLES['medium'],
+      props.className
+    )
+    return (
+      <button
+        className={buttonClass}
+        {...props}
+        ref={ref}
+        aria-label={props.label}
+      >
+        {props.icon}
+      </button>
+    )
+  }
+)
---
+++ packages/ui/lib/types.ts
@@ -0,0 +1,2 @@
+export type Size = 'small' | 'medium' | 'large'
+export type SizeStyleProp = Partial<Record<Size, string>>
