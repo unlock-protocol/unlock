2adfae93b966dc7440af3ddec1e5884c11b0ec97
searchableguy <me@searchableguy.com>
2022-01-14 14:59:35 +0530

    Add routes for publisher

--- locksmith/src/routes/hook.ts
+++ locksmith/src/routes/hook.ts
@@ -1,9 +1,16 @@
 import express from 'express'
-import { subscriptionHandler } from '../controllers/hookController'
+import {
+  subscriptionHandler,
+  publisherHandler,
+} from '../controllers/hookController'
 
 const router = express.Router({ mergeParams: true })
 
 router.post('/:network/locks', subscriptionHandler)
 router.post('/:network/locks/:lock/keys', subscriptionHandler)
+router.get('/:network/locks', publisherHandler)
+router.get('/:network/locks/:lock/keys', publisherHandler)
+router.head('/:network/locks', publisherHandler)
+router.head('/:network/locks/:lock/keys', publisherHandler)
 
 module.exports = router
