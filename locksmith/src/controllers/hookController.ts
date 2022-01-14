164956b930c4f074aeb860df19cad82eef56cff0
searchableguy <me@searchableguy.com>
2022-01-14 15:51:39 +0530

    simplify the code and fix an error

--- locksmith/src/controllers/hookController.ts
+++ locksmith/src/controllers/hookController.ts
@@ -29,100 +29,89 @@ export async function subscribe(
   hub: z.infer<typeof Hub>,
   params: Record<string, string>
 ) {
-  const hook = await Hook.findOne({
-    where: {
-      topic: hub.topic,
-      callback: hub.callback,
-    },
-  })
-
-  if (!hook) {
-    const check = await isValidHubIntent(hub)
-    if (!check) {
-      throw new Error(
-        `Subscriber failed to confirm intent for callback: ${hub.callback} on topic: ${hub.topic}.`
-      )
-    }
-
-    const expiration = getExpiration(hub.lease_seconds)
-
-    const createdHook = await Hook.create({
-      expiration,
-      topic: hub.topic,
-      callback: hub.callback,
-      mode: hub.mode,
-      secret: hub.secret,
-      network: params.network,
-      lock: params.lock,
+  try {
+    const hook = await Hook.findOne({
+      where: {
+        topic: hub.topic,
+        callback: hub.callback,
+      },
     })
 
-    return createdHook
+    if (!hook) {
+      await validHubIntent(hub)
+
+      const expiration = getExpiration(hub.lease_seconds)
+
+      const createdHook = await Hook.create({
+        expiration,
+        topic: hub.topic,
+        callback: hub.callback,
+        mode: hub.mode,
+        secret: hub.secret,
+        network: params.network,
+        lock: params.lock,
+      })
+
+      return createdHook
+    }
+
+    if (hook.mode !== hub.mode) {
+      hook.mode = hub.mode
+    }
+
+    if (hub.lease_seconds) {
+      hook.expiration = getExpiration(hub.lease_seconds)
+    }
+
+    if (hub.secret) {
+      hook.secret = hub.secret
+    }
+
+    await validHubIntent(hub)
+
+    await hook.save()
+    return hook
+  } catch (error) {
+    logger.error(error.message)
+    return error
   }
-
-  if (hook.mode !== hub.mode) {
-    hook.mode = hub.mode
-  }
-
-  if (hub.lease_seconds) {
-    hook.expiration = getExpiration(hub.lease_seconds)
-  }
-
-  if (hub.secret) {
-    hook.secret = hub.secret
-  }
-
-  const check = await isValidHubIntent(hub)
-
-  if (!check) {
-    throw new Error(
-      `Subscriber failed to confirm intent for callback: ${hub.callback} on topic: ${hub.topic}.`
-    )
-  }
-
-  await hook.save()
-  return hook
 }
 
 // Check if the subscriber request is valid or not. This will post a challenge to subscriber to confirm the intent of the request.
-export async function isValidHubIntent(hub: z.infer<typeof Hub>) {
-  try {
-    const challenge = crypto.randomBytes(20).toString('hex')
-    const result = await fetch(hub.callback, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+export async function validHubIntent(hub: z.infer<typeof Hub>) {
+  const challenge = crypto.randomBytes(20).toString('hex')
+  const result = await fetch(hub.callback, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      hub: {
+        ...hub,
+        challenge,
       },
-      body: JSON.stringify({
-        hub: {
-          ...hub,
-          challenge,
-        },
-      }),
-    })
+    }),
+  })
 
-    const json = await result.json()
+  const json = await result.json()
 
-    if (!result.ok) {
-      throw new Error("Subscriber didn't confirm intent.")
-    }
-
-    if (json.challenge !== challenge) {
-      throw new Error('Challenge did not match')
-    }
-
-    if (json.topic !== hub.topic) {
-      throw new Error('Topic did not match')
-    }
-
-    if (json.callback !== hub.callback) {
-      throw new Error('Callback did not match')
-    }
-
-    return hub
-  } catch (error) {
-    logger.error(error.message)
-    return false
+  if (!result.ok) {
+    throw new Error("Subscriber didn't confirm intent.")
   }
+
+  if (json.challenge !== challenge) {
+    throw new Error('Challenge did not match')
+  }
+
+  if (json.topic !== hub.topic) {
+    throw new Error('Topic did not match')
+  }
+
+  if (json.callback !== hub.callback) {
+    throw new Error('Callback did not match')
+  }
+
+  return hub
 }
 
 export function getSupportedNetwork(network: string) {
@@ -144,10 +133,9 @@ export async function subscriptionHandler(req: Request, res: Response) {
     }
     // We send the accepted request to the subscriber and then validate the intent of the subscriber as well as persist the subscription.
     res.status(202).send('Accepted')
-    await subscribe(hub, req.params)
-    return
+    return subscribe(hub, req.params)
   } catch (error) {
-    logger.error(error)
+    logger.error(error.message)
     // We respond with the error if we cannot subscribe or there is an error in the subscriber request.
     return res.status(500).send({
       hub: {
@@ -198,7 +186,7 @@ export function publisherHandler(req: Request, res: Response) {
     `
     return res.status(200).send(htmlResponse)
   } catch (error) {
-    logger.error(error)
+    logger.error(error.message)
     return res.status(500).send(error.message)
   }
 }
