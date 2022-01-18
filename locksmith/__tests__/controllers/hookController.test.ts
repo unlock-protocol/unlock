158c467bcb572cf25c07a0605691f10db0219b4a
searchableguy <me@searchableguy.com>
2022-01-18 20:36:53 +0530

    refactor controllers

--- locksmith/__tests__/controllers/hookController.test.ts
+++ locksmith/__tests__/controllers/hookController.test.ts
@@ -1,56 +1,43 @@
 import { networks } from '@unlock-protocol/networks'
-import request from 'supertest'
+// import request from 'supertest'
+import { HookController } from '../../src/controllers/hookController'
 
-import {
-  getExpiration,
-  getSupportedNetwork,
-  EXPIRATION_SECONDS_LIMIT,
-} from '../../src/controllers/hookController'
-
-const app = require('../../src/app')
+// const app = require('../../src/app')
 
 describe('HookController', () => {
+  const controller = new HookController({
+    leaseSeconds: {
+      limit: 864000 * 9,
+      default: 864000,
+    },
+  })
   describe('Utilities in HookController', () => {
     it('getExpiration', () => {
       expect.assertions(3)
 
-      const expiration = getExpiration().getTime()
+      const expiration = controller.getExpiration().getTime()
       const expectedExpiration = new Date(Date.now() + 864000 * 1000).getTime()
       expect(expiration).toBe(expectedExpiration)
 
-      const expiration2 = getExpiration(EXPIRATION_SECONDS_LIMIT).getTime()
+      const expiration2 = controller
+        .getExpiration(controller.options.leaseSeconds.limit)
+        .getTime()
+
       const expectedExpiration2 = new Date(
-        Date.now() + EXPIRATION_SECONDS_LIMIT * 1000
+        Date.now() + controller.options.leaseSeconds.limit * 1000
       ).getTime()
 
       expect(expiration2).toBe(expectedExpiration2)
 
       expect(() => {
-        getExpiration(EXPIRATION_SECONDS_LIMIT + 1)
+        controller.getExpiration(controller.options.leaseSeconds.limit + 1)
       }).toThrow("Lease seconds can't be greater than 90 days")
     })
 
-    it('getSupportedNetwork', () => {
+    it('getNetwork', () => {
       expect.assertions(2)
-      expect(getSupportedNetwork('1')).toBe(networks['1'])
-      expect(getSupportedNetwork('24242')).toBe(undefined)
-    })
-  })
-
-  describe('Hook publisher endpoints', () => {
-    it('Check if returning the hubs in a head request', async () => {
-      expect.assertions(2)
-      const response = await request(app).head('/api/hooks/4/locks')
-      expect(response.status).toBe(200)
-      expect(response.headers.link.split(',').length).toBe(2)
-    })
-
-    it('Check if returning the hubs in a get request', async () => {
-      expect.assertions(3)
-      const response = await request(app).get('/api/hooks/4/locks')
-      expect(response.status).toBe(200)
-      expect(response.headers.link.split(',').length).toBe(2)
-      expect(response.headers['content-type']).toBe('text/html; charset=utf-8')
+      expect(controller.getNetwork('1')).toBe(networks['1'])
+      expect(controller.getNetwork('24242')).toBe(undefined)
     })
   })
 })
