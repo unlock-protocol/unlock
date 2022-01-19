58749f25bd0693a4dd4d1ce95dcc5a21347f6b60
searchableguy <me@searchableguy.com>
2022-01-19 15:15:56 +0530

    feat(ui): Improve button component

--- packages/ui/lib/components/Button/Button.stories.tsx
+++ packages/ui/lib/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { Button } from './Button'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-
+import { FaLock as LockIcon } from 'react-icons/fa'
 export default {
   component: Button,
   title: 'Button',
@@ -11,5 +11,15 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 export const Primary = Template.bind({})
 
 Primary.args = {
-  children: 'Primary button',
+  children: 'Lock Button',
+  leftIcon: <LockIcon size={12} />,
+}
+
+
+export const ButtonWithRightIcon = Template.bind({})
+
+ButtonWithRightIcon.args = {
+  children: 'Button with right icon',
+  size:"large",
+  rightIcon: <LockIcon size={14} />,
 }
