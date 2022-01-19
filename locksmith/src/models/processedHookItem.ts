ef2a866c1c02778717a3500449c961c69fb623c7
searchableguy <me@searchableguy.com>
2022-01-20 00:32:04 +0530

    feat(websub): Add a ProcessedHookItems model and migration (#8044)

---
+++ locksmith/src/models/processedHookItem.ts
@@ -0,0 +1,20 @@
+import { Table, Model, Column, Validate } from 'sequelize-typescript'
+
+@Table({ tableName: 'ProcessedHookItems', timestamps: true })
+export class ProcessedHookItem extends Model<ProcessedHookItem> {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id!: number
+
+  @Column
+  network!: number
+
+  // This is only code side validation so we can add more types in future without changing the migration for database.
+  @Validate({
+    isIn: [['lock', 'key']],
+  })
+  @Column
+  type!: string
+
+  @Column
+  objectId!: string
+}
