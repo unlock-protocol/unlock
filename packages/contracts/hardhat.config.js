c5885c83c11a46f8cb7d42522d7bd8c28c8a922e
Clement Renaud <clement@clementrenaud.com>
2022-01-19 14:00:04 +0100

    add compilers to hardhat config

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,9 +1,34 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const settings = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+}
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: {
+    compilers: [
+      { version: '0.4.24', settings },
+      { version: '0.4.25', settings },
+      { version: '0.5.0', settings },
+      { version: '0.5.12', settings },
+      { version: '0.5.17', settings },
+      { version: '0.5.14', settings },
+      { version: '0.5.7', settings },
+      { version: '0.5.9', settings },
+      { version: '0.6.12', settings },
+      { version: '0.7.6', settings },
+      { version: '0.8.0', settings },
+      { version: '0.8.2', settings },
+      { version: '0.8.4', settings },
+      { version: '0.8.7', settings },
+    ],
+  },
   paths: {
     sources : 'src/contracts'
   }
-};
+}
