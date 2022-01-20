c615685570d1e459caa8ee4344bab6e295eb7e17
searchableguy <me@searchableguy.com>
2022-01-20 23:49:38 +0530

    Add more tests

--- locksmith/__tests__/controllers/hookController.test.ts
+++ locksmith/__tests__/controllers/hookController.test.ts
@@ -1,6 +1,10 @@
 import { networks } from '@unlock-protocol/networks'
 import request from 'supertest'
-import { HookController } from '../../src/controllers/hookController'
+import { Hook } from '../../src/models'
+import {
+  HookController,
+  SubscribeRequest,
+} from '../../src/controllers/hookController'
 
 const app = require('../../src/app')
 
@@ -59,6 +63,83 @@ describe('HookController', () => {
     })
   })
 
+  describe('createHook', () => {
+    it('should create a hook', async () => {
+      expect.assertions(2)
+      const spyOn = jest.spyOn(controller, 'createHook')
+      spyOn.mockReturnValue(Promise.resolve(new Hook()))
+      const value = await controller.createHook({} as any)
+      expect(value).toBeInstanceOf(Hook)
+      expect(spyOn).toHaveBeenCalled()
+    })
+  })
+
+  describe('updateHook', () => {
+    it("should update or create a hook if it doesn't exit", async () => {
+      expect.assertions(9)
+
+      const spyOn = jest
+        .spyOn(controller, 'updateHook')
+        .mockImplementation((req: SubscribeRequest) => {
+          const hook = new Hook()
+          const { network, lock } = req.params
+          const { mode, topic, callback, lease_seconds } = req.body.hub
+          hook.mode = mode
+          hook.topic = topic
+          hook.callback = callback
+          hook.network = network
+          hook.lock = lock
+          hook.expiration = controller.getExpiration(lease_seconds)
+          return Promise.resolve(hook)
+        })
+
+      const value = await controller.updateHook({
+        params: {
+          network: '1',
+          lock: '424asd',
+        },
+        body: {
+          hub: {
+            mode: 'unsubscribe',
+            topic: 'http://localhost:5000',
+            callback: 'http://localhost:5000',
+          },
+        },
+      } as SubscribeRequest)
+
+      expect(value).toBeInstanceOf(Hook)
+      expect(value.mode).toBe('unsubscribe')
+      expect(value.topic).toBe('http://localhost:5000')
+      expect(value.callback).toBe('http://localhost:5000')
+      const expiration1 = value.expiration.getTime()
+      const expiration2 = controller
+        .getExpiration(controller.options.leaseSeconds.default)
+        .getTime()
+      expect(expiration1).toBeLessThanOrEqual(expiration2)
+
+      expect(spyOn).toHaveBeenCalled()
+
+      const value2 = await controller.updateHook({
+        params: {
+          network: '1',
+          lock: '424asd',
+        },
+        body: {
+          hub: {
+            mode: 'subscribe',
+            topic: 'http://localhost:5000',
+            callback: 'http://localhost:5000',
+            lease_seconds: 45000,
+          },
+        },
+      } as SubscribeRequest)
+      const expiration3 = value.expiration.getTime()
+      const expiration4 = controller.getExpiration(45000).getTime()
+      expect(value2).toBeInstanceOf(Hook)
+      expect(value2.mode).toBe('subscribe')
+      expect(expiration3).toBe(expiration4)
+    })
+  })
   describe('hookController Endpoints', () => {
     it('Subscribe endpoint', async () => {
       expect.assertions(4)
