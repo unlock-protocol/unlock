de30c22ef95c7c4ac6befe15025b2f41799f2a0b
searchableguy <me@searchableguy.com>
2022-01-20 17:10:14 +0530

    Add test for when subscriber is expecting signature but we don't provide one

--- locksmith/__tests__/websub/notify.test.ts
+++ locksmith/__tests__/websub/notify.test.ts
@@ -23,12 +23,21 @@ describe('Test notify function', () => {
     expect(response.status).toBe(200)
   })
 
-  it('should throw with signature mismatch', async () => {
+  it('should result in error with signature mismatch', async () => {
     expect.assertions(1)
     const hook = new Hook()
     hook.callback = 'http://localhost:4000/callback'
     // Change signature here
     hook.secret = 'websu'
+    const fn = notify(hook, { test: true })
+    const response = await fn()
+    expect(response.ok).toBe(false)
+  })
+
+  it('Should result in error if no signature provided to a client expecting it', async () => {
+    expect.assertions(1)
+    const hook = new Hook()
+    hook.callback = 'http://localhost:4000/callback'
     const fn = notify(hook, { test: true })
     const response = await fn()
     expect(response.ok).toBe(false)
