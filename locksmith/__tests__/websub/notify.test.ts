c0b9c49117e28fd9211c82a183ec97e3c22102f1
searchableguy <me@searchableguy.com>
2022-01-20 19:26:25 +0530

    group test functions

--- locksmith/__tests__/websub/notify.test.ts
+++ locksmith/__tests__/websub/notify.test.ts
@@ -3,52 +3,50 @@ import { notify } from '../../src/websub/helpers'
 import { Hook, HookEvent } from '../../src/models'
 
 const notifyHook = jest.fn()
-
-// eslint-disable-next-line
 notifyHook.mockImplementation(() => new HookEvent())
 
-beforeAll(() => subscriberServer.listen())
+describe('Test notify helpers', () => {
+  beforeAll(() => subscriberServer.listen())
+  afterEach(() => subscriberServer.resetHandlers())
+  describe('Test notify function', () => {
+    it('should succeed', async () => {
+      expect.assertions(1)
+      const hook = new Hook()
+      hook.callback = 'http://localhost:4000/callback'
+      hook.secret = 'websub'
+      const fn = notify(hook, { test: true })
+      const response = await fn()
+      expect(response.status).toBe(200)
+    })
 
-afterEach(() => subscriberServer.resetHandlers())
+    it('should result in error with signature mismatch', async () => {
+      expect.assertions(1)
+      const hook = new Hook()
+      hook.callback = 'http://localhost:4000/callback'
+      // Change signature here
+      hook.secret = 'websu'
+      const fn = notify(hook, { test: true })
+      const response = await fn()
+      expect(response.ok).toBe(false)
+    })
 
-afterAll(() => subscriberServer.close())
-describe('Test notify function', () => {
-  it('should succeed', async () => {
-    expect.assertions(1)
-    const hook = new Hook()
-    hook.callback = 'http://localhost:4000/callback'
-    hook.secret = 'websub'
-    const fn = notify(hook, { test: true })
-    const response = await fn()
-    expect(response.status).toBe(200)
+    it('Should result in error if no signature provided to a client expecting it', async () => {
+      expect.assertions(1)
+      const hook = new Hook()
+      hook.callback = 'http://localhost:4000/callback'
+      const fn = notify(hook, { test: true })
+      const response = await fn()
+      expect(response.ok).toBe(false)
+    })
   })
 
-  it('should result in error with signature mismatch', async () => {
-    expect.assertions(1)
-    const hook = new Hook()
-    hook.callback = 'http://localhost:4000/callback'
-    // Change signature here
-    hook.secret = 'websu'
-    const fn = notify(hook, { test: true })
-    const response = await fn()
-    expect(response.ok).toBe(false)
+  describe('Test notify hook function', () => {
+    it('Test notify hook function', async () => {
+      expect.assertions(1)
+      const hook = new Hook()
+      const body = { test: true }
+      expect(notifyHook.call(hook, body)).toBeInstanceOf(HookEvent)
+    })
   })
-
-  it('Should result in error if no signature provided to a client expecting it', async () => {
-    expect.assertions(1)
-    const hook = new Hook()
-    hook.callback = 'http://localhost:4000/callback'
-    const fn = notify(hook, { test: true })
-    const response = await fn()
-    expect(response.ok).toBe(false)
-  })
-})
-
-describe('Test notify hook function', () => {
-  it('Test notify hook function', async () => {
-    expect.assertions(1)
-    const hook = new Hook()
-    const body = { test: true }
-    expect(notifyHook.call(hook, body)).toBeInstanceOf(HookEvent)
-  })
+  afterAll(() => subscriberServer.close())
 })
