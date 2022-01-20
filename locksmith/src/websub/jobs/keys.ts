f51ddab0573b54d1c721583dad80ee2b08a7ae1b
searchableguy <me@searchableguy.com>
2022-01-20 21:33:37 +0530

    feat(websub): Only send keys and locks not sent before

--- locksmith/src/websub/jobs/keys.ts
+++ locksmith/src/websub/jobs/keys.ts
@@ -1,8 +1,34 @@
 import { Key } from '../../graphql/datasource'
-import { Hook } from '../../models'
+import { Hook, ProcessedHookItem } from '../../models'
 import { TOPIC_KEYS } from '../topics'
 
 import { networkMapToFnResult, notifyHook } from '../helpers'
+
+async function getUnprocessedKeys(network: number) {
+  const keySource = new Key(network)
+  const processedKeys = await ProcessedHookItem.findAll({
+    where: {
+      type: 'key',
+      network,
+    },
+    order: [['id', 'DESC']],
+  })
+  const keys = await keySource.getKeys({ first: 25 })
+  const results = keys.filter(
+    (key: any) => !processedKeys.find((item) => item.objectId === key.id)
+  )
+
+  await ProcessedHookItem.bulkCreate(
+    results.map((key: any) => {
+      return {
+        type: 'key',
+        network,
+        objectId: key.id,
+      }
+    })
+  )
+  return results
+}
 
 export async function notifyOfKeys(hooks: Hook[]) {
   const subscribed = hooks.filter((hook) => {
@@ -10,10 +36,7 @@ export async function notifyOfKeys(hooks: Hook[]) {
     return TOPIC_KEYS.test(path)
   })
 
-  const networkToLocksMap = await networkMapToFnResult((network) => {
-    const keySource = new Key(Number(network))
-    return keySource.getKeys({ first: 25 })
-  })
+  const networkToLocksMap = await networkMapToFnResult(getUnprocessedKeys)
 
   for (const hook of subscribed) {
     const data = networkToLocksMap
--- locksmith/src/websub/jobs/locks.ts
+++ locksmith/src/websub/jobs/locks.ts
@@ -1,8 +1,34 @@
 import { Lock } from '../../graphql/datasource'
-import { Hook } from '../../models'
+import { Hook, ProcessedHookItem } from '../../models'
 import { TOPIC_LOCKS } from '../topics'
 
 import { networkMapToFnResult, notifyHook } from '../helpers'
+
+export async function getUnprocessedLocks(network: number) {
+  const lockSource = new Lock(network)
+  const processedLocks = await ProcessedHookItem.findAll({
+    where: {
+      type: 'lock',
+      network,
+    },
+    order: [['id', 'DESC']],
+  })
+  const locks = await lockSource.getLocks({ first: 25 })
+  const results = locks.filter(
+    (lock: any) => !processedLocks.find((item) => item.objectId === lock.id)
+  )
+
+  await ProcessedHookItem.bulkCreate(
+    results.map((lock: any) => {
+      return {
+        type: 'lock',
+        network,
+        objectId: lock.id,
+      }
+    })
+  )
+  return results
+}
 
 export async function notifyOfLocks(hooks: Hook[]) {
   const subscribed = hooks.filter((hook) => {
@@ -10,10 +36,7 @@ export async function notifyOfLocks(hooks: Hook[]) {
     return TOPIC_LOCKS.test(path)
   })
 
-  const networkToLocksMap = await networkMapToFnResult((network) => {
-    const lockSource = new Lock(network)
-    return lockSource.getLocks({ first: 25 })
-  })
+  const networkToLocksMap = await networkMapToFnResult(getUnprocessedLocks)
   for (const hook of subscribed) {
     const data = networkToLocksMap.get(Number(hook.network))
     notifyHook(hook, data)
