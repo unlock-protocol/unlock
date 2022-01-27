a6e168f403218e24226a0bbefd96ea73d1dd069b
searchableguy <me@searchableguy.com>
2022-01-27 20:14:05 +0530

    fix(websub): Add some basic logging to server (#8090)

--- locksmith/src/websub/server.ts
+++ locksmith/src/websub/server.ts
@@ -2,11 +2,16 @@ import cron from 'node-cron'
 import { Op } from 'sequelize'
 import { Hook } from '../models'
 import { notifyOfKeys, notifyOfLocks } from './jobs'
+import { logger } from '../logger'
+
+logger.info('Websub server started.')
 
 // every 5 minute
 const CURRENT_CRON_SCHEDULE = '*/5 * * * *'
 
 cron.schedule(CURRENT_CRON_SCHEDULE, async () => {
+  logger.info('Running keys and locks job')
+
   const subscribers = await Hook.findAll({
     where: {
       mode: 'subscribe',
@@ -15,6 +20,11 @@ cron.schedule(CURRENT_CRON_SCHEDULE, async () => {
       },
     },
   })
-  notifyOfKeys(subscribers)
-  notifyOfLocks(subscribers)
+
+  await Promise.allSettled([
+    notifyOfKeys(subscribers),
+    notifyOfLocks(subscribers),
+  ])
+
+  logger.info('Finished running keys and locks job')
 })
