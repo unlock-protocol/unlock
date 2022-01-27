f802912ec56b8afda85720b7390f42aa7074d64b
Clement Renaud <clement@clementrenaud.com>
2022-01-27 15:12:30 +0100

    eslint config

--- packages/hardhat-plugin/.eslintrc.js
+++ packages/hardhat-plugin/.eslintrc.js
@@ -1,6 +1,43 @@
+const rulesToIgnore = [
+  'no-underscore-dangle',
+  'no-param-reassign',
+  'no-use-before-define',
+  'no-plusplus',
+  'no-await-in-loop',
+  'radix',
+  'prefer-destructuring',
+  'no-shadow',
+  'no-loop-func',
+  'eqeqeq',
+  'no-useless-concat',
+  'prefer-const',
+  'no-return-await',
+  'prefer-object-spread',
+  'func-names',
+  'import/extensions',
+  'jest/prefer-expect-assertions',
+]
+
 module.exports = {
   extends: ['@unlock-protocol/eslint-config'],
+  plugins: ['mocha'],
+  globals: {
+    it: true,
+    artifacts: true,
+    contract: true,
+    describe: true,
+    before: true,
+    beforeEach: true,
+    web3: true,
+    assert: true,
+    abi: true,
+    after: true,
+    afterEach: true,
+  },
   rules: {
-    'no-param-reassign': 'off',
+    'mocha/no-exclusive-tests': 'error',
+    ...rulesToIgnore.reduce((obj, rule) => {
+      return { ...obj, [rule]: 'off' }
+    }, {}),
   },
 }
