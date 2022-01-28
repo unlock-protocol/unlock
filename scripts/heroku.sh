5b75604fdf94479cfe2581504542dbf34946d88e
searchableguy <me@searchableguy.com>
2022-01-28 20:56:42 +0530

    feat(ci): Add option to change default command for starting up container

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -5,6 +5,7 @@ set -e
 SERVICE=$1
 HEROKU_APP_NAME=$2
 HEROKU_CONTAINER_TYPE=web
+COMMAND="yarn prod"
 BUILD_DIRECTORY=$1
 
 # if container type is provided, use it instead of default
@@ -12,9 +13,14 @@ if [ -n "${3}" ]; then
     HEROKU_CONTAINER_TYPE=$3
 fi
 
+# if command to run is provided, use it instead.
+if [ -n "${4}"]; then 
+    COMMAND=$4
+fi
+
 # if build directory argument provided, use it instead
-if [ -n "${4}" ]; then
-    BUILD_DIRECTORY=$4
+if [ -n "${5}" ]; then
+    BUILD_DIRECTORY=$5
 fi
 
 echo "Using $BUILD_DIRECTORY as build directory"
@@ -29,7 +35,7 @@ then
 fi
 
 # build image
-docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$BUILD_DIRECTORY .
+docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$BUILD_DIRECTORY --build-arg COMMAND=$COMMAND .
 
 # push image to Heroku registry
 docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
