86438a20131747953a89188652c811f5567f2ba9
searchableguy <me@searchableguy.com>
2022-01-31 11:02:04 +0530

    feat(ci) Add command and worker option to heroku script and CI (#8104)

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -4,14 +4,28 @@ set -e
 # two args
 SERVICE=$1
 HEROKU_APP_NAME=$2
+HEROKU_CONTAINER_TYPE=web
+COMMAND="yarn prod"
 BUILD_DIRECTORY=$1
 
-# if build directory argument provided, use it instead
+# if container type is provided, use it instead of default
 if [ -n "${3}" ]; then
-    BUILD_DIRECTORY=$3
+    HEROKU_CONTAINER_TYPE=$3
+fi
+
+# if command to run is provided, use it instead.
+if [ -n "${4}" ]; then 
+    COMMAND=$4
+fi
+
+# if build directory argument provided, use it instead
+if [ -n "${5}" ]; then
+    BUILD_DIRECTORY=$5
 fi
 
 echo "Using $BUILD_DIRECTORY as build directory"
+
+echo "Using $COMMAND as the start command for container"
 
 echo "Deploying $SERVICE to Heroku $HEROKU_APP_NAME ..."
 
@@ -23,17 +37,17 @@ then
 fi
 
 # build image
-docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/web --build-arg BUILD_DIR=$BUILD_DIRECTORY .
+docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_CONTAINER_TYPE --build-arg COMMAND="$COMMAND" --build-arg BUILD_DIR="$BUILD_DIRECTORY" .
 
 # push image to Heroku registry
 docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
-docker push registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_CONTAINER_TYPE
 
 #make sure we are logged in
 heroku container:login
 
 # release on heroku 
-heroku container:release -a $HEROKU_APP_NAME web
+heroku container:release -a $HEROKU_APP_NAME $HEROKU_CONTAINER_TYPE
 
 # migrate the database
 heroku run --app $HEROKU_APP_NAME yarn db:migrate
