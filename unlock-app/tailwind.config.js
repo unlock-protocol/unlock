3df315c4f8eb40cb7ed8eb7067b0e263e89d2be5
searchableguy <me@searchableguy.com>
2022-02-01 13:06:30 +0530

    feat(unlock-app): Enable tailwind preflight (#8109)

--- unlock-app/tailwind.config.js
+++ unlock-app/tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
   corePlugins: {
-    preflight: false,
-  }
+    preflight: true,
+  },
 }
