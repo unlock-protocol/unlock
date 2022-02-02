029ae062e42e81eb3cb23709453acee1dc92a274
Clement Renaud <clement@clementrenaud.com>
2022-02-02 15:11:19 +0100

    better testing

--- packages/hardhat-plugin/test/index.test.ts
+++ packages/hardhat-plugin/test/index.test.ts
@@ -1,11 +1,35 @@
 /* eslint-disable prefer-arrow-callback */
 // tslint:disable-next-line no-implicit-dependencies
-import { assert } from 'chai'
+import { assert, expect } from 'chai'
 
 import networks from '@unlock-protocol/networks'
 
 import { useEnvironment } from './helpers'
 import { UnlockHRE } from '../src/Unlock'
+
+/**
+ * Takes in a function and checks for error
+ * @param {Function} method - The function to check
+ * @param {any[]} params - The array of function parameters
+ * @param {string} message - Optional message to match with error message
+ */
+const expectThrowsAsync = async (
+  method: Function,
+  params: any[],
+  message?: string
+) => {
+  let err: unknown | null = null
+  try {
+    await method(...params)
+  } catch (error) {
+    err = error
+  }
+  if (err instanceof Error) {
+    expect(err.message).to.be.equal(message)
+  } else {
+    expect(err).to.be.an('Error')
+  }
+}
 
 describe('Unlock Hardhat plugin', function () {
   describe('HRE extension', function () {
@@ -35,11 +59,48 @@ describe('Unlock Hardhat plugin', function () {
       })
     })
 
+    describe('getSigner()', function () {
+      it('should return a default signer', async function () {
+        const [defaultSigner] = await this.hre.ethers.getSigners()
+        assert.equal(
+          (await this.hre.unlock.getSigner()).address,
+          defaultSigner.address
+        )
+      })
+      it('should parse mnemonic', async function () {
+        process.env.WALLET_PRIVATE_KEY =
+          '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a'
+        assert.equal(
+          (await this.hre.unlock.getSigner()).address.toLowerCase(),
+          '0x15d34aaf54267db7d7c367839aaf71a00a2c6a65'
+        )
+      })
+    })
+
     describe('deployUnlock()', function () {
       it('Should deploy the Unlock contract', async function () {
-        const unlockAddress = await this.hre.unlock.deployUnlock(9, 1)
+        const unlockAddress = await this.hre.unlock.deployUnlock(undefined, 1)
         assert.isTrue(unlockAddress.includes('0x'))
         assert.equal(typeof unlockAddress, 'string')
+      })
+
+      it('Should fail if number version doesnt exist', async function () {
+        await expectThrowsAsync(
+          this.hre.unlock.deployUnlock,
+          [135, 1],
+          "Contract 'UnlockV135' is not in present in @unlock-protocol/contracts"
+        )
+      })
+    })
+
+    describe('deployPublicLock()', function () {
+      it('Should deploy the Unlock contract', async function () {
+        const publicLockAddress = await this.hre.unlock.deployPublicLock(
+          undefined,
+          1
+        )
+        assert.isTrue(publicLockAddress.includes('0x'))
+        assert.equal(typeof publicLockAddress, 'string')
       })
     })
   })
