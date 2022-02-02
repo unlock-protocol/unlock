caff666a4ae9026173e547a3fec543672cd31f0b
searchableguy <me@searchableguy.com>
2022-02-03 01:25:43 +0530

    feat(unlock-protocol-com): Add basic home page (#8186)

---
+++ unlock-protocol-com/src/pages/home.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { Home } from '../components/pages/Home'
+
+const HomePage: NextPage = () => {
+  return <Home />
+}
+
+export default HomePage
---
+++ unlock-protocol-com/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { Home } from '../components/pages/Home'
+
+const IndexPage: NextPage = () => {
+  return <Home />
+}
+
+export default IndexPage
