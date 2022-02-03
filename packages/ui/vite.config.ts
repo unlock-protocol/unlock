20f3980294deabcf53fea0de88ef975e64c5327f
searchableguy <me@searchableguy.com>
2022-02-03 10:09:04 +0530

    feat(ui): Export typescript typings and add an example (#8187)

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: ["es2020", "esnext"],
+    target: ['es2020', 'esnext'],
     lib: {
       entry: path.resolve(__dirname, 'lib/index.tsx'),
       name: '@unlock-protocol/ui',
