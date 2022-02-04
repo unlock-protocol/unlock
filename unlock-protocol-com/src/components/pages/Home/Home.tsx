68d697b428f954f25c1fd92e4961d090cdb35912
searchableguy <me@searchableguy.com>
2022-02-04 18:23:25 +0530

    feat(unlock-protocol-com): Add developer section to home page. Create some fixes for ui library and refactor a bit (#8196)

---
+++ unlock-protocol-com/src/components/pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import { Navigation } from '../../interface/Navigation'
+import { Connect } from './sections/Connect'
+import { Developer } from './sections/Developer'
+import { Steps } from './sections/Steps'
+
+const SECTIONS = [Connect, Steps, Developer]
+
+export function Home() {
+  return (
+    <div>
+      <Navigation />
+      <div className="max-w-screen-lg p-4 mx-auto">
+        <div className="grid gap-8 py-24 sm:gap-24">
+          {SECTIONS.map((Section, index) => (
+            <div key={index}>
+              <Section />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ unlock-protocol-com/src/components/pages/Home/sections/Connect.tsx
@@ -0,0 +1,71 @@
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
+
+const UNLOCK_MARKETING_POINTS = [
+  'Create memberships and sell access NFTs in minutes',
+  'Token-gating, memberships, ticketing, and more',
+  'Open-source, community governed',
+]
+
+export function Connect() {
+  return (
+    <div className="flex flex-col-reverse items-center gap-6 sm:gap-12 md:gap-24 lg:gap-32 sm:flex-row">
+      <div className="w-full">
+        <div className="w-full space-y-4 break-words">
+          <h1 className="text-4xl font-bold sm:text-5xl">
+            Connect with your 1000 true fans
+          </h1>
+          <p className="text-lg sm:text-xl text-brand-gray max-w-[300px]">
+            For creative communities and the humans who build them
+          </p>
+        </div>
+        <div className="py-8">
+          <ul>
+            {UNLOCK_MARKETING_POINTS.map((text, index) => (
+              <li
+                className={`border-t border-brand-gray py-4 items-center flex ${
+                  !UNLOCK_MARKETING_POINTS[index + 1] && 'border-b'
+                }`}
+                key={index}
+              >
+                {text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[380px]">
+        <div className="w-full bg-white bg-shadow-and-glass rounded-3xl ">
+          <header className="items-center justify-between hidden w-full gap-2 px-6 py-4 sm:flex">
+            <p className="font-bold">Tales of Ronin</p>
+            <p className="font-mono text-sm font-bold text-brand-ui-primary">
+              Powered by Unlock
+            </p>
+          </header>
+          <img
+            className="rounded-t-xl sm:rounded-none"
+            alt="Caroline"
+            src="/images/marketing/home-card.png"
+          />
+          <div className="flex items-center gap-4 px-6 py-4">
+            <div>
+              <Avatar className="overflow-hidden">
+                <div className="bg-orange-500 rounded-full bg-opacity-90 brightness-150">
+                  <AvatarImage
+                    className="inline-block w-10 h-10 rounded-full mix-blend-multiply"
+                    src="/images/marketing/caroline.png"
+                    alt="Calorine"
+                  />
+                </div>
+              </Avatar>
+            </div>
+            <div className="grid">
+              <p className="font-bold"> Caroline </p>
+              <p className="text-sm brand-gray"> Sold out mint in one day </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
