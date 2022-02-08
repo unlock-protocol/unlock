be6fc1dafc8aed919983d23cfc388f24b64e069e
Clement Renaud <clement@clementrenaud.com>
2022-02-08 13:57:16 +0100

    properly import hardhat ethers type

--- packages/hardhat-plugin/src/type-extensions.ts
+++ packages/hardhat-plugin/src/type-extensions.ts
@@ -1,3 +1,5 @@
+import type { ethers } from 'ethers'
+import type { HardhatEthersHelpers } from '@nomiclabs/hardhat-ethers/types'
 import 'hardhat/types/config'
 import 'hardhat/types/runtime'
 
@@ -16,5 +18,7 @@ declare module 'hardhat/types/config' {
 declare module 'hardhat/types/runtime' {
   export interface HardhatRuntimeEnvironment {
     unlock: UnlockHRE
+    // We omit the ethers field because it is redundant.
+    ethers: typeof ethers & HardhatEthersHelpers
   }
 }
