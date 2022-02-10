f610e7b76d78c260e404be9db6a6e51a3dfe1af5
Clement Renaud <clement@clementrenaud.com>
2022-02-10 14:51:11 +0100

    bump version 0.0.4

--- packages/hardhat-plugin/src/type-extensions.ts
+++ packages/hardhat-plugin/src/type-extensions.ts
@@ -1,6 +1,6 @@
 import type { ethers } from 'ethers'
 import type { HardhatEthersHelpers } from '@nomiclabs/hardhat-ethers/types'
-import type { HardhatUpgrades } from '@openzeppelin/hardhat-upgrades';
+import type { HardhatUpgrades } from '@openzeppelin/hardhat-upgrades'
 
 import 'hardhat/types/config'
 import 'hardhat/types/runtime'
