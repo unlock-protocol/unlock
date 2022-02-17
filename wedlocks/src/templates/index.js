d8f49bf5120658997d0156eb8e1991278df1f1a4
Julien Genestoux <julien.genestoux@gmail.com>
2022-02-17 09:27:13 -0500

    Pscott/update email ethcc (#8243)
    * Change email content
    
    * Change lock address
    
    * fixing import
    
    * adding pscott to CLA
    
    Co-authored-by: Scott Piriou <30843220+pscott@users.noreply.github.com>

--- wedlocks/src/templates/index.js
+++ wedlocks/src/templates/index.js
@@ -6,7 +6,7 @@ import recoveryKeyConfirmEmail from './recoveryKeyConfirmEmail'
 import confirmEvent from './confirmEvent'
 import keyOwnership from './keyOwnership'
 import keyMined from './keyMined'
-import keyMined0x934FE4A45fDcF13a87835500F05C6B358Ec89F69 from './locks/keyMined-0x934FE4A45fDcF13a87835500F05C6B358Ec89F69'
+import keyMined0x9AB351cB5DAe55abD135dD256726851aae8EFeB5 from './locks/keyMined-0x9AB351cB5DAe55abD135dD256726851aae8EFeB5'
 
 export default {
   confirmEmail,
@@ -17,5 +17,5 @@ export default {
   confirmEvent,
   keyOwnership,
   keyMined,
-  keyMined0x934FE4A45fDcF13a87835500F05C6B358Ec89F69,
+  keyMined0x9AB351cB5DAe55abD135dD256726851aae8EFeB5,
 }
