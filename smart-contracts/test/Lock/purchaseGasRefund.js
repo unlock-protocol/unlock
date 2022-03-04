315fae1aae52ee8dbdaabaea0c94b698c7361779
Clément Renaud <clement@unlock-protocol.com>
2022-03-04 13:12:12 +0100

    fix(smart-contracts): make lock layout upgrade-compatible (#8348)
    * dont initialize _gasRefundValue
    
    prevent oz upgrader from throwing error
    ```
    contracts/mixins/MixinPurchase.sol:28: Variable `_gasRefundValue` is assigned an initial value
        Move the assignment to the initializer
        https://zpl.in/upgrades/error-004
    ```
    
    * edit in contract package too
    
    * add a test
    
    * revert test

--- smart-contracts/test/Lock/purchaseGasRefund.js
+++ smart-contracts/test/Lock/purchaseGasRefund.js
@@ -50,6 +50,10 @@ contract('Lock / GasRefund', (accounts) => {
       })
 
       describe('gas refund value', () => {
+        it('default to zero', async () => {
+          assert.equal((await lock.gasRefundValue()).toNumber(), 0)
+        })
+
         it('get set properly', async () => {
           await lock.setGasRefundValue(gasRefundAmount)
           assert.equal((await lock.gasRefundValue()).eq(gasRefundAmount), true)
