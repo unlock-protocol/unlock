4dd4a8f045df0699043dc2cb26c06ca8f0e27f94
Clement Renaud <clement@clementrenaud.com>
2022-03-04 16:57:49 +0100

    fix max key total supply test

--- smart-contracts/test/Lock/setMaxNumberOfKeys.js
+++ smart-contracts/test/Lock/setMaxNumberOfKeys.js
@@ -115,30 +115,22 @@ contract('Lock / setMaxNumberOfKeys', () => {
       )
     })
 
-    it('should allow setting a value equals to current total supply', async () => {
+    it('should allow setting a value equal to current total supply', async () => {
       // buy 10 keys
       const [, ...buyers] = await ethers.getSigners()
-      const txs = await Promise.all(
-        Array(10)
-          .fill(0)
-          .map((_, i) =>
-            lock
-              .connect(buyers[i])
-              .purchase(
-                keyPrice.toString(),
-                buyers[i].address,
-                web3.utils.padLeft(0, 40),
-                web3.utils.padLeft(0, 40),
-                [],
-                {
-                  value: keyPrice.toString(),
-                }
-              )
-          )
+      const tx = await lock.connect(buyers[0]).purchase(
+        [],
+        buyers.slice(0, 10).map((b) => b.address),
+        buyers.slice(0, 10).map(() => web3.utils.padLeft(0, 40)),
+        buyers.slice(0, 10).map(() => web3.utils.padLeft(0, 40)),
+        [],
+        {
+          value: keyPrice.mul(buyers.length).toString(),
+        }
       )
-      await Promise.all(txs.map((tx) => tx.wait()))
+      await tx.wait()
 
-      // increase supply
+      // set max keys to total supply
       const totalSupply = await lock.totalSupply()
       await lock.setMaxNumberOfKeys(totalSupply)
       assert.equal(
@@ -151,10 +143,10 @@ contract('Lock / setMaxNumberOfKeys', () => {
         lock
           .connect(buyers[11])
           .purchase(
-            keyPrice.toString(),
-            buyers[11].address,
-            web3.utils.padLeft(0, 40),
-            web3.utils.padLeft(0, 40),
+            [keyPrice.toString()],
+            [buyers[11].address],
+            [web3.utils.padLeft(0, 40)],
+            [web3.utils.padLeft(0, 40)],
             [],
             {
               value: keyPrice.toString(),
