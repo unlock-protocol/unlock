4209262e10e6e5ca138cb84e80fb0128e7235b3e
Clement Renaud <clement@clementrenaud.com>
2022-03-08 15:38:49 +0100

    add ERC1155 hook

---
+++ smart-contracts/contracts/test-artifacts/TestERC1155.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TestERC1155 is ERC1155 {
+    constructor() ERC1155("tokenURIExample") {}
+
+    function mint(
+        address holder,
+        uint tokenTypeId,
+        uint256 amount
+    )
+    public
+    returns (uint256)
+    {
+        _mint(holder, tokenTypeId, amount, "");
+    }
+}
