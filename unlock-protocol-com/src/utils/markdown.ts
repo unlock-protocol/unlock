075c0b3b1b1a038d47194f53dee7e7068bca4576
Julien Genestoux <julien.genestoux@gmail.com>
2022-03-08 21:54:40 -0500

    Manual deploy as of commit:c461cd3c4148d24caddf3af9058efa9617c4fc51

--- unlock-protocol-com/src/utils/markdown.ts
+++ unlock-protocol-com/src/utils/markdown.ts
@@ -2,6 +2,11 @@ import { remark } from 'remark'
 import html from 'remark-html'
 
 export async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+    .use(html, {
+      // We use arbitrary html in markdown
+      sanitize: false,
+    })
+    .process(markdown)
   return result.toString()
 }
