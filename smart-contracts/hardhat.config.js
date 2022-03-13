96101e8cd4fd7cf7f07cbfdd24b97545aa356476
Clément Renaud <clement@unlock-protocol.com>
2022-03-13 02:19:08 +0100

    feat(smart-contracts): bump PublicLock pragma to 0.8.7 (#8384)
    * bump pragma 0.8.7
    
    * solc optimizer runs back to 200

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -30,7 +30,7 @@ const { getHardhatNetwork } = require('./helpers/network')
 const settings = {
   optimizer: {
     enabled: true,
-    runs: 140,
+    runs: 200,
   },
   outputSelection: {
     '*': {
