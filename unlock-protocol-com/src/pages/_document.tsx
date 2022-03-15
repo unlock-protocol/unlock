6aafb6309d7f703470cdc7ab9fb0d41e289d625d
searchableguy <me@searchableguy.com>
2022-03-16 00:52:49 +0530

    feat(unlock-protocol-com): Add favicons and update icon (#8407)

--- unlock-protocol-com/src/pages/_document.tsx
+++ unlock-protocol-com/src/pages/_document.tsx
@@ -14,7 +14,32 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="mask-icon"
+          href="/images/favicons/safari-pinned-tab.svg"
+          color="#fff7e8"
+        />
+
+        <link rel="icon" href="/images/favicons/favicon.ico" />
+        <meta name="theme-color" content="#fff7e8" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicons/favicon-16x16.png"
+        />
         <link
           rel="alternate"
           href="blog.rss"
