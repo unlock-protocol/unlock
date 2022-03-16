24dcaae98faf5a2643dedb499473dffc6af37a32
searchableguy <me@searchableguy.com>
2022-03-16 17:03:27 +0530

    feat(unlock-app): Add toast notification for network change (#8409)
    * feat(unlock-app): Add toast notification for network change
    
    * change message slightly

--- unlock-app/src/pages/_app.tsx
+++ unlock-app/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import 'cross-fetch/polyfill'
 import type { AppProps } from 'next/app'
 import TagManager from 'react-gtm-module'
+import { Toaster } from 'react-hot-toast'
 import configure from '../config'
 import GlobalWrapper from '../components/interface/GlobalWrapper'
 import '../index.css'
@@ -19,6 +20,7 @@ const UnlockApp = ({ Component, pageProps }: AppProps) => {
   return (
     <GlobalWrapper pageProps={pageProps}>
       <Component {...pageProps} />
+      <Toaster />
     </GlobalWrapper>
   )
 }
