edcd49dd51d1f32f39a8e407a9ef5ecf404fc29c
Kalidou Diagne <kld.diagne@gmail.com>
2022-03-22 17:00:32 +0100

    web3Service.isLockManager

--- unlock-app/src/components/interface/checkout/Checkout.tsx
+++ unlock-app/src/components/interface/checkout/Checkout.tsx
@@ -39,7 +39,6 @@ import { AuthenticationContext } from '../../../contexts/AuthenticationContext'
 
 import { PaywallConfig, OAuthConfig } from '../../../unlockTypes'
 import { OAuthConnect } from './OauthConnect'
-import { useAppStorage } from '../../../hooks/useAppStorage'
 
 interface CheckoutProps {
   emitCloseModal: (success: boolean) => void
