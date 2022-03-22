c2a08c860f70613a8075b87c75e6953be637ccef
Clement Renaud <clement@clementrenaud.com>
2022-03-22 13:46:59 +0100

    add ownerOf transfer test case

--- smart-contracts/test/Lock/erc721/ownerOf.js
+++ smart-contracts/test/Lock/erc721/ownerOf.js
@@ -34,4 +34,27 @@ contract('Lock / erc721 / ownerOf', (accounts) => {
     let address = await locks.FIRST.ownerOf.call(args.tokenId)
     assert.equal(address, accounts[1])
   })
+
+  it('should work correctly after a transfer', async () => {
+    const tx = await locks.FIRST.purchase(
+      [],
+      [accounts[1]],
+      [web3.utils.padLeft(0, 40)],
+      [web3.utils.padLeft(0, 40)],
+      [],
+      {
+        value: web3.utils.toWei('0.01', 'ether'),
+        from: accounts[1],
+      }
+    )
+    const { args } = tx.logs.find((v) => v.event === 'Transfer')
+    let address = await locks.FIRST.ownerOf.call(args.tokenId)
+    assert.equal(address, accounts[1])
+
+    // transfer
+    await locks.FIRST.transferFrom(accounts[1], accounts[7], args.tokenId, {
+      from: accounts[1],
+    })
+    assert.equal(await locks.FIRST.ownerOf.call(args.tokenId), accounts[7])
+  })
 })
