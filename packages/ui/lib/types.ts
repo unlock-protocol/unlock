fb7f0432d36f44dd5a68014575af885524f15564
searchableguy <me@searchableguy.com>
2022-03-26 01:47:10 +0530

    feat(ui): Redesign input to fit our design system and add additional functions (#8443)

--- packages/ui/lib/types.ts
+++ packages/ui/lib/types.ts
@@ -1,4 +1,4 @@
-export type Size = 'small' | 'medium' | 'large'
+export type Size = 'tiny' | 'small' | 'medium' | 'large'
 export type SizeStyleProp = Partial<Record<Size, string>>
-export type State = "error" | "success"
+export type State = 'error' | 'success'
 export type StateStyleProp = Partial<Record<State, string>>
