be9fe835bf3c07a4772e4af8f997154768a8a263
Clément Renaud <clement@unlock-protocol.com>
2022-03-25 10:42:44 +0100

    Update smart-contracts/contracts/Unlock.sol
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- smart-contracts/contracts/Unlock.sol
+++ smart-contracts/contracts/Unlock.sol
@@ -313,7 +313,6 @@ contract Unlock is
     proxyAdmin.upgrade(proxy, impl);
 
     // let's upgrade the data schema
-    // for lock that have more than 100 active keys, the `migrate` function will need
     // to be called again after the upgrade is done
     lock.migrate();
 
