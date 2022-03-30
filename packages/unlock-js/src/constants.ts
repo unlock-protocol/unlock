8ebf34a24abe4d827e9ffa2a13959a71a24bd70a
searchableguy <me@searchableguy.com>
2022-03-30 19:41:35 +0530

    feat(unlock-js): Migrate to typescript (#8440)
    * feat(unlock-js): Migrate to typescript
    
    * migrate more files to typescript
    
    * fix type issues
    
    * fix more type issues
    
    * fix a test import

--- packages/unlock-js/src/constants.ts
+++ packages/unlock-js/src/constants.ts
@@ -2,11 +2,12 @@ import { ethers } from 'ethers'
 
 export const MAX_UINT =
   '115792089237316195423570985008687907853269984665640564039457584007913129639935'
+
 export const ETHERS_MAX_UINT = ethers.constants.MaxUint256
 
 export const UNLIMITED_KEYS_COUNT = -1
 
-export const KEY_ID = (lock, owner) => [lock, owner].join('-')
+export const KEY_ID = (lock: string, owner: string) => [lock, owner].join('-')
 
 export const ZERO = ethers.constants.AddressZero
 
