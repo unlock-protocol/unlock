566da060a06acea90807240aa7885651b0a5626b
Clément Renaud <clement+git@clementrenaud.com>
2022-03-31 10:49:52 +0200

    remove imports + fix docstring

--- smart-contracts/contracts/mixins/MixinDisable.sol
+++ smart-contracts/contracts/mixins/MixinDisable.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './MixinFunds.sol';
-import './MixinRoles.sol';
-
 /**
- * The ability to disbale locks feature has been removed on v10 to decrease contract code size
- * Disabling locks can be achieved by using `setMaxNumberOfKeys` to `totalSupply`
- * and expiring all  existing keys.
- * @dev the variabales are kept to preserve stoage layout during upgrades
+ * The ability to disable locks has been removed on v10 to decrease contract code size.
+ * Disabling locks can be achieved by setting `setMaxNumberOfKeys` to `totalSupply`
+ * and expire all existing keys.
+ * @dev the variables are kept to prevent conflicts in storage layout during upgrades
  */
 contract MixinDisable {
   bool public isAlive;
