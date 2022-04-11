32b163034b14a918cb4d9fb88c80fb00fa2c39d1
Clement Renaud <clement@clementrenaud.com>
2022-04-11 10:10:35 +0200

    bump latest contracts versions

--- packages/hardhat-plugin/src/constants.ts
+++ packages/hardhat-plugin/src/constants.ts
@@ -1,5 +1,5 @@
-export const UNLOCK_LATEST_VERSION = 10
-export const PUBLIC_LOCK_LATEST_VERSION = 9
+export const UNLOCK_LATEST_VERSION = 11
+export const PUBLIC_LOCK_LATEST_VERSION = 10
 
 // task names
 export const TASK_CREATE_LOCK = 'unlock:create-lock'
