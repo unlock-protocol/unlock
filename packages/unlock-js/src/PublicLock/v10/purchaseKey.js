870bfca3b77a9ac6671e0b90bb6b42050af1c62a
Julien Genestoux <julien.genestoux@gmail.com>
2022-04-20 10:20:17 -0400

    feat(unlock-js) not failing when gas estimates are failing (#8616)
    * not failing when gas estimates are failing and letting the wallet handle it
    
    * drying purchaseKey for v10
    
    * returning the single key bought
    
    * not using an array when the argument is falsy
    
    * fixing issue with key price retrieval

--- packages/unlock-js/src/PublicLock/v10/purchaseKey.js
+++ packages/unlock-js/src/PublicLock/v10/purchaseKey.js
@@ -1,17 +1,7 @@
-import { ZERO } from '../../constants'
-import { approveTransfer, getAllowance } from '../../erc20'
-import formatKeyPrice from '../utils/formatKeyPrice'
+import purchaseKeys from './purchaseKeys'
+
 /**
- * Purchase key function. This implementation requires the following
- * @param {object} params:
- * - {PropTypes.address} lockAddress
- * - {PropTypes.address} owner
- * - {string} keyPrice
- * - {PropTypes.address} erc20Address
- * - {number} decimals
- * - {PropTypes.address} referrer (address which will receive UDT - if applicable)
- * - {PropTypes.array[bytes]} data (array of bytes, not used in transaction but can be used by hooks)
- * @param {function} callback invoked with the transaction hash
+ * Purchase key function. calls the purchaseKeys function with the vlalue in array
  */
 export default async function (
   {
@@ -26,128 +16,18 @@ export default async function (
   },
   callback
 ) {
-  const lockContract = await this.getLockContract(lockAddress)
-
-  if (!owner) {
-    owner = await this.signer.getAddress()
-  }
-
-  if (!referrer) {
-    referrer = ZERO
-  }
-
-  if (!keyManager) {
-    keyManager = ZERO
-  }
-
-  if (!data) {
-    data = []
-  }
-
-  // If erc20Address was not provided, get it
-  if (!erc20Address) {
-    erc20Address = await lockContract.tokenAddress()
-  }
-  let actualAmount
-  if (!keyPrice) {
-    // We might not have the keyPrice, in which case, we need to retrieve from the the lock!
-    actualAmount = await lockContract.keyPrice()
-  } else {
-    actualAmount = await formatKeyPrice(
-      keyPrice,
+  const keys = await purchaseKeys.bind(this)(
+    {
+      owners: owner ? [owner] : null,
+      keyManagers: keyManager ? [keyManager] : null,
+      keyPrices: keyPrice ? [keyPrice] : null,
+      referrers: referrer ? [referrer] : null,
+      data: data ? [data] : null,
+      lockAddress,
       erc20Address,
       decimals,
-      this.provider
-    )
-  }
-
-  const purchaseForOptions = {}
-  if (erc20Address && erc20Address !== ZERO) {
-    const approvedAmount = await getAllowance(
-      erc20Address,
-      lockAddress,
-      this.provider,
-      this.signer
-    )
-    if (!approvedAmount || approvedAmount.lt(actualAmount)) {
-      await approveTransfer(
-        erc20Address,
-        lockAddress,
-        actualAmount,
-        this.provider,
-        this.signer
-      )
-      // Since we sent the approval transaction, we cannot rely on Ethers to do an estimate, because the computation would fail (since the approval might not have been mined yet)
-      purchaseForOptions.gasLimit = 400000
-    }
-  } else {
-    purchaseForOptions.value = actualAmount
-  }
-
-  // Estimate gas. Bump by 30% because estimates are wrong!
-  if (!purchaseForOptions.gasLimit) {
-    // To get good estimates we need the gas price, because it matters in the actual execution (UDT calculation takes it into account)
-    // TODO remove once we move to use block.baseFee in UDT calculation
-    const { gasPrice, maxFeePerGas, maxPriorityFeePerGas } =
-      await this.provider.getFeeData()
-
-    if (maxFeePerGas && maxPriorityFeePerGas) {
-      purchaseForOptions.maxFeePerGas = maxFeePerGas
-      purchaseForOptions.maxPriorityFeePerGas = maxPriorityFeePerGas
-    } else {
-      purchaseForOptions.gasPrice = gasPrice
-    }
-
-    const gasLimit = await lockContract.estimateGas.purchase(
-      [actualAmount],
-      [owner],
-      [referrer],
-      [keyManager],
-      [data],
-      purchaseForOptions
-    )
-    // Remove the gas prices settings for the actual transaction (the wallet will set them)
-    delete purchaseForOptions.maxFeePerGas
-    delete purchaseForOptions.maxPriorityFeePerGas
-    delete purchaseForOptions.gasPrice
-    purchaseForOptions.gasLimit = gasLimit.mul(13).div(10).toNumber()
-  }
-
-  const transactionPromise = lockContract.purchase(
-    [actualAmount],
-    [owner],
-    [referrer],
-    [keyManager],
-    [data],
-    purchaseForOptions
+    },
+    callback
   )
-
-  const hash = await this._handleMethodCall(transactionPromise)
-
-  if (callback) {
-    callback(null, hash, await transactionPromise)
-  }
-
-  // Let's now wait for the transaction to go thru to return the token id
-  const receipt = await this.provider.waitForTransaction(hash)
-
-  if (receipt.status === 0) {
-    throw new Error('Transaction failed')
-  }
-
-  const parser = lockContract.interface
-  const transferEvent = receipt.logs
-    .map((log) => {
-      if (log.address !== lockAddress) return // Some events are triggered by the ERC20 contract
-      return parser.parseLog(log)
-    })
-    .filter((event) => {
-      return event && event.name === 'Transfer'
-    })[0]
-
-  if (transferEvent) {
-    return transferEvent.args.tokenId.toString()
-  }
-  // There was no Transfer log (transaction failed?)
-  return null
+  return keys[0] // Only a single key!
 }
