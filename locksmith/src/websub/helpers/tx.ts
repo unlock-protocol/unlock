12f6537faac3d713f64809c8cfb5f8520c4392f2
Clément Renaud <clement+git@clementrenaud.com>
2022-04-28 15:19:20 +0200

    fix worker

--- locksmith/src/websub/helpers/tx.ts
+++ locksmith/src/websub/helpers/tx.ts
@@ -10,6 +10,53 @@ interface RenewKeyParams {
   network: number
 }
 
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_tokenId',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: '_referrer',
+        type: 'address',
+      },
+    ],
+    name: 'renewMembershipFor',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'gasRefundValue',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '_refundValue',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'publicLockVersion',
+    outputs: [
+      {
+        internalType: 'uint16',
+        name: '',
+        type: 'uint16',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+]
+
 export async function renewKey({
   keyId,
   lockAddress,
@@ -26,10 +73,6 @@ export async function renewKey({
   const signer = new Wallet(purchaserCredentials, rpc)
 
   // parse lock
-  const abi = [
-    'function renewMembershipFor(uint256 _tokenId,address _referrer)',
-    'function publicLockVersion()',
-  ]
   const lock = new Contract(lockAddress, abi, signer)
 
   // make sure reccuring payments are supported
@@ -40,35 +83,29 @@ export async function renewKey({
   }
 
   // estimate gas for the renewMembership function.
-  let gasLimit
-  try {
-    gasLimit = await lock.estimateGas.renewMembershipFor(
-      keyId,
-      constants.AddressZero
-    )
-  } catch (error) {
-    // If the gas estimate fails, we should skip and try the next one.
-    throw new Error(
-      `Gas estimation failed | [${network}, ${lockAddress}, ${keyId}]`
-    )
-  }
-
+  const gasLimit = await lock.estimateGas.renewMembershipFor(
+    keyId,
+    constants.AddressZero
+  )
   // We only renew keys for which we are getting a gas refund whose value cover the gas spent.
   const gasRefund = await lock.gasRefundValue()
-  if (gasRefund.gte(gasLimit)) {
-    // send actual tx
-    const tx = await lock.renewMembershipFor(keyId, constants.AddressZero)
-    // record renewal in db
-    KeyRenewal.create({
-      network,
-      lockAddress,
-      keyId,
-      initiatedBy: signer.address,
-      tx: tx.transactionHash,
-    })
-  } else {
+  if (gasRefund.lte(gasLimit)) {
     throw new Error(
       `GasRefundValue (${gasRefund.toString()}) does not cover gas cost  | [${network}, ${lockAddress}, ${keyId}]`
     )
   }
+
+  // send actual tx
+  const tx = await lock.renewMembershipFor(keyId, constants.AddressZero, {
+    gasLimit: gasLimit.mul(2),
+  })
+
+  // record renewal in db
+  await KeyRenewal.create({
+    network,
+    lockAddress,
+    keyId,
+    initiatedBy: signer.address,
+    tx: tx.hash,
+  })
 }
