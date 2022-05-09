1ddf6ee3ffbed5ee4787d0d85ce4c61e4849c3a2
searchableguy <me@searchableguy.com>
2022-05-09 14:38:43 +0530

    feat(unlock-app): Add transaprency in the background

--- unlock-app/src/components/interface/checkout/CheckoutContainer.tsx
+++ unlock-app/src/components/interface/checkout/CheckoutContainer.tsx
@@ -9,32 +9,11 @@ interface Props {
 export const CheckoutContainer: React.FunctionComponent<Props> = ({
   children,
 }: React.PropsWithChildren<Props>) => {
-  return <Container>{children}</Container>
+  return (
+    <div className="min-h-screen min-w-full flex items-center justify-center bg-black bg-opacity-25">
+      {children}
+    </div>
+  )
 }
-
-export const Container = styled.div`
-  position: fixed;
-  height: 100vh;
-  width: 100vw;
-  left: 0;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  ${Media.phone`
-    padding-top: 0;
-    padding-bottom: 0;
-  `}
-
-  &:before {
-    content: '';
-    background: rgba(0, 0, 0, 25%);
-    position: absolute;
-    inset: 0;
-  }
-`
 
 export default CheckoutContainer
