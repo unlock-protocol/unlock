3db9cdeb2b36d8f3fd2127a3af86a15ca0cb9b63
searchableguy <me@searchableguy.com>
2022-05-11 00:28:37 +0530

    feat(unlock-app): Add transparency in the background (#8779)

--- unlock-app/src/components/interface/checkout/CheckoutContainer.tsx
+++ unlock-app/src/components/interface/checkout/CheckoutContainer.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import styled from 'styled-components'
-import Media from '../../../theme/media'
+import React, { useEffect } from 'react'
 
 interface Props {
   children: React.ReactNode
@@ -9,32 +7,16 @@ interface Props {
 export const CheckoutContainer: React.FunctionComponent<Props> = ({
   children,
 }: React.PropsWithChildren<Props>) => {
-  return <Container>{children}</Container>
+  useEffect(() => {
+    // We use background color on body on normal pages. This adds a class to override it.
+    document.querySelector('body')?.classList.add('bg-transparent')
+  }, [])
+
+  return (
+    <div className="min-h-screen min-w-full flex flex-col bg-opacity-25 bg-black items-center justify-center overflow-auto">
+      {children}
+    </div>
+  )
 }
-
-export const Container = styled.div`
-  position: fixed;
-  height: 100vh;
-  width: 100vw;
-  left: 0;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  ${Media.phone`
-    padding-top: 0;
-    padding-bottom: 0;
-  `}
-
-  &:before {
-    content: '';
-    background: rgba(0, 0, 0, 25%);
-    position: absolute;
-    inset: 0;
-  }
-`
 
 export default CheckoutContainer
