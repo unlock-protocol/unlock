ba268b6474911b4c9ed8f6c85d371dfe31921327
Clément Renaud <clement@unlock-protocol.com>
2022-05-11 20:31:50 +0200

    round and floor nb payments from URL (#8800)

--- unlock-app/src/components/interface/checkout/CryptoCheckout.tsx
+++ unlock-app/src/components/interface/checkout/CryptoCheckout.tsx
@@ -103,7 +103,11 @@ export const CryptoCheckout = ({
   const hasRecipients = recipients?.length > 0
 
   // for recurring purchases
-  const nbPayments = paywallConfig?.locks[lock.address]?.recurringPayments
+  let nbPayments = paywallConfig?.locks[lock.address]?.recurringPayments
+  nbPayments =
+    typeof nbPayments === 'number'
+      ? Math.abs(Math.floor(nbPayments))
+      : undefined
 
   const cantBuyWithCrypto = isAdvanced
     ? !(
