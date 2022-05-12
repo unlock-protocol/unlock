fd01ac434cd1f085ab27648bbf99a4cba047a530
Clément Renaud <clement@unlock-protocol.com>
2022-05-12 13:43:38 +0200

    feat(contracts): add linter to contracts package (#8806)
    * add linter
    
    * lint files

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,60 +1,59 @@
 // This file is generated, please don't edit directly
 // Refer to 'utils/parser.ts' and 'yarn build:index' for more
 
-import PublicLockV0 from './abis/PublicLock/PublicLockV0.json' 
-import PublicLockV1 from './abis/PublicLock/PublicLockV1.json' 
-import PublicLockV10 from './abis/PublicLock/PublicLockV10.json' 
-import PublicLockV2 from './abis/PublicLock/PublicLockV2.json' 
-import PublicLockV3 from './abis/PublicLock/PublicLockV3.json' 
-import PublicLockV4 from './abis/PublicLock/PublicLockV4.json' 
-import PublicLockV5 from './abis/PublicLock/PublicLockV5.json' 
-import PublicLockV6 from './abis/PublicLock/PublicLockV6.json' 
-import PublicLockV7 from './abis/PublicLock/PublicLockV7.json' 
-import PublicLockV8 from './abis/PublicLock/PublicLockV8.json' 
-import PublicLockV9 from './abis/PublicLock/PublicLockV9.json' 
-import UnlockV0 from './abis/Unlock/UnlockV0.json' 
-import UnlockV1 from './abis/Unlock/UnlockV1.json' 
-import UnlockV10 from './abis/Unlock/UnlockV10.json' 
-import UnlockV11 from './abis/Unlock/UnlockV11.json' 
-import UnlockV2 from './abis/Unlock/UnlockV2.json' 
-import UnlockV3 from './abis/Unlock/UnlockV3.json' 
-import UnlockV4 from './abis/Unlock/UnlockV4.json' 
-import UnlockV5 from './abis/Unlock/UnlockV5.json' 
-import UnlockV6 from './abis/Unlock/UnlockV6.json' 
-import UnlockV7 from './abis/Unlock/UnlockV7.json' 
-import UnlockV8 from './abis/Unlock/UnlockV8.json' 
-import UnlockV9 from './abis/Unlock/UnlockV9.json' 
-import UnlockDiscountTokenV0 from './abis/UnlockDiscountToken/UnlockDiscountTokenV0.json' 
-import UnlockDiscountTokenV1 from './abis/UnlockDiscountToken/UnlockDiscountTokenV1.json' 
-import UnlockDiscountTokenV2 from './abis/UnlockDiscountToken/UnlockDiscountTokenV2.json' 
+import PublicLockV0 from './abis/PublicLock/PublicLockV0.json'
+import PublicLockV1 from './abis/PublicLock/PublicLockV1.json'
+import PublicLockV10 from './abis/PublicLock/PublicLockV10.json'
+import PublicLockV2 from './abis/PublicLock/PublicLockV2.json'
+import PublicLockV3 from './abis/PublicLock/PublicLockV3.json'
+import PublicLockV4 from './abis/PublicLock/PublicLockV4.json'
+import PublicLockV5 from './abis/PublicLock/PublicLockV5.json'
+import PublicLockV6 from './abis/PublicLock/PublicLockV6.json'
+import PublicLockV7 from './abis/PublicLock/PublicLockV7.json'
+import PublicLockV8 from './abis/PublicLock/PublicLockV8.json'
+import PublicLockV9 from './abis/PublicLock/PublicLockV9.json'
+import UnlockV0 from './abis/Unlock/UnlockV0.json'
+import UnlockV1 from './abis/Unlock/UnlockV1.json'
+import UnlockV10 from './abis/Unlock/UnlockV10.json'
+import UnlockV11 from './abis/Unlock/UnlockV11.json'
+import UnlockV2 from './abis/Unlock/UnlockV2.json'
+import UnlockV3 from './abis/Unlock/UnlockV3.json'
+import UnlockV4 from './abis/Unlock/UnlockV4.json'
+import UnlockV5 from './abis/Unlock/UnlockV5.json'
+import UnlockV6 from './abis/Unlock/UnlockV6.json'
+import UnlockV7 from './abis/Unlock/UnlockV7.json'
+import UnlockV8 from './abis/Unlock/UnlockV8.json'
+import UnlockV9 from './abis/Unlock/UnlockV9.json'
+import UnlockDiscountTokenV0 from './abis/UnlockDiscountToken/UnlockDiscountTokenV0.json'
+import UnlockDiscountTokenV1 from './abis/UnlockDiscountToken/UnlockDiscountTokenV1.json'
+import UnlockDiscountTokenV2 from './abis/UnlockDiscountToken/UnlockDiscountTokenV2.json'
 import LockSerializer from './abis/utils/LockSerializer.json'
 
-
 // exports
-export {PublicLockV0}
-export {PublicLockV1}
-export {PublicLockV10}
-export {PublicLockV2}
-export {PublicLockV3}
-export {PublicLockV4}
-export {PublicLockV5}
-export {PublicLockV6}
-export {PublicLockV7}
-export {PublicLockV8}
-export {PublicLockV9}
-export {UnlockV0}
-export {UnlockV1}
-export {UnlockV10}
-export {UnlockV11}
-export {UnlockV2}
-export {UnlockV3}
-export {UnlockV4}
-export {UnlockV5}
-export {UnlockV6}
-export {UnlockV7}
-export {UnlockV8}
-export {UnlockV9}
-export {UnlockDiscountTokenV0}
-export {UnlockDiscountTokenV1}
-export {UnlockDiscountTokenV2}
-export {LockSerializer}
+export { PublicLockV0 }
+export { PublicLockV1 }
+export { PublicLockV10 }
+export { PublicLockV2 }
+export { PublicLockV3 }
+export { PublicLockV4 }
+export { PublicLockV5 }
+export { PublicLockV6 }
+export { PublicLockV7 }
+export { PublicLockV8 }
+export { PublicLockV9 }
+export { UnlockV0 }
+export { UnlockV1 }
+export { UnlockV10 }
+export { UnlockV11 }
+export { UnlockV2 }
+export { UnlockV3 }
+export { UnlockV4 }
+export { UnlockV5 }
+export { UnlockV6 }
+export { UnlockV7 }
+export { UnlockV8 }
+export { UnlockV9 }
+export { UnlockDiscountTokenV0 }
+export { UnlockDiscountTokenV1 }
+export { UnlockDiscountTokenV2 }
+export { LockSerializer }
--- packages/contracts/src/utils/parser.ts
+++ packages/contracts/src/utils/parser.ts
@@ -1,43 +1,38 @@
-import { getAbiPaths } from './files';
+import { getAbiPaths } from './files'
 
-async function main () {
-    const paths = await getAbiPaths()
-    const exports = paths
-        .flat()
-        .map(abiPath => {
-            const s = abiPath.split('/')
-            const contractName = s[2]
-            const versionNumber = s[3].replace(contractName, '').replace('.json', '')            
-            return {
-                contractName,
-                versionNumber,
-                abiPath
-            }
-        })
+async function main() {
+  const paths = await getAbiPaths()
+  const exports = paths.flat().map((abiPath) => {
+    const s = abiPath.split('/')
+    const contractName = s[2]
+    const versionNumber = s[3].replace(contractName, '').replace('.json', '')
+    return {
+      contractName,
+      versionNumber,
+      abiPath,
+    }
+  })
 
-    console.log('// This file is generated, please don\'t edit directly')
-    console.log('// Refer to \'utils/parser.ts\' and \'yarn build:index\' for more\n')
+  console.log("// This file is generated, please don't edit directly")
+  console.log("// Refer to 'utils/parser.ts' and 'yarn build:index' for more\n")
 
-    exports.forEach(({
-        contractName,
-        versionNumber,
-        abiPath
-    }) => console.log(`import ${contractName}${versionNumber} from '${abiPath}' `))
-    
-    console.log("import LockSerializer from './abis/utils/LockSerializer.json'")
-    console.log(`\n\n// exports`)
-    
-    exports.forEach(({
-        contractName,
-        versionNumber
-    }) => console.log(`export {${contractName}${versionNumber}}`))
+  exports.forEach(({ contractName, versionNumber, abiPath }) =>
+    console.log(`import ${contractName}${versionNumber} from '${abiPath}' `)
+  )
 
-    console.log('export {LockSerializer}')
+  console.log("import LockSerializer from './abis/utils/LockSerializer.json'")
+  console.log('\n\n// exports')
+
+  exports.forEach(({ contractName, versionNumber }) =>
+    console.log(`export {${contractName}${versionNumber}}`)
+  )
+
+  console.log('export {LockSerializer}')
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
