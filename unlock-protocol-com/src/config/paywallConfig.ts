bd89e50d3a27ce2ee72dfa4aea6d93d600a51816
Julien Genestoux <julien.genestoux@gmail.com>
2022-05-13 14:01:45 -0400

    replaced the lock in our config (#8817)

--- unlock-protocol-com/src/config/paywallConfig.ts
+++ unlock-protocol-com/src/config/paywallConfig.ts
@@ -2,20 +2,14 @@ export const paywallConfig = {
   network: 1,
   pessimistic: true,
   locks: {
-    '0xB0114bbDCe17e0AF91b2Be32916a1e236cf6034F': {
-      name: 'Unlock Community',
-      network: 1,
-    },
-    '0xac1fceC2e4064CCd83ac8C9B0c9B8d944AB0D246': {
+    '0xCE62D71c768aeD7EA034c72a1bc4CF58830D9894': {
       name: 'Unlock Community',
       network: 100,
     },
   },
   icon: 'https://raw.githubusercontent.com/unlock-protocol/unlock/master/design/brand/1808-Unlock-Identity_Unlock-WordMark.svg',
   callToAction: {
-    default: `Get an Unlock membership to access our Discord, blog comments and more!
-  
-        If gas prices are high, pick the xDAI membership and receive an airdrop!`,
+    default: `Get an Unlock membership to access our Discord, blog comments and more! No xDAI to pay for gas? Click the Claim button.`,
   },
 }
 
