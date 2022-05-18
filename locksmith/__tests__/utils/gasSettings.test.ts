c18401c4c1bce852e518f780921fce71eaeb9b30
Clement Renaud <clement@clementrenaud.com>
2022-05-18 19:39:16 +0200

    move `gasSettings` to its own util

---
+++ locksmith/__tests__/utils/gasSettings.test.ts
@@ -0,0 +1,57 @@
+import { getGasSettings } from '../../src/utils/gasSettings'
+
+jest.mock('@unlock-protocol/networks', () => {
+  const networks = jest.requireActual('@unlock-protocol/networks')
+  networks.networks[123] = { publicProvider: null }
+  return networks
+})
+
+jest.mock('ethers', () => {
+  const { ethers, BigNumber } = jest.requireActual('ethers')
+  return {
+    BigNumber,
+    ethers: {
+      ...ethers,
+      providers: {
+        JsonRpcProvider: jest.fn((providerUrl: string) => ({
+          // when providerUrl is undefined gasFeeData throws
+          getFeeData: providerUrl
+            ? jest.fn(async () => ({
+                maxFeePerGas: BigNumber.from(10000000000),
+                maxPriorityFeePerGas: BigNumber.from(10000000000),
+                catch: jest.fn(() => null),
+              }))
+            : jest.fn(() => Promise.reject()),
+        })),
+      },
+      Wallet: jest.fn(),
+    },
+  }
+})
+
+jest.mock('isomorphic-fetch', () => async () => ({
+  json: async () => ({
+    data: { standard: { maxPriorityFee: 36.37, maxFee: 36.37 } },
+  }),
+}))
+
+describe('getGasSettings', () => {
+  it('returns correct value from provider', async () => {
+    expect.assertions(2)
+    const { maxFeePerGas, maxPriorityFeePerGas } = await getGasSettings(1)
+    expect(maxFeePerGas?.toNumber()).toBe(10000000000)
+    expect(maxPriorityFeePerGas?.toNumber()).toBe(20000000000)
+  })
+  it('returns value from gas station on Polygon mainnet', async () => {
+    expect.assertions(2)
+    const { maxFeePerGas, maxPriorityFeePerGas } = await getGasSettings(137)
+    expect(maxFeePerGas?.toNumber()).toBe(37000000000)
+    expect(maxPriorityFeePerGas?.toNumber()).toBe(74000000000)
+  })
+  it('returns default value if gasFee fails ', async () => {
+    expect.assertions(2)
+    const { maxFeePerGas, maxPriorityFeePerGas } = await getGasSettings(123)
+    expect(maxFeePerGas?.toNumber()).toBe(40000000000)
+    expect(maxPriorityFeePerGas?.toNumber()).toBe(40000000000)
+  })
+})
---
+++ locksmith/src/utils/gasSettings.ts
@@ -0,0 +1,66 @@
+import fetch from 'isomorphic-fetch'
+import { ethers } from 'ethers'
+import networks from '@unlock-protocol/networks'
+
+interface GasSettings {
+  maxFeePerGas?: ethers.BigNumber
+  maxPriorityFeePerGas?: ethers.BigNumber
+  gasPrice?: ethers.BigNumber
+}
+
+export const getGasSettings = async (network: number): Promise<GasSettings> => {
+  // workaround for polygon: get max fees from gas station
+  // see https://github.com/ethers-io/ethers.js/issues/2828
+  if (network === 137) {
+    try {
+      const resp = await fetch('https://gasstation-mainnet.matic.network/v2')
+      const { data } = await resp.json()
+
+      const maxFeePerGas = ethers.utils.parseUnits(
+        `${Math.ceil(data.standard.maxFee)}`,
+        'gwei'
+      )
+
+      const maxPriorityFeePerGas = ethers.utils
+        .parseUnits(`${Math.ceil(data.standard.maxPriorityFee)}`, 'gwei')
+        .mul(2)
+
+      return {
+        maxFeePerGas,
+        maxPriorityFeePerGas,
+      }
+    } catch {
+      // ignore
+    }
+  }
+
+  // get fees from network provider
+  const provider = new ethers.providers.JsonRpcProvider(
+    networks[network].publicProvider
+  )
+
+  let feedata
+  try {
+    feedata = await provider.getFeeData()
+  } catch (error) {
+    // do nothing
+  }
+
+  if (feedata) {
+    const { gasPrice, maxFeePerGas, maxPriorityFeePerGas } = feedata
+
+    // We double to increase speed of execution
+    // We may end up paying *more* but we get mined earlier
+    return {
+      maxPriorityFeePerGas: maxFeePerGas?.mul(2),
+      maxFeePerGas: maxPriorityFeePerGas || undefined,
+      gasPrice: gasPrice?.mul(2),
+    }
+  }
+
+  // fallback to 40 gwei if no feeData
+  return {
+    maxFeePerGas: ethers.BigNumber.from(40000000000),
+    maxPriorityFeePerGas: ethers.BigNumber.from(40000000000),
+  }
+}
