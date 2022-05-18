ef17e51c8ab5bb1c27a2ebd89f171d11636480c3
Kalidou Diagne <kld.diagne@gmail.com>
2022-05-18 12:29:37 -0400

    add middlware to endpoints

--- locksmith/src/routes/v2/verifier.ts
+++ locksmith/src/routes/v2/verifier.ts
@@ -3,6 +3,7 @@ import networks from '@unlock-protocol/networks'
 import { Web3Service } from '@unlock-protocol/unlock-js'
 import { VerifierController } from '../../controllers/v2/verifierController'
 import { lockManagerMiddleware } from '../../utils/lockManager'
+import { authenticatedMiddleware } from '../../utils/auth'
 
 const router = express.Router({ mergeParams: true })
 const web3Service = new Web3Service(networks)
@@ -11,20 +12,18 @@ const verifierController = new VerifierController({
   web3Service,
 })
 
-router.get('/list/:network/:lockAddress', lockManagerMiddleware, (req, res) =>
+router.all('/', authenticatedMiddleware, lockManagerMiddleware)
+
+router.get('/list/:network/:lockAddress', (req, res) =>
   verifierController.list(req, res)
 )
 
-router.put(
-  '/:network/:lockAddress/:verifierAddress',
-  lockManagerMiddleware,
-  (req, res) => verifierController.addVerifier(req, res)
+router.put('/:network/:lockAddress/:verifierAddress', (req, res) =>
+  verifierController.addVerifier(req, res)
 )
 
-router.delete(
-  '/:network/:lockAddress/:verifierAddress',
-  lockManagerMiddleware,
-  (req, res) => verifierController.removeVerifier(req, res)
+router.delete('/:network/:lockAddress/:verifierAddress', (req, res) =>
+  verifierController.removeVerifier(req, res)
 )
 
 module.exports = router
