98f74bafba580f3fd772856cac775bf8f739521e
searchableguy <me@searchableguy.com>
2022-05-23 20:06:54 +0530

    feat(unlock-assets): Update existing icons and add unlock-monogram icon based on the new design system (#8852)

--- packages/unlock-assets/build/svg-component/Jobs.js
+++ packages/unlock-assets/build/svg-component/Jobs.js
@@ -7,7 +7,7 @@ const SvgJobs = ({ title, titleId, ...props }) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M13 5h-2a2 2 0 00-2 2H7a2 2 0 00-2 2v1h14V9a2 2 0 00-2-2h-2a2 2 0 00-2-2zm1 2a1 1 0 00-1-1h-2a1 1 0 00-1 1h4zm-9 4h14v4a2 2 0 01-2 2H7a2 2 0 01-2-2v-4z"
+      d="M13 5h-2a2 2 0 0 0-2 2H7a2 2 0 0 0-2 2v1h14V9a2 2 0 0 0-2-2h-2a2 2 0 0 0-2-2Zm1 2a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1h4Zm-9 4h14v4a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-4Z"
     />
   </svg>
 )
