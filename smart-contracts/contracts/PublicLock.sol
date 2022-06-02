68534c467aee73ec94047ecdd9d3331a433fc952
Clément Renaud <clement+git@clementrenaud.com>
2022-06-02 11:59:19 +0200

    public lock uses solc 0.8.11

--- smart-contracts/contracts/PublicLock.sol
+++ smart-contracts/contracts/PublicLock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.11;
 
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/introspection/ERC165StorageUpgradeable.sol';
