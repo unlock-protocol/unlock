d237daf337b16becd7f94d433ba4855d1527eec9
Julien Genestoux <julien.genestoux@gmail.com>
2022-06-02 10:43:39 -0400

    using a special case for localhost

--- smart-contracts/networks.js
+++ smart-contracts/networks.js
@@ -2,10 +2,16 @@ const networks = require('@unlock-protocol/networks')
 // When running CI, we connect to the hardhat node container
 const testHost = process.env.CI === 'true' ? 'eth-node' : '127.0.0.1'
 
-const hardhatNetworks = {}
+const hardhatNetworks = {
+  localhost: {
+    chainId: 31337,
+    url: `http://${testHost}:8545`,
+    name: 'localhost',
+  },
+}
 
 Object.keys(networks).forEach((key) => {
-  if (['default', 'networks'].indexOf(key) === -1) {
+  if (['default', 'networks', 'localhost'].indexOf(key) === -1) {
     hardhatNetworks[key] = {
       chainId: networks[key].id,
       name: networks[key].name,
