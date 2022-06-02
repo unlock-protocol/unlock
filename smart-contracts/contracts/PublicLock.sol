6e66cafeaa30da174a094b3a6a08e693f23945a9
Clément Renaud <clement@unlock-protocol.com>
2022-06-02 11:08:37 +0200

    fix(smart-contracts): remove unused logic from PublicLock contract (#8861)
    * remove logic from `migrate` function
    
    * remove `RenewKeyPurchase` event - fix #8595
    
    * add contract size to CI output
    
    * remove unused `IPublicLock` import
    
    * add back `schemaVersion` to migrate
    
    * remove name in params to avoid compiler warning
    
    * fix migrate docstring

--- smart-contracts/contracts/PublicLock.sol
+++ smart-contracts/contracts/PublicLock.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import './interfaces/IPublicLock.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/introspection/ERC165StorageUpgradeable.sol';
 import './mixins/MixinDisable.sol';
