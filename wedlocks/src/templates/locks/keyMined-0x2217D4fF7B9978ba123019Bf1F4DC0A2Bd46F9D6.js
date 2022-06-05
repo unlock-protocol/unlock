2f1401c8cd89ab4ced99c7f90903a6fdebfc5f5b
Julien Genestoux <julien.genestoux@gmail.com>
2022-06-05 18:03:35 -0400

    wedlock(feature): adding dappcon confirmation email (#8974)
    * dappcon confirmation email
    
    * updated template

---
+++ wedlocks/src/templates/locks/keyMined-0x2217D4fF7B9978ba123019Bf1F4DC0A2Bd46F9D6.js
@@ -0,0 +1,21 @@
+export default {
+  subject: () => 'Your Dappcon NFT Ticket is on your wallet!',
+  html: () =>
+    `
+<img src="cid:dappcon.png"/>
+<p>Your NFT ticket to DappCon22 has been transferred to your wallet! Yay 🎉! <br />
+Look out for announcements as we approach the conference date. For now, lean back and enjoy the summer until DappCon22 starts🏖. </p>
+<p>You can access your ticket using the <a href="https://app.unlock-protocol.com/keychain">Unlock key-chain</a> or in any other Gnosis Chain compatible NFT marketplace. Once we get closer to the conference your NFT ticket will turn into generative artwork created by <a href="https://twitter.com/sgt_sl8termelon">sgt_sl8termelon</a>. We’ll also send out more info about the check-in process as we approach the conference.</p>
+<p>If you have any questions or need an invoice, please contact info@dappcon.io</p>
+<p>Best wishes, 
+The DappCon Team </p>
+<p>Stay up to date and learn everything about the speaker and sponsor <a href="https://twitter.com/dappcon_berlin">@dappcon_berlin</a>.</p>
+    `,
+  attachments: [
+    {
+      filename: 'dappcon.png',
+      path: 'https://wedlocks.unlock-protocol.com/attachments/dappcon.png',
+      cid: 'dappcon.png',
+    },
+  ],
+}
