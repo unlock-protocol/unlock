9df44d52845a899e25ee1227d7e1afa96969f360
Kalidou Diagne <kld.diagne@gmail.com>
2022-06-10 17:28:59 +0200

    update test with login

--- locksmith/__tests__/controllers/v2/verifierController.test.ts
+++ locksmith/__tests__/controllers/v2/verifierController.test.ts
@@ -1,15 +1,19 @@
+import { ethers } from 'ethers'
+import { generateNonce, SiweMessage } from 'siwe'
 import request from 'supertest'
 import { getWalletInput } from '../../test-helpers/utils'
 
 const app = require('../../../src/app')
+
 jest.setTimeout(600000)
 
 const lockAddress = '0xFf8e6a02c4Ae22FB3eaa3b375a4506Bf3A9C8D5B'
-
+const privateKey =
+  'a78e7b5e88c33dc1eebd200f37bae00d49059f76adbfeacdadb02d118a3d7f39'
 describe('Verifier v2 endpoints for locksmith', () => {
   it('Get list items without authorization', async () => {
     expect.assertions(1)
-    const network = 4
+    const network = 80001
 
     const getListEndpoint = await request(app).get(
       `/v2/api/verifier/${network}/${lockAddress}`
@@ -19,7 +23,7 @@ describe('Verifier v2 endpoints for locksmith', () => {
 
   it('Get list items from lock with random address (not lockManager)', async () => {
     expect.assertions(2)
-    const network = 4
+    const network = 80001
 
     const { message, signedMessage } = await getWalletInput()
     const loginResponse = await request(app).post('/v2/auth/login').send({
@@ -37,27 +41,39 @@ describe('Verifier v2 endpoints for locksmith', () => {
 
   it('Add add verifier and delete correctly', async () => {
     expect.assertions(3)
-    const network = 4
+    const network = 80001
 
-    const {
-      walletAddress: address,
-      message,
-      signedMessage,
-    } = await getWalletInput()
+    const wallet = new ethers.Wallet(privateKey)
+    const walletAddress = await wallet.getAddress()
+    const nonce = generateNonce()
+    const message = new SiweMessage({
+      domain: 'locksmith.com',
+      nonce,
+      chainId: 4,
+      uri: 'https://locksmith.unlock-protocol.com',
+      version: '1',
+      statement: 'Authorize',
+      address: walletAddress,
+    })
+
+    const signedMessage = await wallet.signMessage(message.prepareMessage())
+
     const loginResponse = await request(app).post('/v2/auth/login').send({
       signature: signedMessage,
       message: message.prepareMessage(),
     })
+
+    const walletToAdd = ethers.Wallet.createRandom().getAddress()
     expect(loginResponse.status).toBe(200)
 
     const addVerifierResponse = await request(app)
-      .put(`/v2/api/verifier/${network}/${lockAddress}/${address}`)
+      .put(`/v2/api/verifier/${network}/${lockAddress}/${walletToAdd}`)
       .set('authorization', `Bearer ${loginResponse.body.accessToken}`)
 
     expect(addVerifierResponse.status).toBe(201)
 
     const deleteVerifierResponse = await request(app)
-      .delete(`/v2/api/verifier/${network}/${lockAddress}/${address}`)
+      .delete(`/v2/api/verifier/${network}/${lockAddress}/${walletToAdd}`)
       .set('authorization', `Bearer ${loginResponse.body.accessToken}`)
 
     expect(deleteVerifierResponse.status).toBe(200)
