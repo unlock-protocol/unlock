4bce4c2baf3d7f05ae873115250133eb38d587c7
Julien Genestoux <julien.genestoux@gmail.com>
2022-06-12 21:20:50 -0400

    feature(locksmith): Grant keys api (#9007)
    * wip
    
    * finished implementation
    
    * fixed middleware calls
    
    * fixing merge
    
    * fixing tests
    
    * using a helper to log users in
    
    * using zod
    
    * actually returning an error
    
    * moving middleware utils in their own folder
    
    * handling gas costs and ensuring the granter has enough funds
    
    * adding support to grant keys with managers and expirations
    
    * removed changes in locksmith/package.json
    
    * removed changes in scripts/docs-contracts-pr.sh
    
    * fixing tests
    
    * forcing the use of an application token
    
    * removed changes in locksmith/package.json
    
    * adding a 500 when grant key fails

--- locksmith/src/routes/index.ts
+++ locksmith/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authMiddleware } from '../utils/auth'
+import { authMiddleware } from '../utils/middlewares/auth'
 
 const transactionRouter = require('./transaction')
 const lockRouter = require('./lock')
@@ -16,6 +16,7 @@ const authRouterV2 = require('./v2/auth')
 const metadataRouterV2 = require('./v2/metadata')
 const applicationRouter = require('./v2/application')
 const verifierRouter = require('./v2/verifier')
+const grantKeysRouter = require('./v2/grantKeys')
 
 const config = require('../../config/config')
 
@@ -52,6 +53,7 @@ router.use('/v2/auth', authRouterV2)
 router.use('/v2/applications', applicationRouter)
 router.use('/v2/api/metadata', metadataRouterV2)
 router.use('/v2/api/verifier', verifierRouter)
+router.use('/v2/api/grant', grantKeysRouter)
 
 router.use('/', (_, res) => {
   res.send('<a href="https://unlock-protocol.com/">Unlock Protocol</a>')
