369ef465d24b74a761ded9651eb1e054f20c6c6b
Kalidou Diagne <kld.diagne@gmail.com>
2022-06-15 18:02:02 +0200

    add verification middleware

--- locksmith/src/routes/v2/verifier.ts
+++ locksmith/src/routes/v2/verifier.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import VerifierController from '../../controllers/v2/verifierController'
 import { authenticatedMiddleware } from '../../utils/middlewares/auth'
+import { isVerifierMiddleware } from '../../utils/middlewares/isVerifierMiddleware'
 import { lockManagerMiddleware } from '../../utils/middlewares/lockManager'
 
 const router = express.Router({ mergeParams: true })
@@ -25,8 +26,10 @@ router.delete(
   (req, res) => verifierController.removeVerifier(req, res)
 )
 
-router.get('/enabled/:network/:lockAddress/:verifierAddress', (req, res) =>
-  verifierController.isVerifierEnabled(req, res)
+router.get(
+  '/enabled/:network/:lockAddress/:verifierAddress',
+  isVerifierMiddleware,
+  (req, res) => verifierController.isVerifierEnabled(req, res)
 )
 
 module.exports = router
