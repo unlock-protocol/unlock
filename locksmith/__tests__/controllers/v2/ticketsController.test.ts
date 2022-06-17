1254a844ab65b008e6f7e3449d8631a4395de849
Kalidou Diagne <kld.diagne@gmail.com>
2022-06-17 11:51:11 +0200

    token id change

--- locksmith/__tests__/controllers/v2/ticketsController.test.ts
+++ locksmith/__tests__/controllers/v2/ticketsController.test.ts
@@ -8,7 +8,7 @@ jest.setTimeout(600000)
 const lockAddress = '0x3F09aD349a693bB62a162ff2ff3e097bD1cE9a8C'
 const wrongLockAddress = '0x00'
 const network = 4
-const tokenId = '12345'
+const tokenId = '2244'
 const wrongTokenId = '666'
 let owner = `0x00192fb10df37c9fb26829eb2cc623cd1bf599e8`
 
