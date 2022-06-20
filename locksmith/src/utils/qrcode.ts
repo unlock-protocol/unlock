fc2bca168a9c5574a00b18fb710ea202ddd081ed
Kalidou Diagne <kld.diagne@gmail.com>
2022-06-21 00:01:23 +0200

    feat(locksmith) - generate QRcode and send it by email (#9054)
    * add qrcode library and function
    
    * qrCode generation function
    
    * yarn update
    
    * send qrcode as attachment to email
    
    * add send email endpoint
    
    * add attachments when network is present
    
    * update tests
    
    * update tests
    
    * improve send email with locksmith
    
    * add logger
    
    * fix tests

---
+++ locksmith/src/utils/qrcode.ts
@@ -0,0 +1,36 @@
+import { Encoder, ErrorCorrectionLevel } from '@nuintun/qrcode'
+import Dispatcher from '../fulfillment/dispatcher'
+
+interface GenerateQrCodeProps {
+  network: number
+  lockAddress: string
+  tokenId: string
+}
+
+const generateQrCodeUrl = (data: any, signature: any): string => {
+  const url = new URL(`${window.location.origin}/verification`)
+  url.searchParams.append('data', data)
+  url.searchParams.append('sig', signature)
+
+  return url.toString()
+}
+
+export const generateQrCode = async ({
+  network,
+  lockAddress,
+  tokenId,
+}: GenerateQrCodeProps) => {
+  const dispatcher = new Dispatcher()
+  const [payload, signature] = await dispatcher.signToken(
+    network,
+    lockAddress,
+    tokenId
+  )
+  const qrcode = new Encoder()
+  qrcode.setErrorCorrectionLevel(ErrorCorrectionLevel.L)
+  qrcode.write(generateQrCodeUrl(payload, signature))
+  qrcode.make()
+
+  // this will return a base64 image
+  return qrcode.toDataURL(5)
+}
