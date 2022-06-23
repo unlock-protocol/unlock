680b48556f31685ea5c65f27ae2877aa6d92007b
jordang7 <77jgreenberg77@gmail.com>
2022-06-23 13:28:45 -0400

    re-add cross-fetch in locksmithService.ts

--- packages/unlock-js/src/locksmithService.ts
+++ packages/unlock-js/src/locksmithService.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch, { Headers } from 'cross-fetch'
 import { SiweMessage } from 'siwe'
 import { FetchError } from './utils'
 export const PRODUCTION_HOST = 'https://locksmith.unlock-protocol.com'
