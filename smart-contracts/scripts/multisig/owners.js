3121ae2c51dc9c27724168fed0332fde6246d067
Clément Renaud <clement+git@clementrenaud.com>
2022-06-24 12:12:36 +0200

    get owners from multisig directly

--- smart-contracts/scripts/multisig/owners.js
+++ smart-contracts/scripts/multisig/owners.js
@@ -1,17 +1,12 @@
 const { ethers } = require('hardhat')
-const { networks } = require('@unlock-protocol/networks')
+const { getProvider, getSafeAddress } = require('./_helpers')
 const multisigABI = require('../../test/helpers/ABIs/multisig.json')
 
 async function main({ chainId, safeAddress }) {
-  // get the correct provider if chainId is specified
-  let provider
-  if (chainId) {
-    const { publicProvider } = networks[chainId]
-    provider = new ethers.providers.JsonRpcProvider(publicProvider)
-  } else {
-    ;({ provider } = ethers)
+  const { provider } = await getProvider(chainId)
+  if (!safeAddress) {
+    safeAddress = await getSafeAddress(provider)
   }
-
   const safe = new ethers.Contract(safeAddress, multisigABI, provider)
   const owners = await safe.getOwners()
   return owners
