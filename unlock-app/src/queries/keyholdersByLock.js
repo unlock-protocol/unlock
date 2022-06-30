8d31c9bafccc299087bb04f08ffa1ee7876d61be
Kalidou Diagne <kld.diagne@gmail.com>
2022-06-30 18:16:02 +0200

    order locks items by keyId

--- unlock-app/src/queries/keyholdersByLock.js
+++ unlock-app/src/queries/keyholdersByLock.js
@@ -8,12 +8,15 @@ export default function keyholdersByLockQuery() {
       $first: Int! = 100
       $skip: Int! = 0
       $owner: String = ""
+      $keyId: BigInt
     ) {
       locks(where: { address_in: $addresses }) {
         keys(
           where: { expiration_gt: $expiresAfter, owner_contains: $owner }
           first: $first
           skip: $skip
+          orderBy: keyId
+          orderDirection: asc
         ) {
           owner {
             address
