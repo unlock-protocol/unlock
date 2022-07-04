8badc8994a1e0f94dd94e5c69973c35d3cb00fcb
Clément Renaud <clement+git@clementrenaud.com>
2022-07-04 11:58:34 +0200

    add a test time helper

---
+++ smart-contracts/test/helpers/time.js
@@ -0,0 +1,19 @@
+const { network, ethers } = require('hardhat')
+
+async function increaseTimeTo(expirationTs) {
+  expirationTs =
+    typeof expirationTs === 'string' ? expirationTs : expirationTs.toNumber()
+
+  const { timestamp } = await ethers.provider.getBlock('latest')
+
+  if (timestamp > expirationTs) {
+    throw new Error(
+      `Cannot increase time (${timestamp}) to the past (${expirationTs})`
+    )
+  }
+  await network.provider.send('evm_increaseTime', [expirationTs])
+}
+
+module.exports = {
+  increaseTimeTo,
+}
