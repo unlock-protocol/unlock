fdb9844208df4a7485ca2c77bbbc9cb6ef1622ba
Clément Renaud <clement+git@clementrenaud.com>
2022-07-06 15:25:00 +0200

    add script to reorg docs properly

---
+++ packages/contracts/bin/build_docs.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+SRC_DOCS_FOLDER="$(pwd)/docs"
+DEST_DOCS_FOLDER="$(pwd)/docs-reorg"
+
+# build docs
+yarn hardhat dodoc
+
+# delete existing folder
+rm -rf $DEST_DOCS_FOLDER
+mkdir -p $DEST_DOCS_FOLDER
+
+# first archive all 
+cp -R $SRC_DOCS_FOLDER $DEST_DOCS_FOLDER
+mv $DEST_DOCS_FOLDER/docs $DEST_DOCS_FOLDER/archive
+rm -rf $DEST_DOCS_FOLDER/archive/col
+
+# docusaurus titles
+echo '{"label": "Protocol Reference"}' > "$DEST_DOCS_FOLDER/__category__.json"
+echo '{"label": "Past Versions"}' > "$DEST_DOCS_FOLDER/archive/__category__.json"
+
+# find latest versions
+latest_template=$(ls $SRC_DOCS_FOLDER/PublicLock/IPublicLockV* | sed -e 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g' | sort -nr | head -n1)
+latest_unlock=$(ls $SRC_DOCS_FOLDER/Unlock/IUnlockV* | sed -e 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g' | sort -nr | head -n1)
+
+# get latest versions out of archive folder
+mv "$DEST_DOCS_FOLDER/archive/PublicLock/IPublicLockV${latest_template}.md" "$DEST_DOCS_FOLDER/PublicLock.md"
+mv "$DEST_DOCS_FOLDER/archive/Unlock/IUnlockV${latest_unlock}.md" "$DEST_DOCS_FOLDER/Unlock.md"
+
+# replace docs
+rm -rf $SRC_DOCS_FOLDER
+mv $DEST_DOCS_FOLDER $SRC_DOCS_FOLDER
