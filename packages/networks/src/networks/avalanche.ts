c262b159109fcde2d0cf08b2037ea58de41779a5
Clément Renaud <clement+git@clementrenaud.com>
2022-07-06 16:20:14 +0200

    add avalanche to networks

---
+++ packages/networks/src/networks/avalanche.ts
@@ -0,0 +1,34 @@
+import { NetworkConfig } from '@unlock-protocol/types'
+
+export const avalanche: NetworkConfig = {
+  publicProvider: 'https://api.avax.network/ext/bc/C/rpc',
+  provider: 'https://api.avax.network/ext/bc/C/rpc',
+  unlockAddress: '',
+  multisig: '',
+  id: 43114,
+  name: 'Avalanche (C-Chain)',
+  blockTime: 1000,
+  subgraphURI: 'https://api.thegraph.com/subgraphs/name/unlock-protocol/avalanche',
+  explorer: {
+    name: 'Snowtrace (Avalanche)',
+    urls: {
+      address: (address) => `https://snowtrace.io/address/${address}`,
+      transaction: (hash) => `https://snowtrace.io/tx/${hash}`,
+      token: (address, holder) =>
+        `https://snowtrace.io/token/${address}?a=${holder}`,
+    },
+  },
+  requiredConfirmations: 12,
+  erc20: null,
+  baseCurrencySymbol: 'AVAX',
+  locksmithUri: 'https://locksmith.unlock-protocol.com',
+  nativeCurrency: {
+    name: 'AVAX',
+    symbol: 'AVAX',
+    decimals: 18,
+  },
+  startBlock: 26584912,
+  previousDeploys: [],
+}
+
+export default avalanche
