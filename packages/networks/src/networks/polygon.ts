bef5ec020d3452c0abb0e63a9698b0f50d13c3ad
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-07-06 12:55:25 +0530

    Update packages/networks/src/networks/polygon.ts
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- packages/networks/src/networks/polygon.ts
+++ packages/networks/src/networks/polygon.ts
@@ -37,7 +37,7 @@ export const polygon: NetworkConfig = {
       startBlock: 15714206,
     },
   ],
-  description: 'Layer 2 network. Cheaper transaction cost.',
+  description: 'Popular side chain network. Cheaper transaction cost.',
 }
 
 export default polygon
