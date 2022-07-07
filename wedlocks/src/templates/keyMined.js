741edb81c8e91dbbc74854eb7f831ef91fc59275
Kalidou Diagne <kld.diagne@gmail.com>
2022-07-07 19:08:27 +0200

    add tests

--- wedlocks/src/templates/keyMined.js
+++ wedlocks/src/templates/keyMined.js
@@ -1,14 +1,19 @@
 const links = (params) => {
   const hasTxUrl = params?.txUrl?.length > 0
-  const hasOpenSeaUrl = params?.txUrl?.length > 0
+  const hasOpenSeaUrl = params?.openSeaUrl?.length > 0
+  let linksMessage = ''
 
   if (hasTxUrl && hasOpenSeaUrl) {
-    return `You can also see it on a block explorer like ${params.txUrl} or even OpenSea ${params.openSeaUrl}`
+    linksMessage = `You can also see it on a block explorer like ${params.txUrl} or even OpenSea ${params.openSeaUrl}.`
   } else if (hasTxUrl) {
-    return `You can also see it on a block explorer like ${params.txUrl}`
+    linksMessage = `You can also see it on a block explorer ${params.txUrl}.`
   } else if (hasOpenSeaUrl) {
-    return `You can also see it on OpenSea ${params.openSeaUrl}`
+    linksMessage = `You can also see it on OpenSea ${params.openSeaUrl}.`
   }
+  if (linksMessage?.length) {
+    return `\n${linksMessage}\n`
+  }
+  return linksMessage
 }
 
 export default {
@@ -16,14 +21,14 @@ export default {
   text: (params) =>
     `Hello!
 
-A new NFT key (#${params.keyId}) to the lock "${params.lockName}" was just mined for you!
+A new NFT key (#${params.keyId}) to the lock "${
+      params.lockName
+    }" was just mined for you!
 It has been added to your Unlock Keychain, where you can view it and, if needed, print it as a signed QR Code!
 
 Check out your keychain: ${params.keychainUrl}
 Make sure you select the network NETWORK where the the NFT has been minted for you.
-
-${links}
-
+${links(params)}
 If you have any questions (or if you do not want to receive emails like this one in the future), please email us at hello@unlock-protocol.com.
 
 The Unlock team
