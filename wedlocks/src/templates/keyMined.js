cda83353baf334e57f68c3af60c2b5181454312b
Kalidou Diagne <kld.diagne@gmail.com>
2022-07-07 19:10:55 +0200

    keyMined

--- wedlocks/src/templates/keyMined.js
+++ wedlocks/src/templates/keyMined.js
@@ -27,7 +27,9 @@ A new NFT key (#${params.keyId}) to the lock "${
 It has been added to your Unlock Keychain, where you can view it and, if needed, print it as a signed QR Code!
 
 Check out your keychain: ${params.keychainUrl}
-Make sure you select the network ${params.network} where the NFT has been minted for you.
+Make sure you select the network ${
+      params.network
+    } where the NFT has been minted for you.
 ${links(params)}
 If you have any questions (or if you do not want to receive emails like this one in the future), please email us at hello@unlock-protocol.com.
 
