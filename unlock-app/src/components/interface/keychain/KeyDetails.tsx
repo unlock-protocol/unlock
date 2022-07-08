0506922435642c21c0481bafcca7231e4bd7120e
Kalidou Diagne <kld.diagne@gmail.com>
2022-07-08 15:25:14 +0200

    keys grouped by network

--- unlock-app/src/components/interface/keychain/KeyDetails.tsx
+++ unlock-app/src/components/interface/keychain/KeyDetails.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import React, { useContext, useMemo } from 'react'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
 import { AuthenticationContext } from '../../../contexts/AuthenticationContext'
 import keyHolderQuery from '../../../queries/keyHolder'
 import 'cross-fetch/polyfill'
@@ -8,6 +8,46 @@ import Loading from '../Loading'
 import { OwnedKey } from './KeychainTypes'
 import Key from './Key'
 import LoginPrompt from '../LoginPrompt'
+import networks from '@unlock-protocol/networks'
+import ApolloClient from 'apollo-boost'
+
+interface KeysByNetworkProps {
+  account: string
+  network: number
+  networkName: string
+  subgraphURI?: string
+}
+export const KeysByNetwork = ({
+  account,
+  networkName,
+  network,
+}: KeysByNetworkProps) => {
+  const { loading, error, data } = useQuery(keyHolderQuery(), {
+    variables: { address: account },
+  })
+
+  const [keyHolders] = data?.keyHolders ?? []
+  const { keys } = keyHolders ?? []
+  const hasKeys = keys?.length == 0
+
+  if (hasKeys || loading) return null
+
+  return (
+    <div className="flex flex-col mb-[2rem]">
+      <span className="font-semibold">{networkName}</span>
+      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {keys.map((key: OwnedKey) => (
+          <Key
+            key={key.id}
+            ownedKey={key}
+            account={account}
+            network={network}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export const KeyDetails = () => {
   const { account, network } = useContext(AuthenticationContext)
@@ -37,10 +77,39 @@ export const KeyDetails = () => {
   }
 
   return (
-    <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {data.keyHolders[0].keys.map((key: OwnedKey) => (
-        <Key key={key.id} ownedKey={key} account={account} network={network} />
-      ))}
+    <div>
+      <div>
+        {Object.entries(networks).map(
+          ([networkId, { name: networkName, subgraphURI, id }]) => {
+            const apolloClientByNetwork = new ApolloClient({
+              uri: subgraphURI as string,
+            }) as any
+            return (
+              <div key={networkId}>
+                <ApolloProvider client={apolloClientByNetwork}>
+                  <KeysByNetwork
+                    key={networkId}
+                    account={account}
+                    network={id}
+                    subgraphURI={subgraphURI}
+                    networkName={networkName}
+                  />
+                </ApolloProvider>
+              </div>
+            )
+          }
+        )}
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data.keyHolders[0].keys.map((key: OwnedKey) => (
+          <Key
+            key={key.id}
+            ownedKey={key}
+            account={account}
+            network={network}
+          />
+        ))}
+      </div>
     </div>
   )
 }
