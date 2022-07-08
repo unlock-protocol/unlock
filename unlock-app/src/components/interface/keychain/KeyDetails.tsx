6f74066bdea5a8c871b15d501b30e0fb501434fc
Kalidou Diagne <kld.diagne@gmail.com>
2022-07-08 17:29:04 +0200

    feat(unlock-app)- keys grouped by network (#9214)
    * keys grouped by network

--- unlock-app/src/components/interface/keychain/KeyDetails.tsx
+++ unlock-app/src/components/interface/keychain/KeyDetails.tsx
@@ -1,25 +1,50 @@
 import React, { useContext } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
 import { AuthenticationContext } from '../../../contexts/AuthenticationContext'
 import keyHolderQuery from '../../../queries/keyHolder'
 import 'cross-fetch/polyfill'
 import { DefaultError } from '../../creator/FatalError'
-import Loading from '../Loading'
 import { OwnedKey } from './KeychainTypes'
 import Key from './Key'
 import LoginPrompt from '../LoginPrompt'
+import networks from '@unlock-protocol/networks'
+import ApolloClient from 'apollo-boost'
+import { NetworkConfig } from '@unlock-protocol/types'
 
-export const KeyDetails = () => {
-  const { account, network } = useContext(AuthenticationContext)
+interface KeysByNetworkProps {
+  account: string
+  network: NetworkConfig
+}
+export const KeysByNetwork = ({ account, network }: KeysByNetworkProps) => {
   const { loading, error, data } = useQuery(keyHolderQuery(), {
     variables: { address: account },
   })
 
-  if (!account || !network) {
-    return <LoginPrompt />
-  }
+  const { name, id } = network
 
-  if (loading) return <Loading />
+  const [keyHolders] = data?.keyHolders ?? []
+  const { keys } = keyHolders ?? []
+  const hasKeys = keys?.length == 0
+
+  if (loading) {
+    return (
+      <div className="flex flex-col mb-3">
+        <div className="h-[1.2rem] w-[17rem] bg-slate-200 mb-2"></div>
+        <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+          <div className="h-[250px] rounded-xl bg-slate-200 animate-pulse"></div>
+        </div>
+      </div>
+    )
+  }
+  if (hasKeys || loading) return null
   if (error) {
     return (
       <DefaultError
@@ -32,15 +57,52 @@ export const KeyDetails = () => {
     )
   }
 
-  if (data.keyHolders.length == 0) {
-    return <NoKeys />
+  return (
+    <div className="flex flex-col mb-[2rem]">
+      <div className="flex flex-col">
+        <small className="font-semibold uppercase text-gray-300 text-[10px] tracking-[.4px] mb-[.5px]">
+          network
+        </small>
+        <span className="font-semibold text-lg mb-3">{name}</span>
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {keys.map((key: OwnedKey) => (
+          <Key key={key.id} ownedKey={key} account={account} network={id} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const KeyDetails = () => {
+  const { account, network } = useContext(AuthenticationContext)
+
+  if (!account || !network) {
+    return <LoginPrompt />
   }
 
   return (
-    <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {data.keyHolders[0].keys.map((key: OwnedKey) => (
-        <Key key={key.id} ownedKey={key} account={account} network={network} />
-      ))}
+    <div>
+      <div>
+        {Object.entries(networks).map(([networkId, networkObj]) => {
+          const subgraphURI = networkObj.subgraphURI
+          const apolloClientByNetwork = new ApolloClient({
+            uri: subgraphURI!,
+          }) as any
+
+          return (
+            <div key={networkId}>
+              <ApolloProvider client={apolloClientByNetwork}>
+                <KeysByNetwork
+                  key={networkId}
+                  network={networkObj}
+                  account={account}
+                />
+              </ApolloProvider>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
