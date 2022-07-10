93e8f3427abff2aee9db3b0d03bcfa15b96ae411
searchableguy <me@searchableguy.com>
2022-07-10 05:34:55 +0530

    Add authentication back

--- unlock-app/src/components/content/VerificationContent.tsx
+++ unlock-app/src/components/content/VerificationContent.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '~/contexts/AuthenticationContext'
 import { getMembershipVerificationConfig } from '~/utils/verification'
+import { useStorageService } from '~/utils/withStorageService'
+import { useWalletService } from '~/utils/withWalletService'
 import { pageTitle } from '../../constants'
 import LocksContext from '../../contexts/LocksContext'
+import { ToastHelper } from '../helpers/toast.helper'
 import Account from '../interface/Account'
 import Layout from '../interface/Layout'
 import Loading from '../interface/Loading'
@@ -12,6 +16,27 @@ import VerificationStatus from '../interface/VerificationStatus'
 export const VerificationContent: React.FC<unknown> = () => {
   const { query } = useRouter()
   const [locks, setLocks] = useState({})
+  const storageService = useStorageService()
+  const walletService = useWalletService()
+  const { account, network } = useAuth()
+
+  useEffect(() => {
+    const login = async () => {
+      if (account && network && walletService) {
+        const promise = storageService.loginPrompt({
+          walletService,
+          address: account,
+          chainId: network,
+        })
+        await ToastHelper.promise(promise, {
+          error: 'Failed to login',
+          success: 'Successfully logged in',
+          loading: 'Please sign message from your wallet to login.',
+        })
+      }
+    }
+    login()
+  }, [storageService, walletService, account, network])
 
   const membershipVerificationConfig = getMembershipVerificationConfig({
     data: query.data?.toString(),
