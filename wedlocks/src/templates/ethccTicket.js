c37087038c03995dd5732b541e5b0ae764802cc0
Julien Genestoux <julien.genestoux@gmail.com>
2022-07-12 16:20:43 +0100

    adding template for ethcc email

---
+++ wedlocks/src/templates/ethccTicket.js
@@ -0,0 +1,26 @@
+export default {
+  subject: () => 'Your EthCC Ticket 🎉',
+  html: (params) =>
+    `<p>Dear EthCC attendee, </p>
+    <p>EthCC is next week 👾 and we are getting excited! </p>
+    <p><strong>Please find attached to this email a QR code that you will use as your ticket.</strong></p>
+    <p><strong>TL;DR</strong>:</p>
+    <ul>
+    <li>don&#39;t share the QR code with anyone</li>
+    <li>bring the QR code next week so it can be scanned!</li>
+    </ul>
+    <p><strong>But hey! Wasn't the EthCC ticket an NFT?</strong></p>
+    <p>This QR code is in fact, your signed NFT. It was generated for you and you will need to bring it next week. Each QR code is unique.</p>
+    <p>It is linked to the NFT you have received on the address you provided and that you can find through this link: <a href="https://app.unlock-protocol.com/keychain">https://app.unlock-protocol.com/keychain</a> on Polygon.</p>
+    <p><strong>But hey! I didn't provide an eth address!</strong></p>
+    <p>If you did not provide an eth address, don't sweat it. Your QR code is still valid and you will be able to get in the Venue by showing it. </p>
+    <p><strong>How does it work when I get to EthCC?</strong></p>
+    <p>Next week, as you arrive to the EthCC venue, please bring your QR code and your ID. The EthCC team will scan your QR code and it will immediately redeem your NFT (it can only be scanned once!). You will receive a wristband in exchange that you can use to enter the venue the next days. This wristband is the only way to get in the venue once you have scanned your QR code. Keep it safely on your wrist. </p>
+    <p><strong>Sharing is loving, no?</strong></p>
+    <p>🚨 NO 🚨</p>
+    <p>If you share your QR code with someone, and this person uses it to enter - then your ticket is gone. Adios, Farewell. Don’t share it. Dont spread it. </p>
+    <p>See you next week!</p>
+    <p>--
+    Unlock for the EthCC team</p>
+  `,
+}
