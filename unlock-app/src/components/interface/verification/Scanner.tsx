b9b5c863ad71f7fcf152b136930c0f044648655b
searchableguy <me@searchableguy.com>
2022-07-13 00:52:44 +0530

    feat(unlock-app): Use different QR-code scanning library

--- unlock-app/src/components/interface/verification/Scanner.tsx
+++ unlock-app/src/components/interface/verification/Scanner.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useRef, useState, useEffect } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { Transition, Dialog } from '@headlessui/react'
 import {
   getMembershipVerificationConfig,
@@ -6,7 +6,6 @@ import {
 } from '~/utils/verification'
 import VerificationStatus from '../VerificationStatus'
 import QrScanner from 'qr-scanner'
-import { ToastHelper } from '~/components/helpers/toast.helper'
 
 interface QRCodeScannerProps {
   onResult: (content: string) => void | Promise<void>
@@ -68,7 +67,6 @@ export function Scanner() {
         if (!config) {
           return
         }
-        ToastHelper.success(config.sig)
         setMembershipVerificationConfig(config)
         qrScanner.stop()
       },
@@ -85,46 +83,43 @@ export function Scanner() {
 
   return (
     <>
-      {!membershipVerificationConfig ? (
-        <div className="grid justify-center">
-          <div className="text-center p-6">
-            <h3 className="font-bold text-lg">
-              Scan the QR code to check in Ticket
-            </h3>
-          </div>
-          <video className="rounded-xl" muted id="scanner" />
+      <div className="grid justify-center">
+        <div className="text-center p-6">
+          <h3 className="font-bold text-lg">
+            Scan the QR code to check in Ticket
+          </h3>
         </div>
-      ) : (
-        <Transition show appear as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-50"
-            onClose={() => {
-              setMembershipVerificationConfig(null)
-            }}
-            open
+        <video className="rounded-xl" muted id="scanner" />
+      </div>
+      <Transition show={!!membershipVerificationConfig} appear as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-50"
+          onClose={() => {
+            setMembershipVerificationConfig(null)
+          }}
+          open={!!membershipVerificationConfig}
+        >
+          <div className="fixed inset-0 backdrop-filter backdrop-blur-sm bg-zinc-500 bg-opacity-25" />
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-out duration-300"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0 translate-y-1"
           >
-            <div className="fixed inset-0 backdrop-filter backdrop-blur-sm bg-zinc-500 bg-opacity-25" />
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-out duration-300"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <div className="fixed p-6 inset-0 overflow-y-auto">
-                <div className="flex min-h-full items-center justify-center text-center">
-                  <Dialog.Panel className="max-w-sm w-full">
-                    <VerificationStatus config={membershipVerificationConfig} />
-                  </Dialog.Panel>
-                </div>
+            <div className="fixed p-6 inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center text-center">
+                <Dialog.Panel className="max-w-sm w-full">
+                  <VerificationStatus config={membershipVerificationConfig!} />
+                </Dialog.Panel>
               </div>
-            </Transition.Child>
-          </Dialog>
-        </Transition>
-      )}
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
     </>
   )
 }
