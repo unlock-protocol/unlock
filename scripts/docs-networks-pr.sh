8d3c9dd75e79b684d50ec0c1dfaebeebef172835
Clément Renaud <clement@unlock-protocol.com>
2022-07-18 11:21:41 +0200

    doc(networks): scripts to generate the docs for the networks package (#9231)
    * add script to build doc
    
    * add eslint
    
    * add a script to generate the networks doc
    
    * fix help doc in script
    
    * add back eslintrc in contracts package
    
    * delete unwanted git ref
    
    * no need to clean up mktmp
    
    * fix URL syntax
    
    * add goerli description
    
    * add network description
    
    * bump version
    
    * git smaller depth is faster

---
+++ scripts/docs-networks-pr.sh
@@ -0,0 +1,57 @@
+#! /bin/sh
+
+# ==============================================================================
+# This script is used to generate the documentation for the networks.
+# It will : 1) generate a list of networks from the existing package in this repo 
+# and 2) opens a PR in the docs repo
+#
+# Usage:
+# scripts/docs-networks-pr.sh
+#
+# ==============================================================================
+
+# use tmp dir
+here=$(pwd)
+tmpdir=$(mktemp -d)
+dest="docs/core-protocol/unlock/networks.md"
+base="master"
+repo=git@github.com:unlock-protocol/docs.git
+
+# versioning
+version_number=$(yarn workspace @unlock-protocol/networks version:show)
+branch=networks-$version_number
+echo $branch
+
+# cleanup
+cd $tmpdir
+
+# git worflow
+git clone --depth=1 $repo
+cd docs
+
+# check if a branch already exists
+if $(git ls-remote --heads ${repo} ${branch} | grep ${branch} >/dev/null); then 
+echo "Branch ${branch} already exists, pushing to existing branch"; 
+git fetch origin $branch
+git checkout $branch
+else
+git checkout $base
+git checkout -b $branch
+fi
+
+# generate new docs files
+cd $here
+sh -c "yarn workspace @unlock-protocol/networks doc" > $tmpdir/docs/$dest
+
+# # commit changes
+cd $tmpdir/docs
+message="Networks info generated from @unlock-protocol/networks@${version_number}"
+git add .
+git commit -a -m"$message"
+git push origin $branch
+
+# create PR on github
+gh pr create --head "$branch" \
+  --base "$base"  \
+  --title "$message" \
+  --body "This PR adds the latest info about networks - generated from the package."
