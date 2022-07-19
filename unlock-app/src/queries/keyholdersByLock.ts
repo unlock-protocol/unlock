91b786d5bb856a5ace73df0d467d6609a5df8b41
Kalidou Diagne <kld.diagne@gmail.com>
2022-07-19 15:28:25 +0200

    add keyListByLock file

--- unlock-app/src/queries/keyholdersByLock.ts
+++ unlock-app/src/queries/keyholdersByLock.ts
@@ -1,33 +1,6 @@
 import { DocumentNode, gql } from 'apollo-boost'
 import { MemberFilter } from '~/unlockTypes'
-
-const AllKeys = gql`
-  query Lock(
-    $addresses: [String!]
-    $first: Int! = 100
-    $skip: Int! = 0
-    $owner: String = ""
-  ) {
-    locks(where: { address_in: $addresses }) {
-      keys(
-        where: { expiration_gt: 0, owner_contains: $owner }
-        first: $first
-        skip: $skip
-        orderBy: keyId
-        orderDirection: asc
-      ) {
-        owner {
-          address
-        }
-        keyId
-        expiration
-      }
-      name
-      address
-      owner
-    }
-  }
-`
+import { keyListByLock } from './keyListByLock'
 
 const ActiveKeys = gql`
   query Lock(
@@ -90,7 +63,7 @@ const ExpiredKeys = gql`
 const QUERY_BY_TYPE: { [key in MemberFilter]: DocumentNode } = {
   active: ActiveKeys,
   expired: ExpiredKeys,
-  all: AllKeys,
+  all: keyListByLock,
 }
 
 export default function keyholdersByLockQuery(type: MemberFilter) {
