a732101a1d6db3b4d8ed256ac4abb974cb179afd
searchableguy <me@searchableguy.com>
2022-07-22 00:09:22 +0530

    feat(paywall): Add support for loading alpha flagged checkout

--- packages/paywall/src/urls.ts
+++ packages/paywall/src/urls.ts
@@ -4,15 +4,30 @@ let _locksmithUri: string
 const baseUrl =
   document?.currentScript?.getAttribute('src') || 'paywall.unlock-protocol.com' // assume prod
 
+const endpoint = new URL(baseUrl)
+const alpha = endpoint.searchParams.get('alpha')
+
 if (baseUrl.match('staging-paywall.unlock-protocol.com')) {
-  _unlockAppUrl = 'https://staging-app.unlock-protocol.com'
+  if (alpha) {
+    _unlockAppUrl = 'https://staging-app.unlock-protocol.com/alpha'
+  } else {
+    _unlockAppUrl = 'https://staging-app.unlock-protocol.com'
+  }
   _locksmithUri = 'https://staging-locksmith.unlock-protocol.com'
 } else if (baseUrl.match('paywall.unlock-protocol.com')) {
-  _unlockAppUrl = 'https://app.unlock-protocol.com'
+  if (alpha) {
+    _unlockAppUrl = 'https://app.unlock-protocol.com/alpha'
+  } else {
+    _unlockAppUrl = 'https://app.unlock-protocol.com'
+  }
   _locksmithUri = 'https://locksmith.unlock-protocol.com'
 } else {
-  _unlockAppUrl = 'http://0.0.0.0:3000'
-  _locksmithUri = 'http://0.0.0.0:8080'
+  if (alpha) {
+    _unlockAppUrl = 'http://localhost:3000/alpha'
+  } else {
+    _unlockAppUrl = 'http://localhost:3000'
+  }
+  _locksmithUri = 'http://localhost:8080'
 }
 export const unlockAppUrl: string = _unlockAppUrl
 export const locksmithUri: string = _locksmithUri
