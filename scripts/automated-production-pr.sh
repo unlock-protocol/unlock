a5da204a57ad71881051e3dfcf94fe0f706464f1
Julien Genestoux <julien.genestoux@gmail.com>
2022-07-25 20:43:41 +0200

    chore(ci): using github action to run automated deploys (#9298)
    * wip
    
    * Update scripts/automated-production-pr.sh
    
    * Update scripts/automated-production-pr.sh
    
    Co-authored-by: Unlock Deployer <ops@unlock-protocol.com>

--- scripts/automated-production-pr.sh
+++ scripts/automated-production-pr.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# This script gets the git history as by default CircleCI will only get the most recent commit.
-echo "Fetching git history"
-git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git fetch >> /dev/null
-
 # get the timestamp of the latests commit to be deployed
 COMMIT_TO_DEPLOY=`git rev-list -1 --before={2.days.ago} master`
 COMMIT_TO_DEPLOY_TIMESTAMP=`git show -s --format=%ct $COMMIT_TO_DEPLOY`
@@ -38,42 +33,21 @@ then
   exit 0
 fi
 
-
-echo "Installing github hub"
-cd ..
-wget https://github.com/github/hub/releases/download/v2.11.2/hub-linux-amd64-2.11.2.tgz
-tar -xvf hub-linux-amd64-2.11.2.tgz
-cd project
-
-
-echo "Setting up git"
-git config --global user.email "ops@unlock-protocol.com"
-git config --global user.name "Unlock Deployer"
-
-echo "Checking master out 2 days ago into new branch"
-BRANCH="production-$(date +%Y%m%d-%H%M%S)"
-git checkout -b $BRANCH $COMMIT_TO_DEPLOY
-
-echo "Diffing versus latest production"
-LATEST_PRODUCTION=`git rev-parse origin/production`
-git reset --soft $LATEST_PRODUCTION
-
-echo "Committing diff"
 COMMIT_MESSAGE="Automated deploy as of commit:$COMMIT_TO_DEPLOY"
-git commit -m "$COMMIT_MESSAGE" -a --no-verify
 
-echo "Push new production branch"
-git push origin $BRANCH --no-verify
-
-echo "Open pull request"
-PROD_DEPLOY_TYPE="automated"
-source "${BASH_SOURCE%/*}/production-pull-request-template.sh"
+BRANCH="production-$(date +%Y%m%d-%H%M%S)"
 
 DOY=$(date +%j)
 declare -a REVIEWERS
 REVIEWERS=( julien51 clemsos searchableguy kalidiagne )
 REVIEWER_INDEX=$(($DOY % ${#REVIEWERS[@]}))
+REVIEWER=${REVIEWERS[$REVIEWER_INDEX]}
 
-GITHUB_TOKEN=$GITHUB_API_TOKEN ./../hub-linux-amd64-2.11.2/bin/hub pull-request -b production -h $BRANCH --message "$MESSAGE" -r ${REVIEWERS[$REVIEWER_INDEX]}
+echo '["'$COMMIT_MESSAGE'","'$BRANCH'","'$REVIEWER'"]'
 
-git checkout master
+# Checking master out at COMMIT_TO_DEPLOY
+git checkout -b $BRANCH $COMMIT_TO_DEPLOY
+
+# Diffing versus latest production
+LATEST_PRODUCTION=`git rev-parse origin/production`
+git reset --soft $LATEST_PRODUCTION
