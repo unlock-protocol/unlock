a7f6a73831b7d7d38dfdc4720b8e34451300bb0c
Clément Renaud <clement@unlock-protocol.com>
2022-07-25 14:39:01 +0200

    feat(smart-contracts): deploy new lock template and submit txs  (#9300)

---
+++ smart-contracts/scripts/upgrade/submitLockVersion.js
@@ -0,0 +1,110 @@
+/**
+ * Deploy latest PublicLock template and send the txs to the multisig to add/set
+ * it to the Unlock contract.
+ *
+ * ```
+ * # If no existing address is specified, it will deploy the latest public lock.
+ * yarn hardhat submit:version
+ *
+ * # with an existing address, contract gets deployed and specified
+ * yarn hardhat submit:version --network rinkeby --public-lock-address <TEMPLATE_ADDRESS>
+ * ```
+ * NB: You can first test on mainnet to make sure the template is deploying correctly * :
+ *
+ * ```
+ * RUN_MAINNET_FORK=1 yarn hardhat run scripts/upgrade/submitLockVersion.js
+ * ```
+ */
+const { ethers } = require('hardhat')
+const { networks } = require('@unlock-protocol/networks')
+const submitTx = require('../multisig/submitTx')
+const deployTemplate = require('../deployments/template')
+
+const {
+  addSomeETH,
+  confirmMultisigTx,
+  getMultisigSigner,
+  deployLock,
+} = require('../../test/helpers')
+
+async function main({ publicLockAddress }) {
+  // make sure we get the correct chain id on local mainnet fork
+  const { chainId } = process.env.RUN_MAINNET_FORK
+    ? { chainId: '1' }
+    : await ethers.provider.getNetwork()
+
+  const { unlockAddress, multisig } = networks[chainId]
+
+  // if not address is specified, deploy the latest public lock
+  if (!publicLockAddress) {
+    publicLockAddress = await deployTemplate({})
+  }
+
+  // get multisig signer
+  const [signer] = process.env.RUN_MAINNET_FORK
+    ? [await getMultisigSigner(multisig)]
+    : await ethers.getSigners()
+
+  if (process.env.RUN_MAINNET_FORK) {
+    // some ETH for the issuer
+    await addSomeETH(signer.address)
+  }
+
+  const publicLock = await ethers.getContractAt('PublicLock', publicLockAddress)
+  const version = await publicLock.publicLockVersion()
+
+  console.log(`Setting PublicLock v${version} on network ${chainId}`)
+  console.log(`Unlock: ${unlockAddress} - PublicLock ${publicLock.address}`)
+  console.log(`Multisig: ${multisig} - Signer: ${signer.address}`)
+
+  const submit = async (functionName, functionArgs) => {
+    const tx1 = {
+      contractName: 'Unlock',
+      contractAddress: unlockAddress,
+      functionName,
+      functionArgs,
+    }
+    console.log(`submitting ${functionName} to multisig...`)
+    console.log(functionArgs)
+
+    const txId1 = await submitTx({
+      safeAddress: multisig,
+      tx: tx1,
+      signer,
+    })
+
+    return txId1
+  }
+
+  // send txs to multisig
+  const txId1 = await submit('addLockTemplate', [publicLock.address, version])
+  const txId2 = await submit('setLockTemplate', [publicLock.address])
+
+  // check if multisig txs are valid
+  if (process.env.RUN_MAINNET_FORK) {
+    console.log(`Signing multisigs: ${txId1} ${txId2}`)
+    await confirmMultisigTx({
+      transactionId: txId1,
+      multisigAddress: multisig,
+    })
+    await confirmMultisigTx({
+      transactionId: txId2,
+      multisigAddress: multisig,
+    })
+    const unlock = await ethers.getContractAt('Unlock', unlockAddress)
+    const lock = await deployLock({ unlock })
+    console.log(`Lock ${await lock.name()} deployed at ${lock.address}.`)
+  }
+}
+
+// execute as standalone
+if (require.main === module) {
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+module.exports = main
--- smart-contracts/test/helpers/multisig.js
+++ smart-contracts/test/helpers/multisig.js
@@ -1,4 +1,4 @@
-const { ethers } = require('hardhat')
+const { ethers, network } = require('hardhat')
 const { impersonate } = require('./mainnet')
 
 const multisigABI = require('./ABIs/multisig.json')
@@ -6,11 +6,11 @@ const UNLOCK_MULTISIG_ADDRESS = '0xa39b44c4AFfbb56b76a1BF1d19Eb93a5DfC2EBA9'
 const MULTISIG_ADDRESS_OWNER = '0xF5C28ce24Acf47849988f147d5C75787c0103534'
 
 // test helper to reach concensus on multisig
-const confirmMultisigTx = async ({ transactionId }) => {
-  const multisig = await ethers.getContractAt(
-    multisigABI,
-    UNLOCK_MULTISIG_ADDRESS
-  )
+const confirmMultisigTx = async ({
+  transactionId,
+  multisigAddress = UNLOCK_MULTISIG_ADDRESS,
+}) => {
+  const multisig = await ethers.getContractAt(multisigABI, multisigAddress)
   const signers = await multisig.getOwners()
   const txs = await Promise.all(
     signers.slice(1, 4).map(async (signerAddress) => {
@@ -42,8 +42,22 @@ const confirmMultisigTx = async ({ transactionId }) => {
   }
 }
 
+const getMultisigSigner = async (multisigAddress = UNLOCK_MULTISIG_ADDRESS) => {
+  // update contract implementation address in proxy admin using multisig
+  const multisig = await ethers.getContractAt(multisigABI, multisigAddress)
+
+  const signers = await multisig.getOwners()
+  await network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [signers[0]],
+  })
+  const issuer = await ethers.getSigner(signers[0])
+  return issuer
+}
+
 module.exports = {
   confirmMultisigTx,
+  getMultisigSigner,
   UNLOCK_MULTISIG_ADDRESS,
   MULTISIG_ADDRESS_OWNER,
 }
