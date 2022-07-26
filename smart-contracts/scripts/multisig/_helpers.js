18b5aa46d13d2b45db4f82ec68f8f46b6648ec7c
Clément Renaud <clement+git@clementrenaud.com>
2022-07-26 13:57:22 +0200

    gnosis task to detect safe version

--- smart-contracts/scripts/multisig/_helpers.js
+++ smart-contracts/scripts/multisig/_helpers.js
@@ -1,6 +1,7 @@
 const { ethers } = require('hardhat')
 const { networks } = require('@unlock-protocol/networks')
-const multisigABI = require('../../test/helpers/ABIs/multisig.json')
+const multisigOldABI = require('../../test/helpers/ABIs/multisig.json')
+const multisigABI = require('../../test/helpers/ABIs/multisig-1.3.0.json')
 
 // get the correct provider if chainId is specified
 const getProvider = async (chainId) => {
@@ -21,12 +22,39 @@ const getSafeAddress = async (chainId) => {
   return multisig
 }
 
+const getSafeVersion = async (safeAddress) => {
+  const abi = [
+    {
+      inputs: [],
+      name: 'VERSION',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ]
+  const safe = await ethers.getContractAt(abi, safeAddress)
+  try {
+    const version = await safe.VERSION()
+    return version
+  } catch (error) {
+    return 'old'
+  }
+}
+
 const getSafe = async ({ safeAddress, signer }) => {
   const { chainId } = await signer.provider.getNetwork()
   if (!safeAddress) {
     safeAddress = getSafeAddress(chainId)
   }
-  const safe = new ethers.Contract(safeAddress, multisigABI, signer)
+  const version = await getSafeVersion(safeAddress)
+  const abi = version !== 'old' ? multisigABI : multisigOldABI
+  const safe = new ethers.Contract(safeAddress, abi, signer)
   return safe
 }
 
@@ -34,4 +62,5 @@ module.exports = {
   getProvider,
   getSafe,
   getSafeAddress,
+  getSafeVersion,
 }
