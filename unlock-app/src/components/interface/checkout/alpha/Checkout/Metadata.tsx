4e9bf83ab3b83f19ddefa39782ff528e9ad76152
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-07-26 23:33:19 +0530

    feat(unlock-app): Add a top navigational back button and refactor shell components (#9321)

--- unlock-app/src/components/interface/checkout/alpha/Checkout/Metadata.tsx
+++ unlock-app/src/components/interface/checkout/alpha/Checkout/Metadata.tsx
@@ -10,7 +10,12 @@ import { formResultToMetadata } from '~/utils/userMetadata'
 import { useStorageService } from '~/utils/withStorageService'
 import { ToastHelper } from '~/components/helpers/toast.helper'
 import { useActor } from '@xstate/react'
-import { Shell } from '../Shell'
+import {
+  BackButton,
+  CheckoutHead,
+  CheckoutTransition,
+  CloseButton,
+} from '../Shell'
 import { PoweredByUnlock } from '../PoweredByUnlock'
 import { useCheckoutHeadContent } from '../useCheckoutHeadContent'
 import { IconButton, ProgressCircleIcon, ProgressFinishIcon } from '../Progress'
@@ -48,11 +53,12 @@ export function Metadata({
     register,
     control,
     handleSubmit,
-    formState: { errors, isValidating, isSubmitting },
+    formState: { errors, isSubmitting },
   } = useForm({
     shouldUnregister: false,
     shouldFocusError: true,
     mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
   })
   const { fields, append, remove } = useFieldArray({
     name: 'metadata',
@@ -116,124 +122,135 @@ export function Metadata({
       }
     }
   }
-  const isLoading = isValidating || isSubmitting
+  const isLoading = isSubmitting
+
   return (
-    <Shell.Root onClose={() => onClose()}>
-      <Shell.Head
-        title={paywallConfig.title}
-        iconURL={iconURL}
-        description={description}
-      />
-      <div className="flex px-6 mt-6 flex-wrap items-center w-full gap-2">
-        <div className="flex items-center gap-2 col-span-4">
-          <div className="flex items-center gap-0.5">
-            <IconButton
-              title="Select lock"
-              icon={ProgressCircleIcon}
-              onClick={() => {
-                send('SELECT')
-              }}
-            />
-            <IconButton
-              title="Choose quantity"
-              icon={ProgressCircleIcon}
-              onClick={() => {
-                send('QUANTITY')
-              }}
-            />
-            <ProgressCircleIcon />
-          </div>
-          <h4 className="text-sm "> {title}</h4>
+    <CheckoutTransition>
+      <div className="bg-white max-w-md rounded-xl flex flex-col w-full h-[90vh] sm:h-[80vh] max-h-[42rem]">
+        <div className="flex items-center justify-between p-6">
+          <BackButton onClick={() => send('BACK')} />
+          <CloseButton onClick={() => onClose()} />
         </div>
-        <div className="border-t-4 w-full flex-1"></div>
-        <div className="inline-flex items-center gap-1">
-          {paywallConfig.messageToSign && <ProgressCircleIcon disabled />}
-          <ProgressCircleIcon disabled />
-          <ProgressFinishIcon disabled />
-        </div>
-      </div>
-      <main className="p-6 overflow-auto h-64 sm:h-72">
-        <form id="metadata" onSubmit={handleSubmit(onSubmit)}>
-          {fields.map((item, index) => (
-            <div
-              key={item.id}
-              className={twMerge(
-                'py-2',
-                fields.length > index + 1
-                  ? 'border-b-2 border-brand-gray'
-                  : null
-              )}
-            >
-              <Input
-                label={`Recipient #${index + 1}`}
-                size="small"
-                error={
-                  errors?.metadata?.[index]?.recipient?.message as any as string
-                }
-                {...register(`metadata.${index}.recipient`, {
-                  required: 'Recipient is required',
-                  validate: {
-                    max_keys: async (value) => {
-                      try {
-                        const address = await getAddressForName(value)
-                        const contract = await web3Service.lockContract(
-                          lock!.address,
-                          lock!.network
-                        )
-                        const items = await contract.balanceOf(address)
-                        const numberOfMemberships =
-                          ethers.BigNumber.from(items).toNumber()
-                        return numberOfMemberships <
-                          (lock?.maxKeysPerAddress || 1)
-                          ? true
-                          : 'Address already holds the maximum number of memberships.'
-                      } catch (error) {
-                        console.error(error)
-                        return 'There is a problem with using this address. Try another.'
-                      }
-                    },
-                  },
-                })}
+        <CheckoutHead
+          title={paywallConfig.title}
+          iconURL={iconURL}
+          description={description}
+        />
+        <div className="flex px-6 p-2 flex-wrap items-center w-full gap-2">
+          <div className="flex items-center gap-2 col-span-4">
+            <div className="flex items-center gap-0.5">
+              <IconButton
+                title="Select lock"
+                icon={ProgressCircleIcon}
+                onClick={() => {
+                  send('SELECT')
+                }}
               />
-              {metadataInputs?.map((metadataInputItem) => (
+              <IconButton
+                title="Choose quantity"
+                icon={ProgressCircleIcon}
+                onClick={() => {
+                  send('QUANTITY')
+                }}
+              />
+              <ProgressCircleIcon />
+            </div>
+            <h4 className="text-sm "> {title}</h4>
+          </div>
+          <div className="border-t-4 w-full flex-1"></div>
+          <div className="inline-flex items-center gap-1">
+            {paywallConfig.messageToSign && <ProgressCircleIcon disabled />}
+            <ProgressCircleIcon disabled />
+            <ProgressFinishIcon disabled />
+          </div>
+        </div>
+        <main className="px-6 py-2 overflow-auto h-full">
+          <form id="metadata" onSubmit={handleSubmit(onSubmit)}>
+            {fields.map((item, index) => (
+              <div
+                key={item.id}
+                className={twMerge(
+                  'py-2',
+                  fields.length > index + 1
+                    ? 'border-b-2 border-brand-gray'
+                    : null
+                )}
+              >
                 <Input
-                  key={metadataInputItem.name}
-                  label={metadataInputItem.name}
-                  defaultValue={metadataInputItem.defaultValue}
+                  label={`Recipient #${index + 1}`}
                   size="small"
-                  placeholder={metadataInputItem.placeholder}
-                  type={metadataInputItem.type}
                   error={
-                    errors?.metadata?.[index]?.[metadataInputItem.name]
-                      ?.message as any as string
+                    errors?.metadata?.[index]?.recipient
+                      ?.message as unknown as string
                   }
-                  {...register(`metadata.${index}.${metadataInputItem.name}`, {
-                    required:
-                      metadataInputItem.required &&
-                      `${metadataInputItem.name} is required`,
+                  {...register(`metadata.${index}.recipient`, {
+                    required: 'Recipient is required',
+                    validate: {
+                      max_keys: async (value) => {
+                        try {
+                          const address = await getAddressForName(value)
+                          const contract = await web3Service.lockContract(
+                            lock!.address,
+                            lock!.network
+                          )
+                          const items = await contract.balanceOf(address)
+                          const numberOfMemberships =
+                            ethers.BigNumber.from(items).toNumber()
+                          return numberOfMemberships <
+                            (lock?.maxKeysPerAddress || 1)
+                            ? true
+                            : 'Address already holds the maximum number of memberships.'
+                        } catch (error) {
+                          console.error(error)
+                          return 'There is a problem with using this address. Try another.'
+                        }
+                      },
+                    },
                   })}
                 />
-              ))}
-            </div>
-          ))}
-        </form>
-      </main>
-      <footer className="px-6 pt-6 border-t grid items-center">
-        <Connected
-          injectedProvider={injectedProvider}
-          service={checkoutService}
-        >
-          <Button
-            loading={isLoading}
-            disabled={isLoading}
-            className="w-full"
-            form="metadata"
+                {metadataInputs?.map((metadataInputItem) => (
+                  <Input
+                    key={metadataInputItem.name}
+                    label={metadataInputItem.name}
+                    defaultValue={metadataInputItem.defaultValue}
+                    size="small"
+                    placeholder={metadataInputItem.placeholder}
+                    type={metadataInputItem.type}
+                    error={
+                      errors?.metadata?.[index]?.[metadataInputItem.name]
+                        ?.message as unknown as string
+                    }
+                    {...register(
+                      `metadata.${index}.${metadataInputItem.name}`,
+                      {
+                        required:
+                          metadataInputItem.required &&
+                          `${metadataInputItem.name} is required`,
+                      }
+                    )}
+                  />
+                ))}
+              </div>
+            ))}
+          </form>
+        </main>
+        <footer className="px-6 pt-6 border-t grid items-center">
+          <Connected
+            injectedProvider={injectedProvider}
+            service={checkoutService}
           >
-            {isLoading ? 'Continuing' : 'Next'}
-          </Button>
-        </Connected>
-        <PoweredByUnlock />
-      </footer>
-    </Shell.Root>
+            <Button
+              loading={isLoading}
+              disabled={isLoading}
+              className="w-full"
+              form="metadata"
+            >
+              {isLoading ? 'Continuing' : 'Next'}
+            </Button>
+          </Connected>
+          <PoweredByUnlock />
+        </footer>
+      </div>
+    </CheckoutTransition>
   )
 }
