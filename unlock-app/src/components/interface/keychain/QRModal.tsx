7bc76a0f774b04d77469e137f9efba4ebe050f18
Julien Genestoux <julien.genestoux@gmail.com>
2022-07-29 20:55:46 +0200

    chore(unlock-app, locksmith, wedlocks): cleanup ethcc (#9348)
    * cleanup ethcc
    
    * more ethcc cleanup
    
    * more ethcc cleanup
    
    * fixing tests
    
    * typescript fix
    
    * removed unrelated change
    
    Co-authored-by: Unlock Deployer <ops@unlock-protocol.com>

--- unlock-app/src/components/interface/keychain/QRModal.tsx
+++ unlock-app/src/components/interface/keychain/QRModal.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import QRCode from 'qrcode.react'
 import InlineModal from '../InlineModal'
 import { Button, Input, Icon } from '@unlock-protocol/ui'
-import { FaEnvelope, FaMobile } from 'react-icons/fa'
+import { FaEnvelope } from 'react-icons/fa'
 
 interface Props {
   active: boolean
@@ -12,13 +12,7 @@ interface Props {
   lock: any
 }
 
-export const QRModal = ({
-  active,
-  dismiss,
-  sendEmail,
-  signature,
-  lock,
-}: Props) => {
+export const QRModal = ({ active, dismiss, sendEmail, signature }: Props) => {
   const [recipient, setRecipient] = useState('')
   const [isValid, setIsValid] = useState(false)
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,25 +30,6 @@ export const QRModal = ({
     // eslint-disable-next-line no-console
     console.log(url.toString()) // debugging
     return url.toString()
-  }
-  const isEthCC =
-    [
-      '0xd0a031d9f9486b1d914124d0c1fcac2e9e6504fe',
-      '0x072149617e12170696481684598a696e9a4d46ff',
-      '0xf181e18e007517605f369eccf0eee6ebb1b10133',
-      '0x6d8c3d90340fa33693a88d1411b0f32df12d0683',
-      '0xf99eb828ac365c54fcbb6779a78417c25f113829',
-      '0x9ab351cb5dae55abd135dd256726851aae8efeb5',
-      '0x4624bbf6d685b1057eecacc691b0a068e287f0a5',
-      '0x623da3e4d4cb9c98dabb4c23789ed5aaa20ea3aa',
-    ].indexOf(lock.address.toLowerCase()) > -1
-
-  const addToPhone = () => {
-    window.open(
-      `https://ethcc.ethpass.xyz/integrations/ethcc?payload=${encodeURIComponent(
-        QRUrl()
-      )}`
-    )
   }
 
   return (
@@ -77,16 +52,6 @@ export const QRModal = ({
           }
         }}
       />
-      {isEthCC && (
-        <Button
-          className="w-10/12 m-1"
-          iconLeft={<Icon icon={FaMobile} size="medium" key="Smart Phone" />}
-          onClick={addToPhone}
-        >
-          {' '}
-          Add to phone
-        </Button>
-      )}
     </InlineModal>
   )
 }
