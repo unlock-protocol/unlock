0dae00b21c805299075e9c3be6c0d9577e67c9c8
Unlock Deployer <ops@unlock-protocol.com>
2022-07-29 16:56:31 +0200

    cleanup ethcc

--- unlock-app/src/components/interface/keychain/QRModal.tsx
+++ unlock-app/src/components/interface/keychain/QRModal.tsx
@@ -37,25 +37,14 @@ export const QRModal = ({
     console.log(url.toString()) // debugging
     return url.toString()
   }
-  const isEthCC =
-    [
-      '0xd0a031d9f9486b1d914124d0c1fcac2e9e6504fe',
-      '0x072149617e12170696481684598a696e9a4d46ff',
-      '0xf181e18e007517605f369eccf0eee6ebb1b10133',
-      '0x6d8c3d90340fa33693a88d1411b0f32df12d0683',
-      '0xf99eb828ac365c54fcbb6779a78417c25f113829',
-      '0x9ab351cb5dae55abd135dd256726851aae8efeb5',
-      '0x4624bbf6d685b1057eecacc691b0a068e287f0a5',
-      '0x623da3e4d4cb9c98dabb4c23789ed5aaa20ea3aa',
-    ].indexOf(lock.address.toLowerCase()) > -1
 
-  const addToPhone = () => {
-    window.open(
-      `https://ethcc.ethpass.xyz/integrations/ethcc?payload=${encodeURIComponent(
-        QRUrl()
-      )}`
-    )
-  }
+  // const addToPhone = () => {
+  //   window.open(
+  //     `https://ethcc.ethpass.xyz/integrations/ethcc?payload=${encodeURIComponent(
+  //       QRUrl()
+  //     )}`
+  //   )
+  // }
 
   return (
     <InlineModal active={active} dismiss={dismiss}>
@@ -77,16 +66,6 @@ export const QRModal = ({
           }
         }}
       />
-      {isEthCC && (
-        <Button
-          className="w-10/12 m-1"
-          iconLeft={<Icon icon={FaMobile} size="medium" key="Smart Phone" />}
-          onClick={addToPhone}
-        >
-          {' '}
-          Add to phone
-        </Button>
-      )}
     </InlineModal>
   )
 }
