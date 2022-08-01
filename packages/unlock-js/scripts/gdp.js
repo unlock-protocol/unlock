10306c50a84d11b69da4a8ec5adad19e53018a9f
Unlock Deployer <ops@unlock-protocol.com>
2022-08-01 16:31:42 +0200

    adding $ conversion

--- packages/unlock-js/scripts/gdp.js
+++ packages/unlock-js/scripts/gdp.js
@@ -1,16 +1,12 @@
+const fetch = require('node-fetch')
 const ethers = require('ethers')
 const networks = require('@unlock-protocol/networks')
 
-const getGdpForNetwork = async (network) => {
+const getGdpForNetwork = async (provider, network, blockTag) => {
   const abi = ['function grossNetworkProduct() constant view returns (uint256)']
-  const provider = new ethers.providers.JsonRpcProvider(network.provider)
-
-  if (!network.unlockAddress) {
-    return 0
-  }
 
   const contract = new ethers.Contract(network.unlockAddress, abi, provider)
-  let current = await contract.grossNetworkProduct()
+  let current = await contract.grossNetworkProduct({ blockTag })
 
   const getOldNetwork = async (previousDeploys) => {
     let previousDeploy = previousDeploys.pop()
@@ -22,7 +18,7 @@ const getGdpForNetwork = async (network) => {
       abi,
       provider
     )
-    let value = await contract.grossNetworkProduct()
+    let value = await contract.grossNetworkProduct({ blockTag })
     return value.add(await getOldNetwork(previousDeploys))
   }
   if (network.previousDeploys) {
@@ -32,14 +28,61 @@ const getGdpForNetwork = async (network) => {
 
   return current
 }
-const run = () => {
-  Object.keys(networks).forEach(async (id) => {
-    const gdp = await getGdpForNetwork(networks[id])
-    console.log({
-      id,
-      total: ethers.utils.formatUnits(gdp, '18'),
+
+const getLastWeekBlockNumber = async (provider) => {
+  const latestBlockNumber = await provider.getBlockNumber()
+  const latestBlock = await provider.getBlock(latestBlockNumber)
+  const numberOfBlocks = 10000
+  const oldBlock = await provider.getBlock(latestBlockNumber - numberOfBlocks) //10,000 blocks ago!
+  let timePerBlock =
+    (latestBlock.timestamp - oldBlock.timestamp) / numberOfBlocks
+
+  const weekInSeconds = 60 * 60 * 24 * 7
+  return latestBlockNumber - parseInt(weekInSeconds / timePerBlock)
+}
+
+const priceConversion = async (network) => {
+  if (network.isTestNetwork) {
+    return 0
+  }
+  const response = await fetch(
+    `https://api.coinbase.com/v2/exchange-rates?currency=${network.baseCurrencySymbol}`
+  )
+
+  const json = await response.json()
+  return parseFloat(json.data.rates['USD'])
+}
+
+const run = async () => {
+  const values = await Promise.all(
+    Object.keys(networks).map(async (id) => {
+      try {
+        let network = networks[id]
+        if (!network.unlockAddress) {
+          return null
+        }
+        const provider = new ethers.providers.JsonRpcProvider(network.provider)
+        const latestBlockNumber = await provider.getBlockNumber()
+        const gdp = await getGdpForNetwork(provider, network, latestBlockNumber)
+        // TODO: consider retrieving value "last week"...
+        const rate = await priceConversion(network)
+        const total = parseFloat(ethers.utils.formatUnits(gdp, '18'))
+        return {
+          id,
+          total,
+          dollars: total * rate,
+        }
+      } catch (error) {
+        console.error('Error retrieving data for', id)
+        console.error(error)
+        return null
+      }
     })
-  })
+  )
+  const prodNetworks = values.filter((x) => !!x)
+  console.log(prodNetworks)
+  const dollars = prodNetworks.map((n) => n.dollars).reduce((s, a) => s + a, 0)
+  console.log(`$ total: ${dollars}`)
 }
 
 run()
