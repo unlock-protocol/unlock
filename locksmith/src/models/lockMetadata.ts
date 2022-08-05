1b893aaccf7f4b1c0e9849324621d076d59c1104
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-08-05 19:26:44 +0530

    refactor(locksmith): Migrate json columns to jsonb for advance querying (#9416)
    * refactor(locksmith): Migrate json columns to jsonb for advance querying
    
    * remove comment
    
    * change types on model
    
    * remove unwanted test file

--- locksmith/src/models/lockMetadata.ts
+++ locksmith/src/models/lockMetadata.ts
@@ -2,7 +2,7 @@ import { Table, Model, Column, DataType } from 'sequelize-typescript'
 
 @Table({ tableName: 'LockMetadata', timestamps: true })
 export class LockMetadata extends Model<LockMetadata> {
-  @Column(DataType.JSON)
+  @Column(DataType.JSONB)
   data!: JSON
 
   @Column({ primaryKey: true })
