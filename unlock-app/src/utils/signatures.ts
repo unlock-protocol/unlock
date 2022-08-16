848371108b35e8198ebc314da172724536ae57d3
Julien Genestoux <julien.genestoux@gmail.com>
2022-08-16 12:08:15 +0200

    feature(unlock-app): adding hard coded addresses for signers (#9447)
    * adding hard coded addresses for signers
    
    * Apply suggestions from code review
    
    * Update unlock-app/src/utils/signatures.ts
    
    * Update unlock-app/src/utils/signatures.ts

--- unlock-app/src/utils/signatures.ts
+++ unlock-app/src/utils/signatures.ts
@@ -10,13 +10,15 @@ export const isSignatureValidForAddress = (
   sig: string,
   data: string,
   address: string,
-  keyGranter?: string
+  locksmithSigners?: string[]
 ) => {
   try {
     const signer = utils.verifyMessage(data, sig).toLowerCase()
 
-    return (
-      signer === address.toLowerCase() || signer === keyGranter?.toLowerCase()
+    return !!(
+      signer === address.toLowerCase() ||
+      (locksmithSigners &&
+        locksmithSigners?.map((item) => item.toLowerCase()).includes(signer))
     )
   } catch (error) {
     console.error(error)
