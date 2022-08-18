288fc6fef6b21b8dae120161fca492c9aa1c2fae
Julien Genestoux <julien.genestoux@gmail.com>
2022-08-18 22:34:39 +0200

    feature(provider) : Adding a provider that we can share publicly (#9459)
    * adding worker
    
    * not using the networks package anymore
    
    * removed extraneous dep
    
    * extending ts config
    
    * adding more docs

---
+++ provider/src/index.ts
@@ -0,0 +1,38 @@
+export interface Env {}
+
+// This is the list of networks currently supported
+const supportedNetworks = {
+  '1': 'https://eth-mainnet.alchemyapi.io/v2/6idtzGwDtRbzil3s6QbYHr2Q_WBfn100',
+  '4': 'https://eth-rinkeby.alchemyapi.io/v2/n0NXRSZ9olpkJUPDLBC00Es75jaqysyT',
+  '5': 'https://goerli.prylabs.net',
+  '10': 'https://mainnet.optimism.io',
+  '56': 'https://bsc-dataseed.binance.org/',
+  '100':
+    'https://cool-empty-bird.xdai.quiknode.pro/4edba942fb43c718f24480484684e907fe3fe1d3/',
+  '137':
+    'https://snowy-weathered-waterfall.matic.quiknode.pro/5b11a0413a62a295070c0dfb25637d5f8c591aba/',
+  '42220': 'https://forno.celo.org',
+  '80001': 'https://matic-mumbai.chainstacklabs.com',
+}
+
+/**
+ * A proxy worker for JSON RPC endpoints
+ */
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url)
+    const { pathname } = url
+    const [_, networkId] = pathname.match(/\/([0-9]*)/)
+    if (!networkId) {
+      return new Response('Bad Request', { status: 400 })
+    }
+    if (!supportedNetworks[networkId]) {
+      return new Response('Network not supported', { status: 404 })
+    }
+    return fetch(supportedNetworks[networkId], request)
+  },
+}
