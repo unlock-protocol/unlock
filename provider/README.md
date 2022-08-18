140cd89f07ceace5a342e58887f5cbc308547c9e
Julien Genestoux <julien.genestoux@gmail.com>
2022-08-18 15:12:13 +0200

    not using the networks package anymore

--- provider/README.md
+++ provider/README.md
@@ -1,7 +1,6 @@
 # Unlock RPC provider
 
 This provider is a proxy application that connects to all networks supported by Unlock.
-TODO: only support RPC calls to Unlock contracts (or related contracts... such as ERC20 contracts).
 
 Even though this adds centralization (and effectively introduces a middleman), this is solely provided as a developer tool for convenience. Your application should use its own RPC endpoint in production.
 
@@ -9,6 +8,15 @@ Even though this adds centralization (and effectively introduces a middleman), t
 
 You can use the `wrangler` CLI (version 2) from Cloudflare to run locally with `wrangler dev`.
 
+To add support for a new network, just update `src/index.ts`
+
 # Deployment
 
 You can use the `wrangler` CLI (version 2) from Cloudflare to run locally with `wrangler publish`.
+
+## TODO
+
+- Only support RPC calls to Unlock contracts (or related contracts... such as ERC20 contracts).
+- Deploy through Github action
+- Measure all the things
+- Rate limiting
--- provider/src/index.ts
+++ provider/src/index.ts
@@ -1,6 +1,19 @@
-import networks from '@unlock-protocol/networks'
-
 export interface Env {}
+
+// This is the list of networks currently supported
+const supportedNetworks = {
+  '1': 'https://eth-mainnet.alchemyapi.io/v2/6idtzGwDtRbzil3s6QbYHr2Q_WBfn100',
+  '4': 'https://eth-rinkeby.alchemyapi.io/v2/n0NXRSZ9olpkJUPDLBC00Es75jaqysyT',
+  '5': 'https://goerli.prylabs.net',
+  '10': 'https://mainnet.optimism.io',
+  '56': 'https://bsc-dataseed.binance.org/',
+  '100':
+    'https://cool-empty-bird.xdai.quiknode.pro/4edba942fb43c718f24480484684e907fe3fe1d3/',
+  '137':
+    'https://snowy-weathered-waterfall.matic.quiknode.pro/5b11a0413a62a295070c0dfb25637d5f8c591aba/',
+  '42220': 'https://forno.celo.org',
+  '80001': 'https://matic-mumbai.chainstacklabs.com',
+}
 
 /**
  * A proxy worker for JSON RPC endpoints
@@ -13,15 +26,13 @@ export default {
   ): Promise<Response> {
     const url = new URL(request.url)
     const { pathname } = url
-    const match = pathname.match(/\/([0-9]*)/)
-    if (!match[1]) {
+    const [_, networkId] = pathname.match(/\/([0-9]*)/)
+    if (!networkId) {
       return new Response('Bad Request', { status: 400 })
     }
-    const networkId = parseInt(match[1])
-    if (!networks[networkId]) {
+    if (!supportedNetworks[networkId]) {
       return new Response('Network not supported', { status: 404 })
     }
-    // TODO: parse the request BODY, identify the `to` and limit requests only to Unlock contracts
-    return fetch(networks[networkId].provider, request)
+    return fetch(supportedNetworks[networkId], request)
   },
 }
