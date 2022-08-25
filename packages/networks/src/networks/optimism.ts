76cfe1dd8d4a8fd1730cfac1c5f9b00e5f505eaf
Julien Genestoux <julien.genestoux@gmail.com>
2022-08-25 15:14:44 +0200

    feature(networks): using the unlock rpc proxy (#9466)
    using the unlock rpc proxy

--- packages/networks/src/networks/optimism.ts
+++ packages/networks/src/networks/optimism.ts
@@ -2,7 +2,7 @@ import { NetworkConfig } from '@unlock-protocol/types'
 
 export const optimism: NetworkConfig = {
   publicProvider: 'https://mainnet.optimism.io',
-  provider: 'https://mainnet.optimism.io',
+  provider: 'https://rpc.unlock-protocol.com/10',
   unlockAddress: '0x99b1348a9129ac49c6de7F11245773dE2f51fB0c',
   multisig: '0x6E78b4447e34e751EC181DCBed63633aA753e145',
   id: 10,
