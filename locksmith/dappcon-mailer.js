6db8919c1f97cf50e0ee994082f5dbecd4b205bd
Julien Genestoux <julien.genestoux@gmail.com>
2022-09-07 13:15:32 -0400

    adding dappcon email template

--- locksmith/dappcon-mailer.js
+++ locksmith/dappcon-mailer.js
@@ -1,24 +1,27 @@
-import { notifyNewKeyToWedlocks } from './src/operations/wedlocksOperations'
+import { keysByQuery } from './src/graphql/datasource'
+
+import { sendEmail } from './src/operations/wedlocksOperations'
 import * as keysOperations from './src/operations/keysOperations'
+import { resourceLimits } from 'worker_threads'
+import { UserTokenMetadata } from './src/models'
+import * as Normalizer from './src/utils/normalizer'
+import { generateQrCode } from './src/utils/qrcode'
 
 const run = async () => {
-  const lockAddress = '0x2F3c9b71f256E7450c47872aE00389C616B08646'
-  const lockName = 'DappCon22 - General Tickets'
   const network = 100
-  const page = 0
+  const lockAddresses = [
+    '0x2217D4fF7B9978ba123019Bf1F4DC0A2Bd46F9D6',
+    '0x2F3c9b71f256E7450c47872aE00389C616B08646',
+    '0x0AFAcfE618A9F24d54D0115F49cf8045f1BDf192',
+    '0x130014EC766B83B78a86f53ea8Ec772484f60Eed',
+    '0xEBDA633e76C2cb1983E223D2f186ae45575913D3',
+  ]
 
-  const filters = {
-    query: '',
-    page,
-    filterKey: '',
-    expiration: 'active',
-  }
+  const graphQLClient = new keysByQuery(network)
 
-  const keys = await keysOperations.getKeysWithMetadata({
-    network,
-    lockAddress,
-    filters,
-    loggedInUserAddress: '0x2F3c9b71f256E7450c47872aE00389C616B08646',
+  const result = await graphQLClient.get({
+    addresses: lockAddresses,
+    filters: '',
   })
 
   const processNext = async (list) => {
@@ -26,22 +29,50 @@ const run = async () => {
     if (!next) {
       return
     }
-    await notifyNewKeyToWedlocks(
-      {
-        keyId: next.token,
-        lock: {
-          address: lockAddress,
-          name: lockName,
-        },
-        owner: {
-          address: next.keyholderAddress,
-        },
+
+    const userTokenMetadataRecord = await UserTokenMetadata.findOne({
+      where: {
+        tokenAddress: Normalizer.ethereumAddress(next.lock),
+        userAddress: Normalizer.ethereumAddress(next.owner.address),
       },
+    })
+
+    const protectedData = Normalizer.toLowerCaseKeys({
+      ...userTokenMetadataRecord?.data?.userMetadata?.protected,
+    })
+
+    const recipient = protectedData?.email
+
+    const attachments = []
+    const qrCode = await generateQrCode({
       network,
-      true // attach QR code
+      lockAddress: next.lock,
+      tokenId: next.keyId,
+    })
+    attachments.push({ path: qrCode })
+
+    await sendEmail(
+      `dappcon`,
+      '',
+      'julien@unlock-protocol.com',
+      {},
+      attachments
     )
+
     return processNext(list)
   }
+
+  const keys = result
+    .map((result) => {
+      return result.keys.map((key) => {
+        return {
+          ...key,
+          lock: result.address,
+        }
+      })
+    })
+    .flat()
+
   await processNext(keys)
 }
 
