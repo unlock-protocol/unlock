ecdc6c71a6fd013f5a388997c198dc8a13b021d5
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-12 18:56:45 +0200

    feat(unlock-app) - add tokens list to network (#9591)
    * add tokens list to networks

--- packages/networks/src/networks/avalanche.ts
+++ packages/networks/src/networks/avalanche.ts
@@ -36,6 +36,38 @@ export const avalanche: NetworkConfig = {
   isTestNetwork: false,
   description:
     'Avalanche is an open, programmable smart contracts platform for decentralized applications.',
+  tokens: [
+    {
+      name: 'Ethereum',
+      symbol: 'WETH',
+      decimals: 18,
+      address: '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab',
+    },
+    {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 6,
+      address: '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664',
+    },
+    {
+      name: 'Tether',
+      symbol: 'USDT',
+      decimals: 6,
+      address: '0xc7198437980c041c805a1edcba50c1ce5db95118',
+    },
+    {
+      name: 'Dai',
+      symbol: 'DAI',
+      decimals: 18,
+      address: '0xd586e7f844cea2f87f50152665bcbc2c279d8d70',
+    },
+    {
+      name: 'Wrapped Bitcoin',
+      symbol: 'WBTC',
+      decimals: 8,
+      address: '0x50b7545627a5162f82a992c33b87adc75187b218',
+    },
+  ],
 }
 
 export default avalanche
