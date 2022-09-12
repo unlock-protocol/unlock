26cb519b4a5336dcd7678fe6eb2184f607f0877d
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-12 18:56:45 +0200

    feat(unlock-app) - add tokens list to network (#9591)
    * add tokens list to networks

--- packages/networks/src/networks/polygon.ts
+++ packages/networks/src/networks/polygon.ts
@@ -43,6 +43,39 @@ export const polygon: NetworkConfig = {
   description: 'Popular side chain network. Cheaper transaction cost.',
   isTestNetwork: false,
   teamMultisig: '0x479f3830fbd715342868BA95E438609BCe443DFB',
+  tokens: [
+    {
+      name: 'Wrapped Ether',
+      address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+      symbol: 'WETH',
+      decimals: 18,
+    },
+    {
+      name: 'Dai Stablecoin',
+      address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+      symbol: 'DAI',
+      decimals: 18,
+    },
+
+    {
+      name: 'Tether USD',
+      address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+      symbol: 'USDT',
+      decimals: 6,
+    },
+    {
+      name: 'USDCoin',
+      address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+      symbol: 'USDC',
+      decimals: 6,
+    },
+    {
+      name: 'Wrapped BTC',
+      address: '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6',
+      symbol: 'WBTC',
+      decimals: 8,
+    },
+  ],
 }
 
 export default polygon
