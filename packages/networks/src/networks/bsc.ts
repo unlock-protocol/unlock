72f7bd779a151013d2cba8782b9256976f7111a1
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-12 16:35:33 +0200

    add tokens to network

--- packages/networks/src/networks/bsc.ts
+++ packages/networks/src/networks/bsc.ts
@@ -40,6 +40,32 @@ export const bsc: NetworkConfig = {
   description: 'EVM compatible network. Cheaper transaction cost.',
   isTestNetwork: false,
   teamMultisig: '0x373D7cbc4F2700719DEa237500c7a154310B0F9B',
+  tokens: [
+    {
+      name: 'Ethereum',
+      symbol: 'ETH',
+      decimals: 18,
+      address: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+    },
+    {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 18,
+      address: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
+    },
+    {
+      name: 'Tether',
+      symbol: 'USDT',
+      decimals: 18,
+      address: '0x55d398326f99059ff775485246999027b3197955',
+    },
+    {
+      name: 'Dai',
+      symbol: 'DAI',
+      decimals: 18,
+      address: '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3',
+    },
+  ],
 }
 
 export default bsc
