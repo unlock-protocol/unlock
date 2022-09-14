ee22f78893b8a1ee2aa70db8c5195bb30ba36b19
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-14 14:32:08 +0200

    add select component

---
+++ packages/ui/lib/components/Select/Select.tsx
@@ -0,0 +1,61 @@
+import { Listbox } from '@headlessui/react'
+import { useState } from 'react'
+
+export interface Option {
+  label: string
+  value: string | number
+}
+
+interface SelectProps {
+  label?: string
+  options: Option[]
+  onChange?: (option: Option) => void
+}
+
+export const Select = ({
+  options,
+  onChange,
+  label = 'Select',
+}: SelectProps) => {
+  const [selected, setSelected] = useState<Option | null>(null)
+
+  const onChangeOption = (value: Option['value']) => {
+    const currentItem = options?.find((option) => option.value == value)
+    setSelected(currentItem || null)
+    if (currentItem && typeof onChange === 'function') {
+      onChange(currentItem)
+    }
+  }
+
+  return (
+    <Listbox value={selected?.value || ''} onChange={onChangeOption}>
+      <div className="relative">
+        <label className="block px-1 mb-1 text-base" htmlFor="">
+          {label}
+        </label>
+        <Listbox.Button className="box-border flex-1 block w-full py-2 pl-4 text-base text-left transition-all border border-gray-400 rounded-lg shadow-sm hover:border-gray-500 focus:ring-gray-500 focus:border-gray-500 focus:outline-none">
+          {selected?.label || 'Choose option'}
+        </Listbox.Button>
+        <Listbox.Options className="absolute z-10 w-full mt-1 overflow-hidden bg-white border border-gray-400 rounded-xl">
+          {options?.map((option: Option) => {
+            return (
+              <Listbox.Option
+                key={option.value}
+                value={option.value}
+                className="p-3 cursor-pointer hover:bg-gray-100"
+              >
+                {({ selected }) => (
+                  <>
+                    <span className={selected ? 'font-bold' : ''}>
+                      {option.label}
+                    </span>
+                  </>
+                )}
+              </Listbox.Option>
+            )
+          })}
+        </Listbox.Options>
+      </div>
+    </Listbox>
+  )
+}
