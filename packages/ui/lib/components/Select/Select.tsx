c46276aff8d1563b0d43e33c0d9aabb13611748e
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-15 14:05:57 +0200

    improve select component

--- packages/ui/lib/components/Select/Select.tsx
+++ packages/ui/lib/components/Select/Select.tsx
@@ -1,6 +1,9 @@
 import { Listbox } from '@headlessui/react'
 import { useEffect, useState } from 'react'
 import { BsCheck as CheckIcon } from 'react-icons/bs'
+import { MdOutlineKeyboardArrowDown as ArrowDownIcon } from 'react-icons/md'
+import { twMerge } from 'tailwind-merge'
+import { Size, SizeStyleProp } from '~/types'
 
 export interface Option {
   label: string
@@ -10,14 +13,22 @@ export interface Option {
 interface SelectProps<T> {
   label?: string
   options: Option[]
+  size?: Size
   onChange?: (value: string | number) => void
   defaultValue?: T
+}
+
+const SIZE_STYLES: SizeStyleProp = {
+  small: 'px-2.5 py-1.5 text-sm',
+  medium: 'px-4 py-2 text-base',
+  large: 'px-4 py-2.5',
 }
 
 export const Select = <T extends unknown>({
   options,
   onChange,
   label = 'Select',
+  size = 'medium',
   defaultValue,
 }: SelectProps<T>) => {
   const [selected, setSelected] = useState<Option | null>(null)
@@ -38,17 +49,35 @@ export const Select = <T extends unknown>({
     setSelected(defaultSelection)
   }, [])
 
+  const inputSizeStyle = SIZE_STYLES[size]
+
+  const inputClass = twMerge(
+    'box-border flex-1 block w-full text-base text-left transition-all bg-white border border-gray-400 rounded-lg shadow-sm hover:border-gray-500 focus:ring-gray-500 focus:border-gray-500 focus:outline-none',
+    inputSizeStyle
+  )
+
   return (
     <Listbox value={selected?.value || ''} onChange={onChangeOption}>
       <div className="relative">
-        <label className="block px-1 mb-1 text-base" htmlFor="">
-          {label}
-        </label>
-        <Listbox.Button className="box-border flex-1 block w-full py-2 pl-4 text-base text-left transition-all border border-gray-400 rounded-lg shadow-sm hover:border-gray-500 focus:ring-gray-500 focus:border-gray-500 focus:outline-none">
-          {selected?.label || 'Choose option'}
+        {label?.length > 0 && (
+          <label className="block px-1 mb-1 text-base" htmlFor="">
+            {label}
+          </label>
+        )}
+        <Listbox.Button className={inputClass}>
+          <div className="flex items-center justify-between">
+            <span>{selected?.label || 'Choose option'}</span>
+            <ArrowDownIcon />
+          </div>
         </Listbox.Button>
         <Listbox.Options className="absolute z-10 w-full mt-1 overflow-hidden bg-white border border-gray-400 rounded-xl">
           {options?.map((option: Option) => {
+            const optionClass = twMerge(
+              `${
+                selected ? 'bg-gray-100' : ''
+              } flex items-center justify-between p-3 hover:bg-gray-100`,
+              inputSizeStyle
+            )
             return (
               <Listbox.Option
                 key={option.value}
@@ -56,11 +85,7 @@ export const Select = <T extends unknown>({
                 className="cursor-pointer"
               >
                 {({ selected }) => (
-                  <div
-                    className={`${
-                      selected ? 'bg-gray-100' : ''
-                    } flex items-center justify-between p-3 hover:bg-gray-100`}
-                  >
+                  <div className={optionClass}>
                     <span className={selected ? 'font-bold' : ''}>
                       {option.label}
                     </span>
