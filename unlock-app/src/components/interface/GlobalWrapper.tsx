332152796d2774dcdc9a12288477245051270058
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-09-18 00:25:51 +0530

    refactor(unlock-app): Move away from nextjs runtime behavior for loading configuration

--- unlock-app/src/components/interface/GlobalWrapper.tsx
+++ unlock-app/src/components/interface/GlobalWrapper.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import GlobalStyle from '../../theme/globalStyle'
-import configure from '../../config'
 import { WedlockServiceContext } from '../../contexts/WedlocksContext'
 import WedlockService from '../../services/wedlockService'
 import { ConfigContext } from '../../utils/withConfig'
 import ProviderContext from '../../contexts/ProviderContext'
 import Authenticate from './Authenticate'
 import { CONSOLE_MESSAGE } from '../../constants'
+import { config } from '~/config/app'
 
-const config = configure()
 const wedlockService = new WedlockService(config.services.wedlocks.host)
 
 interface GlobalWrapperProps {
