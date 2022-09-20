dd80b576c7bb7d451041c5595e80dd132b41c8b1
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-20 17:57:15 +0200

    app footer mobile

--- unlock-app/src/components/interface/AppFooter.tsx
+++ unlock-app/src/components/interface/AppFooter.tsx
@@ -13,7 +13,7 @@ const links: Link[] = [
 const Link = ({ label, url }: Link) => {
   return (
     <a
-      className="text-base text-white hover:underline"
+      className="text-base font-bold text-white hover:underline"
       target="_blank"
       href={url}
       rel="noreferrer"
@@ -24,10 +24,16 @@ const Link = ({ label, url }: Link) => {
 }
 export const AppFooter = () => {
   return (
-    <div className="py-8 bg-black ">
-      <div className="flex items-center justify-between w-full mx-auto md:max-w-screen-lg lg:max-w-screen-xl">
-        <img src="/images/svg/logo-monogram-square.svg" alt="logo monogram" />
-        <ul className="flex gap-8">
+    <div className="px-4 py-8 bg-black md:px-0">
+      <div className="flex flex-col items-start w-full gap-10 md:gap-0 md:mx-auto md:justify-between md:items-center md:flex-row md:max-w-screen-lg lg:max-w-screen-xl">
+        <a href="/">
+          <img
+            className="h-6"
+            src="/images/svg/logo-monogram-square.svg"
+            alt="logo monogram"
+          />
+        </a>
+        <ul className="flex flex-col gap-8 md:px-0 md:flex-row">
           {links?.map((link, index) => {
             return (
               <li key={index}>
