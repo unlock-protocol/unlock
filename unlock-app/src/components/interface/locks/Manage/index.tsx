4dba2c0245092098cf766952e2cf17ee66a3530c
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-20 10:52:49 +0200

    add manage lock page

---
+++ unlock-app/src/components/interface/locks/Manage/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import { useAuth } from '~/contexts/AuthenticationContext'
+import { ConnectWalletModal } from '../../ConnectWalletModal'
+import { LockDetailCard } from './elements/LockDetailCard'
+import { TotalBar } from './elements/TotalBar'
+
+export const ManageLockPage = () => {
+  const { network } = useAuth()
+  const { query } = useRouter()
+
+  const { address: lockAddress } = query ?? {}
+
+  if (!network) {
+    return <ConnectWalletModal isOpen={true} setIsOpen={() => void 0} />
+  }
+
+  return (
+    <div className="min-w-full min-h-screen bg-ui-secondary-200">
+      <div className="px-4 mx-auto lg:container pt-9">
+        <div className="flex flex-col md:grid md:grid-cols-12 gap-14">
+          <div className="md:col-span-3">
+            <LockDetailCard
+              lockAddress={lockAddress as string}
+              network={network}
+            />
+          </div>
+          <div className="md:col-span-9">
+            <TotalBar lockAddress={lockAddress as string} network={network} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ManageLockPage
