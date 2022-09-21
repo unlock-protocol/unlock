79e30ba93aa1bfccc0e12e0c450cb0858f1c7646
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-21 11:07:24 +0200

    feat(unlock-app) - add footer manage lock page (#9647)
    add footer manage lock page

--- unlock-app/src/pages/locks/lock.tsx
+++ unlock-app/src/pages/locks/lock.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import type { NextPage } from 'next'
 import BrowserOnly from '~/components/helpers/BrowserOnly'
 import ManageLockPage from '~/components/interface/locks/Manage'
+import { AppFooter } from '~/components/interface/AppFooter'
 
 const LockByAddress: NextPage = () => {
   return (
     <BrowserOnly>
       <ManageLockPage />
+      <AppFooter />
     </BrowserOnly>
   )
 }
