72ef533d70f2fc6f9b0600a074af5ab5e81dee00
Clément Renaud <clement+git@clementrenaud.com>
2022-09-23 10:09:32 +0200

    use package in smart-contracts folder

--- smart-contracts/tasks/config.js
+++
@@ -1,8 +0,0 @@
-const { task } = require('hardhat/config')
-
-task('config', 'Show current config')
-  .addFlag('json', 'output as JSON')
-  .setAction(({ json }, { config }) => {
-    // eslint-disable-next-line no-console
-    console.log(json ? JSON.stringify(config) : config)
-  })
