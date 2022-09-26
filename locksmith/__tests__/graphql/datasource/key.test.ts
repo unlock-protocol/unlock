84b443e011d72182f0a96adc5df725b0865954b2
Julien Genestoux <julien.genestoux@gmail.com>
2022-09-26 16:15:05 -0400

    Production Deploy (#9678)
    Manual deploy as of commit:96a5a0d53ca4cd36ba0d9fa7093c2c58202b147f

--- locksmith/__tests__/graphql/datasource/key.test.ts
+++ locksmith/__tests__/graphql/datasource/key.test.ts
@@ -3,7 +3,7 @@ import { Key } from '../../../src/graphql/datasource'
 const TEST_KEY_ID = '0x0035807ec068737973d603a1f7e0fc1b12a78bf8-1'
 // Rinkeby
 const TEST_NETWORK = 4
-describe('Key', () => {
+describe.skip('Key', () => {
   describe('getKey', () => {
     describe('when data is returned for the data source', () => {
       it('return the requested data', async () => {
