f33b5b1bc26a052f0518ffb0c4ce2ed54d940718
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-27 13:55:45 +0200

    feat(unlock-app) - locks list page (#9683)
    locks list page

--- unlock-app/src/components/interface/locks/Manage/elements/ImageBar.tsx
+++ unlock-app/src/components/interface/locks/Manage/elements/ImageBar.tsx
@@ -8,9 +8,9 @@ export const ImageBar = ({ description, src, alt }: ImageBarProps) => {
   return (
     <div className="text-center">
       <div className="overflow-hidden rounded-2xl">
-        <img className="object-cover w-full h-24 md:h-60" src={src} alt={alt} />
+        <img className="object-cover w-full h-24 md:h-80" src={src} alt={alt} />
       </div>
-      <span className="block mt-4 text-base">{description}</span>
+      <span className="block mt-4 text-xl font-bold">{description}</span>
     </div>
   )
 }
