c7e7b91b1768ceeca983c8d759e9e2d524406a36
Kalidou Diagne <kld.diagne@gmail.com>
2022-09-28 14:29:05 +0200

    feat(unlock-app) - app header component (#9690)
    * app header component

--- unlock-app/src/pages/locks/index.tsx
+++ unlock-app/src/pages/locks/index.tsx
@@ -3,10 +3,12 @@ import type { NextPage } from 'next'
 import BrowserOnly from '~/components/helpers/BrowserOnly'
 import { AppFooter } from '~/components/interface/AppFooter'
 import LocksListPage from '~/components/interface/locks/List'
+import { AppHeader } from '~/components/interface/AppHeader'
 
 const Locks: NextPage = () => {
   return (
     <BrowserOnly>
+      <AppHeader />
       <LocksListPage />
       <AppFooter />
     </BrowserOnly>
