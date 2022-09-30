146c9af1624e9aa5fd8470bf17131c21e1853413
Julien Genestoux <julien.genestoux@gmail.com>
2022-09-30 15:56:08 -0400

    fixing test

--- wedlocks/src/templates/welcome.js
+++ wedlocks/src/templates/welcome.js
@@ -11,7 +11,7 @@ export default {
 
     <p>Unlock is designed to make sure <strong>we can never access your data</strong>: it's encrypted with your password. Because of that, we can't reset your password like other services that you're used to.</p>
 
-    <p>We know that sometimes things happen, and you may find that you need to reset your password in the future. To do that, you will need <a href="{{recoveryLink}}">click on this link</a>. (we're also adding a text version of the link that you can copy paste at the bottom of this email.</p>
+    <p>We know that sometimes things happen, and you may find that you need to reset your password in the future. To do that, you will need <a href="{{recoveryLink}}">click on this link</a>. (we're also adding a text version of the link that you can copy paste at the bottom of this email.)</p>
 
     <p>Please, make sure to <strong>keep this link secret</strong>: do not forward this email to anyone. If someone accesses this email, they will be able to take over your Unlock account and all associated memberships.</p>
 
