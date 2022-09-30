bab2a19ad28343fddd7a8607996b98201eabc9ef
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-09-30 21:30:11 +0530

    use package scripts

--- packages/unlock-js/scripts/generate.sh
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env bash
-
-OPEN_API_FILEPATH="${1:-"./openapi.yml"}"
-
-# clean generated folder
-rm -rf src/@generated
-
-yarn openapi-generator-cli generate -i $OPEN_API_FILEPATH -g typescript-axios -c ./openapi.ts.config.json -o ./src/@generated/client
