eb3cc1f8a8395061cd937712560e5d816c601c07
Julien Genestoux <julien.genestoux@gmail.com>
2022-09-30 23:28:50 -0400

    feature(wedlocks): HTML emails (#9729)
    * Revert "Revert "feature(wedlocks): adding a better html email template (#9708)""
    
    This reverts commit a5ffdc518e44a89fb83e3d6761c79f192a4d2410.
    
    * using new netlify function
    
    * fixing preview
    
    * updated dependencies
    
    * updated build script
    
    * using a script to build the config
    
    * fixing test

--- wedlocks/src/__tests__/templates/index.test.js
+++ wedlocks/src/__tests__/templates/index.test.js
@@ -1,10 +1,15 @@
 import templates from '../../templates'
 
+// Remove the default
+let templatesToTest = { ...templates }
+delete templatesToTest.default
+
 describe('templates', () => {
-  test.each(Object.keys(templates))('%s', (template) => {
+  test.each(Object.keys(templatesToTest))('%s', (template) => {
     expect.assertions(2)
-    expect(templates[template].subject).toBeInstanceOf(Function)
-    const render = templates[template].html || templates[template].text
+    expect(templatesToTest[template].subject).toBeInstanceOf(Function)
+    const render =
+      templatesToTest[template].html || templatesToTest[template].text
     expect(render).toBeInstanceOf(Function)
   })
 })
