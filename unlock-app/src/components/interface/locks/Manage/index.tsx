7b46d1c2f71b1670958f782d2172c2bacbe552b6
Kalidou Diagne <kld.diagne@gmail.com>
2022-10-05 19:51:39 +0200

    remove typo

--- unlock-app/src/components/interface/locks/Manage/index.tsx
+++ unlock-app/src/components/interface/locks/Manage/index.tsx
@@ -250,7 +250,7 @@ const NotManagerBanner = () => {
   return (
     <div className="p-2 text-base text-center text-red-700 bg-red-100 border border-red-700 rounded-xl">
       You are connected as {addressMinify(account!)} and this address is not a
-      manager for this lock. If you want to update details, please connect as as
+      manager for this lock. If you want to update details, please connect as
       lock manager.
     </div>
   )
