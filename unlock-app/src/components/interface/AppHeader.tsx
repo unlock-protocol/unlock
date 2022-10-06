273c12f427acb800f12f7f5fa55720b34412e1e1
Kalidou Diagne <kld.diagne@gmail.com>
2022-10-06 17:59:13 +0200

    remove switch network

--- unlock-app/src/components/interface/AppHeader.tsx
+++ unlock-app/src/components/interface/AppHeader.tsx
@@ -3,11 +3,7 @@ import Link from 'next/link'
 import { useAuth } from '~/contexts/AuthenticationContext'
 import { addressMinify } from '~/utils/strings'
 import { useRouter } from 'next/router'
-import { useConfig } from '~/utils/withConfig'
-import { CryptoIcon } from './locks/elements/KeyPrice'
-import { HiChevronDown as ArrowDownButton } from 'react-icons/hi'
-import { Popover, Transition } from '@headlessui/react'
-import { Fragment, useState } from 'react'
+import { useState } from 'react'
 import { AiOutlineMenu as MenuIcon } from 'react-icons/ai'
 import { MdExitToApp as DisconnectIcon } from 'react-icons/md'
 import { GrClose as MenuCloseIcon } from 'react-icons/gr'
@@ -28,79 +24,8 @@ const links: Link[] = [
   { label: 'Settings', url: '/settings' },
 ]
 
-interface SwitchNetworkButtonProps {
-  network: number
-}
-
-const SwitchNetworkButton = ({ network }: SwitchNetworkButtonProps) => {
-  const { networks } = useConfig()
-  const { changeNetwork } = useAuth()
-
-  const { name: connectedNetwork, baseCurrencySymbol } = networks[network] ?? {}
-
-  const onChangeNetwork = async (network: number) => {
-    await changeNetwork(networks[network])
-  }
-
-  return (
-    <>
-      <Popover className="relative">
-        <Popover.Button className="focus:outline-none">
-          <Button
-            variant="outlined-primary"
-            className="w-12 h-12 md:w-full md:h-full focus:outline-none"
-          >
-            <div className="flex items-center gap-1">
-              <CryptoIcon symbol={baseCurrencySymbol} />
-              <div className="items-center hidden gap-1 md:flex">
-                <span className="text-base">{connectedNetwork}</span>
-                <ArrowDownButton size={15} />
-              </div>
-            </div>
-          </Button>
-        </Popover.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-150"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
-        >
-          <Popover.Panel className="absolute z-10 mt-3 transform -translate-x-1/2 w-60 left-1/2 sm:px-0">
-            <div className="relative py-3 overflow-hidden bg-white shadow-lg rounded-3xl">
-              <div className="flex flex-col">
-                {Object.values(networks).map(
-                  ({ name, baseCurrencySymbol, id }: any) => {
-                    const isActive = id === network
-                    return (
-                      <div
-                        key={id}
-                        className={`flex cursor-pointer px-4 py-2 gap-2 hover:bg-gray-100 ${
-                          isActive ? 'bg-gray-200' : ''
-                        }`}
-                        onClick={() => onChangeNetwork(id)}
-                      >
-                        <div className="w-6 h-6">
-                          <CryptoIcon size={24} symbol={baseCurrencySymbol} />
-                        </div>
-                        <span className="text-base">{name}</span>
-                      </div>
-                    )
-                  }
-                )}
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
-    </>
-  )
-}
-
 export const AppHeader = () => {
-  const { account, network, deAuthenticate } = useAuth()
+  const { account, deAuthenticate } = useAuth()
   const [isOpen, setIsOpen] = useState(false)
   const [disconnectModal, setDisconnectModal] = useState(false)
 
@@ -203,11 +128,7 @@ export const AppHeader = () => {
           <div>
             {account ? (
               <div className="flex gap-2">
-                <SwitchNetworkButton network={network!} />
-                <Button
-                  variant="outlined-primary"
-                  onClick={() => setDisconnectModal(true)}
-                >
+                <button onClick={() => setDisconnectModal(true)}>
                   <div className="flex items-center gap-2">
                     <span className="text-brand-ui-primary">
                       {addressMinify(account)}
@@ -217,7 +138,7 @@ export const AppHeader = () => {
                       size={20}
                     />
                   </div>
-                </Button>
+                </button>
               </div>
             ) : (
               <Link href={loginUrl}>
