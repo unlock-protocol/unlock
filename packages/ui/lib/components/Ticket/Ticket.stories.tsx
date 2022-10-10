119e469fe9f5c6c9ffe07f8b4b326ce50247b504
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-10-11 00:01:04 +0530

    Add email story

--- packages/ui/lib/components/Ticket/Ticket.stories.tsx
+++ packages/ui/lib/components/Ticket/Ticket.stories.tsx
@@ -1,5 +1,6 @@
-import { Ticket } from './Ticket'
+import { Ticket, TicketItem } from './Ticket'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { RiMailFill as MailIcon } from 'react-icons/ri'
 
 export default {
   component: Ticket,
@@ -36,6 +37,23 @@ TicketComponentWithENS.args = {
   items: [],
   time: '10:00 - 12:00',
   date: '10/10/2022 - 10/15/2022',
+  location: '190 Bowery, Soho, New York',
+  QRCodeURL: '/images/qrcode.png',
+}
+
+export const TicketComponentWithEmail = Template.bind({})
+
+TicketComponentWithEmail.args = {
+  iconURL: '/images/lock.png',
+  title: 'A Huge event in NYC',
+  id: '1',
+  recipient: 'julien51.eth',
+  lockAddress: '0xC8BD5B876E9c06F30b8f0E79aB25A3aEa7F47D97',
+  network: 100,
+  items: [
+    <TicketItem icon={<MailIcon />} value="julien@unlock-protocol.com" />,
+  ],
+  time: '10:00 - 12:00',
   location: '190 Bowery, Soho, New York',
   QRCodeURL: '/images/qrcode.png',
 }
