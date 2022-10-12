422a95f1a43f3dc29612e495f6783dc1ffa5d969
Clément Renaud <clement+git@clementrenaud.com>
2022-10-12 14:38:08 +0200

    test on erc20 spending

--- smart-contracts/test/Lock/execTransaction.js
+++ smart-contracts/test/Lock/execTransaction.js
@@ -4,52 +4,55 @@ const {
   deployERC20,
   deployLock,
   deployContracts,
+  getBalance,
 } = require('../helpers')
 const { ethers } = require('hardhat')
+const { assert } = require('chai')
 
 let unlock
 let lock
 
 contract('Lock / execTransaction', (accounts) => {
-  const [deployer, lockOwner, keyOwner, spender, other] = accounts
+  const [deployer, lockOwner, keyOwner, spender, other, receiver] = accounts
   let token
+  let interface
+  let keyPrice
 
   before(async () => {
     ;({ unlock } = await deployContracts())
+    ;({ interface } = await ethers.getContractFactory(
+      '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20'
+    ))
+
     token = await deployERC20(deployer)
 
     await token.mint(keyOwner, ethers.utils.parseUnits('100', 'ether'), {
       from: deployer,
     })
+
     lock = await deployLock({
       unlock,
       from: lockOwner,
       tokenAddress: token.address,
     })
 
-    await token.approve(lock.address, await lock.keyPrice(), {
+    keyPrice = await lock.keyPrice()
+
+    await token.approve(lock.address, keyPrice, {
       from: keyOwner,
     })
 
     await purchaseKey(lock, keyOwner, true)
   })
 
-  // An ERC-20 style approval, allowing the spender to transfer funds directly from the lock.
-  describe('approveBeneficiary', () => {
+  describe('ERC20 approval/allowance', () => {
     let calldata
-    let keyPrice
-    before(async () => {
-      const { interface } = await ethers.getContractFactory(
-        '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20'
-      )
-      keyPrice = await lock.keyPrice()
 
+    before(async () => {
       calldata = interface.encodeFunctionData('approve', [
         spender, // spender
         keyPrice.toString(), // amount
       ])
-
-      console.log(calldata)
     })
 
     it('approve fails if called from the wrong account', async () => {
@@ -74,6 +77,30 @@ contract('Lock / execTransaction', (accounts) => {
       await token.transferFrom(lock.address, other, keyPrice, {
         from: spender,
       })
+    })
+  })
+
+  describe('ERC20 transfer of funds', () => {
+    it('can spend ERC20', async () => {
+      // buy a key to increase lock balance
+      await token.approve(lock.address, keyPrice, {
+        from: other,
+      })
+      await purchaseKey(lock, other, true)
+
+      const calldata = interface.encodeFunctionData('transfer', [
+        receiver,
+        keyPrice.toString(),
+      ])
+
+      assert.equal((await getBalance(receiver, token.address)).toString(), '0')
+      await lock.execTransaction(token.address, calldata, 0, {
+        from: lockOwner,
+      })
+      assert.equal(
+        (await getBalance(receiver, token.address)).toString(),
+        keyPrice.toString()
+      )
     })
   })
 })
