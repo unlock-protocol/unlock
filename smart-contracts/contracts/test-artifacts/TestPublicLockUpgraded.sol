e33ba98dd817cab3a563a69ecae82b13cb2daf1b
Clement Renaud <clement@clementrenaud.com>
2022-10-14 22:07:27 +0200

    make test upgrade contract artifact lighter

--- smart-contracts/contracts/test-artifacts/TestPublicLockUpgraded.sol
+++ smart-contracts/contracts/test-artifacts/TestPublicLockUpgraded.sol
@@ -2,9 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "../PublicLock.sol";
+import "../mixins/MixinLockCore.sol";
+import "../mixins/MixinKeys.sol";
 import "../interfaces/IPublicLock.sol";
 
-contract TestPublicLockUpgraded is PublicLock {
+contract TestPublicLockUpgraded is MixinLockCore, MixinKeys {
+
+  function initialize(
+    address payable _lockCreator,
+    uint _expirationDuration,
+    address _tokenAddress,
+    uint _keyPrice,
+    uint _maxNumberOfKeys,
+    string calldata //_lockName
+  ) public
+    initializer()
+  {
+    MixinFunds._initializeMixinFunds(_tokenAddress);
+    MixinLockCore._initializeMixinLockCore(_lockCreator, _expirationDuration, _keyPrice, _maxNumberOfKeys);
+  }
 
   // add a function to try
   function sayHello() external pure returns (string memory) {
@@ -17,5 +33,5 @@ contract TestPublicLockUpgraded is PublicLock {
 }
 
 interface ITestPublicLockUpgraded is IPublicLock {
-  function sayHello() external pure returns (string memory);
+  function sayHello() external pure returns (bool);
 }
