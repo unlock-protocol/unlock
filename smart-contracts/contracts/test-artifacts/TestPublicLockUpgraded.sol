7fa27dd997543d20632f3e3b909bedc24038065b
Clement Renaud <clement@clementrenaud.com>
2022-10-14 22:25:07 +0200

    light but not too light TestPublicLockUpgraded

--- smart-contracts/contracts/test-artifacts/TestPublicLockUpgraded.sol
+++ smart-contracts/contracts/test-artifacts/TestPublicLockUpgraded.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.0;
 import "../PublicLock.sol";
 import "../mixins/MixinLockCore.sol";
 import "../mixins/MixinKeys.sol";
+import "../mixins/MixinLockMetadata.sol";
+import "../mixins/MixinERC721Enumerable.sol";
+import "../mixins/MixinRoles.sol";
 import "../interfaces/IPublicLock.sol";
+import '@openzeppelin/contracts-upgradeable/utils/introspection/ERC165StorageUpgradeable.sol';
 
-contract TestPublicLockUpgraded is MixinLockCore, MixinKeys {
+contract TestPublicLockUpgraded is 
+  ERC165StorageUpgradeable,
+  MixinLockCore, 
+  MixinKeys, 
+  MixinLockMetadata,
+  MixinERC721Enumerable
+{
 
   function initialize(
     address payable _lockCreator,
@@ -14,12 +24,17 @@ contract TestPublicLockUpgraded is MixinLockCore, MixinKeys {
     address _tokenAddress,
     uint _keyPrice,
     uint _maxNumberOfKeys,
-    string calldata //_lockName
+    string calldata _lockName
   ) public
     initializer()
   {
     MixinFunds._initializeMixinFunds(_tokenAddress);
     MixinLockCore._initializeMixinLockCore(_lockCreator, _expirationDuration, _keyPrice, _maxNumberOfKeys);
+    MixinLockMetadata._initializeMixinLockMetadata(_lockName);
+    MixinERC721Enumerable._initializeMixinERC721Enumerable();
+    MixinRoles._initializeMixinRoles(_lockCreator);
+
+    _registerInterface(0x80ac58cd);
   }
 
   // add a function to try
@@ -30,8 +45,26 @@ contract TestPublicLockUpgraded is MixinLockCore, MixinKeys {
   function migrate(bytes calldata) public override {
     schemaVersion = super.publicLockVersion() + 1;
   }
+
+   /**
+   Overrides
+  */
+  function supportsInterface(bytes4 interfaceId) 
+    public 
+    view 
+    virtual 
+    override(
+      MixinERC721Enumerable,
+      MixinLockMetadata,
+      AccessControlUpgradeable, 
+      ERC165StorageUpgradeable
+    ) 
+    returns (bool) 
+    {
+    return super.supportsInterface(interfaceId);
+  }
 }
 
 interface ITestPublicLockUpgraded is IPublicLock {
-  function sayHello() external pure returns (bool);
+  function sayHello() external pure returns (string memory);
 }
