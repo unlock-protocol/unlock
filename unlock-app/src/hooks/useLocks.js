23c64bc48605faaf454c74a00f1cee896ebae0b9
Kalidou Diagne <kld.diagne@gmail.com>
2022-10-17 19:33:28 +0200

    remove comment

--- unlock-app/src/hooks/useLocks.js
+++ unlock-app/src/hooks/useLocks.js
@@ -47,7 +47,6 @@ export const retrieveLocks = async (
   // The locks from the subgraph miss some important things, such as balance,
   // ERC20 info.. etc so we need to retrieve them from unlock-js too.
   // TODO: add these missing fields to the graph!
-  //const locks = await graphService.locksByManager(owner, network)
   const service = new SubgraphService()
   const locks = await service.locks(
     {
