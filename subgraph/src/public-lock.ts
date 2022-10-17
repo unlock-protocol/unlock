dfcbd21be8b064e41376176d4139c683165267a1
Clément Renaud <clement@unlock-protocol.com>
2022-10-17 11:16:09 +0200

    feat(subgraph): support for new `LockConfig` event (#9922)
    * single function to set lock keys config
    
    * test event
    
    * parse default correctly
    
    * update abi with updateLockConfig
    
    * fix typo in event syntax
    
    * add `handleLockConfig` feature
    
    * default max keys per address to 1
    
    * add support for `maxNumberOfKeys` and `maxKeysPerAddress`
    
    * make integration tests for the event
    
    * fix typo in tests
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- subgraph/src/public-lock.ts
+++ subgraph/src/public-lock.ts
@@ -12,6 +12,7 @@ import {
   PricingChanged as PricingChangedEvent,
   RenewKeyPurchase as RenewKeyPurchaseEvent,
   Transfer as TransferEvent,
+  LockConfig as LockConfigEvent,
 } from '../generated/templates/PublicLock/PublicLock'
 
 import { PublicLockV11 as PublicLock } from '../generated/templates/PublicLock/PublicLockV11'
@@ -40,6 +41,16 @@ function newKey(event: TransferEvent): void {
   const lock = Lock.load(event.address.toHexString())
   if (lock) {
     lock.totalKeys = lock.totalKeys.plus(BigInt.fromI32(1))
+    lock.save()
+  }
+}
+
+export function handleLockConfig(event: LockConfigEvent): void {
+  const lock = Lock.load(event.address.toHexString())
+  if (lock) {
+    lock.expirationDuration = event.params.expirationDuration
+    lock.maxNumberOfKeys = event.params.maxNumberOfKeys
+    lock.maxKeysPerAddress = event.params.maxKeysPerAddress
     lock.save()
   }
 }
@@ -99,6 +110,7 @@ export function handleExpirationChangedUntilV11(
     key.save()
   }
 }
+
 export function handleExpirationChanged(event: ExpirationChangedEvent): void {
   const keyID = genKeyID(event.address, event.params.tokenId.toString())
   const key = Key.load(keyID)
