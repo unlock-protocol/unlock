31e1ee96ae86c2c2ce6bd58d513e77239ebab3c8
Clément Renaud <clement+git@clementrenaud.com>
2022-10-17 14:20:49 +0200

    fix template scripts

--- smart-contracts/scripts/deployments/template.js
+++ smart-contracts/scripts/deployments/template.js
@@ -3,11 +3,18 @@ const { addDeployment } = require('../../helpers/deployments')
 const { getNetworkName } = require('../../helpers/network')
 const contracts = require('@unlock-protocol/contracts')
 
+function parse(data) {
+  return ethers.utils.parseUnits(Math.ceil(data) + '', 'gwei')
+}
+
 async function main({ publicLockVersion }) {
   // fetch chain info
   const { chainId } = await ethers.provider.getNetwork()
   const networkName = getNetworkName(chainId)
   const isLocalNet = networkName === 'localhost'
+
+  const [signer] = await ethers.getSigners()
+  console.log(signer.address)
 
   let PublicLock
   if (publicLockVersion) {
@@ -17,7 +24,15 @@ async function main({ publicLockVersion }) {
     PublicLock = await ethers.getContractFactory('PublicLock')
   }
 
-  const publicLock = await PublicLock.deploy()
+  // fix MATIC gas too low
+  const resp = await fetch('https://gasstation-mainnet.matic.network/v2')
+  const data = await resp.json()
+  const maxFeePerGas = parse(data.fast.maxFee).mul(2)
+  const maxPriorityFeePerGas = parse(data.fast.maxPriorityFee).mul(2)
+  const publicLock = await PublicLock.deploy({
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  })
   await publicLock.deployed()
 
   // eslint-disable-next-line no-console
