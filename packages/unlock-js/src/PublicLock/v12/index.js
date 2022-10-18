aaebb93f92153be4d8db3ff61341761903b0cbb0
Clément Renaud <clement+git@clementrenaud.com>
2022-10-18 16:33:09 +0200

    update withdraw / beneficiary

--- packages/unlock-js/src/PublicLock/v12/index.js
+++ packages/unlock-js/src/PublicLock/v12/index.js
@@ -6,15 +6,19 @@ import setExpirationDuration from './setExpirationDuration'
 import setBaseTokenURI from './setBaseTokenURI'
 import updateLockName from './updateLockName'
 import updateLockSymbol from './updateLockSymbol'
+import approveBeneficiary from './approveBeneficiary'
+import withdrawFromLock from './withdrawFromLock'
 
 export default {
   ...v11,
+  approveBeneficiary,
   setMaxKeysPerAddress,
   setExpirationDuration,
   setMaxNumberOfKeys,
   setBaseTokenURI,
   updateLockName,
   updateLockSymbol,
+  withdrawFromLock,
   version: 'v12',
   PublicLock: abis.PublicLock.v12,
 }
