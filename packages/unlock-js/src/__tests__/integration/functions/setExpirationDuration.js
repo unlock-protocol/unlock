63e51f3007bf4d51446a008dfa6ad502597cb611
Clément Renaud <clement+git@clementrenaud.com>
2022-10-19 15:19:45 +0200

    check versions properly

--- packages/unlock-js/src/__tests__/integration/functions/setExpirationDuration.js
+++ packages/unlock-js/src/__tests__/integration/functions/setExpirationDuration.js
@@ -1,29 +1,33 @@
 let walletService, web3Service, lockAddress, lock, chainId
-export default () => () => {
-  let expirationDuration
+export default ({ publicLockVersion }) =>
+  () => {
+    // Test only on lock v9 and above.
+    if (['v9'].indexOf(publicLockVersion) !== -1) {
+      let expirationDuration
 
-  beforeAll(async () => {
-    ;({ walletService, web3Service, lockAddress, lock, chainId } =
-      global.suiteData)
+      beforeAll(async () => {
+        ;({ walletService, web3Service, lockAddress, lock, chainId } =
+          global.suiteData)
 
-    expirationDuration = lock.expirationDuration
-    await walletService.setExpirationDuration(
-      {
-        lockAddress,
-        expirationDuration: parseFloat(200).toString(),
-      },
-      (error) => {
-        if (error) {
-          throw error
-        }
-      }
-    )
-    lock = await web3Service.getLock(lockAddress, chainId)
-  })
+        expirationDuration = lock.expirationDuration
+        await walletService.setExpirationDuration(
+          {
+            lockAddress,
+            expirationDuration: parseFloat(200).toString(),
+          },
+          (error) => {
+            if (error) {
+              throw error
+            }
+          }
+        )
+        lock = await web3Service.getLock(lockAddress, chainId)
+      })
 
-  it('Check if setMaxNumberOfKeys updated the maxNumberOfKeys', () => {
-    expect.assertions(2)
-    expect(expirationDuration).not.toBe(lock.expirationDuration)
-    expect(lock.expirationDuration).toBe(200)
-  })
-}
+      it('Check if setMaxNumberOfKeys updated the maxNumberOfKeys', () => {
+        expect.assertions(2)
+        expect(expirationDuration).not.toBe(lock.expirationDuration)
+        expect(lock.expirationDuration).toBe(200)
+      })
+    }
+  }
