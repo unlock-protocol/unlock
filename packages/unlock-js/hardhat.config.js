7a47dd7e2cd50f7dd9854d3d9bb77fbc170c9c2b
Clément Renaud <clement+git@clementrenaud.com>
2022-10-20 11:35:21 +0200

    jest task to run a single test file

--- packages/unlock-js/hardhat.config.js
+++ packages/unlock-js/hardhat.config.js
@@ -10,13 +10,21 @@ const process = require('process')
 const jestConfig = require('./jest.config')
 
 const TASK_JEST_SINGLE = 'test:single'
+const TASK_JEST_SINGLE_INTEGRATION = 'test:single:integration'
 const TASK_JEST = 'test:jest'
 const TASK_JEST_RUN_TESTS = 'jest:run'
 
 subtask(TASK_JEST_RUN_TESTS).setAction(
-  async ({ watch, projects = [process.cwd()] }) => {
+  async ({ watch = false, files = [] }) => {
+    if (files.length) {
+      jestConfig.testMatch = files.map((it) =>
+        it.endsWith('.js') || it.endsWith('*') ? `**/${it}**` : `${it}\\.js$`
+      )
+    }
+    const projects = [process.cwd()]
+
     const testFailures = await new Promise((resolve, reject) => {
-      return runCLI(jestConfig, projects)
+      return runCLI({ ...jestConfig, watch }, projects)
         .then((result) => resolve(result))
         .catch((error) => reject(error))
     })
@@ -25,28 +33,38 @@ subtask(TASK_JEST_RUN_TESTS).setAction(
   }
 )
 
-task(TASK_JEST_SINGLE, 'Runs jest tests')
+const showTestResults = async (network, testResults) => {
+  if (network.name === HARDHAT_NETWORK_NAME) {
+    const stackTracesFailures = await network.provider.send(
+      'hardhat_getStackTraceFailuresCount'
+    )
+
+    if (stackTracesFailures !== 0) {
+      console.warn(
+        `Failed to generate ${stackTracesFailures} stack trace(s). Run Hardhat with --verbose to learn more.`
+      )
+    }
+  }
+  const exit = testResults.success ? 0 : 1
+  process.exit(exit)
+}
+
+task(TASK_JEST_SINGLE, 'Runs jest integration tests separately')
+  .addParam('file', 'the path of the file to test')
+  .setAction(async ({ file }, { run, network }) => {
+    const testResults = await run(TASK_JEST_RUN_TESTS, { files: [file] })
+    showTestResults(network, testResults)
+  })
+
+task(TASK_JEST_SINGLE_INTEGRATION, 'Runs jest integration tests separately')
   .addVariadicPositionalParam('files', 'the path of the file to test')
   .addOptionalParam('lockVersion', 'the version of the PublicLock contract')
   .addOptionalParam('unlockVersion', 'the version of the Unlock contract')
   .setAction(
     async ({ files, lockVersion, unlockVersion }, { run, network }) => {
       console.log(files, lockVersion, unlockVersion)
-
       const testResults = await run(TASK_JEST_RUN_TESTS, { projects: files })
-      if (network.name === HARDHAT_NETWORK_NAME) {
-        const stackTracesFailures = await network.provider.send(
-          'hardhat_getStackTraceFailuresCount'
-        )
-
-        if (stackTracesFailures !== 0) {
-          console.warn(
-            `Failed to generate ${stackTracesFailures} stack trace(s). Run Hardhat with --verbose to learn more.`
-          )
-        }
-      }
-      const exit = testResults.success ? 0 : 1
-      process.exit(exit)
+      showTestResults(network, testResults)
     }
   )
 
@@ -68,20 +86,7 @@ task(TASK_JEST, 'Runs jest tests')
     await run('compile')
 
     const testResults = await run(TASK_JEST_RUN_TESTS, { watch })
-
-    if (network.name === HARDHAT_NETWORK_NAME) {
-      const stackTracesFailures = await network.provider.send(
-        'hardhat_getStackTraceFailuresCount'
-      )
-
-      if (stackTracesFailures !== 0) {
-        console.warn(
-          `Failed to generate ${stackTracesFailures} stack trace(s). Run Hardhat with --verbose to learn more.`
-        )
-      }
-    }
-    const exit = testResults.success ? 0 : 1
-    process.exit(exit)
+    showTestResults(network, testResults)
   })
 
 module.exports = {
