4ef862e3a530aaccba7f74aed3fe3b22fec0ea90
Clément Renaud <clement@unlock-protocol.com>
2022-10-21 14:52:47 +0200

    fix(unlock-js): split the tests functions into their own files (#9977)
    * refactor setup test logic
    
    * better lib factoring
    
    * split logic into functions
    
    * fix itIsERC20 in test
    
    * refactor few more functions
    
    * split all functions out
    
    * imporot ethers from hardhat
    
    * bring back all versions
    
    * pass lock version correctly
    
    * run all tests
    
    * check versions properly
    
    * factor unlock config
    
    * rename functions folder to lock
    
    * update improts
    
    * factor lock creation tests
    
    * update jest config
    
    * add single test runner
    
    * jest task to run a single test file
    
    * remove hardhat upgrade metions
    
    * cli task to run a single test
    
    * add README
    
    * yarn lock
    
    * fix unlock config in wallet service test
    
    * ignore single test runner
    
    * pass args to lock creation
    
    * remove console log
    
    * nice display of names
    
    * add version helpers
    
    * add version check helper
    
    * fix shareKey test for v10+
    
    * fix how lock versions are passed
    
    * move exit code into tasks
    
    * readme  info
    
    * fix version check
    
    * fix v4 run
    
    * no shareKey for v4

---
+++ packages/unlock-js/src/__tests__/integration/lock/shareKeyToTokenId.js
@@ -0,0 +1,47 @@
+import { versionEqualOrAbove } from '../../helpers/integration'
+let walletService, web3Service, lockAddress, accounts, chainId
+
+export default ({ publicLockVersion }) =>
+  () => {
+    if (versionEqualOrAbove(publicLockVersion, 'v10')) {
+      beforeAll(() => {
+        ;({ walletService, web3Service, lockAddress, accounts, chainId } =
+          global.suiteData)
+      })
+      it('should allow a member to share their key with another one', async () => {
+        expect.assertions(3)
+        const tokenId = await walletService.purchaseKey({
+          lockAddress,
+        })
+
+        // Let's now get the duration for that key!
+        const expiration = await web3Service.getKeyExpirationByTokenId(
+          lockAddress,
+          tokenId,
+          chainId
+        )
+        const now = Math.floor(new Date().getTime() / 1000)
+        expect(expiration).toBeGreaterThan(now)
+
+        // Let's now share the key
+        const recipient = '0x6524dBB97462aC3919866b8fbB22BF181D1D4113'
+        const newTokenId = await walletService.shareKey({
+          lockAddress,
+          tokenId,
+          recipient,
+          duration: expiration - now, // share all of the time!
+        })
+
+        const newExpiration = await web3Service.getKeyExpirationByTokenId(
+          lockAddress,
+          newTokenId,
+          chainId
+        )
+        expect(newExpiration).toBeGreaterThanOrEqual(expiration)
+
+        expect(
+          await web3Service.ownerOf(lockAddress, newTokenId, chainId)
+        ).toEqual(recipient)
+      })
+    }
+  }
---
+++ packages/unlock-js/src/__tests__/integration/unlock/lockCreation.js
@@ -0,0 +1,75 @@
+let lockCreationHash,
+  web3Service,
+  lockAddress,
+  publicLockVersion,
+  lock,
+  lockParams,
+  accounts,
+  chainId
+
+export default () => () => {
+  beforeAll(() => {
+    ;({
+      lockCreationHash,
+      web3Service,
+      lockAddress,
+      publicLockVersion,
+      lock,
+      lockParams,
+      accounts,
+      chainId,
+    } = global.suiteData)
+  })
+  it('should have yielded a transaction hash', () => {
+    expect.assertions(1)
+    expect(lockCreationHash).toMatch(/^0x[0-9a-fA-F]{64}$/)
+  })
+
+  it('should have deployed the right lock version', async () => {
+    expect.assertions(1)
+    const lockVersion = await web3Service.lockContractAbiVersion(lockAddress)
+    expect(lockVersion.version).toEqual(publicLockVersion)
+  })
+
+  it('should have deployed the right lock name', () => {
+    expect.assertions(1)
+    expect(lock.name).toEqual(lockParams.name)
+  })
+
+  it('should have deployed the right lock maxNumberOfKeys', () => {
+    expect.assertions(1)
+    expect(lock.maxNumberOfKeys).toEqual(lockParams.maxNumberOfKeys)
+  })
+
+  it('should have deployed the right lock keyPrice', () => {
+    expect.assertions(1)
+    expect(lock.keyPrice).toEqual(lockParams.keyPrice)
+  })
+
+  it('should have deployed the right lock expirationDuration', () => {
+    expect.assertions(1)
+    expect(lock.expirationDuration).toEqual(lockParams.expirationDuration)
+  })
+
+  it('should have deployed the right currency', () => {
+    expect.assertions(1)
+    expect(lock.currencyContractAddress).toEqual(
+      lockParams.currencyContractAddress
+    )
+  })
+
+  it('should have set the creator as a lock manager', async () => {
+    expect.assertions(1)
+    const isLockManager = await web3Service.isLockManager(
+      lockAddress,
+      accounts[0],
+      chainId
+    )
+    expect(isLockManager).toBe(true)
+  })
+
+  it('should have deployed a lock to the right beneficiary', () => {
+    expect.assertions(1)
+    expect(lock.beneficiary).toEqual(accounts[0]) // This is the default in walletService
+  })
+}
