571de2ecec50b637f10e09206f44dbedc086c6fa
Julien Genestoux <julien.genestoux@gmail.com>
2022-10-23 22:46:26 -0400

    Update locksmith/__tests__/controllers/metadataController/tokenData.test.ts

--- locksmith/__tests__/controllers/metadataController/tokenData.test.ts
+++ locksmith/__tests__/controllers/metadataController/tokenData.test.ts
@@ -8,7 +8,6 @@ import { lockTypedData } from '../../test-helpers/typeDataGenerators'
 import app = require('../../../src/app')
 import Base64 = require('../../../src/utils/base64')
 
-jest.setTimeout(60000)
 
 const lockAddress = '0xb0Feb7BA761A31548FF1cDbEc08affa8FFA3e691'
 const lockOwner = '0xAaAdEED4c0B861cB36f4cE006a9C90BA2E43fdc2'
