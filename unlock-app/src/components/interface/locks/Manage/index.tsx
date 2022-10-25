6a5d00cf740491f904749aa4bda7c6f365ee29e9
Kalidou Diagne <kld.diagne@gmail.com>
2022-10-25 16:51:19 +0200

    feat(unlock-app) - replace url generator with link to checkout builder (#10010)
    add checkout builder url

--- unlock-app/src/components/interface/locks/Manage/index.tsx
+++ unlock-app/src/components/interface/locks/Manage/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Drawer, Input } from '@unlock-protocol/ui'
+import { Button } from '@unlock-protocol/ui'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '~/contexts/AuthenticationContext'
@@ -9,14 +9,9 @@ import { TotalBar } from './elements/TotalBar'
 import { FiKey as KeyIcon } from 'react-icons/fi'
 import { BsArrowLeft as ArrowBackIcon } from 'react-icons/bs'
 import { BiLink as LinkIcon } from 'react-icons/bi'
-import { HiOutlineShare as ShareOptionIcon } from 'react-icons/hi'
-import { useForm } from 'react-hook-form'
-import useClipboard from 'react-use-clipboard'
 import { ToastHelper } from '~/components/helpers/toast.helper'
 import { AirdropKeysDrawer } from '~/components/interface/members/airdrop/AirdropDrawer'
-import { useQuery, useMutation } from '@tanstack/react-query'
-import { useWeb3Service } from '~/utils/withWeb3Service'
-import { useConfig } from '~/utils/withConfig'
+import { useMutation } from '@tanstack/react-query'
 import { Container } from '../../Container'
 import { RiPagesLine as PageIcon } from 'react-icons/ri'
 import { FaSpinner as SpinnerIcon } from 'react-icons/fa'
@@ -28,6 +23,7 @@ import { useStorageService } from '~/utils/withStorageService'
 import { useWalletService } from '~/utils/withWalletService'
 import { useLockManager } from '~/hooks/useLockManager'
 import { addressMinify } from '~/utils/strings'
+import Link from 'next/link'
 
 interface ActionBarProps {
   lockAddress: string
@@ -147,148 +143,13 @@ const ActionBar = ({ lockAddress, network }: ActionBarProps) => {
 
 const TopActionBar = ({ lockAddress, network }: TopActionBarProps) => {
   const router = useRouter()
-  const [generatedURL, setGeneratedURL] = useState('')
-  const [isOpen, setIsOpen] = useState(false)
-  const [linkGenerated, setLinkGenerated] = useState(false)
-  const web3Service = useWeb3Service()
-  const config = useConfig()
 
   const DEMO_URL = `/demo?network=${network}&lock=${lockAddress}`
-  const ICON_URL = `${config.services.storage.host}/lock/${lockAddress}/icon`
 
-  const getLock = async () => {
-    return web3Service.getLock(lockAddress, network)
-  }
-
-  const { data: lock } = useQuery(['getLock', lockAddress, network], async () =>
-    getLock()
-  )
-
-  const {
-    register,
-    formState: { isValid, errors },
-    resetField,
-    setValue,
-    handleSubmit,
-  } = useForm({
-    mode: 'onChange',
-    defaultValues: {
-      url: '',
-    },
-  })
-
-  const [isCopied, setCopied] = useClipboard(generatedURL, {
-    successDuration: 2000,
-  })
-
-  const onGenerateURL = async ({ url }: any) => {
-    let recurringPayments
-    if (
-      lock.publicLockVersion >= 10 &&
-      lock.currencyContractAddress &&
-      lock.selfAllowance !== '0'
-    ) {
-      recurringPayments = (365 * 24 * 3600) / lock.expirationDuration
-    }
-
-    const checkoutURLConfig = {
-      locks: {
-        [lockAddress]: {
-          network,
-          recurringPayments,
-        },
-      },
-      pessimistic: true,
-      persistentCheckout: true,
-      icon: ICON_URL,
-    }
-
-    const urlGenerate = new URL('/checkout', window.location.href)
-    urlGenerate.searchParams.append('redirectURI', url)
-    urlGenerate.searchParams.append(
-      'paywallConfig',
-      encodeURIComponent(JSON.stringify(checkoutURLConfig))
-    )
-
-    setValue('url', urlGenerate?.toString())
-    setGeneratedURL(urlGenerate?.toString())
-    setLinkGenerated(true)
-  }
-
-  useEffect(() => {
-    if (!isCopied) return
-    ToastHelper.success(`URL copied`)
-  }, [isCopied])
+  const checkoutLink = `/locks/checkout-url?lock=${lockAddress}&network=${network}`
 
   return (
     <>
-      <Drawer
-        title="Generate Purchase URL"
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      >
-        <div className="flex flex-col h-full gap-10">
-          <span className="text-base">
-            Generate an URL that you can share with your fans and allow them to
-            easily purchase this membership
-          </span>
-          <div>
-            <form onSubmit={handleSubmit(onGenerateURL)}>
-              <Input
-                placeholder="https://example.com"
-                type="url"
-                {...register('url', {
-                  required: true,
-                })}
-              />
-
-              <span className="text-xs leading-none">
-                Enter the URL to which your members are redirected when they
-                have a membership
-              </span>
-              {errors?.url && (
-                <span className="block mt-2 text-xs text-red-700">
-                  Please enter a valid URL
-                </span>
-              )}
-              <div className="flex flex-col gap-3 mt-4">
-                {!linkGenerated ? (
-                  <Button disabled={!isValid} type="submit">
-                    <div className="flex items-center gap-2">
-                      <span className="text-base">Generate</span>
-                      <ShareOptionIcon size={20} />
-                    </div>
-                  </Button>
-                ) : (
-                  <>
-                    <Button
-                      variant="outlined-primary"
-                      disabled={!isValid}
-                      onClick={setCopied}
-                      className="w-full"
-                    >
-                      <div className="flex items-center gap-2 text-brand-ui-primary">
-                        <span className="text-base">Copy</span>
-                        <ShareOptionIcon size={20} />
-                      </div>
-                    </Button>
-                    <Button
-                      variant="transparent"
-                      className="w-full mt-auto"
-                      onClick={() => {
-                        resetField('url')
-                        setLinkGenerated(false)
-                      }}
-                    >
-                      Reset
-                    </Button>
-                  </>
-                )}
-              </div>
-            </form>
-          </div>
-        </div>
-      </Drawer>
       <div className="flex items-center justify-between">
         <Button variant="transparent" className="p-0" aria-label="arrow back">
           <ArrowBackIcon
@@ -306,12 +167,14 @@ const TopActionBar = ({ lockAddress, network }: TopActionBarProps) => {
               </div>
             </a>
           </Button>
-          <Button className="p-3 md:px-6" onClick={() => setIsOpen(true)}>
-            <div className="flex items-center gap-2">
-              <LinkIcon size={15} />
-              <span className="hidden md:block">Generate URL</span>
-            </div>
-          </Button>
+          <Link href={checkoutLink}>
+            <Button className="p-3 md:px-6">
+              <div className="flex items-center gap-2">
+                <LinkIcon size={15} />
+                <span className="hidden md:block">Generate URL</span>
+              </div>
+            </Button>
+          </Link>
         </div>
       </div>
     </>
