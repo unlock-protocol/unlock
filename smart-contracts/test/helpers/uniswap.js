d7957081d612871a649505bd7e3f744685f1c09b
Clément Renaud <clement+git@clementrenaud.com>
2022-10-25 14:14:14 +0200

    simplify uniswap deployment

--- smart-contracts/test/helpers/uniswap.js
+++ smart-contracts/test/helpers/uniswap.js
@@ -1,39 +1,15 @@
 const { ethers } = require('hardhat')
-const { time } = require('@openzeppelin/test-helpers')
-
-const UniswapV2Factory = require('@uniswap/v2-core/build/UniswapV2Factory.json')
-const UniswapV2Router02 = require('@uniswap/v2-periphery/build/UniswapV2Router02.json')
 const UniswapOracle = require('./ABIs/UniswapOracle.json')
 
-const { deployWETH } = require('./tokens')
-const { MAX_UINT } = require('./constants')
+export const UNISWAP_FACTORY_ADDRESS =
+  '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 
-const deployUniswapV2 = async (wethAddress, deployer) => {
-  const signer =
-    typeof deployer === 'string' ? await ethers.getSigner(deployer) : deployer
+// currencies
+export const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+export const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+export const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
 
-  // Deploy Factory
-  const Factory = await ethers.getContractFactory(
-    UniswapV2Factory.abi,
-    UniswapV2Factory.bytecode,
-    signer
-  )
-  const factory = await Factory.deploy(signer.address)
-  await factory.deployed()
-
-  // Deploy Router passing Factory Address and WETH Address
-  const Router = await ethers.getContractFactory(
-    UniswapV2Router02.abi,
-    UniswapV2Router02.bytecode,
-    signer
-  )
-  const router = await Router.deploy(factory.address, wethAddress)
-  await router.deployed()
-
-  return router
-}
-
-const deployUniswapOracle = async (uniswapFactoryAddress, deployer) => {
+const deployUniswapOracle = async (deployer) => {
   const signer =
     typeof deployer === 'string' ? await ethers.getSigner(deployer) : deployer
 
@@ -42,66 +18,12 @@ const deployUniswapOracle = async (uniswapFactoryAddress, deployer) => {
     UniswapOracle.bytecode,
     signer
   )
-  const oracle = await Oracle.deploy(uniswapFactoryAddress)
+  const oracle = await Oracle.deploy(UNISWAP_FACTORY_ADDRESS)
   await oracle.deployed()
 
   return oracle
 }
 
-const createExchange = async ({
-  protocolOwner,
-  minter,
-  udtAddress,
-  amount = ethers.utils.parseEther('1000000'),
-}) => {
-  const udt = await ethers.getContractAt(
-    'UnlockDiscountTokenV3',
-    udtAddress,
-    minter
-  )
-
-  // Deploy the exchange
-  const weth = await deployWETH(protocolOwner)
-  const uniswapRouter = await deployUniswapV2(weth.address, protocolOwner)
-
-  // Create UDT <-> WETH pool
-  await udt.mint(minter.address, amount)
-  await udt.approve(uniswapRouter.address, MAX_UINT)
-
-  await uniswapRouter
-    .connect(minter)
-    .addLiquidityETH(udt.address, amount, '1', '1', minter.address, MAX_UINT, {
-      value: ethers.utils.parseEther('40', 'ether'),
-    })
-
-  const uniswapOracle = await deployUniswapOracle(
-    await uniswapRouter.factory(),
-    protocolOwner
-  )
-
-  // Advancing time to avoid an intermittent test fail
-  await time.increase(time.duration.hours(1))
-
-  // Do a swap so there is some data accumulated
-  await uniswapRouter
-    .connect(minter)
-    .swapExactETHForTokens(
-      1,
-      [weth.address, udt.address],
-      minter.address,
-      MAX_UINT,
-      { value: ethers.utils.parseEther('1') }
-    )
-
-  return {
-    router: uniswapRouter,
-    oracle: uniswapOracle,
-    weth,
-  }
-}
-
 module.exports = {
-  deployUniswapV2,
   deployUniswapOracle,
-  createExchange,
 }
