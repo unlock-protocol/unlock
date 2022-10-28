60d9370abe26f945405b0a15932849bd8106fdb0
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-10-28 21:08:18 +0530

    feat(locksmith): Pass down default lock metadata to generated key metadata properly (#10089)

--- locksmith/src/utils/keyData.ts
+++ locksmith/src/utils/keyData.ts
@@ -1,50 +1,34 @@
-import { Web3Service } from '@unlock-protocol/unlock-js'
-import networks from '@unlock-protocol/networks'
-
+import { SubgraphService } from '@unlock-protocol/unlock-js'
 import logger from '../logger'
 
-interface Key {
-  owner?: string
-  expiration?: number
-}
+type Data = Partial<Record<'expiration' | 'tokenId' | 'owner', string>>
 export default class KeyData {
   async get(lockAddress: string, tokenId: string, network: number) {
-    const web3Service = new Web3Service(networks)
-    const key: Key = {}
-
     try {
-      const owner = await web3Service.ownerOf(lockAddress, tokenId, network)
-      if (!owner) {
-        return {}
+      const subgraphClient = new SubgraphService()
+      const key = await subgraphClient.key(
+        {
+          where: {
+            lock: lockAddress.toLowerCase(),
+            tokenId: Number(tokenId),
+          },
+        },
+        {
+          network,
+        }
+      )
+      const data: Data = {
+        expiration: key?.expiration,
+        tokenId: key?.tokenId,
+        owner: key?.owner,
       }
-      key.owner = owner
-
-      const lock = await web3Service.getLock(lockAddress, network)
-      let expiration: number | undefined
-
-      if (lock.publicLockVersion >= 10) {
-        expiration = await web3Service.getKeyExpirationByTokenId(
-          lockAddress,
-          tokenId,
-          network
-        )
-      } else {
-        expiration = await web3Service.getKeyExpirationByLockForOwner(
-          lockAddress,
-          owner,
-          network
-        )
-      }
-
-      key.expiration = expiration
-
-      return key
+      return data
     } catch (error) {
       logger.error(
         `There was an error retrieving info for metadata ${lockAddress} ${tokenId} on ${network}`,
         error
       )
-      return key
+      return {} as Data
     }
   }
 
