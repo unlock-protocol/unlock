2629fd7ddaf37b8a215d776cd35da71b566ba5c4
m00n620 <gex6611@gmail.com>
2022-11-09 08:31:11 -0500

    feat: subgraph updates for State

--- subgraph/tests/mocks.ts
+++ subgraph/tests/mocks.ts
@@ -14,6 +14,7 @@ import {
   baseTokenURI,
   maxNumberOfKeys,
   maxKeysPerAddress,
+  now,
 } from './constants'
 
 createMockedFunction(
@@ -113,6 +114,14 @@ createMockedFunction(
 )
   .withArgs([ethereum.Value.fromUnsignedBigInt(BigInt.fromU32(tokenId))])
   .returns([ethereum.Value.fromUnsignedBigInt(BigInt.fromU64(expiration))])
+
+createMockedFunction(
+  Address.fromString(lockAddress),
+  'createdAt',
+  'createdAt():(uint256)'
+)
+  .withArgs([])
+  .returns([ethereum.Value.fromUnsignedBigInt(BigInt.fromU32(now))])
 
 /**
  * Mocks function for < v10 locks
