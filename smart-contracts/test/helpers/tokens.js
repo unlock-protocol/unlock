58eae281dd0165e70107005a10b65a340ad78c19
Clément Renaud <clement+git@clementrenaud.com>
2022-11-09 14:27:17 +0100

    bring back deployWETH

--- smart-contracts/test/helpers/tokens.js
+++ smart-contracts/test/helpers/tokens.js
@@ -1,4 +1,5 @@
 const { ethers } = require('hardhat')
+const WethABI = require('./ABIs/weth.json')
 const TestERC20 = artifacts.require('TestERC20')
 
 const deployERC20 = async (deployer, isEthers = false) => {
@@ -15,6 +16,19 @@ const deployERC20 = async (deployer, isEthers = false) => {
   return TestERC20.at(token.address)
 }
 
+const deployWETH = async (deployer) => {
+  const { abi, bytecode } = WethABI
+  const signer =
+    typeof deployer === 'string' ? await ethers.getSigner(deployer) : deployer
+  const WETH = new ethers.ContractFactory(abi, bytecode, signer)
+  const weth = await WETH.deploy()
+  await weth.deployed()
+
+  // return truffle artifact for now
+  return weth
+}
+
 module.exports = {
   deployERC20,
+  deployWETH,
 }
