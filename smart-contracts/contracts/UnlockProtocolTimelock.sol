6b5692620882e254c70923ed034af06d2d47cd1a
Clement Renaud <clement@clementrenaud.com>
2022-11-14 20:15:56 +0100

    Revert "update OZ timelock  api call"
    This reverts commit 72a01d1df0cf08e18a6a9aec815e30288c16b08a.

--- smart-contracts/contracts/UnlockProtocolTimelock.sol
+++ smart-contracts/contracts/UnlockProtocolTimelock.sol
@@ -4,24 +4,11 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol';
 
 contract UnlockProtocolTimelock is TimelockControllerUpgradeable {
-  /**
-    * @dev Initializes the contract with the following parameters:
-    *
-    * - `minDelay`: initial minimum delay for operations
-    * - `proposers`: accounts to be granted proposer and canceller roles
-    * - `executors`: accounts to be granted executor role
-    * - `admin`: optional account to be granted admin role; disable with zero address
-    *
-    * IMPORTANT: The optional admin can aid with initial configuration of roles after deployment
-    * without being subject to delay, but this role should be subsequently renounced in favor of
-    * administration through timelocked proposals. Previous versions of this contract would assign
-    * this admin to the deployer automatically and should be renounced as well.
-    */
   function initialize (
     uint256 minDelay,
     address[] memory proposers,
     address[] memory executors
   ) public initializer {
-    __TimelockController_init(minDelay, proposers, executors, address(0));
+    __TimelockController_init(minDelay, proposers, executors);
   }
 }
