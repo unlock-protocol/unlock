3383d28d50b67929d04119b24914e3855652a88a
Solomon Foskaay <83863629+SolomonFoskaay@users.noreply.github.com>
2022-11-15 16:48:23 +0100

    Fixed Misspellings on Navigation Page

--- unlock-protocol-com/src/components/interface/Navigation/index.tsx
+++ unlock-protocol-com/src/components/interface/Navigation/index.tsx
@@ -271,8 +271,7 @@ function CommunityPopover() {
                         <p className="text-sm font-bold"> Community </p>
                       </Link>
                       <p className="text-sm text-brand-gray">
-                        Join community of thousands of developers, creators, and
-                        governants building the future of Unlock!
+                        Join a community of thousands of developers and creators building the future of Unlock!
                       </p>
                     </div>
                   </header>
