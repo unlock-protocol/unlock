1697541ee7d8c636020f05907bf28a3916576c73
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-11-17 02:01:43 +0530

    Add captcha middleware

--- locksmith/src/routes/v2/contracts.ts
+++ locksmith/src/routes/v2/contracts.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { ContractsController } from '../../controllers/v2/contractsController'
 import { authMiddleware } from '../../utils/middlewares/auth'
+import { captchaMiddleware } from '../../utils/middlewares/recaptchaMiddleware'
 
 const router = express.Router({ mergeParams: true })
 
 const contractsController = new ContractsController()
 
-router.post('/:network/lock', authMiddleware, (req, res) => {
+router.post('/:network/lock', authMiddleware, captchaMiddleware, (req, res) => {
   contractsController.createLockContract(req, res)
 })
 
