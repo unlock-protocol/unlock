8b13586386f95baa3c8caa0c5857d3d6d1e5e225
Clément Renaud <clement+git@clementrenaud.com>
2022-11-22 14:13:27 +0100

    use bridges sender/receiver contracts

--- smart-contracts/contracts/Unlock.sol
+++ smart-contracts/contracts/Unlock.sol
@@ -364,38 +364,25 @@ contract Unlock is UnlockInitializable, UnlockOwnable {
     return lockAddress;
   }
 
-  function setConnext(address _connextAddress) public {
-    connextAddress = _connextAddress;
+  function _isLockManager(address lockAddress, address _sender) private view returns(bool isManager) {
+    IPublicLock lock = IPublicLock(lockAddress);
+    return lock.isLockManager(_sender);
   }
 
-  function getDomain(uint chainId) public returns (uint32 domain){
-    // parse domain
-    return domain;
+  function setBridgeSenderAddress (address _bridgeAddress) public onlyOwner {
+    bridgeAddress = _bridgeAddress;
   }
 
-  /**
-   * @notice Purchase a key on another chain
-   * Purchase a key from a lock on another chain
-   * @param destChainId: the chain id on which the lock is located
-   * @param lock: address of the lock that the user is attempting to purchase a key from
-   * @param currency : address of the token to be swapped into the lock’s currency
-   * @param amount: the *maximum a*mount of `currency` the user is willing to spend in order to complete purchase. (The user needs to have ERC20 approved the Unlock contract for *at least* that amount).
-   * @param callData: blob of data passed to the lock that includes the following:
-   * @param relayerFee The fee offered to connext relayers. On testnet, this can be 0.
-   * @dev to construct the callData you need the following parameter
-      - `recipients`: address of the recipients of the membership
-      - `referrers`: address of the referrers
-      - `keyManagers`: address of the key managers
-      - `callData`: bytes passed to the purchase function function of the lock
-   */
-  function crossChainPurchase(
+  function sendBridgedLockCall(
     uint destChainId, 
     address lock, 
     address currency, 
     uint amount, 
     bytes calldata callData,
-    uint relayerFee 
-  ) public {
+    uint relayerFee
+  ) public payable {
+    // value to forward to the bridge
+    uint value = msg.value + relayerFee;
 
     // prepare ERC20 if needed
     if (currency != address(0)) {
