34ae505a37875a0c9514f1491636ba01f2ec8981
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-11-23 21:44:49 +0530

    disable webpack

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -27,26 +27,10 @@ for (const [key, value] of Object.entries(requiredEnvs)) {
 
 const config = {
   sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
     hideSourceMaps: true,
   },
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
-module.exports = withSentryConfig(config, sentryWebpackPluginOptions)
+module.exports = withSentryConfig(config)
