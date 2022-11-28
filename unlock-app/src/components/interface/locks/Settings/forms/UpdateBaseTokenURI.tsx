736f6f8a014b801abb010935060b92c55e2a59af
Kalidou Diagne <kld.diagne@gmail.com>
2022-11-28 15:59:24 +0100

    settings page base token uri

---
+++ unlock-app/src/components/interface/locks/Settings/forms/UpdateBaseTokenURI.tsx
@@ -0,0 +1,107 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { Input, Button } from '@unlock-protocol/ui'
+import { useForm } from 'react-hook-form'
+import { ToastHelper } from '~/components/helpers/toast.helper'
+import { useWalletService } from '~/utils/withWalletService'
+import { useWeb3Service } from '~/utils/withWeb3Service'
+
+interface UpdateBaseTokenURIProps {
+  lockAddress: string
+  network: number
+  isManager: boolean
+  disabled: boolean
+}
+
+interface FormProps {
+  baseTokenURI: string
+}
+
+export const UpdateBaseTokenURI = ({
+  lockAddress,
+  network,
+  disabled,
+  isManager,
+}: UpdateBaseTokenURIProps) => {
+  const web3Service = useWeb3Service()
+  const walletService = useWalletService()
+
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    formState: { errors },
+  } = useForm<FormProps>()
+
+  const getTokenURI = async () => {
+    return await web3Service.tokenURI(lockAddress, '1', network)
+  }
+
+  const setBaseTokenURI = async (fields: FormProps) => {
+    await walletService.setBaseTokenURI({
+      lockAddress,
+      baseTokenURI: fields.baseTokenURI,
+    })
+  }
+
+  const setBaseTokenURIMutation = useMutation(setBaseTokenURI)
+
+  const onSetBaseTokenURI = async (fields: FormProps) => {
+    const setBaseTokenURIPromise = setBaseTokenURIMutation.mutateAsync(fields)
+    await ToastHelper.promise(setBaseTokenURIPromise, {
+      error: 'Failed to update base token URI.',
+      success: 'URI successfully updated.',
+      loading: 'Updating URI.',
+    })
+  }
+
+  const { isLoading: isLoadingTokenURI, data: tokenURI } = useQuery(
+    ['getTokenURI', lockAddress, network, setBaseTokenURIMutation.isSuccess],
+    async () => getTokenURI(),
+    {
+      onSuccess: (value: string) => {
+        setValue('baseTokenURI', value)
+      },
+    }
+  )
+
+  const disabledInput =
+    disabled || setBaseTokenURIMutation.isLoading || isLoadingTokenURI
+  const isLoading = setBaseTokenURIMutation.isLoading || disabledInput
+
+  console.log(tokenURI)
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSetBaseTokenURI)}
+      className="flex flex-col gap-6"
+    >
+      <div className="relative">
+        <Input
+          type="url"
+          label="Base token URI"
+          {...register('baseTokenURI', {
+            minLength: 1,
+            required: true,
+          })}
+          autoComplete="off"
+          disabled={disabledInput}
+          error={
+            errors?.baseTokenURI &&
+            'Base token URI should have at least 1 character.'
+          }
+        />
+      </div>
+
+      {isManager && (
+        <Button
+          type="submit"
+          className="w-full md:w-1/3"
+          disabled={disabledInput}
+          loading={setBaseTokenURIMutation.isLoading || isLoading}
+        >
+          Update
+        </Button>
+      )}
+    </form>
+  )
+}
