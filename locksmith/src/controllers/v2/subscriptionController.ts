bb0733f590753e963f41b8b0b54ab144d1433891
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-11-29 20:16:02 +0530

    change stuff

--- locksmith/src/controllers/v2/subscriptionController.ts
+++ locksmith/src/controllers/v2/subscriptionController.ts
@@ -1,6 +1,16 @@
 import { Response, Request } from 'express-serve-static-core'
 import { KeySubscription } from '../../models'
 import normalizer from '../../utils/normalizer'
+import {
+  SubgraphService,
+  Web3Service,
+  getErc20BalanceForAddress,
+  getErc20Decimals,
+  getAllowance,
+} from '@unlock-protocol/unlock-js'
+import networks from '@unlock-protocol/networks'
+import { ethers } from 'ethers'
+import { Op } from 'sequelize'
 
 export class SubscriptionController {
   async getSubscription(request: Request, response: Response) {
@@ -8,16 +18,92 @@ export class SubscriptionController {
     const lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
     const keyId = Number(request.params.keyId)
     const userAddress = normalizer.ethereumAddress(request.user!.walletAddress)
+    const subgraphService = new SubgraphService(networks)
+
+    const key = await subgraphService.key(
+      {
+        where: {
+          tokenId: keyId.toString(),
+          lock: lockAddress.toLowerCase(),
+          owner: userAddress.toLowerCase(),
+        },
+      },
+      {
+        network,
+      }
+    )
+
+    // If no key is found or not erc20 or version < 11 which we don't fully support, return nothing.
+    if (
+      !key ||
+      key.lock.tokenAddress === ethers.constants.AddressZero ||
+      parseInt(key.lock.version) < 11
+    ) {
+      return response.status(200).send({})
+    }
+
+    const web3Service = new Web3Service(networks)
+    const provider = web3Service.providerForNetwork(network)
+    const [userBalance, decimals, userAllowance] = await Promise.all([
+      getErc20BalanceForAddress(key.lock.tokenAddress, userAddress, provider),
+      getErc20Decimals(key.lock.tokenAddress, provider),
+      getAllowance(
+        key.lock.tokenAddress,
+        key.lock.address,
+        provider,
+        userAddress
+      ),
+    ])
+
+    const balance = ethers.utils.formatUnits(userBalance, decimals)
+
+    const price = key.lock.price
+    const next = parseInt(key.expiration)
+
+    // Approved renewals
+    const approvedRenewals = userAllowance.eq(ethers.constants.MaxUint256)
+      ? userAllowance.toString()
+      : userAllowance.div(price).toString()
+
+    const info = {
+      next,
+      balance,
+      price: ethers.utils.formatUnits(price, decimals),
+    }
+
     const subscription = await KeySubscription.findOne({
       where: {
         keyId,
         lockAddress,
         network,
         userAddress,
+        recurring: {
+          [Op.gt]: 0,
+        },
       },
     })
 
-    return response.status(200).send(subscription?.toJSON() ?? {})
+    // if card subscription is found, return that.
+    if (subscription) {
+      return response.status(200).send({
+        ...info,
+        approvedRenewals: subscription.recurring,
+        possibleRenewals: subscription.recurring,
+        type: 'fiat',
+      })
+    }
+
+    // Otherwise return the details on crypto subscription if any.
+    const result = {
+      ...info,
+      approvedRenewals: approvedRenewals,
+      possibleRenewals: ethers.BigNumber.from(userBalance)
+        .div(price)
+        .toString(),
+      type: 'crypto',
+    }
+
+    return response.status(200).send(result)
   }
 
   async cancelSubscription(request: Request, response: Response) {
