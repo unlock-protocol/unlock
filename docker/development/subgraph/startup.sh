017319d27042bbac72bcdae97589c74ea984bc30
Clément Renaud <clement@unlock-protocol.com>
2022-12-07 14:01:21 +0100

    fix(subgraph): remove generated files from repo (#10533)
    * fix typo in subgraph
    
    * untrack ABIs and networks file
    
    * update build command
    
    * add prepare to ci
    
    * build with custom localhost on integration tests

--- docker/development/subgraph/startup.sh
+++ docker/development/subgraph/startup.sh
@@ -5,11 +5,12 @@ echo "Migrating subgraph..."
 # show networks
 cat /home/unlock/subgraph/networks.json
 
-# generate ts code 
+# generate ts code from ABIs
+yarn workspace @unlock-protocol/subgraph prepare:abis
 yarn workspace @unlock-protocol/subgraph codegen
 
 # build the subgraph files
-yarn workspace @unlock-protocol/subgraph graph build --network localhost
+yarn workspace @unlock-protocol/subgraph graph build:graph --network localhost
 
 # init the graph
 yarn workspace @unlock-protocol/subgraph run graph create testgraph --node http://graph-node:8020/ --version 0.0.1
