de59307723bb49944d57c6f5af9c7ebbd65cb5b2
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-07 16:50:44 +0100

    fix

--- unlock-app/src/components/interface/locks/Settings/elements/SettingTerms.tsx
+++ unlock-app/src/components/interface/locks/Settings/elements/SettingTerms.tsx
@@ -59,7 +59,8 @@ export const SettingTerms = ({
     },
     {
       label: 'Maximum number of keys per address',
-      description: 'The maximum number of keys a specific address can own. By default, a given address can only own ',
+      description:
+        'The maximum number of keys a specific address can own. By default, a given address can only own ',
       children: (
         <UpdateMaxKeysPerAddress
           isManager={isManager}
