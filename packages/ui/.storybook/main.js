561721d85cac0aebaac75ae1bf5b457837ce8846
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-12-09 21:57:03 +0530

    downgrade svgr due to webpack

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -17,17 +17,24 @@ module.exports = {
   staticDirs: ['../public'],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
-    config.module.rules = [
-      ...(config.resolve.rules || []),
-      {
-        test: /\.scss$/,
-        use: ['@svgr/webpack'],
-      },
-    ]
-
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin(),
+    ]
+
+    config.module.rules = [
+      ...config.module.rules.map((rule) => {
+        if (/svg/.test(rule.test)) {
+          // Silence the Storybook loaders for SVG files
+          return { ...rule, exclude: /\.svg$/i }
+        }
+        return rule
+      }),
+      // Add your custom SVG loader
+      {
+        test: /\.svg$/i,
+        use: ['@svgr/webpack'],
+      },
     ]
 
     return config
