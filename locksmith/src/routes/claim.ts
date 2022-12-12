dfda43d2848e5efc6ad583422812ab687aac0c05
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-12-13 04:49:32 +0530

    feat(locksmith): Add a geo restriction middleware (#10570)

--- locksmith/src/routes/claim.ts
+++ locksmith/src/routes/claim.ts
@@ -3,12 +3,17 @@ import signatureValidationMiddleware from '../middlewares/signatureValidationMid
 import { PurchaseController } from '../controllers/purchaseController'
 import { SignedRequest } from '../types'
 import { captchaMiddleware } from '../utils/middlewares/recaptchaMiddleware'
+import { createGeoRestriction } from '../utils/middlewares/geoRestriction'
 
 const purchaseController = new PurchaseController()
 const router = express.Router({ mergeParams: true })
 
+// Disallow claim due to spam and bot activity
+const geoRestriction = createGeoRestriction(['RU', 'UA'])
+
 router.post(
   '/',
+  geoRestriction,
   captchaMiddleware,
   signatureValidationMiddleware.generateProcessor({
     name: 'Claim Membership',
@@ -17,10 +22,11 @@ router.post(
   })
 )
 
-router.get('/:network/locks/:lockAddress', (req, res) =>
+router.get('/:network/locks/:lockAddress', geoRestriction, (req, res) =>
   purchaseController.canClaim(req, res)
 )
-router.post('/', (req, res) =>
+
+router.post('/', geoRestriction, (req, res) =>
   purchaseController.claim(req as SignedRequest, res)
 )
 
