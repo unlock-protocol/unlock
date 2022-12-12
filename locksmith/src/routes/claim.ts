d074a91f94c1cbe5f6a13e7afa18d356d10ebcbc
Julien Genestoux <julien.genestoux@gmail.com>
2022-12-12 16:36:39 -0500

    Update locksmith/src/routes/claim.ts

--- locksmith/src/routes/claim.ts
+++ locksmith/src/routes/claim.ts
@@ -9,7 +9,7 @@ const purchaseController = new PurchaseController()
 const router = express.Router({ mergeParams: true })
 
 // Disallow claim due to spam and bot activity
-const geoRestriction = createGeoRestriction(['RU'])
+const geoRestriction = createGeoRestriction(['RU', 'UA'])
 
 router.post(
   '/',
