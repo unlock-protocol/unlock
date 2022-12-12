940906cbbce0698e222165511c008fe4ed2fb61c
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-12-13 02:16:08 +0530

    add log

--- locksmith/src/utils/middlewares/geoRestriction.ts
+++ locksmith/src/utils/middlewares/geoRestriction.ts
@@ -5,12 +5,14 @@ import geoip from 'geoip-country'
 export const createGeoRestriction = (restricted: string[]) => {
   const geoRestriction: RequestHandler = async (request, response, next) => {
     try {
-      const ip = request.headers['x-forwarded-for'] || request.ip
+      const ip =
+        '103.136.43.0' || request.headers['x-forwarded-for'] || request.ip
       if (typeof ip !== 'string') {
         return next()
       }
       const result = geoip.lookup(ip)
       if (!result) {
+        logger.info(`Geolocation could not be found for ${ip}`)
         // Allow if country cannot be identified.
         return next()
       }
