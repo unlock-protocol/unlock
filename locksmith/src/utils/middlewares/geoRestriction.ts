b15d15eff7f8c18d4b1986e3765e5d89d3c2179f
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-12-13 01:49:09 +0530

    feat(locksmith): Add a geo restriction middleware

---
+++ locksmith/src/utils/middlewares/geoRestriction.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from 'express'
+import logger from '../../logger'
+import geoip from 'geoip-country'
+
+export const createGeoRestriction = (restricted: string[]) => {
+  const geoRestriction: RequestHandler = async (request, response, next) => {
+    try {
+      const ip = request.headers['x-forwarded-for'] || request.ip
+      if (typeof ip !== 'string') {
+        return next()
+      }
+      const result = geoip.lookup(ip)
+      if (!result) {
+        // Allow if country cannot be identified.
+        return next()
+      }
+      if (restricted.includes(result.country)) {
+        return response.status(403).send({
+          message: 'Access denied.',
+        })
+      }
+      return next()
+    } catch (error) {
+      // Let it be optimistic and pass if the database is not available or check fails.
+      logger.error(error)
+      return next()
+    }
+  }
+  return geoRestriction
+}
