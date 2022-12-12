182a0aa362b48f8d69f18186a2ad7754ceb5def9
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-12 13:03:23 +0100

    fix import for component

--- packages/ui/lib/components/HeaderNav/HeaderNav.stories.tsx
+++ packages/ui/lib/components/HeaderNav/HeaderNav.stories.tsx
@@ -1,4 +1,4 @@
-import HeaderNav from './HeaderNav'
+import { HeaderNav } from './HeaderNav'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { CgArrowLongRight as ArrowRight } from 'react-icons/cg'
 
