0f23419433f75bdf45482402eb290ed47883e9ac
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-13 18:01:16 +0100

    wip

--- tests/test/e2e/helper.js
+++ tests/test/e2e/helper.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const dappeteer = require('@chainsafe/dappeteer')
 
+// todo: use env
 const E2E_METAMASK_SEED =
   'tiny question spoil sugar alcohol harsh high approve energy relief lobster trade'
 const E2E_METAMASK_PASSWORD = 'password'
