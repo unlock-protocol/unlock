2fc0b20e0949bd555db82992ade60473ee337763
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-14 16:20:01 +0100

    feat(unlock-protocol-com) - use footer component (#10574)
    * use footer component

--- unlock-protocol-com/src/components/layout/DefaultLayout.tsx
+++ unlock-protocol-com/src/components/layout/DefaultLayout.tsx
@@ -1,5 +1,8 @@
+import { Footer } from '@unlock-protocol/ui'
 import { ReactNode } from 'react'
-import Footer from '../interface/Footer'
+import { EMAIL_SUBSCRIPTION_FORM } from '../../config/constants'
+import { unlockConfig } from '../../config/unlock'
+import { useMembership } from '../../hooks/useMembership'
 import { Navigation } from '../interface/Navigation'
 
 interface Props {
@@ -7,11 +10,115 @@ interface Props {
 }
 
 export function Layout({ children }: Props) {
+  const { becomeMember } = useMembership()
+
+  const FOOTER = {
+    subscriptionForm: {
+      title: 'Sign up for Updates',
+      description:
+        'Receive fresh news about Unlock, including new features and opportunities to contribute',
+      onSubmit: async (email: string) => {
+        const { portalId, formGuid } = EMAIL_SUBSCRIPTION_FORM
+        const endpoint = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            fields: [
+              {
+                name: 'email',
+                value: email,
+              },
+            ],
+          }),
+        }
+
+        await fetch(endpoint, options)
+      },
+    },
+    logo: {
+      url: 'https://unlock-protocol.com/',
+    },
+    privacyUrl: '/privacy',
+    termsUrl: '/terms',
+    actions: [
+      {
+        label: 'Launch App',
+        url: `${unlockConfig.appURL!}/locks`,
+      },
+      {
+        label: 'Get Unlock Membership',
+        onClick: async () => {
+          await becomeMember()
+        },
+      },
+    ],
+    menuSections: [
+      {
+        title: 'About',
+        options: [
+          { label: 'About Unlock', url: '/about' },
+          {
+            label: 'Roadmap',
+            url: 'https://docs.unlock-protocol.com/governance/roadmap/',
+          },
+          {
+            label: 'Careers',
+            url: 'https://www.notion.so/unlockprotocol/Unlock-Jobs-907811d15c4d490091eb298f71b0954c',
+          },
+        ],
+      },
+      {
+        title: 'Governance',
+        options: [
+          {
+            label: 'Unlock DAO',
+            url: 'https://unlock-protocol.com/blog/unlock-dao',
+          },
+          { label: 'Forum', url: 'https://unlock.community/' },
+          {
+            label: 'Snapshot',
+            url: 'https://snapshot.org/#/unlock-protocol.eth',
+          },
+        ],
+      },
+      {
+        title: 'Community',
+        options: [
+          { label: 'Showcase', url: 'https://showcase.unlock-protocol.com/' },
+          { label: 'Blog', url: '/blog' },
+          { label: 'Events', url: '/upcoming-events' },
+          { label: 'Grants', url: '/grants' },
+        ],
+      },
+      {
+        title: 'Resources',
+        options: [
+          { label: 'Docs', url: 'https://docs.unlock-protocol.com/' },
+          { label: 'Developers', url: '/developers' },
+          { label: 'Guides', url: '/guides' },
+          {
+            label: 'Integrations',
+            url: 'https://docs.unlock-protocol.com/move-to-guides/plugins-and-integrations/',
+          },
+          {
+            label: 'Media kit',
+            url: 'https://unlockprotocol.notion.site/Press-Kit-35836bdcc88f400eb5bb429c477c3333',
+          },
+        ],
+      },
+    ],
+  }
+
   return (
     <>
       <Navigation />
       {children}
-      <Footer />
+      <div className="container px-6 py-4 mx-auto md:px-0 md:py-14 max-w-7xl">
+        <Footer {...FOOTER} />
+      </div>
     </>
   )
 }
