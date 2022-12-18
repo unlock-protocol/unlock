2f6a0bbf623e827227f48be8f579677d2732cfbc
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-12-18 05:34:15 +0530

    feat(locksmith): Add script to generate access token for local testing (#10601)

---
+++ locksmith/scripts/create_access_token.ts
@@ -0,0 +1,28 @@
+import { createAccessToken } from '../src/utils/middlewares/auth'
+
+import yargs from 'yargs'
+
+const argv = yargs
+  .option('address', {
+    requiresArg: true,
+    type: 'string',
+    alias: 'l',
+    description: 'Wallet address to use.',
+  })
+
+  .help()
+  .alias('h', 'help')
+  .example(
+    'yarn tsx scripts/create_access_token.ts --address 0x123',
+    'create an access token to test locksmith locally.'
+  ).argv
+
+if (argv.address) {
+  const token = createAccessToken({
+    type: 'user',
+    walletAddress: argv.address,
+  })
+  console.log(token)
+} else {
+  console.log(argv)
+}
