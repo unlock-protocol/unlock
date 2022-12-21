a81cfeb7b1992af322e472174e6b33307f3a5634
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-21 18:37:35 +0100

    header height and color adjustments

--- packages/ui/lib/components/HeaderNav/HeaderNav.tsx
+++ packages/ui/lib/components/HeaderNav/HeaderNav.tsx
@@ -97,7 +97,7 @@ const NavImageItem = ({ title, src, alt, url }: NavbarImageProps) => {
   return (
     <Link href={url} className="flex flex-col gap-4 group">
       <div
-        className="overflow-hidden bg-center bg-cover rounded-3xl h-60"
+        className="h-40 overflow-hidden bg-center bg-cover border rounded-3xl border-ui-main-100"
         style={{
           backgroundImage: `url(${src})`,
         }}
@@ -229,7 +229,7 @@ const NavSectionDesktop = (section: MenuSectionProps) => {
             >
               <Popover.Panel className="absolute z-10 w-full pt-4 transform -translate-x-1/2 md:pt-9 left-1/2 sm:px-0">
                 <div className="overflow-hidden border shadow-lg rounded-3xl">
-                  <div className="relative grid gap-8 px-10 py-8 bg-white">
+                  <div className="relative grid gap-8 px-10 py-8 bg-ui-secondary-100">
                     <Navbar />
                   </div>
                 </div>
