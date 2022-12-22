0c0fe9d975018bff7b1bca88914f6ef0d6e8a313
Kalidou Diagne <kld.diagne@gmail.com>
2022-12-22 11:16:06 +0000

    feat(ui) - header adjustments (#10626)
    * header adjustments

--- packages/ui/lib/components/HeaderNav/HeaderNav.stories.tsx
+++ packages/ui/lib/components/HeaderNav/HeaderNav.stories.tsx
@@ -8,7 +8,7 @@ export default {
 } as ComponentMeta<typeof HeaderNav>
 
 const Template: ComponentStory<typeof HeaderNav> = (args) => (
-  <div className="container mx-auto md:px-40">
+  <div className="container mx-auto">
     <HeaderNav {...args} />
   </div>
 )
@@ -22,48 +22,24 @@ Normal.args = {
   logo: {
     url: 'https://unlock-protocol.com/',
   },
-  actions: [
-    {
-      title: 'Launch App',
-      url: 'https://app.unlock-protocol.com',
-      icon: ArrowRight,
-    },
-  ],
   menuSections: [
     {
       title: 'Devs',
       options: [
         {
-          title: 'Watch Tutorials',
-          src: 'https://i.ytimg.com/vi/mq7xYFWe_yw/maxresdefault.jpg',
-          url: 'https://google.it',
-        },
-        {
           title: 'Core Protocol',
           options: [
-            { title: 'Unlock Smart Contracts', url: 'https://google.it' },
-            { title: 'Deploying Locks', url: 'https://google.it' },
-            { title: 'Hooks', url: 'https://google.it' },
-          ],
-        },
-        {
-          title: 'Integrations',
-          options: [
             {
-              title: 'Shopify',
-              url: 'https://google.it',
+              title: 'Unlock Smart Contracts',
+              url: 'https://docs.unlock-protocol.com/core-protocol/',
             },
             {
-              title: 'Discourse',
-              url: 'https://google.it',
+              title: 'Deploying Locks',
+              url: 'https://docs.unlock-protocol.com/core-protocol/public-lock/deploying-locks/',
             },
             {
-              title: 'Guild.xyz',
-              url: 'https://google.it',
-            },
-            {
-              title: 'Wordpress',
-              url: 'https://google.it',
+              title: 'Hooks',
+              url: 'https://docs.unlock-protocol.com/core-protocol/public-lock/hooks',
             },
           ],
         },
@@ -71,49 +47,83 @@ Normal.args = {
           title: 'Tools',
           options: [
             {
-              title: 'Building token gated applications',
-              url: 'https://google.it',
+              title: 'Unlock-js',
+              url: 'https://docs.unlock-protocol.com/tools/unlock.js',
             },
             {
               title: 'Sign in with Ethereum',
-              url: 'https://google.it',
-            },
-            {
-              title: 'Webhooks',
-              url: 'https://google.it',
+              url: 'https://docs.unlock-protocol.com/tools/sign-in-with-ethereum/',
             },
             {
               title: 'Subgraph',
-              url: 'https://google.it',
+              url: 'https://docs.unlock-protocol.com/tutorials/misc/using-subgraphs/',
             },
           ],
+        },
+        {
+          title: 'Integrations',
+          options: [
+            {
+              title: 'Shopify',
+              url: 'https://github.com/pwagner/unlock-shopify-app',
+            },
+            {
+              title: 'Discourse',
+              url: 'https://unlock-protocol.com/guides/unlock-discourse-plugin/',
+            },
+            {
+              title: 'Guild.xyz',
+              url: 'https://unlock-protocol.com/guides/guild-xyz/',
+            },
+            {
+              title: 'Wordpress',
+              url: 'https://unlock-protocol.com/guides/guide-to-the-unlock-protocol-wordpress-plugin/',
+            },
+          ],
+        },
+        {
+          title: 'Watch Tutorials',
+          embed:
+            '<iframe src="https://plugins.flockler.com/embed/iframe/18462b3104603cf8399278dfcbdfdad8/1850c9aba1e022ab838f498cc9cff0ee" height="800" style="display: block; border: none; width: 100%;" allowfullscreen> </iframe>',
         },
       ],
     },
     {
-      title: 'Creator ',
+      title: 'Creator',
       options: [
         {
           title: 'Create Digital Collectibles with Unlock Protocol',
-          src: 'https://i.ytimg.com/vi/mq7xYFWe_yw/maxresdefault.jpg',
-          url: 'https://google.it',
+          src: '/images/marketing/collectible.png',
+          url: 'https://unlock-protocol.com/guides/how-to-use-different-images/',
         },
         {
           title: 'How to Sell NFT Tickets for an Event',
-          src: 'https://i.ytimg.com/vi/mq7xYFWe_yw/maxresdefault.jpg',
-          url: 'https://google.it',
+          src: '/images/marketing/event.png',
+          url: 'https://unlock-protocol.com/guides/how-to-sell-nft-tickets-for-an-event/',
         },
         {
           title: 'Recurring Memberships',
-          src: 'https://i.ytimg.com/vi/mq7xYFWe_yw/maxresdefault.jpg',
-          url: 'https://google.it',
+          src: '/images/marketing/recurring.png',
+          url: 'https://unlock-protocol.com/guides/recurring-memberships/',
         },
         {
           title: 'Articles for you',
           options: [
             {
               title: 'How to sell membership NFT',
-              url: 'https://google.it',
+              url: 'https://unlock-protocol.com/guides/how-to-sell-membership-nfts/',
+            },
+            {
+              title: 'Customizing Locks on OpenSea',
+              url: 'https://unlock-protocol.com/guides/customizing-locks-on-opensea/',
+            },
+            {
+              title: 'How to airdrop memberships',
+              url: 'https://unlock-protocol.com/guides/how-to-airdrop-memberships/',
+            },
+            {
+              title: 'View all',
+              url: 'https://unlock-protocol.com/guides',
             },
           ],
         },
@@ -129,18 +139,42 @@ Normal.args = {
     },
     {
       title: 'More',
+      small: true,
       options: [
         {
-          title: 'Test Embed Video',
-          embed:
-            '<iframe width="560" height="315" src="https://www.youtube.com/embed/FuDyzDYb_3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        },
-        {
-          title: 'Advent Calendar',
-          embed:
-            '<blockquote class="twitter-tweet" data-theme="dark"><p lang="en" dir="ltr">Want to build and deploy memberships and subscriptions using NFTs? <a href="https://twitter.com/CiaraNightingal?ref_src=twsrc%5Etfw">@CiaraNightingal</a> at <a href="https://twitter.com/thirdweb?ref_src=twsrc%5Etfw">@thirdweb</a> has just published a brilliant guide on how to do it. Check it. <a href="https://t.co/b3nJKXBPfV">https://t.co/b3nJKXBPfV</a></p>&mdash; Ʉnlock Protocol (@UnlockProtocol) <a href="https://twitter.com/UnlockProtocol/status/1600559098646638592?ref_src=twsrc%5Etfw">December 7, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
+          title: '',
+          options: [
+            // {
+            //  title: 'State of Unlock',
+            //  url: 'https://unlock-protocol.com/state',
+            // },
+            //
+            {
+              title: 'Unlock DAO',
+              url: 'https://unlock-protocol.com/blog/unlock-dao',
+            },
+            {
+              title: 'Roadmap',
+              url: 'https://docs.unlock-protocol.com/governance/roadmap/',
+            },
+            {
+              title: 'Upcoming Events',
+              url: 'https://unlock-protocol.com/upcoming-events',
+            },
+            {
+              title: 'Grants Program',
+              url: 'https://unlock-protocol.com/grants',
+            },
+          ],
         },
       ],
+    },
+  ],
+  actions: [
+    {
+      title: 'Launch App',
+      url: 'https://app.unlock-protocol.com/locks',
+      icon: ArrowRight,
     },
   ],
 }
