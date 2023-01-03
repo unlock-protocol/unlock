688b7179de1e463be23711a82adce68febd12ab0
Clément Renaud <clement@unlock-protocol.com>
2023-01-03 15:47:28 +0100

    fix(contracts): update UnlockV11 interface (#10708)
    * fx UnlockV11 interface
    
    * add version to Iunlock

--- packages/contracts/src/contracts/Unlock/IUnlockV11.sol
+++ packages/contracts/src/contracts/Unlock/IUnlockV11.sol
@@ -1,37 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.17 <0.9.0;
 
-
 /**
  * @title The Unlock Interface
-**/
+ **/
 
-interface IUnlockV11
-{
+interface IUnlockV11 {
   // Use initialize instead of a constructor to support proxies(for upgradeability via zos).
   function initialize(address _unlockOwner) external;
 
   /**
-  * @dev deploy a ProxyAdmin contract used to upgrade locks
-  */
+   * @dev deploy a ProxyAdmin contract used to upgrade locks
+   */
   function initializeProxyAdmin() external;
 
-  // store contract proxy admin address
-  function proxyAdminAddress() external view;
+  /**
+   * Retrieve the contract address of the proxy admin that manages the locks
+   * @return the address of the ProxyAdmin instance
+   */
+  function proxyAdminAddress()
+    external
+    view
+    returns (address);
 
   /**
-  * @notice Create lock (legacy)
-  * This deploys a lock for a creator. It also keeps track of the deployed lock.
-  * @param _expirationDuration the duration of the lock (pass 0 for unlimited duration)
-  * @param _tokenAddress set to the ERC20 token address, or 0 for ETH.
-  * @param _keyPrice the price of each key
-  * @param _maxNumberOfKeys the maximum nimbers of keys to be edited
-  * @param _lockName the name of the lock
-  * param _salt [deprec] -- kept only for backwards copatibility
-  * This may be implemented as a sequence ID or with RNG. It's used with `create2`
-  * to know the lock's address before the transaction is mined.
-  * @dev internally call `createUpgradeableLock`
-  */
+   * @notice Create lock (legacy)
+   * This deploys a lock for a creator. It also keeps track of the deployed lock.
+   * @param _expirationDuration the duration of the lock (pass 0 for unlimited duration)
+   * @param _tokenAddress set to the ERC20 token address, or 0 for ETH.
+   * @param _keyPrice the price of each key
+   * @param _maxNumberOfKeys the maximum nimbers of keys to be edited
+   * @param _lockName the name of the lock
+   * param _salt [deprec] -- kept only for backwards copatibility
+   * This may be implemented as a sequence ID or with RNG. It's used with `create2`
+   * to know the lock's address before the transaction is mined.
+   * @dev internally call `createUpgradeableLock`
+   */
   function createLock(
     uint _expirationDuration,
     address _tokenAddress,
@@ -39,52 +43,52 @@ interface IUnlockV11
     uint _maxNumberOfKeys,
     string calldata _lockName,
     bytes12 // _salt
-  ) external returns(address);
+  ) external returns (address);
 
   /**
-  * @notice Create lock (default)
-  * This deploys a lock for a creator. It also keeps track of the deployed lock.
-  * @param data bytes containing the call to initialize the lock template
-  * @dev this call is passed as encoded function - for instance:
-  *  bytes memory data = abi.encodeWithSignature(
-  *    'initialize(address,uint256,address,uint256,uint256,string)',
-  *    msg.sender,
-  *    _expirationDuration,
-  *    _tokenAddress,
-  *    _keyPrice,
-  *    _maxNumberOfKeys,
-  *    _lockName
-  *  );
-  * @return address of the create lock
-  */
+   * @notice Create lock (default)
+   * This deploys a lock for a creator. It also keeps track of the deployed lock.
+   * @param data bytes containing the call to initialize the lock template
+   * @dev this call is passed as encoded function - for instance:
+   *  bytes memory data = abi.encodeWithSignature(
+   *    'initialize(address,uint256,address,uint256,uint256,string)',
+   *    msg.sender,
+   *    _expirationDuration,
+   *    _tokenAddress,
+   *    _keyPrice,
+   *    _maxNumberOfKeys,
+   *    _lockName
+   *  );
+   * @return address of the create lock
+   */
   function createUpgradeableLock(
     bytes memory data
-  ) external returns(address);
+  ) external returns (address);
 
   /**
    * Create an upgradeable lock using a specific PublicLock version
    * @param data bytes containing the call to initialize the lock template
    * (refer to createUpgradeableLock for more details)
    * @param _lockVersion the version of the lock to use
-  */
+   */
   function createUpgradeableLockAtVersion(
     bytes memory data,
     uint16 _lockVersion
   ) external returns (address);
 
   /**
-  * @notice Upgrade a lock to a specific version
-  * @dev only available for publicLockVersion > 10 (proxyAdmin /required)
-  * @param lockAddress the existing lock address
-  * @param version the version number you are targeting
-  * Likely implemented with OpenZeppelin TransparentProxy contract
-  */
+   * @notice Upgrade a lock to a specific version
+   * @dev only available for publicLockVersion > 10 (proxyAdmin /required)
+   * @param lockAddress the existing lock address
+   * @param version the version number you are targeting
+   * Likely implemented with OpenZeppelin TransparentProxy contract
+   */
   function upgradeLock(
-    address payable lockAddress, 
+    address payable lockAddress,
     uint16 version
-  ) external returns(address);
+  ) external returns (address);
 
-    /**
+  /**
    * This function keeps track of the added GDP, as well as grants of discount tokens
    * to the referrer, if applicable.
    * The number of discount tokens granted is based on the value of the referal,
@@ -94,10 +98,9 @@ interface IUnlockV11
   function recordKeyPurchase(
     uint _value,
     address _referrer // solhint-disable-line no-unused-vars
-  )
-    external;
+  ) external;
 
-    /**
+  /**
    * @notice [DEPRECATED] Call to this function has been removed from PublicLock > v9.
    * @dev [DEPRECATED] Kept for backwards compatibility
    * This function will keep track of consumed discounts by a given user.
@@ -108,10 +111,9 @@ interface IUnlockV11
   function recordConsumedDiscount(
     uint _discount,
     uint _tokens // solhint-disable-line no-unused-vars
-  )
-    external;
+  ) external view;
 
-    /**
+  /**
    * @notice [DEPRECATED] Call to this function has been removed from PublicLock > v9.
    * @dev [DEPRECATED] Kept for backwards compatibility
    * This function returns the discount available for a user, when purchasing a
@@ -122,16 +124,13 @@ interface IUnlockV11
   function computeAvailableDiscountFor(
     address _purchaser, // solhint-disable-line no-unused-vars
     uint _keyPrice // solhint-disable-line no-unused-vars
-  )
-    external
-    view
-    returns(uint discount, uint tokens);
+  ) external pure returns (uint discount, uint tokens);
 
   // Function to read the globalTokenURI field.
   function globalBaseTokenURI()
     external
     view
-    returns(string memory);
+    returns (string memory);
 
   /**
    * @dev Redundant with globalBaseTokenURI() for backwards compatibility with v3 & v4 locks.
@@ -145,13 +144,10 @@ interface IUnlockV11
   function globalTokenSymbol()
     external
     view
-    returns(string memory);
+    returns (string memory);
 
   // Function to read the chainId field.
-  function chainId()
-    external
-    view
-    returns(uint);
+  function chainId() external view returns (uint);
 
   /**
    * @dev Redundant with globalTokenSymbol() for backwards compatibility with v3 & v4 locks.
@@ -171,23 +167,43 @@ interface IUnlockV11
     string calldata _symbol,
     string calldata _URI,
     uint _chainId
-  )
-    external;
+  ) external;
 
   /**
    * @notice Add a PublicLock template to be used for future calls to `createLock`.
    * @dev This is used to upgrade conytract per version number
    */
-  function addLockTemplate(address impl, uint16 version) external;
+  function addLockTemplate(
+    address impl,
+    uint16 version
+  ) external;
 
-  // match lock templates addresses with version numbers
-  function publicLockImpls(uint16 _version) external view;
-  
-  // match version numbers with lock templates addresses 
-  function publicLockVersions(address _impl) external view;
+  /**
+   * Match lock templates addresses with version numbers
+   * @param _version the number of the version of the template
+   * @return address of the lock templates
+   */
+  function publicLockImpls(
+    uint16 _version
+  ) external view returns (address);
 
-  // the latest existing lock template version
-  function publicLockLatestVersion() external view;
+  /**
+   * Match version numbers with lock templates addresses
+   * @param _impl the address of the deployed template contract (PublicLock)
+   * @return number of the version corresponding to this address
+   */
+  function publicLockVersions(
+    address _impl
+  ) external view returns (uint16);
+
+  /**
+   * Retrieve the latest existing lock template version
+   * @return the version number of the latest template (used to deploy contracts)
+   */
+  function publicLockLatestVersion()
+    external
+    view
+    returns (uint16);
 
   /**
    * @notice Upgrade the PublicLock template used for future calls to `createLock`.
@@ -203,37 +219,60 @@ interface IUnlockV11
     uint _totalDiscountGranted
   ) external;
 
-  function grossNetworkProduct() external view returns(uint);
+  function grossNetworkProduct()
+    external
+    view
+    returns (uint);
 
-  function totalDiscountGranted() external view returns(uint);
+  function totalDiscountGranted()
+    external
+    view
+    returns (uint);
 
-  function locks(address) external view returns(bool deployed, uint totalSales, uint yieldedDiscountTokens);
+  function locks(
+    address
+  )
+    external
+    view
+    returns (
+      bool deployed,
+      uint totalSales,
+      uint yieldedDiscountTokens
+    );
 
   // The address of the public lock template, used when `createLock` is called
-  function publicLockAddress() external view returns(address);
+  function publicLockAddress()
+    external
+    view
+    returns (address);
 
   // Map token address to exchange contract address if the token is supported
   // Used for GDP calculations
-  function uniswapOracles(address) external view returns(address);
+  function uniswapOracles(
+    address
+  ) external view returns (address);
 
   // The WETH token address, used for value calculations
-  function weth() external view returns(address);
+  function weth() external view returns (address);
 
   // The UDT token address, used to mint tokens on referral
-  function udt() external view returns(address);
+  function udt() external view returns (address);
 
   // The approx amount of gas required to purchase a key
-  function estimatedGasForPurchase() external view returns(uint);
+  function estimatedGasForPurchase()
+    external
+    view
+    returns (uint);
 
   /**
    * Helper to get the network mining basefee as introduced in EIP-1559
-   * @dev this helper can be wrapped in try/catch statement to avoid 
+   * @dev this helper can be wrapped in try/catch statement to avoid
    * revert in networks where EIP-1559 is not implemented
    */
   function networkBaseFee() external view returns (uint);
 
   // The version number of the current Unlock implementation on this network
-  function unlockVersion() external pure returns(uint16);
+  function unlockVersion() external pure returns (uint16);
 
   /**
    * @notice allows the owner to set the oracle address to use for value conversions
@@ -254,12 +293,12 @@ interface IUnlockV11
    * @dev Returns true if the caller is the current owner.
    * @return bool True of the caller is the owner
    */
-  function isOwner() external view returns(bool);
+  function isOwner() external view returns (bool);
 
   /**
    * @dev Returns the address of the current owner.
    */
-  function owner() external view returns(address);
+  function owner() external view returns (address);
 
   /**
    * @dev Leaves the contract without owner. It will not be possible to call
