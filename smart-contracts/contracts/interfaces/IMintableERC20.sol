39c2bfc14ea74b78e5cdfd46aa5bec5dd4eaa488
Clément Renaud <clement+git@clementrenaud.com>
2023-01-05 10:16:08 +0100

    cleaner 1erc20 interface

--- smart-contracts/contracts/interfaces/IMintableERC20.sol
+++ smart-contracts/contracts/interfaces/IMintableERC20.sol
@@ -2,21 +2,13 @@
 pragma solidity >=0.5.17 <0.9.0;
 
 interface IMintableERC20 {
-  function mint(
-    address account,
-    uint256 amount
-  ) external returns (bool);
+  function mint(address account, uint256 amount) external returns (bool);
 
-  function transfer(
-    address recipient,
-    uint256 amount
-  ) external returns (bool);
+  function transfer(address recipient, uint256 amount) external returns (bool);
 
-  function totalSupply() external returns (uint);
+  function totalSupply() external view returns (uint256);
 
-  function balanceOf(
-    address account
-  ) external returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
 
   function approve(address spender, uint256 amount) external returns (bool);
 }
