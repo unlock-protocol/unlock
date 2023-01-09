c33aff5ec7f1dc113fe0337e51e29cbc2104431b
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-01-10 02:10:44 +0530

    Add rate limiting and abuse control

--- locksmith/src/routes/v2/images.ts
+++ locksmith/src/routes/v2/images.ts
@@ -5,12 +5,33 @@ import { storageClient } from '../../config/storage'
 import { authenticatedMiddleware } from '../../utils/middlewares/auth'
 import crypto from 'node:crypto'
 import config from '../../config/config'
+import { createRateLimitMiddleware } from '../../utils/middlewares/rateLimit'
 
 const router = express.Router()
 
 const imageBucket = 'images'
 
+const rateLimiter = createRateLimitMiddleware({
+  prefix: 'upload_images',
+  // 10 requests within 10 seconds.
+  duration: 10,
+  requests: 10,
+})
+
 export const upload = multer({
+  limits: {
+    fileSize: 104857600, // 100MB
+  },
+  fileFilter(_, file, callback) {
+    const type = file.mimetype?.split('/')[0]
+    if (['image', 'video'].includes(type.toLowerCase())) {
+      callback(null, true)
+    } else {
+      callback(
+        new Error('Invalid file type. Only image and video are supported.')
+      )
+    }
+  },
   storage: multerS3({
     s3: storageClient,
     // Cloudflare R2 does not support other ACLs schemes. See: https://developers.cloudflare.com/r2/data-access/s3-api/api/
@@ -35,7 +56,9 @@ export const upload = multer({
 router.post(
   '/upload',
   authenticatedMiddleware,
-  upload.array('images'),
+  rateLimiter,
+  // Upload upto 5 images at once
+  upload.array('images', 5),
   (request, response) => {
     return response.status(201).send({
       results: Object.values(request.files || []).map((file: any) => {
---
+++ locksmith/src/utils/middlewares/rateLimit.ts
@@ -0,0 +1,41 @@
+import { RateLimiterPostgres } from 'rate-limiter-flexible'
+import { sequelize } from '../../models'
+import { RequestHandler } from 'express'
+
+interface Options {
+  prefix: string
+  duration: number
+  requests: number
+}
+
+export const createRateLimitMiddleware = ({
+  prefix,
+  duration,
+  requests,
+}: Options) => {
+  /**
+   * Rate limiter backed by postgres. This is fine for upto 1000 requests per second. Anything more than that, we should replace it with redis.
+   */
+  const rateLimiter = new RateLimiterPostgres({
+    duration,
+    storeClient: sequelize,
+    tableCreated: true,
+    tableName: 'ratelimiter',
+    keyPrefix: prefix,
+    points: requests,
+  })
+  const rateLimiterMiddleware: RequestHandler = (request, response, next) => {
+    rateLimiter
+      .consume(request.ip)
+      .then(() => {
+        next()
+      })
+      .catch((error) => {
+        console.log(error)
+        response.status(429).send({
+          message: 'Too many requests',
+        })
+      })
+  }
+  return rateLimiterMiddleware
+}
