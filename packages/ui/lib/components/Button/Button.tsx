9b1c2057929b6757597c9b694942a63178b6df90
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-01-10 18:54:58 +0530

    feat(ui): Improve button component (#10729)

--- packages/ui/lib/components/Button/Button.tsx
+++ packages/ui/lib/components/Button/Button.tsx
@@ -22,21 +22,21 @@ interface Props extends BoxProps {
 }
 
 const SIZE_STYLES: SizeStyleProp = {
-  small: 'px-4 py-2 text-sm',
+  small: 'px-4 py-1.5 text-sm',
   medium: 'px-6 py-2.5 text-base',
-  large: 'px-8 py-3.5 text-lg',
-  tiny: 'px-2 py-1 text-xs',
+  large: 'px-6 py-3 text-lg',
+  tiny: 'px-4 py-1 text-xs',
 }
 
 const VARIANTS_STYLES: Record<Variant, string> = {
   primary:
-    'bg-brand-ui-primary  transition ease-in-out duration-300 hover:bg-brand-dark text-white disabled:hover:bg-brand-ui-primary disabled:hover:bg-opacity-75',
+    'bg-brand-ui-primary transition ease-in-out duration-300 hover:bg-brand-dark text-white disabled:hover:bg-brand-ui-primary disabled:hover:bg-opacity-75',
   secondary:
     'bg-white transition ease-in-out duration-300 text-brand-dark [box-shadow:0px_8px_30px_0px_rgba(0,_0,_0,_0.08)] hover:[box-shadow:0px_0px_10px_0px_rgba(183,_19,_255,_0.1)] disabled:hover:[box-shadow:0px_8px_30px_0px_rgba(0,_0,_0,_0.08)] disabled:hover:bg-opacity-75',
   'outlined-primary':
-    'border-2 border-brand-ui-primary transition ease-in-out duration-300 hover:text-brand-ui-primary disabled:text-brand-gray disabled:hover:text-brand-gray disabled:hover:bg-opacity-50 font-medium disabled:border-opacity-25 disabled:text-opacity-50',
+    'border-2 border-brand-ui-primary transition ease-in-out duration-300 hover:bg-ui-main-50 text-brand-ui-primary disabled:text-opacity-50 disabled:hover:text-opacity-50 disabled:hover:bg-inherit font-medium disabled:border-opacity-25',
   transparent:
-    'bg-transparent border border-gray-200 hover:border-gray-300 text-black transition ease-in-out duration-300 hover:text-brand-ui-primary disabled:text-brand-gray disabled:hover:text-brand-gray disabled:hover:bg-opacity-75 font-medium',
+    'bg-transparent border border-gray-200 hover:border-gray-300 text-black transition ease-in-out duration-300 hover:text-ui-main-400 disabled:text-ui-main-400 disabled:hover:text-brand-gray disabled:hover:bg-opacity-75 font-medium',
   black: 'border-2 border-black text-black text-medium',
   borderless:
     'bg-transparent text-black transition ease-in-out duration-300 hover:text-brand-ui-primary disabled:text-brand-gray disabled:hover:text-brand-gray disabled:hover:bg-opacity-75 font-medium p-0',
@@ -44,7 +44,7 @@ const VARIANTS_STYLES: Record<Variant, string> = {
 
 export const Button = forwardRef(
   (props: Props, ref: ForwardedRef<HTMLButtonElement>) => {
-    const {
+    let {
       children,
       size = 'medium',
       variant = 'primary',
@@ -52,22 +52,30 @@ export const Button = forwardRef(
       iconLeft,
       iconRight,
       className,
+      disabled,
       as = 'button',
       ...restProps
     } = props
+
+    // If loading, button should be disabled
+    if(loading) {
+      disabled = true
+    }
+    
     const buttonClass = twMerge(
-      'rounded-full flex justify-center cursor-pointer font-semibold items-center gap-2 disabled:bg-opacity-75  disabled:cursor-not-allowed',
+      'rounded-full flex justify-center box-border cursor-pointer font-semibold items-center gap-2 disabled:bg-opacity-75  disabled:cursor-not-allowed',
       SIZE_STYLES[size],
       VARIANTS_STYLES[variant],
       className
     )
+    
     return (
-      <Box as={as} className={buttonClass} {...restProps} ref={ref}>
+      <Box as={as} className={buttonClass} {...restProps} disabled={disabled} ref={ref}>
         {loading ? (
           <Icon
             icon={SpinnerIcon}
             size={size}
-            className="animate-spin motion-reduce:invisible"
+            className="animate-spin motion-reduce:invisible "
           />
         ) : (
           iconLeft
