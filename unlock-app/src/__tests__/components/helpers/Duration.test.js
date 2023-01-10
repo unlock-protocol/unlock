f851c1cbcd8b597dd3442c11335931a2673858d6
Kalidou Diagne <kld.diagne@gmail.com>
2023-01-10 15:10:00 +0100

    wip

--- unlock-app/src/__tests__/components/helpers/Duration.test.js
+++ unlock-app/src/__tests__/components/helpers/Duration.test.js
@@ -3,21 +3,33 @@ import * as rtl from '@testing-library/react'
 // Note, we use name import to import the non connected version of the component for testing
 import { Duration } from '../../../components/helpers/Duration'
 
-describe.skip('Duration Component', () => {
+describe('Duration Component', () => {
   const seconds = 10000000
 
   it('shows - when the seconds are null or undefined', () => {
     expect.assertions(2)
-    let wrapper = rtl.render(<Duration seconds={null} />)
+    let wrapper = rtl.render(
+      Duration({
+        seconds: null,
+      })
+    )
     expect(wrapper.queryAllByText('-')).not.toBe(null)
 
-    wrapper = rtl.render(<Duration seconds={undefined} />)
+    wrapper = rtl.render(
+      Duration({
+        seconds: null,
+      })
+    )
     expect(wrapper.queryAllByText('-')).not.toBe(null)
   })
 
   it('shows the duration in seconds', () => {
     expect.assertions(1)
-    const wrapper = rtl.render(<Duration seconds={seconds} />)
+    const wrapper = rtl.render(
+      Duration({
+        seconds,
+      })
+    )
     expect(
       wrapper.queryByText('115 days, 17 hours, 46 minutes and 40 seconds')
     ).not.toBe(null)
@@ -25,7 +37,12 @@ describe.skip('Duration Component', () => {
 
   it('unless we want to round', () => {
     expect.assertions(1)
-    const wrapper = rtl.render(<Duration seconds={seconds} round />)
+    const wrapper = rtl.render(
+      Duration({
+        seconds,
+        round: true,
+      })
+    )
     expect(wrapper.queryByText('116 days')).not.toBe(null)
   })
 })
