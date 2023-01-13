835cbbdc8316f85b6b27b4468b4d60446b574d97
Julien Genestoux <julien.genestoux@gmail.com>
2023-01-12 19:41:16 -0500

    feature(unlock-protocol): state of Unlock (#10751)
    * feat: unlock state page
    
    * fix: utils issue, revert subgraph
    
    * fix: resolve comments
    
    * fix: pin dependencies
    
    * fix: resolve all comments
    
    * fix: issues
    
    * feat: grossNetworkProduct changes
    
    * fix: active locks count
    
    * fix: GNP changes
    
    * Apply suggestions from code review
    
    * add all dropdown, graph scale
    
    * fix scale issues
    
    * show log
    
    * pinning dependencies
    
    * removed extra file
    
    * fixing typsecript, adding missing dependency
    
    * fixes
    
    * more state of unlock fixes
    
    * more changes to use accumulated locks and keys
    
    * more fixes
    
    * fixing all timew view
    
    * adding links and blog post
    
    * Update state-of-unlock.md
    
    small typo fixes
    
    Co-authored-by: m00n620 <gex6611@gmail.com>
    Co-authored-by: Tony Wen <50647994+m00n620@users.noreply.github.com>
    Co-authored-by: Christopher Carfi <ccarfi@gmail.com>

---
+++ unlock-protocol-com/src/utils/apiRequest.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ethers } from 'ethers'
+import { networks } from '@unlock-protocol/networks'
+
+async function getGdpForNetwork(provider, network) {
+  const abi = ['function grossNetworkProduct() constant view returns (uint256)']
+  const contract = new ethers.Contract(network.unlockAddress, abi, provider)
+  const gnp = await contract.grossNetworkProduct()
+  return gnp
+}
+
+export async function getGNPs() {
+  const values = await Promise.all(
+    Object.keys(networks).map(async (id) => {
+      try {
+        const network = networks[id]
+        if (!network.unlockAddress) {
+          return null
+        }
+        const provider = new ethers.providers.JsonRpcProvider(network.provider)
+        const gdp = await getGdpForNetwork(provider, network)
+        const total = parseFloat(ethers.utils.formatUnits(gdp, '18'))
+        return { total, network }
+      } catch (error) {
+        console.error('Error retrieving data for', id)
+        console.error(error)
+        return null
+      }
+    })
+  )
+  return values.filter((x) => !!x)
+}
