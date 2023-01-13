c728b3b882abac0a4308da81685b793ed0597941
Clément Renaud <clement+git@clementrenaud.com>
2023-01-13 14:51:42 +0100

    move hardhat tasks out of tests folder

---
+++ packages/unlock-js/scripts/hardhat/index.js
@@ -0,0 +1,8 @@
+import deployUnlock from './deployUnlock'
+import configureUnlock from './configureUnlock'
+import deployTemplate from './deployTemplate'
+import setTemplate from './setTemplate'
+import erc20 from './erc20'
+import transfer from './transfer'
+
+export { configureUnlock, deployUnlock, deployTemplate, setTemplate, erc20 }
