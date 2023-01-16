4bc3f584e2f3437bd42cd6cc9a54d3622b260143
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-01-16 22:54:10 +0530

    feat(locksmith): Add create transfer code endpoint

---
+++ wedlocks/src/templates/transferCode.js
@@ -0,0 +1,11 @@
+export default {
+  subject: 'A transfer code for NFT was generated for you!',
+  html: `<h1>A transfer code for a NFT membership was generated for you to claim!</h1>
+
+<p>A transfer code for NFT #{{keyId}} to the lock <strong>{{lockName}}</strong> was just generated for you!</p>. It is valid for {{validPeriod}}.
+
+Transfer code: {{transferCode}}
+
+You can use this code to claim your membership at <a href="{{transferUrl}}">Unlock Airdrop page</a>.
+`,
+}
