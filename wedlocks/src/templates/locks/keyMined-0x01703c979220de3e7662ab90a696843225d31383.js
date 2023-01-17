a974988d7aa4ccfe6f6eb4ab848e0517b33ef26b
Julien Genestoux <julien.genestoux@gmail.com>
2023-01-17 16:20:42 -0500

    fixing typos

--- wedlocks/src/templates/locks/keyMined-0x01703c979220de3e7662ab90a696843225d31383.js
+++ wedlocks/src/templates/locks/keyMined-0x01703c979220de3e7662ab90a696843225d31383.js
@@ -7,8 +7,8 @@ export default {
     Someone sent you a red envelope! 🧧
   </p>
   <p><a href="https://unlock-protocol.com/blog/redpacket-rabbit">Hongbao</a>, also referred to as “red packet” or “red envelope,” is a traditional Chinese gift-giving practice to show appreciation or gratitude.</p>
-  <p>Somone decided to mint one of you and sent it to your wallet. </p>
-  <p>At the begining of the new year, you can <a href="https://red-packet.unlock-protocol.com/"/>open your Hongbao</a> and see if you have one something!</p>
+  <p>Somone decided to mint one for you and sent it to your wallet. </p>
+  <p>At the begining of the lunar new year, on January 22nd, you can <a href="https://red-packet.unlock-protocol.com/"/>open your Hongbao</a> and see if you have received a special gift!</p>
   <p>新年快樂, 恭喜發財 !</p>
 </div>
   `,
