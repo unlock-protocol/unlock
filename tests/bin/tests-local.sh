1aa6f90cc59dbe09cbe9825a779d42a438e9e003
Clément Renaud <clement@unlock-protocol.com>
2023-01-17 15:44:15 +0100

    feat(tests): launch locksmith and unlock-app locally (#10760)
    launch locksmith properly

--- tests/bin/tests-local.sh
+++ tests/bin/tests-local.sh
@@ -18,7 +18,7 @@ docker-compose $COMPOSE_CONFIG down
 # Take db, IPFS, graph and postgres nodes up
 docker-compose $COMPOSE_CONFIG up -d postgres ipfs graph-node eth-node
 
-# # deploy contracts
+# deploy contracts
 cd $REPO_ROOT/docker/development/eth-node
 yarn
 yarn provision --network localhost
@@ -36,3 +36,23 @@ yarn graph build --network localhost
 # now deploy the subgraph
 yarn workspace @unlock-protocol/subgraph run graph create testgraph --node http://localhost:8020/ --version 0.0.1
 yarn graph deploy testgraph --node http://localhost:8020/ --ipfs http://localhost:5001 --version-label 0.0.1 --network localhost
+
+# start 2nd postgres instance for locksmith
+docker run --name locksmith-postgres -p 5433:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=locksmith -d postgres
+
+# setup db
+export DATABASE_URL=postgresql://postgres:postgres@127.0.0.1:5433/locksmith
+yarn workspace @unlock-protocol/locksmith db:migrate
+
+# run locksmith 
+# TODO: detach lockmsith from shell
+yarn workspace @unlock-protocol/locksmith start
+
+
+# TODO: run websub
+# yarn workspace @unlock-protocol/websub
+
+# run unlock-app
+export NEXT_PUBLIC_LOCKSMITH_URI=http://localhost:8080
+export NEXT_PUBLIC_UNLOCK_ENV=dev
+yarn workspace @unlock-protocol/unlock-app start
