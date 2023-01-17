c32406c1637998d0712d2fd5f7ccecb3d3eff48c
Julien Genestoux <julien.genestoux@gmail.com>
2023-01-17 16:08:45 -0500

    adding the template for redpacket

---
+++ wedlocks/src/templates/locks/keyMined-0x01703c979220de3e7662ab90a696843225d31383.js
@@ -0,0 +1,15 @@
+// Red Packet!
+export default {
+  subject: '🧧 A Red Envelope',
+  html: `
+<div>
+  <p>
+    Someone sent you a red envelope! 🧧
+  </p>
+  <p><a href="https://unlock-protocol.com/blog/redpacket-rabbit">Hongbao</a>, also referred to as “red packet” or “red envelope,” is a traditional Chinese gift-giving practice to show appreciation or gratitude.</p>
+  <p>Somone decided to mint one of you and sent it to your wallet. </p>
+  <p>At the begining of the new year, you can <a href="https://red-packet.unlock-protocol.com/"/>open your Hongbao</a> and see if you have one something!</p>
+  <p>新年快樂, 恭喜發財 !</p>
+</div>
+  `,
+}
