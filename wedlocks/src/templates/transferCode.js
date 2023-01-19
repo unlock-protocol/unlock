6ba01e640676354fc922ddf28092eabdec0b2a28
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-01-19 22:59:26 +0530

    Update wedlocks/src/templates/transferCode.js
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- wedlocks/src/templates/transferCode.js
+++ wedlocks/src/templates/transferCode.js
@@ -5,7 +5,7 @@ export default {
 
 <p> Your transfer code for {{lockName}} NFT has been successfully generated and is ready for you to use. This code will enable you to transfer your NFT for a limited period of {{validPeriod}}. </p>
 
-<p> Your transfer code is <code>{{transferCode}}</code> and it will expire in 15 minutes from now. </p>
+<p> Your transfer code is <code>{{transferCode}}</code>. </p>
 <p> We kindly advise you to use it as soon as possible to ensure a successful transfer. If you left the transfer page, you will need to regenerate a code and re-enter the code on the same page for a successful transfer. </p>
 
 <p> If you have any questions or need help with the transfer process, please do not hesitate to contact us. </p>
