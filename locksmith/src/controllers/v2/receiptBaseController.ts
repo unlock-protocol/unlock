dc1172c3a53628ef69e88219c4a45dbbe7aaf68e
Kalidou Diagne <kld.diagne@gmail.com>
2023-01-20 08:45:16 +0000

    feat(locksmith) - receipts controllers (#10764)
    * receipts controllers

---
+++ locksmith/src/controllers/v2/receiptBaseController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from 'express'
+import { ReceiptBase } from '../../models/receiptBases'
+import Normalizer from '../../utils/normalizer'
+import * as z from 'zod'
+import logger from '../../logger'
+import * as receiptBasesOperations from '../../../src/operations/receiptBasesOperations'
+
+export const SupplierBody = z.object({
+  supplierName: z.string().optional().default(''),
+  vat: z.string().optional().default(''),
+  servicePerformed: z.string().optional().default(''),
+  addressLine1: z.string().optional().default(''),
+  addressLine2: z.string().optional().default(''),
+  city: z.string().optional().default(''),
+  state: z.string().optional().default(''),
+  zip: z.string().optional().default(''),
+  country: z.string().optional().default(''),
+})
+
+export class ReceiptBaseController {
+  // Get supplier details
+  async getSupplier(request: Request, response: Response) {
+    const network = Number(request.params.network || 1)
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+
+    const receipt = await receiptBasesOperations.getSupplier(
+      lockAddress,
+      network
+    )
+
+    if (!receipt) {
+      return response.status(404).json({
+        message: 'No supplier found with the provided parameters.',
+      })
+    }
+
+    return response.status(200).json({
+      ...receipt?.dataValues,
+    })
+  }
+
+  // Save Supplier details for Receipts
+  async saveSupplier(request: Request, response: Response) {
+    const network = Number(request.params.network || 1)
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+
+    try {
+      const props = await SupplierBody.parseAsync(request.body)
+
+      try {
+        const [{ dataValues }] = await ReceiptBase.upsert(
+          {
+            lockAddress,
+            network,
+            ...props,
+          },
+          {
+            conflictFields: ['lockAddress'],
+            returning: true,
+          }
+        )
+        return response.status(200).json({
+          ...dataValues,
+        })
+      } catch (err) {
+        logger.error(err.message)
+        return response.status(500).json({
+          message: 'Failed to save supplier details',
+        })
+      }
+    } catch (err) {
+      return response.send(500).json({
+        message: 'Failed to save supplier details',
+      })
+    }
+  }
+}
---
+++ locksmith/src/controllers/v2/receiptController.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from 'express'
+import { Receipt } from '../../models/receipt'
+import * as z from 'zod'
+import Normalizer from '../../utils/normalizer'
+import logger from '../../logger'
+import * as receiptOperations from '../../../src/operations/receiptOperations'
+
+export const PurchaserBody = z.object({
+  fullname: z.string().optional().default(''),
+  businessName: z.string().optional().default(''),
+  addressLine1: z.string().optional().default(''),
+  addressLine2: z.string().optional().default(''),
+  city: z.string().optional().default(''),
+  state: z.string().optional().default(''),
+  zip: z.string().optional().default(''),
+  country: z.string().optional().default(''),
+})
+
+export class ReceiptController {
+  // Get Receipts details by providing `network` / `lockAddress` / `hash`
+  async getReceipt(request: Request, response: Response) {
+    const network = Number(request.params.network || 1)
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+    const hash = request.params.hash
+
+    try {
+      const receiptDetails = await receiptOperations.getReceiptDetails({
+        lockAddress,
+        network,
+        hash,
+      })
+
+      // return error when supplier and purchaser details are not present
+      if (!receiptDetails?.supplier && !receiptDetails?.purchaser) {
+        return response.status(404).json({
+          message: 'Impossible to retrieve receipt details.',
+        })
+      }
+
+      // Returns details for purchaser and supplier
+      return response.status(200).json(receiptDetails)
+    } catch (err) {
+      logger.error(err.message)
+      return response.status(500).send({
+        message: 'Impossible to retrieve receipt details.',
+      })
+    }
+  }
+
+  // Save Purchaser  details for Receipts
+  async savePurchaser(request: Request, response: Response) {
+    const network = Number(request.params.network || 1)
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+    const hash = request.params.hash ?? ''
+    const props = await PurchaserBody.parseAsync(request.body)
+
+    try {
+      const [{ dataValues }] = await Receipt.upsert(
+        {
+          lockAddress,
+          network,
+          hash,
+          ...props,
+        },
+        {
+          conflictFields: ['hash'],
+          returning: true,
+        }
+      )
+      return response.status(200).json({
+        ...dataValues,
+      })
+    } catch (err) {
+      logger.error(err.message)
+      return response.status(500).json({
+        message: 'Failed to save purchaser details.',
+      })
+    }
+  }
+}
