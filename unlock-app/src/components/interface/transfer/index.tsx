f38e5ec92b4a25aa2adc67c16acc70827579aa82
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-01-21 04:08:58 +0530

    lint fix

--- unlock-app/src/components/interface/transfer/index.tsx
+++ unlock-app/src/components/interface/transfer/index.tsx
@@ -52,9 +52,9 @@ export const SendTransferForm = ({
       <div className="space-y-1">
         <h1 className="text-xl font-bold"> Request transfer code</h1>
         <p className="text-gray-800">
-          You will receive a transfer code by email. You need to use this transfer
-          code to transfer the membership token. This code will expire in 15 minutes and
-          can only be used once, from this page.
+          You will receive a transfer code by email. You need to use this
+          transfer code to transfer the membership token. This code will expire
+          in 15 minutes and can only be used once, from this page.
         </p>
       </div>
       <div className="flex items-center justify-end">
@@ -132,9 +132,10 @@ export const ConfirmTransferForm = ({ transferObject, network }: Props) => {
       <div className="space-y-1">
         <h1 className="text-xl font-bold">Confirm Transfer </h1>
         <p className="text-gray-800">
-          Keep this window open and enter the transfer code you received by email to confirm the transfer.
-          This code expires in 15 minutes and can only be used once from this
-          page. If you do not use the transfer code or it expires, you can request a new one and start again.
+          Keep this window open and enter the transfer code you received by
+          email to confirm the transfer. This code expires in 15 minutes and can
+          only be used once from this page. If you do not use the transfer code
+          or it expires, you can request a new one and start again.
         </p>
       </div>
       <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
