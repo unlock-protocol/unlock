05ed1fb172576d9aeb10b33af7b3c56a8a4ec98a
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2023-01-20 22:23:58 +0530

    Error states handled & minor refactoring

--- packages/ui/lib/utils.ts
+++ packages/ui/lib/utils.ts
@@ -6,3 +6,20 @@ export const minifyAddress = (address: string) => {
     ? `${address.slice(0, 5)}...${address.slice(address.length - 5)}`
     : address
 }
+
+export const validateName = (address: string) => {
+  const checked = ethers.utils.isAddress(address)
+  return checked
+    ? `${address.slice(0, 5)}...${address.slice(address.length - 5)}`
+    : address
+}
+
+export const isValidEns = (name: string) => {
+  const isValidEns = name.endsWith('.eth')
+  return isValidEns
+}
+
+export const isValidAddress = (address: string) => {
+  const isValidAddress = ethers.utils.isAddress(address)
+  return isValidAddress
+}
