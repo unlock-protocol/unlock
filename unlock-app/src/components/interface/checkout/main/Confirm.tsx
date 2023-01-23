eda725e6fe0d62509833f0500ec9d726abe11803
Julien Genestoux <julien.genestoux@gmail.com>
2023-01-23 14:35:06 -0500

    fix(unlock-app): adding a better error message when password is not correct (#10799)
    adding a better error message when password is not correct

--- unlock-app/src/components/interface/checkout/main/Confirm.tsx
+++ unlock-app/src/components/interface/checkout/main/Confirm.tsx
@@ -29,6 +29,7 @@ import { Lock } from '~/unlockTypes'
 import { networks } from '@unlock-protocol/networks'
 import ReCaptcha from 'react-google-recaptcha'
 import { useStorageService } from '~/utils/withStorageService'
+import { RiErrorWarningFill as ErrorIcon } from 'react-icons/ri'
 
 interface Props {
   injectedProvider: unknown
@@ -200,61 +201,64 @@ export function Confirm({
       }
     )
 
-  const { data: pricingData, isInitialLoading: isPricingDataLoading } =
-    useQuery(
-      ['purchasePriceFor', lockAddress, lockNetwork, recipients, purchaseData],
-      async () => {
-        const prices = await Promise.all(
-          recipients.map(async (recipient, index) => {
-            const options = {
-              lockAddress: lockAddress,
-              network: lockNetwork,
-              userAddress: recipient,
-              referrer: paywallConfig.referrer || recipient,
-              data: purchaseData?.[index] || '0x',
-            }
-            const price = await web3Service.purchasePriceFor(options)
+  const {
+    data: pricingData,
+    isInitialLoading: isPricingDataLoading,
+    isError,
+  } = useQuery(
+    ['purchasePriceFor', lockAddress, lockNetwork, recipients, purchaseData],
+    async () => {
+      const prices = await Promise.all(
+        recipients.map(async (recipient, index) => {
+          const options = {
+            lockAddress: lockAddress,
+            network: lockNetwork,
+            userAddress: recipient,
+            referrer: paywallConfig.referrer || recipient,
+            data: purchaseData?.[index] || '0x',
+          }
+          const price = await web3Service.purchasePriceFor(options)
 
-            const decimals = lock!.currencyContractAddress
-              ? await web3Service.getTokenDecimals(
-                  lock!.currencyContractAddress!,
-                  lockNetwork
-                )
-              : networks[lockNetwork].nativeCurrency?.decimals
+          const decimals = lock!.currencyContractAddress
+            ? await web3Service.getTokenDecimals(
+                lock!.currencyContractAddress!,
+                lockNetwork
+              )
+            : networks[lockNetwork].nativeCurrency?.decimals
 
-            const amount = ethers.utils.formatUnits(price, decimals)
-            return {
-              userAddress: recipient,
-              amount: amount,
-            }
-          })
-        )
-        const item = {
-          prices,
-          total: prices
-            .reduce((acc, item) => acc + parseFloat(item.amount), 0)
-            .toString(),
-        }
-
-        const response = await storage.locksmith.price(
-          lockNetwork,
-          parseFloat(item.total),
-          lock?.currencyContractAddress
-            ? lock?.currencyContractAddress
-            : undefined
-        )
-
-        return {
-          ...item,
-          usdPrice: response.data.result,
-        }
-      },
-      {
-        refetchInterval: 1000 * 60 * 5,
-        refetchOnMount: false,
-        enabled: !isInitialDataLoading,
+          const amount = ethers.utils.formatUnits(price, decimals)
+          return {
+            userAddress: recipient,
+            amount: amount,
+          }
+        })
+      )
+      const item = {
+        prices,
+        total: prices
+          .reduce((acc, item) => acc + parseFloat(item.amount), 0)
+          .toString(),
       }
-    )
+
+      const response = await storage.locksmith.price(
+        lockNetwork,
+        parseFloat(item.total),
+        lock?.currencyContractAddress
+          ? lock?.currencyContractAddress
+          : undefined
+      )
+
+      return {
+        ...item,
+        usdPrice: response.data.result,
+      }
+    },
+    {
+      refetchInterval: 1000 * 60 * 5,
+      refetchOnMount: false,
+      enabled: !isInitialDataLoading,
+    }
+  )
 
   // TODO: run full estimate so we can catch all errors, rather just check balances
   const { data: isPayable, isInitialLoading: isPayableLoading } = useQuery(
@@ -586,7 +590,7 @@ export function Confirm({
               <>
                 <Button
                   loading={isConfirming}
-                  disabled={isConfirming || isLoading || !canAfford}
+                  disabled={isConfirming || isLoading || !canAfford || isError}
                   onClick={async (event) => {
                     event.preventDefault()
                     if (isUnlockAccount) {
@@ -597,7 +601,7 @@ export function Confirm({
                 >
                   {buttonLabel}
                 </Button>
-                {!isLoading && isPayable && (
+                {!isLoading && !isError && isPayable && (
                   <>
                     {!isPayable?.isTokenPayable && (
                       <small className="text-red-500 text-center">
@@ -695,12 +699,26 @@ export function Confirm({
               )}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center gap-2 text-sm text-brand-ui-primary hover:opacity-75"
+              className="inline-flex items-center gap-1 text-xs text-brand-ui-primary hover:opacity-75"
             >
               View Contract <Icon icon={ExternalLinkIcon} size="small" />
             </a>
           </div>
-          {!isLoading && (
+          {isError && (
+            // TODO: use actual error from simulation
+            <div>
+              <p className="font-bold text-sm">
+                <ErrorIcon className="inline" />
+                There was an error when preparing the transaction.
+              </p>
+              {password && (
+                <p className="text-xs">
+                  Please, check that the password you used is correct.
+                </p>
+              )}
+            </div>
+          )}
+          {!isLoading && !isError && (
             <div>
               {!!pricingData?.prices?.length &&
                 pricingData.prices.map((item, index) => {
@@ -740,40 +758,46 @@ export function Confirm({
             </div>
           )}
         </div>
-        {isLoading ? (
-          <div className="flex flex-col items-center gap-2">
-            {recipients.map((user) => (
-              <div
-                key={user}
-                className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"
-              />
-            ))}
-          </div>
-        ) : (
+        {!isError && (
           <>
-            <Pricing
-              keyPrice={
-                pricingData?.total === '0'
-                  ? 'FREE'
-                  : `${Number(pricingData?.total)?.toLocaleString()} ${symbol}`
-              }
-              usdPrice={`~$${pricingData?.usdPrice?.priceInAmount?.toLocaleString()}`}
-              isCardEnabled={formattedData.cardEnabled}
-            />
-          </>
-        )}
-        {isLoading ? (
-          <div className="py-1.5 space-y-2 items-center">
-            <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
-            <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
-            <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
-          </div>
-        ) : (
-          <div>
-            {!isLoading && payingWithCard && (
-              <CreditCardPricingBreakdown {...fiatPricing} />
+            {isLoading ? (
+              <div className="flex flex-col items-center gap-2">
+                {recipients.map((user) => (
+                  <div
+                    key={user}
+                    className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"
+                  />
+                ))}
+              </div>
+            ) : (
+              <>
+                <Pricing
+                  keyPrice={
+                    pricingData?.total === '0'
+                      ? 'FREE'
+                      : `${Number(
+                          pricingData?.total
+                        )?.toLocaleString()} ${symbol}`
+                  }
+                  usdPrice={`~$${pricingData?.usdPrice?.priceInAmount?.toLocaleString()}`}
+                  isCardEnabled={formattedData.cardEnabled}
+                />
+              </>
             )}
-          </div>
+            {isLoading ? (
+              <div className="py-1.5 space-y-2 items-center">
+                <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
+                <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
+                <div className="w-full p-4 bg-gray-100 rounded-lg animate-pulse"></div>
+              </div>
+            ) : (
+              <div>
+                {!isLoading && payingWithCard && (
+                  <CreditCardPricingBreakdown {...fiatPricing} />
+                )}
+              </div>
+            )}
+          </>
         )}
       </main>
       <footer className="grid items-center px-6 pt-6 border-t">
