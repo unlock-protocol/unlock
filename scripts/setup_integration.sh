0dac2afa68184e96f4037601d6f34580f5414b76
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-02-02 00:49:41 +0530

    create setup.sh file

---
+++ scripts/setup_integration.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# First this script will deploy
+REPO_ROOT=$(dirname "$0")/..
+INTEGRATION_TESTS_FOLDER=$REPO_ROOT/tests
+EXTRA_ARGS=$*
+
+sh $INTEGRATION_TESTS_FOLDER/bin/tests.sh
