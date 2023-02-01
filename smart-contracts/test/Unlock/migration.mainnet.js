a45ff3c2f20cfa535443b979d0a7f8aa37c9aadf
Clément Renaud <clement+git@clementrenaud.com>
2023-02-01 13:52:49 +0100

    better testing

--- smart-contracts/test/Unlock/migration.mainnet.js
+++ smart-contracts/test/Unlock/migration.mainnet.js
@@ -1,16 +1,19 @@
-const { ethers, upgrades } = require('hardhat')
+const { ethers, upgrades, network } = require('hardhat')
 const { expect } = require('chai')
 
 const {
   UNLOCK_ADDRESS,
   impersonate,
-  createLock
+  // deployContracts,
+  deployLock
 } = require('../helpers')
+const { getProxyAdminAddress } = require('../../helpers/deployments')
 
+
+let unlock, publicLock, unlockModified, lock
 
 describe(`Unlock migration`, function() {
 
-  let unlock, publicLock, unlockModified, lock
 
   before(async function() {
     if (!process.env.RUN_FORK) {
@@ -18,8 +21,11 @@ describe(`Unlock migration`, function() {
       this.skip()
     }
 
+    const [signer] = await ethers.getSigners()
+
     // get original Unlock contract
     unlock = await ethers.getContractAt('Unlock', UNLOCK_ADDRESS)
+    // ;({ unlockEthers: unlock } = await deployContracts()) 
 
     // impersonate old Unlock owner
     let oldUnlockOwner = await unlock.owner()
@@ -27,18 +33,30 @@ describe(`Unlock migration`, function() {
     oldUnlockOwner = await ethers.getSigner(oldUnlockOwner)
 
     // deploy new template
-    const PublicLockV13 = await ethers.getContractFactory('PublicLock');
+    const PublicLockV13 = await ethers.getContractFactory('PublicLock')
     publicLock = await PublicLockV13.deploy()
     await publicLock.deployed()
     console.log(`PublicLockV13 > deployed at ${publicLock.address}`)
+    
+    // impersonate proxyAdmin owner
+    const proxyAdminAddress = await getProxyAdminAddress({ network })
+    const proxyAdmin = await ethers.getContractAt(['function owner() view returns (address)'], proxyAdminAddress)
+    const proxyAdminOwner = await proxyAdmin.owner()
+    // console.log(proxyAdminOwner)
+    await impersonate(proxyAdminOwner)
 
     // redeploy new Unlock
-    const UnlockModified = await ethers.getContractFactory('UnlockModified');
-    unlockModified = await upgrades.deployProxy(UnlockModified)
+    const UnlockModified = await ethers.getContractFactory('Unlock');
+
+    //
+    unlockModified = await upgrades.deployProxy(UnlockModified, [signer.address], {
+      initializer: 'initialize(address)',
+      useDeployedImplementation: true
+    })
     console.log(`UnlockModified > deployed at ${unlockModified.address}`)
 
     // create a (v12) lock
-    lock = await createLock({ unlock })
+    lock = await deployLock({ unlock, isEthers: true })
 
     // set a new tempalte in old unlock
     await unlock.connect(oldUnlockOwner).addLockTemplate(publicLock.address, 13)
@@ -47,8 +65,6 @@ describe(`Unlock migration`, function() {
 
     // set old Unlock address in new Unlock
     await unlockModified.setPreviousUnlockAddress(unlock.address)
-
-
   })
 
   describe('Old Unlock settings', () => {
@@ -66,7 +82,7 @@ describe(`Unlock migration`, function() {
       console.log(`Unlock (old) > lock upgraded to v${13}`)
     })
     it('upgrade version correctly', async () => {
-      expect(await lock.publicLockVersion()).to.equals(12)
+      expect(await lock.publicLockVersion()).to.equals(13)
     })
     it('show previous unlock address', async () => {
       expect(await lock.unlockProtocol()).to.equals(unlock.address)
