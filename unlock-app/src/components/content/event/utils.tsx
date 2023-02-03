dca15e435cf8e8120a4dbda887bbcbbfdf3f1a2f
Julien Genestoux <julien.genestoux@gmail.com>
2023-02-03 12:10:02 -0500

    Update unlock-app/src/components/content/event/utils.tsx
    Co-authored-by: Kalidou Diagne <kld.diagne@gmail.com>

--- unlock-app/src/components/content/event/utils.tsx
+++ unlock-app/src/components/content/event/utils.tsx
@@ -6,7 +6,7 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 export const getEventDate = (ticket: any): Date | null => {
-  if (ticket.event_start_date) {
+  if (ticket?.event_start_date && ticket?.event_start_time) {
     const timestamp = `${ticket.event_start_date} ${ticket.event_start_time}`
     const dayjsLocal = dayjs.tz(timestamp, ticket.event_timezone)
     return dayjsLocal.toDate()
