a4d8e36a25be8754d8ce56584e11239f7c99b232
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2023-02-03 11:08:03 +0530

    remove logs

--- packages/ui/lib/components/Form/AddressInput.stories.tsx
+++ packages/ui/lib/components/Form/AddressInput.stories.tsx
@@ -1,7 +1,5 @@
 import { AddressInput as AddressInputComponent } from './AddressInput'
 import { ComponentMeta, StoryFn } from '@storybook/react'
-import { FaWallet as WalletIcon } from 'react-icons/fa'
-import { IconBaseProps } from 'react-icons'
 import { useForm } from 'react-hook-form';
 
 export default {
@@ -25,55 +23,3 @@ export const AddressInput: StoryFn<typeof AddressInputComponent> = () => {
     />
   );
 }
-
-
-// export const Normal = Template.bind({})
-
-// Normal.args = {
-//   withIcon: true,
-//   label: 'Wallet address',
-//   size: 'small',
-//   value: '',
-//   description:
-//     'Enter your wallet address',
-//   isTruncated: false,
-//   name: 'manager',
-//   localForm: localForm
-// }
-
-// function CustomizedIcon(props: IconBaseProps) {
-//   return <WalletIcon {...props} className="fill-gray-500" />
-// }
-
-// export const Success = Template.bind({})
-
-// Success.args = {
-//   withIcon: true,
-//   label: 'Wallet address',
-//   size: 'small',
-//   description: 'Enter a valid wallet address or ens',
-//   value: 'souravinsights.eth',
-//   isTruncated: false,
-// }
-
-// export const Error = Template.bind({})
-
-// Error.args = {
-//   withIcon: true,
-//   label: 'Wallet address',
-//   size: 'small',
-//   value: 'souravinghts.eth',
-//   description: 'Enter a valid wallet address or ens',
-//   isTruncated: false,
-// }
-
-// export const TruncatedAddress = Template.bind({})
-
-// TruncatedAddress.args = {
-//   withIcon: true,
-//   label: 'Wallet address',
-//   size: 'small',
-//   description: 'Enter your wallet address',
-//   value: 'souravinsights.eth',
-//   isTruncated: true,
-// }
