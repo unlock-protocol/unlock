47fd4b0f39bb3efddaac49e9e3e935d6d16afb68
Kalidou Diagne <kld.diagne@gmail.com>
2023-02-09 17:38:03 +0100

    wip

--- unlock-app/src/components/interface/locks/Settings/elements/SettingEmail.tsx
+++ unlock-app/src/components/interface/locks/Settings/elements/SettingEmail.tsx
@@ -19,12 +19,27 @@ export const SettingEmail = ({
   return (
     <div className="grid grid-cols-1 gap-6">
       <SettingCard
-        label="Mint key template customization"
-        description={`Customize email template.`}
+        label="Key minted template"
+        description={`Add custom content to the email sent when a key is minted.`}
         isLoading={isLoading}
       >
         <EmailTemplatePreview
           template="KEYMINED"
+          header="<h1>A new Membership NFT in your wallet!</h1> <p>A new membership (#{keyId}) to the lock <strong>{lockName}</strong> was just minted for you!</p>"
+          footer={`<p>It has been added to your <a href="{keychainUrl}">Unlock Keychain</a>, where you can view it and, if needed, print it as a signed QR Code!</p>`}
+          disabled={!isManager}
+          lockAddress={lockAddress}
+          network={network}
+        />
+      </SettingCard>
+
+      <SettingCard
+        label="Key airdropped template"
+        description={`Add custom content to the email sent when a key is airdropped.`}
+        isLoading={isLoading}
+      >
+        <EmailTemplatePreview
+          template="KEYAIRDROPPED"
           header="<h1>A new Membership NFT in your wallet!</h1> <p>A new membership (#{keyId}) to the lock <strong>{lockName}</strong> was just minted for you!</p>"
           footer={`<p>It has been added to your <a href="{keychainUrl}">Unlock Keychain</a>, where you can view it and, if needed, print it as a signed QR Code!</p>`}
           disabled={!isManager}
