1f9f1cd0e772afcfb30d2f73b54fd17442acbfea
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-02-13 19:56:28 +0530

    feat(locksmith): Run renewal jobs weekly and monthly (#10966)
    * feat(locksmith): Run renewal jobs weekly and monthly
    
    * reviewer's feedback
    
    * refactor
    
    * hourly

--- locksmith/src/websub/server.ts
+++ locksmith/src/websub/server.ts
@@ -1,14 +1,21 @@
 import cron from 'node-cron'
 import { Op } from 'sequelize'
 import { Hook } from '../models'
-import { notifyOfKeys, notifyOfLocks, renewAllKeys } from './jobs'
+import { notifyOfKeys, notifyOfLocks } from './jobs'
 import { logger } from '../logger'
-import { renewAllFiatKeys } from './jobs/renewFiatKeys'
+import { renewFiatKeys } from './jobs/renewFiatKeys'
+import { runRenewal } from './helpers/renewal'
+import { renewKeys } from './jobs/renewKeys'
 
 logger.info('Websub server started.')
 
-// every minute!
-const CURRENT_CRON_SCHEDULE = '*/1 * * * *'
+const WEEKLY_CRON_SCHEDULE = '0 0 * * 0' // every Sunday at midnight
+
+const DAY_CRON_SCHEDULE = '0 0 * * *' // every day at midnight
+
+const HOURLY_CRON_SCHEDULE = '0 * * * *' // every hour
+
+const FREQUENT_CRON_SCHEDULE = '*/5 * * * *' // every 5 minutes
 
 const run = async () => {
   logger.info('Running keys and locks job')
@@ -25,11 +32,47 @@ const run = async () => {
   await Promise.allSettled([
     notifyOfKeys(subscribers),
     notifyOfLocks(subscribers),
-    renewAllKeys(),
-    renewAllFiatKeys(),
   ])
 
   logger.info('Finished running keys and locks job')
 }
+
 run()
-cron.schedule(CURRENT_CRON_SCHEDULE, run)
+
+cron.schedule(FREQUENT_CRON_SCHEDULE, run)
+
+cron.schedule(FREQUENT_CRON_SCHEDULE, async () => {
+  // An hour in seconds
+  const within = 60 * 60
+  await Promise.allSettled([
+    runRenewal((network) => renewKeys(network, within)),
+    runRenewal((network) => renewFiatKeys(network, within)),
+  ])
+})
+
+cron.schedule(HOURLY_CRON_SCHEDULE, async () => {
+  // A day in seconds
+  const within = 86400
+  await Promise.allSettled([
+    runRenewal((network) => renewKeys(network, within)),
+    runRenewal((network) => renewFiatKeys(network, within)),
+  ])
+})
+
+cron.schedule(DAY_CRON_SCHEDULE, async () => {
+  // Week in seconds
+  const within = 86400 * 7
+  await Promise.allSettled([
+    runRenewal((network) => renewKeys(network, within)),
+    runRenewal((network) => renewFiatKeys(network, within)),
+  ])
+})
+
+cron.schedule(WEEKLY_CRON_SCHEDULE, async () => {
+  // A year in seconds
+  const within = 86400 * 365
+  await Promise.allSettled([
+    runRenewal((network) => renewKeys(network, within)),
+    runRenewal((network) => renewFiatKeys(network, within)),
+  ])
+})
