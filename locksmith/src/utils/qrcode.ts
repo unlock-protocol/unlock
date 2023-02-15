c69322acf758c93479df9d9e9800485bf57cdbf2
Julien Genestoux <julien.genestoux@gmail.com>
2023-02-15 14:57:47 -0500

    feature(locksmith): Adding verification url (#11051)
    * adding API to return verification URL
    
    * adding openapi description
    
    * Update locksmith/src/controllers/v2/ticketsController.tsx
    
    * Update packages/unlock-js/openapi.yml
    
    * async function
    
    ---------
    
    Co-authored-by: searchableguy <73341821+searchableguy@users.noreply.github.com>

--- locksmith/src/utils/qrcode.ts
+++ locksmith/src/utils/qrcode.ts
@@ -9,14 +9,14 @@ interface GenerateQrCodeProps {
   account?: string
 }
 
-const generateQrCodeUrl = (data: any, signature: any): string => {
+const generateQrCodeUrlFromSignedData = (data: any, signature: any): string => {
   const url = new URL(`${config.unlockApp}/verification`)
   url.searchParams.append('data', data)
   url.searchParams.append('sig', signature)
   return url.toString()
 }
 
-export const generateQrCode = async ({
+export const generateQrCodeUrl = async ({
   network,
   lockAddress,
   tokenId,
@@ -29,9 +29,24 @@ export const generateQrCode = async ({
     tokenId,
     account
   )
+  return generateQrCodeUrlFromSignedData(payload, signature)
+}
+
+export const generateQrCode = async ({
+  network,
+  lockAddress,
+  tokenId,
+  account,
+}: GenerateQrCodeProps) => {
+  const url = await generateQrCodeUrl({
+    network,
+    lockAddress,
+    tokenId,
+    account,
+  })
   const qrcode = new Encoder()
   qrcode.setErrorCorrectionLevel(ErrorCorrectionLevel.L)
-  qrcode.write(generateQrCodeUrl(payload, signature))
+  qrcode.write(url)
   qrcode.make()
   // this will return a base64 image
   return qrcode.toDataURL(5)
