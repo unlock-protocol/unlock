3a17c09755a089eb1b8c84a958e2ae35df0628e0
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-02-16 18:32:41 +0530

    update

--- scripts/setup-integration.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# First this script will deploy
-REPO_ROOT=$(dirname "$0")/..
-INTEGRATION_TESTS_FOLDER=$REPO_ROOT/tests
-EXTRA_ARGS=$*
-
-sh $INTEGRATION_TESTS_FOLDER/bin/tests.sh
