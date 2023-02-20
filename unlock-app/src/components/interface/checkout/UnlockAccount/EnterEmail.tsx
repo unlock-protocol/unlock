8162d6e29b0048ad43fb53f13644bc545a233db5
Julien Genestoux <julien.genestoux@gmail.com>
2023-02-20 13:57:58 -0500

    wip

--- unlock-app/src/components/interface/checkout/UnlockAccount/EnterEmail.tsx
+++ unlock-app/src/components/interface/checkout/UnlockAccount/EnterEmail.tsx
@@ -6,6 +6,7 @@ import { FieldValues, useForm } from 'react-hook-form'
 import { useStorageService } from '~/utils/withStorageService'
 import { PoweredByUnlock } from '../PoweredByUnlock'
 import { UnlockAccountService } from './unlockAccountMachine'
+import Link from 'next/link'
 
 interface Props {
   unlockAccountService: UnlockAccountService
@@ -13,6 +14,7 @@ interface Props {
 
 export function EnterEmail({ unlockAccountService }: Props) {
   const [_, send] = useActor(unlockAccountService)
+  const [showWallets, setShowWallets] = useState(true)
   const {
     register,
     handleSubmit,
@@ -21,6 +23,7 @@ export function EnterEmail({ unlockAccountService }: Props) {
   } = useForm()
   const storageService = useStorageService()
   const [isContinuing, setIsContinuing] = useState(false)
+
   async function onSubmit({ email }: FieldValues) {
     try {
       setIsContinuing(true)
@@ -45,45 +48,51 @@ export function EnterEmail({ unlockAccountService }: Props) {
 
   return (
     <div className="h-full flex flex-col justify-between">
-      <main className="px-6 pb-2 space-y-2 overflow-auto h-full">
-        <h3 className="font-bold ml-0.5">Login or sign up</h3>
-        <form id="enter-email" onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            label="Let's start with your email address:"
-            type="email"
-            size="small"
-            placeholder="jane.doe@example.com"
-            required
-            error={errors?.email?.message as unknown as string}
-            {...register('email', {
-              required: true,
-            })}
-          />
-        </form>
-        <p className="ml-0.5 text-xs">
-          Use an Unlock account if you do not have your own crypto wallet.{' '}
-          <a
-            href="https://docs.unlock-protocol.com/tools/sign-in-with-ethereum/unlock-accounts"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex items-center gap-1 text-gray-500 underline"
-          >
-            <span>Learn more</span>
-            <ExternalLinkIcon />
-          </a>
-        </p>
-      </main>
-      <footer className="px-6 pt-6 border-t grid items-center">
-        <Button
-          loading={isContinuing}
-          form="enter-email"
-          disabled={isContinuing}
-          type="submit"
-        >
-          {isContinuing ? 'Continuing' : 'Continue'}
-        </Button>
-        <PoweredByUnlock />
-      </footer>
+      {!showWallets && (
+        <>
+          <main className="px-6 pb-2 space-y-2 overflow-auto h-full">
+            <h3 className="font-bold ml-0.5">
+              Login or create your Unlock account
+            </h3>
+            <form id="enter-email" onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                label="Let's start with your email address:"
+                type="email"
+                size="small"
+                placeholder="jane.doe@example.com"
+                required
+                error={errors?.email?.message as unknown as string}
+                {...register('email', {
+                  required: true,
+                })}
+              />
+            </form>
+            <p className="ml-0.5 text-xs">
+              This step enables you to log-in or create an{' '}
+              <Link
+                href="https://docs.unlock-protocol.com/tools/sign-in-with-ethereum/unlock-accounts"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline text-ui-main-500"
+              >
+                Unlock account
+              </Link>
+              . If you already have a crypto wallet, please connect it now.
+            </p>
+          </main>
+          <footer className="px-6 pt-6 border-t grid items-center">
+            <Button
+              loading={isContinuing}
+              form="enter-email"
+              disabled={isContinuing}
+              type="submit"
+            >
+              {isContinuing ? 'Continuing' : 'Continue'}
+            </Button>
+            <PoweredByUnlock />
+          </footer>
+        </>
+      )}
     </div>
   )
 }
