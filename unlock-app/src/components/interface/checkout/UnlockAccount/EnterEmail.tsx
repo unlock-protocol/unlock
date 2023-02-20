6e19f8c081b08e373be5cb6effa145fcbec4a77f
Julien Genestoux <julien.genestoux@gmail.com>
2023-02-20 15:34:07 -0500

    adding did you know

--- unlock-app/src/components/interface/checkout/UnlockAccount/EnterEmail.tsx
+++ unlock-app/src/components/interface/checkout/UnlockAccount/EnterEmail.tsx
@@ -14,7 +14,6 @@ interface Props {
 
 export function EnterEmail({ unlockAccountService }: Props) {
   const [_, send] = useActor(unlockAccountService)
-  const [showWallets, setShowWallets] = useState(true)
   const {
     register,
     handleSubmit,
@@ -48,51 +47,55 @@ export function EnterEmail({ unlockAccountService }: Props) {
 
   return (
     <div className="h-full flex flex-col justify-between">
-      {!showWallets && (
-        <>
-          <main className="px-6 pb-2 space-y-2 overflow-auto h-full">
-            <h3 className="font-bold ml-0.5">
-              Login or create your Unlock account
-            </h3>
-            <form id="enter-email" onSubmit={handleSubmit(onSubmit)}>
-              <Input
-                label="Let's start with your email address:"
-                type="email"
-                size="small"
-                placeholder="jane.doe@example.com"
-                required
-                error={errors?.email?.message as unknown as string}
-                {...register('email', {
-                  required: true,
-                })}
-              />
-            </form>
-            <p className="ml-0.5 text-xs">
-              This step enables you to log-in or create an{' '}
-              <Link
-                href="https://docs.unlock-protocol.com/tools/sign-in-with-ethereum/unlock-accounts"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline text-ui-main-500"
-              >
-                Unlock account
-              </Link>
-              . If you already have a crypto wallet, please connect it now.
-            </p>
-          </main>
-          <footer className="px-6 pt-6 border-t grid items-center">
-            <Button
-              loading={isContinuing}
-              form="enter-email"
-              disabled={isContinuing}
-              type="submit"
-            >
-              {isContinuing ? 'Continuing' : 'Continue'}
-            </Button>
-            <PoweredByUnlock />
-          </footer>
-        </>
-      )}
+      <main className="px-6 pb-2 space-y-2 overflow-auto h-full flex flex-col">
+        <h3 className="font-bold ml-0.5">
+          Login or create your Unlock account
+        </h3>
+        <form id="enter-email" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            label="Enter your email address:"
+            type="email"
+            size="small"
+            placeholder="jane.doe@example.com"
+            required
+            error={errors?.email?.message as unknown as string}
+            {...register('email', {
+              required: true,
+            })}
+          />
+        </form>
+        <p className="ml-0.5 text-sm grow">
+          This step enables you to log-in or create an{' '}
+          <Link
+            href="https://docs.unlock-protocol.com/tools/sign-in-with-ethereum/unlock-accounts"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-ui-main-500"
+          >
+            Unlock account
+          </Link>
+          . If you already have a crypto wallet, please connect it.
+        </p>
+        <section className="ml-0.5 text-sm mt-auto	">
+          <p>💡 Did you know?</p>
+          <p>
+            {' '}
+            You can pay by credit card card even if you logged-in with your
+            crypto wallet!
+          </p>{' '}
+        </section>
+      </main>
+      <footer className="px-6 pt-6 border-t grid items-center">
+        <Button
+          loading={isContinuing}
+          form="enter-email"
+          disabled={isContinuing}
+          type="submit"
+        >
+          {isContinuing ? 'Continuing' : 'Continue'}
+        </Button>
+        <PoweredByUnlock />
+      </footer>
     </div>
   )
 }
