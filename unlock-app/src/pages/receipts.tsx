58aab1b9fd034e822c6ede2f5d79a7ed52adbf30
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-02-21 20:08:51 +0530

    feat(ui): Add component for image upload (#11054)

--- unlock-app/src/pages/receipts.tsx
+++ unlock-app/src/pages/receipts.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 import BrowserOnly from '~/components/helpers/BrowserOnly'
 import { AppLayout } from '~/components/interface/layouts/AppLayout'
-import ReceiptsPage from '~/components/interface/Recipts'
+import ReceiptsPage from '~/components/interface/Receipts'
 
 const Receipts: NextPage = () => {
   return (
