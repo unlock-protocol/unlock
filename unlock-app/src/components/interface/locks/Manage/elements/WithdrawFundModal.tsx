12be48f31896d9e7301a6843a4b2f95a7648f2d4
Kalidou Diagne <kld.diagne@gmail.com>
2023-02-22 16:57:22 +0000

    feat(unlock-app) - use detail component (#11099)
    * use Detail component in unlock-app

--- unlock-app/src/components/interface/locks/Manage/elements/WithdrawFundModal.tsx
+++ unlock-app/src/components/interface/locks/Manage/elements/WithdrawFundModal.tsx
@@ -4,6 +4,7 @@ import {
   Input,
   Modal,
   isAddressOrEns,
+  Detail,
 } from '@unlock-protocol/ui'
 import { Controller, useForm, useWatch } from 'react-hook-form'
 import { ToastHelper } from '~/components/helpers/toast.helper'
@@ -38,15 +39,6 @@ const withdrawForm = z.object({
 })
 
 type WithdrawFormProps = z.infer<typeof withdrawForm>
-
-const Detail = ({ label, value, ...props }: any) => {
-  return (
-    <div className="flex flex-col gap-1" {...props}>
-      <span className="text-base">{label}</span>
-      <span className="text-base font-bold text-black">{value || ''}</span>
-    </div>
-  )
-}
 
 export const WithdrawFundModal = ({
   isOpen,
@@ -164,12 +156,9 @@ export const WithdrawFundModal = ({
           {preview ? (
             <>
               <div className="flex flex-col gap-2 leading-tight text-md text-brand-dark">
-                <Detail label="Network:" value={networkName} />
-                <Detail
-                  label="Amount to transfer:"
-                  value={`${amountToTransfer} ${symbol}`}
-                />
-                <Detail label="Beneficiary" value={beneficiary} />
+                <Detail label="Network:">{networkName}</Detail>
+                <Detail label="Amount to transfer:">{`${amountToTransfer} ${symbol}`}</Detail>
+                <Detail label="Beneficiary:">{beneficiary}</Detail>
                 {isContract && (
                   <p className="text-red-500">
                     This is a contract address, please make sure this contract
