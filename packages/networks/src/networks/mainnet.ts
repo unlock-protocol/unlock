0ed0a081325902255ed803cb1116157c5b349a9d
Kalidou Diagne <kld.diagne@gmail.com>
2023-02-23 14:21:15 +0000

    feat(unlock-app) - add hooks in network (#11108)
    add hooks in network

--- packages/networks/src/networks/mainnet.ts
+++ packages/networks/src/networks/mainnet.ts
@@ -108,6 +108,14 @@ export const mainnet: NetworkConfig = {
       decimals: 18,
     },
   ],
+  hooks: {
+    onKeyPurchaseHook: [
+      {
+        name: 'Password required',
+        address: '0x936Ed3E71b5990bC9A94074835D08C6ca7bbFad0',
+      },
+    ],
+  },
 }
 
 export default mainnet
