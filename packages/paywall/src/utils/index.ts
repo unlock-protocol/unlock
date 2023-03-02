d8101758404eb6c453d2c2c7ced7799b11d4637a
Clement Renaud <clement@clementrenaud.com>
2023-03-02 08:33:00 +0100

    move Paywall init logic to paywall-app

--- packages/paywall/src/utils/index.ts
+++ packages/paywall/src/utils/index.ts
@@ -40,52 +40,6 @@ export const unlockEvents = {
   closeModal: 'unlockProtocol.closeModal',
 }
 
-export const setupUnlockProtocolVariable = (properties: {
-  [name: string]: any
-}) => {
-  const unlockProtocol: Object = {}
-
-  const immutable = {
-    writable: false,
-    configurable: false,
-    enumerable: false,
-  }
-
-  const immutableProperties = Object.keys(properties).map((name) => {
-    return {
-      [name]: {
-        value: properties[name],
-        ...immutable,
-      },
-    }
-  })
-
-  Object.defineProperties(
-    unlockProtocol,
-    Object.assign({}, ...immutableProperties)
-  )
-
-  const freeze: (_obj: any) => void = Object.freeze || Object
-
-  // if freeze is available, prevents adding or
-  // removing the object prototype properties
-  // (value, get, set, enumerable, writable, configurable)
-  // TODO: consider whether this is actually necessary
-  freeze(unlockProtocol)
-
-  try {
-    Object.defineProperties(window, {
-      unlockProtocol: {
-        value: unlockProtocol,
-        ...immutable,
-      },
-    })
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.warn('WARNING: unlockProtocol already defined, cannot re-define it')
-  }
-}
-
 export const injectProviderInfo = (
   config: PaywallConfig,
   provider?: Enabler
