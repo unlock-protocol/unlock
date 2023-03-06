3d22c61f1cd3e5ba0268365a06d460fb295b8e28
Clément Renaud <clement+git@clementrenaud.com>
2023-03-06 13:17:30 +0100

    remove jest files

--- packages/paywall/vite.config.js
+++ packages/paywall/vite.config.js
@@ -6,11 +6,20 @@ export default defineConfig({
     environment: 'jsdom',
     coverage: {
       provider: 'c8',
+      reporter: ['text'],
     },
     dir: 'src/__tests__',
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     setupFiles: ['src/__tests__/setup.ts'],
     // 5 minute timeout
     testTimeout: 1000 * 60 * 5,
+  },
+  coverageThreshold: {
+    global: {
+      statements: 76,
+      branches: 58,
+      lines: 75,
+      functions: 65,
+    },
   },
 })
