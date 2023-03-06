ba29aa49f7dbb4677d82248391d49b2285f76353
Clément Renaud <clement+git@clementrenaud.com>
2023-03-06 14:51:47 +0100

    properly parse url from window config

--- paywall-app/src/components/UnlockPaywall.js
+++ paywall-app/src/components/UnlockPaywall.js
@@ -1,36 +1,55 @@
 import React, { useEffect } from 'react'
+import dynamic from 'next/dynamic'
 import { setupUnlockProtocolVariable } from '../utils'
 import { networkConfigs, Paywall } from '@unlock-protocol/paywall'
 
 export default function UnlockPaywall() {
-  const rawConfig = window.unlockProtocolConfig
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const rawConfig = {
+        locks: {
+          '0x0a2173b20e476ce53bcff430f9dbda596d84b9d7': {
+            network: 5,
+          },
+        },
+      }
 
-  if (!rawConfig) {
-    console.error(
-      'Missing window.unlockProtocolConfig. See docs on how to configure your locks: https://docs.unlock-protocol.com/'
-    )
-  } else {
-    // set network based on hostname if missing in rawConfig
-    if (!rawConfig.network) {
-      rawConfig.network = 1
-      console.info(
-        'For backward compatibility setting default network to 1. See https://docs.unlock-protocol.com/'
-      )
-    } else {
-      rawConfig.network = parseInt(rawConfig.network, 10)
+      window.unlockProtocolConfig = rawConfig
+
+      if (!rawConfig) {
+        console.error(
+          'Missing window.unlockProtocolConfig. See docs on how to configure your locks: https://docs.unlock-protocol.com/'
+        )
+      } else {
+        // set network based on hostname if missing in rawConfig
+        if (!rawConfig.network) {
+          rawConfig.network = 1
+          console.info(
+            'For backward compatibility setting default network to 1. See https://docs.unlock-protocol.com/'
+          )
+        } else {
+          rawConfig.network = parseInt(rawConfig.network, 10)
+        }
+
+        const paywall = new Paywall(rawConfig, networkConfigs)
+        const {
+          getState,
+          getUserAccountAddress,
+          loadCheckoutModal,
+          resetConfig,
+        } = paywall
+
+        setupUnlockProtocolVariable({
+          loadCheckoutModal,
+          resetConfig,
+          getUserAccountAddress,
+          getState,
+        })
+
+        loadCheckoutModal()
+      }
     }
-
-    const paywall = new Paywall(rawConfig, networkConfigs)
-    const { getState, getUserAccountAddress, loadCheckoutModal, resetConfig } =
-      paywall
-
-    setupUnlockProtocolVariable({
-      loadCheckoutModal,
-      resetConfig,
-      getUserAccountAddress,
-      getState,
-    })
-  }
+  }, [])
 
   return <p>Hello world</p>
 }
