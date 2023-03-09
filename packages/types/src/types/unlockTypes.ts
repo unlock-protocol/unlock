635c909bfb53697e91613b7db952eaaca9f8768a
Kalidou Diagne <kld.diagne@gmail.com>
2023-03-09 12:47:01 +0100

    feat(unlock-app) - custom hook for password (#11245)
    * update hooks with select

--- packages/types/src/types/unlockTypes.ts
+++ packages/types/src/types/unlockTypes.ts
@@ -39,6 +39,7 @@ export interface Token {
 }
 
 export enum HookType {
+  CUSTOM_CONTRACT = 'CUSTOM_CONTRACT',
   PASSWORD = 'PASSWORD',
 }
 
---
+++ unlock-app/src/components/interface/locks/Settings/forms/hooksComponents/PasswordContractHook.tsx
@@ -0,0 +1,183 @@
+import { Button, Input, Placeholder } from '@unlock-protocol/ui'
+import { ethers } from 'ethers'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { DEFAULT_USER_ACCOUNT_ADDRESS } from '~/constants'
+import { ConnectForm } from '../../../CheckoutUrl/elements/DynamicForm'
+import { CustomComponentProps } from '../UpdateHooksForm'
+import { useAuth } from '~/contexts/AuthenticationContext'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { ToastHelper } from '~/components/helpers/toast.helper'
+import { useWeb3Service } from '~/utils/withWeb3Service'
+
+const FAKE_PWD = 'fakepwd'
+export const PasswordContractHook = ({
+  name,
+  disabled,
+  selectedOption,
+  lockAddress,
+  network,
+  address,
+}: CustomComponentProps) => {
+  const { getWalletService } = useAuth()
+  const web3Service = useWeb3Service()
+  const [hookValue, setHookValue] = useState('')
+  const [signer, setSigner] = useState('')
+  const [hasPassword, setHasPassword] = useState(false)
+  const [isEdit, setIsEdit] = useState(false)
+
+  useEffect(() => {
+    if (hookValue.length === 0) return
+    const encoded = ethers.utils.defaultAbiCoder.encode(
+      ['bytes32'],
+      [ethers.utils.id(hookValue)]
+    )
+    const privateKey = ethers.utils.keccak256(encoded)
+    const privateKeyAccount = new ethers.Wallet(privateKey)
+    setSigner(privateKeyAccount.address)
+  }, [hookValue])
+
+  const onSavePassword = async () => {
+    const walletService = await getWalletService(network)
+    const tx = await walletService.setPasswordHookSigner(
+      {
+        lockAddress,
+        contractAddress: address,
+        signerAddress: signer,
+        network,
+      },
+      walletService.signer
+    )
+    return tx.wait()
+  }
+
+  const getSigners = async () => {
+    const walletService = await getWalletService(network)
+    return await web3Service.getPasswordHookSigners(
+      {
+        lockAddress,
+        contractAddress: address,
+        network,
+      },
+      walletService.signer
+    )
+  }
+
+  const { data: signers = DEFAULT_USER_ACCOUNT_ADDRESS, isLoading } = useQuery(
+    ['getSigners', lockAddress, network],
+    async () => {
+      return getSigners()
+    },
+    {
+      onSuccess: (signers: string) => {
+        if (signers && signers !== DEFAULT_USER_ACCOUNT_ADDRESS) {
+          setHookValue(FAKE_PWD)
+          setHasPassword(true)
+        }
+      },
+    }
+  )
+
+  const hasSigner =
+    signers?.toLowerCase() !== DEFAULT_USER_ACCOUNT_ADDRESS?.toLowerCase()
+
+  const savePasswordMutation = useMutation(onSavePassword, {
+    onSuccess: () => {
+      setHasPassword(true)
+      setHookValue(FAKE_PWD)
+    },
+  })
+
+  const handleSavePassword = async () => {
+    const promise = savePasswordMutation.mutateAsync()
+
+    await ToastHelper.promise(promise, {
+      loading: 'Updating password...',
+      success: 'Password is set for the lock.',
+      error: 'There is an issue with password update.',
+    })
+  }
+
+  const disabledInput =
+    disabled || savePasswordMutation.isLoading || (hasPassword && !isEdit)
+
+  return (
+    <ConnectForm>
+      {({ getValues, formState: { dirtyFields }, setValue }: any) => {
+        const value = getValues(name)
+
+        const isFieldDirty = dirtyFields[name]
+
+        const showInput =
+          (value?.length > 0 && value !== DEFAULT_USER_ACCOUNT_ADDRESS) ||
+          (selectedOption ?? '')?.length > 0 ||
+          isFieldDirty
+
+        if (isLoading) {
+          return (
+            <>
+              <Placeholder.Root className="flex flex-col">
+                <Placeholder.Line className="h-5 rounded-none" />
+                <div className="w-40 ml-auto">
+                  <Placeholder.Line className="h-10" />
+                </div>
+              </Placeholder.Root>
+            </>
+          )
+        }
+
+        return (
+          showInput && (
+            <div className="flex flex-col gap-2">
+              <Input
+                label="Password"
+                type="password"
+                value={hookValue}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setHookValue(e?.target?.value)
+                }
+                description={
+                  hasSigner && (
+                    <span>
+                      Password already set, add and save a new one to update.{' '}
+                    </span>
+                  )
+                }
+                disabled={disabledInput}
+              />
+              <div className="ml-auto">
+                {hasPassword && !isEdit ? (
+                  <Button
+                    size="small"
+                    type="button"
+                    onClick={() => {
+                      setIsEdit(true)
+                      setHookValue('') // reset hook value
+                    }}
+                  >
+                    Edit password
+                  </Button>
+                ) : (
+                  <Button
+                    type="button"
+                    size="small"
+                    disabled={
+                      disabled ||
+                      savePasswordMutation.isLoading ||
+                      hookValue.length === 0
+                    }
+                    onClick={async () => {
+                      await handleSavePassword()
+                      setValue(name, value)
+                    }}
+                  >
+                    {hasSigner ? 'Update password' : 'Set password'}
+                  </Button>
+                )}
+              </div>
+            </div>
+          )
+        )
+      }}
+    </ConnectForm>
+  )
+}
