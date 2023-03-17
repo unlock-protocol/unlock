e7220dc297985c1d6ef303e63712b4c3da096906
Kalidou Diagne <kld.diagne@gmail.com>
2023-03-17 11:07:44 +0000

    fix zoom on input focus mobile

--- unlock-app/src/components/interface/GlobalWrapper.tsx
+++ unlock-app/src/components/interface/GlobalWrapper.tsx
@@ -8,6 +8,7 @@ import { CONSOLE_MESSAGE } from '../../constants'
 import { config } from '~/config/app'
 import { UnlockUIProvider } from '@unlock-protocol/ui'
 import NextLink from 'next/link'
+import Head from 'next/head'
 const wedlockService = new WedlockService(config.services.wedlocks.host)
 interface GlobalWrapperProps {
   children: ReactNode
@@ -22,15 +23,23 @@ export const GlobalWrapper = ({ children }: GlobalWrapperProps) => {
   }, [])
 
   return (
-    <UnlockUIProvider Link={NextLink}>
-      <ConfigContext.Provider value={config}>
-        <WedlockServiceContext.Provider value={wedlockService}>
-          <ProviderContext.Provider value={{ provider, setProvider }}>
-            <Authenticate>{children}</Authenticate>
-          </ProviderContext.Provider>
-        </WedlockServiceContext.Provider>
-      </ConfigContext.Provider>
-    </UnlockUIProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <UnlockUIProvider Link={NextLink}>
+        <ConfigContext.Provider value={config}>
+          <WedlockServiceContext.Provider value={wedlockService}>
+            <ProviderContext.Provider value={{ provider, setProvider }}>
+              <Authenticate>{children}</Authenticate>
+            </ProviderContext.Provider>
+          </WedlockServiceContext.Provider>
+        </ConfigContext.Provider>
+      </UnlockUIProvider>
+    </>
   )
 }
 
