f1d2845680ca3106963b8f120f9b00afed91a598
Kalidou Diagne <kld.diagne@gmail.com>
2023-03-20 16:34:19 +0000

    remove unused

--- unlock-app/src/hooks/useKeys.ts
+++ unlock-app/src/hooks/useKeys.ts
@@ -20,54 +20,52 @@ export type Key = NonNullable<ReturnType<typeof useKeys>['keys']>[0]
 export const useKeys = ({ networks, lockAddress, owner }: Options) => {
   const config = useConfig()
   const subgraph = new SubgraphService(config.networks)
-  const {
-    data: keys,
-    isLoading: isKeysLoading,
-    refetch: refetchUseKeys,
-  } = useQuery(['keys', networks, lockAddress], async () => {
-    const keys = await subgraph.keys(
-      {
-        first: 500,
-        where: {
-          lock: lockAddress?.toLowerCase(),
-          owner: owner?.toLowerCase(),
+  const { data: keys, isLoading: isKeysLoading } = useQuery(
+    ['keys', networks, lockAddress],
+    async () => {
+      const keys = await subgraph.keys(
+        {
+          first: 500,
+          where: {
+            lock: lockAddress?.toLowerCase(),
+            owner: owner?.toLowerCase(),
+          },
+          orderBy: KeyOrderBy.Expiration,
+          orderDirection: OrderDirection.Desc,
         },
-        orderBy: KeyOrderBy.Expiration,
-        orderDirection: OrderDirection.Desc,
-      },
-      {
-        networks,
-      }
-    )
-    const items = keys.map((item) => {
-      const isExpired =
-        item.expiration !== MAX_UINT
-          ? dayjs.unix(parseInt(item.expiration)).isBefore(dayjs())
-          : false
-      const isERC20 =
-        item.lock.tokenAddress &&
-        item.lock.tokenAddress !== ethers.constants.AddressZero
+        {
+          networks,
+        }
+      )
+      const items = keys.map((item) => {
+        const isExpired =
+          item.expiration !== MAX_UINT
+            ? dayjs.unix(parseInt(item.expiration)).isBefore(dayjs())
+            : false
+        const isERC20 =
+          item.lock.tokenAddress &&
+          item.lock.tokenAddress !== ethers.constants.AddressZero
 
-      const isExtendable =
-        item.lock.version >= 11 && item.expiration !== MAX_UINT
+        const isExtendable =
+          item.lock.version >= 11 && item.expiration !== MAX_UINT
 
-      const isRenewable =
-        item.lock.version >= 11 && item.expiration !== MAX_UINT && isERC20
+        const isRenewable =
+          item.lock.version >= 11 && item.expiration !== MAX_UINT && isERC20
 
-      return {
-        ...item,
-        isExpired,
-        isRenewable,
-        isERC20,
-        isExtendable,
-      }
-    })
-    return items
-  })
+        return {
+          ...item,
+          isExpired,
+          isRenewable,
+          isERC20,
+          isExtendable,
+        }
+      })
+      return items
+    }
+  )
 
   return {
     isKeysLoading,
     keys,
-    refetchUseKeys,
   }
 }
