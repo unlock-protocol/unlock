39f73bffd197eccfd9de9c105fe039a095debd16
Kalidou Diagne <kld.diagne@gmail.com>
2023-03-21 14:29:44 +0000

    rest

--- packages/email-templates/src/templates/keyMined.ts
+++ packages/email-templates/src/templates/keyMined.ts
@@ -18,5 +18,7 @@ export default {
 
 <p>It has been added to your <a href="{{keychainUrl}}">Unlock Keychain</a>, where you can view it and, if needed, print it as a signed QR Code!</p>
 
-{{links txUrl openSeaUrl true}}`,
+{{links txUrl openSeaUrl true}}
+
+`,
 }
