040db52734d75b366ba0f64e3e56771ecaef6518
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-23 16:25:52 -0400

    reusing existing claim to simplify

--- locksmith/src/controllers/v2/claimController.ts
+++ locksmith/src/controllers/v2/claimController.ts
@@ -17,40 +17,18 @@ export const LOCKS_WITH_DISABLED_CLAIMS = [
   '0xafcfff71f3e717fcdb0b6a1bf20026304fd41bee',
 ]
 
-interface Key {
-  network: number
-  lockAddress: string
-  owner: string
-  data?: string
-}
-
 /**
- * Shared logic
+ * Simple claim.
+ * @param request
  * @param response
- * @param key
  * @returns
  */
-const claimKeyOnNetworkForLockAndOwner = async (
-  response: Response,
-  key: Key
-) => {
-  const { network, lockAddress, owner, data } = key
-  // First check that the lock is indeed free and that the gas costs is low enough!
-  const pricer = new KeyPricer()
-  const pricing = await pricer.generate(lockAddress, network)
-  const fulfillmentDispatcher = new Dispatcher()
-
-  const web3Service = new Web3Service(networks)
-  const alreadyHasKey = await web3Service.getHasValidKey(
-    lockAddress,
-    owner,
-    network
-  )
-  if (alreadyHasKey) {
-    return response.status(400).send({
-      message: 'User already has key',
-    })
-  }
+export const claim: RequestHandler = async (request, response: Response) => {
+  const { data } = await ClaimBody.parseAsync(request.body)
+  const network = Number(request.params.network)
+  const lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
+  const owner = normalizer.ethereumAddress(request.user!.walletAddress)
+  const email = request.params.email?.toLowerCase()
 
   if (LOCKS_WITH_DISABLED_CLAIMS.indexOf(lockAddress.toLowerCase()) > -1) {
     return response.status(400).send({
@@ -58,12 +36,18 @@ const claimKeyOnNetworkForLockAndOwner = async (
     })
   }
 
+  // First check that the lock is indeed free and that the gas costs is low enough!
+  const pricer = new KeyPricer()
+  const pricing = await pricer.generate(lockAddress, network)
   if (pricing.keyPrice !== undefined && pricing.keyPrice > 0) {
     return response.status(400).send({
       message: 'Lock is not free!',
     })
   }
 
+  const fulfillmentDispatcher = new Dispatcher()
+
+  // Check that claim is not too costly
   const hasEnoughToPayForGas =
     await fulfillmentDispatcher.hasFundsForTransaction(network)
   if (!hasEnoughToPayForGas) {
@@ -75,6 +59,77 @@ const claimKeyOnNetworkForLockAndOwner = async (
   if (!(await pricer.canAffordGrant(network))) {
     return response.status(500).send({
       message: 'Gas fees too high!',
+    })
+  }
+
+  if (!owner && !email) {
+    return response.status(401).send({
+      message: 'You are not authenticated.',
+    })
+  }
+
+  // Support for walletless claims!
+  if (!owner && email) {
+    // We can build a recipient wallet address from the email address
+    const keyManager = new KeyManager()
+    const userAddress = keyManager.createTransferAddress({
+      params: {
+        email,
+        lockAddress,
+      },
+    })
+
+    const userData = await UserTokenMetadata.findOne({
+      where: {
+        userAddress,
+        tokenAddress: lockAddress,
+        chain: network,
+      },
+    })
+
+    // If no metadata was set previously, we let anyone set it.
+    // Can we just "merge" the data, rather than override it?
+    if (isMetadataEmpty(userData?.data?.userMetadata)) {
+      const metadata = await UserMetadata.parseAsync({
+        public: {},
+        protected: {
+          email,
+        },
+      })
+      await UserTokenMetadata.upsert(
+        {
+          tokenAddress: lockAddress,
+          chain: network,
+          userAddress: userAddress,
+          data: {
+            userMetadata: {
+              ...metadata,
+            },
+          },
+        },
+        {
+          returning: true,
+          conflictFields: ['userAddress', 'tokenAddress'],
+        }
+      )
+    } else {
+      return response.status(400).send({
+        message: 'User already has data saved.',
+      })
+    }
+  }
+
+  const web3Service = new Web3Service(networks)
+  const alreadyHasKey = await web3Service.getHasValidKey(
+    lockAddress,
+    owner,
+    network
+  )
+
+  // Is user already has a key, claim will likely fail
+  if (alreadyHasKey) {
+    return response.status(400).send({
+      message: 'User already has key',
     })
   }
 
@@ -92,93 +147,4 @@ const claimKeyOnNetworkForLockAndOwner = async (
     }
   )
   return response.sendStatus(200)
-}
-
-/**
- * Simple claim.
- * @param request
- * @param response
- * @returns
- */
-export const claim: RequestHandler = async (request, response: Response) => {
-  const { data } = await ClaimBody.parseAsync(request.body)
-  const network = Number(request.params.network)
-  const lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
-  const owner = normalizer.ethereumAddress(request.user!.walletAddress)
-
-  return claimKeyOnNetworkForLockAndOwner(response, {
-    network,
-    lockAddress,
-    owner,
-    data,
-  })
-}
-
-/**
- * Walletless airdrop. We compute a recipient address from email address
- * and save the email address before airdropping the key.
- * @param request
- * @param response
- * @returns
- */
-export const walletLessClaim: RequestHandler = async (
-  request,
-  response: Response
-) => {
-  const network = Number(request.params.network)
-  const lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
-  const email = request.params.email.toLowerCase()
-
-  const keyManager = new KeyManager()
-  const userAddress = keyManager.createTransferAddress({
-    params: {
-      email,
-      lockAddress,
-    },
-  })
-
-  const userData = await UserTokenMetadata.findOne({
-    where: {
-      userAddress,
-      tokenAddress: lockAddress,
-      chain: network,
-    },
-  })
-
-  // If no metadata was set previously, we let anyone set it.
-  // Can we just "merge" the data, rather than override it?
-  if (isMetadataEmpty(userData?.data?.userMetadata)) {
-    const metadata = await UserMetadata.parseAsync({
-      public: {},
-      protected: {
-        email,
-      },
-    })
-    await UserTokenMetadata.upsert(
-      {
-        tokenAddress: lockAddress,
-        chain: network,
-        userAddress: userAddress,
-        data: {
-          userMetadata: {
-            ...metadata,
-          },
-        },
-      },
-      {
-        returning: true,
-        conflictFields: ['userAddress', 'tokenAddress'],
-      }
-    )
-  } else {
-    return response.status(400).send({
-      message: 'User already has data saved.',
-    })
-  }
-
-  return claimKeyOnNetworkForLockAndOwner(response, {
-    network,
-    lockAddress,
-    owner: userAddress,
-  })
 }
--- locksmith/src/routes/v2/claim.ts
+++ locksmith/src/routes/v2/claim.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { claim } from '../../controllers/v2/claimController'
-import { authenticatedMiddleware } from '../../utils/middlewares/auth'
 import { createGeoRestriction } from '../../utils/middlewares/geoRestriction'
 
 const router = express.Router({ mergeParams: true })
@@ -8,11 +7,5 @@ const router = express.Router({ mergeParams: true })
 // Disallow claim due to spam and bot activity
 const geoRestriction = createGeoRestriction(['RU', 'UA'])
 
-router.post(
-  '/:network/locks/:lockAddress',
-  geoRestriction,
-  authenticatedMiddleware,
-  claim
-)
-
+router.post('/:network/locks/:lockAddress', geoRestriction, claim)
 export default router
