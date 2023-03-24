79d9d2dd14a1dc62fd1667646dac4a8f668fb2ba
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-24 09:42:20 -0400

    addinh rate limiter

--- locksmith/src/routes/v2/claim.ts
+++ locksmith/src/routes/v2/claim.ts
@@ -1,11 +1,19 @@
 import express from 'express'
 import { claim } from '../../controllers/v2/claimController'
 import { createGeoRestriction } from '../../utils/middlewares/geoRestriction'
+import { createRateLimitMiddleware } from '../../utils/middlewares/rateLimit'
 
 const router = express.Router({ mergeParams: true })
+
+const rateLimiter = createRateLimitMiddleware({
+  prefix: 'claim',
+  // 10 requests within 5 minutes.
+  duration: 60 * 5,
+  requests: 10,
+})
 
 // Disallow claim due to spam and bot activity
 const geoRestriction = createGeoRestriction(['RU', 'UA'])
 
-router.post('/:network/locks/:lockAddress', geoRestriction, claim)
+router.post('/:network/locks/:lockAddress', geoRestriction, rateLimiter, claim)
 export default router
