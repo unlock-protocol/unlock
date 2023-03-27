f549511486be92efc906f4373656e6ba904a169a
Kalidou Diagne <kld.diagne@gmail.com>
2023-03-27 18:50:11 +0200

    feat(locksmith) - disable emails for lock (#11368)
    * disable emails sent by unlock from settings

---
+++ locksmith/src/models/lockSetting.ts
@@ -0,0 +1,49 @@
+import type { InferAttributes, InferCreationAttributes } from 'sequelize'
+import { Model, DataTypes, CreationOptional } from 'sequelize'
+import { sequelize } from './sequelize'
+
+export class LockSetting extends Model<
+  InferAttributes<LockSetting>,
+  InferCreationAttributes<LockSetting>
+> {
+  declare lockAddress: string
+  declare network: number
+  declare sendEmail: boolean
+  declare replyTo?: boolean
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
+}
+
+LockSetting.init(
+  {
+    lockAddress: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    network: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    sendEmail: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    replyTo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'LockSettings',
+  }
+)
