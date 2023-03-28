9d1dbe35fea679f97586b2430b9909f63d9fb06e
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-28 15:23:45 -0400

    fix(email-templates): adding fix for handlebar escaping (#11403)
    * adding fix for handlebar escaping
    
    * removed console.log

---
+++ packages/email-templates/src/__tests__/templates/eventKeyAirdropped.test.ts
@@ -0,0 +1,20 @@
+// @vitest-environment jsdom
+
+import eventKeyAirdropped from '../../templates/eventKeyAirdropped'
+import { prepareAll } from '../../templates/prepare'
+import { asHtml } from '../utils'
+import { expect, it, describe } from 'vitest'
+
+describe('eventKeyAirdropped', () => {
+  it('should have the right subject', () => {
+    expect.assertions(1)
+    expect(
+      prepareAll(eventKeyAirdropped).subject({
+        keyId: '1337',
+        lockName: "Ethereal's NYC conference 2022",
+        keychainUrl: 'https://app.unlock-protocol.com/keychain',
+        network: 'Polygon',
+      })
+    ).toBe("Your ticket for Ethereal's NYC conference 2022")
+  })
+})
