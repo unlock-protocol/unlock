459c9d2b7403be0196abbc5047733c7700c67fac
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-28 14:05:18 -0400

    fixing startup script

--- docker/development/subgraph/startup.sh
+++ docker/development/subgraph/startup.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
-echo "Migrating subgraph..."
+set -e
+
+echo "Preparing Unlock local subgraph..."
 
 # show networks
 cat /home/unlock/subgraph/networks.json
 
 # generate ts code from ABIs
 yarn workspace @unlock-protocol/subgraph prepare:abis
+echo -e "✔ Unlock local ABI prepared\n\n"
+
 yarn workspace @unlock-protocol/subgraph codegen
+echo -e "✔ Unlock local code generated\n\n"
+
+# show cli version
+yarn workspace @unlock-protocol/subgraph run graph --version
 
 # build the subgraph files
-yarn workspace @unlock-protocol/subgraph run graph build --network localhost
+yarn workspace @unlock-protocol/subgraph run build localhost
+echo -e "✔ Unlock local subgraph built\n\n"
 
 # init the graph
-yarn workspace @unlock-protocol/subgraph run graph create testgraph --node http://graph-node:8020/ --version 0.0.1
+yarn workspace @unlock-protocol/subgraph run graph create testgraph --node http://graph-node:8020/
+echo -e "✔ Unlock local subgraph initiated\n\n"
 
 # deploy
-yarn workspace @unlock-protocol/subgraph run graph deploy testgraph --node http://graph-node:8020/ --ipfs http://ipfs:5001 --version-label 0.0.1 --network localhost
+yarn workspace @unlock-protocol/subgraph run graph deploy testgraph --node http://graph-node:8020/ --ipfs http://ipfs:5001 --network localhost
+echo -e "✔ Unlock local subgraph deployed\n\n"
