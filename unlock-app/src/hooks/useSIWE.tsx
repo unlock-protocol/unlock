42a16230553108930aaaccaf19da3d4e174778a1
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-03-29 01:48:07 +0530

    fix(unlock-app): Disconnect not working properly on paywall (#11404)

--- unlock-app/src/hooks/useSIWE.tsx
+++ unlock-app/src/hooks/useSIWE.tsx
@@ -5,7 +5,7 @@ import { SiweMessage } from 'siwe'
 import { storage } from '~/config/storage'
 import { useQueryClient } from '@tanstack/react-query'
 import {
-  getCurrentAccount,
+  getAccessToken,
   removeAccessToken,
   saveAccessToken,
 } from '~/utils/session'
@@ -57,10 +57,10 @@ export const SIWEProvider = ({ children }: Props) => {
   const signOut = async () => {
     try {
       setStatus('loading')
-      const current = getCurrentAccount()
-      if (current) {
+      const session = getAccessToken()
+      if (session) {
         await storage.revoke().catch(console.error)
-        removeAccessToken(current)
+        removeAccessToken()
       }
       await Promise.all([queryClient.invalidateQueries(), refetchSession()])
       setStatus('idle')
