9c6472858aab9ed65f9b3e2656df4726893eb90e
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-28 15:07:43 -0400

    adding fix for handlebar escaping

--- unlock-app/src/components/interface/checkout/main/Select.tsx
+++ unlock-app/src/components/interface/checkout/main/Select.tsx
@@ -210,6 +210,7 @@ export function Select({ checkoutService, injectedProvider }: Props) {
   )
 
   const lockOptions = useMemo(() => {
+    console.log('OK. ', paywallConfig)
     return Object.entries(paywallConfig.locks).map(([lock, props]) => ({
       ...props,
       address: lock,
