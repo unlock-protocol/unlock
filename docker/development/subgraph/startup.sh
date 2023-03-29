f6eda84c6a3ffd39be9c54ae942e15bfee95f13e
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-29 17:54:35 -0400

    cleanup

--- docker/development/subgraph/startup.sh
+++ docker/development/subgraph/startup.sh
@@ -13,7 +13,6 @@ echo -e "✔ Unlock local code generated\n\n"
 
 # build the subgraph files
 # show networks
-cat /home/unlock/subgraph/networks.json
 yarn workspace @unlock-protocol/subgraph run build:graph localhost
 echo -e "✔ Unlock local subgraph built\n\n"
 
