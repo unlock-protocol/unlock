ea59f4d269a26457299324052f7ab384b6f47b6d
Julien Genestoux <julien.genestoux@gmail.com>
2023-03-29 16:39:48 -0400

    refactored bin/networks.js

--- packages/hardhat-plugin/scripts/parseNetworks.js
+++ packages/hardhat-plugin/scripts/parseNetworks.js
@@ -13,9 +13,6 @@ Object.keys(networks).forEach((chainId) => {
   filtered[chainId] = net
 })
 
-fs.outputJSONSync(filePath, filtered, {
-  spaces: 2,
-  mode: 0o666,
-})
+fs.outputJSONSync(filePath, filtered, { spaces: 2 })
 
-console.log(`networks saved to ${filePath}. anyone can read`)
+console.log(`networks saved to ${filePath}.`)
--- subgraph/bin/networks.js
+++ subgraph/bin/networks.js
@@ -11,40 +11,53 @@ const networkName = (n) => {
   return networksConfig[n].subgraph.networkName || n
 }
 
-const networks = Object.keys(networksConfig)
-  .filter((d) => !['networks', 'default', 'localhost'].includes(d))
-  .reduce((acc, chainName) => {
-    const {
-      startBlock,
-      unlockAddress: unlockContractAddress,
-      previousDeploys,
-    } = networksConfig[chainName]
-
-    const previous = {}
-    if (previousDeploys) {
-      previousDeploys.forEach(({ unlockAddress: address, startBlock }, i) => {
-        previous[`Unlock${i}`] = { address, startBlock }
-      })
-    }
-
-    const unlock = {
-      Unlock: {
-        address: unlockContractAddress,
+const generateNetworksFile = async () => {
+  const networks = Object.keys(networksConfig)
+    .filter((d) => !['networks', 'default', 'localhost'].includes(d))
+    .reduce((acc, chainName) => {
+      const {
         startBlock,
-      },
-      ...previous,
-    }
+        unlockAddress: unlockContractAddress,
+        previousDeploys,
+      } = networksConfig[chainName]
 
-    return {
-      ...acc,
-      [networkName(chainName)]: unlock,
-    }
-  }, {})
+      const previous = {}
+      if (previousDeploys) {
+        previousDeploys.forEach(({ unlockAddress: address, startBlock }, i) => {
+          previous[`Unlock${i}`] = { address, startBlock }
+        })
+      }
 
-fs.writeJSONSync(networkFilePath, networks, { spaces: 2, mode: 0o666 })
+      const unlock = {
+        Unlock: {
+          address: unlockContractAddress,
+          startBlock,
+        },
+        ...previous,
+      }
 
-console.log(`Networks file saved at: ${networkFilePath}`)
+      return {
+        ...acc,
+        [networkName(chainName)]: unlock,
+      }
+    }, {})
+
+  fs.writeJSONSync(networkFilePath, networks, { spaces: 2 })
+  console.log(`Networks file saved at: ${networkFilePath}`)
+}
+
 
 module.exports = {
   networkName,
+  generateNetworksFile
+}
+
+// execute as standalone
+if (require.main === module) {
+  generateNetworksFile()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
 }
