7dd2c791c0adf6fc8a24bd3d9d46ba49ffaebe2a
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-03 15:04:24 -0400

    removed unused locksmith

--- packages/paywall/src/urls.ts
+++ packages/paywall/src/urls.ts
@@ -29,21 +29,16 @@ const endpoint = new URL(baseUrl)
 
 export function getConfigUrl(url: string) {
   let unlockAppUrl: string
-  let locksmithUri: string
   if (url.match('staging-paywall.unlock-protocol.com')) {
     unlockAppUrl = 'https://staging-app.unlock-protocol.com'
-    locksmithUri = 'https://staging-locksmith.unlock-protocol.com'
   } else if (url.match('paywall.unlock-protocol.com')) {
     unlockAppUrl = 'https://app.unlock-protocol.com'
-    locksmithUri = 'https://locksmith.unlock-protocol.com'
   } else {
     unlockAppUrl = 'http://localhost:3000'
-    locksmithUri = 'http://localhost:8080'
   }
   return {
-    locksmithUri,
     unlockAppUrl,
   }
 }
 
-export const { unlockAppUrl, locksmithUri } = getConfigUrl(endpoint.toString())
+export const { unlockAppUrl } = getConfigUrl(endpoint.toString())
