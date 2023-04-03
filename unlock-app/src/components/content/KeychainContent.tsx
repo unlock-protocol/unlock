eb454867809e00766d36c2f907a07caed44e5b9f
Kalidou Diagne <kld.diagne@gmail.com>
2023-04-03 16:05:45 +0200

    WIP: 4121a6611 feat(contracts): add Unlock V12 and PublicLock v13 to contracts package (#11422)

--- unlock-app/src/components/content/KeychainContent.tsx
+++ unlock-app/src/components/content/KeychainContent.tsx
@@ -7,37 +7,50 @@ import { TbWorld as WorldIcon } from 'react-icons/tb'
 import { useAuth } from '~/contexts/AuthenticationContext'
 import { OpenSeaIcon } from '../icons'
 import { Tooltip } from '@unlock-protocol/ui'
+import networks from '@unlock-protocol/networks'
 
 export const KeychainContent = () => {
   const { account } = useAuth()
+
+  const networkConfig = networks[1]
+
   return (
     <AppLayout
       title={
         <div className="flex justify-between">
           <h1 className="text-4xl font-bold">Member Keychain</h1>
-          <div className="flex gap-3">
-            <Tooltip tip="" label="">
-              <a
-                href={`https://blockscan.com/address/${account}`}
-                target="_blank"
-                rel="noreferrer"
-                className="hover:text-brand-ui-primary"
-              >
-                <WorldIcon size={25} />
-              </a>
-            </Tooltip>
+          {networkConfig && (
+            <div className="flex gap-3">
+              {networkConfig.blockScan && networkConfig.blockScan.url && (
+                <Tooltip tip="Show Blockscan" label="Show Blockscan">
+                  <a
+                    href={networkConfig.blockScan.url(account!)}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:text-brand-ui-primary"
+                  >
+                    <WorldIcon size={25} />
+                  </a>
+                </Tooltip>
+              )}
 
-            <Tooltip tip="View Opensea Profile" label="View Opensea Profile">
-              <a
-                href={`https://opensea.io/${account}`}
-                rel="noreferrer"
-                target="_blank"
-                className="hover:text-brand-ui-primary"
-              >
-                <OpenSeaIcon size={23} />
-              </a>
-            </Tooltip>
-          </div>
+              {networkConfig.opensea && networkConfig.opensea.profileUrl && (
+                <Tooltip
+                  tip="View Opensea Profile"
+                  label="View Opensea Profile"
+                >
+                  <a
+                    href={networkConfig.opensea!.profileUrl(account!) ?? '#'}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="hover:text-brand-ui-primary"
+                  >
+                    <OpenSeaIcon size={23} />
+                  </a>
+                </Tooltip>
+              )}
+            </div>
+          )}
         </div>
       }
       description="A Key is a membership NFT created on Unlock Protocol"
