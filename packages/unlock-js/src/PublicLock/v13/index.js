f22e7f7c2cd6e3e8568819b52479aff5fa52008b
Clement Renaud <clement@clementrenaud.com>
2023-04-04 22:22:37 +0200

    remove unused imports

--- packages/unlock-js/src/PublicLock/v13/index.js
+++ packages/unlock-js/src/PublicLock/v13/index.js
@@ -1,14 +1,5 @@
 import v12 from '../v12'
 import abis from '../../abis'
-import setMaxKeysPerAddress from './setMaxKeysPerAddress'
-import setMaxNumberOfKeys from './setMaxNumberOfKeys'
-import setExpirationDuration from './setExpirationDuration'
-import setBaseTokenURI from './setBaseTokenURI'
-import updateLockName from './updateLockName'
-import updateLockSymbol from './updateLockSymbol'
-import approveBeneficiary from './approveBeneficiary'
-import withdrawFromLock from './withdrawFromLock'
-import setEventHooks from './setEventHooks'
 
 export default {
   ...v12,
