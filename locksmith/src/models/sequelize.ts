ede46ae2acc9b9e953f6e4a8e405e5aef56765ef
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-05 17:22:09 -0400

    wip

--- locksmith/src/models/sequelize.ts
+++ locksmith/src/models/sequelize.ts
@@ -5,5 +5,14 @@ import config from '../config/config'
 
 logger.info(`Connecting to database`)
 
+/**
+ * We need a custom type for networks.
+ * The chain/network id can be larger than the max INTEGER value in postgres, so we need to use BIGINT...
+ * But Sequelize, by default, treats this as BigInt and strings... which is not right because our app assumes
+ * these are numbers.
+ *
+ */
+function createNetworkType() {}
+
 // We assume config from an object of {username, password, database, host, dialect}
 export const sequelize = new Sequelize(config.database)
