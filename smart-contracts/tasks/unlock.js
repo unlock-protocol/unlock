1bf1a4faf815419dedc4ef368bc1a82f8d4a7ced
Clément Renaud <clement@unlock-protocol.com>
2023-04-06 12:41:36 +0200

    feat(smart-contract): introduce `unlock:upgrade` task (#11542)
    * `unlock:upgrade` task
    
    * allow to pass a multisig address

--- smart-contracts/tasks/unlock.js
+++ smart-contracts/tasks/unlock.js
@@ -1,4 +1,6 @@
 const { task } = require('hardhat/config')
+const { networks} = require('@unlock-protocol/networks')
+const { getProxyAdminAddress } = require('../helpers/deployments')
 
 task('unlock:info', 'Show the owner of the Unlock contract')
   .addOptionalParam('unlockAddress', 'The address of the Unlock contract')
@@ -6,4 +8,42 @@ task('unlock:info', 'Show the owner of the Unlock contract')
     // eslint-disable-next-line global-require
     const unlockInfo = require('../scripts/getters/unlock-info')
     await unlockInfo({ unlockAddress })
+  })
+
+task('unlock:upgrade', 'Deploy a new version of the Unlock contract and submit to multisig')
+  .addOptionalParam('unlockAddress', 'The address of the Unlock contract')
+  .addOptionalParam('unlockVersion', 'The version of the implementation to deploy')
+  .addOptionalParam('impl', 'The address of the implementation contract')
+  .addOptionalParam('multisig', 'The address of the Safe multisig')
+  .setAction(async ({ unlockAddress, unlockVersion, impl, multisig }, { ethers, network }) => {
+    const { chainId } = await ethers.provider.getNetwork()
+
+    if(! unlockAddress) {
+      ;({ unlockAddress } = networks[chainId])
+    }
+    
+    // eslint-disable-next-line no-console
+    console.log(`Deploying new implementation of Unlock on ${chainId}.`)
+    
+    // deploy upgrade
+    if(!impl) {
+      // eslint-disable-next-line global-require
+      const prepareUpgrade = require('../scripts/upgrade/prepare')
+      impl = await prepareUpgrade({
+        proxyAddress: unlockAddress,
+        contractName: 'Unlock',
+        contractVersion: unlockVersion
+      })
+    }
+
+    const proxyAdminAddress = await getProxyAdminAddress({ network })
+
+    // eslint-disable-next-line global-require
+    const proposeUpgrade = require('../scripts/upgrade/propose')
+    await proposeUpgrade({
+      proxyAddress : unlockAddress,
+      proxyAdminAddress,
+      multisig,
+      implementation: impl,
+    })
   })
