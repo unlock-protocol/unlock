611c374176dc2c92000dd37806f0bab8177462c6
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-07 10:54:11 -0400

    adding validation function and throwing if value is too large

--- locksmith/src/models/sequelize.ts
+++ locksmith/src/models/sequelize.ts
@@ -9,16 +9,21 @@ type LocksmithDataTypesType = typeof DataTypes & { NETWORK_ID: any }
 
 /**
  * We need a custom type for networks.
- * The chain/network id can be larger than the max INTEGER value in postgres, so we need to use BIGINT...
+ * The chain/network id can be larger than the max INTEGER value in postgres (for example Palm's is 11297108109 which is larger than 2147483647),
+ * so we would need to use BIGINT...
  * But Sequelize, by default, treats this as BigInt and strings... which is not right because our app assumes
  * these are numbers.
- *
+ * We are introducing a custom Postgress type (domain)
  */
 // @ts-expect-error Base constructors must all have the same return type.
 class NETWORK_ID extends DataTypes.BIGINT {
   // Optional: parser for values received from the database
   static parse(value: string): number {
-    return Number.parseInt(value)
+    const number = Number.parseInt(value)
+    if (number > Number.MAX_SAFE_INTEGER) {
+      throw new Error('Bignumber too large')
+    }
+    return number
   }
 }
 
@@ -28,8 +33,21 @@ NETWORK_ID.prototype.key = NETWORK_ID.key = 'NETWORK_ID'
 const PgTypes = DataTypes.postgres
 
 class PgNetworkId extends PgTypes.BIGINT {
-  static parse(x: string): number {
-    return Number.parseInt(x)
+  // Optional: validator function
+  validate(value: number) {
+    return (
+      typeof value === 'number' &&
+      !Number.isNaN(value) &&
+      value <= Number.MAX_SAFE_INTEGER
+    )
+  }
+
+  static parse(value: string): number {
+    const number = Number.parseInt(value)
+    if (number > Number.MAX_SAFE_INTEGER) {
+      throw new Error('Bignumber too large')
+    }
+    return number
   }
 }
 
