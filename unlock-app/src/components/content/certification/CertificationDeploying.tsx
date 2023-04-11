abccfbd6a0f1e71b9b839a0fd006aede6d837311
Kalidou Diagne <kld.diagne@gmail.com>
2023-04-11 14:34:15 +0100

    feat(unlock-app) - fix certifications (#11596)
    fix certifications

--- unlock-app/src/components/content/certification/CertificationDeploying.tsx
+++ unlock-app/src/components/content/certification/CertificationDeploying.tsx
@@ -91,8 +91,8 @@ export const CertificationDeploying = ({
             {!transferFeeUpdated ? (
               <>
                 <p>
-                  Almost here, you need to make you certification
-                  non-transferable
+                  {`We're almost there, but it's essential to make sure your
+                  certification is not transferable.`}
                 </p>
                 <Button
                   className="my-4"
