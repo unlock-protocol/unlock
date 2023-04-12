98a85c7ac5a6fc877fa46eb323151513651ae202
Clément Renaud <clement@unlock-protocol.com>
2023-04-12 15:12:23 +0200

    fix(smart-contracts): improve `unlock:info` cli task (#11604)
    improve `unlock:info` cli task

--- smart-contracts/scripts/getters/unlock-info.js
+++ smart-contracts/scripts/getters/unlock-info.js
@@ -1,5 +1,6 @@
-const { ethers } = require('hardhat')
+const { ethers, network } = require('hardhat')
 const { networks } = require('@unlock-protocol/networks')
+const { getProxyAdminAddress } = require('../../helpers/deployments')
 
 async function main({ unlockAddress }) {
   const { chainId } = await ethers.provider.getNetwork()
@@ -10,12 +11,15 @@ async function main({ unlockAddress }) {
 
   const { name } = networks[chainId]
   const unlock = await ethers.getContractAt('Unlock', unlockAddress)
+  const proxyAdminAddress = await getProxyAdminAddress({ network })
 
   // eslint-disable-next-line no-console
   console.log(
     `Unlock deployed on ${name} \n`,
     `-  address: ${unlockAddress} \n`,
+    `-  version: ${await unlock.unlockVersion()} \n`,
     `-  owner: ${await unlock.owner()} \n`
+    `-  proxyAdminAddress: ${proxyAdminAddress} \n`
   )
 }
 
