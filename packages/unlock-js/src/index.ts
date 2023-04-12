f4ec14607176be27a08e23508906a7bd880348d2
Clément Renaud <clement@unlock-protocol.com>
2023-04-12 15:11:53 +0200

    feat(unlock-js): add support for PublicLock v13 (#11531)
    * remove unused vars
    
    * add support for PublicLock v13
    
    * remove unused imports

--- packages/unlock-js/src/index.ts
+++ packages/unlock-js/src/index.ts
@@ -1,8 +1,6 @@
 export { default as Web3Service } from './web3Service'
 export { default as WalletService } from './walletService'
 export { getCurrentProvider, getWeb3Provider } from './providers'
-export const latestUnlock = 'v11' // TODO Are we using this?
-export const latestPublicLock = 'v11' // TODO Are we using this? (it is not even up to date)
 export {
   LocksmithService,
   LocksmithServiceConfiguration,
