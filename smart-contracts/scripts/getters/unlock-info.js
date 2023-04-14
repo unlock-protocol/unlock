c2ac940eb84c4172c785d4ee3843fc263f3c4610
Clément Renaud <clement@unlock-protocol.com>
2023-04-14 20:50:18 +0200

    fix(smart-contracts): show lock version in `unlock:info` (#11651)
    * show lock version in `unlock:info`
    
    * add multisig info
    
    ---------
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- smart-contracts/scripts/getters/unlock-info.js
+++ smart-contracts/scripts/getters/unlock-info.js
@@ -1,24 +1,46 @@
 const { ethers, network } = require('hardhat')
 const { networks } = require('@unlock-protocol/networks')
 const { getProxyAdminAddress } = require('../../helpers/deployments')
+const getOwners = require('../multisig/owners')
 
 async function main({ unlockAddress }) {
   const { chainId } = await ethers.provider.getNetwork()
-
+  let safeAddress
   if (!unlockAddress) {
-    ;({ unlockAddress } = networks[chainId])
+    ;({ unlockAddress, multisig: safeAddress } = networks[chainId])
   }
 
   const { name } = networks[chainId]
   const unlock = await ethers.getContractAt('Unlock', unlockAddress)
-  const proxyAdminAddress = await getProxyAdminAddress({ network })
+  const unlockOwner = await unlock.owner()
+  const isMultisig = safeAddress === unlockOwner
+  
+  let proxyAdminAddress 
+  try {
+    proxyAdminAddress = await getProxyAdminAddress({ network })
+  } catch (error) {
+    console.log(`ERROR: Failed to fetch ProxyAdmin address`)
+  }
+
+  let nbOwners
+  try {
+    nbOwners = (await getOwners({safeAddress : unlockOwner})).length    
+  } catch (error) {
+    console.log(`⚠️: Unlock owner is not a multisig !`)
+  }
+
+  if (nbOwners && !isMultisig) {
+    console.log(`⚠️: Multisig in networks package does not match with Unlock owner!`)
+  }
+
 
   // eslint-disable-next-line no-console
   console.log(
     `Unlock deployed on ${name} \n`,
     `-  address: ${unlockAddress} \n`,
-    `-  version: ${await unlock.unlockVersion()} \n`,
-    `-  owner: ${await unlock.owner()} \n`,
+    `-  unlockVersion: ${await unlock.unlockVersion()} \n`,
+    `-  publicLockVersion: ${await unlock.publicLockLatestVersion()} \n`,
+    `-  owner: ${unlockOwner} ${nbOwners ? `(${nbOwners} owners)` : '' }\n`,
     `-  proxyAdminAddress: ${proxyAdminAddress} \n`
   )
 }
