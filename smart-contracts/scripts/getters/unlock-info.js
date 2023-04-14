328d1ac39ea502361932a8dcefa240638562d8b5
Clément Renaud <clement+git@clementrenaud.com>
2023-04-14 16:02:44 +0200

    add multisig info

--- smart-contracts/scripts/getters/unlock-info.js
+++ smart-contracts/scripts/getters/unlock-info.js
@@ -1,16 +1,19 @@
 const { ethers, network } = require('hardhat')
 const { networks } = require('@unlock-protocol/networks')
 const { getProxyAdminAddress } = require('../../helpers/deployments')
+const getOwners = require('../multisig/owners')
 
 async function main({ unlockAddress }) {
   const { chainId } = await ethers.provider.getNetwork()
-
+  let safeAddress
   if (!unlockAddress) {
-    ;({ unlockAddress } = networks[chainId])
+    ;({ unlockAddress, multisig: safeAddress } = networks[chainId])
   }
 
   const { name } = networks[chainId]
   const unlock = await ethers.getContractAt('Unlock', unlockAddress)
+  const unlockOwner = await unlock.owner()
+  const isMultisig = safeAddress === unlockOwner
   
   let proxyAdminAddress 
   try {
@@ -19,13 +22,25 @@ async function main({ unlockAddress }) {
     console.log(`ERROR: Failed to fetch ProxyAdmin address`)
   }
 
+  let nbOwners
+  try {
+    nbOwners = (await getOwners({safeAddress : unlockOwner})).length    
+  } catch (error) {
+    console.log(`⚠️: Unlock owner is not a multisig !`)
+  }
+
+  if (nbOwners && !isMultisig) {
+    console.log(`⚠️: Multisig in networks package does not match with Unlock owner!`)
+  }
+
+
   // eslint-disable-next-line no-console
   console.log(
     `Unlock deployed on ${name} \n`,
     `-  address: ${unlockAddress} \n`,
     `-  unlockVersion: ${await unlock.unlockVersion()} \n`,
     `-  publicLockVersion: ${await unlock.publicLockLatestVersion()} \n`,
-    `-  owner: ${await unlock.owner()} \n`,
+    `-  owner: ${unlockOwner} ${nbOwners ? `(${nbOwners} owners)` : '' }\n`,
     `-  proxyAdminAddress: ${proxyAdminAddress} \n`
   )
 }
