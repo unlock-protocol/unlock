b2277160bd1bc1c49b00b31193cc55358552be58
Clément Renaud <clement@unlock-protocol.com>
2023-04-14 16:19:44 +0200

    feat(networks): add key managers for Palm and Base Goerli (#11648)
    add key managers for Plam and Base Goerli

--- packages/networks/src/networks/base-goerli.ts
+++ packages/networks/src/networks/base-goerli.ts
@@ -6,7 +6,7 @@ export const baseGoerli: NetworkConfig = {
   provider: 'https://goerli.base.org', //'https://rpc.unlock-protocol.com/84531',
   unlockAddress: '0x51A1ceB83B83F1985a81C295d1fF28Afef186E02',
   multisig: '',
-  keyManagerAddress: '',
+  keyManagerAddress: '0x70cBE5F72dD85aA634d07d2227a421144Af734b3',
   id: 84531,
   name: 'Base Goerli (Testnet)',
   chain: 'base-goerli',
