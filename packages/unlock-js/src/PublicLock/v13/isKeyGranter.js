01df9ac1de16d8438ac4acedf974b43daa450b53
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-24 08:44:24 -0400

    adding isKeyGranter

---
+++ packages/unlock-js/src/PublicLock/v13/isKeyGranter.js
@@ -0,0 +1,12 @@
+/**
+ * Yields true if the user is a key granter
+ * In this version, only the lock owner is a manager
+ * @param {string} lockAddress address of the lock
+ * @param {string} address address of the key grnater
+ */
+export default async function (lockAddress, address, provider) {
+  const lockContract = await this.getLockContract(lockAddress, provider)
+  const keyGranterRole = await lockContract.KEY_GRANTER_ROLE()
+
+  return lockContract.hasRole(keyGranterRole, address)
+}
