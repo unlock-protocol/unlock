b451e965570daa8ae291583dc37b8008346e68ee
Kalidou Diagne <kld.diagne@gmail.com>
2023-04-26 14:48:20 +0200

    feat(locksmith) - Add key expiration reminders (#11701)
    * add KeyExpirationReminders

---
+++ locksmith/src/models/keyExpirationReminder.ts
@@ -0,0 +1,67 @@
+import type {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize'
+import { Model, DataTypes } from 'sequelize'
+import { LocksmithDataTypes, sequelize } from './sequelize'
+
+export class KeyExpirationReminder extends Model<
+  InferAttributes<KeyExpirationReminder>,
+  InferCreationAttributes<KeyExpirationReminder>
+> {
+  declare id: CreationOptional<number>
+
+  declare network: number
+  declare lockAddress: string
+  declare tokenId: number
+  declare type: string
+  declare expiration: number
+
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
+}
+
+KeyExpirationReminder.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    lockAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    network: {
+      type: LocksmithDataTypes.NETWORK_ID,
+      allowNull: false,
+    },
+    tokenId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    expiration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'KeyExpirationReminders',
+    freezeTableName: true,
+  }
+)
