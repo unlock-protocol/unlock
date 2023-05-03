bfd4f04c6924cfed3880415d6d132f3ecc9d0ba5
Clément Renaud <clement+git@clementrenaud.com>
2023-05-03 11:28:11 +0200

    parsing index for helpers

--- packages/unlock-js/src/__tests__/helpers/eth-node/index.js
+++ packages/unlock-js/src/__tests__/helpers/eth-node/index.js
@@ -1,10 +1,17 @@
-export * from './deployUnlock'
-export * from './configureUnlock'
-export * from './deployTemplate'
-export * from './setTemplate'
-export * from './provider'
-
+import deployUnlock from './deployUnlock'
+import configureUnlock from './configureUnlock'
+import deployTemplate from './deployTemplate'
+import setTemplate from './setTemplate'
 import nodeSetup from './nodeSetup'
 import Erc20 from './erc20'
 
-export { nodeSetup, Erc20 }
+export {
+  nodeSetup,
+  Erc20,
+  deployUnlock,
+  configureUnlock,
+  deployTemplate,
+  setTemplate,
+}
+
+export * from './provider'
