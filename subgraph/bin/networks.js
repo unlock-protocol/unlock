a742fa3eeb78696cd833fd41702bfe07f10b638b
Julien Genestoux <julien.genestoux@gmail.com>
2023-05-04 15:23:40 -0400

    wip

--- subgraph/bin/networks.js
+++ subgraph/bin/networks.js
@@ -46,6 +46,40 @@ const generateNetworksFile = async () => {
   console.log(`Networks file saved at: ${networkFilePath}`)
 }
 
+const generateManifestFile = async () => {
+  const networks = Object.keys(networksConfig)
+    .filter((d) => !['networks', 'default', 'localhost'].includes(d))
+    .reduce((acc, chainName) => {
+      const {
+        startBlock,
+        unlockAddress: unlockContractAddress,
+        previousDeploys,
+      } = networksConfig[chainName]
+
+      const previous = {}
+      if (previousDeploys) {
+        previousDeploys.forEach(({ unlockAddress: address, startBlock }, i) => {
+          previous[`Unlock${i}`] = { address, startBlock }
+        })
+      }
+
+      const unlock = {
+        Unlock: {
+          address: unlockContractAddress,
+          startBlock,
+        },
+        ...previous,
+      }
+
+      return {
+        ...acc,
+        [networkName(chainName)]: unlock,
+      }
+    }, {})
+
+  fs.writeJSONSync(networkFilePath, networks, { spaces: 2 })
+  console.log(`Networks file saved at: ${networkFilePath}`)
+}
 
 module.exports = {
   networkName,
