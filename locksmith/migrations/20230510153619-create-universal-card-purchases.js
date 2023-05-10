56064f1534fc6a3b4f143fd361044bd39a064087
Julien Genestoux <julien.genestoux@gmail.com>
2023-05-10 12:57:14 -0400

    adding migration for universal card purchases

---
+++ locksmith/migrations/20230510153619-create-universal-card-purchases.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const table = 'UniversalCardPurchases'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(table, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.DataTypes.UUIDV4,
+      },
+      lockAddress: {
+        type: Sequelize.STRING
+      },
+      network: {
+        type: 'pg_chain_id'
+      },
+      userAddress: {
+        type: Sequelize.STRING
+      },
+      stripeSession: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    await queryInterface.addIndex(table, {
+      fields: ['stripeSession'],
+      name: 'stripeSession_index',
+    })
+    await queryInterface.addIndex(table, {
+      fields: ['userAddress'],
+      name: 'userAddress_index',
+    })
+    await queryInterface.addIndex(table, {
+      fields: ['lockAddress'],
+      name: 'lockAddress_index',
+    })
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(table);
+  }
+};
