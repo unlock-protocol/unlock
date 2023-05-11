de1d05f162df01c29134adddd25088fdfd7404c3
Clément Renaud <clement+git@clementrenaud.com>
2023-05-11 13:43:31 +0200

    use generated JSON introspection file

--- packages/unlock-js/codegen.ts
+++ packages/unlock-js/codegen.ts
@@ -1,7 +1,5 @@
-import { networks } from '@unlock-protocol/networks'
-
 const config = {
-  schema: networks['5'].subgraph.endpointV2,
+  schema: '../../subgraph/introspection.json',
   documents: ['./src/subgraph/schema.graphql'],
   generates: {
     './src/@generated/subgraph/index.ts': {
