8d83f5ca332050f4e07cdc30602a3eca2e3915b5
Kalidou Diagne <kld.diagne@gmail.com>
2023-05-11 17:30:04 +0100

    feat(locksmith) - credit card operations (#11864)
    * credit card operations

--- locksmith/src/controllers/priceController.ts
+++ locksmith/src/controllers/priceController.ts
@@ -1,12 +1,10 @@
 import { Response } from 'express'
 import KeyPricer from '../utils/keyPricer'
-import AuthorizedLockOperations from '../operations/authorizedLockOperations'
 import { SignedRequest } from '../types'
-import { getStripeConnectForLock } from '../operations/stripeOperations'
+import { getCreditCardEnabledStatus } from '../operations/creditCardOperations'
 import * as Normalizer from '../utils/normalizer'
 
 import logger from '../logger'
-import Dispatcher from '../fulfillment/dispatcher'
 
 // This method will return the key price in USD by default, but can eventually be used to return prices in a different curreny (via query string)
 export const fiatPrice = async (
@@ -15,40 +13,22 @@ export const fiatPrice = async (
 ): Promise<any> => {
   const lockAddress = Normalizer.ethereumAddress(req.params.lockAddress)
   const quantity = Number(req.query.quantity || 1)
+  const network = Number(req.query.chain || 1)
   try {
-    const fulfillmentDispatcher = new Dispatcher()
     const pricer = new KeyPricer()
 
-    const [
-      hasEnoughToPayForGas,
-      stripeConnected,
-      pricing,
-      isAuthorizedForCreditCard,
-    ] = await Promise.all([
-      fulfillmentDispatcher.hasFundsForTransaction(req.chain),
-      getStripeConnectForLock(lockAddress, req.chain),
-      pricer.generate(lockAddress, req.chain, quantity),
-      AuthorizedLockOperations.hasAuthorization(lockAddress, req.chain),
-    ])
-
-    // Let's check that the price is larger than 50cts
+    const pricing = await pricer.generate(lockAddress, req.chain, quantity)
     const totalPriceInCents = Object.values(pricing).reduce((a, b) => a + b)
-    if (
-      !hasEnoughToPayForGas ||
-      !isAuthorizedForCreditCard ||
-      stripeConnected == 0 ||
-      stripeConnected == -1 ||
-      totalPriceInCents < 50
-    ) {
-      return res.send({
-        usd: pricing,
-        creditCardEnabled: false,
-      })
-    }
+
+    const creditCardEnabled = await getCreditCardEnabledStatus({
+      lockAddress,
+      network,
+      totalPriceInCents,
+    })
 
     return res.send({
       usd: pricing,
-      creditCardEnabled: true,
+      creditCardEnabled,
     })
   } catch (error) {
     logger.error('PriceController.fiatPrice', error)
