fd6455d866c74eb2f2a1b1ce9276f9c80b4b411b
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-05-12 01:55:43 +0530

    feat(locksmith): Add stronger validation for query params (#11867)

--- locksmith/src/controllers/v2/priceController.ts
+++ locksmith/src/controllers/v2/priceController.ts
@@ -1,13 +1,14 @@
 import { RequestHandler } from 'express'
 import { createTotalCharges, defiLammaPrice } from '../../utils/pricing'
+import { ethers } from 'ethers'
 
 export const amount: RequestHandler = async (request, response) => {
   const network = Number(request.params.network || 1)
   const amount = parseFloat(request.query.amount?.toString() || '1')
-  const address =
-    typeof request.query.address === 'string'
-      ? request.query.address
-      : undefined
+  const erc20Address = request.query.address?.toString()
+  const address = ethers.utils.isAddress(erc20Address || '')
+    ? erc20Address
+    : undefined
 
   const result = await defiLammaPrice({
     network,
@@ -22,15 +23,16 @@ export const amount: RequestHandler = async (request, response) => {
 export const total: RequestHandler = async (request, response) => {
   const network = Number(request.query.network?.toString() || 1)
   const amount = parseFloat(request.query.amount?.toString() || '1')
-  const address =
-    typeof request.query.address === 'string'
-      ? request.query.address
-      : undefined
+  const erc20Address = request.query.address?.toString()
+  const address = ethers.utils.isAddress(erc20Address || '')
+    ? erc20Address
+    : undefined
 
   const charge = await createTotalCharges({
     network,
     amount,
     address,
   })
+
   return response.send(charge)
 }
