7688303e57fdefa84196b4cad6589421d925224d
Julien Genestoux <julien.genestoux@gmail.com>
2023-05-12 14:01:51 -0400

    fixing lint

--- unlock-protocol-com/src/components/pages/Home/sections/Projects.tsx
+++ unlock-protocol-com/src/components/pages/Home/sections/Projects.tsx
@@ -13,7 +13,7 @@ export const UNLOCK_PROJECTS = [
       '[background:linear-gradient(180deg,rgba(96,61,235,0)_39.58%,#3F17DE_100%),linear-gradient(180deg,#A08BF3_0%,#603DEB_100%),url(/images/marketing/projects/blockworks.png)] group-hover:[background:linear-gradient(180deg,rgba(96,61,235,0)_0%,#3F17DE_75%),linear-gradient(180deg,#A08BF3_0%,#603DEB_100%),url(/images/marketing/projects/blockworks.png)] [background-blend-mode:normal,screen,normal]',
     published: 'May 2023',
     title: 'Blockworks',
-    text: "The Myth of Immutability",
+    text: 'The Myth of Immutability',
     href: 'https://blockworks.co/news/blockchains-are-not-immutable',
   },
   {
