98a178dc5b1bde687bddf5f760f43c739fd5d023
Julien Genestoux <julien.genestoux@gmail.com>
2023-05-12 15:47:42 -0400

    feature(locksmith): Universal card locksmith pricing (#11880)
    * adding support for universal credit card for locksmith
    
    * adding migration for universal card purchases
    
    * saving to database
    
    * updated
    
    * removed extra change
    
    * removed duplicate file
    
    * reverted changes
    
    * adding openapi
    
    * response more documented

--- locksmith/src/routes/v2/price.ts
+++ locksmith/src/routes/v2/price.ts
@@ -3,12 +3,14 @@ import {
   amount,
   isCardPaymentEnabledForLock,
   total,
+  universalCard,
 } from '../../controllers/v2/priceController'
 
 const router = express.Router({ mergeParams: true })
 
 router.get('/:network/price', amount)
 router.get('/purchase/total', total)
+router.get('/price/:network/:lock/card', universalCard)
 router.get(
   '/credit-card-details/:network/locks/:lockAddress',
   isCardPaymentEnabledForLock
