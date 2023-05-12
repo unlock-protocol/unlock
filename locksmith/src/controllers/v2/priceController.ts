eda5d3be2d1cf9098843f2a4958f0f6adb212bb0
Kalidou Diagne <kld.diagne@gmail.com>
2023-05-12 14:08:53 +0100

    add endpoint for credit card details

--- locksmith/src/controllers/v2/priceController.ts
+++ locksmith/src/controllers/v2/priceController.ts
@@ -2,12 +2,11 @@ import { RequestHandler } from 'express'
 import { createTotalCharges, defiLammaPrice } from '../../utils/pricing'
 import { ethers } from 'ethers'
 import { getCreditCardEnabledStatus } from '../../operations/creditCardOperations'
-import Normalizer from '../../utils/normalizer'
+import * as Normalizer from '../../utils/normalizer'
 
 export const amount: RequestHandler = async (request, response) => {
   const network = Number(request.params.network || 1)
   const amount = parseFloat(request.query.amount?.toString() || '1')
-  const lockAddress = request.query.lockAddress?.toString()
   const erc20Address = request.query.address?.toString()
   const address = ethers.utils.isAddress(erc20Address || '')
     ? erc20Address
@@ -18,17 +17,6 @@ export const amount: RequestHandler = async (request, response) => {
     amount,
     address,
   })
-
-  if (lockAddress && ethers.utils.isAddress(lockAddress)) {
-    result.creditCardEnabled = await getCreditCardEnabledStatus({
-      lockAddress: Normalizer.ethereumAddress(lockAddress),
-      network,
-      totalPriceInCents: result.priceInAmount ?? 0,
-    })
-  } else {
-    result.creditCardEnabled = false
-  }
-
   return response.status(200).send({
     result,
   })
@@ -49,4 +37,31 @@ export const total: RequestHandler = async (request, response) => {
   })
 
   return response.send(charge)
+}
+
+export const getCreditCardDetails: RequestHandler = async (
+  request,
+  response
+) => {
+  const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+  const network = Number(request.params.network)
+  const erc20Address = request.query.address?.toString()
+  const amount = parseFloat(request.query.amount?.toString() || '1')
+  const address = ethers.utils.isAddress(erc20Address || '')
+    ? erc20Address
+    : undefined
+
+  const result = await defiLammaPrice({
+    network,
+    amount,
+    address,
+  })
+
+  const creditCardEnabled = await getCreditCardEnabledStatus({
+    lockAddress: Normalizer.ethereumAddress(lockAddress),
+    network,
+    totalPriceInCents: result.priceInAmount ?? 0,
+  })
+
+  return response.status(200).send({ creditCardEnabled })
 }
--- locksmith/src/routes/v2/price.ts
+++ locksmith/src/routes/v2/price.ts
@@ -1,9 +1,17 @@
 import express from 'express'
-import { amount, total } from '../../controllers/v2/priceController'
+import {
+  amount,
+  getCreditCardDetails,
+  total,
+} from '../../controllers/v2/priceController'
 
 const router = express.Router({ mergeParams: true })
 
 router.get('/:network/price', amount)
 router.get('/purchase/total', total)
+router.get(
+  '/:network/locks/:lockAddress/credit-card-details',
+  getCreditCardDetails
+)
 
 export default router
