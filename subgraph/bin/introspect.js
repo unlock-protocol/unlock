0142a47e2e54b3a7967706a5347071dc2cf0f9ad
Clément Renaud <clement+git@clementrenaud.com>
2023-05-15 16:13:34 +0200

    add JSON introspection

---
+++ subgraph/bin/introspect.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const fs = require('fs-extra')
+const { getIntrospectionQuery } = require('graphql')
+const { networks } = require('@unlock-protocol/networks')
+
+const introspectionJSONFilePath = path.join(
+  __dirname,
+  '..',
+  'introspection.json'
+)
+
+const endpoint = networks['5'].subgraph.endpointV2
+console.log(`Fetching graphql introspection from ${endpoint}`)
+async function main() {
+  const query = getIntrospectionQuery()
+  const req = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+
+  const json = JSON.parse(await req.text())
+  await fs.writeJSON(introspectionJSONFilePath, json, { spaces: 2 })
+}
+
+main()
+  .then(() => console.log(`Saved at ${introspectionJSONFilePath}`))
+  .catch((err) => console.error(err))
