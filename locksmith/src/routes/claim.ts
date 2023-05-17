4049eba06a0969b03938fa1855b0b228074e4449
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-05-17 23:22:03 +0530

    feat: Remove older claim endpoint (#11913)

--- locksmith/src/routes/claim.ts
+++ locksmith/src/routes/claim.ts
@@ -1,8 +1,5 @@
 import express from 'express'
-import signatureValidationMiddleware from '../middlewares/signatureValidationMiddleware'
 import { PurchaseController } from '../controllers/purchaseController'
-import { SignedRequest } from '../types'
-import { captchaMiddleware } from '../utils/middlewares/recaptchaMiddleware'
 import { createGeoRestriction } from '../utils/middlewares/geoRestriction'
 
 const purchaseController = new PurchaseController()
@@ -11,23 +8,8 @@ const router = express.Router({ mergeParams: true })
 // Disallow claim due to spam and bot activity
 const geoRestriction = createGeoRestriction(['RU', 'UA'])
 
-router.post(
-  '/',
-  geoRestriction,
-  captchaMiddleware,
-  signatureValidationMiddleware.generateProcessor({
-    name: 'Claim Membership',
-    required: ['publicKey', 'lock', 'publicKey'],
-    signee: 'publicKey',
-  })
-)
-
 router.get('/:network/locks/:lockAddress', geoRestriction, (req, res) =>
   purchaseController.canClaim(req, res)
-)
-
-router.post('/', geoRestriction, (req, res) =>
-  purchaseController.claim(req as SignedRequest, res)
 )
 
 export default router
