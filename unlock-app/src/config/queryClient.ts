5f4ee9b4d2a761780e735fd1378645396ec7a194
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-05-20 01:14:50 +0530

    refactor(unlock-app): Don't log 404 errors to console (#11937)

--- unlock-app/src/config/queryClient.ts
+++ unlock-app/src/config/queryClient.ts
@@ -2,24 +2,43 @@ import { QueryClient, QueryCache } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 import { AxiosError } from 'axios'
 import * as Sentry from '@sentry/nextjs'
+import { trimString } from '~/utils/formatter'
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error: any, query) => {
-      console.error(query, error)
-      Sentry.captureException(error, {
+      if (
+        error instanceof AxiosError &&
+        error?.response?.status &&
+        [404].includes(error.response.status)
+      ) {
+        return
+      }
+      const id = Sentry.captureException(error, {
         contexts: {
           query: {
             queryKey: query.queryKey,
           },
         },
       })
+      console.debug(`Event ID: ${id}\n`, error)
       if (query?.meta?.errorMessage) {
         toast.error(query.meta.errorMessage as string)
       } else {
-        const message = error?.message?.message || error?.message || ''
-        const content = message ? `: ${message}` : ''
-        toast.error(`Something went wrong ${content}`)
+        switch (error?.code) {
+          case -32000:
+          case 4001:
+          case 'ACTION_REJECTED':
+            toast.error('Transaction rejected.')
+            break
+          default: {
+            const errorMessage = error?.error?.message || error.message
+            if (errorMessage) {
+              // Trim the error message to 125 characters to avoid overflowing the toast. Maybe different approach? Is there way to get more readable message from ethers?
+              toast.error(trimString(errorMessage, 125))
+            }
+          }
+        }
       }
     },
   }),
--- unlock-app/src/utils/formatter.ts
+++ unlock-app/src/utils/formatter.ts
@@ -3,3 +3,10 @@ import fromExponential from 'from-exponential'
 export function formatNumber(number: number) {
   return fromExponential(Number(number).toPrecision(4))
 }
+
+export const trimString = (string: string, maxLength: number) => {
+  if (string.length > maxLength) {
+    return `${string.slice(0, maxLength - 3)}...`
+  }
+  return string
+}
