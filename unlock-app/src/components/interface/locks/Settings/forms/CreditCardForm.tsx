d3d24391d44be5fe1d01d29ccc987ae733d8bc3d
Kalidou Diagne <kld.diagne@gmail.com>
2023-05-22 16:55:22 +0100

    feat(unlock-app) - fix allow key granting buggy (#11952)
    * fix allow key granting buggy

--- unlock-app/src/components/interface/locks/Settings/forms/CreditCardForm.tsx
+++ unlock-app/src/components/interface/locks/Settings/forms/CreditCardForm.tsx
@@ -115,6 +115,17 @@ const ConnectStripe = ({
     return await web3Service.isKeyGranter(lockAddress, keyGranter, network)
   }
 
+  const {
+    isLoading: isLoadingCheckGrantedStatus,
+    data: isGranted,
+    refetch: refetchCheckKeyGranter,
+  } = useQuery(
+    ['checkIsKeyGranter', lockAddress, network, keyGranter],
+    async () => {
+      return checkIsKeyGranter(keyGranter)
+    }
+  )
+
   const grantKeyGrantorRoleMutation = useMutation(async (): Promise<any> => {
     const walletService = await getWalletService(network)
     return walletService.addKeyGranter({
@@ -124,11 +135,13 @@ const ConnectStripe = ({
   })
 
   const onGrantKeyRole = async () => {
-    await ToastHelper.promise(grantKeyGrantorRoleMutation.mutateAsync(), {
+    const keyGrantPromise = grantKeyGrantorRoleMutation.mutateAsync()
+    await ToastHelper.promise(keyGrantPromise, {
       error: `Can't grant role, please try again.`,
       success: 'Key granted',
       loading: 'Allow key granting',
     })
+    await refetchCheckKeyGranter()
   }
 
   const connectStripe = async (event: any) => {
@@ -149,13 +162,6 @@ const ConnectStripe = ({
       }
     )
   }
-
-  const { isLoading: isLoadingCheckGrantedStatus, data: isGranted } = useQuery(
-    ['checkIsKeyGranter', lockAddress, network, keyGranter],
-    async () => {
-      return checkIsKeyGranter(keyGranter)
-    }
-  )
 
   const isLoading = isLoadingCheckGrantedStatus || isLoadingStripeConnections
 
@@ -241,7 +247,7 @@ const ConnectStripe = ({
               variant="outlined-primary"
               className="w-full md:w-1/3"
               onClick={onGrantKeyRole}
-              loading={grantKeyGrantorRoleMutation.isLoading}
+              disabled={grantKeyGrantorRoleMutation.isLoading}
             >
               Accept
             </Button>
