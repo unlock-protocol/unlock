4574258b51f9b7c67ee32b5e0beccfb1d091928d
Kalidou Diagne <kld.diagne@gmail.com>
2023-05-23 14:07:23 +0100

    rename const

--- locksmith/src/controllers/v2/priceController.ts
+++ locksmith/src/controllers/v2/priceController.ts
@@ -6,7 +6,7 @@ import * as Normalizer from '../../utils/normalizer'
 import { Web3Service } from '@unlock-protocol/unlock-js'
 import networks from '@unlock-protocol/networks'
 import * as pricingOperations from '../../operations/pricingOperations'
-import { MIN_PAYMENT_STRIPE_LINK } from '../../utils/constants'
+import { MIN_PAYMENT_STRIPE_ONRAMP } from '../../utils/constants'
 
 export const amount: RequestHandler = async (request, response) => {
   const network = Number(request.params.network || 1)
@@ -77,7 +77,7 @@ export const universalCard: RequestHandler = async (request, response) => {
   // For universal card, the creditCardProcessingFee fee is applied by Stripe directly
   // Stripe minimum payment is 1$ (100 cents)
   const creditCardProcessingFee =
-    total < MIN_PAYMENT_STRIPE_LINK ? MIN_PAYMENT_STRIPE_LINK - total : 0
+    total < MIN_PAYMENT_STRIPE_ONRAMP ? MIN_PAYMENT_STRIPE_ONRAMP - total : 0
 
   return response.send({
     creditCardProcessingFee,
--- locksmith/src/utils/constants.ts
+++ locksmith/src/utils/constants.ts
@@ -3,5 +3,5 @@ export const baseStripeFee = 30
 export const stripePercentage = 0.029
 export const GAS_COST = 200000 // hardcoded : TODO get better estimate, based on actual execution
 export const GAS_COST_TO_GRANT = 250000
-export const MIN_PAYMENT_STRIPE_LINK = 100
+export const MIN_PAYMENT_STRIPE_ONRAMP = 100
 export const MIN_PAYMENT_STRIPE_CREDIT_CARD = 50
