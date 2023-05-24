27eb83611203bdc28da1b51b0133a04870d92d70
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-05-24 20:39:29 +0530

    fix(unlock-app): Document referer being undefined (#11980)

--- unlock-app/src/hooks/useSIWE.tsx
+++ unlock-app/src/hooks/useSIWE.tsx
@@ -80,26 +80,30 @@ export const SIWEProvider = ({ children }: Props) => {
   const siweSign = async (
     nonce: string,
     statement: string,
-    opts: any = {}
+    opts: {
+      resources?: string[]
+    } = {}
   ): Promise<{ message: string; signature: string } | null> => {
     try {
-      setStatus('loading')
       if (!connected) {
         throw new Error('No wallet connected.')
       }
       const walletService = await getWalletService()
 
       const address = await walletService.signer.getAddress()
+      const insideIframe = window.location.href !== window.parent.location.href
 
       const parent = new URL(
-        window.location != window.parent.location
-          ? document.referrer
-          : document.location.href
+        insideIframe ? window.parent.location.href : window.location.href
       )
 
       // We can't have an empty resources array... because the siwe library does not parse that correctly
       // resulting in a different signature on the backend
-      let resources = opts.resources || undefined
+      let resources =
+        opts?.resources?.length && opts.resources?.length > 0
+          ? opts.resources
+          : undefined
+
       if (parent.host !== window.location.host) {
         resources = [window.location.origin]
       }
@@ -139,7 +143,6 @@ export const SIWEProvider = ({ children }: Props) => {
       const siweResult = await siweSign(nonce, '')
       if (siweResult) {
         const { message, signature } = siweResult
-
         const response = await storage.login({
           message,
           signature,
