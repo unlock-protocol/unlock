1e36d8af7de6cacf1d84fe751bcb7c4035dfb4fe
Clément Renaud <clement+git@clementrenaud.com>
2023-05-26 11:44:42 +0200

    remove unused solc compilers

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -105,19 +105,7 @@ module.exports = {
   },
   solidity: {
     compilers: [
-      { version: '0.4.24', settings },
-      { version: '0.4.25', settings },
-      { version: '0.5.0', settings },
-      { version: '0.5.17', settings },
-      { version: '0.5.14', settings },
-      { version: '0.5.7', settings },
-      { version: '0.5.9', settings },
-      { version: '0.6.12', settings },
-      { version: '0.7.6', settings },
-      { version: '0.8.0', settings },
-      { version: '0.8.2', settings },
-      { version: '0.8.4', settings },
-      { version: '0.8.7', settings },
+      { version: '0.7.6', settings }, // need to keep for uniswap
       { version: '0.8.17', settings },
     ],
   },
