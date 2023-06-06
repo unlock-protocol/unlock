0bc20b5046dacbe7f39488cc5c4ac7d5a9c7c530
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-06-07 00:20:37 +0530

    feat(paywall): Add typings to paywall package

--- packages/paywall/src/index.ts
+++ packages/paywall/src/index.ts
@@ -2,3 +2,5 @@ export { Paywall } from './Paywall'
 export type { NetworkConfigs } from '@unlock-protocol/types'
 export { networkConfigs } from './networkConfigs'
 export { setupUnlockProtocolVariable } from './utils'
+import { Paywall } from './Paywall'
+export default Paywall
