5e51c56be4fb896d4bf54511ed9b0bedc8430258
Clément Renaud <clement+git@clementrenaud.com>
2023-06-07 15:38:54 +0200

    fetch addresses from contract

--- smart-contracts/proposals/005-redeploy-governor.js
+++ smart-contracts/proposals/005-redeploy-governor.js
@@ -8,13 +8,16 @@
 const { ethers } = require('hardhat')
 const oldGovAddress = '0x508619074f542b6544c5835f260CC704E988cf65'
 const newGovAddress = '0xDcDE260Df00ba86889e8B112DfBe1A4945B35CA9'
-const timeLockAddress = '0xD7477B7c0CdA4204Cf860e4c27486061b15a5AC3'
-
-const TIMELOCK_ADMIN_ROLE = ethers.utils.keccak256(
-  ethers.utils.toUtf8Bytes('TIMELOCK_ADMIN_ROLE')
-)
 
 async function main() {
+  // get addresses
+  const oldGov = await ethers.getContractAt(
+    'UnlockProtocolGovernor',
+    oldGovAddress
+  )
+  const timeLockAddress = oldGov.timeLock()
+  const TIMELOCK_ADMIN_ROLE = oldGov.TIMELOCK_ADMIN_ROLE()
+
   const proposalGrantRoleArgs = {
     contractName: 'UnlockProtocolTimelock',
     contractAddress: timeLockAddress,
