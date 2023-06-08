037bcaf89381ef4487347004e767bc78725ff69f
Julien Genestoux <julien.genestoux@gmail.com>
2023-06-08 15:58:03 -0400

    ux improvements for clarity

--- unlock-app/src/components/interface/checkout/main/Guild.tsx
+++ unlock-app/src/components/interface/checkout/main/Guild.tsx
@@ -1,6 +1,6 @@
 import { CheckoutService } from './checkoutMachine'
 import { Connected } from '../Connected'
-import { Button } from '@unlock-protocol/ui'
+import { Button, minifyAddress } from '@unlock-protocol/ui'
 import { Fragment } from 'react'
 import { useActor } from '@xstate/react'
 import { PoweredByUnlock } from '../PoweredByUnlock'
@@ -9,6 +9,8 @@ import { useAuth } from '~/contexts/AuthenticationContext'
 import { useLockGuild } from '~/hooks/useLockGuild'
 import Link from 'next/link'
 import { useDataForGuild } from '~/hooks/useDataForGuild'
+import { FiExternalLink as ExternalLinkIcon } from 'react-icons/fi'
+import LoadingIcon from '../../Loading'
 
 interface Props {
   injectedProvider: unknown
@@ -20,8 +22,6 @@ export function Guild({ injectedProvider, checkoutService }: Props) {
   const [state, send] = useActor(checkoutService)
   const { recipients, lock } = state.context
 
-  // Check that all recipients are members  of the guild
-  // First, load the guild? Do we need to?
   const users = recipients.length > 0 ? recipients : [account!]
 
   const { isLoading: isLoadingGuild, data: guild } = useLockGuild({
@@ -29,7 +29,7 @@ export function Guild({ injectedProvider, checkoutService }: Props) {
     network: lock!.network,
   })
 
-  const { data, isLoading: useGuildData } = useDataForGuild({
+  const { data, isLoading: isLoadingGuildData } = useDataForGuild({
     lockAddress: lock!.address,
     network: lock!.network,
     recipients: users,
@@ -44,28 +44,99 @@ export function Guild({ injectedProvider, checkoutService }: Props) {
     }
   }
 
-  const isLoading = isLoadingGuild || useGuildData
+  const isLoading = isLoadingGuild || isLoadingGuildData
 
   const disabled = data && data.filter((d) => !d).length > 0
 
   return (
     <Fragment>
       <Stepper service={checkoutService} />
-      <main className="h-full px-6 py-2 overflow-auto">
-        {!isLoading && (
-          <p className=" text-sm">
-            In order to purchase this membership the recipient&apos;s address
-            needs to belong to the{' '}
-            <Link
-              className="text-brand-ui-primary"
-              target="_blank"
-              rel="noreferrer"
-              href={`https://guild.xyz/${guild!.urlName}`}
-            >
-              {guild!.name}
-            </Link>{' '}
-            guild: <em>{guild!.description}</em>.
-          </p>
+      <main className="h-full px-6 py-2 overflow-auto text-sm pt-4">
+        {!isLoadingGuild && (
+          <>
+            <p>
+              Memberships to this lock are restricted to addresses that belong
+              to the{' '}
+              <Link
+                className="text-brand-ui-primary underline"
+                target="_blank"
+                rel="noreferrer"
+                href={`https://guild.xyz/${guild!.urlName}`}
+              >
+                {guild!.name}{' '}
+                <ExternalLinkIcon
+                  size={14}
+                  className="text-brand-ui-primary inline"
+                />
+              </Link>{' '}
+              guild.
+            </p>
+            <ul className="mt-2 mb-4">
+              {users.map((user, index) => {
+                if (isLoadingGuildData) {
+                  return (
+                    <li
+                      key={user}
+                      className="pl-1 mb-2 flex items-center w-full gap-1 text-center"
+                    >
+                      <LoadingIcon size={14} />
+                      {minifyAddress(user)}
+                    </li>
+                  )
+                }
+                if (data && data[index]) {
+                  return (
+                    <li
+                      key={user}
+                      className="pl-1 mb-2 flex items-center w-full gap-1 text-center"
+                    >
+                      ✅ {minifyAddress(user)}
+                    </li>
+                  )
+                }
+                return (
+                  <li
+                    key={user}
+                    className="pl-1 mb-2 flex items-center w-full gap-1 text-center"
+                  >
+                    ❌ {minifyAddress(user)}
+                  </li>
+                )
+              })}
+            </ul>
+            <>
+              {disabled && users.length === 1 && (
+                <Button
+                  className="inline"
+                  as="a"
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`https://guild.xyz/${guild!.urlName}`}
+                  size="tiny"
+                >
+                  Join the Guild!
+                </Button>
+              )}
+            </>
+            <>
+              {disabled && users.length > 1 && (
+                <p>
+                  Some of the recipients that you have selected are not members
+                  of the Guild.{' '}
+                  <Button
+                    className="inline"
+                    as="a"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`https://guild.xyz/${guild!.urlName}`}
+                    size="tiny"
+                  >
+                    Join the Guild!
+                  </Button>
+                </p>
+              )}
+            </>
+          </>
         )}
       </main>
       <footer className="grid items-center px-6 pt-6 border-t">
