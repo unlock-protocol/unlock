121097186794846c3b74f65f80765a6989dc9ee1
Clément Renaud <clement+git@clementrenaud.com>
2023-06-15 18:30:32 +0200

    improve execute script

--- smart-contracts/scripts/gov/execute.js
+++ smart-contracts/scripts/gov/execute.js
@@ -1,5 +1,4 @@
 const { ethers } = require('hardhat')
-const { time } = require('@openzeppelin/test-helpers')
 const {
   getProposalState,
   executeProposal,
@@ -7,49 +6,30 @@ const {
 } = require('../../helpers/gov')
 
 async function main({ proposal, govAddress }) {
-  // env settings
-  const { chainId } = await ethers.provider.getNetwork()
-  const isDev = chainId === 31337
-
   if (!proposal) {
     throw new Error('GOV EXEC > Missing proposal.')
   }
-
+  console.log(proposal)
   const proposalId = proposal.proposalId || (await getProposalId(proposal))
 
   // contract instance etc
   let state = await getProposalState(proposalId, govAddress)
   const gov = await ethers.getContractAt('UnlockProtocolGovernor', govAddress)
 
-  // check if time is ripe
   if (state === 'Queued') {
+    // check if time is ripe
     const eta = await gov.proposalEta(proposalId)
-    const currentTime = (await time.latest()).toNumber()
-    // reach proposal ETA
-    if (!isDev) {
+    if (eta < Date.now()) {
       console.log(
         `GOV EXEC > Proposal still queued until: ${new Date(
           eta.toNumber() * 1000
         )}`
       )
       return
-    } else {
-      // eslint-disable-next-line no-console
-      console.log(
-        `GOV EXEC > : increasing currentTime ${new Date(
-          currentTime * 1000
-        )} to eta ${new Date(eta * 1000)}`
-      )
-      if (currentTime < eta) {
-        await time.increaseTo(eta + 1)
-      }
-      state = await getProposalState(proposalId)
     }
-  }
 
-  if (state === 'Queued') {
     // execute the tx
-    const tx = await executeProposal({ proposal })
+    const tx = await executeProposal({ proposal, govAddress })
     const { events, transactionHash } = await tx.wait()
     const evt = events.find((v) => v.event === 'ProposalExecuted')
     if (evt) {
@@ -59,9 +39,9 @@ async function main({ proposal, govAddress }) {
       )
     }
   } else if (state === 'Executed') {
-    throw new Error('GOV EXEC > Proposal has already been executed')
+    console.log('GOV EXEC > Proposal has already been executed')
   } else {
-    throw new Error(
+    console.log(
       `GOV VOTE > Proposal state (${state}) does not allow execution.`
     )
   }
