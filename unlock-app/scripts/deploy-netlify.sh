a337c89dfc6dde4af0e11c2faf5203bc6161193e
Julien Genestoux <julien.genestoux@gmail.com>
2023-06-15 17:06:54 -0400

    changed netlify cli command

--- unlock-app/scripts/deploy-netlify.sh
+++ unlock-app/scripts/deploy-netlify.sh
@@ -30,11 +30,13 @@ if [ "$DEPLOY_ENV" = "prod" ]; then
 fi
 
 if [ -n "$SITE_ID" ] && [ -n "$AUTH_TOKEN" ]; then
+  # rebuild
+  UNLOCK_ENV="$DEPLOY_ENV" yarn build
   # Package
   UNLOCK_ENV="$DEPLOY_ENV" yarn deploy
   # And ship!
-  echo $MESSAGE
-  npx -y netlify-cli deploy --build -s $SITE_ID -a $AUTH_TOKEN --dir=$BUILD_PATH $PROD --message="$MESSAGE"
+    echo $MESSAGE
+  npx -y netlify-cli deploy -s $SITE_ID -a $AUTH_TOKEN --dir=$BUILD_PATH $PROD --message="$MESSAGE"
 else
   echo "Failed to deploy to Netlify because we're missing SITE_ID and/or AUTH_TOKEN"
   exit 1
--- unlock-protocol-com/scripts/deploy-netlify.sh
+++ unlock-protocol-com/scripts/deploy-netlify.sh
@@ -26,11 +26,13 @@ else
 fi
 
 if [ -n "$SITE_ID" ] && [ -n "$AUTH_TOKEN" ]; then
+  # rebuild
+  UNLOCK_ENV="$DEPLOY_ENV" yarn build
   # Package
   UNLOCK_ENV="$DEPLOY_ENV" yarn deploy
   # And ship!
   echo $MESSAGE
-  npx -y netlify-cli deploy --build -s $SITE_ID -a $AUTH_TOKEN --dir=$BUILD_PATH $PROD --message="$MESSAGE"
+  npx -y netlify-cli deploy -s $SITE_ID -a $AUTH_TOKEN --dir=$BUILD_PATH $PROD --message="$MESSAGE"
 else
   echo "Failed to deploy to Netlify because we're missing SITE_ID and/or AUTH_TOKEN"
   exit 1
