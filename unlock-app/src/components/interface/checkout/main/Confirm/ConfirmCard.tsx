99d09812656bd862e70c2d06f8a02863fc31ff71
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-06-17 03:45:34 +0530

    fix(unlock-app): Add ts comment back (#12190)

--- unlock-app/src/components/interface/checkout/main/Confirm/ConfirmCard.tsx
+++ unlock-app/src/components/interface/checkout/main/Confirm/ConfirmCard.tsx
@@ -216,6 +216,7 @@ export function ConfirmCard({
 
     const stripeIntent = await createPurchaseIntent({
       pricing: totalPricing!.total,
+      // @ts-expect-error - generated types don't narrow down to the right type
       stripeTokenId: payment.cardId!,
       recipients,
       referrers,
