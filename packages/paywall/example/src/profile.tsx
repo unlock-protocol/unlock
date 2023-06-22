8651ced8ac83d3d07570e15b0206aee3a0f8eff8
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-06-22 20:21:05 +0530

    update example

--- packages/paywall/example/src/profile.tsx
+++ packages/paywall/example/src/profile.tsx
@@ -2,8 +2,9 @@ import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { Paywall } from '../../'
 import { networks } from '@unlock-protocol/networks'
-import { useMemo } from 'react'
-
+import { useMemo, useState } from 'react'
+import { WalletService } from '@unlock-protocol/unlock-js'
+import { ethers } from 'ethers'
 export function Profile() {
   const { address, isConnected } = useAccount()
   const provider = useMemo(() => {
@@ -23,6 +24,7 @@ export function Profile() {
   })
 
   const { disconnect } = useDisconnect()
+  const [isLoading, setIsLoading] = useState(false)
 
   if (isConnected) {
     return (
@@ -34,6 +36,28 @@ export function Profile() {
           }}
         >
           Disconnect
+        </button>
+        <button
+          disabled={isLoading}
+          onClick={async () => {
+            setIsLoading(true)
+            try {
+              const web3Provider = new ethers.providers.Web3Provider(provider)
+              const wallet = new WalletService(networks)
+              await wallet.connect(web3Provider)
+              await wallet.createLock({
+                name: 'Demo lock',
+                keyPrice: '0',
+                maxNumberOfKeys: 100,
+                expirationDuration: 100,
+              })
+            } catch (error) {
+              console.error(error)
+            }
+            setIsLoading(false)
+          }}
+        >
+          {isLoading ? 'Creating lock...' : 'Create Lock'}
         </button>
       </div>
     )
--- packages/paywall/example/vite.config.js
+++ packages/paywall/example/vite.config.js
@@ -1,7 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
 })
