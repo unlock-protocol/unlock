dca8ab9f1f35ee512bfed2b21120a360a21de2a3
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-06-23 05:11:40 +0530

    Update unlock-app/src/hooks/usePostmateParent.ts

--- unlock-app/src/hooks/usePostmateParent.ts
+++ unlock-app/src/hooks/usePostmateParent.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import Postmate from 'postmate'
 
-Postmate.debug = true
 
 export const usePostmateParent = (model: any = {}) => {
   const [parent, setParent] = useState<Postmate.ChildAPI | undefined>(undefined)
