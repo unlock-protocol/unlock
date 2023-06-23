be073b94704c04b768c59a8cae7a70bbcadf633c
Kalidou Diagne <kld.diagne@gmail.com>
2023-06-23 19:37:53 +0200

    feat(locksmith) - wedlocks send Linkedin url certification (#12220)
    * linkedin url in certification emails

---
+++ locksmith/__tests__/utils/certificationHelper.test.ts
@@ -0,0 +1,34 @@
+import { it, expect } from 'vitest'
+import { getCertificateLinkedinShareUrl } from '../../src/utils/certificationHelpers'
+
+const lockAddress = '0xDd4356111193f7B28A20b5FC2Dc7750c249E55d2'
+const network = 5
+const tokenId = '2'
+
+const metadata = {
+  name: 'Example with metadata',
+  description: 'exaple test',
+  external_url: 'https://google.it',
+  slug: 'test-demo',
+  attributes: [
+    {
+      trait_type: 'certification_issuer',
+      value: 'UnlockProcol - Demo',
+    },
+  ],
+}
+
+describe('getCertificateLinkedinShareUrl', () => {
+  it('create correct linkedin share url with slug', () => {
+    expect.assertions(1)
+    const url = getCertificateLinkedinShareUrl({
+      lockAddress,
+      network,
+      tokenId,
+      metadata,
+    })
+    expect(url).toBe(
+      'https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=Example+with+metadata&organizationName=UnlockProcol+-+Demo&certUrl=https%3A%2F%2Fstaging-app.unlock-protocol.com%2Fcertification%3Fs%3Dtest-demo&certId=2'
+    )
+  })
+})
