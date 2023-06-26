103f2beb4be3cfa4e34cb887c1e20876636ad848
Kalidou Diagne <kld.diagne@gmail.com>
2023-06-26 10:40:44 -0400

    unlock fee frontend

---
+++ unlock-app/src/components/interface/locks/Settings/forms/CreditCardUnlockFee.tsx
@@ -0,0 +1,69 @@
+import { ToggleSwitch } from '@unlock-protocol/ui'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { ToastHelper } from '~/components/helpers/toast.helper'
+import { storage } from '~/config/storage'
+import { useSaveLockSettings } from '~/hooks/useLockSettings'
+
+interface CreditCardUnlockFeeFormProps {
+  unlockFeeChargedToUser: boolean
+}
+
+interface CreditCardUnlockFeeProps {
+  lockAddress: string
+  network: number
+  disabled: boolean
+}
+
+export default function CreditCardUnlockFee({
+  lockAddress,
+  network,
+  disabled,
+}: CreditCardUnlockFeeProps) {
+  const [includeUnlockFee, setIncludeUnlockFee] = useState(false)
+  const getDefaultValues = async (): Promise<CreditCardUnlockFeeFormProps> => {
+    const { unlockFeeChargedToUser = true } = (
+      await storage.getLockSettings(network, lockAddress)
+    ).data
+
+    setIncludeUnlockFee(unlockFeeChargedToUser)
+    return {
+      unlockFeeChargedToUser,
+    }
+  }
+
+  const { handleSubmit } = useForm<CreditCardUnlockFeeFormProps>({
+    defaultValues: async () => await getDefaultValues(),
+  })
+
+  const saveSettingMutation = useSaveLockSettings()
+
+  const onSubmit = async () => {
+    await saveSettingMutation.mutateAsync({
+      lockAddress,
+      network,
+      unlockFeeChargedToUser: includeUnlockFee,
+    })
+    ToastHelper.success('Fee setting updated.')
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="grid items-center gap-2">
+      <div className="flex flex-col gap-0.5">
+        <div className="flex items-center gap-2">
+          <span className="text-base font-bold text-gray-700">Unlock fees</span>
+          <ToggleSwitch
+            disabled={disabled || saveSettingMutation.isLoading}
+            enabled={includeUnlockFee}
+            setEnabled={setIncludeUnlockFee}
+            onChange={onSubmit}
+          />
+        </div>
+        <div className="text-sm text-gray-700">
+          {`When active Unlock fees will be applied to the total cost of the lock.
+          Otherwise will be "absorbed" by the the Lock manager.`}
+        </div>
+      </div>
+    </form>
+  )
+}
