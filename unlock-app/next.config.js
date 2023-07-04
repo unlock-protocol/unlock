63ce972f0ac9cf637840dd22e6a56ae23916a892
Julien Genestoux <julien.genestoux@gmail.com>
2023-07-04 11:00:21 -0400

    fixing ethpass key

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -14,7 +14,9 @@ const requiredEnvs = {
   base64WedlocksPublicKey: process.env.NEXT_PUBLIC_BASE64_WEDLOCKS_PUBLIC_KEY,
   stripeApiKey:
     process.env.NEXT_PUBLIC_STRIPE_KEY || 'pk_test_BHXKmScocCfrQ1oW8HTmnVrB',
-  ethPassApiKey: process.env.NEXT_PUBLIC_ETHPASS_KEY,
+  ethPassApiKey:
+    process.env.NEXT_PUBLIC_ETHPASS_KEY ||
+    'sk_live_vKGUa9EuZAVffxI3pnhY3p3C2Ccb34s7',
 }
 
 for (const [key, value] of Object.entries(requiredEnvs)) {
