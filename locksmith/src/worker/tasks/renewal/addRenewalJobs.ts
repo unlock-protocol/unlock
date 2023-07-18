d9f96cb5c5d114d3dd82f65b25c2c96c0e47bf7f
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-07-18 12:37:35 +0530

    feat(worker): Reduce the frequency of retries and capture window (#12380)

--- locksmith/src/worker/tasks/renewal/addRenewalJobs.ts
+++ locksmith/src/worker/tasks/renewal/addRenewalJobs.ts
@@ -32,7 +32,7 @@ export const createAddRenewalJobs = (within: number) => {
             },
             {
               jobKey: `fiat-renewal-${key.tokenId}-${key.lock.address}-${network.id}`,
-              maxAttempts: 3,
+              maxAttempts: 2,
             }
           )
         }
@@ -47,7 +47,7 @@ export const createAddRenewalJobs = (within: number) => {
             },
             {
               jobKey: `crypto-renewal-${key.tokenId}-${key.lock.address}-${network.id}`,
-              maxAttempts: 3,
+              maxAttempts: 2,
             }
           )
         }
@@ -56,7 +56,9 @@ export const createAddRenewalJobs = (within: number) => {
   }
   return addRenewalJobs
 }
+// Run this job frequently to catch any expired keys in the last 30 minutes
+export const addRenewalJobs = createAddRenewalJobs(1800)
 // Run this job once a day to catch any keys that may have been missed during the last week
-export const addRenewalJobs = createAddRenewalJobs(86400 * 7)
+export const addRenewalJobsDaily = createAddRenewalJobs(86400 * 7)
 // Run this job once a week to catch any keys that may have been missed during the last year
 export const addRenewalJobsWeekly = createAddRenewalJobs(86400 * 365)
