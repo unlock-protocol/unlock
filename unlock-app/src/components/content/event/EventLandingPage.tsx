e13b903aad9f80ea3eb24edd8c8d0dbe6f68e4ae
Kalidou Diagne <kld.diagne@gmail.com>
2023-07-20 18:59:56 +0200

    restore the problem + solution

--- unlock-app/src/components/content/event/EventLandingPage.tsx
+++ unlock-app/src/components/content/event/EventLandingPage.tsx
@@ -51,6 +51,27 @@ const customers = [
 
 const features = [
   {
+    image: '/images/illustrations/events/easy.svg',
+    name: 'No-code smart contract deployment',
+    description:
+      'Simply fill up the form and hit the deploy button. All the metadata information will automatically be included in the NFT tickets. You can always add and modify other properties at later date.',
+  },
+  {
+    image: '/images/illustrations/events/qr.svg',
+    name: 'QR codes and proof of purchase ticketing',
+    description:
+      'The easiest way to authenticate tickets. Once an attendee purchases a ticket, they will receive an email along with a QR code to check in at the venue.',
+  },
+  {
+    image: '/images/illustrations/events/verifier.svg',
+    name: 'Check-ins at the venue are a breeze',
+    description:
+      'Volunteers or door staff can check attendees in with just a smartphone, and ensure tickets aren’t transferred or reused once someone has come through the door.',
+  },
+]
+
+const problems = [
+  {
     image: '/images/illustrations/events/img-stuck.svg',
     name: 'No-code smart contract deployment',
     description: `Your web3 or crypto event isn't walking the walk, and is using a legacy ticketing solution like Eventbrite, Meetup or Luma instead of embracing the very technology your attendees expect you to champion.`,
@@ -172,9 +193,15 @@ export const EventLandingPage = ({ handleCreateEvent }: LandingPageProps) => {
       }}
       faqs={faqs}
       features={features}
-      callToAction={{
+      problemSection={{
         title: 'The Problem',
         subtitle: `Legacy ticketing solutions don't meet the needs of web3 and crypto event organizers`,
+        items: problems,
+      }}
+      callToAction={{
+        title: 'Ticketing events with Unlock is simple.',
+        subtitle:
+          'From creating the event description to selling tickets to check-in, Unlock built the tools you need.',
         description:
           'Psst, you can also Airdrop tickets to frens or have them stake. Just cherry on your sundae for other customization.',
       }}
--- unlock-app/src/components/interface/LockTypeLandingPage.tsx
+++ unlock-app/src/components/interface/LockTypeLandingPage.tsx
@@ -20,6 +20,12 @@ interface Feature {
   description: string
 }
 
+interface Problem {
+  image: string
+  name: string
+  description: string
+}
+
 interface Faq {
   title: string
   description: ReactNode
@@ -58,6 +64,11 @@ interface LockTypeLandingPageProps {
     title?: string
     items?: Customer[]
   }
+  problemSection?: {
+    title: string
+    subtitle?: string
+    items?: Problem[]
+  }
   callToAction?: {
     title: ReactNode
     subtitle: ReactNode
@@ -78,6 +89,7 @@ export const LockTypeLandingPage = ({
     items: [],
   },
   callToAction,
+  problemSection,
 }: LockTypeLandingPageProps) => {
   return (
     <div className="w-full">
@@ -125,39 +137,71 @@ export const LockTypeLandingPage = ({
           </ul>
         </div>
       </section>
-      <section className="flex flex-col items-center content-center justify-center mt-60 justify-items-center">
-        <div className="flex flex-col justify-center">
-          <div className="flex flex-col gap-4 text-center">
-            <span className="text-3xl font-semibold text-brand-ui-primary">
-              {callToAction?.title}
-            </span>
-            <span className="text-3xl font-bold text-black md:text-5xl ">
-              {callToAction?.subtitle}
-            </span>
+
+      {problemSection && (
+        <section className="flex flex-col items-center content-center justify-center mt-60 justify-items-center">
+          <div className="flex flex-col justify-center">
+            <div className="flex flex-col gap-4 text-center">
+              <span className="text-3xl font-semibold text-brand-ui-primary">
+                {problemSection?.title}
+              </span>
+              <span className="text-3xl font-bold text-black md:text-5xl ">
+                {problemSection?.subtitle}
+              </span>
+            </div>
+            <ul className="grid gap-8 my-10 md:grid-cols-3">
+              {problemSection?.items?.map(({ image, description }, index) => {
+                return (
+                  <li key={index}>
+                    <Image
+                      width="400"
+                      height="300"
+                      alt="problem-image"
+                      className="h-full max-h-80"
+                      src={image}
+                    />
+                    <p className="mt-8 text-sm">{description}</p>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
-          <ul className="grid gap-8 my-10 md:grid-cols-3">
-            {features?.map(({ image, description }, index) => {
-              return (
-                <li className="flex flex-col gap-8" key={index}>
-                  <Image
-                    width="400"
-                    height="300"
-                    alt="problem-image"
-                    src={image}
-                  />
-                  <p className="text-sm">{description}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        </section>
+      )}
+
+      <section className="flex flex-col items-center content-center justify-center mt-60 justify-items-center">
+        <h3 className="text-5xl font-semibold text-center md:w-2/3">
+          {callToAction?.title}
+        </h3>
+        {callToAction?.subtitle && (
+          <p className="mt-4 text-center md:w-1/2">{callToAction?.subtitle}</p>
+        )}
+        <ul className="grid md:grid-cols-[300px_300px_300px] gap-8 my-8">
+          {features?.map(({ image, name, description }) => {
+            return (
+              <li key={name}>
+                <Image
+                  className="border rounded-lg border-1 border-ui-main-100"
+                  width="400"
+                  height="300"
+                  alt="No-code"
+                  src={image}
+                ></Image>
+                <h3 className="mt-2 mb-2 text-xl font-bold">{name}</h3>
+                <p className="text-sm">{description}</p>
+              </li>
+            )
+          })}
+        </ul>
       </section>
+
       <section className="flex flex-col items-center content-center justify-center mt-8 justify-items-center">
         {actions}
         <div className="text-xs text-center md:w-1/3">
           {callToAction?.description}
         </div>
       </section>
+
       {(faqs || [])?.length > 0 && (
         <section className="my-32">
           <h3 className="my-12 text-3xl font-bold text-center">
