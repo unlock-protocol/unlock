3828d72c6a166eb4de21119d7fbc9312f562f78d
Julien Genestoux <julien.genestoux@gmail.com>
2023-07-31 10:17:04 +0200

    adding sentry cron monitoring

--- locksmith/src/worker/server.ts
+++ locksmith/src/worker/server.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node'
 import cron from 'node-cron'
 import { Op } from 'sequelize'
 import { Hook } from '../models'
@@ -6,6 +7,14 @@ import { logger } from '../logger'
 import { notifyExpiredKeysForNetwork } from './jobs/expiredKeys'
 import { notifyExpiringKeysForNetwork } from './jobs/expiringKeys'
 import { startWorker } from './worker'
+
+Sentry.init({
+  dsn: 'https://30c5b6884872435f8cbda4978c349af9@o555569.ingest.sentry.io/5685514',
+  integrations: [],
+  enabled: true, // process.env.NODE_ENV === 'production',
+  environment: process.env.NODE_ENV,
+})
+const monitorSlug = 'workers-running'
 
 logger.info('Websub server started.')
 
@@ -45,7 +54,28 @@ cron.schedule(DAY_CRON_SCHEDULE, async () => {
   await notifyExpiredKeysForNetwork()
 })
 
-startWorker().catch((err) => {
-  console.error(err)
-  process.exit(1)
+// 🟡 Notify Sentry your job is running:
+const checkInId = Sentry.captureCheckIn({
+  monitorSlug,
+  status: 'in_progress',
 })
+
+startWorker()
+  .then(() => {
+    // 🟢 Notify Sentry your job has completed successfully:
+    Sentry.captureCheckIn({
+      checkInId,
+      monitorSlug,
+      status: 'ok',
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+    // 🔴 Notify Sentry your job has failed:
+    Sentry.captureCheckIn({
+      checkInId,
+      monitorSlug,
+      status: 'error',
+    })
+    process.exit(1)
+  })
