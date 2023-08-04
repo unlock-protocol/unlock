98d5fd1bd28952e8a545d2b2d91127b5722d7991
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-04 09:55:46 +0200

    more clearnup

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -2,56 +2,31 @@
 set -e
 
 # two args
-SERVICE=$1
+SERVICE=$1 # locksmith
 HEROKU_APP_NAME=$2
-HEROKU_CONTAINER_TYPE=web
-COMMAND="yarn prod"
-BUILD_DIRECTORY=$1
-
-echo $1
-echo $2
-echo $3
-echo $4
-
-# if container type is provided, use it instead of default
-if [ -n "${3}" ]; then
-    HEROKU_CONTAINER_TYPE=$3
-fi
-
-# if command to run is provided, use it instead.
-if [ -n "${4}" ]; then
-    COMMAND=$4
-fi
-
-# if build directory argument provided, use it instead
-if [ -n "${5}" ]; then
-    BUILD_DIRECTORY=$5
-fi
-
-echo "Using $BUILD_DIRECTORY as build directory"
 
 # install heroku client
 if ! command -v heroku &>/dev/null; then
-    echo "installing heroku"
-    curl https://cli-assets.heroku.com/install.sh | sh
+  echo "installing heroku"
+  curl https://cli-assets.heroku.com/install.sh | sh
 fi
 
 # build web image
-docker build --rm=false --progress=plain  -t locksmith/web --build-arg COMMAND="yarn start" .
+docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
 
 # build worker image
-docker build --rm=false --progress=plain  -t locksmith/worker --build-arg COMMAND="yarn worker:start" .
+docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
 
 # build release image
-docker build --rm=false --progress=plain  -t locksmith/release --build-arg COMMAND="yarn release" .
+docker build --rm=false --progress=plain -t $SERVICE/release --build-arg COMMAND="yarn release" .
 
 docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
 
-docker tag locksmith/web registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
-docker tag locksmith/web registry.heroku.com/$HEROKU_APP_NAME/worker
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/worker
 docker push registry.heroku.com/$HEROKU_APP_NAME/worker
-docker tag locksmith/web registry.heroku.com/$HEROKU_APP_NAME/release
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/release
 docker push registry.heroku.com/$HEROKU_APP_NAME/release
 
 # make sure we are logged in to Heroku
