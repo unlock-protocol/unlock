44bfeefaadca537492dc7024753d7581cf85caa3
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-07 14:09:42 +0200

    refactoring

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -11,27 +11,27 @@ if ! command -v heroku &>/dev/null; then
   curl https://cli-assets.heroku.com/install.sh | sh
 fi
 
-# build web image
-docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
-
-# build worker image
-docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
+docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
 
 # build release image
 docker build --rm=false --progress=plain -t $SERVICE/release --build-arg COMMAND="yarn release" .
-
-docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
-
-docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
-docker push registry.heroku.com/$HEROKU_APP_NAME/web
-docker tag $SERVICE/worker registry.heroku.com/$HEROKU_APP_NAME/worker
-docker push registry.heroku.com/$HEROKU_APP_NAME/worker
 docker tag $SERVICE/release registry.heroku.com/$HEROKU_APP_NAME/release
 docker push registry.heroku.com/$HEROKU_APP_NAME/release
+
+# build web image
+docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+
+# build worker image
+docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
+docker tag $SERVICE/worker registry.heroku.com/$HEROKU_APP_NAME/worker
+docker push registry.heroku.com/$HEROKU_APP_NAME/worker
+
 
 # make sure we are logged in to Heroku
 heroku container:login
 
-# Release 
+# Release all processes
 heroku container:release -a $HEROKU_APP_NAME web worker release
 
