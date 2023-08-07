8e53955526ad838679dc7eb959626e692350350b
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-07 16:20:04 +0200

    refactor(locksmith): changing the deployment scripts for locksmith (#12496)
    * wip
    
    * wip
    
    * more cleanup
    
    * more clearnup
    
    * more cleanup
    
    * adding examples
    
    * adding migration to the release script
    
    * adding deploy-locksmith-staging job temporarily
    
    * replaced the default command
    
    * wip
    
    * Revert "wip"
    
    This reverts commit a5a5e2da0fd9cdadf929389c4100ce0deec37383.
    
    * tagging images correctly
    
    * refactoring
    
    * adding comment about checking that locksmith is up
    
    * cleanup before merging

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -2,56 +2,36 @@
 set -e
 
 # two args
-SERVICE=$1
-HEROKU_APP_NAME=$2
-HEROKU_CONTAINER_TYPE=web
-COMMAND="yarn prod"
-BUILD_DIRECTORY=$1
-
-# if container type is provided, use it instead of default
-if [ -n "${3}" ]; then
-    HEROKU_CONTAINER_TYPE=$3
-fi
-
-# if command to run is provided, use it instead.
-if [ -n "${4}" ]; then
-    COMMAND=$4
-fi
-
-# if build directory argument provided, use it instead
-if [ -n "${5}" ]; then
-    BUILD_DIRECTORY=$5
-fi
-
-echo "Using $BUILD_DIRECTORY as build directory"
-
-echo "Using $COMMAND as the start command for container"
-
-echo "Deploying $SERVICE to Heroku $HEROKU_APP_NAME ..."
+SERVICE=$1 # locksmith
+HEROKU_APP_NAME=$2 # unlock-locksmith-staging or unlock-locksmith-production
 
 # install heroku client
 if ! command -v heroku &>/dev/null; then
-    echo "installing heroku"
-    curl https://cli-assets.heroku.com/install.sh | sh
+  echo "installing heroku"
+  curl https://cli-assets.heroku.com/install.sh | sh
 fi
 
-# build image
-docker build --rm=false --progress=plain -t registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_CONTAINER_TYPE --build-arg COMMAND="$COMMAND" .
-
-# push image to Heroku registry
 docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
-docker push registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_CONTAINER_TYPE
 
-#make sure we are logged in
+# build release image
+docker build --rm=false --progress=plain -t $SERVICE/release --build-arg COMMAND="yarn release" .
+docker tag $SERVICE/release registry.heroku.com/$HEROKU_APP_NAME/release
+docker push registry.heroku.com/$HEROKU_APP_NAME/release
+
+# build web image
+docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+
+# build worker image
+docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
+docker tag $SERVICE/worker registry.heroku.com/$HEROKU_APP_NAME/worker
+docker push registry.heroku.com/$HEROKU_APP_NAME/worker
+
+
+# make sure we are logged in to Heroku
 heroku container:login
 
-# release on heroku
-heroku container:release -a $HEROKU_APP_NAME $HEROKU_CONTAINER_TYPE
+# Release all processes
+heroku container:release -a $HEROKU_APP_NAME web worker release
 
-# migrate the database
-heroku run --app $HEROKU_APP_NAME --type $HEROKU_CONTAINER_TYPE --env BUILD_DIR="$BUILD_DIRECTORY" yarn db:migrate 
-
-if [ $? -ne 0 ]; then
-    echo "Migration failed. Exiting."
-    exit 1
-fi
