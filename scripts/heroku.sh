8ba4f5be8b5f083aecab60787b00bb1d5ec632a0
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-07 14:05:09 +0200

    Revert "wip"
    This reverts commit a5a5e2da0fd9cdadf929389c4100ce0deec37383.

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -11,32 +11,27 @@ if ! command -v heroku &>/dev/null; then
   curl https://cli-assets.heroku.com/install.sh | sh
 fi
 
-rm Dockerfile.web Dockerfile.worker Dockerfile.release
-cp Dockerfile Dockerfile.web
-cp Dockerfile Dockerfile.worker
-cp Dockerfile Dockerfile.release
+# build web image
+docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
 
-# # build web image
-# docker build --rm=false --progress=plain -t $SERVICE/web --build-arg COMMAND="yarn start" .
+# build worker image
+docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
 
-# # build worker image
-# docker build --rm=false --progress=plain -t $SERVICE/worker --build-arg COMMAND="yarn worker:start" .
+# build release image
+docker build --rm=false --progress=plain -t $SERVICE/release --build-arg COMMAND="yarn release" .
 
-# # build release image
-# docker build --rm=false --progress=plain -t $SERVICE/release --build-arg COMMAND="yarn release" .
+docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
 
-# docker login -username=$HEROKU_EMAIL --password=$HEROKU_API_KEY registry.heroku.com
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/worker
+docker push registry.heroku.com/$HEROKU_APP_NAME/worker
+docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/release
+docker push registry.heroku.com/$HEROKU_APP_NAME/release
 
-# docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/web
-# docker push registry.heroku.com/$HEROKU_APP_NAME/web
-# docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/worker
-# docker push registry.heroku.com/$HEROKU_APP_NAME/worker
-# docker tag $SERVICE/web registry.heroku.com/$HEROKU_APP_NAME/release
-# docker push registry.heroku.com/$HEROKU_APP_NAME/release
+# make sure we are logged in to Heroku
+heroku container:login
 
-# # make sure we are logged in to Heroku
-# heroku container:login
-
-# # Release 
-# heroku container:release -a $HEROKU_APP_NAME web worker release
+# Release 
+heroku container:release -a $HEROKU_APP_NAME web worker release
 
