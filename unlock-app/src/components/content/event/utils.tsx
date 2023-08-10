f2ee0a4912566751490d79f98ee10b78bbe67bce
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-10 20:56:30 +0200

    adding server side rendering for events

--- unlock-app/src/components/content/event/utils.tsx
+++ unlock-app/src/components/content/event/utils.tsx
@@ -40,7 +40,7 @@ export const getEventPath = ({
   const slug = metadata?.slug
 
   if (slug) {
-    return `/event?s=${slug}`
+    return `/event/${slug}`
   }
   return `/event?lockAddress=${lockAddress}&network=${network}`
 }
