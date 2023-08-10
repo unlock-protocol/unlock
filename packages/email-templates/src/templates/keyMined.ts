5c0c17fa11c7049f07a13563764276433d398b13
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-10 17:12:47 +0200

    feat(unlock-app): update title and h1 on email templates (#12525)
    update title and h1 on email templates

--- packages/email-templates/src/templates/keyMined.ts
+++ packages/email-templates/src/templates/keyMined.ts
@@ -7,8 +7,8 @@ handlebars.registerHelper('links', links)
 handlebars.registerHelper('transactionLink', transactionLink)
 
 export default {
-  subject: 'A membership was added to your wallet!',
-  html: `<h1>A new Membership NFT in your wallet!</h1>
+  subject: 'You have received a new NFT!',
+  html: `<h1>You have received a new NFT!</h1>
 
 <p>A new membership (#{{keyId}}) to the lock <strong>{{lockName}}</strong> was just minted for you!</p>
 
