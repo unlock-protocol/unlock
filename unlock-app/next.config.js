7efd92a53f5e5e433a23d59378ee9148bfb98680
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-08-15 01:08:24 +0530

    fix(unlock-app): Vercel deployment not considering API routes properly

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -40,6 +40,11 @@ const config = {
   images: {
     unoptimized: true,
   },
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../'),
+  },
 }
 
 module.exports = withSentryConfig(withTM(config))
