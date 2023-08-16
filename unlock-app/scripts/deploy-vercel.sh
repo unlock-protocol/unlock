2f1e7662ea3495cd094f8cfeec6a327ed8cf7166
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-16 09:31:17 +0200

    using the right value to export the env

--- unlock-app/scripts/deploy-vercel.sh
+++ unlock-app/scripts/deploy-vercel.sh
@@ -29,6 +29,7 @@ echo "READY TO DEPLOY TO VERCEL $VERCEL_PROJECT_ID $VERCEL_ORG_ID"
 if [ -n "$VERCEL_PROJECT_ID" ] && [ -n "$VERCEL_TOKEN" ] && [ -n "$VERCEL_ORG_ID" ]; then
   # And ship!
   export UNLOCK_ENV="$DEPLOY_ENV"
+  export NEXT_PUBLIC_UNLOCK_ENV="$DEPLOY_ENV"
   # move to root directory
   cd ..
   npx -y vercel build -y --cwd . --token $VERCEL_TOKEN $PROD
