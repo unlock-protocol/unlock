0bd434ecb21a5d0339fbc81a8f33131578216d28
Julien Genestoux <julien.genestoux@gmail.com>
2023-08-17 13:44:11 +0200

    fix(unlock-app): more fixes to make sure prod deploys on vercel are working (#12565)
    * using the right value to export the env
    
    * adding log messages
    
    * adding missing NEXT_PUBLIC_BASE64_WEDLOCKS_PUBLIC_KEY:
    
    * adding env vars
    
    * copying all env vars inside the container when building
    
    * using the right value to export the env
    
    * adding log messages
    
    * adding missing NEXT_PUBLIC_BASE64_WEDLOCKS_PUBLIC_KEY:
    
    * adding env vars
    
    * copying all env vars inside the container when building
    
    * adding the prefix
    
    * using old approach
    
    * adding missing env var

--- unlock-app/sentry.client.config.js
+++ unlock-app/sentry.client.config.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
-Sentry.init({
+const sentryConfig = {
   dsn:
     SENTRY_DSN ||
     'https://36ea0faad22f4f99a9abc32bd4cb9695@o555569.ingest.sentry.io/4504976441737216',
@@ -21,4 +21,8 @@ Sentry.init({
   enabled: ['production', 'staging'].includes(process.env.NODE_ENV),
   environment: process.env.NODE_ENV,
   autoSessionTracking: true,
-})
+}
+
+console.log('Initializing Sentry with', sentryConfig)
+
+Sentry.init(sentryConfig)
