e60305771aa6bbf859ae997e777d81338c85f553
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-08-29 02:20:03 +0530

    add docs

--- locksmith/src/worker/worker.ts
+++ locksmith/src/worker/worker.ts
@@ -67,7 +67,6 @@ export async function startWorker() {
       addHookJobs,
       sendEmailJob,
       sendHook,
-      sendEmail: sendEmailJob,
       fiatRenewalJob,
       cryptoRenewalJob,
     },
