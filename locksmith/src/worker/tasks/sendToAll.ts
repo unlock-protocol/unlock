277917c770eda6182d948840f6f4e06e8eef7246
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-08-29 23:38:09 +0530

    feat(unlock-app): Frontend custom email changes

--- locksmith/src/worker/tasks/sendToAll.ts
+++ locksmith/src/worker/tasks/sendToAll.ts
@@ -1,11 +1,11 @@
 import { Task } from 'graphile-worker'
 import { z } from 'zod'
-import { UserTokenMetadata } from '../../models'
+import { UnsubscribeList, UserTokenMetadata } from '../../models'
 import normalizer from '../../utils/normalizer'
 
 const Payload = z.object({
   lockAddress: z.string().transform((item) => normalizer.ethereumAddress(item)),
-  network: z.string(),
+  network: z.coerce.number(),
   content: z.string(),
   subject: z.string(),
 })
@@ -24,17 +24,35 @@ export const sendToAllJob: Task = async (payload, helper) => {
       return null
     }
     const lowercased = normalizer.toLowerCaseKeys(metadata)
-    return (
-      lowercased.email || lowercased.emailaddress || lowercased.email_address
-    )
+    return {
+      email:
+        lowercased.email || lowercased.emailaddress || lowercased.email_address,
+      walletAddress: item.userAddress,
+    }
   })
+  const unsubscribedList = await UnsubscribeList.findAll({
+    where: {
+      lockAddress: parsed.lockAddress,
+      network: parsed.network,
+    },
+  })
+
+  const unsubscribed = unsubscribedList.map((item) =>
+    item.userAddress.toLowerCase().trim()
+  )
 
   for (const recipient of recipients) {
     if (!recipient) {
       continue
     }
+
+    // Skip if the user has unsubscribed
+    if (unsubscribed.includes(recipient.walletAddress.toLowerCase().trim())) {
+      continue
+    }
+
     await helper.addJob('sendEmail', {
-      recipient,
+      recipient: recipient.email,
       attachments: [],
       params: {
         content: parsed.content,
