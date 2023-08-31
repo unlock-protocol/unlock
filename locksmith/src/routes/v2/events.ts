11dd4622a332454b4d28294be96a509edb326395
searchableguy <73341821+searchableguy@users.noreply.github.com>
2023-08-31 18:13:31 +0530

    add authenticated middleware

--- locksmith/src/routes/v2/events.ts
+++ locksmith/src/routes/v2/events.ts
@@ -3,9 +3,10 @@ import {
   getEventDetails,
   saveEventDetails,
 } from '../../controllers/v2/eventsController'
+import { authenticatedMiddleware } from '../../utils/middlewares/auth'
 const router = express.Router({ mergeParams: true })
 
 router.get('/:network/:lockAddress', getEventDetails)
-router.post('/save', saveEventDetails)
+router.post('/save', authenticatedMiddleware, saveEventDetails)
 
 export default router
