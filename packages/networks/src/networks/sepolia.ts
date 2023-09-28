752d03a2e9bb8099cad01c751e67cbe31553f7b2
Clément Renaud <clement+git@clementrenaud.com>
2023-09-28 13:57:06 +0200

    basic sepolia config

---
+++ packages/networks/src/networks/sepolia.ts
@@ -0,0 +1,66 @@
+import { NetworkConfig } from '@unlock-protocol/types'
+
+export const sepolia: NetworkConfig = {
+  blockScan: {
+    url: (address: string) => `https://blockscan.com/address/${address}`,
+  },
+  chain: 'base',
+  description:
+    'Sepolia is the primary testnet recommended by Ethereum for dapp development.',
+  explorer: {
+    name: 'Sepolia Etherscan',
+    urls: {
+      address: (address: string) =>
+        `https://sepolia.etherscan.io/address/${address}`,
+      base: `https://sepolia.etherscan.io/`,
+      token: (address: string, holder: string) =>
+        `https://sepolia.etherscan.io/token/${address}?a=${holder}`,
+      transaction: (hash: string) => `https://sepolia.etherscan.io/tx/${hash}`,
+    },
+  },
+  featured: true,
+  fullySubsidizedGas: false,
+  hooks: {
+    onKeyPurchaseHook: [],
+  },
+  id: 11155111,
+  isTestNetwork: true,
+  keyManagerAddress: '',
+  maxFreeClaimCost: 10,
+  multisig: '',
+  name: 'Sepolia',
+  nativeCurrency: {
+    coingecko: 'ethereum',
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+
+  opensea: {
+    collectionUrl: (lockAddress) =>
+      `https://testnets.opensea.io/assets/base/${lockAddress}`,
+    tokenUrl: (_lockAddress, _tokenId) =>
+      `https://testnets.opensea.io/assets/base/${_lockAddress}/${_tokenId}`,
+  },
+
+  previousDeploys: [],
+
+  provider: 'https://rpc.unlock-protocol.com/11155111',
+
+  publicLockVersionToDeploy: 13,
+
+  publicProvider: 'https://developer-access-mainnet.base.org',
+
+  startBlock: 1750000,
+  subgraph: {
+    endpoint: 'https://api.studio.thegraph.com/',
+    endpointV2: 'https://api.studio.thegraph.com/',
+    networkName: 'sepolia',
+    studioEndpoint: 'unlock-protocol-sepolia',
+  },
+  tokens: [],
+  unlockAddress: '',
+  url: 'https://github.com/eth-clients/sepolia',
+}
+
+export default sepolia
