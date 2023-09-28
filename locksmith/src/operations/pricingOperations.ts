5c1ecd943ad7a5dacea9ef0dffe8904e220b5c14
Julien Genestoux <julien.genestoux@gmail.com>
2023-09-28 14:36:11 -0400

    fix(unlock-app, locksmith): fixed fiat symbols (#12709)
    fixed fiat symbols

--- locksmith/src/operations/pricingOperations.ts
+++ locksmith/src/operations/pricingOperations.ts
@@ -66,7 +66,9 @@ export function getCurrencySymbol(currency?: string) {
   return (
     Currencies.find(
       (item) => item?.currency?.toLowerCase() === currency?.toLowerCase()
-    )?.symbol || '$'
+    )?.symbol ||
+    currency?.toUpperCase() ||
+    '$'
   )
 }
 
