bd8e20819f31123dbb8c702d77412c8a00c9665a
Clément Renaud <clement@unlock-protocol.com>
2023-09-29 13:21:23 +0200

    Update smart-contracts/contracts/Unlock.sol

--- smart-contracts/contracts/Unlock.sol
+++ smart-contracts/contracts/Unlock.sol
@@ -436,8 +436,7 @@ contract Unlock is UnlockInitializable, UnlockOwnable {
 
           // tokensToDistribute is either == to the gas cost
           uint tokensToDistribute = ((estimatedGasForPurchase * baseFee) *
-            (100 * 10 ** 18)) /
-            100 /
+            (10 ** 18)
             udtPrice;
 
           // or tokensToDistribute is capped by network GDP growth
