6d673134a3cdd20add2c350897abb5b42dcea133
Pawan <93368863+pavvann@users.noreply.github.com>
2023-10-03 02:36:25 +0530

    deployed password hook on avalanche (#12716)
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- packages/networks/src/networks/avalanche.ts
+++ packages/networks/src/networks/avalanche.ts
@@ -25,6 +25,11 @@ export const avalanche: NetworkConfig = {
         id: HookType.CAPTCHA,
         name: 'Captcha',
       },
+      {
+        address: '0x58D86Dc056c442867485941FeBeA8D3bB4657eAC',
+        id: HookType.PASSWORD,
+        name: 'Password required',
+      }
     ],
   },
   id: 43114,
