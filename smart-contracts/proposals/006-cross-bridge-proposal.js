c093fec83928da8e6150bff94ee77ffc18803c90
Clément Renaud <clement+git@clementrenaud.com>
2023-10-06 12:35:59 +0200

    better parsing of a multichain proposal w desc

--- smart-contracts/proposals/006-cross-bridge-proposal.js
+++ smart-contracts/proposals/006-cross-bridge-proposal.js
@@ -58,8 +58,10 @@ const abiIConnext = [
   },
 ]
 
+const targetChains = [137, 137, 137]
+
 module.exports = async ([
-  destChainId = 137,
+  destChainId,
   destMultisigAddress,
   destAddress,
 ] = []) => {
@@ -82,65 +84,134 @@ module.exports = async ([
     tokenAmount,
   ])
 
-  console.log(`transferTokens(${tokenAmount})`)
+  console.log(`action: transferTokens(${tokenAmount})`)
 
   // src info
   const { chainId } = await ethers.provider.getNetwork()
-  console.log(`From ${chainId} to ${destChainId}`)
+  console.log(`from ${chainId} to ${targetChains.join(', ')}`)
 
   const {
     bridge: { connext: bridgeAddress },
   } = networks[chainId]
 
   // dest info
-  const { bridge, unlockAddress } = networks[destChainId]
-  if (!destAddress) {
-    destAddress = unlockAddress
-  }
-  const { domainId: destDomainId, connextZodiacGnosisAddress } = bridge
+  const explainers = []
+  const parsedCalls = await Promise.all(
+    (destChainId ? [destChainId] : targetChains).map(async (destChainId) => {
+      const {
+        bridge,
+        unlockAddress,
+        name: destChainName,
+      } = networks[destChainId]
+      if (!destAddress) {
+        destAddress = unlockAddress
+      }
 
-  if (!destMultisigAddress) {
-    destMultisigAddress = connextZodiacGnosisAddress
-  }
+      // make sure we have bridge infor in networks package
+      if (!bridge) return {}
 
-  // encode data to be passed to Gnosis Zodiac module
-  // following instructions at https://github.com/gnosis/zodiac-module-connext
-  const moduleData = await ethers.utils.defaultAbiCoder.encode(
-    ['address', 'uint256', 'bytes', 'bool'],
-    [
-      destAddress, // to
-      0, // value
-      calldata, // data
-      0, // operation: 0 for CALL, 1 for DELEGATECALL
-      // 0,
-    ]
+      const { domainId: destDomainId, connextZodiacModuleAddress } = bridge
+
+      if (!destMultisigAddress) {
+        destMultisigAddress = connextZodiacModuleAddress
+      }
+
+      if (!destMultisigAddress || !destDomainId) {
+        throw Error('Missing bridge information')
+      }
+
+      // encode data to be passed to Gnosis Zodiac module
+      // following instructions at https://github.com/gnosis/zodiac-module-connext
+      const moduleData = await ethers.utils.defaultAbiCoder.encode(
+        ['address', 'uint256', 'bytes', 'bool'],
+        [
+          destAddress, // to
+          0, // value
+          calldata, // data
+          0, // operation: 0 for CALL, 1 for DELEGATECALL
+          // 0,
+        ]
+      )
+
+      console.log(moduleData)
+
+      // add small explanation
+      explainers.push([destChainId, destChainName, unlockAddress])
+
+      // proposed changes
+      return {
+        contractAddress: bridgeAddress,
+        contractNameOrAbi: abiIConnext,
+        functionName: 'xcall',
+        functionArgs: [
+          destDomainId,
+          destMultisigAddress, // destMultisigAddress,
+          ADDRESS_ZERO, // asset
+          ADDRESS_ZERO, // delegate
+          0, // amount
+          30, // slippage
+          moduleData, // calldata
+        ],
+      }
+    })
   )
 
-  console.log(moduleData)
-
-  // proposed changes
-  const calls = [
-    {
-      contractAddress: bridgeAddress,
-      contractNameOrAbi: abiIConnext,
-      functionName: 'xcall',
-      functionArgs: [
-        destDomainId,
-        destMultisigAddress, // destMultisigAddress,
-        ADDRESS_ZERO, // asset
-        ADDRESS_ZERO, // delegate
-        0, // amount
-        30, // slippage
-        moduleData, // calldata
-      ],
-    },
-  ]
-
+  // filter out empty calls
+  const calls = parsedCalls.filter((call) => Object.keys(call).length !== 0)
   console.log(calls)
 
+  const proposalName = `Testing the new cross-chain governance workflow
+
+This DAO proposal aims at testing the new cross-chain governance process of Unlock Protocol's DAO. This new governance 
+protocol allow proposals to propagate directly from the main DAO contract to protocol contracts on other chains.
+
+# How it works
+
+To reach another chain, all calls go though the [Connext bridge](https://www.connext.network/) and are 
+executed on the other side of the bridge, after a period of cooldown.
+
+The workflow is as follow
+
+1. a DAO proposal is created, containing 1 call per chain
+2. if the vote succeed, the DAO proposal is executed. All calls are send to the bridge
+3. each call cross the bridge seperately towards its destination on a specific chain
+4. the call is received on the destination chain by Unlock multisig
+5. once received, the call is held in the multisig for a period of X days during which it can be cancelled.
+6. once the cooldown period ends, the call is executed
+
+NB: The cooldown period is useful to prevent malicious or errored calls from being executed 
+
+
+# This proposal
+
+The goal of this proposal is to test this new cross-chain workflow. Here, we are sending instructions
+from this DAO to Unlock core contract on other chains. This is a common case used in protocol upgrades
+or protocol settings change.
+
+Here we don't do anything crazy, just a simple transfer of native tokens on each chain to the zero 
+address. By burning a small amount of tokens we just aim at proving that the process is sound 
+and actually works in practice.
+
+# The calls
+
+All calls are sent to Connext bridge at ${bridgeAddress} on chain ${chainId}:
+
+${explainers
+  .map(
+    ([destChainId, destChainName, destAddress]) =>
+      `- \`transferTokens(${tokenAmount})\` to ${destAddress} on chain ${destChainName} (${destChainId})`
+  )
+  .join('\n')}
+
+
+Onwards !
+
+The Unlock Protocol Team
+`
+  console.log(proposalName)
   // send to multisig / DAO
   return {
-    proposalName: `Bridge Proposal Test: ${destAddress}`,
+    proposalName,
     calls,
   }
 }
