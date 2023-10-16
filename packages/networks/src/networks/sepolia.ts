cfdcc119fd2923298d44e4e4c6000ada7e305c23
Julien Genestoux <julien.genestoux@gmail.com>
2023-10-16 11:48:09 -0400

    fix(unlock-app, locksmith): fixing claim gas costs calculation (#12810)
    fixing claim gas costs calculation

--- packages/networks/src/networks/sepolia.ts
+++ packages/networks/src/networks/sepolia.ts
@@ -47,7 +47,7 @@ export const sepolia: NetworkConfig = {
   id: 11155111,
   isTestNetwork: true,
   keyManagerAddress: '0x338b1f296217485bf4df6CE9f93ab4C73F72b57D',
-  maxFreeClaimCost: 10,
+  maxFreeClaimCost: 1000,
   multisig: '', // SAFE does not support Sepolia as of October 11th 2023
   name: 'Sepolia',
   nativeCurrency: {
