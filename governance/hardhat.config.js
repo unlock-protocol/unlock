960549efed4f475bf7b20987a9329385bb3efc3e
Clément Renaud <clement+git@clementrenaud.com>
2023-10-17 15:18:22 +0200

    move gov,safe tasks over

---
+++ governance/hardhat.config.js
@@ -0,0 +1,45 @@
+// hardhat.config.js
+require('@nomiclabs/hardhat-ethers')
+require('@nomiclabs/hardhat-etherscan')
+
+// import helpers
+const { etherscan, networks } = require('@unlock-protocol/hardhat-helpers')
+
+const settings = {
+  optimizer: {
+    enabled: true,
+    runs: 80,
+  },
+  outputSelection: {
+    '*': {
+      '*': ['storageLayout'],
+    },
+  },
+}
+
+// tasks
+require('./tasks/safe')
+require('./tasks/gov')
+
+// require('./tasks/accounts')
+// require('./tasks/balance')
+// require('./tasks/deploy')
+// require('./tasks/upgrade')
+// require('./tasks/set')
+// require('./tasks/release')
+// require('./tasks/utils')
+// require('./tasks/lock')
+// require('./tasks/verify')
+// require('./tasks/keys')
+// require('./tasks/unlock')
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks,
+  etherscan,
+  solidity: {
+    compilers: [{ version: '0.8.21', settings }],
+  },
+}
