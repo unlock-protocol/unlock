4aded04c0c28599d38bd051e0e4891afa9795e7c
Clément Renaud <clement+git@clementrenaud.com>
2023-10-19 17:06:45 +0200

    add Governor contracts

---
+++ packages/contracts/src/contracts/Governor/UnlockProtocolTimelock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+
+contract UnlockProtocolTimelock is TimelockControllerUpgradeable {
+  function initialize(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
+  ) public initializer {
+    __TimelockController_init(
+      minDelay,
+      proposers,
+      executors,
+      msg.sender // param added when buymping to OZ 4.8.2 - not present in Unlock's deployed version
+    );
+  }
+}
