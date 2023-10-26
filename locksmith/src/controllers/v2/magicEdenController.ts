761a0d459613890c2befceac5cb6953e001cc4bc
Julien Genestoux <julien.genestoux@gmail.com>
2023-10-26 10:02:12 -0400

    computing price for user

--- locksmith/src/controllers/v2/magicEdenController.ts
+++ locksmith/src/controllers/v2/magicEdenController.ts
@@ -1,3 +1,4 @@
+import { Web3Service } from '@unlock-protocol/unlock-js'
 import { z } from 'zod'
 import normalizer from '../../utils/normalizer'
 import networks from '@unlock-protocol/networks'
@@ -9,21 +10,31 @@ const PurchaseBody = z.object({
 })
 
 export const purchase = async (request: Request, response: Response) => {
-  // const _lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
+  const lockAddress = normalizer.ethereumAddress(request.params.lockAddress)
   const network = Number(request.params.network)
+  const web3Service = new Web3Service(networks)
 
   const { address } = await PurchaseBody.parseAsync(request.body)
-  console.log({ address })
   const recipient = normalizer.ethereumAddress(address)
-
   const referrer = normalizer.ethereumAddress(
     networks[network]?.multisig || recipient
   )
+  const data = '0x'
+
+  // Get the price of that user
+  const keyPrice = await web3Service.purchasePriceFor({
+    lockAddress,
+    userAddress: recipient,
+    network,
+    data,
+    referrer,
+  })
 
   // TODO: check version of the lock as it may impact the signature
   // TODO: simulate tx to identify if it would fail!
   // We need to return the params for the purchase function
   const params = {
+    _values: [keyPrice],
     _recipients: [recipient],
     _referrers: [referrer],
     _keyManagers: [ethers.constants.AddressZero],
