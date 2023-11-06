0b744edb7b5c3966c71ece9680fe73ac2d5d9ea4
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-06 15:48:53 -0500

    feature(locksmith): adding a task to monitor balances (#12944)
    * adding a task to monitor balances
    
    * checking all signers

---
+++ locksmith/src/worker/tasks/checkBalances.ts
@@ -0,0 +1,53 @@
+import * as Sentry from '@sentry/node'
+import { Task } from 'graphile-worker'
+import {
+  getAllPurchasers,
+  getProviderForNetwork,
+} from '../../fulfillment/dispatcher'
+import { getDefiLammaPrice } from '../../operations/pricingOperations'
+import networks from '@unlock-protocol/networks'
+import logger from '../../logger'
+import { ethers } from 'ethers'
+
+const MIN_BALANCE = 50
+
+export const checkBalances: Task = async () => {
+  // Look for balnces for all purchasers, trigger if they are below a threashold.
+  Object.values(networks)
+    .filter((network) => network.name !== 'localhost')
+    .map(async (network: any) => {
+      try {
+        const [provider, purchasers] = await Promise.all([
+          getProviderForNetwork(network.id),
+          getAllPurchasers({ network: network.id }),
+        ])
+        purchasers.forEach(async (purchaser) => {
+          const address = await purchaser.getAddress()
+          const balance = ethers.utils.formatEther(
+            await provider.getBalance(address)
+          )
+          const usdPricing = await getDefiLammaPrice({
+            network: parseInt(network.id, 10),
+          })
+          if (!usdPricing.price) {
+            // We can't get a $ price for this network, so we can't check the balance
+            logger.info(
+              `Missing price conversion for ${network.name}. Can't compare balance.`
+            )
+            return
+          }
+          const balanceInUSD = usdPricing.price! * parseFloat(balance)
+          if (balanceInUSD < MIN_BALANCE) {
+            Sentry.captureMessage(
+              `Insufficient balance (${balanceInUSD}) for ${address} for network ${network.id} (${network.name})`
+            )
+          }
+        })
+      } catch (error) {
+        logger.error(
+          `Error checking balance for network ${network.name}`,
+          error
+        )
+      }
+    })
+}
