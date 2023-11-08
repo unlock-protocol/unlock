4dfa1c2e061a2b4a1ce9ae73c2286107b119f93d
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-08 11:59:41 -0500

    feature(smart-contracts): adding a cap for the password hook (#12958)
    * adding a cap for the password hook
    
    * fixing contract
    
    * adding cap on discount hook
    
    * reviewer feedback

--- smart-contracts/test/Lock/hooks/DiscountCodeHook.js
+++ smart-contracts/test/Lock/hooks/DiscountCodeHook.js
@@ -89,13 +89,14 @@ describe('DiscountHook', function () {
 
     const code = 'PROMOCODE'
     const discount = 3000
+    const cap = 10
     const [data, signer] = await getSignatureForPassword(
       code,
       user.address.toLowerCase()
     )
 
     // Set the code on the hook for the lock
-    await (await hook.setSigner(lock.address, signer, discount)).wait()
+    await (await hook.setSigner(lock.address, signer, discount, cap)).wait()
 
     // Let's get the price without a promo code
     const price = await lock.purchasePriceFor(user.address, user.address, data)
@@ -116,5 +117,89 @@ describe('DiscountHook', function () {
     ).wait()
 
     expect((await lock.balanceOf(user.address)).toNumber()).to.equal(1)
+  })
+
+  it('should enforce the cap', async function () {
+    const [user, other] = await ethers.getSigners()
+
+    await unlock.deployProtocol()
+    const expirationDuration = 60 * 60 * 24 * 7
+    const maxNumberOfKeys = 100
+    const keyPrice = ethers.utils.parseEther('0.1')
+
+    const { lock } = await unlock.createLock({
+      expirationDuration,
+      maxNumberOfKeys,
+      keyPrice,
+      name: 'ticket',
+    })
+    const DiscountHook = await ethers.getContractFactory('DiscountHook')
+    const hook = await DiscountHook.deploy()
+    await hook.deployed()
+
+    await (
+      await lock.setEventHooks(
+        hook.address,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero
+      )
+    ).wait()
+    // Let's get the price without a promo code
+    const priceWithout = await lock.purchasePriceFor(
+      user.address,
+      user.address,
+      []
+    )
+    assert.equal(
+      ethers.utils.formatEther(priceWithout),
+      ethers.utils.formatEther(keyPrice)
+    )
+
+    const code = 'PROMOCODE'
+    const discount = 3000
+    const cap = 1
+    const [data, signer] = await getSignatureForPassword(
+      code,
+      user.address.toLowerCase()
+    )
+
+    // Set the code on the hook for the lock
+    await (await hook.setSigner(lock.address, signer, discount, cap)).wait()
+
+    // Let's get the price without a promo code
+    const price = await lock.purchasePriceFor(user.address, user.address, data)
+    assert.equal(ethers.utils.formatEther(price), '0.07')
+
+    // Let's make a purchase!
+    await (
+      await lock.purchase(
+        [price],
+        [user.address],
+        [user.address],
+        [user.address],
+        [data],
+        {
+          value: price,
+        }
+      )
+    ).wait()
+
+    expect((await lock.balanceOf(user.address)).toNumber()).to.equal(1)
+
+    // Let's now get the price again for another user with the same code
+    const [dataOther] = await getSignatureForPassword(
+      code,
+      other.address.toLowerCase()
+    )
+    const priceOther = await lock.purchasePriceFor(
+      other.address,
+      other.address,
+      dataOther
+    )
+    assert.equal(ethers.utils.formatEther(priceOther), '0.1')
   })
 })
