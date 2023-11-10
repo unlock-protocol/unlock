4f3b07c878df8a76c1431d948a2b4be4c34f193e
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-10 09:50:34 -0500

    feature(locksmith): adding an API endpoint to list all events (#12979)
    adding an API endpoint to list all events

--- locksmith/src/routes/v2/events.ts
+++ locksmith/src/routes/v2/events.ts
@@ -3,6 +3,7 @@ import {
   getEventDetailsByLock,
   saveEventDetails,
   getEvent,
+  getAllEvents,
 } from '../../controllers/v2/eventsController'
 import { authenticatedMiddleware } from '../../utils/middlewares/auth'
 import { eventOrganizerMiddleware } from '../../utils/middlewares/eventOrganizerMiddleware'
@@ -10,6 +11,7 @@ const router = express.Router({ mergeParams: true })
 
 router.get('/:network/:lockAddress', getEventDetailsByLock)
 router.get('/:slug', getEvent)
+router.get('/', getAllEvents)
 router.post(
   '/save',
   authenticatedMiddleware,
