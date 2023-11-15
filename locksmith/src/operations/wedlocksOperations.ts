4eb99917985e54548322698268ef4e95f0c8b621
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-15 21:15:22 +0300

    disabled emails for filbangalore

--- locksmith/src/operations/wedlocksOperations.ts
+++ locksmith/src/operations/wedlocksOperations.ts
@@ -84,7 +84,11 @@ export const sendEmail = async ({
     emailSender,
   } = await getLockSettings(params.lockAddress, network)
 
-  if (!canSendEmail) {
+  if (
+    !canSendEmail ||
+    params.lockAddress.toLocaleLowerCase() ===
+      '0x02c510be69fe87e052e065d8a40b437d55907b48'.toLowerCase()
+  ) {
     logger.info('Email sending disabled for', {
       lockAddress: params.lockAddress,
     })
