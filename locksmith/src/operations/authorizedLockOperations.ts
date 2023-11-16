a9aace7c4019f45ae813e261ae7e327cfbce13d9
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-16 11:23:35 +0300

    refactor(locksmith): making computing card payment faster

--- locksmith/src/operations/authorizedLockOperations.ts
+++ locksmith/src/operations/authorizedLockOperations.ts
@@ -1,33 +1,19 @@
-import { Web3Service } from '@unlock-protocol/unlock-js'
-import networks from '@unlock-protocol/networks'
-import * as Normalizer from '../utils/normalizer'
-import logger from '../logger'
-import { getPurchaser } from '../fulfillment/dispatcher'
+import { getSignerWhoIsKeyGranterOnLock } from '../fulfillment/dispatcher'
 
 export const hasAuthorization = async (
   address: string,
   network: number
 ): Promise<boolean> => {
-  const lockAddress = Normalizer.ethereumAddress(address)
-  const web3Service = new Web3Service(networks)
+  const wallet = await getSignerWhoIsKeyGranterOnLock({
+    lockAddress: address,
+    network,
+  })
 
-  const wallet = await getPurchaser({ network })
-
-  const purchaserAddress = await wallet.getAddress()
-
-  try {
-    const isKeyGranter = await web3Service.isKeyGranter(
-      lockAddress,
-      purchaserAddress,
-      network
-    )
-    return isKeyGranter
-  } catch (error: any) {
-    logger.error(
-      `Could not check if lock ${lockAddress} authorized ${purchaserAddress} to grant keys on ${network}. ${error.message}`
-    )
+  if (!wallet) {
     return false
   }
+
+  return true
 }
 
 const AuthorizedLockOperations = {
