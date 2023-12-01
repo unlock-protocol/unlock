753d0da7f3ea21d8b3d63183d0710e1f945725d7
Julien Genestoux <julien.genestoux@gmail.com>
2023-12-01 13:04:16 -0500

    revealing filbangalore tickets

--- locksmith/src/operations/metadataOperations.ts
+++ locksmith/src/operations/metadataOperations.ts
@@ -115,12 +115,12 @@ export const getBaseTokenData = async (
   // Temporary for FilBangalore
   // Ok to remove after 03/31/2024
   // Uncoment for reveal!
-  // if (
-  //   address.toLowerCase() == '0x02c510be69fe87e052e065d8a40b437d55907b48' &&
-  //   network == 42161
-  // ) {
-  //   result.image = `${host}/${network}/lock/${address}/icon?id=${keyId}`
-  // }
+  if (
+    address.toLowerCase() == '0x02c510be69fe87e052e065d8a40b437d55907b48' &&
+    network == 42161
+  ) {
+    result.image = `${host}/${network}/lock/${address}/icon?id=${keyId}`
+  }
 
   return result
 }
