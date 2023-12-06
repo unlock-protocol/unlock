cfbb6c6cc051952cc5d86a2c158c7b88a5ab8e9e
Julien Genestoux <julien.genestoux@gmail.com>
2023-12-06 13:55:27 -0500

    fix deployment script

--- governance/scripts/deployments/unlock.js
+++ governance/scripts/deployments/unlock.js
@@ -12,13 +12,14 @@ async function main({ unlockVersion } = {}) {
   // need to fetch previous unlock versions
   if (unlockVersion) {
     console.log(`Setting up version ${unlockVersion} from package`)
-    Unlock = await copyAndBuildContractsAtVersion(__dirname, [
-      { contractName: 'Unlock', version: unlockVersion },
-    ])
+    Unlock = (
+      await copyAndBuildContractsAtVersion(__dirname, [
+        { contractName: 'Unlock', version: unlockVersion },
+      ])
+    )[0]
   } else {
     throw 'Need to set --unlock-version'
   }
-  console.log(Unlock.interface)
 
   const unlock = await upgrades.deployProxy(Unlock, [deployer.address], {
     initializer: 'initialize(address)',
