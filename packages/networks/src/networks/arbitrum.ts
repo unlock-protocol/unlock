5a44e6bbebb1bf4c1468730128299abd9fb1819a
Julien Genestoux <julien.genestoux@gmail.com>
2023-12-19 11:22:38 -0500

    wip

--- packages/networks/src/networks/arbitrum.ts
+++ packages/networks/src/networks/arbitrum.ts
@@ -88,7 +88,7 @@ export const arbitrum: NetworkConfig = {
       address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
       decimals: 6,
       name: 'USD Coin',
-      symbol: 'USDC',
+      symbol: 'USDC.e',
     },
     {
       address: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
