65f2ffd08fe48f260ef7f114f87a647b1f060640
Julien Genestoux <julien.genestoux@gmail.com>
2024-01-04 18:19:11 -0500

    fix(linting): more linting fixes

--- unlock-app/src/config/app.ts
+++ unlock-app/src/config/app.ts
@@ -56,8 +56,8 @@ export const config = {
   locksmithSigners: [
     '0x58b5CeDE554a39666091F96C8058920dF5906581',
     '0x22c095c69c38b66afAad4eFd4280D94Ec9D12f4C',
-    '0xc7c6759c2cf2ad209f7aebd2c7012543f7bb5db4', // Goerli OZ Relay
-    '0xd851fe9ba8efa66e65d7865690bd2b9522c6e99f', // OZ Relay
+    '0xC7C6759C2Cf2AD209F7AebD2C7012543F7BB5db4', // Goerli OZ Relay
+    '0xd851fe9ba8EfA66e65d7865690bD2B9522C6E99f', // OZ Relay
   ], // TODO: cleanup? We should use config from networks package!
   networks: Object.keys(networksConfig).reduce<NetworkConfigs>(
     (networks, network) => {
