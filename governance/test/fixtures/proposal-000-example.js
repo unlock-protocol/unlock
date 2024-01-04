d49ef383dad691bd23378eadc18d61c0ea1ef798
Julien Genestoux <julien.genestoux@gmail.com>
2024-01-04 12:31:21 -0500

    fix(governance): fixing lint

--- governance/test/fixtures/proposal-000-example.js
+++ governance/test/fixtures/proposal-000-example.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers')
 const { UnlockDiscountTokenV2 } = require('@unlock-protocol/contracts')
-const tokenRecipientAddress = '0x70997970c51812dc3a010c7d01b50e0d17dc79c8'
-const proposerAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+const tokenRecipientAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
+const proposerAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
 
 module.exports = {
   proposalName: '#000 This is just an example!',
