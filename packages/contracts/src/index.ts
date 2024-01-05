343d7668d22242005e3c5bb1b8dffa1fa4d6e772
Clément Renaud <clement+git@clementrenaud.com>
2024-01-05 20:25:44 +0100

    export purchaser abi correctly

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -36,6 +36,7 @@ import UnlockDiscountTokenV2 from './abis/UnlockDiscountToken/UnlockDiscountToke
 import GovernorUnlockProtocol from './abis/Governor/UnlockProtocolGovernor.json'
 import GovernorUnlockProtocolTimelock from './abis/Governor/UnlockProtocolTimelock.json'
 import LockSerializer from './abis/utils/LockSerializer.json'
+import UnlockSwapPurchaser from './abis/utils/UnlockSwapPurchaser.json'
 
 // exports
 export { PublicLockV0 }
@@ -72,4 +73,4 @@ export { UnlockDiscountTokenV1 }
 export { UnlockDiscountTokenV2 }
 export { GovernorUnlockProtocol }
 export { GovernorUnlockProtocolTimelock }
-export { LockSerializer }
+export { LockSerializer, UnlockSwapPurchaser }
