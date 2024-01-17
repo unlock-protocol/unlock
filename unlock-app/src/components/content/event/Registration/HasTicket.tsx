8652b7c9110f2c9bd409871f82686ba57ad1168c
Julien Genestoux <julien.genestoux@gmail.com>
2024-01-17 16:20:32 -0500

    fixing card

--- unlock-app/src/components/content/event/Registration/HasTicket.tsx
+++ unlock-app/src/components/content/event/Registration/HasTicket.tsx
@@ -1,13 +1,18 @@
+import { Card } from '@unlock-protocol/ui'
 import Link from 'next/link'
 
 export const HasTicket = () => {
   return (
-    <p className="text-lg">
-      🎉 You already have a ticket! You can view it in{' '}
-      <Link className="underline" href="/keychain">
-        your keychain
-      </Link>
-      .
-    </p>
+    <Card className="grid gap-6 mt-10 lg:mt-0">
+      <div className="grid gap-6 md:gap-8">
+        <p className="text-lg">
+          🎉 You already have a ticket! You can view it in{' '}
+          <Link className="underline" href="/keychain">
+            your keychain
+          </Link>
+          .
+        </p>
+      </div>
+    </Card>
   )
 }
