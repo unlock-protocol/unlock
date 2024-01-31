f51e7457a23c9dbeff3fc3029d4ee3c46cb72ef6
Clément Renaud <clement+git@clementrenaud.com>
2024-01-31 14:54:15 +0100

    add network file

---
+++ packages/networks/src/networks/scroll.ts
@@ -0,0 +1,80 @@
+import { NetworkConfig } from '@unlock-protocol/types'
+
+export const scroll: NetworkConfig = {
+  chain: 'ethereum',
+  description:
+    'The original and most secure EVM network. Gas fees are expensive on this network.',
+  explorer: {
+    name: 'Etherscan',
+    urls: {
+      address: (address) => `https://scrollscan.com/address/${address}`,
+      base: 'https://scrollscan.com/',
+      token: (address, holder) =>
+        `https://etherscan.com/token/${address}?a=${holder}`,
+      transaction: (hash) => `https://scrollscan.com/tx/${hash}`,
+    },
+  },
+  featured: false,
+  id: 534352,
+  isTestNetwork: false,
+  multisig: '',
+  name: 'Scroll',
+  nativeCurrency: {
+    coingecko: 'ethereum',
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+    wrapped: '0x5300000000000000000000000000000000000004',
+  },
+  provider: 'https://rpc.unlock-protocol.com/534352',
+  publicLockVersionToDeploy: 14,
+  publicProvider: 'https://rpc.scroll.io',
+  startBlock: 0,
+  subgraph: {
+    endpoint:
+      'https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock-scroll',
+    endpointV2:
+      'https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock-scroll',
+    studioEndpoint: 'unlock-protocol-scroll',
+  },
+  tokens: [
+    {
+      address: '0x5300000000000000000000000000000000000004',
+      decimals: 18,
+      featured: true,
+      name: 'Wrapped Ether',
+      symbol: 'WETH',
+    },
+    {
+      address: '0xcA77eB3fEFe3725Dc33bccB54eDEFc3D9f764f97',
+      decimals: 18,
+      featured: true,
+      name: 'Dai Stablecoin',
+      symbol: 'DAI',
+    },
+    {
+      address: '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4',
+      decimals: 6,
+      featured: true,
+      name: 'USD Coin',
+      symbol: 'USDC',
+    },
+    {
+      address: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
+      decimals: 6,
+      name: 'Tether USD',
+      symbol: 'USDT',
+    },
+    {
+      address: '0x3C1BCa5a656e69edCD0D4E36BEbb3FcDAcA60Cf1',
+      decimals: 8,
+      featured: true,
+      name: 'Wrapped BTC',
+      symbol: 'WBTC',
+    },
+  ],
+  unlockAddress: '',
+  url: 'https://scroll.io',
+}
+
+export default scroll
