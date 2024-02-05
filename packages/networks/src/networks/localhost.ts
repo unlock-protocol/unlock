f7ab235f360665f8478789801b6db67138b7d2af
Julien Genestoux <julien.genestoux@gmail.com>
2024-02-05 14:58:06 -0500

    Revert "fix(networks): remove localhost from networks package (#13308)"
    This reverts commit d9390f4f766aea77333c48290575dae303933968.

---
+++ packages/networks/src/networks/localhost.ts
@@ -0,0 +1,25 @@
+import { NetworkConfig } from '@unlock-protocol/types'
+
+// We use Partial<NetworkConfig> for localhost as we don't have all the information
+export const localhost: Partial<NetworkConfig> = {
+  chain: 'localhost',
+  description: 'Localhost network.',
+  featured: false,
+  fullySubsidizedGas: true,
+  id: 31337,
+  isTestNetwork: true,
+  name: 'localhost',
+  nativeCurrency: {
+    coingecko: 'ethereum',
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  provider: 'http://127.0.0.1:8545',
+  publicLockVersionToDeploy: 13,
+  publicProvider: 'http://127.0.0.1:8545',
+  subgraph: {
+    endpoint: 'http://localhost:8000/subgraphs/name/unlock-protocol/unlock',
+    endpointV2: 'http://localhost:8000/subgraphs/name/testgraph',
+  },
+}
