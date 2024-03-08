21d5605211ea1037874e6853190aec5003807314
Julien Genestoux <julien.genestoux@gmail.com>
2024-03-08 17:24:10 -0500

    event frames shows description

--- unlock-app/app/frames/event/Components/Description.tsx
+++ unlock-app/app/frames/event/Components/Description.tsx
@@ -1,36 +1,32 @@
 import React from 'react'
+import Container from './Container'
+import { FrameButton, FrameImage } from 'frames.js/next/server'
+import { config } from '../../../../src/config/app'
 
-interface Props {
-  name: string
-  startTime?: string
-  location?: string
-  iconURL?: string
-  bannerURL?: string
-}
-
-export const DefaultImage = ({
-  name,
-  startTime,
-  location,
-  iconURL,
-  bannerURL,
-}: Props) => {
+export const Description = ({
+  event,
+  previousFrame,
+  state,
+  dispatch,
+}: {
+  event: any
+  previousFrame: any
+  state: any
+  dispatch: any
+}) => {
   return (
-    <div tw="flex flex-col bg-[#F5F5F5] h-full w-full">
-      <div tw="relative flex flex-col w-full h-80">
-        {iconURL && (
-          <img
-            width="64"
-            height="64"
-            src={iconURL}
-            tw="w-64 h-64 top-16 left-12 rounded-xl border-4 shadow-xl border-white"
-            aria-label={name}
-          />
-        )}
-      </div>
-      <div tw="flex items-center justify-between w-full px-12">
-        <p>Description!</p>
-      </div>
-    </div>
+    <Container slug={event.slug} previousFrame={previousFrame} state={state}>
+      <FrameImage>
+        <p tw="px-4">{event.data.description}</p>
+      </FrameImage>
+      <FrameButton
+        action="link"
+        target={`${config.unlockApp}/event/${event.slug}`}
+      >
+        Register
+      </FrameButton>
+      {/* @ts-expect-error Type '{ children: string; onClick: any; }' is not assignable to type 'IntrinsicAttributes & FrameButtonProvidedProps'. */}
+      <FrameButton onClick={dispatch}>Back</FrameButton>
+    </Container>
   )
 }
--- unlock-app/app/frames/event/[slug]/page.tsx
+++ unlock-app/app/frames/event/[slug]/page.tsx
@@ -1,24 +1,16 @@
 import { storage } from '../../../../src/config/storage'
-import { toFormData } from '../../../../src/components/interface/locks/metadata/utils'
-import dayjs from '../../../../src/utils/dayjs'
+import React from 'react'
+import { useFramesReducer, getPreviousFrame } from 'frames.js/next/server'
+import { DefaultFrame } from '../Components/DefaultFrame'
+import { Description } from '../Components/Description'
 
-import {
-  FrameContainer,
-  FrameImage,
-  FrameButton,
-  useFramesReducer,
-  getPreviousFrame,
-  validateActionSignature,
-  FrameInput,
-} from 'frames.js/next/server'
-import { config } from '../../../../src/config/app'
-import { DefaultImage } from '../Components/DefaultImage'
-
-const reducer = (state, action) => {
-  console.log('action')
-  console.log(action)
-  console.log('state')
-  console.log(state)
+const reducer = (state: any, action: any) => {
+  if (action.prevState.view === 'default') {
+    return { view: 'description' }
+  }
+  if (action.prevState.view === 'description') {
+    return { view: 'default' }
+  }
   return { ...state }
 }
 
@@ -35,48 +27,34 @@ export default async function Frame(props: HomeProps) {
   const slug = props.params.slug
   const previousFrame = getPreviousFrame(props.searchParams)
   // await validateActionSignature(previousFrame.postBody)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [state, dispatch] = useFramesReducer(
     reducer,
     { view: 'default' },
     previousFrame
   )
 
-  const { data: eventMetadata } = await storage
-    .getEvent(slug)
-    .catch((error) => {
-      console.error(error)
-      return { data: null }
-    })
-  if (!eventMetadata?.data) {
+  const { data: event } = await storage.getEvent(slug)
+  if (!event?.data) {
     return new Response('Event not found', { status: 404 })
   }
 
-  const { ticket: event, image } = toFormData(eventMetadata.data)
-
+  if (state.view === 'description') {
+    return (
+      <Description
+        dispatch={dispatch}
+        state={state}
+        previousFrame={previousFrame}
+        event={event}
+      />
+    )
+  }
   return (
-    <FrameContainer
-      pathname={`${config.unlockApp}/frames/event/${slug}`}
-      postUrl={`${config.unlockApp}/frames/event`}
-      state={{}}
+    <DefaultFrame
+      dispatch={dispatch}
+      state={state}
       previousFrame={previousFrame}
-    >
-      <FrameImage>
-        <DefaultImage
-          // @ts-expect-error Property 'name' does not exist on type 'GetEvent200Response'.
-          name={eventMetadata.name}
-          startTime={dayjs
-            .tz(event.event_start_date, event.event_timezone)
-            .toDate()
-            .toDateString()}
-          location={event?.event_address}
-          bannerURL={event.event_cover_image}
-          iconURL={image}
-        />
-      </FrameImage>
-      <FrameButton action="link" target={`${config.unlockApp}/event/${slug}`}>
-        Register
-      </FrameButton>
-      <FrameButton onClick={dispatch}>See Description</FrameButton>
-    </FrameContainer>
+      event={event}
+    />
   )
 }
