a13f7ca5a1ceee8a08acdb42be126ebb630e9f3e
0xTxbi <techiejossy@gmail.com>
2024-03-10 15:26:53 +0100

    add gitcoin passport contract hook

---
+++ unlock-app/src/components/interface/locks/Settings/forms/hooksComponents/GitcoinContractHook.tsx
@@ -0,0 +1,56 @@
+import { CustomComponentProps } from '../UpdateHooksForm'
+import { Button } from '@unlock-protocol/ui'
+import networks from '@unlock-protocol/networks'
+import { Hook, HookType } from '@unlock-protocol/types'
+import { useFormContext } from 'react-hook-form'
+import { ADDRESS_ZERO } from '~/constants'
+
+export const GitcoinContractHook = ({
+  disabled,
+  defaultValue,
+  network,
+  setEventsHooksMutation,
+}: CustomComponentProps) => {
+  const hookAddress =
+    networks?.[network]?.hooks?.onKeyPurchaseHook?.find(
+      (hook: Hook) => hook.id === HookType.GITCOIN
+    )?.address ?? ''
+
+  const { handleSubmit } = useFormContext()
+
+  const isActive = defaultValue === hookAddress
+
+  const onSubmit = async (values: any) => {
+    if (isActive) {
+      // Disable!
+      setEventsHooksMutation.mutateAsync({
+        ...values,
+        keyPurchase: ADDRESS_ZERO,
+      })
+    } else {
+      setEventsHooksMutation.mutateAsync(values)
+    }
+  }
+
+  const disabledInput = disabled || setEventsHooksMutation.isLoading
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+      <p className="text-sm">
+        {isActive
+          ? 'Gitcoin passport verification is active, and will be required as part of the Unlock checkout process.'
+          : 'Gitcoin passport verification is not currently active.'}
+      </p>
+      <div className="ml-auto">
+        <Button
+          loading={setEventsHooksMutation.isLoading}
+          size="small"
+          type="submit"
+          disabled={disabledInput}
+        >
+          {isActive ? 'Disable' : 'Enable'}
+        </Button>
+      </div>
+    </form>
+  )
+}
