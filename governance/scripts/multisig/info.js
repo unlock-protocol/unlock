03f5eb432c1420bba19f76c2b787c543885258b6
Clément Renaud <clement+git@clementrenaud.com>
2024-03-13 17:38:28 +0100

    catch multisig errors

--- governance/scripts/multisig/info.js
+++ governance/scripts/multisig/info.js
@@ -1,75 +1,62 @@
 const { ethers } = require('hardhat')
 const { getProvider, safeServiceURLs } = require('./_helpers')
-const oldMultisigABI = require('@unlock-protocol/hardhat-helpers/dist/ABIs/multisig.json')
 const multisigABI = require('@unlock-protocol/hardhat-helpers/dist/ABIs/multisig2.json')
 const { networks } = require('@unlock-protocol/networks')
 
 const SafeApiKit = require('@safe-global/api-kit').default
 
-const errorLog = (txt) => console.log(`⚠️: ${txt}`)
+const getMultigiInfo = async (chainId, multisig) => {
+  const errors = []
+  const provider = await getProvider(chainId)
+  // get Safe service
+  const safeService = new SafeApiKit({
+    chainId,
+    txServiceUrl: safeServiceURLs[chainId] || null,
+  })
+
+  const { count } = await safeService.getPendingTransactions(multisig)
+  if (count) {
+    errors.push(`${count} pending txs are waiting to be signed`)
+  }
+  // the flags to get only un-executed transactions does not work
+  // filed here https://github.com/safe-global/safe-core-sdk/issues/690
+  // const allTxs = await safeService.getAllTransactions(multisig, {
+  //   executed: false,
+  //   trusted: false,
+  //   queued: false,
+  // })
+
+  const safe = new ethers.Contract(multisig, multisigABI, provider)
+  const owners = await safe.getOwners()
+  const policy = await safe.getThreshold()
+  if (!multisig) {
+    errors.push('Missing multisig')
+  } else {
+    if (policy == 1) {
+      errors.push('❌ Single policy owner!')
+    }
+    if (policy <= 2 || policy / BigInt(owners.length) > 1.5) {
+      errors.push(`Low policy owner (${policy}/${owners.length})`)
+    }
+  }
+  return errors
+}
+
+const log = (name, chainId, msg) =>
+  console.log(`[${name} (${chainId})]: ${msg}`)
 
 async function main() {
   for (let chainId in networks) {
-    if (chainId !== '31337') {
-      let owners, policy
-      const errors = []
-      const { name, multisig } = networks[chainId]
+    let errors
+    if (chainId === 31337) return
+    const { multisig, name } = networks[chainId]
 
-      if (!multisig || chainId == '80001') {
-        errors.push('Missing multisig')
-      } else {
-        const provider = await getProvider(chainId)
-
-        // get Safe service
-        const safeService = new SafeApiKit({
-          chainId,
-          txServiceUrl: safeServiceURLs[chainId] || null,
-        })
-
-        try {
-          const { count } = await safeService.getPendingTransactions(multisig)
-          if (count) {
-            errors.push(`${count} pending txs are waiting to be signed`)
-          }
-          // the flags to get only un-executed transactions does not work
-          // filed here https://github.com/safe-global/safe-core-sdk/issues/690
-          // const allTxs = await safeService.getAllTransactions(multisig, {
-          //   executed: false,
-          //   trusted: false,
-          //   queued: false,
-          // })
-        } catch (error) {
-          errorLog(`Couldn't fetch pending txs: ${error.message}`)
-        }
-
-        if (chainId === 1) {
-          const safe = new ethers.Contract(multisig, oldMultisigABI, provider)
-          owners = await safe.getOwners()
-          policy = await safe.required()
-        } else {
-          const safe = new ethers.Contract(multisig, multisigABI, provider)
-          owners = await safe.getOwners()
-          policy = await safe.getThreshold()
-        }
-
-        if (policy == 1) {
-          errors.push('❌ Single policy owner!')
-        }
-        if (policy <= 2) {
-          errors.push(`Low policy owner (${policy})`)
-        }
-      }
-
-      if (errors.length) {
-        console.log(`# ${name} (${chainId}) - ${multisig}`)
-        if (policy && owners) {
-          console.log(`${policy}/${owners.length} owners`)
-        }
-        errors.forEach((error) => errorLog(error))
-        console.log(`----\n`)
-      }
-      // owners.forEach((o) => console.log(`- ${o}`))
+    try {
+      errors = await getMultigiInfo(chainId, multisig)
+    } catch (error) {
+      errors = [`Couldn't fetch multisig info: ${error.message}`]
     }
+    errors.forEach((error) => log(name, chainId, error))
   }
 }
 
