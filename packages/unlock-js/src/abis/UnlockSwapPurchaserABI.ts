d933aa9fce1108210168b79499aaa5f85cb906fe
Julien Genestoux <julien.genestoux@gmail.com>
2024-03-13 10:02:59 -0400

    cleanup

--- packages/unlock-js/src/abis/UnlockSwapPurchaserABI.ts
+++ packages/unlock-js/src/abis/UnlockSwapPurchaserABI.ts
@@ -1,3 +1,54 @@
+// TODO: import from package!
+// import { networks } from '@unlock-protocol/contracts'
+
 export const UnlockSwapPurchaserABI = [
-  `function swapAndCall(address lock, address srcToken, uint amountInMax,address uniswapRouter, bytes memory swapCalldata, bytes memory callData) public payable returns(bytes memory)`,
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'lock',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'srcToken',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'keyPrice',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amountInMax',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'uniswapRouter',
+        type: 'address',
+      },
+      {
+        internalType: 'bytes',
+        name: 'swapCalldata',
+        type: 'bytes',
+      },
+      {
+        internalType: 'bytes',
+        name: 'callData',
+        type: 'bytes',
+      },
+    ],
+    name: 'swapAndCall',
+    outputs: [
+      {
+        internalType: 'bytes',
+        name: '',
+        type: 'bytes',
+      },
+    ],
+    stateMutability: 'payable',
+    type: 'function',
+  },
 ]
