56c7e4bd127466c9e2d1661738905e3443f0a775
Pawan <pawanbajaj2012@gmail.com>
2024-03-14 20:50:21 +0530

    added password hook to astarzkevm

--- packages/networks/src/networks/astarzkevm.ts
+++ packages/networks/src/networks/astarzkevm.ts
@@ -1,8 +1,9 @@
-import { NetworkConfig } from '@unlock-protocol/types'
+import { NetworkConfig, HookType } from '@unlock-protocol/types'
 
 export const astarzkevm: NetworkConfig = {
   chain: 'Astar zkEVM',
-  description: '',
+  description:
+    'Astar zkEVM is an Ethereum Layer-2 scaling solution leveraging Polygon Chain Development Kit and cutting edge zero-knowledge cryptography to enable off-chain transaction execution, with finality and security guarantees provided by Ethereum.',
   explorer: {
     name: 'Astar zkEVM (Startale)',
     urls: {
@@ -16,6 +17,15 @@ export const astarzkevm: NetworkConfig = {
     },
   },
   featured: false,
+  hooks: {
+    onKeyPurchaseHook: [
+      {
+        address: '0xe270a288cb5a4633C26B79d8386afa195BcdC610',
+        id: HookType.PASSWORD,
+        name: 'Password required',
+      },
+    ],
+  },
   id: 3776,
   isTestNetwork: false,
   name: 'Astar zkEVM',
@@ -28,15 +38,14 @@ export const astarzkevm: NetworkConfig = {
   provider: 'https://rpc.startale.com/astar-zkevm',
   publicLockVersionToDeploy: 14,
   publicProvider: 'https://rpc.startale.com/astar-zkevm',
-  startBlock: 0,
+  startBlock: 69147,
   subgraph: {
     endpoint: '',
     networkName: '',
     studioName: '',
   },
 
-  unlockAddress: '',
-  //   url: 'https://polygon.technology/polygon-zkevm',
+  unlockAddress: '0xeBec66d1f006FEC42633B6fa30b0397de8a4D965',
 }
 
 export default astarzkevm
