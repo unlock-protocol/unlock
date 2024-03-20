6a299f713e9fef5dbbef45e310f4f6a00901020b
Clément Renaud <clement+git@clementrenaud.com>
2024-03-20 16:28:54 +0100

    actually add localhost to networks in integration tests

--- tests/bin/prepare.sh
+++ tests/bin/prepare.sh
@@ -6,6 +6,11 @@ unlock_adress=$(cat "$INFO_FILE_PATH" | jq -r '.localhost.Unlock.address')
 
 # create localhost file in networks package
 yarn workspace @unlock-protocol/networks create-localhost "$unlock_adress"
+
+# append to networks index
+echo "export * from './localhost'" >> src/networks/index.ts
+
+# rebuild networks
 yarn workspace @unlock-protocol/networks build
 
 # rebuild unlock-js to get latest networks package
