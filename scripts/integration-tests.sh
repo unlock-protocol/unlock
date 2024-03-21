ff6e5ed9534fa408b1d89993ac6f7ec3aeb47ec4
Clément Renaud <clement+git@clementrenaud.com>
2024-03-21 15:59:37 +0100

    use infra script in tests

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# First this script will deploy 
-REPO_ROOT=`dirname "$0"`/..
-
-# fetch compose config paths
-BASE_DOCKER_FOLDER=$REPO_ROOT/docker
-BASE_DOCKER_COMPOSE=$BASE_DOCKER_FOLDER/docker-compose.yml
-DOCKER_COMPOSE_FILE=$BASE_DOCKER_FOLDER/docker-compose.integration.yml
-COMPOSE_CONFIG="-f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE"
-
 # run Unlock Protocol stack
-sh -c "$REPO_ROOT/scripts/run-stack-dockerized.sh"
-
-# Setting the right env var
-export UNLOCK_ENV=test
+sh -c "$REPO_ROOT/scripts/start-infra.sh"
 
 # run the actual tests
 echo "Running integration tests \n"
