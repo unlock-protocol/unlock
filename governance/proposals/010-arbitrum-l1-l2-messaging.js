b9c34672c8e312e2de35bc67f7bb292113ab5e05
DannyThomx <dannithomx@gmail.com>
2024-03-26 13:02:35 +0200

    removed console logs, and .env file

--- governance/proposals/010-arbitrum-l1-l2-messaging.js
+++ governance/proposals/010-arbitrum-l1-l2-messaging.js
@@ -115,9 +115,11 @@ const INBOX_ABI = [
 /**
  * Set up: instantiate L1 / L2 wallets connected to providers
  */
-const walletPrivateKey = process.env.DEVNET_PRIVKEY
-const l1Provider = new ethers.JsonRpcProvider(process.env.L1RPC)
-const l2Provider = new ethers.JsonRpcProvider(process.env.L2RPC)
+const walletPrivateKey = 'Your_Private_key'
+const L1RPC = 'https://mainnet.infura.io/v3/<your_infura_api_key>'
+const L2RPC = 'https://arbitrum-mainnet.infura.io/v3/<your_infura_api_key>'
+const l1Provider = new ethers.JsonRpcProvider(L1RPC)
+const l2Provider = new ethers.JsonRpcProvider(L2RPC)
 // const l1Wallet = new ethers.Wallet(walletPrivateKey, l1Provider)
 const l2Wallet = new ethers.Wallet(walletPrivateKey, l2Provider)
 
@@ -130,7 +132,7 @@ module.exports = async () => {
   const inboxAddress = ethBridger.l2Network.ethBridge.inbox
   const ARBTokenAddressOnL2 = '0x912CE59144191C1204E64559FE8253a0e49E6548' // ARB TOKEN ADDRESS ON ARBITRUM ONE
   const grantsContractAddress = '0x00D5E0d31d37cc13C645D86410aB4cB7Cb428ccA'
-  const L2Alias = '0x28ffDfB0A6e6E06E95B3A1f928Dc4024240bD87c' // Timelock Alias Address on L2
+  const timelockL2Alias = '0x28ffDfB0A6e6E06E95B3A1f928Dc4024240bD87c' // Timelock Alias Address on L2
   const L1TimelockContract = '0x17EEDFb0a6E6e06E95B3A1F928dc4024240BC76B' // Timelock Address mainnet
 
   const L2TokenContract = new ethers.Contract(
@@ -139,9 +141,7 @@ module.exports = async () => {
     l2Wallet
   ).connect(l2Wallet)
 
-  const balanceOf = await L2TokenContract.balanceOf(L2Alias)
-  console.log('ARB BALANCE::', ethers.formatEther(await balanceOf))
-
+  const balanceOf = await L2TokenContract.balanceOf(timelockL2Alias)
   const tokenAmount = ethers.parseEther('1')
 
   // Create an instance of the Interface from the ABIs
@@ -176,27 +176,7 @@ module.exports = async () => {
     await getBaseFee(l1Provider),
     l1Provider
   )
-  console.log(':::::::::L1ToL2MessageGasParams::::::::::')
-  console.log(
-    'GasParams::::gasLimit',
-    L1ToL2MessageGasParams.gasLimit.toNumber()
-  )
-  console.log(
-    'GasParams::::maxSubmissionCost',
-    L1ToL2MessageGasParams.maxSubmissionCost.toNumber()
-  )
-  console.log(
-    'GasParams::::maxGas',
-    L1ToL2MessageGasParams.maxFeePerGas.toNumber()
-  )
-  console.log('GasParams::::deposit', L1ToL2MessageGasParams.deposit.toNumber())
-
-  console.log(
-    `Current retryable base submission price is: ${L1ToL2MessageGasParams.maxSubmissionCost.toString()}`
-  )
-
   const gasPriceBid = await l2Provider.getGasPrice()
-  console.log(`L2 gas price: ${gasPriceBid.toString()}`)
 
   const inbox_calldata = iface_inbox.encodeFunctionData(
     'createRetryableTicket',
@@ -213,29 +193,11 @@ module.exports = async () => {
   )
 
   const proposalName = `# Test Transaction before 7k ARB Transfer To Fund Unlock Protocol’s Ecosystem via Grants Stack  This proposal requests to use 1 ARB from the tokens given to Unlock Protocol DAO by ArbitrumDAO to run a test transaction to de-risk the transfer of 7k ARB tokens to fund the retroQF round on Grants Stack.`
-  console.log(proposalName)
   // Proposal ARGS i.e Call Governor.propose() directly with these values
-  // targets: Inbox contract
-  // values: depost * 10
-  // calldatas: inbox_calldata
-  // description
   const targets = [inboxAddress]
   const values = [L1ToL2MessageGasParams.deposit.toNumber() * 10] // I Multiply by 10 to add extra in case gas changes
   const calldatas = [inbox_calldata]
-  console.log(
-    '______________________________________________________________________\n'
-  )
-  console.log(
-    'PROPOSAL ARGS - Can Call Propose function on Governor with the following::'
-  )
-  console.log(
-    '______________________________________________________________________\n'
-  )
-
-  console.log('TARGETS:: ', targets)
-  console.log('VALUES:: ', values)
-  console.log('CALLDATAS:: ', calldatas)
-  console.log('DESCRIPTION:: ', proposalName)
+  const description = proposalName
 
   const calls = [
     {
