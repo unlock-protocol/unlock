dfa61c9aec8c23968b16e81b4de5a9bd62377c1e
Anton Dosta <a.dosta@icloud.com>
2024-03-28 16:37:08 -0700

    fix: Wrapped @cookbookdev/docsbot into <BrowserOnly>

--- docs/src/theme/SearchBar/index.js
+++ docs/src/theme/SearchBar/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import SearchBar from '@theme-original/SearchBar'
-import AskCookbook from '@cookbookdev/docsbot/react'
+import BrowserOnly from '@docusaurus/BrowserOnly'
 
 export default function SearchBarWrapper(props) {
   return (
     <>
       <SearchBar {...props} />
-      <AskCookbook
+      <BrowserOnly fallback={null}>
+        {() => {
+          const AskCookbook = require('@cookbookdev/docsbot/react')
+          return (
+            <AskCookbook
+              apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ5OTljM2Q0NWUzNzIxM2UxZDgzMGMiLCJpYXQiOjE3MDg3NTk0OTEsImV4cCI6MjAyNDMzNTQ5MX0.2DgWnOpRQgPWfQeIOH5XRP3v1v_XDU5RSy6pqUUwwVg"
+              noFastMode
+            />
+          )
+        }}
+      </BrowserOnly>
+      {/* <AskCookbook
         apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ5OTljM2Q0NWUzNzIxM2UxZDgzMGMiLCJpYXQiOjE3MDg3NTk0OTEsImV4cCI6MjAyNDMzNTQ5MX0.2DgWnOpRQgPWfQeIOH5XRP3v1v_XDU5RSy6pqUUwwVg"
         noFastMode
-      />
+      /> */}
     </>
   )
 }
