d04e6897dbf069b1b35aa3327f341d9bb3b0a7f4
Clément Renaud <clement@unlock-protocol.com>
2024-03-29 20:00:10 +0100

    feat(networks): add support for Base sepolia (#13529)
    * add basic network file
    
    * readme typo
    
    * add verification
    
    * remove unused param
    
    * unlock deploymeny
    
    * etherscan api key
    
    * update base scan api key again
    
    * correct unlock address
    
    * use actual proxy address
    
    * add subgraph
    
    * update readme
    
    * add multisig
    
    * add provider
    
    * Update packages/networks/src/networks/base-sepolia.ts
    
    * use unlock provider
    
    ---------
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- packages/hardhat-helpers/src/etherscan.js
+++ packages/hardhat-helpers/src/etherscan.js
@@ -16,8 +16,8 @@ const etherscan = {
     polygonMumbai: 'W9TVEYKW2CDTQ94T3A2V93IX6U3IHQN5Y3',
     avalanche: 'N4AF8AYN8PXY2MFPUT8PAFSZNVJX5Q814X',
     celo: '6KBKUFYV3NQR4Y1BQN3Q34S2U7NTZBBPQZ',
-    baseGoerli: 'YourApiKeyToken',
     base: 'F9E5R4E8HIJQZMRE9U9IZMP7NVZ2IAXNB8',
+    baseSepolia: 'F9E5R4E8HIJQZMRE9U9IZMP7NVZ2IAXNB8',
     linea: 'S66J314Q7PICPB4RP2G117KDFQRBEUYIFX',
     polygonZkEVM: '8H4ZB9SQBMQ7WA1TCIXFQVCHTVX8DXTY9Y',
     scroll: 'BZEXNPN6KKKJQ8VIMNXZDZNEX7QQZWZQ3P',
@@ -33,11 +33,11 @@ const etherscan = {
       },
     },
     {
-      network: 'baseGoerli',
-      chainId: 84531,
+      network: 'baseSepolia',
+      chainId: 84532,
       urls: {
-        apiURL: 'https://api-goerli.basescan.org/api',
-        browserURL: 'https://goerli.basescan.org/',
+        apiURL: 'https://api-sepolia.basescan.org/api',
+        browserURL: 'https://sepolia.basescan.org/',
       },
     },
     {
---
+++ packages/networks/src/networks/base-sepolia.ts
@@ -0,0 +1,75 @@
+import { NetworkConfig } from '@unlock-protocol/types'
+
+export const baseSepolia: NetworkConfig = {
+  blockScan: {
+    url: (address: string) => `https://blockscan.com/address/${address}`,
+  },
+  chain: 'base-sepolia',
+  description: 'A public testnet for Base.',
+  explorer: {
+    name: 'Base Sepolia Etherscan',
+    urls: {
+      address: (address: string) =>
+        `https://sepolia.basescan.org/address/${address}`,
+      base: `https://sepolia.basescan.org/`,
+      token: (address: string, holder: string) =>
+        `https://sepolia.basescan.org/token/${address}?a=${holder}`,
+      transaction: (hash: string) => `https://sepolia.basescan.org/tx/${hash}`,
+    },
+  },
+  featured: false,
+  fullySubsidizedGas: true,
+  hooks: {},
+  id: 84532,
+  isTestNetwork: true,
+  keyManagerAddress: '',
+  maxFreeClaimCost: 1000,
+  multisig: '0x68F2c5D9009dc4d553f814D689102a53B2b349Cc',
+  name: 'Base Sepolia',
+  nativeCurrency: {
+    coingecko: 'ethereum',
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+
+  previousDeploys: [],
+
+  provider: 'https://rpc.unlock-protocol.com/84532',
+
+  publicLockVersionToDeploy: 14,
+
+  publicProvider: 'https://sepolia.base.org',
+
+  startBlock: 7889118,
+  subgraph: {
+    endpoint:
+      'https://api.studio.thegraph.com/query/65299/unlock-protocol-base-sepolia/version/latest',
+    networkName: 'base-sepolia',
+    studioName: 'unlock-protocol-base-sepolia',
+  },
+  swapPurchaser: '',
+  tokens: [
+    {
+      address: '0x4200000000000000000000000000000000000006',
+      decimals: 18,
+      featured: true,
+      mainnetAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      name: 'Wrapped Ether',
+      symbol: 'WETH',
+    },
+    {
+      address: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
+      decimals: 6,
+      featured: true,
+      name: 'USDC',
+      symbol: 'USDC',
+    },
+  ],
+  unlockAddress: '0x259813B665C8f6074391028ef782e27B65840d89',
+  unlockDaoToken: {
+    address: '',
+  },
+}
+
+export default baseSepolia
