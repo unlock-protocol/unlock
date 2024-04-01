bd70e23b549debb5c03a5bdfc1ebcd2156e922aa
Anton Dosta <a.dosta@icloud.com>
2024-04-01 11:03:30 -0700

    feat: bumped @cookbookdev/docsbot to version 4.5.0 and modified it to utilize @cookbookdev/docsbot/react-fixed, as @cookbookdev/docsbot/react now dynamically loads the latest version of the package.

--- docs/src/theme/SearchBar/index.js
+++ docs/src/theme/SearchBar/index.js
@@ -8,7 +8,7 @@ export default function SearchBarWrapper(props) {
       <SearchBar {...props} />
       <BrowserOnly fallback={null}>
         {() => {
-          const AskCookbook = require('@cookbookdev/docsbot/react')
+          const AskCookbook = require('@cookbookdev/docsbot/react-fixed')
           return (
             <AskCookbook
               apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ5OTljM2Q0NWUzNzIxM2UxZDgzMGMiLCJpYXQiOjE3MDg3NTk0OTEsImV4cCI6MjAyNDMzNTQ5MX0.2DgWnOpRQgPWfQeIOH5XRP3v1v_XDU5RSy6pqUUwwVg"
