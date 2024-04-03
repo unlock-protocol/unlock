4d42172c70a62a3555ce478226639a8cc983c229
Clément Renaud <clement.renaud@gmail.com>
2024-04-03 15:05:13 +0200

    Update governance/proposals/constants.js

--- governance/proposals/constants.js
+++ governance/proposals/constants.js
@@ -5,9 +5,10 @@ const L2_RPC = arbitrum.provider // Arbitrum RPC
 const GRANTS_CONTRACT_ADDRESS = '0x00D5E0d31d37cc13C645D86410aB4cB7Cb428ccA' // Grants contract on Arbitrum
 const TIMELOCK_L2_ALIAS = '0x28ffDfB0A6e6E06E95B3A1f928Dc4024240bD87c' // Timelock Alias Address on L2
 const L1_TIMELOCK_CONTRACT = '0x17EEDFb0a6E6e06E95B3A1F928dc4024240BC76B' // Timelock Address mainnet
-const ARB_TOKEN_ADRESS_ON_L2 = arbitrum.tokens.filter(
-  (token) => token.symbol === 'ARB'
-)[0].address // ARB TOKEN ADDRESS ON ARBITRUM ONE
+// ARB TOKEN ADDRESS ON ARBITRUM ONE
+const { address : ARB_TOKEN_ADRESS_ON_L2} = arbitrum.tokens.find(
+  ({ symbol }) =>  symbol === 'ARB'
+)
 
 module.exports = {
   L1_RPC,
