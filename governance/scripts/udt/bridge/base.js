ceede7400394d80cab03ddf336fc18fb66634f6c
Clément Renaud <clement+git@clementrenaud.com>
2024-04-04 13:00:56 +0200

    add gas estimate

--- governance/scripts/udt/bridge/base.js
+++ governance/scripts/udt/bridge/base.js
@@ -7,19 +7,25 @@ const l1BridgeAbi = require('./abi/l1standardbridge.json')
 const ethers = require('ethers5')
 const abiERC20 = require('@unlock-protocol/hardhat-helpers/dist/ABIs/erc20.json')
 
-const L1_CHAIN_ID = 11155111 // mainnet (Sepolia 11155111)
-const l2_CHAIN_ID = 84532 // BASE (BASE Sepolia 84532)
-const standardBridges = {
-  11155111: '0x3154Cf16ccdb4C6d922629664174b904d80F2C35',
-  84532: '0xfd0Bf71F60660E2f608ed56e1659C450eB113120',
-}
+// tweak these params
+const USE_TESTNET = false
+const SIMULATION = true
+const gasLimit = 1000000
+const defaultGasAmount = '200000'
 
-const defaultGasAmount = '500000'
+// parse network details
+const L1_CHAIN_ID = USE_TESTNET ? 11155111 : 1 // mainnet (Sepolia 11155111)
+const l2_CHAIN_ID = USE_TESTNET ? 84532 : 8453 // BASE (BASE Sepolia 84532)
+const standardBridges = {
+  1: '0x3154Cf16ccdb4C6d922629664174b904d80F2C35',
+  11155111: '0xfd0Bf71F60660E2f608ed56e1659C450eB113120',
+}
+const L1_STANDARD_BRIDGE = standardBridges[L1_CHAIN_ID]
 
 async function main({
   l1ChainId = L1_CHAIN_ID,
   l2ChainId = l2_CHAIN_ID,
-  l1StandardBridge = standardBridges[l2_CHAIN_ID],
+  l1StandardBridge = L1_STANDARD_BRIDGE,
   amount = `100000000000000000`, // default to 0.1
 } = {}) {
   const { DEPLOYER_PRIVATE_KEY } = process.env
@@ -66,7 +72,7 @@ async function main({
     const { hash } = await l1Token.approve(l1StandardBridge, amount)
     console.log(`approved (tx: ${hash})`)
   } else {
-    console.log('token is approved to deposit')
+    console.log('token approval ok for deposit')
   }
 
   const bridgeArgs = {
@@ -77,16 +83,33 @@ async function main({
     emptyData: '0x',
   }
 
+  // show gas estimate
+  const gasEstimate = await bridgeContract.estimateGas.bridgeERC20(
+    ...Object.values(bridgeArgs),
+    {
+      gasLimit,
+    }
+  )
+  const { baseFeePerGas } = await l1Provider.getBlock('latest')
+  const gasCostEstimate = baseFeePerGas.mul(gasEstimate)
+  console.log(
+    `Gas cost estimate: ${ethers.utils.formatEther(gasCostEstimate)} ETH`
+  )
   // deposit to bridge
-  try {
-    const bridgeResult = await bridgeContract.bridgeERC20(
-      ...Object.values(bridgeArgs)
-    )
-    console.log('bridge deposit ok', bridgeResult)
-    const { transactionHash } = await bridgeResult.wait()
-    console.log(`(tx: ${transactionHash})`)
-  } catch (e) {
-    console.log('bridge token result error', e)
+  if (!SIMULATION) {
+    try {
+      const bridgeResult = await bridgeContract.bridgeERC20(
+        ...Object.values(bridgeArgs),
+        {
+          gasLimit,
+        }
+      )
+      console.log('bridge deposit done', bridgeResult)
+      const { transactionHash } = await bridgeResult.wait()
+      console.log(`(tx: ${transactionHash})`)
+    } catch (e) {
+      console.log('bridge token result error', e)
+    }
   }
 }
 
