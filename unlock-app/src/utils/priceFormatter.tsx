8d535ea6b92d1ae8ec33b2814dc4cdc5e6c89c1a
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-04-04 12:41:17 +0300

    Removed trailing zeroes

--- unlock-app/src/utils/priceFormatter.tsx
+++ unlock-app/src/utils/priceFormatter.tsx
@@ -10,6 +10,16 @@ function PriceFormatter({ price, precision = 4 }: PriceFormatterProps) {
 
   const decimalIndex = price.indexOf('.')
 
+  const removeTrailingZeroes = (number: string): string => {
+    for (let i = number.length - 1; i >= 0; i--) {
+      if (number.charAt(i) !== '0' && number.charAt(i) !== '.') {
+        return number.substring(0, i + 1)
+      }
+    }
+
+    return number
+  }
+
   // If not decimal - return
   if (decimalIndex === -1 || decimalIndex === price.length - 1) {
     return price
@@ -32,11 +42,15 @@ function PriceFormatter({ price, precision = 4 }: PriceFormatterProps) {
       <>
         {`${price.substring(0, decimalIndex)}.0`}
         <span className="lower text-xs">{numZeroes - 1}</span>
-        {price.substring(decimalIndex + 1 + numZeroes).substring(0, 4)}
+        {removeTrailingZeroes(
+          price.substring(decimalIndex + 1 + numZeroes).substring(0, 4)
+        )}
       </>
     )
   } else {
-    return price.substring(0, decimalIndex + 1 + numZeroes + precision)
+    return removeTrailingZeroes(
+      price.substring(0, decimalIndex + 1 + numZeroes + precision)
+    )
   }
 }
 
