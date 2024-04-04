61cd00a0f7100d95c6b296ea1efdd172e8e34bba
Clément Renaud <clement+git@clementrenaud.com>
2024-04-04 14:42:54 +0200

    add check to make sure tokens are set in oracle

--- packages/networks/bin/check-tokens.ts
+++ packages/networks/bin/check-tokens.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers'
+import { Unlock } from '@unlock-protocol/contracts'
 import networks from '../src'
 import ERC20 from '../utils/erc20.abi.json'
 
@@ -6,6 +7,12 @@ const run = async () => {
   for (const networkId in networks) {
     const network = networks[networkId]
     const provider = new ethers.JsonRpcProvider(network.provider)
+    const unlock = new ethers.Contract(
+      network.unlockAddress,
+      Unlock.abi,
+      provider
+    )
+
     if (network.tokens) {
       for (const token of network.tokens) {
         const contract = new ethers.Contract(token.address, ERC20, provider)
@@ -13,7 +20,6 @@ const run = async () => {
           const symbol = await contract.symbol()
           const name = await contract.name()
           const decimals = parseInt(await contract.decimals())
-
           if (decimals !== token.decimals) {
             console.error(
               `❌ Decimals mismatch for ${token.address} on ${networkId}. It needs to be "${decimals}"`
@@ -27,6 +33,14 @@ const run = async () => {
           if (symbol !== token.symbol) {
             console.error(
               `❌ Symbol mismatch for ${token.address} on ${networkId}. It needs to be "${symbol}"`
+            )
+          }
+
+          // check if oracle is set in Unlock
+          const isSetInUnlock = await unlock.uniswapOracles(token.address)
+          if (isSetInUnlock === ethers.ZeroAddress) {
+            console.error(
+              `❌ Token ${name} (${symbol}) at ${token.address} on ${networkId} is not set correctly as Oracle"`
             )
           }
         } catch (error) {
