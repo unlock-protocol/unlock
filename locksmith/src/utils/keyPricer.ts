55fe72e623c60f0d759247ad70067a2519f21717
Julien Genestoux <julien.genestoux@gmail.com>
2024-04-05 21:49:25 -0400

    using defilama for canAffordGrant

--- locksmith/src/utils/keyPricer.ts
+++ locksmith/src/utils/keyPricer.ts
@@ -4,6 +4,7 @@ import networks from '@unlock-protocol/networks'
 import PriceConversion from './priceConversion'
 import { GAS_COST_TO_GRANT } from './constants'
 import { getProviderForNetwork, getPurchaser } from '../fulfillment/dispatcher'
+import GasPrice from './gasPrice'
 
 // @deprecated - Remove once no longer used anywhere. Use functions in pricing.ts instead.
 export default class KeyPricer {
@@ -22,28 +23,22 @@ export default class KeyPricer {
     if (networks[network].fullySubsidizedGas) {
       return { canAfford: true }
     }
-    const [provider, wallet] = await Promise.all([
-      getProviderForNetwork(network),
-      getPurchaser({ network }),
+    const [gasCost, balance] = await Promise.all([
+      new GasPrice().gasPriceUSD(network, GAS_COST_TO_GRANT),
+      (await getProviderForNetwork(network)).getBalance(
+        await (await getPurchaser({ network })).getAddress()
+      ),
     ])
-    const [gasPrice, balance] = await Promise.all([
-      provider.getGasPrice(),
-      provider.getBalance(await wallet.getAddress()),
-    ])
-    const gasCost = gasPrice.mul(GAS_COST_TO_GRANT)
     // Balance is too low to afford the gas cost
     if (balance.lt(gasCost)) {
       return { canAfford: false, reason: 'Insufficient purchaser balance' }
     }
-    // And now check the value in USD
-    const symbol = networks[network].nativeCurrency.symbol
-    const priceConversion = new PriceConversion()
-    const usdPrice = await priceConversion.convertToUSD(
-      symbol,
-      parseFloat(ethers.utils.formatEther(gasCost).toString())
-    )
-    if (usdPrice > networks[network].maxFreeClaimCost!) {
-      return { canAfford: false, reason: `Gas costs too high: $${usdPrice}` }
+
+    if (gasCost > networks[network].maxFreeClaimCost!) {
+      return {
+        canAfford: false,
+        reason: `Gas costs too high: $${gasCost / 100}`,
+      }
     }
     return { canAfford: true }
   }
