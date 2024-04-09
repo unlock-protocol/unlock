2e0a78fdc7d26ede822b0c66d2bf1667506871ad
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-04-09 19:38:01 +0300

    Fixed storybook v7 (#13614)
    Fixed storybook

--- packages/crypto-icon/.storybook/main.ts
+++ packages/crypto-icon/.storybook/main.ts
@@ -3,18 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 export default {
   stories: ['../lib/**/*.stories.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react-vite',
   async viteFinal(config) {
     // Merge custom configuration into the default config
--- packages/ui/lib/components/Tooltip/Tooltip.tsx
+++ packages/ui/lib/components/Tooltip/Tooltip.tsx
@@ -5,6 +5,7 @@ import {
   Portal,
   TooltipContentProps,
   Root,
+  Provider,
 } from '@radix-ui/react-tooltip'
 import { ReactNode } from 'react'
 
@@ -29,20 +30,22 @@ export function Tooltip({
     theme === 'clear' ? 'text-brand-dark bg-white' : 'text-white bg-brand-dark'
   const arrowColor = theme === 'clear' ? 'fill-white' : 'fill-brand-dark'
   return (
-    <Root>
-      <TooltipTrigger className="cursor-pointer" aria-label={label} asChild>
-        {children}
-      </TooltipTrigger>
-      <Portal>
-        <Content
-          side={side}
-          sideOffset={sideOffset}
-          className={`z-50 rounded text-sm ${contentColors} [box-shadow:0px_8px_30px_rgba(0,0,0,0.08)] px-4 py-0.5`}
-        >
-          <TooltipArrow className={`${arrowColor}`} />
-          {tip}
-        </Content>
-      </Portal>
-    </Root>
+    <Provider>
+      <Root>
+        <TooltipTrigger className="cursor-pointer" aria-label={label} asChild>
+          {children}
+        </TooltipTrigger>
+        <Portal>
+          <Content
+            side={side}
+            sideOffset={sideOffset}
+            className={`z-50 rounded text-sm ${contentColors} [box-shadow:0px_8px_30px_rgba(0,0,0,0.08)] px-4 py-0.5`}
+          >
+            <TooltipArrow className={`${arrowColor}`} />
+            {tip}
+          </Content>
+        </Portal>
+      </Root>
+    </Provider>
   )
 }
