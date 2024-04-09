e306703dde4e6334641798a57d44311387360dff
Julien Genestoux <julien.genestoux@gmail.com>
2024-04-09 13:57:05 -0400

    feat(networks): redeployed UDT on OP, Base, Base Sepolia to match mainnet name and symbol

--- packages/networks/src/networks/base-sepolia.ts
+++ packages/networks/src/networks/base-sepolia.ts
@@ -68,7 +68,7 @@ export const baseSepolia: NetworkConfig = {
   ],
   unlockAddress: '0x259813B665C8f6074391028ef782e27B65840d89',
   unlockDaoToken: {
-    address: '0x345F99F6873866f8b73aD4a500396c4635E0E755',
+    address: '0x68a8011d72E6D41bf7CE9dC49De0aeaEBAAC9b39',
   },
 }
 
--- packages/networks/src/networks/optimism.ts
+++ packages/networks/src/networks/optimism.ts
@@ -153,7 +153,7 @@ export const optimism: NetworkConfig = {
   },
   unlockAddress: '0x99b1348a9129ac49c6de7F11245773dE2f51fB0c',
   unlockDaoToken: {
-    address: '0x35CCe76D854c6E90F665C2B49899E158da9D517E',
+    address: '0xc709c9116dBf29Da9c25041b13a07A0e68aC5d2D',
   },
   url: 'https://www.optimism.io/',
 }
