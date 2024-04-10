d9a6345727064d443a4b890f26b142ae84d7d624
Tyler Sehr <tysehr37@gmail.com>
2024-04-10 06:44:25 -0700

    feat(docs): adding ask cookbook (#13526)
    * trigger lint
    
    * feat: Ask Cookbook
    
    * feat(docsbot): updated @cookbookdev/docsbot to v4.4.1
    
    * fix: Wrapped @cookbookdev/docsbot into <BrowserOnly>
    
    * refactor: removal of commented out lines
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>
    
    * feat: bumped @cookbookdev/docsbot to version 4.5.0 and modified it to utilize @cookbookdev/docsbot/react-fixed, as @cookbookdev/docsbot/react now dynamically loads the latest version of the package.
    
    * add api key comment
    
    ---------
    
    Co-authored-by: Anton Dosta <a.dosta@icloud.com>
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

---
+++ docs/src/theme/SearchBar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import SearchBar from '@theme-original/SearchBar'
+import BrowserOnly from '@docusaurus/BrowserOnly'
+
+export default function SearchBarWrapper(props) {
+  return (
+    <>
+      <SearchBar {...props} />
+      <BrowserOnly fallback={null}>
+        {() => {
+          const AskCookbook = require('@cookbookdev/docsbot/react-fixed')
+          return (
+            <AskCookbook
+              apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ5OTljM2Q0NWUzNzIxM2UxZDgzMGMiLCJpYXQiOjE3MDg3NTk0OTEsImV4cCI6MjAyNDMzNTQ5MX0.2DgWnOpRQgPWfQeIOH5XRP3v1v_XDU5RSy6pqUUwwVg"
+              noFastMode
+            />
+          )
+        }}
+      </BrowserOnly>
+    </>
+  )
+}
+
+// Ask Cookbook API key obtainable at https://cms.cookbook.dev with an invite from an admin of the project.
