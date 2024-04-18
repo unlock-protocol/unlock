b26014e1b98160a9dc0d994192d27d1a6d66cae8
Julien Genestoux <julien.genestoux@gmail.com>
2024-04-18 11:17:02 -0400

    Update unlock-app/src/components/content/event/Registration/AttendeeCues.tsx
    Co-authored-by: Viacheslav <surzhenkoviacheslav@gmail.com>

--- unlock-app/src/components/content/event/Registration/AttendeeCues.tsx
+++ unlock-app/src/components/content/event/Registration/AttendeeCues.tsx
@@ -52,7 +52,7 @@ export const AttendeeCuesInternal = ({
               )
             })}
         </ul>
-        <p className="">
+        <p>
           {Object.values(socials)
             .filter((social) => social.profileDisplayName)
             .slice(0, 5)
