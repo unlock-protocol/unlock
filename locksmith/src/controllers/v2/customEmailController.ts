f63441fb273b6e879ba23d7dad20ad22e557e11c
Julien Genestoux <julien.genestoux@gmail.com>
2024-04-23 11:02:04 -0400

    feat(locksmith): Event invites API (#13664)
    * adding route
    
    * wip
    
    * removed undue changes
    
    * removed extra files
    
    * removed undue changes
    
    * updated openapi
    
    * fixing tests

--- locksmith/src/controllers/v2/customEmailController.ts
+++ locksmith/src/controllers/v2/customEmailController.ts
@@ -5,6 +5,9 @@ import logger from '../../logger'
 import { CustomEmailContent } from '../../models/customEmailContent'
 import * as emailOperations from '../../operations/emailOperations'
 import { addJob } from '../../worker/worker'
+import { getEventBySlug } from '../../operations/eventOperations'
+import { sendEmail } from '../../operations/wedlocksOperations'
+import { getEventUrl } from '../../utils/eventHelpers'
 
 const CustomEmail = z.object({
   content: z
@@ -15,60 +18,64 @@ const CustomEmail = z.object({
     .default(''),
 })
 
-export class CustomEmailController {
-  async saveCustomContent(request: Request, response: Response) {
-    try {
-      const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
-      const network = Number(request.params.network)
-      const template = request.params.template?.toUpperCase()
+export const saveCustomContent = async (
+  request: Request,
+  response: Response
+) => {
+  try {
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+    const network = Number(request.params.network)
+    const template = request.params.template?.toUpperCase()
 
-      const { content } = await CustomEmail.parseAsync(request.body)
+    const { content } = await CustomEmail.parseAsync(request.body)
 
-      const [customEmail] = await CustomEmailContent.upsert(
-        {
-          lockAddress,
-          network,
-          content,
-          template,
-        },
-        {
-          returning: true,
-          conflictFields: ['lockAddress', 'network', 'template'],
-        }
-      )
-      return response.status(200).send(customEmail)
-    } catch (err: any) {
-      logger.error(err.message)
-      return response.status(500).send({
-        message: 'Could not save custom email content.',
-      })
-    }
-  }
-
-  async getCustomContent(request: Request, response: Response) {
-    try {
-      const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
-      const network = Number(request.params.network)
-      const template = request.params.template?.toUpperCase()
-
-      const customEmail = await emailOperations.getCustomTemplateContent({
+    const [customEmail] = await CustomEmailContent.upsert(
+      {
         lockAddress,
         network,
+        content,
         template,
-      })
-
-      if (customEmail) {
-        return response.status(200).send(customEmail)
+      },
+      {
+        returning: true,
+        conflictFields: ['lockAddress', 'network', 'template'],
       }
-      return response.status(404).json({
-        message: 'Custom email content not found for this template.',
-      })
-    } catch (err: any) {
-      logger.error(err.message)
-      return response.status(500).send({
-        message: 'Could not get custom email content.',
-      })
+    )
+    return response.status(200).send(customEmail)
+  } catch (err: any) {
+    logger.error(err.message)
+    return response.status(500).send({
+      message: 'Could not save custom email content.',
+    })
+  }
+}
+
+export const getCustomContent = async (
+  request: Request,
+  response: Response
+) => {
+  try {
+    const lockAddress = Normalizer.ethereumAddress(request.params.lockAddress)
+    const network = Number(request.params.network)
+    const template = request.params.template?.toUpperCase()
+
+    const customEmail = await emailOperations.getCustomTemplateContent({
+      lockAddress,
+      network,
+      template,
+    })
+
+    if (customEmail) {
+      return response.status(200).send(customEmail)
     }
+    return response.status(404).json({
+      message: 'Custom email content not found for this template.',
+    })
+  } catch (err: any) {
+    logger.error(err.message)
+    return response.status(500).send({
+      message: 'Could not get custom email content.',
+    })
   }
 }
 
@@ -104,4 +111,34 @@ export const sendCustomEmail: RequestHandler = async (request, response) => {
   return response.status(200).send({
     success: true,
   })
+}
+
+export const EventInviteBody = z.object({
+  recipients: z.array(z.string().email()).max(10),
+})
+
+export const sendEventInvite: RequestHandler = async (request, response) => {
+  const slug = request.params.slug.toLowerCase().trim()
+  const event = await getEventBySlug(slug)
+
+  const { recipients } = await EventInviteBody.parseAsync(request.body)
+  const results = await Promise.all(
+    recipients.map(
+      async (recipient) =>
+        await sendEmail({
+          template: 'inviteEvent',
+          recipient,
+          // @ts-expect-error object incomplete
+          params: {
+            eventName: event!.name,
+            eventDate: event!.data.ticket.event_start_date,
+            eventTime: event!.data.ticket.event_start_time,
+            eventUrl: getEventUrl(event!),
+          },
+          attachments: [],
+        })
+    )
+  )
+
+  return response.send(results).status(200)
 }
