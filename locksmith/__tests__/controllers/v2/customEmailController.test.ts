18bed6f4ea89a344c9e9ff53ee4f52818b0b67a8
Julien Genestoux <julien.genestoux@gmail.com>
2024-04-25 16:49:52 -0400

    undue change

--- locksmith/__tests__/controllers/v2/customEmailController.test.ts
+++ locksmith/__tests__/controllers/v2/customEmailController.test.ts
@@ -6,7 +6,6 @@ import { saveEvent } from '../../../src/operations/eventOperations'
 import { ethers } from 'ethers'
 import { sendEmail } from '../../../src/operations/wedlocksOperations'
 import { EventData } from '../../../src/models'
-import events from '../fixtures/events'
 
 let lockAddress = '0x62CcB13A72E6F991dE53b9B7AC42885151588Cd2'
 const anotherLockAddress = '0x62CcB13A72E6F991dE53b9B7AC42885151588Cd3'
@@ -138,15 +137,44 @@ describe('Email Controller v2', () => {
     })
 
     it('should return 401 if the user is not an event organizer', async () => {
-      const eventParams = { ...events[0] }
-      eventParams.checkoutConfig.config = {
-        locks: {
-          [anotherLockAddress]: {
-            network,
+      const eventParams = {
+        data: {
+          name: 'An Event',
+          image: 'https://host.tld/image.jpg',
+          attributes: [
+            { trait_type: 'event_start_date', value: '2024-05-22' },
+            { trait_type: 'event_start_time', value: '08:30' },
+            { trait_type: 'event_end_date', value: '2024-05-22' },
+            { trait_type: 'event_end_time', value: '14:00' },
+            { trait_type: 'event_timezone', value: 'America/New_York' },
+            {
+              trait_type: 'event_address',
+              value: '29 Little W 12th St, New York, NY 10014, USA',
+            },
+          ],
+          description: 'An Event',
+          ticket: {
+            event_start_date: '2024-05-22',
+            event_start_time: '08:30',
+            event_end_date: '2024-05-22',
+            event_end_time: '14:00',
+            event_timezone: 'America/New_York',
+            event_address: '29 Little W 12th St, New York, NY 10014, USA',
+          },
+          requiresApproval: false,
+          emailSender: 'Julien Genestoux',
+          replyTo: 'julien@unlock-protocol.com',
+        },
+        checkoutConfig: {
+          config: {
+            locks: {
+              [anotherLockAddress]: {
+                network,
+              },
+            },
           },
         },
       }
-
       const [{ slug }] = await saveEvent(
         eventParams,
         ethers.Wallet.createRandom().address // Use a random address for the organizer
