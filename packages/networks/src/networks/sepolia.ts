89240281f0293e89fb5a40c3f5daf838ff543cfe
Clément Renaud <clement+git@clementrenaud.com>
2024-04-26 15:28:41 +0200

    correct address

--- packages/networks/src/networks/sepolia.ts
+++ packages/networks/src/networks/sepolia.ts
@@ -77,7 +77,7 @@ export const sepolia: NetworkConfig = {
     decimals: 18,
     name: 'ETH',
     symbol: 'ETH',
-    wrapped: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    wrapped: '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9',
   },
 
   opensea: {
