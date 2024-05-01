8dfd960621dff45cb839908a83e9b38ced9b139a
Julien Genestoux <julien.genestoux@gmail.com>
2024-05-01 12:56:25 -0400

    adding a runner infra + a script to email QR codes to Farcon attendees

---
+++ locksmith/src/scripts/sample.ts
@@ -0,0 +1,9 @@
+import { logger } from '../logger'
+
+const run = async () => {
+  logger.info('Runner started.')
+  // Do stuff!
+  logger.info('Runner done!')
+}
+
+run()
---
+++ locksmith/src/scripts/send-qr-farcon.ts
@@ -0,0 +1,37 @@
+import { logger } from '../logger'
+import * as keysOperations from '../operations/keysOperations'
+import { notifyNewKeyToWedlocks } from '../operations/wedlocksOperations'
+
+logger.info('Runner started.')
+
+const network = 8453
+const lockAddress = '0x456CC03543d41Eb1c9a7cA9FA86e9383B404f50d'
+
+const run = async () => {
+  const { keys, total } = await keysOperations.getKeysWithMetadata({
+    network,
+    lockAddress,
+    filters: {},
+    loggedInUserAddress: '0xF5C28ce24Acf47849988f147d5C75787c0103534',
+  })
+  console.log(keys.length, total)
+  for (let i = 251; i < keys.length; i++) {
+    const key = keys[i]
+    const sent = await notifyNewKeyToWedlocks(
+      {
+        tokenId: key.token,
+        lock: {
+          address: key.lockAddress,
+          name: key.lockName,
+        },
+        manager: key.keyManager,
+        owner: key.keyholderAddress,
+      },
+      network
+    )
+    console.log(i, sent)
+  }
+  logger.info('Runner done!')
+}
+
+run()
