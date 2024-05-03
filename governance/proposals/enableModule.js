0d237f680c55ea9aff20ee0567593553e4f9659a
Clément Renaud <clement+git@clementrenaud.com>
2024-05-03 14:29:50 +0200

    simplify module addresses parsing

--- governance/proposals/enableModule.js
+++ governance/proposals/enableModule.js
@@ -1,5 +1,8 @@
 /**
+ * This script is used to setup a fresh install of SAFE modules used
+ * for cross chain governance.
  *
+ * For deployments, see `./scripts/deploy_multisig_mods`
  */
 
 const { networks } = require('@unlock-protocol/networks')
@@ -752,38 +755,40 @@ const connextModAbi = [
 async function main() {
   const { chainId } = await ethers.provider.getNetwork()
   console.log(`Sending on ${chainId}`)
-  const { bridge, multisig } = networks[chainId]
-  if (
-    !bridge ||
-    !bridge.modules ||
-    !bridge.modules.delayMod ||
-    !bridge.modules.connextMod
-  ) {
+  const {
+    governanceBridge: {
+      modules: { delayMod: delayModAddress, connextMod: connextModAddress },
+    },
+    multisig,
+  } = networks[chainId]
+
+  console.log({ delayModAddress, connextModAddress })
+
+  if (!delayModAddress || !connextModAddress) {
     throw Error('Missing bridge SAFE modules.')
   }
 
-  console.log(bridge.modules)
   // parse contracts
-  const delayMod = await ethers.getContractAt(
-    delayModAbi,
-    bridge.modules.delayMod
-  )
+  const delayMod = await ethers.getContractAt(delayModAbi, delayModAddress)
   const connextMod = await ethers.getContractAt(
     connextModAbi,
-    bridge.modules.connextMod
+    connextModAddress
   )
 
   // make sure config is correct
   if (
-    (await delayMod.target()) != multisig ||
-    (await connextMod.target()) != delayMod.address
+    (await delayMod.getFunction('target')()) != multisig ||
+    (await connextMod.getFunction('target')()) != delayModAddress
   ) {
-    throw Error('Config error in SAFE modules.')
+    throw Error(`Config error in SAFE modules. Should be:
+    - delayMod target : ${multisig}
+    - connextMod target ${delayModAddress}
+    `)
   }
 
   // enable Connext as a sub-module of Delay
-  if (!(await delayMod.isModuleEnabled(connextMod.address))) {
-    const tx = await delayMod.enableModule(connextMod.address)
+  if (!(await delayMod.isModuleEnabled(connextModAddress))) {
+    const tx = await delayMod.enableModule(connextModAddress)
     console.log(tx)
   }
 
@@ -792,7 +797,6 @@ async function main() {
     console.log('Transferring ownership of Delay module to SAFE.')
     await delayMod.transferOwnership(multisig)
   }
-
   if ((await connextMod.owner()) != multisig) {
     console.log('Transferring ownership of Connext module to SAFE.')
     await connextMod.transferOwnership(multisig)
@@ -801,11 +805,10 @@ async function main() {
   // parse call to enable the Delay module on multisig
   // NB: we use the delayMod `enableModule` ABI as it is identical to the one in SAFE
   const functionName = 'enableModule'
-  const functionArgs = [delayMod.address]
-  const calldata = delayMod.interface.encodeFunctionData(
-    functionName,
-    functionArgs
-  )
+  const functionArgs = [delayModAddress]
+  const calldata = delayMod.interface.encodeFunctionData('enableModule', [
+    delayModAddress,
+  ])
 
   const calls = [
     {
