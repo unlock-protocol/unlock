7a580a43bfd887239ed105e435f14a11e2793825
Clément Renaud <clement+git@clementrenaud.com>
2024-05-03 14:56:17 +0200

    move ABIs to their own files

--- governance/helpers/bridge/index.js
+++ governance/helpers/bridge/index.js
@@ -2,10 +2,14 @@ const delayMod = require('./delayMod')
 const xCall = require('./xCall')
 const IConnext = require('./abis/IConnext')
 const IXCalled = require('./abis/IXCalled')
+const ConnextMod = require('./abis/ConnextMod')
+const DelayMod = require('./abis/DelayMod')
 
 module.exports = {
   ...delayMod,
   ...xCall,
   IConnext,
   IXCalled,
+  DelayMod,
+  ConnextMod,
 }
