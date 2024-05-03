e247c01a747d8e356eba04e3f15dd5001f87a42f
Julien Genestoux <julien.genestoux@gmail.com>
2024-05-03 14:34:39 -0700

    Revert "removed extra code"
    This reverts commit a3aa9a299db7238a14eb1d99665d88f41ec3d064.

--- locksmith/__tests__/operations/eventOperations.test.ts
+++ locksmith/__tests__/operations/eventOperations.test.ts
@@ -8,6 +8,13 @@ import {
   getEventBySlug,
   saveEvent,
 } from '../../src/operations/eventOperations'
+import { sendEmail } from '../../src/operations/wedlocksOperations'
+
+vi.mock('../../src/operations/wedlocksOperations', () => {
+  return {
+    sendEmail: vi.fn().mockResolvedValue(true),
+  }
+})
 
 describe('eventOperations', () => {
   beforeEach(async () => {
