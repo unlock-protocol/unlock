896b48feece06dfb676e88c4b7f965b12360216e
Clément Renaud <clement+git@clementrenaud.com>
2024-05-12 17:29:30 +0200

    migrate lock data

--- smart-contracts/test/Unlock/migrateLockData/v10.js
+++ smart-contracts/test/Unlock/migrateLockData/v10.js
@@ -33,7 +33,7 @@ const purchaseFails = async (lock) => {
       someBuyers.map(() => ADDRESS_ZERO),
       someBuyers.map(() => '0x'),
       {
-        value: (keyPrice * someBuyers.length).toFixed(),
+        value: keyPrice * BigInt(someBuyers.length),
       }
     ),
     'MIGRATION_REQUIRED'
@@ -146,60 +146,51 @@ describe('upgradeLock / data migration v9 > v10', () => {
   })
 
   describe('data / schema migration with more than 100 records', () => {
-    let keyOwners
+    let receipts
     let tokenIds
-    let expirationTimestamps
     let totalSupply = 500
 
     before(async () => {
       const [, lockOwner, generousBuyer] = await ethers.getSigners()
 
       // create 500 random wallets
-      keyOwners = await Promise.all(
+      // lets buy some key for each (with v9)
+      receipts = await Promise.all(
         Array(totalSupply)
           .fill(0)
-          .map(() => ethers.Wallet.createRandom())
+          .map(async () => {
+            const { address: keyOwner } = await ethers.Wallet.createRandom()
+            const tx = await lock
+              .connect(generousBuyer)
+              .purchase(0, keyOwner, ADDRESS_ZERO, ADDRESS_ZERO, '0x', {
+                value: keyPrice,
+              })
+            const receipt = await tx.wait()
+            const {
+              args: { tokenId },
+            } = await getEvent(receipt, 'Transfer')
+            const expirationTimestamp = await lock.keyExpirationTimestampFor(
+              keyOwner
+            )
+            return {
+              keyOwner,
+              tokenId,
+              expirationTimestamp,
+            }
+          })
       )
 
-      // lets buy some key for each (with v9)
-      await Promise.all(
-        keyOwners.map(
-          async (_, i) =>
-            await lock
-              .connect(generousBuyer)
-              .purchase(
-                0,
-                await keyOwners[i].getAddress(),
-                ADDRESS_ZERO,
-                ADDRESS_ZERO,
-                '0x',
-                {
-                  value: keyPrice,
-                }
-              )
-        )
+      // sort by token id
+      receipts = receipts.sort((a, b) =>
+        parseInt(parseInt(a.tokenId.toString() - b.tokenId.toString()))
       )
 
       // make sure buys went thru
       assert.equal(await lock.totalSupply(), totalSupply)
-
-      tokenIds = await Promise.all(
-        keyOwners.map(
-          async (b) => await lock.getTokenIdFor(await b.getAddress())
-        )
-      )
-      expirationTimestamps = await Promise.all(
-        keyOwners.map(
-          async (b) =>
-            await lock.keyExpirationTimestampFor(await b.getAddress())
-        )
-      )
+      assert.equal(await lock.totalSupply(), BigInt(receipts.length))
 
       // update abi before upgrade, so we can track event
-      lock = await ethers.getContractAt(
-        PublicLockLatest.interface.format(ethers.FormatTypes.full),
-        await lock.getAddress()
-      )
+      lock = await PublicLockLatest.attach(await lock.getAddress())
 
       // we listen to event in the lock itself
       lock.once('KeysMigrated', (data) => {
@@ -211,7 +202,7 @@ describe('upgradeLock / data migration v9 > v10', () => {
       const [, creator] = await ethers.getSigners()
       const tx = await unlock
         .connect(creator)
-        .upgradeLock(await lock.getAddress(), pastVersion + 1)
+        .upgradeLock(await lock.getAddress(), pastVersion + 1n)
       const receipt = await tx.wait()
 
       // check if box instance works
@@ -220,7 +211,7 @@ describe('upgradeLock / data migration v9 > v10', () => {
 
       // make sure upgrade event is correct
       assert.equal(lockAddress, await lock.getAddress())
-      assert.equal(version, pastVersion + 1)
+      assert.equal(version, pastVersion + 1n)
 
       // set multiple keys
       await lock.connect(lockOwner).setMaxKeysPerAddress(10)
@@ -237,7 +228,7 @@ describe('upgradeLock / data migration v9 > v10', () => {
     })
 
     it('Should have upgraded the lock with the new template', async () => {
-      assert.equal(await lock.publicLockVersion(), pastVersion + 1)
+      assert.equal(await lock.publicLockVersion(), pastVersion + 1n)
     })
 
     it('schemaVersion still undefined', async () => {
@@ -245,39 +236,27 @@ describe('upgradeLock / data migration v9 > v10', () => {
     })
 
     it('relevant records have been updated', async () => {
-      for (let i = 0; i < 100; i++) {
-        const tokenId = i + 1
+      for (const { expirationTimestamp, tokenId, keyOwner } of receipts.splice(
+        0,
+        100
+      )) {
         assert.equal(await lock.isValidKey(tokenId), true)
-        assert.equal(
-          await lock.ownerOf(tokenId),
-          await keyOwners[i].getAddress()
-        )
-        assert.equal(await lock.balanceOf(await keyOwners[i].getAddress()), 1)
-        assert.equal(
-          await lock.getHasValidKey(await keyOwners[i].getAddress()),
-          true
-        )
+        assert.equal(await lock.ownerOf(tokenId), keyOwner)
+        assert.equal(await lock.balanceOf(keyOwner), 1)
+        assert.equal(await lock.getHasValidKey(keyOwner), true)
         assert.equal(
           await lock.keyExpirationTimestampFor(tokenId),
-          expirationTimestamps[i]
+          expirationTimestamp
         )
       }
     })
 
     it('rest of the records have NOT been updated', async () => {
-      const totalSupply = await lock.totalSupply()
-      for (let i = 100; i < totalSupply; i++) {
-        const tokenId = i + 1
+      for (const { tokenId, keyOwner } of receipts.splice(100, 200)) {
         assert.equal(await lock.isValidKey(tokenId), false)
-        assert.equal(
-          await lock.ownerOf(tokenId),
-          await keyOwners[i].getAddress()
-        ) // ownerOf should work regardless
-        assert.equal(await lock.balanceOf(await keyOwners[i].getAddress()), 0)
-        assert.equal(
-          await lock.getHasValidKey(await keyOwners[i].getAddress()),
-          false
-        )
+        assert.equal(await lock.ownerOf(tokenId), keyOwner) // ownerOf should work regardless
+        assert.equal(await lock.balanceOf(keyOwner), 0)
+        assert.equal(await lock.getHasValidKey(keyOwner), false)
         assert.equal(await lock.keyExpirationTimestampFor(tokenId), 0)
       }
     })
@@ -308,21 +287,18 @@ describe('upgradeLock / data migration v9 > v10', () => {
       })
 
       it('batch of 100 next records have been updated', async () => {
-        for (let i = 100; i < 200; i++) {
-          const tokenId = i + 1
+        for (const {
+          expirationTimestamp,
+          tokenId,
+          keyOwner,
+        } of receipts.splice(parseInt(updatedRecordsCount.toString()), 100)) {
           assert.equal(await lock.isValidKey(tokenId), true)
-          assert.equal(
-            await lock.ownerOf(tokenId),
-            await keyOwners[i].getAddress()
-          )
-          assert.equal(await lock.balanceOf(await keyOwners[i].getAddress()), 1)
-          assert.equal(
-            await lock.getHasValidKey(await keyOwners[i].getAddress()),
-            true
-          )
+          assert.equal(await lock.ownerOf(tokenId), keyOwner)
+          assert.equal(await lock.balanceOf(keyOwner), 1)
+          assert.equal(await lock.getHasValidKey(keyOwner), true)
           assert.equal(
             await lock.keyExpirationTimestampFor(tokenId),
-            expirationTimestamps[i]
+            expirationTimestamp
           )
         }
       })
@@ -343,28 +319,24 @@ describe('upgradeLock / data migration v9 > v10', () => {
         // migrate another batch of 200
         const calldata2 = encoder.encode(['uint', 'uint'], [300, 200])
         const tx2 = await lock.connect(lockOwner).migrate(calldata2)
-        const { events: events2 } = await tx2.wait()
-        const { args: args2 } = events2.find((v) => v.event === 'KeysMigrated')
+        const receipt2 = await tx2.wait()
+        const { args: args2 } = await getEvent(receipt2, 'KeysMigrated')
         assert.equal(args2.updatedRecordsCount, 200)
       })
 
       it('rest of the records have been updated', async () => {
-        const totalSupply = await lock.totalSupply()
-        for (let i = 100; i < totalSupply; i++) {
-          const tokenId = i + 1
+        for (const {
+          expirationTimestamp,
+          tokenId,
+          keyOwner,
+        } of receipts.splice(200, receipts.length - 1)) {
           assert.equal(await lock.isValidKey(tokenId), true)
-          assert.equal(
-            await lock.ownerOf(tokenId),
-            await keyOwners[i].getAddress()
-          )
-          assert.equal(await lock.balanceOf(await keyOwners[i].getAddress()), 1)
-          assert.equal(
-            await lock.getHasValidKey(await keyOwners[i].getAddress()),
-            true
-          )
+          assert.equal(await lock.ownerOf(tokenId), keyOwner)
+          assert.equal(await lock.balanceOf(keyOwner), 1)
+          assert.equal(await lock.getHasValidKey(keyOwner), true)
           assert.equal(
             await lock.keyExpirationTimestampFor(tokenId),
-            expirationTimestamps[i]
+            expirationTimestamp
           )
         }
       })
@@ -407,7 +379,7 @@ describe('upgradeLock / data migration v9 > v10', () => {
               someBuyers.map(() => ADDRESS_ZERO),
               someBuyers.map(() => '0x'),
               {
-                value: (keyPrice * someBuyers.length).toFixed(),
+                value: keyPrice * BigInt(someBuyers.length),
               }
             )
             const receipt = await tx.wait()
@@ -431,15 +403,16 @@ describe('upgradeLock / data migration v9 > v10', () => {
           })
 
           it('extend should now work ', async () => {
+            const { tokenId, expirationTimestamp } = receipts[0]
             const tx = await lock
               .connect(someBuyers[0])
-              .extend(0, tokenIds[0], ADDRESS_ZERO, '0x', {
+              .extend(0, tokenId, ADDRESS_ZERO, '0x', {
                 value: keyPrice,
               })
             await tx.wait()
             assert.equal(
-              (await lock.keyExpirationTimestampFor(tokenIds[0])) >
-                expirationTimestamps[0],
+              (await lock.keyExpirationTimestampFor(tokenId)) >
+                expirationTimestamp,
               true
             )
           })
