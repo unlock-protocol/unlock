28223028416ddcaa63d87dc4a14daf2160beb337
Clément Renaud <clement+git@clementrenaud.com>
2024-05-15 18:11:41 +0200

    better terndely integration

--- governance/helpers/crossChain.js
+++ governance/helpers/crossChain.js
@@ -3,7 +3,80 @@ const { ethers } = require('ethers')
 const { createFork } = require('./tenderly')
 const { delayABI } = require('./bridge')
 
+async function simulateDelayCall({ rpcUrl, network, moduleCall }) {
+  const {
+    name,
+    id,
+    governanceBridge: {
+      modules: { delayMod, connextMod },
+    },
+  } = network
+
+  const {
+    to, // to
+    value, // value
+    data, // data
+    operation,
+  } = moduleCall
+
+  console.log({
+    to, // to
+    value, // value
+    data, // data
+    operation,
+  })
+
+  console.log(
+    `To simulate results on receiving end - chain ${name} (${id})
+  - call \`execTransactionFromModule\` on contract ${delayMod} with from set as ${connextMod}
+  - wait for cooldown period (default: +172800)
+  - call \`executeNexTx\` on contract ${delayMod} with from set as ${connextMod}
+  `
+  )
+  // ethers provider and signer
+  const forkProvider = new ethers.JsonRpcProvider(rpcUrl)
+
+  // send all tx as connext
+  const signer = await forkProvider.getSigner(connextMod)
+
+  // parse calls
+  const delayInterface = new ethers.Interface(delayABI)
+
+  // 1. add tx to delay module
+  await signer.sendTransaction({
+    from: connextMod,
+    to: delayMod,
+    data: delayInterface.encodeFunctionData('execTransactionFromModule', [
+      to,
+      value,
+      data,
+      operation ? 1n : 0n,
+    ]),
+    gasLimit: 800000,
+  })
+
+  // 2. wait for a while
+  const coolDown = 172800n // TODO: fetch from contract
+  const params = [ethers.toQuantity(coolDown)]
+  await forkProvider.send('evm_increaseTime', params)
+  console.log(`> skipping cool down period: advancing ${coolDown} seconds`)
+
+  // 3. execute tx
+  await signer.sendTransaction({
+    from: connextMod,
+    to: delayMod,
+    data: delayInterface.encodeFunctionData('executeNexTx', [
+      to,
+      value,
+      data,
+      operation,
+    ]),
+    gasLimit: 800000,
+  })
+}
+
 async function simulateDestCalls(xCalls) {
+  // parse multisig calls from xCall events
   const abiCoder = ethers.AbiCoder.defaultAbiCoder()
   const destChainCalls = xCalls.map(
     ({ transferId, params: { callData, destinationDomain } }) => {
@@ -15,106 +88,35 @@ async function simulateDestCalls(xCalls) {
             destinationDomain.toString()
           : false
       )
-      const [
-        to, // to
-        value, // value
-        data, // data
-        operation, // operation: 0 for CALL, 1 for DELEGATECALL
-      ] = abiCoder.decode(['address', 'uint256', 'bytes', 'bool'], callData)
+      const moduleCall = abiCoder.decode(
+        ['address', 'uint256', 'bytes', 'bool'],
+        callData
+      )
 
       return {
         network,
-        to,
-        value,
-        data,
-        operation,
+        moduleCall,
       }
     }
   )
-
-  console.log(destChainCalls)
-
+  // simulate
   await Promise.all(
-    destChainCalls.map(
-      async ({
-        to, // to
-        value, // value
-        data, // data
-        operation,
+    destChainCalls.map(async ({ network, moduleCall }) => {
+      // create a fork
+      const { rpcUrl } = await createFork(network.id)
+      console.log('Fork URL\n\t' + rpcUrl)
+
+      // simulate calls on Tenderly
+      await simulateDelayCall({
+        rpcUrl,
         network,
-      }) => {
-        const {
-          name,
-          id,
-          governanceBridge: {
-            modules: { delayMod, connextMod },
-          },
-        } = network
-
-        console.log({
-          to, // to
-          value, // value
-          data, // data
-          operation,
-        })
-
-        console.log(
-          `To simulate results on receiving end - chain ${name} (${id})
-        - call \`execTransactionFromModule\` on contract ${delayMod} with from set as ${connextMod}
-        - wait for cooldown period (default: +172800)
-        - call \`executeNexTx\` on contract ${delayMod} with from set as ${connextMod}
-        `
-        )
-
-        // create a fork
-        const { rpcUrl } = await createFork(id)
-        console.log('Fork URL\n\t' + rpcUrl)
-
-        // ethers provider and signer
-        const forkProvider = new ethers.providers.JsonRpcProvider(rpcUrl)
-        const [connextModSigner] = [forkProvider.getSigner(connextMod)]
-
-        // parse calls
-        const delayInterface = new ethers.Interface(delayABI)
-
-        // 1. add tx to delay module
-        await connextModSigner.sendTransaction({
-          from: connextMod,
-          to: delayMod,
-          data: delayInterface.encodeFunctionData('execTransactionFromModule', [
-            to,
-            value,
-            data,
-            operation,
-          ]),
-          gasLimit: 800000,
-        })
-
-        // 2. wait for a while
-        const coolDown = 172800n // TODO: fetch from contract
-        const params = [ethers.toQuantity(coolDown)]
-        await forkProvider.send('evm_increaseTime', params)
-        console.log(
-          `> skipping cool down period: advancing ${coolDown} seconds`
-        )
-
-        // 3. execute tx
-        await connextModSigner.sendTransaction({
-          from: connextMod,
-          to: delayMod,
-          data: delayInterface.encodeFunctionData('executeNexTx', [
-            to,
-            value,
-            data,
-            operation,
-          ]),
-          gasLimit: 800000,
-        })
-      }
-    )
+        moduleCall,
+      })
+    })
   )
 }
 
 module.exports = {
   simulateDestCalls,
+  simulateDelayCall,
 }
--- governance/helpers/tenderly.js
+++ governance/helpers/tenderly.js
@@ -18,6 +18,14 @@ async function deleteFork(forkId) {
 }
 
 async function createFork(network_id) {
+  if (
+    !TENDERLY_ACCOUNT_SLUG ||
+    !TENDERLY_PROJECT_SLUG ||
+    !TENDERLY_ACCESS_KEY
+  ) {
+    throw new Error(`Missing tenderly credentials. Please export 
+TENDERLY_ACCOUNT_SLUG, TENDERLY_PROJECT_SLUG, TENDERLY_ACCESS_KEY to your env`)
+  }
   const res = await fetch(
     `https://api.tenderly.co/api/v1/account/${TENDERLY_ACCOUNT_SLUG}/project/${TENDERLY_PROJECT_SLUG}/fork`,
     {
@@ -31,14 +39,17 @@ async function createFork(network_id) {
       }),
     }
   )
-
-  const { data } = res.json()
-  const forkId = data.simulation_fork.id
-  const rpcUrl = `https://rpc.tenderly.co/fork/${forkId}`
-  return {
-    rpcUrl,
-    data,
+  if (res.status === 201) {
+    const data = await res.json()
+    const forkId = data.simulation_fork.id
+    console.log(`Fork for chain ${network_id} created: ${forkId}`)
+    const rpcUrl = `https://rpc.tenderly.co/fork/${forkId}`
+    return {
+      rpcUrl,
+      data,
+    }
   }
+  throw new Error(`Failed to create a Tenderly fork ${res}`)
 }
 
 module.exports = { deleteFork, createFork }
