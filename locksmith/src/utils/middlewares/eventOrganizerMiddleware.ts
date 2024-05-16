951e8cb7a448b3ee7543a5732656dc809dafe2de
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-05-16 11:14:57 +0300

    Fixed eventOrganizerMiddleware

--- locksmith/src/utils/middlewares/eventOrganizerMiddleware.ts
+++ locksmith/src/utils/middlewares/eventOrganizerMiddleware.ts
@@ -1,6 +1,4 @@
 import { RequestHandler } from 'express'
-import { PaywallLocksConfigType } from '@unlock-protocol/core'
-import { EventBody } from '../../controllers/v2/eventsController'
 import { isEventOrganizer } from '../eventOrganizers'
 
 export const eventOrganizerMiddleware: RequestHandler = async (
@@ -10,16 +8,9 @@ export const eventOrganizerMiddleware: RequestHandler = async (
 ) => {
   const userAddress = request.user!.walletAddress
 
-  let locks: PaywallLocksConfigType = {}
-
   let slug = request.params.slug
 
   if (!slug) {
-    const parsed = await EventBody.parseAsync(request.body)
-
-    locks = parsed.checkoutConfig.config.locks
-    slug = parsed.data.slug
-
     return next()
   }
 
