54d30bee4c342664a6074f696e58ff3f0a290995
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-05-16 10:55:24 +0300

    Fixed eventOrganizerMiddleware

--- locksmith/src/utils/middlewares/eventOrganizerMiddleware.ts
+++ locksmith/src/utils/middlewares/eventOrganizerMiddleware.ts
@@ -1,19 +1,13 @@
 import { RequestHandler } from 'express'
-import { getEventBySlug } from '../../operations/eventOperations'
-import { getCheckoutConfigById } from '../../operations/checkoutConfigOperations'
-import { isEmpty } from 'lodash'
-import { Web3Service } from '@unlock-protocol/unlock-js'
-import networks from '@unlock-protocol/networks'
 import { PaywallLocksConfigType } from '@unlock-protocol/core'
 import { EventBody } from '../../controllers/v2/eventsController'
+import { isEventOrganizer } from '../eventOrganizers'
 
-// TODO: This middlewares duplicates code from the isEventOrganizer function
 export const eventOrganizerMiddleware: RequestHandler = async (
   request,
   response,
   next
 ) => {
-  const web3Service = new Web3Service(networks)
   const userAddress = request.user!.walletAddress
 
   let locks: PaywallLocksConfigType = {}
@@ -25,41 +19,19 @@ export const eventOrganizerMiddleware: RequestHandler = async (
 
     locks = parsed.checkoutConfig.config.locks
     slug = parsed.data.slug
+
+    return next()
   }
 
-  // If this is an existing event!
-  if (slug) {
-    const existingEvent = await getEventBySlug(
-      slug,
-      false /** includeProtected */
-    )
-    if (existingEvent?.checkoutConfigId) {
-      const checkoutConfig = await getCheckoutConfigById(
-        existingEvent.checkoutConfigId
-      )
-      locks = checkoutConfig?.config.locks || {}
-    }
-  }
+  const isLockManager = await isEventOrganizer(userAddress, slug)
 
-  if (isEmpty(locks)) {
+  if (isLockManager == undefined) {
     return response.status(404).send({
       message: `No such event`,
     })
   }
 
-  const lockManagers = await Promise.all(
-    Object.keys(locks).map((lockAddress: string) => {
-      const networkId = locks[lockAddress].network
-      return web3Service.isLockManager(
-        lockAddress,
-        userAddress,
-        Number(networkId)
-      )
-    })
-  )
-  const isLockManager = lockManagers.some((isManager) => isManager)
-
-  if (!isLockManager) {
+  if (isLockManager == false) {
     return response.status(403).send({
       message: `${userAddress} is not a lock manager of this transaction`,
     })
