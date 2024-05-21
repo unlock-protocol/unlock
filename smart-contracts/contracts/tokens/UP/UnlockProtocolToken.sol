ab8cb902026786c2324a0d454079da9c0fe7a76a
Clément Renaud <clement+git@clementrenaud.com>
2024-05-21 16:38:12 +0200

    add votes to erc20

--- smart-contracts/contracts/tokens/UP/UnlockProtocolToken.sol
+++ smart-contracts/contracts/tokens/UP/UnlockProtocolToken.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts-upgradeable5/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable5/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable5/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable5/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable5/proxy/utils/Initializable.sol";
+import {NoncesUpgradeable} from "@openzeppelin/contracts-upgradeable5/utils/NoncesUpgradeable.sol";
 
+/// @custom:security-contact hello@unlock-protocol.com
 contract UnlockProtocolToken is
   Initializable,
   ERC20Upgradeable,
   ERC20PermitUpgradeable,
+  ERC20VotesUpgradeable,
   OwnableUpgradeable
 {
   uint public constant TOTAL_SUPPLY = 1_000_000_000;
@@ -26,9 +30,31 @@ contract UnlockProtocolToken is
   ) public initializer {
     __ERC20_init("UnlockProtocolToken", "UP");
     __ERC20Permit_init("UnlockProtocolToken");
+    __ERC20Votes_init();
     __Ownable_init(initialOwner);
 
     // premint the supply
     _mint(preMinter, TOTAL_SUPPLY * 10 ** decimals());
+  }
+
+  // The following functions are overrides required by Solidity.
+
+  function _update(
+    address from,
+    address to,
+    uint256 value
+  ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+    super._update(from, to, value);
+  }
+
+  function nonces(
+    address owner
+  )
+    public
+    view
+    override(ERC20PermitUpgradeable, NoncesUpgradeable)
+    returns (uint256)
+  {
+    return super.nonces(owner);
   }
 }
