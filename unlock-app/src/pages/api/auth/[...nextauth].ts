ac2b388d78bbb062b74d95742325f7d98c5ef3ba
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-05-27 21:19:41 +0300

    Next-auth

---
+++ unlock-app/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
