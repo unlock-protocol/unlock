1edd1ac86db023dc25c8930291922493ad31d10c
Clément Renaud <clement@unlock-protocol.com>
2024-05-28 11:09:33 +0200

    feat(smart-contracts):  pass contract deploy args as file in cli (#13891)

--- packages/hardhat-helpers/src/tasks/deploy.js
+++ packages/hardhat-helpers/src/tasks/deploy.js
@@ -5,11 +5,16 @@ const path = require('path')
 task('deploy:contract', 'Deploy any contract by name')
   .addFlag('proxied', 'deploy using OZ Transparent Proxy')
   .addParam('contract', 'the path of the contract to deploy')
+  .addOptionalParam(
+    'constructorArgs',
+    'the absolute path of a file with constructor args - ex. `pwd`/scripts/args.js'
+  )
   .addOptionalVariadicPositionalParam(
     'params',
     'List of deployment args to pass to the constructor'
   )
-  .setAction(async ({ contract, params, proxied, verifyOnly }) => {
+  .setAction(async ({ contract, params, proxied, constructorArgs }) => {
+
     const contractName = path
       .basename(contract)
       .replace('.sol', '')
@@ -17,6 +22,12 @@ task('deploy:contract', 'Deploy any contract by name')
     const qualified = `${contract}:${contractName}`
 
     let address, hash
+
+    if (constructorArgs) {
+      params = require(`${constructorArgs}`)
+      console.log(params)
+    }
+
     if (proxied) {
       ;({ address, hash } = await deployContract(qualified, params))
     } else {
