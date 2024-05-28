594679b7ba40a6bc9a3e27b21c20fca53617bcb2
Clément Renaud <clement@unlock-protocol.com>
2024-05-28 12:25:58 +0200

    fix(smart-contracts): fix flaky lock upgrade test (#13896)
    * use for...of pattern for await calls
    
    * remove log

--- smart-contracts/test/Lock/upgrades/v11.js
+++ smart-contracts/test/Lock/upgrades/v11.js
@@ -135,7 +135,7 @@ describe('PublicLock upgrade v10 > v11', () => {
 
       it('preserves all keys data', async () => {
         const totalSupply = await lock.totalSupply()
-        for (let i = 0; i < totalSupply; i++) {
+        for (let i of Array(totalSupply).fill(0)) {
           const tokenId = i + 1
           assert.equal(await lock.isValidKey(tokenId), true)
           assert.equal(
