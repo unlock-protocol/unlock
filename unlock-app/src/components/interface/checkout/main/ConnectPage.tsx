c1483a990c60f60df6bbac187a436625a6b2fd6b
Julien Genestoux <julien.genestoux@gmail.com>
2024-05-30 18:10:14 -0400

    some simplification and refactoring for our login flows

--- unlock-app/src/components/interface/checkout/main/ConnectPage.tsx
+++ unlock-app/src/components/interface/checkout/main/ConnectPage.tsx
@@ -3,6 +3,7 @@ import { ConnectedWallet } from '../../connect/ConnectedWallet'
 import { ConnectWallet } from '../../connect/Wallet'
 import { PoweredByUnlock } from '../PoweredByUnlock'
 import { ConnectUnlockAccount } from '../../connect/UnlockAccount'
+import { useSIWE } from '~/hooks/useSIWE'
 
 interface ConnectPageProps {
   style: string
