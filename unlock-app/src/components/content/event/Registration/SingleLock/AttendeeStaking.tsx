22244711f50848e304e706c738e33049ded21edd
Julien Genestoux <julien.genestoux@gmail.com>
2024-05-31 08:49:16 -0400

    feat(unlock-app): adding support to set up events with stake (#13912)
    * adding kickback
    
    * updated
    
    * letting organizers set their events with attendee refunds

---
+++ unlock-app/src/components/content/event/Registration/SingleLock/AttendeeStaking.tsx
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query'
+import networks from '@unlock-protocol/networks'
+import { Web3Service } from '@unlock-protocol/unlock-js'
+import { ethers } from 'ethers'
+import { AttendeeRefundType } from '@unlock-protocol/core'
+
+interface AttendeeStakingProps {
+  attendeeRefund?: AttendeeRefundType
+}
+
+export const AttendeeStaking = ({ attendeeRefund }: AttendeeStakingProps) => {
+  const { data: refund } = useQuery(
+    ['attendeeRefund', attendeeRefund],
+    async () => {
+      const web3Service = new Web3Service(networks)
+      const [decimals, symbol] = await Promise.all([
+        web3Service.getTokenDecimals(
+          attendeeRefund!.currency,
+          attendeeRefund!.network
+        ),
+        web3Service.getTokenSymbol(
+          attendeeRefund!.currency,
+          attendeeRefund!.network
+        ),
+      ])
+      return `${ethers.utils.formatUnits(attendeeRefund!.amount, decimals)} ${symbol}`
+    },
+    {
+      enabled: !!attendeeRefund,
+    }
+  )
+
+  if (!attendeeRefund) {
+    return null
+  }
+  return (
+    <p>
+      This event requires attendees to stake when they register, and they will
+      be refunded {refund} if they joined!
+    </p>
+  )
+}
