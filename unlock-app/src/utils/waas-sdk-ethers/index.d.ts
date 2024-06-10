a35c8ff460d18c3c50177f503cbf709f44fafd4f
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-06-10 18:50:56 +0300

    Waas Ethers

---
+++ unlock-app/src/utils/waas-sdk-ethers/index.d.ts
@@ -0,0 +1,26 @@
+import {
+  Signer,
+  providers as EthersProviders,
+  TypedDataDomain,
+  TypedDataField,
+} from 'ethers'
+import { Address, ProtocolFamily } from '@coinbase/waas-sdk-web'
+export declare class WaasEthersSigner extends Signer {
+  waasAddress: Address<ProtocolFamily.EVM>
+  provider?: EthersProviders.Provider
+  constructor(
+    address: Address<ProtocolFamily.EVM>,
+    provider?: EthersProviders.Provider
+  )
+  connect(provider?: EthersProviders.Provider): Signer
+  getAddress(): Promise<string>
+  signTransaction(
+    transaction: EthersProviders.TransactionRequest
+  ): Promise<string>
+  signMessage(message: string | Uint8Array): Promise<string>
+  signTypedData(
+    domain: TypedDataDomain,
+    types: Record<string, Array<TypedDataField>>,
+    value: Record<string, any>
+  ): Promise<string>
+}
