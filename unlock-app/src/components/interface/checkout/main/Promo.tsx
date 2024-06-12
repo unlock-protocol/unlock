92487b0554f7bbeae84c7cdbd52cfea0d2d6e0f9
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-06-12 17:02:44 +0300

    fix(unlock-app): Fixed promo hook error (#14038)
    Update Promo.tsx

--- unlock-app/src/components/interface/checkout/main/Promo.tsx
+++ unlock-app/src/components/interface/checkout/main/Promo.tsx
@@ -85,6 +85,7 @@ export function PromoContent({
             signerAddress: privateKeyFromAccount.address,
           }
         )
+        promoCodeDetails.discount = Number(promoCodeDetails.discount)
         setPromoCodeDetails(promoCodeDetails)
         setPromoCodeLoading(false)
       }
