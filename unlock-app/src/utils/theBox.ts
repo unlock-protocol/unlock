cb999056781ab324b5a8b0c9d35bebf866b31bb6
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-12 22:06:45 -0400

    more cleanup

--- unlock-app/src/utils/theBox.ts
+++ unlock-app/src/utils/theBox.ts
@@ -3,7 +3,6 @@ import { ethers } from 'ethers'
 import axios from 'axios'
 import { networks } from '@unlock-protocol/networks'
 import { ADDRESS_ZERO } from '~/constants'
-import { BoxChainInfo, BoxChains } from '@decent.xyz/box-common'
 
 export interface BoxActionRequest {
   sender: string
@@ -28,16 +27,6 @@ export interface CrossChainRoute {
   symbol: string
   networkName: string
   currency: string
-}
-
-interface getCrossChainRoutesParams {
-  sender: string
-  lock: Lock
-  prices: any[]
-  recipients: string[]
-  keyManagers: string[]
-  referrers: string[]
-  purchaseData: string[]
 }
 
 const bigintSerializer = (_key: string, value: unknown): unknown => {
@@ -150,121 +139,4 @@ export const getCrossChainRoute = async ({
       networkName: network.name,
     } as CrossChainRoute
   }
-}
-
-export const getCrossChainRoutes = async ({
-  sender,
-  lock,
-  prices,
-  recipients,
-  keyManagers,
-  referrers,
-  purchaseData,
-}: getCrossChainRoutesParams): Promise<CrossChainRoute[]> => {
-  const baseUrl = 'https://box-v2.api.decent.xyz/api/getBoxAction'
-  const apiKey = '6477b3b3671589d81df0cba67ba9f3e6'
-  const actionRequest: BoxActionRequest = {
-    actionType: 'evm-function',
-    sender,
-
-    srcToken: ADDRESS_ZERO, // use the native token. Later: check the user balances!
-    dstToken: lock.currencyContractAddress || ADDRESS_ZERO,
-    slippage: 1, // 1%
-
-    srcChainId: 1, // will be replaced when looping over networks
-    dstChainId: lock.network,
-    actionConfig: {
-      chainId: lock.network,
-      contractAddress: lock.address,
-
-      cost: {
-        isNative: true,
-        amount: prices.reduce(
-          (acc, current) =>
-            acc +
-            ethers.parseUnits(current.amount.toString(), current.decimals),
-          BigInt('0')
-        ),
-      },
-
-      signature: encodeURIComponent(
-        'function purchase(uint256[] _values,address[] _recipients,address[] _referrers,address[] _keyManagers,bytes[] _data) payable returns (uint256[])'
-      ), // We need to get this from walletService!
-
-      args: [
-        prices.map((price) => {
-          const priceParsed = ethers.parseUnits(
-            price.amount.toString(),
-            price.decimals
-          )
-          return priceParsed
-        }),
-        recipients,
-        referrers,
-        keyManagers,
-        purchaseData,
-      ],
-    },
-  }
-
-  const routes: CrossChainRoute[] = (
-    await Promise.all(
-      BoxChains.filter((boxChain: BoxChainInfo) => {
-        if (
-          boxChain.id === lock.network &&
-          (!lock.currencyContractAddress ||
-            lock.currencyContractAddress === ADDRESS_ZERO)
-        ) {
-          return false // Not checking the chain's lock.
-        }
-        const network = networks[boxChain.id]
-        return !!network && !network.isTestNetwork
-      }).map(
-        async (
-          boxChain: BoxChainInfo
-        ): Promise<CrossChainRoute | undefined> => {
-          const network = networks[boxChain.id]
-          const query = JSON.stringify(
-            {
-              ...actionRequest,
-              srcChainId: network.id,
-            },
-            bigintSerializer
-          )
-
-          const url = `${baseUrl}?arguments=${query}`
-          const response = await axios
-            .get(url, {
-              headers: {
-                'x-api-key': apiKey,
-              },
-            })
-            .catch(function (error) {
-              console.error(error)
-            })
-          if (response?.status === 200) {
-            const { data } = response
-            return {
-              ...data,
-              tx: {
-                ...data.tx,
-                value: BigInt(data.tx.value.slice(0, -1)),
-              },
-              network: network.id,
-              currency: network.nativeCurrency.name,
-              symbol: network.nativeCurrency.symbol,
-              networkName: network.name,
-            } as CrossChainRoute
-          }
-          return
-        }
-      )
-    )
-  ).filter<CrossChainRoute>(
-    (route: CrossChainRoute | undefined): route is CrossChainRoute => {
-      return !!route
-    }
-  )
-
-  return routes
 }
