898d41df1efd9fa07bc5fb8d4ac5e9eb9414b868
Clément Renaud <clement+git@clementrenaud.com>
2024-06-14 15:32:15 +0200

    remove from contracts

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -33,15 +33,6 @@ const settings = {
 module.exports = {
   solidity: {
     compilers: [
-      { version: '0.4.24', settings },
-      { version: '0.4.25', settings },
-      { version: '0.5.0', settings },
-      { version: '0.5.12', settings },
-      { version: '0.5.17', settings },
-      { version: '0.5.14', settings },
-      { version: '0.5.7', settings },
-      { version: '0.5.9', settings },
-      { version: '0.6.12', settings },
       { version: '0.7.6', settings },
       { version: '0.8.0', settings },
       { version: '0.8.2', settings },
