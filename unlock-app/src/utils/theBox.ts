d5a8f8492f9a4cde467a82f49a6a6bb174695e0f
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-17 22:33:52 -0400

    fix(unlock-app): fixing decent serialization

--- unlock-app/src/utils/theBox.ts
+++ unlock-app/src/utils/theBox.ts
@@ -29,12 +29,12 @@ export interface CrossChainRoute {
   currency: string
 }
 
-const bigintSerializer = (_key: string, value: unknown): unknown => {
-  if (typeof value === 'bigint') {
-    return value.toString() + 'n'
-  }
-  return value
-}
+// const bigintSerializer = (_key: string, value: unknown): unknown => {
+//   if (typeof value === 'bigint') {
+//     return value.toString() + 'n'
+//   }
+//   return value
+// }
 
 interface getCrossChainRouteParams {
   sender: string
@@ -65,33 +65,34 @@ export const getCrossChainRoute = async ({
   const baseUrl = 'https://box-v2.api.decent.xyz/api/getBoxAction'
   const apiKey = '6477b3b3671589d81df0cba67ba9f3e6'
   const actionRequest: BoxActionRequest = {
-    actionType: 'evm-function',
     sender,
 
     srcToken,
     dstToken: lock.currencyContractAddress || ADDRESS_ZERO,
-    slippage: 1, // 1%
-
     srcChainId,
     dstChainId: lock.network,
+    slippage: 1, // 1%
+    actionType: 'evm-function',
+
     actionConfig: {
       chainId: lock.network,
       contractAddress: lock.address,
 
       cost: {
         isNative: true,
-        amount: prices.reduce(
-          (acc, current) =>
-            acc +
-            ethers.parseUnits(current.amount.toString(), current.decimals),
-          BigInt('0')
-        ),
+        amount:
+          prices
+            .reduce(
+              (acc, current) =>
+                acc +
+                ethers.parseUnits(current.amount.toString(), current.decimals),
+              BigInt('0')
+            )
+            .toString() + 'n',
       },
 
-      signature: encodeURIComponent(
-        'function purchase(uint256[] _values,address[] _recipients,address[] _referrers,address[] _keyManagers,bytes[] _data) payable returns (uint256[])'
-      ), // We need to get this from walletService!
-
+      signature:
+        'function purchase(uint256[] _values,address[] _recipients,address[] _referrers,address[] _keyManagers,bytes[] _data) payable returns (uint256[])', // We need to get this from walletService!
       args: [
         prices.map((price) => {
           const priceParsed = ethers.parseUnits(
@@ -107,13 +108,11 @@ export const getCrossChainRoute = async ({
       ],
     },
   }
+  console.log(actionRequest)
 
-  const query = JSON.stringify(
-    {
-      ...actionRequest,
-    },
-    bigintSerializer
-  )
+  const query = JSON.stringify({
+    ...actionRequest,
+  })
 
   const url = `${baseUrl}?arguments=${query}`
   const response = await axios
