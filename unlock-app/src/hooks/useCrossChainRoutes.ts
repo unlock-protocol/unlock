7294ffc42d66cef1af0f30ac0ad66d6f6eb364cb
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-19 12:51:03 -0400

    feat(unlock-app): not loading crosschain routes for extensions (#14075)
    not loading crosschain routes for extensions

--- unlock-app/src/hooks/useCrossChainRoutes.ts
+++ unlock-app/src/hooks/useCrossChainRoutes.ts
@@ -69,6 +69,8 @@ export const useCrossChainRoutes = ({
     }
   )
 
+  const hasPrices = Array.isArray(prices) && prices.length > 0
+
   const balanceResults = useQueries({
     queries: BoxEvmChains.filter((network) => {
       return networks[network.id]
@@ -82,7 +84,7 @@ export const useCrossChainRoutes = ({
           }
         },
         staleTime: 1000 * 60 * 5, // 5 minutes
-        enabled,
+        enabled: enabled && hasPrices,
       }
     }),
   })
@@ -196,7 +198,7 @@ export const useCrossChainRoutes = ({
                 ...route,
               } as CrossChainRouteWithBalance
             },
-            enabled,
+            enabled: enabled && hasPrices,
             staleTime: 1000 * 60 * 5, // 5 minutes
           }
         }
@@ -212,8 +214,8 @@ export const useCrossChainRoutes = ({
   return {
     isLoading:
       isLoadingPrices ||
-      balanceResults.some((result) => result.isLoading) ||
-      routeResults.some((result) => result.isLoading),
+      (hasPrices && balanceResults.some((result) => result.isLoading)) ||
+      (hasPrices && routeResults.some((result) => result.isLoading)),
     routes: routes.sort(
       (a: CrossChainRouteWithBalance, b: CrossChainRouteWithBalance) =>
         a!.resolvedAt - b!.resolvedAt
