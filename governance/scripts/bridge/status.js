6f939520a0b753225719ae1c26de0cd9d631fe0a
Clément Renaud <clement+git@clementrenaud.com>
2024-06-19 14:47:04 +0200

    remove use of cache file

--- governance/scripts/bridge/status.js
+++ governance/scripts/bridge/status.js
@@ -46,54 +46,54 @@ async function main({
   // store all status for all calls
   let statuses = {}
   if (await fs.exists(filepath)) {
-    console.log(`Cache file exists: ${filepath}. Using cache`)
-    statuses = await fs.readJSON(filepath)
-  } else {
-    console.log(`No file cache found: ${filepath}. Fetching results...`)
+    console.log(`Cache file exists: ${filepath}. Deleting.`)
+    await fs.rm(filepath)
+  }
 
-    // fetch statuses from Connext subgraphs for all calls
-    await Promise.all(
-      Object.keys(sorted).map(async (domainId) => {
-        const xcalls = sorted[domainId]
-        const transferIds = xcalls.map(({ transferId }) => transferId)
+  console.log(`Fetching results...`)
 
-        // get statuses for all calls
-        const originStatuses = await fetchOriginXCall({ transferIds })
-        originStatuses.forEach(
-          ({ transferId, status, transactionHash, originDomain, chainId }) => {
-            statuses[transferId] = {
-              origin: { status, chainId, originDomain, transactionHash },
-            }
+  // fetch statuses from Connext subgraphs for all calls
+  await Promise.all(
+    Object.keys(sorted).map(async (domainId) => {
+      const xcalls = sorted[domainId]
+      const transferIds = xcalls.map(({ transferId }) => transferId)
+
+      // get statuses for all calls
+      const originStatuses = await fetchOriginXCall({ transferIds })
+      originStatuses.forEach(
+        ({ transferId, status, transactionHash, originDomain, chainId }) => {
+          statuses[transferId] = {
+            origin: { status, chainId, originDomain, transactionHash },
           }
-        )
+        }
+      )
 
-        // get status on destination chain (mainnet)
-        const destStatuses = await fetchDestinationXCall({
-          transferIds,
-          chainId: destChains[domainId].id,
-        })
-        destStatuses.forEach(
-          ({
-            transferId,
+      // get status on destination chain (mainnet)
+      const destStatuses = await fetchDestinationXCall({
+        transferIds,
+        chainId: destChains[domainId].id,
+      })
+      destStatuses.forEach(
+        ({
+          transferId,
+          status,
+          chainId,
+          executedTransactionHash,
+          reconciledTransactionHash,
+          destinationDomain,
+        }) => {
+          statuses[transferId].dest = {
             status,
             chainId,
+            destinationDomain,
             executedTransactionHash,
             reconciledTransactionHash,
-            destinationDomain,
-          }) => {
-            statuses[transferId].dest = {
-              status,
-              chainId,
-              destinationDomain,
-              executedTransactionHash,
-              reconciledTransactionHash,
-            }
           }
-        )
-      })
-    )
-    await fs.outputJson(filepath, statuses, { spaces: 2 })
-  }
+        }
+      )
+    })
+  )
+  await fs.outputJson(filepath, statuses, { spaces: 2 })
 
   // log all results
   Object.keys(statuses).map((transferId) =>
