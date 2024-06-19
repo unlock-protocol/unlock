39aa4195ac26b51fb8521783624409f5754a4d61
Clément Renaud <clement@unlock-protocol.com>
2024-06-19 15:58:25 +0200

    docs(governance): improved docs for DAO cross-chain proposal execution (#14071)
    * improved docs for DAO cross-chain proposal execution
    
    * remove use of cache file
    
    * add subgraph step to the docs
    
    * Update governance/README.md
    
    * Update governance/README.md
    
    ---------
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>

--- governance/scripts/bridge/status.js
+++ governance/scripts/bridge/status.js
@@ -3,7 +3,7 @@
  * tx
  *
  * Usage:
- * 1. update the `txId` with the DAO proposal execution tx has
+ * 1. export the `txId` from the DAO proposal execution tx as PROPOSAL_EXECUTION_TX
  * 2. run the script with :
  *  `yarn hardhat run scripts/bridge/status.js --network mainnet`
  */
@@ -21,9 +21,14 @@ const fs = require('fs-extra')
 const filepath = './xcalled.tmp.json'
 
 async function main({
-  // TODO: pass this hash via cli
-  txId = '0x16b4e4d8608e469d89daed52d38edeb04158380d156d2f5ca6b4341a15b56430',
+  // if of the tx from the DAO proposal execution
+  txId = process.env.PROPOSAL_EXECUTION_TX,
 } = {}) {
+  if (!txId) {
+    throw Error(
+      `Missing txId. Please export PROPOSAL_EXECUTION_TX in your shell`
+    )
+  }
   const xCalls = await getXCalledEventsFromTx(txId)
 
   // sort by domain id
@@ -41,54 +46,54 @@ async function main({
   // store all status for all calls
   let statuses = {}
   if (await fs.exists(filepath)) {
-    console.log(`Cache file exists: ${filepath}. Using cache`)
-    statuses = await fs.readJSON(filepath)
-  } else {
-    console.log(`No file cache found: ${filepath}. Fetching results...`)
+    console.log(`Cache file exists: ${filepath}. Deleting.`)
+    await fs.rm(filepath)
+  }
 
-    // fetch statuses from Connext subgraphs for all calls
-    await Promise.all(
-      Object.keys(sorted).map(async (domainId) => {
-        const xcalls = sorted[domainId]
-        const transferIds = xcalls.map(({ transferId }) => transferId)
+  console.log(`Fetching results...`)
 
-        // get statuses for all calls
-        const originStatuses = await fetchOriginXCall({ transferIds })
-        originStatuses.forEach(
-          ({ transferId, status, transactionHash, originDomain, chainId }) => {
-            statuses[transferId] = {
-              origin: { status, chainId, originDomain, transactionHash },
-            }
+  // fetch statuses from Connext subgraphs for all calls
+  await Promise.all(
+    Object.keys(sorted).map(async (domainId) => {
+      const xcalls = sorted[domainId]
+      const transferIds = xcalls.map(({ transferId }) => transferId)
+
+      // get statuses for all calls
+      const originStatuses = await fetchOriginXCall({ transferIds })
+      originStatuses.forEach(
+        ({ transferId, status, transactionHash, originDomain, chainId }) => {
+          statuses[transferId] = {
+            origin: { status, chainId, originDomain, transactionHash },
           }
-        )
+        }
+      )
 
-        // get status on destination chain (mainnet)
-        const destStatuses = await fetchDestinationXCall({
-          transferIds,
-          chainId: destChains[domainId].id,
-        })
-        destStatuses.forEach(
-          ({
-            transferId,
+      // get status on destination chain (mainnet)
+      const destStatuses = await fetchDestinationXCall({
+        transferIds,
+        chainId: destChains[domainId].id,
+      })
+      destStatuses.forEach(
+        ({
+          transferId,
+          status,
+          chainId,
+          executedTransactionHash,
+          reconciledTransactionHash,
+          destinationDomain,
+        }) => {
+          statuses[transferId].dest = {
             status,
             chainId,
+            destinationDomain,
             executedTransactionHash,
             reconciledTransactionHash,
-            destinationDomain,
-          }) => {
-            statuses[transferId].dest = {
-              status,
-              chainId,
-              destinationDomain,
-              executedTransactionHash,
-              reconciledTransactionHash,
-            }
           }
-        )
-      })
-    )
-    await fs.outputJson(filepath, statuses, { spaces: 2 })
-  }
+        }
+      )
+    })
+  )
+  await fs.outputJson(filepath, statuses, { spaces: 2 })
 
   // log all results
   Object.keys(statuses).map((transferId) =>
