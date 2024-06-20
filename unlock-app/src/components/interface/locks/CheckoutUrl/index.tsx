ffaf041dfe8855c3ca546cdaddcb3886680545f8
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-20 16:37:17 -0400

    fixing error when creating new checkout URL

--- unlock-app/src/components/interface/locks/CheckoutUrl/index.tsx
+++ unlock-app/src/components/interface/locks/CheckoutUrl/index.tsx
@@ -193,11 +193,11 @@ export const CheckoutUrlPage = () => {
         name: configName,
         config: DEFAULT_CONFIG.config,
       })
-    }
-
-    if (!checkoutConfig?.id) {
-      ToastHelper.error('Please select a configuration or create a new one.')
-      return Promise.reject() // no config selected, prevent skip to next step
+    } else {
+      if (!checkoutConfig?.id) {
+        ToastHelper.error('Please select a configuration or create a new one.')
+        return Promise.reject() // no config selected, prevent skip to next step
+      }
     }
   }
 
