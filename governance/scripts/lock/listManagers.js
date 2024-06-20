feb0450020472cab2f436d2a500129c65213b7cb
Clément Renaud <clement+git@clementrenaud.com>
2024-06-20 17:48:06 +0200

    add script to check versions

--- governance/scripts/lock/listManagers.js
+++ governance/scripts/lock/listManagers.js
@@ -1,19 +1,11 @@
 const { ethers } = require('hardhat')
-const { networks } = require('@unlock-protocol/networks')
+const { fetchFromSubgraph } = require('../../helpers/subgraph')
 
 async function main({ lockAddress }) {
   const PublicLock = await ethers.getContractFactory('PublicLock')
   const lock = PublicLock.attach(lockAddress)
 
   const { chainId } = await ethers.provider.getNetwork()
-  const { subgraph } = networks[chainId]
-
-  if (!subgraph || !subgraph.endpoint) {
-    console.log(
-      'Missing subGraphURI for this network. Can not fetch from The Graph'
-    )
-    return []
-  }
 
   const query = `
     {
@@ -27,25 +19,11 @@ async function main({ lockAddress }) {
     }
   `
 
-  const q = await fetch(subgraph.endpoint, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query,
-    }),
-  })
-
-  const { data, errors } = await q.json()
-  if (errors) {
-    console.log('LOCK > Error while fetching the graph', errors)
-    return []
-  }
-
   const {
     locks: [{ LockManagers }],
-  } = data
-  const managers = LockManagers.map((m) => m.address)
+  } = await fetchFromSubgraph({ chainId, query })
 
+  const managers = LockManagers.map((m) => m.address)
   console.log(
     `LOCK > managers for the lock '${await lock.name()}' (${lockAddress}):`
   )
---
+++ governance/scripts/lock/outdated.js
@@ -0,0 +1,145 @@
+const { fetchFromSubgraph } = require('../../helpers/subgraph')
+const { networks } = require('@unlock-protocol/networks')
+
+async function getAllReceipts({ timeLimit, chainId }) {
+  const limit = 1000
+  let skip = 0
+  let more = true
+  const receipts = []
+  while (more) {
+    // console.log(`fetching results from  ${skip} to ${limit}`)
+    const receiptsQuery = `
+    {
+      receipts(
+        skip: ${skip}
+        first: ${limit}
+        where:{
+          timestamp_gte: ${timeLimit} 
+        }
+        ) {
+        lockAddress
+      }
+    }
+`
+    const { receipts: results } = await fetchFromSubgraph({
+      chainId,
+      query: receiptsQuery,
+    })
+
+    if (results.length < limit) {
+      more = false
+    } else {
+      skip += limit
+    }
+    receipts.push(...results)
+  }
+  return receipts
+}
+
+async function getLockVersions({ chainId, lockAddresses }) {
+  const versionsQuery = `
+      {
+        locks(
+          orderBy: version
+          sortDirection: DESC
+          where:{
+            address_in: ${JSON.stringify(lockAddresses)}
+            
+          }
+        ) {
+          version
+          address
+        }
+      }
+    `
+
+  const { locks } = await fetchFromSubgraph({
+    chainId,
+    query: versionsQuery,
+  })
+
+  // append chainId
+  return locks.map((lock) => ({ ...lock, chainId }))
+}
+
+async function getAllLockVersionInfo({ chainId, timeLimit }) {
+  // get all receipts before the date
+  const receipts = await getAllReceipts({ chainId, timeLimit })
+
+  // parse all addresses
+  const lockAddresses = receipts
+    .map(({ lockAddress }) => lockAddress)
+    .reduce((prev, curr) => (!prev.includes(curr) ? [...prev, curr] : prev), [])
+
+  // get all receipts before the date
+  const versions = await getLockVersions({ chainId, lockAddresses })
+  const count = versions.reduce(
+    (prev, { version }) => ((prev[version] = (prev[version] || 0) + 1), prev),
+    {}
+  )
+  const earliest = Object.keys(count)[0]
+  const earliestLocks = versions.filter(({ version }) => version == earliest)
+  return {
+    chainId,
+    receipts,
+    lockAddresses,
+    earliest,
+    earliestLocks,
+    count,
+  }
+}
+
+function logLocks({
+  chainId,
+  receipts,
+  lockAddresses,
+  earliest,
+  earliestLocks,
+  count,
+}) {
+  const { name } = networks[chainId]
+  console.log(
+    `${name} (${chainId}): 
+    - receipts: ${receipts.length}
+    - locks: ${lockAddresses.length} unique lock addresses
+    - earliest version ${earliest} 
+    - earliest ${earliestLocks.length} locks
+    - versions: ${Object.keys(count)
+      .map((v) => `v${v}:${count[v]}`)
+      .join(',')}
+    `
+  )
+}
+
+async function main() {
+  const timeLimit = new Date('2022-01-01').getTime() / 1000
+
+  const chains = Object.keys(networks).filter(
+    (id) => !isNaN(parseInt(id)) && !networks[id].isTestNetwork
+  )
+  console.log(`Chains: ${chains.join(',')} `)
+  const infos = {}
+  await Promise.all(
+    chains.map(async (chainId) => {
+      try {
+        const info = await getAllLockVersionInfo({ chainId, timeLimit })
+        infos[chainId] = info
+        logLocks(info)
+      } catch (error) {
+        console.log(`Couldn't fetch chain ${chainId}: ${error.message}`)
+      }
+    })
+  )
+}
+
+// execute as standalone
+if (require.main === module) {
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+module.exports = main
