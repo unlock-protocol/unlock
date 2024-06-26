5f55b0957b391810ca81977c39eca3cfd3873563
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-26 17:34:11 -0400

    wip

--- smart-contracts/contracts/utils/Kickback.sol
+++ smart-contracts/contracts/utils/Kickback.sol
@@ -1,9 +1,11 @@
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "../interfaces/IPublicLock.sol";
 
-contract Kickback {
-  mapping(address => mapping(address => uint)) issuedRefunds;
-  mapping(address => bytes32) roots;
+ RefundsApproved(address lockAddress, bytes32 root);
+  event Refunded(address lockAddress, address recipient, uint256 amount);
+
+  mapping(address => mapping(address => uint)) public issuedRefunds;
+  mapping(address => bytes32) public roots;
 
   function approveRefunds(address lockAddress, bytes32 root) public {
     IPublicLock lock = IPublicLock(lockAddress);
@@ -16,6 +18,7 @@ contract Kickback {
       "You must be a lock manager to approve refunds."
     );
     roots[lockAddress] = root;
+    emit RefundsApproved(lockAddress, root);
   }
 
   function refund(
@@ -36,9 +39,11 @@ contract Kickback {
       issuedRefunds[lockAddress][msg.sender] > 0,
       "Refund already issued"
     );
-    issuedRefunds[lockAddress][msg.sender] = amount;
+    
+    issuedRefunds[lockAddresblicLock lock = IPublicLock(lockAddress);
+    
 
-    IPublicLock lock = IPublicLock(lockAddress);
-    lock.withdraw(lock.tokenAddress(), payable(msg.sender), amount);
+    lock.withdraw(lock.tokenAddress(), payable(msg.sender), dress, msg.sender, amount);
+    
   }
 }
--- smart-contracts/test/Kickback/kickback.js
+++ smart-contracts/test/Kickback/kickback.js
@@ -1,22 +1,19 @@
+const { StandardMerkleTree } = require('@openzeppelin/merkle-tree')
 const { ethers, upgrades } = require('hardhat')
 const { assert } = require('chai')
 const {
-  ADDRESS_ZERO,
-  increaseTime,
-  increaseTimeTo,
   reverts,
   deployContracts,
   deployLock,
   purchaseKeys,
 } = require('../helpers')
-const {
-  getEvent,
-  createLockCalldata,
-} = require('@unlock-protocol/hardhat-helpers')
-
-const keyPrice = ethers.parseEther('0.01')
 
 describe('Kickback contract', () => {
+  let kickback
+  let keyOwners
+  let lock
+  let tree
+  let refundAmount
   before(async () => {
     const [deployer] = await ethers.getSigners()
 
@@ -24,174 +21,102 @@ describe('Kickback contract', () => {
     const { unlock } = await deployContracts()
 
     // create a new lock
-    const lock = await deployLock()
-    const { tokenIds, keyOwners } = await purchaseKeys(lock, 5)
+    lock = await deployLock()
+    const result = await purchaseKeys(lock, 5)
+    keyOwners = result.keyOwners
 
-    console.log(tokenIds, keyOwners, await lock.keyPrice())
+    // Deploy upgradable KeyManager
+    const Kickback = await ethers.getContractFactory('Kickback')
+    kickback = await upgrades.deployProxy(Kickback, [])
 
-    // Deploy a lock
-    // Buy a few keys
+    refundAmount = await lock.keyPrice()
+
+    tree = StandardMerkleTree.of(
+      keyOwners.map((recipient) => [recipient, refundAmount.toString()]),
+      ['address', 'uint256']
+    )
   })
 
   describe('approveRefunds', () => {
-    it('should fail if the Kickback contract is not a lock manager', () => {
-      console.log('OKI HERE I GO!')
+    it('should revert if the Kickback contract is not a lock manager', async () => {
+      await reverts(
+        kickback.approveRefunds(lock, tree.root),
+        `VM Exception while processing transaction: reverted with reason string 'Add the Kickback contract as a lock manager first.'`
+      )
     })
-    it('should fail if callers is not a lock manager')
-    it('should store the root of the merkle tree')
+    it('should fail if callers is not a lock manager', async () => {
+      const [, _randomSigner] = await ethers.getSigners()
+
+      await lock.addLockManager(await kickback.getAddress())
+      await reverts(
+        kickback.connect(_randomSigner).approveRefunds(lock, tree.root),
+        `VM Exception while processing transaction: reverted with reason string 'You must be a lock manager to approve refunds.'`
+      )
+    })
+    it('should store the root of the merkle tree', async () => {
+      const lockAddress = (await lock.getAddress()).toLowerCase()
+      await lock.addLockManager(await kickback.getAddress())
+      await kickback.approveRefunds(lockAddress, tree.root)
+      assert.equal(await kickback.roots(lockAddress), tree.root)
+    })
   })
 
   describe('refund', () => {
-    it(
-      'should fail if the proof is invalid because the recipient is not in the list of recipients'
-    )
-    it('should fail if the proof is invalid because the amount is incorrect')
-    it('should fail if the refund was already claimed')
-    it('should withdraw funds for the user')
-    // before(async () => {
-    //   // transfer and delegate anout tokens for voter to reach quorum alone
-    //   await up
-    //     .connect(minter)
-    //     .transfer(await delegater.getAddress(), expectedQuorum + 1n)
-    //   await up.connect(delegater).delegate(await voter.getAddress())
+    let proof
+    beforeEach(async () => {
+      await lock.addLockManager(await kickback.getAddress())
+      await kickback.approveRefunds(lock, tree.root)
+      for (const [i, v] of tree.entries()) {
+        if (v[0] === keyOwners[0]) {
+          proof = tree.getProof(i)
+        }
+      }
+    })
 
-    //   const { timestamp } = await ethers.provider.getBlock()
-    //   await increaseTime()
-
-    //   // make sure voter as enough vote
-    //   assert.equal(
-    //     (await up.getVotes(await voter.getAddress())) >
-    //     (await gov.quorum(timestamp)),
-    //     true
-    //   )
-    // })
-
-    // it('should only be possible through voting', async () => {
-    //   assert.equal(await gov.votingDelay(), votingDelay)
-    //   await reverts(gov.setVotingDelay(2), 'GovernorOnlyExecutor')
-    //   await reverts(gov.updateQuorumNumerator(2), 'GovernorOnlyExecutor')
-    //   await reverts(gov.setVotingPeriod(2), 'GovernorOnlyExecutor')
-    // })
-
-    // describe('Quorum', () => {
-    //   it('should be properly updated through voting', async () => {
-    //     const newQuorumNumerator = 10n
-
-    //     const encoded = gov.interface.encodeFunctionData(
-    //       'updateQuorumNumerator',
-    //       [newQuorumNumerator]
-    //     )
-
-    //     // propose
-    //     const proposal = [
-    //       [await gov.getAddress()],
-    //       ['0'],
-    //       [encoded],
-    //       '<proposal description: update the quorum>',
-    //     ]
-
-    //     const execReceipt = await launchVotingProcess(voter, proposal)
-
-    //     // make sure quorum has been changed succesfully
-    //     const { timestamp } = await ethers.provider.getBlock(
-    //       execReceipt.blockNumber
-    //     )
-
-    //     assert.equal(await gov.quorumNumerator(), newQuorumNumerator)
-    //     assert.equal(
-    //       await gov.quorum(timestamp),
-    //       ((await up.TOTAL_SUPPLY()) * 10n ** 18n * newQuorumNumerator) /
-    //       defaultQuorumDenominator
-    //     )
-
-    //     // make sure event has been fired
-    //     const { args } = await getEvent(execReceipt, 'QuorumNumeratorUpdated')
-
-    //     assert.equal(args.newQuorumNumerator, newQuorumNumerator)
-    //     assert.equal(args.oldQuorumNumerator, defaultQuorumNumerator)
-    //   })
-    // })
-
-    // describe('VotingPeriod', () => {
-    //   it('should be properly updated through voting', async () => {
-    //     const votingPeriod = 10
-
-    //     const encoded = gov.interface.encodeFunctionData('setVotingPeriod', [
-    //       votingPeriod,
-    //     ])
-
-    //     // propose
-    //     const proposal = [
-    //       [await gov.getAddress()],
-    //       ['0'],
-    //       [encoded],
-    //       '<proposal description>',
-    //     ]
-
-    //     const execTx = await launchVotingProcess(voter, proposal)
-
-    //     const changed = await gov.votingPeriod()
-    //     assert.equal(changed == votingPeriod, true)
-
-    //     // make sure event has been fired
-    //     const { args } = await getEvent(execTx, 'VotingPeriodSet')
-    //     const { newVotingPeriod } = args
-    //     assert.equal(newVotingPeriod == votingPeriod, true)
-    //   })
-    // })
-
-    // describe('VotingDelay', () => {
-    //   it('should be properly updated through voting', async () => {
-    //     const votingDelay = 10000n
-    //     const encoded = gov.interface.encodeFunctionData('setVotingDelay', [
-    //       votingDelay,
-    //     ])
-
-    //     const proposal = [
-    //       [await gov.getAddress()],
-    //       ['0'],
-    //       [encoded],
-    //       '<proposal description>',
-    //     ]
-
-    //     const execReceipt = await launchVotingProcess(voter, proposal)
-    //     const changed = await gov.votingDelay()
-    //     assert.equal(changed == votingDelay, true)
-
-    //     // make sure event has been fired
-    //     const { args } = await getEvent(execReceipt, 'VotingDelaySet')
-    //     const { newVotingDelay } = args
-    //     assert.equal(newVotingDelay, votingDelay)
-    //   })
-    // })
-
-    // afterEach(async () => {
-    //   // reset to original state after tests
-    //   const { timestamp } = await ethers.provider.getBlock()
-    //   await increaseTime()
-    //   const quorum = await gov.quorum(timestamp)
-
-    //   // transfer and delegate anout tokens for voter to reach quorum alone
-    //   if (quorum > expectedQuorum) {
-    //     await up
-    //       .connect(minter)
-    //       .transfer(await resetter.getAddress(), quorum + 1n)
-    //     await up.connect(resetter).delegate(await resetter.getAddress())
-    //     await increaseTime()
-
-    //     // reset quorum through proposal
-    //     const proposal = [
-    //       [await gov.getAddress()],
-    //       ['0'],
-    //       [
-    //         gov.interface.encodeFunctionData('updateQuorumNumerator', [
-    //           defaultQuorumNumerator,
-    //         ]),
-    //       ],
-    //       `<proposal description: reset quorum ${Math.random()}>`,
-    //     ]
-    //     await launchVotingProcess(resetter, proposal)
-    //   }
-    // })
+    it('should fail if the proof is invalid because the recipient is not in the list of recipients', async () => {
+      const [, , , , , , , _randomSigner] = await ethers.getSigners()
+      assert(keyOwners.indexOf(_randomSigner.address) === -1)
+      await reverts(
+        kickback.connect(_randomSigner).refund(lock, proof, refundAmount),
+        "VM Exception while processing transaction: reverted with reason string 'Invalid proof'"
+      )
+    })
+    it('should fail if the proof is invalid because the amount is incorrect', async () => {
+      const [refundedUser] = await ethers.getSigners()
+      await reverts(
+        kickback.connect(refundedUser).refund(lock, proof, refundAmount * 2n),
+        "VM Exception while processing transaction: reverted with reason string 'Invalid proof'"
+      )
+    })
+    it('should fail if the refund was already claimed', async () => {
+      const [, refundedUser] = await ethers.getSigners()
+      let proof
+      const refundedUserAddress = await refundedUser.getAddress()
+      for (const [i, v] of tree.entries()) {
+        if (v[0] === refundedUserAddress) {
+          proof = tree.getProof(i)
+        }
+      }
+      await kickback.connect(refundedUser).refund(lock, proof, refundAmount)
+      await reverts(
+        kickback.connect(refundedUser).refund(lock, proof, refundAmount),
+        "VM Exception while processing transaction: reverted with reason string 'Refund already issued'"
+      )
+    })
+    it('should withdraw funds for the user', async () => {
+      const [, , refundedUser] = await ethers.getSigners()
+      let proof
+      const refundedUserAddress = await refundedUser.getAddress()
+      for (const [i, v] of tree.entries()) {
+        if (v[0] === refundedUserAddress) {
+          proof = tree.getProof(i)
+        }
+      }
+      const balanceBefore =
+        await ethers.provider.getBalance(refundedUserAddress)
+      await kickback.connect(refundedUser).refund(lock, proof, refundAmount)
+      const balanceAfter = await ethers.provider.getBalance(refundedUserAddress)
+      assert(balanceBefore < balanceAfter)
+    })
   })
 })
