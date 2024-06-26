39a3e46a6338c24598ec71447036349c7d398be9
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-26 17:51:54 -0400

    fix(unlock-app): wait for address before letting user click on the button to ma manage the lock

--- unlock-app/src/components/interface/locks/Create/elements/CreateLockFormSummary.tsx
+++ unlock-app/src/components/interface/locks/Create/elements/CreateLockFormSummary.tsx
@@ -131,7 +131,7 @@ export const CreateLockFormSummary = ({
 
   const currentStatus: DeployStatus = hasError
     ? 'txError'
-    : isDeployed
+    : isDeployed && !!lockAddress
       ? 'deployed'
       : 'progress'
 
