765529631e3f9c5fe42cc17b7e40aedb5a07af46
Julien Genestoux <julien.genestoux@gmail.com>
2024-06-28 02:53:35 -0400

    feat(unlock-app): explicit loading (#14122)
    * adding a loading indication to clarify that this is where RSVP will happen
    
    * moved on top

--- unlock-app/src/components/content/event/Registration/LoadingRegistrationCard.tsx
+++ unlock-app/src/components/content/event/Registration/LoadingRegistrationCard.tsx
@@ -1,8 +1,13 @@
 import { Placeholder } from '@unlock-protocol/ui'
+import LoadingIcon from '~/components/interface/Loading'
 
 export const LoadingRegistrationCard = () => {
   return (
     <>
+      <div className="flex flex-row align-middle gap-2">
+        <LoadingIcon size={24} />
+        <p className="text-gray-600">Loading ticket options...</p>
+      </div>
       <Placeholder.Root>
         <Placeholder.Line width="sm" />
         <Placeholder.Line width="sm" />
