6444df34c6d9ac8ef79462f5c2dbc0391b8625f5
Julien Genestoux <julien.genestoux@gmail.com>
2024-07-09 18:59:36 -0400

    fix(unlock-app): merged conflicts

--- unlock-app/src/components/interface/checkout/main/Select.tsx
+++ unlock-app/src/components/interface/checkout/main/Select.tsx
@@ -280,16 +280,16 @@ export function Select({ checkoutService }: Props) {
       return
     }
 
-    // checkoutService.send({
-    //   type: 'CONNECT',
-    //   lock,
-    //   existingMember: autoSelectedLock.isMember,
-    //   expiredMember: autoSelectedLock.isExpired,
-    //   skipQuantity,
-    //   skipRecipient,
-    //   recipients: account ? [account] : [],
-    //   hook: hookType,
-    // })
+    checkoutService.send({
+      type: 'CONNECT',
+      lock,
+      existingMember: autoSelectedLock.isMember,
+      expiredMember: autoSelectedLock.isExpired,
+      skipQuantity,
+      skipRecipient,
+      recipients: account ? [account] : [],
+      hook: hookType,
+    })
   }, [autoSelectedLock])
 
   const locksGroupedByNetwork = useMemo(
@@ -438,15 +438,6 @@ export function Select({ checkoutService }: Props) {
       await signIn()
       return
     }
-
-    // const url = new URL(
-    //   `${window.location.protocol}//${window.location.host}${router.asPath}`
-    // )
-    // const params = new URLSearchParams(url.search)
-    // params.set('lock', encodeURIComponent(lock.address))
-    // url.search = params.toString()
-
-    // router.push(url, undefined, { shallow: true })
 
     checkoutService.send({
       type: 'CONNECT',
