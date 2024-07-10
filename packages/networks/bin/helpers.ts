bc0353fa38254e20707ba3d10ee6361a18a365fb
Clément Renaud <clement+git@clementrenaud.com>
2024-07-10 13:40:29 +0200

    use impl address

--- packages/networks/bin/helpers.ts
+++ packages/networks/bin/helpers.ts
@@ -66,9 +66,20 @@ export const validateBytecode = async ({
   contractAddress,
   providerURL,
   contractName = 'UnlockV13',
+  isProxy = true,
 }) => {
-  const { bytecode } = contracts[contractName]
   const provider = new ethers.JsonRpcProvider(providerURL)
+  // find impl address
+  if (isProxy) {
+    const hex = await provider.getStorage(
+      contractAddress,
+      '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+    )
+    const implAddress = ethers.stripZerosLeft(hex)
+    contractAddress = implAddress
+  }
+  const { bytecode } = contracts[contractName]
+
   const deployedByteCode = await provider.getCode(contractAddress)
   return deployedByteCode === bytecode
 }
