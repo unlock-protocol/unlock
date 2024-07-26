6224dc72734418bcc5521db7050bb19571a0caf8
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-07-26 12:28:14 +0300

    Update DefaultLayoutSkeleton.tsx

--- unlock-app/src/components/content/event/Settings/DefaultLayoutSkeleton.tsx
+++ unlock-app/src/components/content/event/Settings/DefaultLayoutSkeleton.tsx
@@ -17,8 +17,8 @@ export const DefaultLayoutSkeleton = ({
         <div className="h-full">
           <div className="h-full relative">
             <div className="h-full w-full sm:w-64 bg-slate-200 p-4">
-              <div className="h-1/3 bg-slate-300 rounded-lg"></div>
-              <div className="w-full h-2/3 flex mt-2">
+              <div className="h-1/3 min-h-10 bg-slate-300 rounded-lg"></div>
+              <div className="w-full h-3/5 flex mt-2">
                 <div className="w-2/3 h-2/3">
                   <div className="w-1/3 h-1/3 mb-8">
                     <div className="aspect-h-1 aspect-w-1 bg-slate-400 rounded-lg"></div>
@@ -26,7 +26,7 @@ export const DefaultLayoutSkeleton = ({
                   <div className="h-4 bg-slate-300 rounded w-3/4 mb-2"></div>
                   <div className="h-4 bg-slate-300 rounded w-1/2"></div>
                 </div>
-                <div className="w-1/3 h-full bg-slate-300 rounded-lg"></div>
+                <div className="w-1/3 bg-slate-300 rounded-lg"></div>
               </div>
             </div>
           </div>
