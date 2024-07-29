1dc91aaf8711c0921e18afe6f3eefd500b456594
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-07-29 15:51:30 +0300

    feat(unlock-app): Event layout setting (#14313)
    * Event layout option
    
    * Update General.tsx
    
    * Update General.tsx
    
    * Layout
    
    * Update BannerlessLayoutSkeleton.tsx
    
    * Update DefaultLayoutSkeleton.tsx
    
    * Layout select
    
    * Update BannerlessLayoutSkeleton.tsx

---
+++ unlock-app/src/components/content/event/Settings/DefaultLayoutSkeleton.tsx
@@ -0,0 +1,37 @@
+interface DefaultLayoutSkeletonProps {
+  selectedLayout: string
+  handleSelect: () => void
+}
+
+export const DefaultLayoutSkeleton = ({
+  selectedLayout,
+  handleSelect,
+}: DefaultLayoutSkeletonProps) => {
+  return (
+    <div className="flex flex-col">
+      <p>Default</p>
+      <div
+        className={`h-full cursor-pointer border-4 border-ui-main-500 rounded-lg overflow-hidden ${selectedLayout === 'default' ? '' : 'border-transparent'}`}
+        onClick={() => handleSelect()}
+      >
+        <div className="h-full">
+          <div className="h-full relative">
+            <div className="h-full w-full sm:w-64 bg-slate-200 p-4">
+              <div className="h-1/3 min-h-10 bg-slate-300 rounded-lg"></div>
+              <div className="w-full h-3/5 flex mt-2">
+                <div className="w-2/3 h-2/3">
+                  <div className="w-1/3 h-1/3 mb-8">
+                    <div className="aspect-h-1 aspect-w-1 bg-slate-400 rounded-lg"></div>
+                  </div>
+                  <div className="h-4 bg-slate-300 rounded w-3/4 mb-2"></div>
+                  <div className="h-4 bg-slate-300 rounded w-1/2"></div>
+                </div>
+                <div className="w-1/3 bg-slate-300 rounded-lg"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
