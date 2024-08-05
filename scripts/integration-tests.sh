acae9fbf2203188dd4b417b056f99567922ec68c
Viacheslav <surzhenkoviacheslav@gmail.com>
2024-08-05 10:30:08 +0300

    Update integration-tests.sh

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -7,6 +7,7 @@ source ./scripts/start/envs.sh
 # run Unlock Protocol stack
 sh -c "./scripts/start-infra.sh"
 
+#
 # run the actual tests
 echo "Running integration tests \n"
 CI=true yarn workspace tests ci --network localhost
