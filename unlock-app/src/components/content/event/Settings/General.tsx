71c910b4de175503466f1ec56f832e6af8177bd9
Julien Genestoux <julien.genestoux@gmail.com>
2024-08-06 10:38:52 +0200

    Manual deploy as of commit:7245034c3b2a9c05a67561a0b245ef89e2547c52

--- unlock-app/src/components/content/event/Settings/General.tsx
+++ unlock-app/src/components/content/event/Settings/General.tsx
@@ -22,6 +22,8 @@ import { useState } from 'react'
 import { config } from '~/config/app'
 import dayjs from 'dayjs'
 import { GoogleMapsAutoComplete } from '../Form'
+import { DefaultLayoutSkeleton } from './DefaultLayoutSkeleton'
+import { BannerlessLayoutSkeleton } from './BannerlessLayoutSkeleton'
 
 interface GeneralProps {
   event: Event
@@ -50,6 +52,7 @@ export const General = ({ event, checkoutConfig }: GeneralProps) => {
       description: event.description,
       image: event.image,
       ticket: event.ticket,
+      layout: event.layout,
     },
   })
   const [mapAddress, setMapAddress] = useState(
@@ -69,10 +72,19 @@ export const General = ({ event, checkoutConfig }: GeneralProps) => {
     'YYYY-MM-DD'
   )
 
+  const [selectedLayout, setSelectedLayout] = useState(
+    getValues('layout') || 'default'
+  )
+
+  const handleSelect = (layout: any) => {
+    setSelectedLayout(layout)
+  }
+
   const save = async (values: {
     name: string
     description: string
     image: string
+    layout: string
   }) => {
     await ToastHelper.promise(
       locksmith.saveEventData({
@@ -95,7 +107,7 @@ export const General = ({ event, checkoutConfig }: GeneralProps) => {
   return (
     <form className="grid grid-cols-1 gap-6" onSubmit={handleSubmit(save)}>
       <SettingCard
-        label="Name, description, and image"
+        label="Name, description, layout and image"
         description="Change the name description and image for your event!"
       >
         <div className="flex gap-4 flex-col md:flex-row">
@@ -164,6 +176,47 @@ export const General = ({ event, checkoutConfig }: GeneralProps) => {
         </div>
         <div className="flex flex-end w-full pt-8 flex-row-reverse">
           <Button loading={isSubmitting} type="submit" className="w-48">
+            Save
+          </Button>
+        </div>
+      </SettingCard>
+
+      <SettingCard
+        label="Layout"
+        description="Update the layout of your event."
+      >
+        <div>
+          <Controller
+            name="layout"
+            control={control}
+            render={({ field: { onChange } }) => {
+              return (
+                <div className="flex flex-col sm:flex-row justify-around gap-8 mx-4 sm:mx-8 my-4 h-auto sm:h-64">
+                  <DefaultLayoutSkeleton
+                    selectedLayout={selectedLayout}
+                    handleSelect={() => {
+                      onChange('default')
+                      handleSelect('default')
+                    }}
+                  />
+                  <BannerlessLayoutSkeleton
+                    selectedLayout={selectedLayout}
+                    handleSelect={() => {
+                      onChange('bannerless')
+                      handleSelect('bannerless')
+                    }}
+                  />
+                </div>
+              )
+            }}
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row-reverse w-full pt-8">
+          <Button
+            loading={isSubmitting}
+            type="submit"
+            className="w-full sm:w-48"
+          >
             Save
           </Button>
         </div>
