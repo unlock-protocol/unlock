ece07c16bb8268c3873e433ff425ef3efcbb723e
Clément Renaud <clement+git@clementrenaud.com>
2024-08-19 14:38:54 +0200

    add missing udt solc

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -33,6 +33,7 @@ const settings = {
 module.exports = {
   solidity: {
     compilers: [
+      { version: '0.5.17', settings },
       { version: '0.7.6', settings },
       { version: '0.8.0', settings },
       { version: '0.8.2', settings },
