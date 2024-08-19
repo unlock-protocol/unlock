7c4a7f7163dae570ea0afaeda9af259a6af19756
Clément Renaud <clement+git@clementrenaud.com>
2024-08-19 15:27:59 +0200

    unlockjs: remove unlock config > v10

--- packages/unlock-js/src/__tests__/helpers/configureUnlock.js
+++ packages/unlock-js/src/__tests__/helpers/configureUnlock.js
@@ -31,20 +31,10 @@ export default async (
 
 function getConfigure(version) {
   switch (version) {
-    case 'v4':
-      return configureUnlockV4
-    case 'v6':
-      return configureUnlockV6
-    case 'v7':
-      return configureUnlockV7
-    case 'v8':
-      return configureUnlockV8
-    case 'v9':
-      return configureUnlockV9
     case 'v10':
-      return configureUnlockV9
+      return configureUnlockV10
     case 'v11':
-      return configureUnlockV9
+      return configureUnlockV10
     case 'v12':
       return configureUnlockV12
     default:
@@ -53,143 +43,13 @@ function getConfigure(version) {
 }
 
 /**
- * Configures unlock version v4
+ * Configures unlock version v10
  * @param {PropTypes.lock} publicLockTemplateAddress
  * @param {PropTypes.string} globalTokenSymbol
  * @param {PropTypes.string} globalBaseTokenURI
  * @param {function} callback invoked with the transaction hash
  */
-async function configureUnlockV4(
-  unlockContract,
-  { publicLockTemplateAddress, globalTokenSymbol, globalBaseTokenURI },
-  transactionOptions = {},
-  callback
-) {
-  const { ethers } = hre
-  const transaction = await unlockContract.configUnlock(
-    publicLockTemplateAddress,
-    globalTokenSymbol,
-    globalBaseTokenURI,
-    transactionOptions
-  )
-
-  if (callback) {
-    callback(null, transaction.hash)
-  }
-
-  return ethers.provider.waitForTransaction(transaction.hash)
-}
-
-/**
- * Configures unlock version v6
- * @param {PropTypes.lock} publicLockTemplateAddress
- * @param {PropTypes.string} globalTokenSymbol
- * @param {PropTypes.string} globalBaseTokenURI
- * @param {function} callback invoked with the transaction hash
- */
-async function configureUnlockV6(
-  unlockContract,
-  { publicLockTemplateAddress, globalTokenSymbol, globalBaseTokenURI },
-  transactionOptions = {},
-  callback
-) {
-  const { ethers } = hre
-  const transaction = await unlockContract.configUnlock(
-    publicLockTemplateAddress,
-    globalTokenSymbol,
-    globalBaseTokenURI
-  )
-
-  if (callback) {
-    callback(null, transaction.hash)
-  }
-
-  return ethers.provider.waitForTransaction(transaction.hash)
-}
-
-/**
- * Configures unlock version v7
- * @param {PropTypes.lock} publicLockTemplateAddress
- * @param {PropTypes.string} globalTokenSymbol
- * @param {PropTypes.string} globalBaseTokenURI
- * @param {function} callback invoked with the transaction hash
- */
-async function configureUnlockV7(
-  unlockContract,
-  { publicLockTemplateAddress, globalTokenSymbol, globalBaseTokenURI },
-  transactionOptions = {},
-  callback
-) {
-  const { ethers } = hre
-  const configTransaction = await unlockContract.configUnlock(
-    globalTokenSymbol,
-    globalBaseTokenURI
-  )
-  if (callback) {
-    callback(null, configTransaction.hash)
-  }
-  ethers.provider.waitForTransaction(configTransaction.hash)
-
-  const deployTemplateTransaction = await unlockContract.setLockTemplate(
-    publicLockTemplateAddress
-  )
-
-  if (callback) {
-    callback(null, deployTemplateTransaction.hash)
-  }
-  return ethers.provider.waitForTransaction(deployTemplateTransaction.hash)
-}
-/**
- * Configures unlock version v8
- * @param {PropTypes.lock} publicLockTemplateAddress
- * @param {PropTypes.string} globalTokenSymbol
- * @param {PropTypes.string} globalBaseTokenURI
- * @param {function} callback invoked with the transaction hash
- */
-async function configureUnlockV8(
-  unlockContract,
-  {
-    publicLockTemplateAddress,
-    globalTokenSymbol,
-    globalBaseTokenURI,
-    unlockDiscountToken,
-    wrappedEth,
-    estimatedGasForPurchase,
-  },
-  transactionOptions = {},
-  callback
-) {
-  const { ethers } = hre
-  const configTransaction = await unlockContract.configUnlock(
-    unlockDiscountToken,
-    wrappedEth,
-    estimatedGasForPurchase,
-    globalTokenSymbol,
-    globalBaseTokenURI
-  )
-  if (callback) {
-    callback(null, configTransaction.hash)
-  }
-  ethers.provider.waitForTransaction(configTransaction.hash)
-
-  const deployTemplateTransaction = await unlockContract.setLockTemplate(
-    publicLockTemplateAddress
-  )
-
-  if (callback) {
-    callback(null, deployTemplateTransaction.hash)
-  }
-  return ethers.provider.waitForTransaction(deployTemplateTransaction.hash)
-}
-
-/**
- * Configures unlock version v9
- * @param {PropTypes.lock} publicLockTemplateAddress
- * @param {PropTypes.string} globalTokenSymbol
- * @param {PropTypes.string} globalBaseTokenURI
- * @param {function} callback invoked with the transaction hash
- */
-async function configureUnlockV9(
+async function configureUnlockV10(
   unlockContract,
   {
     publicLockTemplateAddress,
