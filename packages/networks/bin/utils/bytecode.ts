b00ab4d01c483d7a6ddbad18d7438e1e0c19749f
Clément Renaud <clement+git@clementrenaud.com>
2024-08-21 14:20:05 +0200

    use deployTx

--- packages/networks/bin/utils/bytecode.ts
+++ packages/networks/bin/utils/bytecode.ts
@@ -21,23 +21,25 @@ export const validateBytecode = async ({
     contractAddress = implAddress
   }
 
-  console.log(contractAddress)
+  const { abi, deployedBytecode, bytecode } = contracts[contractName]
+  const ContractFactory = new ethers.ContractFactory(abi, bytecode)
+  const deployTx = await ContractFactory.getDeployTransaction()
+  console.log({ chainId, contractAddress, contractName })
   // get the data used in the creation tx to match the bytecode
   // we can get that tx id using etherscan
-  console.log(Object.keys(contracts[contractName]))
-  const { deployedBytecode, bytecode } = contracts[contractName]
   const { bytecode: creationBytecode } = await getCreationTx({
     chainId,
     contractAddress,
   })
 
   // TODO: check that the bytecodes match accross networks
-  const getCode = await provider.getCode(contractAddress)
+  // const getCode = await provider.getCode(contractAddress)
 
   // TODO: remove written files used for debug
-  await fs.writeFile('bytecode .txt', bytecode)
-  await fs.writeFile('deployedBytecode.txt', deployedBytecode)
-  await fs.writeFile('creationBytecode .txt', creationBytecode)
-  await fs.writeFile('getCode.txt', getCode)
-  return getCode === bytecode
+  // await fs.writeFile('bytecode .txt', bytecode)
+  // await fs.writeFile('deployedBytecode.txt', deployedBytecode)
+  await fs.writeFile(`creationBytecode-${chainId}.txt`, creationBytecode)
+  await fs.writeFile(`deployTx-${chainId}.txt`, deployTx.data)
+  // await fs.writeFile('getCode.txt', getCode)
+  return deployTx.data === creationBytecode
 }
