c4f31a6dd2686d5e155dd135bed96b5a140880f4
Clément Renaud <clement+git@clementrenaud.com>
2024-08-23 14:09:40 +0200

    unlockjs remove versions

--- packages/unlock-js/src/PublicLock/v4/totalKeys.js
+++
@@ -1,9 +0,0 @@
-/**
- * Returns the number of keys in owner's account.
- * @param {string} lockAddres address of the lock
- * @param {string} owner address of the user
- */
-export default async function (lockAddres, owner, provider) {
-  const lockContract = await this.getLockContract(lockAddres, provider)
-  return lockContract.balanceOf(owner)
-}
