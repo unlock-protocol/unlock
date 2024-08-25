a6500e423878701336bbeaa49899140b08f836b2
= <macharian77@gmail.com>
2024-08-26 01:41:40 +0300

    get price

--- unlock-app/app/frames/checkout/route.tsx
+++ unlock-app/app/frames/checkout/route.tsx
@@ -1,11 +1,19 @@
+import { ImageResponse } from '@vercel/og'
 import { locksmith } from '../../../src/config/locksmith'
 import { frames } from './frames'
 import { Button } from 'frames.js/next'
+import { FrameButton, FrameImage, FrameContainer, FrameInput, getPreviousFrame} from "frames.js/next/server"
 import React from 'react'
+import networks from '@unlock-protocol/networks'
+import { Web3Service } from '@unlock-protocol/unlock-js'
+
+const myadd = '0xA331802a9668CD744e603B8Cd901fa96c1800bB2'
 
 const handler = frames(async (ctx) => {
   const id = ctx.searchParams.id
   const state = ctx.state
+  const add = ctx.message?.connectedAddress
+  console.log('\nadd=', add, myadd)
 
   async function getInitialFrame() {
     const config = await fetch(
@@ -19,24 +27,63 @@ const handler = frames(async (ctx) => {
     const { data } = await locksmith.lockMetadata(network, lockAddress)
     const { image, description } = data
 
-    //TODO: get price
+    const img = <div>hey</div>
+
+    const web3Service = new Web3Service(networks)
+    const mydata = '0x'
+    // let keyPrice = await web3Service.purchasePriceFor({
+    //   lockAddress,
+    //   userAddress: myadd,
+    //   network,
+    //   data: mydata,
+    //   referrer: myadd,
+    // })
+    // keyPrice = keyPrice.toString()
+    // console.log('price=', keyPrice, Number(keyPrice))
+  
 
     const lock = { name, address: lockAddress, network, image }
     state.lock = lock
 
+
     return {
-      image: image,
+      image: <p>hey</p>,
+      imageOptions: {
+        dynamic: true,
+        headers: {
+          // make sure this is always equal or great than minimumCacheTTL when using Next.js Image component
+          // @see https://nextjs.org/docs/app/api-reference/components/image#minimumcachettl
+          "Cache-Control": "max-age=1",
+        },
+      },
       buttons: [
         <Button
           key="i"
           action="post"
           target={{ pathname: `/frames/checkout`, query: { step: 'purchase' } }}
         >
-          To purchase page
+          {name}
         </Button>,
       ],
       state,
     }
+    // const previousFrame = getPreviousFrame(ctx.searchParams)
+
+
+    // return <FrameContainer
+    //     postUrl="/frames"
+    //     state={state}
+    //     previousFrame={previousFrame}
+    //   >
+    //     <FrameImage>
+    //     <div tw="flex bg-[#F5F5F5] h-full">
+    //       <p tw="px-8 h-full w-full">
+    //         this is an img
+    //       </p>
+    //     </div>
+    //     </FrameImage>
+    //     <FrameInput text="put some text here" />
+    //   </FrameContainer>
   }
 
   return getInitialFrame()
--- unlock-app/app/frames/checkout/txdata/route.ts
+++ unlock-app/app/frames/checkout/txdata/route.ts
@@ -2,18 +2,42 @@ import { Abi, encodeFunctionData } from 'viem'
 import { frames } from '../frames'
 import { transaction } from 'frames.js/core'
 import { PublicLockV14 } from '@unlock-protocol/contracts'
+import { Web3Service } from '@unlock-protocol/unlock-js'
+import networks from '@unlock-protocol/networks'
 
+const checkouturl = 'https://app.unlock-protocol.com/checkout?id=fe4092ae-8cee-4563-963b-5703f077bf60'//frametest2
 const abi = PublicLockV14.abi
 const chain = 11155111
-const lockAddress = '0x9B03Ab44C945114CFDc0A111E07f7A11600252eF'
-const value = 1e5
+const lockAddress = '0x6dd71ec4a3d85cbe1f0d026a4e720ff5fcbdf8b2' //frametest2
+// '0x9B03Ab44C945114CFDc0A111E07f7A11600252eF'
+const value = 2_000_000_000_000_000
 
 export const POST = frames(async (ctx) => {
   if (!ctx?.message) {
     throw new Error('Invalid frame message')
   }
 
-  const userAddress = ctx.message.connectedAddress
+  const userAddress = ctx.message.connectedAddress!
+  const network = Number(ctx.state.lock!.network)
+  const lockAddress = ctx.state.lock!.address
+
+  async function getKeyPrice() {
+    const web3Service = new Web3Service(networks)
+    const mydata = '0x'
+    let price = await web3Service.purchasePriceFor({
+      lockAddress,
+      userAddress: userAddress,
+      network,
+      data: mydata,
+      referrer: userAddress,
+    })
+    price = price.toString()
+    return price
+  }
+
+  const keyPrice = await getKeyPrice()
+  
+  console.log('price\n', keyPrice, 'add', lockAddress, network)
 
   // PARAMS
   // function purchase(
@@ -27,7 +51,7 @@ export const POST = frames(async (ctx) => {
   const calldata = encodeFunctionData({
     abi,
     functionName: 'purchase',
-    args: [[value], [userAddress], [userAddress], [userAddress], ['0x']],
+    args: [[keyPrice], [userAddress], [userAddress], [userAddress], ['0x']],
   })
 
   return transaction({
@@ -37,7 +61,7 @@ export const POST = frames(async (ctx) => {
       abi: abi as Abi,
       to: lockAddress,
       data: calldata,
-      // value: value.toString(),
+      value: keyPrice.toString(),
     },
   })
 })
