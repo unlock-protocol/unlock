4e65ef8d4759e6a3637876b78e8bdeedd7925c45
Clément Renaud <clement@unlock-protocol.com>
2024-08-26 16:56:20 +0200

    chore(contracts): deprecate Unlock and PublicLock before v4 (#14455)
    * remove contracts files
    
    * cleanup unlock js
    
    * fix contracts test
    
    * remove unlock up to v4

--- packages/contracts/src/contracts/PublicLock/PublicLockV2.sol
+++
@@ -1,2 +0,0 @@
-// Error HH603: Hardhat flatten doesn't support cyclic dependencies.
-// For more info go to https://hardhat.org/HH603 or run Hardhat with --show-stack-traces
--- packages/unlock-js/scripts/compressAbi.js
+++ packages/unlock-js/scripts/compressAbi.js
@@ -11,19 +11,8 @@ const fs = require('fs')
 const path = require('path')
 const abis = require('@unlock-protocol/contracts')
 
-const unlockVersions = [
-  'v4',
-  'v6',
-  'v7',
-  'v8',
-  'v9',
-  'v10',
-  'v11',
-  'v12',
-  'v13',
-]
+const unlockVersions = ['v6', 'v7', 'v8', 'v9', 'v10', 'v11', 'v12', 'v13']
 const publicLockVersions = [
-  'v4',
   'v6',
   'v7',
   'v8',
