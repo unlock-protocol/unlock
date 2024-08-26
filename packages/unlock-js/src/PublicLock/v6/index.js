e02aea99f53e6f3d61c3bd8a607c690649518902
Clément Renaud <clement@unlock-protocol.com>
2024-08-26 22:00:49 +0200

    chore(unlock-js): remove v4 functions from the lib (#14460)

--- packages/unlock-js/src/PublicLock/v6/index.js
+++ packages/unlock-js/src/PublicLock/v6/index.js
@@ -14,17 +14,13 @@ import addLockManager from './addLockManager'
 import renounceLockManager from './renounceLockManager'
 import updateTransferFee from './updateTransferFee'
 
-import v4 from '../v4'
-
-const {
-  getTokenIdForOwner,
-  getKeyExpirationByLockForOwner,
-  totalKeys,
-  setBaseTokenURI,
-  updateLockName,
-  updateLockSymbol,
-  updateRefundPenalty,
-} = v4
+import getTokenIdForOwner from './getTokenIdForOwner'
+import getKeyExpirationByLockForOwner from './getKeyExpirationByLockForOwner'
+import totalKeys from './totalKeys'
+import setBaseTokenURI from './setBaseTokenURI'
+import updateLockName from './updateLockName'
+import updateLockSymbol from './updateLockSymbol'
+import updateRefundPenalty from './updateRefundPenalty'
 
 export default {
   version: 'v6',
