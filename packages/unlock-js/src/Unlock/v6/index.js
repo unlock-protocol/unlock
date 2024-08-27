8959fb56507dfdc4d594d0e35a1f2ed02941b289
Clément Renaud <clement@unlock-protocol.com>
2024-08-27 16:29:51 +0200

    chore(contracts): deprecate Unlock prior to v11 (#14458)
    * remove unlock prior to v11
    
    * remove ABIs
    
    * fix tests
    
    * fix contracts tests again
    
    * remove support from unlock-js

--- packages/unlock-js/src/Unlock/v6/index.js
+++
@@ -1,9 +0,0 @@
-import abis from '../../abis'
-import createLock from './createLock'
-
-export default {
-  createLock,
-  version: 'v6',
-  Unlock: abis.Unlock.v6,
-  PublicLock: abis.PublicLock.v6,
-}
