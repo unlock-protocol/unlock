c2071f7e9e1d2fc2872dcd4d8df9f93fa795c0ad
Clément Renaud <clement@unlock-protocol.com>
2024-08-27 23:28:40 +0200

    chore(contracts): deprec PublicLock v6 (#14462)

--- packages/unlock-js/src/PublicLock/v7/index.js
+++ packages/unlock-js/src/PublicLock/v7/index.js
@@ -1,67 +1,63 @@
 import abis from '../../abis'
+import addKeyGranter from './addKeyGranter'
+import addLockManager from './addLockManager'
+import cancelAndRefund from './cancelAndRefund'
+import expireAndRefundFor from './expireAndRefundFor'
+import getCancelAndRefundValueFor from './getCancelAndRefundValueFor'
+import getKeyExpirationByLockForOwner from './getKeyExpirationByLockForOwner'
+import getLock from './getLock'
+import getTokenIdForOwner from './getTokenIdForOwner'
 import grantKey from './grantKey'
 import grantKeys from './grantKeys'
-import isLockManager from './isLockManager'
+import initializeTemplate from './initializeTemplate'
 import isKeyGranter from './isKeyGranter'
-import addKeyGranter from './addKeyGranter'
-import expireAndRefundFor from './expireAndRefundFor'
-import cancelAndRefund from './cancelAndRefund'
+import isLockManager from './isLockManager'
 import keyManagerOf from './keyManagerOf'
-import getCancelAndRefundValueFor from './getCancelAndRefundValueFor'
+import purchaseKey from './purchaseKey'
+import purchaseKeys from './purchaseKeys'
+import renounceLockManager from './renounceLockManager'
+import setBaseTokenURI from './setBaseTokenURI'
 import setEventHooks from './setEventHooks'
 import setKeyManagerOf from './setKeyManagerOf'
-import v6 from '../v6'
-
-const {
-  shareKey,
-  initializeTemplate,
-  withdrawFromLock,
-  updateKeyPrice,
-  purchaseKey,
-  purchaseKeys,
-  getLock,
-  getTokenIdForOwner,
-  getKeyExpirationByLockForOwner,
-  totalKeys,
-  updateLockName,
-  updateLockSymbol,
-  setBaseTokenURI,
-  addLockManager,
-  renounceLockManager,
-  updateRefundPenalty,
-  updateTransferFee,
-} = v6
+import shareKey from './shareKey'
+import totalKeys from './totalKeys'
+import updateKeyPrice from './updateKeyPrice'
+import updateLockName from './updateLockName'
+import updateLockSymbol from './updateLockSymbol'
+import updateRefundPenalty from './updateRefundPenalty'
+import updateTransferFee from './updateTransferFee'
+import withdrawFromLock from './withdrawFromLock'
 
 export default {
   version: 'v7',
   Unlock: abis.Unlock.v7,
   PublicLock: abis.PublicLock.v7,
-  initializeTemplate,
-  updateKeyPrice,
-  purchaseKey,
-  purchaseKeys,
+  addKeyGranter,
+  addLockManager,
+  cancelAndRefund,
+  expireAndRefundFor,
+  getCancelAndRefundValueFor,
+  getKeyExpirationByLockForOwner,
   getLock,
+  getTokenIdForOwner,
   grantKey,
   grantKeys,
-  withdrawFromLock,
-  isLockManager,
+  initializeTemplate,
   isKeyGranter,
-  addKeyGranter,
-  expireAndRefundFor,
-  cancelAndRefund,
-  shareKey,
+  isLockManager,
   keyManagerOf,
-  getCancelAndRefundValueFor,
-  getTokenIdForOwner,
-  getKeyExpirationByLockForOwner,
+  purchaseKey,
+  purchaseKeys,
+  renounceLockManager,
+  setBaseTokenURI,
+  setEventHooks,
+  setKeyManagerOf,
+  shareKey,
   totalKeys,
+  updateKeyPrice,
   updateLockName,
   updateLockSymbol,
-  setBaseTokenURI,
-  addLockManager,
-  renounceLockManager,
   updateRefundPenalty,
-  setEventHooks,
   updateTransferFee,
-  setKeyManagerOf,
+  withdrawFromLock,
 }
