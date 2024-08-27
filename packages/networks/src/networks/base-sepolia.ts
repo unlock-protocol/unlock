33058ef849e76d0b3dc10e0730de19e95a567822
Julien Genestoux <julien.genestoux@gmail.com>
2024-08-27 17:37:03 -0400

    replaced subgraph addresses

--- packages/networks/src/networks/base-sepolia.ts
+++ packages/networks/src/networks/base-sepolia.ts
@@ -73,8 +73,7 @@ export const baseSepolia: NetworkConfig = {
   publicProvider: 'https://sepolia.base.org',
   startBlock: 7889118,
   subgraph: {
-    endpoint:
-      'https://api.studio.thegraph.com/query/65299/unlock-protocol-base-sepolia/version/latest',
+    endpoint: 'https://subgraph.unlock-protocol.com/84532',
     networkName: 'base-sepolia',
     studioName: 'unlock-protocol-base-sepolia',
   },
