728030585495248f9be969e7801eda5977951728
Chiwetelu Johnpaul Chidera <49923152+Myestery@users.noreply.github.com>
2024-08-29 16:41:25 +0000

    feat (lint): correct contract dist path

---
+++ packages/contracts/eslint.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const unlockProtocolConfig = require('@unlock-protocol/eslint-config')
+
+module.exports = [
+  ...unlockProtocolConfig,
+  {
+    rules: {
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+    languageOptions: {
+      globals: {
+        it: 'readonly',
+        describe: 'readonly',
+        before: 'readonly',
+        beforeEach: 'readonly',
+        after: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+  },
+]
