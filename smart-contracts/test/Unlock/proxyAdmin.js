c1a59c85d2350501c806f62bdba62cbfce587f0c
Deep Sea <deepsea930514@gmail.com>
2024-09-04 12:57:50 -0400

    add test for proxyAdmin

--- smart-contracts/test/Unlock/proxyAdmin.js
+++ smart-contracts/test/Unlock/proxyAdmin.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const { ethers, upgrades } = require('hardhat')
+const { ADDRESS_ZERO } = require('../helpers')
 const { reverts } = require('../helpers/errors')
 
 describe('proxyAdmin', () => {
@@ -43,5 +44,32 @@ describe('proxyAdmin', () => {
 
   it('forbid to deploy twice', async () => {
     reverts(unlock.initializeProxyAdmin(), 'ALREADY_DEPLOYED')
+  })
+})
+
+describe('proxyAdmin unset', () => {
+  let unlock
+  before(async () => {
+    const Unlock = await ethers.getContractFactory('Unlock')
+    unlock = await Unlock.deploy()
+  })
+
+  it('check ProxyAdmin', async () => {
+    const proxyAdmin = await unlock.proxyAdminAddress()
+    assert.equal(proxyAdmin, ADDRESS_ZERO)
+  })
+
+  it('createUpgradeableLockAtVersion should fail as proxyAdmin is not set', async () => {
+    await reverts(
+      unlock.createUpgradeableLockAtVersion('0x', 13),
+      'Unlock__MISSING_PROXY_ADMIN'
+    )
+  })
+
+  it('upgradeLock should fail as proxyAdmin is not set', async () => {
+    await reverts(
+      unlock.upgradeLock(ADDRESS_ZERO, 13),
+      'Unlock__MISSING_PROXY_ADMIN'
+    )
   })
 })
