fdd86a1cb488b8f3a8d3787ec830be0599d432cd
Clément Renaud <clement+git@clementrenaud.com>
2024-09-04 17:46:50 +0200

    draft proposal

---
+++ governance/proposals/017-cross-chain-base-config.js
@@ -0,0 +1,99 @@
+const { ethers } = require('hardhat')
+const {
+  getNetwork,
+  getProxyAdminAddress,
+} = require('@unlock-protocol/hardhat-helpers')
+const { targetChains, ConnextMod } = require('../helpers/bridge')
+const { parseSafeMulticall } = require('../helpers/multisig')
+const { parseBridgeCall } = require('../helpers/crossChain')
+
+const BASE_TIMELOCK = '0xB34567C4cA697b39F72e1a8478f285329A98ed1b'
+
+module.exports = async () => {
+  const mainnet = await getNetwork(1)
+  const base = await getNetwork(8543)
+
+  // mainnet owmership
+  const ownable = new ethers.Interface([
+    `function transferOwnership(address newOwner)`,
+  ])
+  const mainnetCalls = [
+    // 1. transfer unlock mainnet to multisig
+    {
+      contractAddress: mainnet.unlockAddress,
+      calldata: ownable.encodeFunctionData('approve', [mainnet.multisig]),
+    },
+    // 2. transfer unlock mainnet proxyAdmin to multisig
+    {
+      contractAddress: await getProxyAdminAddress(mainnet.unlockAddress),
+      calldata: ownable.encodeFunctionData('approve', [mainnet.multisig]),
+    },
+  ]
+
+  // bridge calls to update safe/connext multisig plugins
+  const bridgeCalls = await Promise.all(
+    targetChains.map(async (network) => {
+      const {
+        governanceBridge: {
+          modules: { connextMod },
+        },
+        id: destChainId,
+      } = network
+
+      const connextModInterface = new ethers.Interface(ConnextMod)
+
+      // parse calls for bridge
+      const destCalls = [
+        // set origin chain as base
+        {
+          contractAddress: connextMod,
+          calldata: connextModInterface.encodeFunctionData('setOrigin', [
+            base.governanceBridge.domainId,
+          ]),
+        },
+        {
+          // set origin contract as base DAO timelock
+          contractAddress: connextMod,
+          calldata: connextModInterface.encodeFunctionData('setOriginSender', [
+            BASE_TIMELOCK,
+          ]),
+        },
+      ]
+      const { to, value, data, operation } = await parseSafeMulticall({
+        chainId: destChainId,
+        calls: destCalls,
+      })
+
+      // encode multicall instructions to be executed by the SAFE
+      const abiCoder = ethers.AbiCoder.defaultAbiCoder()
+      const moduleData = abiCoder.encode(
+        ['address', 'uint256', 'bytes', 'bool'],
+        [
+          to, // to
+          value, // value
+          data, // data
+          operation, // operation: 0 for CALL, 1 for DELEGATECALL
+        ]
+      )
+
+      const bridgeCall = await parseBridgeCall({
+        chainId: destChainId,
+        moduleData,
+      })
+      return bridgeCall
+    })
+  )
+
+  const proposalName = `Transfer cross-chain governance to the Base DAO
+
+**How it works**
+
+1. transfer ownership of Unlock contract + proxy admin on mainnet to the mainnet multisig 
+2. change the “authority source” of the multisig/connext plugin on all destination chains to the DAO timelock on Base 
+`
+
+  return {
+    proposalName,
+    calls: [...mainnetCalls, ...bridgeCalls],
+  }
+}
