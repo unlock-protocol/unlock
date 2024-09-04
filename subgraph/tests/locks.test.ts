63b8f4840d4d212e42d0d77394d0ea1d66ed3f54
Clément Renaud <clement+git@clementrenaud.com>
2024-09-04 15:09:50 +0200

    bring back v8 tests

--- subgraph/tests/locks.test.ts
+++ subgraph/tests/locks.test.ts
@@ -29,6 +29,7 @@ import {
   createPricingChangedEvent,
   createLockUpgradedEvent,
   createLockMetadata,
+  mockDataSourceV9,
   createRoleRevokedKeyGranterRemovedEvent,
 } from './locks-utils'
 import {
@@ -56,6 +57,7 @@ import {
   baseTokenURI,
   maxNumberOfKeys,
   maxKeysPerAddress,
+  lockAddressV9,
   tokenId,
   expiration,
   keyGranters,
@@ -370,5 +372,79 @@ describe('Describe Locks events', () => {
       'lastKeyRenewedAt',
       newKeyExtended.block.timestamp.toString()
     )
+  })
+})
+
+describe('Describe Locks events (v8)', () => {
+  beforeAll(() => {
+    mockDataSourceV8()
+    const newLockEvent = createNewLockEvent(
+      Address.fromString(lockOwner),
+      Address.fromString(lockAddressV8)
+    )
+    handleNewLock(newLockEvent)
+  })
+  test('Creation of a new lock (v8)', () => {
+    assert.entityCount('Lock', 1)
+    assert.fieldEquals('Lock', lockAddressV8, 'address', lockAddressV8)
+    assert.fieldEquals('Lock', lockAddressV8, 'createdAtBlock', '1')
+    assert.fieldEquals('Lock', lockAddressV8, 'version', '8')
+    assert.fieldEquals('Lock', lockAddressV8, 'price', '1000')
+    assert.fieldEquals('Lock', lockAddressV8, 'name', 'My lock v8')
+    assert.fieldEquals(
+      'Lock',
+      lockAddressV8,
+      'expirationDuration',
+      `${duration}`
+    )
+    assert.fieldEquals('Lock', lockAddressV8, 'tokenAddress', nullAddress)
+    assert.fieldEquals('Lock', lockAddressV8, 'lockManagers', `[${lockOwner}]`)
+    assert.fieldEquals('Lock', lockAddressV8, 'keyGranters', `[${lockOwner}]`)
+
+    assert.fieldEquals('Lock', lockAddressV8, 'totalKeys', '0')
+    assert.fieldEquals('Lock', lockAddressV8, 'numberOfReceipts', '0')
+    assert.fieldEquals(
+      'Lock',
+      lockAddressV8,
+      'maxNumberOfKeys',
+      `${maxNumberOfKeys}`
+    )
+    assert.fieldEquals('Lock', lockAddressV8, 'maxKeysPerAddress', '1')
+  })
+
+  test('Lock manager added (v8)', () => {
+    mockDataSourceV8()
+    assert.fieldEquals('Lock', lockAddressV8, 'lockManagers', `[${lockOwner}]`)
+    const newLockManagerAdded = createLockManagerAddedEvent(
+      Address.fromString(lockManagers[0])
+    )
+    handleLockManagerAdded(newLockManagerAdded)
+
+    assert.fieldEquals(
+      'Lock',
+      lockAddressV8,
+      'lockManagers',
+      `[${lockOwner}, ${lockManagers[0]}]`
+    )
+  })
+
+  test('Key granter added (v8)', () => {
+    mockDataSourceV8()
+    assert.fieldEquals('Lock', lockAddressV8, 'keyGranters', `[${lockOwner}]`)
+    const newKeyGranterAdded = createKeyGranterAddedEvent(
+      Address.fromString(keyGranters[0])
+    )
+    handleKeyGranterAdded(newKeyGranterAdded)
+
+    assert.fieldEquals(
+      'Lock',
+      lockAddressV8,
+      'keyGranters',
+      `[${lockOwner}, ${keyGranters[0]}]`
+    )
+  })
+
+  afterAll(() => {
+    clearStore()
   })
 })
