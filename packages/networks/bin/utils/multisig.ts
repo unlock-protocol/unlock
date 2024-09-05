cd4607b6dbdf6c9f83b05d7fd5cab8b8ddb34eee
Clément Renaud <clement@unlock-protocol.com>
2024-09-05 14:21:43 +0200

    feat(networks): automated check when adding a new network (#14243)
    * refactor tokens check
    
    * refactor keys check
    
    * update chain name
    
    * add validate task
    
    * add validate step to ci
    
    * fix path
    
    * parse errors
    
    * post comment
    
    * add test file
    
    * use complete path in check
    
    * check for changes in packages/networks
    
    * parse errors correctly
    
    * better parsing
    
    * use impl address
    
    * use log file
    
    * exit code
    
    * use error log
    
    * refactor helper file
    
    * show changed files
    
    * ignore directories
    
    * parse changed files properly
    
    * fix subgraph checks
    
    * proper validation
    
    * make missing deployer key a simple warning
    
    * debug log level for missing deployer key
    
    * add todo
    
    * small refactor
    
    * use cration tx to verify
    
    * small TODO
    
    * use deployTx
    
    * more bytecode experiment
    
    * delete remains of ethers v5
    
    * use contract source code comparison
    
    * validate publicLock source code
    
    * remove unused etherscan methods
    
    * parse success and failure too
    
    * fix public lock source verif
    
    * fix ci file change check
    
    * remove old ethers5 pattern in script
    
    * show shole log as pr comment
    
    * remove unwanted log
    
    * bring back console error for deployer key
    
    * fix comment parsing
    
    * sticky gh message
    
    * update pr comment
    
    * add ownership checks
    
    * show success on multisig check
    
    * typo
    
    * remove code block formatted
    
    * delete test file
    
    * revert network changes

---
+++ packages/networks/bin/utils/multisig.ts
@@ -0,0 +1,47 @@
+import { ethers } from 'ethers'
+const multisigABI = require('@unlock-protocol/hardhat-helpers/dist/ABIs/multisig2.json')
+
+const expectedSigners = [
+  '0x9d3ea9e9adde71141f4534dB3b9B80dF3D03Ee5f', // cc
+  '0x4Ce2DD8373ECe0d7baAA16E559A5817CC875b16a', // jg
+  '0x4011d09a86D0acA8377a4A8baD691F1ACeeCd672', // nf
+  '0xcFd35259E3A468E7bDF84a95bCddAc0B614A9212', // aa
+  '0xccb5D94FbfBFDc4953Ca8a114f88773C2fF98e80', // sm
+  '0x246A13358Fb27523642D86367a51C2aEB137Ac6C', // cr
+  '0x2785f2a3DDaCfDE5947F1A9D6c878CCD7F885400', // cn
+  '0x7A23608a8eBe71868013BDA0d900351A83bb4Dc2', // nm
+  '0x8de33D8204929ceb2F7AA6299d0643a7f6664c9b', // bw
+].sort()
+
+export const checkMultisig = async ({
+  safeAddress,
+  providerURL,
+}: {
+  safeAddress: string
+  providerURL: string
+}) => {
+  const errors: string[] = []
+  const provider = new ethers.JsonRpcProvider(providerURL)
+
+  const safe = new ethers.Contract(safeAddress, multisigABI, provider)
+  const owners = await safe.getOwners()
+  const policy = await safe.getThreshold()
+
+  if (policy < 4) {
+    errors.push(
+      `❌ Unexpected policy: ${policy}/${owners.length} for 4/${expectedSigners.length} expected`
+    )
+  }
+
+  const extraSigners = owners.filter((x) => !expectedSigners.includes(x))
+  if (extraSigners.length > 0) {
+    errors.push(`❌ Extra signers: ${[...extraSigners].sort()}`)
+  }
+
+  const missingSigners = expectedSigners.filter((x) => !owners.includes(x))
+  if (missingSigners.length > 0) {
+    errors.push(`❌ Missing signers: ${missingSigners}`)
+  }
+
+  return errors
+}
