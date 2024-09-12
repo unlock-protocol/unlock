de9fc9413d501dac9f1836be7bf32fbe723f9a7b
Julien Genestoux <julien.genestoux@gmail.com>
2024-09-12 16:57:19 -0400

    cleanup for universal card

--- locksmith/src/controllers/v2/priceController.ts
+++ locksmith/src/controllers/v2/priceController.ts
@@ -4,7 +4,6 @@ import { ethers } from 'ethers'
 import { getCreditCardEnabledStatus } from '../../operations/creditCardOperations'
 import * as Normalizer from '../../utils/normalizer'
 import * as pricingOperations from '../../operations/pricingOperations'
-import { MIN_PAYMENT_STRIPE_ONRAMP } from '../../utils/constants'
 import logger from '../../logger'
 
 export const amount: RequestHandler = async (request, response) => {
@@ -40,59 +39,6 @@ export const total: RequestHandler = async (request, response) => {
   })
 
   return response.send(charge)
-}
-
-/**
- * Gets the pricing for the universal card payment method
- * TODO: support swap and purchase to get the route in USDC if the lock is not USDC
- * @param request
- * @param response
- * @returns
- */
-export const universalCard: RequestHandler = async (request, response) => {
-  const network = Number(request.params.network)
-  const lockAddress = request.params.lock
-  const { recipients: recipientQ = [], purchaseData = [] } = request.query
-
-  // Setup values
-  const recipients: string[] = Array.isArray(recipientQ)
-    ? recipientQ.map((x) => x.toString())
-    : [recipientQ.toString()]
-  const data: string[] = Array.isArray(purchaseData)
-    ? purchaseData.map((x) => x.toString())
-    : [purchaseData.toString()]
-
-  // Ok so now we use the pricing API to get the price for each recipient!
-  const pricing = await createPricingForPurchase({
-    lockAddress,
-    recipients,
-    network,
-    referrers: Array.from({ length: recipients.length }).map(() => ''),
-    data,
-  })
-
-  const total = pricing.total - pricing.creditCardProcessingFee
-
-  // For universal card, the creditCardProcessingFee fee is applied by Stripe directly
-  // Stripe minimum payment is 1$ (100 cents)
-  const creditCardProcessingFee =
-    total < MIN_PAYMENT_STRIPE_ONRAMP ? MIN_PAYMENT_STRIPE_ONRAMP - total : 0
-
-  return response.send({
-    creditCardProcessingFee,
-    unlockServiceFee: pricing.unlockServiceFee,
-    gasCost: pricing.gasCost,
-    total: total + creditCardProcessingFee,
-    prices: [
-      ...pricing.recipients.map((recipient) => {
-        return {
-          userAddress: recipient.address,
-          amount: recipient.price.amount,
-          symbol: '$',
-        }
-      }),
-    ],
-  })
 }
 
 export const isCardPaymentEnabledForLock: RequestHandler = async (
