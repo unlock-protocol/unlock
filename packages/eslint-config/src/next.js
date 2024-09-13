e45d3038203b65c2f5f3d296da28a02374e2293d
Johnpaul Chiwetelu <49923152+Myestery@users.noreply.github.com>
2024-09-13 20:15:11 +0100

    Eslint cleanup 2 (#14603)
    * feat (lint): synchronize eslint rules
    
    * feat (lint): synchronize eslint rules
    
    * feat (lint): synchronize eslint rules
    
    * feat(lint): Synchronize eslint rules and add evm-address-to-checksummed warning
    
    * feat(lint): Synchronize eslint rules and update evm-address-to-checksummed warning
    
    This commit synchronizes the eslint rules and updates the evm-address-to-checksummed warning. It also fixes the formatting of recipient and userAddress variables to use checksummed addresses.
    
    Fixes #14592
    
    * refactor(eslint-config): Remove unnecessary eslint-disable comments and unused imports
    
    * feat(unlock-app) timezone set by default based on location (#14594)
    
    timezone set by deafault based on location
    
    * fix(locksmit): fixed relayer API
    
    * fix(packages/ui): Improve `AddressInput` UX (#14596)
    
    improve ux for resolving address or name inputs
    
    * fix(unlock-app): Attendees specific view (#14589)
    
    * add UpdateEmailModal component
    
    * strip attendee specific actions from locks' view
    
    * strip unnwanted metadata and add specific event actions to attendees' view
    
    * linting
    
    * update lock settings metadata card to include email actions
    
    * docs: adding UP to title
    
    * docs: adding tags to the UP token page
    
    * fix(ci): correct github comment proxy deployment URL (#14598)
    
    ---------
    
    Co-authored-by: Julien Genestoux <julien.genestoux@gmail.com>
    Co-authored-by: txbì <46839250+0xTxbi@users.noreply.github.com>
    Co-authored-by: Clément Renaud <clement@unlock-protocol.com>

--- packages/eslint-config/src/next.js
+++ packages/eslint-config/src/next.js
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 /** @typedef {import('eslint').Linter.Config} ESLintConfig */
 const baseConfig = require('./base.js')
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 const { FlatCompat } = require('@eslint/eslintrc')
 const { fixupConfigRules } = require('@eslint/compat')
-const path = require('node:path')
 const js = require('@eslint/js')
-const { fileURLToPath } = require('node:url')
 
 // https://blog.linotte.dev/eslint-9-next-js-935c2b6d0371 describes how the nextjs core vitals rule was added
 const compat = new FlatCompat({
@@ -28,7 +25,6 @@ module.exports = [
     rules: {
       '@next/next/no-img-element': 0,
       '@next/next/no-html-link-for-pages': 0,
-      '@typescript-eslint/no-explicit-any': 1,
       quotes: 'warn',
       '@typescript-eslint/no-unused-expressions': 'warn',
     },
