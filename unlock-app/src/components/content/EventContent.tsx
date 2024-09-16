33321ca25945a158cc134aa636d51bac1f52ed38
0xTxbi <techiejossy@gmail.com>
2024-09-16 13:05:46 +0100

    migrate components to use next/navigation

--- unlock-app/src/components/content/EventContent.tsx
+++ unlock-app/src/components/content/EventContent.tsx
@@ -1,36 +1,10 @@
-import React from 'react'
-import Head from 'next/head'
-import { pageTitle } from '../../constants'
-import { useRouter } from 'next/router'
-import { AppLayout, FOOTER } from '../interface/layouts/AppLayout'
+'use client'
+
+import { useRouter } from 'next/navigation'
 import LoadingIcon from '../interface/Loading'
 import EventDetails from './event/EventDetails'
 import { EventLandingPage } from './event/EventLandingPage'
-import { useRouterQueryForLockAddressAndNetworks } from '~/hooks/useRouterQueryForLockAddressAndNetworks'
-import { useMetadata } from '~/hooks/metadata'
-import { toFormData } from '~/components/interface/locks/metadata/utils'
 import { Event, PaywallConfigType } from '@unlock-protocol/core'
-import { defaultEventCheckoutConfigForLockOnNetwork } from './event/NewEvent'
-
-export const EventContent = () => {
-  const {
-    lockAddress,
-    network,
-    isLoading: isLoadingQuery,
-  } = useRouterQueryForLockAddressAndNetworks()
-  const { data: metadata, isLoading: isMetadataLoading } = useMetadata({
-    lockAddress,
-    network,
-  })
-  const event = metadata ? (toFormData(metadata) as Event) : undefined
-  const isLoading = isLoadingQuery || isMetadataLoading
-
-  // Create a checkout config if none is set
-  const checkoutConfig = {
-    config: defaultEventCheckoutConfigForLockOnNetwork(lockAddress, network),
-  }
-  return EventContentWithProps({ isLoading, checkoutConfig, event })
-}
 
 interface EventContentWithPropsProps {
   event?: Event
@@ -42,7 +16,7 @@ interface EventContentWithPropsProps {
 }
 
 export const EventContentWithProps = ({
-  isLoading,
+  isLoading = false,
   checkoutConfig,
   event,
 }: EventContentWithPropsProps) => {
@@ -59,23 +33,11 @@ export const EventContentWithProps = ({
   }
 
   return (
-    <AppLayout
-      showFooter={{ ...FOOTER, subscriptionForm: undefined }}
-      showLinks={false}
-      authRequired={false}
-      logoRedirectUrl="/event"
-      logoImageUrl="/images/svg/logo-unlock-events.svg"
-    >
-      <Head>
-        <title>{pageTitle('Event')}</title>
-      </Head>
-
+    <>
       {!event && <EventLandingPage handleCreateEvent={handleCreateEvent} />}
-      {!!event && (
-        <EventDetails event={event} checkoutConfig={checkoutConfig} />
-      )}
-    </AppLayout>
+      {event && <EventDetails event={event} checkoutConfig={checkoutConfig} />}
+    </>
   )
 }
 
-export default EventContent
+export default EventContentWithProps
