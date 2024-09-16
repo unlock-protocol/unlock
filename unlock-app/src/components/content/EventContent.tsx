2abc8aef596db61deaf36e3cfdb056be1435bf23
0xTxbi <techiejossy@gmail.com>
2024-09-16 14:11:53 +0100

    migrate events landing page to app router

--- unlock-app/src/components/content/EventContent.tsx
+++ unlock-app/src/components/content/EventContent.tsx
@@ -1,10 +1,33 @@
 'use client'
-
-import { useRouter } from 'next/navigation'
 import LoadingIcon from '../interface/Loading'
 import EventDetails from './event/EventDetails'
 import { EventLandingPage } from './event/EventLandingPage'
+import { useRouterQueryForLockAddressAndNetworks } from '~/hooks/useRouterQueryForLockAddressAndNetworks'
+import { useMetadata } from '~/hooks/metadata'
+import { toFormData } from '~/components/interface/locks/metadata/utils'
 import { Event, PaywallConfigType } from '@unlock-protocol/core'
+import { defaultEventCheckoutConfigForLockOnNetwork } from './event/NewEvent'
+import { useRouter } from 'next/navigation'
+
+export const EventContent = () => {
+  const {
+    lockAddress,
+    network,
+    isLoading: isLoadingQuery,
+  } = useRouterQueryForLockAddressAndNetworks()
+  const { data: metadata, isLoading: isMetadataLoading } = useMetadata({
+    lockAddress,
+    network,
+  })
+  const event = metadata ? (toFormData(metadata) as Event) : undefined
+  const isLoading = isLoadingQuery || isMetadataLoading
+
+  // Create a checkout config if none is set
+  const checkoutConfig = {
+    config: defaultEventCheckoutConfigForLockOnNetwork(lockAddress, network),
+  }
+  return EventContentWithProps({ isLoading, checkoutConfig, event })
+}
 
 interface EventContentWithPropsProps {
   event?: Event
@@ -16,18 +39,16 @@ interface EventContentWithPropsProps {
 }
 
 export const EventContentWithProps = ({
-  isLoading = false,
+  isLoading,
   checkoutConfig,
   event,
 }: EventContentWithPropsProps) => {
   const router = useRouter()
-
   const handleCreateEvent = () => {
     router.push(
       'https://unlock-protocol-1.hubspotpagebuilder.com/unlock-protocol-newsletter-signup-0'
     )
   }
-
   if (isLoading) {
     return <LoadingIcon />
   }
@@ -35,9 +56,11 @@ export const EventContentWithProps = ({
   return (
     <>
       {!event && <EventLandingPage handleCreateEvent={handleCreateEvent} />}
-      {event && <EventDetails event={event} checkoutConfig={checkoutConfig} />}
+      {!!event && (
+        <EventDetails event={event} checkoutConfig={checkoutConfig} />
+      )}
     </>
   )
 }
 
-export default EventContentWithProps
+export default EventContent
