9bb8d6450557908fc539885a062641677abfeef6
0xTxbi <techiejossy@gmail.com>
2024-09-17 19:41:19 +0100

    fix lock index direct page reload

--- unlock-app/src/components/interface/locks/List/index.tsx
+++ unlock-app/src/components/interface/locks/List/index.tsx
@@ -1,18 +1,43 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+
 import { useAuth } from '~/contexts/AuthenticationContext'
+
 import { WalletNotConnected } from '../../layouts/AppLayout'
+
 import { LockList } from './elements/LockList'
+
+import { Placeholder } from '@unlock-protocol/ui'
 
 export const LocksListPage = () => {
   const { network, account } = useAuth()
+
   const searchParams = useSearchParams()
+
   const manager = searchParams.get('account')
 
-  // show lock for the specific manager if present in query parameters
-  const locksOwner = manager ?? account!
+  // show lock for the specific manager if present in query parameters and ensure account is defined before using it
+
+  const locksOwner = manager ?? account
+
+  if (!network || !locksOwner) {
+    return (
+      <Placeholder.Root>
+        <Placeholder.Card />
+
+        <Placeholder.Card />
+
+        <Placeholder.Card />
+
+        <Placeholder.Card />
+
+        <Placeholder.Card />
+
+        <Placeholder.Card />
+      </Placeholder.Root>
+    )
+  }
 
   return (
     <>
