0b287b9a6a7d0a4c578d54777730ff412ab83c4c
Deep Sea <deepsea930514@gmail.com>
2024-09-16 22:59:19 -0400

    fix ERC20

--- smart-contracts/test/Lock/fail.js
+++ smart-contracts/test/Lock/fail.js
@@ -3,8 +3,8 @@ const { deployLock, reverts } = require('../helpers')
 
 describe('Lock / fail to deploy', async () => {
   it('fail to deploy Lock if invalid ERC20 input', async () => {
-    const NonToken = await ethers.getContractFactory('ERC20')
-    const nonToken = await NonToken.deploy('Non Token', 'Token')
+    const NonToken = await ethers.getContractFactory('TestERC20')
+    const nonToken = await NonToken.deploy()
     await reverts(
       deployLock({
         tokenAddress: await nonToken.getAddress(),
