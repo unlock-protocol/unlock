86be8a266a7f2ca6850e89dcd9013a108fdeb902
Clément Renaud <clement+git@clementrenaud.com>
2024-09-18 15:05:46 +0200

    make sure ci runs properly when a file changes

--- packages/networks/src/networks/mainnet.ts
+++ packages/networks/src/networks/mainnet.ts
@@ -175,11 +175,6 @@ export const mainnet: NetworkConfig = {
   },
 
   url: 'https://ethereum.org/en/',
-  // universalCard: {
-  //   cardPurchaserAddress: '0x49814dd8a03594bE78a18455CC4Df3876ecFbD69',
-  //   stripeDestinationNetwork: 'ethereum',
-  //   stripeDestinationCurrency: 'usdc',
-  // },
 }
 
 export default mainnet
