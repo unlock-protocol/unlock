e56b2224f4ce96cdda182bbf170a384013287e65
Joseph David <techiejossy@gmail.com>
2024-09-23 14:06:03 +0100

    add event collection settings page

---
+++ unlock-app/app/events/[slug]/settings/page.tsx
@@ -0,0 +1,33 @@
+import { AuthRequired } from 'app/Components/ProtectedContent'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { EventCollectionSettings } from '~/components/content/events-collection/EventCollectionSettings'
+import { getEventCollection } from '~/utils/eventCollections'
+
+export const metadata: Metadata = {
+  title: 'Event Settings',
+  description: 'Manage your event settings.',
+}
+
+const EventCollectionSettingsPage = async ({
+  params,
+}: {
+  params: { slug: string }
+}) => {
+  const { slug } = params
+
+  // fetch the event collection details
+  const eventCollection = await getEventCollection(slug)
+
+  if (!eventCollection) {
+    return notFound()
+  }
+
+  return (
+    <AuthRequired>
+      <EventCollectionSettings eventCollection={eventCollection} />
+    </AuthRequired>
+  )
+}
+
+export default EventCollectionSettingsPage
