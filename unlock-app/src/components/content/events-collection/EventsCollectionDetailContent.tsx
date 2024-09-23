59b1dc2a4b2cdfc7b44c4297a79bdf99bc93c444
Joseph David <techiejossy@gmail.com>
2024-09-23 19:34:19 +0100

    type fixes

--- unlock-app/src/components/content/events-collection/EventsCollectionDetailContent.tsx
+++ unlock-app/src/components/content/events-collection/EventsCollectionDetailContent.tsx
@@ -8,10 +8,10 @@ import { EventCollection } from '@unlock-protocol/unlock-js'
 import { useRouter } from 'next/navigation'
 import { EventOverviewCard } from './EventOverviewCard'
 import { ImageBar } from '~/components/interface/locks/Manage/elements/ImageBar'
-import EventDetailDrawer from './EventDetailDawer'
 import { FaGithub, FaYoutube, FaGlobe, FaTwitter } from 'react-icons/fa'
 import Link from 'next/link'
 import FarcasterIcon from './icons/FarcasterIcon'
+import EventDetailDrawer from './EventDetailDawer'
 
 export interface EventTicket {
   event_address: string
@@ -42,7 +42,6 @@ export interface EventData {
 }
 
 export interface Event {
-  eventUrl: string
   name: string
   data: EventData
   createdBy: string
@@ -62,18 +61,18 @@ export default function EventsCollectionDetailContent({
   const { account } = useAuth()
   const router = useRouter()
 
-  // event detail drawer
+  // Event detail drawer
   const [isEventDetailDrawerOpen, setIsEventDetailDrawerOpen] = useState(false)
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null)
 
   const hasValidEvents = useMemo(() => {
     return (
       eventCollection.events?.some(
-        (event) =>
-          event.name !== null && event.slug !== null && event.eventUrl !== null
+        (event) => event.name !== null && event.slug !== null
       ) ?? false
     )
   }, [eventCollection.events])
+
   const isManager = useMemo(() => {
     if (!account) return false
     return eventCollection.managerAddresses?.includes(account)
@@ -130,80 +129,81 @@ export default function EventsCollectionDetailContent({
               alt="Cover image"
             />
           </div>
+        </div>
+        <div className="sm:absolute flex sm:flex-col w-full gap-6 sm:pl-10 -bottom-12">
+          <section className="flex justify-between flex-col sm:flex-row w-full">
+            <div className="flex p-1 bg-white sm:p-2 sm:w-48 sm:h-48 sm:rounded-3xl rounded-xl border mb-4 sm:mb-0">
+              <img
+                alt={eventCollection.title}
+                className="object-cover w-full m-auto aspect-1 sm:rounded-2xl rounded-lg"
+                src={
+                  eventCollection.coverImage ||
+                  'https://avatars.githubusercontent.com/u/46839250?v=4'
+                }
+              />
+            </div>
 
-          <div className="sm:absolute flex sm:flex-col w-full gap-6 sm:pl-10 -bottom-12">
-            <section className="flex justify-between flex-col sm:flex-row w-full">
-              <div className="flex p-1 bg-white sm:p-2 sm:w-48 sm:h-48 sm:rounded-3xl rounded-xl border mb-4 sm:mb-0">
-                <img
-                  alt={eventCollection.title}
-                  className="object-cover w-full m-auto aspect-1 sm:rounded-2xl rounded-lg"
-                  src={
-                    eventCollection.coverImage ||
-                    'https://avatars.githubusercontent.com/u/46839250?v=4'
-                  }
-                />
-              </div>
+            <div className="hidden md:flex items-center justify-end gap-0 mt-auto md:gap-2 w-full sm:w-auto"></div>
+          </section>
+        </div>
+      </div>
 
-              <div className="hidden md:flex items-center justify-end gap-0 mt-auto md:gap-2 w-full sm:w-auto"></div>
-            </section>
+      <section className="grid items-start grid-cols-1 md:gap-4 md:grid-cols-3 md:mt-16 mt-8">
+        <div className="flex flex-col col-span-3 gap-4 md:col-span-2">
+          <h1 className="text-3xl font-bold md:text-6xl">
+            {eventCollection.title}
+          </h1>
+          <p className="text-sm md:text-base">{eventCollection.description}</p>
+          <div className="flex space-x-6">
+            {eventCollection.links &&
+              Array.isArray(eventCollection.links) &&
+              eventCollection.links.map(
+                (link: { type: string; url: string }, index: number) => (
+                  <Link
+                    key={index}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {getLinkIcon(link.type)}
+                  </Link>
+                )
+              )}
           </div>
         </div>
+      </section>
 
-        <section className="grid items-start grid-cols-1 md:gap-4 md:grid-cols-3 md:mt-16 mt-8">
-          <div className="flex flex-col col-span-3 gap-4 md:col-span-2">
-            <h1 className="text-3xl font-bold md:text-6xl">
-              {eventCollection.title}
-            </h1>
-            <p className="text-sm md:text-base">
-              {eventCollection.description}
-            </p>
-            <div className="flex space-x-6">
-              {eventCollection.links?.map((link, index) => (
-                <Link
-                  key={index}
-                  href={link.url!}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {getLinkIcon(link.type)}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </section>
-
-        {/* list of events */}
-        <section className="mt-16">
-          <h2 className="text-3xl font-bold">Events</h2>
-          <div className="flex flex-col lg:grid lg:grid-cols-12 gap-14 mt-5">
-            <div className="flex flex-col gap-6 lg:col-span-8">
-              {hasValidEvents ? (
-                <div className="">
-                  <div className="space-y-6">
-                    {eventCollection.events?.map((eventItem: any) => (
-                      <EventOverviewCard
-                        key={eventItem.slug}
-                        event={eventItem}
-                        onClick={handleEventDetailClick}
-                      />
-                    ))}
-                  </div>
+      {/* List of Events */}
+      <section className="mt-16">
+        <h2 className="text-3xl font-bold">Events</h2>
+        <div className="flex flex-col lg:grid lg:grid-cols-12 gap-14 mt-5">
+          <div className="flex flex-col gap-6 lg:col-span-8">
+            {hasValidEvents ? (
+              <div className="">
+                <div className="space-y-6">
+                  {eventCollection.events?.map((eventItem: any) => (
+                    <EventOverviewCard
+                      key={eventItem.slug}
+                      event={eventItem}
+                      onClick={handleEventDetailClick}
+                    />
+                  ))}
                 </div>
-              ) : (
-                <ImageBar
-                  src="/images/illustrations/no-locks.svg"
-                  description={<p>No events have been added yet.</p>}
-                />
-              )}
-            </div>
-            <div className="lg:col-span-4">
-              <Placeholder.Root>
-                <Placeholder.Card size="xl" />
-              </Placeholder.Root>
-            </div>
+              </div>
+            ) : (
+              <ImageBar
+                src="/images/illustrations/no-locks.svg"
+                description={<p>No events have been added yet.</p>}
+              />
+            )}
           </div>
-        </section>
-      </div>
+          <div className="lg:col-span-4">
+            <Placeholder.Root>
+              <Placeholder.Card size="xl" />
+            </Placeholder.Root>
+          </div>
+        </div>
+      </section>
 
       {/* Event Detail Drawer */}
       <EventDetailDrawer
