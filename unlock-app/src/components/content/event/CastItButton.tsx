c4a636f7eb00d144bd6417fcecd7d21f298a435b
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-09-26 16:09:32 +0100

    feature(unlock-app): Event collection detail UX (#14674)
    * improve hooks
    
    * pre-fetch collection details for hyhdration
    
    * update event addition component
    
    * tidy event location
    
    * update event detail drawer
    
    * tidy event detail
    
    * loading ux for event detail fetch
    
    * update castit component
    
    * update tweetit component
    
    * update collection detail to allow sharing
    
    * cleanup
    
    * event overview rwd

--- unlock-app/src/components/content/event/CastItButton.tsx
+++ unlock-app/src/components/content/event/CastItButton.tsx
@@ -2,32 +2,57 @@ import { Tooltip } from '@unlock-protocol/ui'
 import Link from 'next/link'
 import { Metadata } from '~/components/interface/locks/metadata/utils'
 import { getEventDate } from './utils'
+import { config } from '~/config/app'
 
 interface CastItButtonProps {
-  event: Partial<Metadata>
-  eventUrl: string
+  event?: Partial<Metadata>
+  eventUrl?: string
+  eventCollection?: any
 }
 
-export const CastItButton = ({ event, eventUrl }: CastItButtonProps) => {
-  const eventDate = getEventDate(event.ticket)
-  if (!event.name || !eventDate) {
+export const CastItButton = ({
+  event,
+  eventUrl,
+  eventCollection,
+}: CastItButtonProps) => {
+  let castText = ''
+  let urlToShare = ''
+
+  if (event && eventUrl) {
+    const eventDate = getEventDate(event.ticket)
+    if (event.name && eventDate) {
+      castText = `🎟️ I will be attending ${event.name} on ${eventDate.toLocaleDateString(
+        undefined,
+        {
+          timeZone: event?.ticket?.event_timezone,
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }
+      )}. Join me and get your tickets with @unlock-protocol.`
+      urlToShare = eventUrl.toString()
+    } else {
+      return null
+    }
+  } else if (eventCollection) {
+    const hasEvents =
+      eventCollection.events && eventCollection.events.length > 0
+    const firstEventName = hasEvents ? eventCollection.events[0].name : ''
+
+    castText = hasEvents
+      ? `✨ Explore the "${eventCollection.title}" collection powered by @unlock-protocol! Featuring events like "${firstEventName}". RSVP and join the community today!`
+      : `✨ Discover the "${eventCollection.title}" collection powered by @unlock-protocol! Stay tuned for upcoming events and join the community.`
+
+    castText += `\n\n${eventCollection.description}`
+    urlToShare = `${config.unlockApp}/events/${eventCollection.slug}`
+  } else {
     return null
   }
 
-  const tweetIntent = new URL('https://warpcast.com/~/compose')
-  tweetIntent.searchParams.set(
-    'text',
-    `🎟️ I will be attending ${event.name} on ${eventDate.toLocaleDateString(
-      undefined,
-      {
-        timeZone: event?.ticket?.event_timezone,
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      }
-    )}.\n\n${eventUrl.toString()}`
-  )
+  const castIntent = new URL('https://warpcast.com/~/compose')
+  castIntent.searchParams.set('text', `${castText}\n\n${urlToShare}`)
+
   return (
     <Tooltip
       delay={0}
@@ -37,7 +62,7 @@ export const CastItButton = ({ event, eventUrl }: CastItButtonProps) => {
     >
       <Link
         target="_blank"
-        href={tweetIntent.toString()}
+        href={castIntent.toString()}
         className="w-12 h-12 flex justify-center items-center p-2"
       >
         <svg
--- unlock-app/src/components/content/event/EventLocation.tsx
+++ unlock-app/src/components/content/event/EventLocation.tsx
@@ -5,58 +5,48 @@ import { EventDetail } from './EventDetail'
 import { FiMapPin as MapPinIcon } from 'react-icons/fi'
 import { BiLogoZoom as ZoomIcon } from 'react-icons/bi'
 
-interface EventLocationProps {
-  event?: Partial<Metadata>
-  inPerson?: boolean
-  eventLocation?: string
-  eventAddress?: string
-  backgroundColor?: string
-  compact?: boolean
-}
-
 export const EventLocation = ({
   event,
-  inPerson,
-  eventLocation,
-  eventAddress,
-  backgroundColor,
-  compact = false,
-}: EventLocationProps) => {
-  const isInPerson = inPerson ?? event?.ticket?.event_is_in_person
-  const location = eventLocation ?? event?.ticket?.event_location
-  const address = eventAddress ?? event?.ticket?.event_address
-  const bgColor = backgroundColor ?? event?.background_color
+  compact,
+}: {
+  event: Partial<Metadata>
+  compact?: boolean
+}) => {
+  const inPerson = event.ticket?.event_is_in_person
 
   return (
     <EventDetail
       label="Location"
-      icon={isInPerson ? MapPinIcon : ZoomIcon}
+      icon={inPerson ? MapPinIcon : ZoomIcon}
       compact={compact}
     >
-      <div style={{ color: `#${bgColor}` }} className="flex flex-col gap-0.5">
-        {isInPerson && (
+      <div
+        style={{ color: `#${event.background_color}` }}
+        className="flex flex-col gap-0.5"
+      >
+        {inPerson && (
           <>
             <span
-              className={`${compact ? 'text-sm' : 'text-lg'} font-normal capitalize text-brand-dark`}
+              className={`font-normal capitalize text-brand-dark ${compact ? 'text-base' : 'text-lg'}`}
             >
-              {location || address}
+              {event.ticket?.event_location
+                ? event.ticket.event_location
+                : event.ticket?.event_address}
             </span>
-            {address && (
-              <Link
-                target="_blank"
-                className={`${compact ? 'text-xs' : 'text-base'} font-bold`}
-                href={`https://www.google.com/maps/search/?api=1&query=${address}`}
-              >
-                Show map
-              </Link>
-            )}
+            <Link
+              target="_blank"
+              className={`font-bold ${compact ? 'text-sm text-brand-ui-primary' : 'text-base'}`}
+              href={`https://www.google.com/maps/search/?api=1&query=${event.ticket?.event_address}`}
+            >
+              Show map
+            </Link>
           </>
         )}
-        {!isInPerson && address && (
+        {!inPerson && (
           <Link
             target="_blank"
-            className={`${compact ? 'text-xs' : 'text-base'} flex items-center gap-2 hover:text-brand-ui-primary`}
-            href={address}
+            className={`flex items-center gap-2 hover:text-brand-ui-primary ${compact ? 'text-sm' : 'text-base'}`}
+            href={event.ticket?.event_address}
           >
             Open video-conference <ExternalLinkIcon />
           </Link>
