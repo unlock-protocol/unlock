abaa8ad38d91f0b9a672cd53ac06ce9d7db27d83
Julien Genestoux <julien.genestoux@gmail.com>
2024-09-30 10:46:54 -0400

    wip

--- unlock-app/src/components/interface/connect/Wallet.tsx
+++ unlock-app/src/components/interface/connect/Wallet.tsx
@@ -4,33 +4,31 @@ import { useAuth } from '~/contexts/AuthenticationContext'
 
 export const ConnectWallet = () => {
   // https://docs.privy.io/guide/react/wallets/external/#connect-or-create
-  const { connectOrCreateWallet } = usePrivy()
+  const { login, logout } = usePrivy()
   const { wallets } = useWallets()
-  const { authenticate } = useAuth()
-
-  console.log(wallets)
 
   useEffect(() => {
-    console.log('render connectOrCreateWallet')
-    connectOrCreateWallet()
+    logout()
+    console.log('render login')
+    login()
   }, [])
 
-  useEffect(() => {
-    console.log('render connectOrCreateWallet')
-    const connectWalletProvider = async () => {
-      const provider = await wallets[0].getEthereumProvider()
-      const signature = await wallets[0].sign('Please sign!')
-      console.log({ signature })
-      const message = 'I hereby vote for foobar'
-      const signature2 = await provider.request({
-        method: 'personal_sign',
-        params: [message, wallets[0].address],
-      })
-      console.log(signature2)
-      authenticate(provider)
-    }
-    connectWalletProvider()
-  }, [])
+  // useEffect(() => {
+  //   console.log('render connectOrCreateWallet')
+  //   const connectWalletProvider = async () => {
+  //     const provider = await wallets[0].getEthereumProvider()
+  //     const signature = await wallets[0].sign('Please sign!')
+  //     console.log({ signature })
+  //     const message = 'I hereby vote for foobar'
+  //     const signature2 = await provider.request({
+  //       method: 'personal_sign',
+  //       params: [message, wallets[0].address],
+  //     })
+  //     console.log(signature2)
+  //     authenticate(provider)
+  //   }
+  //   connectWalletProvider()
+  // }, [])
 
   return <LoginModal open={true} />
 }
