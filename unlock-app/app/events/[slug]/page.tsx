32df29f96402eca41460891ecf4083d5dac1f8f1
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-10-01 22:58:34 +0100

    feature(unlock-app): 404 if queried event collection doesn't exist (#14785)
    throw 404 if collection not found

--- unlock-app/app/events/[slug]/page.tsx
+++ unlock-app/app/events/[slug]/page.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query'
 import { Event } from '@unlock-protocol/unlock-js'
+import { notFound } from 'next/navigation'
 import EventsCollectionDetailContent from '~/components/content/events-collection/EventsCollectionDetailContent'
 import { getEventCollection } from '~/utils/eventCollections'
 
@@ -36,6 +37,17 @@ const EventCollectionDetailPage = async ({
 }: EventCollectionDetailPageProps) => {
   const { slug } = params
   const queryClient = new QueryClient()
+
+  let eventCollection
+  try {
+    eventCollection = await getEventCollection(slug)
+  } catch (error) {
+    notFound()
+  }
+
+  if (!eventCollection) {
+    notFound()
+  }
 
   // prefetch the event collection details
   await queryClient.prefetchQuery({
