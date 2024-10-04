f1070d529bdb8be29284fcfa94c4a57d58b9db60
Joseph David <techiejossy@gmail.com>
2024-10-04 16:21:10 +0100

    add new rsvp hook

--- unlock-app/src/hooks/useRsvp.ts
+++ unlock-app/src/hooks/useRsvp.ts
@@ -1,4 +1,6 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { SubgraphService } from '@unlock-protocol/unlock-js'
+import dayjs from 'dayjs'
 import { locksmith } from '~/config/locksmith'
 
 interface RsvpOption {
@@ -8,10 +10,24 @@ interface RsvpOption {
   recipient?: string
 }
 
+interface UseEventRSVPProps {
+  lockAddress: string
+  network: number
+  eventEndDate: string
+}
+
 interface Options {
   lockAddress: string
   network: number
 }
+
+interface KeysQuery {
+  where: {
+    lock: string
+    expiration_gt?: number
+  }
+}
+
 export const useRsvp = ({ lockAddress, network }: Options) => {
   return useMutation({
     mutationKey: ['rsvp', network, lockAddress],
@@ -31,5 +47,39 @@ export const useRsvp = ({ lockAddress, network }: Options) => {
       }
     },
     retry: 2,
+  })
+}
+
+// fetch the number of RSVPs for an event
+export const useEventRSVP = ({
+  lockAddress,
+  network,
+  eventEndDate,
+}: UseEventRSVPProps) => {
+  return useQuery({
+    queryKey: ['eventRSVP', lockAddress, network, eventEndDate],
+    queryFn: async () => {
+      const service = new SubgraphService()
+      const currentTime = Math.floor(Date.now() / 1000)
+      const isEventExpired = dayjs(eventEndDate).isBefore(dayjs())
+
+      const keysQuery: KeysQuery = {
+        where: {
+          lock: lockAddress.toLowerCase(),
+        },
+      }
+
+      // If the event is not expired, only count non-expired keys
+      if (!isEventExpired) {
+        keysQuery.where.expiration_gt = currentTime
+      }
+
+      const keys = await service.keys(keysQuery, { networks: [network] })
+
+      return {
+        rsvpCount: keys.length,
+        isExpired: isEventExpired,
+      }
+    },
   })
 }
