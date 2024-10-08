4075ff0a2ef22ff0f7e5c4ec7f2797d7ade7ef2a
Joseph David <techiejossy@gmail.com>
2024-10-08 23:15:27 +0300

    clean up

--- unlock-app/src/components/interface/layouts/index/DashboardHeader.tsx
+++ unlock-app/src/components/interface/layouts/index/DashboardHeader.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Button, HeaderNav } from '@unlock-protocol/ui'
 import { useConnectModal } from '~/hooks/useConnectModal'
-import useEns from '~/hooks/useEns'
 import { useAuth } from '~/contexts/AuthenticationContext'
 import { UserMenu } from '../../connect/UserMenu'
 
@@ -28,11 +27,8 @@ const MENU = {
 }
 
 export default function DashboardHeader() {
-  const { account, email, deAuthenticate } = useAuth()
+  const { account } = useAuth()
   const { openConnectModal } = useConnectModal()
-  const userEns = useEns(account || '')
-
-  console.log(account)
 
   return (
     <HeaderNav
