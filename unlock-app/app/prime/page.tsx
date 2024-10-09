03b7652867e692536b36f976d31a03e7b81e72a8
Julien Genestoux <julien.genestoux@gmail.com>
2024-10-09 14:31:45 -0400

    adding a description

--- unlock-app/app/prime/page.tsx
+++ unlock-app/app/prime/page.tsx
@@ -4,7 +4,8 @@ import PrimeContent from '~/components/interface/prime/PrimeContent'
 
 export const metadata: Metadata = {
   title: 'Unlock Prime',
-  description: 'TK',
+  description:
+    "Unlock Prime is Unlock Lab's premium membership tier, offering enhanced features and exclusive benefits for creators and communities using Unlock Protocol. It includes everything from Unlock Basic, plus monthly rewards, airdrops, and more tools to build and grow your experiences onchain.",
 }
 
 const PrimeLandingPage: React.FC = () => {
