1bd1a738421d68c9efae6e5f65641b0a21947f37
Joseph David <techiejossy@gmail.com>
2024-10-11 19:45:33 +0100

    trigger signin when user connects via privy

--- unlock-app/src/components/interface/connect/Wallet.tsx
+++ unlock-app/src/components/interface/connect/Wallet.tsx
@@ -1,17 +1,27 @@
 import { LoginModal, usePrivy, useWallets } from '@privy-io/react-auth'
 import { useEffect } from 'react'
-import { useAuth } from '~/contexts/AuthenticationContext'
+import { useSIWE } from '~/hooks/useSIWE'
 
 export const ConnectWallet = () => {
   // https://docs.privy.io/guide/react/wallets/external/#connect-or-create
-  const { login, logout } = usePrivy()
+  const { login, logout, ready, authenticated, user } = usePrivy()
+  const { signIn } = useSIWE()
   const { wallets } = useWallets()
 
   useEffect(() => {
-    logout()
+    console.log('ready', ready)
     console.log('render login')
+    console.log('authenticated', authenticated)
+    console.log('user', user)
     login()
   }, [])
+
+  useEffect(() => {
+    if (authenticated) {
+      console.log('signing in')
+      signIn()
+    }
+  }, [authenticated, signIn])
 
   // useEffect(() => {
   //   console.log('render connectOrCreateWallet')
