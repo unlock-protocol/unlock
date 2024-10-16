6bda6304d9614227e5344db326f9e841ab840650
0xTxbi <techiejossy@gmail.com>
2024-10-16 18:44:00 +0100

    cleanup auth context

--- unlock-app/src/contexts/AuthenticationContext.tsx
+++ unlock-app/src/contexts/AuthenticationContext.tsx
@@ -1,6 +1,6 @@
 import networks from '@unlock-protocol/networks'
 import { WalletService } from '@unlock-protocol/unlock-js'
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext } from 'react'
 import { usePrivy } from '@privy-io/react-auth'
 
 interface AuthenticationContextType {
@@ -10,13 +10,12 @@ interface AuthenticationContextType {
   account?: string
   network?: number
   email?: string
-  connected?: boolean
+  connected?: boolean | string | undefined
   encryptedPrivateKey?: any
   isUnlockAccount?: boolean
   getWalletService(network?: number): Promise<WalletService>
   providerSend(method: string, params: string[]): void
   displayAccount?: string
-  accessToken: string | null
 }
 
 export const defaultValues = {
@@ -25,7 +24,6 @@ export const defaultValues = {
   watchAsset: () => {},
   providerSend: (_method: string, _params: string[]) => {},
   getWalletService: async (_network?: number) => new WalletService(networks),
-  accessToken: null,
 }
 
 export const AuthenticationContext =
@@ -33,28 +31,12 @@ export const AuthenticationContext =
 
 export const useAuth = () => {
   const context = useContext(AuthenticationContext)
-  const {
-    authenticated,
-    user,
-    getAccessToken: privyGetAccessToken,
-  } = usePrivy()
-  const [accessToken, setAccessToken] = useState<string | null>(null)
-
-  useEffect(() => {
-    const fetchAccessToken = async () => {
-      if (authenticated) {
-        const token = await privyGetAccessToken()
-        setAccessToken(token)
-      }
-    }
-    fetchAccessToken()
-  }, [authenticated, privyGetAccessToken])
+  const { authenticated, user } = usePrivy()
 
   return {
     ...context,
     connected: authenticated,
     account: user?.wallet?.address || context.account,
-    accessToken,
   }
 }
 
