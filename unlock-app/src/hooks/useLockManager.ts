678c0c43cfd5bf17929083962bbb2ae3e8b219b4
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-10-17 14:17:21 +0100

    feature(unlock-app): Implement lock manager access with custom `checkoutConfig` ID (#14838)
    * update checkout config listing to permit custom input
    
    * enhancements
    
    * cleanup
    
    * add improved hook
    
    * update component

--- unlock-app/src/hooks/useLockManager.ts
+++ unlock-app/src/hooks/useLockManager.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQueries, useQuery } from '@tanstack/react-query'
 import networks from '@unlock-protocol/networks'
 import { Web3Service } from '@unlock-protocol/unlock-js'
 import { useAuth } from '~/contexts/AuthenticationContext'
@@ -8,6 +8,12 @@ interface UseLocKManagerProps {
   network: number
   lockManagerAddress?: string
 }
+
+interface UseMultipleLockManagersProps {
+  locks: Array<{ address: string; network: number }>
+  lockManagerAddress?: string
+}
+
 /**
  * Check if currently authenticated user is manager and manager could give us the manager object for that lock if we need it.
  * @param {String} lockAddress - lock address
@@ -42,5 +48,68 @@ export const useLockManager = ({
     refetch,
     isManager,
     isPending,
+  }
+}
+
+/**
+ * Check if currently authenticated user is a manager for any of the multiple locks.
+ * @param locks Array of lock addresses
+ * @param lockManagerAddress Optional specific manager address
+ */
+export const useMultipleLockManagers = ({
+  locks,
+  lockManagerAddress,
+}: UseMultipleLockManagersProps) => {
+  const { account } = useAuth()
+  const addressToCheck = lockManagerAddress || account
+
+  const groupedLocks = locks.reduce(
+    (acc, lock) => {
+      const { network, address } = lock
+      if (!acc[network]) {
+        acc[network] = []
+      }
+      acc[network].push(address)
+      return acc
+    },
+    {} as Record<number, string[]>
+  )
+
+  const queries = useQueries({
+    queries: Object.entries(groupedLocks).flatMap(([network, addresses]) =>
+      addresses.map((address) => ({
+        queryKey: [
+          'getLockManagerStatus',
+          Number(network),
+          address,
+          addressToCheck,
+        ],
+        queryFn: async () => {
+          if (!addressToCheck || !address || !network) {
+            return false
+          }
+          const web3Service = new Web3Service(networks)
+          return web3Service.isLockManager(
+            address,
+            addressToCheck,
+            Number(network)
+          )
+        },
+        staleTime: 1000 * 60,
+        enabled: !!address && !!network,
+      }))
+    ),
+  })
+
+  // check if the user is a manager for any of the locks
+  const isManager = queries.some((query) => query.data)
+  const isPending = queries.some((query) => query.isLoading || query.isFetching)
+
+  return {
+    isManager,
+    isPending,
+    refetch: () => {
+      queries.forEach((query) => query.refetch())
+    },
   }
 }
