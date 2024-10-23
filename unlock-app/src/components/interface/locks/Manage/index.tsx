628adfc6a2bf2c184924be520f335be13bd0e156
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-10-23 21:38:26 +0100

    fix(unlock-app): Fix Overlap Issue Between Dashboard User Menu and Components (#14866)
    * arbitrary z-index for tools popover
    
    * arbitrary z-index for user's menu

--- unlock-app/src/components/interface/locks/Manage/index.tsx
+++ unlock-app/src/components/interface/locks/Manage/index.tsx
@@ -4,7 +4,7 @@ import { MdOutlineTipsAndUpdates } from 'react-icons/md'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { Button, Icon } from '@unlock-protocol/ui'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Fragment, useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { ConnectWalletModal } from '../../ConnectWalletModal'
 import { LockDetailCard } from './elements/LockDetailCard'
 import { Members } from './elements/Members'
@@ -257,7 +257,7 @@ const ToolsMenu = ({ lockAddress, network }: TopActionBarProps) => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute right-0 z-10 max-w-sm px-4 mt-3 transform w-80">
+              <Popover.Panel className="absolute right-0 z-[5] max-w-sm px-4 mt-3 transform w-80">
                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                   <div className="relative grid gap-8 bg-white p-7">
                     <a href={DEMO_URL} target="_blank" rel="noreferrer">
