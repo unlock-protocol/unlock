e6a704c05363109d670a5aad16463c931b82df71
0xTxbi <techiejossy@gmail.com>
2024-10-23 16:07:34 +0100

    update index page

--- unlock-app/src/components/content/HomeContent.tsx
+++ unlock-app/src/components/content/HomeContent.tsx
@@ -1,31 +1,33 @@
 'use client'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import Loading from '../interface/Loading'
 import { Launcher } from '../interface/Launcher'
 import { useSession } from '~/hooks/useSession'
 import { useAuthenticate } from '~/hooks/useAuthenticate'
 import { Container } from '../interface/Container'
 import DashboardHeader from '../interface/layouts/index/DashboardHeader'
 import { ConnectModal } from '../interface/connect/ConnectModal'
+import LocksContent from './lock/LocksContent'
+import { Placeholder } from '@unlock-protocol/ui'
 
 export const HomeContent = () => {
   const { isLoading } = useSession()
-  const router = useRouter()
-  const { account } = useAuthenticate()
 
-  useEffect(() => {
-    if (account) {
-      router.push('/locks')
-    }
-  }, [account, router])
+  const { account } = useAuthenticate()
 
   return (
     <Container>
       <ConnectModal />
       <DashboardHeader />
-      {account && <Loading />}
+      {isLoading && (
+        <Placeholder.Root>
+          <Placeholder.Card />
+        </Placeholder.Root>
+      )}
+      {account && (
+        <div className="flex flex-col gap-4">
+          <LocksContent />
+        </div>
+      )}
       {!account && !isLoading && <Launcher />}
     </Container>
   )
