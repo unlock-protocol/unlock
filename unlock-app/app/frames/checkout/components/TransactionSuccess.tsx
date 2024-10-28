0ff5b9b58177d29f5cb6413972195303d9de0ed5
Julien Genestoux <julien.genestoux@gmail.com>
2024-10-28 16:28:45 -0400

    ensuring the frame content is formatted correctly

--- unlock-app/app/frames/checkout/components/TransactionSuccess.tsx
+++ unlock-app/app/frames/checkout/components/TransactionSuccess.tsx
@@ -3,7 +3,7 @@ import { DefaultImage } from './DefaultImage'
 
 export function TransactionSuccess({ lock }: any) {
   const rightSideContent = (
-    <div tw="flex-1 flex flex-col justify-center items-center ml-4">
+    <div tw="flex flex-col">
       <p tw="text-6xl">Success! Purchased {lock.name} membership!</p>
     </div>
   )
