31432f1720f9c56e8e368d24e96ce8b04f3d8578
Julien Genestoux <julien.genestoux@gmail.com>
2024-10-29 08:31:20 -0400

    fix(unlock-app): ensuring the frame content is formatted correctly (#14886)
    ensuring the frame content is formatted correctly

--- unlock-app/app/frames/checkout/components/DefaultImage.tsx
+++ unlock-app/app/frames/checkout/components/DefaultImage.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Lock } from '../frames'
+import { truncateString } from '~/utils/truncateString'
+import removeMd from 'remove-markdown'
 
 interface DefaultImageProps {
   lock: Lock
@@ -7,27 +9,35 @@ interface DefaultImageProps {
 }
 
 export function DefaultImage({ lock, rightSideContent }: DefaultImageProps) {
-  const { image, defaultImage, name, description, price } = lock
+  const { image, defaultImage, price, name, description } = lock
 
   const defaultRightSideContent = (
-    <div tw="flex-1 flex flex-col ml-4 justify-center">
-      <p tw="text-6xl">{name}</p>
-      <p>{description}</p>
-      <p>{price}</p>
+    <div tw="flex flex-col">
+      <div tw="flex flex-col">
+        <p tw="m-0 p-0 text-6xl overflow-hidden mb-4 max-h-40">
+          {truncateString(name, 34)}
+        </p>
+        <p tw="m-0 p-0 text-2xl mb-4 overflow-hidden">
+          {truncateString(removeMd(description, { useImgAltText: false }), 450)}
+        </p>
+      </div>
+      <p tw="m-0 p-0 text-3xl">🏷️ {price}</p>
     </div>
   )
-
   const leftImage = (
     <img
+      alt="Membership"
       src={defaultImage ? defaultImage : image}
-      tw="flex-1 min-h-full border-4 border-white rounded-lg"
+      tw="min-h-full border-4 border-white rounded-lg"
     />
   )
 
   return (
     <div tw="flex w-full h-full bg-gray-200 p-2">
-      {leftImage}
-      {rightSideContent || defaultRightSideContent}
+      <div tw="flex w-1/2">{leftImage}</div>
+      <div tw="flex flex-col w-1/2 pl-2">
+        {rightSideContent || defaultRightSideContent}
+      </div>
     </div>
   )
 }
