f2d33d3cdaf11159e6b950dadb629c309f616b9f
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-10-30 13:33:21 +0100

    fix(deps): Upgrade `path-to-regexp` to `v8` and update usage (#14890)
    * fix(deps): update dependency path-to-regexp to v8 [security]
    
    * update usage
    
    * upgrade to v8.2.0
    
    * update usage
    
    * update usage
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- locksmith/src/worker/topics.ts
+++ locksmith/src/worker/topics.ts
@@ -1,10 +1,12 @@
 import { pathToRegexp } from 'path-to-regexp'
 
-export const TOPIC_LOCKS = pathToRegexp('/api/hooks/:network/locks')
+export const TOPIC_LOCKS = pathToRegexp('/api/hooks/:network/locks').regexp
 export const TOPIC_KEYS_ON_LOCK = pathToRegexp(
   '/api/hooks/:network/locks/:lock/keys'
-)
-export const TOPIC_KEYS_ON_NETWORK = pathToRegexp('/api/hooks/:network/keys')
+).regexp
+export const TOPIC_KEYS_ON_NETWORK = pathToRegexp(
+  '/api/hooks/:network/keys'
+).regexp
 export const TOPIC_EXPIRED_KEYS_ON_NETWORK = pathToRegexp(
   '/api/hooks/:network/expired-keys'
-)
+).regexp
