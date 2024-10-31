0eec04c5eccff4b3624f374b98c8fd2e9967a5aa
0xTxbi <techiejossy@gmail.com>
2024-10-31 14:57:25 +0100

    update subscriptions and events opengraph images

--- unlock-app/app/event/page.tsx
+++ unlock-app/app/event/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Metadata } from 'next'
 import EventContent from '~/components/content/EventContent'
+import { SHARED_METADATA } from '~/config/seo'
 
 export const metadata: Metadata = {
   title: 'Unlock Events',
   description:
     'Unlock Protocol empowers everyone to create events the true web3 way. Deploy a contract, sell tickets as NFTs, and perform check-in with a dedicated QR code. We got it covered.',
+  openGraph: {
+    ...SHARED_METADATA.openGraph,
+    images: [
+      {
+        alt: 'Event',
+        url: 'https://events.unlock-protocol.com/',
+      },
+    ],
+  },
 }
 
 const EventLandingPage: React.FC = () => {
--- unlock-app/app/subscription/page.tsx
+++ unlock-app/app/subscription/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Metadata } from 'next'
 import SubscriptionContent from '~/components/content/SubscriptionContent'
+import { SHARED_METADATA } from '~/config/seo'
 
 export const metadata: Metadata = {
   title: 'Onchain Subscriptions',
   description:
     'SUBSCRIPTIONS by Unlock Labs is best way for fans to subscribe to writing, podcasts, music, digital art, and creativity onchain with automatically recurring payments.',
+  openGraph: {
+    ...SHARED_METADATA.openGraph,
+    images: [
+      {
+        alt: 'Onchain subscriptions',
+        url: 'https://subscriptions.unlock-protocol.com/',
+      },
+    ],
+  },
 }
 
 const SubscriptionPage: React.FC = () => {
