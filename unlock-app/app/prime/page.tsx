df37d374ddc5c33be559459d8227fad2fade63f5
0xTxbi <techiejossy@gmail.com>
2024-10-31 11:52:56 +0100

    update spread order in prime page

--- unlock-app/app/prime/page.tsx
+++ unlock-app/app/prime/page.tsx
@@ -4,10 +4,10 @@ import PrimeContent from '~/components/interface/prime/PrimeContent'
 import { SHARED_METADATA } from '~/config/seo'
 
 export const metadata: Metadata = {
+  ...SHARED_METADATA,
   title: 'Unlock Prime',
   description:
     'Unlock Prime offers enhanced features, monthly ETH rewards, unlimited events, and exclusive partner discounts for members. Bring your onchain experiences to the next level with a premium membership that works across all the apps from Unlock Labs.',
-  ...SHARED_METADATA,
   openGraph: {
     ...SHARED_METADATA.openGraph,
     images: [
