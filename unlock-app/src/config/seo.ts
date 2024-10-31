93d1469b35c3f33aae62853c4980d8cf02e3ad3b
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-10-31 13:40:06 +0100

    feature(unlock-app): Add default `opengraph` metadata (#14907)
    * add dashboard layout wrapper
    
    * seo/metadata config for app router
    
    * update root layout
    
    * update unlock prime's metadata
    
    * update spread order in prime page

--- unlock-app/src/config/seo.ts
+++ unlock-app/src/config/seo.ts
@@ -1,6 +1,35 @@
 import type { DefaultSeoProps, NextSeoProps } from 'next-seo'
 import { config } from './app'
+import { Metadata } from 'next'
 
+// seo config for app router
+export const SHARED_METADATA: Metadata = {
+  title: {
+    default: 'Unlock App',
+    template: '%s | Unlock App',
+  },
+  description:
+    'Unlock is a protocol which enables creators to monetize their content with a few lines of code in a fully decentralized way.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: config.unlockApp,
+    siteName: 'Unlock App',
+    images: [
+      {
+        url: `${config.unlockStaticUrl}/images/unlock.png`,
+        alt: 'Unlock Protocol',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@UnlockProtocol',
+    creator: '@UnlockProtocol',
+  },
+}
+
+// seo config for pages directory
 export const DEFAULT_SEO: DefaultSeoProps = {
   title: 'Unlock Protocol',
   description: 'Unlock',
