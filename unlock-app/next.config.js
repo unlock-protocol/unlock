38f225d6dc9a567cbcade5e7679a114ddf1f44bc
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-11-01 15:15:27 +0100

    feature(unlock-app): Remove `dashboard` page and configure redirect in Nextjs (#14987)
    * remove dashboard component
    
    * remove dashboard page
    
    * handle redirection in config

--- unlock-app/next.config.js
+++ unlock-app/next.config.js
@@ -44,6 +44,16 @@ const config = {
     // this includes files from the monorepo base directory up
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
+  async redirects() {
+    return [
+      // redirect to locks page
+      {
+        source: '/dashboard',
+        destination: '/locks',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = withSentryConfig(config)
