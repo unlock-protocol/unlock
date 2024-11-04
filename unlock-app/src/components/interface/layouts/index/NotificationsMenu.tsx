245a8a2faac4c6dd512fbb582a35d42c7c089c48
0xTxbi <techiejossy@gmail.com>
2024-11-04 13:20:47 +0100

    more improvements

--- unlock-app/src/components/interface/layouts/index/NotificationsMenu.tsx
+++ unlock-app/src/components/interface/layouts/index/NotificationsMenu.tsx
@@ -1,17 +1,12 @@
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from '@headlessui/react'
-import { Fragment, ReactNode } from 'react'
+import { Menu, MenuButton, MenuItems, Transition } from '@headlessui/react'
+import { Fragment, ReactNode, useState } from 'react'
 import { BiBell as BellIcon } from 'react-icons/bi'
 import { Button } from '@unlock-protocol/ui'
-import { useState } from 'react'
 import { PromptEmailLink } from '../../PromptEmailLink'
 import { useAuthenticate } from '~/hooks/useAuthenticate'
 import { usePathname } from 'next/navigation'
+import { Modal } from '@unlock-protocol/ui'
+import { LoginModal } from '@privy-io/react-auth'
 
 interface NotificationAction {
   label: string
@@ -27,6 +22,7 @@ interface NotificationProps {
 
 export function NotificationsMenu() {
   const [isOpen, setIsOpen] = useState(false)
+  const [showModal, setShowModal] = useState(false)
   const { account, email } = useAuthenticate()
   const pathname = usePathname()
 
@@ -45,70 +41,76 @@ export function NotificationsMenu() {
   if (!email && !['/checkout', '/demo'].includes(pathname)) {
     notifications.push({
       id: '1',
-      content: <PromptEmailLink />,
+      content: <PromptEmailLink setModalOpen={setShowModal} />,
       timestamp: new Date(),
     })
   }
 
   return (
-    <Menu as="div" className="relative mr-4 z-10">
-      {({ open }) => (
-        <>
-          <MenuButton as={Fragment}>
-            <Button
-              variant="borderless"
-              aria-label="Notifications"
-              className="relative md:static"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <BellIcon className="h-6 w-6" />
-              {notifications.length > 0 && (
-                <span className="absolute -top-2 -right-1 h-4 w-4 rounded-full bg-red-500 text-xs text-white flex items-center justify-center">
-                  {notifications.length}
-                </span>
-              )}
-            </Button>
-          </MenuButton>
-
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <MenuItems
-              static
-              className={`absolute right-1/2 translate-x-1/2 mt-2 w-80 ${notifications.length < 3 ? 'h-auto max-h-48' : 'h-96'} origin-top rounded-md divide-y divide-gray-300 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto`}
-            >
-              <div className="p-2">
-                {notifications.length === 0 ? (
-                  <div className="px-4 py-2 text-sm text-gray-500">
-                    No new notifications
-                  </div>
-                ) : (
-                  notifications.map((notification) => (
-                    <MenuItem key={notification.id} as="div">
-                      <div
-                        className="rounded-md px-4 py-3 cursor-pointer hover:bg-gray-100"
-                        onClick={(e) => {
-                          e.preventDefault()
-                          e.stopPropagation()
-                        }}
-                      >
-                        {notification.content}
-                      </div>
-                    </MenuItem>
-                  ))
+    <>
+      <Menu as="div" className="relative mr-4 z-10">
+        {({ open }) => (
+          <>
+            <MenuButton as={Fragment}>
+              <Button
+                variant="borderless"
+                aria-label="Notifications"
+                className="relative md:static"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <BellIcon className="h-6 w-6" />
+                {notifications.length > 0 && (
+                  <span className="absolute -top-2 -right-1 h-4 w-4 rounded-full bg-red-500 text-xs text-white flex items-center justify-center">
+                    {notifications.length}
+                  </span>
                 )}
-              </div>
-            </MenuItems>
-          </Transition>
-        </>
+              </Button>
+            </MenuButton>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                static
+                className={`absolute right-1/2 translate-x-1/2 mt-2 w-80 ${
+                  notifications.length < 3 ? 'h-auto max-h-48' : 'h-96'
+                } origin-top rounded-md divide-y divide-gray-300 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto`}
+              >
+                <div className="p-2">
+                  {notifications.length === 0 ? (
+                    <div className="px-4 py-2 text-sm text-gray-500">
+                      No new notifications
+                    </div>
+                  ) : (
+                    notifications.map((notification) => (
+                      <div key={notification.id}>
+                        <div className="rounded-md px-4 py-3 cursor-pointer hover:bg-gray-100">
+                          {notification.content}
+                        </div>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </MenuItems>
+            </Transition>
+          </>
+        )}
+      </Menu>
+
+      {showModal && (
+        <Modal isOpen={showModal} setIsOpen={setShowModal} size="small">
+          <div onClick={(e) => e.stopPropagation()}>
+            <LoginModal open={true} />
+          </div>
+        </Modal>
       )}
-    </Menu>
+    </>
   )
 }
