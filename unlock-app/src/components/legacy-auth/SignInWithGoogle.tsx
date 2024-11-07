252ff4bf5e366e5341bc9bbcbb10962ddd2304c8
0xTxbi <techiejossy@gmail.com>
2024-11-07 22:13:59 +0100

    update google signin component

--- unlock-app/src/components/legacy-auth/SignInWithGoogle.tsx
+++ unlock-app/src/components/legacy-auth/SignInWithGoogle.tsx
@@ -1,3 +1,53 @@
-export const SignInWithGoogle = ({ setWalletPk }) => {
-  return <>Sign in with Google</>
+import SvgComponents from '../interface/svg'
+
+import { popupCenter } from '~/utils/popup'
+
+import { ConnectButton } from '../interface/connect/Custom'
+import { signIn } from 'next-auth/react'
+
+export interface SignInWithGoogleProps {
+  email: string
+  useIcon?: boolean
+  setWalletPk: (pk: string) => void
+  onNext: () => void
+}
+
+export const SignInWithGoogle = ({
+  email,
+  setWalletPk,
+  onNext,
+}: SignInWithGoogleProps) => {
+  const handleSignWithGoogle = async () => {
+    const popup = popupCenter('/google-sign-in', 'Google Sign In')
+
+    const messageHandler = async (event: MessageEvent) => {
+      if (event.data === 'nextAuthGoogleSignInComplete') {
+        window.removeEventListener('message', messageHandler)
+        await signIn('google', {
+          redirect: false,
+        })
+      }
+    }
+
+    window.addEventListener('message', messageHandler)
+
+    const checkPopup = setInterval(() => {
+      if (popup?.closed) {
+        clearInterval(checkPopup)
+        window.removeEventListener('message', messageHandler)
+      }
+    }, 1000)
+  }
+
+  return (
+    <div className="w-full">
+      <ConnectButton
+        className="w-full"
+        icon={<SvgComponents.Google width={40} height={40} />}
+        onClick={handleSignWithGoogle}
+      >
+        Sign in with Google
+      </ConnectButton>
+    </div>
+  )
 }
