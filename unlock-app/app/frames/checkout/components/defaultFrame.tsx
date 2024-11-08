03cbc888261a40ad52cc3bf00ea53b36737353de
iMac <macharian77@gmail.com>
2024-11-08 15:20:14 +0300

    use fid for address

--- unlock-app/app/frames/checkout/components/defaultFrame.tsx
+++ unlock-app/app/frames/checkout/components/defaultFrame.tsx
@@ -1,32 +1,67 @@
 /* eslint-disable react/jsx-key */
 import React from 'react'
 import { Button } from 'frames.js/next'
+import { getAddressForFid } from 'frames.js'
 import { DefaultImage } from './DefaultImage'
+import { isMember as checkIsMember, getKeyPrice, checkAllowance } from './utils'
 
-export function getDefaultFrame(ctx: any) {
+export async function getDefaultFrame(ctx: any) {
   const lock = ctx.state.lock!
   const isErc20 = !!ctx.state.lock?.tokenAddress
   const approved = ctx.searchParams.approved || lock.erc20Approved || !isErc20
-  const userAddress = ctx.message?.address
-  const buttonText = !userAddress
-    ? 'Continue with wallet'
-    : approved
-      ? 'Mint'
-      : `Approve ${lock.currencySymbol} spending`
+  let userAddress: string
+  const buttonText = approved
+    ? 'Mint'
+    : `Approve ${lock.currencySymbol} spending`
+
+  const fid = ctx.message?.requesterFid
+  if (fid && !lock.priceForUser) {
+    userAddress = await getAddressForFid({
+      fid,
+      options: { fallbackToCustodyAddress: true },
+    })
+
+    const { address: lockAddress, network, tokenAddress } = lock
+
+    const isMember = await checkIsMember(lockAddress, network, userAddress)
+    lock.isMember = isMember
+
+    const keyPrice = await getKeyPrice({
+      lockAddress,
+      network,
+      userAddress,
+    })
+    lock.priceForUser = keyPrice
+
+    if (tokenAddress) {
+      const allowance = await checkAllowance(
+        lockAddress,
+        Number(network),
+        userAddress as string,
+        tokenAddress!
+      )
+      if (Number(allowance) >= Number(keyPrice)) {
+        lock.erc20Approved = true
+      }
+    }
+  }
+
   const buttons =
     !lock.isSoldOut && !lock.isMember
       ? [
-          <Button
-            action="tx"
-            target={
-              !userAddress ? '/sign_message' : approved ? '/txdata' : '/approve'
-            }
-            post_url={
-              !userAddress ? '/' : approved ? '?success=true' : '?approved=true'
-            }
-          >
-            {buttonText}
-          </Button>,
+          fid ? (
+            <Button
+              action="tx"
+              target={approved ? '/txdata' : '/approve'}
+              post_url={approved ? '?success=true' : '?approved=true'}
+            >
+              {buttonText}
+            </Button>
+          ) : (
+            <Button action="post" target="/">
+              Continue
+            </Button>
+          ),
         ]
       : []
 
