6d6c91938ca4cde7a421d3f61340e9d264e1053f
0xTxbi <techiejossy@gmail.com>
2024-11-08 22:02:53 +0100

    sign out nextauth session upon successful privy login

--- unlock-app/src/components/legacy-auth/ConnectToPrivy.tsx
+++ unlock-app/src/components/legacy-auth/ConnectToPrivy.tsx
@@ -3,6 +3,7 @@
 import { useLogin } from '@privy-io/react-auth'
 import { useEffect, useRef } from 'react'
 import { LoginModal } from '@privy-io/react-auth'
+import { signOut as nextAuthSignOut, useSession } from 'next-auth/react'
 
 interface ConnectToPrivyProps {
   userEmail: string
@@ -13,9 +14,15 @@ export default function ConnectToPrivy({
   userEmail,
   onNext,
 }: ConnectToPrivyProps) {
+  const { data: session } = useSession()
+
   const { login } = useLogin({
-    onComplete: () => {
-      // When login is complete, proceed to next step (tab)
+    onComplete: async () => {
+      // When Privy login is complete, sign out of next-auth session
+      if (session) {
+        await nextAuthSignOut({ redirect: false })
+      }
+      // Then proceed to next step
       onNext()
     },
     onError: (error) => {
