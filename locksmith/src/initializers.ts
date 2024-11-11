00f7c802172921612cba8c64fb67802de5b7d669
Julien Genestoux <julien.genestoux@gmail.com>
2024-11-11 14:23:41 -0500

    fixing tests

--- locksmith/src/initializers.ts
+++ locksmith/src/initializers.ts
@@ -4,7 +4,7 @@ import { Web3Service } from '@unlock-protocol/unlock-js'
 const globalAny: any = global
 
 export const getWeb3Service = () => {
-  if (globalAny.web3Service) {
+  if (globalAny.web3Service && 'test' !== process.env?.NODE_ENV) {
     return globalAny.web3Service
   } else {
     globalAny.web3Service = new Web3Service(networks)
