0a98ed14bfc60166ff207aee0d0417fae3d4a747
0xTxbi <techiejossy@gmail.com>
2024-11-15 00:04:59 +0100

    update migration notif modal

--- unlock-app/src/components/legacy-auth/MigrationNotificationModal.tsx
+++ unlock-app/src/components/legacy-auth/MigrationNotificationModal.tsx
@@ -4,18 +4,21 @@ import { usePathname } from 'next/navigation'
 
 export const MigrationModal = ({
   isOpen,
-  setIsOpen,
 }: {
   isOpen: boolean
   setIsOpen: (isOpen: boolean) => void
 }) => {
   const pathname = usePathname()
   const isCheckoutPage = pathname?.includes('checkout')
-  // don't show the modal on the checkout page
-  if (isCheckoutPage) return null
+  const isMigrationPage = pathname?.includes('migrate-user')
+  // don't show the modal on the checkout or migration page
+  if (isCheckoutPage || isMigrationPage) return null
+
+  // no-op function to prevent modal from closing, even by entering the escape key
+  const preventClose = () => {}
 
   return (
-    <Modal isOpen={isOpen} setIsOpen={setIsOpen} size="small">
+    <Modal isOpen={isOpen} setIsOpen={preventClose} size="small" hideCloseIcon>
       <div className="flex flex-col gap-4 text-center">
         <h2 className="text-xl font-bold">Legacy Account Detected</h2>
         <p>
