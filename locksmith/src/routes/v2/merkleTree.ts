df83d4266b84a728e198d74dd64256821ea125db
Julien Genestoux <julien.genestoux@gmail.com>
2024-11-21 12:00:56 -0500

    undue changes

--- locksmith/src/routes/v2/merkleTree.ts
+++ locksmith/src/routes/v2/merkleTree.ts
@@ -1,12 +1,8 @@
 import express from 'express'
-import {
-  createMerkleTree,
-  getMerkleTree,
-} from '../../controllers/v2/merkleTreeController'
+import { createMerkleTree } from '../../controllers/v2/merkleTreeController'
 
 const router: express.Router = express.Router({ mergeParams: true })
 
 router.post('/', createMerkleTree)
-router.get('/:root', getMerkleTree)
 
 export default router
