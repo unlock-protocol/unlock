b681646a9a6d519e1cb87d00be504f1a990347b1
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-11-26 22:24:26 +0100

    feature(unlock-app): Embedded wallet checkout ux (#15131)
    * add hook
    
    * update privy funding component
    
    * update funding notification and payment component

--- unlock-app/src/components/interface/checkout/InsufficientFundsWarning.tsx
+++ unlock-app/src/components/interface/checkout/InsufficientFundsWarning.tsx
@@ -1,5 +1,9 @@
 import { RiArrowRightLine as RightArrowIcon } from 'react-icons/ri'
 import { CheckoutService } from './main/checkoutMachine'
+import { AmountBadge } from './main/Payment'
+import { useSelector } from '@xstate/react'
+import { useEthPrice } from '~/hooks/useEthPrice'
+import { useBaseRoute } from '~/hooks/useCrosschainBaseRoute'
 
 interface InsufficientFundsWarningProps {
   enableCreditCard: boolean
@@ -14,6 +18,25 @@ const InsufficientFundsWarning = ({
   hasCrossChainRoutes,
   checkoutService,
 }: InsufficientFundsWarningProps) => {
+  const { recipients, paywallConfig, keyManagers, lock, renew, data } =
+    useSelector(checkoutService, (state) => state.context)
+
+  const { fundingAmount } = useBaseRoute({
+    lock,
+    recipients,
+    keyManagers,
+    paywallConfig,
+    renew,
+    data,
+  })
+
+  // get dollar equivalent to hint user
+  const { data: ethPrice } = useEthPrice({
+    amount: fundingAmount,
+    network: 8453,
+    currency: 'ETH',
+  })
+
   // Don't render if routes are still loading or if there are cross-chain routes available
   if (isCrossChainRoutesLoading || hasCrossChainRoutes) {
     return null
@@ -24,16 +47,23 @@ const InsufficientFundsWarning = ({
   }
 
   return (
-    <div
-      onClick={() => {
+    <button
+      onClick={(event) => {
+        event.preventDefault()
         checkoutService.send({
           type: 'PRIVY_FUNDING',
         })
       }}
-      className="mt-5 grid w-full p-4 text-left border border-gray-400 rounded-lg shadow cursor-pointer group hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-white"
+      className="mt-5 grid w-full p-4 space-y-2 text-left border border-gray-400 rounded-lg shadow cursor-pointer group hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-white"
     >
-      Fund your account
-      <div className="flex mt-1 items-center justify-between w-full">
+      <div className="flex justify-between w-full">
+        <h3 className="font-bold">Fund your account</h3>
+        <AmountBadge
+          amount={ethPrice?.toString() || '0.00000'}
+          symbol={'USD'}
+        />
+      </div>
+      <div className="flex items-center justify-between w-full">
         <div className="flex items-center w-full text-sm text-left text-gray-500">
           You don&apos;t have enough funds in your account to pay for this.
           Please add funds to continue.
@@ -43,7 +73,7 @@ const InsufficientFundsWarning = ({
           size={20}
         />
       </div>
-    </div>
+    </button>
   )
 }
 
