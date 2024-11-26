0bfb85a44206cdfb858c3a14773a93b45e654ea6
Clément Renaud <clement+git@clementrenaud.com>
2024-11-26 12:57:39 +0100

    add support for lock v15 and unlock v14

--- packages/unlock-js/scripts/compressAbi.js
+++ packages/unlock-js/scripts/compressAbi.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const path = require('path')
 const abis = require('@unlock-protocol/contracts')
 
-const unlockVersions = ['v11', 'v12', 'v13']
-const publicLockVersions = ['v9', 'v10', 'v11', 'v12', 'v13', 'v14']
+const unlockVersions = ['v11', 'v12', 'v13', 'v14']
+const publicLockVersions = ['v9', 'v10', 'v11', 'v12', 'v13', 'v14', 'v15']
 
 const data = {
   PublicLock: publicLockVersions.reduce(
---
+++ packages/unlock-js/src/PublicLock/v15/index.js
@@ -0,0 +1,8 @@
+import abis from '../../abis'
+import v14 from '../v14'
+
+export default {
+  ...v14,
+  version: 'v15',
+  PublicLock: abis.PublicLock.v15,
+}
