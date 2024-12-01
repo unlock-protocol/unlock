a8a33fe12f507bfeb53d5be4bd5d354556fc0d0a
0xTxbi <techiejossy@gmail.com>
2024-12-01 23:16:55 +0100

    support dark mode wip

--- docs/src/components/Card.tsx
+++ docs/src/components/Card.tsx
@@ -13,16 +13,20 @@ interface CardProps {
 export default function Card({ title, description, action }: CardProps) {
   return (
     <div
-      className="bg-white rounded-2xl space-y-4 p-4"
+      className="bg-white bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-80 rounded-2xl space-y-4 p-4"
       style={{ marginBottom: '1.5rem' }}
     >
-      <div className="">
-        <h3 className="text-center">{title}</h3>
+      <div>
+        <h3 className="text-center text-gray-900 dark:text-gray-100">
+          {title}
+        </h3>
       </div>
       <div className="p-4">
-        <p className="text-left">{description}</p>
+        <p className="text-left text-gray-800 dark:text-gray-200">
+          {description}
+        </p>
       </div>
-      <div className="">
+      <div>
         <a href={action.url} className="block w-full">
           <Button className="w-full">{action.title}</Button>
         </a>
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -8,5 +8,19 @@ module.exports = {
     './src/**/*.{tsx,ts,jsx,js,html}',
     './../node_modules/@unlock-protocol/ui/dist/*.{js,css}',
   ],
+  darkMode: ['class', '[data-theme="dark"]'],
   presets: [require('@unlock-protocol/ui/dist/unlock-tailwind-preset')],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'dark-card': '#282a2d',
+      },
+      colors: {
+        gray: {
+          800: '#282a2d',
+          900: '#1a1c1e',
+        },
+      },
+    },
+  },
 }
