bf12e94b425ce36ca0ad93750e4151488ea36df9
Clément Renaud <clement+git@clementrenaud.com>
2024-12-02 16:23:38 +0100

    add prepare tx for v15

--- packages/unlock-js/src/PublicLock/v15/index.js
+++ packages/unlock-js/src/PublicLock/v15/index.js
@@ -2,11 +2,15 @@ import abis from '../../abis'
 import v14 from '../v14'
 import purchaseKey from './purchaseKey'
 import purchaseKeys from './purchaseKeys'
+import preparePurchaseKeysTx from './preparePurchaseKeysTx'
+import preparePurchaseKeyTx from './preparePurchaseKeyTx'
 
 export default {
   ...v14,
   purchaseKey,
   purchaseKeys,
+  preparePurchaseKeysTx,
+  preparePurchaseKeyTx,
   version: 'v15',
   PublicLock: abis.PublicLock.v15,
 }
