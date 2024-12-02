d2f06418ba145de558478a91b68f77a520321782
txbì <46839250+0xTxbi@users.noreply.github.com>
2024-12-02 18:55:23 +0100

    chore(deps): Update `locksmith` dependencies (#15231)
    * fix(deps): update locksmith
    
    * update stripe api version
    
    * update yarn.lock
    
    * update yarn.lock
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- locksmith/src/config/stripe.ts
+++ locksmith/src/config/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import config from '../config/config'
 
 const stripe = new Stripe(config.stripeSecret!, {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2024-11-20.acacia',
 })
 
 export default stripe
