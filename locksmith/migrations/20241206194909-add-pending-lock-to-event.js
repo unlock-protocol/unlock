08ad4e354c4d870e6931d9534907aa6f60e08515
0xTxbi <techiejossy@gmail.com>
2024-12-06 21:13:24 +0100

    update migrations

--- locksmith/migrations/20241206194909-add-pending-lock-to-event.js
+++ locksmith/migrations/20241206194909-add-pending-lock-to-event.js
@@ -1,28 +1,35 @@
 'use strict'
 const table = 'EventData'
-/** @type {import('sequelize-cli').Migration} */
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn(table, 'pendingLockTransaction', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    })
-
-    await queryInterface.addColumn(table, 'pendingLockNetwork', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    })
-
     await queryInterface.addColumn(table, 'lockAddress', {
       type: Sequelize.STRING,
       allowNull: true,
     })
+
+    await queryInterface.addColumn(table, 'pendingTransactionHash', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    })
+
+    await queryInterface.addColumn(table, 'network', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    })
+
+    await queryInterface.addColumn(table, 'isPending', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    })
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn(table, 'pendingLockTransaction')
-    await queryInterface.removeColumn(table, 'pendingLockNetwork')
     await queryInterface.removeColumn(table, 'lockAddress')
+    await queryInterface.removeColumn(table, 'pendingTransactionHash')
+    await queryInterface.removeColumn(table, 'network')
+    await queryInterface.removeColumn(table, 'isPending')
   },
 }
