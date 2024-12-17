2e3202d077fe20b26f015c87d391cc8d2c4358e8
0xTxbi <techiejossy@gmail.com>
2024-12-17 17:10:19 +0100

    dedup auth context

--- unlock-app/src/config/PrivyProvider.tsx
+++ unlock-app/src/config/PrivyProvider.tsx
@@ -185,31 +185,34 @@ export const PrivyMigration = () => {
 }
 
 export const Privy = ({ children }: { children: ReactNode }) => {
+  const [account, setAccount] = useState<string | undefined>(undefined)
   const isMigratePage =
     typeof window !== 'undefined' &&
     window.location.pathname.includes('migrate-user')
 
   return (
-    <PrivyProvider
-      config={{
-        loginMethods: isMigratePage
-          ? ['email']
-          : ['wallet', 'email', 'google', 'farcaster'],
-        embeddedWallets: {
-          createOnLogin: 'off',
-        },
-        appearance: {
-          landingHeader: '',
-        },
-        // @ts-expect-error internal api
-        _render: {
-          standalone: true,
-        },
-      }}
-      appId={config.privyAppId}
-    >
-      <PrivyChild>{children}</PrivyChild>
-    </PrivyProvider>
+    <AuthenticationContext.Provider value={{ account, setAccount }}>
+      <PrivyProvider
+        config={{
+          loginMethods: isMigratePage
+            ? ['email']
+            : ['wallet', 'email', 'google', 'farcaster'],
+          embeddedWallets: {
+            createOnLogin: 'off',
+          },
+          appearance: {
+            landingHeader: '',
+          },
+          // @ts-expect-error internal api
+          _render: {
+            standalone: true,
+          },
+        }}
+        appId={config.privyAppId}
+      >
+        <PrivyChild>{children}</PrivyChild>
+      </PrivyProvider>
+    </AuthenticationContext.Provider>
   )
 }
 
