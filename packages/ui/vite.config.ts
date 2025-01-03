5839d6f525ac90acef5900e657caf63cc65379f2
0xTxbi <techiejossy@gmail.com>
2025-01-03 13:17:03 +0100

    update ui vite config

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -22,28 +22,67 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'lib/index.tsx'),
       name: '@unlock-protocol/ui',
       fileName: (format) => `index.${format}.js`,
+      formats: isCDN ? ['umd'] : ['es'],
     },
     rollupOptions: {
-      external: isCDN ? Object.keys(peerDependencies) : external,
+      external: [
+        ...(isCDN ? Object.keys(peerDependencies) : external),
+        'react/jsx-runtime',
+      ],
       shimMissingExports: true,
       output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+        },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) {
             return 'style.css'
           }
-          // Return a default name if assetInfo.name is undefined
           return assetInfo.name ?? 'asset'
         },
+        exports: 'named',
+        ...(isCDN
+          ? {}
+          : {
+              preserveModules: true,
+              preserveModulesRoot: 'lib',
+              format: 'es',
+            }),
       },
     },
     sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/],
+      requireReturnsDefault: 'auto',
+    },
   },
   plugins: [
     tsconfigPaths(),
-    react(),
+    react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: 'react',
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+        ],
+      },
+    }),
     svgr(),
     nodePolyfills({
       protocolImports: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './lib'),
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
 })
