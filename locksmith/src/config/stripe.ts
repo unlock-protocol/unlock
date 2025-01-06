d5b95de3a1196ad502ceacdeb0f86242a05ded96
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-01-06 12:19:23 +0100

    chore(deps): Update `locksmith` dependencies (#15357)
    * fix(deps): update locksmith
    
    * update stripe api version
    
    * update paywall typing ref
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- locksmith/src/config/stripe.ts
+++ locksmith/src/config/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import config from '../config/config'
 
 const stripe = new Stripe(config.stripeSecret!, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2024-12-18.acacia',
 })
 
 export default stripe
