015ebc742f580865267c0966a6c6e102c3b9fc03
0xTxbi <techiejossy@gmail.com>
2025-01-08 12:24:20 +0100

    update app storage hook

--- unlock-app/src/hooks/useAppStorage.ts
+++ unlock-app/src/hooks/useAppStorage.ts
@@ -2,60 +2,75 @@ import { useCallback } from 'react'
 
 export const APP_NAME = '@unlock-app'
 
-export function useAppStorage() {
-  const isObject = useCallback((value: any) => typeof value === 'object', [])
-
-  const getKey = useCallback((key: string, withAppName = true) => {
-    return withAppName ? `${APP_NAME}.${key}` : key
-  }, [])
-
-  const setStorage = useCallback((key: string, value: any) => {
-    const currentValue = getStorage(key)
-    if (currentValue === value) return false
-    try {
-      localStorage.setItem(
-        getKey(key),
-        isObject(value) ? JSON.stringify(value) : value
-      )
-    } catch (error) {
-      console.error(error)
-    }
+/**
+ * Store objects as JSON, so it's easier to just always try parse, then fallback to the raw value on parse errors.
+ */
+function parseIfNeeded(value: string) {
+  try {
+    return JSON.parse(value)
+  } catch {
+    // If not valid JSON, just return the original string
     return value
-  }, [])
+  }
+}
 
-  const removeKey = useCallback((key: string, withAppName = true) => {
-    try {
-      localStorage.removeItem(getKey(key, withAppName))
-    } catch (error) {
-      console.error(error)
-    }
-  }, [])
+function getKey(key: string, withAppName = true) {
+  return withAppName ? `${APP_NAME}.${key}` : key
+}
 
-  const getStorage = useCallback((key: string): string | null => {
-    if (typeof window === 'undefined') return null
-    try {
-      const value = localStorage.getItem(getKey(key))
-      if (!value) return null
-      return isObject(value) ? JSON.parse(value) : value
-    } catch (error) {
-      console.error(error)
-    }
+export function getLocalStorageItem(key: string, withAppName = true) {
+  if (typeof window === 'undefined') return null
+  try {
+    const stored = localStorage.getItem(getKey(key, withAppName))
+    if (!stored) return null
+    return parseIfNeeded(stored)
+  } catch (error) {
+    console.error(error)
     return null
-  }, [])
+  }
+}
 
-  const clearStorage = useCallback(
-    (clearItems: string[], addAppName: boolean): void => {
-      for (const item of clearItems) {
-        removeKey(item, addAppName)
-      }
-    },
-    []
-  )
+export function setLocalStorageItem(
+  key: string,
+  value: any,
+  withAppName = true
+) {
+  try {
+    localStorage.setItem(
+      getKey(key, withAppName),
+      typeof value === 'object' ? JSON.stringify(value) : String(value)
+    )
+    return value
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export function deleteLocalStorageItem(key: string, withAppName = true) {
+  try {
+    localStorage.removeItem(getKey(key, withAppName))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export function clearLocalStorage(keys: string[], withAppName = true) {
+  for (const key of keys) {
+    deleteLocalStorageItem(key, withAppName)
+  }
+}
+
+export function useAppStorage() {
+  const getStorage = useCallback(getLocalStorageItem, [])
+  const setStorage = useCallback(setLocalStorageItem, [])
+  const removeKey = useCallback(deleteLocalStorageItem, [])
+  const clearStorage = useCallback(clearLocalStorage, [])
 
   return {
-    setStorage,
     getStorage,
-    clearStorage,
+    setStorage,
     removeKey,
+    clearStorage,
   }
 }
