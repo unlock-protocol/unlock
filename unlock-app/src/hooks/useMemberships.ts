ac73b13ac5ed05a5a1d2bcd6c8fb1712eb319049
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-02-04 17:06:24 +0100

    feature(unlock-app): Add `tokenId` to checkout machine and deduplicate retrieval (#15484)
    * update checkout machine
    
    * update connected component
    
    * update useMembership hook
    
    * update crosschain route hook to use checkout context
    
    * update returning component
    
    * update confirmcrossmint component
    
    * update useLock hook
    
    * refactor memberships hook
    
    * update checkoutmachine's mint event
    
    * update minting component
    
    * fix ci

--- unlock-app/src/hooks/useMemberships.ts
+++ unlock-app/src/hooks/useMemberships.ts
@@ -17,9 +17,10 @@ export const getMembership = async (
   account: string,
   lockNetwork: number
 ) => {
-  const [member, total] = await Promise.all([
+  const [member, total, tokenId] = await Promise.all([
     web3Service.getHasValidKey(lockAddress, account!, lockNetwork),
     web3Service.totalKeys(lockAddress, account!, lockNetwork),
+    web3Service.getTokenIdForOwner(lockAddress, account!, lockNetwork),
   ])
   const expired = !member && total > 0
   return {
@@ -27,6 +28,7 @@ export const getMembership = async (
     expired,
     member,
     network: lockNetwork,
+    tokenId: tokenId ? tokenId.toString() : null,
   }
 }
 
@@ -44,21 +46,11 @@ export const getMemberships = async (
           expired: false,
           member: false,
           network: props.network || network,
+          tokenId: null,
         }
       }
       const lockNetwork = props.network || network
-      const [member, total] = await Promise.all([
-        web3Service.getHasValidKey(lockAddress, account!, lockNetwork),
-        web3Service.totalKeys(lockAddress, account!, lockNetwork),
-      ])
-      // if not member but total is above 0
-      const expired = !member && total > 0
-      return {
-        lock: lockAddress,
-        expired,
-        member,
-        network: lockNetwork,
-      }
+      return getMembership(web3Service, lockAddress, account, lockNetwork)
     })
   )
 }
