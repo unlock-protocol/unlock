e5946ebad2fbbe975aa381da9bb3405d9ec80c89
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-02-06 15:42:03 +0100

    feature(unlock-app): upgrade to `@sentry/nextjs` `v8` (#15500)
    * fix(deps): update dependency @sentry/nextjs to v8
    
    * add instrumentation
    
    * add config for sentry edge
    
    * linting – remove unecessary escape character
    
    * bump zod
    
    * update yarn.lock
    
    * update config
    
    * update config
    
    * update edge config
    
    * cleanup
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ unlock-app/instrumentation.js
@@ -0,0 +1,16 @@
+import * as Sentry from '@sentry/nextjs'
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config')
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config')
+  }
+}
+
+/**
+ * export onRequestError to capture request errors in nested React Server components
+ */
+export const onRequestError = Sentry.captureRequestError
