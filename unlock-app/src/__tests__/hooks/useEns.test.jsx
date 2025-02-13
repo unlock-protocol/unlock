a3024351a984d45cd4b94b6aef62d021e66b3401
Julien Genestoux <julien.genestoux@gmail.com>
2025-02-13 12:22:23 -0500

    using useNameResolver where applicable (#15522)

--- unlock-app/src/__tests__/hooks/useEns.test.jsx
+++ unlock-app/src/__tests__/hooks/useEns.test.jsx
@@ -1,33 +1,11 @@
 import { ethers } from 'ethers'
 import {
-  getNameOrAddressForAddress,
   getAddressForName,
 } from '../../hooks/useEns'
 import { vi, it, describe, beforeAll } from 'vitest'
 
 vi.mock('ethers')
 
-describe('getNameOrAddressForAddress', () => {
-  beforeAll(() => {
-    const fn = vi.fn(() => {
-      return {
-        lookupAddress: () => {
-          return 'julien51.eth'
-        },
-      }
-    })
-    ethers.JsonRpcProvider = fn
-    ethers.JsonRpcProvider = fn
-  })
-
-  it('should yield the name if there is one', async () => {
-    expect.assertions(1)
-    const name = await getNameOrAddressForAddress(
-      '0xE5Cd62AC8d2Ca2A62a04958f07Dd239c1Ffe1a9E'
-    )
-    expect(name).toEqual('julien51.eth')
-  })
-})
 
 describe('getAddressForName', () => {
   beforeAll(() => {
