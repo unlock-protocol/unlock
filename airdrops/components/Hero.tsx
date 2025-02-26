14b41f42b322e30a72666d664c675787d0aa4423
0xtxbi <techiejossy@gmail.com>
2025-02-26 17:06:40 +0100

    update hero

--- airdrops/components/Hero.tsx
+++ airdrops/components/Hero.tsx
@@ -18,8 +18,9 @@ export default function Hero() {
             <p className="text-xl text-gray-600 mb-6">
               The Unlock Protocol belongs to its community of creators,
               developers, and users. Contribute code, docs, or just use the
-              protocol and claim UP tokens. Connect your wallet to check your
-              eligibility for various airdrops.
+              protocol and claim UP tokens.
+              {!authenticated &&
+                ' Connect your wallet to check your eligibility for various airdrops.'}
             </p>
             <div className="flex gap-4 justify-center">
               {!authenticated && <ConnectButton />}
