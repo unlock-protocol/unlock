c6f6af8fcccf085a2cd0aeca14da3cd6e063aa10
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-02-27 14:04:52 +0100

    feature(airdrops): Add seo and centralized config (#15571)
    * add app config
    
    * add seo config
    
    * update header and footer links
    
    * update root layout
    
    * update index page
    
    * update index page
    
    * update campaign page

--- airdrops/app/page.tsx
+++ airdrops/app/page.tsx
@@ -1,5 +1,11 @@
 import Campaigns from '../components/Campaigns'
 import Hero from '../components/Hero'
+import { Metadata } from 'next'
+import { config } from '../src/config/app'
+
+export const metadata: Metadata = {
+  title: `${config.appName.default} | ${config.appName.brand}`,
+}
 
 export default function Home() {
   return (
