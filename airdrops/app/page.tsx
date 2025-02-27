cf0c41c0bc2e10b04a055c35677f0c392636c8e9
0xtxbi <techiejossy@gmail.com>
2025-02-27 12:48:31 +0100

    update index page

--- airdrops/app/page.tsx
+++ airdrops/app/page.tsx
@@ -1,5 +1,20 @@
 import Campaigns from '../components/Campaigns'
 import Hero from '../components/Hero'
+import { Metadata } from 'next'
+import { config } from '../src/config/app'
+
+export const metadata: Metadata = {
+  title: `${config.appName.default} | ${config.appName.brand}`,
+  description: config.descriptions.default,
+  openGraph: {
+    title: `${config.appName.default} | ${config.appName.brand}`,
+    description: config.descriptions.default,
+  },
+  twitter: {
+    title: `${config.appName.default} | ${config.appName.brand}`,
+    description: config.descriptions.default,
+  },
+}
 
 export default function Home() {
   return (
