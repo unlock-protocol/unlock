75051ab4a5143fc34242a45762d4cf4e4c21b48b
Julien Genestoux <julien.genestoux@gmail.com>
2025-03-03 18:34:57 -0500

    handling already claimed tokens

--- airdrops/components/CampaignCard.tsx
+++ airdrops/components/CampaignCard.tsx
@@ -3,21 +3,24 @@ import Link from 'next/link'
 import { AirdropData } from './Campaigns'
 import { ethers } from 'ethers'
 import { useEligibility } from './hooks/useEligibility'
+import { usePrivy } from '@privy-io/react-auth'
 
 interface CampaignCardProps {
   airdrop: AirdropData
-  authenticated: boolean
-  eligible?: number
 }
 
-const CampaignCardInternal = ({
-  eligible,
-  airdrop: { name, description, token, contractAddress, url },
-  authenticated,
-}: CampaignCardProps) => {
+const CampaignCardInternal = ({ airdrop }: CampaignCardProps) => {
+  const {
+    data: { eligible, claimed },
+  } = useEligibility(airdrop)
+  const { authenticated } = usePrivy()
+
+  const { name, description, token, contractAddress, url } = airdrop
+
   const eligibleFormatted = eligible
     ? ethers.formatUnits(eligible, token?.decimals)
     : 0
+
   return (
     <div className="space-y-4 md:min-w-96 block min-h-48 p-6 border min-w-[24rem] sm:min-w-[28rem] rounded-xl transition-all duration-200">
       <h3 className="text-xl font-medium">{name}</h3>
@@ -25,18 +28,24 @@ const CampaignCardInternal = ({
       <div className="flex items-center justify-between">
         {authenticated && contractAddress && (
           <>
-            <Button disabled={!authenticated || !eligible}>
-              {eligible > 0 ? 'Claim Rewards' : 'Not Eligible'}
-            </Button>
-            <div
-              className={`text-sm font-medium ${
-                eligible > 0 ? 'text-green-600' : 'text-gray-500'
-              }`}
-            >
-              {eligible > 0
-                ? `Eligible for ${Number(eligibleFormatted).toLocaleString()} UP`
-                : ''}
-            </div>
+            {Number(eligibleFormatted) > 0 && !claimed && (
+              <>
+                <Button disabled={!eligible}>Claim Rewards</Button>
+                <div className="text-sm font-medium text-green-600">
+                  Eligible for ${Number(eligibleFormatted).toLocaleString()} UP
+                </div>
+              </>
+            )}
+            {Number(eligibleFormatted) > 0 && claimed && (
+              <div className="text-sm font-medium text-gray-500">
+                🥳 You have claimed your UP!
+              </div>
+            )}
+            {Number(eligibleFormatted) === 0 && (
+              <div className="text-sm font-medium text-gray-500">
+                😔 You are not eligible
+              </div>
+            )}
           </>
         )}
         {url && (
@@ -53,30 +62,23 @@ const CampaignCardInternal = ({
   )
 }
 
-export const CampaignCard = ({ airdrop, authenticated }: CampaignCardProps) => {
-  const { data: eligible } = useEligibility(airdrop)
+export const CampaignCard = ({ airdrop }: CampaignCardProps) => {
+  const { authenticated } = usePrivy()
+  const {
+    data: { eligible, claimed },
+  } = useEligibility(airdrop)
 
   if (!eligible || !authenticated) {
-    return (
-      <CampaignCardInternal airdrop={airdrop} authenticated={authenticated} />
-    )
+    return <CampaignCardInternal airdrop={airdrop} />
+  } else if (claimed) {
+    return <CampaignCardInternal airdrop={airdrop} />
   } else {
     return (
       <Link
         href={`/campaigns/${airdrop.id}`}
-        className={`${
-          authenticated
-            ? eligible > 0
-              ? 'hover:border-brand-ui-primary'
-              : 'opacity-50 cursor-not-allowed hover:border-gray-200'
-            : ''
-        }`}
+        className="hover:border-brand-ui-primary"
       >
-        <CampaignCardInternal
-          airdrop={airdrop}
-          eligible={eligible}
-          authenticated={authenticated}
-        />
+        <CampaignCardInternal airdrop={airdrop} />
       </Link>
     )
   }
--- airdrops/components/hooks/useEligibility.ts
+++ airdrops/components/hooks/useEligibility.ts
@@ -1,6 +1,7 @@
 import { isEligible } from '../../src/utils/eligibility'
 import { usePrivy, useWallets } from '@privy-io/react-auth'
 import { useQuery } from '@tanstack/react-query'
+import { hasClaimed } from '../CampaignDetailContent'
 
 export const useEligibility = (airdrop) => {
   const { authenticated } = usePrivy()
@@ -8,9 +9,21 @@ export const useEligibility = (airdrop) => {
 
   return useQuery({
     queryKey: ['useEligibility', wallets, airdrop],
-    queryFn: () => {
-      return isEligible(wallets[0].address, airdrop)
+    queryFn: async () => {
+      const eligible = await isEligible(wallets[0].address, airdrop)
+      if (eligible) {
+        const wasClaimed = await hasClaimed(
+          wallets[0].address,
+          eligible,
+          airdrop
+        )
+        if (wasClaimed) {
+          return { eligible, claimed: true }
+        }
+      }
+      return { eligible, wasClaimed: false }
     },
+    initialData: { eligible: '0', wasClaimed: false },
     enabled: !!(authenticated && wallets[0]),
     refetchOnWindowFocus: false,
   })
