46cfcb74404d6d25a2d9c462c11283f74e321c48
Clément Renaud <clement+git@clementrenaud.com>
2025-03-03 15:58:39 +0100

    use warnings

--- packages/networks/bin/check-keys.ts
+++ packages/networks/bin/check-keys.ts
@@ -3,15 +3,15 @@ import { log } from './logger'
 import { validateKeys } from './utils/keys'
 
 const run = () => {
-  let hasErrors = false
+  const hasErrors = false
   Object.keys(networks).forEach((chainId) => {
     if (chainId !== '31337') {
       const missingProperties = validateKeys(networks[chainId])
       if (missingProperties.length !== 0) {
-        hasErrors = true
+        // missing keys only throw warnings, not errors
         missingProperties.forEach((missingProperty) => {
           log(
-            `[Networks/Keys]: ${networks[chainId].name} -  ❌ Missing property ${missingProperty}`,
+            `[Networks/Keys]: ${networks[chainId].name} -  ⚠️ Missing property ${missingProperty}`,
             'info'
           )
         })
