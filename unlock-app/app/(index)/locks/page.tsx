44d35bd159a4ea17cf042ad2aed1b773d700064d
0xtxbi <techiejossy@gmail.com>
2025-03-06 17:27:35 +0100

    more cleanup

--- unlock-app/app/(index)/locks/page.tsx
+++ unlock-app/app/(index)/locks/page.tsx
@@ -1,13 +1,7 @@
 'use client'
 
-import { Suspense } from 'react'
-import LocksLoading from '~/components/interface/layouts/LocksLoading'
 import { LockList } from '~/components/interface/locks/List/elements/LockList'
 
 export default function LocksPage() {
-  return (
-    <Suspense fallback={<LocksLoading />}>
-      <LockList />
-    </Suspense>
-  )
+  return <LockList />
 }
