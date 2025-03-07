5d75cb18afbfdf48f5828bf3da1de82dec9e4598
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-03-07 17:59:14 +0100

    feature(unlock-app): `useAuthenticate` loading state (#15695)
    * update hook
    
    * update components
    
    * include locksmith auth state
    
    * improved comments

--- unlock-app/app/Components/ProtectedContent.tsx
+++ unlock-app/app/Components/ProtectedContent.tsx
@@ -3,13 +3,18 @@
 import { ReactNode } from 'react'
 import { WalletNotConnected } from '~/components/interface/layouts/index/WalletNotConnected'
 import { useAuthenticate } from '~/hooks/useAuthenticate'
+import { LoadingIcon } from '~/components/interface/Loading'
 
 interface AuthRequiredProps {
   children: ReactNode
 }
 
 export function AuthRequired({ children }: AuthRequiredProps) {
-  const { account } = useAuthenticate()
+  const { account, isLoading } = useAuthenticate()
+
+  if (isLoading) {
+    return <LoadingIcon size={40} />
+  }
 
   if (!account) {
     return <WalletNotConnected />
