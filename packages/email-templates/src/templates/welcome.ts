43c0be5bc92fe3896335af883fbc84d1e7b9119f
Julien Genestoux <julien.genestoux@gmail.com>
2025-03-11 20:20:00 -0400

    more cleanup

--- packages/email-templates/src/templates/welcome.ts
+++ packages/email-templates/src/templates/welcome.ts
@@ -5,7 +5,7 @@ export default prepareAll({
   html: `<h1>Welcome to Unlock!</h1> 
     
     <p>We're excited to have you with us!</p>
-    <p>If you have any questions or need assistance, our support team is here to help. You can reach us via <a href="https://discord.com/invite/ueJDH2qwRs">Discord</a>.</p>
+    <p>If you have any questions or need assistance, our support team is here to help. You can reach us via <a href="https://discord.unlock-protocol.com/">Discord</a>.</p>
     <p>Welcome aboard, and we look forward to serving you!</p>
     <p>Best Regards,</p>
     <p>The Unlock Team</p>
