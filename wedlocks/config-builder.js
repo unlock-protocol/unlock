bb0a2cf17e9c09c3cc4a6748192c840753636d0f
0xtxbi <techiejossy@gmail.com>
2025-03-19 13:19:55 +0100

    update config builder

--- wedlocks/config-builder.js
+++ wedlocks/config-builder.js
@@ -42,10 +42,12 @@ console.log(
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT || 587,
     secure: false,
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
+    credentials: {
+      username: process.env.SMTP_USERNAME,
+      password: process.env.SMTP_PASSWORD,
     },
+    authType: 'plain',
+
     unlockEnv,
     wedlocksPrivateKey,
     sender: process.env.SMTP_FROM_ADDRESS || 'hello@unlock-protocol.com',
