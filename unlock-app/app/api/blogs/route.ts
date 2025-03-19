1400d32bd0719084b52be8b0be08e6c8b1b44373
= <macharian77@gmail.com>
2025-03-19 21:17:19 +0300

    fix ci

--- unlock-app/app/api/blogs/route.ts
+++ unlock-app/app/api/blogs/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import axios from 'axios'
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const { data } = await axios.get('https://unlock-protocol.com/blog.rss')
     return NextResponse.json(data)
