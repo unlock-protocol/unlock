95be197d40a95ce8ac795049feee3bb61c1a38af
Clément Renaud <clement+git@clementrenaud.com>
2025-03-24 15:59:35 +0100

    hardcode values in contract

--- smart-contracts/contracts/utils/UnlockDAOArbitrumBridge.sol
+++ smart-contracts/contracts/utils/UnlockDAOArbitrumBridge.sol
@@ -1,6 +1,23 @@
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../interfaces/IUniversalRouter.sol";
-import "../interfaces/IWETH.sol";
+import {IUniversalRouter} from "../interfaces/IUniversalRouter.sol";
+import {IWETH} from "../interfaces/IWETH.sol";
+
+interface IQuoterV2 {
+  function quoteExactInputSingle(
+    address tokenIn,
+    address tokenOut,
+    uint24 fee,
+    uint256 amountIn,
+    uint160 sqrtPriceLimitX96
+  )
+    external
+    returns (
+      uint256 amountOut,
+      uint160 sqrtPriceX96After,
+      uint32 initializedTicksCrossed,
+      uint256 gasEstimate
+    );
+}
 
 interface IArbSys {
   /**
@@ -74,47 +91,52 @@ contract UnlockDAOArbitrumBridge {
   IArbSys public immutable ARB_SYS =
     IArbSys(0x0000000000000000000000000000000000000064);
 
+  IQuoterV2 public immutable QUOTER_V2;
   IUniversalRouter public immutable UNISWAP_UNIVERSAL_ROUTER;
   IL2GatewayRouter public immutable GATEWAY_ROUTER;
-  address public immutable L1_TIMELOCK;
+  IWETH public immutable L2_WETH;
+  IERC20 public immutable L2_ARB_TOKEN;
   address public immutable L1_UDT;
-  address public immutable L2_ARB_TOKEN;
-  address public immutable L2_WETH;
+  address public immutable L1_TIMELOCK;
 
-  constructor(
-    address universalRouter,
-    address gatewayRouter,
-    address l2Weth,
-    address l2Arb,
-    address l1Udt,
-    address l1Timelock
-  ) {
-    UNISWAP_UNIVERSAL_ROUTER = IUniversalRouter(universalRouter);
-    GATEWAY_ROUTER = IL2GatewayRouter(gatewayRouter);
-    L2_WETH = l2Weth;
-    L2_ARB_TOKEN = l2Arb;
-    L1_UDT = l1Udt;
-    L1_TIMELOCK = l1Timelock;
+  constructor(address l1Timelock) {
+    UNISWAP_UNIVERSAL_ROUTER = IUniversalRouter(
+      0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5
+    ); // Arb UniversalRouter
+    GATEWAY_ROUTER = IL2GatewayRouter(
+      0x5288c571Fd7aD117beA99bF60FE0846C4E84F933
+    ); // Arb Gateway Router
+    QUOTER_V2 = IQuoterV2(0x61fFE014bA17989E743c5F6cB21bF9697530B21e); // Arbitrum Quoter V2
+    L2_WETH = IWETH(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1); // Arb WETH
+    L2_ARB_TOKEN = IERC20(0x912CE59144191C1204E64559FE8253a0e49E6548); // ARB token on Arb
+    L1_UDT = 0x90DE74265a416e1393A450752175AED98fe11517; // UDT token on Mainnet
+    L1_TIMELOCK = l1Timelock; //0x17EEDFb0a6E6e06E95B3A1F928dc4024240BC76B;
   }
 
   /**
    * @notice Swaps ARB tokens for ETH and bridges both ETH and UDT tokens from L2 (Arbitrum) back to L1 (Mainnet)
-   * This function:
-   * 1. Swaps ARB tokens to WETH using Uniswap V3
-   * 2. Unwraps WETH to ETH
-   * 3. Withdraws all ETH balance to the L1 timelock
-   * @param amountOutMinimum The minimum amount of WETH to receive from the ARB swap
-   * @dev Can only be called by the L2 timelock alias address
    */
-  function swapAndBridgeArb(uint amountOutMinimum) external {
+  function swapAndBridgeArb() external {
     // swap arb tokens to WETH
-    uint arbBalance = IERC20(L2_ARB_TOKEN).balanceOf(address(this)); // send tokens to universal router to manipulate the token
+    uint arbBalance = IERC20(L2_ARB_TOKEN).balanceOf(address(this));
 
     // send tokens to universal router to manipulate the token
     IERC20(L2_ARB_TOKEN).transfer(
       address(UNISWAP_UNIVERSAL_ROUTER),
       arbBalance
     );
+
+    // Get quote for ARB -> WETH swap
+    (uint256 amountOut, , , ) = QUOTER_V2.quoteExactInputSingle(
+      address(L2_ARB_TOKEN),
+      address(L2_WETH),
+      uint24(3000), // 0.3% fee tier
+      arbBalance,
+      0 // no price limit
+    );
+
+    // Apply 2% slippage tolerance
+    uint amountOutMinimum = (amountOut * 98) / 100;
 
     // encode the V3 swap command
     bytes memory commands = new bytes(1);
@@ -127,9 +149,9 @@ contract UnlockDAOArbitrumBridge {
       arbBalance, // amount in
       amountOutMinimum, // amount out minimum
       abi.encodePacked(
-        L2_ARB_TOKEN, // token in
+        address(L2_ARB_TOKEN), // token in
         uint24(3000), // fee tier (0.3%)
-        L2_WETH // token out (WETH)
+        address(L2_WETH) // token out (WETH)
       ),
       false // use stored tokens in router
     );
@@ -138,8 +160,8 @@ contract UnlockDAOArbitrumBridge {
     UNISWAP_UNIVERSAL_ROUTER.execute(commands, inputs);
 
     // unwrap WETH
-    uint wethBalance = IERC20(L2_WETH).balanceOf(address(this));
-    IWETH(L2_WETH).withdraw(wethBalance);
+    uint wethBalance = L2_WETH.balanceOf(address(this));
+    L2_WETH.withdraw(wethBalance);
 
     // send native tokens to L1
     uint nativeBalance = address(this).balance;
