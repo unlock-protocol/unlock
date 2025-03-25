1da658bfe4369cf16d6d50a604c4f696d7a6e648
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-03-25 16:55:51 +0100

    feature(unlock-app): Update conditional rendering for email template settings (#15752)

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { ZeroAddress } from 'ethers'
 
 const LockTypeSchema = z.object({
   isEvent: z.boolean().default(false),
@@ -22,3 +23,36 @@ export const getLockTypeByMetadata = (metadata?: any): LockType => {
   }
 }
 export type LockType = z.infer<typeof LockTypeSchema>
+
+/**
+ * Determines if a lock is a subscription-type lock
+ * @param lock - The lock object
+ * @returns boolean - True if the lock is a subscription
+ */
+export const isLockForSub = (
+  lock:
+    | {
+        keyPrice?: string
+        currencyContractAddress?: string | null
+        expirationDuration?: number
+      }
+    | null
+    | undefined
+): boolean => {
+  if (!lock) return false
+
+  const isFree = Number(lock?.keyPrice) === 0
+  const isNative =
+    !lock?.currencyContractAddress ||
+    lock.currencyContractAddress === ZeroAddress
+  const isExpiring =
+    typeof lock.expirationDuration === 'number' &&
+    lock.expirationDuration !== -1 &&
+    lock.expirationDuration < 60 * 60 * 24 * 365 * 100
+
+  if (isFree || isNative || !isExpiring) {
+    return false
+  }
+
+  return true
+}
