bbd8f2f7b938e5e90d0911bf630b758a17a747e2
= <macharian77@gmail.com>
2025-03-26 17:09:33 +0300

    utility fns for calldata

--- unlock-app/app/frames/checkout/approve/route.ts
+++ unlock-app/app/frames/checkout/approve/route.ts
@@ -1,6 +1,7 @@
-import { Abi, encodeFunctionData, erc20Abi } from 'viem'
 import { frames } from '../frames'
 import { transaction } from 'frames.js/core'
+import { Web3Service } from '@unlock-protocol/unlock-js'
+import networks from '@unlock-protocol/networks'
 
 export const POST = frames(async (ctx) => {
   if (!ctx?.message) {
@@ -9,21 +10,23 @@ export const POST = frames(async (ctx) => {
 
   const lock = ctx.state.lock!
   const { address: lockAddress, network, priceForUser } = lock
+  const userAddress = ctx.message.address!
 
-  const calldata = encodeFunctionData({
-    abi: erc20Abi,
-    functionName: 'approve',
-    args: [lockAddress as `0x${string}`, BigInt(priceForUser!)],
+  const web3Service = new Web3Service(networks)
+
+  const calldata = await web3Service.purchaseKey({
+    lockAddress,
+    network,
+    params: {
+      lockAddress,
+      owner: userAddress,
+      totalApproval: priceForUser,
+    },
   })
 
   return transaction({
     chainId: `eip155:${network}`,
     method: 'eth_sendTransaction',
-    params: {
-      abi: erc20Abi as Abi,
-      to: ctx.state.lock?.tokenAddress as `0x${string}`,
-      data: calldata,
-      value: '0x0',
-    },
+    params: calldata[0],
   })
 })
--- unlock-app/app/frames/checkout/txdata/route.ts
+++ unlock-app/app/frames/checkout/txdata/route.ts
@@ -1,7 +1,7 @@
-import { Abi, encodeFunctionData } from 'viem'
 import { frames } from '../frames'
 import { transaction } from 'frames.js/core'
-import { PublicLock } from '@unlock-protocol/contracts'
+import { Web3Service } from '@unlock-protocol/unlock-js'
+import networks from '@unlock-protocol/networks'
 
 export const POST = frames(async (ctx) => {
   if (!ctx?.message) {
@@ -11,22 +11,21 @@ export const POST = frames(async (ctx) => {
   const userAddress = ctx.message.address!
   const { address: lockAddress, priceForUser } = ctx.state.lock!
   const network = Number(ctx.state.lock!.network)
-  const abi = PublicLock.abi
 
-  const calldata = encodeFunctionData({
-    abi,
-    functionName: 'purchase',
-    args: [[priceForUser], [userAddress], [userAddress], [userAddress], ['0x']],
+  const web3Service = new Web3Service(networks)
+
+  const calldata = await web3Service.purchaseKey({
+    lockAddress,
+    network,
+    params: {
+      lockAddress,
+      owner: userAddress,
+    },
   })
 
   return transaction({
     chainId: `eip155:${network}`,
     method: 'eth_sendTransaction',
-    params: {
-      abi: abi as Abi,
-      to: lockAddress as `0x${string}`,
-      data: calldata,
-      value: priceForUser,
-    },
+    params: calldata[calldata.length - 1],
   })
 })
