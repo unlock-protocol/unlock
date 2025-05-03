c7e23074d9ccd29a7b4c40c3f5572ff4f026ec81
txbì <46839250+0xTxbi@users.noreply.github.com>
2025-05-03 11:21:37 +0100

    chore(deps): update dependency stripe to v18 (#15886)
    * fix(deps): update dependency stripe to v18
    
    * update api version
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- locksmith/src/config/stripe.ts
+++ locksmith/src/config/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import config from '../config/config'
 
 const stripe = new Stripe(config.stripeSecret!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-03-31.basil',
 })
 
 export default stripe
